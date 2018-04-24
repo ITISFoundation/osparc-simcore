302ddd4355c75d792feb54056db82718fb569fae
odeimaiz <maiz@zurichmedtech.com>
2018-04-24 09:32:05 +0200

    dummy login window added

--- services/web-client/source/class/qxapp/Application.js
+++ services/web-client/source/class/qxapp/Application.js
@@ -66,8 +66,39 @@ qx.Class.define("qxapp.Application", {
       this._socket = new qxapp.wrappers.WebSocket("app");
       this._socket.connect();
 
-      this._layoutManager = new qxapp.layout.LayoutManager();
-      doc.add(this._layoutManager);
+      let win = new qx.ui.window.Window("Login");
+      win.setLayout(new qx.ui.layout.VBox());
+
+      let form = new qx.ui.form.Form();
+      let userName = new qx.ui.form.TextField();
+      // userName.setRequired(true);
+      form.add(userName, "Name");
+      let password = new qx.ui.form.PasswordField();
+      // password.setRequired(true);
+      form.add(password, "Password");
+      let sendButton = new qx.ui.form.Button("Login");
+      let scope = this;
+      sendButton.addListener("execute", function() {
+        if (form.validate()) {
+          if (qxapp.utils.Utils.inHouse(form.getGroups()[0].items[1].getValue())) {
+            win.close();
+            this._layoutManager = new qxapp.layout.LayoutManager();
+            doc.add(this._layoutManager);
+          }
+        }
+      }, scope);
+      form.addButton(sendButton);
+
+      win.add(new qx.ui.form.renderer.Single(form));
+;
+      win.setShowMinimize(false);
+      win.setShowMaximize(false);
+      win.setShowClose(false);
+      win.open();
+      this.getRoot().add(win, {
+        left:400,
+        top:400
+      });
     }
   }
 });
