efea6b1edc89dce8f5be5e1675e9415f6e394632
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-04-25 16:33:22 +0200

    connect docker images through network in swarm

--- services/director/producer.py
+++ services/director/producer.py
@@ -3,6 +3,8 @@ import registry_proxy
 import json
 import docker
 
+SERVICE_RUNTIME_SETTINGS = 'simcore.service.settings'
+
 def IsServiceAWebServer(dockerImagePath):    
     return str(dockerImagePath).find('webserver') != -1
 
@@ -23,18 +25,24 @@ def check_service_uuid_available(dockerClient, service_uuid):
         raise Exception('A service with the same uuid is already running: ' + service_uuid)
 
 def get_service_runtime_parameters_labels(image, tag):
-    image_labels = registry_proxy.retrieve_labels_of_image(image, tag)
-    SERVICE_RUNTIME_SETTINGS = 'simcore.service.settings'
+    image_labels = registry_proxy.retrieve_labels_of_image(image, tag)    
     runtime_parameters = dict()
     if SERVICE_RUNTIME_SETTINGS in image_labels:
         runtime_parameters = json.loads(image_labels[SERVICE_RUNTIME_SETTINGS])
     return runtime_parameters
 
-def convert_labels_to_docker_runtime_parameters(service_runtime_parameters_labels):
+def convert_labels_to_docker_runtime_parameters(service_runtime_parameters_labels, service_uuid):
     runtime_params = dict()
     for param in service_runtime_parameters_labels:
         if 'name' not in param or 'type' not in param or 'value' not in param:
             pass
+        index = str(param['value']).find("%service_uuid%")
+        if  str(param['value']).find("%service_uuid%") != -1:
+            dummy_string = json.dumps(param['value'])
+            dummy_string = dummy_string.replace("%service_uuid%", service_uuid)
+            param['value'] = json.loads(dummy_string)
+            # replace string by actual value
+            #param['value'] = str(param['value']).replace("%service_uuid%", service_uuid)
 
         if param['name'] == 'ports':
             # special handling for we need to open a port with 0:XXX this tells the docker engine to allocate whatever free port
@@ -42,17 +50,23 @@ def convert_labels_to_docker_runtime_parameters(service_runtime_parameters_label
             runtime_params["endpoint_spec"] = enpoint_spec
         else:
             runtime_params[param['name']] = param['value']
-
-    
-    
     return runtime_params
 
-def add_uuid_label(docker_service_runtime_parameters, service_uuid):
+def add_uuid_label_to_service_runtime_params(docker_service_runtime_parameters, service_uuid):
     # add the service uuid to the docker service
     if "labels" in docker_service_runtime_parameters:
         docker_service_runtime_parameters["labels"]["uuid"] = service_uuid
     else:
         docker_service_runtime_parameters["labels"] = {"uuid":service_uuid}
+
+def add_network_to_service_runtime_params(docker_service_runtime_parameters, docker_network):
+    if "networks" in docker_service_runtime_parameters:
+        docker_service_runtime_parameters["networks"].append(docker_network.id)
+    else:
+        docker_service_runtime_parameters["networks"] = [docker_network.id]
+
+def set_service_name(docker_service_runtime_parameters, service_name, service_uuid):
+    docker_service_runtime_parameters["name"] = service_name + "_" + service_uuid
 
 def get_docker_image_published_ports(service_id):
     low_level_client = docker.APIClient()
@@ -67,13 +81,30 @@ def get_docker_image_published_ports(service_id):
                     published_ports.append(port['PublishedPort'])
     return published_ports
 
+def create_network_name(service_name, service_uuid):
+    return service_name + '_' + service_uuid
+
+def create_overlay_network_in_swarm(docker_client, service_name, service_uuid):
+    network_name = create_network_name(service_name, service_uuid)
+    try:
+        docker_network = docker_client.networks.create(network_name, driver="overlay", scope="swarm", labels={"uuid":service_uuid})
+        return docker_network
+    except docker.errors.APIError as e:
+        raise Exception('Docker server error while creating network: ' + str(e))
+
+def remove_overlay_network_of_swarm(docker_client, service_uuid):
+    try:
+        networks = docker_client.networks.list(filters={"label":"uuid=" + service_uuid})
+        # remove any network in the list (should be only one)
+        for network in networks:
+            network.remove()
+    except docker.errors.APIError as e:
+        raise Exception("docker server error while removing networks for service with uuid " + service_uuid)
+
 def start_service(service_name, service_tag, service_uuid):
-    # get the repos implied by the service name
-    listOfInteractiveServicesRepositories = registry_proxy.retrieve_list_of_repos_with_interactive_services()
     # find the ones containing the service name
-    listOfReposForService = []
-    [listOfReposForService.append(i) for i in listOfInteractiveServicesRepositories if registry_proxy.get_service_name(i) == service_name]
-    # get the available tags for each service
+    listOfReposForService = registry_proxy.retrieve_list_of_interactive_services_with_name(service_name)
+    # get the available image for each service (syntax is image:tag)
     listOfImages = {}
     for repo in listOfReposForService:
         listOfImages[repo] = registry_proxy.retrieve_list_of_images_in_repo(repo)
@@ -83,6 +114,9 @@ def start_service(service_name, service_tag, service_uuid):
     check_service_uuid_available(dockerClient, service_uuid)
     login_docker_registry(dockerClient)
     
+    # create a new network to connect the differnt containers
+    docker_network = create_overlay_network_in_swarm(dockerClient, service_name, service_uuid)
+    # create services
     containers_meta_data = list()
     for dockerImagePath in listOfImages:
         availableTagsList = sorted(listOfImages[dockerImagePath]['tags'])
@@ -95,9 +129,12 @@ def start_service(service_name, service_tag, service_uuid):
         
         dockerImageFullPath = os.environ.get('REGISTRY_URL') +'/' + dockerImagePath + ':' + tag
         
+        # prepare runtime parameters
         service_runtime_parameters_labels = get_service_runtime_parameters_labels(dockerImagePath, tag)
-        docker_service_runtime_parameters = convert_labels_to_docker_runtime_parameters(service_runtime_parameters_labels)
-        add_uuid_label(docker_service_runtime_parameters, service_uuid)
+        docker_service_runtime_parameters = convert_labels_to_docker_runtime_parameters(service_runtime_parameters_labels, service_uuid)
+        add_uuid_label_to_service_runtime_params(docker_service_runtime_parameters, service_uuid)        
+        add_network_to_service_runtime_params(docker_service_runtime_parameters, docker_network)
+        set_service_name(docker_service_runtime_parameters, registry_proxy.get_service_sub_name(dockerImagePath), service_uuid)
         # let-s start the service
         try:            
             service = dockerClient.services.create(dockerImageFullPath, **docker_service_runtime_parameters)
@@ -120,10 +157,10 @@ def stop_service(service_uuid):
     dockerClient = docker.from_env()
     login_docker_registry(dockerClient)
     
-    try:
+    try:        
         listOfRunningServicesWithUUID = dockerClient.services.list(filters={'label':'uuid=' + service_uuid})
         [service.remove() for service in listOfRunningServicesWithUUID]
-
+        remove_overlay_network_of_swarm(dockerClient, service_uuid)
     except docker.errors.APIError as e:
         raise Exception('Error while stopping container' + str(e))
     
--- services/director/registry_proxy.py
+++ services/director/registry_proxy.py
@@ -46,6 +46,21 @@ def retrieve_list_of_repos_with_interactive_services():
     list_of_interactive_repos = [repo for repo in listOfAllRepos if str(repo).startswith(INTERACTIVE_SERVICES_PREFIX)]
     return list_of_interactive_repos
 
+def retrieve_list_of_interactive_services_with_name(service_name):
+    listOfInteractiveServicesRepositories = retrieve_list_of_repos_with_interactive_services()
+    # find the ones containing the service name
+    listOfReposForService = []
+    [listOfReposForService.append(i) for i in listOfInteractiveServicesRepositories if get_service_name(i) == service_name]
+    return listOfReposForService
+
 def get_service_name(repository_name):
     service_name_suffixes = str(repository_name)[len(INTERACTIVE_SERVICES_PREFIX):]
     return service_name_suffixes.split('/')[0]
+
+def get_service_sub_name(repository_name):
+    service_name_suffixes = str(repository_name)[len(INTERACTIVE_SERVICES_PREFIX):]
+    list_of_suffixes = service_name_suffixes.split('/')
+    last_suffix_index = len(list_of_suffixes) - 1
+    if last_suffix_index < 0:
+        raise Exception('Invalid service name: ' + repository_name)
+    return list_of_suffixes[last_suffix_index]
