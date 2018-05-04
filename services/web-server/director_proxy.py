7184ed3f73a8c132efbe1ee9dfcf8c9e6341f7dd
Sylvain <35365065+sanderegg@users.noreply.github.com>
2018-05-04 15:48:21 +0200

    Auto switch off service when disconnecting (#53)
    * fixed automatic switch off of interactive services
    
    * fixed docker file/compose for development

--- services/web-server/director_proxy.py
+++ services/web-server/director_proxy.py
@@ -7,8 +7,8 @@ from requests import Session, RequestException
 session = Session()
 
 def director_request(path, method="GET", data=dict()):
-    api_url = os.environ.get('DIRECTOR_HOST', 'http://172.16.9.89') + ':' + os.environ.get('DIRECTOR_PORT', '8001') + '/' + path
-    try:        
+    api_url = os.environ.get('DIRECTOR_HOST', 'http://localhost') + ':' + os.environ.get('DIRECTOR_PORT', '8001') + '/' + path
+    try:
         if len(data) == 0:
             request_result = getattr(session, method.lower())(api_url)
         else:
@@ -27,26 +27,16 @@ def director_request(path, method="GET", data=dict()):
             logging.warning('return ok: %s', request_result.json())
             return request_result
     except RequestException as e:
-        raise Exception("Problem during connection to director")
+        raise Exception("Problem during connection to director" + str(e))
 
 def retrieve_interactive_services():
-    try:
-        request = director_request('list_interactive_services')
-        return request.json()
-    except Exception as e:
-        return 'failed: ' + str(e)
+  request = director_request('list_interactive_services')
+  return request.json()
 
 def start_service(service_name, service_uuid):
-    try:
-        request = director_request('start_service', method='POST', data={'service_name':service_name, 'service_uuid':str(service_uuid)})
-        return request.json()
-    except Exception as e:
-        return 'failed: ' + str(e)
+  request = director_request('start_service', method='POST', data={'service_name':service_name, 'service_uuid':str(service_uuid)})
+  return request.json()
 
 def stop_service(service_uuid):
-    try:
-        request = director_request('stop_service', method='POST', data={'service_uuid':str(service_uuid)})
-        return request.json()
-    except Exception as e:
-        return 'failed: ' + str(e)
-    
+  request = director_request('stop_service', method='POST', data={'service_uuid':str(service_uuid)})
+  return request.json()
