5ec225cac572a6f7dea3f45e6216a0e5ee5c9987
odeimaiz <maiz@zurichmedtech.com>
2018-05-07 15:08:27 +0200

    Project editor reviewed

--- services/web-client/source/class/qxapp/desktop/PrjEditor.js
+++ services/web-client/source/class/qxapp/desktop/PrjEditor.js
@@ -1,5 +1,3 @@
-/* global document */
-
 qx.Class.define("qxapp.desktop.PrjEditor", {
   extend: qx.ui.container.Composite,
 
@@ -11,68 +9,51 @@ qx.Class.define("qxapp.desktop.PrjEditor", {
     });
 
     // Create a horizontal split pane
-    this._Pane = new qx.ui.splitpane.Pane("horizontal");
+    this.__Pane = new qx.ui.splitpane.Pane("horizontal");
 
     const settingsWidth = 500;
-    this._SettingsView = new qxapp.components.workbench.SettingsView();
-    this._SettingsView.set({
+    this.__SettingsView = new qxapp.components.workbench.SettingsView();
+    this.__SettingsView.set({
       minWidth: settingsWidth*0.5,
       maxWidth: settingsWidth,
       width: settingsWidth*0.75
     });
-    this._Pane.add(this._SettingsView, 0);
+    this.__Pane.add(this.__SettingsView, 0);
 
-    this._Workbench = new qxapp.components.workbench.Workbench();
-    this._Pane.add(this._Workbench, 1);
+    this.__Workbench = new qxapp.components.workbench.Workbench();
+    this.__Pane.add(this.__Workbench, 1);
 
-    this.add(this._Pane, {
+    this.add(this.__Pane, {
       left: 0,
       top: 0,
       right: 0,
       bottom: 0
     });
 
-    this._showSettings(false);
+    this.__showSettings(false);
 
-    let scope = this;
-    this._SettingsView.addListener("SettingsEditionDone", function() {
-      scope._showSettings(false);
-    }, scope);
+    this.__SettingsView.addListener("SettingsEditionDone", function() {
+      this.__showSettings(false);
+    }, this);
 
-    this._Workbench.addListener("NodeDoubleClicked", function(e) {
-      scope._showSettings(true);
-      scope._SettingsView.setNodeMetadata(e.getData());
-    }, scope);
-
-    /*
-    window.addEventListener("resize", function() {
-      scope.set({
-        width: scope._getDocWidth(),
-        height: scope._getDocHeight()
-      });
-    }, scope);
-    */
+    this.__Workbench.addListener("NodeDoubleClicked", function(e) {
+      this.__showSettings(true);
+      this.__SettingsView.setNodeMetadata(e.getData());
+    }, this);
   },
 
   members: {
-    _Pane: null,
-    _SettingsView: null,
-    _Workbench: null,
+    __Pane: null,
+    __SettingsView: null,
+    __Workbench: null,
 
-    _getDocWidth: function() {
-      let body = document.body;
-      let html = document.documentElement;
-      let docWidth = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth);
-      return docWidth;
-    },
-
-    _showSettings: function(showSettings) {
+    __showSettings: function(showSettings) {
       if (showSettings) {
-        this._SettingsView.show();
-        this._Workbench.show();
+        this.__SettingsView.show();
+        this.__Workbench.show();
       } else {
-        this._SettingsView.exclude();
-        this._Workbench.show();
+        this.__SettingsView.exclude();
+        this.__Workbench.show();
       }
     }
   }
