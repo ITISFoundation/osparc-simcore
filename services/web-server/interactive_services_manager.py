9ba14b796ec1cfdbfa77c71b48f1d0e280ccbd38
Pedro Crespo-Valero <crespo@itis.ethz.ch>
2018-05-08 11:34:38 +0200

    Fixed pylint issues in interactive_*

--- services/web-server/interactive_services_manager.py
+++ services/web-server/interactive_services_manager.py
@@ -1,19 +1,23 @@
+""" Manages lifespan of interactive services.
+
+"""
 # pylint: disable=W0703
+# pylint: disable=C0111
 import director_proxy
 
-running_services = dict()
+__RUNNING_SERVICES = dict()
 
 
 def session_connect(session_id):
-    running_services[session_id] = list()
+    __RUNNING_SERVICES[session_id] = list()
 
 
 def session_disconnected(session_id):
     # let's stop all running interactive services
-    running_services_for_session = running_services[session_id]
+    running_services_for_session = __RUNNING_SERVICES[session_id]
     for service_session_uuid in running_services_for_session:
         director_proxy.stop_service(service_session_uuid)
-    running_services[session_id] = list()
+    __RUNNING_SERVICES[session_id] = list()
 
 
 def retrieve_list_of_services():
@@ -26,7 +30,7 @@ def retrieve_list_of_services():
 def start_service(session_id, service_name, service_uuid):
     try:
         result = director_proxy.start_service(service_name, service_uuid)
-        running_services[session_id].append(service_uuid)
+        __RUNNING_SERVICES[session_id].append(service_uuid)
         return result
     except Exception as err:
         return "Failed starting service " + service_name + ": " + str(err)
@@ -35,7 +39,7 @@ def start_service(session_id, service_name, service_uuid):
 def stop_service(session_id, service_uuid):
     try:
         result = director_proxy.stop_service(service_uuid)
-        running_services[session_id].remove(service_uuid)
+        __RUNNING_SERVICES[session_id].remove(service_uuid)
         return result
     except Exception as err:
         return "Failed stopping service " + str(err)
