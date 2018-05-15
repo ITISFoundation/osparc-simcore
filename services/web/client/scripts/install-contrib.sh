afd472df18ec32e36af7a46522bc389e8de8454f
Tobias Oetiker <tobi@itis.ethz.ch>
2018-05-15 13:36:11 +0200

    some additional fixes

--- services/web/client/scripts/install-contrib.sh
+++ services/web/client/scripts/install-contrib.sh
@@ -25,8 +25,8 @@ mkdir -p ${FONTS_DIR}
 pushd ${FONTS_DIR};
 
 # FIXME: set proper contrib version automatically!
-ln -s ../../../contrib/ITISFoundation_qx-iconfont-fontawesome5_v0.0.1/source/resource/iconfont/fontawesome5/ fontawesome5
-ln -s ../../../contrib/ITISFoundation_qx-iconfont-material_v0.0.0/source/resource/iconfont/material/ material
+ln -s ../../../contrib/ITISFoundation_qx-iconfont-fontawesome5_v0.0.2/source/resource/iconfont/fontawesome5/ fontawesome5
+ln -s ../../../contrib/ITISFoundation_qx-iconfont-material_v0.0.1/source/resource/iconfont/material/ material
 ls -l
 
 popd;
