e87455fa62cf7e278c876a58757302c03417963a
Tobias Oetiker <tobi@oetiker.ch>
2018-05-15 17:15:46 +0200

    Update install-contrib.sh

--- services/web/client/scripts/install-contrib.sh
+++ services/web/client/scripts/install-contrib.sh
@@ -25,7 +25,7 @@ mkdir -p ${FONTS_DIR}
 cd ${FONTS_DIR}
 
 
-# FIXME: set proper contrib version automatically!
+# TODO: with the next release of qx-compiler these lines can be removed
 rm *
 ln -s ../../../contrib/ITISFoundation_qx-iconfont-fontawesome5_v0.0.2/source/resource/iconfont/fontawesome5/ fontawesome5
 ln -s ../../../contrib/ITISFoundation_qx-iconfont-material_v0.0.1/source/resource/iconfont/material/ material
