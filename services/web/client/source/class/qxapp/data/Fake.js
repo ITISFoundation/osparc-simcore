f746cbfde0adecbcfc29a246df9d49124c341608
odeimaiz <maiz@zurichmedtech.com>
2018-05-18 15:06:08 +0200

    Available services do not need an uuid in the ports

--- services/web/client/source/class/qxapp/data/Fake.js
+++ services/web/client/source/class/qxapp/data/Fake.js
@@ -389,7 +389,6 @@ qx.Class.define("qxapp.data.Fake", {
         "name": "Modeler",
         "inputs": [],
         "outputs": [{
-          "uuid": "772337ea-cc39-4117-a60d-ec5d2820355e",
           "name": "Scene",
           "type": "scene",
           "value": ""
@@ -413,7 +412,6 @@ qx.Class.define("qxapp.data.Fake", {
         "name": "File Reader",
         "inputs": [],
         "outputs": [{
-          "uuid": "aca2065e-d3a1-4cdb-89c2-cbffab8d8cc9",
           "name": "File-link",
           "type": "file-link",
           "value": ""
@@ -434,7 +432,6 @@ qx.Class.define("qxapp.data.Fake", {
         "name": "Number Generator",
         "inputs": [],
         "outputs": [{
-          "uuid": "22cb66af-5315-4eef-bf7a-deebc57c0e8d",
           "name": "Number",
           "type": "number",
           "value": ""
@@ -454,23 +451,19 @@ qx.Class.define("qxapp.data.Fake", {
         "id": "ColleenClancy",
         "name": "Colleen Clancy - dummy",
         "inputs": [{
-          "uuid": "437f156d-c243-4a85-9b96-59d180141601",
           "name": "File-link",
           "type": "file-link",
           "value": ""
         }, {
-          "uuid": "de3529f1-aa5b-4cb3-8257-e84b6b0eebb5",
           "name": "File-link",
           "type": "file-link",
           "value": ""
         }],
         "outputs": [{
-          "uuid": "4f07978b-9c24-4756-b9e0-7655146c70c2",
           "name": "outputFolder",
           "type": "folder",
           "value": "url"
         }, {
-          "uuid": "9b77dc5d-0a79-4f95-8941-5005b66280f3",
           "name": "Allresults",
           "order": [
             "t",
@@ -537,13 +530,11 @@ qx.Class.define("qxapp.data.Fake", {
         "id": "Computational2",
         "name": "Computational 2",
         "inputs": [{
-          "uuid": "83d9c13a-6ddd-4de1-a949-ff92c4570627",
           "name": "Scene",
           "type": "scene",
           "value": ""
         }],
         "outputs": [{
-          "uuid": "17cf124a-c020-41ea-8a87-6d21221d2de8",
           "name": "Other numbers",
           "type": "number",
           "value": ""
@@ -553,13 +544,11 @@ qx.Class.define("qxapp.data.Fake", {
         "id": "Computational3",
         "name": "Computational 3",
         "inputs": [{
-          "uuid": "3774b462-f112-4b95-bb38-12d20386ccbc",
           "name": "Number",
           "type": "number",
           "value": ""
         }],
         "outputs": [{
-          "uuid": "f29bd50d-4744-4519-940f-81ced733d497",
           "name": "Some numbers",
           "type": "number",
           "value": ""
@@ -569,13 +558,11 @@ qx.Class.define("qxapp.data.Fake", {
         "id": "Computational4",
         "name": "Computational 4",
         "inputs": [{
-          "uuid": "e003029a-fc04-42bc-80cc-6d97a54a399a",
           "name": "Number",
           "type": "number",
           "value": ""
         }],
         "outputs": [{
-          "uuid": "b5d576f1-6cb0-4fd9-ad9c-d0c0a3234474",
           "name": "Other numbers",
           "type": "number",
           "value": ""
@@ -590,7 +577,6 @@ qx.Class.define("qxapp.data.Fake", {
         "id": "jupyter-base-notebook",
         "name": "Jupyter",
         "inputs": [{
-          "uuid": "47f9aa28-6536-4ca2-9a88-89047635475c",
           "name": "Number",
           "type": "number",
           "value": ""
@@ -605,7 +591,6 @@ qx.Class.define("qxapp.data.Fake", {
         "id": "Analysis2",
         "name": "Analysis 2",
         "inputs": [{
-          "uuid": "a774384c-417b-4aed-9868-f90bd0371229",
           "name": "Number",
           "type": "number",
           "value": ""
