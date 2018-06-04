304bdafc4af0d859629d7f7cf0dc2321e61d14c1
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-06-04 09:10:57 +0200

    refactoring

--- services/dy-2Dgraph/simcore_api/__init__.py
+++ services/dy-2Dgraph/simcore_api/__init__.py
@@ -2,9 +2,9 @@
 #pylint: disable=C0103
 import logging
 import os
-import simcore_api.config
-import simcore_api.simcore
+from simcore_api import simcore 
+from simcore_api import config
 
-CONFIG = simcore_api.config.CONFIG[os.environ.get("SIMCORE_API_CONFIG", "default")]
+CONFIG = config.CONFIG[os.environ.get("SIMCORE_API_CONFIG", "default")]
 # create initial Simcore object
-simcore = simcore_api.simcore.Simcore.create_from_json(CONFIG.get_ports_configuration)
+simcore = simcore.Simcore.create_from_json(CONFIG.get_ports_configuration)
