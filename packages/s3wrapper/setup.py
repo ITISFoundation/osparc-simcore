ffe06ff2a82ea6530419f48906de8786e0c0fe41
Manuel Guidon <guidon@itis.ethz.ch>
2018-06-12 18:18:14 +0200

    Rename modules to packages and change folder organization

---
+++ packages/s3wrapper/setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='s3wrapper',
+    version='0.1.0',
+)
---
+++ packages/simcore-sdk/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+
+setup(
+    name='simcore-sdk',
+    version='0.1.0',
+)
