a208e8e20056a94602f3c176bd3d60f4da72a4ad
Pedro Crespo <crespo@itis.ethz.ch>
2018-06-19 14:09:11 +0200

    If fails to create venv, then installs in system python. Tested in win computer.

--- scripts/win-watcher.bat
+++ scripts/win-watcher.bat
@@ -2,17 +2,25 @@
 :: Run this script to watch changes in files on a windows host.
 :: This provides a workaround to overcome a known limitation of docker for windows [1]
 ::
-:: REQUIRES python installed and in the system path
+:: REQUIRES python 3 installed and in the system path
 ::
 :: [1] https://docs.docker.com/docker-for-windows/troubleshoot/#/inotify-on-shared-drives-does-not-work
 :: [2] http://blog.subjectify.us/miscellaneous/2017/04/24/docker-for-windows-watch-bindings.html
 ::
 
-:: installs tool in a venv
-python -m venv .VENV
-.VENV/bin/pip install --upgrade pip
-.VENV/bin/pip install docker-windows-volume-watcher
+python --version
 
-:: runs watcher
-.VENV/bin/activate
+:: installs tool in a venv (expects python 3)
+python -m venv .VENV
+
+if %ERRORLEVEL% EQU 0 (
+	.VENV/bin/pip install --upgrade pip
+	.VENV/bin/pip install docker-windows-volume-watcher	
+	:: runs watcher
+	.VENV/bin/activate
+) else (
+	python -m pip install --upgrade pip
+	python -m pip install docker-windows-volume-watcher
+)
+
 docker-volume-watcher
