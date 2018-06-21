63d77565f2688a4faedbfc5c29e660666c88da51
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-06-22 00:10:25 +0200

    cleanup

--- packages/simcore-sdk/tests/conftest.py
+++ packages/simcore-sdk/tests/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 import os
-import sys
 # pylint:disable=unused-argument
 
 @pytest.fixture(scope='session')
@@ -8,6 +7,6 @@ def docker_compose_file(pytestconfig):
     my_path = os.path.join(os.path.dirname(__file__), 'docker-compose.yml')
     return my_path
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "helpers"))
+
 
 
