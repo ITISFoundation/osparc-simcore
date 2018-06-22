d81b1a8751e8331df411aa08f2838cb5a9562079
odeimaiz <maiz@zurichmedtech.com>
2018-06-22 17:46:15 +0200

    minor

--- services/web/client/source/class/qxapp/components/workbench/servicesCatalogue/SearchTypeFilter.js
+++ services/web/client/source/class/qxapp/components/workbench/servicesCatalogue/SearchTypeFilter.js
@@ -9,7 +9,7 @@ qx.Class.define("qxapp.components.workbench.servicesCatalogue.SearchTypeFilter",
     // apply the filter funtion on creation time because the 'this' context
     // needs to be bound to the function
     this.filter = qx.lang.Function.bind(function(data) {
-      if (data === null) {
+      if (data === undefined || data === null) {
         console.log("Service with no data");
         return false;
       }
