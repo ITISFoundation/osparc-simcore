33a4669ba9c3d4c4695e867092f2822815ca0711
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-06-22 10:18:35 +0200

    fix travis

--- packages/simcore-sdk/tests/nodeports/helpers/helpers.py
+++ packages/simcore-sdk/tests/nodeports/helpers/helpers.py
@@ -1,13 +1,16 @@
 #pylint: disable=C0111
 
-
+from sqlalchemy.orm.attributes import flag_modified
 from simcore_sdk.models.pipeline_models import ComputationalTask
 
 def update_configuration(session, pipeline_id, node_uuid, new_configuration):
     task = session.query(ComputationalTask).filter(ComputationalTask.pipeline_id==str(pipeline_id), ComputationalTask.node_id==str(node_uuid)).one()
     task.input = new_configuration["inputs"]
+    flag_modified(task, "input")
     task.output = new_configuration["outputs"]
+    flag_modified(task, "output")
     session.commit()
+    
 
 def update_config_file(path, config):
     import json
