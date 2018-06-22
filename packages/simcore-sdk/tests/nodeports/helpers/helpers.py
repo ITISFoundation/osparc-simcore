8db809c639686d77414ca2e69b40a1733f7a38b4
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-06-22 09:20:03 +0200

    added function to update db configuration

--- packages/simcore-sdk/tests/nodeports/helpers/helpers.py
+++ packages/simcore-sdk/tests/nodeports/helpers/helpers.py
@@ -1,8 +1,19 @@
 #pylint: disable=C0111
+
+
+from simcore_sdk.models.pipeline_models import ComputationalTask
+
+def update_configuration(session, pipeline_id, node_uuid, new_configuration):
+    task = session.query(ComputationalTask).filter(ComputationalTask.pipeline_id==str(pipeline_id), ComputationalTask.node_id==str(node_uuid)).one()
+    task.input = new_configuration["inputs"]
+    task.output = new_configuration["outputs"]
+    session.commit()
+
 def update_config_file(path, config):
     import json
     with open(path, "w") as json_file:
         json.dump(config, json_file)
+
 
 def get_empty_config():
     return {
