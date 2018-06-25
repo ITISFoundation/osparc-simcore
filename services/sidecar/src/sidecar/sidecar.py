3148054729a1a44875a05b6788510add1cdd86e9
Manuel Guidon <33161876+mguidon@users.noreply.github.com>
2018-06-25 18:10:43 +0200

    Clancy workflow (#122)
    * Support for workflow for all 3 ucdavis use-cases

--- services/sidecar/src/sidecar/sidecar.py
+++ services/sidecar/src/sidecar/sidecar.py
@@ -64,7 +64,10 @@ class Sidecar(object):
         if isinstance(port_value, str) and port_value.startswith("link."):
             if port['type'] == 'file-url':
                 _LOGGER.debug('Fetch S3 %s', port_value)
-                object_name = os.path.join(str(self._task.pipeline_id),*port_value.split(".")[1:])
+                #parse the link assuming it is link.id.file.ending
+                _parts = port_value.split(".")
+                object_name = os.path.join(str(self._task.pipeline_id), _parts[1], ".".join(_parts[2:]))
+                #object_name = os.path.join(str(self._task.pipeline_id),*port_value.split(".")[1:])
                 input_file = os.path.join(self._executor.in_dir, port_name)
                 _LOGGER.debug('Downloading from  S3 %s/%s', self._s3.bucket, object_name)
                 success = False
@@ -156,10 +159,12 @@ class Sidecar(object):
                     if clean_line.lower().startswith("[progress]"):
                         progress = clean_line.lower().lstrip("[progress]").rstrip("%").strip()
                         prog_data = {"Channel" : "Progress", "Node": task.node_id, "Progress" : progress}
+                        _LOGGER.debug('PROGRESS %s', progress)
                         prog_body = json.dumps(prog_data)
                         channel.basic_publish(exchange=self._pika.progress_channel, routing_key='', body=prog_body)
                     else:
                         log_data = {"Channel" : "Log", "Node": task.node_id, "Message" : clean_line}
+                        _LOGGER.debug('LOG %s', clean_line)
                         log_body = json.dumps(log_data)
                         channel.basic_publish(exchange=self._pika.log_channel, routing_key='', body=log_body)
 
@@ -236,6 +241,10 @@ class Sidecar(object):
                            "OUTPUT_FOLDER=" + self._executor.out_dir,
                            "LOG_FOLDER=" + self._executor.log_dir]
 
+        # self._docker.env = ["INPUT_FOLDER=/input",
+        #                    "OUTPUT_FOLDER=/output",
+        #                    "LOG_FOLDER=/log"]
+
 
     def preprocess(self):
         _LOGGER.debug('Pre-Processing Pipeline %s and node %s from container', self._task.pipeline_id, self._task.internal_id)
@@ -278,6 +287,7 @@ class Sidecar(object):
         _LOGGER.debug('DONE Processing Pipeline %s and node %s from container', self._task.pipeline_id, self._task.internal_id)
 
     def run(self):
+        _LOGGER.debug("ENTERING run")
         self.preprocess()
         self.process()
         self.postprocess()
@@ -314,6 +324,8 @@ class Sidecar(object):
         return True
 
     def inspect(self, celery_task, pipeline_id, node_id):
+        _LOGGER.debug("ENTERING inspect pipeline:node %s: %s", pipeline_id, node_id)
+
         _pipeline = self._db.session.query(ComputationalPipeline).filter_by(pipeline_id=pipeline_id).one()
         graph = _pipeline.execution_graph
         next_task_nodes = []
@@ -365,7 +377,11 @@ class Sidecar(object):
 
             next_task_nodes = list(graph.successors(node_id))
         else:
+            _LOGGER.debug("NODE id was zero")
+            _LOGGER.debug("graph looks like this %s", graph)
+
             next_task_nodes = find_entry_point(graph)
+            _LOGGER.debug("Next task nodes %s", next_task_nodes)
 
         celery_task.update_state(state=CSUCCESS)
 
@@ -374,6 +390,7 @@ class Sidecar(object):
 SIDECAR = Sidecar()
 @celery.task(name='comp.task', bind=True)
 def pipeline(self, pipeline_id, node_id=None):
+    _LOGGER.debug("ENTERING run")
     next_task_nodes = SIDECAR.inspect(self, pipeline_id, node_id)
     for _node_id in next_task_nodes:
         _task = celery.send_task('comp.task', args=(pipeline_id, _node_id), kwargs={})
