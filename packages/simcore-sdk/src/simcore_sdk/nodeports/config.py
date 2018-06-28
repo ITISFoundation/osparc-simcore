67b03e873ab82fba17443225bcb1cfd1dcec7e88
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-06-28 21:35:12 +0200

    added link def in configuration

--- packages/simcore-sdk/src/simcore_sdk/nodeports/config.py
+++ packages/simcore-sdk/src/simcore_sdk/nodeports/config.py
@@ -22,6 +22,8 @@ TYPE_TO_PYTHON_TYPE_MAP = {"int":int,
 TYPE_TO_S3_FILE_LIST = ["file-url"]
 TYPE_TO_S3_FOLDER_LIST = ["folder-url"]
 
+LINK_PREFIX = "link."
+
 # nodeports is a library for accessing data linked to the node
 # in that sense it should not log stuff unless the application code wants it to be so.
 logging.getLogger(__name__).addHandler(logging.NullHandler())
