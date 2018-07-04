9181e7d4849b1853c0dedfc198f3dbe323951603
Sylvain <35365065+sanderegg@users.noreply.github.com>
2018-07-04 19:30:55 +0200

    Nodeports allow update to s3 (#150)
    - nodeports now able to uplad to s3 a file or a folder
    - added minio-based unit tests with py-docker
    - code cleanup

--- packages/simcore-sdk/tests/nodeports/test_nodeports.py
+++ packages/simcore-sdk/tests/nodeports/test_nodeports.py
@@ -2,6 +2,8 @@
 #pylint: disable=W0212
 #pylint: disable=C0111
 import pytest
+from pathlib import Path
+from simcore_sdk.nodeports import config as node_config
 
 def test_access_with_key(default_nodeports_configuration): # pylint: disable=W0613, W0621
     from simcore_sdk.nodeports.nodeports import PORTS
@@ -10,31 +12,127 @@ def test_access_with_key(default_nodeports_configuration): # pylint: disable=W06
     assert PORTS.inputs["in_5"] == PORTS.inputs[1]
     assert PORTS.outputs["out_1"] == PORTS.outputs[0]
 
-def test_port_value_getters(default_nodeports_configuration): # pylint: disable=W0613, W0621
-    from simcore_sdk.nodeports.nodeports import PORTS
-
-    assert PORTS.inputs["in_1"].get() == "/home/jovyan/data/outputControllerOut.dat"
-    assert PORTS.inputs["in_5"].get() == 666
-    assert PORTS.outputs["out_1"].get() is None
-
-def test_port_value_setters(special_nodeports_configuration): # pylint: disable=W0613, W0621
+@pytest.mark.parametrize("item_type, item_value", [
+    ("integer", 26),
+    ("integer", 0),
+    ("integer", -52),
+    ("number", -746.4748),
+    ("number", 0.0),
+    ("number", 4566.11235),
+    ("bool", False),    
+    ("bool", True),
+    ("string", "test-string"),
+    ("string", ""),
+])
+def test_port_value_accessors_no_s3(special_nodeports_configuration, item_type, item_value): # pylint: disable=W0613, W0621
     import helpers
     special_config = helpers.get_empty_config() #pylint: disable=E1101
     special_config["outputs"].append({
         "key": "out_15",
         "label": "additional data",
         "desc": "here some additional data",
-        "type": "int",
+        "type": item_type,
         "value": "null",
         "timestamp": "2018-05-22T19:34:53.511Z"
     })
     special_nodeports_configuration(special_config)
     from simcore_sdk.nodeports.nodeports import PORTS
-
     assert PORTS.outputs["out_15"].get() is None
 
-    PORTS.outputs["out_15"].set(26)
-    assert PORTS.outputs["out_15"].get() == 26
+    PORTS.outputs["out_15"].set(item_value)
+    assert PORTS.outputs["out_15"].value == str(item_value)
+    converted_value = PORTS.outputs["out_15"].get()
+    assert isinstance(converted_value, node_config.TYPE_TO_PYTHON_TYPE_MAP[item_type]["type"])
+    assert converted_value == item_value
+
+@pytest.mark.parametrize("item_type, item_value", [
+    ("file-url", __file__),
+    ("folder-url", str(Path(__file__).parent))
+])
+def test_port_value_accessors_s3(special_nodeports_configuration, bucket, item_type, item_value): # pylint: disable=W0613, W0621
+    import helpers
+    import os
+    import tempfile
+    special_config = helpers.get_empty_config() #pylint: disable=E1101
+    item_key = "out_blah"
+    special_config["outputs"].append({
+        "key": item_key,
+        "label": "additional data",
+        "desc": "here some additional data",
+        "type": item_type,
+        "value": "null",
+        "timestamp": "2018-05-22T19:34:53.511Z"
+    })
+    special_nodeports_configuration(special_config)
+    from simcore_sdk.nodeports.nodeports import PORTS
+    assert PORTS.outputs[item_key].get() is None # check emptyness
+
+    # this triggers an upload to S3 + configuration change
+    PORTS.outputs[item_key].set(item_value)
+    # this is the link to S3 storage
+    assert PORTS.outputs[item_key].value == ".".join(["link", os.environ["SIMCORE_NODE_UUID"], item_key])
+    # this triggers a download from S3 to a location in /tempdir/simcorefiles/item_key or /tempdir/simcorefiles/item_key/item_key.simcore
+    converted_value = PORTS.outputs[item_key].get()
+    assert isinstance(converted_value, node_config.TYPE_TO_PYTHON_TYPE_MAP[item_type]["type"])
+
+    assert Path(converted_value).exists()
+    converted_value_to_check_for = str(Path(tempfile.gettempdir(), "simcorefiles", item_key))
+    assert PORTS.outputs[item_key].get().startswith(converted_value_to_check_for)
+
+def test_file_integrity(special_nodeports_configuration, bucket): # pylint: disable=W0613, W0621
+    import helpers
+    special_config = helpers.get_empty_config() #pylint: disable=E1101
+    item_key = "out_blah"
+    special_config["outputs"].append({
+        "key": item_key,
+        "label": "additional data",
+        "desc": "here some additional data",
+        "type": "file-url",
+        "value": "null",
+        "timestamp": "2018-05-22T19:34:53.511Z"
+    })
+    special_nodeports_configuration(special_config)
+    from simcore_sdk.nodeports.nodeports import PORTS
+    assert PORTS.outputs[item_key].get() is None # check emptyness
+
+    # this triggers an upload to S3 + configuration change
+    PORTS.outputs[item_key].set(__file__)
+
+    downloaded_file_path = PORTS.outputs[item_key].get()
+    import filecmp
+    filecmp.clear_cache()
+    assert filecmp.cmp(__file__, downloaded_file_path, shallow=False)
+
+def test_folder_integrity(special_nodeports_configuration, bucket): # pylint: disable=W0613, W0621
+    import helpers
+    special_config = helpers.get_empty_config() #pylint: disable=E1101
+    item_key = "out_blah"
+    special_config["outputs"].append({
+        "key": item_key,
+        "label": "additional data",
+        "desc": "here some additional data",
+        "type": "folder-url",
+        "value": "null",
+        "timestamp": "2018-05-22T19:34:53.511Z"
+    })
+    special_nodeports_configuration(special_config)
+    from simcore_sdk.nodeports.nodeports import PORTS
+    assert PORTS.outputs[item_key].get() is None # check emptyness
+
+    # this triggers an upload to S3 + configuration change
+    original_path = str(Path(__file__).parent)
+    PORTS.outputs[item_key].set(original_path)
+    downloaded_folder_path = PORTS.outputs[item_key].get()
+
+    original_files = [f for f in Path(original_path).glob("*") if f.is_file()]
+    downloaded_files = list(Path(downloaded_folder_path).glob("*"))
+
+    assert len(original_files) == len(downloaded_files)
+
+    import filecmp
+    for i in range(len(original_files)):
+        assert filecmp.cmp(original_files[i], downloaded_files[i])
+    
 
 @pytest.mark.skip(reason="SAN: this does not pass on travis but does on my workstation")
 def test_adding_new_ports(special_nodeports_configuration):
@@ -51,7 +149,7 @@ def test_adding_new_ports(special_nodeports_configuration):
         "key": "in_15",
         "label": "additional data",
         "desc": "here some additional data",
-        "type": "int",
+        "type": "integer",
         "value": "15",
         "timestamp": "2018-05-22T19:34:53.511Z"
     })
@@ -61,7 +159,7 @@ def test_adding_new_ports(special_nodeports_configuration):
     assert PORTS.inputs[0].key == "in_15"
     assert PORTS.inputs[0].label == "additional data"
     assert PORTS.inputs[0].desc == "here some additional data"
-    assert PORTS.inputs[0].type == "int"
+    assert PORTS.inputs[0].type == "integer"
     assert PORTS.inputs[0].value == "15"
     assert PORTS.inputs[0].timestamp == "2018-05-22T19:34:53.511Z"
 
@@ -81,7 +179,7 @@ def test_adding_new_ports(special_nodeports_configuration):
     assert PORTS.inputs[0].key == "in_15"
     assert PORTS.inputs[0].label == "additional data"
     assert PORTS.inputs[0].desc == "here some additional data"
-    assert PORTS.inputs[0].type == "int"
+    assert PORTS.inputs[0].type == "integer"
     assert PORTS.inputs[0].value == "15"
     assert PORTS.inputs[0].timestamp == "2018-05-22T19:34:53.511Z"
     # # new output
@@ -102,7 +200,7 @@ def test_removing_ports(special_nodeports_configuration):
         "key": "in_15",
         "label": "additional data",
         "desc": "here some additional data",
-        "type": "int",
+        "type": "integer",
         "value": "15",
         "timestamp": "2018-05-22T19:34:53.511Z"
     })
@@ -110,7 +208,7 @@ def test_removing_ports(special_nodeports_configuration):
         "key": "in_17",
         "label": "additional data",
         "desc": "here some additional data",
-        "type": "int",
+        "type": "integer",
         "value": "15",
         "timestamp": "2018-05-22T19:34:53.511Z"
     })
@@ -118,7 +216,7 @@ def test_removing_ports(special_nodeports_configuration):
         "key": "out_15",
         "label": "additional data",
         "desc": "here some additional data",
-        "type": "int",
+        "type": "integer",
         "value": "15",
         "timestamp": "2018-05-22T19:34:53.511Z"
     })
@@ -126,7 +224,7 @@ def test_removing_ports(special_nodeports_configuration):
         "key": "out_17",
         "label": "additional data",
         "desc": "here some additional data",
-        "type": "int",
+        "type": "integer",
         "value": "15",
         "timestamp": "2018-05-22T19:34:53.511Z"
     })
@@ -144,7 +242,7 @@ def test_removing_ports(special_nodeports_configuration):
     assert PORTS.inputs[0].key == "in_17"
     assert PORTS.inputs[0].label == "additional data"
     assert PORTS.inputs[0].desc == "here some additional data"
-    assert PORTS.inputs[0].type == "int"
+    assert PORTS.inputs[0].type == "integer"
     assert PORTS.inputs[0].value == "15"
     assert PORTS.inputs[0].timestamp == "2018-05-22T19:34:53.511Z"
 
@@ -157,7 +255,7 @@ def test_removing_ports(special_nodeports_configuration):
     assert PORTS.outputs[0].key == "out_15"
     assert PORTS.outputs[0].label == "additional data"
     assert PORTS.outputs[0].desc == "here some additional data"
-    assert PORTS.outputs[0].type == "int"
+    assert PORTS.outputs[0].type == "integer"
     assert PORTS.outputs[0].value == "15"
     assert PORTS.outputs[0].timestamp == "2018-05-22T19:34:53.511Z"
 
@@ -175,7 +273,7 @@ def test_changing_inputs_error(default_nodeports_configuration): # pylint: disab
     new_input = DataItem(key="dummy_1", 
                          label="new label", 
                          desc="new description", 
-                         type="int", 
+                         type="integer", 
                          value="233", 
                          timestamp="2018-06-04T09:46:43:343")
     with pytest.raises(exceptions.ReadOnlyError, message="Expecting ReadOnlyError") as excinfo:
@@ -196,7 +294,7 @@ def test_changing_outputs_error(default_nodeports_configuration): # pylint: disa
     new_output = DataItem(key="dummy_1", 
                           label="new label", 
                           desc="new description", 
-                          type="int", 
+                          type="integer", 
                           value="233", 
                           timestamp="2018-06-04T09:46:43:343")
      
