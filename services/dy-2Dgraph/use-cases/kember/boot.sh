ec9418e8a3497dfc0c36a6a4e6dc2fb88243b27c
Sylvain <35365065+sanderegg@users.noreply.github.com>
2018-07-06 15:23:54 +0200

    #5 kember postprocessing (#157)
    * added link def in configuration
    
    * removed int from available types.
    changed float to number
    
    * refactoring get value
    
    * changed type from int to integer
    
    * added exception for invalid item type error
    added exception for s3 transfer error
    
    * typo
    
    * nodeports now able to upload files and folders to S3
    
    * pylint
    
    * Makefile usable from Windows bash
    
    * renamed methods
    
    * fixed type
    
    * fixed tests after renaming int to integer
    
    * added default values for SIMCORE_NODE_UUID and SIMCORE_PIPELINE_ID
    
    * moved environment variable inside init function
    
    * typo
    
    * added minio as a fixture
    
    * added unit tests that use minio
    
    * removed empty file
    
    * renamed minio fixture, pylint
    
    * fixed issue with converting booleans
    added test with setters
    
    * fixed conversions
    added test for folderûrl
    
    * pylint
    
    * moved getting env variable in init function
    
    * moved file to test folder
    moved json encoding/decoding of db configuration in io module
    removed unnecessary test_postgres module
    
    * simplifications
    
    * renamed io module to dbmanager
    
    * removed unnecessary test
    
    * get the ip directly from docker
    
    * moved creation of env variables to a better location
    use docker API to get the IP and port
    
    * skip 2 tests that do not run on travis
    
    * added Dockerfile for kember postpro case
    extended docker-compose accordingly
    
    * completed all graphs
    
    * create dummy file
    
    * fixed figure 7 and partly 6
    
    * completed figure 1 with annotations
    
    * all plots but figure 5 missing now
    
    * autorun notebook
    completeted heatmap code
    
    * added button to show/hide raw code
    added auto slicing of data with a max display of 50k points for now
    all graphs are in now
    
    * code cleanup
    
    * added Makefile, docker-compose for jupyter notebook solo
    
    * makefile similar to root makefile
    
    * fix development dummy file
    
    * use jupyter notebook 1.3
    
    * make pylint happy
    
    * fixed JS
    added hide_input
    
    * sort lines according to review @manuel

---
+++ services/dy-2Dgraph/use-cases/kember/boot.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if test "${CREATE_DUMMY_TABLE}" = "1"
+then
+    echo "Creating dummy tables ... using ${USE_CASE_CONFIG_FILE}"
+    result="$(python develdbs3init.py ${USE_CASE_CONFIG_FILE})"
+    echo "Received result node uuid of $result"
+    export SIMCORE_NODE_UUID="$result"
+fi
+jupyter trust ${NOTEBOOK_URL}
+start-notebook.sh \
+    --NotebookApp.token='' \
+    --NotebookApp.tornado_settings="{\"headers\":{\"Content-Security-Policy\":\"frame-ancestors+'self'+http://osparc01.speag.com:9081;+report-uri/api/security/csp-report\"}}" \
+    --NotebookApp.default_url=/notebooks/${NOTEBOOK_URL}
