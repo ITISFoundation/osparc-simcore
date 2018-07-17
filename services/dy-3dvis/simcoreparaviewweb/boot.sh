a971adfd662a8b1d71b7e2e5e4298b69c5eb42e9
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-07-17 08:45:20 +0200

    adding docker-compose development adapted makefiles fixed LF endings

--- services/dy-3dvis/simcoreparaviewweb/boot.sh
+++ services/dy-3dvis/simcoreparaviewweb/boot.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+echo "current directory is ${PWD}"
 
-python3 input-retriever.py
-./startup.sh
+if [-z "${CREATE_DUMMY_TABLE+x}"]
+then
+    echo "development mode, creating dummy tables"
+    result="$(python develdbs3init.py ${USE_CASE_CONFIG_FILE})"
+    echo "Received result node uuid of $result"
+    export SIMCORE_NODE_UUID="$result"
+fi
+
+
+python3 "input-retriever.py"
+startup.sh
