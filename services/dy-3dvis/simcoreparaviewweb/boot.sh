fc6a3976d9a1a7e7bde2ab7ed47f841c63a976e8
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-07-17 11:36:55 +0200

    development docker/makefile in place

--- services/dy-3dvis/simcoreparaviewweb/boot.sh
+++ services/dy-3dvis/simcoreparaviewweb/boot.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "current directory is ${PWD}"
 
-if [-z "${CREATE_DUMMY_TABLE+x}"]
+if [[ -v CREATE_DUMMY_TABLE ]];
 then
-    echo "development mode, creating dummy tables"
-    result="$(python develdbs3init.py ${USE_CASE_CONFIG_FILE})"
-    echo "Received result node uuid of $result"
-    export SIMCORE_NODE_UUID="$result"
+    echo "development mode, creating dummy tables..."
+    result="$(python3 devel-initconfiguration.py ${USE_CASE_CONFIG_FILE})";
+    echo "Received result node uuid of $result";
+    export SIMCORE_NODE_UUID="$result";
 fi
 
 
-python3 "input-retriever.py"
-startup.sh
+python3 "input-retriever.py";
+./startup.sh;
--- services/dy-3dvis/simcoreparaviewweb/input-retriever.py
+++ services/dy-3dvis/simcoreparaviewweb/input-retriever.py
@@ -1,18 +1,22 @@
 from simcore_sdk.nodeports.nodeports import PORTS
-import pathlib
+from pathlib import Path
 import shutil
 import logging
 
 _LOGGER = logging.getLogger(__name__)
 
-INPUT_PATH = pathlib.Path("/home/scu/inputs")
-
+INPUT_PATH = Path("/home/scu/input")
+if not INPUT_PATH.exists():    
+    INPUT_PATH.mkdir()
+    _LOGGER.debug("Created input folder at %s", INPUT_PATH)
 # get all files in the local system and copy them to the input folder
 for node_input in PORTS.inputs:
-    if node_input.type != "file-url":
-        continue
-        
-    local_file_path = pathlib.Path(node_input.get())
-    if local_file_path.exists():
-        file_name = local_file_path.name
-        shutil.copy(local_file_path, INPUT_PATH / file_name)
+    if node_input.type in ("file-url", "folder-url"):
+        _LOGGER.debug("Start downloading path %s", node_input.value)
+        local_path = Path(node_input.get())
+        _LOGGER.debug("Completed download of %s in local path %s", node_input.value, local_path)
+        if local_path.exists():
+            file_name = local_path.name
+            _LOGGER.debug("Start moving %s to input path %s", local_path, INPUT_PATH)
+            shutil.move(str(local_path), str(INPUT_PATH / file_name))
+            _LOGGER.debug("Move completed")
