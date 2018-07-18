bca32665c872668d2104c9883382be5c7e480377
Pedro Crespo <crespo@itis.ethz.ch>
2018-07-18 20:20:37 +0200

    server: setup docker  - created docker-specific requirements  - renamed docker into .docker  -  sync install/test requires with req.txt

--- services/web/server/setup.py
+++ services/web/server/setup.py
@@ -1,41 +1,23 @@
 import sys
-import os
+import pathlib
 from setuptools import (
     setup,
     find_packages
 )
 
-_CDIR = os.path.dirname( sys.argv[0] if __name__ == "__main__" else __file__)
+_CDIR = pathlib.Path(sys.argv[0] if __name__ == "__main__" else __file__).parent
 
-INSTALL_REQUIRES = [
-    'aiohttp==3.3.2',
-    'aiohttp-security==0.2.0',
-    'aiohttp_session[secure]==2.5.1',
-    'aiohttp-swagger==1.0.5',
-    'aiopg[sa]==0.14.0',
-    'aio-pika==2.9.0',
-    'celery==4.1.0',
-    'kombu==4.1.0',
-    'minio==4.0.0',
-    'networkx==2.1',
-    'passlib==1.7.1',
-    'psycopg2==2.7.4',
-    'python-socketio==1.9.0',
-    'requests==2.19.0',
-    'sqlalchemy==1.2.8',
-    'tenacity==4.12.0',
-    'trafaret-config==2.0.1'
-]
+def list_requirements_in(filename):
+    requires = []
+    with (_CDIR / "requirements" / filename).open() as fh:
+        requires = [line.strip() for line in fh.readlines() if not line.lstrip().startswith("#")]
+    return requires
 
-TESTS_REQUIRE = [
-    'coveralls~=1.3',
-    'mock~=2.0',
-    'passlib',
-    'pylint~=2.0',
-    'pytest~=3.6',
-    'pytest-cov~=2.5',
-    'pytest-docker~=0.6'
-]
+#####################################################################################
+# NOTE see https://packaging.python.org/discussions/install-requires-vs-requirements/
+
+INSTALL_REQUIRES = list_requirements_in("docker-base.txt")
+TESTS_REQUIRE = list_requirements_in("docker-dev.txt")
 
 setup(
     name='simcore-web-server',
