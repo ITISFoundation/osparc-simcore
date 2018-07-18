0c25fb407340455f2fd33af34d4592c700a0443c
Pedro Crespo <crespo@itis.ethz.ch>
2018-07-18 18:05:47 +0200

    Simplified package requirements

--- packages/s3wrapper/setup.py
+++ packages/s3wrapper/setup.py
@@ -1,26 +1,29 @@
-from setuptools import (
-    setup,
-    find_packages
-)
+from setuptools import setup
 
-def load_requirements(requirement_filepath='requirements.txt'):
-    requires = []
-    with open(requirement_filepath, "r") as fh:
-        requires = [line.strip() for line in fh.readlines() if "#" not in line]
-    return requires
+install_requires = [
+    'minio==4.0.0',
+]
 
-test_deps=load_requirements("requirements/testing.txt")
+tests_require = [
+    'coveralls~=1.3',
+    'mock~=2.0',
+    'pylint~=2.0',
+    'pytest~=3.6',
+    'pytest-cov~=2.5',
+    'pytest-docker~=0.6',
+    'requests~=2.19'
+]
 
 setup(
     name='s3wrapper',
     version='0.1.0',
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    # requirements
-    install_requires=load_requirements("requirements/production.txt"),
-    tests_require=test_deps,
+    package_dir={'': 'src'},
+    packages=['s3wrapper'],
+    python_requires='>=3.6',
+    install_requires=install_requires,
+    tests_require=tests_require,
     extras_require= {
-        'test': test_deps
+        'test': tests_require
     },
-    setup_requires=["pytest-runner"]
+    setup_requires=['pytest-runner']
 )
