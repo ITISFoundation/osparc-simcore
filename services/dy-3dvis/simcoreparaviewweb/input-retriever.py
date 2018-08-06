8a512c6fdec1b03323cbb3b4b5074458b207930c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-08-06 08:19:19 +0200

    refactoring

--- services/dy-3dvis/simcoreparaviewweb/input-retriever.py
+++ services/dy-3dvis/simcoreparaviewweb/input-retriever.py
@@ -17,10 +17,10 @@ print(os.environ)
 
 from simcore_sdk.nodeports.nodeports import PORTS
 
-INPUT_PATH = Path(os.environ.get("PARAVIEW_INPUT_PATH"))
-if not INPUT_PATH.exists():    
-    INPUT_PATH.mkdir()
-    _LOGGER.debug("Created input folder at %s", INPUT_PATH)
+_INPUT_PATH = Path(os.environ.get("PARAVIEW_INPUT_PATH"))
+if not _INPUT_PATH.exists():    
+    _INPUT_PATH.mkdir()
+    _LOGGER.debug("Created input folder at %s", _INPUT_PATH)
 # get all files in the local system and copy them to the input folder
 for node_input in PORTS.inputs:
     if node_input.type in ("file-url", "folder-url"):
@@ -29,6 +29,6 @@ for node_input in PORTS.inputs:
         _LOGGER.debug("Completed download of %s in local path %s", node_input.value, local_path)
         if local_path.exists():
             file_name = local_path.name
-            _LOGGER.debug("Start moving %s to input path %s", local_path, INPUT_PATH)
-            shutil.move(str(local_path), str(INPUT_PATH / file_name))
+            _LOGGER.debug("Start moving %s to input path %s", local_path, _INPUT_PATH)
+            shutil.move(str(local_path), str(_INPUT_PATH / file_name))
             _LOGGER.debug("Move completed")
