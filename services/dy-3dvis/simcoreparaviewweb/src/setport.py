1899865c8c78facc2abb271d33b433f3d8d8fb04
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-08-06 22:45:05 +0200

    retrieve port number

--- services/dy-3dvis/simcoreparaviewweb/src/setport.py
+++ services/dy-3dvis/simcoreparaviewweb/src/setport.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
+import cgi
 import cgitb
+import logging
+from pathlib import Path
+
 cgitb.enable()
 print("Content-Type: text/html;charset=utf-8")
 print()
 
-import logging
-from pathlib import Path
-
+form = cgi.FieldStorage()
+server_port = form["port"].value
 _LOGGER = logging.getLogger(__name__)
 _INPUT_FILE = Path(r"/home/root/trigger/server_port")
-
-server_port = 23456
 
 with _INPUT_FILE.open(mode='w') as fp:
     fp.write(str(server_port))
