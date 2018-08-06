6ef67f46019f540247f2e4f709911bb8ba522e61
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-08-06 09:39:03 +0200

    added entrypoint in simcore service settings

--- services/director/source/producer.py
+++ services/director/source/producer.py
@@ -78,6 +78,15 @@ def __convert_labels_to_docker_runtime_parameters(service_runtime_parameters_lab
     _LOGGER.debug("Converted labels to docker runtime parameters: %s", runtime_params)
     return runtime_params
 
+def __get_service_entrypoint(service_runtime_parameters_labels):
+    _LOGGER.debug("Getting service entrypoint")
+    for param in service_runtime_parameters_labels:
+        if 'name' not in param or 'type' not in param or 'value' not in param:
+            pass
+        if param['name'] == 'entry_point':
+            return param['value']
+    return ''
+
 def __add_to_swarm_network_if_ports_published(docker_client, docker_service_runtime_parameters):
     # TODO: SAN this is a brain killer... change services to something better...
     if "endpoint_spec" in docker_service_runtime_parameters:
@@ -244,6 +253,7 @@ def start_service(service_name, service_tag, service_uuid):
         # prepare runtime parameters
         service_runtime_parameters_labels = __get_service_runtime_parameters_labels(docker_image_path, tag)
         docker_service_runtime_parameters = __convert_labels_to_docker_runtime_parameters(service_runtime_parameters_labels, service_uuid)
+        service_entrypoint = __get_service_entrypoint(service_runtime_parameters_labels)
         __add_to_swarm_network_if_ports_published(docker_client, docker_service_runtime_parameters)
         __add_uuid_label_to_service_runtime_params(docker_service_runtime_parameters, service_uuid)
         if len(list_of_images) > 1:
@@ -263,7 +273,8 @@ def start_service(service_name, service_tag, service_uuid):
             _LOGGER.debug("Service with parameters %s successfully started, published ports are %s", docker_service_runtime_parameters, published_ports)
             container_meta_data = {
                 "container_id": service.id,
-                "published_ports": published_ports
+                "published_ports": published_ports,
+                "entry_points": service_entrypoint
                 }
             containers_meta_data.append(container_meta_data)
         except docker.errors.ImageNotFound as err:
