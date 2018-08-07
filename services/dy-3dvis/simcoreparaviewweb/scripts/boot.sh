1f63b97db7b76ebe4fcb5b111c5c6382a89609b6
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-08-07 11:29:15 +0200

    changed input names to A and B as paraview shows a in* instead of 2 separate files fixed boot currently with osparc01 hard-coded input-retriever sets a fixed .vtk extension for the time being

--- services/dy-3dvis/simcoreparaviewweb/scripts/boot.sh
+++ services/dy-3dvis/simcoreparaviewweb/scripts/boot.sh
@@ -7,7 +7,11 @@ then
     result="$(python3 devel/devel-initconfiguration.py ${USE_CASE_CONFIG_FILE})";
     echo "Received result node uuid of $result";
     export SIMCORE_NODE_UUID="$result";
+    host_name="localhost"
+else
+    host_name="$(hostname -f)"
 fi
+
 
 echo "modifying apache configuration..."
 . scripts/apachePatch.sh
@@ -16,5 +20,6 @@ service apache2 restart
 
 echo "Waiting for server port to be defined"
 server_port="$(python3 src/getport.py)";
-echo "starting paraview using websocket port $server_port..."
-/opt/paraviewweb/scripts/start.sh "ws://localhost:$server_port"
+
+echo "starting paraview using hostname ${host_name} and websocket port $server_port..."
+/opt/paraviewweb/scripts/start.sh "ws://osparc01.speag.com:$server_port"
