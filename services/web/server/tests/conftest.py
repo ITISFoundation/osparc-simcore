06e7fee6c0a19decfeea97ead88f6e30ed554170
Pedro Crespo <crespo@itis.ethz.ch>
2018-08-10 14:50:51 +0200

    Refator web-server: cleaned up conftest

--- services/web/server/tests/conftest.py
+++ services/web/server/tests/conftest.py
@@ -1,23 +1,32 @@
+"""
+
+    pytest-docker https://github.com/AndreLouisCaron/pytest-docker
+"""
 # pylint: disable=unused-argument
+# pylint: disable=unused-import
 # pylint: disable=bare-except
 
 import logging
 import os
-import psycopg2
 
 import pytest
-#from pytest_docker import docker_ip, docker_services  # pylint:disable=W0611
 
-#from sqlalchemy import create_engine
-#from sqlalchemy.orm import sessionmaker
-
-# FIXME:
-# import server.utils.init_db as init_db
 import init_db
 
 _LOGGER = logging.getLogger(__name__)
 _CDIR = os.path.dirname(__file__)
 
+def _is_responsive(**kargs):
+    try:
+        conn = init_db.admin_engine.connect()
+        conn.close()
+    except:
+        _LOGGER.exception("Connection to db failed")
+        return False
+    return True
+
+
+# extends pytest-docker -------------------------------------------------------
 @pytest.fixture(scope='session')
 def docker_compose_file(pytestconfig):
     """
@@ -29,33 +38,6 @@ def docker_compose_file(pytestconfig):
         _CDIR,
         'mock',
         'docker-compose.yml')
-
-def is_responsive2(database, user, password, host, port, **kargs):
-    """
-        Checks if the database is ready to be used
-    """
-    try:
-        conn = psycopg2.connect( dbname=database,
-            user=user,
-            password=password,
-            host=host,
-            port=port)
-        conn.close()
-    except psycopg2.OperationalError as _ex:
-        logging.exception("Connection to db failed")
-        return False
-
-    return True
-
-def is_responsive(**kargs):
-    try:
-        conn = init_db.admin_engine.connect()
-        conn.close()
-    except:
-        logging.exception("Connection to db failed")
-        return False
-    return True
-
 
 @pytest.fixture(scope="session")
 def postgres_service(docker_ip, docker_services):
@@ -69,7 +51,7 @@ def postgres_service(docker_ip, docker_services):
     engine = init_db.test_engine
 
     docker_services.wait_until_responsive(
-        check=lambda: is_responsive(**pg_config),
+        check=lambda: _is_responsive(**pg_config),
         timeout=30.0,
         pause=1.0,
     )
