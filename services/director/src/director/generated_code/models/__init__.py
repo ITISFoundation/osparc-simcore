1b81094b09374d4363a3faed8e0d2b426ed3d96c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-08-22 14:41:29 +0200

    renamed service into runningservice

--- services/director/src/director/generated_code/models/__init__.py
+++ services/director/src/director/generated_code/models/__init__.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 # import models into model package
 from director.generated_code.models.author_details import AuthorDetails
 from director.generated_code.models.port import Port
-from director.generated_code.models.service import Service
+from director.generated_code.models.running_service import RunningService
 from director.generated_code.models.service_description import ServiceDescription
 from director.generated_code.models.service_details import ServiceDetails
 from director.generated_code.models.viewer import Viewer
