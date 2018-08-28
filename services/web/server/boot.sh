a5d8220ff60729b9f6da330ff2098c4630fc455a
Pedro Crespo <crespo@itis.ethz.ch>
2018-08-28 22:06:59 +0200

    Implements entry in #186: - upgrades Dockerfile with .venv and pip installing 3rd parties

--- services/web/server/boot.sh
+++ services/web/server/boot.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-#echo "activating python virtual env..."
-#source /home/scu/venv/bin/activate
+echo "activating python virtual env..."
+source $HOME/.venv/bin/activate
 
 if [[ ${DEBUG} == "1" ]]
 then
   echo "Booting in development mode ..."
+  pip install -r requirements/dev.txt
   service-web-server --config server-docker-dev.yaml
 else
   echo "Booting in production mode ..."
