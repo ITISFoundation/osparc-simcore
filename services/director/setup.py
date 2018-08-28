a91988865787ce1ce8e655f6a6c4d409b232d6c0
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-08-28 10:36:07 +0200

    renamed director to agreed service naming

--- services/director/setup.py
+++ services/director/setup.py
@@ -3,16 +3,16 @@ from setuptools import find_packages, setup
 
 INSTALL_REQUIRES = [
 'docker==3.5.0',
-'Flask==1.0.1',
 'tenacity==4.12.0',
+'aiohttp==3.3.2',
 'aiohttp_apiset==0.9.3',
 'requests==2.19.1',
 ]
 
 setup(
-    name='director-service',
+    name='simcore-director-webserver',
     version="0.1.0",
-    description='oSparc Director service',
+    description='oSparc Director webserver service',
     platforms=['POSIX'],
     package_dir={'': 'src'},
     packages=find_packages('src'),
@@ -20,7 +20,7 @@ setup(
         '': ['.openapi/v1/*']
     },
     entry_points={
-        'console_scripts': ['director-service=director.__main__:main']},
+        'console_scripts': ['simcore-director-webserver=director.__main__:main']},
     include_package_data=True,
     install_requires=INSTALL_REQUIRES,
     zip_safe=False,    
