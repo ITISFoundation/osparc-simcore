5b6f4c7a902f46e91a6d24b89a93527c102c2fe5
Pedro Crespo <crespo@itis.ethz.ch>
2018-08-29 17:47:13 +0200

    Minor cleanup and doc

--- services/sidecar/.docker/entrypoint.sh
+++ services/sidecar/.docker/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+# This entrypoint script:
+#
+# - Executes with root privileges *inside* of the container upon start
+# - Allows starting the container as root to perform some root-level operations at runtime
+#  (e.g. on volumes mapped inside)
+# - Notice that this way, the container *starts* as root but *runs* as scu (non-root user)
 #
 # See https://stackoverflow.com/questions/39397548/how-to-give-non-root-user-in-docker-container-access-to-a-volume-mounted-on-the
 
-# This container *starts* as root but *runs* as scu (non-root user)
+
 chown -R scu:scu /home/scu/input
 chown -R scu:scu /home/scu/output
 chown -R scu:scu /home/scu/log
 
-#exec runuser --user scu "$@" <-- this is the equivalent in ubuntu
+#exec runuser --user scu "$@" <-- if ubuntu, use this
 su-exec scu:scu "$@"
--- services/sidecar/boot.sh
+++ services/sidecar/boot.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 source $HOME/.venv/bin/activate
 
-echo "This is running as `id $(whoami)`"
+echo "INFO: Sidecar running as `id $(whoami)`"
 
 if [[ ${DEBUG} == "1" ]]
 then
-  echo "Booting in development mode ..."
-  echo "Installing director service ..."
+  echo "INFO: Booting in development mode ..."
+  echo "INFO: Installing director service ..."
 
   pip3 install --no-cache-dir -r requirements/dev.txt
   celery worker --app sidecar.celery:app --concurrency 2 --loglevel=debug
 else
-  echo "Booting in production mode ..."
+  echo "INFO: Booting in production mode ..."
   celery worker --app sidecar.celery:app --concurrency 2 --loglevel=info
 fi
