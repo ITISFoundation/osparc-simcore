20e27bb391467cefca801ab3c4d053dc4b970eaf
Pedro Crespo <crespo@itis.ethz.ch>
2018-08-30 19:28:37 +0200

    imp #186 webapi: added scripts

---
+++ scripts/openapi/openapi_codegen.sh
@@ -0,0 +1,92 @@
+#/bin/bash
+#
+# TODO: Uses https://github.com/OpenAPITools tools instead of swagger-codegen.
+#  THIS IS THE LAST VERSION!
+#    https://github.com/OpenAPITools/openapi-generator/blob/master/docs/migration-from-swagger-codegen.md
+#
+# https://github.com/OpenAPITools/openapi-generator/releases/tag/v3.0.0
+# https://angular.schule/blog/2018-06-swagger-codegen-is-now-openapi-generator
+
+usage()
+{
+    echo "usage: openapi_codegen [[[-i input] [-o output directory] [-g generator]] | [-h help]]"
+}
+
+##### Main
+
+input_file= 
+output_directory= 
+generator= 
+# process arguments
+while [ "$1" != "" ]; do
+    case $1 in
+        -i | --input )          shift
+                                input_file=$1
+                                ;;
+        -o | --outdir )         shift
+                                output_directory=$1
+                                ;;
+        -g | --generator )      shift
+                                generator=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+# SAN this allows to know whether we are running in the Windows linux environment or under linux/mac
+VERSION=$(uname -a);
+MICROSOFT_STRING="Microsoft"
+if echo "$VERSION" | grep -q "$MICROSOFT_STRING"; then
+export DOCKER_COMPOSE=docker-compose.exe
+export DOCKER=docker.exe
+export COMPOSE_CONVERT_WINDOWS_PATHS=1
+else
+export DOCKER_COMPOSE=docker-compose
+export DOCKER=docker
+fi
+
+# check arguments
+if [ -z "$input_file" ]; then
+    echo "no specification file defined..."
+    usage
+    exit 1
+elif [ -z "$output_directory" ]; then
+    echo "please define an output directory..."
+    usage
+    exit 1
+elif [ -z "$generator" ]; then
+    echo "please define a generator..."
+    usage
+    exit 1
+fi
+
+input_file_absolute_path="$(realpath "${input_file}")"
+input_parent_dir="$(dirname "${input_file_absolute_path}")"
+input_filename="$(basename "${input_file_absolute_path}")"
+echo "input file parent dir is ${input_parent_dir}"
+echo "input file name is ${input_filename}"
+
+output_absolute_dir="$(realpath ${output_directory})"
+echo "output directory is ${output_absolute_dir}"
+
+if echo "$VERSION" | grep -q "$MICROSOFT_STRING"; then
+    # if windows WSL we need to correct the mounted path to be understandable for docker
+    # /mnt/k/folder/subfolder/... -> k:/folder/subfolder/...
+    input_parent_dir=$(echo $input_parent_dir | sed -e 's,/mnt/\(.\)/,\1:/,')
+    echo "windows corrected input directory is ${input_parent_dir}"
+    output_absolute_dir=$(echo $output_absolute_dir | sed -e 's,/mnt/\(.\)/,\1:/,')
+    echo "windows corrected output directory is ${output_absolute_dir}"
+fi
+
+echo "generating code..."
+exec ${DOCKER} run --rm -v ${input_parent_dir}:/local -v ${output_absolute_dir}:/output openapitools/openapi-generator-cli \
+    generate \
+    -i /local/${input_filename} \
+    -g ${generator} \
+    -o /output/${generator}
+echo "code generated"
---
+++ scripts/openapi/openapi_python_server_codegen.sh
@@ -0,0 +1,62 @@
+#/bin/bash
+cd $(dirname $0)
+usage()
+{
+    echo "usage: openapi_python_server_codegen [[[-i input] [-o output directory]] | [-h help]]"
+}
+
+input_file=
+output_directory=
+generator="python-flask"
+# process arguments
+while [ "$1" != "" ]; do
+    case $1 in
+        -i | --input )          shift
+                                input_file=$1
+                                ;;
+        -o | --outdir )         shift
+                                output_directory=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+if [ -z "$output_directory" ]; then
+    echo "please define an output directory..."
+    usage
+    exit 1
+fi
+output_absolute_dir="$(realpath ${output_directory})"
+echo "output directory is ${output_absolute_dir}"
+
+# generate a python-flask server
+temp_folder=${PWD}/tmp
+if [ -d "$temp_folder" ]; then
+  rm -rf $temp_folder
+fi
+mkdir ${temp_folder}
+./openapi_codegen.sh -i ${input_file} -o ${temp_folder} -g ${generator}
+
+if [ ! -d "$output_directory" ]; then
+  mkdir ${output_directory}
+fi
+
+sudo chown -R $USER $temp_folder
+
+echo "retrieving util.py..."
+mv -uf ${temp_folder}/${generator}/openapi_server/util.py ${output_directory}/util.py
+echo "retrieving __init__.py..."
+mv -uf ${temp_folder}/${generator}/openapi_server/__init__.py ${output_directory}/__init__.py
+echo "retrieving models..."
+if [ ! -d "$output_directory/models" ]; then
+  mkdir ${output_directory}/models
+fi
+mv -uf ${temp_folder}/${generator}/openapi_server/models/* ${output_directory}/models
+
+echo "cleaning up..."
+rm -rf ${temp_folder}
