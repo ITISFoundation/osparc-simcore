b42352fdf543fc2386c57d1b1d9971f0cb4cb198
Pedro Crespo <crespo@itis.ethz.ch>
2018-08-30 16:36:23 +0200

    is#186 web: fix import in docker context  - temporarily a thick production stage  - boot and entrypoint sh to .docker/

--- services/web/server/.docker/boot.sh
+++ services/web/server/.docker/boot.sh
@@ -5,7 +5,11 @@ source $HOME/.venv/bin/activate
 if [[ ${DEBUG} == "1" ]]
 then
   echo "Booting in development mode ..."
+  cd $HOME/services/web/server
   pip install -r requirements/dev.txt
+  pip list
+
+  cd $HOME/
   simcore-service-webserver --config server-docker-dev.yaml
 else
   echo "Booting in production mode ..."
