68aa0c1904099a98d3fdcc8c41e01ca8e3251174
Pedro Crespo <crespo@itis.ethz.ch>
2018-08-30 19:29:59 +0200

    imp #186 webapi: autogenerated code from v1/...oas3.yaml

---
+++ services/web/server/src/simcore_service_webserver/rest/generated_code/models/__init__.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from .document import Document
+from .error import Error
+from .error_error import ErrorError
+from .registration_data import RegistrationData
+from .service_info import ServiceInfo
+from .service_info_data import ServiceInfoData
---
+++ services/web/server/src/simcore_service_webserver/rest/handlers.py
@@ -0,0 +1,16 @@
+"""This is a generated stub of handlers to be connected to the paths defined in the API
+
+"""
+import logging
+
+from aiohttp import web_exceptions
+
+_LOGGER = logging.getLogger(__name__)
+
+# This module shall contain the handlers of the API (implementation side of the openapi server side).
+# Each operation is typically defined as
+# async def root_get(request):
+#   return "hello API world"
+
+# The API shall define a path where the entry operationId:
+# operationId: root_get
