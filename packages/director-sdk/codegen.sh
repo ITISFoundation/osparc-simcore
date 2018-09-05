5f301b0b6058cc31f2e3bafe4f1d6907d907275b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-09-05 10:39:04 +0200

    output folder current folder

--- packages/director-sdk/codegen.sh
+++ packages/director-sdk/codegen.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 exec ../../scripts/openapi/openapi_codegen.sh \
     -i ../../services/director/src/simcore_service_director/.openapi/v1/director_api.yaml \
-    -o ./src \
+    -o . \
     -g python \
     -c ./codegen_config.json
