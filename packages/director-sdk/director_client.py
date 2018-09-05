6f87aa93dfe93d1a73fd21e52a14dd4a9c754559
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-09-05 10:35:58 +0200

    example code

---
+++ packages/director-sdk/director_client.py
@@ -0,0 +1,29 @@
+import asyncio
+import simcore_director_sdk
+
+from simcore_director_sdk.rest import ApiException
+
+configuration = simcore_director_sdk.Configuration()
+configuration.host = "http://localhost:8001/v1"
+api_instance = simcore_director_sdk.UsersApi(simcore_director_sdk.ApiClient(configuration))
+
+async def get_root():
+    try:
+        api_response = await api_instance.root_get()    
+        print(api_response)
+    except ApiException as e:
+        print("Exception when calling UserApi->root_get: %s\n" % e)
+async def get_services():
+    try:
+        api_response = await api_instance.services_get()
+        print(api_response)
+    except ApiException as e:
+        print("Exception when calling UserApi->root_get: %s\n" % e)
+
+async def test_api():
+    await get_root()
+    await get_services()
+    print("do work")
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test_api())
