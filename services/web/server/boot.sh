2c8705e856598a44ad8506eaee0f24e32baaa803
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-09-06 12:26:09 +0200

    now installs director-sdk

--- services/web/server/boot.sh
+++ services/web/server/boot.sh
@@ -6,6 +6,7 @@ if [[ ${DEBUG} == "1" ]]
 then
   echo "Booting in development mode ..."
   pip install -r requirements/dev.txt
+  pip install $HOME/packages/director-sdk/python
   simcore-service-webserver --config server-docker-dev.yaml
 else
   echo "Booting in production mode ..."
