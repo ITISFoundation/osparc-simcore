dc01b4677cb401ef3bb1bd5fbc9bc858417e2cc0
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-09-07 17:49:00 +0200

    added some more logging in the director

--- services/director/src/simcore_service_director/rest/handlers.py
+++ services/director/src/simcore_service_director/rest/handlers.py
@@ -20,6 +20,7 @@ from .generated_code.models import (HealthCheck, HealthCheckEnveloped,
 _LOGGER = logging.getLogger(__name__)
 
 async def root_get(request):  # pylint:disable=unused-argument
+    _LOGGER.debug("Client does root_get request %s", request)
     distb = pkg_resources.get_distribution('simcore-service-director')
     api_path = config.OPEN_API_BASE_FOLDER / config.OPEN_API_SPEC_FILE
     with api_path.open() as file_ptr:
@@ -33,6 +34,7 @@ async def root_get(request):  # pylint:disable=unused-argument
     return HealthCheckEnveloped(data=service_health, status=200).to_dict()
 
 async def services_get(request, service_type=None):  # pylint:disable=unused-argument
+    _LOGGER.debug("Client does services_get request %s with service_type %s", request, service_type)
     try:
         services = []
         if not service_type or "computational" in service_type:
@@ -46,7 +48,7 @@ async def services_get(request, service_type=None):  # pylint:disable=unused-arg
     except Exception as err:
         raise web_exceptions.HTTPInternalServerError(reason=str(err))
 
-def list_services(list_service_fct):
+def list_services(list_service_fct):    
     services = list_service_fct()
     if config.CONVERT_OLD_API:
         services = [api_converters.convert_service_from_old_api(x) for x in services]
@@ -56,7 +58,10 @@ def list_services(list_service_fct):
     return service_descs
 
 
-async def running_interactive_services_post(request, service_key, service_uuid, service_tag=None):  # pylint:disable=unused-argument
+async def running_interactive_services_post(request, service_key, service_uuid, service_tag):  # pylint:disable=unused-argument
+    _LOGGER.debug("Client does running_interactive_services_post request %s with service_key %s, service_uuid %s and service_tag %s", 
+                request, service_key, service_uuid, service_tag)
+    
     try:
         service = producer.start_service(service_key, service_tag, service_uuid)
         running_service = RunningService.from_dict(service)
@@ -71,6 +76,7 @@ async def running_interactive_services_post(request, service_key, service_uuid, 
         raise web_exceptions.HTTPInternalServerError(reason=str(err))
 
 async def running_interactive_services_get(request, service_uuid):  # pylint:disable=unused-argument
+    _LOGGER.debug("Client does running_interactive_services_get request %s with service_uuid %s", request, service_uuid)
     try:
         producer.is_service_up(service_uuid)
     except exceptions.ServiceUUIDNotFoundError as err:
@@ -81,6 +87,7 @@ async def running_interactive_services_get(request, service_uuid):  # pylint:dis
     return Response204Enveloped(status=204).to_dict()
 
 async def running_interactive_services_delete(request, service_uuid):  # pylint:disable=unused-argument
+    _LOGGER.debug("Client does running_interactive_services_delete request %s with service_uuid %s", request, service_uuid)
     try:
         producer.stop_service(service_uuid)
     except exceptions.ServiceUUIDNotFoundError as err:
