a70675f111a4d854bd3d37ee698545874a9fe282
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-09-07 17:48:05 +0200

    added test call to starting a service through the director sdk

--- packages/director-sdk/sample.py
+++ packages/director-sdk/sample.py
@@ -7,7 +7,7 @@ import simcore_director_sdk
 from simcore_director_sdk.rest import ApiException
 
 configuration = simcore_director_sdk.Configuration()
-configuration.host = configuration.host.format(host="localhost", port=8001, version="/v1/")
+configuration.host = configuration.host.format(host="localhost", port=8001, version="v1")
 api_instance = simcore_director_sdk.UsersApi(simcore_director_sdk.ApiClient(configuration))
 
 async def get_root():
@@ -23,9 +23,17 @@ async def get_services():
     except ApiException as e:
         print("Exception when calling UserApi->root_get: %s\n" % e)
 
+async def start_service():
+    try:
+        api_response = await api_instance.running_interactive_services_post("simcore/services/dynamic/3d-viewer", "testing6")
+        print(api_response)
+    except ApiException as e:
+        print("Exception when calling UserApi->root_get: %s\n" % e)
+
 async def test_api():
-    await get_root()
-    await get_services()
+    # await get_root()
+    # await get_services()
+    await start_service()
     print("do work")
 
 loop = asyncio.get_event_loop()
