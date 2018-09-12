d82d7cb09eecfecf26577fe8e35dfe358f67b4fb
Pedro Crespo <crespo@itis.ethz.ch>
2018-09-12 18:26:57 +0200

    Added autogenerated code for webserver-sdk. Added some ignores

---
+++ packages/webserver-sdk/python/simcore_webserver_sdk/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from simcore_webserver_sdk.api.authentication_api import AuthenticationApi
+from simcore_webserver_sdk.api.test_api import TestApi
