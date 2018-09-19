bbfcfa331d5048b4fe08f503135a256cb6d7e42e
Pedro Crespo <crespo@itis.ethz.ch>
2018-09-19 14:19:29 +0200

    Entripoint to webserver docker to set container as non-root at runtime

--- services/web/server/.docker/boot.sh
+++ services/web/server/.docker/boot.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-echo "activating python virtual env..."
+echo "Activating python virtual env..."
 source $HOME/.venv/bin/activate
 
 if [[ ${DEBUG} == "1" ]]
 then
   echo "Booting in development mode ..."
+  echo "DEBUG: User    :`id $(whoami)`"
+  echo "DEBUG: Workdir :`pwd`"
+
+  cd $HOME/services/web/server
   pip install -r requirements/dev.txt
+  pip list
+
+  cd $HOME/
   simcore-service-webserver --config server-docker-dev.yaml
 else
   echo "Booting in production mode ..."
