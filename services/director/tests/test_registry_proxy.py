c5d6363bfe16d093b5ab16e3819f65fb9e49cc56
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-09-21 15:51:25 +0200

    refactored tests

--- services/director/tests/test_registry_proxy.py
+++ services/director/tests/test_registry_proxy.py
@@ -5,6 +5,11 @@ from simcore_service_director import (
     )
 from simcore_service_director.exceptions import DirectorException
 
+@pytest.fixture
+def proxy_setup(docker_registry):
+    config.REGISTRY_URL = docker_registry
+    config.REGISTRY_SSL = False
+    registry_proxy.setup_registry_connection()
 
 def test_setup_registry_connection():
     config.REGISTRY_AUTH = False
@@ -22,32 +27,23 @@ def test_setup_registry_connection():
     config.REGISTRY_PW = "TheUgly"
     registry_proxy.setup_registry_connection()
 
-def test_list_no_services_available(docker_registry):
-    config.REGISTRY_URL = docker_registry
-    config.REGISTRY_SSL = False
-    registry_proxy.setup_registry_connection()
+def test_list_no_services_available(docker_registry, proxy_setup):    
     computational_services = registry_proxy.list_computational_services()
     assert (not computational_services) # it's empty
     interactive_services = registry_proxy.list_interactive_services()
     assert (not interactive_services)
 
-def test_list_computational_services(push_services, docker_registry ):
-    push_services(6, 3)
-    config.REGISTRY_URL = docker_registry
-    config.REGISTRY_SSL = False
-    registry_proxy.setup_registry_connection()
+def test_list_computational_services(docker_registry, push_services, proxy_setup):
+    push_services(6, 3)    
     computational_services = registry_proxy.list_computational_services()
     assert len(computational_services) == 6
 
-def test_list_interactive_services(docker_registry, push_services):
-    push_services(5, 4)
-    config.REGISTRY_URL = docker_registry
-    config.REGISTRY_SSL = False
-    registry_proxy.setup_registry_connection()
+def test_list_interactive_services(docker_registry, push_services, proxy_setup):
+    push_services(5, 4)    
     interactive_services = registry_proxy.list_interactive_services()
     assert len(interactive_services) == 4
 
-def test_retrieve_list_of_images_in_repo(docker_registry, push_services):
+def test_retrieve_list_of_images_in_repo(docker_registry, push_services, proxy_setup):
     images = push_services(5, 3)
     image_number = {}
     for image in images:
@@ -56,10 +52,7 @@ def test_retrieve_list_of_images_in_repo(docker_registry, push_services):
         if key not in image_number:
             image_number[key] = 0
         image_number[key] = image_number[key]+1
-
-    config.REGISTRY_URL = docker_registry
-    config.REGISTRY_SSL = False
-    registry_proxy.setup_registry_connection()
+    
     for key, number in image_number.items():
         list_of_images = registry_proxy.retrieve_list_of_images_in_repo(key)
         assert len(list_of_images["tags"]) == number
@@ -69,11 +62,8 @@ def test_list_interactive_service_dependencies():
     # need to setup a fake registry to test this
     pass
 
-def test_retrieve_labels_of_image(docker_registry, push_services):
-    images = push_services(1, 1)
-    config.REGISTRY_URL = docker_registry
-    config.REGISTRY_SSL = False
-    registry_proxy.setup_registry_connection()
+def test_retrieve_labels_of_image(docker_registry, push_services, proxy_setup):
+    images = push_services(1, 1)    
     for image in images:
         service_description = image["service_description"]
         labels = registry_proxy.retrieve_labels_of_image(service_description["key"], service_description["version"])
@@ -123,11 +113,8 @@ def test_get_service_last_namess():
     repo = "services/dynamic/modeler"
     assert registry_proxy.get_service_last_names(repo) == "invalid service"
 
-def test_get_service_details(docker_registry, push_services):
-    images = push_services(1, 1)
-    config.REGISTRY_URL = docker_registry
-    config.REGISTRY_SSL = False
-    registry_proxy.setup_registry_connection()
+def test_get_service_details(push_services, proxy_setup):
+    images = push_services(1, 1)    
     for image in images:
         service_description = image["service_description"]
         details = registry_proxy.get_service_details(service_description["key"], service_description["version"])
