5911c5da6623971bc384f100e6eb99f63d4b2293
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-09-21 08:55:58 +0200

    added unziping procedure if file is a zip file

--- services/dy-3dvis/simcoreparaviewweb/src/input-retriever.py
+++ services/dy-3dvis/simcoreparaviewweb/src/input-retriever.py
@@ -8,6 +8,8 @@ import os
 import shutil
 from pathlib import Path
 
+import zipfile
+
 _LOGGER = logging.getLogger(__name__)
 
 print("Content-Type: text/html;charset=utf-8")
@@ -18,6 +20,15 @@ print(os.environ)
 from simcore_sdk.nodeports.nodeports import PORTS
 
 _INPUT_PATH = Path(os.environ.get("PARAVIEW_INPUT_PATH"))
+
+def move_file(filename):
+    if local_path.exists():
+        file_name = filename.name
+        _LOGGER.debug("Start moving %s to input path %s", filename, _INPUT_PATH)
+        shutil.move(str(filename), str(_INPUT_PATH / file_name) + ".vtk")
+        _LOGGER.debug("Move completed")
+        print("Move completed")
+
 if not _INPUT_PATH.exists():    
     _INPUT_PATH.mkdir()
     _LOGGER.debug("Created input folder at %s", _INPUT_PATH)
@@ -28,8 +39,10 @@ for node_input in PORTS.inputs:
         local_path = Path(node_input.get())
         _LOGGER.debug("Completed download of %s in local path %s", node_input.value, local_path)
         if local_path.exists():
-            file_name = local_path.name
-            _LOGGER.debug("Start moving %s to input path %s", local_path, _INPUT_PATH)
-            shutil.move(str(local_path), str(_INPUT_PATH / file_name) + ".vtk")
-            _LOGGER.debug("Move completed")
-            print("Move completed")
+            if zipfile.is_zipfile(str(local_path)):
+                zip_ref = zipfile.ZipFile(str(local_path), 'r')
+                zip_ref.extractall(str(_INPUT_PATH))
+                zip_ref.close()
+                print("Unzipped")
+            else:
+                move_file(local_path)
