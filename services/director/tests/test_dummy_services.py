905942b1a8981c2c835135175fa9d1fcebe74e0f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-09-25 18:15:42 +0200

    refactoring

--- services/director/tests/test_dummy_services.py
+++ services/director/tests/test_dummy_services.py
@@ -1,7 +1,7 @@
 import pytest
 import json
 import logging
-from jsonschema import (validate, ValidationError)
+from helpers import json_schema_validator
 from simcore_service_director import resources
 
 log = logging.getLogger(__name__)
@@ -10,15 +10,16 @@ def test_v0_services_nonconformity(push_v0_schema_services):
     services = push_v0_schema_services(1,1)
     with resources.stream(resources.RESOURCE_NODE_SCHEMA) as file_pt:
         service_schema = json.load(file_pt)
+    
     for service in services:
         # validate service
-        with pytest.raises(ValidationError, message="expecting json schema validation error"):
-            validate(service["service_description"], service_schema)
+        with pytest.raises(Exception, message="expecting json schema validation error"):
+            json_schema_validator.validate_instance_object(service["service_description"], service_schema)
 
-def test_v1_services_nonconformity(push_services):
+def test_v1_services_conformity(push_services):
     services = push_services(1,1)
     with resources.stream(resources.RESOURCE_NODE_SCHEMA) as file_pt:
         service_schema = json.load(file_pt)
     for service in services:
         # validate service
-        validate(service["service_description"], service_schema)
+        json_schema_validator.validate_instance_object(service["service_description"], service_schema)
