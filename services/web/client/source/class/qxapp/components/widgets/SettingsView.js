c6cfa61f3ca9dbc5343c4979fc0d6ba5468d6be8
odeimaiz <maiz@zurichmedtech.com>
2018-10-03 16:38:18 +0200

    InputNodes layout added to SettingsView

--- services/web/client/source/class/qxapp/components/widgets/SettingsView.js
+++ services/web/client/source/class/qxapp/components/widgets/SettingsView.js
@@ -1,17 +1,38 @@
+const INPUTS_WIDTH = 200;
+
 qx.Class.define("qxapp.components.widgets.SettingsView", {
   extend: qx.ui.container.Composite,
 
   construct: function() {
     this.base();
 
-    let box = new qx.ui.layout.VBox(10);
-    box.set({
-      alignX: "center"
+    let hBox = new qx.ui.layout.HBox(10);
+    this.set({
+      layout: hBox,
+      padding: 10
     });
 
-    this.set({
-      layout: box,
-      padding: 10
+    let inputNodesLayout = this.__inputNodesLayout = new qx.ui.container.Composite(new qx.ui.layout.VBox(5));
+    inputNodesLayout.set({
+      width: INPUTS_WIDTH,
+      maxWidth: INPUTS_WIDTH,
+      allowGrowX: false
+    });
+    const navBarLabelFont = qx.bom.Font.fromConfig(qxapp.theme.Font.fonts["nav-bar-label"]);
+    let inputLabel = new qx.ui.basic.Label("Inputs").set({
+      font: navBarLabelFont,
+      alignX: "center"
+    });
+    inputNodesLayout.add(inputLabel);
+    this.add(inputNodesLayout);
+
+
+    let mainLayout = this.__mainLayout = new qx.ui.container.Composite(new qx.ui.layout.VBox(10));
+    mainLayout.set({
+      alignX: "center"
+    });
+    this.add(mainLayout, {
+      flex: 1
     });
 
     this.__initTitle();
@@ -25,7 +46,6 @@ qx.Class.define("qxapp.components.widgets.SettingsView", {
 
   properties: {
     node: {
-      // check: "qxapp.components.workbench.NodeBase",
       check: "qxapp.data.model.NodeModel",
       apply: "__applyNode"
     }
@@ -34,6 +54,8 @@ qx.Class.define("qxapp.components.widgets.SettingsView", {
   members: {
     __settingsBox: null,
     // __dynamicViewer: null,
+    __inputNodesLayout: null,
+    __mainLayout: null,
 
     __initTitle: function() {
       let box = new qx.ui.layout.HBox();
@@ -42,7 +64,7 @@ qx.Class.define("qxapp.components.widgets.SettingsView", {
         alignX: "right"
       });
       let titleBox = new qx.ui.container.Composite(box);
-      let settLabel = new qx.ui.basic.Label(this.tr("Inputs"));
+      let settLabel = new qx.ui.basic.Label(this.tr("Settings"));
       settLabel.set({
         alignX: "center",
         alignY: "middle"
@@ -53,7 +75,7 @@ qx.Class.define("qxapp.components.widgets.SettingsView", {
         width: "75%"
       });
       titleBox.add(doneBtn);
-      this.add(titleBox);
+      this.__mainLayout.add(titleBox);
 
       doneBtn.addListener("execute", function() {
         this.fireEvent("SettingsEdited");
@@ -62,7 +84,7 @@ qx.Class.define("qxapp.components.widgets.SettingsView", {
 
     __initSettings: function() {
       this.__settingsBox = new qx.ui.container.Composite(new qx.ui.layout.Grow());
-      this.add(this.__settingsBox);
+      this.__mainLayout.add(this.__settingsBox);
 
       // this.__dynamicViewer = new qx.ui.container.Composite(new qx.ui.layout.VBox(10));
       // this.add(this.__dynamicViewer);
