9edc88ddb478644d0137778d51f9d15ab538693b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-10-10 11:54:10 +0200

    examples are not allowed in openAPI in schemas and properties. example without s is

--- scripts/jsonschema/openapi_converter/converter.js
+++ scripts/jsonschema/openapi_converter/converter.js
@@ -15,12 +15,20 @@ filenames.forEach(filepath => {
   if (extName == ".json") {
     console.log("converting " + filepath + "...");
     const contents = fs.readFileSync(inputPath + "/" + filepath, 'utf8');    
-    const object = JSON.parse(contents);
+    const object = JSON.parse(contents);    
     const convertedSchema = toOpenApi(object);
     console.log("converted " + filepath + " succesfully");
+    
+    var yamlSchema = yaml.safeDump(convertedSchema)
+    // console.log(yamlSchema);
+    
+    // there is a BUG here. examples in a schema are not allowed in openapi, they should be replaced by example
+    // Note that schemas and properties support singular example but not plural examples.
+    // [link to problem](https://swagger.io/docs/specification/adding-examples/)
+    yamlSchema = yamlSchema.replace(/examples:\n/g, "example:\n");
     // write as yaml
-    fs.writeFileSync(outputPath + path.basename(filepath, ".json") + ".yaml", yaml.safeDump(convertedSchema));
-    //fs.writeFileSync(outputPath + "converted_" + filepath, JSON.stringify(convertedSchema));    
+    fs.writeFileSync(outputPath + path.basename(filepath, ".json") + ".yaml", yamlSchema);
+    // fs.writeFileSync(outputPath + "converted_" + filepath, JSON.stringify(convertedSchema));    
   }
   
 });
