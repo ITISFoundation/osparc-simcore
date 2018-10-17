e0c66c7c04426b68f89b6183647fdbe212ce25fc
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-10-17 15:53:23 +0200

    file name is kept in S3

--- services/dy-jupyter/devel/initialise_dummy_platorm.py
+++ services/dy-jupyter/devel/initialise_dummy_platorm.py
@@ -54,6 +54,10 @@ def create_dummy(json_configuration_file_path):
     with open(json_configuration_file_path) as file_pointer:
         json_configuration = file_pointer.read()
     
+    # create a dummy file filled with dummy data
+    temp_file = tempfile.NamedTemporaryFile(suffix=".csv")
+    temp_file.close()
+    # set up db
     db = init_db()
     new_Pipeline = ComputationalPipeline()
     db.session.add(new_Pipeline)
@@ -62,15 +66,13 @@ def create_dummy(json_configuration_file_path):
     node_uuid = str(uuid.uuid4())
     # correct configuration with node uuid
     json_configuration = json_configuration.replace("SIMCORE_NODE_UUID", node_uuid)
+    json_configuration = json_configuration.replace("FILENAME_ID", Path(temp_file.name).name)
     configuration = json.loads(json_configuration)
+    
     # now create the node in the db with links to S3
     new_Node = ComputationalTask(pipeline_id=new_Pipeline.pipeline_id, node_id=node_uuid, input=configuration["inputs"], output=configuration["outputs"])
     db.session.add(new_Node)
     db.session.commit()
-
-    # create a dummy file filled with dummy data
-    temp_file = tempfile.NamedTemporaryFile()
-    temp_file.close()
 
     # create a dummy table
     number_of_rows = 5000
@@ -85,7 +87,7 @@ def create_dummy(json_configuration_file_path):
             with open(temp_file.name, "w") as file_pointer:
                 df.to_csv(path_or_buf=file_pointer, sep="\t", header=False, index=False)        
             
-            s3_object_name = Path(str(new_Pipeline.pipeline_id), node_uuid, input_item["key"])
+            s3_object_name = Path(str(new_Pipeline.pipeline_id), node_uuid, Path(temp_file.name).name)
             s3.client.upload_file(s3.bucket, s3_object_name.as_posix(), temp_file.name)
         elif input_item["type"] == "folder-url":
             for i in range(number_of_files):
@@ -94,7 +96,7 @@ def create_dummy(json_configuration_file_path):
                 with open(temp_file.name, "w") as file_pointer:
                     df.to_csv(path_or_buf=file_pointer, sep="\t", header=False, index=False)        
                 
-                s3_object_name = Path(str(new_Pipeline.pipeline_id), node_uuid, input_item["key"], str(i) + ".dat")
+                s3_object_name = Path(str(new_Pipeline.pipeline_id), node_uuid, Path(temp_file.name).parent.name, str(i) + ".dat")
                 s3.client.upload_file(s3.bucket, s3_object_name.as_posix(), temp_file.name)
 
     Path(temp_file.name).unlink()
