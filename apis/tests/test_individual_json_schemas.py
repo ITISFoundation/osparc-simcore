4dbb09ee7d62177658ed2e3423ec6ff1fd6bab24
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-10-18 22:10:29 +0200

    fixed openapi tests

--- apis/tests/test_individual_json_schemas.py
+++ apis/tests/test_individual_json_schemas.py
@@ -1,28 +1,16 @@
-import json
-import yaml
 from pathlib import Path
 
 import pytest
-from jsonschema import (
-    SchemaError, 
-    ValidationError, 
-    validate)
+from jsonschema import SchemaError, ValidationError, validate
+
+from utils import read_schema, is_json_schema
 
 _API_DIR = Path(__file__).parent.parent
-
-def read_schema(spec_file_path: Path) -> dict:
-    assert spec_file_path.exists()
-    with spec_file_path.open() as file_ptr:
-        if ".json" in  spec_file_path.suffix:
-            schema_specs = json.load(file_ptr)
-        else:
-            schema_specs = yaml.load(file_ptr)
-        return schema_specs
 
 def validate_individual_schemas(list_of_paths):
     for spec_file_path in list_of_paths:
         specs_dict = read_schema(spec_file_path)
-        if "$schema" in specs_dict:
+        if is_json_schema(specs_dict):
             # it seems it is a json schema file
             try:
                 dummy_instance = {}
--- apis/tests/test_individual_openapi_schemas.py
+++ apis/tests/test_individual_openapi_schemas.py
@@ -1,13 +1,25 @@
 from pathlib import Path
 
 import pytest
-import yaml
-
 from openapi_spec_validator import validate_spec
 from openapi_spec_validator.exceptions import OpenAPIValidationError
 
+from utils import is_json_schema, read_schema
+
 _API_DIR = Path(__file__).parent.parent
 
+_FAKE_OPEN_API_HEADERS = {
+        "openapi": "3.0.0",
+        "info":{
+            "title": "An include file to define sortable attributes",
+            "version": "1.0.0"
+        },        
+        "paths": {},
+        "components": {
+            "parameters":{},
+            "schemas":{}
+        }
+    }
 
 def correct_schema_local_references(schema_specs):
     for key, value in schema_specs.items():
@@ -28,36 +40,31 @@ def add_namespace_for_converted_schemas(schema_specs):
         }
     return fake_schema_specs
     
-def validate_individual_schemas(list_of_paths):
-    fake_openapi_headers = {
-        "openapi": "3.0.0",
-        "info":{
-            "title": "An include file to define sortable attributes",
-            "version": "1.0.0"
-        },        
-        "paths": {},
-        "components": {
-            "parameters":{},
-            "schemas":{}
-        }
-    }
-    
-    for spec_file_path in list_of_paths:
-        assert spec_file_path.exists()
-        # only consider schemas
-        if not "openapi.yaml" in str(spec_file_path.name) and "schemas" in str(spec_file_path):
-            with spec_file_path.open() as file_ptr:
-                schema_specs = yaml.load(file_ptr)
-                # correct local references
-                correct_schema_local_references(schema_specs)
-                if str(spec_file_path).endswith("-converted.yaml"):
-                    schema_specs = add_namespace_for_converted_schemas(schema_specs)
-                fake_openapi_headers["components"]["schemas"] = schema_specs
+def validate_individual_schemas(list_of_paths: list):
+    for spec_file_path in list_of_paths:        
+        # skip full specs
+        if "openapi.yaml" in spec_file_path.name:
+            continue
+        # only consider schemas in a /schemas/ subfolder
+        if "schemas" in str(spec_file_path):
+            specs = read_schema(spec_file_path)
+            if is_json_schema(specs):
+                continue
+
+            # correct local references
+            correct_schema_local_references(specs)
+
+            if str(spec_file_path).endswith("-converted.yaml"):
+                # this is a json to openapi converted file
+                specs = add_namespace_for_converted_schemas(specs)
+            fake_openapi_headers = _FAKE_OPEN_API_HEADERS
+            fake_openapi_headers["components"]["schemas"] = specs
             try:
                 validate_spec(fake_openapi_headers, spec_url=spec_file_path.as_uri())
             except OpenAPIValidationError as err:
                 pytest.fail(err.message)
 
 def test_valid_individual_openapi_schemas_specs():
+    validate_individual_schemas(_API_DIR.rglob("*.json"))
     validate_individual_schemas(_API_DIR.rglob("*.yaml"))
     validate_individual_schemas(_API_DIR.rglob("*.yml"))
