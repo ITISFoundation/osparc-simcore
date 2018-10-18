d51d0bd3fe32c254309635a03c559602d65a5d09
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-10-18 08:49:42 +0200

    improved development modeautomatic opening of notebook

--- services/dy-jupyter/docker/boot.sh
+++ services/dy-jupyter/docker/boot.sh
@@ -1,13 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 if test "${CREATE_DUMMY_TABLE}" = "1"
 then
+    pip install -r /home/jovyan/devel/requirements.txt
+    pushd /packages/simcore-sdk; pip install -r requirements-dev.txt; popd
+    pushd /packages/s3wrapper; pip install -r requirements-dev.txt; popd
+
     echo "Creating dummy tables ... using ${USE_CASE_CONFIG_FILE}"
     result="$(python devel/initialise_dummy_platorm.py ${USE_CASE_CONFIG_FILE})"
     echo "Received result node uuid of $result"
     export SIMCORE_NODE_UUID="$result"
 fi
 
+jupyter trust ${NOTEBOOK_URL}
 start-notebook.sh \
     --NotebookApp.token='' \
-    --NotebookApp.tornado_settings="{\"headers\":{\"Content-Security-Policy\":\"frame-ancestors+'self'+http://osparc01.speag.com:9081;+report-uri/api/security/csp-report\"}}"
+    --NotebookApp.tornado_settings="{\"headers\":{\"Content-Security-Policy\":\"frame-ancestors+'self'+http://osparc01.speag.com:9081;+report-uri/api/security/csp-report\"}}" \
+    --NotebookApp.default_url=/notebooks/${NOTEBOOK_URL}
