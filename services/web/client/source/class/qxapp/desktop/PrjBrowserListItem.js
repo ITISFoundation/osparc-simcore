c1dbbf71848637c49bdc9da5fd96c773b19bbc51
odeimaiz <maiz@zurichmedtech.com>
2018-10-19 15:28:57 +0200

    PrjBrowserListItem improved

--- services/web/client/source/class/qxapp/desktop/PrjBrowserListItem.js
+++ services/web/client/source/class/qxapp/desktop/PrjBrowserListItem.js
@@ -49,15 +49,25 @@ qx.Class.define("qxapp.desktop.PrjBrowserListItem", {
       let control;
       switch (id) {
         case "icon":
-          control = new qx.ui.basic.Image(this.getIcon());
-          this._add(control);
+          {
+            control = new qx.ui.basic.Image(this.getIcon());
+            this._add(control);
+            let dummyProgress = new qx.ui.indicator.ProgressBar().set({
+              height: 10,
+              maximum: 100,
+              value: Math.floor(Math.random() * 101)
+            });
+            this._add(dummyProgress);
+          }
           break;
         case "prjTitle":
           control = new qx.ui.basic.Label(this.getPrjTitle()).set({
             rich: true,
             allowGrowY: false
           });
+          this._add(new qx.ui.core.Spacer(null, 5));
           this._add(control);
+          this._add(new qx.ui.core.Spacer(null, 5));
           break;
         case "creator":
           control = new qx.ui.basic.Label(this.getCreator()).set({
