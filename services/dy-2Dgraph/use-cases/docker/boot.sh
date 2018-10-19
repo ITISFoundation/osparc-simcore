28d8d7fa5833fcfb04f5c8168fb6d8555862ce4f
Sylvain <35365065+sanderegg@users.noreply.github.com>
2018-10-19 09:02:48 +0200

    Add output ports to notebooks (#264)
    * Fixes typos in client after bad merge
    
    * Created skeleton for storage service
    
    * Moving storage manager code from git@github.com:mguidon/aiohttp-dsm.git
    Adapting cli
    
    * Fixed pylint under src
    Added rest api generated models
    
    * Fixing linting errors in tests/
    Added some useful dev packages in requirements/dev.txt
    
    * Added client-sdk
    Adapted requirements and versioning to sync all versions (see issue #229)
    
    * Activated test in makefile
    
    * Changes after moving cookiecutter repo to itisfoundation
    
    * Merge mguidon openapi specs and took file models to a separate file
    
    * Fixing client package namespace
    
    * Adds service/storage to travis
    
    * Merged and adjusted changes of latest cookiecutter
    
    * Replaced model by attr simplified model
    
    * Added service-library
     - opeapi-core wrappers
     - rest support
     - constants
    
    * Adapted package to servicelib
     - entry point runs
     - fixed linter errors
    
    * Fixes test_resources.py
    
    * Fixes test_opeanpi.py
    
    * Shortened test_service.py and passes
    
    * WIP: fixes to make test_dsm.py run
    
    * Renamed simcore_servicelib to servicelib package
    Requirement files now bound with servicelib setup
    
    * WIP: fixing test_dsm.py to run
    
    * Minor cleanup
    
    * Implementing rest API handlers
    
    * Fixes setup version issues
    
    * - added submodule s3 to deail with minio service/setup
    - manual test of check_health and check_actions in api
    
    * Cleanup
    
    * Adds python2 virtualenv
    
    * storage service docker installs python2 virtualenv
    
    * Changed config schema to allocate python path and test data
    
    * Minor fixes in test
    
    * Fixes test_dsm
     - new python27 fixture
     - fixes to run python2 scripts from python3
    
    * Redefined test_framework as test_rest. Minimal rest api test.
    
    * Modifications to run tests in travis:
     - disabled tests that requires datacore accounts
     - installed python2 requirements as a fixture
    
    * Added pylint in the tests
    Tweaked pylint in global makefile
    
    * Removed python2 from travis
    Added pytest-profiler outputs to ignore
    
    * fixed merge
    
    * refactoring jupyter notebook
    
    * refactored makefile, docker file and compose for flexibility
    added 1 input file and 1 output file
    added development docker for testing
    
    * file name is kept in S3
    
    * removed scipy notebook (very large image size)
    fixed docker labels
    
    * improved development modeautomatic opening of notebook
    
    * removed scipy notebook
    
    * removed unused files
    
    * refactoring
    
    * changed dependency to psycop2-binary
    
    * fixed upload of folders
    
    * added parameterization of dummy tables
    
    * adapted kember to also have outputs
    
    * removed stuff from dashboard extension
    
    * the menubar is not hidden by default anymore
    added button to show/hide all outputs
    
    * themed base notebook
    uniformised labeling
    
    * bumped version of 2d graphs
    
    * same same as in use case
    
    * pylint
    
    * have base image as argument
    
    * pylint

--- services/dy-2Dgraph/use-cases/docker/boot.sh
+++ services/dy-2Dgraph/use-cases/docker/boot.sh
@@ -1,12 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 if test "${CREATE_DUMMY_TABLE}" = "1"
 then
+    pip install -r /home/jovyan/devel/requirements.txt
+    pushd /packages/simcore-sdk; pip install -r requirements-dev.txt; popd
+    pushd /packages/s3wrapper; pip install -r requirements-dev.txt; popd
+
     echo "Creating dummy tables ... using ${USE_CASE_CONFIG_FILE}"
-    result="$(python develdbs3init.py ${USE_CASE_CONFIG_FILE})"
+    result="$(python devel/initialise_dummy_platorm.py ${USE_CASE_CONFIG_FILE} ${INIT_OPTIONS})"
     echo "Received result node uuid of $result"
     export SIMCORE_NODE_UUID="$result"
 fi
+
 jupyter trust ${NOTEBOOK_URL}
 start-notebook.sh \
     --NotebookApp.token='' \
