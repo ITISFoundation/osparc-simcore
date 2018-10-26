fa6702fb367f84e822c8f8af530b1d718440c2ca
Manuel Guidon <guidon@itis.ethz.ch>
2018-10-26 14:30:09 +0200

    pylint

--- services/web/server/src/simcore_service_webserver/storage_handlers.py
+++ services/web/server/src/simcore_service_webserver/storage_handlers.py
@@ -26,36 +26,36 @@ async def get_storage_locations(request: web.Request):
     return envelope
 
 async def get_files_metadata(request: web.Request):
-    params, query, body = await extract_and_validate(request)
+    _params, _query, _body = await extract_and_validate(request)
     # get user_id, add to query and pass to storage
     raise NotImplementedError
 
 async def get_file_metadata(request: web.Request):
-    params, query, body = await extract_and_validate(request)
+    _params, _query, _body = await extract_and_validate(request)
 
     # get user_id, add to query and pass to storage
     raise NotImplementedError
 
 async def update_file_meta_data(request: web.Request):
-    params, query, body = await extract_and_validate(request)
+    _params, _query, _body = await extract_and_validate(request)
 
     # get user_id, add to query and pass to storage
     raise NotImplementedError
 
 async def download_file(request: web.Request):
-    params, query, body = await extract_and_validate(request)
+    _params, _query, _body = await extract_and_validate(request)
 
     # get user_id, add to query and pass to storage
     raise NotImplementedError
 
 async def upload_file(request: web.Request):
-    params, query, body = await extract_and_validate(request)
+    _params, _query, _body = await extract_and_validate(request)
 
     # get user_id, add to query and pass to storage
     raise NotImplementedError
 
 async def delete_file(request: web.Request):
-    params, query, body = await extract_and_validate(request)
+    _params, _query, _body = await extract_and_validate(request)
 
     # get user_id, add to query and pass to storage
     raise NotImplementedError
