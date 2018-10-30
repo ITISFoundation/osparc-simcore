880e764d539aecf11306e735f88795551e6563c5
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-10-30 17:33:24 +0100

    disabled conversion of services by director

--- services/director/src/simcore_service_director/config.py
+++ services/director/src/simcore_service_director/config.py
@@ -9,7 +9,7 @@ logging.basicConfig(
     format='%(levelname)s:%(name)s-%(lineno)d: %(message)s'
     )
 
-CONVERT_OLD_API = True
+CONVERT_OLD_API = False
 API_VERSION = "v0"
 
 REGISTRY_AUTH = os.environ.get("REGISTRY_AUTH", False) in ["true", "True"]
