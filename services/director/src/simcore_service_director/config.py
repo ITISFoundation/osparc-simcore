079b154cf306c55a9df93653dce6c124407e1292
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-10-31 00:43:38 +0100

    added published hostname variable for director to pass to services

--- services/director/src/simcore_service_director/config.py
+++ services/director/src/simcore_service_director/config.py
@@ -29,3 +29,6 @@ S3_ENDPOINT = os.environ.get("S3_ENDPOINT", "")
 S3_ACCESS_KEY = os.environ.get("S3_ACCESS_KEY", "")
 S3_SECRET_KEY = os.environ.get("S3_SECRET_KEY", "")
 S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "")
+
+# some services need to know the published host to be functional (paraview)
+PUBLISHED_HOST_NAME = os.environ.get("PUBLISHED_HOST_NAME", "")
