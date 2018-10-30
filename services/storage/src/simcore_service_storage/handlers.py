2fbe942798ec4e2285f6be6381d84cf59991914b
Manuel Guidon <guidon@itis.ethz.ch>
2018-10-31 00:13:09 +0100

    print url

--- services/storage/src/simcore_service_storage/handlers.py
+++ services/storage/src/simcore_service_storage/handlers.py
@@ -69,7 +69,7 @@ async def check_action(request: web.Request):
     return output
 
 async def get_storage_locations(request: web.Request):
-    log.info("CHECK LOCATION PATH %s",request.path)
+    log.info("CHECK LOCATION PATH %s %s",request.path, request.url)
 
     params, query, body = await extract_and_validate(request)
 
