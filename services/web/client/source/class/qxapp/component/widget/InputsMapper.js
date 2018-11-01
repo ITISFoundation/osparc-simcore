64b44400a445053128ea26dbd97f546b5f045271
odeimaiz <maiz@zurichmedtech.com>
2018-11-01 16:52:36 +0100

    InputsMapper widget added

---
+++ services/web/client/source/class/qxapp/component/widget/InputsMapper.js
@@ -0,0 +1,55 @@
+qx.Class.define("qxapp.component.widget.InputsMapper", {
+  extend: qx.ui.core.Widget,
+
+  construct: function(label) {
+    this.base();
+
+    let tree = this.__tree = new qx.ui.tree.VirtualTree(null, "label", "children").set({
+      openMode: "none"
+    });
+
+    tree.setDelegate({
+      bindItem: (c, item, id) => {
+        c.bindDefaultProperties(item, id);
+        c.bindProperty("key", "model", null, item, id);
+      },
+      configureItem: item => {
+        item.setDroppable(true);
+        item.addListener("dragover", e => {
+          let compatible = false;
+          if (e.supportsType("osparc-mapping")) {
+            const from = e.getRelatedTarget();
+            const to = e.getCurrentTarget();
+            compatible = true;
+            console.log(from, to);
+          }
+          if (!compatible) {
+            e.preventDefault();
+          }
+        });
+        item.addListener("drop", e => {
+          if (e.supportsType("osparc-mapping")) {
+            const from = e.getRelatedTarget();
+            const to = e.getCurrentTarget();
+            console.log("Map", from.path, "to", to.path);
+          }
+        });
+      }
+    });
+
+    let data = {
+      label: label,
+      children: []
+    };
+    let model = qx.data.marshal.Json.createModel(data, true);
+    tree.setModel(model);
+  },
+
+  members: {
+    __tree: null,
+
+    getOutputWidget: function() {
+      return this.__tree;
+    }
+  }
+});
