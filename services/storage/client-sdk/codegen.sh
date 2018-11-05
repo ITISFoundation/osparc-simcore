6ebe809b3902e063c7849abd7cf0e7d4d4950fff
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-11-05 10:47:32 +0100

    improved script to automatically remove output.yaml

--- services/storage/client-sdk/codegen.sh
+++ services/storage/client-sdk/codegen.sh
@@ -4,8 +4,10 @@ docker run -v $PWD/../src/simcore_service_storage/oas3/v0:/input \
             -v $PWD:/output \
             oas_resolver /input/openapi.yaml /output/output.yaml
 
-exec ../../../scripts/openapi/openapi_codegen.sh \
+../../../scripts/openapi/openapi_codegen.sh \
     -i output.yaml \
     -o . \
     -g python \
     -c ./codegen_config.json
+
+rm -f output.yaml
