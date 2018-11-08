6226cc0c2a7117e16e91dd33d132c53777d2b90b
odeimaiz <maiz@zurichmedtech.com>
2018-11-08 16:52:38 +0100

    prettified NodeOutputLabel

--- services/web/client/source/class/qxapp/component/widget/inputs/NodeOutputLabel.js
+++ services/web/client/source/class/qxapp/component/widget/inputs/NodeOutputLabel.js
@@ -6,15 +6,43 @@ qx.Class.define("qxapp.component.widget.inputs.NodeOutputLabel", {
 
     this.setNodeModel(nodeModel);
 
-    let toolTip = new qx.ui.tooltip.ToolTip(port.description);
-    let portLabel = this.__portLabel = new qx.ui.basic.Label(port.label).set({
+    this._setLayout(new qx.ui.layout.HBox(5));
+
+    const toolTip = new qx.ui.tooltip.ToolTip(port.description);
+    const title14Font = qx.bom.Font.fromConfig(qxapp.theme.Font.fonts["title-14"]);
+    let portLabel = new qx.ui.basic.Label("<b>" + port.label + "</b>: ").set({
       toolTip: toolTip,
+      font: title14Font,
       textAlign: "right",
       allowGrowX: true,
-      paddingRight: 20
+      padding: 15,
+      rich: true
     });
 
-    this.__createDragMechanism(portLabel, portKey);
+    let outputValue = "Unknown value";
+    if (Object.prototype.hasOwnProperty.call(port, "value")) {
+      if (typeof port.value === "object") {
+        outputValue = this.__pretifyObject(port.value);
+      } else {
+        outputValue = JSON.stringify(port.value);
+      }
+    }
+    let portValue = new qx.ui.basic.Label().set({
+      toolTip: toolTip,
+      font: title14Font,
+      textAlign: "right",
+      allowGrowX: true,
+      padding: 15,
+      rich: true,
+      value: outputValue
+    });
+
+    this._add(portLabel, {
+      flex: 1
+    });
+    this._add(portValue);
+
+    this.__createDragMechanism(this, portKey);
   },
 
   properties: {
@@ -25,8 +53,6 @@ qx.Class.define("qxapp.component.widget.inputs.NodeOutputLabel", {
   },
 
   members: {
-    __portLabel: null,
-
     __createDragMechanism: function(uiPort, portKey) {
       uiPort.setDraggable(true);
       uiPort.nodeId = this.getNodeModel().getNodeId();
@@ -40,8 +66,21 @@ qx.Class.define("qxapp.component.widget.inputs.NodeOutputLabel", {
       }, this);
     },
 
+    __pretifyObject: function(object) {
+      let myText = "";
+      const entries = Object.entries(object);
+      for (let i=0; i<entries.length; i++) {
+        const entry = entries[i];
+        myText += String(entry[0]);
+        myText += ": ";
+        myText += String(entry[1]);
+        myText += "<br/>";
+      }
+      return myText;
+    },
+
     getOutputWidget: function() {
-      return this.__portLabel;
+      return this;
     }
   }
 });
