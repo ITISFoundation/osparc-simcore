24da1c6900dc7deff560aba5fe93a05c93f9b008
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-11-08 13:21:19 +0100

    try to satisfy travis with log output

--- packages/simcore-sdk/tests/fixtures/storage.py
+++ packages/simcore-sdk/tests/fixtures/storage.py
@@ -1,5 +1,6 @@
 #pylint: disable=W0621, unused-argument
 import logging
+import threading
 
 import pytest
 import requests
@@ -9,7 +10,10 @@ from simcore_service_storage_sdk import ApiClient, Configuration, UsersApi
 
 log = logging.getLogger(__name__)
 
-def is_responsive(url, code=200):
+def _fake_logger_while_building_storage():
+    print("Hey Travis I'm still alive...")
+
+def _is_responsive(url, code=200):
     try:
         if requests.get(url).status_code == code:
             return True
@@ -25,11 +29,14 @@ def storage(bucket, engine, docker_ip, docker_services):
     port = docker_services.port_for('storage', 8080)
     endpoint = "http://{}:{}".format(host, port)
     # Wait until we can connect
+    keep_alive_timer = threading.Timer(interval=60.0, function=_fake_logger_while_building_storage)
+    keep_alive_timer.start()
     docker_services.wait_until_responsive(
-        check=lambda: is_responsive(endpoint, 404),
+        check=lambda: _is_responsive(endpoint, 404),
         timeout=20.0*60.0,
         pause=1.0,
     )
+    keep_alive_timer.cancel()
     
     yield endpoint
     # cleanup
