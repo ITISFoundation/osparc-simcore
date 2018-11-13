893d9373bf825c44ef76062fe9294b0317a61efe
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-11-13 17:46:00 +0100

    only allow doing stuff in the notebooks folder

--- services/dy-jupyter/docker/boot.sh
+++ services/dy-jupyter/docker/boot.sh
@@ -18,6 +18,8 @@ fi
 
 jupyter trust ${NOTEBOOK_URL}
 start-notebook.sh \
+    --NotebookApp.notebook_dir='/home/jovyan/notebooks' \
     --NotebookApp.token='' \
     --NotebookApp.tornado_settings="{\"headers\":{\"Content-Security-Policy\":\"frame-ancestors+'self'+http://osparc01.speag.com:9081;+report-uri/api/security/csp-report\"}}" \
-    --NotebookApp.default_url=/notebooks/${NOTEBOOK_URL}
+    # --NotebookApp.default_url='/notebooks' \
+    
