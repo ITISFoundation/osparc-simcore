e375dd1b946427dc86c2fa0867e59e4cd95687c5
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-11-13 21:41:51 +0100

    added comments

--- services/dy-jupyter/docker/boot.sh
+++ services/dy-jupyter/docker/boot.sh
@@ -21,5 +21,5 @@ start-notebook.sh \
     --NotebookApp.notebook_dir='/home/jovyan/notebooks' \
     --NotebookApp.token='' \
     --NotebookApp.tornado_settings="{\"headers\":{\"Content-Security-Policy\":\"frame-ancestors+'self'+http://osparc01.speag.com:9081;+report-uri/api/security/csp-report\"}}" \
-    # --NotebookApp.default_url='/notebooks' \
+    # --NotebookApp.default_url=/notebooks/${NOTEBOOK_URL} #uncomment this to start the notebook right away in that mode
     
