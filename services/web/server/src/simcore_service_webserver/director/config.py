2d6e1545463e35ad415af38bd03a20c26fa10d13
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-11-14 23:29:48 +0100

    added version to director schema fixed typo

--- services/web/server/src/simcore_service_webserver/director/config.py
+++ services/web/server/src/simcore_service_webserver/director/config.py
@@ -10,17 +10,18 @@ from aiohttp import ClientSession, web
 
 from servicelib.application_keys import APP_CONFIG_KEY
 
-APP_STORAGE_SESSION_KEY = __name__ + ".director_session"
+APP_DIRECTOR_SESSION_KEY = __name__ + ".director_session"
 CONFIG_SECTION_NAME = 'director'
 
 schema = T.Dict({
     T.Key("enabled", default=True, optional=True): T.Bool(),
     T.Key("host", default="director", ): T.String(),
-    T.Key("port", default=8001): T.Int()
+    T.Key("port", default=8001): T.Int(),
+    T.Key("version", default="v0"): T.String()
 })
 
 def get_config(app: web.Application) -> Dict:
     return app[APP_CONFIG_KEY][CONFIG_SECTION_NAME]
 
 def get_client_session(app: web.Application) -> ClientSession:
-    return app[APP_STORAGE_SESSION_KEY]
+    return app[APP_DIRECTOR_SESSION_KEY]
