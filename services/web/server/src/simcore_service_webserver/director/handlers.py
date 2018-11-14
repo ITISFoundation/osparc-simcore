65f37eaf9432bffba5fc32413efba8add93e2c75
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-11-15 00:29:24 +0100

    refactoring

--- services/web/server/src/simcore_service_webserver/director/handlers.py
+++ services/web/server/src/simcore_service_webserver/director/handlers.py
@@ -11,9 +11,10 @@ from .config import get_client_session, get_config
 
 log = logging.getLogger(__name__)
 
-
-async def _request_storage(request: web.Request, method: str):
+async def _request_storage(request: web.Request, method: str = None) -> web.Response:
     await extract_and_validate(request)
+    if method is None:
+        method = request.method
     # replace raw path, to keep the quotes
     url_path = request.rel_url.raw_path.replace("director/", "")
 
@@ -24,46 +25,32 @@ async def _request_storage(request: web.Request, method: str):
     url = urlbase.with_path(url_path).with_query(user_id=userid)
 
     session = get_client_session(request.app)
-    async with session.request(method.upper(), url, ssl=False) as resp:
+    async with session.request(str(method).upper(), url, ssl=False) as resp:
         payload = await resp.json()
         return payload
 
 @login_required
 async def running_interactive_services_post(request: web.Request):
-    payload = await _request_storage(request, 'POST')
+    payload = await _request_storage(request)
+    if payload.status == 200:
+        # TODO: add the service to the list of service for this user
+        pass
     return payload
-    # log.debug("client starts dynamic service %s", request)
-    # try:
-    #     service_key = data["serviceKey"]
-    #     service_version = "latest"
-    #     # if "serviceVersion" in data:
-    #     #     service_version = data["serviceVersion"]
-    #     node_id = data["nodeId"]
-    #     result = await interactive_services_manager.start_service(sid, service_key, node_id, service_version)
-    #     await sio.emit("startDynamic", data=result, room=sid)
-    # except IOError:
-    #     log.exception("Error emitting results")
-    # except Exception:
-    #     log.exception("Error while starting service")
 
 @login_required
 async def running_interactive_services_get(request: web.Request):
-    payload = await _request_storage(request, 'GET')
+    payload = await _request_storage(request)
     return payload
 
 @login_required
 async def running_interactive_services_delete(request: web.Request):
-    payload = await _request_storage(request, 'DELETE')
+    payload = await _request_storage(request)
+    if payload.status == 204:
+        #TODO: remove the service from the list of services for this user
+        pass
     return payload
-
-    # log.debug("client %s stops dynamic service %s", sid, data)
-    # try:
-    #     node_id = data["nodeId"]
-    #     await interactive_services_manager.stop_service(sid, node_id)
-    # except Exception:
-    #     log.exception("Error while stopping service")
 
 @login_required
 async def services_get(request):
-    payload = await _request_storage(request, 'GET')
+    payload = await _request_storage(request)
     return payload
