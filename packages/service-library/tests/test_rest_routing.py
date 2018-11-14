974feb593ad9fa553683dad340f48ff21612d9c4
Pedro Crespo <crespo@itis.ethz.ch>
2018-11-14 20:31:14 +0100

    Enhances auto routing in servicelib   - separation of concerns to allow filtering   - added strict/non-strict checks

--- packages/service-library/tests/test_rest_routing.py
+++ packages/service-library/tests/test_rest_routing.py
@@ -2,9 +2,12 @@
 # pylint: disable=unused-argument
 
 import pytest
+
 from servicelib import openapi
-from servicelib.rest_routing import (create_routes_from_map,
-                                     create_routes_from_namespace)
+from servicelib.rest_routing import (create_routes_from_namespace,
+                                     get_handlers_from_namespace,
+                                     iter_path_operations,
+                                     map_handlers_with_operations)
 from tutils import Handlers
 
 
@@ -16,19 +19,42 @@ def specs(here):
     return specs
 
 
-def test_create_routes_from_map(specs):
+def test_filtered_routing(specs):
     handlers = Handlers()
-    available_handlers = {'get_dict': handlers.get_dict, 'get_mixed': handlers.get_mixed}
-    routes = create_routes_from_map(specs, available_handlers)
+    found = get_handlers_from_namespace(handlers)
 
-    assert len(routes) == len(available_handlers)
+    hdl_sel = { name:hdl
+                    for name, hdl in found.items()
+                        if "i" in name
+    }
+    opr_iter = ( (mth, url, opname)
+                    for mth, url, opname in iter_path_operations(specs)
+                        if "i" in opname
+    )
+
+    routes = map_handlers_with_operations(hdl_sel, opr_iter, strict=True)
+
     for rdef in routes:
         assert rdef.method == "GET"
-        assert rdef.handler in available_handlers.values()
+        assert rdef.handler in hdl_sel.values()
+
 
 def test_create_routes_from_namespace(specs):
     handlers = Handlers()
-    routes = create_routes_from_namespace(specs, handlers)
+
+    # not - strict
+    try:
+        routes = create_routes_from_namespace(specs, handlers, strict=False)
+    except Exception: # pylint: disable=W0703
+        pytest.fail("Non-strict failed", pytrace=True)
+
+    # strict
+    with pytest.raises((RuntimeError, ValueError)):
+        routes = create_routes_from_namespace(specs, handlers, strict=True)
+
+    # Removing non-spec handler
+    handlers.get_health_wrong = None
+    routes = create_routes_from_namespace(specs, handlers, strict=True)
 
     assert len(routes) == len(specs.paths)
     for rdef in routes:
