6f4c843749d639871438da8c4750f5628dc7d94d
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-11-15 12:29:44 +0100

    call forwarding now also forwards the query parameters

--- services/web/server/src/simcore_service_webserver/director/handlers.py
+++ services/web/server/src/simcore_service_webserver/director/handlers.py
@@ -11,46 +11,49 @@ from .config import get_client_session, get_config
 
 log = logging.getLogger(__name__)
 
-async def _request_storage(request: web.Request, method: str = None) -> web.Response:
-    await extract_and_validate(request)
+async def _request_director(request: web.Request, method: str = None) -> web.Response:
+    params, query, body = await extract_and_validate(request)
     if method is None:
         method = request.method
     # replace raw path, to keep the quotes
-    url_path = request.rel_url.raw_path.replace("director/", "")
+    url_path = request.rel_url.raw_path
 
     cfg = get_config(request.app)
     urlbase = URL.build(scheme='http', host=cfg['host'], port=cfg['port'])
 
-    userid = request[RQT_USERID_KEY]
-    url = urlbase.with_path(url_path).with_query(user_id=userid)
+    # add the user id
+    userid = request[RQT_USERID_KEY]  
+    query["user_id"] = userid
+    url = urlbase.with_path(url_path).with_query(query)
 
+    # forward the call
     session = get_client_session(request.app)
-    async with session.request(str(method).upper(), url, ssl=False) as resp:
+    async with session.request(str(method).upper(), url, ssl=False, params=params, data=body) as resp:
         payload = await resp.json()
         return payload
 
 @login_required
 async def running_interactive_services_post(request: web.Request) -> web.Response:
-    payload = await _request_storage(request)
-    if payload.status == 200:
-        # TODO: add the service to the list of service for this user
-        pass
+    payload = await _request_director(request)
+    # if payload.status == 200:
+    #     # TODO: add the service to the list of service for this user
+    #     pass
     return payload
 
 @login_required
 async def running_interactive_services_get(request: web.Request) -> web.Response:
-    payload = await _request_storage(request)
+    payload = await _request_director(request)
     return payload
 
 @login_required
 async def running_interactive_services_delete(request: web.Request) -> web.Response:
-    payload = await _request_storage(request)
-    if payload.status == 204:
-        #TODO: remove the service from the list of services for this user
-        pass
+    payload = await _request_director(request)
+    # if payload.status == 204:
+    #     #TODO: remove the service from the list of services for this user
+    #     pass
     return payload
 
 @login_required
 async def services_get(request: web.Request) -> web.Response:
-    payload = await _request_storage(request)
+    payload = await _request_director(request)
     return payload
