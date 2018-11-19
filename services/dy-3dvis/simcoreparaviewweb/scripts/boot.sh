81109a3e3397463bb3bd5bfee50987177cd8e37e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-11-19 23:46:47 +0100

    creating a common script for dynamic services to upload file to a lightweight platform

--- services/dy-3dvis/simcoreparaviewweb/scripts/boot.sh
+++ services/dy-3dvis/simcoreparaviewweb/scripts/boot.sh
@@ -5,19 +5,19 @@ echo "current directory is ${PWD}"
 
 if [[ -v CREATE_DUMMY_TABLE ]];
 then
-    pushd /home/root/packages/simcore-sdk; pip install -r requirements-dev.txt; popd
-    pushd /home/root/packages/s3wrapper; pip install -r requirements-dev.txt; popd
+    pushd /home/root/packages/simcore-sdk; pip3 install -r requirements-dev.txt; popd
+    pushd /home/root/packages/s3wrapper; pip3 install -r requirements-dev.txt; popd
+    pushd /home/root/scripts/dy_services_helpers; pip3 install -r requirements.txt; popd
     # in dev mode, data located in mounted volume /test-data are uploaded to the S3 server
     # also a fake configuration is set in the DB to simulate the osparc platform
     echo "development mode, creating dummy tables..."
     # in style: pipelineid,nodeuuid
-    result="$(python3 devel/devel-initconfiguration.py ${USE_CASE_CONFIG_FILE})";
+    result="$(python3 scripts/dy_services_helpers/platform_initialiser.py ${USE_CASE_CONFIG_FILE} --folder ${TEST_DATA_PATH})";
     echo "Received result of $result";
     IFS=, read -a array <<< "$result"; 
     echo "Received result pipeline id of ${array[0]}";
     echo "Received result node uuid of ${array[1]}";
     # the fake SIMCORE_NODE_UUID is exported to be available to the service
-    export SIMCORE_PIPELINE_ID="${array[0]}";
     export SIMCORE_NODE_UUID="${array[1]}";
 fi
 
