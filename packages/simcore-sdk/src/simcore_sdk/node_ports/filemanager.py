4245546c1d2601ac121191c46fec17fd08b64d97
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-11-21 16:34:09 +0100

    pylint

--- packages/simcore-sdk/src/simcore_sdk/node_ports/filemanager.py
+++ packages/simcore-sdk/src/simcore_sdk/node_ports/filemanager.py
@@ -5,7 +5,6 @@ from pathlib import Path
 
 import aiofiles
 import aiohttp
-import async_timeout
 from yarl import URL
 
 from simcore_service_storage_sdk import ApiClient, Configuration, UsersApi
@@ -76,7 +75,6 @@ async def _get_upload_link(store_id:int, file_id:str, api:UsersApi):
 
 async def _download_link_to_file(session:aiohttp.ClientSession, url:URL, file_path:Path, store: str, s3_object: str):
     log.debug("Downloading from %s to %s", url, file_path)
-    # with async_timeout.timeout(10):
     async with session.get(url) as response:
         if response.status == 404:
             raise exceptions.S3InvalidPathError(s3_object)
