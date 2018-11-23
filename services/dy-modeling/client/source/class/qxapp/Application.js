8f20c507dc746435723fb9903e29ece260a01395
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-11-23 14:57:35 +0100

    the application sends a confirmation when it receives the scene

--- services/dy-modeling/client/source/class/qxapp/Application.js
+++ services/dy-modeling/client/source/class/qxapp/Application.js
@@ -201,13 +201,12 @@ qx.Class.define("qxapp.Application", {
     _initSignals: function() {
       this._socket.addListener("connect", function() {
         console.log("connecting to server via websocket...");
-        // if (!this._socket.slotExists("importModelScene")) {
-        this._socket.on("importModelScene", function(val) {
+        this._socket.on("importModelScene", function(val, ackCb) {
+          ackCb();
           if (val.type === "importModelScene") {
             this.__threeView.importSceneFromBuffer(val.value);
           }
         }, this);
-        // }
       }, this);
       this._socket.addListener("disconnect", function() {
         console.log("disconnected from server websocket");
@@ -217,14 +216,12 @@ qx.Class.define("qxapp.Application", {
       }, this);
       this._socket.addListener("reconnect", function(e) {
         console.log("REconnecting to server via websocket...");
-        // if (!this._socket.slotExists("importModelScene")) {
         this._socket.on("importModelScene", function(val, ackCb) {
           ackCb();
           if (val.type === "importModelScene") {
             this.__threeView.importSceneFromBuffer(val.value);
           }
         }, this);
-        // }
       }, this);
 
       // Menu bar
