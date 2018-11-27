ac55bc9e4d007606d4a0c63175c6fdc683964326
Pedro Crespo <crespo@itis.ethz.ch>
2018-11-27 11:59:30 +0100

    WIP: application proxy implementation using new director api

--- services/web/server/src/simcore_service_webserver/application_proxy.py
+++ services/web/server/src/simcore_service_webserver/application_proxy.py
@@ -19,6 +19,7 @@ from .director.director_sdk import (ApiException, UsersApi,
                                     create_director_api_client)
 from .reverse_proxy import setup_reverse_proxy
 from .reverse_proxy.abc import ServiceResolutionPolicy
+from servicelib.rest_responses import unwrap_envelope
 
 logger = logging.getLogger(__name__)
 
@@ -28,40 +29,49 @@ class ServiceMonitor(ServiceResolutionPolicy):
     # See API specs in api/specs/director/v0/openapi.yaml
     cli: UsersApi
 
-    # caches consecutive calls to same service_identifier
-    info_service_id: str=None
-    info_service_image: str=None
-    info_service_url: str=None
+    async def _request_info(self, service_identifier: str):
+        data = {}
 
-    async def request_info(self, service_identifier: str):
         # GET /running_interactive_services/{service_uuid}
         #  200 -> RunningServiceEnveloped
         #  404 not found
+
+        # TODO: see if client can cache consecutive calls. SEE self.cli.api_client.last_response is a
+        # https://docs.aiohttp.org/en/stable/client_reference.html#response-object
         try:
-            service = await self.cli.running_interactive_services_get(service_uuid=service_identifier)
+            response, _status, _headers = await self.cli.running_interactive_services_get(service_uuid=service_identifier)
+
+            payload = await response.json()
+            data, _error = unwrap_envelope(payload)
+
         except ApiException:
             # FIXME: define error treatment policy!?
-            logger.exception("Failed to request service info %s", str)
+            logger.exception("Failed to request service info %s", service_identifier)
+            data.clear()
 
-        self.info_service_id = service.service_uuid
-        self.info_service_image = "{}:{}".format(service.service_key, service.service_version)
-
-        # FIXME:Not sure format is correct!?
-        self.info_service_url = service.entry_point + ":%d" % service.published_port \
-                                + self.base_mountpoint + "/" + self.info_service_id
+        return data
 
     # override
     async def get_image_name(self, service_identifier: str) -> str:
-        if not self.info_service_image or self.info_service_id != service_identifier:
-            await self.request_info(service_identifier)
-        return self.info_service_image
+        data = await self._request_info(service_identifier)
+        #data.get('service_uuid')
+        #data.get('service_basepath')
+        #data.get('service_host')
+        #data.get('service_port')
+        #data.get('service_version')
+        return data.get('service_key')
+
 
     # override
     async def find_url(self, service_identifier: str) -> URL:
-        if not self.info_service_url or self.info_service_id != service_identifier:
-            await self.request_info(service_identifier)
-        return self.info_service_url
+        """ Returns the url = origin + mountpoint of the backend dynamic service identified
 
+        """
+        data = await self._request_info(service_identifier)
+        return URL.build(scheme="http",
+                         host=data.get('service_host'),
+                         port=data.get('service_port'),
+                         path=data.get('service_basepath', "/"))
 
 
 def setup(app: web.Application):
