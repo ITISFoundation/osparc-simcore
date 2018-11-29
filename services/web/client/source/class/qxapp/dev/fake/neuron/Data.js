114990465920fa160454c5f0900a0586b4466e4b
odeimaiz <maiz@zurichmedtech.com>
2018-11-29 15:19:18 +0100

    modeler-neuron fake data added

---
+++ services/web/client/source/class/qxapp/dev/fake/neuron/Data.js
@@ -0,0 +1,282 @@
+/* eslint-disable no-trailing-spaces */
+qx.Class.define("qxapp.dev.fake.neuron.Data", {
+  type: "static",
+
+  statics: {
+    itemList: [
+      {
+        label: "Splines/Axon_0",
+        key: "Axon_0 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_1",
+        key: "Axon_1 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_2",
+        key: "Axon_2 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_3",
+        key: "Axon_3 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_4",
+        key: "Axon_4 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_5",
+        key: "Axon_5 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_6",
+        key: "Axon_6 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_7",
+        key: "Axon_7 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_8",
+        key: "Axon_8 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_9",
+        key: "Axon_9 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_10",
+        key: "Axon_10 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_11",
+        key: "Axon_11 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_12",
+        key: "Axon_12 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_13",
+        key: "Axon_13 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_14",
+        key: "Axon_14 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_15",
+        key: "Axon_15 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_16",
+        key: "Axon_16 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_17",
+        key: "Axon_17 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_18",
+        key: "Axon_18 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_19",
+        key: "Axon_19 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_20",
+        key: "Axon_20 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_21",
+        key: "Axon_21 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_22",
+        key: "Axon_22 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_23",
+        key: "Axon_23 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_24",
+        key: "Axon_24 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_25",
+        key: "Axon_25 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_26",
+        key: "Axon_26 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_27",
+        key: "Axon_27 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_28",
+        key: "Axon_28 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_29",
+        key: "Axon_29 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_30",
+        key: "Axon_30 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_31",
+        key: "Axon_31 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_32",
+        key: "Axon_32 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_33",
+        key: "Axon_33 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_34",
+        key: "Axon_34 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_35",
+        key: "Axon_35 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_36",
+        key: "Axon_36 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_37",
+        key: "Axon_37 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_38",
+        key: "Axon_38 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_39",
+        key: "Axon_39 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_40",
+        key: "Axon_40 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_41",
+        key: "Axon_41 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_42",
+        key: "Axon_42 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_43",
+        key: "Axon_43 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_44",
+        key: "Axon_44 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_45",
+        key: "Axon_45 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_46",
+        key: "Axon_46 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_47",
+        key: "Axon_47 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_48",
+        key: "Axon_48 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_49",
+        key: "Axon_49 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_50",
+        key: "Axon_50 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_51",
+        key: "Axon_51 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_52",
+        key: "Axon_52 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_53",
+        key: "Axon_53 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_54",
+        key: "Axon_54 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_55",
+        key: "Axon_55 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_56",
+        key: "Axon_56 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_57",
+        key: "Axon_57 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_58",
+        key: "Axon_58 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_59",
+        key: "Axon_59 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_60",
+        key: "Axon_60 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_61",
+        key: "Axon_61 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_62",
+        key: "Axon_62 ___Splines_-UUID"
+      },
+      {
+        label: "Splines/Axon_63",
+        key: "Axon_63 ___Splines_-UUID"
+      }      
+    ],
+
+    compare: function(a, b) {
+      if (a.label < b.label) {
+        return -1;
+      }
+      if (a.label > b.label) {
+        return 1;
+      }
+      return 0;
+    },
+
+    getItemList: function() {
+      let itemList = qxapp.dev.fake.neuron.Data.itemList;
+      itemList.sort(this.compare);
+      return itemList;
+    }
+  } // statics
+
+});
