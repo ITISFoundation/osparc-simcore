557dbeb73c9d74dc1e45fbc18512c149196fad71
Pedro Crespo <crespo@itis.ethz.ch>
2018-11-30 19:03:38 +0100

    Added some comments

--- scripts/docker/healthcheck_curl_host.py
+++ scripts/docker/healthcheck_curl_host.py
@@ -1,3 +1,24 @@
+""" Healthcheck script to run inside docker
+
+Example of usage in a Dockerfile
+```
+    COPY --chown=scu:scu scripts/docker/healthcheck_curl_host.py /healthcheck/healthcheck_curl_host.py
+    HEALTHCHECK --interval=30s \
+                --timeout=30s \
+                --start-period=1s \
+                --retries=3 \
+                CMD [ "python", "/healthcheck/healthcheck_curl_host.py", "http://localhost:8080/v0/" ]
+    ...
+```
+
+
+Q&A:
+    1. why not to use curl instead of a python script?
+        - SEE https://blog.sixeyed.com/docker-healthchecks-why-not-to-use-curl-or-iwr/
+
+"""
+
+
 import os
 import sys
 
