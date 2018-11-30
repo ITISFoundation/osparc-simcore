047657c6fedb70870b65ce558724b41847089c5e
Pedro Crespo <crespo@itis.ethz.ch>
2018-11-30 22:22:36 +0100

    Added environ to control login config variable

--- services/web/server/src/simcore_service_webserver/login/config.py
+++ services/web/server/src/simcore_service_webserver/login/config.py
@@ -12,5 +12,5 @@ CONFIG_SECTION_NAME = 'login'
 # TODO: merge with cfg.py
 schema = T.Dict({
     T.Key("enabled", default=True, optional=True): T.Bool(),
-    T.Key("registration_confirmation_required", default=DEFAULTS["REGISTRATION_CONFIRMATION_REQUIRED"], optional=True): T.Bool(),
+    T.Key("registration_confirmation_required", default=DEFAULTS["REGISTRATION_CONFIRMATION_REQUIRED"], optional=True): T.Or(T.Bool, T.Int),
 })
