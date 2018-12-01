6a7a7af2557efa08306a78d87a2adfeedb58d8fd
Pedro Crespo <crespo@itis.ethz.ch>
2018-12-01 14:59:30 +0100

    Cleanup webserver.login.setup

--- services/web/server/src/simcore_service_webserver/login/__init__.py
+++ services/web/server/src/simcore_service_webserver/login/__init__.py
@@ -4,12 +4,13 @@ This submodule is a modification of aiohttp-login
 
  TODO: create stand-alone fork of aiohttp-login
 """
+import asyncio
 import logging
 
 import asyncpg
 from aiohttp import web
 
-from servicelib.application_keys import APP_CONFIG_KEY, APP_DB_POOL_KEY
+from servicelib.application_keys import APP_CONFIG_KEY
 
 from ..db import DSN
 from ..db_config import CONFIG_SECTION_NAME as DB_SECTION
@@ -17,18 +18,25 @@ from ..email_config import CONFIG_SECTION_NAME as SMTP_SECTION
 from ..rest_config import APP_OPENAPI_SPECS_KEY
 from ..statics import INDEX_RESOURCE_NAME
 from .cfg import APP_LOGIN_CONFIG, cfg
+from .config import CONFIG_SECTION_NAME
 from .routes import create_routes
 from .storage import AsyncpgStorage
-from .config import CONFIG_SECTION_NAME
 
 log = logging.getLogger(__name__)
 
+TIMEOUT_SECS = 5
 
-async def pg_pool(app: web.Application):
+
+async def _cleanup_context(app: web.Application):
     login_cfg = app[APP_CONFIG_KEY].get(CONFIG_SECTION_NAME, {}) # optional!
     stmp_cfg = app[APP_CONFIG_KEY][SMTP_SECTION]
     db_cfg = app[APP_CONFIG_KEY][DB_SECTION]['postgres']
 
+    # db
+    pool = await asyncpg.create_pool(dsn=DSN.format(**db_cfg), loop=app.loop)
+    storage = AsyncpgStorage(pool) #NOTE: this key belongs to cfg, not settings!
+
+    # config
     config = {}
     for key, value in login_cfg.items():
         config[key.upper()] = value
@@ -37,18 +45,25 @@ async def pg_pool(app: web.Application):
         config["SMTP_{}".format(key.upper())] = value
 
     config['APP'] = app
-
-    app[APP_DB_POOL_KEY] = await asyncpg.create_pool(dsn=DSN.format(**db_cfg), loop=app.loop)
-    config["STORAGE"] = AsyncpgStorage(app[APP_DB_POOL_KEY]) #NOTE: this key belongs to cfg, not settings!
+    config["STORAGE"] = storage
 
     cfg.configure(config)
 
     if INDEX_RESOURCE_NAME in app.router:
         cfg['LOGIN_REDIRECT'] = app.router[INDEX_RESOURCE_NAME].url_for()
     else:
-        log.warning("Unknown location for login page. Defaulting redirection to %s", cfg['LOGIN_REDIRECT'] )
+        log.warning("Unknown location for login page. Defaulting redirection to %s",
+                        cfg['LOGIN_REDIRECT'] )
 
     app[APP_LOGIN_CONFIG] = cfg
+
+    yield
+
+    try:
+        await asyncio.wait_for( pool.close(), timeout=TIMEOUT_SECS, loop=app.loop)
+    except asyncio.TimeoutError:
+        log.exception("Failed to close login storage loop")
+
 
 
 def setup(app: web.Application):
@@ -70,7 +85,7 @@ def setup(app: web.Application):
     app.router.add_routes(routes)
 
     # signals
-    app.on_startup.append(pg_pool)
+    app.cleanup_ctx.append(_cleanup_context)
 
 
 # alias
