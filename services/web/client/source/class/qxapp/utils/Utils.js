e4a7675e0259e0cf0db54b4db21c5d2bb53e7ec5
odeimaiz <maiz@zurichmedtech.com>
2018-12-04 00:48:55 +0100

    minor

--- services/web/client/source/class/qxapp/utils/Utils.js
+++ services/web/client/source/class/qxapp/utils/Utils.js
@@ -9,7 +9,7 @@ qx.Class.define("qxapp.utils.Utils", {
       return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
         (c ^ window.crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
     },
-    
+
     replaceTemplateUUIDs: function(data) {
       let dataStr = JSON.stringify(data);
       data = JSON.parse(dataStr);
