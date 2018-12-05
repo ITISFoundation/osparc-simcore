448fbdc7f6bf535dd36bb963a9833bce0b6f79c3
Pedro Crespo-Valero <pedro.crespovalero@gmail.com>
2018-12-05 02:56:15 +0100

    Cleanup webserver.db  - reraises if cannot connect to db after retry  - Stops if cannot init db

--- services/web/server/src/simcore_service_webserver/db.py
+++ services/web/server/src/simcore_service_webserver/db.py
@@ -12,8 +12,7 @@ from aiopg.sa import create_engine
 from tenacity import before_sleep_log, retry, stop_after_attempt, wait_fixed
 
 from servicelib.aiopg_utils import DBAPIError
-from servicelib.application_keys import (APP_CONFIG_KEY, APP_DB_ENGINE_KEY,
-                                         APP_DB_SESSION_KEY)
+from servicelib.application_keys import APP_CONFIG_KEY, APP_DB_ENGINE_KEY
 
 # from .computation_api import init_database as _init_db
 from .db_config import CONFIG_SECTION_NAME
@@ -35,7 +34,8 @@ log = logging.getLogger(__name__)
 
 @retry( wait=wait_fixed(RETRY_WAIT_SECS),
         stop=stop_after_attempt(RETRY_COUNT),
-        before_sleep=before_sleep_log(log, logging.INFO) )
+        before_sleep=before_sleep_log(log, logging.INFO),
+        reraise=True)
 async def __create_tables(**params):
     # TODO: move _init_db.metadata here!?
     sa_engine = sa.create_engine(DSN.format(**params))
@@ -46,24 +46,20 @@ async def pg_engine(app: web.Application):
     try:
         cfg = app[APP_CONFIG_KEY][CONFIG_SECTION_NAME]
         params = {k:cfg["postgres"][k] for k in 'database user password host port minsize maxsize'.split()}
+
+        if cfg.get("init_tables"):
+            # TODO: get keys from __name__ (see notes in servicelib.application_keys)
+            await __create_tables(**params)
+        
         engine = await create_engine(**params)
 
-        # TODO: get keys from __name__ (see notes in servicelib.application_keys)
-        if cfg.get("init_tables"):
-            await __create_tables(**params)
-
     except DBAPIError:
-        log.exception("Could not create engine")
-
-    session = None
-    app[APP_DB_ENGINE_KEY] = engine
-    app[APP_DB_SESSION_KEY] = session # placeholder for session (if needed)
+        log.exception("Could init db. Stopping :\n %s", cfg)
+        raise
+    else:
+        app[APP_DB_ENGINE_KEY] = engine
 
     yield
-
-    session = app.get(APP_DB_SESSION_KEY)
-    if session:
-        session.close()
 
     engine = app.get(APP_DB_ENGINE_KEY)
     if engine:
@@ -97,14 +93,12 @@ def setup(app: web.Application):
 
     cfg = app[APP_CONFIG_KEY][CONFIG_SECTION_NAME]
 
-    if not cfg["enabled"]:
-        app[APP_DB_ENGINE_KEY] = app[APP_DB_SESSION_KEY] = None
-        log.warning("Service '%s' explicitly disabled in config", THIS_SERVICE_NAME)
-        return
-
     # ensures keys exist
     app[APP_DB_ENGINE_KEY] = None
-    app[APP_DB_SESSION_KEY] = None
+
+    if not cfg["enabled"]:
+        log.warning("Service '%s' explicitly disabled in config", THIS_SERVICE_NAME)
+        return
 
     # async connection to db
     # app.on_startup.append(_init_db) # TODO: review how is this disposed
