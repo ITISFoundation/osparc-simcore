a53c2541b303c0e15691b2abae6ef801f7802b3e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-12-11 14:11:57 +0100

    code cleanup fix uploading too many times compressed file

--- services/dy-jupyter/handlers/input_retriever.py
+++ services/dy-jupyter/handlers/input_retriever.py
@@ -3,25 +3,30 @@ import shutil
 import tarfile
 import tempfile
 from pathlib import Path
+import logging
 
 from notebook.base.handlers import IPythonHandler
 from notebook.utils import url_path_join
 from simcore_sdk import node_ports
 
+# logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+_FILE_TYPE_PREFIX = "data:"
 
 async def download_data(inputs_path):
-    print("retrieving data...")
+    logger.info("retrieving data from simcore...")
     PORTS = node_ports.ports()
 
     values = {}
     for port in PORTS.inputs:        
         if not port or port.value is None:
             continue
-        print("downloading data from port '{}' with value '{}'...".format(port.key, port.value))
+        logger.debug("downloading data from port '%s' with value '%s'...", port.key, port.value)
         value = await port.get()
         values[port.key] = {"key": port.key, "value": value}
 
-        if "data:" in port.type:
+        if _FILE_TYPE_PREFIX in port.type:
             dest = inputs_path / port.key
             dest.mkdir(exist_ok=True, parents=True)
             dest = dest / Path(value).name
@@ -31,46 +36,47 @@ async def download_data(inputs_path):
     values_file = inputs_path / "key_values.json"
     with values_file.open('w') as fp:
         json.dump(values, fp)
+    logger.info("all data retrieved from simcore: %s", values_file)
 
 async def upload_data(outputs_path):
-    print("retrieving data...")
+    logger.info("uploading data to simcore...")
     PORTS = node_ports.ports()
 
-    values = {}
     for port in PORTS.outputs:        
-        print("uploading data from port '{}' with value '{}'...".format(port.key, port.value))
-        value = await port.get()
-        values[port.key] = {"key": port.key, "value": value}
-
-        if "data:" in port.type:
+        logger.debug("uploading data to port '%s' with value '%s'...", port.key, port.value)
+        if _FILE_TYPE_PREFIX in port.type:
             src_folder = outputs_path / port.key
-            list_files = list(src_folder.glob("*.*"))
+            list_files = list(src_folder.glob("*"))
             if len(list_files) == 1:
-                # directly upload
+                # special case, direct upload
                 await port.set(list_files[0])
-            else:
-                temp_file = tempfile.NamedTemporaryFile(suffix=".tgz")
-                temp_file.close()
+                continue
+            # generic case let's create an archive
+            temp_file = tempfile.NamedTemporaryFile(suffix=".tgz")
+            temp_file.close()
+            for _file in list_files:
                 with tarfile.open(temp_file.name, mode='w:gz') as tar_ptr:
                     for file_path in list_files:
                         tar_ptr.add(file_path, arcname=file_path.name, recursive=False)
-                await port.set(temp_file.name)
-                Path(temp_file).unlink()
+            await port.set(temp_file.name)
+            Path(temp_file.name).unlink()
+    logger.info("all data uploaded to simcore")
+
+def _create_ports_sub_folders(ports: node_ports._items_list.ItemsList, parent_path: Path):
+    for port in ports:
+        if _FILE_TYPE_PREFIX in port.type:
+            sub_folder = parent_path / port.key
+            sub_folder.mkdir(exist_ok=True, parents=True)
 
 class RetrieveHandler(IPythonHandler):
 
-    async def initialize(self):
+    def initialize(self):
         self.inputs_path = Path("~/inputs").expanduser()
         self.outputs_path = Path("~/outputs").expanduser()
-        create_ports_sub_folders(self.inputs_path)
-        create_ports_sub_folders(self.outputs_path)
-
-        def create_ports_sub_folders(ports: node_ports._items_list.ItemsList, parent_path: Path):
-            for port in ports:
-                if "data:" in port.type:
-                    sub_folder = parent_path / port.key
-                    sub_folder.mkdir(exist_ok=True, parents=True)
-
+        PORTS = node_ports.ports()
+        _create_ports_sub_folders(PORTS.inputs, self.inputs_path)
+        _create_ports_sub_folders(PORTS.outputs, self.outputs_path)
+    
     async def get(self):
         await download_data(self.inputs_path)
         await upload_data(self.outputs_path)
