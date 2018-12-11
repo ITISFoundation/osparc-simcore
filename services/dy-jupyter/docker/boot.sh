eebc1d99262def326322b1bbcd5f6a91420288e5
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-12-11 10:20:37 +0100

    code cleanup and dependencies

--- services/dy-jupyter/docker/boot.sh
+++ services/dy-jupyter/docker/boot.sh
@@ -4,7 +4,6 @@ if test "${CREATE_DUMMY_TABLE}" = "1"
 then
     pip install -r /home/jovyan/devel/requirements.txt
     pushd /home/jovyan/packages/simcore-sdk; pip install -r requirements-dev.txt; popd
-    pushd /home/jovyan/packages/s3wrapper; pip install -r requirements-dev.txt; popd
 
     echo "Creating dummy tables ... using ${USE_CASE_CONFIG_FILE}"
     result="$(python scripts/dy_services_helpers/platform_initialiser_csv_files.py ${USE_CASE_CONFIG_FILE} ${INIT_OPTIONS})"
@@ -23,5 +22,4 @@ start-notebook.sh \
     --NotebookApp.notebook_dir='/home/jovyan/notebooks' \
     --NotebookApp.token='' \
     --NotebookApp.nbserver_extensions="{'input_retriever':True}"
-    #--NotebookApp.tornado_settings="{\"headers\":{\"Content-Security-Policy\":\"frame-ancestors+'self'+http://osparc01.itis.ethz.ch:9081;+report-uri/api/security/csp-report\"}}" \
     # --NotebookApp.default_url=/notebooks/${NOTEBOOK_URL} #uncomment this to start the notebook right away in that notebook
