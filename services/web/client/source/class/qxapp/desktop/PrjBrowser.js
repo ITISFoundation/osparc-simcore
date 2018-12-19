fb8bc8d1fc6362c2ee3426853a792b8256c23eab
odeimaiz <maiz@zurichmedtech.com>
2018-12-19 14:29:39 +0100

    sort list in Browser by lastChangeDate

--- services/web/client/source/class/qxapp/desktop/PrjBrowser.js
+++ services/web/client/source/class/qxapp/desktop/PrjBrowser.js
@@ -505,7 +505,25 @@ qx.Class.define("qxapp.desktop.PrjBrowser", {
     },
 
     __getProjectArrayModel: function(prjList) {
-      return new qx.data.Array(
+      let sortByProperty = function(prop) {
+        return function(a, b) {
+          if (prop === "lastChangeDate") {
+            return new Date(b[prop]) - new Date(a[prop]);
+          }
+          if (typeof a[prop] == "number") {
+            return a[prop] - b[prop];
+          }
+          if (a[prop] < b[prop]) {
+            return -1;
+          } else if (a[prop] > b[prop]) {
+            return 1;
+          }
+          return 0;
+        };
+      };
+      prjList.sort(sortByProperty("lastChangeDate"));
+
+      let prjArray = new qx.data.Array(
         prjList
           .map(
             (p, i) => qx.data.marshal.Json.createModel({
