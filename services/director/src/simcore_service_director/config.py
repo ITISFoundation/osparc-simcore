8de63f42b55624ca4e6e7bdd011b18bec45c321d
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-01-09 10:47:29 +0100

    fixed reading of booleans from env variable

--- services/director/src/simcore_service_director/config.py
+++ services/director/src/simcore_service_director/config.py
@@ -15,7 +15,7 @@ REGISTRY_AUTH = os.environ.get("REGISTRY_AUTH", False) in ["true", "True"]
 REGISTRY_USER = os.environ.get("REGISTRY_USER", "")
 REGISTRY_PW = os.environ.get("REGISTRY_PW", "")
 REGISTRY_URL = os.environ.get("REGISTRY_URL", "")
-REGISTRY_SSL = os.environ.get("REGISTRY_SSL", True)
+REGISTRY_SSL = os.environ.get("REGISTRY_SSL", True) in ["true", "True"]
 EXTRA_HOSTS_SUFFIX = os.environ.get("EXTRA_HOSTS_SUFFIX", "undefined")
 
 # these are the envs passed to the dynamic services by default
