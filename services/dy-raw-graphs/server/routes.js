36055353f7d2c287a21638b5d8b9e75f9ea577e0
odeimaiz <maiz@zurichmedtech.com>
2019-01-25 15:48:43 +0100

    added api ends for listing and reading files

--- services/dy-raw-graphs/server/routes.js
+++ services/dy-raw-graphs/server/routes.js
@@ -8,6 +8,7 @@ const path = require('path');
 let appRouter = express.Router();
 const events = require('events');
 const config = require('./config');
+const fs = require('fs')
 
 let eventEmitter = new events.EventEmitter()
 
@@ -18,9 +19,45 @@ appRouter.get('/', function (request, response) {
   response.sendFile(path.resolve(config.APP_PATH, 'index.html'));
 });
 
+appRouter.get('/inputs', getInputFiles);
+
+appRouter.get('/input', getInputFile);
+
 appRouter.get('/retrieve', callInputRetriever);
 
 module.exports = appRouter;
+
+function getInputFiles(request, response) {
+  console.log('getInputFiles');
+  const inputsDir = '../inputs/'
+  fs.readdir(inputsDir, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    let metadata = [];
+    for (let i=0; i<files.length; i++) {
+      metadata.push({
+        title: files[i],
+        type: 'Other',
+        url: inputsDir + files[i]
+      });
+    }
+    response.send(metadata);
+  });
+}
+
+function getInputFile(request, response) {
+  const fileName = request.query["fileName"]
+  console.log('getInputFile', fileName);
+  fs.readFile(fileName, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    response.send(data);
+  });
+}
 
 function callInputRetriever(request, response) {
   console.log('Received a call to retrieve the data on input ports from ' + request.ip);
