9cbbfc18a87b83affaf7b1f1d8c59e36ffb93017
Pedro Crespo <crespo@itis.ethz.ch>
2019-01-28 17:22:03 +0100

    Fixes director Dockerfile using same workaround in pip as with storage

--- services/director/docker/boot.sh
+++ services/director/docker/boot.sh
@@ -7,10 +7,10 @@ then
   echo "INFO: Booting in development mode ..."
   echo "DEBUG: Director running as `id $(whoami)`"
   echo "DEBUG: Director running groups `groups`"
-  
+
   echo "Installing director service ..."
   cd $HOME/services/director
-  $PIP install --no-cache-dir -r requirements/dev.txt
+  $PIP install -r requirements/dev.txt
   $PIP list
   cd $HOME
   simcore-service-director --loglevel=debug
@@ -18,5 +18,3 @@ else
   echo "INFO: Booting in production mode ..."
   simcore-service-director --loglevel=info
 fi
-
-
