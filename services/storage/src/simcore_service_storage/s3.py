2a43f4eb8227d07041ea9de9727833121d566f83
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2019-01-31 00:13:25 +0100

    Is537/fix system testing (#542)
    -  fixes known security vulnerabilities issues for docker 3.5
    - fixes permissions issue with pip-cache in development mode (``make up-swarm-devel``)
    - fixes webserver boot error ``pkg_resources.UnknownExtra: SQLAlchemy 1.2.9 has no such extra feature 'postgresql_psycopg2binary'`` in docker devel mode.
    -  fixes system-testing failure test_warm_runs::test_services_running due to failure starting storage (crashes when it cannot connect to minio)
    
    ----
    * fixes knonw security vulnerabilities issues of docker 3.5.0 (https://pyup.io/account/repos/github/ITISFoundation/osparc-simcore/commits/?page=1#86bf015463e3624b7c41bdb0a39546598b1b712e)
    
    * Moves development stage at the end to reduce buid time of Dockefiles targeting production
    
    * Added more verbose to tests
    
    * Fixes permissions issue with .pipcache folder in development mode for webserver
    
    * Fixes permissions issue with pipcache folder in other services
    
    * Fixes security vulnerability in pyyaml
     Changes to be committed:
    	modified:   api/tests/requirements.txt
    
    * Changes in storage:
     - rest subsystem retries until apihub service is running
     - sorts setup order
    
    * Removing pyup services. waiting for issue #243
    
    * fixes webserver boot error pkg_resources.UnknownExtra: SQLAlchemy 1.2.9 has no such extra feature 'postgresql_psycopg2binary' in docker devel mode
    
    * Fixes failure during setup in storage.s3. Now retries to connect to minio
    
    * Improved system-testing log and fixtures
     - created parametrized fixture per service
     - if service fails, prints container log
     Changes to be committed:
    	modified:   ops/travis/system-testing/tests/test_swarm_runs.py
    
    * Forgot in previous commits
    
    * Fixes storage retry
     Changes to be committed:
    	modified:   services/storage/src/simcore_service_storage/utils.py

--- services/storage/src/simcore_service_storage/s3.py
+++ services/storage/src/simcore_service_storage/s3.py
@@ -1,6 +1,7 @@
 """ Module to access s3 service
 
 """
+import asyncio
 import logging
 from typing import Dict
 
@@ -9,10 +10,34 @@ from aiohttp import web
 from s3wrapper.s3_client import S3Client
 
 from .settings import APP_CONFIG_KEY, APP_S3_KEY
+from .utils import RETRY_COUNT, RETRY_WAIT_SECS
 
 log = logging.getLogger(__name__)
 
 _SERVICE_NAME = 's3'
+
+async def _on_startup_and_cleanup(app):
+    # setup
+    s3_client = app[APP_S3_KEY]
+    cfg = app[APP_CONFIG_KEY]
+
+    s3_cfg = cfg[_SERVICE_NAME]
+    s3_bucket = s3_cfg["bucket_name"]
+
+    ok, failures_count = False, 0
+    while not ok:
+        try:
+            s3_client.create_bucket(s3_bucket)
+            ok = True
+        except Exception: # pylint: disable=W0703
+            failures_count +=1
+            if failures_count>RETRY_COUNT:
+                raise
+            await asyncio.sleep(RETRY_WAIT_SECS)
+    yield
+    # tear-down
+    app[APP_S3_KEY] = None
+
 
 def setup(app: web.Application):
     """ minio/s3 service setup"""
@@ -29,17 +54,12 @@ def setup(app: web.Application):
     s3_access_key = s3_cfg["access_key"]
     s3_endpoint = s3_cfg["endpoint"]
     s3_secret_key = s3_cfg["secret_key"]
-    s3_bucket = s3_cfg["bucket_name"]
     s3_secure = s3_cfg["secure"]
 
-    secure = s3_secure == 1
-
-    s3_client = S3Client(s3_endpoint, s3_access_key, s3_secret_key, secure=secure)
-    s3_client.create_bucket(s3_bucket)
-
+    s3_client = S3Client(s3_endpoint, s3_access_key, s3_secret_key, secure=s3_secure == 1)
     app[APP_S3_KEY] = s3_client
 
-
+    app.cleanup_ctx.append(_on_startup_and_cleanup)
 
 def get_config(app: web.Application) -> Dict:
     cfg = app[APP_CONFIG_KEY][_SERVICE_NAME]
