9ece56e1ed46128927cf3bdb2e2ed85f4259320b
Pedro Crespo <crespo@itis.ethz.ch>
2019-02-08 17:41:35 +0100

    Updated test_application_proxy.py  - Upgraded to interface calls  - Test parameterized currently for jupyter and modeler  - Added a wait until dynamic service is responsive  - parameterized API_VERSION

--- services/web/server/tests/integration-proxy/test_application_proxy.py
+++ services/web/server/tests/integration-proxy/test_application_proxy.py
@@ -20,6 +20,8 @@ from simcore_service_webserver.application import (APP_CONFIG_KEY,
 from simcore_service_webserver.reverse_proxy.settings import PROXY_MOUNTPOINT
 
 
+API_VERSION = 'v0'
+
 @pytest.fixture
 #def webserver_service(loop, app_config, director_service, aiohttp_unused_port, aiohttp_server, here):
 def webserver_service(loop, app_config, aiohttp_unused_port, aiohttp_server, here):
@@ -44,7 +46,7 @@ def webserver_service(loop, app_config, aiohttp_unused_port, aiohttp_server, her
 
     setup_rest(app, debug=True)
     setup_director(app, disable_login=True)
-    setup_app_proxy(app) # <-----------|
+    setup_app_proxy(app) # <-----------|UNDER TEST
 
     server = loop.run_until_complete(aiohttp_server(app, port=port))
     return server
@@ -59,18 +61,24 @@ def client(loop, webserver_service,  aiohttp_client):
 
 
 # TESTS ----------------------------------------------------------------------------
+# + [(service_key, "????", "NJKfISIRB-%d"%i) for i, service_key in enumerate(rp_jupyter.SUPPORTED_IMAGE_NAME)]
 
 @pytest.mark.parametrize("service_key,service_version,service_uuid", [
- (rp_jupyter.SUPPORTED_IMAGE_NAME, "1.7.0", "NJKfISIRB"),
- ("simcore/services/dynamic/modeler/webserver", "2.7.0", "4k4zZL90S"),
- #(rp_paraview.SUPPORTED_IMAGE_NAME, "1.0.5", "EkE7LSU0r"),
-])
+    (rp_jupyter.SUPPORTED_IMAGE_NAME[0], "1.7.0", "NJKfISIRB"),
+    ("simcore/services/dynamic/modeler/webserver", "2.7.0", "4k4zZL90S"),
+    #(rp_paraview.SUPPORTED_IMAGE_NAME, "1.0.5", "EkE7LSU0r"),
+    #("simcore/services/dynamic/raw-graphs", "2.8.0", "4J6GoxSNL"),
+    ])
 async def test_reverse_proxy_workflow(client, service_key, service_version, service_uuid):
     """
         client <--> webserver <--> director
+
+        - Tests interaction webserver<--->director
+        - Tests webserserver.director subsystem
+        - Tests webserserver.reverser proxy subsystem as well
     """
-    # List services in registry ----------------
-    resp = await client.get("/v0/services?service_type=interactive")
+    # List services in registry ------------------------------------------------
+    resp = await client.get("/"+API_VERSION+"/services?service_type=interactive")
     assert resp.status == 200, (await resp.text())
 
     payload = await resp.json()
@@ -78,10 +86,11 @@ async def test_reverse_proxy_workflow(client, service_key, service_version, serv
     assert data
     assert not error
 
-    assert any(srv['key']==service_key and srv['version']==service_version for srv in data)
+    assert any(srv['key']==service_key and srv['version']==service_version for srv in data), \
+        "version of service NOT listed in registry"
 
-    # Start backend dynamic service --------------
-    resp = await client.post( URL("/v0/running_interactive_services").with_query(
+    # Start backend dynamic service ------------------------------------------------
+    resp = await client.post( URL("/"+API_VERSION+"/running_interactive_services").with_query(
          service_key=service_key,
          service_version =service_version,
          service_uuid = service_uuid)
@@ -96,11 +105,23 @@ async def test_reverse_proxy_workflow(client, service_key, service_version, serv
     service_basepath = data['service_basepath']
     assert service_basepath == PROXY_MOUNTPOINT + "/" + service_uuid
 
-    # TODO: wait until service is responsive!
-    await asyncio.sleep(5)
+    # Wait until service is responsive----------------------------------------------
+    #TODO: all dynamic services boot time should be bounded!!
+    WAIT_FIXED_SECS = 5
+    MAX_TRIALS = 5
 
+    count = 0
+    while count<MAX_TRIALS:
+        resp = await client.get("/"+API_VERSION+"/running_interactive_services/{}".format(service_uuid))
+        # TODO: noticed some inconsistenciew between director's API and director's section of webserver API
+        if resp.status==200:
+            break
+        await asyncio.sleep(WAIT_FIXED_SECS) # Does this make a difference being async??
+        count +=1
 
-    # Communicate with backend dynamic service --------------
+    assert resp.status == 200, (await resp.text())
+
+    # Talk with backend dynamic service ---------------------------------------------
     # TODO: webserver should not respond identical to the director!!
     service_basepath = PROXY_MOUNTPOINT + "/" + service_uuid
 
@@ -110,6 +131,6 @@ async def test_reverse_proxy_workflow(client, service_key, service_version, serv
     assert content
     assert resp.status == 200, content
 
-    # Stop backend dynamic service
-    resp = await client.delete("/v0/running_interactive_services/{}".format(service_uuid))
+    # Stop backend dynamic service ----------------------------------------------------
+    resp = await client.delete("/"+API_VERSION+"/running_interactive_services/{}".format(service_uuid))
     assert resp.status == 204, (await resp.text())
