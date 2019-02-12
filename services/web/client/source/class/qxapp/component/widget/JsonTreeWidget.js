e22e19d0266d7ff68d3b8a185b9667e4d6a7cec7
odeimaiz <maiz@zurichmedtech.com>
2019-02-12 13:39:10 +0100

    Doc: JsonTreeWidget

--- services/web/client/source/class/qxapp/component/widget/JsonTreeWidget.js
+++ services/web/client/source/class/qxapp/component/widget/JsonTreeWidget.js
@@ -15,9 +15,26 @@
 
 ************************************************************************ */
 
+/**
+ * Widget containing a JsonTreeViewer dom element
+ *
+ * *Example*
+ *
+ * Here is a little example of how to use the widget.
+ *
+ * <pre class='javascript'>
+ *   let jsonTreeWidget = new qxapp.component.widget.JsonTreeWidget(data, "elemId");
+ *   this.getRoot().add(jsonTreeWidget);
+ * </pre>
+ */
+
 qx.Class.define("qxapp.component.widget.JsonTreeWidget", {
   extend: qx.ui.core.Widget,
 
+  /**
+    * @param data {Object} Json object to be displayed by JsonTreeViewer
+    * @param elemId {String} Element id to set it as dom attribute
+  */
   construct: function(data, elemId) {
     this.base();
 
