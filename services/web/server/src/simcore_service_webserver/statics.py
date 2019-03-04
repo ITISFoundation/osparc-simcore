f6d9c26659971069059756707c28e7205e5a5759
odeimaiz <maiz@zurichmedtech.com>
2019-03-04 13:09:50 +0100

    minor

--- services/web/server/src/simcore_service_webserver/statics.py
+++ services/web/server/src/simcore_service_webserver/statics.py
@@ -39,6 +39,7 @@ async def index(request: web.Request):
     log.debug("index.request:\n %s", request)
 
     index_path = get_client_outdir(request.app) / APP_TO_BE_SERVED / "index.html"
+    log.debug("Serving %s ...", index_path)
     with index_path.open() as ofh:
         return web.Response(text=ofh.read(), content_type="text/html")
 
