d1158b571811c6de5e6ccd327c00a7e2d35a1195
odeimaiz <maiz@zurichmedtech.com>
2019-03-07 11:49:22 +0100

    clean up cell editor

--- services/web/client/source/class/qxapp/component/widget/cell/Editor.js
+++ services/web/client/source/class/qxapp/component/widget/cell/Editor.js
@@ -9,13 +9,10 @@ qx.Class.define("qxapp.component.widget.cell.Editor", {
     this._setLayout(new qx.ui.layout.VBox(10));
 
     let controls = this._createChildControlImpl("controls");
-    this._addAt(controls, 0);
-
-    // let loadingUri = qx.util.ResourceManager.getInstance().toUri("dashGrid/raw/index.html");
-    let loadingUri = qx.util.ResourceManager.getInstance().toUri("qxapp/loading/loader.html");
-    let iframe = new qx.ui.embed.Iframe(loadingUri);
-    iframe.setBackgroundColor("transparent");
-    this.setContent(iframe);
+    this._add(controls);
+    this._add(cellData.getNode().getIFrame(), {
+      flex: 1
+    });
   },
 
   properties: {
@@ -42,48 +39,21 @@ qx.Class.define("qxapp.component.widget.cell.Editor", {
           }, this);
           control.add(back);
 
-          control.add(new qx.ui.core.Spacer(), {
-            flex: 1
-          });
+          control.add(new qx.ui.core.Spacer(100, null));
 
-          let titleField = this.__titleField = new qx.ui.form.TextField(this.tr("title")).set({
-            maxLength: 25,
+          let titleField = new qx.ui.basic.Label().set({
+            alignY: "middle",
             minWidth: 200
           });
-          titleField.addListener("changeValue", e => {
-            this.getHandler().setTitle(e.getData());
-          }, this);
+          this.getHandler().getNode()
+            .bind("label", titleField, "value");
           control.add(titleField);
-
-          control.add(new qx.ui.core.Spacer(), {
-            flex: 1
-          });
-
-          let change = new qx.ui.form.Button(this.tr("Change content"));
-          change.addListener("execute", e => {
-            this.getHandler().createRandomData();
-            this.setContent(this.getHandler().getEditor());
-          }, this);
-          control.add(change);
 
           break;
         }
       }
 
       return control || this.base(arguments, id);
-    },
-
-    getTitle: function() {
-      this.__titleField.getValue();
-    },
-
-    setContent: function(content) {
-      if (this._getChildren().length > 1) {
-        this._removeAt(1);
-      }
-      this._addAt(content, 1, {
-        flex: 1
-      });
     }
   }
 });
