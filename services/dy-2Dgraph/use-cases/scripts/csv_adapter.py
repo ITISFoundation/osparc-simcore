69eb72771c937bb63f3b18b535b03d671be7e0b8
odeimaiz <maiz@zurichmedtech.com>
2019-03-18 14:25:06 +0100

    renaming

--- services/dy-2Dgraph/use-cases/scripts/csv_adapter.py
+++ services/dy-2Dgraph/use-cases/scripts/csv_adapter.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from simcore_sdk import node_ports
 
 
-async def export_to_csv(data_frame, title, header=False, port_number=0):
+async def pandas_dataframe_to_csv(data_frame, title, header=False, port_number=0):
     title = title.replace(" ", "_") + ".csv"
     dummy_file_path = Path(title)
     data_frame.to_csv(dummy_file_path, sep=',', header=header, index=False, encoding='utf-8')
