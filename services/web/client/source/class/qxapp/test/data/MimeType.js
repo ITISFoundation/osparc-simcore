72e3b901121d952c13e5ce5f4f6e13067e76dc4b
odeimaiz <maiz@itis.swiss>
2019-03-20 14:25:02 +0100

    MimeType test added

---
+++ services/web/client/source/class/qxapp/test/data/MimeType.js
@@ -0,0 +1,52 @@
+/* ************************************************************************
+
+   qxapp - the simcore frontend
+
+   https://osparc.io
+
+   Copyright:
+     2019 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+/**
+ * Test MimeType class
+ *
+ */
+qx.Class.define("qxapp.test.data.MimeType",
+  {
+    extend: qx.dev.unit.TestCase,
+    include: [qx.dev.unit.MRequirements, qx.dev.unit.MMock],
+
+    members:
+    {
+      setUp: function() {
+        console.debug("Setting up .. ");
+        this.debug("Setting up ...");
+      },
+
+      tearDown: function() {
+        console.debug("Tear down .. ");
+        this.debug("Tear down ...");
+        this.getSandbox().restore();
+      },
+
+      /*
+      ---------------------- -----------------------------------------------------
+        TESTS
+      ---------------------------------------------------------------------------
+      */
+
+      testMatch: function() {
+        const a = "data:*/*";
+        const b = "data:text/csv";
+        this.assert(a.match(b), "*/* should match everything");
+      }
+    }
+  });
