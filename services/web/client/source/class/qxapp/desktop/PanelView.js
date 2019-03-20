8fdaa712001ae8e08c59cffe1486aa7519676c8a
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-03-20 11:07:19 +0100

    Use Grow instead of canvas

--- services/web/client/source/class/qxapp/desktop/PanelView.js
+++ services/web/client/source/class/qxapp/desktop/PanelView.js
@@ -112,7 +112,7 @@ qx.Class.define("qxapp.desktop.PanelView", {
 
     _applyContent: function(content, oldContent) {
       if (this.__innerContainer === null) {
-        this.__innerContainer = new qx.ui.container.Composite(new qx.ui.layout.Canvas()).set({
+        this.__innerContainer = new qx.ui.container.Composite(new qx.ui.layout.Grow()).set({
           appearance: "panelview-content",
           decorator: "panelview-content",
           visibility: this.getContentVisibility() ? "visible" : "excluded"
@@ -141,12 +141,7 @@ qx.Class.define("qxapp.desktop.PanelView", {
 
       this.__innerContainer.removeAll();
       content.setMinHeight(0);
-      this.__innerContainer.add(content, {
-        top: 0,
-        right: 0,
-        left: 0,
-        bottom: 0
-      });
+      this.__innerContainer.add(content);
 
       if (this.__caret === null) {
         this.__caret = new qx.ui.basic.Image(this.getContentVisibility() ? this.self().LESS_CARET : this.self().MORE_CARET).set({
