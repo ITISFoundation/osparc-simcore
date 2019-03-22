2d2025062b6d0ebfb3e6ad9c6fa372294faf9936
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-03-22 10:22:56 +0100

    allow configuring where jsonschema is located

--- services/director/src/simcore_service_director/rest/node_validator.py
+++ services/director/src/simcore_service_director/rest/node_validator.py
@@ -2,30 +2,29 @@ import json
 import logging
 
 from jsonschema import (
-    SchemaError, 
-    ValidationError, 
+    SchemaError,
+    ValidationError,
     validate)
 from simcore_service_director import (
-    exceptions, 
+    exceptions,
     resources)
 
 log = logging.getLogger(__name__)
 
 
-with resources.stream(resources.RESOURCE_NODE_SCHEMA) as fp:
-    schema = json.load(fp)
-
 def is_service_valid(service):
-    try:
-        validate(service, schema)
-        log.debug("service [%s] validated", service["key"])
-        return True
-    except ValidationError:
-        log.exception("Node validation error:")
-        return False
-    except SchemaError:
-        log.exception("Schema error:")        
-        raise exceptions.DirectorException("Incorrect json schema used from %s" % (resources.get_path(resources.RESOURCE_NODE_SCHEMA)))
+    with resources.stream(resources.RESOURCE_NODE_SCHEMA) as fp:
+        schema = json.load(fp)
+        try:
+            validate(service, schema)
+            log.debug("service [%s] validated", service["key"])
+            return True
+        except ValidationError:
+            log.exception("Node validation error:")
+            return False
+        except SchemaError:
+            log.exception("Schema error:")
+            raise exceptions.DirectorException("Incorrect json schema used from %s" % (resources.get_path(resources.RESOURCE_NODE_SCHEMA)))
 
 def validate_nodes(services):
     validated_services = []
