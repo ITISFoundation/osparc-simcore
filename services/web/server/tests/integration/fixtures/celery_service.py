a1b7f9e9f807a68d7451fbfb7c0bcbd78735ca6e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-04-02 14:43:45 +0200

    pylint

--- services/web/server/tests/integration/fixtures/celery_service.py
+++ services/web/server/tests/integration/fixtures/celery_service.py
@@ -8,9 +8,9 @@ import celery
 import celery.bin.base
 import celery.bin.celery
 import celery.platforms
-
 import pytest
 import tenacity
+
 
 @pytest.fixture(scope="module")
 def celery_service(app_config, docker_stack):
