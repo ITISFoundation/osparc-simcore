8647591a04bd9b414b3808c0b48e3b7009cadc12
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-04-08 10:31:18 +0200

    set project uuid to be unique in db

--- services/web/server/src/simcore_service_webserver/projects/projects_models.py
+++ services/web/server/src/simcore_service_webserver/projects/projects_models.py
@@ -41,7 +41,7 @@ projects = sa.Table("projects", metadata,
     sa.Column("id", sa.BigInteger, nullable=False, primary_key=True),
     sa.Column("type", sa.Enum(ProjectType), nullable=False, default=ProjectType.STANDARD),
 
-    sa.Column("uuid", sa.String, nullable=False),
+    sa.Column("uuid", sa.String, nullable=False, unique=True),
     sa.Column("name", sa.String, nullable=False),
     sa.Column("description", sa.String, nullable=False),
     sa.Column("notes", sa.String, nullable=False),
