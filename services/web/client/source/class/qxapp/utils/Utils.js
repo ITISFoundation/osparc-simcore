5be26c332e0761b2f21bdc86b9ea6fdb7a2ae9e9
odeimaiz <maiz@itis.swiss>
2019-04-24 10:03:40 +0200

    minor cleanup

--- services/web/client/source/class/qxapp/utils/Utils.js
+++ services/web/client/source/class/qxapp/utils/Utils.js
@@ -209,26 +209,23 @@ qx.Class.define("qxapp.utils.Utils", {
       //
 
       // Place in top-left corner of screen regardless of scroll position.
-      textArea.style.position = "fixed";
-      textArea.style.top = 0;
-      textArea.style.left = 0;
-
       // Ensure it has a small width and height. Setting to 1px / 1em
       // doesn't work as this gives a negative w/h on some browsers.
-      textArea.style.width = "2em";
-      textArea.style.height = "2em";
-
       // We don't need padding, reducing the size if it does flash render.
-      textArea.style.padding = 0;
-
       // Clean up any borders.
-      textArea.style.border = "none";
-      textArea.style.outline = "none";
-      textArea.style.boxShadow = "none";
-
       // Avoid flash of white box if rendered for any reason.
-      textArea.style.background = "transparent";
-
+      textArea.style = {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "2em",
+        height: "2em",
+        padding: 0,
+        border: "none",
+        outline: "none",
+        boxShadow: "none",
+        background: "transparent"
+      };
       textArea.value = text;
 
       document.body.appendChild(textArea);
