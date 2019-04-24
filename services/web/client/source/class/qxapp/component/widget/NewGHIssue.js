8a403f7fd4dfc15e88ecc37cbfae4f5506d7a182
odeimaiz <maiz@itis.swiss>
2019-04-24 09:52:33 +0200

    NewGHIssue is just a static class

--- services/web/client/source/class/qxapp/component/widget/NewGHIssue.js
+++ services/web/client/source/class/qxapp/component/widget/NewGHIssue.js
@@ -25,18 +25,7 @@
  */
 
 qx.Class.define("qxapp.component.widget.NewGHIssue", {
-  extend: qx.ui.core.Widget,
-
-  construct: function() {
-    this.base(arguments);
-
-    this._setLayout(new qx.ui.layout.VBox(5));
-
-    this.__createSteps();
-    this._add(new qx.ui.core.Spacer(null, 10));
-    this.__createGHLinkButton();
-    this.__createCopyEnvButton();
-  },
+  type: "static",
 
   statics: {
     getNewIssueUrl: function() {
@@ -78,53 +67,6 @@ qx.Class.define("qxapp.component.widget.NewGHIssue", {
 
 ## Your environment
 `;
-    }
-  },
-
-  members: {
-    __issueTitle: null,
-    __issueDescription: null,
-    __mdEditor: null,
-
-    __createSteps: function() {
-      [
-        this.tr("Follow these steps to Open a New Issue:"),
-        this.tr("1) Press the 'Copy environment information' button. This will copy some relevant environment information to the clipboard."),
-        this.tr("2) Press the 'Open New Issue' button. This will open a 'Open New Issue' tab in GitHub."),
-        this.tr("3) In GitHub, in the '## Your environment' section, paste what have been automaticaly copied"),
-        this.tr("4) Fill up the information about the issue.")
-      ].forEach(step => {
-        const label = this.__createHelpLabel(step);
-        this._add(label);
-      });
-    },
-
-    __createHelpLabel: function(message=null) {
-      const label = new qx.ui.basic.Label().set({
-        value: message,
-        rich : true,
-        font : qx.bom.Font.fromConfig(qxapp.theme.Font.fonts["text-14"])
-      });
-      return label;
-    },
-
-    __createCopyEnvButton: function() {
-      const copyEnvBtn = new qx.ui.form.Button(this.tr("Copy environment information")).set({
-        allowGrowX: false
-      });
-      copyEnvBtn.addListener("execute", () => {
-        const env = JSON.stringify(qxapp.component.widget.NewGHIssue.getEnv());
-        if (qxapp.utils.Utils.copyTextToClipboard(env)) {
-          copyEnvBtn.addState("hovered");
-        }
-      }, this);
-      this._add(copyEnvBtn);
-    },
-
-    __createGHLinkButton: function() {
-      const url = qxapp.component.widget.NewGHIssue.getNewIssueUrl();
-      const toNewIssue = new qxapp.component.widget.LinkButton(this.tr("Open New Issue"), url);
-      this._add(toNewIssue);
     }
   }
 });
