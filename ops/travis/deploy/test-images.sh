2fd7e92f825a0190fa3c273b88f9167adfc5695c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-04-29 15:07:59 +0200

    set deployment for test

--- ops/travis/deploy/test-images.sh
+++ ops/travis/deploy/test-images.sh
@@ -3,6 +3,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+slugify () {
+    echo "$1" | iconv -t ascii//TRANSLIT | sed -r s/[~\^]+//g | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z
+}
+
 # show current images on system
 docker images
 
@@ -13,11 +17,8 @@ env
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # TRAVIS_PLATFORM_STAGE_VERSION=staging-$(date +"%Y-%m-%d").${TRAVIS_BUILD_NUMBER}.$(git rev-parse HEAD)
-# export DOCKER_IMAGE_PREFIX=itisfoundation
-# export DOCKER_IMAGE_TAG=master
-# make tag
-# make push
+export DOCKER_IMAGE_PREFIX=${DOCKER_REGISTRY}
+export DOCKER_IMAGE_TAG=$(slugify "${TRAVIS_BRANCH}-latest")
 
-# export DOCKER_IMAGE_TAG=$TRAVIS_PLATFORM_STAGE_VERSION
-# make tag
-# make push
+make tag
+make push
