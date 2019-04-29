fd916e0f7e16fa758298a15b82c83eeb36d4f26e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-04-29 13:15:27 +0200

    added x flag

--- ops/travis/deploy/master.sh
+++ ops/travis/deploy/master.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-# as of http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\n\t'
+
+# show current images on system
+docker images
+
 # these variable must be available securely from travis
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+TRAVIS_PLATFORM_STAGE_VERSION=staging-$(date +"%Y-%m-%d").${TRAVIS_BUILD_NUMBER}.$(git rev-parse HEAD)
 export DOCKER_IMAGE_PREFIX=itisfoundation
-export DOCKER_IMAGE_TAG_PREFIX=master
+export DOCKER_IMAGE_TAG=master
+make tag
+make push
+
+export DOCKER_IMAGE_TAG=$TRAVIS_PLATFORM_STAGE_VERSION
+make tag
 make push
