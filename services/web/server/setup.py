9984a9acc074d9b0be2c4ca867b79d9f3b4c4f3a
Pedro Crespo <crespo@itis.ethz.ch>
2019-05-06 18:15:54 +0200

    webserver: Removed ci-base and updated  - updates steup  - renamed specs with _ prefix to distinguish from shortcuts  - simplified requirements/makefile

--- services/web/server/setup.py
+++ services/web/server/setup.py
@@ -13,13 +13,13 @@ def read_reqs( reqs_path: Path):
 
 #-----------------------------------------------------------------
 
-install_requirements = read_reqs( here / "requirements" / "base.txt" ) + [
+install_requirements = read_reqs( here / "requirements" / "_base.txt" ) + [
     "s3wrapper==0.1.0",
     "simcore-sdk==0.1.0",
     "simcore-service-library==0.1.0",
     "simcore-director-sdk==1.0.0"  # FIXME: notice that this is version 1 while the others are 0!
 ]
-test_requirements = read_reqs( here / "requirements" / "test.in" )
+test_requirements = read_reqs( here / "requirements" / "_test.txt" )
 
 setup(
     name='simcore-service-webserver',
