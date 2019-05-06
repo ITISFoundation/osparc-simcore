d216496a7f8c99a95a4f5dc68b7c054e1a1170af
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-05-06 16:31:38 +0200

    created deployment scripts for staging, production

--- ops/travis/deploy/staging.sh
+++ ops/travis/deploy/staging.sh
@@ -2,9 +2,30 @@
 # as of http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\n\t'
+
+# pull the current master build
+export DOCKER_IMAGE_PREFIX=${DOCKER_REGISTRY}/
+export DOCKER_IMAGE_TAG="master-latest"
+make pull
+
+# show current images on system
+docker images
+
 # these variable must be available securely from travis
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-export DOCKER_IMAGE_PREFIX=itisfoundation
-export DOCKER_IMAGE_TAG_PREFIX=staging
+# re-tag master build to staging-latest
+export DOCKER_IMAGE_PREFIX_NEW=${DOCKER_IMAGE_PREFIX}
+export DOCKER_IMAGE_TAG_NEW=staging-latest
+make tag
+export DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG_NEW}
+export DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG_NEW}
+make push
+
+# re-tag master to staging-DATE.BUILD_NUMBER.GIT_SHA
+TRAVIS_PLATFORM_STAGE_VERSION=staging-$(date +"%Y-%m-%d").${TRAVIS_BUILD_NUMBER}.$(git rev-parse HEAD)
+export DOCKER_IMAGE_TAG_NEW=$TRAVIS_PLATFORM_STAGE_VERSION
+make tag
+export DOCKER_IMAGE_PREFIX=${DOCKER_IMAGE_PREFIX_NEW}
+export DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG_NEW}
 make push
