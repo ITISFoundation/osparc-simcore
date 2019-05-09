50759824b43e39ed9b38cc795a31abe099dac708
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-05-09 09:00:28 +0200

    Make eslint happy.

--- services/web/client/source/class/qxapp/utils/Utils.js
+++ services/web/client/source/class/qxapp/utils/Utils.js
@@ -275,10 +275,10 @@ qx.Class.define("qxapp.utils.Utils", {
 
       getCookie: cname => {
         var name = cname + "=";
-        var ca = document.cookie.split(';');
-        for(var i = 0; i < ca.length; i++) {
+        var ca = document.cookie.split(";");
+        for (var i = 0; i < ca.length; i++) {
           var c = ca[i];
-          while (c.charAt(0) == ' ') {
+          while (c.charAt(0) == " ") {
             c = c.substring(1);
           }
           if (c.indexOf(name) == 0) {
