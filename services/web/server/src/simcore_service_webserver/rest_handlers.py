adc413bc684a711199cc5ee4119bbe82b6e50fca
Pedro Crespo <crespo@itis.ethz.ch>
2019-05-22 11:34:14 +0200

    Doc [skip ci-travis]

--- services/web/server/src/simcore_service_webserver/rest_handlers.py
+++ services/web/server/src/simcore_service_webserver/rest_handlers.py
@@ -50,6 +50,16 @@ async def check_action(request: web.Request):
 
 
 async def get_config(request: web.Request):
+    """
+        This entrypoint aims to provide an extra configuration mechanism for
+        the front-end app.
+
+        Some of the server configuration can be forwarded to the front-end here
+
+        Example use case: the front-end app is served to the client. Then the user wants to
+        register but the server has been setup to require an invitation. This option is setup
+        at runtime and the front-end can only get it upon request to /config
+    """
     params, query, body = await extract_and_validate(request)
 
     assert not params
@@ -59,6 +69,7 @@ async def get_config(request: web.Request):
     cfg = request.app[APP_CONFIG_KEY]
     login_cfg = cfg.get('login',{})
 
+    # Schema api/specs/webserver/v0/components/schemas/config.yaml
     data = {
         'invitation_required': login_cfg.get('registration_invitation_required', False)
     }
