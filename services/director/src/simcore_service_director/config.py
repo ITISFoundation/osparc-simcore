ff2bb9278820e62a1a34504ff81370d78499af42
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-05-28 23:11:05 +0200

    added configuration to define service prefix name

--- services/director/src/simcore_service_director/config.py
+++ services/director/src/simcore_service_director/config.py
@@ -31,6 +31,10 @@ SERVICES_DEFAULT_ENVS = {
 
 # some services need to know the published host to be functional (paraview)
 PUBLISHED_HOST_NAME = os.environ.get("PUBLISHED_HOST_NAME", "")
+# used when in devel mode vs release mode
 NODE_SCHEMA_LOCATION = os.environ.get("NODE_SCHEMA_LOCATION",
     "{root}/{version}/schemas/node-meta-v0.0.1.json".format(root=API_ROOT, version=API_VERSION))
+# used to find the right network name
 SWARM_STACK_NAME = os.environ.get("SWARM_STACK_NAME")
+# useful when developing with an alternative registry namespace
+SIMCORE_SERVICES_PREFIX = os.environ.get("SIMCORE_SERVICES_PREFIX", "simcore/services")
