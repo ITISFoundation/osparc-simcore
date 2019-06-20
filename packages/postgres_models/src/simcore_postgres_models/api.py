8522170fcd82b08a6ebfc237a7d60692fb5ef4cb
Pedro Crespo <crespo@itis.ethz.ch>
2019-06-20 21:54:56 +0200

    Added first migration revision

---
+++ packages/postgres_models/src/simcore_postgres_models/api.py
@@ -0,0 +1,7 @@
+# FIXME: temporary import from other modules until all models are found. Then revert the import!
+
+from simcore_sdk.models import metadata # imports pipeline stuff as well
+import simcore_service_webserver.db_models
+import simcore_service_webserver.projects
+
+#import simcore_service_storage.models
