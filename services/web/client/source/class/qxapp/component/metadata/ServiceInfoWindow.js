a04ee7b23fc1e790f7b6da0b050cf7eb4737ea15
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-06-28 09:35:34 +0200

    Added untracked file ServiceInfoWindow

---
+++ services/web/client/source/class/qxapp/component/metadata/ServiceInfoWindow.js
@@ -0,0 +1,31 @@
+/*
+ * oSPARC - The SIMCORE frontend - https://osparc.io
+ * Copyright: 2019 IT'IS Foundation - https://itis.swiss
+ * License: MIT - https://opensource.org/licenses/MIT
+ * Authors: Ignacio Pascual (ignapas)
+ */
+
+qx.Class.define("qxapp.component.metadata.ServiceInfoWindow", {
+  extend: qx.ui.window.Window,
+  construct: function(metadata) {
+
+    this.base(arguments, this.tr("Service information") + " · " + metadata.name);
+
+    this.set({
+      layout: new qx.ui.layout.Grow(),
+      contentPadding: 0,
+      showMinimize: false,
+      resizable: false,
+      modal: true
+    });
+
+    this.add(new qxapp.component.metadata.ServiceInfo(metadata));
+  },
+
+  properties: {
+    appearance: {
+      refine: true,
+      init: "info-service-window"
+    }
+  }
+});
