afdbc659803865fcbf5e420283d7524032a5626a
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-07-02 10:59:14 +0200

    Make eslint happy

--- services/web/client/source/class/qxapp/utils/Utils.js
+++ services/web/client/source/class/qxapp/utils/Utils.js
@@ -329,7 +329,9 @@ qx.Class.define("qxapp.utils.Utils", {
       let hash = 0;
       let i;
       let chr;
-      if (str.length === 0) return hash;
+      if (str.length === 0) {
+        return hash;
+      }
       for (i=0; i<str.length; i++) {
         chr = str.charCodeAt(i);
         hash = ((hash << 5) - hash) + chr;
