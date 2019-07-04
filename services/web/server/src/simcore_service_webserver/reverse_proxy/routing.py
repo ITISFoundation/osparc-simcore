302be89580e00dbfbe492aa4863ad749417a3754
odeimaiz <maiz@itis.swiss>
2019-07-04 18:48:34 +0200

    make jupyter's Reverse Proxy the default one

--- services/web/server/src/simcore_service_webserver/reverse_proxy/routing.py
+++ services/web/server/src/simcore_service_webserver/reverse_proxy/routing.py
@@ -15,7 +15,7 @@ import attr
 from aiohttp import web
 
 from .abc import ServiceResolutionPolicy
-from .handlers.generic import handler as default_handler
+from .handlers.jupyter import handler as default_handler #TODO: jupyter shall be the generic one
 from .settings import PROXY_PATH_KEY, SERVICE_ID_KEY
 
 logger = logging.getLogger(__name__)
@@ -88,7 +88,6 @@ class ReverseChooser:
 
         # FIXME: add version as well
         handler = self.handlers.get(image_name, default_handler)
-
 
         response = await handler(request, service_url,
                                  mount_point=mountpoint,
