73dac9f969c9ff49d4863189290bed5f03a6b5c0
Sylvain <35365065+sanderegg@users.noreply.github.com>
2019-07-25 07:26:37 -0700

    Is970/get service status (#976)
    frontend poll when service is starting until it runs or fails
    connects with interactive service state displayed in frontend

--- services/director/src/simcore_service_director/rest/generated_code/models/__init__.py
+++ services/director/src/simcore_service_director/rest/generated_code/models/__init__.py
@@ -9,7 +9,6 @@ from .inline_response2001_authors import InlineResponse2001Authors
 from .inline_response200_data import InlineResponse200Data
 from .inline_response201 import InlineResponse201
 from .inline_response201_data import InlineResponse201Data
-from .inline_response204 import InlineResponse204
 from .inline_response_default import InlineResponseDefault
 from .inline_response_default_error import InlineResponseDefaultError
 from .simcore_node import SimcoreNode
