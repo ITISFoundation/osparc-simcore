9b124123ea3bc3911ea64dfe5c6e157282f18afd
odeimaiz <maiz@itis.swiss>
2019-08-27 14:14:51 +0200

    exit with failure if error is thrown

--- ops/travis/system-testing/index.js
+++ ops/travis/system-testing/index.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const puppeteer = require('puppeteer');
 
 const url = "http://localhost:9081/"
@@ -11,10 +12,14 @@ async function run () {
   throw Error('here my error');
 
   const title = await page.title();
-  console.log(title);
+  assert.equal(title, 'oSPARC', 'Page title is not what expected');
 
+  console.log('puppeteer is happy');
   await browser.close();
 }
 
 run()
-  .catch((e) => console.log('err: ' + e));
+  .catch((e) => {
+    console.log('err: ' + e);
+    process.exit(1);
+  });
