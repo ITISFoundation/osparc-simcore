2b6cc4040466e4094249a3cddc570b55e5ee5409
odeimaiz <maiz@itis.swiss>
2019-09-02 15:10:52 +0200

    using jest

--- ops/travis/system-testing/title.test.js
+++ ops/travis/system-testing/title.test.js
@@ -1,23 +1,15 @@
-const assert = require('assert');
 const puppeteer = require('puppeteer');
 
 const url = "http://localhost:9081/"
 
-async function checkTitle () {
+test('check site title', async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
   await page.goto(url);
 
   const title = await page.title();
-  assert.equal(title, 'oSPARC', 'Page title is not what we expected');
-
+  expect(title).toBe('oSPARC');
+  
   console.log('Puppeteer is happy');
   await browser.close();
-}
-
-checkTitle()
-  .catch((e) => {
-    console.log('Puppeteer error: ' + e);
-    process.exit(1);
-  });
+}, 16000);
