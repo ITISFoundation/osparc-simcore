a9cc073771ed2772ba54452ede6839652a9eaa03
odeimaiz <maiz@itis.swiss>
2019-09-02 11:58:08 +0200

    minor renaming

--- ops/travis/system-testing/checkTitle.js
+++ ops/travis/system-testing/checkTitle.js
@@ -3,20 +3,20 @@ const puppeteer = require('puppeteer');
 
 const url = "http://localhost:9081/"
 
-async function run () {
+async function checkTitle () {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await page.goto(url);
 
   const title = await page.title();
-  assert.equal(title, 'oSPARC', 'Page title is not what expected');
+  assert.equal(title, 'oSPARC', 'Page title is not what we expected');
 
   console.log('Puppeteer is happy');
   await browser.close();
 }
 
-run()
+checkTitle()
   .catch((e) => {
     console.log('Puppeteer error: ' + e);
     process.exit(1);
