e46c65488ae61acf6026fdab968d27c180fe0c95
odeimaiz <maiz@itis.swiss>
2019-09-02 15:26:28 +0200

    using jest-puppeteer

--- ops/travis/system-testing/title.test.js
+++ ops/travis/system-testing/title.test.js
@@ -1,15 +1,9 @@
-const puppeteer = require('puppeteer');
-
 const url = "http://localhost:9081/"
 
 test('check site title', async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  // page is already defined by the jest-puppeteer preset
   await page.goto(url);
 
   const title = await page.title();
   expect(title).toBe('oSPARC');
-  
-  console.log('Puppeteer is happy');
-  await browser.close();
-}, 16000);
+}, 10000);
