caf04ead0cec812bf0978343a11f618d06ac9a89
odeimaiz <maiz@itis.swiss>
2019-09-02 16:00:39 +0200

    got rid of jest-puppeteer

--- ops/travis/system-testing/title.test.js
+++ ops/travis/system-testing/title.test.js
@@ -1,9 +1,15 @@
+const puppeteer = require('puppeteer');
+
 const url = "http://localhost:9081/"
 
 test('check site title', async () => {
-  // page is already defined by the jest-puppeteer preset
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
   await page.goto(url);
 
   const title = await page.title();
   expect(title).toBe('oSPARC');
-}, 10000);
+
+  browser.close();
+}, 14000);
