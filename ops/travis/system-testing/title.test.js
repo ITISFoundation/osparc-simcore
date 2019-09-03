d42698de04180995e59f11a57c0f3b232a13e81c
odeimaiz <maiz@itis.swiss>
2019-09-03 13:43:33 +0200

    Added Register and Log In to e2e testing

--- ops/travis/system-testing/title.test.js
+++ ops/travis/system-testing/title.test.js
@@ -1,12 +1,17 @@
 const puppeteer = require('puppeteer');
 
 let browser;
-let page;
+const demo = true;
 const url = "http://localhost:9081/"
 
 beforeAll(async () => {
-  browser = await puppeteer.launch();
-  page = await browser.newPage();
+  const visibleOptions = {
+    headless: false,
+    defaultViewport: null, // Defaults to an 800x600 viewport. null disables the default viewport.
+    slowMo: 60 // Slows down Puppeteer operations by the specified amount of milliseconds.
+  }
+  const options = demo ? visibleOptions : {};
+  browser = await puppeteer.launch(options);
 });
 
 afterAll(() => {
@@ -14,8 +19,64 @@ afterAll(() => {
 });
 
 test('Check site title', async () => {
+  const page = await browser.newPage();
   await page.goto(url);
 
   const title = await page.title();
   expect(title).toBe('oSPARC');
+
+  page.close();
 }, 14000);
+
+
+test('Register and Log In', async () => {
+  const randUser = Math.random().toString(36).substring(7);
+  const userEmail = 'puppeteer_'+randUser+'@itis.testing';
+  const pass = Math.random().toString(36).substring(7);
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  page.on('response', response => {
+    if (response.url().endsWith("register")) {
+      const respStatus = response.status();
+      expect(respStatus).toBe(200);
+    }
+  });
+
+  await register(page, userEmail, pass);
+
+  page.on('response', response => {
+    if (response.url().endsWith("me")) {
+      const respStatus = response.status();
+      expect(respStatus).toBe(200);
+    }
+    else if (response.url().endsWith("services")) {
+      const respStatus = response.status();
+      expect(respStatus).toBe(200);
+    }
+    else if (response.url().endsWith("locations")) {
+      const respStatus = response.status();
+      expect(respStatus).toBe(200);
+    }
+  });
+
+  await logIn(page, userEmail, pass);
+}, 20000);
+
+async function register(page, user, pass) {
+  await page.waitForSelector('#loginCreateAccountBtn');
+  await page.click('#loginCreateAccountBtn');
+
+  await page.waitForSelector('#registrationEmailFld');
+  await page.type('#registrationEmailFld', user);
+  await page.type('#registrationPass1Fld', pass);
+  await page.type('#registrationPass2Fld', pass);
+  await page.click('#registrationSubmitBtn');
+}
+
+async function logIn(page, user, pass) {
+  await page.waitForSelector('#loginUserEmailFld');
+  await page.type('#loginUserEmailFld', user);
+  await page.type('#loginPasswordFld', pass);
+  await page.click('#loginSubmitBtn');
+}
