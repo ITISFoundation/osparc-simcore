ea343f197c99c3286c0287a1d6beda19ebe7a79b
odeimaiz <maiz@itis.swiss>
2019-09-05 14:27:02 +0200

    more

--- ops/travis/system-testing/e2e_utils/startBrowser.js
+++ ops/travis/system-testing/e2e_utils/startBrowser.js
@@ -2,20 +2,22 @@ const puppeteer = require('puppeteer');
 
 async function launch(demo) {
   let options = {};
-  const visibleOptions = {
-    headless: false,
-    devTools: true,
-    defaultViewport: null, // Defaults to an 800x600 viewport. null disables the default viewport.
-    slowMo: 60 // Slows down Puppeteer operations by the specified amount of milliseconds.
-  }
-  const woSandbox = {
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  };
   if (demo) {
+    const visibleOptions = {
+      headless: false,
+      devTools: true,
+      defaultViewport: null, // Defaults to an 800x600 viewport. null disables the default viewport.
+      slowMo: 60 // Slows down Puppeteer operations by the specified amount of milliseconds.
+    };
     Object.assign(options, visibleOptions);
   }
-  Object.assign(options, woSandbox);
-  const browser = await puppeteer.launch();
+  else {
+    const woSandbox = {
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    };
+    Object.assign(options, woSandbox);
+  }
+  const browser = await puppeteer.launch(options);
   return browser;
 }
 
