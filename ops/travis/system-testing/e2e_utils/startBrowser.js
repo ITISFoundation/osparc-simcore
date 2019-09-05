3884155efa031364a3158b1330c578b15833902e
odeimaiz <maiz@itis.swiss>
2019-09-05 14:06:55 +0200

    without Sandbox

--- ops/travis/system-testing/e2e_utils/startBrowser.js
+++ ops/travis/system-testing/e2e_utils/startBrowser.js
@@ -1,14 +1,21 @@
 const puppeteer = require('puppeteer');
 
 async function launch(demo) {
+  let options = {};
   const visibleOptions = {
     headless: false,
     devTools: true,
     defaultViewport: null, // Defaults to an 800x600 viewport. null disables the default viewport.
     slowMo: 60 // Slows down Puppeteer operations by the specified amount of milliseconds.
   }
-  const options = demo ? visibleOptions : {};
-  const browser = await puppeteer.launch(options);
+  const woSandbox = {
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  };
+  if (demo) {
+    Object.assign(options, visibleOptions);
+  }
+  Object.assign(options, woSandbox);
+  const browser = await puppeteer.launch();
   return browser;
 }
 
