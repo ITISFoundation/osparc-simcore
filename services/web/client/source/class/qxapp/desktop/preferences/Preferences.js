8eb330f09ba973518545be5f96c70dcb0354493e
odeimaiz <maiz@itis.swiss>
2019-09-05 14:59:53 +0200

    more IDs

--- services/web/client/source/class/qxapp/desktop/preferences/Preferences.js
+++ services/web/client/source/class/qxapp/desktop/preferences/Preferences.js
@@ -27,6 +27,7 @@ qx.Class.define("qxapp.desktop.preferences.Preferences", {
   construct: function() {
     this.base(arguments, this.tr("Preferences"));
     this.set({
+      layout: new qx.ui.layout.VBox(10),
       modal: true,
       width: 500,
       height: 500 * 1.2,
@@ -35,14 +36,27 @@ qx.Class.define("qxapp.desktop.preferences.Preferences", {
       showMinimize: false,
       resizable: false
     });
-    this.setLayout(new qx.ui.layout.VBox(10));
+    const closeBtn = this.getChildControl("close-button");
+    qxapp.utils.Utils.setIdToWidget(closeBtn, "preferencesWindowCloseBtn");
 
     const tabView = new qx.ui.tabview.TabView().set({
       barPosition: "left"
     });
-    tabView.add(new qxapp.desktop.preferences.pages.ProfilePage());
-    tabView.add(new qxapp.desktop.preferences.pages.SecurityPage());
-    tabView.add(new qxapp.desktop.preferences.pages.ExperimentalPage());
+
+    const profPage = new qxapp.desktop.preferences.pages.ProfilePage();
+    const profBtn = profPage.getChildControl("button");
+    qxapp.utils.Utils.setIdToWidget(profBtn, "preferencesProfileTabBtn");
+    tabView.add(profPage);
+
+    const secPage = new qxapp.desktop.preferences.pages.SecurityPage();
+    const secBtn = secPage.getChildControl("button");
+    qxapp.utils.Utils.setIdToWidget(secBtn, "preferencesSecurityTabBtn");
+    tabView.add(secPage);
+
+    const expPage = new qxapp.desktop.preferences.pages.ExperimentalPage();
+    const expBtn = expPage.getChildControl("button");
+    qxapp.utils.Utils.setIdToWidget(expBtn, "preferencesExperimentalTabBtn");
+    tabView.add(expPage);
 
     this.add(tabView, {
       flex: 1
