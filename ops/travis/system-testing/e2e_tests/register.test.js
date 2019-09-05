47d82a1c3b4e5ae36c638624e16596d74073a2b6
odeimaiz <maiz@itis.swiss>
2019-09-05 16:00:26 +0200

    bring back the before/after each'es

--- ops/travis/system-testing/e2e_tests/register.test.js
+++ ops/travis/system-testing/e2e_tests/register.test.js
@@ -2,6 +2,7 @@ const startBrowser = require('../e2e_utils/startBrowser');
 const auto = require('../e2e_utils/auto');
 
 let browser;
+let page;
 const demo = false;
 const url = "http://localhost:9081/"
 
@@ -17,9 +18,36 @@ afterAll(async () => {
   await browser.close();
 });
 
-test('Register', async () => {
-  const page = await browser.newPage();
+beforeEach(async () => {
+  page = await browser.newPage();
+  page.on('response', async response => {
+    if (response.url().endsWith("register")) {
+      const respStatus = response.status();
+      expect(respStatus).toBe(200);
+    }
+    else if (response.url().endsWith("services")) {
+      console.log(response.url());
+      const respStatus = response.status();
+      expect(respStatus).toBe(200);
+      const dataObj = await response.json();
+      console.log("services", dataObj);
+    }
+    else if (response.url().endsWith("locations")) {
+      console.log(response.url());
+      const respStatus = response.status();
+      expect(respStatus).toBe(200);
+      const dataObj = await response.json();
+      console.log("locations", dataObj);
+    }
+  });
   await page.goto(url);
+}, 30000);
+
+afterEach(async () => {
+  await page.close();
+});
+
+test('Register', async () => {
   await auto.register(page, userEmail, pass);
 }, 30000);
 
