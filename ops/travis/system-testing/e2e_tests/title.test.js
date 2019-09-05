6061c40aea4c1f4147d02b6afaf3d4ccd575b3e0
odeimaiz <maiz@itis.swiss>
2019-09-05 17:05:04 +0200

    add before/after each to simple test

--- ops/travis/system-testing/e2e_tests/title.test.js
+++ ops/travis/system-testing/e2e_tests/title.test.js
@@ -2,6 +2,7 @@ const startBrowser = require('../e2e_utils/startBrowser');
 const utils = require('../e2e_utils/utils');
 
 let browser;
+let page;
 const demo = false;
 const url = "http://localhost:9081/"
 
@@ -13,12 +14,16 @@ afterAll(async () => {
   await browser.close();
 });
 
-test('Check site title', async () => {
-  let page = await browser.newPage();
+beforeEach(async () => {
+  page = await browser.newPage();
   await page.goto(url);
+}, 30000);
 
+afterEach(async () => {
+  await page.close();
+});
+
+test('Check site title', async () => {
   const title = await utils.getPageTitle(page);
   expect(title).toBe('oSPARC');
-
-  await page.close();
 }, 20000);
