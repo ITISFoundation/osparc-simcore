f2871ee3d51312f359df7754969b3b772018afa2
odeimaiz <maiz@itis.swiss>
2019-09-08 16:22:14 +0200

    minor

--- ops/travis/system-testing/e2e_utils/utils.js
+++ ops/travis/system-testing/e2e_utils/utils.js
@@ -6,7 +6,16 @@ function getPageUrl(page) {
   return page.url();
 }
 
+async function dragAndDrop(page, start, end) {
+  await page.mouse.move(start.x, start.y);
+  await page.mouse.down();
+  
+  await page.mouse.move(end.x, end.y);
+  await page.mouse.up();
+}
+
 module.exports = {
   getPageTitle,
   getPageUrl,
+  dragAndDrop,
 }
