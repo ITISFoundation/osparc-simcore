19c5f8c3817d1f83e89d81f25a6c0c5d1a270cfb
odeimaiz <maiz@itis.swiss>
2019-09-10 14:51:58 +0200

    more fetches

--- ops/travis/e2e-testing/tests/services.test.js
+++ ops/travis/e2e-testing/tests/services.test.js
@@ -11,6 +11,45 @@ beforeEach(async () => {
   await page.goto(url);
 }, goToTimeout);
 
+test('Get my profile', async () => {
+  await auto.register(page, user, pass);
+  await auto.logIn(page, user, pass);
+
+  const responseEnv = await page.evaluate(async () => {
+    const response = await fetch('http://localhost:9081/v0/me');
+    return await response.json();
+  });
+  expect(responseEnv.data["login"]).toBe(user);
+
+  await auto.logOut(page);
+}, 30000);
+
+test('Get my studies', async () => {
+  await auto.register(page, user, pass);
+  await auto.logIn(page, user, pass);
+
+  const responseEnv = await page.evaluate(async () => {
+    const response = await fetch('http://localhost:9081/v0/projects?type=user');
+    return await response.json();
+  });
+  expect(Array.isArray(responseEnv.data)).toBeTruthy();
+
+  await auto.logOut(page);
+}, 30000);
+
+test('Get templates', async () => {
+  await auto.register(page, user, pass);
+  await auto.logIn(page, user, pass);
+
+  const responseEnv = await page.evaluate(async () => {
+    const response = await fetch('http://localhost:9081/v0/projects?type=template');
+    return await response.json();
+  });
+  expect(Array.isArray(responseEnv.data)).toBeTruthy();
+
+  await auto.logOut(page);
+}, 30000);
+
 test('Get services', async () => {
   await auto.register(page, user, pass);
   await auto.logIn(page, user, pass);
@@ -19,7 +58,7 @@ test('Get services', async () => {
     const response = await fetch('http://localhost:9081/v0/services');
     return await response.json();
   });
-  console.log();
+  expect(Array.isArray(responseEnv.data)).toBeTruthy();
   expect(responseEnv.data.length).toBeGreaterThan(0);
 
   await auto.logOut(page);
