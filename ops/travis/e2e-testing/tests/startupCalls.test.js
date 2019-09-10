21e6e7ba6c71d10f4eb5f51ab8fc39601842de1c
odeimaiz <maiz@itis.swiss>
2019-09-10 16:35:11 +0200

    console removed

--- ops/travis/e2e-testing/tests/startupCalls.test.js
+++ ops/travis/e2e-testing/tests/startupCalls.test.js
@@ -43,7 +43,6 @@ describe('Calls after logging in', () => {
 
   test('Locations', async () => {
     const responseEnv = await utils.fetch('storage/locations');
-    console.log("locations", responseEnv.data);
     expect(Array.isArray(responseEnv.data)).toBeTruthy();
     expect(responseEnv.data.length).toBeGreaterThan(0);
   }, ourTimeout);
