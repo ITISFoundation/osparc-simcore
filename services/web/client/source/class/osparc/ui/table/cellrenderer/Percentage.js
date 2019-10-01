1c7d6aeb9f1a893e00a8ed2f1f69672ff4308d43
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-10-01 09:51:20 +0200

    Added new percentage cellrenderer

---
+++ services/web/client/source/class/osparc/ui/table/cellrenderer/Percentage.js
@@ -0,0 +1,27 @@
+/*
+ * oSPARC - The SIMCORE frontend - https://osparc.io
+ * Copyright: 2019 IT'IS Foundation - https://itis.swiss
+ * License: MIT - https://opensource.org/licenses/MIT
+ * Authors: Ignacio Pascual (ignapas)
+ */
+
+qx.Class.define("osparc.ui.table.cellrenderer.Percentage", {
+  extend: qx.ui.table.cellrenderer.Html,
+
+  construct: function(color) {
+    this.base(arguments, "center");
+    this.__color = color;
+  },
+
+  members: {
+    // overridden
+    _getContentHtml: function(cellInfo) {
+      if (cellInfo.value == null) {
+        return "";
+      }
+      return "" +
+        `<div style="position: absolute; left: 0; right: 0;">${cellInfo.value}%</div>` +
+        `<div style="height: 100%; width: ${cellInfo.value}%; background-color: ${this.__color};"></div>`;
+    }
+  }
+});
