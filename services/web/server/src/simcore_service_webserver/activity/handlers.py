1cfc5adc83a2b56ef06ea3a3bff1925297bd3635
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-10-16 15:22:41 +0200

    URL encoding now working for Prometheus

--- services/web/server/src/simcore_service_webserver/activity/handlers.py
+++ services/web/server/src/simcore_service_webserver/activity/handlers.py
@@ -1,13 +1,13 @@
 from aiohttp import web
 from ..login.decorators import login_required
 import aiohttp
-from yarl import URL
+from urllib.parse import quote
 
 @login_required
 async def get_status(request: web.Request):
     async with aiohttp.ClientSession() as session:
-        query = 'sum by (container_label_node_id) (irate(container_cpu_usage_seconds_total{container_label_node_id=~".+"}[30s])*100)'
-        url = URL('http://prometheus:9090/api/v1/query').with_query('query=' + query)
+        query = 'sum by (container_label_node_id) (irate(container_cpu_usage_seconds_total{container_label_node_id=~".+"}[30s]) * 100)'
+        url = 'http://prometheus:9090/api/v1/query?query=' + quote(query)
         async with session.get(url) as resp:
             status = resp.status
             result = await resp.text()
