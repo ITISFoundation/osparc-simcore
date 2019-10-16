4bfef4002773b4105d038abb5033148b3e9858d5
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-10-16 19:29:37 +0200

    WiP added memory metrics

--- services/web/server/src/simcore_service_webserver/activity/handlers.py
+++ services/web/server/src/simcore_service_webserver/activity/handlers.py
@@ -1,23 +1,37 @@
-from aiohttp import web
 from ..login.decorators import login_required
 import aiohttp
 from urllib.parse import quote
+import asyncio
+import json
 
 @login_required
-async def get_status(request: web.Request):
+async def get_status(request: aiohttp.web.Request):
     async with aiohttp.ClientSession() as session:
-        query = 'sum by (container_label_node_id) (irate(container_cpu_usage_seconds_total{container_label_node_id=~".+"}[30s]) * 100)'
-        url = 'http://prometheus:9090/api/v1/query?query=' + quote(query)
-        async with session.get(url) as resp:
-            status = resp.status
-            result = await resp.text()
-            return {
-                'error': None,
-                'data': {
-                    'stats': {
-                        'cpuUsage': 0,
-                        'memoryUsage': 0
-                    },
-                    'result': result
-                }
+        cpu_query = 'sum by (container_label_node_id) (irate(container_cpu_usage_seconds_total{container_label_node_id=~".+"}[30s]) * 100)'
+        memory_query = 'sum by (container_label_node_id) (container_memory_usage_bytes{container_label_node_id=~".+"} / 1000000)'
+        url = 'http://prometheus:9090/api/v1/query'
+
+        async def get_cpu_usage():
+            async with session.get(url + '?query=' + quote(cpu_query)) as resp:
+                status = resp.status
+                result = await resp.text()
+                return result
+
+        async def get_memory_usage():
+            async with session.get(url + '?query=' + quote(memory_query)) as resp:
+                status = resp.status
+                result = await resp.text()
+                return result
+
+        results = await asyncio.gather(get_cpu_usage(), get_memory_usage())
+        json_results = list(map(json.loads, results))
+        return {
+            'error': None,
+            'data': {
+                'stats': {
+                    'cpuUsage': 0,
+                    'memoryUsage': 0
+                },
+                'result': json_results
             }
+        }
