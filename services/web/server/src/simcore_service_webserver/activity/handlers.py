e0729009be5baf757ee2d0640569580a4c04cef2
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-10-16 11:31:53 +0200

    Work in progress on querying Prometheus

--- services/web/server/src/simcore_service_webserver/activity/handlers.py
+++ services/web/server/src/simcore_service_webserver/activity/handlers.py
@@ -1,16 +1,20 @@
 from aiohttp import web
 from ..login.decorators import login_required
+import aiohttp
 
 @login_required
 async def get_status(request: web.Request):
-    
-    return {
-        'error': None,
-        'data': {
-            'stats': {
-                'cpuUsage': 0,
-                'memoryUsage': 0
-            },
-            'text': 'your mamma'
-        }
-    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://127.0.0.1:9090/api/v1/query?query=irate%28container_cpu_user_seconds_total%7Bcontainer_label_user_id%3D~%22.%2B%22%7D%5B12s%5D%29%20%2A%20100') as resp:
+            status = resp.status
+            result = await resp.text()
+            return {
+                'error': None,
+                'data': {
+                    'stats': {
+                        'cpuUsage': 0,
+                        'memoryUsage': 0
+                    },
+                    'result': result
+                }
+            }
