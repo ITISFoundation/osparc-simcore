81284d48cd2ebb0d593b6bb0a8f809f10257e50b
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-10-17 17:43:13 +0200

    URLs moved to external files Already extracting json from responses Making use of automatic response envelopement

--- services/web/server/src/simcore_service_webserver/activity/handlers.py
+++ services/web/server/src/simcore_service_webserver/activity/handlers.py
@@ -1,36 +1,33 @@
 import asyncio
-import json
-from urllib.parse import quote
 
 import aiohttp
 from yarl import URL
 
 from ..login.decorators import login_required
 
-
 @login_required
 async def get_status(request: aiohttp.web.Request):
+
     async with aiohttp.ClientSession() as session:
         cpu_query = 'sum by (container_label_node_id) (irate(container_cpu_usage_seconds_total{container_label_node_id=~".+"}[30s]) * 100)'
         memory_query = 'sum by (container_label_node_id) (container_memory_usage_bytes{container_label_node_id=~".+"} / 1000000)'
-        url = 'http://prometheus:9090/api/v1/query'
+        
+        config = request.app['servicelib.application_keys.config']['activity']
+        url = URL(config.get('prometheus_host')).with_port(config.get('prometheus_port')).with_path('api/' + config.get('prometheus_api_version') + '/query')
 
         async def get_cpu_usage():
-            async with session.get(URL(url).with_query(query=cpu_query)) as resp:
-                status = resp.status
-                result = await resp.text()
+            async with session.get(url.with_query(query=cpu_query)) as resp:
+                result = await resp.json()
                 return result
 
         async def get_memory_usage():
-            async with session.get(URL(url).with_query(query=memory_query)) as resp:
-                status = resp.status
-                result = await resp.text()
+            async with session.get(url.with_query(query=memory_query)) as resp:
+                result = await resp.json()
                 return result
 
         results = await asyncio.gather(get_cpu_usage(), get_memory_usage())
-        json_results = list(map(json.loads, results))
-        cpu_usage = json_results[0]['data']['result']
-        mem_usage = json_results[1]['data']['result']
+        cpu_usage = results[0]['data']['result']
+        mem_usage = results[1]['data']['result']
 
         res = {}
         for node in cpu_usage:
@@ -47,7 +44,4 @@ async def get_status(request: aiohttp.web.Request):
             usage = node['value'][1]
             res[node_id]['stats']['memUsage'] = usage
         
-        return {
-            'error': None,
-            'data': res
-        }
+        return res
