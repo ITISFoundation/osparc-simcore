385a5f5e27d15bb2d794ae6242070a18f4eb8059
odeimaiz <maiz@itis.swiss>
2019-10-21 16:31:38 +0200

    Issues coming from users feedback go to osparc-issues

--- services/web/client/source/class/osparc/component/widget/NewGHIssue.js
+++ services/web/client/source/class/osparc/component/widget/NewGHIssue.js
@@ -34,7 +34,7 @@ qx.Class.define("osparc.component.widget.NewGHIssue", {
       env += JSON.stringify(osparc.utils.LibVersions.getEnvLibs(), null, 2);
       env += "\n```";
       const body = encodeURIComponent(temp+env);
-      let url = "https://github.com/ITISFoundation/osparc-simcore/issues/new";
+      let url = "https://github.com/ITISFoundation/osparc-issues/issues/new";
       url += "?labels=tester_review,UX_improvements";
       url += "&projects=ITISFoundation/3";
       url += "&body=" + body;
