46958df64848effdbdaea1c49c246bad070a2830
Pedro Crespo <crespo@itis.ethz.ch>
2019-10-22 11:02:38 +0200

    Minor fixes in loggers

--- services/storage/src/simcore_service_storage/cli.py
+++ services/storage/src/simcore_service_storage/cli.py
@@ -72,7 +72,6 @@ def main(args=None):
     log_level = config["main"]["log_level"]
     logging.basicConfig(level=getattr(logging, log_level))
     logging.root.setLevel(getattr(logging, log_level))
-    print("Log level set to %s", log_level)
 
     application.run(config)
 
