32669f710bcc5ec136f3b43dd6e2ebb7193b256b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-11-06 09:24:41 +0100

    added tracing support in director

--- services/director/src/simcore_service_director/main.py
+++ services/director/src/simcore_service_director/main.py
@@ -5,12 +5,22 @@ import logging
 from aiohttp import web
 from servicelib.client_session import persistent_client_session
 from servicelib.monitoring import setup_monitoring
-from simcore_service_director import registry_cache_task, resources
+from servicelib.tracing import setup_tracing
+from simcore_service_director import registry_cache_task, resources, config
 from simcore_service_director.rest import routing
 
 
 log = logging.getLogger(__name__)
 
+
+def setup_app_tracing(app: web.Application, app_name: str):
+    host="0.0.0.0"
+    port=8080
+    cfg = {
+        "enabled": config.TRACING_ENABLED,
+        "zipkin_endpoint": config.TRACING_ZIPKIN_ENDPOINT
+    }
+    return setup_tracing(app, app_name, host, port, cfg)
 
 def setup_app() -> web.Application:
     api_spec_path = resources.get_path(resources.RESOURCE_OPEN_API)
@@ -24,6 +34,8 @@ def setup_app() -> web.Application:
     # TODO: temporary disabled until service is updated
     if False: #pylint: disable=using-constant-test
         setup_monitoring(app, "simcore_service_director")
+    
+    setup_app_tracing(app, "simcore_service_director")
 
     return app
 
