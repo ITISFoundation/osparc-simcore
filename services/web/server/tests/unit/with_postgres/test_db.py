085be4cf2cac58c0a2eb3e2d2f4a5ee8d7271500
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2019-11-13 01:30:20 +0100

    Added a test to keep image version in sync

--- services/web/server/tests/unit/with_postgres/test_db.py
+++ services/web/server/tests/unit/with_postgres/test_db.py
@@ -1,5 +1,18 @@
-from simcore_service_webserver.db import is_service_enabled, is_service_responsive
+import io
 
+import yaml
+
+from simcore_service_webserver.db import (is_service_enabled,
+                                          is_service_responsive)
+
+def test_uses_same_postgres_version(docker_compose_file, osparc_simcore_root_dir):
+    with io.open(docker_compose_file) as fh:
+        fixture = yaml.safe_load(fh)
+
+    with io.open(osparc_simcore_root_dir / "services" / "docker_compose.yml") as fh:
+        expected = yaml.safe_load(fh)
+
+    assert fixture['services']['postgres']['image'] == expected['services']['postgres']['image']
 
 
 async def test_responsive(server):
