2df820af37f0f35f538b1720a80888449f6b4f67
Pedro Crespo <crespo@itis.ethz.ch>
2019-11-13 22:19:36 +0100

    Disposes engines

--- services/sidecar/tests/utils.py
+++ services/sidecar/tests/utils.py
@@ -48,8 +48,11 @@ def create_tables(url, engine=None):
             client_encoding="utf8",
             connect_args={"connect_timeout": 30},
                 pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        engine.dispose()
+    else:
+        Base.metadata.create_all(engine)
 
-    Base.metadata.create_all(engine)
 
 def drop_tables(url, engine=None):
     if not engine:
