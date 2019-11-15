86bf4eed67f5c148495c330f95dff19dac31c529
Pedro Crespo <crespo@itis.ethz.ch>
2019-11-15 18:26:48 +0100

    Adds watchdemo to autorestart server in dev mode

--- services/web/server/docker/boot.sh
+++ services/web/server/docker/boot.sh
@@ -25,9 +25,13 @@ then
   echo "  PIP :"
   $SC_PIP list | sed 's/^/    /'
 
+  echo "  setting entrypoint to use watchmedo autorestart..."
+  entrypoint='watchmedo auto-restart --recursive --pattern="*.py" --'
+
 elif [[ ${SC_BUILD_TARGET} == "production" ]]
 then
   APP_CONFIG=server-docker-prod.yaml
+  entrypoint=''
 fi
 
 
@@ -37,13 +41,13 @@ then
   # NOTE: needs stdin_open: true and tty: true
   echo "Debugger attached: https://docs.python.org/3.6/library/pdb.html#debugger-commands  ..."
   echo "Running: import pdb, simcore_service_server.cli; pdb.run('simcore_service_server.cli.main([\'-c\',\'${APP_CONFIG}\'])')"
-  python -c "import pdb, simcore_service_server.cli; \
+  eval "$entrypoint" python -c "import pdb, simcore_service_server.cli; \
              pdb.run('simcore_service_server.cli.main([\'-c\',\'${APP_CONFIG}\'])')"
 elif [[ ${SC_BOOT_MODE} == "debug-ptvsd" ]]
 then
   # NOTE: needs ptvsd installed
   echo "PTVSD Debugger initializing in port 3000 with ${APP_CONFIG}"
-  python3 -m ptvsd --host 0.0.0.0 --port 3000 -m simcore_service_webserver --config $APP_CONFIG
+  eval "$entrypoint" python3 -m ptvsd --host 0.0.0.0 --port 3000 -m simcore_service_webserver --config $APP_CONFIG
 else
-  simcore-service-webserver --config $APP_CONFIG
+  eval "$entrypoint" simcore-service-webserver --config $APP_CONFIG
 fi
