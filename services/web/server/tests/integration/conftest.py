0f3a9c9fb27506cd314cfc46e1bd9126cbc43a61
Pedro Crespo <crespo@itis.ethz.ch>
2019-11-25 14:02:59 +0100

    Updates utils to find published port Ensures environ variable exist before replacing them

--- services/web/server/tests/integration/conftest.py
+++ services/web/server/tests/integration/conftest.py
@@ -73,19 +73,20 @@ def webserver_environ(request, docker_stack: Dict, simcore_docker_compose: Dict)
     services_with_published_ports = [name for name in core_services
                 if 'ports' in simcore_docker_compose['services'][name] ]
 
-
-
     for name in services_with_published_ports:
+
+        host_key = f'{name.upper()}_HOST'
+        port_key = f'{name.upper()}_PORT'
+
         # published port is sometimes dynamically defined by the swarm
-        assert f'{name.upper()}_HOST' in environ, "Variables names expected to be prefix with service names in docker-compose"
-        assert f'{name.upper()}_PORT' in environ
+        assert host_key in environ, "Variables names expected to be prefix with service names in docker-compose"
+        assert port_key in environ
 
-        # FIXME: pass expose port in case multiple choices
-        published_port = get_service_published_port(name)
-
-        environ[f'{name.upper()}_HOST'] = '127.0.0.1'
-        environ[f'{name.upper()}_PORT'] = published_port
         # to swarm boundary since webserver is installed in the host and therefore outside the swarm's network
+        published_port = get_service_published_port(name, int(environ.get(port_key)))
+        environ[host_key] = '127.0.0.1'
+        environ[port_key] = published_port
+
 
     pprint(environ) # NOTE: displayed only if error
     return environ
