58dea8568ca841aa953ab4058faac9fa550b079d
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-12-20 10:49:12 +0100

    fix issue

--- services/web/server/src/simcore_service_webserver/resource_manager/websocket_manager.py
+++ services/web/server/src/simcore_service_webserver/resource_manager/websocket_manager.py
@@ -53,14 +53,15 @@ class WebsocketRegistry:
         await registry.set_key_alive(self._resource_key(), False, get_service_deletion_timeout(self.app))
 
     async def find_socket_ids(self) -> List[str]:
-        user_sockets = await self.find(SOCKET_ID_KEY)
+        log.debug("user %s/tab %s finding %s from registry...", self.user_id, self.client_session_id, SOCKET_ID_KEY)
+        registry = get_registry(self.app)
+        user_sockets = await registry.find_resources({"user_id": self.user_id}, SOCKET_ID_KEY)
         return user_sockets
 
-    #TODO: add test
     async def find(self, key: str) -> List[str]:
         log.debug("user %s/tab %s finding %s from registry...", self.user_id, self.client_session_id, key)
         registry = get_registry(self.app)
-        user_resources = await registry.find_resources({"user_id": self.user_id}, key)
+        user_resources = await registry.find_resources(self._resource_key(), key)
         return user_resources
 
     async def add(self, key: str, value: str) -> None:
