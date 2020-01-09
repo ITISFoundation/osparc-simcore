8ecf798d681513c4db1139bb744ad319b54381b0
odeimaiz <maiz@itis.swiss>
2020-01-09 13:52:18 +0100

    minor

--- services/web/client/source/class/osparc/theme/Color.js
+++ services/web/client/source/class/osparc/theme/Color.js
@@ -23,16 +23,22 @@ qx.Theme.define("osparc.theme.Color", {
     "workbench-edge-comp-active": "#777",
     "workbench-edge-api-active": "#BBB",
     "workbench-edge-selected": "#00F",
+
     "logger-debug-message": "#FFF",
     "logger-info-message": "#FFF",
     "logger-warning-message": "#FF0",
     "logger-error-message": "#F00",
+
     "background-main-lighter": "#303030",
     "background-main-lighter+": "#373737",
+
     "text-placeholder": "text-disabled",
     "text-darker": "text-disabled",
+
     "tabview-pane-background": "background-main",
+
     "workbench-start-hint": "#505050",
+
     "ready-green": "#33925A",
     "failed-red": "#FF2D2D"
   }
