04f96d64834964e77da6a9acf4d3249dcdeff137
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-01-10 12:01:03 +0100

    eslint

--- services/web/client/source/class/osparc/data/model/Node.js
+++ services/web/client/source/class/osparc/data/model/Node.js
@@ -800,7 +800,8 @@ qx.Class.define("osparc.data.model.Node", {
     __nodeState: function() {
       const params = {
         url: {
-          "project_id": this.getWorkbench().getStudy().getUuid(),
+          "project_id": this.getWorkbench().getStudy()
+            .getUuid(),
           "node_id": this.getNodeId()
         }
       };
