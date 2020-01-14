83ab585769de5425fd828147d5389652e2300bf1
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2020-01-14 15:02:32 +0100

    added scrollbar

--- services/web/client/source/class/osparc/desktop/preferences/pages/TagsPage.js
+++ services/web/client/source/class/osparc/desktop/preferences/pages/TagsPage.js
@@ -12,7 +12,10 @@ qx.Class.define("osparc.desktop.preferences.pages.TagsPage", {
   extend: qx.ui.tabview.Page,
   construct: function() {
     this.base(arguments, null, "@FontAwesome5Solid/tags/24");
-    this.setLayout(new qx.ui.layout.VBox(10));
+    this.setLayout(new qx.ui.layout.Grow());
+    this.__container = new qx.ui.container.Composite(new qx.ui.layout.VBox(10));
+    const scroll = new qx.ui.container.Scroll(this.__container);
+    this.add(scroll);
     this.__createComponents();
   },
   members: {
@@ -31,33 +34,33 @@ qx.Class.define("osparc.desktop.preferences.pages.TagsPage", {
         .catch(err => console.error(err));
     },
     __renderLayout: function() {
-      this.removeAll();
+      this.__container.removeAll();
       // Print tag items
-      this.__tagItems.forEach(tagItem => this.add(tagItem));
+      this.__tagItems.forEach(tagItem => this.__container.add(tagItem));
       // New tag button
       const buttonContainer = new qx.ui.container.Composite(new qx.ui.layout.HBox().set({
         alignX: "center"
       }));
       buttonContainer.add(this.__addTagButton);
-      this.add(buttonContainer);
+      this.__container.add(buttonContainer);
     },
     __attachEventHandlers: function() {
       this.__addTagButton.addListener("execute", () => {
-        const itemCount = this.getChildren().length;
+        const itemCount = this.__container.getChildren().length;
         const newItem = new osparc.component.form.tag.TagItem().set({
           mode: osparc.component.form.tag.TagItem.modes.EDIT
         });
         this.__attachTagItemEvents(newItem);
-        this.addAt(newItem, Math.max(0, itemCount - 1));
+        this.__container.addAt(newItem, Math.max(0, itemCount - 1));
       });
       this.__tagItems.forEach(tagItem => this.__attachTagItemEvents(tagItem));
     },
     __attachTagItemEvents: function(tagItem) {
       tagItem.addListener("cancelNewTag", e => {
-        this.remove(e.getTarget());
+        this.__container.remove(e.getTarget());
       }, this);
       tagItem.addListener("deleteTag", e => {
-        this.remove(e.getTarget());
+        this.__container.remove(e.getTarget());
       })
     }
   }
