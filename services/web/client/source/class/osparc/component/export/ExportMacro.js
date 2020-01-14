a043083bcbd3dd93da222253dbf35b7b3af25710
odeimaiz <maiz@itis.swiss>
2020-01-14 16:54:16 +0100

    minor

--- services/web/client/source/class/osparc/component/export/ExportMacro.js
+++ services/web/client/source/class/osparc/component/export/ExportMacro.js
@@ -28,7 +28,9 @@ qx.Class.define("osparc.component.export.ExportMacro", {
     * @param node {osparc.data.model.Node} Node owning the widget
     */
   construct: function(node) {
-    this.base(arguments, new qx.ui.layout.VBox(5));
+    this.base(arguments);
+
+    this._setLayout(new qx.ui.layout.VBox(5));
 
     const key = "simcore/macros/" + osparc.utils.Utils.uuidv4();
     const version = "1.0.0";
@@ -56,18 +58,31 @@ qx.Class.define("osparc.component.export.ExportMacro", {
   members: {
     __buildLayout: function() {
       const formRenderer = this.__buildMetaDataForm();
+      this._add(formRenderer);
+
+      const scroll = new qx.ui.container.Scroll();
+      const settingsLayout = new qx.ui.container.Composite(new qx.ui.layout.HBox(10));
       this.__buildInputSettings();
       this.__buildExposedSettings();
+      scroll.add(settingsLayout);
+      this._add(scroll, {
+        flex: 1
+      });
 
-      this._add(formRenderer);
-      const settingsLayout = new qx.ui.container.Composite(new qx.ui.layout.HBox(10));
-      this._add(settingsLayout);
+      const exportBtn = new qx.ui.form.Button(this.tr("Export")).set({
+        allowGrowX: false,
+        alignX: "right",
+        padding: 10
+      });
+      exportBtn.addListener("execute", () => {
+      }, this);
+      this._add(exportBtn);
     },
 
     __buildMetaDataForm: function() {
       const metaDataForm = new qx.ui.form.Form();
 
-      const serviceName = new qx.ui.form.TextField();
+      const serviceName = new qx.ui.form.TextField(this.getInputNode().getLabel());
       serviceName.setRequired(true);
       metaDataForm.add(serviceName, this.tr("Name"));
 
