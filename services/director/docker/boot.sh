bdda0d7111dde46d8e339516484a579ed0a981dd
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-01-22 17:23:57 +0100

    added watchdog in director

--- services/director/docker/boot.sh
+++ services/director/docker/boot.sh
@@ -9,6 +9,7 @@ echo "  User    :`id $(whoami)`"
 echo "  Workdir :`pwd`"
 
 LOG_LEVEL=info
+entrypoint=''
 if [[ ${SC_BUILD_TARGET} == "development" ]]
 then
   echo $INFO "Environment :"
@@ -27,6 +28,10 @@ then
   which python | sed 's/^/    /'
   echo $INFO "PIP :"
   $SC_PIP list | sed 's/^/    /'
+
+  #------------
+  echo $INFO "setting entrypoint to use watchmedo autorestart..."
+  entrypoint='watchmedo auto-restart --recursive --pattern="*.py" --'
 fi
 
 # RUNNING application ----------------------------------------
@@ -34,7 +39,7 @@ if [[ ${SC_BOOT_MODE} == "debug-ptvsd" ]]
 then
   echo
   echo $INFO "PTVSD Debugger initializing in port 3004"
-  python3 -m ptvsd --host 0.0.0.0 --port 3000 -m \
+  eval "$entrypoint" python3 -m ptvsd --host 0.0.0.0 --port 3000 -m \
     simcore_service_director --loglevel=$LOG_LEVEL
 else
   exec simcore-service-director --loglevel=$LOG_LEVEL
