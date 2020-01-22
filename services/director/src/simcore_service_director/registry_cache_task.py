a75f5759cb54cf49e7fa561f65d524335f3eb4ac
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-01-22 16:43:06 +0100

    clear cache if exception

--- services/director/src/simcore_service_director/registry_cache_task.py
+++ services/director/src/simcore_service_director/registry_cache_task.py
@@ -1,62 +1,49 @@
 import asyncio
 import logging
-from enum import IntEnum
 
 from aiohttp import web
 
-from simcore_service_director import config, registry_proxy
+from simcore_service_director import config, exceptions, registry_proxy
 from simcore_service_director.config import APP_REGISTRY_CACHE_DATA_KEY
 
 _logger = logging.getLogger(__name__)
 
 TASK_NAME: str = __name__ + "_registry_caching_task"
-TASK_STATE: str = "{}_state".format(TASK_NAME)
-
-class State(IntEnum):
-    STARTING = 0
-    RUNNING = 1
-    FAILED = 2
-    STOPPED = 3
-
 async def registry_caching_task(app: web.Application) -> None:
     try:
         _logger.info("initializing...")
-        app[TASK_STATE] = State.STARTING
-        app[APP_REGISTRY_CACHE_DATA_KEY] = {}
+        app[APP_REGISTRY_CACHE_DATA_KEY].clear()
         _logger.info("initialisation completed")
-        app[TASK_STATE] = State.RUNNING
         while True:
             app[APP_REGISTRY_CACHE_DATA_KEY].clear()
             await registry_proxy.list_services(app, registry_proxy.ServiceType.ALL)
             await asyncio.sleep(config.REGISTRY_CACHING_TTL)
     except asyncio.CancelledError:
-        _logger.info("cancelling task...")
-        app[TASK_STATE] = State.STOPPED
-        raise
-    except:
-        _logger.exception("task closing:")
-        app[TASK_STATE] = State.FAILED
-        raise
+        _logger.info("cancelling task...")        
+    except exceptions.DirectorException:
+        _logger.exception("exception while retrieving list of services in cache")
     finally:
-        _logger.info("finished task...")
+        _logger.info("finished task...clearing cache...")
+        app[APP_REGISTRY_CACHE_DATA_KEY].clear()
 
-async def start(app: web.Application) -> None:
-    _logger.info("starting registry caching task")    
+async def setup_registry_caching_task(app: web.Application) -> None:
+    _logger.info("starting registry caching task")
+    app[APP_REGISTRY_CACHE_DATA_KEY] = {}
     app[TASK_NAME] = asyncio.get_event_loop().create_task(registry_caching_task(app))
 
-async def cleanup(app: web.Application) -> None:
+    yield
+
     task = app[TASK_NAME]
     task.cancel()
+    await task
 
 def setup(app: web.Application) -> None:
     if config.REGISTRY_CACHING:
-        app.on_startup.append(start)
-        app.on_cleanup.append(cleanup)
-
+        app.cleanup_ctx.append(setup_registry_caching_task)
+        
 
 
 __all__ = [
     "setup",
-    "State",
     "APP_REGISTRY_CACHE_DATA_KEY"
 ]
