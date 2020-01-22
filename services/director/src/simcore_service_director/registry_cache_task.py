7c3a02df07a2a35ef51ec252c0ef7e612b702f88
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-01-22 17:24:07 +0100

    added better debugging

--- services/director/src/simcore_service_director/registry_cache_task.py
+++ services/director/src/simcore_service_director/registry_cache_task.py
@@ -11,23 +11,25 @@ _logger = logging.getLogger(__name__)
 TASK_NAME: str = __name__ + "_registry_caching_task"
 async def registry_caching_task(app: web.Application) -> None:
     try:
-        _logger.info("initializing...")
+        _logger.info("%s: initializing...", TASK_NAME)
         app[APP_REGISTRY_CACHE_DATA_KEY].clear()
-        _logger.info("initialisation completed")
+        _logger.info("%s: initialisation completed", TASK_NAME)
         while True:
+            _logger.debug("%s: waking up, cleaning registry cache...", TASK_NAME)
             app[APP_REGISTRY_CACHE_DATA_KEY].clear()
+            _logger.debug("%s: retrieving services list...", TASK_NAME)
             await registry_proxy.list_services(app, registry_proxy.ServiceType.ALL)
+            _logger.debug("%s: sleeping services list...", TASK_NAME)
             await asyncio.sleep(config.REGISTRY_CACHING_TTL)
     except asyncio.CancelledError:
-        _logger.info("cancelling task...")        
+        _logger.info("%s: cancelling task...", TASK_NAME)
     except exceptions.DirectorException:
-        _logger.exception("exception while retrieving list of services in cache")
+        _logger.exception("%s: exception while retrieving list of services in cache", TASK_NAME)
     finally:
-        _logger.info("finished task...clearing cache...")
+        _logger.info("%s: finished task...clearing cache...", TASK_NAME)
         app[APP_REGISTRY_CACHE_DATA_KEY].clear()
 
 async def setup_registry_caching_task(app: web.Application) -> None:
-    _logger.info("starting registry caching task")
     app[APP_REGISTRY_CACHE_DATA_KEY] = {}
     app[TASK_NAME] = asyncio.get_event_loop().create_task(registry_caching_task(app))
 
