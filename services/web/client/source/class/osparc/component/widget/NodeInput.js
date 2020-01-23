08f1ba79d2d17f30467afabd6657f9715ba3cd5e
odeimaiz <maiz@itis.swiss>
2020-01-23 13:53:24 +0100

    Make NodeIput look like a nodeUI

--- services/web/client/source/class/osparc/component/widget/NodeInput.js
+++ services/web/client/source/class/osparc/component/widget/NodeInput.js
@@ -39,9 +39,63 @@ qx.Class.define("osparc.component.widget.NodeInput", {
   */
   construct: function(node) {
     this.base(arguments, node);
+
+    this.__populateInput();
   },
 
   members: {
+    __populateInput: function() {
+      const node = this.getNode();
+      if (!node) {
+        return;
+      }
+
+      const nodeUILayoutBig = new qx.ui.container.Composite(new qx.ui.layout.VBox());
+      nodeUILayoutBig.add(new qx.ui.core.Spacer(), {
+        flex: 1
+      });
+
+      const nodeUILayout = new qx.ui.container.Composite(new qx.ui.layout.VBox()).set({
+        backgroundColor: "node-notselected-backgroud",
+        draggable: true,
+        droppable: true
+      });
+
+      const captionBar = new qx.ui.basic.Label().set({
+        font: "title-13",
+        textColor: "#DCDCDC",
+        textAlign: "left",
+        height: 20,
+        padding: 3
+      });
+      node.bind("label", captionBar, "value");
+      nodeUILayout.add(captionBar);
+
+
+      const outPort = new qx.ui.basic.Label(this.tr("out")).set({
+        font: "text-13",
+        textColor: "#BABABA",
+        textAlign: "right",
+        allowGrowX: true,
+        alignY: "middle",
+        height: 40,
+        padding: 5
+      });
+      nodeUILayout.add(outPort);
+
+      nodeUILayout.add(new qx.ui.core.Spacer(null, 20));
+
+      nodeUILayoutBig.add(nodeUILayout);
+
+      nodeUILayoutBig.add(new qx.ui.core.Spacer(), {
+        flex: 1
+      });
+
+      this._add(nodeUILayoutBig, {
+        flex: 1
+      });
+    },
+
     populateNodeLayout: function() {
       this._populateNodeLayout(false);
     }
