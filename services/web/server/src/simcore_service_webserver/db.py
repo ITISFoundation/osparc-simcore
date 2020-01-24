062dc9e37941c988299c89db45879857cb83c4ba
Pedro Crespo <crespo@itis.ethz.ch>
2020-01-24 20:51:38 +0100

    Final version of pg setup

--- services/web/server/src/simcore_service_webserver/db.py
+++ services/web/server/src/simcore_service_webserver/db.py
@@ -1,18 +1,15 @@
 """ database submodule associated to the postgres uservice
 
-
-FIXME: _init_db is temporary here so database gets properly initialized
 """
 
 import logging
 
-import sqlalchemy as sa
+from aiopg.sa import Engine
 from aiohttp import web
-from aiopg.sa import Engine, create_engine
 from tenacity import retry
 
-from servicelib.aiopg_utils import (DataSourceName, DBAPIError,
-                                    PostgresRetryPolicyUponInitialization)
+from servicelib.aiopg_utils import (DataSourceName, PostgresRetryPolicyUponInitialization,
+                                    create_pg_engine, init_pg_tables, is_pg_responsive, raise_if_not_responsive)
 from servicelib.application_keys import APP_CONFIG_KEY, APP_DB_ENGINE_KEY
 from servicelib.application_setup import ModuleCategory, app_module_setup
 
@@ -26,18 +23,13 @@ log = logging.getLogger(__name__)
 
 
 @retry(**PostgresRetryPolicyUponInitialization(log).kwargs)
-async def _create_pg_engine(dsn: DataSourceName, minsize:int, maxsize:int) -> Engine:
-    log.info("Creating pg engine for %s", dsn)
-    engine = await create_engine(minsize=minsize, maxsize=maxsize, **dsn.asdict())
+async def _safe_create_pg_engine(dsn:DataSourceName, **kwargs_engine) -> Engine:
+    """
+        Creates engine and ensures pg services is responsive
+    """
+    engine = await create_pg_engine(dsn, **kwargs_engine)
+    await raise_if_not_responsive(engine)
     return engine
-
-def init_pg_tables(dsn: DataSourceName, schema: sa.schema.MetaData):
-    log.info("Initializing tables for %s", dsn)
-    try:
-        sa_engine = sa.create_engine(dsn.to_uri(with_query=True))
-        schema.create_all(sa_engine)
-    finally:
-        sa_engine.dispose()
 
 
 async def pg_engine(app: web.Application):
@@ -54,10 +46,15 @@ async def pg_engine(app: web.Application):
             port=pg_cfg['port']
         )
 
+    log.info("Creating pg engine for %s", dsn)
     app[APP_DB_ENGINE_KEY] = engine = \
-        await _create_pg_engine(dsn, minsize=pg_cfg['minsize'], maxsize=pg_cfg['maxsize'])
+        await _safe_create_pg_engine(dsn,
+            minsize=pg_cfg['minsize'],
+            maxsize=pg_cfg['maxsize']
+        )
 
     if cfg['init_tables']:
+        log.debug("Initializing tables for %s", dsn)
         init_pg_tables(dsn, schema=metadata)
 
     yield #-------------------
@@ -81,16 +78,8 @@ async def is_service_responsive(app:web.Application):
     """
     if not is_service_enabled(app):
         return False
-
-    engine = app[APP_DB_ENGINE_KEY]
-    try:
-        async with engine.acquire() as conn:
-            await conn.execute("SELECT 1 as is_alive")
-            log.debug("%s is alive", THIS_SERVICE_NAME)
-            return True
-    except DBAPIError as err:
-        log.debug("%s is NOT responsive: %s", THIS_SERVICE_NAME, err)
-        return False
+    is_responsive = await is_pg_responsive(engine=app[APP_DB_ENGINE_KEY])
+    return is_responsive
 
 
 @app_module_setup(__name__, ModuleCategory.SYSTEM, logger=log)
