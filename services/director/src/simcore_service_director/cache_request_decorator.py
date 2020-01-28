7d6b9494e054d310a4698ba19230698dacd1ffff
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-01-28 12:25:57 +0100

    code review from @crespov

--- services/director/src/simcore_service_director/cache_request_decorator.py
+++ services/director/src/simcore_service_director/cache_request_decorator.py
@@ -1,16 +1,16 @@
 from functools import wraps
-from typing import Dict, Tuple
+from typing import Callable, Dict, Tuple
 
 from aiohttp import web
+from simcore_service_director import config
 from yarl import URL
 
-from simcore_service_director import config
 
-
-def cache_requests(http_request: callable):
+def cache_requests(http_request: Callable):
     @wraps(http_request)
     async def wrapped(app: web.Application, url: URL, method: str ="GET") -> Tuple[Dict, Dict]:
-        if config.DIRECTOR_REGISTRY_CACHING and method == "GET":
+        is_cache_enabled = config.DIRECTOR_REGISTRY_CACHING and method == "GET"
+        if is_cache_enabled:
             cache_data = app[config.APP_REGISTRY_CACHE_DATA_KEY]
             cache_key = "{}_{}".format(url, method)
             if cache_key in cache_data:
@@ -18,11 +18,11 @@ def cache_requests(http_request: callable):
 
         resp_data, resp_headers = await http_request(app, url, method)
 
-        if config.DIRECTOR_REGISTRY_CACHING and method == "GET":
+        if is_cache_enabled:
             cache_data[cache_key] = (resp_data, resp_headers)
-        
+
         return (resp_data, resp_headers)
-            
+
     return wrapped
 
 __all__ = [
