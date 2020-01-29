827ee1857f16de486ec734ee23071c20888e816e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-01-29 15:41:09 +0100

    parsing progress in the backend updating project table with computational services updates

--- services/web/server/src/simcore_service_webserver/projects/projects_api.py
+++ services/web/server/src/simcore_service_webserver/projects/projects_api.py
@@ -7,7 +7,8 @@
         - upon failure raise errors that can be also HTTP reponses
 """
 import logging
-from asyncio import gather, ensure_future
+from asyncio import ensure_future, gather
+from pprint import pprint
 from typing import Dict, Optional
 from uuid import uuid4
 
@@ -39,16 +40,16 @@ def validate_project(app: web.Application, project: Dict):
     validate_instance(project, project_schema) # TODO: handl
 
 
-async def get_project_for_user(request: web.Request, project_uuid, user_id, *, include_templates=False) -> Dict:
+async def get_project_for_user(app: web.Application, project_uuid, user_id, *, include_templates=False) -> Dict:
     """ Returns a project accessible to user
 
     :raises web.HTTPNotFound: if no match found
     :return: schema-compliant project data
     :rtype: Dict
     """
-    await check_permission(request, "project.read")
+
     try:
-        db = request.config_dict[APP_PROJECT_DBAPI]
+        db = app[APP_PROJECT_DBAPI]
 
         project = None
         if include_templates:
@@ -59,7 +60,7 @@ async def get_project_for_user(request: web.Request, project_uuid, user_id, *, i
 
         # TODO: how to handle when database has an invalid project schema???
         # Notice that db model does not include a check on project schema.
-        validate_project(request.app, project)
+        validate_project(app, project)
         return project
 
     except ProjectNotFoundError:
@@ -181,3 +182,37 @@ async def delete_project_node(request: web.Request, project_uuid: str, user_id: 
             break
     # remove its data if any
     await delete_data_folders_of_project_node(request.app, project_uuid, node_uuid, user_id)
+
+
+async def update_project_node_progress(app: web.Application, user_id: str, project_id: str, node_id: str, progress: float) -> None:
+    log.debug("updating node %s progress in project %s for user %s with %s", node_id, project_id, user_id, progress)
+    project = await get_project_for_user(app, project_id, user_id)
+    if not node_id in project["workbench"]:
+        log.warning("progress update: node %s is not in project %s, was it deleted?", node_id, project_id)
+        return
+    project["workbench"][node_id]["progress"] = int(100.0 * float(progress) + .5)
+    db = app[APP_PROJECT_DBAPI]
+    await db.update_user_project(project, user_id, project_id)
+
+async def update_project_node_outputs(app: web.Application, user_id: str, project_id: str, node_id: str, data: Dict) -> None:
+    log.debug("updating node %s outputs in project %s for user %s with %s", node_id, project_id, user_id, pprint(data))
+    project = await get_project_for_user(app, project_id, user_id)
+    if not node_id in project["workbench"]:
+        log.warning("progress update: node %s is not in project %s, was it deleted?", node_id, project_id)
+        return
+    node_description = project["workbench"][node_id]
+    node_description["outputs"] = data
+    # update outputs if necessary
+    for output_key in node_description["outputs"].keys():
+        if not isinstance(node_description["outputs"][output_key], dict):
+            continue
+        if "path" in node_description["outputs"][output_key]:
+            # file_id is of type study_id/node_id/file.ext
+            file_id = node_description["outputs"][output_key]["path"]
+            study_id, _, file_ext = file_id.split("/")
+            node_description["outputs"][output_key]["dataset"] = study_id
+            node_description["outputs"][output_key]["label"] = file_ext
+
+    db = app[APP_PROJECT_DBAPI]
+    await db.update_user_project(project, user_id, project_id)
+
