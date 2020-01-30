a0bfacd8adde2dd6e6f5e047b75277f3ab3f95b8
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-01-30 15:51:01 +0100

    Fix/proper deallocation (#1265)
    Fixes postgres setup in both webserver and storage services
    
    - fixes pg init in webserver and storage
    - fixes redis init in webserver and adds dependency in docker-compose
    - moves common functionality to servicelib (packages/service-library/src/servicelib/aiopg_utils.py)
     enhances log when when pg retry policy fails
    - upgrades tenacity
    - upgrades to postgres:10.11 (some post recommends to upgrade if many disconnects happen...)
    - sanderegg review: upgraded psycopg2-binary==2.8.4
    - Recipes to upgrade requirements

--- packages/service-library/src/servicelib/aiopg_utils.py
+++ packages/service-library/src/servicelib/aiopg_utils.py
@@ -1,4 +1,8 @@
-""" Helpers for aiopg
+""" Holderplace for random helpers using aiopg
+
+    - Drop here functions/constants that at that time does
+    not fit in any of the setups. Then, they can be moved and
+    refactor when new abstractions are used in place.
 
     - aiopg is used as a client sdk to interact asynchronously with postgres service
 
@@ -15,7 +19,7 @@ from typing import Dict, Optional
 import attr
 import sqlalchemy as sa
 from aiohttp import web
-from aiopg.sa import create_engine
+from aiopg.sa import Engine, create_engine
 from psycopg2 import DatabaseError
 from psycopg2 import Error as DBAPIError
 from tenacity import (RetryCallState, after_log, before_sleep_log, retry,
@@ -23,6 +27,7 @@ from tenacity import (RetryCallState, after_log, before_sleep_log, retry,
 
 log = logging.getLogger(__name__)
 
+DSN = "postgresql://{user}:{password}@{host}:{port}/{database}"
 
 @attr.s(auto_attribs=True)
 class DataSourceName:
@@ -36,21 +41,64 @@ class DataSourceName:
     # Attributes about the caller
     application_name: Optional[str]=None
 
-
     def asdict(self) -> Dict:
         return attr.asdict(self)
 
     def to_uri(self, with_query=False) -> str:
-        uri = f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+        uri = DSN.format(**attr.asdict(self))
         if with_query and self.application_name:
-            uri += f"?ApplicationName={self.application_name}"
+            uri += f"?application_name={self.application_name}"
         return uri
 
 
 
-def is_postgres_responsive(dsn: DataSourceName) -> bool:
-    """ Returns True if can connect and operate postgres service
+def create_pg_engine(dsn: DataSourceName, minsize:int=1, maxsize:int=4):
+    """ Adapts the arguments of aiopg.sa.create_engine
+
+        Returns a coroutine that is awaitable, i.e.
+
+        async with create_pg_engine as engine:
+            assert not engine.closed
+
+        assert engine.closed
     """
+    awaitable_engine_coro = create_engine(dsn.to_uri(),
+        application_name=dsn.application_name,
+        minsize=minsize,
+        maxsize=maxsize
+    )
+    return awaitable_engine_coro
+
+
+async def raise_if_not_responsive(engine: Engine):
+    async with engine.acquire() as conn:
+        await conn.execute("SELECT 1 as is_alive")
+
+
+async def is_pg_responsive(engine: Engine, *, raise_if_fails=False) -> bool:
+    try:
+        await raise_if_not_responsive(engine)
+    except DBAPIError as err:
+        log.debug("%s is not responsive: %s", engine.dsn, err)
+        if raise_if_fails:
+            raise
+        return False
+    else:
+        return True
+
+
+def init_pg_tables(dsn: DataSourceName, schema: sa.schema.MetaData):
+    try:
+        # CONS: creates and disposes an engine just to create tables
+        # TODO: find a way to create all tables with aiopg engine
+        sa_engine = sa.create_engine(dsn.to_uri(with_query=True))
+        schema.create_all(sa_engine)
+    finally:
+        sa_engine.dispose()
+
+
+def is_postgres_responsive(dsn: DataSourceName) -> bool:
+    # NOTE: keep for tests
     engine = conn = None
     try:
         engine = sa.create_engine(dsn.to_uri())
@@ -67,21 +115,11 @@ def is_postgres_responsive(dsn: DataSourceName) -> bool:
     return ok
 
 
-async def is_postgres_responsive_async(dsn: DataSourceName) -> bool:
-    try:
-        async with create_engine(dsn.to_uri(), application_name=dsn.application_name) as engine:
-            async with engine.acquire() as conn:
-                await conn.execute("SELECT 1 as is_alive")
-                return True
-    except DBAPIError as err:
-        log.debug("%s not responsive: %s", dsn, err)
-        return False
-
-
 
 def raise_http_unavailable_error(retry_state: RetryCallState):
-    # aiopg reuses DBAPI exceptions
-    #
+    # TODO: mark incident on db to determine the quality of service. E.g. next time we do not stop. TIP: obj, query = retry_state.args; obj.app.register_incidents
+
+    exc :DatabaseError  = retry_state.outcome.exception()
     # StandardError
     # |__ Warning
     # |__ Error
@@ -94,15 +132,19 @@ def raise_http_unavailable_error(retry_state: RetryCallState):
     #         |__ ProgrammingError
     #         |__ NotSupportedError
     #
+    # aiopg reuses DBAPI exceptions
     # SEE https://aiopg.readthedocs.io/en/stable/core.html?highlight=Exception#exceptions
     # SEE http://initd.org/psycopg/docs/module.html#dbapi-exceptions
-    # TODO: mark incident on db to determine the quality of service. E.g. next time we do not stop.
-    # TODO: add header with Retry-After
-    #obj, query = retry_state.args
-    #obj.app.register_incidents
-    # https://tools.ietf.org/html/rfc7231#section-7.1.3
-    raise web.HTTPServiceUnavailable()
 
+
+    # TODO: add header with Retry-After https://tools.ietf.org/html/rfc7231#section-7.1.3
+    resp = web.HTTPServiceUnavailable()
+
+    # logs
+    msg = f"Postgres service non-responsive, responding {resp.status_code}: {str(exc) or repr(exc)}"
+    log.error(msg)
+
+    raise resp
 
 
 class PostgresRetryPolicyUponInitialization:
@@ -134,7 +176,7 @@ class PostgresRetryPolicyUponOperation:
             retry=retry_if_exception_type(DatabaseError),
             wait=wait_fixed(self.WAIT_SECS),
             stop=stop_after_attempt(self.ATTEMPTS_COUNT),
-            after=after_log(logger, logging.ERROR),
+            after=after_log(logger, logging.WARNING),
             retry_error_callback=raise_http_unavailable_error
         )
 
