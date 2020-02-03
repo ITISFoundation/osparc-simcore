808bcc9730745670e6b48354e198390724c210f1
Pedro Crespo <crespo@itis.ethz.ch>
2020-02-03 13:40:22 +0100

    Includes code-climate config and script

---
+++ scripts/code-climate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Runs code climate locally on CWD
+#
+# SEE https://github.com/codeclimate/codeclimate#manual-docker-invocation
+#
+echo Running codeclimate on "$PWD"
+
+TMPDIR=/tmp/cc
+mkdir --parent ${TMPDIR}
+
+docker run \
+  --interactive --tty --rm \
+  --env CODECLIMATE_CODE="$PWD" \
+  --volume "$PWD":/code \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
+  --volume ${TMPDIR}:/tmp/cc \
+  codeclimate/codeclimate "$@"
+
+
+if [ -z "$@" ];then
+  echo "----"
+  echo "Listing other engines (in dockers)"
+  docker images codeclimate/*
+fi
