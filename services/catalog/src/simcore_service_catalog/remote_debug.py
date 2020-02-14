c35e31bde8f5a384e8038123e16bd0c8dd8b4f93
Pedro Crespo <crespo@itis.ethz.ch>
2020-02-14 16:54:51 +0100

    Adding remote debugging module

---
+++ services/catalog/src/simcore_service_catalog/remote_debug.py
@@ -0,0 +1,42 @@
+""" Setup remote debugger with Python Tools for Visual Studio (PTVSD)
+
+"""
+import logging
+import os
+
+REMOTE_DEBUG_PORT = 3000
+
+log = logging.getLogger(__name__)
+
+
+def setup_remote_debugging(force_enabled=False):
+    """
+        Programaticaly enables remote debugging if SC_BOOT_MODE==debug-ptvsd
+    """
+    try:
+        boot_mode = os.environ["SC_BOOT_MODE"]
+    except KeyError:
+        raise ValueError(
+            "Remote debugging only allowed when running in a container "
+            "and SC_BOOT_MODE environ variable is defined"
+        )
+
+    if boot_mode == "debug-ptvsd" or force_enabled:
+        try:
+            log.debug("Enabling attach ptvsd ...")
+            #
+            # SEE https://github.com/microsoft/ptvsd#enabling-debugging
+            #
+            import ptvsd
+            ptvsd.enable_attach(address=('0.0.0.0', REMOTE_DEBUG_PORT), redirect_output=True) # nosec
+        except ImportError:
+            raise ValueError("Cannot enable remote debugging. Please install ptvsd first")
+
+        log.info("Remote debugging enabled: listening port %s", REMOTE_DEBUG_PORT)
+    else:
+        log.debug("Booting without remote debugging since SC_BOOT_MODE=%s", boot_mode)
+
+
+__all__ = [
+    'setup_remote_debugging'
+]
