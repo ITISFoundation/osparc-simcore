382c6627ca62d93c76da0ef7c5b2a7ef72181f67
Pedro Crespo <crespo@itis.ethz.ch>
2020-02-15 15:17:21 +0100

    Cleanup

--- services/catalog/src/simcore_service_catalog/__main__.py
+++ services/catalog/src/simcore_service_catalog/__main__.py
@@ -2,10 +2,6 @@
 
  `python -m simcore_service_catalog ...`
 
-Why does this file exist, and why __main__? For more info, read:
-
-- https://www.python.org/dev/peps/pep-0338/
-- https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
 import uvicorn
 
