6e65107102be08bf9361d32e90beaaae2d11a308
Pedro Crespo <crespo@itis.ethz.ch>
2020-02-17 14:26:20 +0100

    Fixes project schema

--- services/catalog/src/simcore_service_catalog/schemas/project.py
+++ services/catalog/src/simcore_service_catalog/schemas/project.py
@@ -5,26 +5,37 @@
 # pylint: disable=no-name-in-module
 
 
+import sys
 from enum import Enum
-from typing import Dict, List, Optional
+from pathlib import Path
+from typing import Dict, List, Optional, Union
 
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 KEY_RE = r'^(simcore)/(services)(/demodec)?/(comp|dynamic|frontend)(/[^\s]+)+$'
 VERSION_RE = r'^(0|[1-9]\d*)(\.(0|[1-9]\d*)){2}(-(0|[1-9]\d*|\d*[-a-zA-Z][-\da-zA-Z]*)(\.(0|[1-9]\d*|\d*[-a-zA-Z][-\da-zA-Z]*))*)?(\+[-\da-zA-Z]+(\.[-\da-zA-Z-]+)*)?$'
 
+current_dir = Path( sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
-class AccessEnum(Enum):
+class Connection(BaseModel):
+    nodeUuid: Optional[str]
+    output: Optional[str]
+
+class FilePickerOutput(BaseModel):
+    store: Union[str, int] # simcore/datcore
+    dataset: Optional[str] #
+    path: str #
+    label: str # name of the file
+
+InputTypes = Union[int, bool, str, float, Connection, FilePickerOutput]
+OutputTypes = Union[int, bool, str, float, FilePickerOutput]
+
+
+class AccessEnum(str, Enum):
     ReadAndWrite="ReadAndWrite"
     Invisible="Invisible"
     ReadOnly="ReadOnly"
 
-
-class Input(BaseModel):
-    pass
-
-class Output(BaseModel):
-    pass
 
 class Position(BaseModel):
     x: int
@@ -34,24 +45,34 @@ class Position(BaseModel):
 class Node(BaseModel):
     key: str = Field(..., regex=KEY_RE, example="simcore/services/comp/sleeper")
     version: str = Field(..., regex=VERSION_RE, example="6.2.0")
-    label: str
-    progress: float
+    label: str = Field(...)
+    progress: float = Field(0, ge=0, le=100)
     thumbnail: Optional[str]
-    inputs: Dict[str, Input]
-    inputAccess: Dict[str, AccessEnum]
-    inputNodes: List[str]
-    outputs: Dict[str, Output]
-    # outputNodes:
-    parent: str
-    position: Position
+
+    inputs: Optional[Dict[str, InputTypes]]
+    inputAccess: Dict[str, AccessEnum] = Field(AccessEnum.ReadAndWrite)
+    inputNodes: List[str] = []
+
+    outputs: Optional[Dict[str, OutputTypes]] = None
+    outputNode: Optional[bool] = Field(None, deprecated=True)
+    outputNodes: List[str] = []
+
+    parent: Optional[str] = Field(None, description="Parent's (group-nodes') node ID s.", example="nodeUUid1")
+
+    position: Position = Field(...)
 
 
 class Project(BaseModel):
     uuid: str
     name: str
     description: str
-    prjOwner: Optional[EmailStr]
+    prjOwner: EmailStr
     creationDate: str
     lastChangeDate: str
     thumbnail: str
     workbench: Dict[str, Node]
+    tags: Optional[List[int]] = []
+
+
+#with open( current_dir / "project.json", 'wt') as fh:
+#    print(Project.schema_json(indent=2), file=fh)
