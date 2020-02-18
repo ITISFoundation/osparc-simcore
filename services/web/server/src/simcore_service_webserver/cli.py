cd45f0f0af595cc2186ad834e6a8fd7e98c651d1
Pedro Crespo <crespo@itis.ethz.ch>
2020-02-18 11:56:05 +0100

    EMAIL environs set as defaults instead of explicit

--- services/web/server/src/simcore_service_webserver/cli.py
+++ services/web/server/src/simcore_service_webserver/cli.py
@@ -12,25 +12,25 @@ Why does this file exist, and why not put this in __main__?
     there's no ``simcore_service_webserver.__main__`` in ``sys.modules``.
 
 """
-import argparse
 import logging
 import os
 import sys
+from argparse import ArgumentParser
+from typing import Dict
 
 from .application import run_service
 from .application_config import CLI_DEFAULT_CONFIGFILE, app_schema
 from .cli_config import add_cli_options, config_from_options
 from .utils import search_osparc_repo_dir
-from typing import Dict
 
 log = logging.getLogger(__name__)
 
 
-def create_default_parser():
-    return argparse.ArgumentParser(description='Service to manage data webserver in simcore.')
+def create_default_parser() -> ArgumentParser:
+    return ArgumentParser(description='Service to manage data webserver in simcore.')
 
 
-def setup_parser(parser):
+def setup_parser(parser: ArgumentParser) -> ArgumentParser:
     """ Adds all options to a parser"""
     #parser.add_argument('names', metavar='NAME', nargs=argparse.ZERO_OR_MORE,
     #                help="A name of something.")
@@ -62,42 +62,47 @@ def create_environ(*, skip_host_environ: bool=False) -> Dict[str, str]:
             'OSPARC_SIMCORE_REPO_ROOTDIR': str(rootdir),
         })
 
-    # Defaults if not defined in environ
+    # DEFAULTS if not defined in environ
     # NOTE: unfortunately, trafaret does not allow defining default directly in the config.yamla
-    # as docker-compose does: i.e. x = ${VARIABLE:default}. Intead, the variable
-    # has to be defined here
+    # as docker-compose does: i.e. x = ${VARIABLE:default}.
+    #
+    # Instead, the variable has to be defined here ------------
     environ.setdefault("WEBSERVER_DB_INITTABLES", "0")
+    environ.setdefault("SMTP_USERNAME", "None")
+    environ.setdefault("SMTP_PASSWORD", "None")
+    environ.setdefault("SMTP_TLS_ENABLED", "0")
+
+    #----------------------------------------------------------
+
     return environ
 
 
-
-def parse(args, parser):
+def parse(args: Optional[List], parser: ArgumentParser) -> Dict:
     """ Parse options and returns a configuration object """
     if args is None:
         args = sys.argv[1:]
 
     # ignore unknown options
     options, _ = parser.parse_known_args(args)
-
     config = config_from_options(options, app_schema, vars=create_environ())
 
-    # TODO: check whether extra options can be added to the config?!
     return config
 
 
-def main(args=None):
+def main(args: Optional[List]=None):
+
     # parse & config file
-    parser = argparse.ArgumentParser(description='Service to manage data webserver in simcore.')
+    parser = ArgumentParser(description='Service to manage data webserver in simcore.')
     setup_parser(parser)
     config = parse(args, parser)
 
     # logging
-    # TODO: improve keys!
     log_level = getattr(logging, config["main"]["log_level"])
     logging.basicConfig(level=log_level)
 
     logging.root.setLevel(log_level)
-    # mute noisy
+
+    # mute noisy loggers
     logging.getLogger("openapi_spec_validator").setLevel(logging.WARNING)
     logging.getLogger("sqlalchemy").setLevel(logging.WARNING)
 
