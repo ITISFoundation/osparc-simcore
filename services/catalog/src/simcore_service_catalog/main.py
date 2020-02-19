c00baeb5eef1fa26b89f06666169461c62ce3ab5
Pedro Crespo <crespo@itis.ethz.ch>
2020-02-19 18:47:50 +0100

    Removed dummy data

--- services/catalog/src/simcore_service_catalog/main.py
+++ services/catalog/src/simcore_service_catalog/main.py
@@ -3,9 +3,8 @@ import sys
 from pathlib import Path
 
 import yaml
-from tenacity import Retrying, before_sleep_log, stop_after_attempt, wait_fixed
-
 from fastapi import FastAPI
+from tenacity import Retrying, before_sleep_log, stop_after_attempt, wait_fixed
 
 from .__version__ import api_version, api_version_prefix
 from .config import is_testing_enabled
@@ -13,13 +12,14 @@ from .db import create_tables, setup_engine, teardown_engine
 from .endpoints import dags, diagnostics
 from .remote_debug import setup_remote_debugging
 
-current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+current_dir = Path(sys.argv[0] if __name__ ==
+                   "__main__" else __file__).resolve().parent
 
 log = logging.getLogger(__name__)
 
 
 app = FastAPI(
-    debug = is_testing_enabled,
+    debug=is_testing_enabled,
     title="Components Catalog Service",
     # TODO: get here extended description from setup
     description="Manages and maintains a **catalog** of all published components (e.g. macro-algorithms, scripts, etc)",
@@ -31,20 +31,17 @@ app = FastAPI(
 app.include_router(diagnostics.router, tags=['diagnostics'])
 app.include_router(dags.router, tags=['dags'], prefix=f"/{api_version_prefix}")
 
-#TODO: remove
-#from .endpoints import dusers
-#  app.include_router(dusers.router, tags=['dummy'], prefix=f"/v{API_MAJOR_VERSION}")
 
 def dump_openapi():
     oas_path: Path = current_dir / f"api/{api_version_prefix}/openapi.yaml"
     log.info("Saving openapi schema to %s", oas_path)
-    with open( oas_path, 'wt') as fh:
+    with open(oas_path, 'wt') as fh:
         yaml.safe_dump(app.openapi(), fh)
 
 
 @app.on_event("startup")
 def startup_event():
-    log.info( "Application started")
+    log.info("Application started")
     setup_remote_debugging()
 
 
@@ -71,10 +68,8 @@ async def start_db():
 def shutdown_event():
     log.info("Application shutdown")
 
+
 @app.on_event("shutdown")
 async def shutdown_db():
     log.info("Shutting down db")
     await teardown_engine()
-
-## DEBUG: uvicorn simcore_service_components_catalog.main:app --reload
-# TODO: use entry-point to call uvicorn's entrypoint above
