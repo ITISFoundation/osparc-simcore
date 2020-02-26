907bfce2b3b87040adb88f6fd2d3b991fd5783e0
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-02-26 14:42:01 +0100

    linting

--- services/web/server/src/simcore_service_webserver/socketio/__init__.py
+++ services/web/server/src/simcore_service_webserver/socketio/__init__.py
@@ -7,7 +7,7 @@ import logging
 from aiohttp import web
 from servicelib.application_keys import APP_CONFIG_KEY
 from servicelib.application_setup import ModuleCategory, app_module_setup
-from socketio import AsyncAioPikaManager, AsyncServer
+from socketio import AsyncServer
 
 from . import handlers, handlers_utils
 from .config import APP_CLIENT_SOCKET_SERVER_KEY, CONFIG_SECTION_NAME
@@ -18,7 +18,6 @@ log = logging.getLogger(__name__)
 @app_module_setup(__name__, ModuleCategory.SYSTEM, logger=log)
 def setup(app: web.Application):
     mgr = None
-    cfg = app[APP_CONFIG_KEY][CONFIG_SECTION_NAME]
     sio = AsyncServer(async_mode="aiohttp", client_manager=mgr, logging=log)
     sio.attach(app)
     app[APP_CLIENT_SOCKET_SERVER_KEY] = sio    
