dea2ee9e62c48b0d3cf1d467ebf59a892c82a027
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-02-28 12:55:37 +0100

    shellcheck

--- services/sidecar/docker/boot.sh
+++ services/sidecar/docker/boot.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 #
-INFO="INFO: [`basename "$0"`] "
-ERROR="ERROR: [`basename "$0"`] "
+INFO="INFO: [$(basename "$0")] "
 
 # BOOTING application ---------------------------------------------
-echo $INFO "Booting in ${SC_BOOT_MODE} mode ..."
-echo "  User    :`id $(whoami)`"
-echo "  Workdir :`pwd`"
+echo "$INFO" "Booting in ${SC_BOOT_MODE} mode ..."
+echo "  User    :$(id "$(whoami)")"
+echo "  Workdir :$(pwd)"
 
-if [[ ${SC_BUILD_TARGET} == "development" ]]
+if [ "${SC_BUILD_TARGET}" = "development" ]
 then
-  echo $INFO "Environment :"
+  echo "$INFO" "Environment :"
   printenv  | sed 's/=/: /' | sed 's/^/    /' | sort
   #--------------------
 
-  cd services/sidecar
+  cd services/sidecar || exit
   $SC_PIP install --user -r requirements/dev.txt
-  cd /devel
+  cd /devel || exit
 
   #--------------------
-  echo $INFO "Python :"
+  echo "$INFO" "Python :"
   python --version | sed 's/^/    /'
   which python | sed 's/^/    /'
-  echo $INFO "PIP :"
+  echo "$INFO" "PIP :"
   $SC_PIP list | sed 's/^/    /'
 fi
 
@@ -32,7 +31,7 @@ fi
 CONCURRENCY=1
 POOL=prefork
 
-if [[ ${SC_BOOT_MODE} == "debug-ptvsd" ]]
+if [ "${SC_BOOT_MODE}" = "debug-ptvsd" ]
 then
   # NOTE: in this case, remote debugging is only available in development mode!
   # FIXME: workaround since PTVSD does not support prefork subprocess debugging: https://github.com/microsoft/ptvsd/issues/943
@@ -42,5 +41,5 @@ fi
 exec celery worker \
     --app sidecar.celery:app \
     --concurrency ${CONCURRENCY} \
-    --loglevel=${SIDECAR_LOGLEVEL-WARNING} \
+    --loglevel="${SIDECAR_LOGLEVEL-WARNING}" \
     --pool=${POOL}
