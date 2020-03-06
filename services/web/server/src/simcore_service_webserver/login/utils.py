6e848486501badec193b30c47f5c34f80d2deeb2
Pedro Crespo <crespo@itis.ethz.ch>
2020-03-06 19:52:43 +0100

    Using password generator for get_random_string

--- services/web/server/src/simcore_service_webserver/login/utils.py
+++ services/web/server/src/simcore_service_webserver/login/utils.py
@@ -1,43 +1,43 @@
 import random
-import string
 from email.mime.text import MIMEText
 from logging import getLogger
 from os.path import join
 from pprint import pformat
+from typing import Mapping, Optional
 
 import aiosmtplib
 import attr
 import passlib.hash
-from aiohttp_jinja2 import render_string
-
 from aiohttp import web
+from aiohttp_jinja2 import render_string
+from passlib import pwd
+
 from servicelib.rest_models import LogMessageType
 
 from ..resources import resources
 from .cfg import cfg  # TODO: remove this singleton!!!
 
-CHARS = string.ascii_uppercase + string.ascii_lowercase + string.digits
 log = getLogger(__name__)
 
 
-def encrypt_password(password):
+def encrypt_password(password: str) -> str:
     # TODO: add settings sha256_crypt.using(**settings).hash(secret)
     # see https://passlib.readthedocs.io/en/stable/lib/passlib.hash.sha256_crypt.html
     #
     return passlib.hash.sha256_crypt.using(rounds=1000).hash(password)
 
 
-def check_password(password, password_hash):
+def check_password(password: str, password_hash: str) -> bool:
     return passlib.hash.sha256_crypt.verify(password, password_hash)
 
 
-def get_random_string(min_len, max_len=None):
+def get_random_string(min_len: int, max_len: Optional[int] = None) -> str:
     max_len = max_len or min_len
     size = random.randint(min_len, max_len)
-    return "".join(random.choice(CHARS) for x in range(size))
+    return pwd.genword(entropy=52, length=size)
 
 
-def get_client_ip(request):
+def get_client_ip(request: web.Request) -> str:
     try:
         ips = request.headers["X-Forwarded-For"]
     except KeyError:
@@ -45,7 +45,7 @@ def get_client_ip(request):
     return ips.split(",")[0]
 
 
-async def send_mail(recipient, subject, body):
+async def send_mail(recipient: str, subject: str, body: str) -> None:
     # TODO: move to email submodule
     smtp_args = dict(
         loop=cfg.APP.loop,
@@ -82,7 +82,9 @@ async def send_mail(recipient, subject, body):
             await smtp.send_message(msg)
 
 
-async def render_and_send_mail(request, to, template, context=None):
+async def render_and_send_mail(
+    request: web.Request, to: str, template: str, context: Mapping
+):
     page = render_string(str(template), request, context)
     subject, body = page.split("\n", 1)
     await send_mail(to, subject.strip(), body)
@@ -96,7 +98,7 @@ def common_themed(template):
     return resources.get_path(join(cfg.COMMON_THEME, template))
 
 
-def flash_response(msg: str, level: str = "INFO"):
+def flash_response(msg: str, level: str = "INFO") -> web.Response:
     response = web.json_response(
         data={"data": attr.asdict(LogMessageType(msg, level)), "error": None}
     )
