ba23a3756af9ec8f725eb2275bfcc2a97abd229b
Pedro Crespo <crespo@itis.ethz.ch>
2020-03-09 15:32:40 +0100

    Refined test_ujson_installation

--- tests/swarm-deploy/test_service_images.py
+++ tests/swarm-deploy/test_service_images.py
@@ -7,11 +7,16 @@ from typing import Dict
 import pytest
 
 
+# search ujson in all _base.txt and add here all services that contains it
 @pytest.mark.parametrize("service", [
     'director',
     'webserver',
+    'storage',
+    'catalog'
 ])
 def test_ujson_installation(service:str, osparc_deploy: Dict):
+    # tets failing installation undetected
+    # and fixed in PR https://github.com/ITISFoundation/osparc-simcore/pull/1353
     image_name = osparc_deploy['simcore']['services'][service]['image']
 
     assert subprocess.run(
