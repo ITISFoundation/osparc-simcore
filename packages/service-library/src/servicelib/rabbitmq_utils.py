304c222947b9a0fabf182e9e0d9f49bea7d3c5fe
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-13 18:21:44 +0100

    lintin

--- packages/service-library/src/servicelib/rabbitmq_utils.py
+++ packages/service-library/src/servicelib/rabbitmq_utils.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Optional
 
-from tenacity import before_sleep_log, retry, stop_after_attempt, wait_fixed
+from tenacity import before_sleep_log, stop_after_attempt, wait_fixed
 
 log = logging.getLogger(__file__)
 
