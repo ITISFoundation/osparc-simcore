452eae746091751749078709a7260f0b20ad95f1
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-13 11:14:40 +0100

    add environs fixture

--- services/web/server/tests/integration/conftest.py
+++ services/web/server/tests/integration/conftest.py
@@ -34,6 +34,7 @@ current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve(
 sys.path.append(str(current_dir / '../../../../../packages'))
 # imports the fixtures for the integration tests
 pytest_plugins = [
+    "pytest-fixtures.environs",
     "pytest-fixtures.docker_compose",
     "pytest-fixtures.docker_swarm",
     "pytest-fixtures.docker_registry",
