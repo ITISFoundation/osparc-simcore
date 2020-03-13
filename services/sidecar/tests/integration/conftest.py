e2375a4900d2b63382846ccf84e6884ec3b08389
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-13 10:39:46 +0100

    minor

--- services/sidecar/tests/integration/conftest.py
+++ services/sidecar/tests/integration/conftest.py
@@ -1,9 +1,10 @@
 import logging
 import sys
+from pathlib import Path
 
 current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 # TODO: this should be done as a real pytest plugin instead
-sys.path.append(str(current_dir / '../../../../packages'))
+sys.path.append(str(current_dir / "../../../../packages"))
 print(sys.path)
 # imports the fixtures for the integration tests
 pytest_plugins = [
@@ -14,6 +15,6 @@ pytest_plugins = [
     "pytest-fixtures.celery_service",
     "pytest-fixtures.postgres_service",
     "pytest-fixtures.redis_service",
-    "pytest-fixtures.websocket_client"
+    "pytest-fixtures.websocket_client",
 ]
 log = logging.getLogger(__name__)
