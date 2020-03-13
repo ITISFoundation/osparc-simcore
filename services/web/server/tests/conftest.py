ab92b2ce832c99d1fcedf1d5121dcf9f6ead1e21
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-13 11:47:47 +0100

    unit tests not finding helpers

--- services/web/server/tests/conftest.py
+++ services/web/server/tests/conftest.py
@@ -22,7 +22,9 @@ log = logging.getLogger(__name__)
 logging.getLogger("openapi_spec_validator").setLevel(logging.WARNING)
 logging.getLogger("sqlalchemy").setLevel(logging.WARNING)
 
-
+# TODO: this should be done as a real pytest plugin instead
+sys.path.append(str(current_dir / '../../../../packages'))
+sys.path.append(str(current_dir / '../../../../packages/helpers'))
 
 @pytest.fixture(scope="session")
 def package_dir() -> Path:
