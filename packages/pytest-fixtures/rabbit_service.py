92ee5aa23b083fd062875a3fb228718a89ace4e9
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-13 10:14:08 +0100

    moving fixtures to package pytest-fixtures

--- packages/pytest-fixtures/rabbit_service.py
+++ packages/pytest-fixtures/rabbit_service.py
@@ -4,7 +4,6 @@
 # pylint:disable=unused-argument
 # pylint:disable=redefined-outer-name
 
-import os
 from copy import deepcopy
 from typing import Dict
 
@@ -12,39 +11,19 @@ import aio_pika
 import pytest
 import tenacity
 
-from servicelib.rabbitmq_utils import RabbitMQRetryPolicyUponInitialization
-from utils_docker import get_service_published_port
-
-
-@pytest.fixture(scope="module")
-def rabbit_config(docker_stack: Dict, devel_environ: Dict) -> Dict:
-    assert "simcore_rabbit" in docker_stack["services"]
-
-    config = {
-        "host": "127.0.0.1",
-        "port": get_service_published_port("rabbit", devel_environ["RABBIT_PORT"]),
-        "user": devel_environ["RABBIT_USER"],
-        "password": devel_environ["RABBIT_PASSWORD"],
-    }
-
-    # nodeports takes its configuration from env variables
-    os.environ["RABBIT_HOST"] = "127.0.0.1"    
-    os.environ["RABBIT_PORT"] = config["port"]    
-    os.environ["RABBIT_USER"] = devel_environ["RABBIT_USER"]
-    os.environ["RABBIT_PASSWORD"] = devel_environ["RABBIT_PASSWORD"]
-    os.environ["RABBIT_PROGRESS_CHANNEL"] = devel_environ["RABBIT_PROGRESS_CHANNEL"]
-
-    yield config
-
 
 @pytest.fixture(scope="function")
-async def rabbit_service(rabbit_config: Dict, docker_stack: Dict) -> str:
-    url = "amqp://{user}:{password}@{host}:{port}".format(**rabbit_config)
+async def rabbit_service(_webserver_dev_config: Dict, docker_stack):
+    cfg = deepcopy(_webserver_dev_config["rabbit"])
+    host = cfg["host"]
+    port = cfg["port"]
+    user = cfg["user"]
+    password = cfg["password"]
+    url = "amqp://{}:{}@{}:{}".format(user, password, host, port)
     await wait_till_rabbit_responsive(url)
-    yield url
 
 
-@tenacity.retry(**RabbitMQRetryPolicyUponInitialization().kwargs)
+@tenacity.retry(wait=tenacity.wait_fixed(0.1), stop=tenacity.stop_after_delay(60))
 async def wait_till_rabbit_responsive(url: str):
     await aio_pika.connect(url)
     return True
