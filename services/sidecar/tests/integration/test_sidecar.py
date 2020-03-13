a8309e046f43ecf4992a4da76c995c85929840b0
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-13 13:54:44 +0100

    celery service independent of webserver

--- services/sidecar/tests/integration/test_sidecar.py
+++ services/sidecar/tests/integration/test_sidecar.py
@@ -5,11 +5,11 @@ import pytest
 # Selection of core and tool services started in this swarm fixture (integration)
 core_services = ["storage", "postgres", "rabbit"]
 
-ops_services = [
-    "minio",
-    #    'adminer'
-]
+ops_services = ["minio", "adminer"]
 
 
-async def test_sleepers(loop):
-    pass
+
+async def test_run_sleepers(loop, docker_stack, postgres_session, sleeper_service, celery_service):
+    import pdb
+    pdb.set_trace()
+    # next_task_nodes = await SIDECAR.inspect(task, user_id, pipeline_id, node_id)
