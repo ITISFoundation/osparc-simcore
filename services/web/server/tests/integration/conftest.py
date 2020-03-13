ec4e7c63cc19511a1370394bf771292312e95d16
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-13 10:14:08 +0100

    moving fixtures to package pytest-fixtures

--- services/web/server/tests/integration/conftest.py
+++ services/web/server/tests/integration/conftest.py
@@ -28,19 +28,22 @@ from simcore_service_webserver.cli import create_environ
 from simcore_service_webserver.resources import resources as app_resources
 from utils_docker import get_service_published_port
 
+current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+
+# TODO: this should be done as a real pytest plugin instead
+sys.path.append(str(current_dir / '../../../../../packages'))
+print(sys.path)
 # imports the fixtures for the integration tests
 pytest_plugins = [
-    "fixtures.docker_compose",
-    "fixtures.docker_swarm",
-    "fixtures.docker_registry",
-    "fixtures.rabbit_service",
-    "fixtures.celery_service",
-    "fixtures.postgres_service",
-    "fixtures.redis_service",
-    "fixtures.websocket_client",
+    "pytest-fixtures.docker_compose",
+    "pytest-fixtures.docker_swarm",
+    "pytest-fixtures.docker_registry",
+    "pytest-fixtures.rabbit_service",
+    "pytest-fixtures.celery_service",
+    "pytest-fixtures.postgres_service",
+    "pytest-fixtures.redis_service",
+    "pytest-fixtures.websocket_client"
 ]
-
-current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
 log = logging.getLogger(__name__)
 
