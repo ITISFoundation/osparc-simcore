76118fefbd0693a820cd2c56f0953e4008bce9e5
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-16 10:48:53 +0100

    format + fixed type

--- services/sidecar/src/sidecar/config.py
+++ services/sidecar/src/sidecar/config.py
@@ -3,16 +3,20 @@ import multiprocessing
 import os
 
 
-SERVICES_MAX_NANO_CPUS = min(multiprocessing.cpu_count(), os.environ.get("SIDECAR_SERVICES_MAX_NANO_CPUS", 4 * pow(10, 9)))
-SERVICES_MAX_MEMORY_BYTES = os.environ.get("SIDECAR_SERVICES_MAX_MEMORY_BYTES", 2 * pow(1024, 3))
-SERVICES_TIMEOUT_SECONDS = os.environ.get("SIDECAR_SERVICES_TIMEOUT_SECONDS", 20*60)
-SWARM_STACK_NAME = os.environ["SWARM_STACK_NAME"]
+SERVICES_MAX_NANO_CPUS: int = min(
+    multiprocessing.cpu_count() * pow(10,9),
+    int(os.environ.get("SIDECAR_SERVICES_MAX_NANO_CPUS", 4 * pow(10, 9))),
+)
+SERVICES_MAX_MEMORY_BYTES: int = os.environ.get(
+    "SIDECAR_SERVICES_MAX_MEMORY_BYTES", 2 * pow(1024, 3)
+)
+SERVICES_TIMEOUT_SECONDS: int = os.environ.get("SIDECAR_SERVICES_TIMEOUT_SECONDS", 20 * 60)
+SWARM_STACK_NAME: str = os.environ["SWARM_STACK_NAME"]
 
-SIDECAR_LOGLEVEL = getattr(
-    logging,
-    os.environ.get("SIDECAR_LOGLEVEL", "WARNING").upper(),
-    logging.WARNING)
+SIDECAR_LOGLEVEL: str = getattr(
+    logging, os.environ.get("SIDECAR_LOGLEVEL", "WARNING").upper(), logging.WARNING
+)
 
 logging.basicConfig(level=SIDECAR_LOGLEVEL)
-logging.getLogger('sqlalchemy.engine').setLevel(SIDECAR_LOGLEVEL)
-logging.getLogger('sqlalchemy.pool').setLevel(SIDECAR_LOGLEVEL)
+logging.getLogger("sqlalchemy.engine").setLevel(SIDECAR_LOGLEVEL)
+logging.getLogger("sqlalchemy.pool").setLevel(SIDECAR_LOGLEVEL)
