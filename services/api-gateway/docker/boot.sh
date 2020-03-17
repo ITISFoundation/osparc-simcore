669d8e48ae223bfdf1b2e0da796f51f21affbdea
Pedro Crespo <crespo@itis.ethz.ch>
2020-03-17 20:38:42 +0100

    Creates a dockerfile in python:3.6.10-slim

--- services/api-gateway/docker/boot.sh
+++ services/api-gateway/docker/boot.sh
@@ -1,37 +1,33 @@
 #!/bin/sh
-#
-INFO="INFO: [`basename "$0"`] "
-ERROR="ERROR: [`basename "$0"`] "
+set -o errexit
+set -o nounset
+
+IFS=$(printf '\n\t')
+
+INFO="INFO: [$(basename "$0")] "
 
 # BOOTING application ---------------------------------------------
-echo $INFO "Booting in ${SC_BOOT_MODE} mode ..."
+echo "$INFO" "Booting in ${SC_BOOT_MODE} mode ..."
+echo "  User    :$(id "$(whoami)")"
+echo "  Workdir :$(pwd)"
 
-if [[ ${SC_BUILD_TARGET} == "development" ]]
+if [ "${SC_BUILD_TARGET}" = "development" ]
 then
-  echo "  User    :`id $(whoami)`"
-  echo "  Workdir :`pwd`"
-  echo "  Environment :"
+  echo "$INFO" "Environment :"
   printenv  | sed 's/=/: /' | sed 's/^/    /' | sort
-  #--------------------
-
-  APP_CONFIG=config-host-dev.yaml
-  $SC_PIP install --user -e services/api-gateway
-
-  #--------------------
-  echo "  Python :"
+  echo "$INFO" "Python :"
   python --version | sed 's/^/    /'
-  which python | sed 's/^/    /'
-  echo "  PIP :"
-  $SC_PIP list | sed 's/^/    /'
+  command -v python | sed 's/^/    /'
+  echo "$INFO" "PIP :"
+  pip list | sed 's/^/    /'
 fi
 
-
 # RUNNING application ----------------------------------------
-if [[ ${SC_BOOT_MODE} == "debug-ptvsd" ]]
+if [ "${SC_BOOT_MODE}" == "debug-ptvsd" ]
 then
   # NOTE: ptvsd is programmatically enabled inside of the service
   # this way we can have reload in place as well
-  exec uvicorn simcore_service_api_gateway.main:app --reload --host 0.0.0.0
+  exec uvicorn simcore_service_api_gateway.main:the_app --reload --host 0.0.0.0
 else
   exec simcore-service-api-gateway
 fi
--- services/api-gateway/docker/entrypoint.sh
+++ services/api-gateway/docker/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-#
-INFO="INFO: [`basename "$0"`] "
-ERROR="ERROR: [`basename "$0"`] "
+set -o errexit
+set -o nounset
+
+IFS=$(printf '\n\t')
+
+INFO="INFO: [$(basename "$0")] "
+WARNING="WARNING: [$(basename "$0")] "
+ERROR="ERROR: [$(basename "$0")] "
 
 # This entrypoint script:
 #
@@ -9,70 +14,72 @@ ERROR="ERROR: [`basename "$0"`] "
 # - Notice that the container *starts* as --user [default root] but
 #   *runs* as non-root user [scu]
 #
-echo $INFO "Entrypoint for stage ${SC_BUILD_TARGET} ..."
-echo "  User    :`id $(whoami)`"
-echo "  Workdir :`pwd`"
+echo "$INFO" "Entrypoint for stage ${SC_BUILD_TARGET} ..."
+echo   User    :"$(id "$(whoami)")"
+echo   Workdir :"$(pwd)"
+echo   scuUser :"$(id scu)"
 
 
-if [[ ${SC_BUILD_TARGET} == "development" ]]
+USERNAME=scu
+GROUPNAME=scu
+
+if [ "${SC_BUILD_TARGET}" = "development" ]
 then
+    echo "$INFO" "development mode detected..."
     # NOTE: expects docker run ... -v $(pwd):/devel/services/api-gateway
     DEVEL_MOUNT=/devel/services/api-gateway
 
-    stat $DEVEL_MOUNT &> /dev/null || \
-        (echo $ERROR ": You must mount '$DEVEL_MOUNT' to deduce user and group ids" && exit 1) # FIXME: exit does not stop script
+    stat $DEVEL_MOUNT > /dev/null 2>&1 || \
+        (echo "$ERROR" "You must mount '$DEVEL_MOUNT' to deduce user and group ids" && exit 1)
 
-    USERID=$(stat -c %u $DEVEL_MOUNT)
-    GROUPID=$(stat -c %g $DEVEL_MOUNT)
-    GROUPNAME=$(getent group ${GROUPID} | cut -d: -f1)
+    USERID=$(stat --format=%u $DEVEL_MOUNT)
+    GROUPID=$(stat --format=%g $DEVEL_MOUNT)
+    GROUPNAME=$(getent group "${GROUPID}" | cut --delimiter=: --fields=1)
 
-    if [[ $USERID -eq 0 ]]
+    if [ "$USERID" -eq 0 ]
     then
-        addgroup scu root
+        echo "$WARNING" Folder mounted owned by root user... adding "$SC_USER_NAME" to root...
+        adduser "${SC_USER_NAME}" root
     else
-        # take host's credentials in myu
-        if [[ -z "$GROUPNAME" ]]
+        # take host's credentials in scu
+        if [ -z "$GROUPNAME" ]
         then
-            GROUPNAME=myu
-            addgroup -g $GROUPID $GROUPNAME
+            echo "$INFO" mounted folder from "$USERID", creating new group my"${SC_USER_NAME}"
+            GROUPNAME=my"${SC_USER_NAME}"
+            addgroup --gid "$GROUPID" "$GROUPNAME"
+            # change group property of files already around
+            find / -path /proc -prune -group "$SC_USER_ID" -exec chgrp --no-dereference "$GROUPNAME" {} \;
         else
-            addgroup scu $GROUPNAME
+            echo "$INFO" "mounted folder from $USERID, adding ${SC_USER_NAME} to $GROUPNAME..."
+            adduser "$SC_USER_NAME" "$GROUPNAME"
         fi
 
-        deluser scu &> /dev/null
-        adduser -u $USERID -G $GROUPNAME -D -s /bin/sh scu
+        echo "$INFO changing $SC_USER_NAME $SC_USER_ID:$SC_USER_ID to $USERID:$GROUPID"
+        deluser "${SC_USER_NAME}" > /dev/null 2>&1
+        if [ "$SC_USER_NAME" = "$GROUPNAME" ]
+        then
+            addgroup --gid "$GROUPID" "$GROUPNAME"
+        fi
+        adduser --disabled-password --gecos "" --uid "$USERID" --gid "$GROUPID" --shell /bin/sh "$SC_USER_NAME" --no-create-home
+        # change user property of files already around
+        find / -path /proc -prune -user "$SC_USER_ID" -exec chown --no-dereference "$SC_USER_NAME" {} \;
     fi
+
+    echo "$INFO installing pythong dependencies..."
+    cd services/api-gateway || exit 1
+    pip install --no-cache-dir -r requirements/dev.txt
+    cd - || exit 1
 fi
 
-if [[ ${SC_BOOT_MODE} == "debug-ptvsd" ]]
+if [ ${SC_BOOT_MODE} == "debug-ptvsd" ]
 then
   # NOTE: production does NOT pre-installs ptvsd
-  python3 -m pip install ptvsd
+  pip install --no-cache-dir ptvsd
 fi
 
 
+echo "$INFO Starting $* ..."
+echo "  $SC_USER_NAME rights    : $(id "$SC_USER_NAME")"
+echo "  local dir : $(ls -al)"
 
-
-# Appends docker group if socket is mounted
-DOCKER_MOUNT=/var/run/docker.sock
-
-stat $DOCKER_MOUNT &> /dev/null
-if [[ $? -eq 0 ]]
-then
-    GROUPID=$(stat -c %g $DOCKER_MOUNT)
-
-    GROUPNAME=scdocker
-
-    addgroup -g $GROUPID $GROUPNAME &> /dev/null
-    if [[ $? -gt 0 ]]
-    then
-        # if group already exists in container, then reuse name
-        GROUPNAME=$(getent group ${GROUPID} | cut -d: -f1)
-    fi
-    addgroup scu $GROUPNAME
-fi
-
-
-
-echo $INFO "Starting boot ..."
-su-exec scu "$@"
+su --preserve-environment --command "export PATH=${PATH}; $*" "$SC_USER_NAME"
