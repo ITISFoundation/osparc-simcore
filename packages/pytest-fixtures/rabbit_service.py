9f16d2d9f030c4993f53c9367f9dca81caa85ee5
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-17 08:02:35 +0100

    added os environ for rabbit

--- packages/pytest-fixtures/rabbit_service.py
+++ packages/pytest-fixtures/rabbit_service.py
@@ -4,6 +4,7 @@
 # pylint:disable=unused-argument
 # pylint:disable=redefined-outer-name
 
+import os
 from copy import deepcopy
 from typing import Dict
 
@@ -25,7 +26,16 @@ def rabbit_config(docker_stack: Dict, devel_environ: Dict) -> Dict:
         "user": devel_environ["RABBIT_USER"],
         "password": devel_environ["RABBIT_PASSWORD"],
     }
+
+    # nodeports takes its configuration from env variables
+    os.environ["RABBIT_HOST"] = "127.0.0.1"    
+    os.environ["RABBIT_PORT"] = config["port"]    
+    os.environ["RABBIT_USER"] = devel_environ["RABBIT_USER"]
+    os.environ["RABBIT_PASSWORD"] = devel_environ["RABBIT_PASSWORD"]
+    os.environ["RABBIT_PROGRESS_CHANNEL"] = devel_environ["RABBIT_PROGRESS_CHANNEL"]
+
     yield config
+
 
 @pytest.fixture(scope="function")
 async def rabbit_service(rabbit_config: Dict, docker_stack: Dict) -> str:
