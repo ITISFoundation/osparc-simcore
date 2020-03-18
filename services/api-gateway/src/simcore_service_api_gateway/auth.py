98f57e306cd0c8a2ebae1167d88f6ce01de47044
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-03-19 00:26:33 +0100

    Is1268/public api gateway service (#1298)
    Implementing #1268: Work in front and backend.
    
    - backend
      - Minimal testable and buildable service
      - new api-gateway service
      - app using fastAPI; settings using pydantic
      - debian-based Dockerfile (thanks to @sanderegg )
      - includes Oath2 authentication with dummy user and three entrypoints for testing
      - builds and minimal tests pass but for the moment NOT added to the swarm
    - frontend
     - UI component to create Authentication Grant (i.e.. client key and secrets) for the client API
    - tools
      - scripts/openapi-generator-cli.bash: runs openapi-generator in a docker
       - services/api-gateway/tools: code generator of entrypoints
    
    Co-authored-by: Sylvain <35365065+sanderegg@users.noreply.github.com>

---
+++ services/api-gateway/src/simcore_service_api_gateway/auth.py
@@ -0,0 +1,90 @@
+import logging
+from typing import Optional
+
+from fastapi import Depends, HTTPException, Security, status
+from fastapi.security import OAuth2PasswordBearer, SecurityScopes
+
+from . import crud_users as crud
+from .__version__ import api_version_prefix
+from .auth_security import get_access_token_data
+from .schemas import TokenData, User, UserInDB
+
+log = logging.getLogger(__name__)
+
+
+# Resource SERVER ----------------------------------------------
+#
+#  +--------+                               +---------------+
+#  |        |--(A)- Authorization Request ->|   Resource    |
+#  |        |                               |     Owner     | Authorization request
+#  |        |<-(B)-- Authorization Grant ---|               |
+#  |        |                               +---------------+
+#  |        |
+#  |        |                               +---------------+
+#  |        |--(C)-- Authorization Grant -->| Authorization |
+#  | Client |                               |     Server    | Token request
+#  |        |<-(D)----- Access Token -------|               |
+#  |        |                               +---------------+
+#  |        |
+#  |        |                               +---------------+
+#  |        |--(E)----- Access Token ------>|    Resource   |
+#  |        |                               |     Server    |
+#  |        |<-(F)--- Protected Resource ---|               |
+#  +--------+                               +---------------+
+#
+#                  Figure 1: Abstract Protocol Flow
+
+
+# callable with request as argument -> extracts token from Authentication header
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl=f"{api_version_prefix}/token",
+    scopes={
+        "me": "Read information about the current user.",
+        "projects": "Read projects.",
+        "you": "Some other scope",
+    },
+)
+
+
+async def get_current_user(
+    security_scopes: SecurityScopes, access_token: str = Depends(oauth2_scheme)
+) -> User:
+    if security_scopes.scopes:
+        authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
+    else:
+        authenticate_value = f"Bearer"
+
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": authenticate_value},
+    )
+
+    # validates and decode jwt-based access token
+    token_data: Optional[TokenData] = get_access_token_data(access_token)
+    if token_data is None:
+        raise credentials_exception
+
+    # identify user
+    user: Optional[UserInDB] = crud.get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+
+    # validate scope
+    for scope in security_scopes.scopes:
+        if scope not in token_data.scopes:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Not enough permissions",
+                headers={"WWW-Authenticate": authenticate_value},
+            )
+    # auto-converst into User??
+    return user
+
+
+async def get_current_active_user(
+    current_user: User = Security(get_current_user, scopes=["me"])
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
