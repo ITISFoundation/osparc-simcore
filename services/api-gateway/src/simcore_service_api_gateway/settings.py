02d6ee60e86f60aa0f3165b7b7605b8c9ef3fa57
Pedro Crespo <crespo@itis.ethz.ch>
2020-03-18 05:44:47 +0100

    Fixes settings tests

--- services/api-gateway/src/simcore_service_api_gateway/settings.py
+++ services/api-gateway/src/simcore_service_api_gateway/settings.py
@@ -1,21 +1,55 @@
-from pydantic import BaseSettings, PostgresDsn, Field #pylint: disable=no-name-in-module
+# pylint: disable=no-name-in-module
+
+from pydantic import BaseSettings, Field, SecretStr, validator
+from enum import Enum
+from typing import Optional
+from yarl import URL
+import logging
+
+class BootModeEnum(str, Enum):
+    production = "production"
+    development = "development"
 
 
 class Settings(BaseSettings):
+    #pylint: disable=no-self-use
+    #pylint: disable=no-self-argument
+
     # DOCKER
-    boot_mode: bool = Field(..., env="SC_BOOT_MODE")
+    boot_mode: Optional[BootModeEnum] = Field(None, env="SC_BOOT_MODE")
 
     # LOGGING
-    loglevel: str
+    log_level_name: str = Field("DEBUG", env="loglevel")
+
+    @validator('loglevel_name')
+    def match_logging_level(cls, value) -> str:
+        try:
+            getattr(logging, value.upper())
+        except AttributeError:
+            raise ValueError(f"{value.upper()} is not a valid level")
+        return value.upper()
+
+    @property
+    def loglevel(self) -> int:
+        return getattr(logging, self.log_level_name)
 
     # POSTGRES
     postgres_user: str
-    postgres_password: str
+    postgres_password: SecretStr
     postgres_db: str
     postgres_host: str = "localhost"
     postgres_port: int = 5432
 
-    postgres_dsn: PostgresDsn  # TODO: compose with above
+    @property
+    def postgres_dsn(self) -> URL:
+        return URL.build(
+            scheme="postgresql",
+            user=self.postgres_user,
+            password=self.postgres_password.get_secret_value(),
+            host=self.postgres_host,
+            port=self.postgres_port,
+            path=f"/{self.postgres_db}"
+        )
 
     # WEBSERVER
     webserver_host: str = "webserver"
@@ -26,5 +60,5 @@ class Settings(BaseSettings):
     port: int = 8000
 
     class Config:
-        env_prefix=""
+        env_prefix = ""
         case_sensitive = False
--- services/api-gateway/tests/unit/test_settings.py
+++ services/api-gateway/tests/unit/test_settings.py
@@ -1,15 +1,18 @@
-from simcore_service_api_gateway.settings import Settings
+from simcore_service_api_gateway.settings import Settings, BootModeEnum, URL
 import pytest
 import logging
-
+from pprint import pprint
 
 def test_it(monkeypatch):
     monkeypatch.setenv("POSTGRES_USER", "test")
     monkeypatch.setenv("POSTGRES_PASSWORD", "test")
     monkeypatch.setenv("POSTGRES_DB", "test")
     monkeypatch.setenv("LOGLEVEL", "debug")
+    monkeypatch.setenv("SC_BOOT_MODE", "production")
 
     settings = Settings()
 
-    assert settings.postgres_dsn == "postgresql://test:test@localhost:5432/test"
+    pprint(settings.dict())
+    assert settings.boot_mode == BootModeEnum.production
+    assert settings.postgres_dsn == URL("postgresql://test:test@localhost:5432/test")
     assert settings.loglevel == logging.DEBUG
