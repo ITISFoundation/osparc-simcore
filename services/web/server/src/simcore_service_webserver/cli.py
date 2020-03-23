6dbb4a7dd6dc9a1c3bfa4c6bf390b16ab820fb8f
Pedro Crespo <crespo@itis.ethz.ch>
2020-03-23 15:37:21 +0100

    disables aiodebug

--- services/web/server/src/simcore_service_webserver/cli.py
+++ services/web/server/src/simcore_service_webserver/cli.py
@@ -19,7 +19,6 @@ from argparse import ArgumentParser
 from typing import Dict, List, Optional
 
 from aiohttp.log import access_logger
-from aiodebug import log_slow_callbacks
 
 from .application import run_service
 from .application_config import CLI_DEFAULT_CONFIGFILE, app_schema
@@ -109,15 +108,13 @@ def main(args: Optional[List] = None):
     access_logger.setLevel(log_level)
 
     # keep mostly quiet noisy loggers
-    quiet_level = max(min(log_level + LOG_LEVEL_STEP, logging.CRITICAL), logging.WARNING)
+    quiet_level = max(
+        min(log_level + LOG_LEVEL_STEP, logging.CRITICAL), logging.WARNING
+    )
     logging.getLogger("engineio").setLevel(quiet_level)
     logging.getLogger("openapi_spec_validator").setLevel(quiet_level)
     logging.getLogger("sqlalchemy").setLevel(quiet_level)
     logging.getLogger("sqlalchemy.engine").setLevel(quiet_level)
-
-    # NOTE: Every task blocking > AIODEBUG_SLOW_DURATION_SECS secs is considered slow and logged as warning
-    slow_duration = float(os.environ.get("AIODEBUG_SLOW_DURATION_SECS", 0.1))
-    log_slow_callbacks.enable(slow_duration)
 
     # run
     run_service(config)
