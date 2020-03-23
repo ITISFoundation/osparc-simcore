f67b6ff0f59f2fc2333a5f0672d089dac82e9f7d
Pedro Crespo <crespo@itis.ethz.ch>
2020-03-23 18:45:56 +0100

    Bit of formatting

--- services/web/server/docker/entrypoint.sh
+++ services/web/server/docker/entrypoint.sh
@@ -15,67 +15,60 @@ ERROR="ERROR: [$(basename "$0")] "
 #   *runs* as non-root user [scu]
 #
 echo "$INFO" "Entrypoint for stage ${SC_BUILD_TARGET} ..."
-echo   User    :"$(id "$(whoami)")"
-echo   Workdir :"$(pwd)"
-echo   scuUser :"$(id scu)"
-
+echo User :"$(id "$(whoami)")"
+echo Workdir :"$(pwd)"
+echo scuUser :"$(id scu)"
 
 GROUPNAME=scu
 
-if [ "${SC_BUILD_TARGET}" = "development" ]
-then
-    echo "$INFO" "development mode detected..."
-    # NOTE: expects docker run ... -v $(pwd):/devel/services/web/server
-    DEVEL_MOUNT=/devel/services/web/server
+if [ "${SC_BUILD_TARGET}" = "development" ]; then
+  echo "$INFO" "development mode detected..."
+  # NOTE: expects docker run ... -v $(pwd):/devel/services/web/server
+  DEVEL_MOUNT=/devel/services/web/server
 
-    stat $DEVEL_MOUNT > /dev/null 2>&1 || \
-        (echo "$ERROR" "You must mount '$DEVEL_MOUNT' to deduce user and group ids" && exit 1)
+  stat $DEVEL_MOUNT >/dev/null 2>&1 ||
+    (echo "$ERROR" "You must mount '$DEVEL_MOUNT' to deduce user and group ids" && exit 1)
 
-    USERID=$(stat --format=%u $DEVEL_MOUNT)
-    GROUPID=$(stat --format=%g $DEVEL_MOUNT)
-    GROUPNAME=$(getent group "${GROUPID}" | cut --delimiter=: --fields=1)
+  USERID=$(stat --format=%u $DEVEL_MOUNT)
+  GROUPID=$(stat --format=%g $DEVEL_MOUNT)
+  GROUPNAME=$(getent group "${GROUPID}" | cut --delimiter=: --fields=1)
 
-    if [ "$USERID" -eq 0 ]
-    then
-        echo "$WARNING" Folder mounted owned by root user... adding "$SC_USER_NAME" to root...
-        adduser "${SC_USER_NAME}" root
+  if [ "$USERID" -eq 0 ]; then
+    echo "$WARNING" Folder mounted owned by root user... adding "$SC_USER_NAME" to root...
+    adduser "${SC_USER_NAME}" root
+  else
+    # take host's credentials in scu
+    if [ -z "$GROUPNAME" ]; then
+      echo "$INFO" mounted folder from "$USERID", creating new group my"${SC_USER_NAME}"
+      GROUPNAME=my"${SC_USER_NAME}"
+      addgroup --gid "$GROUPID" "$GROUPNAME"
+      # change group property of files already around
+      find / -path /proc -prune -group "$SC_USER_ID" -exec chgrp --no-dereference "$GROUPNAME" {} \;
     else
-        # take host's credentials in scu
-        if [ -z "$GROUPNAME" ]
-        then
-            echo "$INFO" mounted folder from "$USERID", creating new group my"${SC_USER_NAME}"
-            GROUPNAME=my"${SC_USER_NAME}"
-            addgroup --gid "$GROUPID" "$GROUPNAME"
-            # change group property of files already around
-            find / -path /proc -prune -group "$SC_USER_ID" -exec chgrp --no-dereference "$GROUPNAME" {} \;
-        else
-            echo "$INFO" "mounted folder from $USERID, adding ${SC_USER_NAME} to $GROUPNAME..."
-            adduser "$SC_USER_NAME" "$GROUPNAME"
-        fi
-
-        echo "$INFO changing $SC_USER_NAME $SC_USER_ID:$SC_USER_ID to $USERID:$GROUPID"
-        deluser "${SC_USER_NAME}" > /dev/null 2>&1
-        if [ "$SC_USER_NAME" = "$GROUPNAME" ]
-        then
-            addgroup --gid "$GROUPID" "$GROUPNAME"
-        fi
-        adduser --disabled-password --gecos "" --uid "$USERID" --gid "$GROUPID" --shell /bin/sh "$SC_USER_NAME" --no-create-home
-        # change user property of files already around
-        find / -path /proc -prune -user "$SC_USER_ID" -exec chown --no-dereference "$SC_USER_NAME" {} \;
+      echo "$INFO" "mounted folder from $USERID, adding ${SC_USER_NAME} to $GROUPNAME..."
+      adduser "$SC_USER_NAME" "$GROUPNAME"
     fi
 
-    echo "$INFO installing pythong dependencies..."
-    cd services/web/server || exit 1
-    pip install --no-cache-dir -r requirements/dev.txt
-    cd - || exit 1
+    echo "$INFO changing $SC_USER_NAME $SC_USER_ID:$SC_USER_ID to $USERID:$GROUPID"
+    deluser "${SC_USER_NAME}" >/dev/null 2>&1
+    if [ "$SC_USER_NAME" = "$GROUPNAME" ]; then
+      addgroup --gid "$GROUPID" "$GROUPNAME"
+    fi
+    adduser --disabled-password --gecos "" --uid "$USERID" --gid "$GROUPID" --shell /bin/sh "$SC_USER_NAME" --no-create-home
+    # change user property of files already around
+    find / -path /proc -prune -user "$SC_USER_ID" -exec chown --no-dereference "$SC_USER_NAME" {} \;
+  fi
+
+  echo "$INFO installing pythong dependencies..."
+  cd services/web/server || exit 1
+  pip install --no-cache-dir -r requirements/dev.txt
+  cd - || exit 1
 fi
 
-if [ "${SC_BOOT_MODE}" = "debug-ptvsd" ]
-then
+if [ "${SC_BOOT_MODE}" = "debug-ptvsd" ]; then
   # NOTE: production does NOT pre-installs ptvsd
   pip install --no-cache-dir ptvsd
 fi
-
 
 echo "$INFO Starting $* ..."
 echo "  $SC_USER_NAME rights    : $(id "$SC_USER_NAME")"
