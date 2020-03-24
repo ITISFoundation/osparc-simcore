67bca878dd5a1a39f87da984ed80dbf1ad9c0a82
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-24 09:16:56 +0100

    cleanup

--- packages/simcore-sdk/src/simcore_sdk/node_ports/_schema_items_list.py
+++ packages/simcore-sdk/src/simcore_sdk/node_ports/_schema_items_list.py
@@ -1,5 +1,5 @@
 import logging
-from collections import Mapping
+from collections.abc import Mapping
 from typing import Dict
 
 from . import exceptions
@@ -7,14 +7,15 @@ from ._schema_item import SchemaItem
 
 log = logging.getLogger(__name__)
 
+
 class SchemaItemsList(Mapping):
-    def __init__(self, data:Dict[str, SchemaItem]=None):
+    def __init__(self, data: Dict[str, SchemaItem] = None):
         log.debug("creating SchemaItemsList with %s", data)
         if not data:
             data = {}
         self._store = data
 
-    def __getitem__(self, key)->SchemaItem:
+    def __getitem__(self, key) -> SchemaItem:
         if isinstance(key, int):
             if key < len(self._store):
                 key = list(self._store.keys())[key]
