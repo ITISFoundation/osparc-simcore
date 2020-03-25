76172b719d4d2b49b8f11c1ebbf16b9adc81d099
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-25 15:17:49 +0100

    migrate director to debian docker

--- services/director/docker/entrypoint.sh
+++ services/director/docker/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-#
+set -o errexit
+set -o nounset
+
+IFS=$(printf '\n\t')
+
 INFO="INFO: [$(basename "$0")] "
+WARNING="WARNING: [$(basename "$0")] "
 ERROR="ERROR: [$(basename "$0")] "
 
 # This entrypoint script:
@@ -12,7 +17,11 @@ ERROR="ERROR: [$(basename "$0")] "
 echo "$INFO" "Entrypoint for stage ${SC_BUILD_TARGET} ..."
 echo "$INFO" "User    :$(id "$(whoami)")"
 echo "$INFO" "Workdir :$(pwd)"
+echo   scuUser :"$(id scu)"
 
+
+USERNAME=scu
+GROUPNAME=scu
 
 if [ "${SC_BUILD_TARGET}" = "development" ]
 then
@@ -22,26 +31,43 @@ then
     stat $DEVEL_MOUNT > /dev/null 2>&1 || \
         (echo "$ERROR" "You must mount '$DEVEL_MOUNT' to deduce user and group ids" && exit 1) # FIXME: exit does not stop script
 
-    USERID=$(stat -c %u $DEVEL_MOUNT)
-    GROUPID=$(stat -c %g $DEVEL_MOUNT)
-    GROUPNAME=$(getent group "${GROUPID}" | cut -d: -f1)
+    USERID=$(stat --format=%u $DEVEL_MOUNT)
+    GROUPID=$(stat --format=%g $DEVEL_MOUNT)
+    GROUPNAME=$(getent group "${GROUPID}" | cut --delimiter=: --fields=1)
 
     if [ "$USERID" -eq 0 ]
     then
-        addgroup scu root
+        echo "$WARNING" Folder mounted owned by root user... adding "$SC_USER_NAME" to root...
+        adduser "${SC_USER_NAME}" root
     else
-        # take host's credentials
+        # take host's credentials in scu
         if [ -z "$GROUPNAME" ]
         then
-            GROUPNAME=host_group
-            addgroup -g "$GROUPID" $GROUPNAME
+            echo "$INFO" mounted folder from "$USERID", creating new group my"${SC_USER_NAME}"
+            GROUPNAME=my"${SC_USER_NAME}"
+            addgroup --gid "$GROUPID" "$GROUPNAME"
+            # change group property of files already around
+            find / -path /proc -prune -group "$SC_USER_ID" -exec chgrp --no-dereference "$GROUPNAME" {} \;
         else
-            addgroup scu $GROUPNAME
+            echo "$INFO" "mounted folder from $USERID, adding ${SC_USER_NAME} to $GROUPNAME..."
+            adduser "$SC_USER_NAME" "$GROUPNAME"
         fi
 
-        deluser scu > /dev/null 2>&1
-        adduser -u "$USERID" -G $GROUPNAME -D -s /bin/sh scu
+        echo "$INFO changing $SC_USER_NAME $SC_USER_ID:$SC_USER_ID to $USERID:$GROUPID"
+        deluser "${SC_USER_NAME}" > /dev/null 2>&1
+        if [ "$SC_USER_NAME" = "$GROUPNAME" ]
+        then
+            addgroup --gid "$GROUPID" "$GROUPNAME"
+        fi
+        adduser --disabled-password --gecos "" --uid "$USERID" --gid "$GROUPID" --shell /bin/sh "$SC_USER_NAME" --no-create-home
+        # change user property of files already around
+        find / -path /proc -prune -user "$SC_USER_ID" -exec chown --no-dereference "$SC_USER_NAME" {} \;
     fi
+
+    echo "$INFO installing python dependencies..."
+    cd services/director || exit 1
+    pip install --no-cache-dir -r requirements/dev.txt
+    cd - || exit 1
 fi
 
 
@@ -53,20 +79,27 @@ fi
 
 # Appends docker group if socket is mounted
 DOCKER_MOUNT=/var/run/docker.sock
-
-
 if stat $DOCKER_MOUNT > /dev/null 2>&1
 then
-    GROUPID=$(stat -c %g $DOCKER_MOUNT)
-    GROUPNAME=docker
+    echo "$INFO detected docker socket is mounted, adding user to group..."
+    GROUPID=$(stat --format=%g $DOCKER_MOUNT)
+    GROUPNAME=scdocker
 
-
-    if ! addgroup -g "$GROUPID" $GROUPNAME > /dev/null 2>&1
+    if ! addgroup --gid "$GROUPID" $GROUPNAME > /dev/null 2>&1
     then
+        echo "$WARNING docker group with $GROUPID already exists, getting group name..."
         # if group already exists in container, then reuse name
-        GROUPNAME=$(getent group "${GROUPID}" | cut -d: -f1)
+        GROUPNAME=$(getent group "${GROUPID}" | cut --delimiters=: --fields=1)
+        echo "$WARNING docker group with $GROUPID has name $GROUPNAME"
     fi
-    addgroup scu "$GROUPNAME"
+    adduser "$SC_USER_NAME" "$GROUPNAME"
 fi
 
-exec su-exec scu "$@"
+echo "$INFO Starting $* ..."
+echo "  $SC_USER_NAME rights    : $(id "$SC_USER_NAME")"
+echo "  local dir : $(ls -al)"
+echo "  input dir : $(ls -al "${SIDECAR_INPUT_FOLDER}")"
+echo "  output dir : $(ls -al "${SIDECAR_OUTPUT_FOLDER}")"
+echo "  log dir : $(ls -al "${SIDECAR_LOG_FOLDER}")"
+
+su --preserve-environment --command "export PATH=${PATH}; $*" "$SC_USER_NAME"
