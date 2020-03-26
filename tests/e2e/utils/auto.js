d2b3e6bc6b69c6a5d26f092662f7bc3932796541
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2020-03-26 10:14:24 +0100

    Fix e2e (#1407)
    * Study delete adapted to new study browser
    * there might be no json body in the response
    * new users also remove studies

--- tests/e2e/utils/auto.js
+++ tests/e2e/utils/auto.js
@@ -256,6 +256,7 @@ async function runStudy(page, waitFor = 0) {
     await responsesQueue.waitUntilResponse("/start");
   }
   catch(err) {
+    console.error(err);
     throw(err);
   }
 
@@ -269,11 +270,11 @@ async function dashboardDeleteFirstStudy(page) {
   await page.waitForSelector('[osparc-test-id="studiesTabBtn"]')
   await page.click('[osparc-test-id="studiesTabBtn"]')
 
-  await page.waitForSelector('[osparc-test-id="userStudiesList"] > .qx-pb-listitem:nth-child(1)')
-  await page.click('[osparc-test-id="userStudiesList"] > .qx-pb-listitem:nth-child(1)')
+  await page.waitForSelector('[osparc-test-id="userStudiesList"] > .qx-pb-listitem:nth-child(1) > [osparc-test-id="studyItemMenuButton"]')
+  await page.click('[osparc-test-id="userStudiesList"] > .qx-pb-listitem:nth-child(1) > [osparc-test-id="studyItemMenuButton"]')
 
-  await page.waitForSelector('[osparc-test-id="deleteStudiesBtn"]')
-  await page.click('[osparc-test-id="deleteStudiesBtn"]')
+  await page.waitForSelector('[osparc-test-id="studyItemMenuDelete"]')
+  await page.click('[osparc-test-id="studyItemMenuDelete"]')
 
   await page.waitForSelector('[osparc-test-id="confirmDeleteStudyBtn"]')
   await page.click('[osparc-test-id="confirmDeleteStudyBtn"]')
@@ -357,6 +358,7 @@ async function downloadSelectedFile(page) {
     console.log("valid output file value", value)
   }
   catch(err) {
+    console.error(err);
     throw(err);
   }
 }
