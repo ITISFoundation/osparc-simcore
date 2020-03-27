52675d83d0bd1da2da1aaa4c17c691d564b70fdc
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-27 09:49:30 +0100

    fix path

--- ci/github/system-testing/swarm-deploy.bash
+++ ci/github/system-testing/swarm-deploy.bash
@@ -15,7 +15,7 @@ export DOCKER_IMAGE_TAG
 
 install() {
     bash ci/helpers/ensure_python_pip.bash
-    pip3 install -r tests/swarm-deploy/requirements/ci.txt
+    pushd tests/swarm-deploy; pip3 install -r requirements/ci.txt; popd
     make pull-version || ( (make pull-cache || true) && make build-x tag-version)
     make .env
     echo WEBSERVER_DB_INITTABLES=1 >> .env
