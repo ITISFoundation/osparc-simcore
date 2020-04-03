3528641ccd1b9290e012271dc876dac7b2a0be0b
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-04-03 08:25:45 +0200

    Fixes failing test

--- services/web/server/src/simcore_service_webserver/rest.py
+++ services/web/server/src/simcore_service_webserver/rest.py
@@ -81,11 +81,13 @@ def setup(app: web.Application, *, swagger_doc_enabled: bool = True):
     app.router.add_routes(routes)
 
     # middlewares
+    # NOTE: using safe get here since some tests use incomplete configs
+    is_diagnostics_enabled = app[APP_CONFIG_KEY].get("diagnostics",{}).get("enabled",{})
     app.middlewares.extend(
         [
             error_middleware_factory(
                 api_version_prefix,
-                log_exceptions=not app[APP_CONFIG_KEY]["diagnostics"]["enabled"],
+                log_exceptions=not is_diagnostics_enabled,
             ),
             envelope_middleware_factory(api_version_prefix),
         ]
