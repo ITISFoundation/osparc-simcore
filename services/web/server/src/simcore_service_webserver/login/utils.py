9ea9c6515e57673e262185cfedcc3d0ee943e38f
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2020-04-07 16:01:51 +0200

    Working email (sketchy)

--- services/web/server/src/simcore_service_webserver/login/utils.py
+++ services/web/server/src/simcore_service_webserver/login/utils.py
@@ -1,4 +1,7 @@
 import random
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from logging import getLogger
 from os.path import join
@@ -11,7 +14,6 @@ import passlib.hash
 from aiohttp import web
 from aiohttp_jinja2 import render_string
 from passlib import pwd
-
 from servicelib.rest_models import LogMessageType
 
 from ..resources import resources
@@ -83,11 +85,14 @@ async def send_mail(recipient: str, subject: str, body: str) -> None:
 
 
 async def render_and_send_mail(
-    request: web.Request, to: str, template: str, context: Mapping
+    request: web.Request, to: str, template: str, context: Mapping, attachment: bytearray
 ):
     page = render_string(str(template), request, context)
     subject, body = page.split("\n", 1)
-    await send_mail(to, subject.strip(), body)
+    if attachment:
+        await send_multipart_mail(to, subject.strip(), body, attachment)
+    else:
+        await send_mail(to, subject.strip(), body)
 
 
 def themed(template):
@@ -103,3 +108,46 @@ def flash_response(msg: str, level: str = "INFO") -> web.Response:
         data={"data": attr.asdict(LogMessageType(msg, level)), "error": None}
     )
     return response
+
+async def send_multipart_mail(recipient: str, subject: str, body: str, attachment: bytearray) -> None:
+    smtp_args = dict(
+        loop=cfg.APP.loop,
+        hostname=cfg.SMTP_HOST,
+        port=cfg.SMTP_PORT,
+        use_tls=bool(cfg.SMTP_TLS_ENABLED),
+    )
+    log.debug("Sending email with smtp configuration: %s", pformat(smtp_args))
+
+    msg = MIMEMultipart('alternative')
+    msg["Subject"] = subject
+    msg["From"] = cfg.SMTP_SENDER
+    msg["To"] = recipient
+
+    part1 = MIMEText(body, 'html')
+    part2 = MIMEBase('application', 'zip')
+    part2.set_payload(attachment)
+    part2.add_header('Content-Disposition', 'attachment; filename="README.zip"')
+    encoders.encode_base64(part2)
+    msg.attach(part1)
+    msg.attach(part2)
+
+    if cfg.SMTP_PORT == 587:
+        # NOTE: aiosmtplib does not handle port 587 correctly
+        # plaintext first, then use starttls
+        # this is a workaround
+        smtp = aiosmtplib.SMTP(**smtp_args)
+        await smtp.connect(use_tls=False, port=cfg.SMTP_PORT)
+        if cfg.SMTP_TLS_ENABLED:
+            log.info("Starting TLS ...")
+            await smtp.starttls(validate_certs=False)
+        if cfg.SMTP_USERNAME:
+            log.info("Login email server ...")
+            await smtp.login(cfg.SMTP_USERNAME, cfg.SMTP_PASSWORD)
+        await smtp.send_message(msg)
+        await smtp.quit()
+    else:
+        async with aiosmtplib.SMTP(**smtp_args) as smtp:
+            if cfg.SMTP_USERNAME:
+                log.info("Login email server ...")
+                await smtp.login(cfg.SMTP_USERNAME, cfg.SMTP_PASSWORD)
+            await smtp.send_message(msg)
--- services/web/server/src/simcore_service_webserver/publication_handlers.py
+++ services/web/server/src/simcore_service_webserver/publication_handlers.py
@@ -1,6 +1,6 @@
 from aiohttp import MultipartReader, hdrs, web
 
-from .login.decorators import login_required
+from .login.decorators import RQT_USERID_KEY, login_required
 
 
 @login_required
@@ -21,4 +21,20 @@ async def service_submission(request: web.Request):
         raise web.HTTPUnsupportedMediaType(reason=f'One part had an unexpected type: {part.headers[hdrs.CONTENT_TYPE]}')
     # data (dict) and file (bytearray) have the necessary information to compose the email
     print(data, filedata)
+    user_id = request.get(RQT_USERID_KEY, -1)
+    try:
+        from .login.utils import (render_and_send_mail, common_themed)
+        await render_and_send_mail(
+            request,
+            'support@osparc.io',
+            common_themed('service_submission.html'),
+            {
+                'user': user_id,
+                'json': data
+            },
+            filedata
+        )
+    except Exception:
+        raise web.HTTPServiceUnavailable()
+
     return True
