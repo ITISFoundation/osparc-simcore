83369368a0047d73dad363981b26995ac3288e34
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-04-15 13:40:55 +0200

    @pcrespov verbosify

--- scripts/hadolint.bash
+++ scripts/hadolint.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 # dockerfile linter tool
 # - https://github.com/hadolint/hadolint
-exec docker run --rm -i hadolint/hadolint < "$@"
+exec docker run --rm --interactive hadolint/hadolint < "$@"
