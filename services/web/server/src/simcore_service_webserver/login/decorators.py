be26d97754b3ccaece7a958d6d99b8f4736a44c1
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2020-04-27 16:00:24 +0200

    Removed unused decorator

--- services/web/server/src/simcore_service_webserver/login/decorators.py
+++ services/web/server/src/simcore_service_webserver/login/decorators.py
@@ -1,24 +1,8 @@
-import asyncio
 from functools import wraps
 
 from aiohttp_security.api import check_authorized
 from servicelib.request_keys import RQT_USERID_KEY
 from servicelib.requests_utils import get_request
-
-
-@asyncio.coroutine
-def user_to_request(handler):
-    """ Handler decorator that injects in request, current authorized user ID
-
-    """
-
-    @wraps(handler)
-    async def wrapped(*args, **kwargs):
-        request = get_request(*args, **kwargs)
-        request[RQT_USERID_KEY] = await check_authorized(request)
-        return await handler(*args)
-
-    return wrapped
 
 
 def login_required(handler):
