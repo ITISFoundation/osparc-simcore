928ff3406992ac20a1bf754c52e99a9770035de6
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-05-01 11:39:22 +0200

    added group type added constaint on everyone group to be unique

--- packages/postgres-database/src/simcore_postgres_database/models/groups.py
+++ packages/postgres-database/src/simcore_postgres_database/models/groups.py
@@ -3,9 +3,26 @@
     - List of groups in the framework
     - Groups have a ID, name and a list of users that belong to the group
 """
+
+import enum
+
 import sqlalchemy as sa
 from sqlalchemy.sql import func
+
 from .base import metadata
+
+
+class GroupType(enum.Enum):
+    """
+        standard: standard group
+        primary: primary group
+        everyone: the only group for all users
+    """
+
+    STANDARD = "standard"
+    PRIMARY = "primary"
+    EVERYONE = "everyone"
+
 
 # NOTE: using func.now() insted of python datetime ensure the time is computed server side
 groups = sa.Table(
@@ -14,6 +31,7 @@ groups = sa.Table(
     sa.Column("gid", sa.BigInteger, nullable=False, primary_key=True),
     sa.Column("name", sa.String, nullable=False),
     sa.Column("description", sa.String, nullable=False),
+    sa.Column("type", sa.Enum(GroupType), nullable=False, server_default="STANDARD"),
     sa.Column("created", sa.DateTime(), nullable=False, server_default=func.now()),
     sa.Column(
         "modified",
@@ -22,6 +40,7 @@ groups = sa.Table(
         server_default=func.now(),
         onupdate=func.now(),  # this will auto-update on modification
     ),
+    sa.CheckConstraint("check_group_uniqueness(name, type) = 0"),
 )
 
 user_to_groups = sa.Table(
@@ -57,3 +76,18 @@ user_to_groups = sa.Table(
     ),
     sa.UniqueConstraint("uid", "gid"),
 )
+
+# ---------------- PROCEDURES
+set_check_uniqueness_procedure = sa.DDL(
+    """
+CREATE OR REPLACE FUNCTION check_group_uniqueness(name text, type text) RETURNS INT AS $$
+BEGIN
+    IF type = 'EVERYONE' AND (SELECT COUNT(*) FROM groups WHERE groups.type = 'EVERYONE') = 1 THEN
+        RETURN 127;
+    END IF;
+    RETURN 0;
+END; $$ LANGUAGE 'plpgsql';
+"""
+)
+
+sa.event.listen(groups, "before_create", set_check_uniqueness_procedure)
