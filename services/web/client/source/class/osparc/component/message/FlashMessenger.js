6d27d9c91e20fdcd298dfb1351bbf67916646355
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2020-05-05 13:36:16 +0200

    Flash messenger container now has a higher zIndex

--- services/web/client/source/class/osparc/component/message/FlashMessenger.js
+++ services/web/client/source/class/osparc/component/message/FlashMessenger.js
@@ -40,7 +40,9 @@ qx.Class.define("osparc.component.message.FlashMessenger", {
     this.base(arguments);
     this.__messages = new qx.data.Array();
 
-    this.__messageContainer = new qx.ui.container.Composite(new qx.ui.layout.VBox(10));
+    this.__messageContainer = new qx.ui.container.Composite(new qx.ui.layout.VBox(10)).set({
+      zIndex: 110000
+    });
     const root = qx.core.Init.getApplication().getRoot();
     root.add(this.__messageContainer, {
       top: 10
