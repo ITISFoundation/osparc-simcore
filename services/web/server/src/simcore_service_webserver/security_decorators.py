32c3d80da88e778d92ff159a022c6217daada58d
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-06-10 18:48:27 +0200

    @crespov review: what exception

--- services/web/server/src/simcore_service_webserver/security_decorators.py
+++ services/web/server/src/simcore_service_webserver/security_decorators.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from typing import Coroutine
+from typing import Callable
 
 from aiohttp import web
 
@@ -11,9 +11,12 @@ def permission_required(permissions: str):
         The function will throw an exception in case of disallowance.
 
     :param handler: the function to check syntax must have web.Request as parameter
+    If user is not authorized - raises HTTPUnauthorized,
+    if user is authorized and does not have permission -
+    raises HTTPForbidden.
     """
 
-    def decorator(handler: Coroutine):
+    def decorator(handler: Callable):
         @wraps(handler)
         async def wrapped(request: web.Request):
             await check_permission(request, permissions)
