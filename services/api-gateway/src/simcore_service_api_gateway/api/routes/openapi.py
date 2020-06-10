dca902d57ed663dec9e17e10e8fe62ca0558444d
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-06-10 19:49:59 +0200

    disables tmp the studies

--- services/api-gateway/src/simcore_service_api_gateway/api/routes/openapi.py
+++ services/api-gateway/src/simcore_service_api_gateway/api/routes/openapi.py
@@ -9,4 +9,6 @@ router.include_router(meta.router, tags=["meta"], prefix="/meta")
 # router.include_router(authentication.router, tags=["authentication"], prefix="/users")
 
 router.include_router(users.router, tags=["users"], prefix="/me")
-router.include_router(studies.router, tags=["studies"], prefix="/studies")
+
+## TODO: disables studies for the moment
+# router.include_router(studies.router, tags=["studies"], prefix="/studies")
