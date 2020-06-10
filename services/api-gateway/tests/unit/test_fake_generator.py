c2ae823da1368c5ba470974718c120de1aec15f1
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-06-10 21:25:13 +0200

    WIP

---
+++ services/api-gateway/tests/unit/test_fake_generator.py
@@ -0,0 +1,13 @@
+
+# TODO:
+
+def create_fake(model: BaseModel):
+    from faker import Faker
+    fake = Faker()
+
+
+    for field in model.__fie
+    # navigate fields
+    # if example cls.Config.schema_extra.get("example")
+    # type
+    # default or some value in fake
