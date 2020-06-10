113a118f13552ddf14d371a085eccd6a3a5a5c01
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-06-10 16:10:00 +0200

    Schema for meta model

--- services/api-gateway/src/simcore_service_api_gateway/api/routes/meta.py
+++ services/api-gateway/src/simcore_service_api_gateway/api/routes/meta.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter
 
 from ...__version__ import __version__, api_version, api_vtag
+from ...models.schemas.meta import Meta
 
 router = APIRouter()
 
 
-@router.get("")
+@router.get("", response_model=Meta)
 async def get_service_metadata():
-    return {
-        "name": __name__.split(".")[0],
-        "version": api_version,
+    return Meta(
+        name=__name__.split(".")[0],
+        version=api_version,
         # TODO: a way to get first part of the url?? "version_prefix": f"/{api_vtag}",
         # TODO: sync this info
-        "released": {api_vtag: api_version},
-    }
+        released={api_vtag: api_version},
+    )
---
+++ services/api-gateway/src/simcore_service_api_gateway/models/schemas/meta.py
@@ -0,0 +1,26 @@
+from typing import Dict, Optional
+
+from pydantic import BaseModel, Field, constr
+
+# TODO: review this RE
+# use https://www.python.org/dev/peps/pep-0440/#version-scheme
+# or https://www.python.org/dev/peps/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions
+#
+VERSION_RE = r"^(0|[1-9]\d*)(\.(0|[1-9]\d*)){2}(-(0|[1-9]\d*|\d*[-a-zA-Z][-\da-zA-Z]*)(\.(0|[1-9]\d*|\d*[-a-zA-Z][-\da-zA-Z]*))*)?(\+[-\da-zA-Z]+(\.[-\da-zA-Z-]+)*)?$"
+
+
+class Meta(BaseModel):
+    name: str
+    version: constr(regex=VERSION_RE)
+    released: Optional[Dict[str, str]] = Field(
+        None, description="Maps every route's path tag with a released version"
+    )
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "simcore_service_foo",
+                "version": "2.4.45",
+                "released": {"v1": "1.3.4", "v2": "2.4.45"},
+            }
+        }
