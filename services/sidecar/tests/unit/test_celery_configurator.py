e2ccb4a8e189630aa5be24de8e0fa3a203cd7a49
Andrei Neagu <neagu@itis.swiss>
2020-06-11 16:19:13 +0200

    refactored tests - names are more readable - removed some typos

--- services/sidecar/tests/unit/test_celery_configurator.py
+++ services/sidecar/tests/unit/test_celery_configurator.py
@@ -11,7 +11,7 @@ from celery import Celery
 from simcore_sdk.config.rabbit import Config as RabbitConfig
 
 
-def _toggle_gpy_mock(mocker, has_gpu: bool) -> None:
+def _toggle_gpu_mock(mocker, has_gpu: bool) -> None:
     # mock ouput of cat /proc/self/cgroup
     CAT_DATA = b"""
     12:hugetlb:/docker/2c52ab5a825dea0b074741fb1521c972866af7997a761eb312405b50ad289263
@@ -65,21 +65,22 @@ def _toggle_gpy_mock(mocker, has_gpu: bool) -> None:
 
 
 @pytest.fixture()
-def mock_no_gpu(mocker) -> None:
-    _toggle_gpy_mock(mocker, False)
+def mock_node_no_gpu(mocker) -> None:
+    _toggle_gpu_mock(mocker, False)
 
 
 @pytest.fixture()
-def mock_with_gpu(mocker) -> None:
-    _toggle_gpy_mock(mocker, True)
+def mock_node_with_gpu(mocker) -> None:
+    _toggle_gpu_mock(mocker, True)
 
 
 @pytest.fixture(params=[True, False])
-def mock_gpu_both_modes(request, mocker):
-    return _toggle_gpy_mock(mocker, request.param)
+def mock_node_has_gpu(request, mocker) -> None:
+    _toggle_gpu_mock(mocker, request.param)
 
 
-def test_force_start_cpu_mode(mocker, mock_no_gpu, monkeypatch):
+@pytest.mark.parametrize("gpu_support", [(pytest.lazy_fixture("mock_node_has_gpu")),])
+def test_force_start_cpu_mode(mocker, monkeypatch, gpu_support) -> None:
     monkeypatch.setenv("START_AS_MODE_CPU", "1")
 
     mocked_configure_cpu_mode = mocker.patch(
@@ -93,7 +94,8 @@ def test_force_start_cpu_mode(mocker, mock_no_gpu, monkeypatch):
     mocked_configure_cpu_mode.assert_called()
 
 
-def test_force_start_gpu_mode(mocker, mock_no_gpu, monkeypatch):
+@pytest.mark.parametrize("gpu_support", [(pytest.lazy_fixture("mock_node_has_gpu")),])
+def test_force_start_gpu_mode(mocker, monkeypatch, gpu_support) -> None:
     monkeypatch.setenv("START_AS_MODE_GPU", "1")
 
     mocked_configure_gpu_mode = mocker.patch(
@@ -106,7 +108,7 @@ def test_force_start_gpu_mode(mocker, mock_no_gpu, monkeypatch):
     mocked_configure_gpu_mode.assert_called()
 
 
-def test_auto_detects_gpu(mocker, mock_with_gpu, monkeypatch):
+def test_auto_detects_gpu(mocker, mock_node_with_gpu) -> None:
     mocked_configure_gpu_mode = mocker.patch(
         "simcore_service_sidecar.celery_configurator.configure_gpu_mode"
     )
@@ -120,16 +122,16 @@ def test_auto_detects_gpu(mocker, mock_with_gpu, monkeypatch):
 @pytest.mark.parametrize(
     "gpu_support,expected_value",
     [
-        (pytest.lazy_fixture("mock_no_gpu"), False),
-        (pytest.lazy_fixture("mock_with_gpu"), True),
+        (pytest.lazy_fixture("mock_node_no_gpu"), False),
+        (pytest.lazy_fixture("mock_node_with_gpu"), True),
     ],
 )
-def test_proper_has_gpu_mocking(expected_value, gpu_support):
+def test_proper_has_gpu_mocking(expected_value, gpu_support) -> None:
     assert is_gpu_node() is expected_value
 
 
-@pytest.mark.parametrize("gpu_support", [(pytest.lazy_fixture("mock_gpu_both_modes")),])
-def test_force_start_cpu_mode_no_mocks(monkeypatch, gpu_support):
+@pytest.mark.parametrize("gpu_support", [(pytest.lazy_fixture("mock_node_has_gpu")),])
+def test_force_start_cpu_ext_dep_mocking(monkeypatch, gpu_support) -> None:
     monkeypatch.setenv("START_AS_MODE_CPU", "1")
 
     rabbit_cfg, celery_app = get_rabbitmq_config_and_celery_app()
@@ -137,8 +139,8 @@ def test_force_start_cpu_mode_no_mocks(monkeypatch, gpu_support):
     assert isinstance(celery_app, Celery)
 
 
-@pytest.mark.parametrize("gpu_support", [(pytest.lazy_fixture("mock_gpu_both_modes")),])
-def test_force_start_gpu_mode_no_mocks(gpu_support, monkeypatch):
+@pytest.mark.parametrize("gpu_support", [(pytest.lazy_fixture("mock_node_has_gpu")),])
+def test_force_start_gpu_ext_dep_mocking(monkeypatch, gpu_support) -> None:
     monkeypatch.setenv("START_AS_MODE_GPU", "1")
 
     rabbit_cfg, celery_app = get_rabbitmq_config_and_celery_app()
