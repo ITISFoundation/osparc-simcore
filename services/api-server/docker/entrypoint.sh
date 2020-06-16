5c2f3187975a50cdc9f0c123a0108b4dc1f8d32b
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-06-16 21:11:10 +0200

    Updating dockerfile

--- services/api-server/docker/entrypoint.sh
+++ services/api-server/docker/entrypoint.sh
@@ -15,73 +15,62 @@ ERROR="ERROR: [$(basename "$0")] "
 #   *runs* as non-root user [scu]
 #
 echo "$INFO" "Entrypoint for stage ${SC_BUILD_TARGET} ..."
-echo   User    :"$(id "$(whoami)")"
-echo   Workdir :"$(pwd)"
-echo   scuUser :"$(id scu)"
-
+echo "$INFO" "User :$(id "$(whoami)")"
+echo "$INFO" "Workdir : $(pwd)"
+echo "$INFO" "User : $(id scu)"
+echo "$INFO" "python : $(command -v python)"
+echo "$INFO" "pip : $(command -v pip)"
 
 USERNAME=scu
 GROUPNAME=scu
 
-if [ "${SC_BUILD_TARGET}" = "development" ]
-then
-    echo "$INFO" "development mode detected..."
-    # NOTE: expects docker run ... -v $(pwd):/devel/services/api-server
-    DEVEL_MOUNT=/devel/services/api-server
+if [ "${SC_BUILD_TARGET}" = "development" ]; then
+  echo "$INFO" "development mode detected..."
+  # NOTE: expects docker run ... -v $(pwd):$DEVEL_MOUNT
+  DEVEL_MOUNT=/devel/services/web/server
 
-    stat $DEVEL_MOUNT > /dev/null 2>&1 || \
-        (echo "$ERROR" "You must mount '$DEVEL_MOUNT' to deduce user and group ids" && exit 1)
+  stat $DEVEL_MOUNT >/dev/null 2>&1 ||
+    (echo "$ERROR" "You must mount '$DEVEL_MOUNT' to deduce user and group ids" && exit 1)
 
-    USERID=$(stat --format=%u $DEVEL_MOUNT)
-    GROUPID=$(stat --format=%g $DEVEL_MOUNT)
-    GROUPNAME=$(getent group "${GROUPID}" | cut --delimiter=: --fields=1)
-
-    if [ "$USERID" -eq 0 ]
-    then
-        echo "$WARNING" Folder mounted owned by root user... adding "$SC_USER_NAME" to root...
-        adduser "${SC_USER_NAME}" root
+  echo "$INFO" "setting correct user id/group id..."
+  HOST_USERID=$(stat --format=%u "${DEVEL_MOUNT}")
+  HOST_GROUPID=$(stat --format=%g "${DEVEL_MOUNT}")
+  CONT_GROUPNAME=$(getent group "${HOST_GROUPID}" | cut --delimiter=: --fields=1)
+  if [ "$HOST_USERID" -eq 0 ]; then
+    echo "$WARNING" "Folder mounted owned by root user... adding $SC_USER_NAME to root..."
+    adduser "$SC_USER_NAME" root
+  else
+    echo "$INFO" "Folder mounted owned by user $HOST_USERID:$HOST_GROUPID-'$CONT_GROUPNAME'..."
+    # take host's credentials in $SC_USER_NAME
+    if [ -z "$CONT_GROUPNAME" ]; then
+      echo "$WARNING" "Creating new group grp$SC_USER_NAME"
+      CONT_GROUPNAME=grp$SC_USER_NAME
+      addgroup --gid "$HOST_GROUPID" "$CONT_GROUPNAME"
     else
-        # take host's credentials in scu
-        if [ -z "$GROUPNAME" ]
-        then
-            echo "$INFO" mounted folder from "$USERID", creating new group my"${SC_USER_NAME}"
-            GROUPNAME=my"${SC_USER_NAME}"
-            addgroup --gid "$GROUPID" "$GROUPNAME"
-            # change group property of files already around
-            find / -path /proc -prune -group "$SC_USER_ID" -exec chgrp --no-dereference "$GROUPNAME" {} \;
-        else
-            echo "$INFO" "mounted folder from $USERID, adding ${SC_USER_NAME} to $GROUPNAME..."
-            adduser "$SC_USER_NAME" "$GROUPNAME"
-        fi
-
-        echo "$INFO changing $SC_USER_NAME $SC_USER_ID:$SC_USER_ID to $USERID:$GROUPID"
-        deluser "${SC_USER_NAME}" > /dev/null 2>&1
-        if [ "$SC_USER_NAME" = "$GROUPNAME" ]
-        then
-            addgroup --gid "$GROUPID" "$GROUPNAME"
-        fi
-        adduser --disabled-password --gecos "" --uid "$USERID" --gid "$GROUPID" --shell /bin/sh "$SC_USER_NAME" --no-create-home
-        # change user property of files already around
-        find / -path /proc -prune -user "$SC_USER_ID" -exec chown --no-dereference "$SC_USER_NAME" {} \;
+      echo "$INFO" "group already exists"
     fi
+    echo "$INFO" "Adding $SC_USER_NAME to group $CONT_GROUPNAME..."
+    adduser "$SC_USER_NAME" "$CONT_GROUPNAME"
 
-    echo "$INFO installing pythong dependencies..."
-    cd services/api-server || exit 1
-    pip install --no-cache-dir -r requirements/dev.txt
-    cd - || exit 1
+    echo "$WARNING" "Changing ownership [this could take some time]"
+    echo "$INFO" "Changing $SC_USER_NAME:$SC_USER_NAME ($SC_USER_ID:$SC_USER_ID) to $SC_USER_NAME:$CONT_GROUPNAME ($HOST_USERID:$HOST_GROUPID)"
+    usermod --uid "$HOST_USERID" --gid "$HOST_GROUPID" "$SC_USER_NAME"
+
+    echo "$INFO" "Changing group properties of files around from $SC_USER_ID to group $CONT_GROUPNAME"
+    find / -path /proc -prune -o -group "$SC_USER_ID" -exec chgrp --no-dereference "$CONT_GROUPNAME" {} \;
+    # change user property of files already around
+    echo "$INFO" "Changing ownership properties of files around from $SC_USER_ID to group $CONT_GROUPNAME"
+    find / -path /proc -prune -o -user "$SC_USER_ID" -exec chown --no-dereference "$SC_USER_NAME" {} \;
+  fi
 fi
 
-if [ ${SC_BOOT_MODE} == "debug-ptvsd" ]
-then
+if [ "${SC_BOOT_MODE}" = "debug-ptvsd" ]; then
   # NOTE: production does NOT pre-installs ptvsd
   pip install --no-cache-dir ptvsd
 fi
-
 
 echo "$INFO Starting $* ..."
 echo "  $SC_USER_NAME rights    : $(id "$SC_USER_NAME")"
 echo "  local dir : $(ls -al)"
 
-
-# TODO: replace with gosu
-su --command "$*" "$SC_USER_NAME"
+exec gosu "$SC_USER_NAME" "$@"
