c4e28ad653bb985918b700f915bca02faacb62a8
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-06-16 18:47:21 +0200

    ensure project owner access rights cannot be removed

--- services/web/server/src/simcore_service_webserver/projects/projects_db.py
+++ services/web/server/src/simcore_service_webserver/projects/projects_db.py
@@ -458,6 +458,16 @@ OR prj_owner = {user_id})
             # uuid can ONLY be set upon creation
             if row[projects.c.uuid.key] != project_data["uuid"]:
                 raise ProjectInvalidRightsError(user_id, project_data["uuid"])
+            # ensure the prj owner is always in the access rights
+            owner_primary_gid = await self._get_user_primary_group_gid(
+                conn, row[projects.c.prj_owner.key]
+            )
+            project_data["accessRights"].update(
+                _create_project_access_rights(
+                    owner_primary_gid, ProjectAccessRights.OWNER
+                )
+            )
+
             # update timestamps
             project_data["lastChangeDate"] = now_str()
             # now update it
