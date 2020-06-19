7e52707b4bdc20cc0d10922699a34cc0aeb41196
odeimaiz <maiz@itis.swiss>
2020-06-19 16:47:23 +0200

    maximize dynamic service if it has no input node connected

--- services/web/client/source/class/osparc/desktop/StudyEditor.js
+++ services/web/client/source/class/osparc/desktop/StudyEditor.js
@@ -604,7 +604,7 @@ qx.Class.define("osparc.desktop.StudyEditor", {
       this.__scrollContainer.setVisibility("visible");
       this.__nodeView._maximizeIFrame(false); // eslint-disable-line no-underscore-dangle
       const node = this.getStudy().getWorkbench().getNode(this.__currentNodeId);
-      if (node && node.getIFrame() && !this.__nodeView.isSettingsGroupShowable()) {
+      if (node && node.getIFrame() && (node.getInputNodes().length === 0)) {
         node.getLoadingPage().maximizeIFrame(true);
         node.getIFrame().maximizeIFrame(true);
       }
