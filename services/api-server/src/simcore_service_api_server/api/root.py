accbc7e08e10ee10ce79eeca6dc0f966895dc1eb
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-06-22 20:45:46 +0200

    Removes unused routes

--- services/api-server/src/simcore_service_api_server/api/root.py
+++ services/api-server/src/simcore_service_api_server/api/root.py
@@ -5,11 +5,4 @@ from .routes import health, meta, users
 router = APIRouter()
 router.include_router(health.router)
 router.include_router(meta.router, tags=["meta"], prefix="/meta")
-
-# TODO: keeps for oauth or apikey schemes
-# router.include_router(authentication.router, tags=["authentication"], prefix="/users")
-
 router.include_router(users.router, tags=["users"], prefix="/me")
-
-## TODO: disables studies for the moment
-# router.include_router(studies.router, tags=["studies"], prefix="/studies")
