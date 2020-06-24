fdd540b57667ff80fd2e36bf4b9f8978eb53cc43
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-06-24 10:59:50 +0200

    mypy

--- services/web/server/src/simcore_service_webserver/resource_manager/websocket_manager.py
+++ services/web/server/src/simcore_service_webserver/resource_manager/websocket_manager.py
@@ -16,7 +16,7 @@
 
 import logging
 from contextlib import contextmanager
-from typing import Dict, List
+from typing import Dict, Iterator, List, Optional
 
 import attr
 from aiohttp import web
@@ -32,7 +32,7 @@ SOCKET_ID_KEY = "socket_id"
 @attr.s(auto_attribs=True)
 class WebsocketRegistry:
     user_id: str
-    client_session_id: str
+    client_session_id: Optional[str]
     app: web.Application
 
     def _resource_key(self) -> Dict[str, str]:
@@ -150,8 +150,8 @@ class WebsocketRegistry:
 
 @contextmanager
 def managed_resource(
-    user_id: str, client_session_id: str, app: web.Application
-) -> WebsocketRegistry:
+    user_id: str, client_session_id: Optional[str], app: web.Application
+) -> Iterator[WebsocketRegistry]:
     registry = WebsocketRegistry(user_id, client_session_id, app)
     try:
         yield registry
