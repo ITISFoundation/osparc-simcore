dc0d5761df1ecbcbaa55106bef254c07565af336
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-06-24 21:38:48 +0200

    Fixes boot

--- services/catalog/docker/boot.sh
+++ services/catalog/docker/boot.sh
@@ -18,7 +18,7 @@ if [ "${SC_BUILD_TARGET}" = "development" ]; then
   python --version | sed 's/^/    /'
   command -v python | sed 's/^/    /'
 
-  cd services/api-server || exit 1
+  cd services/catalog || exit 1
   pip --quiet --no-cache-dir install -r requirements/dev.txt
   cd - || exit 1
   echo "$INFO" "PIP :"
@@ -30,7 +30,7 @@ if [ "${SC_BOOT_MODE}" = "debug-ptvsd" ]
 then
   # NOTE: ptvsd is programmatically enabled inside of the service
   # this way we can have reload in place as well
-  exec uvicorn simcore_service_catalog.main:app --reload --host 0.0.0.0
+  exec uvicorn simcore_service_catalog.__main__:the_app --reload --host 0.0.0.0
 else
   exec simcore-service-catalog
 fi
