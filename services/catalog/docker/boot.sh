9c263ad7a590a8d373ee935b6af6c1119b9f13e7
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-06-24 18:46:14 +0200

    Upgrades dockerfile to python-slim

--- services/catalog/docker/boot.sh
+++ services/catalog/docker/boot.sh
@@ -1,30 +1,29 @@
 #!/bin/sh
-#
+set -o errexit
+set -o nounset
+
+IFS=$(printf '\n\t')
+
 INFO="INFO: [$(basename "$0")] "
 
 # BOOTING application ---------------------------------------------
 echo "$INFO" "Booting in ${SC_BOOT_MODE} mode ..."
-echo "  User    :$(id "$(whoami)")"
-echo "  Workdir :$(pwd)"
+echo "$INFO" "User :$(id "$(whoami)")"
+echo "$INFO" "Workdir : $(pwd)"
 
-if [ "${SC_BUILD_TARGET}" = "development" ]
-then
-  echo "  Environment :"
-  printenv  | sed 's/=/: /' | sed 's/^/    /' | sort
-  #--------------------
-
-  cd /devel/services/catalog || exit
-  pip3 --no-cache-dir install --user -r requirements/dev.txt
-  cd /devel || exit
-
-  #--------------------
-  echo "$INFO" "  Python :"
+if [ "${SC_BUILD_TARGET}" = "development" ]; then
+  echo "$INFO" "Environment :"
+  printenv | sed 's/=/: /' | sed 's/^/    /' | sort
+  echo "$INFO" "Python :"
   python --version | sed 's/^/    /'
   command -v python | sed 's/^/    /'
-  echo "$INFO" "  PIP :"
-  pip3 --no-cache-dir list | sed 's/^/    /'
-fi
 
+  cd services/api-server || exit 1
+  pip --quiet --no-cache-dir install -r requirements/dev.txt
+  cd - || exit 1
+  echo "$INFO" "PIP :"
+  pip list | sed 's/^/    /'
+fi
 
 # RUNNING application ----------------------------------------
 if [ "${SC_BOOT_MODE}" = "debug-ptvsd" ]
