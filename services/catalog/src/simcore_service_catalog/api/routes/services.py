59e9d7976e8155979771113162df51bb6ff6db4c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-07-09 22:47:27 +0200

    lintin

--- services/catalog/src/simcore_service_catalog/api/routes/services.py
+++ services/catalog/src/simcore_service_catalog/api/routes/services.py
@@ -1,10 +1,8 @@
 import logging
-from typing import List, Optional
+from typing import List
 
-from fastapi import APIRouter, Body, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends
 from pydantic import ValidationError
-from pydantic.tools import parse_obj_as
-from starlette import status
 
 from simcore_service_catalog.api.dependencies.director import get_director_session
 
@@ -16,18 +14,7 @@ logger = logging.getLogger(__name__)
 
 
 @router.get("", response_model=List[ServiceOut])
-async def list_services(
-    client: AuthSession = Depends(get_director_session),
-    page_token: Optional[str] = Query(
-        None, description="Requests a specific page of the list results"
-    ),
-    page_size: int = Query(
-        0, ge=0, description="Maximum number of results to be returned by the server"
-    ),
-    order_by: Optional[str] = Query(
-        None, description="Sorts in ascending order comma-separated fields"
-    ),
-):
+async def list_services(client: AuthSession = Depends(get_director_session)):
     data = await client.get("/services")
     services: List[ServiceOut] = []
     for x in data:
