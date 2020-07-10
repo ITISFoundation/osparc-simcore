82484c7cd97e75d7b9481bf6a77519c664b04a10
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-07-10 17:27:49 +0200

    add director mockup

--- services/catalog/tests/unit/with_dbs/test_entrypoint_services.py
+++ services/catalog/tests/unit/with_dbs/test_entrypoint_services.py
@@ -1,13 +1,18 @@
+import pytest
 from starlette.testclient import TestClient
 
 from simcore_service_catalog.models.domain.service import ServiceData
 
-
-core_services = ["postgres", "director"]
+core_services = ["postgres"]
 ops_services = ["adminer"]
 
 
-def test_list_services(client: TestClient):
+@pytest.fixture()
+def director_mockup():
+    pass
+
+
+def test_list_services(client: TestClient, director_mockup):
     pass
     # response = client.get("/v0/services")
     # assert response.status_code == 200
