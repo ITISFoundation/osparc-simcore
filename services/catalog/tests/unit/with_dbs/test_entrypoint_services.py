74594043b98e6787b13fa3d9590dea30dcd7ab49
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-07-10 11:24:07 +0200

    prepare test for checking mocker

--- services/catalog/tests/unit/with_dbs/test_entrypoint_services.py
+++ services/catalog/tests/unit/with_dbs/test_entrypoint_services.py
@@ -7,7 +7,7 @@
 import pytest
 from starlette.testclient import TestClient
 
-from simcore_service_catalog.models.domain.service import ServiceData
+# from simcore_service_catalog.models.domain.service import ServiceData
 
 core_services = ["postgres"]
 ops_services = ["adminer"]
@@ -15,7 +15,20 @@ ops_services = ["adminer"]
 
 @pytest.fixture()
 def director_mockup(mocker):
+    director_mockup = mocker.patch(
+        "simcore_service_catalog.services.director", spec=True
+    )
+    # director_mockup.upload_file.return_value = Future()
+    # director_mockup.upload_file.return_value.set_result("")
+
+
+def test_director_mockup(director_mockup, app):
+    from simcore_service_catalog.services import director
+
     pass
+    # import pdb
+
+    # pdb.set_trace()
 
 
 def test_list_services(client: TestClient, director_mockup):
