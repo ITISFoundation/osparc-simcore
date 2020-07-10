fd40f53b1087b0d6de2ccbc9e52e96aacf8d9cca
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-07-10 21:15:10 +0200

    Split installation in three steps

--- ci/github/system-testing/e2e.bash
+++ ci/github/system-testing/e2e.bash
@@ -11,69 +11,91 @@ export DOCKER_IMAGE_TAG
 SWARM_STACK_NAME=e2e_test_stack
 export SWARM_STACK_NAME
 
+setup_images() {
+  echo "--------------- getting simcore docker images..."
+  make pull-version || ( (make pull-cache || true) && make build-x tag-version)
+  make info-images
+
+  # configure simcore for testing with a private registry
+  bash tests/e2e/scripts/setup_env_insecure_registry.bash
+
+  # start simcore and set log-level
+  export LOG_LEVEL=WARNING
+  make up-version
+}
+
+setup_environment() {
+
+  echo "--------------- installing environment ..."
+  /bin/bash -c 'sudo apt install -y postgresql-client'
+
+  echo "-------------- installing test framework..."
+  # create a python venv and activate
+  make .venv
+  # shellcheck disable=SC1091
+  source .venv/bin/activate
+
+  bash ci/helpers/ensure_python_pip.bash
+  pushd tests/e2e
+  make install
+  popd
+}
+
+setup_testing() {
+  make .venv
+  # shellcheck disable=SC1091
+  source .venv/bin/activate
+  pushd tests/e2e
+  echo "--------------- deploying the registry..."
+  make registry-up
+  echo "--------------- waiting for all services to be up..."
+  make wait-for-services
+  echo "--------------- transfering the images to the local registry..."
+  make transfer-images-to-registry
+  echo "--------------- injecting templates in postgres db..."
+  make pg-db-tables
+  make inject-templates-in-db
+  popd
+}
+
+
 install() {
-    echo "--------------- installing psql client..."
-    /bin/bash -c 'sudo apt install -y postgresql-client'
-    echo "--------------- getting simcore docker images..."
-    make pull-version || ( (make pull-cache || true) && make build-x tag-version)
-    make info-images
-
-    # configure simcore for testing with a private registry
-    bash tests/e2e/scripts/setup_env_insecure_registry.bash
-
-    # start simcore and set log-level
-    export LOG_LEVEL=WARNING; make up-version
-
-    echo "-------------- installing test framework..."
-    # create a python venv and activate
-    make .venv
-    # shellcheck disable=SC1091
-    source .venv/bin/activate
-    bash ci/helpers/ensure_python_pip.bash
-    pushd tests/e2e;
-    make install
-    echo "--------------- deploying the registry..."
-    make registry-up
-    echo "--------------- waiting for all services to be up..."
-    make wait-for-services
-    echo "--------------- transfering the images to the local registry..."
-    make transfer-images-to-registry
-    echo "--------------- injecting templates in postgres db..."
-    make pg-db-tables
-    make inject-templates-in-db
-    popd
+  setup_images
+  setup_environment
+  setup_testing
 }
 
 test() {
-    pushd tests/e2e; make test; popd
+  pushd tests/e2e
+  make test
+  popd
 
 }
 
 recover_artifacts() {
-    # all screenshots are in tests/e2e/screenshots if any
+  # all screenshots are in tests/e2e/screenshots if any
 
-    # get docker logs.
-    # WARNING: dumping long logs might take hours!!
-    mkdir simcore_logs
-    (docker service logs --timestamps --tail=300 --details simcore_webserver > simcore_logs/webserver.log 2>&1) || true
-    (docker service logs --timestamps --tail=200 --details simcore_director > simcore_logs/director.log  2>&1) || true
-    (docker service logs --timestamps --tail=200 --details simcore_storage > simcore_logs/storage.log  2>&1) || true
-    (docker service logs --timestamps --tail=200 --details simcore_sidecar > simcore_logs/sidecar.log  2>&1) || true
-    (docker service logs --timestamps --tail=200 --details simcore_catalog > simcore_logs/catalog.log  2>&1) || true
+  # get docker logs.
+  # WARNING: dumping long logs might take hours!!
+  mkdir simcore_logs
+  (docker service logs --timestamps --tail=300 --details simcore_webserver >simcore_logs/webserver.log 2>&1) || true
+  (docker service logs --timestamps --tail=200 --details simcore_director >simcore_logs/director.log 2>&1) || true
+  (docker service logs --timestamps --tail=200 --details simcore_storage >simcore_logs/storage.log 2>&1) || true
+  (docker service logs --timestamps --tail=200 --details simcore_sidecar >simcore_logs/sidecar.log 2>&1) || true
+  (docker service logs --timestamps --tail=200 --details simcore_catalog >simcore_logs/catalog.log 2>&1) || true
 }
 
 clean_up() {
-    echo "--------------- listing services running..."
-    docker service ls
-    echo "--------------- listing images available..."
-    docker images
-    echo "--------------- switching off..."
-    make leave
+  echo "--------------- listing services running..."
+  docker service ls
+  echo "--------------- listing images available..."
+  docker images
+  echo "--------------- switching off..."
+  make leave
 }
 
 # Check if the function exists (bash specific)
-if declare -f "$1" > /dev/null
-then
+if declare -f "$1" >/dev/null; then
   # call arguments verbatim
   "$@"
 else
