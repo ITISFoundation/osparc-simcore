b6db6014b1c198e0d3982d7b3abf6e19689e100d
odeimaiz <maiz@itis.swiss>
2020-07-15 17:31:29 +0200

    osparc.ui.window.Window close with "Esc"

--- services/web/client/source/class/osparc/ui/window/Window.js
+++ services/web/client/source/class/osparc/ui/window/Window.js
@@ -9,10 +9,12 @@ qx.Class.define("osparc.ui.window.Window", {
   extend: qx.ui.window.Window,
   construct: function(caption, icon) {
     this.base(arguments, caption, icon);
+
     this.set({
       appearance: "service-window",
       backgroundColor: "material-button-background"
     });
+
     this.addListener("appear", () => {
       // Enable closing when clicking outside the modal
       const thisDom = this.getContentElement().getDomElement();
@@ -31,6 +33,11 @@ qx.Class.define("osparc.ui.window.Window", {
         modalFrame.style.backgroundColor = "black";
         modalFrame.style.opacity = 0.4;
       }
+    });
+
+    const commandEsc = new qx.ui.command.Command("Esc");
+    commandEsc.addListener("execute", e => {
+      this.close();
     });
   },
   properties: {
