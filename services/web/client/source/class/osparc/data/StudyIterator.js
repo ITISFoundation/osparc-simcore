3ff3682066ee0a0bdde41e3ed0b3e12811aa284b
odeimaiz <maiz@itis.swiss>
2020-07-17 17:43:17 +0200

    Collection of static methods for handling study iterations

---
+++ services/web/client/source/class/osparc/data/StudyIterator.js
@@ -0,0 +1,84 @@
+/* ************************************************************************
+
+   osparc - the simcore frontend
+
+   https://osparc.io
+
+   Copyright:
+     2020 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+/**
+ *   Collection of static methods for handling study iterations
+ */
+
+qx.Class.define("osparc.data.StudyIterator", {
+  type: "static",
+
+  statics: {
+    createStudyIterations: function(primaryStudyData) {
+      const secondaryStudiesData = [];
+      const newVals = [3, 4, 5];
+      newVals.forEach((newVal, idx) => {
+        const delta = {};
+        const firstSleeperId = Object.keys(primaryStudyData["workbench"])[0];
+        delta[firstSleeperId] = {
+          "inputs": {
+            "in_2": newVal
+          }
+        };
+        const secondaryStudyData = osparc.data.StudyIterator.createStudyIteration(primaryStudyData, delta, idx);
+        if (secondaryStudyData) {
+          console.log(secondaryStudyData);
+          secondaryStudiesData.push(secondaryStudyData);
+        }
+      });
+      return secondaryStudiesData;
+    },
+
+    __applyDelta: function(secondaryStudyData, delta) {
+      // apply iteration delta
+      if (Object.keys(delta).length !== 1) {
+        console.error("One nodeId per delta");
+        return false;
+      }
+      const nodeId = Object.keys(delta)[0];
+      if (nodeId in secondaryStudyData["workbench"]) {
+        const inputs = secondaryStudyData["workbench"][nodeId]["inputs"];
+        if (Object.keys(delta[nodeId]["inputs"]).length !== 1) {
+          console.error("One inputId per delta");
+          return false;
+        }
+        const inputKey = Object.keys(delta[nodeId]["inputs"])[0];
+        if (inputKey in inputs) {
+          inputs[inputKey] = delta[nodeId]["inputs"][inputKey];
+          return true;
+        }
+      }
+      return false;
+    },
+
+    createStudyIteration: function(primaryStudyData, delta, idx) {
+      const secondaryStudyData = osparc.data.model.Study.deepCloneStudyObject(primaryStudyData);
+      if (osparc.data.StudyIterator.__applyDelta(secondaryStudyData, delta)) { // eslint-disable-line no-underscore-dangle
+        // give new study id
+        secondaryStudyData["uuid"] = osparc.utils.Utils.uuidv4();
+        // give a different name
+        secondaryStudyData["name"] = secondaryStudyData["name"] + " it-" + (idx+1);
+        // replace orignal uuids
+        secondaryStudyData["workbench"] = osparc.data.Converters.replaceUuids(secondaryStudyData["workbench"]);
+        // make them read only
+
+        return secondaryStudyData;
+      }
+      return null;
+    }
+  }
+});
