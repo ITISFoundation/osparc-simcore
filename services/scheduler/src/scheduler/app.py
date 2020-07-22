1f794b22ca72e0ce83e4ce28693c06739a35169a
Andrei Neagu <neagu@itis.swiss>
2020-07-22 10:25:32 +0200

    split the initialization and run - added some tests - divided in 2 files - added auto discovery of paths

--- services/scheduler/src/scheduler/app.py
+++ services/scheduler/src/scheduler/app.py
@@ -1,8 +1,18 @@
+# pylint: disable=wrong-import-position
 from fastapi import FastAPI
 
 app = FastAPI()
 
+# imports all submodules to ensure service discovery
+from scheduler import api as _  # pylint: disable=unused-import
+from scheduler.dbs.mongo_models import initialize_mongo_driver
 
-@app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
+
+@app.on_event("startup")
+async def startup_event():
+    await initialize_mongo_driver()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    pass
--- services/scheduler/tests/test_app.py
+++ services/scheduler/tests/test_app.py
@@ -1,11 +1,35 @@
-from fastapi.testclient import TestClient
+# pylint: disable=unused-argument
+from collections import deque
 
+import pytest
+from async_asgi_testclient import TestClient
+
+from scheduler import queues
 from scheduler.app import app
 
-client = TestClient(app)
+
+@pytest.mark.asyncio
+async def test_queues_initialized():
+
+    async with TestClient(app) as _:
+        # discover all queues.AsyncQueue instances defined in the module
+        queues_to_test = deque()
+        for entry_key in vars(queues):
+            module_entry = getattr(queues, entry_key)
+            if isinstance(module_entry, queues.AsyncQueue):
+                queues_to_test.append(module_entry)
+
+        for k, test_queue in enumerate(queues_to_test):
+            test_item = {"index": k}
+            print(test_queue)
+            assert test_queue is not None
+            await test_queue.add(test_item)
+            assert test_item == await test_queue.get()
 
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+@pytest.mark.asyncio
+async def test_read_main():
+    async with TestClient(app) as client:
+        response = await client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"msg": "Hello World"}
