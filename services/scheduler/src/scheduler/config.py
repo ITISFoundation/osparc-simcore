c3ca730e654b786a66046067d5da063f03f93f62
Andrei Neagu <neagu@itis.swiss>
2020-07-22 10:27:02 +0200

    added env vars configuration parser

---
+++ services/scheduler/src/scheduler/config.py
@@ -0,0 +1,16 @@
+from environs import Env
+
+env = Env()
+env.read_env()  # read .env file, if it exists
+
+# OSPARC
+# used to distinguish between multiple deployments
+swarm_stack_name = env("SWARM_STACK_NAME", "")
+
+# Redis endpoint configuration
+redis_host = env("REDIS_HOST", "redis")
+redis_port = env.int("REDIS_PORT", 6379)
+
+# MongoDB endpoint URI
+mongo_uri = env("MONGO_DB_URI", "mongodb://localhost:27017")
+mongo_db_name = env("MONGO_DB_NAME", "scheduler")
