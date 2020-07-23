4a4509b9d9c2d49399306b13f9a648a73b210ac0
odeimaiz <maiz@itis.swiss>
2020-07-23 14:58:29 +0200

    cleanup

--- services/web/client/source/class/osparc/data/StudyParametrizer.js
+++ services/web/client/source/class/osparc/data/StudyParametrizer.js
@@ -87,6 +87,7 @@ qx.Class.define("osparc.data.StudyParametrizer", {
           secondaryStudyDataStr = secondaryStudyDataStr.replace(mustachedStr, varValue);
         });
         secondaryStudyData = JSON.parse(secondaryStudyDataStr);
+
         secondaryStudiesData.push(secondaryStudyData);
       });
 
@@ -102,48 +103,8 @@ qx.Class.define("osparc.data.StudyParametrizer", {
       secondaryStudyData["name"] = secondaryStudyData["name"] + " (it-" + (idx+1) + ")";
       // replace orignal uuids
       secondaryStudyData["workbench"] = osparc.data.Converters.replaceUuids(secondaryStudyData["workbench"]);
-      // make them read only
 
       return secondaryStudyData;
-    },
-
-    __applyDelta: function(secondaryStudyData, delta) {
-      // apply iteration delta
-      if (Object.keys(delta).length !== 1) {
-        console.error("One nodeId per delta");
-        return false;
-      }
-      const nodeId = Object.keys(delta)[0];
-      if (nodeId in secondaryStudyData["workbench"]) {
-        const inputs = secondaryStudyData["workbench"][nodeId]["inputs"];
-        if (Object.keys(delta[nodeId]["inputs"]).length !== 1) {
-          console.error("One inputId per delta");
-          return false;
-        }
-        const inputKey = Object.keys(delta[nodeId]["inputs"])[0];
-        if (inputKey in inputs) {
-          inputs[inputKey] = delta[nodeId]["inputs"][inputKey];
-          return true;
-        }
-      }
-      return false;
-    },
-
-    __createStudyParameterization: function(primaryStudyData, delta, idx) {
-      const secondaryStudyData = osparc.data.model.Study.deepCloneStudyObject(primaryStudyData);
-      // eslint-disable-next-line no-underscore-dangle
-      if (osparc.data.StudyParametrizer.__applyDelta(secondaryStudyData, delta)) {
-        // give new study id
-        secondaryStudyData["uuid"] = osparc.utils.Utils.uuidv4();
-        // give a different name
-        secondaryStudyData["name"] = secondaryStudyData["name"] + " (it-" + (idx+1) + ")";
-        // replace orignal uuids
-        secondaryStudyData["workbench"] = osparc.data.Converters.replaceUuids(secondaryStudyData["workbench"]);
-        // make them read only
-
-        return secondaryStudyData;
-      }
-      return null;
     }
   }
 });
