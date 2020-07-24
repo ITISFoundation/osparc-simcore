132f8037c78f97972c2041ed70998ab875e9dec5
odeimaiz <maiz@itis.swiss>
2020-07-24 13:40:02 +0200

    dev column added to project model

--- packages/postgres-database/src/simcore_postgres_database/models/projects.py
+++ packages/postgres-database/src/simcore_postgres_database/models/projects.py
@@ -61,5 +61,6 @@ projects = sa.Table(
         "access_rights", JSONB, nullable=False, server_default=sa.text("'{}'::jsonb")
     ),
     sa.Column("workbench", sa.JSON, nullable=False),
+    sa.Column("dev", sa.JSON, nullable=True, default={}),
     sa.Column("published", sa.Boolean, nullable=False, default=False),
 )
