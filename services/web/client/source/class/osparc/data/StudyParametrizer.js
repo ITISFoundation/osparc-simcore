00f159f1a390666df32376842d73bb43c4a4e06d
odeimaiz <maiz@itis.swiss>
2020-07-27 10:51:46 +0200

    steps -> nSteps

--- services/web/client/source/class/osparc/data/StudyParametrizer.js
+++ services/web/client/source/class/osparc/data/StudyParametrizer.js
@@ -25,7 +25,7 @@ qx.Class.define("osparc.data.StudyParametrizer", {
   statics: {
     /**
      * Calculate the steps for the given parameter specs.
-     * In: {low: 0, high: 3, steps: 4}
+     * In: {low: 0, high: 3, nSteps: 4}
      * Out:[0, 1, 2, 3]
      * @param args {Array} Array of arrays
      */
@@ -33,8 +33,8 @@ qx.Class.define("osparc.data.StudyParametrizer", {
       const arrs = [];
       params.forEach(param => {
         const arr = [];
-        const step = param.steps > 1 ? ((param.high - param.low) / (param.steps-1)) : 0;
-        for (let i=0; i<param.steps; i++) {
+        const step = param.nSteps > 1 ? ((param.high - param.low) / (param.nSteps-1)) : 0;
+        for (let i=0; i<param.nSteps; i++) {
           arr.push(param.low + step*i);
         }
         arrs.push(arr);
