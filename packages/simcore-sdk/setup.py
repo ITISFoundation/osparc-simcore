2aa551eb6e7fdf86192eb8efba33579de1552a7b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-07-27 10:39:29 +0200

    correct depencies when installing postgres package

--- packages/simcore-sdk/setup.py
+++ packages/simcore-sdk/setup.py
@@ -7,28 +7,26 @@ from setuptools import setup, find_packages
 here = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
 
-def read_reqs( reqs_path: Path):
-    return re.findall(r'(^[^#-][\w]+[-~>=<.\w]+)', reqs_path.read_text(), re.MULTILINE)
+def read_reqs(reqs_path: Path):
+    return re.findall(r"(^[^#-][\w]+[-~>=<.\w]+)", reqs_path.read_text(), re.MULTILINE)
 
 
-install_requirements = read_reqs( here / "requirements" / "_base.in" )
-test_requirements = read_reqs( here / "requirements" / "_test.txt" ) + [
-    "simcore-postgres-database",
+install_requirements = read_reqs(here / "requirements" / "_base.in")
+test_requirements = read_reqs(here / "requirements" / "_test.txt") + [
+    "simcore-postgres-database[migration]",
     "simcore-service-library",
     "s3wrapper",
-    "simcore-service-storage-sdk"
+    "simcore-service-storage-sdk",
 ]
 
 setup(
-    name='simcore-sdk',
-    version='0.1.0',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    python_requires='>=3.6',
+    name="simcore-sdk",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.6",
     install_requires=install_requirements,
     tests_require=test_requirements,
-    extras_require= {
-        'test': test_requirements
-    },
-    test_suite='tests'
+    extras_require={"test": test_requirements},
+    test_suite="tests",
 )
