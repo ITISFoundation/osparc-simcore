43ce5a03bd04391c963c7341e63a7138adaec496
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-07-29 17:21:17 +0200

    added tables for usage by catalog

--- services/catalog/src/simcore_service_catalog/db/tables.py
+++ services/catalog/src/simcore_service_catalog/db/tables.py
@@ -1,3 +1,6 @@
 from simcore_postgres_database.models.direct_acyclic_graphs import dags
+from simcore_postgres_database.models.services import services
+from simcore_postgres_database.models.groups import user_to_groups, groups, GroupType
+from simcore_postgres_database.models.users import users
 
-__all__ = ["dags"]
+__all__ = ["dags", "services", "users", "user_to_groups", "groups", "GroupType"]
