f985d59d6dab72b47f2608a44d88c2afdd799a87
Andrei Neagu <neagu@itis.swiss>
2020-07-31 14:49:38 +0200

    migrated service-sidecar project to osparc

---
+++ services/service-sidecar/setup.py
@@ -0,0 +1,36 @@
+import os
+import re
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+current_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+
+
+def read_reqs(reqs_path: Path):
+    return re.findall(r"(^[^#-][\w]+[-~>=<.\w]+)", reqs_path.read_text(), re.MULTILINE)
+
+
+# -----------------------------------------------------------------
+# Hard requirements on third-parties and latest for in-repo packages
+install_requires = read_reqs(current_dir / "requirements" / "_base.txt")
+tests_require = read_reqs(current_dir / "requirements" / "_test.txt")
+
+current_version = (current_dir / "VERSION").read_text().strip()
+
+setup(
+    name="sidecar",
+    version=current_version,
+    packages=find_packages(where="src"),
+    package_dir={"": "src",},
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=install_requires,
+    tests_require=tests_require,
+    setup_requires=["setuptools_scm"],
+    entry_points={
+        "console_scripts": [
+            "service-sidecar-startup = sidecar.main:main",
+        ],
+    },
+)
---
+++ services/service-sidecar/src/sidecar/storage.py
@@ -0,0 +1,22 @@
+import collections
+
+from typing import Any
+
+
+class Store:
+    """Define custom storage abstraction for easy future extention"""
+
+    KEY = "compose_spec"  # default key for all actions
+
+    def __init__(self):
+        self._storage = {}
+
+    async def get(self, default=None) -> Any:
+        return self._storage.get(Store.KEY, default)
+
+    async def update(self, value: Any) -> None:
+        self._storage[Store.KEY] = value
+
+
+store = Store()
+
