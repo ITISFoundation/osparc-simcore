0fc0cf98906ac2b5caff90b42d7948f3d5f1c4da
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-08-03 14:57:19 +0200

    fix unit test

--- services/catalog/tests/unit/with_dbs/test_entrypoint_services.py
+++ services/catalog/tests/unit/with_dbs/test_entrypoint_services.py
@@ -5,14 +5,16 @@
 
 
 import unittest
+from random import randint
 from typing import Container, List
 
 import pytest
 from fastapi import FastAPI
-from simcore_service_catalog.api.dependencies.director import get_director_session
 from starlette.testclient import TestClient
+from yarl import URL
 
 from pytest_simcore.helpers.utils_mock import future_with_result
+from simcore_service_catalog.api.dependencies.director import get_director_session
 from simcore_service_catalog.api.routes import services
 from simcore_service_catalog.models.domain.service import ServiceType
 from simcore_service_catalog.models.schemas.service import ServiceOut
@@ -39,7 +41,7 @@ def fake_services() -> List[ServiceOut]:
 
 @pytest.fixture()
 async def director_mockup(loop, monkeypatch, fake_services, app: FastAPI):
-    async def return_list_services() -> List[ServiceOut]:
+    async def return_list_services(user_id: int) -> List[ServiceOut]:
         return fake_services
 
     monkeypatch.setattr(services, "list_services", return_list_services)
@@ -52,12 +54,18 @@ async def director_mockup(loop, monkeypatch, fake_services, app: FastAPI):
     app.dependency_overrides[get_director_session] = FakeDirector
 
 
+@pytest.fixture
+def user_id() -> int:
+    return randint(1, 10000)
+
+
 async def test_director_mockup(
-    director_mockup, app: FastAPI, fake_services: List[ServiceOut]
+    director_mockup, app: FastAPI, fake_services: List[ServiceOut], user_id: int
 ):
-    await services.list_services() == fake_services
+    assert await services.list_services(user_id) == fake_services
 
 
-def test_list_services(director_mockup, app: FastAPI, client: TestClient):
-    response = client.get("/v0/services")
+def test_list_services(director_mockup, app: FastAPI, client: TestClient, user_id: int):
+    url = URL("/v0/services").with_query(user_id=user_id)
+    response = client.get(str(url))
     assert response.status_code == 200
