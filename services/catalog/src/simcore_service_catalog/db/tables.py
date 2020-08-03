5cbb2f0203845f4a8c6cdb767206f753ab41caa2
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-08-03 15:22:06 +0200

    change table names

--- services/catalog/src/simcore_service_catalog/db/tables.py
+++ services/catalog/src/simcore_service_catalog/db/tables.py
@@ -1,6 +1,17 @@
 from simcore_postgres_database.models.direct_acyclic_graphs import dags
-from simcore_postgres_database.models.services import services
+from simcore_postgres_database.models.services import (
+    services_meta_data,
+    services_access_rights,
+)
 from simcore_postgres_database.models.groups import user_to_groups, groups, GroupType
 from simcore_postgres_database.models.users import users
 
-__all__ = ["dags", "services", "users", "user_to_groups", "groups", "GroupType"]
+__all__ = [
+    "dags",
+    "services_meta_data",
+    "services_access_rights",
+    "users",
+    "user_to_groups",
+    "groups",
+    "GroupType",
+]
