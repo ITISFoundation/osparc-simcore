9adc8cd88a99516d4e4ea1983ff25711a5140205
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-08-05 15:40:31 +0200

    cleanup

--- services/catalog/src/simcore_service_catalog/api/routes/services.py
+++ services/catalog/src/simcore_service_catalog/api/routes/services.py
@@ -1,8 +1,6 @@
 import logging
-import pdb
 import urllib.parse
-from os import write
-from typing import Dict, List, Set, Tuple
+from typing import List, Set, Tuple
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import ValidationError, constr
@@ -14,8 +12,6 @@ from ...models.domain.service import (
     KEY_RE,
     VERSION_RE,
     ServiceAccessRightsAtDB,
-    ServiceDockerData,
-    ServiceGroupAccessRights,
     ServiceMetaDataAtDB,
 )
 from ...models.schemas.service import ServiceIn, ServiceOut
@@ -160,10 +156,9 @@ async def modify_service(
     services_repo: ServicesRepository = Depends(get_repository(ServicesRepository)),
 ):
     # check the service exists
-    services_in_registry = await director_client.get(
+    await director_client.get(
         f"/services/{urllib.parse.quote_plus(service_key)}/{service_version}"
     )
-    service = ServiceOut.parse_obj(services_in_registry[0])
     # the director client already raises an exception if not found
 
     # get the user groups
