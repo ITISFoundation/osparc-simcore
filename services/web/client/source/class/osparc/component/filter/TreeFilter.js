ff7cbfd053495a411b3bfde389f3de4982428056
odeimaiz <maiz@itis.swiss>
2020-08-06 12:24:26 +0200

    predefiend treeData checked

--- services/web/client/source/class/osparc/component/filter/TreeFilter.js
+++ services/web/client/source/class/osparc/component/filter/TreeFilter.js
@@ -6,15 +6,22 @@
  */
 qx.Class.define("osparc.component.filter.TreeFilter", {
   extend: osparc.component.filter.UIFilter,
-  construct: function(filterId, filterGroupId) {
+
+  /**
+    * @param {string} filterId Group-unique id for the filter.
+    * @param {string} filterGroupId Unique group id where the filter belongs.
+    * @param {Array} treeData Initialization data
+    */
+  construct: function(filterId, filterGroupId, treeData = []) {
     this.base(arguments, filterId, filterGroupId);
     this._setLayout(new qx.ui.layout.Grow());
-    this.__createTree();
+    this.__createClassifiersTree(treeData);
   },
+
   members: {
     __tree: null,
-    __model: null,
-    __createTree: function() {
+
+    __createClassifiersTree: function(checkedClassifiers) {
       osparc.utils.Classifiers.getClassifiersAsTree().then(classifiers => {
         this.__tree = new osparc.ui.tree.CheckboxTree(classifiers);
         this.__tree.addListener("checkedChanged", e => {
@@ -22,6 +29,27 @@ qx.Class.define("osparc.component.filter.TreeFilter", {
         });
         this._removeAll();
         this._add(this.__tree);
+
+        this.__setCheckedClassifiers(this.__tree.getModel(), checkedClassifiers);
+      });
+    },
+
+    __setCheckedClassifiers: function(model, checkedClassifiers) {
+      if (checkedClassifiers.length === 0) {
+        return;
+      }
+
+      const nodes = model.getChildren();
+      nodes.forEach(node => {
+        if ("getData" in node) {
+          // this is a leaf
+          if (checkedClassifiers.includes(node.getData().getClassifier())) {
+            node.setChecked(true);
+          }
+        } else {
+          // this is a branch
+          this.__setCheckedClassifiers(node, checkedClassifiers);
+        }
       });
     }
   }
