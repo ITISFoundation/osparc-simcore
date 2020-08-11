381f1360de889ffd8827ae1718fdd90d1ae167cd
odeimaiz <maiz@itis.swiss>
2020-08-11 13:38:06 +0200

    JupyterLab part working

--- tests/e2e/tutorials/jupyters.js
+++ tests/e2e/tutorials/jupyters.js
@@ -28,14 +28,14 @@ async function runTutorial() {
   await tutorial.openTemplate(5000);
 
   // Some time for loading notebook and jupyter lab
-  await tutorial.waitFor(30000);
+  await tutorial.waitFor(25000);
 
 
   // open notebook
   await tutorial.openNode(1);
 
   const iframeHandles = await tutorial.getIframe();
-  // expected two iframes = loading + raw-graph
+  // expected two iframes = loading + jupyterNB
   const nbIframe = await iframeHandles[1].contentFrame();
 
   // inside the iFrame, open the first notebook
@@ -50,14 +50,14 @@ async function runTutorial() {
   await tutorial.takeScreenshot("openNotebook");
 
   // inside the first notebook, click Run button 4 times
-  for (let i=0; i<4; i++) {
-    const runBtnSelector = '#run_int > button:nth-child(1)';
-    await nbIframe.waitForSelector(runBtnSelector);
-    await nbIframe.click(runBtnSelector);
+  const runNBBtnSelector = '#run_int > button:nth-child(1)';
+  const runNotebookTimes = 4;
+  for (let i=0; i<runNotebookTimes; i++) {
+    await nbIframe.waitForSelector(runNBBtnSelector);
+    await nbIframe.click(runNBBtnSelector);
     await tutorial.waitFor(3000);
-    await tutorial.takeScreenshot("pressRun_" + i+1);
+    await tutorial.takeScreenshot("pressRunNB_" + i+1);
   }
-
 
   await tutorial.retrieve();
 
@@ -68,6 +68,45 @@ async function runTutorial() {
     "TheNumberNumber.txt"
   ];
   await tutorial.checkResults(outFiles.length);
+
+
+  // open jupyter lab
+  await tutorial.openNode(2);
+
+  await tutorial.retrieve();
+
+  const iframeHandles2 = await tutorial.getIframe();
+  // expected three iframes = loading + jupyterNB + jupyterLab
+  const jLabIframe = await iframeHandles2[2].contentFrame();
+
+  // inside the iFrame, open the first notebook
+  const workFolderSelector = '#filebrowser > div.lm-Widget.p-Widget.jp-DirListing.jp-FileBrowser-listing.jp-DirListing-narrow > ul > li:nth-child(3)';
+  await jLabIframe.waitForSelector(workFolderSelector);
+  await jLabIframe.click(workFolderSelector, {
+    clickCount: 2
+  });
+  await tutorial.waitFor(2000);
+
+  const input2outputFileSelector = '#filebrowser > div.lm-Widget.p-Widget.jp-DirListing.jp-FileBrowser-listing.jp-DirListing-narrow > ul > li:nth-child(1)';
+  await jLabIframe.waitForSelector(input2outputFileSelector);
+  await jLabIframe.click(input2outputFileSelector, {
+    clickCount: 2
+  });
+  await tutorial.waitFor(2000);
+
+  // click Run Menu
+  const mainRunMenuBtnSelector = '#jp-MainMenu > ul > li:nth-child(4)';
+  await nbIframe.waitForSelector(mainRunMenuBtnSelector);
+  await nbIframe.click(mainRunMenuBtnSelector);
+  await tutorial.waitFor(2000);
+
+  // click Run All Cells
+  const mainRunAllBtnSelector = 'body > div.lm-Widget.p-Widget.lm-Menu.p-Menu.lm-MenuBar-menu.p-MenuBar-menu > ul > li:nth-child(17)';
+  await nbIframe.waitForSelector(mainRunAllBtnSelector);
+  await nbIframe.click(mainRunAllBtnSelector);
+  await tutorial.waitFor(6000);
+  await tutorial.takeScreenshot("pressRunJLab");
+
 
   await tutorial.removeStudy();
   await tutorial.logOut();
