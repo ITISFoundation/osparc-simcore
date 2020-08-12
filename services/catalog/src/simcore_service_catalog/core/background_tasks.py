468102d242cfd8a7b12b4883ae454788ae289eaa
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-08-12 19:02:53 +0200

    initial services metadata is using contact as owner, then authors, and then adding affiliation to set read writes

--- services/catalog/src/simcore_service_catalog/core/background_tasks.py
+++ services/catalog/src/simcore_service_catalog/core/background_tasks.py
@@ -2,11 +2,15 @@ import asyncio
 import logging
 from asyncio.futures import CancelledError
 from pprint import pformat
-from typing import Dict, Set, Tuple
+from typing import Dict, List, Optional, Set, Tuple
 
 from aiopg.sa import Engine
+from aiopg.sa.connection import SAConnection
 from fastapi import FastAPI
 from pydantic import ValidationError
+from pydantic.types import PositiveInt
+
+from simcore_postgres_database.models.services import services_access_rights
 
 from ..api.dependencies.director import get_director_session
 from ..db.repositories.groups import GroupsRepository
@@ -62,25 +66,73 @@ async def _get_everyone_gid(app: FastAPI) -> int:
         return (await groups_repo.get_everyone_group()).gid
 
 
+async def _get_service_access_rights(
+    service: ServiceDockerData, connection: SAConnection
+) -> Tuple[Optional[PositiveInt], List[ServiceAccessRightsAtDB]]:
+    groups_repo = GroupsRepository(connection)
+    everyone_gid = (await groups_repo.get_everyone_group()).gid
+    owner_gid = None
+    reader_gids: List[PositiveInt] = []
+    # first get the owner emails
+    possible_owner_email = [service.contact] + [
+        author.email for author in service.authors
+    ]
+
+    for user_email in possible_owner_email:
+        possible_gid = await groups_repo.get_user_gid_from_email(user_email)
+        if possible_gid:
+            reader_gids.append(possible_gid)
+            if not owner_gid:
+                owner_gid = possible_gid
+
+    # there was no owner here, try with affiliation to some group
+    possible_affiliations = [
+        author.affiliation for author in service.authors if author.affiliation
+    ]
+    for user_affiliation in possible_affiliations:
+        possible_gid = await groups_repo.get_gid_from_affiliation(user_affiliation)
+        if possible_gid:
+            reader_gids.append(possible_gid)
+            if not owner_gid:
+                owner_gid = possible_gid
+
+    # if no owner gid yet, we pass readable rights to the everyone group
+    if not owner_gid:
+        reader_gids.append(everyone_gid)
+
+    # we add the owner with full rights, unless it's everyone
+    access_rights = [
+        ServiceAccessRightsAtDB(
+            key=service.key,
+            version=service.version,
+            gid=gid,
+            execute_access=True,
+            write_access=(gid == owner_gid),
+        )
+        for gid in set(reader_gids)
+    ]
+
+    return (owner_gid, access_rights)
+
+
 async def _create_services_in_db(
     app: FastAPI,
     service_keys: Set[Tuple[ServiceKey, ServiceVersion]],
     services: Dict[Tuple[ServiceKey, ServiceVersion], ServiceDockerData],
 ) -> None:
-    everyone_gid = await _get_everyone_gid(app)
     engine: Engine = app.state.engine
     async with engine.acquire() as conn:
         services_repo = ServicesRepository(conn)
         for service_key, service_version in service_keys:
             service: ServiceDockerData = services[(service_key, service_version)]
+            # find the service owner
+            owner_gid, service_access_rights = await _get_service_access_rights(
+                service, conn
+            )
+            # set the service in the DB
             await services_repo.create_service(
-                ServiceMetaDataAtDB.parse_obj(service),
-                ServiceAccessRightsAtDB(
-                    key=service_key,
-                    version=service_version,
-                    gid=everyone_gid,
-                    execute_access=True,
-                ),
+                ServiceMetaDataAtDB(**service.dict(), owner=owner_gid),
+                service_access_rights,
             )
 
 
@@ -107,7 +159,7 @@ async def sync_registry_task(app: FastAPI) -> None:
                     app, missing_services_in_db, services_in_registry
                 )
 
-            await asyncio.sleep(5)
+            await asyncio.sleep(app.state.settings.background_task_rest_time)
 
         except CancelledError:
             # task is stopped
