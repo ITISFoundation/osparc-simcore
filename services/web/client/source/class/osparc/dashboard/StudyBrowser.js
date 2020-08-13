59cf64ac48d63392bf8115964f3c962f7a9a17f9
odeimaiz <maiz@itis.swiss>
2020-08-13 14:24:10 +0200

    minor

--- services/web/client/source/class/osparc/dashboard/StudyBrowser.js
+++ services/web/client/source/class/osparc/dashboard/StudyBrowser.js
@@ -508,7 +508,7 @@ qx.Class.define("osparc.dashboard.StudyBrowser", {
     },
 
     __getSaveAsTemplateMenuButton: function(studyData) {
-      const saveAsTemplateButton = new qx.ui.menu.Button(this.tr("Publish as Template"));
+      const saveAsTemplateButton = new qx.ui.menu.Button(this.tr("Publish"));
       saveAsTemplateButton.addListener("execute", () => {
         const saveAsTemplateView = new osparc.component.export.SaveAsTemplate(studyData.uuid, studyData);
         const title = this.tr("Publish as Template");
