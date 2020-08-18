af8a29cf7088628972752cafddbafa048bf6957d
Sylvain <35365065+sanderegg@users.noreply.github.com>
2020-08-18 11:53:03 +0200

    Split webserver tests (#1711)
    split webserver testing in 4 jobs

--- ci/github/unit-testing/webserver.bash
+++ ci/github/unit-testing/webserver.bash
@@ -9,10 +9,36 @@ install() {
     pip list -v
 }
 
-test() {
+# isolated = these tests are (IMO) real unit tests, they do not need any dependencies and were already in the root test/unit folder before
+# db slow = it is currently the slowest test module it takes about 10 minutes (db stand for database-dependency, these tests were already in test/unit/with_dbs folder)
+# db medium = are tests that take 3-5 minutes, altogether about 10 minutes (db stand for database-dependency, these tests were already in test/unit/with_dbs folder)
+# db fast = all the others (db stand for database-dependency, these tests were already in test/unit/with_dbs folder)
+
+# As the plan is to strip the webserver into small micro-services I did not create now a super fancy classification but merely split the tests in ~equivalent test times.
+
+
+test_isolated() {
     pytest --cov=simcore_service_webserver --durations=10 --cov-append \
           --color=yes --cov-report=term-missing --cov-report=xml --cov-config=.coveragerc \
-          -v -m "not travis" services/web/server/tests/unit
+          -v -m "not travis" services/web/server/tests/unit/isolated
+}
+
+test_with_db_slow() {
+    pytest --cov=simcore_service_webserver --durations=10 --cov-append \
+          --color=yes --cov-report=term-missing --cov-report=xml --cov-config=.coveragerc \
+          -v -m "not travis" services/web/server/tests/unit/with_dbs/slow
+}
+
+test_with_db_medium() {
+    pytest --cov=simcore_service_webserver --durations=10 --cov-append \
+          --color=yes --cov-report=term-missing --cov-report=xml --cov-config=.coveragerc \
+          -v -m "not travis" services/web/server/tests/unit/with_dbs/medium
+}
+
+test_with_db_fast() {
+    pytest --cov=simcore_service_webserver --durations=10 --cov-append \
+          --color=yes --cov-report=term-missing --cov-report=xml --cov-config=.coveragerc \
+          -v -m "not travis" services/web/server/tests/unit/with_dbs/fast
 }
 
 # Check if the function exists (bash specific)
