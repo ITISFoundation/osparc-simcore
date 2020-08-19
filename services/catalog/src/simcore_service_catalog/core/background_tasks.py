401cd8231591778b30a9e88d51fd2977a767b71f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-08-19 22:33:48 +0200

    new rationale in place for ownership

--- services/catalog/src/simcore_service_catalog/core/background_tasks.py
+++ services/catalog/src/simcore_service_catalog/core/background_tasks.py
@@ -10,15 +10,17 @@
 import asyncio
 import logging
 from asyncio.futures import CancelledError
+from datetime import datetime
 from pprint import pformat
 from typing import Dict, List, Optional, Set, Tuple
+from urllib.parse import quote_plus
 
 from aiopg.sa import Engine
 from aiopg.sa.connection import SAConnection
-
 from fastapi import FastAPI
 from pydantic import ValidationError
 from pydantic.types import PositiveInt
+
 from simcore_service_catalog.db.repositories.projects import ProjectsRepository
 
 from ..api.dependencies.director import get_director_session
@@ -69,19 +71,41 @@ async def _list_db_services(
     }
 
 
+OLD_SERVICES_DATE: datetime = datetime(2020, 8, 19)
+
+
 async def _create_service_default_access_rights(
-    service: ServiceDockerData, connection: SAConnection
+    app: FastAPI, service: ServiceDockerData, connection: SAConnection
 ) -> Tuple[Optional[PositiveInt], List[ServiceAccessRightsAtDB]]:
     """Rationale as of 19.08.2020: all services that were put in oSparc before today
     will be visible to everyone.
     The services afterwards will be visible ONLY to his/her owner.
     """
 
+    async def _is_old_service(app: FastAPI, service: ServiceDockerData) -> bool:
+        # get service build date
+        client = get_director_session(app)
+        data = await client.get(
+            f"/service_extras/{quote_plus(service.key)}/{service.version}"
+        )
+        if not data or "build_date" not in data:
+            return True
+
+        logger.debug("retrieved service extras are %s", data)
+
+        service_build_data = datetime.strptime(data["build_date"], "%Y-%m-%dT%H:%M:%SZ")
+        return service_build_data < OLD_SERVICES_DATE
+
     groups_repo = GroupsRepository(connection)
     everyone_gid = (await groups_repo.get_everyone_group()).gid
     owner_gid = None
     reader_gids: List[PositiveInt] = []
-    # first get the owner emails
+    if await _is_old_service(app, service):
+        logger.debug("service %s:%s is old", service.key, service.version)
+        # let's make that one available to everyone
+        reader_gids.append(everyone_gid)
+
+    # try to find the owner
     possible_owner_email = [service.contact] + [
         author.email for author in service.authors
     ]
@@ -89,35 +113,10 @@ async def _create_service_default_access_rights(
     for user_email in possible_owner_email:
         possible_gid = await groups_repo.get_user_gid_from_email(user_email)
         if possible_gid:
-            reader_gids.append(possible_gid)
             if not owner_gid:
                 owner_gid = possible_gid
-
-    # there was no owner here, try with affiliation to some group
-    possible_affiliations = [
-        author.affiliation for author in service.authors if author.affiliation
-    ]
-    for user_affiliation in possible_affiliations:
-        possible_gid = await groups_repo.get_gid_from_affiliation(user_affiliation)
-        if possible_gid:
-            reader_gids.append(possible_gid)
-            if not owner_gid:
-                owner_gid = possible_gid
-
-    # if the service is part of a published template, it has to be available to everyone
-    projects_repo = ProjectsRepository(connection)
-    published_services: List[
-        ServiceKeyVersion
-    ] = await projects_repo.list_services_from_published_templates()
-    if (
-        ServiceKeyVersion(key=service.key, version=service.version)
-        in published_services
-    ):
-        reader_gids.append(everyone_gid)
-
-    # if no owner gid yet, we pass readable rights to the everyone group
     if not owner_gid:
-        reader_gids.append(everyone_gid)
+        logger.warning("service %s:%s has no owner", service.key, service.version)
 
     # we add the owner with full rights, unless it's everyone
     access_rights = [
@@ -135,6 +134,7 @@ async def _create_service_default_access_rights(
 
 
 async def _create_services_in_db(
+    app: FastAPI,
     connection: SAConnection,
     service_keys: Set[Tuple[ServiceKey, ServiceVersion]],
     services: Dict[Tuple[ServiceKey, ServiceVersion], ServiceDockerData],
@@ -145,7 +145,7 @@ async def _create_services_in_db(
         service: ServiceDockerData = services[(service_key, service_version)]
         # find the service owner
         owner_gid, service_access_rights = await _create_service_default_access_rights(
-            service, connection
+            app, service, connection
         )
         # set the service in the DB
         await services_repo.create_service(
@@ -172,7 +172,7 @@ async def _ensure_registry_insync_with_db(
         )
         # update db (rationale: missing services are shared with everyone for now)
         await _create_services_in_db(
-            connection, missing_services_in_db, services_in_registry
+            app, connection, missing_services_in_db, services_in_registry
         )
 
 
