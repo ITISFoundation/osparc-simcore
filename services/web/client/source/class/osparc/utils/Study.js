9d8dc95817b8bde18f5d4b649b70c65bf4b4e6e2
odeimaiz <maiz@itis.swiss>
2020-08-21 11:18:19 +0200

    utils.Study

---
+++ services/web/client/source/class/osparc/utils/Study.js
@@ -0,0 +1,57 @@
+/* ************************************************************************
+
+   osparc - the simcore frontend
+
+   https://osparc.io
+
+   Copyright:
+     2020 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+/**
+ * Collection of methods for studies
+ */
+
+qx.Class.define("osparc.utils.Study", {
+  type: "static",
+
+  statics: {
+
+    mustache: {
+      mustacheRegEx: function() {
+        return /{{([^{}]*)}}/g;
+      },
+
+      getVariables: function(obj) {
+        const variables = new Set();
+        const secondaryStudyDataStr = JSON.stringify(obj);
+        const mustaches = secondaryStudyDataStr.match(this.mustache.mustacheRegEx()) || [];
+        mustaches.forEach(mustache => {
+          const variable = mustache.replace("{{", "").replace("}}", "");
+          variables.add(variable);
+        });
+        return Array.from(variables);
+      },
+
+      replace: function(obj, parameters) {
+        const mustaches = this.mustache.getVariables(obj);
+        let objStr = JSON.stringify(obj);
+        mustaches.forEach(mustache => {
+          const paramId = mustache.replace("{{", "").replace("}}", "");
+          const parameter = parameters.find(param => param.id === paramId);
+          if (parameter) {
+            objStr = objStr.replace(mustache, parameter.label);
+          }
+        });
+        return JSON.parse(objStr);
+      }
+    }
+  }
+});
