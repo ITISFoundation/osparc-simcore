b67d1e3daf53865f5698139b2c4f8b5389206c05
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-08-26 18:17:35 +0200

    allow cancelling task when shutting down sidecar

--- services/sidecar/src/simcore_service_sidecar/celery.py
+++ services/sidecar/src/simcore_service_sidecar/celery.py
@@ -1,8 +1,27 @@
-from .remote_debug import setup_remote_debugging
+import asyncio
+
+from celery.signals import worker_shutting_down
+
 from .celery_configurator import get_rabbitmq_config_and_celery_app
+from .celery_log_setup import get_task_logger
+from .cli import run_sidecar
+from .remote_debug import setup_remote_debugging
 
 setup_remote_debugging()
 
 rabbit_config, app = get_rabbitmq_config_and_celery_app()
+
+log = get_task_logger(__name__)
+
+
+@worker_shutting_down.connect
+def worker_shutting_down_handler(sig, how, exitcode, **kwargs):
+    log.info("detected worker_shutting_down signal(%s, %s, %s)", sig, how, exitcode)
+    tasks = asyncio.Task.all_tasks()
+    for task in tasks:
+        if task._coro.__name__ == run_sidecar.__name__:
+            log.warning("canceling task....................")
+            task.cancel()
+
 
 __all__ = ["rabbit_config", "app"]
