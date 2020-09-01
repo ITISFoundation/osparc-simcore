89af33066f3f5acd774ca5ca9c5b4a0735d1c452
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-09-01 23:44:36 +0200

    Simplified test

--- services/director_v2/tests/unit/test_code_syntax.py
+++ services/director_v2/tests/unit/test_code_syntax.py
@@ -2,25 +2,17 @@
 # pylint:disable=unused-argument
 # pylint:disable=redefined-outer-name
 
-import os
-import re
-from pathlib import Path
-
-from pytest_simcore.helpers.utils_pylint import assert_pylint_is_passing
+from pytest_simcore.helpers.utils_pylint import (
+    assert_pylint_is_passing,
+    assert_no_pdb_in_code,
+)
 
 
 def test_run_pylint(pylintrc, installed_package_dir):
+    # fixtures in pytest_simcore.environs
     assert_pylint_is_passing(pylintrc=pylintrc, package_dir=installed_package_dir)
 
 
 def test_no_pdbs_in_place(installed_package_dir):
-    MATCH = re.compile(r"pdb.set_trace()")
-    EXCLUDE = ["__pycache__", ".git"]
-    for root, dirs, files in os.walk(installed_package_dir):
-        for name in files:
-            if name.endswith(".py"):
-                pypth = Path(root) / name
-                code = pypth.read_text()
-                found = MATCH.findall(code)
-                assert not found, "pbd.set_trace found in %s" % pypth
-        dirs[:] = [d for d in dirs if d not in EXCLUDE]
+    # fixtures in pytest_simcore.environs
+    assert_no_pdb_in_code(code_dir=installed_package_dir)
