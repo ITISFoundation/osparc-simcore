9c55b45ad2627cbe27b81060fa2970e57a9d5ca4
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-09-01 23:29:40 +0200

    Removed database dependencies

--- services/director_v2/src/simcore_service_director_v2/core/events.py
+++ services/director_v2/src/simcore_service_director_v2/core/events.py
@@ -3,7 +3,6 @@ from typing import Callable
 
 from fastapi import FastAPI
 
-from ..db.events import close_db_connection, connect_to_db
 from ..services.remote_debug import setup_remote_debugging
 from .settings import BootModeEnum
 
@@ -19,10 +18,6 @@ def create_start_app_handler(app: FastAPI) -> Callable:
             force_enabled=app.state.settings.boot_mode == BootModeEnum.debug
         )
 
-        # setup connection to pg db
-        if app.state.settings.postgres.enabled:
-            await connect_to_db(app)
-
     return start_app
 
 
@@ -30,8 +25,7 @@ def create_stop_app_handler(app: FastAPI) -> Callable:
     async def stop_app() -> None:
         try:
             logger.info("Application stopping")
-            if app.state.settings.postgres.enabled:
-                await close_db_connection(app)
+
         except Exception:  # pylint: disable=broad-except
             logger.exception("Stopping application")
 
--- services/director_v2/src/simcore_service_director_v2/core/settings.py
+++ services/director_v2/src/simcore_service_director_v2/core/settings.py
@@ -4,8 +4,21 @@ from typing import Optional
 
 from pydantic import BaseSettings, Field, SecretStr, validator
 from yarl import URL
+from ..__version__ import api_vtag
 
-# TODO: services/director/src/simcore_service_director/config.py
+MINS = 60
+API_ROOT: str = "api"
+APP_REGISTRY_CACHE_DATA_KEY: str = __name__ + "_registry_cache_data"
+
+SERVICE_RUNTIME_SETTINGS: str = "simcore.service.settings"
+SERVICE_REVERSE_PROXY_SETTINGS: str = "simcore.service.reverse-proxy-settings"
+SERVICE_RUNTIME_BOOTSETTINGS: str = "simcore.service.bootsettings"
+
+ORG_LABELS_TO_SCHEMA_LABELS = {
+    "org.label-schema.build-date": "build_date",
+    "org.label-schema.vcs-ref": "vcs_ref",
+    "org.label-schema.vcs-url": "vcs_url",
+}
 
 
 class BootModeEnum(str, Enum):
@@ -19,10 +32,18 @@ class _CommonConfig:
     env_file = ".env"  # SEE https://pydantic-docs.helpmanual.io/usage/settings/#dotenv-env-support
 
 
+class RegistrySettings(BaseSettings):
+    auth: bool = False
+    user: str = ""
+    pw: str = ""
+    url: str = ""
+    ssl: bool = True
+
+    class Config(_CommonConfig):
+        env_prefix = "REGISTRY_"
+
+
 class PostgresSettings(BaseSettings):
-    enabled: bool = Field(
-        True, description="Enables/Disables connection with postgres service"
-    )
     user: str
     password: SecretStr
     db: str
@@ -47,11 +68,23 @@ class PostgresSettings(BaseSettings):
         env_prefix = "POSTGRES_"
 
 
+class TracingSetttings(BaseSettings):
+    enabled: bool = True
+    zipkin_endpoint: str = "http://jaeger:9411"
+
+    class Config(_CommonConfig):
+        env_prefix = "TRACING_"
+
+
 class AppSettings(BaseSettings):
     @classmethod
     def create_default(cls) -> "AppSettings":
         # This call triggers parsers
-        return cls(postgres=PostgresSettings())
+        return cls(
+            registry=RegistrySettings(),
+            postgres=PostgresSettings(),
+            tracing=TracingSetttings(),
+        )
 
     # pylint: disable=no-self-use
     # pylint: disable=no-self-argument
@@ -66,16 +99,66 @@ class AppSettings(BaseSettings):
     def match_logging_level(cls, value) -> str:
         try:
             getattr(logging, value.upper())
-        except AttributeError:
-            raise ValueError(f"{value.upper()} is not a valid level")
+        except AttributeError as err:
+            raise ValueError(f"{value.upper()} is not a valid level") from err
         return value.upper()
 
     @property
     def loglevel(self) -> int:
         return getattr(logging, self.log_level_name)
 
+    # REGISTRY
+    registry: RegistrySettings
+
     # POSTGRES
     postgres: PostgresSettings
+
+    # STORAGE
+    storage_endpoint = Field("storage:8080", env="STORAGE_ENDPOINT")
+
+    # caching registry and TTL (time-to-live)
+    registry_caching: bool = True
+    registry_caching_ttl: int = 15 * MINS
+
+    # for passing self-signed certificate to spawned services
+    self_signed_ssl_secret_id: str = ""
+    self_signed_ssl_secret_name: str = ""
+    self_signed_ssl_filename: str = ""
+
+    # extras
+    extra_hosts_suffix: str = Field("undefined", env="EXTRA_HOSTS_SUFFIX")
+    published_hosts_name: str = Field("", env="PUBLISHED_HOSTS_NAME")
+    swarm_stack_name: str = Field("undefined-please-check", env="SWARM_STACK_NAME")
+
+    #
+    node_schema_location: str = Field(
+        f"{API_ROOT}/{api_vtag}/schemas/node-meta-v0.0.1.json",
+        description="used when in devel mode vs release mode",
+        env="NODE_SCHEMA_LOCATION",
+    )
+
+    #
+    simcore_services_network_name: Optional[str] = Field(
+        None,
+        description="used to find the right network name",
+        env="SIMCORE_SERVICES_NETWORK_NAME",
+    )
+    simcore_services_prefix: Optional[str] = Field(
+        "simcore/services",
+        description="useful when developing with an alternative registry namespace",
+        env="SIMCORE_SERVICES_PREFIX",
+    )
+
+    # traefik
+    traefik_simcore_zone: str = Field(
+        "internal_simcore_stack", env="TRAEFIK_SIMCORE_ZONE"
+    )
+
+    # monitoring
+    monitoring_enabled: str = Field(False, env="MONITORING_ENABLED")
+
+    # tracing
+    tracing: TracingSetttings
 
     # SERVICE SERVER (see : https://www.uvicorn.org/settings/)
     host: str = "0.0.0.0"  # nosec
@@ -83,4 +166,4 @@ class AppSettings(BaseSettings):
     debug: bool = False  # If True, debug tracebacks should be returned on errors.
 
     class Config(_CommonConfig):
-        env_prefix = ""
+        env_prefix = "DIRECTOR_"
