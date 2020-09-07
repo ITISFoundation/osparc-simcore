de0486e8046d1d8aff7d3ede8ddd3728b3c67e59
odeimaiz <maiz@itis.swiss>
2020-09-07 17:10:51 +0200

    initScreenshoter taken out from start

--- tests/e2e/tutorials/tutorialBase.js
+++ tests/e2e/tutorials/tutorialBase.js
@@ -22,13 +22,15 @@ class TutorialBase {
     this.__interval = null;
   }
 
-  async start() {
+  initScreenshoter() {
     utils.createScreenshotsDir();
 
     this.__interval = setInterval(async() => {
       await this.takeScreenshot();
     }, 2000);
+  }
 
+  async start() {
     await this.beforeScript();
     await this.goTo();
 
