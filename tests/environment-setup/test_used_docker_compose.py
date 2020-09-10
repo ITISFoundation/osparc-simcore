ae4027c273994bc9d19a3db28d454772ecbd7983
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-09-10 17:05:05 +0200

    testing environs

--- tests/environment-setup/test_used_docker_compose.py
+++ tests/environment-setup/test_used_docker_compose.py
@@ -3,14 +3,19 @@
 # pylint: disable=redefined-outer-name
 
 import re
-import pytest
-from typing import Tuple, List
 from pathlib import Path
+from typing import List, Tuple
+
+import pytest
 
 
 @pytest.fixture(scope="module")
-def reqs_versions(osparc_simcore_root_dir: Path) -> List[Tuple[Path, str]]:
+def docker_compose_in_requirement_files(
+    osparc_simcore_root_dir: Path,
+) -> List[Tuple[Path, str]]:
+
     reqs_path_version_tuples = []
+
     for req_path in osparc_simcore_root_dir.rglob("*.txt"):
         found = re.search(r"docker-compose==([\d\.]+)", req_path.read_text())
         if found:
@@ -21,7 +26,7 @@ def reqs_versions(osparc_simcore_root_dir: Path) -> List[Tuple[Path, str]]:
 
 
 @pytest.fixture(scope="module")
-def ci_setup_version(osparc_simcore_root_dir: Path) -> Tuple[Path, str]:
+def docker_compose_in_ci_script(osparc_simcore_root_dir: Path) -> Tuple[Path, str]:
     setup_ci = osparc_simcore_root_dir / "ci/github/helpers/setup_docker_compose.bash"
 
     found = re.search(r"DOCKER_COMPOSE_VERSION=\"([\d\.]+)\"", setup_ci.read_text())
@@ -31,11 +36,13 @@ def ci_setup_version(osparc_simcore_root_dir: Path) -> Tuple[Path, str]:
     return (setup_ci, version)
 
 
-def test_all_docker_compose_same_version(
-    reqs_versions, ci_setup_version, osparc_simcore_root_dir
+def test_all_docker_compose_have_same_version(
+    docker_compose_in_requirement_files,
+    docker_compose_in_ci_script,
+    osparc_simcore_root_dir,
 ):
-    previous = reqs_versions[0]
-    for req_path, docker_compose_version in reqs_versions:
+    previous = docker_compose_in_requirement_files[0]
+    for req_path, docker_compose_version in docker_compose_in_requirement_files:
         print(
             str(req_path.relative_to(osparc_simcore_root_dir)),
             "->",
@@ -45,10 +52,10 @@ def test_all_docker_compose_same_version(
         previous = (req_path, docker_compose_version)
 
     print(
-        str(ci_setup_version[0].relative_to(osparc_simcore_root_dir)),
+        str(docker_compose_in_ci_script[0].relative_to(osparc_simcore_root_dir)),
         "->",
-        ci_setup_version[1],
+        docker_compose_in_ci_script[1],
     )
     assert (
-        ci_setup_version[1] == previous[1]
-    ), f"CI installs {ci_setup_version[1]}=!{previous[1]} (see {ci_setup_version[0]}"
+        docker_compose_in_ci_script[1] == previous[1]
+    ), f"CI installs {docker_compose_in_ci_script[1]}=!{previous[1]} (see {docker_compose_in_ci_script[0]}"
