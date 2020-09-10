2693b5de896d6bd1293a69777287621e5fc80086
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-09-10 17:42:44 +0200

    adds tests on used python versions

--- tests/environment-setup/test_used_python.py
+++ tests/environment-setup/test_used_python.py
@@ -4,17 +4,39 @@
 
 
 import re
+import sys
 from pathlib import Path
 from typing import List, Tuple
 
-import pytest
 
+import pytest
 
 PIP_INSTALL_UPGRADE_PATTERN = re.compile(
     r"pip .* install\s+--upgrade .* pip([=~><]+)([\d\.]+)", re.DOTALL
 )
 
 PYTHON_VERSION_DOCKER_PATTERN = re.compile(r"ARG PYTHON_VERSION=\"([\d\.]+)\"")
+
+# TODO: enhance version comparison with from packaging.version from setuptools
+
+def to_version(version: str) -> Tuple[int]:
+    return tuple(int(v) for v in version.split("."))
+
+
+def to_str(version: Tuple[int]) -> str:
+    return ".".join(map(str, version))
+
+
+def make_versions_comparable(*versions) -> List[Tuple[int]]:
+    vers = []
+    n = 10000
+    for v in versions:
+        if isinstance(v, str):
+            v = to_version(v)
+        if len(v) < n:
+            n = len(v)
+        vers.append(v)
+    return [v[:n] for v in vers]
 
 
 @pytest.fixture(scope="session")
@@ -34,6 +56,11 @@ def expected_pip_version(osparc_simcore_root_dir: Path) -> str:
 
 
 @pytest.fixture(scope="session")
+def expected_python_version() -> Tuple[int]:
+    return (3, 6)
+
+
+@pytest.fixture(scope="session")
 def pip_in_dockerfiles(osparc_simcore_root_dir: Path) -> List[Tuple[Path, str]]:
     res = []
     for dockerfile_path in osparc_simcore_root_dir.rglob("Dockerfile"):
@@ -48,17 +75,50 @@ def pip_in_dockerfiles(osparc_simcore_root_dir: Path) -> List[Tuple[Path, str]]:
             )
         res.append((dockerfile_path, version))
 
+    assert res
     return res
 
 
-
-def test_all_image_use_same_python_version():
+@pytest.fixture(scope="session")
+def python_in_dockerfiles(osparc_simcore_root_dir: Path) -> List[Tuple[Path, str]]:
+    res = []
     for dockerfile_path in osparc_simcore_root_dir.rglob("Dockerfile"):
+        found = PYTHON_VERSION_DOCKER_PATTERN.search(dockerfile_path.read_text())
+        if found:
+            version = found.group(1)
+            print(
+                str(dockerfile_path.relative_to(osparc_simcore_root_dir)),
+                "->",
+                version,
+            )
+            res.append((dockerfile_path, version))
+    assert res
+    return res
 
+
+def test_all_image_use_same_python_version(
+    python_in_dockerfiles, expected_python_version
+):
+    for dockerfile, python_version in python_in_dockerfiles:
+        current_version, expected_version = make_versions_comparable(
+            python_version, expected_python_version
+        )
+        assert (
+            current_version == expected_version
+        ), f"Expected python {expected_python_version} in {dockerfile}, got {python_version}"
+
+
+def test_running_python_version(expected_python_version):
+    current_version, expected_version = make_versions_comparable(
+        sys.version_info, expected_python_version
+    )
+    assert (
+        current_version == expected_version
+    ), f"Expected python {to_str(sys.version_info)} installed, got {to_str(expected_python_version)}"
 
 
 def test_all_pip_have_same_version(expected_pip_version, pip_in_dockerfiles):
     for dockerfile, pip_version in pip_in_dockerfiles:
         assert (
             pip_version == expected_pip_version
-        ), f"Expected {expected_pip_version} in {dockerfile}, got {pip_version}"
+        ), f"Expected pip {expected_pip_version} in {dockerfile}, got {pip_version}"
