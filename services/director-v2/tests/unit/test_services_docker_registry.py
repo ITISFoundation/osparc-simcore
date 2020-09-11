c5f177228019e59f3210117111e9e10bb9b81bd6
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2020-09-12 01:32:39 +0200

    Prototype for testing docker-registry service

---
+++ services/director-v2/tests/unit/test_services_docker_registry.py
@@ -0,0 +1,65 @@
+# pylint:disable=unused-variable
+# pylint:disable=unused-argument
+# pylint:disable=redefined-outer-name
+# pylint:disable=protected-access
+
+from fastapi import FastAPI
+
+import pytest
+from simcore_service_director_v2.core.settings import AppSettings, RegistrySettings
+from simcore_service_director_v2.core.application import init_app
+from starlette.testclient import TestClient
+
+
+@pytest.fixture
+def minimal_app(loop, devel_environ) -> FastAPI:
+
+    # avoid init of pg or director API clients
+    settings = AppSettings(
+        registry=RegistrySettings(enabled=False),
+    )
+    app = init_app(settings)
+
+    # NOTE: this way we ensure the events are run in the application
+    # since it starts the app on a test server
+    with TestClient(app):
+        yield app
+
+
+import respx
+
+
+@pytest.fixture
+def mocked_registry_service_api(minimal_app):
+
+
+    with respx.mock(
+        base_url=minimal_app.state.settings.docker_registry.base_url,
+        assert_all_called=False,
+        assert_all_mocked=True,
+    ) as respx_mock:
+        # lists images catalog
+        respx_mock.get(
+            "_catalog",
+            content={"repositories": ["/simcore/services/comp/itis/sleeper"]},
+            alias="catalog",
+        )
+        # lists tags of sleeper
+        respx_mock.get(
+            "/simcore/services/comp/itis/sleeper/tags/list?n=50",
+            content={"tags": ["1.0", "2.0"]},
+            alias="sleeper-tags",
+        )
+        yield respx_mock
+
+
+async def test_docker_registry_client(minimal_app, mocked_registry_service_api):
+    registry_api = minimal_app.state.registry_api
+
+    catalog = await registry_api.list_repositories()
+    assert catalog
+    assert mocked_registry_service_api["catalog"].called
+
+    tags = await registry_api.list_image_tags(image_key="services/comp/itis/sleeper")
+    assert tags == ["1.0", "2.0"]
+    assert mocked_registry_service_api["sleeper-tags"].called
