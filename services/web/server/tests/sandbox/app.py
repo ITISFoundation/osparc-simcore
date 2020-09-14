aabf63428daa89c34f81ba072d50f6b9155e61ae
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-09-14 15:21:16 +0200

    Trials with different products

--- services/web/server/tests/sandbox/app.py
+++ services/web/server/tests/sandbox/app.py
@@ -1,7 +1,7 @@
 from aiohttp import web
 import json
 from pathlib import Path
-
+from multidict import MultiDict
 
 client_folder = Path.home() / "devp/osparc-simcore/services/web/client"
 
@@ -13,41 +13,79 @@ client_outdir = client_folder / target["outputPath"]
 # ----
 
 routes = web.RouteTableDef()
-
 client_apps = [ capp["name"] for capp in client_info["applications"]]
 
+default_app_name = next( capp["name"] for capp in client_info["applications"] if capp["default"] )
 
-#@routes.get("/")
-async def default_app(request):
-    index_path = client_outdir / client_app / "index.html"
-    return web.Response(text=index_path.read_text(), content_type="text/html")
+PRODUCT_NAME_HEADER = "X-Simcore-Products-Name"
 
 
-@routes.get("/v0/{name}")
+
+#
+import json
+(client_outdir / "resource" / "statics.json").write_text(json.dumps({"appName": "demo"}))
+
+
+
+
+#
+# http://localhost:8080/
+#
+# http://localhost:8080/osparc/index.html#
+# http://localhost:8080/s4l/index.html#
+# http://localhost:8080/tis/index.html#
+
+#
+# http://localhost:8080/explorer/index.html#
+# http://localhost:8080/apiviewer/index.html#
+# http://localhost:8080/testtapper/index.html#
+
+
+## MAIN ENTRYPOINT #############################
+@routes.get("/")
+async def serve_default_app(request):
+    # TODO: check url and defined what is the default??
+    print("Request from", request.headers['Host'])
+    target_product = "s4l" # default_app_name
+
+    raise web.HTTPFound(f"/{target_product}/index.html#")
+
+
+
+
+## API ###################################
+@routes.get("/v0/")
 async def get_info(request):
-    name = request.match_info["name"]
-    for a in client_info["applications"]:
-        if a["name"] == name:
-            return web.json_response(a)
-    return web.Response(text=f"I know nothing about {name}")
+    for key in request.headers:
+        print(f"{key:5s}:", request.headers[key])
 
+    #name = request.headers[PRODUCT_NAME_HEADER]
+    #print("Product", name)
+
+    return web.json_response( { k:str(v) for k,v in request.headers.items() }, headers=MultiDict({PRODUCT_NAME_HEADER: default_app_name}) )
+
+
+
+#####################################
+
+base_path = "http://localhost:8080"
+print(f"{base_path}/")
 for name in client_apps:
-    print(f"http://localhost:8080/{name}/index.html#")
+    print(f"{base_path}/{name}/index.html#")
 
 for name in client_apps + ["resource", "transpiled"]:
     folder = client_outdir / name
     assert folder.exists()
-    print(folder)
     routes.static(f"/{folder.name}", folder, show_index=True, follow_symlinks=True)
 
 
-# routes = [web.get('/v0/'), ]
+
 
 app = web.Application()
 app.add_routes(routes)
 
-print(routes, "-"*10)
-for resource in app.router.resources():
-    print(resource)
+#print(routes, "-"*10)
+#for resource in app.router.resources():
+#    print(resource)
 
 web.run_app(app)
