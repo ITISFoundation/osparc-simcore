917043782738a8b56096f8d80f49e30f45f98d94
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-09-15 14:09:14 +0200

    updates

--- services/web/server/src/simcore_service_webserver/products.py
+++ services/web/server/src/simcore_service_webserver/products.py
@@ -12,7 +12,7 @@ from .constants import RQ_PRODUCT_KEY
 log = logging.getLogger(__name__)
 
 # TODO: <--- This is defined by services/web/client/compile.json
-FE_APPS = ["osparc", "s4l", "tis", "explorer", "apiviewer", "testtapper"]
+FE_APPS = ["osparc", "s4l", "tis"]
 DEFAULT_FE_APP = FE_APPS[0]
 
 PRODUCT_PATH_RE = re.compile(r"^/(" + "|".join(FE_APPS) + r")/index.html")
