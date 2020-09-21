1cd919487ae314f891fe6c6e1edcb552cc4224b7
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-09-21 13:44:29 +0200

    Splits webserver in travis

--- ci/travis/unit-testing/webserver.bash
+++ ci/travis/unit-testing/webserver.bash
@@ -6,54 +6,88 @@ IFS=$'\n\t'
 FOLDER_CHECKS=(api/ webserver packages/ services/web .travis.yml)
 
 before_install() {
-    if bash ci/travis/helpers/test-for-changes.bash "${FOLDER_CHECKS[@]}";
-    then
-        bash ci/travis/helpers/update-docker.bash
-        bash ci/travis/helpers/install-docker-compose.bash
-        bash ci/helpers/show_system_versions.bash
-    fi
+  if bash ci/travis/helpers/test-for-changes.bash "${FOLDER_CHECKS[@]}"; then
+    bash ci/travis/helpers/update-docker.bash
+    bash ci/travis/helpers/install-docker-compose.bash
+    bash ci/helpers/show_system_versions.bash
+  fi
 }
 
 install() {
-    if bash ci/travis/helpers/test-for-changes.bash "${FOLDER_CHECKS[@]}";
-    then
-        bash ci/helpers/ensure_python_pip.bash
-        pushd services/web/server; pip3 install -r requirements/ci.txt; popd;
-    fi
+  if bash ci/travis/helpers/test-for-changes.bash "${FOLDER_CHECKS[@]}"; then
+    bash ci/helpers/ensure_python_pip.bash
+    pushd services/web/server
+    pip3 install -r requirements/ci.txt
+    popd
+  fi
 }
 
 before_script() {
-    if bash ci/travis/helpers/test-for-changes.bash "${FOLDER_CHECKS[@]}";
-    then
-        pip list -v
-    fi
+  if bash ci/travis/helpers/test-for-changes.bash "${FOLDER_CHECKS[@]}"; then
+    pip list -v
+  fi
 }
 
-script() {
-    if bash ci/travis/helpers/test-for-changes.bash "${FOLDER_CHECKS[@]}";
-    then
-        pytest --cov=simcore_service_webserver --durations=10 --cov-append \
-                --color=yes --cov-report=term-missing --cov-report=xml --cov-config=.coveragerc \
-                -v -m "not travis" services/web/server/tests/unit
-    else
-        echo "No changes detected. Skipping unit-testing of webserver."
-    fi
+test_isolated() {
+  if bash ci/travis/helpers/test-for-changes.bash "${FOLDER_CHECKS[@]}"; then
+
+    pytest --cov=simcore_service_webserver --durations=10 --cov-append \
+      --color=yes --cov-report=term-missing --cov-report=xml --cov-config=.coveragerc \
+      -v -m "not travis" services/web/server/tests/unit/isolated
+  else
+    echo "No changes detected. Skipping unit-testing of webserver."
+  fi
+
+}
+
+test_with_db_slow() {
+  if bash ci/travis/helpers/test-for-changes.bash "${FOLDER_CHECKS[@]}"; then
+
+    pytest --cov=simcore_service_webserver --durations=10 --cov-append \
+      --color=yes --cov-report=term-missing --cov-report=xml --cov-config=.coveragerc \
+      -v -m "not travis" services/web/server/tests/unit/with_dbs/slow
+  else
+    echo "No changes detected. Skipping unit-testing of webserver."
+  fi
+
+}
+
+test_with_db_medium() {
+  if bash ci/travis/helpers/test-for-changes.bash "${FOLDER_CHECKS[@]}"; then
+
+    pytest --cov=simcore_service_webserver --durations=10 --cov-append \
+      --color=yes --cov-report=term-missing --cov-report=xml --cov-config=.coveragerc \
+      -v -m "not travis" services/web/server/tests/unit/with_dbs/medium
+  else
+    echo "No changes detected. Skipping unit-testing of webserver."
+  fi
+
+}
+
+test_with_db_fast() {
+  if bash ci/travis/helpers/test-for-changes.bash "${FOLDER_CHECKS[@]}"; then
+
+    pytest --cov=simcore_service_webserver --durations=10 --cov-append \
+      --color=yes --cov-report=term-missing --cov-report=xml --cov-config=.coveragerc \
+      -v -m "not travis" services/web/server/tests/unit/with_dbs/fast
+  else
+    echo "No changes detected. Skipping unit-testing of webserver."
+  fi
+
 }
 
 after_success() {
-    if bash ci/travis/helpers/test-for-changes.bash "${FOLDER_CHECKS[@]}";
-    then
-        coveralls
-    fi
+  if bash ci/travis/helpers/test-for-changes.bash "${FOLDER_CHECKS[@]}"; then
+    coveralls
+  fi
 }
 
 after_failure() {
-    echo "failure... you can always write something more interesting here..."
+  echo "failure... you can always write something more interesting here..."
 }
 
 # Check if the function exists (bash specific)
-if declare -f "$1" > /dev/null
-then
+if declare -f "$1" >/dev/null; then
   # call arguments verbatim
   "$@"
 else
