ff482b3fcf50920834a4e25211d5df82871017bb
odeimaiz <maiz@itis.swiss>
2020-09-30 11:28:14 +0200

    Add template name to screenshots

--- tests/e2e/portal/2D_Plot.js
+++ tests/e2e/portal/2D_Plot.js
@@ -15,10 +15,9 @@ const screenshotPrefix = "2DPlot_";
 
 
 async function runTutorial () {
-  const tutorial = new tutorialBase.TutorialBase(anonURL);
+  const tutorial = new tutorialBase.TutorialBase(anonURL, screenshotPrefix);
 
   tutorial.startScreenshooter();
-
   const page = await tutorial.beforeScript();
   const studyData = await tutorial.openStudyLink();
 
