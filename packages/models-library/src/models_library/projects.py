379659c865b287f55c1b3ddee6a8956325c4cdc9
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-10-05 09:09:23 +0200

    added models library python package

---
+++ packages/models-library/src/models_library/projects.py
@@ -0,0 +1,210 @@
+import sys
+from enum import Enum
+from pathlib import Path
+from typing import Dict, List, Optional, Union
+
+from pydantic import UUID4, BaseModel, EmailStr, Extra, Field, HttpUrl, constr
+
+from .services import KEY_RE, PROPERTY_KEY_RE, VERSION_RE
+
+current_file = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve()
+
+DATE_RE = r"\d{4}-(12|11|10|0?[1-9])-(31|30|[0-2]?\d)T(2[0-3]|1\d|0?[0-9])(:(\d|[0-5]\d)){2}(\.\d{3})?Z"
+
+
+class Connection(BaseModel):
+    nodeUuid: Optional[str]
+    output: Optional[str]
+
+
+class FilePickerOutput(BaseModel):
+    store: Union[str, int]  # simcore/datcore
+    dataset: Optional[str]
+    path: str
+    label: str  # name of the file
+
+
+class AccessEnum(str, Enum):
+    ReadAndWrite = "ReadAndWrite"
+    Invisible = "Invisible"
+    ReadOnly = "ReadOnly"
+
+
+class Position(BaseModel):
+    x: int
+    y: int
+
+    class Config:
+        extra = Extra.forbid
+
+
+InputTypes = Union[int, bool, str, float, Connection, FilePickerOutput]
+OutputTypes = Union[int, bool, str, float, FilePickerOutput]
+InputID = constr(regex=PROPERTY_KEY_RE)
+OutputID = InputID
+
+
+class Node(BaseModel):
+    key: str = Field(
+        ...,
+        description="distinctive name for the node based on the docker registry path",
+        regex=KEY_RE,
+        example=[
+            "simcore/services/comp/sleeper",
+            "simcore/services/dynamic/3dviewer",
+            "simcore/services/frontend/file-picker",
+        ],
+    )
+    version: str = Field(
+        ...,
+        description="semantic version number of the node",
+        regex=VERSION_RE,
+        example=["1.0.0", "0.0.1"],
+    )
+    label: str = Field(...)
+    progress: float = Field(..., ge=0, le=100, description="the node progress value")
+    thumbnail: Optional[HttpUrl] = Field(
+        None,
+        description="url of the latest screenshot of the node",
+        example=["https://placeimg.com/171/96/tech/grayscale/?0.jpg"],
+    )
+
+    inputs: Optional[Dict[InputID, InputTypes]] = Field(
+        ..., description="values of input properties"
+    )
+    inputAccess: Optional[Dict[InputID, AccessEnum]] = Field(
+        ..., description="map with key - access level pairs"
+    )
+    inputNodes: Optional[List[UUID4]] = Field(
+        ...,
+        description="node IDs of where the node is connected to",
+        example=["nodeUuid1", "nodeUuid2"],
+    )
+
+    outputs: Optional[Dict[OutputID, OutputTypes]] = None
+    outputNode: Optional[bool] = Field(None, deprecated=True)
+    outputNodes: Optional[List[UUID4]] = Field(
+        ...,
+        description="Used in group-nodes. Node IDs of those connected to the output",
+        example=["nodeUuid1", "nodeUuid2"],
+    )
+
+    parent: Optional[UUID4] = Field(
+        None,
+        description="Parent's (group-nodes') node ID s.",
+        example=["nodeUUid1", "nodeUuid2"],
+    )
+
+    position: Position = Field(...)
+
+    class Config:
+        extra = Extra.forbid
+
+
+class AccessRights(BaseModel):
+    read: bool = Field(..., description="gives read access")
+    write: bool = Field(..., description="gives write access")
+    delete: bool = Field(..., description="gives deletion rights")
+
+    class Config:
+        extra = Extra.forbid
+
+
+GroupID = constr(regex=r"^\d+$")
+NodeID = constr(regex=r"^\d+$")
+ClassifierID = str
+
+
+class Project(BaseModel):
+    uuid: UUID4 = Field(
+        ...,
+        description="project unique identifier",
+        example=[
+            "07640335-a91f-468c-ab69-a374fa82078d",
+            "9bcf8feb-c1b1-41b6-b201-639cd6ccdba8",
+        ],
+    )
+    name: str = Field(
+        ..., description="project name", example=["Temporal Distortion Simulator"]
+    )
+    description: str = Field(
+        ...,
+        description="longer one-line description about the project",
+        example=["Dabbling in temporal transitions ..."],
+    )
+    prjOwner: EmailStr = Field(..., description="user email")
+    accessRights: Dict[GroupID, AccessRights] = Field(
+        ...,
+        description="object containing the GroupID as key and read/write/execution permissions as value",
+        additionalProperties=False,
+    )
+    creationDate: str = Field(
+        ...,
+        regex=DATE_RE,
+        description="project creation date",
+        example=["2018-07-01T11:13:43Z"],
+    )
+    lastChangeDate: str = Field(
+        ...,
+        regex=DATE_RE,
+        description="last save date",
+        example=["2018-07-01T11:13:43Z"],
+    )
+    thumbnail: HttpUrl = Field(
+        ...,
+        description="url of the project thumbnail",
+        example=["https://placeimg.com/171/96/tech/grayscale/?0.jpg"],
+    )
+    workbench: Dict[NodeID, Node]
+    tags: Optional[List[int]] = Field(None)
+    classifiers: Optional[List[ClassifierID]] = Field(
+        None,
+        description="Contains the reference to the project classifiers",
+        example=["some:id:to:a:classifier"],
+    )
+    dev: Optional[Dict] = Field(
+        None, description="object used for development purposes only"
+    )
+
+    class Config:
+        description = "Description of a simcore project"
+        title = "simcore project"
+        extra = Extra.forbid
+
+
+class Owner(BaseModel):
+    first_name: str
+    last_name: str
+
+
+class ProjectLocked(BaseModel):
+    value: bool
+    owner: Optional[Owner]
+
+
+class RunningState(str, Enum):
+    unknown = "UNKNOWN"
+    not_started = "NOT_STARTED"
+    pending = "PENDING"
+    started = "STARTED"
+    retrying = "RETRY"
+    success = "SUCCESS"
+    failure = "FAILURE"
+
+
+class ProjectRunningState(BaseModel):
+    value: RunningState
+
+
+class ProjectState(BaseModel):
+    locked: ProjectLocked
+    state: ProjectRunningState
+
+
+__all__ = [
+    "ProjectState",
+    "ProjectRunningState",
+    "ProjectLocked",
+    "RunningState",
+    "Owner",
+]
