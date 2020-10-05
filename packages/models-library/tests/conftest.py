e7f51866d46a2d65a36094c4a9142134f348b114
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-10-05 10:43:56 +0200

    review @pcrespov removed duplicates

--- packages/models-library/tests/conftest.py
+++ packages/models-library/tests/conftest.py
@@ -18,25 +18,10 @@ pytest_plugins = [
 
 
 @pytest.fixture(scope="session")
-def here():
-    return Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
-
-
-@pytest.fixture(scope="session")
 def package_dir():
     pdir = Path(models_library.__file__).resolve().parent
     assert pdir.exists()
     return pdir
-
-
-@pytest.fixture(scope="session")
-def osparc_simcore_root_dir(here):
-    root_dir = here.parent.parent.parent.resolve()
-    assert root_dir.exists(), "Is this service within osparc-simcore repo?"
-    assert any(root_dir.glob("packages/models-library")), (
-        "%s not look like rootdir" % root_dir
-    )
-    return root_dir
 
 
 @pytest.fixture(scope="session")
