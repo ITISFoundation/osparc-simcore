635ce738e3389cb65b75ed9b9cf673f73ed614fd
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-10-05 18:04:01 +0200

    logs adquire and release of db connections

--- services/catalog/src/simcore_service_catalog/api/dependencies/database.py
+++ services/catalog/src/simcore_service_catalog/api/dependencies/database.py
@@ -1,3 +1,4 @@
+import logging
 from typing import AsyncGenerator, Callable, Type
 
 from aiopg.sa import Engine
@@ -5,6 +6,8 @@ from fastapi import Depends
 from fastapi.requests import Request
 
 from ...db.repositories import BaseRepository
+
+logger = logging.getLogger(__name__)
 
 
 def _get_db_engine(request: Request) -> Engine:
@@ -15,7 +18,32 @@ def get_repository(repo_type: Type[BaseRepository]) -> Callable:
     async def _get_repo(
         engine: Engine = Depends(_get_db_engine),
     ) -> AsyncGenerator[BaseRepository, None]:
+
+        logger.debug(
+            "Acquiring pg connection from pool: current=%d, free=%d, reserved=[%d, %d]",
+            engine.size,
+            engine.freesize,
+            engine.minsize,
+            engine.maxsize,
+        )
+        if engine.freesize <= 1:
+            logger.warning(
+                "Last or no pg connection in pool: current=%d, free=%d, reserved=[%d, %d]",
+                engine.size,
+                engine.freesize,
+                engine.minsize,
+                engine.maxsize,
+            )
+
         async with engine.acquire() as conn:
             yield repo_type(conn)
+
+        logger.debug(
+            "Released pg connection: current=%d, free=%d, reserved=[%d, %d]",
+            engine.size,
+            engine.freesize,
+            engine.minsize,
+            engine.maxsize,
+        )
 
     return _get_repo
