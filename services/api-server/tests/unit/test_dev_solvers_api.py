e248b0b737f1ea14ed67eccda01c88c3f0773c74
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-10-08 10:18:46 +0200

    autoformat

--- services/api-server/tests/unit/test_dev_solvers_api.py
+++ services/api-server/tests/unit/test_dev_solvers_api.py
@@ -1,12 +1,16 @@
 # pylint:disable=unused-variable
 # pylint:disable=unused-argument
 # pylint:disable=redefined-outer-name
-
-from simcore_service_api_server.models.schemas.solvers import Solver, SolverRelease
-from simcore_service_api_server.api.routes.solvers import compose_solver_id
-
-from datetime import datetime, timedelta
+import copy
 import uuid
+from datetime import datetime, timedelta
+
+from simcore_service_api_server.api.routes.solvers import compose_solver_id
+from simcore_service_api_server.models.schemas.solvers import (
+    Solver,
+    SolverOverview,
+    SolverRelease,
+)
 
 
 def test_id_composer():
@@ -16,43 +20,41 @@ def test_id_composer():
     ##assert str(u) ==
 
 
-
 def create_solver(key):
     return Solver(
-            solver_key=key,
-            title="S4L isolve",
-            maintainer="pcrespov",
-            releases=[
-                SolverRelease(
-                    solver_id=compose_solver_id(key, "1.0.1"),
-                    version="1.0.1",
-                    version_alias = ["1", "1.0", "latest"],
-                    release_data=datetime.now(),
-                ),
-                SolverRelease(
-                    solver_id=compose_solver_id(key, "1.0.0"),
-                    version="1.0.0",
-                    release_data=datetime.now() - timedelta(days=1),
-                )
-            ],)
+        solver_key=key,
+        title="S4L isolve",
+        maintainer="pcrespov",
+        releases=[
+            SolverRelease(
+                solver_id=compose_solver_id(key, "1.0.1"),
+                version="1.0.1",
+                version_alias=["1", "1.0", "latest"],
+                release_data=datetime.now(),
+            ),
+            SolverRelease(
+                solver_id=compose_solver_id(key, "1.0.0"),
+                version="1.0.0",
+                release_data=datetime.now() - timedelta(days=1),
+            ),
+        ],
+    )
+
 
 SOLVERS = [
-        create_solver("simcore/services/comp/isolve"),
-        create_solver("simcore/services/comp/mpi-isolve")
-    ]
+    create_solver("simcore/services/comp/isolve"),
+    create_solver("simcore/services/comp/mpi-isolve"),
+]
 
 
 SOLVERS_OVERVIEW = [
-        SolverOverview(
-            latest_version= s.releases[0].version,
-            solver_url= f"http://localhost/v0/solvers/{s.releases[0].solver_id}"
-             **s.dict(include={"solver_key", "title", "maintainer"}),
-        )
-            for s in SOLVERS
-    ]
-
-
-import copy
+    SolverOverview(
+        latest_version=s.releases[0].version,
+        solver_url=f"http://localhost/v0/solvers/{s.releases[0].solver_id}"
+        ** s.dict(include={"solver_key", "title", "maintainer"}),
+    )
+    for s in SOLVERS
+]
 
 
 def test_it():
