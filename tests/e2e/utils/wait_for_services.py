892ec1ddfe81f2ad409bef058a80f0ec52d73111
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-10-09 16:13:40 +0200

    Fixes on waiting services

--- tests/e2e/utils/wait_for_services.py
+++ tests/e2e/utils/wait_for_services.py
@@ -1,10 +1,11 @@
 import logging
-from pdb import Pdb
 import sys
 import time
+from datetime import datetime
 from pathlib import Path
-from typing import List
+from pdb import Pdb
 from pprint import pformat
+from typing import Dict, List
 
 import docker
 import yaml
@@ -17,49 +18,40 @@ WAIT_TIME_SECS = 20
 RETRY_COUNT = 7
 MAX_WAIT_TIME = 240
 
-# https://docs.docker.com/engine/swarm/how-swarm-mode-works/swarm-task-states/
-pre_states = ["NEW", "PENDING", "ASSIGNED", "PREPARING", "STARTING"]
+# SEE https://docs.docker.com/engine/swarm/how-swarm-mode-works/swarm-task-states/
 
-failed_states = [
-    "COMPLETE",
-    "FAILED",
-    "SHUTDOWN",
-    "REJECTED",
-    "ORPHANED",
-    "REMOVE",
-    "CREATED",
+PRE_STATES = [
+    "new",  # The task was initialized.
+    "pending",  # Resources for the task were allocated.
+    "assigned",  # Docker assigned the task to nodes.
+    "accepted",  # The task was accepted by a worker node. If a worker node rejects the task, the state changes to REJECTED.
+    "preparing",  # Docker is preparing the task.
+    "starting",  # Docker is starting the task.
 ]
-# UTILS --------------------------------
+
+RUNNING_STATE = "running"  # The task is executing.
+
+FAILED_STATES = [
+    "complete",  # The task exited without an error code.
+    "failed",  # The task exited with an error code.
+    "shutdown",  # Docker requested the task to shut down.
+    "rejected",  # The worker node rejected the task.
+    "orphaned",  # The node was down for too long.
+    "remove",  # The task is not terminal but the associated service was removed or scaled down.
+]
 
 
 def get_tasks_summary(tasks):
     msg = ""
-    for t in tasks:
-        t["Status"].setdefault("Err", "")
-        msg += "- task ID:{ID}, STATE: {Status[State]}, ERROR: '{Status[Err]}' \n".format(
-            **t
-        )
+    for task in tasks:
+        status: Dict = task["Status"]
+        msg += f"- task ID:{task['ID']}, STATE: {status['State']}"
+        error = status.get("Err")
+        if error:
+            msg += f", ERROR: {error}"
+        msg += "\n"
+
     return msg
-
-
-def get_failed_tasks_logs(service, docker_client):
-    failed_logs = ""
-    for t in service.tasks():
-        if t["Status"]["State"].upper() in failed_states:
-            cid = t["Status"]["ContainerStatus"]["ContainerID"]
-            failed_logs += "{2} {0} - {1} BEGIN {2}\n".format(
-                service.name, t["ID"], "=" * 10
-            )
-            if cid:
-                container = docker_client.containers.get(cid)
-                failed_logs += container.logs().decode("utf-8")
-            else:
-                failed_logs += "  log unavailable. container does not exists\n"
-            failed_logs += "{2} {0} - {1} END {2}\n".format(
-                service.name, t["ID"], "=" * 10
-            )
-
-    return failed_logs
 
 
 # --------------------------------------------------------------------------------
@@ -101,51 +93,61 @@ def ops_services() -> List[str]:
 
 
 def wait_for_services() -> None:
-    # get all services
-    services = core_services() + ops_services()
+    expected_services = core_services() + ops_services()
 
     client = docker.from_env()
-    running_services = [
-        x for x in client.services.list() if x.name.split("_")[-1] in services
+    started_services = [
+        s for s in client.services.list() if s.name.split("_")[-1] in expected_services
     ]
 
-    # check all services are in
-    assert len(running_services), "no services started!"
-    assert len(services) == len(
-        running_services
-    ), f"Some services are missing or unexpected:\nexpected: {len(services)} {services}\ngot: {len(running_services)} {[service.name for service in running_services]}"
+    assert len(started_services), "no services started!"
+    assert len(expected_services) == len(started_services), (
+        f"Some services are missing or unexpected:\n"
+        "expected: {len(expected_services)} {expected_services}\n"
+        "got: {len(started_services)} {[s.name for s in started_services]}"
+    )
     # now check they are in running mode
-    for service in running_services:
-        task = None
-        for n in range(RETRY_COUNT):
-            # get last updated task
-            sorted_tasks = sorted(service.tasks(), key=lambda task: task["UpdatedAt"])
-            task = sorted_tasks[-1]
+    for service in started_services:
 
-            if task["Status"]["State"].upper() in pre_states:
+        # get last updated task
+
+        def by_updated_timestamp(task) -> datetime:
+            datetime_str = task["UpdatedAt"]
+            # datetime_str is typically '2020-10-09T12:28:14.771034099Z'
+            #  - The T separates the date portion from the time-of-day portion
+            #  - The Z on the end means UTC, that is, an offset-from-UTC
+            # The 099 before the Z is not clear, therefore we will truncate the last part
+            N = len("2020-10-09T12:28:14.771034")
+            if len(datetime_str) > N:
+                datetime_str = datetime_str[:N]
+            return datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S.%f")
+
+        task = sorted(service.tasks(), key=by_updated_timestamp)[-1]
+        assert task
+
+        # retry loop
+        for n in range(RETRY_COUNT):
+            if task["Status"]["State"] in PRE_STATES:
                 print(
-                    "Waiting [{}/{}] for {}...\n{}".format(
-                        n, RETRY_COUNT, service.name, get_tasks_summary(service.tasks())
-                    )
+                    f"Waiting [{n}/{RETRY_COUNT}] for {service.name}...\n",
+                    get_tasks_summary(service.tasks()),
                 )
                 time.sleep(WAIT_TIME_SECS)
-            elif task["Status"]["State"].upper() in failed_states:
+            elif task["Status"]["State"] in FAILED_STATES:
                 print(
-                    f"Waiting [{n}/{RETRY_COUNT}] Service {service.name} failed once...\n{get_tasks_summary(service.tasks())}"
+                    f"Waiting [{n}/{RETRY_COUNT}] for {service.name} which failed ...\n",
+                    get_tasks_summary(service.tasks()),
                 )
                 time.sleep(WAIT_TIME_SECS)
             else:
                 break
-        assert task
-        assert (
-            task["Status"]["State"].upper() == "RUNNING"
-        ), "Expected running, got \n{}\n{}".format(
-            pformat(task), get_tasks_summary(service.tasks())
+
+        assert task["Status"]["State"] == task.get(
+            "DesiredState", RUNNING_STATE
+        ), f"Expected running, got \n{pformat(task)}\n" + get_tasks_summary(
+            service.tasks()
         )
-        # get_failed_tasks_logs(service, client))
 
 
 if __name__ == "__main__":
-    # get retry parameters
-    # wait for the services
     sys.exit(wait_for_services())
