f2a8a2ecfac765819298a73d4baa5f2e596dea92
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-10-13 00:55:34 +0200

    Improves logging of db connections

--- services/api-server/src/simcore_service_api_server/api/dependencies/database.py
+++ services/api-server/src/simcore_service_api_server/api/dependencies/database.py
@@ -54,4 +54,4 @@ def get_repository(repo_type: Type[BaseRepository]) -> Callable:
         # Be very careful if you change this!!! or we will end up in the problem described in https://github.com/ITISFoundation/osparc-simcore/pull/1966
         yield repo_type(db_connection)
 
-    return _get_repo
+    return _acquire_connection_and_get_repo
