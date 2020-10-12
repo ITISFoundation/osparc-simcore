8f872ed1f01727fdd7eed3935d3794ec80b8dbe8
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-10-12 11:26:47 +0200

    use a state type in the database for easier reading

---
+++ packages/postgres-database/src/simcore_postgres_database/migration/versions/009c81406676_add_project_running_state.py
@@ -0,0 +1,120 @@
+"""add project running state
+
+Revision ID: 009c81406676
+Revises: d67672189ce1
+Create Date: 2020-10-12 08:38:40.807576+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '009c81406676'
+down_revision = 'd67672189ce1'
+branch_labels = None
+depends_on = None
+
+
+UNKNOWN = 0
+PENDING = 1
+RUNNING = 2
+SUCCESS = 3
+FAILED = 4
+
+DB_PROCEDURE_NAME: str = "notify_comp_tasks_changed"
+DB_TRIGGER_NAME: str = f"{DB_PROCEDURE_NAME}_event"
+
+def upgrade():
+
+    # create the new type
+    state_type = postgresql.ENUM('NOT_STARTED', 'PENDING', 'RUNNING', 'SUCCESS', 'FAILED', name='statetype')
+    state_type.create(op.get_bind())
+    # keep the old column for migration
+    op.alter_column('comp_pipeline', 'state', new_column_name='deprecated_state')
+    op.alter_column('comp_tasks', 'state', new_column_name='deprecated_state')
+    # add the new columns
+    op.add_column('comp_pipeline', sa.Column('state', sa.Enum('NOT_STARTED', 'PENDING', 'RUNNING', 'SUCCESS', 'FAILED', name='statetype'), nullable=False, server_default="NOT_STARTED"))
+    op.add_column('comp_tasks', sa.Column('state', sa.Enum('NOT_STARTED', 'PENDING', 'RUNNING', 'SUCCESS', 'FAILED', name='statetype'), nullable=False, server_default="NOT_STARTED"))
+
+    # migrate from deprecated state to state
+    migration_map = {UNKNOWN: "NOT_STARTED", PENDING:"PENDING", RUNNING: "RUNNING", SUCCESS: "SUCCESS", FAILED: "FAILED"}
+    [op.execute(
+        sa.DDL(f"""
+UPDATE comp_pipeline
+SET state='{new}'
+WHERE comp_pipeline.deprecated_state = '{old}'
+""")) for old,new in migration_map.items()
+    ]
+    [op.execute(
+        sa.DDL(f"""
+UPDATE comp_tasks
+SET state='{new}'
+WHERE comp_tasks.deprecated_state = '{old}'
+""")) for old,new in migration_map.items()
+    ]
+
+    # replace trigger to remove dependency on old state
+    replace_trigger_query = sa.DDL(
+        f"""
+DROP TRIGGER IF EXISTS {DB_TRIGGER_NAME} on comp_tasks;
+CREATE TRIGGER {DB_TRIGGER_NAME}
+AFTER UPDATE OF outputs,state ON comp_tasks
+    FOR EACH ROW
+    WHEN ((OLD.outputs::jsonb IS DISTINCT FROM NEW.outputs::jsonb OR OLD.state IS DISTINCT FROM NEW.state)
+        AND NEW.node_class <> 'FRONTEND')
+    EXECUTE PROCEDURE {DB_PROCEDURE_NAME}();
+"""
+    )
+    op.execute(replace_trigger_query)
+    # remove the old columns
+    op.drop_column('comp_tasks', 'deprecated_state')
+    op.drop_column('comp_pipeline', 'deprecated_state')
+
+
+
+
+
+
+def downgrade():
+    # rename columns
+    op.alter_column('comp_pipeline', 'state', new_column_name='deprecated_state')
+    op.alter_column('comp_tasks', 'state', new_column_name='deprecated_state')
+    # create the old columns again
+    op.add_column('comp_pipeline', sa.Column('state', sa.String(), nullable=True))
+    op.add_column('comp_tasks', sa.Column('state', sa.Integer(), nullable=True))
+    # migrate the columns
+    migration_map = {UNKNOWN: "NOT_STARTED", PENDING:"PENDING", RUNNING: "RUNNING", SUCCESS: "SUCCESS", FAILED: "FAILED"}
+    [op.execute(
+        sa.DDL(f"""
+UPDATE comp_pipeline
+SET state='{old}'
+WHERE comp_pipeline.deprecated_state = '{new}'
+""")) for old,new in migration_map.items()
+    ]
+    [op.execute(
+        sa.DDL(f"""
+UPDATE comp_tasks
+SET state='{old}'
+WHERE comp_tasks.deprecated_state = '{new}'
+""")) for old,new in migration_map.items()
+    ]
+    # replace trigger to remove dependency on old state
+    replace_trigger_query = sa.DDL(
+        f"""
+DROP TRIGGER IF EXISTS {DB_TRIGGER_NAME} on comp_tasks;
+CREATE TRIGGER {DB_TRIGGER_NAME}
+AFTER UPDATE OF outputs,state ON comp_tasks
+    FOR EACH ROW
+    WHEN ((OLD.outputs::jsonb IS DISTINCT FROM NEW.outputs::jsonb OR OLD.state IS DISTINCT FROM NEW.state)
+        AND NEW.node_class <> 'FRONTEND')
+    EXECUTE PROCEDURE {DB_PROCEDURE_NAME}();
+"""
+    )
+    op.execute(replace_trigger_query)
+    # drop the columns
+    op.drop_column('comp_tasks', 'deprecated_state')
+    op.drop_column('comp_pipeline', 'deprecated_state')
+
+    state_type = postgresql.ENUM('NOT_STARTED', 'PENDING', 'RUNNING', 'SUCCESS', 'FAILED', name='statetype')
+    state_type.drop(op.get_bind())
--- packages/postgres-database/src/simcore_postgres_database/models/comp_pipeline.py
+++ packages/postgres-database/src/simcore_postgres_database/models/comp_pipeline.py
@@ -1,17 +1,19 @@
 """ Computational Pipeline Table
 
 """
+import enum
 import uuid
 
 import sqlalchemy as sa
 
 from .base import metadata
 
-UNKNOWN = 0
-PENDING = 1
-RUNNING = 2
-SUCCESS = 3
-FAILED = 4
+class StateType(enum.Enum):
+    NOT_STARTED = "NOT_STARTED"
+    PENDING = "PENDING"
+    RUNNING = "RUNNING"
+    SUCCESS = "SUCCESS"
+    FAILED = "FAILED"
 
 
 def _new_uuid():
@@ -23,5 +25,10 @@ comp_pipeline = sa.Table(
     metadata,
     sa.Column("project_id", sa.String, primary_key=True, default=_new_uuid),
     sa.Column("dag_adjacency_list", sa.JSON),
-    sa.Column("state", sa.String, default=UNKNOWN),  # TODO: MaG should not string??
+    sa.Column(
+        "state",
+        sa.Enum(StateType),
+        nullable=False,
+        server_default=StateType.NOT_STARTED.value,
+    ),
 )
