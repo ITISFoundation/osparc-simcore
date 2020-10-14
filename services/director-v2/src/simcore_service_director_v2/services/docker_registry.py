f438787c055e8d1032dfb8c6f3c835a85481bb24
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-10-14 22:02:56 +0200

    dc

--- services/director-v2/src/simcore_service_director_v2/services/docker_registry.py
+++ services/director-v2/src/simcore_service_director_v2/services/docker_registry.py
@@ -9,15 +9,16 @@ from typing import List
 
 from fastapi import FastAPI
 from httpx import AsyncClient
+import logging
 
 from ..core.settings import RegistrySettings
 
 
+logger = logging.getLogger(__name__)
+
 def setup_docker_registry(app: FastAPI) -> None:
     settings: RegistrySettings = app.state.settings.registry
-
-    # adds client to access Registry API
-    app.state.docker_registry_api = RegistryApiClient(settings.api_url)
+    app.state.docker_registry_api = RegistryApiClient(settings)
 
 
 async def shutdown_docker_registry(app: FastAPI) -> None:
@@ -31,11 +32,33 @@ async def shutdown_docker_registry(app: FastAPI) -> None:
 
 
 class RegistryApiClient:
-    def __init__(self, api_url):
-        self.client = AsyncClient(base_url=api_url)
+    """
 
-    async def list_repositories() -> List[str]:
-        pass
+        Basic Authentication or Bearer
+    """
+
+    def __init__(self, settings: RegistrySettings):
+        self.settings = settings.copy()
+
+        # TODO: add auth https://www.python-httpx.org/advanced/#customizing-authentication
+        # TODO: see https://colin-b.github.io/httpx_auth/
+
+        self.client = AsyncClient(base_url=self.settings.api_url(with_credentials=False))
+
+    def get_basic_auth(self):
+        auth=(self.settings.user, self.settings.pw.get_secret_value())
+        return auth
+
+
+    async def list_repositories(self, number_of_retrieved_repos=50) -> List[str]:
+
+        while True:
+            r = await client.get("_catalog", params={"n":number_of_retrieved_repos})
+            body = r.json()
+            if body.get("repositories"):
+                repos.extend()
+
+
 
     async def list_image_tags() -> List[str]:
         pass
