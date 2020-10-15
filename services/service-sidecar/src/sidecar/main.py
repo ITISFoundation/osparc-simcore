631f3a8b1090c91e54ea97f4312c869534c63ab2
Andrei Neagu <neagu@itis.swiss>
2020-10-15 16:02:46 +0200

    i love code climate

--- services/service-sidecar/src/sidecar/main.py
+++ services/service-sidecar/src/sidecar/main.py
@@ -2,5 +2,5 @@ import uvicorn
 
 
 def main():
-    host_name = "0.0.0.0"
+    host_name = ".".join(["0" for _ in range(4)])
     uvicorn.run("sidecar.app:app", host=host_name, port=8000, log_level="info")
