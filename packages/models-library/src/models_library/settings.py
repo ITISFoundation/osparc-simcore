80a3901cacdc70f7317798d3bea50446db33fe1e
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2020-10-17 23:44:40 +0200

    Common settings tmp in models-library

---
+++ packages/models-library/src/models_library/settings.py
@@ -0,0 +1,51 @@
+# pylint: disable=no-self-argument
+# pylint: disable=no-self-use
+from typing import Optional
+
+from pydantic import BaseSettings, PostgresDsn, SecretStr, conint, constr, validator
+
+PortInt = conint(gt=0, lt=65535)
+VersionTag = constr(regex=r"^v\d$")
+
+
+class PostgresSettings(BaseSettings):
+    dns: Optional[PostgresDsn] = None
+
+    # entrypoint
+    host: str
+    port: PortInt = 5432
+
+    # auth
+    user: str
+    password: SecretStr
+
+    # database
+    db: str
+
+    # pool connection limits
+    minsize: int = 10
+    maxsize: int = 10
+
+    @validator("maxsize")
+    def check_size(cls, v, values):
+        if not (values["minsize"] <= v):
+            raise ValueError(f"assert minsize={values['minsize']} <= maxsize={v}")
+        return v
+
+    @property
+    def postgres_dsn(self) -> str:
+        if self.dns:
+            return str(self.dns)
+        else:
+            return PostgresDsn.build(
+                scheme="postgresql",
+                user=self.user,
+                password=self.password.get_secret_value(),
+                host=self.host,
+                port=self.port,
+                path=f"/{self.db}",
+            )
+
+    class Config:
+        case_sensitive = False
+        env_prefix = "POSTGRES_"
