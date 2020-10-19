174dc005f63daae02e9aa7f41d5b2b52d55f0317
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-10-19 14:08:40 +0200

    Adds separated layers to control retries and errors

--- services/director-v2/src/simcore_service_director_v2/services/client_utils.py
+++ services/director-v2/src/simcore_service_director_v2/services/client_utils.py
@@ -5,19 +5,41 @@ from typing import Coroutine
 import httpx
 from fastapi import HTTPException
 from starlette import status
-from tenacity import retry, stop_after_attempt, wait_fixed
+from tenacity import (
+    before_sleep_log,
+    retry,
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_fixed,
+)
 
-logger = logging.getLogger(__name__)
+
+def retry_middleware(logger: logging.Logger):
+    return retry(
+        # SEE https://www.python-httpx.org/exceptions/
+        retry=retry_if_exception_type((httpx.TimeoutException, httpx.NetworkError)),
+        wait=wait_fixed(2),
+        stop=stop_after_attempt(3),
+        reraise=True,
+        before_sleep=before_sleep_log(logger, logging.DEBUG),
+    )
 
 
-retry_handler = retry(wait=wait_fixed(2), stop=stop_after_attempt(3))
+def errors_middleware(service_name: str, logger: logging.Logger):
+    def decorator_func(request_func: Coroutine):
+        @functools.wraps(request_func)
+        async def wrapper_func(*args, **kwargs) -> httpx.Response:
+            try:
+                resp: httpx.Response = await request_func(*args, **kwargs)
 
-
-async def error_handler(request_func: Coroutine):
-    @functools.wraps(request_func)
-    async def wrapper(*args, **kwargs) -> httpx.Response:
-        try:
-            resp: httpx.Response = await request_func(*args, **kwargs)
+            except httpx.RequestError as err:
+                logger.error(
+                    "Failed request %s(%s, %s)", request_func.__name__, args, kwargs
+                )
+                raise HTTPException(
+                    status.HTTP_503_SERVICE_UNAVAILABLE,
+                    detail=f"{service_name} is not reponsive",
+                ) from err
 
             if httpx.codes.is_client_error(resp.status_code):
                 # Forward return clients errors
@@ -25,7 +47,8 @@ async def error_handler(request_func: Coroutine):
 
             elif httpx.codes.is_server_error(resp.status_code):  # 500<= code <=599
                 logger.error(
-                    "Director service error %d [%s]: %s",
+                    "%s service error %d [%s]: %s",
+                    service_name,
                     resp.reason_phrase,
                     resp.status_code,
                     resp.text(),
@@ -33,12 +56,8 @@ async def error_handler(request_func: Coroutine):
                 # Server errors are retured as service not available?
                 raise HTTPException(status.HTTP_503_SERVICE_UNAVAILABLE)
 
-        except Exception as err:
-            logger.exception(
-                "Failed client request %s(%s, %s)", request_func.__name__, args, kwargs
-            )
-            raise HTTPException(status.HTTP_503_SERVICE_UNAVAILABLE) from err
+            return resp
 
-        return resp
+        return wrapper_func
 
-    return wrapper
+    return decorator_func
--- services/director-v2/src/simcore_service_director_v2/services/director_v0.py
+++ services/director-v2/src/simcore_service_director_v2/services/director_v0.py
@@ -6,40 +6,49 @@ import httpx
 from fastapi import FastAPI
 
 from ..core.settings import DirectorV0Settings
-from .client_utils import error_handler, retry_handler
+from .client_utils import errors_middleware, retry_middleware
 
 logger = logging.getLogger(__name__)
 
 
 def on_start(app: FastAPI) -> None:
     settings: DirectorV0Settings = app.state.settings.director_v0
-    app.states.director_v0_client = DirectorV0Client(
+    app.state.director_v0_client = DirectorV0Client(
         client=httpx.AsyncClient(base_url=settings.api_base_url)
     )
 
 
 async def on_stop(app: FastAPI) -> None:
-    await app.state.settings.director_v0.client.aclose()
-    del app.state.settings.director_v0_client
+    await app.state.director_v0_client.client.aclose()
+    del app.state.director_v0_client
 
 
 # TODO: add this functionlity
 async def on_cleanup_context(app: FastAPI) -> Iterator[None]:
     settings: DirectorV0Settings = app.state.settings.director_v0
     async with httpx.AsyncClient(base_url=settings.api_base_url) as client:
-        app.states.director_v0_client = DirectorV0Client(client)
+        app.state.director_v0_client = DirectorV0Client(client)
 
         yield
 
-    del app.state.settings.director_v0_client
+    del app.state.director_v0_client
 
 
 @dataclass
 class DirectorV0Client:
     client: httpx.AsyncClient
 
-    @error_handler
-    @retry_handler
+    @classmethod
+    def create(cls, app: FastAPI, **kwargs):
+        app.state.director_v0_client = cls(**kwargs)
+        return cls.instance(app)
+
+    @classmethod
+    def instance(cls, app: FastAPI):
+        return app.state.director_v0_client
+
+    @errors_middleware("Director", logger)
+    @retry_middleware(logger)
     async def request(self, method: str, tail_path: str, **kwargs) -> Dict:
         resp = await self.client.request(method, tail_path, **kwargs)
         return resp.json()
