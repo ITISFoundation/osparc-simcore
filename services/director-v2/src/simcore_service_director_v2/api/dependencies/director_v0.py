666d2d9802f2f0af61f183a72b76760e47a43e51
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-10-27 18:26:46 +0100

    make dependency simpler

--- services/director-v2/src/simcore_service_director_v2/api/dependencies/director_v0.py
+++ services/director-v2/src/simcore_service_director_v2/api/dependencies/director_v0.py
@@ -1,40 +1,18 @@
 from typing import Coroutine
 
-from fastapi import HTTPException, Request, Response
-
-from starlette.datastructures import URL
+from fastapi import Depends, Request, Response
 
 from ...modules.director_v0 import DirectorV0Client
 
 
-def get_director_v0_client(request: Request, response: Response) -> DirectorV0Client:
+def get_director_v0_client(request: Request) -> DirectorV0Client:
     client = DirectorV0Client.instance(request.app)
     return client
 
 
-def get_request_to_director_v0(request: Request, response: Response) -> Coroutine:
-
-    client = DirectorV0Client.instance(request.app)
-
-    async def forward():
-        url_tail = URL(
-            path=request.url.path.replace("/v0", ""), fragment=request.url.fragment,
-        )
-        body: bytes = await request.body()
-
-        resp = await client.request(
-            request.method,
-            str(url_tail),
-            params=dict(request.query_params),
-            content=body,
-            headers=dict(request.headers),
-        )
-
-        # Prepared response
-        response.body = resp.content
-        response.status_code = resp.status_code
-
-        # NOTE: the response is NOT validated!
-        return response
-
-    return forward
+def get_request_to_director_v0(
+    request: Request,
+    response: Response,
+    director_v0_client: DirectorV0Client = Depends(get_director_v0_client),
+) -> Coroutine:
+    return director_v0_client.forward(request, response)
