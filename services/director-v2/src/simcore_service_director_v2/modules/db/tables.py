79b2a188c3cdc2c2aa05da50802d69735e6149a8
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-10-27 16:58:48 +0100

    use comp_tasks db

--- services/director-v2/src/simcore_service_director_v2/modules/db/tables.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/tables.py
@@ -1,6 +1,9 @@
 from simcore_postgres_database.models.projects import ProjectType, projects
+from simcore_postgres_database.models.comp_tasks import comp_tasks, NodeClass
 
 __all__ = [
+    "comp_tasks",
+    "NodeClass",
     "projects",
     "ProjectType",
 ]
