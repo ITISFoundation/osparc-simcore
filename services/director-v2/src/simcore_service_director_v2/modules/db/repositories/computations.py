2cf11281c82233339993d2b1d7da557f19786fce
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-10-27 16:59:35 +0100

    get comp tasks

--- services/director-v2/src/simcore_service_director_v2/modules/db/repositories/computations.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/repositories/computations.py
@@ -1,5 +1,12 @@
 import logging
+from datetime import datetime
+from typing import Dict, Tuple
 
+import sqlalchemy as sa
+from ....models.domains.comp_tasks import CompTaskAtDB
+from models_library.projects import NodeID, ProjectID, RunningState
+
+from ..tables import NodeClass, comp_tasks
 from ._base import BaseRepository
 
 logger = logging.getLogger(__name__)
@@ -10,4 +17,18 @@ class CompPipelinesRepository(BaseRepository):
 
 
 class CompTasksRepository(BaseRepository):
-    pass
+    async def get_comp_tasks(
+        self,
+        project_id: ProjectID,
+    ) -> Dict[NodeID, CompTaskAtDB]:
+        tasks: Dict[NodeID, CompTaskAtDB] = {}
+        async for row in self.connection.execute(
+            sa.select([comp_tasks]).where(
+                (comp_tasks.c.project_id == str(project_id))
+                & (comp_tasks.c.node_class == NodeClass.COMPUTATIONAL)
+            )
+        ):
+            task_db = CompTaskAtDB(**row)
+            tasks[task_db.node_id] = task_db
+
+        return tasks
