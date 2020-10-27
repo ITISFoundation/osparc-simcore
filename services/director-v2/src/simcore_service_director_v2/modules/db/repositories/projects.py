37c313e61754119a4913235b5e897286ce5a9ca1
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-10-27 16:59:01 +0100

    get projects

--- services/director-v2/src/simcore_service_director_v2/modules/db/repositories/projects.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/repositories/projects.py
@@ -2,7 +2,7 @@ import logging
 
 import sqlalchemy as sa
 from aiopg.sa.result import RowProxy
-from models_library.projects import ProjectAtDB
+from models_library.projects import ProjectAtDB, ProjectID
 
 from ..tables import projects
 from ._base import BaseRepository
@@ -11,10 +11,10 @@ logger = logging.getLogger(__name__)
 
 
 class ProjectsRepository(BaseRepository):
-    async def get_project(self, project_id: str) -> ProjectAtDB:
+    async def get_project(self, project_id: ProjectID) -> ProjectAtDB:
         row: RowProxy = await (
             await self.connection.execute(
-                sa.select([projects]).where(projects.c.uuid == project_id)
+                sa.select([projects]).where(projects.c.uuid == str(project_id))
             )
         ).first()
         return ProjectAtDB(**row)
