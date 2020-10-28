cc1dc3e7c9dccea29dce069ba53285f9155e1588
odeimaiz <maiz@itis.swiss>
2020-10-28 10:30:06 +0100

    minor

--- services/web/client/source/class/osparc/desktop/StudyEditor.js
+++ services/web/client/source/class/osparc/desktop/StudyEditor.js
@@ -208,6 +208,7 @@ qx.Class.define("osparc.desktop.StudyEditor", {
         .then(data => {
           this.__lastSavedStudy = osparc.wrapper.JsonDiffPatch.getInstance().clone(newObj);
         }).catch(error => {
+          console.error(error);
           this.getLogger().error(null, "Error updating pipeline");
           // Need to throw the error to be able to handle it later
           throw error;
