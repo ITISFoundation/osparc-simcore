7dc2fe2f9acc54ddec3833e2f4dc064b5eab3494
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-10-30 17:10:17 +0100

    projects module: move setup logic and added assert cfg

--- services/web/server/src/simcore_service_webserver/projects/config.py
+++ services/web/server/src/simcore_service_webserver/projects/config.py
@@ -7,6 +7,9 @@ from typing import Optional
 
 import trafaret as T
 from pydantic import BaseSettings
+from servicelib.application_keys import APP_CONFIG_KEY
+from aiohttp.web import Application
+from typing import Dict
 
 CONFIG_SECTION_NAME = "projects"
 
@@ -15,3 +18,9 @@ schema = T.Dict({T.Key("enabled", default=True, optional=True): T.Bool()})
 
 class ProjectSettings(BaseSettings):
     enabled: Optional[bool] = True
+
+
+def assert_valid_config(app: Application) -> Dict:
+    cfg = app[APP_CONFIG_KEY][CONFIG_SECTION_NAME]
+    _settings = ProjectSettings(**cfg)
+    return cfg
