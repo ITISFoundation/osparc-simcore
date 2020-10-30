0f96400dad5e1c62a12a7fd3d10462874acf7848
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-10-30 17:17:55 +0100

    socketio module adapted

--- services/web/server/src/simcore_service_webserver/socketio/config.py
+++ services/web/server/src/simcore_service_webserver/socketio/config.py
@@ -6,7 +6,7 @@
 from typing import Dict, Optional
 
 import trafaret as T
-from aiohttp import web
+from aiohttp.web import Application
 from pydantic import BaseSettings
 from servicelib.application_keys import APP_CONFIG_KEY
 from socketio import AsyncServer
@@ -26,9 +26,15 @@ class SocketIOSettings(BaseSettings):
     enabled: Optional[bool] = True
 
 
-def get_config(app: web.Application) -> Dict:
+def get_config(app: Application) -> Dict:
     return app[APP_CONFIG_KEY][CONFIG_SECTION_NAME]
 
 
-def get_socket_server(app: web.Application) -> AsyncServer:
+def get_socket_server(app: Application) -> AsyncServer:
     return app[APP_CLIENT_SOCKET_SERVER_KEY]
+
+
+def assert_valid_config(app: Application) -> Dict:
+    cfg = get_config(app)
+    _settings = SocketIOSettings(**cfg)
+    return cfg
---
+++ services/web/server/src/simcore_service_webserver/socketio/module_setup.py
@@ -0,0 +1,29 @@
+""" socketio subsystem based on socket-io
+    and https://github.com/miguelgrinberg/python-socketio
+
+"""
+import logging
+
+from aiohttp import web
+from socketio import AsyncServer
+
+from servicelib.application_setup import ModuleCategory, app_module_setup
+
+from . import handlers, handlers_utils
+from .config import APP_CLIENT_SOCKET_SERVER_KEY, assert_valid_config
+
+log = logging.getLogger(__name__)
+
+
+@app_module_setup("simcore_service_webserver.socketio", ModuleCategory.SYSTEM, logger=log)
+def setup_sockets(app: web.Application):
+    # ----------------------------------------------
+    # TODO: temporary, just to check compatibility between
+    # trafaret and pydantic schemas
+    assert_valid_config(app)
+    # ---------------------------------------------
+    mgr = None
+    sio = AsyncServer(async_mode="aiohttp", client_manager=mgr, logging=log)
+    sio.attach(app)
+    app[APP_CLIENT_SOCKET_SERVER_KEY] = sio
+    handlers_utils.register_handlers(app, handlers)
