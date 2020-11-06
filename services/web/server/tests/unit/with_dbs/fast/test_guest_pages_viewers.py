a925dc035267f4eae9f7b78131871502634d746c
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-11-07 00:34:15 +0100

    Placeholder for testing

---
+++ services/web/server/tests/unit/with_dbs/fast/test_guest_pages_viewers.py
@@ -0,0 +1,11 @@
+# pylint:disable=unused-variable
+# pylint:disable=unused-argument
+# pylint:disable=redefined-outer-name
+
+
+from simcore_service_webserver.guest_pages_viewers import *
+
+
+
+def test_portal_workflow():
+    pass
