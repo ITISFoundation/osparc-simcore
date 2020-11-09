742f3dbfb6f82d2c384a56755c4198fe2379a84e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-11-09 17:55:46 +0100

    create different use cases

--- services/director-v2/tests/integration/test_computation_api.py
+++ services/director-v2/tests/integration/test_computation_api.py
@@ -2,12 +2,25 @@
 # pylint:disable=unused-argument
 # pylint:disable=redefined-outer-name
 # pylint:disable=protected-access
+# pylint:disable=no-value-for-parameter
 
-from typing import Dict
+import json
+from pathlib import Path
+from random import randint
+from typing import Callable, Dict
+from uuid import UUID, uuid4
 
 import pytest
+import sqlalchemy as sa
+from models_library.projects import Project, Workbench
 from models_library.settings.rabbit import RabbitConfig
 from models_library.settings.redis import RedisConfig
+from pydantic.types import PositiveInt
+from simcore_postgres_database.models.projects import ProjectType, projects
+from simcore_service_director_v2.models.domains.comp_tasks import ComputationTaskOut
+from simcore_service_director_v2.models.domains.projects import ProjectAtDB
+from sqlalchemy import literal_column
+from starlette import status
 from starlette.testclient import TestClient
 from yarl import URL
 
@@ -19,6 +32,7 @@ ops_services = ["minio"]
 def minimal_configuration(
     sleeper_service: Dict[str, str],
     redis_service: RedisConfig,
+    postgres_db: sa.engine.Engine,
     postgres_host_config: Dict[str, str],
     rabbit_service: RabbitConfig,
     simcore_services: Dict[str, URL],
@@ -27,10 +41,114 @@ def minimal_configuration(
     pass
 
 
-def test_start_computation(client: TestClient):
-    import pdb
+@pytest.fixture
+def user_id() -> PositiveInt:
+    return randint(0, 10000)
 
-    pdb.set_trace()
+
+@pytest.fixture(scope="session")
+def sleepers_workbench_file(mocks_dir: Path) -> Path:
+    file_path = mocks_dir / "4sleepers_workbench.json"
+    assert file_path.exists()
+    return file_path
+
+
+@pytest.fixture(scope="session")
+def sleepers_workbench(sleepers_workbench_file: Path) -> Dict:
+    return json.loads(sleepers_workbench_file.read_text())
+
+
+@pytest.fixture
+def project(postgres_db: sa.engine.Engine) -> Callable:
+    created_project_ids = []
+
+    def creator(**overrides):
+        project_config = {
+            "uuid": uuid4(),
+            "name": "my test project",
+            "type": ProjectType.STANDARD.name,
+            "description": "my test description",
+            "workbench": {},
+        }
+        project_config.update(**overrides)
+        with postgres_db.connect() as con:
+            result = con.execute(
+                projects.insert()
+                .values(**project_config)
+                .returning(literal_column("*"))
+            )
+
+            project = ProjectAtDB.parse_obj(result.first())
+            created_project_ids.append(project.uuid)
+            return project
+
+    yield creator
+
+    # cleanup
+    with postgres_db.connect() as con:
+        for pid in created_project_ids:
+            con.execute(projects.delete().where(projects.c.uuid == str(pid)))
+
+
+def test_start_computation(
+    client: TestClient,
+    user_id: PositiveInt,
+    project: Callable,
+    sleepers_workbench: Dict,
+):
+    entrypoint = "v2/computations"
+
+    # create a bunch of invalid stuff
+    expected_resp = status.HTTP_422_UNPROCESSABLE_ENTITY
+    response = client.post(
+        entrypoint,
+        json={"user_id": "some invalid id", "project_id": "not a uuid"},
+    )
+    assert (
+        response.status_code == expected_resp
+    ), f"response code is {response.status_code}, error: {response.text}"
+
+    response = client.post(
+        entrypoint,
+        json={"user_id": user_id, "project_id": "not a uuid"},
+    )
+    assert (
+        response.status_code == expected_resp
+    ), f"response code is {response.status_code}, error: {response.text}"
+
+    # send an invalid project to process
+    expected_resp = status.HTTP_404_NOT_FOUND
+    response = client.post(
+        entrypoint,
+        json={"user_id": user_id, "project_id": str(uuid4())},
+    )
+    assert (
+        response.status_code == expected_resp
+    ), f"response code is {response.status_code}, error: {response.text}"
+
+    # send an empty project to process
+    expected_resp = status.HTTP_422_UNPROCESSABLE_ENTITY
+    empty_project = project()
+    response = client.post(
+        entrypoint,
+        json={"user_id": user_id, "project_id": str(empty_project.uuid)},
+    )
+    assert (
+        response.status_code == expected_resp
+    ), f"response code is {response.status_code}, error: {response.text}"
+
+    # send a valid project with a random number of sleepers
+    sleepers_project = project(workbench=sleepers_workbench)
+    expected_resp = status.HTTP_201_CREATED
+    response = client.post(
+        entrypoint,
+        json={"user_id": user_id, "project_id": str(sleepers_project.uuid)},
+    )
+    assert (
+        response.status_code == expected_resp
+    ), f"response code is {response.status_code}, error: {response.text}"
+
+    task_out = ComputationTaskOut.parse_obj(response.json())
 
 
 def test_abort_computation():
