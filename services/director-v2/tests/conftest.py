472e7c4df880e3087171868f1b1d049bf73955cc
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-11-09 15:51:15 +0100

    use common conftest

--- services/director-v2/tests/conftest.py
+++ services/director-v2/tests/conftest.py
@@ -15,12 +15,15 @@ from starlette.testclient import TestClient
 
 current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 pytest_plugins = [
-    "pytest_simcore.repository_paths",
-    "pytest_simcore.schemas",
     "pytest_simcore.docker_compose",
+    "pytest_simcore.docker_registry",
     "pytest_simcore.docker_swarm",
+    "pytest_simcore.repository_paths",
     "pytest_simcore.postgres_service",
+    "pytest_simcore.rabbit_service",
     "pytest_simcore.redis_service",
+    "pytest_simcore.schemas",
+    "pytest_simcore.simcore_services",
 ]
 
 
