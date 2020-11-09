2478f7f8c5dba39fa4fdcb5cb5e8b6757958af84
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-11-09 20:57:28 +0100

    minor

--- services/api-server/src/simcore_service_api_server/api/routes/health.py
+++ services/api-server/src/simcore_service_api_server/api/routes/health.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
 
 router = APIRouter()
 
 
-@router.get("/", include_in_schema=False)
+@router.get("/", include_in_schema=False, response_class=PlainTextResponse)
 async def check_service_health():
-    return {"msg": "yes, I am here :-)"}
+    return __name__
