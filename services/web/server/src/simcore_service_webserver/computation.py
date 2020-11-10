dbfdab86defc4667b76220f9ad507c54634ef5db
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-11-10 15:54:46 +0100

    removin old computation_handlers

--- services/web/server/src/simcore_service_webserver/computation.py
+++ services/web/server/src/simcore_service_webserver/computation.py
@@ -6,14 +6,11 @@ import logging
 
 from aiohttp import web
 from servicelib.application_setup import ModuleCategory, app_module_setup
-from servicelib.rest_routing import iter_path_operations, map_handlers_with_operations
 
-from . import computation_handlers
 from .computation_comp_tasks_listening_task import setup as setup_comp_tasks_listener
 from .computation_config import CONFIG_SECTION_NAME
 from .computation_config import create_settings as create_computation_settings
 from .computation_subscribe import subscribe
-from .rest_config import APP_OPENAPI_SPECS_KEY
 
 log = logging.getLogger(__file__)
 
@@ -26,28 +23,9 @@ def setup_computation(app: web.Application):
     create_computation_settings(app)
 
     # subscribe to rabbit upon startup
-    # TODO: Define connection policies (e.g. {on-startup}, lazy). Could be defined in config-file
     app.on_startup.append(subscribe)
 
-    # TODO: add function to "unsubscribe"
-    # app.on_cleanup.append(unsubscribe)
-
-    if not APP_OPENAPI_SPECS_KEY in app:
-        log.warning(
-            "rest submodule not initialised? computation routes will not be defined!"
-        )
-        return
-
-    specs = app[APP_OPENAPI_SPECS_KEY]
-    # routes = map_handlers_with_operations(
-    #     {
-    #         # "start_pipeline": computation_handlers.start_pipeline,
-    #         # "stop_pipeline": computation_handlers.stop_pipeline,
-    #     },
-    #     filter(lambda o: "stop_pipeline" in o[2], iter_path_operations(specs)),
-    #     strict=True,
-    # )
-    # app.router.add_routes(routes)
+    # setup comp_task listener
     setup_comp_tasks_listener(app)
 
 
