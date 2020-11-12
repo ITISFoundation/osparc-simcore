b719a405fa2052b5d7539e8a358ea1d064518a0a
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-11-12 09:27:16 +0100

    adds timestamp in healthcheck

--- services/api-server/src/simcore_service_api_server/api/routes/health.py
+++ services/api-server/src/simcore_service_api_server/api/routes/health.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from fastapi import APIRouter
 from fastapi.responses import PlainTextResponse
 
@@ -6,4 +8,4 @@ router = APIRouter()
 
 @router.get("/", include_in_schema=False, response_class=PlainTextResponse)
 async def check_service_health():
-    return __name__
+    return f"{__name__}@{datetime.now().isoformat()}"
