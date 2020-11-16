0d84801b0229461bcb3693a15d6eff261116dfd6
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-11-16 17:14:52 +0100

    Fixes and testing link validation check

--- services/web/server/src/simcore_service_webserver/studies_dispatcher/handlers_redirects.py
+++ services/web/server/src/simcore_service_webserver/studies_dispatcher/handlers_redirects.py
@@ -7,6 +7,7 @@ from typing import Optional
 import aiohttp
 from aiohttp import web
 from pydantic import BaseModel, HttpUrl, ValidationError
+from pydantic.types import PositiveInt
 from yarl import URL
 
 from ..statics import INDEX_RESOURCE_NAME
@@ -24,17 +25,19 @@ from ._users import UserInfo, acquire_user, ensure_authentication
 log = logging.getLogger(__name__)
 
 
-def create_redirect_response(app: web.Application, page: str, **parameters) -> web.HTTPFound:
+def create_redirect_response(
+    app: web.Application, page: str, **parameters
+) -> web.HTTPFound:
     """
-        Returns a redirect response to the front-end with information on page and parameters embedded in the fragment.
+    Returns a redirect response to the front-end with information on page and parameters embedded in the fragment.
 
-        For instance,
-            https://osparc.io/#/error?message=Sorry%2C%20I%20could%20not%20find%20this%20&status_code=404
-        results from
-                - page=error
-            and parameters
-                - message="Sorry, I could not find this"
-                - status_code=404
+    For instance,
+        https://osparc.io/#/error?message=Sorry%2C%20I%20could%20not%20find%20this%20&status_code=404
+    results from
+            - page=error
+        and parameters
+            - message="Sorry, I could not find this"
+            - status_code=404
     """
     page = page.strip(" /")
     # TODO: test that fragment queries are understood by front-end
@@ -48,30 +51,39 @@ def create_redirect_response(app: web.Application, page: str, **parameters) -> w
 
 # HANDLERS --------------------------------
 from aiohttp.client_exceptions import ClientError
+
+
 class QueryParams(BaseModel, ValidationMixin):
     # TODO: create dinamically with pydantic class
     file_name: Optional[str] = None
-    file_size: int
-    file_type: str
+    file_size: PositiveInt
+    file_type: str  # TODO: should we define some types?
     download_link: HttpUrl
 
     async def check_download_link(self):
-        """ Explicit validation of download link that performs a light fetch of url's head
-        """
+        """Explicit validation of download link that performs a light fetch of url's hea"""
+        #
+        # NOTE: Amazon download links has HEAD operation forbidden!
         try:
             async with aiohttp.request("HEAD", self.download_link) as response:
                 response.raise_for_status()
-        except ClientError as err:
-            log.debug("Invalid download link '%s'. If failed fetch check with %s", self.download_link, err)
-            raise web.HTTPBadRequest(reason="The download link provided is invalid") from err
 
+        except ClientError as err:
+            log.debug(
+                "Invalid download link '%s'. If failed fetch check with %s",
+                self.download_link,
+                err,
+            )
+            raise web.HTTPBadRequest(
+                reason="The download link provided is invalid"
+            ) from err
 
 
 async def get_redirection_to_viewer(request: web.Request):
     try:
         # query parameters in request parsed and validated
         p = QueryParams.create_from(request)
-        await p.check_download_link()
+        # TMP removed await p.check_download_link()
 
         viewer: ViewerInfo = find_compatible_viewer(p.file_size, p.file_type)
 
