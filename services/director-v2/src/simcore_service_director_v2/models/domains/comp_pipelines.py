8e6aaa41523d35f57606abc4bd449e35f0b9b2fe
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-11-16 15:33:59 +0100

    @pcrespov review: add comment as to why that is so

--- services/director-v2/src/simcore_service_director_v2/models/domains/comp_pipelines.py
+++ services/director-v2/src/simcore_service_director_v2/models/domains/comp_pipelines.py
@@ -12,6 +12,8 @@ class CompPipelineAtDB(BaseModel):
     @validator("dag_adjacency_list", pre=True)
     @classmethod
     def auto_convert_dag(cls, v):
+        # this enforcement is here because the serialization using json is not happy with non str Dict keys, also comparison gets funny if the lists are having sometimes UUIDs or str.
+        # NOTE: this might not be necessary anymore once we have something fully defined
         return {str(key): [str(n) for n in value] for key, value in v.items()}
 
     class Config:
