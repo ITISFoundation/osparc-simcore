af17087d0effd506b1056bbc100b1a0200a4edd0
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-11-16 22:48:21 +0100

    Adds envelopes and fixes request query requires in OAS

--- services/web/server/tests/sandbox/viewers_openapi_generator.py
+++ services/web/server/tests/sandbox/viewers_openapi_generator.py
@@ -1,9 +1,10 @@
 # pylint: skip-file
 
-from typing import List
+from typing import List, Optional
 
 import yaml
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
 from pydantic import BaseModel, Field
 from pydantic.networks import HttpUrl
 from pydantic.types import PositiveInt
@@ -11,6 +12,13 @@ from yaml import safe_dump
 
 app = FastAPI()
 
+
+
+# TODO:
+#class ErrorType(BaseModel):
+#    logs: List[LogMessageType] = []
+#    errors: List[ErrorItemType]
+#    status: int
 
 class FileType2Viewer(BaseModel):
     file_type: str
@@ -23,15 +31,36 @@ class FileType2Viewer(BaseModel):
     )
 
 
-@app.get("v0/viewers/filetypes", response_model=List[FileType2Viewer], tags=["viewer"])
+class FielType2ViewerEnveloped(BaseModel):
+    data: FileType2Viewer
+    # error: Optional[ErrorType] = None
+
+class FieldType2ViewerListEnveloped(BaseModel):
+    data: List[FileType2Viewer]
+
+
+@app.get("/viewers/filetypes", response_model=FieldType2ViewerListEnveloped, tags=["viewer"])
 def list_supported_filetypes():
     pass
 
 
-@app.get("v0/viewers", response_model=FileType2Viewer, tags=["viewer"])
-def get_viewer_for_file(file_name: str, file_size: PositiveInt, file_type: str):
+@app.get("/viewers", response_model=FielType2ViewerEnveloped, tags=["viewer"])
+def get_viewer_for_file(
+    file_type: str,
+    file_name: Optional[str] = None,
+    file_size: Optional[PositiveInt] = None
+    #Field(
+    #    None, description="Expected file size in bytes"
+    #),
+):
     pass
 
 
-with open("openapi-viewer.yaml", "wt") as fh:
+# use handler names as operation_id
+for route in app.routes:
+    if isinstance(route, APIRoute):
+        route.operation_id = route.name
+
+# generate file
+with open("openapi-viewer.ignore.yaml", "wt") as fh:
     yaml.safe_dump(app.openapi(), fh)
