2c1ff8856825c11b57b14daedf3444e864826f11
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-11-16 22:15:55 +0100

    copy pasted fixture since it does not use the pytest ones

--- services/web/server/tests/integration/test_garbage_collection.py
+++ services/web/server/tests/integration/test_garbage_collection.py
@@ -3,6 +3,7 @@
 
 import asyncio
 import logging
+import re
 from copy import deepcopy
 from typing import Dict, List
 from uuid import uuid4
@@ -11,7 +12,7 @@ import aiopg
 import aioredis
 import pytest
 from aioresponses import aioresponses
-
+from models_library.project_nodes import RunningState
 from models_library.settings.redis import RedisConfig
 from pytest_simcore.helpers.utils_login import log_client_in
 from pytest_simcore.helpers.utils_projects import create_project, empty_project_data
@@ -72,6 +73,32 @@ async def __delete_all_redis_keys__(redis_service: RedisConfig):
 
     yield
     # do nothing on teadown
+
+
+@pytest.fixture
+async def director_v2_subsystem_mock() -> aioresponses:
+    """uses aioresponses to mock all calls of an aiohttpclient
+    WARNING: any request done through the client will go through aioresponses. It is
+    unfortunate but that means any valid request (like calling the test server) prefix must be set as passthrough.
+    Other than that it seems to behave nicely
+    """
+    PASSTHROUGH_REQUESTS_PREFIXES = ["http://127.0.0.1", "ws://"]
+    get_computation_pattern = re.compile(
+        r"^http://[a-z\-_]*director-v2:[0-9]+/v2/computations/.*$"
+    )
+    delete_computation_pattern = get_computation_pattern
+    # NOTE: GitHK I have to copy paste that fixture for some unclear reason for now.
+    # I think this is due to some conflict between these non-pytest-simcore fixtures and the loop fixture being defined at different locations?? not sure..
+    # anyway I think this should disappear once the garbage collector moves to its own micro-service
+    with aioresponses(passthrough=PASSTHROUGH_REQUESTS_PREFIXES) as mock:
+        mock.get(
+            get_computation_pattern,
+            status=202,
+            payload={"state": str(RunningState.NOT_STARTED.value)},
+            repeat=True,
+        )
+        mock.delete(delete_computation_pattern, status=204, repeat=True)
+        yield mock
 
 
 @pytest.fixture(autouse=True)
