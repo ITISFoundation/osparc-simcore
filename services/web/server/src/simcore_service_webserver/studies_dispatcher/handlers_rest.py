0d4588c44714755704b574983984a3c2d7a0845a
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-11-16 18:12:47 +0100

    implements entrypoint to list supported files

--- services/web/server/src/simcore_service_webserver/studies_dispatcher/handlers_rest.py
+++ services/web/server/src/simcore_service_webserver/studies_dispatcher/handlers_rest.py
@@ -3,23 +3,42 @@
 """
 from aiohttp import web
 from pydantic import BaseModel
+from typing import Optional
 
 from ._core import (
-    find_compatible_viewer,
-    ViewerInfo,
-    ValidationMixin,
     MatchNotFoundError,
+    ValidationMixin,
+    ViewerInfo,
+    find_compatible_viewer,
+    iter_supported_filetypes,
 )
 
 
+# GET /v0/viewers/filetypes
+async def list_supported_filetypes(request: web.Request):
+    data = []
+    for file_type, viewer in iter_supported_filetypes():
+        data.append(
+            {
+                "file_type": file_type,
+                "viewer_title": viewer.title,
+                "redirection_url": request.app.router["get_redirection_to_viewer"]
+                .url_for()
+                .with_query(file_type=file_type),
+            }
+        )
+    return data
+
+
+# GET /v0/viewers -----
 class RequestParams(BaseModel, ValidationMixin):
     # TODO: create dinamically with pydantic class
-    file_name: str
-    file_size: int  # mime-types
+    file_name: Optional[str] = None
+    file_size: Optional[int] = None  # TODO: mime-types
     file_type: str  # Bytes
 
 
-async def get_viewers_handler(request: web.Request):
+async def get_viewer_for_file(request: web.Request):
     try:
         p = RequestParams.create_from(request)
 
@@ -27,12 +46,13 @@ async def get_viewers_handler(request: web.Request):
         viewer: ViewerInfo = find_compatible_viewer(p.file_size, p.file_type)
 
         return {
-            "name": viewer.label,
-            # "description": "some meaninful descpriton",
-            "base_url": request.app.router["get_redirection_to_viewer"]
+            "file_type": p.file_type,
+            "viewer_title": viewer.title,
+            "redirection_url": request.app.router["get_redirection_to_viewer"]
             .url_for()
-            .with_query(**p.dict()),
-            # TODO: make sure parameters are always in syn
+            .with_query(
+                **p.dict(exclude_defaults=True, exclude_unset=True, exclude_none=True)
+            ),
         }
 
     except MatchNotFoundError as err:
