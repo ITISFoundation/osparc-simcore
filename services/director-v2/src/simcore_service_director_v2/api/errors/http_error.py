13dc6a007a1fbd57dc1d296c188a711c84086496
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-11-19 22:09:17 +0100

    Added error handling in director-v2 application. All Validation errors should be reported as json

--- services/director-v2/src/simcore_service_director_v2/api/errors/http_error.py
+++ services/director-v2/src/simcore_service_director_v2/api/errors/http_error.py
@@ -1,7 +1,21 @@
+from typing import Callable
+
 from fastapi import HTTPException
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
 
 async def http_error_handler(_: Request, exc: HTTPException) -> JSONResponse:
-    return JSONResponse({"error": [exc.detail]}, status_code=exc.status_code)
+    return JSONResponse({"errors": [exc.detail]}, status_code=exc.status_code)
+
+
+def make_http_error_handler_for_exception(status_code: int) -> Callable:
+    """
+        Handles a BaseException as an error JSON response with a given status code
+
+        SEE https://docs.python.org/3/library/exceptions.html#concrete-exceptions
+    """
+    async def _http_error_handler(_: Request, exc: BaseException) -> JSONResponse:
+        return JSONResponse({"errors": [str(exc)]}, status_code=status_code)
+
+    return _http_error_handler
