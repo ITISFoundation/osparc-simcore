e6611c14c6cd3cf65cf845b1565e709bc53736c9
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-11-19 19:07:51 +0100

    Fixes tear-donw in test and add extra validation

--- services/web/server/tests/unit/with_dbs/fast/test_studies_dispatcher_projects.py
+++ services/web/server/tests/unit/with_dbs/fast/test_studies_dispatcher_projects.py
@@ -9,9 +9,14 @@ from copy import deepcopy
 import pytest
 
 from models_library.projects import Project
+from pytest_simcore.helpers.utils_login import NewUser
 from pytest_simcore.helpers.utils_mock import future_with_result
+from pytest_simcore.helpers.utils_projects import delete_all_projects
+from simcore_service_webserver.groups_api import auto_add_user_to_groups
+
 # from simcore_postgres_database.models.projects import projects as projects_table
 from simcore_service_webserver.log import setup_logging
+from simcore_service_webserver.projects.projects_api import get_project_for_user
 from simcore_service_webserver.studies_dispatcher._core import _FILETYPE_TO_VIEWER
 from simcore_service_webserver.studies_dispatcher._projects import (
     UserInfo,
@@ -19,7 +24,7 @@ from simcore_service_webserver.studies_dispatcher._projects import (
     add_new_project,
     create_viewer_project_model,
 )
-from pytest_simcore.helpers.utils_login import NewUser
+from simcore_service_webserver.users_api import get_user
 
 
 @pytest.fixture
@@ -75,44 +80,63 @@ def app_cfg(default_app_cfg, aiohttp_unused_port, qx_client_outdir, redis_servic
 
     return cfg
 
-from simcore_service_webserver.groups_api import auto_add_user_to_groups
-from simcore_service_webserver.users_api import get_user
 
 @pytest.mark.parametrize(
     "file_type,viewer", [(k, v) for k, v in _FILETYPE_TO_VIEWER.items()]
 )
-async def test_add_new_project(
+async def test_add_new_project_from_model_instance(
     file_type: str,
     viewer: ViewerInfo,
     client,
     mocker,
 ):
-    update_func = mocker.patch(
+    mock_func = mocker.patch(
         "simcore_service_webserver.director_v2.create_or_update_pipeline",
         return_value=future_with_result(result=None),
     )
 
-    # ----
     async with NewUser() as user_db:
+        try:
+            # preparation
+            await auto_add_user_to_groups(client.app, user_db["id"])
+            user_db = await get_user(client.app, user_db["id"])
 
-        await auto_add_user_to_groups(client.app, user_db["id"])
-        user_db = await get_user(client.app, user_db["id"])
+            # this part is under test  ---
+            user = UserInfo(
+                id=user_db["id"],
+                name=user_db["name"],
+                primary_gid=user_db["primary_gid"],
+                email=user_db["email"],
+            )
 
-        user = UserInfo(
-            id=user_db["id"],
-            name=user_db["name"],
-            primary_gid=user_db["primary_gid"],
-            email=user_db["email"],
-        )
+            project_id = "e3ee7dfc-25c3-11eb-9fae-02420a01b846"
+            file_picker_id = "4c69c0ce-00e4-4bd5-9cf0-59b67b3a9343"
+            viewer_id = "fc718e5a-bf07-4abe-b526-d9cafd34830c"
 
-        project: Project = create_viewer_project_model(
-            project_id="e3ee7dfc-25c3-11eb-9fae-02420a01b846",
-            file_picker_id="4c69c0ce-00e4-4bd5-9cf0-59b67b3a9343",
-            viewer_id="fc718e5a-bf07-4abe-b526-d9cafd34830c",
-            owner=user,
-            download_link="http://httpbin.org/image/jpeg",
-            viewer_info=viewer,
-        )
+            project: Project = create_viewer_project_model(
+                project_id,
+                file_picker_id,
+                viewer_id,
+                owner=user,
+                download_link="http://httpbin.org/image/jpeg",
+                viewer_info=viewer,
+            )
 
-        await add_new_project(client.app, project, user)
-        assert update_func.called
+            await add_new_project(client.app, project, user)
+
+            # checks start here   ---
+            assert mock_func.called
+
+            # internally validates project injected in db
+            project_db = await get_project_for_user(
+                client.app,
+                project.uuid,
+                user.id,
+                include_state=False,
+                include_templates=False,
+            )
+            assert set(project_db["workbench"].keys()) == {file_picker_id, viewer_id}
+
+        finally:
+            # tear-down: delete before user gets deleted
+            await delete_all_projects(client.app)
