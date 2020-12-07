9fbc33714aa357c5677602576c308e9e94ecf2ff
odeimaiz <maiz@itis.swiss>
2020-12-07 14:33:15 +0100

    Create ServiceMetadataEditor.js

---
+++ services/web/client/source/class/osparc/component/metadata/ServiceMetadataEditor.js
@@ -0,0 +1,74 @@
+/* ************************************************************************
+
+   osparc - the simcore frontend
+
+   https://osparc.io
+
+   Copyright:
+     2020 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+qx.Class.define("osparc.component.metadata.ServiceMetadataEditor", {
+  extend: qx.ui.core.Widget,
+
+  /**
+    * @param serviceData {Object} Object containing the Service Data
+    */
+  construct: function(serviceData) {
+    this.base(arguments);
+
+    this._setLayout(new qx.ui.layout.Grow());
+
+    this.__serviceData = serviceData;
+
+    this.__stack = new qx.ui.container.Stack();
+    this.__displayView = this.__createDisplayView(serviceData);
+    this.__stack.add(this.__displayView);
+    this._add(this.__stack);
+  },
+
+  properties: {
+    mode: {
+      check: ["display", "edit"],
+      init: "display",
+      nullable: false,
+      apply: "_applyMode"
+    }
+  },
+
+  members: {
+    __serviceData: null,
+    __stack: null,
+    __displayView: null,
+    __editView: null,
+
+    __createDisplayView: function(serviceData) {
+      const displayView = new qx.ui.container.Composite(new qx.ui.layout.VBox(10));
+      console.log(serviceData);
+      return displayView;
+    },
+
+    _applyMode: function(mode) {
+      switch (mode) {
+        case "display":
+          this.__stack.setSelection([this.__displayView]);
+          break;
+      }
+    },
+
+    __isUserOwner: function() {
+      const myGid = osparc.auth.Data.getInstance().getGroupId();
+      if (myGid && osparc.component.export.ServicePermissions.canGroupWrite(this.__serviceData["access_rights"], myGid)) {
+        return true;
+      }
+      return false;
+    }
+  }
+});
