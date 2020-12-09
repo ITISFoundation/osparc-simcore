754fffe93b28ff3e41f99f750ef58d86d89c8412
odeimaiz <maiz@itis.swiss>
2020-12-09 18:18:01 +0100

    ongoing

--- services/web/client/source/class/osparc/component/metadata/ServiceMetadataEditor.js
+++ services/web/client/source/class/osparc/component/metadata/ServiceMetadataEditor.js
@@ -24,14 +24,30 @@ qx.Class.define("osparc.component.metadata.ServiceMetadataEditor", {
   construct: function(serviceData) {
     this.base(arguments);
 
-    this._setLayout(new qx.ui.layout.Grow());
+    this._setLayout(new qx.ui.layout.VBox());
+
+    if (!("metadataTSR" in serviceData)) {
+      osparc.component.message.FlashMessenger.logAs(this.tr("Metadata not found"), "ERROR");
+      return;
+    }
 
     this.__serviceData = serviceData;
 
-    this.__stack = new qx.ui.container.Stack();
-    this.__displayView = this.__createDisplayView(serviceData);
-    this.__stack.add(this.__displayView);
-    this._add(this.__stack);
+    const grid = new qx.ui.layout.Grid(10, 8);
+    grid.setColumnAlign(0, "left", "middle");
+    grid.setColumnAlign(1, "left", "middle");
+    this.__tsrGrid = new qx.ui.container.Composite(grid);
+    this._add(this.__tsrGrid, {
+      flex: 1
+    });
+
+    this.__populateTSR();
+
+    if (this.__isUserOwner()) {
+      const metadata = this.__serviceData;
+      this.__copyMetadata = osparc.utils.Utils.deepCloneObject(metadata);
+      this.__createEditBtns();
+    }
   },
 
   properties: {
@@ -39,28 +55,211 @@ qx.Class.define("osparc.component.metadata.ServiceMetadataEditor", {
       check: ["display", "edit"],
       init: "display",
       nullable: false,
-      apply: "_applyMode"
+      apply: "__populateTSRData"
     }
   },
 
   members: {
     __serviceData: null,
-    __stack: null,
-    __displayView: null,
-    __editView: null,
+    __copyMetadata: null,
+    __tsrGrid: null,
 
-    __createDisplayView: function(serviceData) {
-      const displayView = new qx.ui.container.Composite(new qx.ui.layout.VBox(10));
-      displayView.add(new osparc.component.metadata.ServiceMetadataView(serviceData));
-      return displayView;
+    __populateTSR: function() {
+      this.__tsrGrid.removeAll();
+      this.__populateTSRHeaders();
+      this.__populateTSRData();
     },
 
-    _applyMode: function(mode) {
-      switch (mode) {
-        case "display":
-          this.__stack.setSelection([this.__displayView]);
+    __populateTSRHeaders: function() {
+      const rules = osparc.component.metadata.ServiceMetadata.getMetadataTSR();
+
+      const header0 = new qx.ui.basic.Label(this.tr("Ten Simple Rules")).set({
+        font: "title-14"
+      });
+      this.__tsrGrid.add(header0, {
+        row: 0,
+        column: 0
+      });
+      const header1 = new qx.ui.basic.Label(this.tr("Conformance Level")).set({
+        font: "title-14"
+      });
+      this.__tsrGrid.add(header1, {
+        row: 0,
+        column: 1
+      });
+
+      let row = 1;
+      Object.values(rules).forEach(rule => {
+        const label = new qx.ui.basic.Label(rule.title);
+        const ruleWHint = new osparc.component.form.FieldWHint(null, rule.description, label).set({
+          hintPosition: "left"
+        });
+        this.__tsrGrid.add(ruleWHint, {
+          row,
+          column: 0
+        });
+        row++;
+      });
+      const label = new qx.ui.basic.Label("TSR score").set({
+        font: "title-13"
+      });
+      this.__tsrGrid.add(label, {
+        row,
+        column: 0
+      });
+      row++;
+    },
+
+    __populateTSRData: function() {
+      switch (this.getMode()) {
+        case "edit":
+          this.__populateTSRDataEdit();
+          break;
+        default:
+          this.__populateTSRDataView();
           break;
       }
+    },
+
+    __populateTSRDataView: function() {
+      const metadataTSR = this.__serviceData["metadataTSR"];
+      let row = 1;
+      Object.values(metadataTSR).forEach(rule => {
+        const ruleRating = new osparc.ui.basic.StarsRating();
+        ruleRating.set({
+          score: rule.level,
+          maxScore: 4,
+          nStars: 4
+        });
+        const confLevel = osparc.component.metadata.ServiceMetadata.findConformanceLevel(rule.level);
+        const hint = confLevel.title + "<br>" + confLevel.description;
+        const ruleRatingWHint = new osparc.component.form.FieldWHint(null, hint, ruleRating).set({
+          hintPosition: "left"
+        });
+        this.__tsrGrid.add(ruleRatingWHint, {
+          row,
+          column: 1
+        });
+        row++;
+      });
+      const {
+        score,
+        maxScore
+      } = osparc.component.metadata.ServiceMetadata.computeTSRScore(metadataTSR);
+      const tsrRating = new osparc.ui.basic.StarsRating();
+      tsrRating.set({
+        score,
+        maxScore,
+        nStars: 4,
+        showScore: true
+      });
+      this.__tsrGrid.add(tsrRating, {
+        row,
+        column: 1
+      });
+    },
+
+    __populateTSRDataEdit: function() {
+      const copyMetadataTSR = this.__copyMetadata["metadataTSR"];
+      const tsrRating = new osparc.ui.basic.StarsRating();
+      tsrRating.set({
+        nStars: 4,
+        showScore: true
+      });
+      const updateTSRScore = () => {
+        const {
+          score,
+          maxScore
+        } = osparc.component.metadata.ServiceMetadata.computeTSRScore(copyMetadataTSR);
+
+        tsrRating.set({
+          score,
+          maxScore
+        });
+      };
+      updateTSRScore();
+
+      let row = 1;
+      Object.keys(copyMetadataTSR).forEach(ruleKey => {
+        const rule = copyMetadataTSR[ruleKey];
+        const layout = new qx.ui.container.Composite(new qx.ui.layout.HBox(10));
+
+        const slider = new qx.ui.form.Slider().set({
+          width: 150,
+          minimum: 0,
+          maximum: 4,
+          singleStep: 1,
+          value: rule.level
+        });
+        layout.add(slider);
+
+        const updateLevel = value => {
+          // remove all but slider
+          while (layout.getChildren().length > 1) {
+            layout.removeAt(1);
+          }
+          const ruleRating = new osparc.ui.basic.StarsRating();
+          ruleRating.set({
+            maxScore: 4,
+            nStars: 4,
+            score: value
+          });
+          const confLevel = osparc.component.metadata.ServiceMetadata.findConformanceLevel(value);
+          const hint = confLevel.title + "<br>" + confLevel.description;
+          const ruleRatingWHint = new osparc.component.form.FieldWHint(null, hint, ruleRating).set({
+            hintPosition: "left"
+          });
+          layout.add(ruleRatingWHint);
+        };
+
+        updateLevel(rule.level);
+        slider.addListener("changeValue", e => {
+          rule.level = e.getData();
+          updateLevel(rule.level);
+          updateTSRScore();
+        }, this);
+
+        this.__tsrGrid.add(layout, {
+          row,
+          column: 1
+        });
+        row++;
+      });
+
+
+      this.__tsrGrid.add(tsrRating, {
+        row,
+        column: 1
+      });
+    },
+
+    __createEditBtns: function() {
+      const editButton = new qx.ui.toolbar.Button(this.tr("Edit")).set({
+        appearance: "toolbar-md-button"
+      });
+      editButton.addListener("execute", () => {
+        this.setMode("edit");
+      }, this);
+
+      const saveButton = new qx.ui.toolbar.Button(this.tr("Save"), "@FontAwesome5Solid/save/16").set({
+        appearance: "toolbar-md-button"
+      });
+      saveButton.addListener("execute", e => {
+
+      }, this);
+      const cancelButton = new qx.ui.toolbar.Button(this.tr("Cancel")).set({
+        appearance: "toolbar-md-button"
+      });
+      cancelButton.addListener("execute", () => {
+        this.setMode("display");
+      }, this);
+
+      const buttonsToolbar = new qx.ui.toolbar.ToolBar();
+      buttonsToolbar.add(editButton);
+      buttonsToolbar.addSpacer();
+      buttonsToolbar.add(saveButton);
+      buttonsToolbar.add(cancelButton);
+      this._add(buttonsToolbar);
     },
 
     __isUserOwner: function() {
