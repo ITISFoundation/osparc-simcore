3389e3f1dc18011f3311f88b7dba39bfdfaa38c8
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-12-09 01:03:00 +0100

    Adds testing around project nodes

--- packages/models-library/tests/test_project_nodes.py
+++ packages/models-library/tests/test_project_nodes.py
@@ -13,7 +13,7 @@ from models_library.projects_state import RunningState
 def minimal_node_data_sample() -> Dict:
     return dict(
         key="simcore/services/dynamic/3dviewer",
-        version="1.0.0",
+        version="1.3.0-alpha",
         label="3D viewer human message",
     )
 
@@ -21,17 +21,25 @@ def minimal_node_data_sample() -> Dict:
 def test_create_minimal_node(minimal_node_data_sample):
     node = Node(**minimal_node_data_sample)
 
+    # a nice way to see how the simplest node looks like
     assert node.inputs == {}
     assert node.outputs == {}
     assert node.state == RunningState.NOT_STARTED
+
+    assert node.parent is None
+    assert node.progress is None
+
+    assert node.dict(exclude_unset=True) == minimal_node_data_sample
 
 
 def test_backwards_compatibility_node_data(minimal_node_data_sample):
     old_node_data = minimal_node_data_sample
     # found some old data with this aspect
-    old_node_data.update(thumbnail="", state="FAILURE")  # <----  # <----
+    old_node_data.update({"thumbnail": "", "state": "FAILURE"})
 
     node = Node(**old_node_data)
 
     assert node.thumbnail is None
     assert node.state == RunningState.FAILED
+
+    assert node.dict(exclude_unset=True) != old_node_data
