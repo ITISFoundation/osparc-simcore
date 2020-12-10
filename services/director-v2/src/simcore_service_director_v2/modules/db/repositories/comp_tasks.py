9d810a0cce865d628febd1d76666fc1b64ff76a6
Andrei Neagu <neagu@itis.swiss>
2020-12-10 16:44:44 +0100

    implementing upsert pattern

--- services/director-v2/src/simcore_service_director_v2/modules/db/repositories/comp_tasks.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/repositories/comp_tasks.py
@@ -132,11 +132,12 @@ class CompTasksRepository(BaseRepository):
             )
             internal_id = internal_id + 1
 
-            await self.connection.execute(
-                insert(comp_tasks).values(
-                    **task_db.dict(by_alias=True, exclude_unset=True)
-                )
+            insert_stmt = insert(comp_tasks).values(**task_db.dict(by_alias=True))
+            on_update_stmt = insert_stmt.on_conflict_do_update(
+                index_elements=[comp_tasks.c.project_id, comp_tasks.c.node_id],
+                set_=task_db.dict(by_alias=True, exclude_unset=True),
             )
+            await self.connection.execute(on_update_stmt)
 
     @log_decorator(logger=logger)
     async def mark_project_tasks_as_aborted(self, project: ProjectAtDB) -> None:
