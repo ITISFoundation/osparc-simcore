a63ed1631cfa0c76763e4d4cf4b3d3f9f3c5312e
Andrei Neagu <neagu@itis.swiss>
2020-12-10 14:01:03 +0100

    refactored the test - provided a more helpful error - checks for the timeout=timeout parameter

--- services/storage/tests/test_python_client_sdk.py
+++ services/storage/tests/test_python_client_sdk.py
@@ -5,21 +5,65 @@
 from typing import Dict
 import pytest
 
+TEMPLATE_CODE_NEEDED_HINT = """
+-------------------------
+
+import os
+
+def get_http_client_request_aiohttp_connect_timeout() -> int:
+    return int(os.environ.get("HTTP_CLIENT_REQUEST_AIOHTTP_CONNECT_TIMEOUT", "5"))
+
+
+def get_http_client_request_aiohttp_sock_connect_timeout() -> int:
+    return int(os.environ.get("HTTP_CLIENT_REQUEST_AIOHTTP_SOCK_CONNECT_TIMEOUT", "5"))
+
+
+class RESTClientObject(object):
+
+    def __init__(self, configuration, pools_size=4, maxsize=4):
+        
+        ...
+
+        # We are interested in fast connections, if a connection is established
+        # there is no timeout for file download operations
+        timeout = aiohttp.ClientTimeout(
+            total=None, 
+            connect=get_http_client_request_aiohttp_connect_timeout(), 
+            sock_connect=get_http_client_request_aiohttp_sock_connect_timeout(),
+        )
+        if configuration.proxy:
+            self.pool_manager = aiohttp.ClientSession(
+                connector=connector,
+                proxy=configuration.proxy,
+                timeout=timeout,
+            )
+        else:
+            self.pool_manager = aiohttp.ClientSession(
+                connector=connector, timeout=timeout
+            )
+
+-------------------------
+"""
+
 
 @pytest.fixture
 def search_dict() -> Dict[str, int]:
     return {
-        "ClientTimeout(": 0,
+        "import os": 0,
+        "timeout=timeout": 0,
         '"HTTP_CLIENT_REQUEST_AIOHTTP_CONNECT_TIMEOUT", "5"': 0,
         '"HTTP_CLIENT_REQUEST_AIOHTTP_SOCK_CONNECT_TIMEOUT", "5"': 0,
     }
 
 
-def assert_all_hits_found(filled_search_dict: Dict[str, int]) -> None:
+def assert_all_hits_found(filled_search_dict: Dict[str, int], file_path: str) -> None:
     for search_key, value in filled_search_dict.items():
-        assert (
-            value > 0
-        ), f"Could not find an entry for search_key='{search_key}' in storge's python client"
+        message = (
+            f"Could not find an entry for search_key='{search_key}' in "
+            "storage's python client.\nMake sure something similar like below "
+            f"is present in '{file_path}'\n{TEMPLATE_CODE_NEEDED_HINT}"
+        )
+        assert value > 0, message
 
 
 def test_are_settings_present(search_dict, here):
@@ -32,4 +76,4 @@ def test_are_settings_present(search_dict, here):
             if search_hit in python_file.read_text():
                 search_dict[search_hit] += 1
 
-    assert_all_hits_found(search_dict)
+    assert_all_hits_found(search_dict, str(client_sdk_dir))
