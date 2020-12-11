d6c6ccf7f03cb73bdaf226e128f32fa9ebc9886c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-12-11 17:13:26 +0100

    get nodes that are dirty

--- services/director-v2/src/simcore_service_director_v2/utils/dags.py
+++ services/director-v2/src/simcore_service_director_v2/utils/dags.py
@@ -4,6 +4,7 @@ from typing import List, Set
 import networkx as nx
 from models_library.projects import Workbench
 from models_library.projects_nodes import NodeID
+from models_library.projects_nodes_io import PortLink
 
 from .computations import NodeClass, to_node_class
 from .logging_utils import log_decorator
@@ -24,7 +25,12 @@ def create_dag_graph(workbench: Workbench) -> nx.DiGraph:
     for node_id, node in workbench.items():
         if to_node_class(node.key) == NodeClass.COMPUTATIONAL:
             dag_graph.add_node(
-                node_id, name=node.label, key=node.key, version=node.version
+                node_id,
+                name=node.label,
+                key=node.key,
+                version=node.version,
+                inputs=node.inputs,
+                outputs=node.outputs,
             )
             for input_node_id in node.input_nodes:
                 predecessor_node = workbench.get(str(input_node_id))
@@ -37,17 +43,57 @@ def create_dag_graph(workbench: Workbench) -> nx.DiGraph:
     return dag_graph
 
 
+def mark_node_dirty(graph: nx.DiGraph, node_id: NodeID):
+    graph.nodes()[str(node_id)]["dirty"] = True
+
+
+def is_node_dirty(graph: nx.DiGraph, node_id: NodeID) -> bool:
+    return graph.nodes()[str(node_id)].get("dirty", False)
+
+
+def _node_outdated(full_dag_graph: nx.DiGraph, node_id: NodeID) -> bool:
+    node = full_dag_graph.nodes(data=True)[str(node_id)]
+    # if the node has no output it is outdated for sure
+    if not node["outputs"]:
+        return True
+    for output_port in node["outputs"]:
+        if output_port is None:
+            return True
+    # ok so we have outputs, but maybe the inputs are old? let's check recursively
+    for input_port in node["inputs"]:
+        if isinstance(input_port, PortLink):
+            if is_node_dirty(full_dag_graph, input_port.node_uuid):
+                return True
+        else:
+            # FIXME: here we should check if the current inputs are the ones used to generate the current outputs
+            # this could be done by saving the inputs as metadata together with the outputs (see blockchain)
+            # we should compare the current inputs with the inputs used for generating the current outputs!!!
+            pass
+    return False
+
+
 @log_decorator(logger=logger)
 def create_minimal_graph_based_on_selection(
-    workbench: Workbench, full_dag_graph: nx.DiGraph, selected_nodes: Set[NodeID]
+    full_dag_graph: nx.DiGraph, selected_nodes: Set[NodeID]
 ) -> nx.DiGraph:
-    # find depending nodes (if their linked output is missing, they should be run as well)
-    for node in selected_nodes:
-        # get list of predecessors of that node (will return only first degree)
-        for parent_node in full_dag_graph.predecessors(node):
-            pass
 
-    return full_dag_graph.subgraph(selected_nodes)
+    # first pass, set the dirty attribute on the graph
+    for node in nx.topological_sort(full_dag_graph):
+        if node in selected_nodes or _node_outdated(full_dag_graph, node):
+            mark_node_dirty(full_dag_graph, node)
+
+    # now we want all the outdated nodes that are in the tree from the selected nodes
+    minimal_selection_nodes: Set[NodeID] = set()
+    for node in selected_nodes:
+        minimal_selection_nodes.update(
+            set(
+                n
+                for n in nx.bfs_tree(full_dag_graph, str(node), reverse=True)
+                if is_node_dirty(full_dag_graph, n)
+            )
+        )
+
+    return full_dag_graph.subgraph(minimal_selection_nodes)
 
 
 @log_decorator(logger=logger)
