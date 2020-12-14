cf0b6f677cd18066be484f8602ac26ae3cfb32fb
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-12-14 13:59:40 +0100

    linter

--- services/director-v2/src/simcore_service_director_v2/api/routes/computations.py
+++ services/director-v2/src/simcore_service_director_v2/api/routes/computations.py
@@ -1,5 +1,4 @@
 import logging
-import pdb
 from typing import List
 
 import networkx as nx
@@ -34,11 +33,7 @@ from ...utils.computations import (
     is_pipeline_running,
     is_pipeline_stopped,
 )
-from ...utils.dags import (
-    create_dag_graph,
-    create_minimal_graph_based_on_selection,
-    find_entrypoints,
-)
+from ...utils.dags import create_dag_graph, create_minimal_graph_based_on_selection
 from ...utils.exceptions import ProjectNotFoundError
 from ..dependencies.celery import CeleryClient, get_celery_client
 from ..dependencies.database import get_repository
