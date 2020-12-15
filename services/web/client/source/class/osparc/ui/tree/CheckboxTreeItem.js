21b42e0b08c43eef64fe2be91b9b3e9d75490cbb
odeimaiz <maiz@itis.swiss>
2020-12-15 16:47:16 +0100

    render description + url

--- services/web/client/source/class/osparc/ui/tree/CheckboxTreeItem.js
+++ services/web/client/source/class/osparc/ui/tree/CheckboxTreeItem.js
@@ -21,6 +21,7 @@ qx.Class.define("osparc.ui.tree.CheckboxTreeItem", {
       check: "String",
       init: null,
       event: "changeDescription",
+      apply: "__recreateInfoButton",
       nullable: true
     },
 
@@ -28,6 +29,7 @@ qx.Class.define("osparc.ui.tree.CheckboxTreeItem", {
       check: "String",
       init: null,
       event: "changeUrl",
+      apply: "__recreateInfoButton",
       nullable: true
     }
   },
@@ -35,13 +37,17 @@ qx.Class.define("osparc.ui.tree.CheckboxTreeItem", {
   events: {
     checkboxClicked: "qx.event.type.Event"
   },
+
   members: {
+    __infoButton: null,
+
     _addWidgets: function() {
       this.addSpacer();
       this.addOpenButton();
       this._add(this.getChildControl("checkbox"));
       this.addLabel();
     },
+
     _createChildControlImpl: function(id) {
       let control;
       switch (id) {
@@ -51,8 +57,32 @@ qx.Class.define("osparc.ui.tree.CheckboxTreeItem", {
           control.bind("value", this, "checked");
           this.bind("checked", control, "value");
           control.addListener("tap", () => this.fireEvent("checkboxClicked"));
+          break;
       }
       return control || this.base(arguments, id);
+    },
+
+    __recreateInfoButton: function() {
+      if (this.__infoButton) {
+        this._remove(this.__infoButton);
+      }
+      const desc = this.getDescription();
+      const url = this.getUrl();
+      const hints = [];
+      if (desc !== "" && desc !== null) {
+        hints.push(desc);
+      }
+      if (url !== "" && url !== null) {
+        const link = "<a href=" + url + " target='_blank'>More...</a>";
+        hints.push(link);
+      }
+      if (hints.length) {
+        const hint = hints.join("<br>");
+        this.__infoButton = new osparc.component.form.FieldWHint("", hint, new qx.ui.basic.Label("")).set({
+          maxWidth: 150
+        });
+        this._add(this.__infoButton);
+      }
     }
   }
 });
