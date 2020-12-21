db93b33cea2238bb3647d1d5c8a7a304eb06871e
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-12-21 07:28:03 +0100

    adding tests

---
+++ services/web/server/tests/unit/isolated/test_scicrunch_models.py
@@ -0,0 +1,16 @@
+# pylint:disable=unused-variable
+# pylint:disable=unused-argument
+# pylint:disable=redefined-outer-name
+
+import pytest
+from simcore_service_webserver.scicrunch.scicrunch_models import (
+    ListOfResourceHits,
+    ResearchResource,
+    ResearchResourceAtdB,
+    ResourceView,
+)
+
+
+def test_models_conversion():
+    assert True
+    pass
