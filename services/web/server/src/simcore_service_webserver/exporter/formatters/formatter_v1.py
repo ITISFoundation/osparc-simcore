6ac2bfaca8623ebaa9e2d5228d77821aa40ec3db
Andrei Neagu <neagu@itis.swiss>
2020-12-23 09:16:55 +0100

    moved LinkAndFile to Pydantic model

--- services/web/server/src/simcore_service_webserver/exporter/formatters/formatter_v1.py
+++ services/web/server/src/simcore_service_webserver/exporter/formatters/formatter_v1.py
@@ -4,7 +4,6 @@ import logging
 from itertools import chain
 from collections import deque
 from typing import Deque
-from collections import namedtuple
 from pathlib import Path
 from aiohttp import web
 
@@ -19,37 +18,42 @@ from simcore_service_webserver.storage_handlers import (
     get_project_files_metadata,
 )
 
+from .models import LinkAndPath2
 
 log = logging.getLogger(__name__)
 
 
-# used in the future, will change if export format changes
-EXPORT_VERSION = "1"
-KEYS_TO_VALIDATE = {"store", "path", "dataset", "label"}
-
-LinkAndPath = namedtuple("LinkAndPath", ["link", "path"])
-
-
-async def ensure_files_downloaded(
-    download_links: Deque[LinkAndPath], storage_path: Path
-):
-    """ Downloads links to files in storage_path """
+async def ensure_files_downloaded(download_links: Deque[LinkAndPath2]) -> None:
+    """ Downloads links to files in their designed store_path """
     # TODO: use utility
     parallel_downloader = ParallelDownloader()
     for link_and_path in download_links:
-        download_path = storage_path / link_and_path.path
-        log.info("Will download %s -> '%s'", link_and_path.link, download_path)
+        log.info(
+            "Will download %s -> '%s'",
+            link_and_path.download_link,
+            link_and_path.store_path,
+        )
         await parallel_downloader.append_file(
-            link=link_and_path.link, download_path=download_path
+            link=link_and_path.download_link, download_path=link_and_path.store_path
         )
 
     await parallel_downloader.download_files()
 
+    # check all files have been downloaded
+    for link_and_path in download_links:
+        if not link_and_path.is_file():
+            raise web.HTTPException(
+                reason=(
+                    f"Could not download file {link_and_path.download_link} "
+                    f"to {link_and_path.store_path}"
+                )
+            )
+
 
 async def extract_download_links(
-    app: web.Application, project_id: str, user_id: int
-) -> Deque[LinkAndPath]:
-    download_links: Deque[LinkAndPath] = deque()
+    app: web.Application, dir_path: Path, project_id: str, user_id: int
+) -> Deque[LinkAndPath2]:
+    download_links: Deque[LinkAndPath2] = deque()
 
     s3_metadata = await get_project_files_metadata(
         app=app,
@@ -77,7 +81,13 @@ async def extract_download_links(
             user_id=user_id,
         )
         save_path = Path(file_metadata["location_id"]) / file_metadata["raw_file_path"]
-        download_links.append(LinkAndPath(link=download_link, path=str(save_path)))
+        download_links.append(
+            LinkAndPath2(
+                root_dir=dir_path,
+                storage_path_to_file=str(save_path),
+                download_link=download_link,
+            )
+        )
 
     return download_links
 
@@ -85,8 +95,6 @@ async def extract_download_links(
 async def generate_directory_contents(
     app: web.Application, dir_path: Path, project_id: str, user_id: int, version: str
 ) -> None:
-    storage_path = dir_path / "storage"
-
     project_data = await get_project_for_user(
         app=app,
         project_uuid=project_id,
@@ -97,17 +105,18 @@ async def generate_directory_contents(
 
     log.info("Project data: %s", project_data)
 
-    download_links: Deque[LinkAndPath] = await extract_download_links(
-        app=app, project_id=project_id, user_id=user_id
+    download_links: Deque[LinkAndPath2] = await extract_download_links(
+        app=app, dir_path=dir_path, project_id=project_id, user_id=user_id
     )
 
     # make sure all files from storage services are persisted on disk
-    await ensure_files_downloaded(
-        download_links=download_links, storage_path=storage_path
-    )
+    await ensure_files_downloaded(download_links=download_links)
 
     # store manifest on disk
-    manifest_params = dict(version=version)
+    manifest_params = dict(
+        version=version,
+        attachments=[str(x.storage_path_to_file) for x in download_links],
+    )
     await Manifest.model_to_file(root_dir=dir_path, **manifest_params)
     # store project data on disk
     project_params = project_data
@@ -137,7 +146,20 @@ class FormatterV1(BaseFormatter):
 
         project = await Project.model_from_file(root_dir=self.root_folder)
 
-        # validate files as well
+        # check all attachments are present
+        manifest = await Manifest.model_from_file(root_dir=self.root_folder)
+        for attachment in manifest.attachments:
+            link_and_path = LinkAndPath2(
+                root_dir=self.root_folder,
+                storage_path_to_file=attachment,
+                download_link="",
+            )
+            if not link_and_path.is_file():
+                raise web.HTTPException(
+                    reason=(
+                        f"Could not find {link_and_path.download_link} in import document"
+                    )
+                )
 
         log.info("Loaded project data: %s", project)
 
@@ -145,4 +167,3 @@ class FormatterV1(BaseFormatter):
 # TODO: create Pydantic models for:
 #   - directory_storage_files
 # TODO: models must switch uuid for project and nodes
-# TODO: add list of downloaded files to the manifest
--- services/web/server/src/simcore_service_webserver/exporter/formatters/models.py
+++ services/web/server/src/simcore_service_webserver/exporter/formatters/models.py
@@ -1,9 +1,52 @@
 from datetime import datetime
+from pathlib import Path
+from typing import Dict, Any, List
 
-from typing import Dict, Any
-from pydantic import Field, validator, EmailStr
+from pydantic import Field, validator, EmailStr, BaseModel
 
 from .base_models import BaseLoadingModel
+
+
+class LinkAndPath2(BaseModel):
+    _FILES_DIRECTORY: str = "storage"  # where all files are stored
+    root_dir: Path = Field(
+        ...,
+        description="temporary directory where all data is stored, to be ignored from serialization",
+    )
+    storage_path_to_file: Path = Field(
+        ...,
+        description="full path to where the file is going to be stored",
+    )
+
+    download_link: str = Field(..., description="Link from where to download the file")
+
+    @validator("root_dir")
+    @classmethod
+    def _validate_root_dir(cls, v):
+        if not isinstance(v, Path):
+            v = Path(v)
+        if not v.is_dir():
+            raise ValueError(f"Provided path {str(v)} is not a directory!")
+        return v
+
+    @validator("storage_path_to_file")
+    @classmethod
+    def _validate_path_in_root_dir(cls, v):
+        if not isinstance(v, Path):
+            v = Path(v)
+        if v.is_absolute():
+            raise ValueError(f"Must provide a relative path, not {str(v)}")
+
+        return v
+
+    @property
+    def store_path(self) -> Path:
+        """Returns an absolute path to the file"""
+        return self.root_dir / self._FILES_DIRECTORY / self.storage_path_to_file
+
+    async def is_file(self) -> bool:
+        """Checks if the file was saved at the given link"""
+        return self.store_path.is_file()
 
 
 class Manifest(BaseLoadingModel):
@@ -20,6 +63,10 @@ class Manifest(BaseLoadingModel):
     creation_date_utc: datetime = Field(
         description="UTC date and time from when the project was exported",
         default_factory=datetime.utcnow,
+    )
+
+    attachments: List[str] = Field(
+        ..., description="list of paths for attachments found in the project directory"
     )
 
     @validator("version")
@@ -51,5 +98,3 @@ class Project(BaseLoadingModel):
         ...,
         description="representation all the information required to run and render studies",
     )
-
-    # TODO: also use uuid from project which needs to get mixed
