b3e51d63f3d72c864ec0a2e6226f46c905378197
odeimaiz <maiz@itis.swiss>
2021-01-06 17:04:55 +0100

    attachQualityToObject to studies

--- services/web/client/source/class/osparc/dashboard/StudyBrowser.js
+++ services/web/client/source/class/osparc/dashboard/StudyBrowser.js
@@ -108,6 +108,9 @@ qx.Class.define("osparc.dashboard.StudyBrowser", {
         return new Promise((resolve, reject) => {
           osparc.data.Resources.get("studies", null, false)
             .then(studies => {
+              studies.forEach(study => {
+                osparc.component.metadata.Quality.attachQualityToObject(study);
+              });
               this.__resetStudyList(studies);
               this.resetSelection();
               resolve(studies);
