64466629857e1aadd8ed45d3c2369f1dac4e33a7
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-01-07 17:58:03 +0100

    Updates python requirements and classifier in  setup

--- services/director-v2/setup.py
+++ services/director-v2/setup.py
@@ -6,9 +6,9 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
-if sys.version_info.major != 3 and sys.version_info.minor >= 6:
+if not (sys.version_info.major == 3 and sys.version_info.minor == 8):
     raise RuntimeError(
-        "Expected ~=3.6, got %s (Tip: did you forget to 'source .venv/bin/activate' or 'pyenv local'?)"
+        "Expected ~=3.8, got %s (Tip: did you forget to 'source .venv/bin/activate' or 'pyenv local'?)"
         % str(sys.version_info)
     )
 
@@ -41,7 +41,7 @@ setup(
         "Development Status :: 1 - Planning",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
     ],
     long_description=readme,
     license="MIT license",
