152d747b936af6ed3c70274154abe7cdf10932ba
Andrei Neagu <neagu@itis.swiss>
2021-01-07 11:02:15 +0100

    added failing test which is skipped

---
+++ services/web/server/tests/integration/test_exporter.py
@@ -0,0 +1,197 @@
+# pylint:disable=redefined-outer-name,unused-argument,too-many-arguments
+import asyncio
+import logging
+import sys
+import re
+from copy import deepcopy
+from typing import Dict, List
+from pathlib import Path
+
+import aiopg
+import aioredis
+import aiohttp
+import pytest
+from aioresponses import aioresponses
+from yarl import URL
+from models_library.projects_state import RunningState
+from models_library.settings.redis import RedisConfig
+from pytest_simcore.helpers.utils_login import log_client_in
+from servicelib.application import create_safe_application
+from simcore_service_webserver.db import setup_db
+from simcore_service_webserver.director import setup_director
+from simcore_service_webserver.director_v2 import setup_director_v2
+from simcore_service_webserver.login import setup_login
+from simcore_service_webserver.projects import setup_projects
+from simcore_service_webserver.resource_manager import setup_resource_manager
+from simcore_service_webserver.rest import setup_rest
+from simcore_service_webserver.security import setup_security
+from simcore_service_webserver.security_roles import UserRole
+from simcore_service_webserver.session import setup_session
+from simcore_service_webserver.socketio import setup_socketio
+from simcore_service_webserver.users import setup_users
+
+log = logging.getLogger(__name__)
+
+core_services = [
+    "redis",
+    "rabbit",
+    "director",
+    "director-v2",
+    "postgres",
+    "storage",
+]
+ops_services = ["minio", "adminer"]
+
+
+CURRENT_DIR = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+
+API_VERSION = "v0"
+API_PREFIX = "/" + API_VERSION
+
+SUPPORTED_EXPORTER_VERSIONS = {"v1"}
+
+
+@pytest.fixture
+async def db_engine(postgres_dsn: Dict) -> aiopg.sa.Engine:
+    dsn = "postgresql://{user}:{password}@{host}:{port}/{database}".format(
+        **postgres_dsn
+    )
+    return await aiopg.sa.create_engine(dsn)
+
+
+@pytest.fixture(autouse=True)
+def __drop_and_recreate_postgres__(database_from_template_before_each_function) -> None:
+    yield
+
+
+@pytest.fixture(autouse=True)
+async def __delete_all_redis_keys__(redis_service: RedisConfig):
+    client = await aioredis.create_redis_pool(redis_service.dsn, encoding="utf-8")
+    await client.flushall()
+    client.close()
+    await client.wait_closed()
+
+    yield
+    # do nothing on teadown
+
+
+@pytest.fixture
+async def director_v2_service_mock() -> aioresponses:
+    """uses aioresponses to mock all calls of an aiohttpclient
+    WARNING: any request done through the client will go through aioresponses. It is
+    unfortunate but that means any valid request (like calling the test server) prefix must be set as passthrough.
+    Other than that it seems to behave nicely
+    """
+    PASSTHROUGH_REQUESTS_PREFIXES = ["http://127.0.0.1", "ws://"]
+    get_computation_pattern = re.compile(
+        r"^http://[a-z\-_]*director-v2:[0-9]+/v2/computations/.*$"
+    )
+    delete_computation_pattern = get_computation_pattern
+    # NOTE: GitHK I have to copy paste that fixture for some unclear reason for now.
+    # I think this is due to some conflict between these non-pytest-simcore fixtures and the loop fixture being defined at different locations?? not sure..
+    # anyway I think this should disappear once the garbage collector moves to its own micro-service
+    with aioresponses(passthrough=PASSTHROUGH_REQUESTS_PREFIXES) as mock:
+        mock.get(
+            get_computation_pattern,
+            status=202,
+            payload={"state": str(RunningState.NOT_STARTED.value)},
+            repeat=True,
+        )
+        mock.delete(delete_computation_pattern, status=204, repeat=True)
+        yield mock
+
+
+@pytest.fixture(autouse=True)
+async def auto_mock_director_v2(
+    director_v2_service_mock: aioresponses,
+) -> aioresponses:
+    return director_v2_service_mock
+
+
+@pytest.fixture
+def client(
+    loop, aiohttp_client, app_config, postgres_with_template_db, mock_orphaned_services
+):
+    cfg = deepcopy(app_config)
+
+    assert cfg["rest"]["version"] == API_VERSION
+    assert cfg["rest"]["enabled"]
+    cfg["projects"]["enabled"] = True
+    cfg["director"]["enabled"] = True
+
+    # fake config
+    app = create_safe_application(cfg)
+
+    # activates only security+restAPI sub-modules
+    setup_db(app)
+    setup_session(app)
+    setup_security(app)
+    setup_rest(app)
+    setup_login(app)
+    setup_users(app)
+    setup_socketio(app)
+    setup_projects(app)
+    setup_director(app)
+    setup_director_v2(app)
+    assert setup_resource_manager(app)
+
+    yield loop.run_until_complete(
+        aiohttp_client(
+            app,
+            server_kwargs={"port": cfg["main"]["port"], "host": cfg["main"]["host"]},
+        )
+    )
+
+
+################ utils
+
+
+async def login_user(client):
+    """returns a logged in regular user"""
+    return await log_client_in(client=client, user_data={"role": UserRole.USER.name})
+
+
+def get_exported_projects() -> List[Path]:
+    exporter_dir = CURRENT_DIR / ".." / "data" / "exporter"
+    return [x for x in exporter_dir.glob("*.osparc")]
+
+
+################ end utils
+
+
+@pytest.mark.skip(
+    reason="There is an issue, need python 3.8 to make it work see comments in test body"
+)
+@pytest.mark.parametrize("export_version", get_exported_projects())
+async def test_import_export_import(
+    client, socketio_client, db_engine, redis_client, export_version
+):
+    """Check that the full import -> export -> import cycle produces the same result in the DB"""
+
+    # TODO: The below bug is not allowing me to fully test
+    # Already tried to patch the current system module at runtime
+    # withing a wrapper script before calling pytest but it dose not work
+    # https://bugs.python.org/issue35621
+
+    logged_user = await login_user(client)
+    export_file_name = export_version.name
+    version_from_name = export_file_name.split("#")[0]
+    assert version_from_name in SUPPORTED_EXPORTER_VERSIONS
+
+    url_import = client.app.router["import_project"].url_for()
+
+    assert url_import == URL(API_PREFIX + "/projects/import")
+
+    data = aiohttp.FormData()
+    data.add_field(
+        "fileName",
+        open(export_version, "rb"),
+        filename=export_file_name,
+        content_type="application/octest-stream",
+    )
+
+    async with await client.post(url_import, data=data, timeout=10) as import_response:
+        assert import_response.status == 200, await import_response.text()
+
+    # TODO: this test is not finished and needs to be continued
+    assert False
