12e371fa95314e87c5c6b7a12a7dd24292bffe40
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-01-07 21:54:06 +0100

    pytst-simcore has to be installed in director services as well

--- packages/pytest-simcore/setup.py
+++ packages/pytest-simcore/setup.py
@@ -6,7 +6,8 @@ setup(
     maintainer="pcrespov, sanderegg",
     description="pytest plugin with fixtures and test helpers for osparc-simcore repo modules",
     py_modules=["pytest_simcore"],
-    python_requires="~=3.8",
+    # WARNING: this is used in frozen services as well
+    python_requires=">=3.6",
     # TODO create partial extensions:
     install_requires=["pytest>=3.5.0"],
     extras_require={
