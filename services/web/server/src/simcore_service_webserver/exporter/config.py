435501b20eff704c9efdb1a739cecf9096fb0a5a
Andrei Neagu <neagu@itis.swiss>
2021-01-08 15:15:44 +0100

    trying to tix schema

--- services/web/server/src/simcore_service_webserver/exporter/config.py
+++ services/web/server/src/simcore_service_webserver/exporter/config.py
@@ -5,7 +5,12 @@ from servicelib.application_keys import APP_CONFIG_KEY
 
 CONFIG_SECTION_NAME = "exporter"
 
-schema = T.Dict({T.Key("max_upload_file_size_gb", default=10, optional=False): T.Int()})
+schema = T.Dict(
+    {
+        T.Key("max_upload_file_size_gb", default=10, optional=False): T.Int(),
+        T.Key("enabled", default=True, optional=False): T.Bool(),
+    }
+)
 
 
 def get_max_upload_file_size_gb(app: Application) -> int:
