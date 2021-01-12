293ba96bc05810ce0e2b81906d39a70c4dd50a7f
odeimaiz <maiz@itis.swiss>
2021-01-12 17:36:07 +0100

    Create Kember.js

---
+++ tests/e2e/portal/Kember.js
@@ -0,0 +1,49 @@
+// node kember.js [url_prefix] [template_uuid] [--demo]
+
+const tutorialBase = require('../tutorials/tutorialBase');
+const utils = require('../utils/utils');
+
+const args = process.argv.slice(2);
+const {
+  urlPrefix,
+  templateUuid,
+  enableDemoMode
+} = utils.parseCommandLineArgumentsTemplate(args);
+
+const anonURL = urlPrefix + templateUuid;
+const screenshotPrefix = "Kember";
+
+
+async function runTutorial () {
+  const tutorial = new tutorialBase.TutorialBase(anonURL, screenshotPrefix, null, null, null, enableDemoMode);
+
+  tutorial.startScreenshooter();
+  const page = await tutorial.beforeScript();
+  const studyData = await tutorial.openStudyLink();
+  const studyId = studyData["data"]["uuid"];
+  console.log("Study ID:", studyId);
+
+  // Some time for loading the workbench
+  await tutorial.waitFor(10000);
+  await utils.takeScreenshot(page, screenshotPrefix + 'workbench_loaded');
+
+  await tutorial.runPipeline(studyId, 30000);
+  await utils.takeScreenshot(page, screenshotPrefix + 'pipeline_run');
+
+  await tutorial.openNodeFiles(0);
+  const outFiles = [
+    "logs.zip",
+    "outputController.dat"
+  ];
+  await tutorial.checkResults(outFiles.length);
+
+  await tutorial.logOut();
+  tutorial.stopScreenshooter();
+  await tutorial.close();
+}
+
+runTutorial()
+  .catch(error => {
+    console.log('Puppeteer error: ' + error);
+    process.exit(1);
+  });
