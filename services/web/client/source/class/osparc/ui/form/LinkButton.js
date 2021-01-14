64f9f9a15a0e43534979ef7daf62199176e58b8a
odeimaiz <maiz@itis.swiss>
2021-01-14 16:11:30 +0100

    LinkButton supports icon

--- services/web/client/source/class/osparc/ui/form/LinkButton.js
+++ services/web/client/source/class/osparc/ui/form/LinkButton.js
@@ -29,14 +29,14 @@
  */
 
 qx.Class.define("osparc.ui.form.LinkButton", {
-  extend: osparc.ui.form.FetchButton,
+  extend: qx.ui.form.Button,
 
   /**
     * @param label {String} Label to use
+    * @param icon {String} Icon to use
     * @param url {String} Url to point to
-    * @param height {Integer?12} Height of the link icon
   */
-  construct: function(label, url, height = 12) {
+  construct: function(label, icon, url) {
     this.base(arguments, label);
 
     this.set({
@@ -45,10 +45,13 @@ qx.Class.define("osparc.ui.form.LinkButton", {
     });
 
     if (url) {
-      this.setIcon("@FontAwesome5Solid/external-link-alt/" + height);
+      this.setIcon("@FontAwesome5Solid/external-link-alt/12");
       this.addListener("execute", () => {
         window.open(url);
       }, this);
+    }
+    if (icon) {
+      this.setIcon(icon);
     }
   }
 });
