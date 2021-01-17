48f03bb0da0dd436f1e3bd69bcc02b4a178c95c9
odeimaiz <maiz@itis.swiss>
2021-01-17 17:34:25 +0100

    Update isolve.js

--- tests/e2e/tutorials/isolve.js
+++ tests/e2e/tutorials/isolve.js
@@ -29,10 +29,11 @@ async function runTutorial() {
     // Some time for loading the workbench
     await tutorial.waitFor(5000);
 
-    await tutorial.runPipeline(studyId, 60000);
+    await tutorial.runPipeline(studyId, 20000);
     console.log('Checking isolve results:');
     await tutorial.openNodeFiles(1);
     const outFiles = [
+      "logs.zip",
       "output.h5",
       "log.tgz"
     ];
