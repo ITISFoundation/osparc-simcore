8421f500684b3f645ed8ddd84639a328dba39a3f
Andrei Neagu <neagu@itis.swiss>
2021-01-18 14:33:45 +0100

    refactored fucntion names

--- services/sidecar/src/simcore_service_sidecar/mpi_lock.py
+++ services/sidecar/src/simcore_service_sidecar/mpi_lock.py
@@ -22,13 +22,13 @@ from . import config
 
 # ptsv cause issues with multiprocessing
 # SEE: https://github.com/microsoft/ptvsd/issues/1443
-if os.environ.get("SC_BOOT_MODE") == "debug-ptvsd":
+if os.environ.get("SC_BOOT_MODE") == "debug-ptvsd":  # pragma: no cover
     multiprocessing.set_start_method("spawn", True)
 
 logger = logging.getLogger(__name__)
 
 
-async def retry_for_result(
+async def _retry_for_result(
     result_validator: Callable[[Any], Any], coroutine_factory: Callable
 ) -> Tuple[bool, Any]:
     """
@@ -49,7 +49,7 @@ async def retry_for_result(
     return False, None
 
 
-async def try_to_acquire_lock(
+async def _try_to_acquire_lock(
     lock_manager: Aioredlock, resource_name: str
 ) -> Optional[Tuple[bool, Lock]]:
     # Try to acquire the lock:
@@ -68,18 +68,18 @@ async def wrapped_acquire_and_extend_lock_worker(
 ) -> None:
     try:
         # if the lock is acquired the above function will block here
-        await acquire_and_extend_lock_forever(reply_queue, cpu_count)
+        await _acquire_and_extend_lock_forever(reply_queue, cpu_count)
     except Exception:  # pylint: disable=broad-except
         logger.exception("An exception ocurred while trying to acquire the mpi lock")
         reply_queue.put(False)
     finally:
-        # if the acquire_and_extend_lock_forever function returns
+        # if the _acquire_and_extend_lock_forever function returns
         # the lock was not acquired, need to make sure the acquire_mpi_lock
         # always has a result to avoid issues
         reply_queue.put(False)
 
 
-async def acquire_and_extend_lock_forever(
+async def _acquire_and_extend_lock_forever(
     reply_queue: multiprocessing.Queue, cpu_count: int
 ) -> None:
     resource_name = f"aioredlock:mpi_lock:{cpu_count}"
@@ -89,7 +89,7 @@ async def acquire_and_extend_lock_forever(
     def is_locked_factory() -> bool:
         return lock_manager.is_locked(resource_name)
 
-    is_lock_free, _ = await retry_for_result(
+    is_lock_free, _ = await _retry_for_result(
         result_validator=lambda x: x is False,
         coroutine_factory=is_locked_factory,
     )
@@ -99,10 +99,10 @@ async def acquire_and_extend_lock_forever(
         return
 
     def try_to_acquire_lock_factory() -> bool:
-        return try_to_acquire_lock(lock_manager, resource_name)
+        return _try_to_acquire_lock(lock_manager, resource_name)
 
     # lock is free try to acquire and start background extention
-    managed_to_acquire_lock, lock = await retry_for_result(
+    managed_to_acquire_lock, lock = await _retry_for_result(
         result_validator=lambda x: type(x) == Lock,
         coroutine_factory=try_to_acquire_lock_factory,
     )
@@ -119,7 +119,7 @@ async def acquire_and_extend_lock_forever(
         await asyncio.sleep(sleep_interval)
 
 
-def process_worker(queue: multiprocessing.Queue, cpu_count: int) -> None:
+def _process_worker(queue: multiprocessing.Queue, cpu_count: int) -> None:
     logger.info("Starting background process for mpi lock result")
     asyncio.get_event_loop().run_until_complete(
         wrapped_acquire_and_extend_lock_worker(queue, cpu_count)
@@ -135,12 +135,8 @@ def acquire_mpi_lock(cpu_count: int) -> bool:
     """
     reply_queue = multiprocessing.Queue()
     multiprocessing.Process(
-        target=process_worker, args=(reply_queue, cpu_count), daemon=True
+        target=_process_worker, args=(reply_queue, cpu_count), daemon=True
     ).start()
 
-    response = reply_queue.get()
-    if not isinstance(response, bool):
-        logger.error("Expected a boolean response got %s %s", type(response), response)
-        return False
-
-    return response
+    lock_acquired = reply_queue.get()
+    return lock_acquired
