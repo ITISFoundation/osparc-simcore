df93ae3a26d6368c63a0070ad8226d239502b5c6
Andrei Neagu <neagu@itis.swiss>
2021-01-19 17:22:01 +0100

    downloader timeout bumped to 1h max

--- services/web/server/src/simcore_service_webserver/exporter/file_downloader.py
+++ services/web/server/src/simcore_service_webserver/exporter/file_downloader.py
@@ -10,6 +10,8 @@ from .utils import makedirs
 
 log = logging.getLogger(__name__)
 
+DOWNLOAD_TIMEOUT_ONE_HOUR: int = 60 * 60  # seconds
+
 if parfive.__version__ != "1.0.2":
     raise RuntimeError(
         "Parfive was upgraded, please make sure this version supports "
@@ -38,7 +40,12 @@ class ParallelDownloader:
 
         # run this async, parfive will support aiofiles in the future as stated above
         wrapped_function = aiofiles_os.wrap(self.downloader.download)
-        results = await wrapped_function()
+        results = await wrapped_function(
+            timeouts={
+                "total": DOWNLOAD_TIMEOUT_ONE_HOUR,
+                "sock_read": 90,  # default as in parfive code
+            }
+        )
         log.debug("Download results %s", results)
 
         if len(results) != self.total_files_added:
