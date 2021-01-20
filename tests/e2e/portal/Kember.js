a70739f6337e97e8d7a1ed627090b54a0441d0af
odeimaiz <maiz@itis.swiss>
2021-01-20 10:57:37 +0100

    Checking Kember's viewer output

--- tests/e2e/portal/Kember.js
+++ tests/e2e/portal/Kember.js
@@ -1,6 +1,7 @@
 // node kember.js [url_prefix] [template_uuid] [--demo]
 
 const tutorialBase = require('../tutorials/tutorialBase');
+const auto = require('../utils/auto');
 const utils = require('../utils/utils');
 
 const args = process.argv.slice(2);
@@ -27,7 +28,7 @@ async function runTutorial () {
   await tutorial.waitFor(10000);
   await utils.takeScreenshot(page, screenshotPrefix + 'workbench_loaded');
 
-  await tutorial.runPipeline(studyId, 30000);
+  await tutorial.runPipeline(studyId, 120000);
   await utils.takeScreenshot(page, screenshotPrefix + 'pipeline_run');
 
   await tutorial.openNodeFiles(0);
@@ -37,10 +38,40 @@ async function runTutorial () {
   ];
   await tutorial.checkResults(outFiles.length);
 
-  // TODO when kember viewer gets fixed:
-  // - get the frame from the second node
-  // - rerun all cells
+
+  // open kember viewer
+  auto.openNode(page, 1);
+
+  await tutorial.waitFor(2000);
+  await utils.takeScreenshot(page, screenshotPrefix + 'iFrame0');
+  const iframeHandles = await page.$$("iframe");
+  // expected two iframes = loading + raw-graph
+  const frame = await iframeHandles[1].contentFrame();
+
+  // - restart kernel: click restart and accept
+  const restartSelector = "#run_int > button:nth-child(3)";
+  await frame.waitForSelector(restartSelector);
+  await frame.click(restartSelector);
+  await tutorial.waitFor(2000);
+  await utils.takeScreenshot(page, screenshotPrefix + 'restart_pressed');
+  const acceptSelector = "body > div.modal.fade.in > div > div > div.modal-footer > button.btn.btn-default.btn-sm.btn-danger";
+  await frame.waitForSelector(acceptSelector);
+  await frame.click(acceptSelector);
+  await tutorial.waitFor(2000);
+  await utils.takeScreenshot(page, screenshotPrefix + 'restart_accept');
+
+  await tutorial.waitFor(8000);
+  await utils.takeScreenshot(page, screenshotPrefix + 'notebook_run');
+
   // - check output
+  await tutorial.openNodeFiles(0);
+  const outFiles2 = [
+    "Hear_Rate.csv",
+    "notebooks.zip",
+    "Parasympathetic_Cell_Activity.csv",
+    "Table_Data.csv"
+  ];
+  await tutorial.checkResults(outFiles2.length);
 
   await tutorial.logOut();
   tutorial.stopScreenshooter();
