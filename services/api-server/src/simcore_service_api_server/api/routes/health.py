7b9f08110eb9ba221e1bdfe316fcc78e1f5db889
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-01-20 19:57:43 +0100

    Using UTC overall

--- services/api-server/src/simcore_service_api_server/api/routes/health.py
+++ services/api-server/src/simcore_service_api_server/api/routes/health.py
@@ -8,4 +8,4 @@ router = APIRouter()
 
 @router.get("/", include_in_schema=False, response_class=PlainTextResponse)
 async def check_service_health():
-    return f"{__name__}@{datetime.now().isoformat()}"
+    return f"{__name__}@{datetime.utcnow().isoformat()}"
