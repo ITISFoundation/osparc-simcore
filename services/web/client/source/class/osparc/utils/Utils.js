c6ab3fdc4286bbf291f1f6338437c13938cba24e
odeimaiz <maiz@itis.swiss>
2021-01-25 18:02:45 +0100

    Notify when download starts

--- services/web/client/source/class/osparc/utils/Utils.js
+++ services/web/client/source/class/osparc/utils/Utils.js
@@ -203,6 +203,15 @@ qx.Class.define("osparc.utils.Utils", {
         let xhr = new XMLHttpRequest();
         xhr.open("GET", url, true);
         xhr.responseType = "blob";
+        xhr.addEventListener("progress", () => {
+          if (xhr.readyState === XMLHttpRequest.LOADING) {
+            if (xhr.status === 0 || (xhr.status >= 200 && xhr.status < 400)) {
+              resolve();
+            } else {
+              reject();
+            }
+          }
+        });
         xhr.addEventListener("load", () => {
           if (xhr.status == 200) {
             let blob = new Blob([xhr.response]);
