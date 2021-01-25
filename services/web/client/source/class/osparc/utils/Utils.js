4b1215ea5f2490547349218369737ef2d8244f32
odeimaiz <maiz@itis.swiss>
2021-01-25 17:32:22 +0100

    minor

--- services/web/client/source/class/osparc/utils/Utils.js
+++ services/web/client/source/class/osparc/utils/Utils.js
@@ -232,7 +232,7 @@ qx.Class.define("osparc.utils.Utils", {
         const filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
         const matches = filenameRegex.exec(disposition);
         if (matches != null && matches[1]) {
-          filename = matches[1].replace(/['"]/g, '');
+          filename = matches[1].replace(/['"]/g, "");
         }
       }
       return filename;
