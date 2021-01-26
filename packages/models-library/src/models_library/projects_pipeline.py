353dc94da2e13156ade003e77f18e5a24b0a9bbc
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-01-26 15:28:38 +0100

    moved ComputationTask model to models-library

--- packages/models-library/src/models_library/projects_pipeline.py
+++ packages/models-library/src/models_library/projects_pipeline.py
@@ -1,8 +1,10 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
+from uuid import UUID
 
 from pydantic import BaseModel, Field
 
 from .projects_nodes import NodeID, NodeState
+from .projects_state import RunningState
 
 
 class PipelineDetails(BaseModel):
@@ -11,4 +13,18 @@ class PipelineDetails(BaseModel):
     )
     node_states: Dict[NodeID, NodeState] = Field(
         ..., description="The states of each of the pipeline node"
+    )
+
+
+TaskID = UUID
+
+
+class ComputationTask(BaseModel):
+    id: TaskID = Field(..., description="the id of the computation task")
+    state: RunningState = Field(..., description="the state of the computational task")
+    result: Optional[str] = Field(
+        None, description="the result of the computational task"
+    )
+    pipeline_details: PipelineDetails = Field(
+        ..., description="the details of the generated pipeline"
     )
