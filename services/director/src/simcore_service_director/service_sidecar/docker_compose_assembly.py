593303c3cb8282b5858bfb4f72f0f97c564892c6
Andrei Neagu <neagu@itis.swiss>
2021-01-27 18:00:04 +0100

    new spec composition

--- services/director/src/simcore_service_director/service_sidecar/docker_compose_assembly.py
+++ services/director/src/simcore_service_director/service_sidecar/docker_compose_assembly.py
@@ -1,11 +1,23 @@
-MOCK_COMPOSE_SPEC = """
-version: "3.7"
-services:
-  whocontainer:
-    image: "containous/whoami"
-"""
+from copy import deepcopy
+from typing import Dict, Any
+
+import yaml
+
+
+BASE_SERVICE_SPEC: Dict[str, Any] = {
+    "version": "3.7",
+    "services": {},
+}
 
 
 async def assemble_spec(service_key: str, service_tag: str) -> str:
     """returns a docker-compose spec which will be use by the service-sidecar to start the service """
-    return MOCK_COMPOSE_SPEC
+    service_spec = deepcopy(BASE_SERVICE_SPEC)
+    service_spec["services"] = {
+        "whocontainer": {
+            "container_name": "ohhhhhhh",
+            "image": f"{service_key}:{service_tag}",
+        }
+    }
+
+    return yaml.safe_dump(service_spec)
