84d1d2bfdb77ad5a8c15b790b81ea60f2e860c72
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-02-01 23:38:02 +0100

    really upsert in comp_task

--- services/director-v2/src/simcore_service_director_v2/modules/db/repositories/comp_tasks.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/repositories/comp_tasks.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from typing import List, Optional
 
 import sqlalchemy as sa
+from aiopg.sa.result import RowProxy
 from models_library.projects import ProjectID
 from models_library.projects_nodes import Node
 from models_library.projects_nodes_io import NodeID
@@ -57,6 +58,69 @@ def _get_fake_service_details(
     return None
 
 
+async def _generate_tasks_list_from_project(
+    project: ProjectAtDB,
+    director_client: DirectorV0Client,
+    published_nodes: List[NodeID],
+) -> List[CompTaskAtDB]:
+
+    list_comp_tasks = []
+
+    for internal_id, node_id in enumerate(project.workbench, 1):
+        node: Node = project.workbench[node_id]
+
+        service_key_version = ServiceKeyVersion(
+            key=node.key,
+            version=node.version,
+        )
+        node_class = to_node_class(service_key_version.key)
+        node_details: ServiceDockerData = None
+        node_extras: ServiceExtras = None
+        if node_class == NodeClass.FRONTEND:
+            node_details = _get_fake_service_details(service_key_version)
+        else:
+            node_details = await director_client.get_service_details(
+                service_key_version
+            )
+            node_extras: ServiceExtras = await director_client.get_service_extras(
+                service_key_version
+            )
+        if not node_details:
+            continue
+
+        requires_mpi = False
+        requires_gpu = False
+        if node_extras:
+            requires_gpu = NodeRequirement.GPU in node_extras.node_requirements
+            requires_mpi = NodeRequirement.MPI in node_extras.node_requirements
+        image = Image(
+            name=service_key_version.key,
+            tag=service_key_version.version,
+            requires_gpu=requires_gpu,
+            requires_mpi=requires_mpi,
+        )
+
+        task_db = CompTaskAtDB(
+            project_id=project.uuid,
+            node_id=node_id,
+            schema=NodeSchema(inputs=node_details.inputs, outputs=node_details.outputs),
+            inputs=node.inputs,
+            outputs=node.outputs,
+            image=image,
+            submit=datetime.utcnow(),
+            state=(
+                RunningState.PUBLISHED
+                if node_id in published_nodes and node_class == NodeClass.COMPUTATIONAL
+                else RunningState.NOT_STARTED
+            ),
+            internal_id=internal_id,
+            node_class=node_class,
+        )
+
+        list_comp_tasks.append(task_db)
+    return list_comp_tasks
+
+
 class CompTasksRepository(BaseRepository):
     @log_decorator(logger=logger)
     async def get_comp_tasks(
@@ -84,71 +148,107 @@ class CompTasksRepository(BaseRepository):
         published_nodes: List[NodeID],
         str_project_uuid: str,
     ) -> None:
-        # start by removing the old tasks if they exist
-        await self.connection.execute(
-            sa.delete(comp_tasks).where(comp_tasks.c.project_id == str(project.uuid))
+
+        # NOTE: really do an upsert here because of issue https://github.com/ITISFoundation/osparc-simcore/issues/2125
+
+        list_of_comp_tasks_in_project = await _generate_tasks_list_from_project(
+            project, director_client, published_nodes
         )
-        # create the tasks
 
-        for internal_id, node_id in enumerate(project.workbench, 1):
-            node: Node = project.workbench[node_id]
-
-            service_key_version = ServiceKeyVersion(
-                key=node.key,
-                version=node.version,
+        result: RowProxy = await self.connection.execute(
+            sa.select([comp_tasks.c.node_id]).where(
+                comp_tasks.c.project_id == str(project.uuid)
             )
-            node_class = to_node_class(service_key_version.key)
-            node_details: ServiceDockerData = None
-            node_extras: ServiceExtras = None
-            if node_class == NodeClass.FRONTEND:
-                node_details = _get_fake_service_details(service_key_version)
-            else:
-                node_details = await director_client.get_service_details(
-                    service_key_version
-                )
-                node_extras: ServiceExtras = await director_client.get_service_extras(
-                    service_key_version
-                )
-            if not node_details:
-                continue
+        )
+        node_id_to_delete = [
+            t.node_id
+            for t in await result.fetchall()
+            if t.node_id not in project.workbench
+        ]
 
-            requires_mpi = False
-            requires_gpu = False
-            if node_extras:
-                requires_gpu = NodeRequirement.GPU in node_extras.node_requirements
-                requires_mpi = NodeRequirement.MPI in node_extras.node_requirements
-            image = Image(
-                name=service_key_version.key,
-                tag=service_key_version.version,
-                requires_gpu=requires_gpu,
-                requires_mpi=requires_mpi,
-            )
-
-            task_db = CompTaskAtDB(
-                project_id=project.uuid,
-                node_id=node_id,
-                schema=NodeSchema(
-                    inputs=node_details.inputs, outputs=node_details.outputs
-                ),
-                inputs=node.inputs,
-                outputs=node.outputs,
-                image=image,
-                submit=datetime.utcnow(),
-                state=(
-                    RunningState.PUBLISHED
-                    if node_id in published_nodes
-                    and node_class == NodeClass.COMPUTATIONAL
-                    else RunningState.NOT_STARTED
-                ),
-                internal_id=internal_id,
-                node_class=node_class,
-            )
-
+        # start by removing the old tasks if they exist
+        for node_id in node_id_to_delete:
             await self.connection.execute(
-                insert(comp_tasks).values(
-                    **task_db.dict(by_alias=True, exclude_unset=True)
+                sa.delete(comp_tasks).where(
+                    (comp_tasks.c.project_id == str(project.uuid))
+                    & (comp_tasks.c.node_id == node_id)
                 )
             )
+
+        # insert or update the remaining tasks
+
+        for comp_task_db in list_of_comp_tasks_in_project:
+
+            insert_stmt = insert(comp_tasks).values(
+                **comp_task_db.dict(by_alias=True, exclude_unset=True)
+            )
+            on_update_stmt = insert_stmt.on_conflict_do_update(
+                index_elements=[comp_tasks.c.project_id, comp_tasks.c.node_id],
+                set_=comp_task_db.dict(
+                    by_alias=True, exclude_unset=True, exclude={"outputs", "state"}
+                ),
+            )
+            await self.connection.execute(on_update_stmt)
+
+        # for internal_id, node_id in enumerate(project.workbench, 1):
+        #     node: Node = project.workbench[node_id]
+
+        #     service_key_version = ServiceKeyVersion(
+        #         key=node.key,
+        #         version=node.version,
+        #     )
+        #     node_class = to_node_class(service_key_version.key)
+        #     node_details: ServiceDockerData = None
+        #     node_extras: ServiceExtras = None
+        #     if node_class == NodeClass.FRONTEND:
+        #         node_details = _get_fake_service_details(service_key_version)
+        #     else:
+        #         node_details = await director_client.get_service_details(
+        #             service_key_version
+        #         )
+        #         node_extras: ServiceExtras = await director_client.get_service_extras(
+        #             service_key_version
+        #         )
+        #     if not node_details:
+        #         continue
+
+        #     requires_mpi = False
+        #     requires_gpu = False
+        #     if node_extras:
+        #         requires_gpu = NodeRequirement.GPU in node_extras.node_requirements
+        #         requires_mpi = NodeRequirement.MPI in node_extras.node_requirements
+        #     image = Image(
+        #         name=service_key_version.key,
+        #         tag=service_key_version.version,
+        #         requires_gpu=requires_gpu,
+        #         requires_mpi=requires_mpi,
+        #     )
+
+        #     task_db = CompTaskAtDB(
+        #         project_id=project.uuid,
+        #         node_id=node_id,
+        #         schema=NodeSchema(
+        #             inputs=node_details.inputs, outputs=node_details.outputs
+        #         ),
+        #         inputs=node.inputs,
+        #         outputs=node.outputs,
+        #         image=image,
+        #         submit=datetime.utcnow(),
+        #         state=(
+        #             RunningState.PUBLISHED
+        #             if node_id in published_nodes
+        #             and node_class == NodeClass.COMPUTATIONAL
+        #             else RunningState.NOT_STARTED
+        #         ),
+        #         internal_id=internal_id,
+        #         node_class=node_class,
+        #     )
+
+        #     await self.connection.execute(
+        #         insert(comp_tasks).values(
+        #             **task_db.dict(by_alias=True, exclude_unset=True)
+        #         )
+        #     )
 
     @log_decorator(logger=logger)
     async def upsert_tasks_from_project(
