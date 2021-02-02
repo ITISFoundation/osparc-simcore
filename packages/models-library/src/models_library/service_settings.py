cb7d21b906971349b99da12503cbf53361b2234a
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-02-02 12:34:15 +0100

    add testing of model

--- packages/models-library/src/models_library/service_settings.py
+++ packages/models-library/src/models_library/service_settings.py
@@ -1,6 +1,6 @@
 from typing import Any, List
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Extra, Field
 
 
 class ServiceSetting(BaseModel):
@@ -14,6 +14,49 @@ class ServiceSetting(BaseModel):
         ...,
         description="The value of the service setting (shall follow Docker REST API scheme for services",
     )
+
+    class Config:
+        extra = Extra.forbid
+        schema_extra = {
+            "examples": [
+                # constraints
+                {
+                    "name": "constraints",
+                    "type": "string",
+                    "value": ["node.platform.os == linux"],
+                },
+                # resources
+                {
+                    "name": "Resources",
+                    "type": "Resources",
+                    "value": {
+                        "Limits": {"NanoCPUs": 4000000000, "MemoryBytes": 17179869184},
+                        "Reservations": {
+                            "NanoCPUs": 100000000,
+                            "MemoryBytes": 536870912,
+                            "GenericResources": [
+                                {"DiscreteResourceSpec": {"Kind": "VRAM", "Value": 1}}
+                            ],
+                        },
+                    },
+                },
+                # mounts
+                {
+                    "name": "mount",
+                    "type": "object",
+                    "value": [
+                        {
+                            "ReadOnly": True,
+                            "Source": "/tmp/.X11-unix",
+                            "Target": "/tmp/.X11-unix",
+                            "Type": "bind",
+                        }
+                    ],
+                },
+                # environments
+                {"name": "env", "type": "string", "value": ["DISPLAY=:0"]},
+            ]
+        }
 
 
 class ServiceSettings(BaseModel):
