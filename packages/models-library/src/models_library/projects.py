5c9fd87ee2d1b8c83874695a09283a151f374dac
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-02-04 23:01:53 +0100

    moved ProjectAtDB to the models_library

--- packages/models-library/src/models_library/projects.py
+++ packages/models-library/src/models_library/projects.py
@@ -2,6 +2,8 @@
     Models a study's project document
 """
 from copy import deepcopy
+from datetime import datetime
+from enum import Enum
 from typing import Any, Dict, List, Optional
 from uuid import UUID
 
@@ -21,7 +23,19 @@ ClassifierID = str
 Workbench = Dict[NodeID_AsDictKey, Node]
 
 
-class Project(BaseModel):
+# NOTE: careful this is in sync with packages/postgres-database/src/simcore_postgres_database/models/projects.py!!!
+class ProjectType(str, Enum):
+    """
+    template: template project
+    standard: standard project
+    """
+
+    TEMPLATE = "TEMPLATE"
+    STANDARD = "STANDARD"
+
+
+class ProjectCommons(BaseModel):
+    # Description of the project
     uuid: ProjectID = Field(
         ...,
         description="project unique identifier",
@@ -30,8 +44,6 @@ class Project(BaseModel):
             "9bcf8feb-c1b1-41b6-b201-639cd6ccdba8",
         ],
     )
-
-    # Description of the project
     name: str = Field(
         ..., description="project name", examples=["Temporal Distortion Simulator"]
     )
@@ -46,25 +58,66 @@ class Project(BaseModel):
         examples=["https://placeimg.com/171/96/tech/grayscale/?0.jpg"],
     )
 
-    # Ownership and Access  (SEE projects_access.py)
-    prjOwner: EmailStr = Field(..., description="user email")
-    accessRights: Dict[GroupID, AccessRights] = Field(
-        ...,
-        description="object containing the GroupID as key and read/write/execution permissions as value",
+    creation_date: datetime = Field(...)
+    last_change_date: datetime = Field(...)
+
+    # Pipeline of nodes (SEE projects_nodes.py)
+    workbench: Workbench = ...
+
+    @validator("thumbnail", always=True, pre=True)
+    @classmethod
+    def convert_empty_str_to_none(v):
+        if isinstance(v, str) and v == "":
+            return None
+        return v
+
+
+class ProjectAtDB(ProjectCommons):
+    # specific DB fields
+    id: int = Field(..., description="The table primary index")
+    project_type: ProjectType = Field(..., alias="type", description="The project type")
+    prj_owner: Optional[int] = Field(..., description="The project owner id")
+    published: Optional[bool] = Field(
+        False, description="Defines if a study is available publicly"
     )
 
+    @validator("project_type", pre=True)
+    @classmethod
+    def convert_sql_alchemy_enum(v):
+        if isinstance(v, Enum):
+            return v.value
+        return v
+
+    class Config:
+        orm_mode = True
+        use_enum_values = True
+
+
+class Project(ProjectCommons):
+    # NOTE: This is the pydantic pendant of project-v0.0.1.json used in the API of the webserver/webclient
+    # NOT for usage with DB!!
+
+    # Ownership and Access  (SEE projects_access.py)
+    prj_owner: EmailStr = Field(..., description="user email", alias="prjOwner")
+
     # Timestamps   TODO: should we use datetime??
-    creationDate: str = Field(
+    creation_date: str = Field(
         ...,
         regex=DATE_RE,
         description="project creation date",
         examples=["2018-07-01T11:13:43Z"],
+        alias="creationDate",
     )
-    lastChangeDate: str = Field(
+    last_change_date: str = Field(
         ...,
         regex=DATE_RE,
         description="last save date",
         examples=["2018-07-01T11:13:43Z"],
+        alias="lastChangeDate",
+    )
+    accessRights: Dict[GroupID, AccessRights] = Field(
+        ...,
+        description="object containing the GroupID as key and read/write/execution permissions as value",
     )
 
     # Classification
@@ -74,9 +127,6 @@ class Project(BaseModel):
         description="Contains the reference to the project classifiers",
         examples=["some:id:to:a:classifier"],
     )
-
-    # Pipeline of nodes (SEE projects_nodes.py)
-    workbench: Workbench = ...
 
     # Project state (SEE projects_state.py)
     state: Optional[ProjectState] = None
@@ -91,13 +141,6 @@ class Project(BaseModel):
 
     # Dev only
     dev: Optional[Dict] = Field(description="object used for development purposes only")
-
-    @validator("thumbnail", pre=True, always=True)
-    @classmethod
-    def null_thumbnail(cls, v):
-        if isinstance(v, str) and v == "":
-            return None
-        return v
 
     class Config:
         description = "Document that stores metadata, pipeline and UI setup of a study"
