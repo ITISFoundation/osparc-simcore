619e0815b1905891043dc2583993ed6f0ac32c51
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-02-04 19:29:52 +0100

    Minor rename

--- services/api-server/tests/unit/test_model_schemas_profiles.py
+++ services/api-server/tests/unit/test_model_schemas_profiles.py
@@ -8,7 +8,7 @@ from simcore_service_api_server.models.schemas.profiles import Profile
 
 
 @pytest.mark.parametrize("model_cls", (Profile,))
-def test_meta_model_examples(model_cls, model_cls_examples):
+def test_profiles_model_examples(model_cls, model_cls_examples):
     for example in model_cls_examples:
         pprint(example)
         model_instance = model_cls(**example)
