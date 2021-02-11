20e3e5ad21986934440f383f57a5f2b479a87c3d
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-02-11 17:04:32 +0100

    Updates generator

--- services/web/server/tests/sandbox/catalog_services_openapi_generator.py
+++ services/web/server/tests/sandbox/catalog_services_openapi_generator.py
@@ -6,64 +6,16 @@ from pathlib import Path
 from typing import List, Optional
 
 from fastapi import FastAPI, Query
-from models_library.services import (
-    KEY_RE,
-    VERSION_RE,
-    PropertyName,
-    ServiceInput,
-    ServiceOutput,
+from simcore_service_webserver.catalog_api_models import (
+    ServiceInputApiOut,
+    ServiceInputKey,
+    ServiceKey,
+    ServiceOutputApiOut,
+    ServiceOutputKey,
+    ServiceVersion,
 )
-from pydantic import BaseModel, Extra, Field, constr
-from simcore_service_webserver.utils import snake_to_camel
-
-this_file = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve()
-
-
-ServiceKey = constr(regex=KEY_RE)
-ServiceVersion = constr(regex=VERSION_RE)
 
 app = FastAPI()
-
-
-ServiceInputKey = PropertyName
-ServiceOutputKey = PropertyName
-
-
-# Using ApiOut/ApiIn suffix to distinguish API models vs internal domain model
-#  - internal domain models should be consise, non-verbose, minimal, correct
-#  - API models should be adapted to API user needs
-#  - warning with couplings! Add example to ensure that API model maintain
-#    backwards compatibility
-#
-# TODO: uuid instead of key+version?
-
-
-class ServiceInputApiOut(ServiceInput):
-    key_id: ServiceInputKey = Field(
-        ..., description="Unique name identifier for this input"
-    )
-    unit_long: Optional[str] = Field(
-        None, description="Long name of the unit, if available"
-    )
-    unit_short: Optional[str] = Field(
-        None, description="Short name for the unit, if available"
-    )
-
-    class Config:
-        extra = Extra.forbid
-        alias_generator = snake_to_camel
-        # Add example v1, v2, ...
-
-
-class ServiceOutputApiOut(ServiceOutput):
-    key_id: ServiceInputKey = Field(
-        ..., description="Unique name identifier for this input"
-    )
-
-    class Config:
-        extra = Extra.forbid
-        alias_generator = snake_to_camel
-        # Add example v1, v2, ...
 
 
 @app.get(
@@ -151,36 +103,6 @@ def get_compatible_outputs_given_target_input(
 
     Returns compatible output port of a connected node for a given input
 
-    """
-    pass
-
-
-##############
-
-
-class TargetMatch(BaseModel):
-    service_key: ServiceKey
-    service_version: ServiceVersion
-    input_key: Optional[PropertyName]
-
-
-class MatchResult(BaseModel):
-    ok: bool = Field(description="Compatibility was found")
-
-
-# @app.get(
-#    "/catalog/services/{service_key:path}/{service_version}/inputs:match",
-#    response_model=MatchResult,
-# )
-def get_compatible_inputs_given_source(
-    service_key: ServiceKey,
-    service_version: ServiceVersion,
-    from_service_key: ServiceKey,
-    from_service_version: ServiceVersion,
-    from_output_key: PropertyName,
-):
-    """
-    Returns compatible ports
     """
     pass
 
