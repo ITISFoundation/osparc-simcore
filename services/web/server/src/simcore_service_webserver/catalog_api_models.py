cdbf0dfcab2dfa288c2b0c62cb60eaf1f8571a51
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-02-11 15:39:52 +0100

    Adds layout for api handlers and models

---
+++ services/web/server/src/simcore_service_webserver/catalog_api_models.py
@@ -0,0 +1,58 @@
+import sys
+from pathlib import Path
+from typing import List, Optional
+
+from models_library.services import (
+    KEY_RE,
+    VERSION_RE,
+    PropertyName,
+    ServiceInput,
+    ServiceOutput,
+)
+from pydantic import Extra, Field, constr
+
+from .utils import snake_to_camel
+
+ServiceKey = constr(regex=KEY_RE)
+ServiceVersion = constr(regex=VERSION_RE)
+
+
+ServiceInputKey = PropertyName
+ServiceOutputKey = PropertyName
+
+
+# Using ApiOut/ApiIn suffix to distinguish API models vs internal domain model
+#  - internal domain models should be consise, non-verbose, minimal, correct
+#  - API models should be adapted to API user needs
+#  - warning with couplings! Add example to ensure that API model maintain
+#    backwards compatibility
+#
+# TODO: uuid instead of key+version?
+
+
+class ServiceInputApiOut(ServiceInput):
+    key_id: ServiceInputKey = Field(
+        ..., description="Unique name identifier for this input"
+    )
+    unit_long: Optional[str] = Field(
+        None, description="Long name of the unit, if available"
+    )
+    unit_short: Optional[str] = Field(
+        None, description="Short name for the unit, if available"
+    )
+
+    class Config:
+        extra = Extra.forbid
+        alias_generator = snake_to_camel
+        # Add example v1, v2, ...
+
+
+class ServiceOutputApiOut(ServiceOutput):
+    key_id: ServiceInputKey = Field(
+        ..., description="Unique name identifier for this input"
+    )
+
+    class Config:
+        extra = Extra.forbid
+        alias_generator = snake_to_camel
+        # Add example v1, v2, ...
