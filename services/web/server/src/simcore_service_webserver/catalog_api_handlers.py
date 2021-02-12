2dc45755b952f1a56229fbe1dc5699f566945b35
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-02-12 05:54:32 +0100

    Drafting implementation of handlers

--- services/web/server/src/simcore_service_webserver/catalog_api_handlers.py
+++ services/web/server/src/simcore_service_webserver/catalog_api_handlers.py
@@ -1,12 +1,15 @@
 import json
 import sys
+from dataclasses import dataclass
 from pathlib import Path
-from typing import List, Optional
+from typing import Iterator, List, Optional, Tuple
 
 from aiohttp import web
 from aiohttp.web import Request, RouteTableDef
+from models_library.services import ServiceInput, ServiceOutput
 from pydantic import ValidationError
 
+from . import catalog_client
 from .catalog_api_models import (
     ServiceInputApiOut,
     ServiceInputKey,
@@ -15,6 +18,9 @@ from .catalog_api_models import (
     ServiceOutputKey,
     ServiceVersion,
 )
+from .constants import RQ_PRODUCT_KEY
+from .login.decorators import RQT_USERID_KEY, login_required
+from .security_decorators import permission_required
 
 ###############
 # API HANDLERS
@@ -26,7 +32,24 @@ from .catalog_api_models import (
 routes = RouteTableDef()
 
 
+@dataclass
+class _RequestContext:
+    app: web.Application
+    user_id: int
+    product_name: str
+
+    @classmethod
+    def create(cls, request: Request) -> "_RequestContext":
+        return cls(
+            app=request.app,
+            user_id=request[RQT_USERID_KEY],
+            product_name=request[RQ_PRODUCT_KEY],
+        )
+
+
 @routes.get("/catalog/services/{service_key:path}/{service_version}/inputs")
+@login_required
+@permission_required("services.catalog.*")
 async def list_service_inputs_handler(request: Request):
     try:
         # match, parse and validate
@@ -38,7 +61,9 @@ async def list_service_inputs_handler(request: Request):
         raise web.HTTPUnprocessableEntity() from err
 
     # Evaluate and return validated model
-    response_model = await list_service_inputs(service_key, service_version)
+    response_model = await list_service_inputs(
+        service_key, service_version, _RequestContext.create(request)
+    )
 
     # format response
     enveloped: str = json.dumps(
@@ -48,17 +73,9 @@ async def list_service_inputs_handler(request: Request):
     return web.Response(text=enveloped, content_type="application/json")
 
 
-async def list_service_inputs(
-    service_key: ServiceKey,
-    service_version: ServiceVersion,
-) -> List[ServiceOutputApiOut]:
-
-    # call to catalog and extract inputs here
-
-    return list()
-
-
 @routes.get("/catalog/services/{service_key:path}/{service_version}/inputs/{input_key}")
+@login_required
+@permission_required("services.catalog.*")
 async def get_service_input_handler(request: Request):
     try:
         # match, parse and validate
@@ -70,23 +87,18 @@ async def get_service_input_handler(request: Request):
         raise web.HTTPUnprocessableEntity() from err
 
     # Evaluate and return validated model
-    response_model = await get_service_input(service_key, service_version, input_key)
+    response_model = await get_service_input(
+        service_key, service_version, input_key, _RequestContext.create(request)
+    )
 
     # format response
     enveloped: str = json.dumps({"data": json.loads(response_model.json())})
     return web.Response(text=enveloped, content_type="application/json")
 
 
-async def get_service_input(
-    service_key: ServiceKey,
-    service_version: ServiceVersion,
-    input_key: ServiceInputKey,
-) -> ServiceInputApiOut:
-    # request catalog for a specific input of a service
-    return ServiceInputApiOut()
-
-
 @routes.get("/catalog/services/{service_key:path}/{service_version}/inputs:match")
+@login_required
+@permission_required("services.catalog.*")
 async def get_compatible_inputs_given_source_output_handler(request: Request):
     try:
         # match, parse and validate
@@ -106,6 +118,7 @@ async def get_compatible_inputs_given_source_output_handler(request: Request):
         from_service_key,
         from_service_version,
         from_output_key,
+        _RequestContext.create(request),
     )
 
     # format response
@@ -114,28 +127,11 @@ async def get_compatible_inputs_given_source_output_handler(request: Request):
     return web.Response(text=enveloped, content_type="application/json")
 
 
-async def get_compatible_inputs_given_source_output(
-    service_key: ServiceKey,
-    service_version: ServiceVersion,
-    from_service_key: ServiceKey,
-    from_service_version: ServiceVersion,
-    from_output_key: ServiceOutputKey,
-) -> List[ServiceInputKey]:
-    """
-        Filters inputs of this service that match a given service output
-
-    Returns compatible input ports of the service, provided an output port of
-    a connected node.
-
-    """
-    # TODO: uuid instead of key+version?
-
-    return list()
-
-
 @routes.get(
     "/catalog/services/{service_key:path}/{service_version}/outputs",
 )
+@login_required
+@permission_required("services.catalog.*")
 async def list_service_outputs_handler(request: Request):
     try:
         # match, parse and validate
@@ -146,7 +142,9 @@ async def list_service_outputs_handler(request: Request):
         raise web.HTTPUnprocessableEntity() from err
 
     # Evaluate and return validated model
-    response_model = await list_service_outputs(service_key, service_version)
+    response_model = await list_service_outputs(
+        service_key, service_version, _RequestContext.create(request)
+    )
 
     # format response
     enveloped: str = json.dumps(
@@ -154,14 +152,6 @@ async def list_service_outputs_handler(request: Request):
     )
 
     return web.Response(text=enveloped, content_type="application/json")
-
-
-async def list_service_outputs(
-    service_key: ServiceKey,
-    service_version: ServiceVersion,
-) -> List[ServiceOutputApiOut]:
-    # TODO: implement same call to catalog
-    return list()
 
 
 @routes.get(
@@ -178,24 +168,18 @@ async def get_service_output_handler(request: Request):
         raise web.HTTPUnprocessableEntity() from err
 
     # Evaluate and return validated model
-    response_model = await get_service_output(service_key, service_version, output_key)
+    response_model = await get_service_output(
+        service_key, service_version, output_key, _RequestContext.create(request)
+    )
 
     # format response
     enveloped: str = json.dumps({"data": json.loads(response_model.json())})
     return web.Response(text=enveloped, content_type="application/json")
 
 
-async def get_service_output(
-    service_key: ServiceKey,
-    service_version: ServiceVersion,
-    output_key: ServiceOutputKey,
-) -> ServiceOutputApiOut:
-    return ServiceInputApiOut()
-
-
-@routes.get(
-    "/catalog/services/{service_key:path}/{service_version}/outputs:match",
-)
+@routes.get("/catalog/services/{service_key:path}/{service_version}/outputs:match")
+@login_required
+@permission_required("services.catalog.*")
 async def get_compatible_outputs_given_target_input_handler(request: Request):
     """
     Filters outputs of this service that match a given service input
@@ -220,6 +204,7 @@ async def get_compatible_outputs_given_target_input_handler(request: Request):
         to_service_key,
         to_service_version,
         to_input_key,
+        _RequestContext.create(request),
     )
 
     # format response
@@ -228,12 +213,109 @@ async def get_compatible_outputs_given_target_input_handler(request: Request):
     return web.Response(text=enveloped, content_type="application/json")
 
 
+# IMPLEMENTATIONS -------------------------------------
+
+
+def can_connect(from_output: ServiceOutput, to_input: ServiceInput) -> bool:
+    # compatible units
+    ok = from_output.unit == to_input.unit
+    if ok:
+        # compatible types TODO: see mimetypes examples in property_type
+        ok = from_output.property_type == to_input.property_type
+        if not ok:
+            ok = "data:*/*" in (from_output.property_type, to_input.property_type)
+    return ok
+
+
+async def list_service_inputs(
+    service_key: ServiceKey, service_version: ServiceVersion, ctx: _RequestContext
+) -> List[ServiceOutputApiOut]:
+    return list()
+
+
+async def get_service_input(
+    service_key: ServiceKey,
+    service_version: ServiceVersion,
+    input_key: ServiceInputKey,
+    ctx: _RequestContext,
+) -> ServiceInputApiOut:
+
+    service = await catalog_client.get_service(
+        ctx.app, ctx.user_id, service_key, service_version, ctx.product_name
+    )
+    service_input = ServiceInputApiOut.from_service(service, input_key)
+
+    return service_input
+
+
+async def get_compatible_inputs_given_source_output(
+    service_key: ServiceKey,
+    service_version: ServiceVersion,
+    from_service_key: ServiceKey,
+    from_service_version: ServiceVersion,
+    from_output_key: ServiceOutputKey,
+    ctx: _RequestContext,
+) -> List[ServiceInputKey]:
+    """
+        Filters inputs of this service that match a given service output
+
+    Returns keys of compatible input ports of the service, provided an output port of
+    a connected node.
+    """
+    # get service service_key, service_version -> to_service -> inputs
+    def iter_service_inputs() -> Iterator[Tuple[ServiceInputKey, ServiceInput]]:
+        yield "input_1", ServiceInput(**ServiceInput.Config.schema_extra["example"])
+
+    # get output of service from_service_key, from_service_version
+    from_output: ServiceOutput = ServiceOutput(
+        **ServiceOutput.Config.schema_extra["example"]
+    )
+
+    result = []
+    for key_id, to_input in iter_service_inputs():
+        if can_connect(from_output, to_input):
+            result.append(key_id)
+
+    return list()
+
+
+async def list_service_outputs(
+    service_key: ServiceKey,
+    service_version: ServiceVersion,
+    ctx: _RequestContext,
+) -> List[ServiceOutputApiOut]:
+    # TODO: implement same call to catalog
+    return list()
+
+
+async def get_service_output(
+    service_key: ServiceKey,
+    service_version: ServiceVersion,
+    output_key: ServiceOutputKey,
+    ctx: _RequestContext,
+) -> ServiceOutputApiOut:
+    return ServiceInputApiOut()
+
+
 async def get_compatible_outputs_given_target_input(
     service_key: ServiceKey,
     service_version: ServiceVersion,
     to_service_key: ServiceKey,
     to_service_version: ServiceVersion,
     to_input_key: ServiceInputKey,
+    ctx: _RequestContext,
 ) -> List[ServiceOutputKey]:
+
+    # get service service_key, service_version -> to_service -> inputs
+    def iter_service_outputs() -> Iterator[Tuple[ServiceOutputKey, ServiceOutput]]:
+        yield "output_1", ServiceOutput(**ServiceOutput.Config.schema_extra["example"])
+
+    # get output of service from_service_key, from_service_version
+    to_input: ServiceInput = ServiceInput(**ServiceInput.Config.schema_extra["example"])
+
+    result = []
+    for key_id, from_output in iter_service_outputs():
+        if can_connect(from_output, to_input):
+            result.append(key_id)
 
     return list()
