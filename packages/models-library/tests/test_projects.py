606d1e3680d29f9788fb7caa22ba445f2553f249
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-02-15 12:37:30 +0100

    fix test to check if types are the same

--- packages/models-library/tests/test_projects.py
+++ packages/models-library/tests/test_projects.py
@@ -47,5 +47,5 @@ def test_project_type_in_models_package_same_as_in_postgres_database_package():
 
     # pylint: disable=no-member
     assert (
-        ml_project_type.__members__.values() == pg_project_type.__members__.values()
+        ml_project_type.__members__.keys() == pg_project_type.__members__.keys()
     ), f"The enum in models_library package and postgres package shall have the same values. models_pck: {ml_project_type.__members__}, postgres_pck: {pg_project_type.__members__}"
