2d210b2b498815d9160b07e5b5bcab4a28e3984b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-02-15 22:05:21 +0100

    fix some typos

--- packages/models-library/tests/test_models_fit_schemas.py
+++ packages/models-library/tests/test_models_fit_schemas.py
@@ -6,10 +6,9 @@ import json
 from typing import Callable
 
 import pytest
-from pydantic.main import BaseModel
-
 from models_library.projects import Project
 from models_library.services import ServiceDockerData
+from pydantic.main import BaseModel
 
 
 @pytest.mark.parametrize(
@@ -24,6 +23,10 @@ def test_generated_schema_same_as_original(
 ):
     generated_schema = json.loads(pydantic_model.schema_json(indent=2))
     original_schema = json_schema_dict(original_json_schema)
+
+    # NOTE: A change is considered an addition when the destination schema has become more permissive relative to the source schema. For example {"type": "string"} -> {"type": ["string", "number"]}.
+    # A change is considered a removal when the destination schema has become more restrictive relative to the source schema. For example {"type": ["string", "number"]} -> {"type": "string"}.
+    # The addition and removal changes detected are returned in JsonSchema format. These schemas represent the set of values that have been added or removed.
 
     # run one direction original schema encompass generated one
     process_completion = diff_json_schemas(original_schema, generated_schema)
