be102d5bedf451450fed64a870c968cb2d9feeeb
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-02-18 12:25:10 +0100

    Fixes linter

--- services/web/server/src/simcore_service_webserver/catalog_config.py
+++ services/web/server/src/simcore_service_webserver/catalog_config.py
@@ -7,10 +7,10 @@ import os
 from typing import Dict
 
 import trafaret as T
-from aiohttp import ClientSession, web
+from aiohttp import web
 from models_library.basic_types import PortInt, VersionTag
 from pydantic import BaseSettings, Field
-from servicelib.application_keys import APP_CLIENT_SESSION_KEY, APP_CONFIG_KEY
+from servicelib.application_keys import APP_CONFIG_KEY
 
 CONFIG_SECTION_NAME = "catalog"
 
