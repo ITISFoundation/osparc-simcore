50ccc9b1dbc80975e14aef28c1e82cf31187aa75
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-02-18 17:27:39 +0100

    chain and group tasks

--- services/director-v2/src/simcore_service_director_v2/modules/celery.py
+++ services/director-v2/src/simcore_service_director_v2/modules/celery.py
@@ -2,12 +2,12 @@ import logging
 from dataclasses import dataclass
 from typing import List
 
-from celery import Celery, Task
+from celery import Celery, Task, signature
 from celery.contrib.abortable import AbortableAsyncResult
 from fastapi import FastAPI
 from models_library.projects import ProjectID
 from models_library.settings.celery import CeleryConfig
-
+from models_library.projects_nodes_io import NodeID
 from ..models.schemas.constants import UserID
 from ..utils.client_decorators import handle_retry
 
@@ -35,7 +35,7 @@ def setup(app: FastAPI, settings: CeleryConfig) -> None:
     app.add_event_handler("startup", on_startup)
     app.add_event_handler("shutdown", on_shutdown)
 
-
+from celery import group, chain
 @dataclass
 class CeleryClient:
     client: Celery
@@ -56,6 +56,25 @@ class CeleryClient:
         return self.client.send_task(task_name, *args, **kwargs)
 
     def send_computation_task(self, user_id: UserID, project_id: ProjectID) -> Task:
+        s = signature(self.settings.task_name, expires=self.settings.publication_timeout, kwargs={"user_id": user_id, "project_id": str(project_id)},)
+        return s.apply_async()
+        return self.send_task(
+            self.settings.task_name,
+            expires=self.settings.publication_timeout,
+            kwargs={"user_id": user_id, "project_id": str(project_id)},
+        )
+
+    def send_computation_tasks(self, user_id: UserID, project_id: ProjectID, node_ids: List[List[NodeID]]) -> Task:
+        def _create_task_signature(self, user_id, project_id, node_id) -> signature:
+            return signature(self.settings.task_name, expires=self.settings.publication_timeout, kwargs={"user_id": user_id, "project_id": str(project_id), "node_id": str(node_id)},)
+
+
+
+        flow = chain(group(_create_task_signature(self, user_id, project_id, n) for n in node_groups) for node_groups in node_ids)
+
+        return flow.apply_async()
+
+
         return self.send_task(
             self.settings.task_name,
             expires=self.settings.publication_timeout,
