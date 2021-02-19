0f4b2c497061e692dd9ea6f7e29631213186198b
Andrei Neagu <neagu@itis.swiss>
2021-02-19 17:43:27 +0100

    introduced new models for dynamic-sidecar api

---
+++ services/director-v2/src/simcore_service_director_v2/models/domains/dynamic_sidecar.py
@@ -0,0 +1,67 @@
+from typing import Dict, Any, Optional, List
+
+from pydantic import BaseModel, Field, validator
+
+
+class ComposeSpecModel(BaseModel):
+    inputs_path: str = Field(
+        ..., description="path where the service expects all the inputs folder"
+    )
+    outputs_path: str = Field(
+        ..., description="path where the service expects all the outputs folder"
+    )
+    other_paths: List[str] = Field(
+        [],
+        description="optional list of path which contents need to be saved and restored",
+    )
+
+    @validator("other_paths", always=True)
+    @classmethod
+    def other_paths_always_a_list(cls, v):
+        return [] if v is None else v
+
+
+class StartServiceSidecarModel(BaseModel):
+    user_id: str
+    project_id: str
+    service_key: str
+    service_tag: str
+    node_uuid: str
+    settings: List[Dict[str, Any]] = Field(
+        ...,
+        description="settings for the services define by the service maintainer in the labels",
+    )
+    paths_mapping: ComposeSpecModel = Field(
+        ...,
+        description=(
+            "the service explicitly requests where to mount all paths "
+            "which will be handeled by the service-sidecar"
+        ),
+    )
+    compose_spec: Optional[Dict[str, Any]] = Field(
+        ...,
+        description=(
+            "if the user provides a compose_spec, it will be used instead "
+            "of compsing one from the service_key and service_tag"
+        ),
+    )
+    target_container: Optional[str] = Field(
+        ...,
+        description="when the user defines a compose spec, it should pick a container inside the spec to receive traffic on a defined port",
+    )
+
+    @validator("target_container")
+    @classmethod
+    def target_container_must_exist_if_compose_spec_present(
+        cls, v, values, **kwargs
+    ):  # pylint: disable=unused-argument
+        if values.get("compose_spec", None) is not None and v is None:
+            raise ValueError(
+                "target_container is required when compose_spec is defined. "
+                f"The following compose spec was defined: {values['compose_spec']}"
+            )
+        return v
+
+
+class NodeUUIDModel(BaseModel):
+    node_uuid: str
