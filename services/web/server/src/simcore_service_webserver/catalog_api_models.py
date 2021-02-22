ab4a7dbc1ed0368c946b091f3a17f7e03818aac4
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-02-22 22:07:02 +0100

    Adds a layer in the web-server that extends response of catalog

--- services/web/server/src/simcore_service_webserver/catalog_api_models.py
+++ services/web/server/src/simcore_service_webserver/catalog_api_models.py
@@ -14,44 +14,8 @@ from .utils import snake_to_camel
 
 ServiceKey = constr(regex=KEY_RE)
 ServiceVersion = constr(regex=VERSION_RE)
-
-
 ServiceInputKey = PropertyName
 ServiceOutputKey = PropertyName
-
-
-# Using ApiOut/ApiIn suffix to distinguish API models vs internal domain model
-#  - internal domain models should be consise, non-verbose, minimal, correct
-#  - API models should be adapted to API user needs
-#  - warning with couplings! Add example to ensure that API model maintain
-#    backwards compatibility
-#   - schema samples could have multiple schemas to tests backwards compatibility
-#
-# TODO: reduce to a minimum returned input/output models (ask OM)
-#
-INPUT_SAMPLE = {
-    "displayOrder": 2,
-    "label": "Sleep Time",
-    "description": "Time to wait before completion",
-    "type": "number",
-    "defaultValue": 0,
-    "unit": "second",
-    "widget": {"type": "TextArea", "details": {"minHeight": 1}},
-    "keyId": "input_2",
-    "unitLong": "seconds",
-    "unitShort": "sec",
-}
-
-OUTPUT_SAMPLE = {
-    "displayOrder": 2,
-    "label": "Time Slept",
-    "description": "Time the service waited before completion",
-    "type": "number",
-    "unit": "second",
-    "unitLong": "seconds",
-    "unitShort": "sec",
-    "keyId": "output_2",
-}
 
 
 # TODO: will be replaced by pynt functionality
@@ -65,7 +29,19 @@ def get_formatted_unit(data: dict):
     return [None, None]
 
 
-class _CommonApiExtension(BaseModel):
+#####
+#
+#  API models specifics to front-end needs
+#
+# Using WebApi prefix and In/Out suffix to distinguish web API models
+#  - internal domain models should be consise, non-verbose, minimal, correct
+#  - API models should be adapted to API user needs
+#  - warning with couplings! Add example to ensure that API model maintain
+#    backwards compatibility
+#   - schema samples could have multiple schemas to tests backwards compatibility
+#
+# TODO: reduce to a minimum returned input/output models (ask OM)
+class _BaseCommonApiExtension(BaseModel):
     unit_long: Optional[str] = Field(
         None, description="Long name of the unit, if available"
     )
@@ -74,7 +50,7 @@ class _CommonApiExtension(BaseModel):
     )
 
 
-class ServiceInputApiOut(ServiceInput, _CommonApiExtension):
+class ServiceInputApiOut(ServiceInput, _BaseCommonApiExtension):
     key_id: ServiceInputKey = Field(
         ..., description="Unique name identifier for this input"
     )
@@ -82,17 +58,30 @@ class ServiceInputApiOut(ServiceInput, _CommonApiExtension):
     class Config:
         extra = Extra.forbid
         alias_generator = snake_to_camel
-        schema_extra = {"example": INPUT_SAMPLE}
+        schema_extra = {
+            "example": {
+                "displayOrder": 2,
+                "label": "Sleep Time",
+                "description": "Time to wait before completion",
+                "type": "number",
+                "defaultValue": 0,
+                "unit": "second",
+                "widget": {"type": "TextArea", "details": {"minHeight": 1}},
+                "keyId": "input_2",
+                "unitLong": "seconds",
+                "unitShort": "sec",
+            }
+        }
 
     @classmethod
-    def from_service(cls, service: Dict[str, Any], input_key: ServiceInputKey):
+    def from_catalog_service(cls, service: Dict[str, Any], input_key: ServiceInputKey):
         data = service["inputs"][input_key]
         ushort, ulong = get_formatted_unit(data)
 
         return cls(keyId=input_key, unitLong=ulong, unitShort=ushort, **data)
 
 
-class ServiceOutputApiOut(ServiceOutput, _CommonApiExtension):
+class ServiceOutputApiOut(ServiceOutput, _BaseCommonApiExtension):
     key_id: ServiceOutputKey = Field(
         ..., description="Unique name identifier for this input"
     )
@@ -100,14 +89,43 @@ class ServiceOutputApiOut(ServiceOutput, _CommonApiExtension):
     class Config:
         extra = Extra.forbid
         alias_generator = snake_to_camel
-        schema_extra = {"example": OUTPUT_SAMPLE}
+        schema_extra = {
+            "example": {
+                "displayOrder": 2,
+                "label": "Time Slept",
+                "description": "Time the service waited before completion",
+                "type": "number",
+                "unit": "second",
+                "unitLong": "seconds",
+                "unitShort": "sec",
+                "keyId": "output_2",
+            }
+        }
 
     @classmethod
-    def from_service(cls, service: Dict[str, Any], output_key: ServiceOutputKey):
+    def from_catalog_service(
+        cls, service: Dict[str, Any], output_key: ServiceOutputKey
+    ):
         data = service["outputs"][output_key]
         ushort, ulong = get_formatted_unit(data)
 
         return cls(keyId=output_key, unitLong=ulong, unitShort=ushort, **data)
 
 
-# TODO: from models_library.api_schemas_catalog
+#######################
+# Helper functions
+#
+
+
+def replace_service_input_outputs(service: Dict[str, Any], **export_options):
+    """ Thin wrapper to replace i/o ports in returned service model """
+    # This is a fast solution until proper models are available for the web API
+    # TODO: from models_library.api_schemas_catalog
+
+    for input_key in service["inputs"]:
+        new_input = ServiceInputApiOut.from_catalog_service(service, input_key)
+        service["inputs"][input_key] = new_input.dict(**export_options)
+
+    for output_key in service["outputs"]:
+        new_output = ServiceOutputApiOut.from_catalog_service(service, output_key)
+        service["outputs"][output_key] = new_output.dict(**export_options)
