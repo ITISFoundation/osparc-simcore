d8ab0637b71c414b9f3cc592144be06e490141c8
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2021-02-22 12:38:17 +0100

    Update Ports.js

--- services/web/client/source/class/osparc/utils/Ports.js
+++ services/web/client/source/class/osparc/utils/Ports.js
@@ -36,7 +36,7 @@ qx.Class.define("osparc.utils.Ports", {
 
     arePortsCompatible: function(node1, portId1, node2, portId2) {
       return osparc.data.Resources.getCompatibleInputs(node1, portId1, node2)
-          .then(compatiblePorts => compatiblePorts.includes(portId2));
+        .then(compatiblePorts => compatiblePorts.includes(portId2));
     },
 
     isDataALink: function(data) {
