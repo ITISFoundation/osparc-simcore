315afa7f8f049c5a2789994f183150afe692b153
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-02-22 15:11:32 +0100

    use function Signature to start workflow chain and group tasks

--- services/director-v2/src/simcore_service_director_v2/modules/celery.py
+++ services/director-v2/src/simcore_service_director_v2/modules/celery.py
@@ -2,12 +2,14 @@ import logging
 from dataclasses import dataclass
 from typing import List
 
-from celery import Celery, Task, signature
+from celery import Celery, Task, chain, group, signature
+from celery.canvas import Signature
 from celery.contrib.abortable import AbortableAsyncResult
 from fastapi import FastAPI
 from models_library.projects import ProjectID
-from models_library.settings.celery import CeleryConfig
 from models_library.projects_nodes_io import NodeID
+from models_library.settings.celery import CeleryConfig
+
 from ..models.schemas.constants import UserID
 from ..utils.client_decorators import handle_retry
 
@@ -35,7 +37,7 @@ def setup(app: FastAPI, settings: CeleryConfig) -> None:
     app.add_event_handler("startup", on_startup)
     app.add_event_handler("shutdown", on_shutdown)
 
-from celery import group, chain
+
 @dataclass
 class CeleryClient:
     client: Celery
@@ -56,30 +58,45 @@ class CeleryClient:
         return self.client.send_task(task_name, *args, **kwargs)
 
     def send_computation_task(self, user_id: UserID, project_id: ProjectID) -> Task:
-        s = signature(self.settings.task_name, expires=self.settings.publication_timeout, kwargs={"user_id": user_id, "project_id": str(project_id)},)
-        return s.apply_async()
-        return self.send_task(
+        s = signature(
             self.settings.task_name,
-            expires=self.settings.publication_timeout,
             kwargs={"user_id": user_id, "project_id": str(project_id)},
+            immutable=True,  # this prevents the result to be added to the next task
+        )
+        return s.apply_async(
+            expires=self.settings.publication_timeout,
         )
 
-    def send_computation_tasks(self, user_id: UserID, project_id: ProjectID, node_ids: List[List[NodeID]]) -> Task:
-        def _create_task_signature(self, user_id, project_id, node_id) -> signature:
-            return signature(self.settings.task_name, expires=self.settings.publication_timeout, kwargs={"user_id": user_id, "project_id": str(project_id), "node_id": str(node_id)},)
+    def send_computation_tasks(
+        self, user_id: UserID, project_id: ProjectID, node_ids: List[List[NodeID]]
+    ) -> Task:
+        def _create_task_signature(self, user_id, project_id, node_id) -> Signature:
+            task_signature = signature(
+                self.settings.task_name,
+                kwargs={
+                    "user_id": user_id,
+                    "project_id": str(project_id),
+                    "node_id": str(node_id),
+                },
+                immutable=True,  # this prevents the result to be added to the next task
+            )
+            return task_signature
 
+        celery_groups = []
+        for node_group in node_ids:
+            celery_groups.append(
+                group(
+                    _create_task_signature(self, user_id, project_id, n)
+                    for n in node_group
+                )
+            )
 
-
-        flow = chain(group(_create_task_signature(self, user_id, project_id, n) for n in node_groups) for node_groups in node_ids)
-
-        return flow.apply_async()
-
-
-        return self.send_task(
-            self.settings.task_name,
+        celery_flow = chain(celery_groups)
+        task = celery_flow.apply_async(
             expires=self.settings.publication_timeout,
-            kwargs={"user_id": user_id, "project_id": str(project_id)},
         )
+        logger.debug("created celery workflow %s", str(celery_flow))
+        return task
 
     @classmethod
     def abort_computation_tasks(cls, task_ids: List[str]) -> None:
