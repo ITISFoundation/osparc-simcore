0e74941e051ea6a782f3650d36cc45f4269da802
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-02-23 14:26:26 +0100

    create test for checking new function

--- services/web/server/tests/unit/isolated/test_projects_utils.py
+++ services/web/server/tests/unit/isolated/test_projects_utils.py
@@ -4,16 +4,20 @@
 
 import json
 from copy import deepcopy
+from pathlib import Path
+from typing import Any, Dict, Set
 
 import jsonschema
 import pytest
 from jsonschema import ValidationError
-
-from simcore_service_webserver.projects.projects_utils import clone_project_document
+from simcore_service_webserver.projects.projects_utils import (
+    clone_project_document,
+    project_get_dependent_nodes,
+)
 from simcore_service_webserver.resources import resources
 
 
-def load_template_projects():
+def load_template_projects() -> Dict[str, Any]:
     projects = []
     projects_names = [
         name for name in resources.listdir("data") if "template-projects" in name
@@ -25,7 +29,7 @@ def load_template_projects():
 
 
 @pytest.fixture
-def project_schema(project_schema_file):
+def project_schema(project_schema_file: Path) -> Dict[str, Any]:
     with open(project_schema_file) as fh:
         schema = json.load(fh)
     return schema
@@ -34,7 +38,9 @@ def project_schema(project_schema_file):
 @pytest.mark.parametrize(
     "name,project", [(p["name"], p) for p in load_template_projects()]
 )
-def test_clone_project_document(name, project, project_schema):
+def test_clone_project_document(
+    name: str, project: Dict[str, Any], project_schema: Dict[str, Any]
+):
 
     source = deepcopy(project)
     clone, _ = clone_project_document(source)
@@ -53,3 +59,31 @@ def test_clone_project_document(name, project, project_schema):
         jsonschema.validate(instance=clone, schema=project_schema)
     except ValidationError as err:
         pytest.fail(f"Invalid clone of '{name}': {err.message}")
+
+
+@pytest.fixture(scope="session")
+def fake_project_data(fake_data_dir: Path) -> Dict[str, Any]:
+    with (fake_data_dir / "fake-project.json").open() as fp:
+        return json.load(fp)
+
+
+@pytest.mark.parametrize(
+    "node_uuid, expected_dependencies",
+    [
+        (
+            "b4b20476-e7c0-47c2-8cc4-f66ac21a13bf",
+            {
+                "5739e377-17f7-4f09-a6ad-62659fb7fdec",
+            },
+        ),
+        ("5739e377-17f7-4f09-a6ad-62659fb7fdec", set()),
+        ("351fd505-1ee3-466d-ad6c-ea2915ffd364", set()),
+    ],
+)
+async def test_project_get_depending_nodes(
+    fake_project_data: Dict[str, Any], node_uuid: str, expected_dependencies: Set[str]
+):
+    set_of_depending_nodes = await project_get_dependent_nodes(
+        fake_project_data, node_uuid
+    )
+    assert set_of_depending_nodes == expected_dependencies
