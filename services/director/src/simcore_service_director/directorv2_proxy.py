9fdb15849cb413212f9541eec876e9021e071a09
Andrei Neagu <neagu@itis.swiss>
2021-02-24 15:31:33 +0100

    this method can return None somethimes

--- services/director/src/simcore_service_director/directorv2_proxy.py
+++ services/director/src/simcore_service_director/directorv2_proxy.py
@@ -180,10 +180,11 @@ async def get_service_sidecar_stack_status(app: web.Application, node_uuid: str)
         return result
 
     except _DirectorServiceError:
-        log.error(
+        log.exception(
             "Could not retrive status for service-sidecar stack for node_uuid=%s",
             node_uuid,
         )
+        return None
 
 
 __all__ = ["setup_director_v2", "start_service_sidecar_stack"]
