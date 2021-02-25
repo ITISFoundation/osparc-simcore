13ed404cdee48f64fcee678f2aa7e6793899eb3d
Sylvain <35365065+sanderegg@users.noreply.github.com>
2021-02-25 08:20:44 +0100

    🏗️ maintenance: github CI - docker buildx is integrated in docker since version 19.03 (#2163)
    * docker buildx is integrated in docker since version 19.03
    * remove setup of docker buildx
    * show docker buildx version
    * filter files from watchdog

--- services/catalog/docker/boot.sh
+++ services/catalog/docker/boot.sh
@@ -26,11 +26,10 @@ if [ "${SC_BUILD_TARGET}" = "development" ]; then
 fi
 
 # RUNNING application ----------------------------------------
-if [ "${SC_BOOT_MODE}" = "debug-ptvsd" ]
-then
+if [ "${SC_BOOT_MODE}" = "debug-ptvsd" ]; then
   # NOTE: ptvsd is programmatically enabled inside of the service
   # this way we can have reload in place as well
-  exec uvicorn simcore_service_catalog.__main__:the_app --reload --host 0.0.0.0
+  exec uvicorn simcore_service_catalog.__main__:the_app --reload --host 0.0.0.0 --reload-dir services/catalog/src/simcore_service_catalog
 else
   exec simcore-service-catalog
 fi
