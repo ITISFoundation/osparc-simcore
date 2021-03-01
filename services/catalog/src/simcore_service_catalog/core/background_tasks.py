742e05033d007bf67e20841bb094af839601552a
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2021-03-01 09:32:42 +0100

    BugFix: Get service entrypoint also account for front-end (#2173)
    FIX: GET v0/catalog/services/simcore%2Fservices%2Ffrontend%2Ffile-picker/1.0.0 for any front-end service failed
    
    * Get service entrypoint also account for front-end
    
    * Removes unset
    
    * Fixes front-end services in catalog.
    
    * Set file-picker as front-end type

--- services/catalog/src/simcore_service_catalog/core/background_tasks.py
+++ services/catalog/src/simcore_service_catalog/core/background_tasks.py
@@ -30,7 +30,7 @@ from ..api.dependencies.director import get_director_api
 from ..db.repositories.groups import GroupsRepository
 from ..db.repositories.projects import ProjectsRepository
 from ..db.repositories.services import ServicesRepository
-from ..services.frontend_services import get_services as get_frontend_services
+from ..services.frontend_services import iter_service_docker_data
 
 logger = logging.getLogger(__name__)
 
@@ -39,14 +39,16 @@ ServiceVersion = str
 
 OLD_SERVICES_DATE: datetime = datetime(2020, 8, 19)
 
+ServiceDockerDataMap = Dict[Tuple[ServiceKey, ServiceVersion], ServiceDockerData]
+
 
 async def _list_registry_services(
     app: FastAPI,
-) -> Dict[Tuple[ServiceKey, ServiceVersion], ServiceDockerData]:
+) -> ServiceDockerDataMap:
     client = get_director_api(app)
     data = await client.get("/services")
-    services: Dict[Tuple[ServiceKey, ServiceVersion], ServiceDockerData] = {
-        (s.key, s.version): s for s in get_frontend_services()
+    services: ServiceDockerDataMap = {
+        (s.key, s.version): s for s in iter_service_docker_data()
     }
     for x in data:
         try:
