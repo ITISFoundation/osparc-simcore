1cc0ebba974e1d8045218f5ba8e55550ff40890a
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2021-03-03 15:09:08 +0100

    E2E: Increase isolve timeouts (#2188)
    - Increase mpi e2e test timeout to 80". @mguidon, isolve-mpi it spends 60" changing permissions.
    - Delete puppeteer_XX's study even if it fails
    - Don't rely on iframes array position, check _url instead

--- tests/e2e/tutorials/jupyters.js
+++ tests/e2e/tutorials/jupyters.js
@@ -17,11 +17,12 @@ const templateName = "Jupyters";
 async function runTutorial() {
   const tutorial = new tutorialBase.TutorialBase(url, templateName, user, pass, newUser, enableDemoMode);
 
+  let studyId = null;
   try {
     tutorial.startScreenshooter();
     await tutorial.start();
     const studyData = await tutorial.openTemplate(1000);
-    const studyId = studyData["data"]["uuid"];
+    studyId = studyData["data"]["uuid"];
     console.log("Study ID:", studyId);
 
     const workbenchData = utils.extractWorkbenchData(studyData["data"]);
@@ -31,11 +32,12 @@ async function runTutorial() {
     await tutorial.openNode(1);
 
     const iframeHandles = await tutorial.getIframe();
-    // expected two iframes = loading + jupyterNB
-    const iFrame0 = await iframeHandles[0].contentFrame();
-    const iFrame1 = await iframeHandles[1].contentFrame();
-
-    const nbIframe = [iFrame0, iFrame1].find(iframe => iframe._url.endsWith("tree?"));
+    const iframes = [];
+    for (let i=0; i<iframeHandles.length; i++) {
+      const frame = await iframeHandles[i].contentFrame();
+      iframes.push(frame);
+    }
+    const nbIframe = iframes.find(iframe => iframe._url.endsWith("tree?"));
 
     // inside the iFrame, open the first notebook
     const notebookCBSelector = '#notebook_list > div:nth-child(2) > div > input[type=checkbox]';
@@ -74,11 +76,12 @@ async function runTutorial() {
     await tutorial.openNode(2);
 
     const iframeHandles2 = await tutorial.getIframe();
-    // expected three iframes = loading + jupyterNB + jupyterLab
-    const iFrame20 = await iframeHandles2[0].contentFrame();
-    const iFrame21 = await iframeHandles2[1].contentFrame();
-
-    const jLabIframe = [iFrame20, iFrame21].find(iframe => iframe._url.endsWith("lab?"));
+    const iframes2 = [];
+    for (let i=0; i<iframeHandles2.length; i++) {
+      const frame = await iframeHandles2[i].contentFrame();
+      iframes2.push(frame);
+    }
+    const jLabIframe = iframes2.find(iframe => iframe._url.endsWith("lab?"));
 
     // inside the iFrame, open the first notebook
     const input2outputFileSelector = '#filebrowser > div.lm-Widget.p-Widget.jp-DirListing.jp-FileBrowser-listing.jp-DirListing-narrow > ul > li:nth-child(3)';
@@ -108,16 +111,18 @@ async function runTutorial() {
       "TheNumber.txt"
     ];
     await tutorial.checkResults(outFiles2.length);
-
-    await tutorial.toDashboard();
-
-    await tutorial.removeStudy(studyId);
   }
   catch (err) {
     tutorial.setTutorialFailed(true);
     console.log('Tutorial error: ' + err);
   }
   finally {
+    // delete study if succesfull or puppeteer_XX
+    if (!tutorial.getTutorialFailed() || (user.includes("puppeteer_") && studyId)) {
+      await tutorial.toDashboard();
+      await tutorial.removeStudy(studyId);
+    }
+
     await tutorial.logOut();
     tutorial.stopScreenshooter();
     await tutorial.close();
