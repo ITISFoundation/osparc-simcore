bc9a53d4d1e2eb0a269577a8fa36890d724cfc5c
odeimaiz <maiz@itis.swiss>
2021-03-09 14:39:22 +0100

    Update NodeStatus.js

--- services/web/client/source/class/osparc/data/model/NodeStatus.js
+++ services/web/client/source/class/osparc/data/model/NodeStatus.js
@@ -71,7 +71,7 @@ qx.Class.define("osparc.data.model.NodeStatus", {
   },
 
   members: {
-    __hasDependencies: function() {
+    hasDependencies: function() {
       const dependencies = this.getDependencies();
       if (dependencies && dependencies.length) {
         return true;
@@ -80,12 +80,12 @@ qx.Class.define("osparc.data.model.NodeStatus", {
     },
 
     __applyDependencies: function() {
-      this.__applyModified(this.__hasDependencies());
+      this.__applyModified(this.hasDependencies());
     },
 
     __applyModified: function(modified) {
       if (this.getHasOutputs()) {
-        this.setModified(modified || this.__hasDependencies());
+        this.setModified(modified || this.hasDependencies());
       } else {
         this.setModified(null);
       }
