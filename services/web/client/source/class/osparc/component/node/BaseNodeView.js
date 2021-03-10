97a9c920f239a5af73ecf8fa681847ef949f2656
odeimaiz <maiz@itis.swiss>
2021-03-10 10:34:43 +0100

    reuse window

--- services/web/client/source/class/osparc/component/node/BaseNodeView.js
+++ services/web/client/source/class/osparc/component/node/BaseNodeView.js
@@ -377,8 +377,11 @@ qx.Class.define("osparc.component.node.BaseNodeView", {
 
     __openNodeDataManager: function() {
       const nodeDataManager = new osparc.component.widget.NodeDataManager(this.getNode(), true);
-      const win = nodeDataManager.getWindow();
-      win.open();
+      const win = osparc.ui.window.Window.popUpInWindow(nodeDataManager, this.getNode().getLabel(), 900, 600).set({
+        appearance: "service-window"
+      });
+      const closeBtn = win.getChildControl("close-button");
+      osparc.utils.Utils.setIdToWidget(closeBtn, "nodeDataManagerCloseBtn");
     },
 
     __openServiceDetails: function() {
