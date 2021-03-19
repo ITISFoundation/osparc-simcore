3a5aad3c8678ddd2df7d2f81576bdfda63683885
Andrei Neagu <neagu@itis.swiss>
2021-03-19 13:56:21 +0100

    added default exports

---
+++ services/web/server/src/simcore_service_webserver/exporter/formatters/xlsx/__init__.py
@@ -0,0 +1,11 @@
+from .templates.code_description import CodeDescriptionXLSXDocument
+from .templates.dataset_description import DatasetDescriptionXLSXDocument
+from .templates.directory_manifest import DirectoryManifestXLSXDocument
+from .templates.submission import SubmissionXLSXDocument
+
+__all__ = [
+    "CodeDescriptionXLSXDocument",
+    "DatasetDescriptionXLSXDocument",
+    "DirectoryManifestXLSXDocument",
+    "SubmissionXLSXDocument",
+]
