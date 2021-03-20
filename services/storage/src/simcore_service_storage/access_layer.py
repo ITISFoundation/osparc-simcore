7a87742b673eb877464c9fa3e6674526bcf23727
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-03-20 16:18:28 +0100

    Added doc Fixed linter import errors

--- services/storage/src/simcore_service_storage/access_layer.py
+++ services/storage/src/simcore_service_storage/access_layer.py
@@ -1,41 +1,85 @@
+"""
+
+Draft Rationale:
+
+    osparc-simcore defines TWO authorization methods: i.e. a set of rules on what,
+    how and when any resource can be accessed or operated by a user
+
+    ROLE-BASED METHOD:
+    In this method, a user is assigned a role (user/tester/admin) upon registration. Each role is
+    system-wide and defines a set of operations that the user *can* perform
+        - Every operation is named as a resource and an action (e.g. )
+        - Resource is named hierarchically
+        - Roles can inherit permitted operations from other role
+    This method is static because is system-wide and it is defined directly in the
+    code at services/web/server/src/simcore_service_webserver/security_roles.py
+    It is defined on top of every API entrypoint and applied just after authentication of the user.
+
+    GROUP-BASED METHOD:
+    The second method is designed to share a resource among groups of users dynamically. A group
+    defines a set of rules that apply to a resource and users can be added to the group dynamically.
+    So far, there are two resources that define access rights (AR):
+        - one applies to projects (read/write/delete) and
+        - the other to services (execute/write)
+    The project access rights are set in the column "access_rights" of the "projects" table .
+    The service access rights has its own table: service_access_rights
+
+    Access rights apply hierarchically, meaning that the access granted to a project applies
+    to all nodes inside and stored data in nodes.
+
+    How do these two AR coexist?: Access to read, write or delete a project are defined in the project AR but execution
+    will depend on the service AR attached to nodes inside.
+
+
+    What about stored data?
+    - nodes data are stored in s3 hierarchically and inherits the AR from the associated project
+    -
+    - Some s3 data objects are associated with nodes in a project
+    - Access to a project is also regulated via access rights and it is also
+    the case for all data objects under this project
+"""
+
+
 from dataclasses import dataclass
 from typing import Dict, List
 
-import sqlalchemy as sa
 from aiopg.sa.connection import SAConnection
-from aiopg.sa.result import ResultProxy, RowProxy
-from simcore_postgres_database.storage_models import (
-    file_meta_data,
-    projects,
-    user_to_groups,
-)
 from sqlalchemy.sql import text
+
+# import sqlalchemy as sa
+# from aiopg.sa.result import ResultProxy, RowProxy
+# from simcore_postgres_database.storage_models import (
+#    file_meta_data,
+#    projects,
+#    user_to_groups,
+# )
+
 
 ProjectID = str
 
 
 @dataclass
 class AccessRights:
-    read: bool
-    write: bool
-    delete: bool
+    read: bool = False
+    write: bool = False
+    delete: bool = False
 
 
 async def get_projects_access_rights(
     conn: SAConnection, user_id: int
 ) -> Dict[ProjectID, AccessRights]:
-    """ returns rights that user_id has on projects """
+    """ Returns projects included in any user's group and its access rights """
 
     smt = text(
         f"""\
     SELECT uuid, access_rights
     FROM projects
     WHERE (
-        jsonb_exists_any( access_rights, (
+        prj_owner = {user_id}
+        OR jsonb_exists_any( access_rights, (
                SELECT ARRAY( SELECT gid::TEXT FROM user_to_groups WHERE uid = {user_id} )
             )
         )
-        OR prj_owner = {user_id}
     )
     """
     )
