e0e201531aa585cde5acf3f273e852afdf4e17fb
Andrei Neagu <neagu@itis.swiss>
2021-03-23 17:53:53 +0100

    connecting TO data

--- services/web/server/src/simcore_service_webserver/exporter/formatters/formatter_v2.py
+++ services/web/server/src/simcore_service_webserver/exporter/formatters/formatter_v2.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from pathlib import Path
 from concurrent.futures import ProcessPoolExecutor
+from collections import deque
 
 from aiohttp import web
 
@@ -21,27 +22,125 @@ from simcore_service_webserver.exporter.formatters.sds.xlsx.templates.dataset_de
 from simcore_service_webserver.exporter.formatters.sds.xlsx.templates.code_description import (
     CodeDescriptionParams,
     CodeDescriptionModel,
+    RRIDEntry,
+    InputsEntryModel,
+    OutputsEntryModel,
 )
 
-logger = logging.getLogger(__name__)
+from ..exceptions import ExporterException
+from simcore_service_webserver.projects.projects_exceptions import ProjectsException
+from simcore_service_webserver.projects.projects_api import get_project_for_user
+from simcore_service_webserver.catalog_client import get_service
+
+
+log = logging.getLogger(__name__ + "[SDS]")
 
 
 async def _write_sds_content(
-    base_path: Path, app: web.Application, project_id: str, user_id: int
+    base_path: Path,
+    app: web.Application,
+    project_id: str,
+    user_id: int,
+    product_name: str,
 ) -> None:
-    # assemble params here
-    submission_params = SubmissionDocumentParams(
-        award_number="_TODO_REPLACE_ME",
-        milestone_archived="_TODO_REPLACE_ME",
-        milestone_completion_date="_TODO_REPLACE_ME",
-    )
-    dataset_description_params = DatasetDescriptionParams(
-        name="_TODO_REPLACE_ME", description="_TODO_REPLACE_ME"
-    )
-    code_description = CodeDescriptionModel()
-    code_description_params = CodeDescriptionParams(code_description=code_description)
+    try:
+        project_data = await get_project_for_user(
+            app=app,
+            project_uuid=project_id,
+            user_id=user_id,
+            include_templates=True,
+            include_state=True,
+        )
+    except ProjectsException as e:
+        raise ExporterException(f"Could not find project {project_id}") from e
 
-    # writing with process pool
+    log.debug("Project data: %s", project_data)
+
+    # assemble params here
+    submission_params = SubmissionDocumentParams()
+    dataset_description_params = DatasetDescriptionParams(
+        name=project_data["name"], description=project_data["description"]
+    )
+
+    params_code_description = {}
+
+    # Classifiers
+    # TODO: continue with this
+    RRIDEntry()
+
+    # adding TSR data
+    quality_data = project_data["quality"]
+    if quality_data.get("enabled", False):
+        tsr_data = quality_data["tsr_current"]
+        for i in range(1, 11):
+            tsr_entry_key = "r%.02d" % i
+            tsr_entry = tsr_data[tsr_entry_key]
+
+            rating_store_key = f"tsr{i}_rating" if i != 10 else f"tsr{i}a_rating"
+            reference_store_key = (
+                f"tsr{i}_reference" if i != 10 else f"tsr{i}a_reference"
+            )
+
+            params_code_description[rating_store_key] = tsr_entry["level"]
+            params_code_description[reference_store_key] = tsr_entry["references"]
+
+    workbench = project_data["workbench"]
+
+    inputs = deque()
+    outputs = deque()
+
+    for entry in workbench.values():
+        service_key = entry["key"]
+        service_version = entry["version"]
+        # TODO: add this filed to the format
+        # label = entry["label"] a field for this will come later
+
+        service_data = await get_service(
+            app=app,
+            user_id=user_id,
+            service_key=service_key,
+            service_version=service_version,
+            product_name=product_name,
+        )
+        log.error("Have data to use for compiling inputs and outputs %s", service_data)
+
+        service_data_inputs = service_data["inputs"]
+
+        for service_input in service_data_inputs.values():
+            input_entry = InputsEntryModel(
+                service_name=service_data["name"],
+                service_version=service_data["version"],
+                input_name=service_input["label"],
+                # not present on the service
+                input_data_ontology_identifier="",
+                input_data_type=service_input["type"],
+                # this is optional
+                input_data_units=service_input.get("unit", ""),
+                # not always available
+                input_data_default_value=str(service_input.get("defaultValue", "")),
+            )
+            inputs.append(input_entry)
+
+        service_data_outputs = service_data["outputs"]
+        for service_output in service_data_outputs.values():
+            output_entry = OutputsEntryModel(
+                service_name=service_data["name"],
+                service_version=service_data["version"],
+                output_name=service_output["label"],
+                # not present on the service
+                output_data_ontology_identifier="",
+                output_data_type=service_output["type"],
+                # this is optional
+                output_data_units=service_output.get("unit", ""),
+            )
+            outputs.append(output_entry)
+
+    code_description = CodeDescriptionModel(**params_code_description)
+    code_description_params = CodeDescriptionParams(
+        code_description=code_description, inputs=list(inputs), outputs=list(outputs)
+    )
+
+    # writing SDS structure with process pool to avoid blocking
     with ProcessPoolExecutor(max_workers=1) as pool:
         return await asyncio.get_event_loop().run_in_executor(
             pool,
@@ -76,9 +175,14 @@ class FormatterV2(BaseFormatter):
         app: web.Application = kwargs["app"]
         project_id: str = kwargs["project_id"]
         user_id: int = kwargs["user_id"]
+        product_name: str = kwargs["product_name"]
 
         await _write_sds_content(
-            base_path=self.root_folder, app=app, project_id=project_id, user_id=user_id
+            base_path=self.root_folder,
+            app=app,
+            project_id=project_id,
+            user_id=user_id,
+            product_name=product_name,
         )
 
         # continue filling up everuthing here
