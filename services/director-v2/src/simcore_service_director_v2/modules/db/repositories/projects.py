721fad638ac6894e4aaf6ff5c096c0298244a85a
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-03-29 10:58:01 +0200

    director-v2 is not blocked by amount of requests anymore

--- services/director-v2/src/simcore_service_director_v2/modules/db/repositories/projects.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/repositories/projects.py
@@ -13,11 +13,12 @@ logger = logging.getLogger(__name__)
 
 class ProjectsRepository(BaseRepository):
     async def get_project(self, project_id: ProjectID) -> ProjectAtDB:
-        row: RowProxy = await (
-            await self.connection.execute(
-                sa.select([projects]).where(projects.c.uuid == str(project_id))
-            )
-        ).first()
-        if not row:
-            raise ProjectNotFoundError(project_id)
-        return ProjectAtDB.from_orm(row)
+        async with self.db_engine.acquire() as conn:
+            row: RowProxy = await (
+                await conn.execute(
+                    sa.select([projects]).where(projects.c.uuid == str(project_id))
+                )
+            ).first()
+            if not row:
+                raise ProjectNotFoundError(project_id)
+            return ProjectAtDB.from_orm(row)
