74710088532581ec832c15c17873fc965a37a932
Andrei Neagu <neagu@itis.swiss>
2021-03-29 09:17:27 +0200

    refactored to use pydantic

--- services/web/server/src/simcore_service_webserver/exporter/formatters/sds/text_files.py
+++ services/web/server/src/simcore_service_webserver/exporter/formatters/sds/text_files.py
@@ -1,26 +1,14 @@
 from typing import Union
 from pathlib import Path
 
+from pydantic import BaseModel, Field
 
-class BaseTextFile:
-    def _check_attribute(self, attribute_name: str):
-        if getattr(self, attribute_name) is None:
-            raise ValueError(f"'{attribute_name}' attribute is None, please define it")
 
-    def __init__(self, path: Union[str, Path] = None, text: str = None):
-        # gather all class attributes
-        # a class attribute is considered a member not starting with "__" or "_"
-        for name, value in vars(self.__class__).items():
-            if name.startswith("__") or name.startswith("_"):
-                continue
-            self.__setattr__(name, value)
-
-        self.path = self.__getattribute__("path") if path is None else path
-        self.text = self.__getattribute__("text") if text is None else text
-
-        # check mandatory fields are present
-        self._check_attribute("path")
-        self._check_attribute("text")
+class BaseTextFile(BaseModel):
+    path: Union[str, Path] = Field(
+        ..., description="relative path where to store this file"
+    )
+    text: str = Field(..., description="contens of the text file")
 
     def store_in_base_path(self, base_path: Path) -> None:
         destination = base_path / Path(self.path)
@@ -28,48 +16,48 @@ class BaseTextFile:
         destination.write_text(self.text)
 
 
-class ChangesFile(BaseTextFile):
-    path = "CHANGES"
-    text = (
+CHANGES_FILE = BaseTextFile(
+    path="CHANGES",
+    text=(
         "Optional text file that contains information about the "
         "history of the dataset"
-    )
+    ),
+)
 
+README_ROOT_LEVEL_FILE = BaseTextFile(
+    path="README",
+    text="",
+)
+README_CODE_FOLDER_FILE = BaseTextFile(
+    path="code/README",
+    text="Optional folder meant to contain any supporting code",
+)
 
-class ReadmeRootLevelFile(BaseTextFile):
-    path = "README"
-    text = ""
-
-
-class ReadmeCodeFolderFile(BaseTextFile):
-    path = "code/README"
-    text = "Optional folder meant to contain any supporting code"
-
-
-class ReadmeDerivativeFolderFile(BaseTextFile):
-    path = "derivative/README"
-    text = (
+README_DERIVATIVE_FOLDER_FILE = BaseTextFile(
+    path="derivative/README",
+    text=(
         "Optional folder that contains data derived from the data in "
         "the primary data folder or using the code in the code folder."
-    )
+    ),
+)
 
-
-class ReadmeDocsFolderFile(BaseTextFile):
-    path = "docs/README"
-    text = (
+README_DOCS_FOLDER_FILE = BaseTextFile(
+    path="docs/README",
+    text=(
         "Optional folder that contains data derived from the data in "
         "the primary data folder or using the code in the code folder."
-    )
+    ),
+)
 
 
 def write_text_files(base_path: Path) -> None:
     """Assembles and writes all the text files required by the standard"""
     text_files_to_store = [
-        ChangesFile(),
-        ReadmeRootLevelFile(),
-        ReadmeCodeFolderFile(),
-        ReadmeDerivativeFolderFile(),
-        ReadmeDocsFolderFile(),
+        CHANGES_FILE,
+        README_ROOT_LEVEL_FILE,
+        README_CODE_FOLDER_FILE,
+        README_DERIVATIVE_FOLDER_FILE,
+        README_DOCS_FOLDER_FILE,
     ]
     for text_file in text_files_to_store:
         text_file.store_in_base_path(base_path)
