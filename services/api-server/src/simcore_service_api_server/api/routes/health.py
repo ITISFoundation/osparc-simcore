5b0ffa10566efc2e1fabf6232757a36f511ddc58
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-04-07 21:37:33 +0200

    adds status entrypoint

--- services/api-server/src/simcore_service_api_server/api/routes/health.py
+++ services/api-server/src/simcore_service_api_server/api/routes/health.py
@@ -1,7 +1,12 @@
+import asyncio
 from datetime import datetime
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.responses import PlainTextResponse
+
+from ...modules.catalog import CatalogApi
+from ...modules.director_v2 import DirectorV2Api
+from ..dependencies.services import get_api_client
 
 router = APIRouter()
 
@@ -9,3 +14,16 @@ router = APIRouter()
 @router.get("/", include_in_schema=False, response_class=PlainTextResponse)
 async def check_service_health():
     return f"{__name__}@{datetime.utcnow().isoformat()}"
+
+
+@router.get("/state")
+async def get_service_state(
+    catalog_client: CatalogApi = Depends(get_api_client(CatalogApi)),
+    director2_api: DirectorV2Api = Depends(get_api_client(DirectorV2Api)),
+):
+    apis = (catalog_client, director2_api)
+    heaths = await asyncio.gather(*[api.is_responsive() for api in apis])
+
+    return {
+        api.service_name: {"healthy": is_healty} for api, is_healty in zip(apis, heaths)
+    }
