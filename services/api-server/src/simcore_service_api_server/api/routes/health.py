9332ce98cae41026bec105010a566abb904769fc
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-04-12 22:17:52 +0200

    minor

--- services/api-server/src/simcore_service_api_server/api/routes/health.py
+++ services/api-server/src/simcore_service_api_server/api/routes/health.py
@@ -16,13 +16,13 @@ async def check_service_health():
     return f"{__name__}@{datetime.utcnow().isoformat()}"
 
 
-@router.get("/state")
+@router.get("/state", include_in_schema=False)
 async def get_service_state(
     catalog_client: CatalogApi = Depends(get_api_client(CatalogApi)),
     director2_api: DirectorV2Api = Depends(get_api_client(DirectorV2Api)),
 ):
     apis = (catalog_client, director2_api)
-    heaths = await asyncio.gather(*[api.is_responsive() for api in apis])
+    heaths: List[bool] = await asyncio.gather(*[api.is_responsive() for api in apis])
 
     return {
         api.service_name: {"healthy": is_healty} for api, is_healty in zip(apis, heaths)
