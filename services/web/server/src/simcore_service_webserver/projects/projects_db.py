81164b98f7bbf08253c5383c71f1c52104a9ef3c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-04-14 18:28:22 +0200

    order by last change date descending

--- services/web/server/src/simcore_service_webserver/projects/projects_db.py
+++ services/web/server/src/simcore_service_webserver/projects/projects_db.py
@@ -337,6 +337,7 @@ class ProjectDBAPI:
                 {'AND projects.published ' if only_published else ''}
                 AND (jsonb_exists_any(projects.access_rights, array[{', '.join(f"'{group.gid}'" for group in user_groups)}])
                 OR prj_owner = {user_id})
+                ORDER BY last_change_date DESC
                 OFFSET {offset}
                 LIMIT {limit or 'NULL'}
                 """
