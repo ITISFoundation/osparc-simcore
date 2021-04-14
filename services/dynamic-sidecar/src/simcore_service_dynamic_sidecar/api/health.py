a7ea456b6fe51a91fbe390e5d70b75b953b50bdf
Andrei Neagu <neagu@itis.swiss>
2021-04-14 13:50:22 +0200

    fixed healt endpoint to fail when status is not healthy

--- services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/api/health.py
+++ services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/api/health.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Response
 
 from ..models import ApplicationHealth
 
@@ -6,8 +6,13 @@ health_router = APIRouter()
 
 
 @health_router.get("/health", response_model=ApplicationHealth)
-async def health_endpoint(request: Request) -> ApplicationHealth:
-    return request.app.state.application_health
+async def health_endpoint(request: Request, response: Response) -> ApplicationHealth:
+    application_health: ApplicationHealth = request.app.state.application_health
+
+    if application_health.is_healty is False:
+        response.status_code = 400
+
+    return application_health
 
 
 __all__ = ["health_router"]
