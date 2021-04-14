31b60eabcd74c5c689c4e88481eb12a61d55d5ba
Andrei Neagu <neagu@itis.swiss>
2021-04-14 11:00:54 +0200

    updating log warning messages for mocked calls

--- services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/api/mocked.py
+++ services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/api/mocked.py
@@ -15,31 +15,31 @@ mocked_router = APIRouter()
 
 @mocked_router.post("/push")
 async def post_push() -> str:
-    logger.warning("TODO: still need to implement")
+    logger.warning("ignoring call POST /push from frontend")
     return ""
 
 
 @mocked_router.get("/retrive")
 async def get_retrive() -> str:
-    logger.warning("TODO: still need to implement")
+    logger.warning("ignoring call GET /retrive from frontend")
     return ""
 
 
 @mocked_router.post("/retrive")
 async def post_retrive() -> str:
-    logger.warning("TODO: still need to implement")
+    logger.warning("ignoring call POST /retrive from frontend")
     return ""
 
 
 @mocked_router.get("/state")
 async def get_state() -> str:
-    logger.warning("TODO: still need to implement")
+    logger.warning("ignoring call GET /state from frontend")
     return ""
 
 
 @mocked_router.post("/state")
 async def post_state() -> str:
-    logger.warning("TODO: still need to implement")
+    logger.warning("ignoring call POST /state from frontend")
     return ""
 
 
