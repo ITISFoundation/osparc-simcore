d75d5bd338900c2b7fe536f9b83fb60ecd398cb5
odeimaiz <maiz@itis.swiss>
2021-04-15 16:19:00 +0200

    isInViewport

--- services/web/client/source/class/osparc/dashboard/StudyBrowserButtonLoadMore.js
+++ services/web/client/source/class/osparc/dashboard/StudyBrowserButtonLoadMore.js
@@ -32,7 +32,18 @@ qx.Class.define("osparc.dashboard.StudyBrowserButtonLoadMore", {
     this.__applyFetching(false);
 
     this.addListener("appear", () => {
-      console.log("Hi there");
+      const isInViewport = element => {
+        const rect = element.getBoundingClientRect();
+        const html = document.documentElement;
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || html.clientHeight) &&
+          rect.right <= (window.innerWidth || html.clientWidth)
+        );
+      };
+
+      console.log(isInViewport(this.getContentElement().getDomElement()));
     }, this);
   },
 
