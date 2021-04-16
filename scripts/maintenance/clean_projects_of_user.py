043ff45e4d528fe91f3781ce60fa5c88c66ba5bb
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-04-16 08:42:45 +0200

    use typer use logging

--- scripts/maintenance/clean_projects_of_user.py
+++ scripts/maintenance/clean_projects_of_user.py
@@ -1,11 +1,15 @@
 #! /usr/bin/env python3
 
 import asyncio
+import logging
 import pdb
 import sys
 from typing import Any, Dict, List
 
+import typer
 from httpx import URL, AsyncClient
+
+logger = logging.getLogger(__name__)
 
 
 async def login_user(client: AsyncClient, email: str, password: str):
@@ -23,38 +27,32 @@ async def get_all_projects_for_user(client: AsyncClient) -> List[Dict[str, Any]]
 
 async def delete_project(client: AsyncClient, project_id: str):
     path = f"/projects/{project_id}"
-    print("deleting project", project_id)
+    logger.info("deleting project %s", project_id)
     r = await client.delete(path)
-    print(r)
+    if r.status_code != httpx.codes.OK:
+        logger.error(
+            "deleting project %s failed with status %s", project_id, r.status_code
+        )
 
 
-async def main(endpoint: URL, username: str, password: str) -> int:
+async def main(endpoint: str, username: str, password: str) -> int:
     try:
         async with AsyncClient(base_url=endpoint.join("v0")) as client:
             await login_user(client, username, password)
             all_projects = await get_all_projects_for_user(client)
             if not all_projects:
-                print("no projects found!")
+                logger.error("no projects found!")
                 return 1
-            print("number of projects to delete:", len(all_projects))
+            logger.info("%s projects will be deleted...", len(all_projects))
             await asyncio.gather(
                 *[delete_project(client, prj["uuid"]) for prj in all_projects]
             )
-            print(f"deleted {len(all_projects)}")
+            logger.info("completed project deletion.")
     except Exception as e:
-        print("Unexpected issue:", type(e), str(e))
+        logger.exception("Unexpected issue", exc_info=True)
         return 1
     return 0
 
 
 if __name__ == "__main__":
-    print("Number of arguments:", len(sys.argv), "arguments.")
-    print("Argument List:", str(sys.argv))
-    endpoint = URL(sys.argv[1])
-    username = sys.argv[2]
-    password = sys.argv[3]
-    sys.exit(
-        asyncio.get_event_loop().run_until_complete(
-            main(endpoint=endpoint, username=username, password=password)
-        )
-    )
+    typer.run(main)
