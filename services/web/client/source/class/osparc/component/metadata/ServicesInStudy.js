534633e5d8e02f9f52038bd033b68941259b053a
odeimaiz <maiz@itis.swiss>
2021-04-20 12:29:17 +0200

    more info in ServicesInStudy

--- services/web/client/source/class/osparc/component/metadata/ServicesInStudy.js
+++ services/web/client/source/class/osparc/component/metadata/ServicesInStudy.js
@@ -27,43 +27,65 @@ qx.Class.define("osparc.component.metadata.ServicesInStudy", {
   construct: function(studyData) {
     this.base(arguments);
 
-    const grid = new qx.ui.layout.Grid(10);
+    const grid = new qx.ui.layout.Grid(20);
     grid.setColumnFlex(this.self().gridPos.name, 1);
     this._setLayout(grid);
 
     this.__studyData = studyData;
 
-    this.__populateLayout();
+    const store = osparc.store.Store.getInstance();
+    store.getServicesDAGs()
+      .then(services => {
+        this.__services = services;
+        this.__populateLayout();
+      });
   },
 
   statics: {
     gridPos: {
-      name: 0,
-      key: 1,
-      version: 2,
-      infoButton: 3
+      infoButton: 0,
+      name: 1,
+      key: 2,
+      currentVersion: 3,
+      latestVersion: 4
     }
   },
 
   members: {
+    __services: null,
+
     __populateLayout: function() {
+      let i=0;
+
+      this._add(new qx.ui.basic.Label(this.tr("Name")).set({
+        font: "title-14"
+      }), {
+        row: i,
+        column: this.self().gridPos.name
+      });
+      this._add(new qx.ui.basic.Label(this.tr("Key")).set({
+        font: "title-14"
+      }), {
+        row: i,
+        column: this.self().gridPos.key
+      });
+      this._add(new qx.ui.basic.Label(this.tr("Current")).set({
+        font: "title-14"
+      }), {
+        row: i,
+        column: this.self().gridPos.currentVersion
+      });
+      this._add(new qx.ui.basic.Label(this.tr("Latest")).set({
+        font: "title-14"
+      }), {
+        row: i,
+        column: this.self().gridPos.latestVersion
+      });
+      i++;
+
       const nodes = Object.values(this.__studyData.workbench);
-      for (let i=0; i<nodes.length; i++) {
-        const node = nodes[i];
-
-        const nameLabel = new qx.ui.basic.Label(node["label"]).set({
-          font: "text-14"
-        });
-
-        const parts = node["key"].split("/");
-        const keyLabel = new qx.ui.basic.Label(parts[parts.length-1]).set({
-          font: "text-14",
-          toolTipText: node["key"]
-        });
-
-        const versionLabel = new qx.ui.basic.Label(node["version"]).set({
-          font: "text-14"
-        });
+      nodes.forEach(node => {
+        const latestMetadata = osparc.utils.Services.getLatest(this.__services, node["key"]);
 
         const infoButton = new qx.ui.form.Button(null, "@FontAwesome5Solid/info-circle/14");
         infoButton.addListener("execute", () => {
@@ -75,6 +97,35 @@ qx.Class.define("osparc.component.metadata.ServicesInStudy", {
           osparc.ui.window.Window.popUpInWindow(serviceDetails, title, width, height);
         }, this);
 
+        const nameLabel = new qx.ui.basic.Label(node["label"]).set({
+          font: "text-14"
+        });
+
+        const parts = node["key"].split("/");
+        const keyLabel = new qx.ui.basic.Label(parts[parts.length-1]).set({
+          font: "text-14",
+          toolTipText: node["key"]
+        });
+
+        const currentVersionLabel = new qx.ui.basic.Label(node["version"]).set({
+          font: "text-14",
+          textAlign: "center"
+        });
+        if (node["version"] !== latestMetadata["version"]) {
+          currentVersionLabel.set({
+            backgroundColor: "yellow"
+          });
+        }
+
+        const latestVersionLabel = new qx.ui.basic.Label(latestMetadata["version"]).set({
+          font: "text-14",
+          textAlign: "center"
+        });
+
+        this._add(infoButton, {
+          row: i,
+          column: this.self().gridPos.infoButton
+        });
         this._add(nameLabel, {
           row: i,
           column: this.self().gridPos.name
@@ -83,15 +134,16 @@ qx.Class.define("osparc.component.metadata.ServicesInStudy", {
           row: i,
           column: this.self().gridPos.key
         });
-        this._add(versionLabel, {
+        this._add(currentVersionLabel, {
           row: i,
-          column: this.self().gridPos.version
+          column: this.self().gridPos.currentVersion
         });
-        this._add(infoButton, {
+        this._add(latestVersionLabel, {
           row: i,
-          column: this.self().gridPos.infoButton
+          column: this.self().gridPos.latestVersion
         });
-      }
+        i++;
+      });
     }
   }
 });
