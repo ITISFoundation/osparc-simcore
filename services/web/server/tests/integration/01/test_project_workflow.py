e1893369a474d293c8d2f2958621bc1e3b18aee3
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-04-21 17:24:08 +0200

    removed projects_fakes.py

--- services/web/server/tests/integration/01/test_project_workflow.py
+++ services/web/server/tests/integration/01/test_project_workflow.py
@@ -87,37 +87,6 @@ def client(
     )
 
 
-@pytest.fixture(scope="session")
-def fake_template_projects(package_dir: Path) -> Dict:
-    projects_file = package_dir / "data" / "fake-template-projects.json"
-    assert projects_file.exists()
-    with projects_file.open() as fp:
-        return json.load(fp)
-
-
-@pytest.fixture(scope="session")
-def fake_template_projects_isan(package_dir: Path) -> Dict:
-    projects_file = package_dir / "data" / "fake-template-projects.isan.json"
-    assert projects_file.exists()
-    with projects_file.open() as fp:
-        return json.load(fp)
-
-
-@pytest.fixture(scope="session")
-def fake_template_projects_osparc(package_dir: Path) -> Dict:
-    projects_file = package_dir / "data" / "fake-template-projects.osparc.json"
-    assert projects_file.exists()
-    with projects_file.open() as fp:
-        return json.load(fp)
-
-
-@pytest.fixture
-def fake_db():
-    Fake.reset()
-    yield Fake
-    Fake.reset()
-
-
 @pytest.fixture
 def fake_project_data(fake_data_dir: Path) -> Dict:
     with (fake_data_dir / "fake-project.json").open() as fp:
@@ -352,36 +321,3 @@ async def test_delete_invalid_project(
     resp = await client.delete(url)
 
     await assert_status(resp, web.HTTPNotFound)
-
-
-@pytest.mark.parametrize("user_role", [UserRole.USER])
-async def test_list_template_projects(
-    client,
-    postgres_db: sa.engine.Engine,
-    docker_registry: str,
-    simcore_services,
-    logged_user,
-    fake_db,
-    fake_template_projects,
-    fake_template_projects_isan,
-    fake_template_projects_osparc,
-    catalog_subsystem_mock,
-    director_v2_service_mock,
-):
-    catalog_subsystem_mock(
-        fake_template_projects
-        + fake_template_projects_isan
-        + fake_template_projects_osparc
-    )
-    fake_db.load_template_projects()
-    url = client.app.router["list_projects"].url_for()
-    resp = await client.get(url.with_query(type="template"))
-
-    projects, _ = await assert_status(resp, web.HTTPOk)
-
-    # fake-template-projects.json + fake-template-projects.isan.json + fake-template-projects.osparc.json
-    assert len(projects) == (
-        len(fake_template_projects)
-        + len(fake_template_projects_isan)
-        + len(fake_template_projects_osparc)
-    )
