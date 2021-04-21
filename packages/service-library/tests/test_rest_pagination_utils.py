4f5839a3274beae089c050d907e77beb1b0e5888
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-04-21 10:44:35 +0200

    rest_pagination_utils fully tested

--- packages/service-library/tests/test_rest_pagination_utils.py
+++ packages/service-library/tests/test_rest_pagination_utils.py
@@ -1,7 +1,12 @@
 from copy import deepcopy
 
 import pytest
-from servicelib.rest_pagination_utils import PageResponseLimitOffset
+from servicelib.rest_pagination_utils import (
+    PageLinks,
+    PageMetaInfoLimitOffset,
+    PageResponseLimitOffset,
+)
+from yarl import URL
 
 
 def test_page_response_limit_offset_model():
@@ -29,3 +34,78 @@ def test_more_data_than_limit_raises():
 
     with pytest.raises(ValueError):
         PageResponseLimitOffset(**example)
+
+
+def test_paginating_data():
+    # create random data
+    total_number_of_data = 29
+    limit = 9
+    offset = 0
+    partial_data = [range(7)]
+    request_url = URL("http://some/random/url.com?some=1&random=4&query=true")
+
+    # first "call"
+    model_instance: PageResponseLimitOffset = PageResponseLimitOffset.paginate_data(
+        partial_data, request_url, total_number_of_data, limit, offset
+    )
+
+    assert model_instance
+    assert model_instance.meta == PageMetaInfoLimitOffset(
+        total=total_number_of_data, count=len(partial_data), limit=limit, offset=offset
+    )
+    assert model_instance.links == PageLinks(
+        self=f"http://some/random/url.com?some=1&random=4&query=true&offset={offset}&limit={limit}",
+        first=f"http://some/random/url.com?some=1&random=4&query=true&offset=0&limit={limit}",
+        prev=None,
+        next=f"http://some/random/url.com?some=1&random=4&query=true&offset=1&limit={limit}",
+        last=f"http://some/random/url.com?some=1&random=4&query=true&offset=3&limit={limit}",
+    )
+
+    # next "call"s
+    for i in [1, 2]:
+        model_instance: PageResponseLimitOffset = PageResponseLimitOffset.paginate_data(
+            partial_data,
+            URL(model_instance.links.next),
+            total_number_of_data,
+            limit,
+            offset + i,
+        )
+
+        assert model_instance
+        assert model_instance.meta == PageMetaInfoLimitOffset(
+            total=total_number_of_data,
+            count=len(partial_data),
+            limit=limit,
+            offset=offset + i,
+        )
+        assert model_instance.links == PageLinks(
+            self=f"http://some/random/url.com?some=1&random=4&query=true&offset={offset+i}&limit={limit}",
+            first=f"http://some/random/url.com?some=1&random=4&query=true&offset=0&limit={limit}",
+            prev=f"http://some/random/url.com?some=1&random=4&query=true&offset={i-1}&limit={limit}",
+            next=f"http://some/random/url.com?some=1&random=4&query=true&offset={i+1}&limit={limit}",
+            last=f"http://some/random/url.com?some=1&random=4&query=true&offset=3&limit={limit}",
+        )
+
+    # last "call"
+    model_instance: PageResponseLimitOffset = PageResponseLimitOffset.paginate_data(
+        partial_data,
+        URL(model_instance.links.next),
+        total_number_of_data,
+        limit,
+        offset + 3,
+    )
+
+    assert model_instance
+    assert model_instance.meta == PageMetaInfoLimitOffset(
+        total=total_number_of_data,
+        count=len(partial_data),
+        limit=limit,
+        offset=offset + 3,
+    )
+    assert model_instance.links == PageLinks(
+        self=f"http://some/random/url.com?some=1&random=4&query=true&offset={offset+3}&limit={limit}",
+        first=f"http://some/random/url.com?some=1&random=4&query=true&offset=0&limit={limit}",
+        prev=f"http://some/random/url.com?some=1&random=4&query=true&offset=2&limit={limit}",
+        next=None,
+        last=f"http://some/random/url.com?some=1&random=4&query=true&offset=3&limit={limit}",
+    )
