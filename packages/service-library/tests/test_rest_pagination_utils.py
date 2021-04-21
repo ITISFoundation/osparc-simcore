8e82c79e3b004bbb39883dda2a879f1dc9bf2b9b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-04-21 10:21:21 +0200

    test validators

--- packages/service-library/tests/test_rest_pagination_utils.py
+++ packages/service-library/tests/test_rest_pagination_utils.py
@@ -1,7 +1,10 @@
+from copy import deepcopy
+
+import pytest
 from servicelib.rest_pagination_utils import PageResponseLimitOffset
 
 
-def test_paginate_limit_offset_models():
+def test_page_response_limit_offset_model():
     examples = PageResponseLimitOffset.Config.schema_extra["examples"]
 
     for index, example in enumerate(examples):
@@ -9,3 +12,20 @@ def test_paginate_limit_offset_models():
 
         model_instance = PageResponseLimitOffset(**example)
         assert model_instance
+
+
+def test_empty_data_is_converted_to_list():
+    example = deepcopy(PageResponseLimitOffset.Config.schema_extra["examples"][0])
+    example["data"] = None
+
+    model_instance = PageResponseLimitOffset(**example)
+    assert model_instance
+    assert model_instance.data == []
+
+
+def test_more_data_than_limit_raises():
+    example = deepcopy(PageResponseLimitOffset.Config.schema_extra["examples"][0])
+    example["_meta"]["limit"] = len(example["data"]) - 1
+
+    with pytest.raises(ValueError):
+        PageResponseLimitOffset(**example)
