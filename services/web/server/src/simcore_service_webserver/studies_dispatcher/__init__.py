442211af1f766065f658b51b1d0f55c7bb81421f
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2021-04-22 19:07:57 +0200

    typo
    Co-authored-by: Sylvain <35365065+sanderegg@users.noreply.github.com>

--- services/web/server/src/simcore_service_webserver/studies_dispatcher/__init__.py
+++ services/web/server/src/simcore_service_webserver/studies_dispatcher/__init__.py
@@ -2,7 +2,7 @@
     This app module dispatches pre-configure or on-the-fly studies to user from a link request
     that is redirected to the front-end which customizes its view
 
-    - Table services_consume_filetypes defines a map between a servive (key,version,input_port) and a filetype
+    - Table services_consume_filetypes defines a map between a service (key,version,input_port) and a filetype
     - Exposes viewers resource in API (handlers_rest.py)
     - Entrypoint that dispatches a study to download & view a file (handlers_redirects.py)
         - finds default viewer
