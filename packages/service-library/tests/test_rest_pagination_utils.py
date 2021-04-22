f8333f7ed6d1eba719338001af1cc8cf34e2af1f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-04-22 10:13:15 +0200

    fix unit testing pagination utils and added some corner cases

--- packages/service-library/tests/test_rest_pagination_utils.py
+++ packages/service-library/tests/test_rest_pagination_utils.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 
 import pytest
+from pydantic.main import BaseModel
 from servicelib.rest_pagination_utils import (
     PageLinks,
     PageMetaInfoLimitOffset,
@@ -9,31 +10,51 @@ from servicelib.rest_pagination_utils import (
 from yarl import URL
 
 
-def test_page_response_limit_offset_model():
-    examples = PageResponseLimitOffset.Config.schema_extra["examples"]
+@pytest.mark.parametrize(
+    "cls_model", [PageResponseLimitOffset, PageMetaInfoLimitOffset]
+)
+def test_page_response_limit_offset_models(cls_model: BaseModel):
+    examples = cls_model.Config.schema_extra["examples"]
 
     for index, example in enumerate(examples):
         print(f"{index:-^10}:\n", example)
 
-        model_instance = PageResponseLimitOffset(**example)
+        model_instance = cls_model(**example)
         assert model_instance
+
+
+def test_invalid_offset():
+    with pytest.raises(ValueError):
+        PageMetaInfoLimitOffset(limit=6, total=5, offset=5, count=2)
+
+
+@pytest.mark.parametrize(
+    "count, offset",
+    [
+        pytest.param(7, 0, id="count bigger than limit"),
+        pytest.param(6, 0, id="count bigger than total"),
+        pytest.param(5, 1, id="count + offset bigger than total"),
+    ],
+)
+def test_invalid_count(count: int):
+    with pytest.raises(ValueError):
+        PageMetaInfoLimitOffset(limit=6, total=5, offset=0, count=count)
+
+
+def test_data_size_does_not_fit_count():
+    example = deepcopy(PageResponseLimitOffset.Config.schema_extra["examples"][0])
+    example["_meta"]["count"] = len(example["data"]) - 1
+    with pytest.raises(ValueError):
+        PageResponseLimitOffset(**example)
 
 
 def test_empty_data_is_converted_to_list():
     example = deepcopy(PageResponseLimitOffset.Config.schema_extra["examples"][0])
     example["data"] = None
-
+    example["_meta"]["count"] = 0
     model_instance = PageResponseLimitOffset(**example)
     assert model_instance
     assert model_instance.data == []
-
-
-def test_more_data_than_limit_raises():
-    example = deepcopy(PageResponseLimitOffset.Config.schema_extra["examples"][0])
-    example["_meta"]["limit"] = len(example["data"]) - 1
-
-    with pytest.raises(ValueError):
-        PageResponseLimitOffset(**example)
 
 
 def test_paginating_data():
@@ -41,7 +62,7 @@ def test_paginating_data():
     total_number_of_data = 29
     limit = 9
     offset = 0
-    partial_data = [range(7)]
+    partial_data = [range(9)]
     request_url = URL("http://some/random/url.com?some=1&random=4&query=true")
 
     # first "call"
