0da40fd8cda280ef434d561659771eb72b9c9f12
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-04-22 08:51:40 +0200

    linter

--- services/web/server/tests/unit/with_dbs/09/test_projects_01.py
+++ services/web/server/tests/unit/with_dbs/09/test_projects_01.py
@@ -1,6 +1,7 @@
 # pylint:disable=unused-variable
 # pylint:disable=unused-argument
 # pylint:disable=redefined-outer-name
+# pylint:disable=too-many-arguments
 import asyncio
 import json
 import time
