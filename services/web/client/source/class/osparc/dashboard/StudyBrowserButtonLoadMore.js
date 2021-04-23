01ec6a916f53e81e8a06a3f924f5714206454852
odeimaiz <maiz@itis.swiss>
2021-04-23 12:44:11 +0200

    Update StudyBrowserButtonLoadMore.js

--- services/web/client/source/class/osparc/dashboard/StudyBrowserButtonLoadMore.js
+++ services/web/client/source/class/osparc/dashboard/StudyBrowserButtonLoadMore.js
@@ -40,17 +40,23 @@ qx.Class.define("osparc.dashboard.StudyBrowserButtonLoadMore", {
   members: {
     checkIsOnScreen: function() {
       const isInViewport = element => {
-        const rect = element.getBoundingClientRect();
-        const html = document.documentElement;
-        return (
-          rect.top >= 0 &&
-          rect.left >= 0 &&
-          rect.bottom <= (window.innerHeight || html.clientHeight) &&
-          rect.right <= (window.innerWidth || html.clientWidth)
-        );
+        if (element) {
+          const rect = element.getBoundingClientRect();
+          const html = document.documentElement;
+          return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || html.clientHeight) &&
+            rect.right <= (window.innerWidth || html.clientWidth)
+          );
+        }
+        return false;
       };
 
-      const checkIsOnScreen = isInViewport(this.getContentElement().getDomElement());
+      const domElem = this.getContentElement().getDomElement();
+      const checkIsOnScreen = isInViewport(domElem);
+      console.log(domElem);
+      console.log(checkIsOnScreen);
       return checkIsOnScreen;
     },
 
