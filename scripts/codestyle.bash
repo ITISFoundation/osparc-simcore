0cc50a4ebe5ca8a7526208336993e177e4f0f687
Andrei Neagu <neagu@itis.swiss>
2021-04-23 08:31:05 +0200

    fixed typos

--- scripts/codestyle.bash
+++ scripts/codestyle.bash
@@ -10,7 +10,7 @@ BASE_PATH_DIR=${3-MISSING_DIR}
 
 # used for development (fails on pylint and mypy)
 development() {
-  echo "enforcing codestyle to soruce_directory=$SRC_DIRECTORY_NAME"
+  echo "enforcing codestyle to source_directory=$SRC_DIRECTORY_NAME"
   echo "isort"
   isort setup.py src/"$SRC_DIRECTORY_NAME" tests
   echo "black"
@@ -23,7 +23,7 @@ development() {
 
 # invoked by ci as test (also fails on isort and black)
 ci() {
-  echo "checking codestyle in service=$BASE_PATH_DIR with soruce_directory=$SRC_DIRECTORY_NAME"
+  echo "checking codestyle in service=$BASE_PATH_DIR with source_directory=$SRC_DIRECTORY_NAME"
   echo "isort"
   isort --check setup.py "$BASE_PATH_DIR"/src/"$SRC_DIRECTORY_NAME" "$BASE_PATH_DIR"/tests
   echo "black"
