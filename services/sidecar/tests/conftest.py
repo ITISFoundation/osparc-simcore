54907b6724dadc012e00d1c3983e366b53e41df3
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-04-26 18:28:45 +0200

    typo

--- services/sidecar/tests/conftest.py
+++ services/sidecar/tests/conftest.py
@@ -11,7 +11,8 @@ import simcore_service_sidecar
 pytest_plugins = [
     "pytest_simcore.docker_compose",
     "pytest_simcore.docker_swarm",
-    "pytest_simcore.monkeypatch_extra" "pytest_simcore.postgres_service",
+    "pytest_simcore.monkeypatch_extra",
+    "pytest_simcore.postgres_service",
     "pytest_simcore.rabbit_service",
     "pytest_simcore.repository_paths",
 ]
