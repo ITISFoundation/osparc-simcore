90ca8e9579004f7f2a31bcf8ccadc3cd6e3f6141
Andrei Neagu <neagu@itis.swiss>
2021-04-27 08:38:59 +0200

    added missing refactor

--- packages/service-library/tests/test_rest_pagination_utils.py
+++ packages/service-library/tests/test_rest_pagination_utils.py
@@ -58,9 +58,7 @@ def test_empty_data_is_converted_to_list():
     assert model_instance.data == []
 
 
-@pytest.mark.parametrize(
-    "base_url", ["http://some/random/url.com", "http://10.0.0.1.nip.io"]
-)
+@pytest.mark.parametrize("base_url", ["http://some/random/url.com"])
 def test_paginating_data(base_url):
     # create random data
     total_number_of_data = 29
