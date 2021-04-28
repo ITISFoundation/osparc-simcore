8ce29aadd4653148053e03d08df0a0329f435d31
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-04-28 18:52:33 +0200

    Added tooling to test performance of the API (for the moment manually)

---
+++ tests/performance/locustfile.py
@@ -0,0 +1,62 @@
+#
+# SEE https://docs.locust.io/en/stable/quickstart.html
+#
+
+import faker
+from locust import HttpUser, between, task
+
+fake = faker.Faker()
+
+
+class WebApiUser(HttpUser):
+    wait_time = between(
+        1, 2.5
+    )  #  simulated users wait between 1 and 2.5 seconds after each task (see below) is executed.
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.email = fake.email()
+        self.count = 0
+
+    @task
+    def health_check(self):
+        self.client.get("/v0/health")
+
+    @task(weight=2)
+    def create_project_from_template(self):
+        self.count += 1
+
+        # WARNING: this template needs to be created and shared with everybody
+        self.client.post(
+            "/v0/projects?from_template=8b6fa65c-a830-11eb-a370-02420a000012",
+            json={
+                "uuid": "",
+                "name": f"TEST {self.count}",
+                "description": f"{__name__}-{self.email}",
+                "prjOwner": self.email,
+                "accessRights": {},
+                "creationDate": "2021-04-28T14:46:53.674Z",
+                "lastChangeDate": "2021-04-28T14:46:53.674Z",
+                "thumbnail": "",
+                "workbench": {},
+            },
+        )
+
+    def on_start(self):
+        print("Created User ", self.email)
+        self.client.post(
+            "/v0/auth/register",
+            json={
+                "email": self.email,
+                "password": "my secret",
+                "confirm": "my secret",
+            },
+        )
+        self.client.post(
+            "/v0/auth/login", json={"email": self.email, "password": "my secret"}
+        )
+
+    def on_stop(self):
+        self.client.post("/v0/auth/logout")
+        print("Stopping", self.email)
