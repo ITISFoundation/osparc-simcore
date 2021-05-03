18ff99231c1fd89931ccc893fe55cc6419773bd0
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-05-03 21:47:24 +0200

    removed api dependencies

--- services/comp-resource-manager/src/simcore_service_comp_resource_manager/api/routes/health.py
+++ services/comp-resource-manager/src/simcore_service_comp_resource_manager/api/routes/health.py
@@ -1,17 +1,14 @@
-import asyncio
 from datetime import datetime
-from typing import Callable, Tuple
+from typing import Callable
 
 from fastapi import APIRouter, Depends
 from fastapi.responses import PlainTextResponse
 from models_library.app_diagnostics import AppStatusCheck
 
 from ..._meta import api_version, project_name
-from ...modules.catalog import CatalogApi
-from ...modules.director_v2 import DirectorV2Api
-from ...modules.storage import StorageApi
 from ..dependencies.application import get_reverse_url_mapper
-from ..dependencies.services import get_api_client
+
+# from ..dependencies.services import get_api_client
 
 router = APIRouter()
 
@@ -23,20 +20,15 @@ async def check_service_health():
 
 @router.get("/state", include_in_schema=False)
 async def get_service_state(
-    catalog_client: CatalogApi = Depends(get_api_client(CatalogApi)),
-    director2_api: DirectorV2Api = Depends(get_api_client(DirectorV2Api)),
-    storage_client: StorageApi = Depends(get_api_client(StorageApi)),
     url_for: Callable = Depends(get_reverse_url_mapper),
 ):
-    apis = (catalog_client, director2_api, storage_client)
-    heaths: Tuple[bool] = await asyncio.gather(*[api.is_responsive() for api in apis])
+    # healths: Tuple[bool] = await asyncio.gather(*[api.is_responsive() for api in apis])
 
     current_status = AppStatusCheck(
         app_name=project_name,
         version=api_version,
         services={
-            api.service_name: {"healthy": is_healty}
-            for api, is_healty in zip(apis, heaths)
+            # api.service_name: {"healthy": is_healty} for api, is_healty in zip(healths)
         },
         url=url_for("get_service_state"),
     )
