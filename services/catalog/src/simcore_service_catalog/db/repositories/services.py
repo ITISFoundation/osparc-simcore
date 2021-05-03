7aad971bc99ec7876c6884ce0b38f55e9b17ace6
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-05-03 17:43:27 +0200

    improve speed of retrieval

--- services/catalog/src/simcore_service_catalog/db/repositories/services.py
+++ services/catalog/src/simcore_service_catalog/db/repositories/services.py
@@ -1,18 +1,60 @@
 import logging
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import sqlalchemy as sa
+from aiopg.sa import SAConnection
 from aiopg.sa.result import RowProxy
 from models_library.services import ServiceAccessRightsAtDB, ServiceMetaDataAtDB
 from psycopg2.errors import ForeignKeyViolation  # pylint: disable=no-name-in-module
 from sqlalchemy import literal_column
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.sql import and_, or_
+from sqlalchemy.sql.selectable import Select
 
-from ..tables import services_access_rights, services_meta_data
+from ..tables import services_access_rights, services_meta_data, users
 from ._base import BaseRepository
 
 logger = logging.getLogger(__name__)
+
+
+def _make_list_services_query(
+    gids: Optional[List[int]] = None,
+    execute_access: Optional[bool] = None,
+    write_access: Optional[bool] = None,
+    combine_access_with_and: Optional[bool] = True,
+    product_name: Optional[str] = None,
+) -> Select:
+    query = sa.select([services_meta_data])
+    if gids or execute_access or write_access:
+        access_query_part = and_(
+            services_access_rights.c.execute_access if execute_access else True,
+            services_access_rights.c.write_access if write_access else True,
+        )
+        if not combine_access_with_and:
+            access_query_part = or_(
+                services_access_rights.c.execute_access if execute_access else True,
+                services_access_rights.c.write_access if write_access else True,
+            )
+        query = (
+            sa.select(
+                [services_meta_data],
+                distinct=[services_meta_data.c.key, services_meta_data.c.version],
+            )
+            .select_from(services_meta_data.join(services_access_rights))
+            .where(
+                and_(
+                    or_(*[services_access_rights.c.gid == gid for gid in gids])
+                    if gids
+                    else True,
+                    access_query_part,
+                    (services_access_rights.c.product_name == product_name)
+                    if product_name
+                    else True,
+                )
+            )
+            .order_by(services_meta_data.c.key, services_meta_data.c.version)
+        )
+    return query
 
 
 class ServicesRepository(BaseRepository):
@@ -26,39 +68,56 @@ class ServicesRepository(BaseRepository):
     ) -> List[ServiceMetaDataAtDB]:
         services_in_db = []
 
-        query = sa.select([services_meta_data])
-        if gids or execute_access or write_access:
-            access_query_part = and_(
-                services_access_rights.c.execute_access if execute_access else True,
-                services_access_rights.c.write_access if write_access else True,
-            )
-            if not combine_access_with_and:
-                access_query_part = or_(
-                    services_access_rights.c.execute_access if execute_access else True,
-                    services_access_rights.c.write_access if write_access else True,
+        async with self.db_engine.acquire() as conn:
+            async for row in conn.execute(
+                _make_list_services_query(
+                    gids,
+                    execute_access,
+                    write_access,
+                    combine_access_with_and,
+                    product_name,
                 )
-            query = (
-                sa.select(
-                    [services_meta_data],
-                    distinct=[services_meta_data.c.key, services_meta_data.c.version],
+            ):
+                services_in_db.append(ServiceMetaDataAtDB(**row))
+        return services_in_db
+
+    async def list_services_and_rights_owner(
+        self,
+        gids: Optional[List[int]] = None,
+        execute_access: Optional[bool] = None,
+        write_access: Optional[bool] = None,
+        combine_access_with_and: Optional[bool] = True,
+        product_name: Optional[str] = None,
+    ) -> List[Tuple[ServiceMetaDataAtDB, List[ServiceAccessRightsAtDB], str]]:
+        services_in_db = []
+
+        async with self.db_engine.acquire() as conn:
+            async for row in conn.execute(
+                _make_list_services_query(
+                    gids,
+                    execute_access,
+                    write_access,
+                    combine_access_with_and,
+                    product_name,
                 )
-                .select_from(services_meta_data.join(services_access_rights))
-                .where(
-                    and_(
-                        or_(*[services_access_rights.c.gid == gid for gid in gids])
-                        if gids
-                        else True,
-                        access_query_part,
-                        (services_access_rights.c.product_name == product_name)
-                        if product_name
-                        else True,
+            ):
+                services_in_db.append(
+                    (
+                        ServiceMetaDataAtDB(**row),
+                        await self.get_service_access_rights(
+                            row[services_meta_data.c.key],
+                            row[services_meta_data.c.version],
+                            product_name,
+                            conn,
+                        ),
+                        await conn.scalar(
+                            sa.select([users.c.email]).where(
+                                users.c.primary_gid == row[services_meta_data.c.owner]
+                            )
+                        )
+                        or None,
                     )
                 )
-                .order_by(services_meta_data.c.key, services_meta_data.c.version)
-            )
-        async with self.db_engine.acquire() as conn:
-            async for row in conn.execute(query):
-                services_in_db.append(ServiceMetaDataAtDB(**row))
         return services_in_db
 
     async def get_service(
@@ -149,6 +208,7 @@ class ServicesRepository(BaseRepository):
         key: str,
         version: str,
         product_name: str,
+        connection: Optional[SAConnection],
     ) -> List[ServiceAccessRightsAtDB]:
         services_in_db = []
         query = sa.select([services_access_rights]).where(
@@ -156,9 +216,13 @@ class ServicesRepository(BaseRepository):
             & (services_access_rights.c.version == version)
             & (services_access_rights.c.product_name == product_name)
         )
-        async with self.db_engine.acquire() as conn:
-            async for row in conn.execute(query):
+        if connection:
+            async for row in connection.execute(query):
                 services_in_db.append(ServiceAccessRightsAtDB(**row))
+        else:
+            async with self.db_engine.acquire() as conn:
+                async for row in conn.execute(query):
+                    services_in_db.append(ServiceAccessRightsAtDB(**row))
         return services_in_db
 
     async def upsert_service_access_rights(
