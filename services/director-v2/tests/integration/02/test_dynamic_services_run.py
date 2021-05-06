09dc8b1f698202a3663539e4da8cabd7fd2b3979
Andrei Neagu <neagu@itis.swiss>
2021-05-06 10:31:14 +0200

    enabled dynamic_services

--- services/director-v2/tests/integration/02/test_dynamic_services_run.py
+++ services/director-v2/tests/integration/02/test_dynamic_services_run.py
@@ -83,16 +83,16 @@ async def httpbins_project(
                 "version": httpbin_service["image"]["tag"],
                 "label": "legacy dynamic service",
             },
-            # uuid_dynamic_sidecar: {
-            #     "key": httpbin_dynamic_sidecar_service["image"]["name"],
-            #     "version": httpbin_dynamic_sidecar_service["image"]["tag"],
-            #     "label": "dynamic sidecar",
-            # },
-            # uuid_dynamic_sidecar_compose: {
-            #     "key": httpbin_dynamic_sidecar_compose_service["image"]["name"],
-            #     "version": httpbin_dynamic_sidecar_compose_service["image"]["tag"],
-            #     "label": "dynamic sidecar with docker compose spec",
-            # },
+            uuid_dynamic_sidecar: {
+                "key": httpbin_dynamic_sidecar_service["image"]["name"],
+                "version": httpbin_dynamic_sidecar_service["image"]["tag"],
+                "label": "dynamic sidecar",
+            },
+            uuid_dynamic_sidecar_compose: {
+                "key": httpbin_dynamic_sidecar_compose_service["image"]["name"],
+                "version": httpbin_dynamic_sidecar_compose_service["image"]["tag"],
+                "label": "dynamic sidecar with docker compose spec",
+            },
         }
     )
 
