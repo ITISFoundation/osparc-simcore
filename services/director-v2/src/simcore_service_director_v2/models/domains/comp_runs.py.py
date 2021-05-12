c1614dc5db2faa3d96ba5b36c5869704dd8311d3
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-05-12 10:32:19 +0200

    added comp_run table

---
+++ services/director-v2/src/simcore_service_director_v2/models/domains/comp_runs.py.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from typing import Optional
+
+from models_library.projects import ProjectID
+from models_library.projects_state import RunningState
+from pydantic import BaseModel, PositiveInt, validator
+from simcore_postgres_database.models.comp_pipeline import StateType
+
+from ...utils.db import DB_TO_RUNNING_STATE
+from ..schemas.constants import UserID
+
+
+class CompRunsAtDB(BaseModel):
+    run_id: PositiveInt
+    project_id: ProjectID
+    user_id: UserID
+    result: RunningState
+    created: datetime
+    modified: datetime
+    start: Optional[datetime]
+    end: Optional[datetime]
+
+    @validator("result", pre=True)
+    @classmethod
+    def convert_result_if_needed(cls, v):
+        if isinstance(v, StateType):
+            return RunningState(DB_TO_RUNNING_STATE[StateType(v)])
+        return v
+
+    class Config:
+        orm_mode = True
