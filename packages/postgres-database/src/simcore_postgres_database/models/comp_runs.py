01c5f8ee5f52504feeae823367a1db9eaf5e523c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-05-12 10:23:57 +0200

    created comp_runs table

---
+++ packages/postgres-database/src/simcore_postgres_database/models/comp_runs.py
@@ -0,0 +1,55 @@
+""" Computational Runs Table
+
+"""
+import sqlalchemy as sa
+from sqlalchemy.sql import func
+
+from .base import metadata
+from .comp_pipeline import StateType
+
+comp_runs = sa.Table(
+    "comp_runs",
+    metadata,
+    # this task db id
+    sa.Column("run_id", sa.BigInteger, nullable=False, primary_key=True),
+    sa.Column(
+        "project_uuid",
+        sa.String,
+        sa.ForeignKey(
+            "projects.uuid",
+            name="fk_comp_runs_project_uuid_projects",
+            onupdate="CASCADE",
+            ondelete="CASCADE",
+        ),
+        nullable=False,
+    ),
+    sa.Column(
+        "user_id",
+        sa.BigInteger(),
+        sa.ForeignKey(
+            "users.id",
+            name="fk_comp_runs_user_id_users",
+            onupdate="CASCADE",
+            ondelete="CASCADE",
+        ),
+        nullable=False,
+    ),
+    sa.Column(
+        "result",
+        sa.Enum(StateType),
+        nullable=False,
+        server_default=StateType.NOT_STARTED.value,
+    ),
+    # dag node id and class
+    sa.Column("created", sa.DateTime(), nullable=False, server_default=func.now()),
+    sa.Column(
+        "modified",
+        sa.DateTime(),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now(),  # this will auto-update on modification
+    ),
+    # utc timestamps for submission/start/end
+    sa.Column("start", sa.DateTime),
+    sa.Column("end", sa.DateTime),
+)
