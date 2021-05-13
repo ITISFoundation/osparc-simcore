8a8c16768beb4bd881d5c5016c886b597863780b
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-05-13 10:50:10 +0200

    Cleanup an doc products table

--- packages/postgres-database/src/simcore_postgres_database/models/products.py
+++ packages/postgres-database/src/simcore_postgres_database/models/products.py
@@ -16,8 +16,13 @@ from .base import metadata
 products = sa.Table(
     "products",
     metadata,
-    sa.Column("name", sa.String, nullable=False),
-    sa.Column("host_regex", sa.String, nullable=False),
+    sa.Column("name", sa.String, nullable=False, doc="Uniquely identifies a product"),
+    sa.Column(
+        "host_regex",
+        sa.String,
+        nullable=False,
+        doc="Regular expression that matches product hostname from an url string",
+    ),
     sa.Column("created", sa.DateTime(), nullable=False, server_default=func.now()),
     sa.Column(
         "modified",
@@ -25,6 +30,7 @@ products = sa.Table(
         nullable=False,
         server_default=func.now(),
         onupdate=func.now(),  # this will auto-update on modification
+        doc="Automaticaly updates on modification of the row",
     ),
     sa.PrimaryKeyConstraint("name", name="products_pk"),
 )
