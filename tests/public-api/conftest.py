e07383175d9ca928b0425e704bb503ee539acacd
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-05-14 20:39:23 +0200

    Added tmp_path_extra pytest_plugin and sorted list

--- tests/public-api/conftest.py
+++ tests/public-api/conftest.py
@@ -18,11 +18,12 @@ log = logging.getLogger(__name__)
 
 
 pytest_plugins = [
-    "pytest_simcore.repository_paths",
     "pytest_simcore.docker_compose",
-    "pytest_simcore.docker_swarm",
     "pytest_simcore.docker_registry",
+    "pytest_simcore.docker_swarm",
+    "pytest_simcore.repository_paths",
     "pytest_simcore.schemas",
+    "pytest_simcore.tmp_path_extra",
 ]
 
 
