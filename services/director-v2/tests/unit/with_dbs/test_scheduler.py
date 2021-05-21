456b573cfebe188b03239c1cf0e2cdd789a93c59
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-05-21 16:28:25 +0200

    fix scheduler unit test

--- services/director-v2/tests/unit/with_dbs/test_scheduler.py
+++ services/director-v2/tests/unit/with_dbs/test_scheduler.py
@@ -28,22 +28,6 @@ from simcore_service_director_v2.modules.scheduler import (
     scheduler_task,
 )
 
-# 1. setup database
-# 2. setup tables
-# 3. init scheduler, it should be empty
-# 4. create a project, set some pipeline, tasks
-# 5. based on the task states, the scheduler should schedule,
-# or stop, or mark as failed, aborted.
-
-
-#####################333
-# Rationale
-#
-# 1. each time the Play button is pressed, a new comp_run entry is inserted
-# 2. ideally, on play a snapshot of the workbench should be created, such that the scheduler can schedule
-# 3. each time a task is completed, it should return its results, retrieves them, and updates the databases instead of the sidecar
-# 4. each time completes, the scheduler starts the next one or aborts it in case of failure or cancellation
-#
 pytest_simcore_core_services_selection = ["postgres", "redis", "rabbit"]
 pytest_simcore_ops_services_selection = ["adminer", "redis-commander"]
 
@@ -104,14 +88,13 @@ def test_scheduler_knows_these_are_completed_states(state: RunningState):
             "mpi",
         ),
         (
-            # FIXME: What should happen here??
             Image(
                 name="simcore/services/dynamic/fake",
                 tag="1.2.3",
                 requires_gpu=True,
                 requires_mpi=True,
             ),
-            "gpu",
+            "gpu:mpi",
         ),
     ],
 )
@@ -191,20 +174,18 @@ async def test_scheduler_throws_error_for_missing_dependencies(
     await CeleryScheduler.create_from_db(correctly_configured_app)
 
 
-async def test_scheduler_initializes_with_correct_state(
+async def test_scheduler_initializes_without_error(
     fake_app: FakeApp,
 ):
-    scheduler = await CeleryScheduler.create_from_db(fake_app)
+    await CeleryScheduler.create_from_db(fake_app)
 
 
 async def test_scheduler_task_starts_and_stops_gracefully(fake_app: FakeApp):
-    task = asyncio.get_event_loop().create_task(scheduler_task(fake_app))
+    scheduler = await CeleryScheduler.create_from_db(fake_app)
+    task = asyncio.get_event_loop().create_task(scheduler_task(scheduler))
     # let it run
     await asyncio.sleep(3)
-    # now check the scheduler was registered correctly
-    assert fake_app.state.scheduler is not None
     # stop the scheduler gracefully
     task.cancel()
     with pytest.raises(asyncio.CancelledError):
         await task
-    assert fake_app.state.scheduler is None
