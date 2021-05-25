afc4b44f220ea341768bcccb30a14d2dc0165bcb
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-05-25 13:26:13 +0200

    linter

--- services/director-v2/src/simcore_service_director_v2/utils/dags.py
+++ services/director-v2/src/simcore_service_director_v2/utils/dags.py
@@ -203,8 +203,6 @@ def find_computational_node_cycles(dag: nx.DiGraph) -> List[List[str]]:
     computational_node_cycles = []
     list_potential_cycles = nx.simple_cycles(dag)
     for cycle in list_potential_cycles:
-        if any(
-            [_is_node_computational(dag.nodes[node_id]["key"]) for node_id in cycle]
-        ):
+        if any(_is_node_computational(dag.nodes[node_id]["key"]) for node_id in cycle):
             computational_node_cycles += [deepcopy(cycle)]
     return computational_node_cycles
