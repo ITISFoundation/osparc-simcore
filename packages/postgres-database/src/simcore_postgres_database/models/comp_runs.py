6a9c4b7c02fd7445cd9645187e8c455856168cd4
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-05-26 23:09:31 +0200

    @pcrespov review: add doc for all new columns in the table

--- packages/postgres-database/src/simcore_postgres_database/models/comp_runs.py
+++ packages/postgres-database/src/simcore_postgres_database/models/comp_runs.py
@@ -12,7 +12,12 @@ comp_runs = sa.Table(
     metadata,
     # this task db id
     sa.Column(
-        "run_id", sa.BigInteger, nullable=False, autoincrement=True, primary_key=True
+        "run_id",
+        sa.BigInteger,
+        nullable=False,
+        autoincrement=True,
+        primary_key=True,
+        doc="Primary key, identifies the run",
     ),
     sa.Column(
         "project_uuid",
@@ -24,6 +29,7 @@ comp_runs = sa.Table(
             ondelete="CASCADE",
         ),
         nullable=False,
+        doc="The project uuid with which the run entry is associated",
     ),
     sa.Column(
         "user_id",
@@ -35,30 +41,48 @@ comp_runs = sa.Table(
             ondelete="CASCADE",
         ),
         nullable=False,
+        doc="The user id with which the run entry is associated",
     ),
     sa.Column(
         "iteration",
         sa.BigInteger,
         nullable=False,
         autoincrement=False,
+        doc="A computational run is always associated to a user, a project and a specific iteration",
     ),
     sa.Column(
         "result",
         sa.Enum(StateType),
         nullable=False,
         server_default=StateType.NOT_STARTED.value,
+        doc="The result of the run entry",
     ),
     # dag node id and class
-    sa.Column("created", sa.DateTime(), nullable=False, server_default=func.now()),
+    sa.Column(
+        "created",
+        sa.DateTime(),
+        nullable=False,
+        server_default=func.now(),
+        doc="When the run entry was created",
+    ),
     sa.Column(
         "modified",
         sa.DateTime(),
         nullable=False,
         server_default=func.now(),
         onupdate=func.now(),  # this will auto-update on modification
+        doc="When the run entry was last modified",
     ),
     # utc timestamps for submission/start/end
-    sa.Column("start", sa.DateTime),
-    sa.Column("end", sa.DateTime),
+    sa.Column(
+        "start",
+        sa.DateTime,
+        doc="When the run was started",
+    ),
+    sa.Column(
+        "end",
+        sa.DateTime,
+        doc="When the run was finished",
+    ),
     sa.UniqueConstraint("project_uuid", "user_id", "iteration"),
 )
