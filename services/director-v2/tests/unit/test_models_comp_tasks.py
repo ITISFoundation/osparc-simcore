dbe33cad7fc1ac1d607896afa0da0c386eb0b24f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-05-26 22:45:22 +0200

    completed tests

--- services/director-v2/tests/unit/test_models_comp_tasks.py
+++ services/director-v2/tests/unit/test_models_comp_tasks.py
@@ -6,7 +6,9 @@ from pprint import pformat
 from typing import Any, Dict
 
 import pytest
+from models_library.projects_state import RunningState
 from pydantic.main import BaseModel
+from simcore_postgres_database.models.comp_pipeline import StateType
 from simcore_service_director_v2.models.domains.comp_tasks import CompTaskAtDB
 
 
@@ -27,13 +29,41 @@ def test_computation_task_model_examples(
     "model_cls",
     (CompTaskAtDB,),
 )
+def test_computation_task_model_export_to_db_model(
+    model_cls: BaseModel, model_cls_examples: Dict[str, Dict[str, Any]]
+):
+    for name, example in model_cls_examples.items():
+        print(name, ":", pformat(example))
+        model_instance = model_cls(**example)
+        assert model_instance, f"Failed with {name}"
+
+        db_model = model_instance.to_db_model()
+        assert isinstance(db_model, dict)
+        StateType(db_model["state"])
+
+
+@pytest.mark.parametrize(
+    "model_cls",
+    (CompTaskAtDB,),
+)
+def test_computation_task_model_with_running_state_value_field(
+    model_cls: BaseModel, model_cls_examples: Dict[str, Dict[str, Any]]
+):
+    for name, example in model_cls_examples.items():
+        print(name, ":", pformat(example))
+        model_instance = model_cls(**example)
+        assert model_instance, f"Failed with {name}"
+
+@pytest.mark.parametrize(
+    "model_cls",
+    (CompTaskAtDB,),
+)
 def test_computation_task_model_with_wrong_default_value_field(
     model_cls: BaseModel, model_cls_examples: Dict[str, Dict[str, Any]]
 ):
     for name, example in model_cls_examples.items():
+        example["state"] = RunningState.RETRY.value
 
-        for output_schema in example.get("schema", {}).get("outputs", {}).values():
-            output_schema["defaultValue"] = None
         print(name, ":", pformat(example))
         model_instance = model_cls(**example)
         assert model_instance, f"Failed with {name}"
