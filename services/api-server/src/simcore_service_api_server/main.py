d39a143d98405fee23ea95034bf8b0f2acb97c53
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-05-28 18:17:26 +0200

    fixed api-server as well

---
+++ services/api-server/src/simcore_service_api_server/main.py
@@ -0,0 +1,8 @@
+"""Main application to be deployed in for example uvcorn.
+"""
+from fastapi import FastAPI
+from simcore_service_api_server.core.application import init_app
+
+
+# SINGLETON FastAPI app
+the_app: FastAPI = init_app()
