f8716fe84cd9ff39af58ce7d49b70268d1c128e6
odeimaiz <maiz@itis.swiss>
2021-06-02 15:18:25 +0200

    click on document icon

--- tests/e2e/portal/VTK_file.js
+++ tests/e2e/portal/VTK_file.js
@@ -31,11 +31,28 @@ async function runTutorial () {
     console.log("Study ID:", studyId);
 
     const workbenchData = utils.extractWorkbenchData(studyData["data"]);
-    await tutorial.waitForServices(workbenchData["studyId"], [workbenchData["nodeIds"][1]]);
+    const nodeIdViewer = workbenchData["nodeIds"][1];
+    await tutorial.waitForServices(workbenchData["studyId"], [nodeIdViewer]);
 
     // Some time for starting the service
     await tutorial.waitFor(10000);
     await utils.takeScreenshot(page, screenshotPrefix + 'service_started');
+
+    const iframeHandles = await page.$$("iframe");
+    const iframes = [];
+    for (let i=0; i<iframeHandles.length; i++) {
+      const frame = await iframeHandles[i].contentFrame();
+      iframes.push(frame);
+    }
+    // url/x/nodeIdViewer
+    const frame = iframes.find(iframe => iframe._url.includes(nodeIdViewer));
+
+    // inside the iFrame, click on doscument icon on top
+    const docSelector = 'body > div > div > div.MainView-topBar_4t9p4 > div.MainView-title_2sz83 > div.MainView-menu_37qbe > div.ToggleIcons-container_4prdd > div:nth-child(1) > i.ToggleIcons-openFileButtonActive_3q2p5.ToggleIcons-openFileButton_3bz5y.ToggleIcons-button_2phh8.font-awesome-fa_4nxvz.font-awesome-fa-fw_vg4ef.font-awesome-fa-file-text-o_3dsxg.ToggleIcons-activeButton_3cj9s';
+    await frame.waitForSelector(docSelector);
+    await frame.click(docSelector);
+    await tutorial.waitFor(2000);
+    await utils.takeScreenshot(page, screenshotPrefix + 'iFrame1');
   }
   catch(err) {
     tutorial.setTutorialFailed(true);
