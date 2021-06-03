76d1b970b9d5057d4473c5bdba3bb495c9a62148
odeimaiz <maiz@itis.swiss>
2021-06-03 13:33:24 +0200

    minor

--- tests/e2e/portal-files/VTK_file.js
+++ tests/e2e/portal-files/VTK_file.js
@@ -1,4 +1,4 @@
-// node VTK_file.js [url] [file_type] [viewer_key] [viewer_vs] [download_link] [file_name] [file_size] [--demo]
+// node VTK_file.js [url] [download_link] [file_size] [--demo]
 
 const fetch = require('node-fetch');
 const tutorialBase = require('../tutorials/tutorialBase');
