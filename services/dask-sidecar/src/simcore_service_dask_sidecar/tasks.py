d68bc319442e2878ca7ef1e07e1c124d481a67aa
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-06-10 16:54:04 +0200

    started unit testing

--- services/dask-sidecar/src/simcore_service_dask_sidecar/tasks.py
+++ services/dask-sidecar/src/simcore_service_dask_sidecar/tasks.py
@@ -4,9 +4,9 @@ from simcore_service_sidecar.cli import run_sidecar
 from simcore_service_sidecar.utils import wrap_async_call
 
 
-def run_task_as_sidecar(job_id: int, user_id: int, project_id: UUID, node_id: UUID):
+def run_task_in_service(job_id: int, user_id: int, project_id: UUID, node_id: UUID):
     """
-    Runs as a sidecar `project.node_id` under `user_id` session
+    To run a task, it spawns a service corresponding to `project.node_id` under `user_id` session and
     """
     wrap_async_call(
         run_sidecar(str(job_id), str(user_id), str(project_id), node_id=str(node_id))
