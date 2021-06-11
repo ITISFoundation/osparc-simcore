8a09a62bdba092f61b765a07456471d8a104b37a
Andrei Neagu <neagu@itis.swiss>
2021-06-11 15:12:27 +0200

    refactored most of the test

--- services/director-v2/tests/integration/02/test_dynamic_services_routes.py
+++ services/director-v2/tests/integration/02/test_dynamic_services_routes.py
@@ -15,20 +15,32 @@ from async_timeout import timeout
 from fastapi import FastAPI
 from pydantic import PositiveInt
 from simcore_service_director_v2.core.application import init_app
-from simcore_service_director_v2.core.settings import AppSettings, BootModeEnum
+from simcore_service_director_v2.core.settings import (
+    AppSettings,
+    BootModeEnum,
+    DynamicSidecarSettings,
+)
 from simcore_service_director_v2.models.schemas.constants import (
     DYNAMIC_SIDECAR_SERVICE_PREFIX,
-)
-from simcore_service_director_v2.modules.dynamic_sidecar.config import (
-    DynamicSidecarSettings,
 )
 from simcore_service_director_v2.modules.dynamic_sidecar.monitor.core import (
     DynamicSidecarsMonitor,
 )
 
+SERVICE_WAS_CREATED_BY_DIRECTOR_V2 = 20
 SERVICE_IS_READY_TIMEOUT = 2 * 60
 
 logger = logging.getLogger(__name__)
+
+pytest_simcore_core_services_selection = ["director"]
+
+
+@pytest.fixture(autouse=True)
+def minimal_configuration(
+    dy_static_file_server_dynamic_sidecar_service: Dict,
+    simcore_services: None,
+):
+    pass
 
 
 @pytest.fixture
@@ -37,7 +49,9 @@ def node_uuid() -> str:
 
 
 @pytest.fixture
-async def ensure_swarm_and_networks(docker_swarm: None) -> None:
+async def ensure_swarm_and_networks(
+    simcore_services_network_name: str, docker_swarm: None
+) -> None:
     """
     Make sure to always have a docker swarm network.
     If one is not present crete one. There can not be more then one.
@@ -46,10 +60,7 @@ async def ensure_swarm_and_networks(docker_swarm: None) -> None:
     async with aiodocker.Docker() as docker_client:
         all_networks = await docker_client.networks.list()
 
-        simcore_services_network_name = os.environ.get(
-            "SIMCORE_SERVICES_NETWORK_NAME", "_default"
-        )
-
+        simcore_services_network_name = os.environ["SIMCORE_SERVICES_NETWORK_NAME"]
         networks = [
             x
             for x in all_networks
@@ -60,7 +71,7 @@ async def ensure_swarm_and_networks(docker_swarm: None) -> None:
 
         if create_and_remove_network:
             network_config = {
-                "Name": "test_swarm_mock_network_default",
+                "Name": simcore_services_network_name,
                 "Driver": "overlay",
                 "Attachable": True,
                 "Internal": False,
@@ -79,16 +90,17 @@ async def ensure_swarm_and_networks(docker_swarm: None) -> None:
 def start_request_data(
     node_uuid: str,
     user_id: PositiveInt,
-    httpbin_dynamic_sidecar_service: Dict,
+    dy_static_file_server_dynamic_sidecar_service: Dict,
     ensure_swarm_and_networks: None,
 ) -> Dict[str, Any]:
     return dict(
         user_id=user_id,
-        project_id=node_uuid,
-        service_key=httpbin_dynamic_sidecar_service["image"]["name"],
-        service_tag=httpbin_dynamic_sidecar_service["image"]["tag"],
+        project_id=str(uuid4()),
+        service_uuid=node_uuid,
+        service_key=dy_static_file_server_dynamic_sidecar_service["image"]["name"],
+        service_version=dy_static_file_server_dynamic_sidecar_service["image"]["tag"],
         request_scheme="http",
-        request_dns="http://localhost:50000",
+        request_dns="localhost:50000",
         settings=[
             {
                 "name": "resources",
@@ -110,17 +122,18 @@ def start_request_data(
 async def test_client(
     loop: asyncio.BaseEventLoop, dynamic_sidecar_image: None, monkeypatch
 ) -> TestClient:
-    monkeypatch.setenv("DIRECTOR_HOST", "not-existing-host")
 
     settings = AppSettings.create_from_env(boot_mode=BootModeEnum.PRODUCTION)
     settings.postgres.enabled = False
+    settings.scheduler.enabled = False
+    settings.dynamic_services.dynamic_sidecar.expose_port = True
     app = init_app(settings)
 
     async with TestClient(app) as client:
         dynamic_sidecar_settings: DynamicSidecarSettings = (
-            client.application.state.dynamic_sidecar_settings
+            client.application.state.settings.dynamic_services.dynamic_sidecar
         )
-        dynamic_sidecar_settings.dev_expose_dynamic_sidecar = True
+        dynamic_sidecar_settings.mount_path_dev = None
         yield client
 
 
@@ -131,7 +144,7 @@ async def ensure_services_stopped(start_request_data: Dict[str, Any]) -> None:
     async with aiodocker.Docker() as docker_client:
         service_names = {x["Spec"]["Name"] for x in await docker_client.services.list()}
 
-        project_id = start_request_data["project_id"]
+        project_id = start_request_data["service_uuid"]
         for service_name in service_names:
             # if node_uuid is present in the service name it needs to be removed
             if project_id in service_name:
@@ -147,56 +160,74 @@ async def ensure_services_stopped(start_request_data: Dict[str, Any]) -> None:
                 assert delete_result is True
 
 
-async def _patch_dynamic_service_url(app: FastAPI, node_uuid: str) -> None:
+async def _patch_dynamic_service_url(app: FastAPI, node_uuid: str) -> str:
     """
     Normally director-v2 talks via docker-netwoks with the dynamic-sidecar.
     Since the director-v2 was started outside docker and is not
     running in a container, the service port needs to be exposed and the
     url needs to be changed to localhost
-    """
-    port = None
-    async with aiodocker.Docker() as docker_client:
-        for service in await docker_client.services.list():
-            service_name = service["Spec"]["Name"]
-            if (
-                node_uuid in service_name
-                and f"{DYNAMIC_SIDECAR_SERVICE_PREFIX}-" in service_name
-            ):
-                ports = service["Endpoint"]["Spec"]["Ports"]
-                assert len(ports) == 1
-                port = ports[0]["PublishedPort"]
-                break
 
-    assert port is not None
+    returns: the local endpoint
+    """
+    service_name = f"{DYNAMIC_SIDECAR_SERVICE_PREFIX}_{node_uuid}"
+    port = None
+
+    async with aiodocker.Docker() as docker_client:
+        async with timeout(SERVICE_WAS_CREATED_BY_DIRECTOR_V2):
+            # it takes a bit of time for the port to be auto generated
+            # keep trying until it is there
+            while port is None:
+                services = await docker_client.services.list()
+                for service in services:
+                    if service["Spec"]["Name"] == service_name:
+                        ports = service["Endpoint"].get("Ports", [])
+                        if len(ports) == 1:
+                            port = ports[0]["PublishedPort"]
+                            break
+
+                await asyncio.sleep(1)
 
     # patch the endppoint inside the monitor
     monitor: DynamicSidecarsMonitor = app.state.dynamic_sidecar_monitor
-    for entry in monitor._to_monitor.values():  # pylint: disable=protected-access
-        if entry.monitor_data.service_name == service_name:
-            entry.monitor_data.dynamic_sidecar.hostname = "172.17.0.1"
-            entry.monitor_data.dynamic_sidecar.port = port
+    async with monitor._lock:  # pylint: disable=protected-access
+        for entry in monitor._to_monitor.values():  # pylint: disable=protected-access
+            if entry.monitor_data.service_name == service_name:
+                entry.monitor_data.dynamic_sidecar.hostname = "172.17.0.1"
+                entry.monitor_data.dynamic_sidecar.port = port
+
+                endpoint = entry.monitor_data.dynamic_sidecar.endpoint
+                assert endpoint == f"http://172.17.0.1:{port}"
+
+                return endpoint
 
 
 async def test_start(
     test_client: TestClient, node_uuid: str, start_request_data: Dict[str, Any]
 ):
     # starting the service
+    headers = {
+        "x-dynamic-sidecar-request-dns": start_request_data["request_dns"],
+        "x-dynamic-sidecar-request-scheme": start_request_data["request_scheme"],
+    }
     response: Response = await test_client.post(
-        f"/v2/dynamic_services/{node_uuid}:start", json=start_request_data
+        "/v2/dynamic_services", json=start_request_data, headers=headers
     )
-    assert response.status_code == 200, response.text
+    assert response.status_code == 201, response.text
 
-    await _patch_dynamic_service_url(app=test_client.application, node_uuid=node_uuid)
+    local_address = await _patch_dynamic_service_url(
+        app=test_client.application, node_uuid=node_uuid
+    )
+    assert response.json()["service_host"] in local_address
 
     # awaiting for service to be running
     async with timeout(SERVICE_IS_READY_TIMEOUT):
         status_is_not_running = True
         while status_is_not_running:
 
-            response: Response = await test_client.post(
-                f"/v2/dynamic_services/{node_uuid}:status", json=start_request_data
+            response: Response = await test_client.get(
+                f"/v2/dynamic_services/{node_uuid}", json=start_request_data
             )
-            logger.warning("sidecar :status result %s", response.text)
+            logger.warning("sidecar status result %s", response.text)
             assert response.status_code == 200, response.text
             data = response.json()
 
