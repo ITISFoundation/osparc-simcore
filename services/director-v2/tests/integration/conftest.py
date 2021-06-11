6e5032b2e5803fb7b798da4386394174a2412926
Andrei Neagu <neagu@itis.swiss>
2021-06-11 15:11:37 +0200

    added mocked values

--- services/director-v2/tests/integration/conftest.py
+++ services/director-v2/tests/integration/conftest.py
@@ -75,3 +75,20 @@ def project(postgres_db: sa.engine.Engine, user_db: Dict) -> Callable:
     with postgres_db.connect() as con:
         for pid in created_project_ids:
             con.execute(projects.delete().where(projects.c.uuid == str(pid)))
+
+
+@pytest.fixture
+def simcore_services_network_name() -> str:
+    return "test_mocked_network_name"
+
+
+@pytest.fixture(autouse=True)
+def mock_dynamic_sidecar_settings(
+    simcore_services_network_name: str, monkeypatch
+) -> None:
+    monkeypatch.setenv(
+        "DYNAMIC_SIDECAR_IMAGE", "local/dynamic-sidecar:TEST_MOCKED_TAG_NOT_PRESENT"
+    )
+    monkeypatch.setenv("SIMCORE_SERVICES_NETWORK_NAME", simcore_services_network_name)
+    monkeypatch.setenv("TRAEFIK_SIMCORE_ZONE", "test_mocked_simcore_zone")
+    monkeypatch.setenv("SWARM_STACK_NAME", "test_mocked_stack_name")
