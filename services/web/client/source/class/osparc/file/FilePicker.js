2be2f2049541e910721db42b5586589579f60d61
odeimaiz <maiz@itis.swiss>
2021-06-14 14:40:53 +0200

    Back to prev node after selecting file

--- services/web/client/source/class/osparc/file/FilePicker.js
+++ services/web/client/source/class/osparc/file/FilePicker.js
@@ -55,6 +55,10 @@ qx.Class.define("osparc.file.FilePicker", {
     }
   },
 
+  events: {
+    "itemSelected": "qx.event.type.Event"
+  },
+
   statics: {
     getOutput: function(outputs) {
       if ("outFile" in outputs && "value" in outputs["outFile"]) {
@@ -313,6 +317,7 @@ qx.Class.define("osparc.file.FilePicker", {
       const selectedItem = this.__selectedFileLayout.getItemSelected();
       if (selectedItem && osparc.file.FilesTree.isFile(selectedItem)) {
         this.__setOutputValueFromStore(selectedItem.getLocation(), selectedItem.getDatasetId(), selectedItem.getFileId(), selectedItem.getLabel());
+        this.fireEvent("itemSelected");
       }
     },
 
