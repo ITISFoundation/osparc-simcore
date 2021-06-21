f60639521449c31b1fdd6bb3af66a359434aa133
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-06-21 21:26:37 +0200

    prepare fixtures for testing the app

--- services/datcore-adapter/tests/unit/conftest.py
+++ services/datcore-adapter/tests/unit/conftest.py
@@ -3,9 +3,14 @@
 # pylint:disable=redefined-outer-name
 
 from pathlib import Path
+from typing import Iterator
 
+import httpx
 import pytest
 import simcore_service_datcore_adapter
+from asgi_lifespan import LifespanManager
+from fastapi.applications import FastAPI
+from starlette.testclient import TestClient
 
 pytest_plugins = ["pytest_simcore.repository_paths"]
 
@@ -24,3 +29,33 @@ def installed_package_dir() -> Path:
     dirpath = Path(simcore_service_datcore_adapter.__file__).resolve().parent
     assert dirpath.exists()
     return dirpath
+
+
+@pytest.fixture()
+def minimal_app() -> FastAPI:
+    from simcore_service_datcore_adapter.main import the_app
+
+    return the_app
+
+
+@pytest.fixture()
+def client(minimal_app: FastAPI) -> TestClient:
+    with TestClient(minimal_app) as cli:
+        return cli
+
+
+@pytest.fixture()
+async def initialized_app(minimal_app: FastAPI) -> Iterator[FastAPI]:
+    async with LifespanManager(minimal_app):
+        yield minimal_app
+
+
+@pytest.fixture(scope="function")
+async def async_client(initialized_app: FastAPI) -> httpx.AsyncClient:
+
+    async with httpx.AsyncClient(
+        app=initialized_app,
+        base_url="http://datcore-adapter.testserver.io",
+        headers={"Content-Type": "application/json"},
+    ) as client:
+        yield client
--- services/datcore-adapter/tests/unit/test_route_health.py
+++ services/datcore-adapter/tests/unit/test_route_health.py
@@ -5,23 +5,10 @@
 
 from datetime import datetime
 
+import httpx
 import pytest
-from fastapi.applications import FastAPI
 from starlette import status
 from starlette.testclient import TestClient
-
-
-@pytest.fixture()
-def minimal_app() -> FastAPI:
-    from simcore_service_datcore_adapter.main import the_app
-
-    return the_app
-
-
-@pytest.fixture()
-def client(minimal_app: FastAPI) -> TestClient:
-    with TestClient(minimal_app) as cli:
-        return cli
 
 
 def test_live_entrypoint(client: TestClient):
@@ -35,5 +22,6 @@ def test_live_entrypoint(client: TestClient):
     )
 
 
-def test_check_subsystem_health(client: TestClient):
-    response = client.get("/ready")
+@pytest.mark.asyncio
+async def test_check_subsystem_health(async_client: httpx.AsyncClient):
+    response = await async_client.get("/ready")
