e60e245ac2c5250209d0c975fdfd46369833b50c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-06-21 22:28:13 +0200

    creating some tests

--- services/datcore-adapter/tests/unit/test_route_files.py
+++ services/datcore-adapter/tests/unit/test_route_files.py
@@ -4,6 +4,12 @@ from starlette import status
 
 
 @pytest.mark.asyncio
+async def test_list_datasets_entrypoint(async_client: httpx.AsyncClient):
+    response = await async_client.get("/datasets")
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.asyncio
 async def test_list_files_entrypoint(async_client: httpx.AsyncClient):
     response = await async_client.get("/files")
     assert response.status_code == status.HTTP_200_OK
