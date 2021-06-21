b26b3f75a18854b3f0f742958c977a406f0e8ec9
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-06-21 15:25:46 +0200

    updates paths to global configs

--- scripts/codestyle.bash
+++ scripts/codestyle.bash
@@ -7,7 +7,14 @@ IFS=$'\n\t'
 
 SRC_DIRECTORY_NAME=${2}
 BASE_PATH_DIR=${3-MISSING_DIR}
+
+
 CACHE_DIR=$(dirname "$(mktemp -u)")
+
+# global config for entire repo
+PYLINT_CONFIG="$(git rev-parse --show-toplevel)/.pylintrc"
+MYPY_CONFIG="$(git rev-parse --show-toplevel)/mypy.ini"
+
 
 # used for development (fails on pylint and mypy)
 development() {
@@ -17,9 +24,11 @@ development() {
   echo "black"
   black src/"$SRC_DIRECTORY_NAME" tests/
   echo "pylint"
-  pylint --rcfile=../../.pylintrc src/"$SRC_DIRECTORY_NAME" tests/
+  pylint --rcfile="$PYLINT_CONFIG" src/"$SRC_DIRECTORY_NAME" tests/
   echo "mypy"
-  mypy --ignore-missing-imports --config-file ../../mypy.ini src/"$SRC_DIRECTORY_NAME" tests/
+  mkdir --parents "$CACHE_DIR"
+  mypy --install-types
+  mypy --ignore-missing-imports --config-file "$MYPY_CONFIG" src/"$SRC_DIRECTORY_NAME" tests/
 }
 
 # invoked by ci as test (also fails on isort and black)
@@ -30,13 +39,13 @@ ci() {
   echo "black"
   black --check "$BASE_PATH_DIR"/src/"$SRC_DIRECTORY_NAME" "$BASE_PATH_DIR"/tests
   echo "pylint"
-  pylint --rcfile=.pylintrc "$BASE_PATH_DIR"/src/"$SRC_DIRECTORY_NAME" "$BASE_PATH_DIR"/tests
+  pylint --rcfile="$PYLINT_CONFIG" "$BASE_PATH_DIR"/src/"$SRC_DIRECTORY_NAME" "$BASE_PATH_DIR"/tests
   echo "mypy"
   # installing all missing stub packages (e.g. types-PyYAML, types-aiofiles, etc)
   mkdir --parents "$CACHE_DIR"
-  mypy --cache-dir="$CACHE_DIR" --install-types
+  mypy --config-file "$MYPY_CONFIG" --cache-dir="$CACHE_DIR" --install-types
   # runs mypy
-  mypy --config-file mypy.ini --ignore-missing-imports "$BASE_PATH_DIR"/src/"$SRC_DIRECTORY_NAME" "$BASE_PATH_DIR"/tests
+  mypy --config-file "$MYPY_CONFIG" --ignore-missing-imports "$BASE_PATH_DIR"/src/"$SRC_DIRECTORY_NAME" "$BASE_PATH_DIR"/tests
 }
 
 # Allows to call a function based on arguments passed to the script
