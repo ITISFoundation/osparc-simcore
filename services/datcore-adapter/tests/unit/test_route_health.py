7870b3d3ace583d3f89107a55e0c0352dee1567f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-06-21 22:13:54 +0200

    checking health

--- services/datcore-adapter/tests/unit/test_route_health.py
+++ services/datcore-adapter/tests/unit/test_route_health.py
@@ -7,6 +7,8 @@ from datetime import datetime
 
 import httpx
 import pytest
+import respx
+from models_library.app_diagnostics import AppStatusCheck
 from starlette import status
 
 
@@ -24,4 +26,27 @@ async def test_live_entrypoint(async_client: httpx.AsyncClient):
 
 @pytest.mark.asyncio
 async def test_check_subsystem_health(async_client: httpx.AsyncClient):
-    response = await async_client.get("/ready")
+    async with respx.mock:
+        pennsieve_health_route = respx.get("https://api.pennsieve.io/health/").respond(
+            status.HTTP_200_OK
+        )
+        response = await async_client.get("/ready")
+
+        assert pennsieve_health_route.called
+        assert response.status_code == status.HTTP_200_OK
+        app_status = AppStatusCheck.parse_obj(response.json())
+        assert app_status
+        assert app_status.app_name == "simcore-service-datcore-adapter"
+        assert app_status.services == {"pennsieve": True}
+
+    async with respx.mock:
+        pennsieve_health_route = respx.get("https://api.pennsieve.io/health/")
+        pennsieve_health_route.side_effect = [httpx.ConnectError]
+        response = await async_client.get("/ready")
+
+        assert pennsieve_health_route.called
+        assert response.status_code == status.HTTP_200_OK
+        app_status = AppStatusCheck.parse_obj(response.json())
+        assert app_status
+        assert app_status.app_name == "simcore-service-datcore-adapter"
+        assert app_status.services == {"pennsieve": False}
