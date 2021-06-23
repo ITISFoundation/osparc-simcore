247a82ac01806c4d3ded7dfa56c28e5e755f56b0
Andrei Neagu <neagu@itis.swiss>
2021-06-23 13:50:31 +0200

    updated non_blocking_process_pool_executor in catalog

--- services/catalog/src/simcore_service_catalog/utils/pools.py
+++ services/catalog/src/simcore_service_catalog/utils/pools.py
@@ -1,5 +1,19 @@
-from contextlib import contextmanager
 from concurrent.futures import ProcessPoolExecutor
+from contextlib import contextmanager
+
+# only gets created on use and is guaranteed to be the s
+# ame for the entire lifetime of the application
+__shared_process_pool_executor = None
+
+
+def get_shared_process_pool_executor(**kwargs) -> ProcessPoolExecutor:
+    global __shared_process_pool_executor  # pylint: disable=global-statement
+
+    if __shared_process_pool_executor is None:
+        # pylint: disable=consider-using-with
+        __shared_process_pool_executor = ProcessPoolExecutor(**kwargs)
+
+    return __shared_process_pool_executor
 
 
 # because there is no shared fastapi library, this is a
@@ -10,8 +24,13 @@ def non_blocking_process_pool_executor(**kwargs) -> ProcessPoolExecutor:
     Avoids default context manger behavior which calls
     shutdown with wait=True an blocks.
     """
-    pool = ProcessPoolExecutor(**kwargs)  # pylint: disable=consider-using-with
+    executor = get_shared_process_pool_executor(**kwargs)
     try:
-        yield pool
+        yield executor
     finally:
-        pool.shutdown(wait=False)
+        # due to an issue in cpython https://bugs.python.org/issue34073
+        # bypassing shutdown and using a shared pool
+        # remove call to get_shared_process_pool_executor and replace with
+        # a new instance when the issue is fixed
+        # executor.shutdown(wait=False)
+        pass
