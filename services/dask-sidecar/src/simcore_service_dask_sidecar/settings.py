0d1ef913359777c0d42305f7b5f9e5cf8f7878bc
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-06-23 21:30:17 +0200

    adding meta

--- services/dask-sidecar/src/simcore_service_dask_sidecar/settings.py
+++ services/dask-sidecar/src/simcore_service_dask_sidecar/settings.py
@@ -43,10 +43,16 @@ class Settings(BaseCustomSettings):
 
     # dask config ----
 
-    DASK_SCHEDULER_ADDRESS: str = Field(
-        "tcp://scheduler:8786",
-        description="Address of the scheduler that manages this worker",
+    DASK_SCHEDULER_ADDRESS: Optional[str] = Field(
+        None,
+        description="Address of the scheduler to register (only if started as worker )",
     )
+
+    def as_scheduler(self) -> bool:
+        return self.DASK_SCHEDULER_ADDRESS is None
+
+    def as_worker(self) -> bool:
+        return not self.is_scheduler()
 
     @classmethod
     def create_from_envs(cls) -> "Settings":
