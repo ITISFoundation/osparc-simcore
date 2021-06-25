d879dbeead6c414745bc3a69e6835bee13ca9b07
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-06-25 16:59:48 +0200

    get a modern paged data endpoint

--- services/datcore-adapter/src/simcore_service_datcore_adapter/api/routes/datasets.py
+++ services/datcore-adapter/src/simcore_service_datcore_adapter/api/routes/datasets.py
@@ -32,23 +32,6 @@ async def list_datasets(
     return paginate(datasets, params)
 
 
-# @router.get(
-#     "/datasets/{dataset_id}",
-#     summary="get dataset",
-#     status_code=status.HTTP_200_OK,
-#     response_model=DatasetsOut,
-# )
-# async def list_datasets(
-#     x_datcore_api_key: str = Header(..., description="Datcore API Key"),
-#     x_datcore_api_secret: str = Header(..., description="Datcore API Secret"),
-#     pennsieve_client: PennsieveApiClient = Depends(get_pennsieve_api_client),
-# ) -> Page[DatasetsOut]:
-#     datasets: List[DatasetMetaData] = await pennsieve_client.get_datasets(
-#         api_key=x_datcore_api_key, api_secret=x_datcore_api_secret
-#     )
-#     return datasets
-
-
 @router.get(
     "/datasets/{dataset_id}/files",
     summary="list all file meta data in dataset",
--- services/datcore-adapter/tests/unit/test_route_datasets.py
+++ services/datcore-adapter/tests/unit/test_route_datasets.py
@@ -74,3 +74,28 @@ async def test_list_dataset_files_legacy_entrypoint(
     data = response.json()
     assert data
     parse_obj_as(List[FileMetaData], data)
+
+
+@pytest.mark.asyncio
+async def test_list_dataset_files_entrypoint(
+    async_client: httpx.AsyncClient,
+    pennsieve_dataset_id: str,
+    pennsieve_client_mock: Optional[Any],
+    pennsieve_mock_dataset_packages: Dict[str, Any],
+    pennsieve_api_headers: Dict[str, str],
+):
+    dataset_id = pennsieve_dataset_id
+    if pennsieve_client_mock:
+        # pylint: disable=protected-access
+        pennsieve_client_mock.return_value.get_dataset.return_value._api.datasets._get.return_value = (
+            pennsieve_mock_dataset_packages
+        )
+
+    response = await async_client.get(
+        f"v0/datasets/{dataset_id}/files",
+        headers=pennsieve_api_headers,
+    )
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert data
+    parse_obj_as(Page[FileMetaData], data)
