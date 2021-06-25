a340f7cbec3d7275a399e3470549d5ce26c9a469
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-06-25 14:51:26 +0200

    mocked access to file package

--- services/datcore-adapter/tests/unit/test_route_files.py
+++ services/datcore-adapter/tests/unit/test_route_files.py
@@ -2,7 +2,8 @@
 # pylint:disable=unused-argument
 # pylint:disable=redefined-outer-name
 
-from typing import Dict
+from typing import Dict, Any
+import faker
 
 import httpx
 import pytest
@@ -11,11 +12,34 @@ from simcore_service_datcore_adapter.models.domains.files import FileDownloadOut
 from starlette import status
 
 
+@pytest.fixture()
+def pennsieve_data_package_mock(mocker) -> Any:
+    data_package_mock = mocker.patch(
+        "simcore_service_datcore_adapter.modules.pennsieve.pennsieve.models.DataPackage"
+    )
+    return data_package_mock
+
+
+@pytest.fixture()
+def pennsieve_file_package_mock(mocker) -> Any:
+    file_mock = mocker.patch(
+        "simcore_service_datcore_adapter.modules.pennsieve.pennsieve.models.File"
+    )
+    return file_mock
+
+
 @pytest.mark.asyncio
 async def test_download_file_entrypoint(
     async_client: httpx.AsyncClient,
+    pennsieve_client_mock: Any,
+    pennsieve_data_package_mock: Any,
+    pennsieve_file_package_mock: Any,
     pennsieve_api_headers: Dict[str, str],
 ):
+    pennsieve_client_mock.return_value.get.return_value = pennsieve_data_package_mock
+    pennsieve_data_package_mock.files = [pennsieve_file_package_mock]
+    pennsieve_file_package_mock.url = faker.Faker().url()
+
     file_id = "N:package:09c142c4-d013-4431-b266-aa1c563105b0"
     response = await async_client.get(
         f"v0/files/{file_id}",
