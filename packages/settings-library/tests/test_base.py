5da3499bbc5ff64220cb467dfb19c8b13f43af5b
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-06-27 20:45:07 +0200

    Adds general policy check

--- packages/settings-library/tests/test_base.py
+++ packages/settings-library/tests/test_base.py
@@ -3,11 +3,16 @@
 # pylint: disable=unused-variable
 
 
+import importlib
+import inspect
 import os
+from pathlib import Path
 from typing import Dict
 
 import pytest
+import settings_library
 from pydantic import Field, ValidationError
+from pydantic.env_settings import BaseSettings
 from settings_library.base import BaseCustomSettings
 from settings_library.basic_types import PortInt
 from settings_library.postgres import PostgresSettings
@@ -55,3 +60,31 @@ def test_settigs_with_modules_settings(env_file: str, mock_environment: Dict):
         assert settings.APP_PORT == int(mock_environment["APP_PORT"])
 
         assert settings.APP_POSTGRES
+
+
+def _get_all_settings_classes():
+
+    modules = [
+        importlib.import_module(f"settings_library.{p.name[:-3]}")
+        for p in Path(settings_library.__file__).parent.glob("*.py")
+        if not p.name.endswith("__init__.py")
+    ]
+    assert modules
+
+    classes = [
+        cls
+        for mod in modules
+        for _, cls in inspect.getmembers(mod, inspect.isclass)
+        if issubclass(cls, BaseSettings) and cls != BaseSettings
+    ]
+    assert classes
+
+    return classes
+
+
+@pytest.mark.parametrize(
+    "settings_cls", _get_all_settings_classes(), ids=lambda cls: cls.__name__
+)
+def test_settings_class_policies(settings_cls):
+    assert issubclass(settings_cls, BaseCustomSettings)
+    assert all(key == key.upper() for key in settings_cls.__fields__.keys())
