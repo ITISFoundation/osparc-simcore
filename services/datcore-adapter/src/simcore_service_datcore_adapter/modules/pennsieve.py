161f0fa1b037cc58ca5224b7650bdc38e94af568
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-06-28 19:04:59 +0200

    use the REST API it's way faster

--- services/datcore-adapter/src/simcore_service_datcore_adapter/modules/pennsieve.py
+++ services/datcore-adapter/src/simcore_service_datcore_adapter/modules/pennsieve.py
@@ -23,27 +23,65 @@ def create_pennsieve_client(api_key: str, api_secret: str) -> Pennsieve:
     return Pennsieve(api_token=api_key, api_secret=api_secret)
 
 
+def _compute_file_path(all_packages: List[Dict[str, Any]], pck: Dict[str, Any]) -> Path:
+    file_path = Path(pck["content"]["name"])
+    if "extension" in pck:
+        file_path = ".".join([f"{file_path}", pck["extension"]])
+    if parent_id := pck["content"].get("parentId"):
+        file_path = (
+            _compute_file_path(all_packages, all_packages[parent_id]) / file_path
+        )
+    return Path(file_path)
 
 
 class PennsieveApiClient(BaseServiceClientApi):
+    """The client uses a combination of the pennsieve official API client to authenticate,
+    then uses the internal httpx-based client to call the REST API asynchronously"""
+
+    # NOTE: this trick allows to bypass all the python parsing/slowing down of the interface
+    # info given by Joost from Datcore
     async def _get_client(self, api_key: str, api_secret: str) -> Pennsieve:
         return await asyncio.get_event_loop().run_in_executor(
             None,
             lambda: create_pennsieve_client(api_key=api_key, api_secret=api_secret),
         )
 
+    async def _get_authorization_bearer_header(
+        self, api_key: str, api_secret: str
+    ) -> Dict[str, str]:
+        ps: Pennsieve = await self._get_client(api_key=api_key, api_secret=api_secret)
+        bearer_code = ps._api.token  # pylint: disable=protected-access
+        return {"Authorization": f"Bearer {bearer_code}"}
+
     async def get_user_profile(self, api_key: str, api_secret: str) -> Profile:
         ps: Pennsieve = await self._get_client(api_key=api_key, api_secret=api_secret)
         return Profile(id=ps.profile.id)
 
     async def get_datasets(
-        self, api_key: str, api_secret: str
-    ) -> List[DatasetMetaData]:
-        ps = Pennsieve(api_token=api_key, api_secret=api_secret)
-        pennsieve_datasets: pennsieve.models.Dataset = ps.datasets()
-        return [
-            DatasetMetaData(id=d.id, display_name=d.name) for d in pennsieve_datasets
-        ]
+        self, api_key: str, api_secret: str, limit: int, offset: int
+    ) -> Tuple[Sequence, int]:
+        response = await self.client.get(
+            "/datasets/paginated",
+            headers=await self._get_authorization_bearer_header(api_key, api_secret),
+            params={
+                "includeBannerUrl": False,
+                "includePublishedDataset": False,
+                "limit": limit,
+                "offset": offset,
+            },
+        )
+        dataset_page = response.json()
+
+        return (
+            [
+                DatasetMetaData(
+                    id=d["content"]["id"],
+                    display_name=d["content"]["name"],
+                )
+                for d in dataset_page["datasets"]
+            ],
+            dataset_page["totalCount"],
+        )
 
     async def get_dataset_files(
         self,
@@ -54,7 +92,7 @@ class PennsieveApiClient(BaseServiceClientApi):
         offset: int,
         collection_id: Optional[str] = None,
     ) -> Tuple[Sequence, int]:
-        ps = Pennsieve(api_token=api_key, api_secret=api_secret)
+        ps: Pennsieve = await self._get_client(api_key=api_key, api_secret=api_secret)
         ds: pennsieve.models.BaseCollection = ps.get_dataset(dataset_id)
         if collection_id:
             ds = ps.get(collection_id)
@@ -70,47 +108,48 @@ class PennsieveApiClient(BaseServiceClientApi):
     async def list_dataset_files(
         self, api_key: str, api_secret: str, dataset_id: str
     ) -> List:
-        ps = Pennsieve(api_token=api_key, api_secret=api_secret)
-        ds = ps.get_dataset(dataset_id)
+        headers = await self._get_authorization_bearer_header(api_key, api_secret)
 
-        async def _parse_dataset_items(
-            dataset: pennsieve.models.Dataset, base_path: Path
-        ) -> List[FileMetaData]:
+        async def _parse_dataset_items() -> List[FileMetaData]:
             file_meta_data = []
             cursor = ""
             page_size = 1000
-            # NOTE: this trick allows to bypass all the python parsing/slowing down of the interface
-            # info given by Joost from Datcore
-            ds_api = dataset._api.datasets  # pylint: disable=protected-access
+
+            response = await self.client.get(
+                f"/datasets/{dataset_id}/packageTypeCounts", headers=headers
+            )
+            response.raise_for_status()
+            num_packages = sum([v for v in response.json().values()])
 
             all_packages: Dict[str, Dict[str, Any]] = {}
 
-            while resp := ds_api._get(  # pylint: disable=protected-access
-                ds_api._uri(  # pylint: disable=protected-access
-                    f"/{dataset_id}/packages?includeSourceFiles=false&pageSize={page_size}&cursor={cursor}"
-                )
+            while resp := await self.client.get(
+                f"/datasets/{dataset_id}/packages",
+                params={
+                    "includeSourceFiles": False,
+                    "pageSize": page_size,
+                    "cursor": cursor,
+                },
+                headers=headers,
             ):
+                resp.raise_for_status()
+                resp = resp.json()
                 cursor = resp.get("cursor")
                 all_packages.update(
                     {p["content"]["id"]: p for p in resp.get("packages", [])}
+                )
+                logger.warning(
+                    "received packages [%s/%s]", len(all_packages), num_packages
                 )
                 if cursor is None:
                     # the whole collection is there now
                     break
 
-            def _get_file_path(pck: Dict[str, Any]) -> Path:
-                file_path = Path(pck["content"]["name"])
-                if "extension" in pck:
-                    file_path = ".".join([f"{file_path}", pck["extension"]])
-                if parent_id := pck["content"].get("parentId"):
-                    file_path = _get_file_path(all_packages[parent_id]) / file_path
-                return file_path
-
             for package_id, package in all_packages.items():
                 if package["content"]["packageType"] == "Collection":
                     continue
 
-                file_path = base_path / _get_file_path(package)
+                file_path = _compute_file_path(all_packages, package)
 
                 file_meta_data.append(
                     FileMetaData(
@@ -129,13 +168,13 @@ class PennsieveApiClient(BaseServiceClientApi):
 
             return file_meta_data
 
-        dataset_files = await _parse_dataset_items(ds, Path(ds.name))
+        dataset_files = await _parse_dataset_items()
         return dataset_files
 
     async def get_presigned_download_link(
         self, api_key: str, api_secret: str, file_id: str
     ) -> URL:
-        ps = Pennsieve(api_token=api_key, api_secret=api_secret)
+        ps: Pennsieve = await self._get_client(api_key=api_key, api_secret=api_secret)
         dp: pennsieve.models.DataPackage = ps.get(file_id)
         assert len(dp.files) == 1  # nosec
         file: pennsieve.models.File = dp.files[0]
