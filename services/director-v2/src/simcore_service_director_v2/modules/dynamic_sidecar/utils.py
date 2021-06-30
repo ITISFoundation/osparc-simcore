6d07fd2edf64211acc2400d9254773f766d2b1a1
Andrei Neagu <neagu@itis.swiss>
2021-06-30 08:59:42 +0200

    removed unused code

--- services/director-v2/src/simcore_service_director_v2/modules/dynamic_sidecar/utils.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-
-def unused_port() -> int:
-    """Returns a port that is unused on the current host."""
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(("127.0.0.1", 0))
-        return s.getsockname()[1]
