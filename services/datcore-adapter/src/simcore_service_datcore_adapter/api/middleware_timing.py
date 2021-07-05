fbb4ba49b7788c09dec4be13415502be0c406e8e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-07-05 10:26:38 +0200

    improve logging formatting

--- services/datcore-adapter/src/simcore_service_datcore_adapter/api/middleware_timing.py
+++ services/datcore-adapter/src/simcore_service_datcore_adapter/api/middleware_timing.py
@@ -9,6 +9,6 @@ async def add_process_time_header(request: Request, call_next):
     start_time = time.time()
     response = await call_next(request)
     process_time = time.time() - start_time
-    logger.info("time to process %s", process_time)
+    logger.debug("time to process %.2fs", process_time)
     response.headers["X-Process-Time"] = str(process_time)
     return response
