d7e2c85dbdabdd790f700915ec748aab3eb98ae5
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-07-06 17:03:21 +0200

    added dask scheduler

--- services/director-v2/src/simcore_service_director_v2/api/dependencies/scheduler.py
+++ services/director-v2/src/simcore_service_director_v2/api/dependencies/scheduler.py
@@ -1,7 +1,12 @@
 from fastapi import Request
 
 from ...modules.celery_scheduler import CeleryScheduler
+from ...modules.dask_scheduler import DaskScheduler
 
 
 def get_celery_scheduler(request: Request) -> CeleryScheduler:
     return request.app.state.celery_scheduler
+
+
+def get_dask_scheduler(request: Request) -> DaskScheduler:
+    return request.app.state.dask_scheduler
