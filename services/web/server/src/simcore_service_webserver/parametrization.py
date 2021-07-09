c7bc2ebe2a184514cfe6ca89230bc8d06f318b03
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-07-09 02:12:44 +0200

    WIP [skip ci]

--- services/web/server/src/simcore_service_webserver/parametrization.py
+++ services/web/server/src/simcore_service_webserver/parametrization.py
@@ -26,7 +26,7 @@ def setup(app: web.Application):
 
     settings: ApplicationSettings = app[APP_SETTINGS_KEY]
     if not settings.WEBSERVER_DEV_FEATURES_ENABLED:
-        log.warning("App module %s disabled: Marked as dev feature", __name__)
+        log.warning("App module '%s' is disabled: Marked as dev feature", __name__)
         return False
 
     app.add_routes(parametrization_api_handlers.routes)
---
+++ services/web/server/src/simcore_service_webserver/parametrization_core.py
@@ -0,0 +1,8 @@
+#
+#
+# Parameter node needs to be evaluated before the workflow is submitted
+# Every evaluation creates a snapshot
+#
+# Constant Param has 1 input adn one output
+#   Optimizer can produce semi-cyclic feedback loop by connecting to output of target to
+#
