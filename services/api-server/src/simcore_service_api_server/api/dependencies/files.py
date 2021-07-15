7cc7d3fb2e3e3cec7d98753c3cfd906fe5b2dc0f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-07-15 13:57:36 +0200

    Adds header reader to implement heuristics to upload files

---
+++ services/api-server/src/simcore_service_api_server/api/dependencies/files.py
@@ -0,0 +1,18 @@
+from fastapi import Header
+from pydantic.types import PositiveInt
+
+#
+# Based on discussion https://github.com/tiangolo/fastapi/issues/362#issuecomment-584104025
+#
+# TODO: add heuristics with max file size to config Timeout?
+# SEE api/routes/files.py::upload_file
+#
+
+GB = 1024 * 1024 * 1024
+MAX_UPLOAD_SIZE = 1 * GB  # TODO: settings?
+
+
+async def valid_content_length(
+    content_length: PositiveInt = Header(..., lt=MAX_UPLOAD_SIZE)
+):
+    return content_length
