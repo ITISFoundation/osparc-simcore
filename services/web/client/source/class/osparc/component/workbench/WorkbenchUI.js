43b0d3ccf7f8c32e939abed95e01972296dd16e1
odeimaiz <maiz@itis.swiss>
2021-07-21 14:48:31 +0200

    fixes interaction after moving a node

--- services/web/client/source/class/osparc/component/workbench/WorkbenchUI.js
+++ services/web/client/source/class/osparc/component/workbench/WorkbenchUI.js
@@ -364,6 +364,17 @@ qx.Class.define("osparc.component.workbench.WorkbenchUI", {
 
       nodeUI.addListener("nodeStoppedMoving", () => {
         this.__updateWorkbenchBounds();
+
+        //  After moving a nodeUI, a new element appears on the DOM tree and prevents from clicking
+        // elsewhere. Here we go through the children of the qx.desktop and remove the undesired element
+        if (this.__desktop.getChildren().length !== this.__desktop.getContentElement().getChildren().length) {
+          this.__desktop.getContentElement().getChildren().forEach(child => {
+            if (child.getAttribute("qxClass") === null) {
+              console.warn("removing undesired element after node move");
+              this.__desktop.getContentElement().remove(child);
+            }
+          });
+        }
       }, this);
 
       nodeUI.addListener("appear", () => {
