b7a29346bcb3a773b6911ad7c69880ae7d67a977
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-07-30 09:57:05 +0200

    remove unused fct

--- ci/build/test-images.bash
+++ ci/build/test-images.bash
@@ -6,21 +6,17 @@ IFS=$'\n\t'
 DOCKER_IMAGE_TAG=$(exec ci/helpers/build_docker_image_tag.bash)
 export DOCKER_IMAGE_TAG
 
-pull_images() {
-    make pull-version || true
-}
-
 build_images() {
-    make build
-    make info-images
+  make build
+  make info-images
 }
 
 # Check if the function exists (bash specific)
 if declare -f "$1" >/dev/null; then
-    # call arguments verbatim
-    "$@"
+  # call arguments verbatim
+  "$@"
 else
-    # Show a helpful error
-    echo "'$1' is not a known function name" >&2
-    exit 1
+  # Show a helpful error
+  echo "'$1' is not a known function name" >&2
+  exit 1
 fi
