72a73b958f76ff1011293e47309a330ecbc41844
Andrei Neagu <neagu@itis.swiss>
2021-08-02 08:40:33 +0200

    refactor

--- packages/models-library/src/models_library/settings/services_common.py
+++ packages/models-library/src/models_library/settings/services_common.py
@@ -1,20 +1,19 @@
 from pydantic import BaseSettings, Field, PositiveInt
 
 _MINUTE = 60
-_BASE_TIMEOUT_FOR_STOPPING_SERVICES = 60 * _MINUTE
 
 
 class ServicesCommonSettings(BaseSettings):
     # set this interval to 1 hour
     director_dynamic_service_save_timeout: PositiveInt = Field(
-        _BASE_TIMEOUT_FOR_STOPPING_SERVICES,
+        60 + _MINUTE,
         description=(
             "When stopping a dynamic service, if it has "
             "big payloads it is important to have longer timeouts."
         ),
     )
     webserver_director_stop_service_timeout: PositiveInt = Field(
-        _BASE_TIMEOUT_FOR_STOPPING_SERVICES + 10,
+        60 * _MINUTE + 10,
         description=(
             "When the webserver invokes the director API to stop "
             "a service which has a very long timeout, it also "
@@ -22,7 +21,7 @@ class ServicesCommonSettings(BaseSettings):
         ),
     )
     storage_service_upload_download_timeout: PositiveInt = Field(
-        _MINUTE * _MINUTE,
+        60 * _MINUTE,
         description=(
             "When dynamic services upload and download data from storage, "
             "sometimes very big payloads are involved. In order to handle "
