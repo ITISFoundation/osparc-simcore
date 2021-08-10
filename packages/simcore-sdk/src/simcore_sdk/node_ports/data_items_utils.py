2857ac4cbfd826001d60218b396c7d9032489a7b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-08-10 15:48:33 +0200

    use thread identifier in temporary path to allow usage in multi-threaded code

--- packages/simcore-sdk/src/simcore_sdk/node_ports/data_items_utils.py
+++ packages/simcore-sdk/src/simcore_sdk/node_ports/data_items_utils.py
@@ -1,4 +1,5 @@
 import tempfile
+import threading
 from pathlib import Path
 from typing import Dict, Tuple, Union
 
@@ -49,8 +50,8 @@ _INTERNAL_DIR = Path(tempfile.gettempdir(), "simcorefiles")
 
 
 def create_folder_path(key: str) -> Path:
-    return Path(_INTERNAL_DIR, key)
+    return Path(_INTERNAL_DIR, f"{threading.get_ident()}", key)
 
 
 def create_file_path(key: str, name: str) -> Path:
-    return Path(_INTERNAL_DIR, key, name)
+    return Path(_INTERNAL_DIR, f"{threading.get_ident()}", key, name)
