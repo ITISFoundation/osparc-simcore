33e2c17b2f7a2bc8015a7e8660d410d7827999c7
odeimaiz <maiz@itis.swiss>
2021-08-10 11:00:47 +0200

    minor

--- services/web/client/source/class/osparc/desktop/SlideShowToolbar.js
+++ services/web/client/source/class/osparc/desktop/SlideShowToolbar.js
@@ -137,13 +137,13 @@ qx.Class.define("osparc.desktop.SlideShowToolbar", {
           nodeIds.push(node.nodeId);
         });
 
+        this.getChildControl("breadcrumb-navigation").populateButtons(nodeIds);
+        this.getChildControl("breadcrumb-navigation-edit").populateButtons(study);
         const currentModeBtn = editSlideshowButtons.getSelection()[0];
         if ("editing" in currentModeBtn && currentModeBtn["editing"]) {
           this.getChildControl("breadcrumbs-scroll").exclude();
-          this.getChildControl("breadcrumb-navigation-edit").populateButtons(study);
           this.getChildControl("breadcrumbs-scroll-edit").show();
         } else {
-          this.getChildControl("breadcrumb-navigation").populateButtons(nodeIds);
           this.getChildControl("breadcrumbs-scroll").show();
           this.getChildControl("breadcrumbs-scroll-edit").exclude();
         }
