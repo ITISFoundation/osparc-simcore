3d6ea63c89c2328ce3aab78ef601e0814e62da53
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-08-12 14:36:45 +0200

    fixed db test

--- packages/simcore-sdk/tests/integration/test_node_ports_common_dbmanager.py
+++ packages/simcore-sdk/tests/integration/test_node_ports_common_dbmanager.py
@@ -12,15 +12,18 @@ from simcore_sdk.node_ports_common.dbmanager import DBManager
 
 pytest_simcore_core_services_selection = ["postgres"]
 
+pytest_simcore_ops_services_selection = ["minio"]
+
 
 async def test_db_manager_read_config(
-    loop: asyncio.events.AbstractEventLoop,
+    project_id: str,
+    node_uuid: str,
     node_ports_config: None,
     default_configuration: Dict,
 ):
     db_manager = DBManager()
     ports_configuration_str = await db_manager.get_ports_configuration_from_node_uuid(
-        config.NODE_UUID
+        project_id, node_uuid
     )
 
     loaded_config_specs = json.loads(ports_configuration_str)
@@ -28,7 +31,8 @@ async def test_db_manager_read_config(
 
 
 async def test_db_manager_write_config(
-    loop: asyncio.events.AbstractEventLoop,
+    project_id: str,
+    node_uuid: str,
     node_ports_config: None,
     special_configuration: Callable,
     default_configuration_file: Path,
@@ -39,9 +43,11 @@ async def test_db_manager_write_config(
     json_configuration = default_configuration_file.read_text()
     # write the default config to the database
     db_manager = DBManager()
-    await db_manager.write_ports_configuration(json_configuration, config.NODE_UUID)
+    await db_manager.write_ports_configuration(
+        json_configuration, project_id, node_uuid
+    )
 
     ports_configuration_str = await db_manager.get_ports_configuration_from_node_uuid(
-        config.NODE_UUID
+        project_id, node_uuid
     )
     assert json.loads(ports_configuration_str) == json.loads(json_configuration)
