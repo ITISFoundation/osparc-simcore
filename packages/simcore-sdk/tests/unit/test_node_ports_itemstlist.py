31e7d03214b2d3307b35c9b4253b490bccd632f1
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-08-12 11:17:59 +0200

    also pass the user id

--- packages/simcore-sdk/tests/unit/test_node_ports_itemstlist.py
+++ packages/simcore-sdk/tests/unit/test_node_ports_itemstlist.py
@@ -108,8 +108,12 @@ async def test_modifying_items_triggers_cb(
         [("1", "integer", 333), ("2", "integer", 333), ("3", "integer", 333)],
     )
     itemslist.change_notifier = mock_method
-    await itemslist[0].set(-123)
+    await itemslist[0].set(
+        user_id=-1, project_id="fake project", node_uuid="fake node", value=-123
+    )
     mock_method.assert_called_once()
     mock_method.reset_mock()
-    await itemslist[0].set(234)
+    await itemslist[0].set(
+        user_id=-1, project_id="fake project", node_uuid="fake node", value=234
+    )
     mock_method.assert_called_once()
