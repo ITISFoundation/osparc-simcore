2d3dec50c09478d26e8c1a656649bad29c4b3e0e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-08-17 23:25:42 +0200

    fixes default start of sidecar

--- packages/settings-library/src/settings_library/comp_services.py
+++ packages/settings-library/src/settings_library/comp_services.py
@@ -1,4 +1,4 @@
-from pydantic.types import ByteSize, NonNegativeInt
+from pydantic import ByteSize, NonNegativeInt, validator
 from settings_library.base import BaseCustomSettings
 
 
@@ -6,3 +6,17 @@ class CompServices(BaseCustomSettings):
     DEFAULT_MAX_NANO_CPUS: NonNegativeInt = 4 * pow(10, 9)
     DEFAULT_MAX_MEMORY: ByteSize = 2 * pow(1024, 3)  # 2 GiB
     DEFAULT_RUNTIME_TIMEOUT: int = 0
+
+    @validator("DEFAULT_MAX_NANO_CPUS", pre=True)
+    @classmethod
+    def set_default_cpus_if_negative(cls, v):
+        if v is None or v == "" or v <= 0:
+            v = 4 * pow(10, 9)
+        return v
+
+    @validator("DEFAULT_MAX_MEMORY", pre=True)
+    @classmethod
+    def set_default_memory_if_negative(cls, v):
+        if v is None or v == "" or v <= 0:
+            v = 2 * pow(1024, 3)
+        return v
