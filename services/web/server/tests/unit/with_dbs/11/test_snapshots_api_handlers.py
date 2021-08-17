ff80208e147302c8439772ac5bc961ee61a3d3a3
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-08-17 10:58:08 +0200

    refactoring test snapshots

--- services/web/server/tests/unit/with_dbs/11/test_snapshots_api_handlers.py
+++ services/web/server/tests/unit/with_dbs/11/test_snapshots_api_handlers.py
@@ -2,115 +2,16 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
-import logging
-from copy import deepcopy
-from typing import Any, Dict, Iterator
 
-import pytest
+from typing import Any, Dict
+
 from aiohttp import web
 from models_library.projects import Project
-from pytest_simcore.helpers.rawdata_fakers import random_project
 from pytest_simcore.helpers.utils_assert import assert_status
-from pytest_simcore.helpers.utils_projects import NewProject
-from simcore_service_webserver import catalog
 from simcore_service_webserver._meta import api_vtag as vtag
-from simcore_service_webserver.db_models import UserRole
-from simcore_service_webserver.log import setup_logging
 from simcore_service_webserver.snapshots_models import SnapshotItem
 
 ProjectDict = Dict[str, Any]
-
-
-@pytest.fixture
-def user_role() -> UserRole:
-    return UserRole.USER
-
-
-@pytest.fixture
-def fake_project() -> ProjectDict:
-    # API model project data
-    return random_project(name=f"{__name__}-project")
-
-
-@pytest.fixture
-async def catalog_subsystem_mock(monkeypatch, fake_project):
-    services_in_project = [
-        {"key": s["key"], "version": s["version"]}
-        for _, s in fake_project["workbench"].items()
-    ]
-
-    async def mocked_get_services_for_user(*args, **kwargs):
-        return services_in_project
-
-    monkeypatch.setattr(
-        catalog, "get_services_for_user_in_product", mocked_get_services_for_user
-    )
-
-
-@pytest.fixture
-def app_cfg(default_app_cfg, aiohttp_unused_port, catalog_subsystem_mock, monkeypatch):
-    """App's configuration used for every test in this module
-
-    NOTE: Overrides services/web/server/tests/unit/with_dbs/conftest.py::app_cfg to influence app setup
-    """
-    cfg = deepcopy(default_app_cfg)
-
-    monkeypatch.setenv("WEBSERVER_DEV_FEATURES_ENABLED", True)
-
-    cfg["main"]["port"] = aiohttp_unused_port()
-    cfg["main"]["studies_access_enabled"] = True
-
-    exclude = {
-        "activity",
-        "catalog",
-        "computation",
-        "diagnostics",
-        "director",
-        "groups",
-        "publications",
-        "resource_manager",
-        "smtp",
-        "socketio",
-        "storage",
-        "studies_access",
-        "studies_dispatcher",
-        "tags",
-        "tracing",
-    }
-    include = {
-        "db",
-        "login",
-        "products",
-        "projects",
-        "snapshots",
-        "rest",
-        "users",
-    }
-
-    assert include.intersection(exclude) == set()
-
-    for section in include:
-        cfg[section]["enabled"] = True
-    for section in exclude:
-        cfg[section]["enabled"] = False
-
-    # NOTE: To see logs, use pytest -s --log-cli-level=DEBUG
-    setup_logging(level=logging.DEBUG)
-
-    # Enforces smallest GC in the background task
-    cfg["resource_manager"]["garbage_collection_interval_seconds"] = 1
-
-    return cfg
-
-
-@pytest.fixture
-async def user_project(
-    client, fake_project: ProjectDict, logged_user
-) -> Iterator[ProjectDict]:
-    async with NewProject(
-        fake_project, client.app, user_id=logged_user["id"]
-    ) as project:
-        yield project
 
 
 async def test_create_snapshot_workflow(client, user_project: ProjectDict):
--- services/web/server/tests/unit/with_dbs/11/test_snapshots_core.py
+++ services/web/server/tests/unit/with_dbs/11/test_snapshots_core.py
@@ -2,97 +2,33 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
-from typing import Dict, Iterator, Tuple
-from uuid import UUID, uuid4
 
-import pytest
-from aiohttp import web
-from models_library.projects import Project  # , ProjectAtDB
-from models_library.projects_nodes import Node
-from models_library.projects_nodes_io import NodeID
-from simcore_service_webserver.projects.projects_api import get_project_for_user
-from simcore_service_webserver.projects.projects_db import APP_PROJECT_DBAPI
-from simcore_service_webserver.projects.projects_utils import clone_project_document
+from typing import Any, Dict
+
+from models_library.projects import Project
+from simcore_service_webserver.snapshots_core import take_snapshot
+from simcore_service_webserver.snapshots_models import Snapshot
+
+ProjectDict = Dict[str, Any]
 
 
-# is parametrized project?
-def is_param(node: Node):
-    return node.key.split("/")[-2] == "param"
+async def test_take_snapshot(user_project: ProjectDict):
 
-
-def is_parametrized_project(project: Project):
-    return any(is_param(node) for node in project.workbench.values())
-
-
-def iter_params(project: Project) -> Iterator[Tuple[NodeID, Node]]:
-    for node_id, node in project.workbench.items():
-        if is_param(node):
-            yield NodeID(node_id), node
-
-
-def is_const_param(node: Node) -> bool:
-    return is_param(node) and "const" in node.key and not node.inputs
-
-
-@pytest.fixture()
-def app():
-    pass
-
-
-@pytest.fixture
-def project_id() -> UUID:
-    return uuid4()
-
-
-@pytest.fixture
-def user_id() -> int:
-    return 1
-
-
-async def test_snapshot_standard_project(
-    app: web.Application, project_id: UUID, user_id: int
-):
-
-    prj_dict: Dict = await get_project_for_user(
-        app, str(project_id), user_id, include_templates=False, include_state=False
+    project, snapshot = await take_snapshot(
+        parent=user_project, snapshot_label="some snapshot"
     )
 
-    # validates API project data
-    project = Project.parse_obj(prj_dict)
+    assert isinstance(project, dict)
+    assert isinstance(snapshot, Snapshot)
 
-    cloned_prj_dict = clone_project_document(prj_dict)
+    # project overrides ORM-only fields
+    assert project["hidden"]
+    assert not project["published"]
 
-    cloned_project = Project.parse_obj(cloned_prj_dict)
+    # domain models
+    parent = Project.parse_obj(user_project)
+    child = Project.parse_obj(project)
 
-    # project_snapshot = snapshot_project(standard_project)
-    # assert isinstance(project_snapshot, Project)
-    assert cloned_project.uuid != project.uuid
-
-    projects_repo = app[APP_PROJECT_DBAPI]
-
-    new_project = await projects_repo.replace_user_project(
-        new_project, user_id, project_uuid, include_templates=True
-    )
-
-    # have same pipeline but different node uuids
-    # assert project_snapshot.
-
-    # removes states: snapshots is always un-run??
-
-    # three types of "parametrization" nodes
-
-    # const -> replace in connecting nodes
-
-    # generators -> iterable data sources (no inputs connected!)
-
-    # feedback  -> parametrizations with connected inputs!
-
-
-#
-# - I have a project
-# - Take a snapshot
-#   - if the project is standard -> clone
-#   - if the project is parametrized
-#      - clone parametrized?
-#      - evaluate param-nodes?
-#
+    # snapshot timestamp corresponds to the last change of the project
+    assert snapshot.created_at == parent.last_change_date
+    assert child.creation_date == snapshot.created_at
