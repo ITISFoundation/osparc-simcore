2858847b294eb53d6b2fe751a32f4cf396552ad8
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-08-17 08:31:48 +0200

    refactor

--- services/dask-sidecar/docker/boot.sh
+++ services/dask-sidecar/docker/boot.sh
@@ -52,35 +52,52 @@ else
   DASK_SCHEDULER_ADDRESS="tcp://${DASK_SCHEDULER_HOST}:8786"
 
   #
-  # by default a dask worker will use as many threads as there are CPUs on the machine regardless of what limit the
-  # the docker container has set (e.g. if the docker container is limited to 4 CPUs out of 10, dask will still use 10 threads by default)
-  # so for now we lock the number of threads to 1, so that only 1 job is done by 1 sidecar, thus --nthreads 1.
-
+  # DASK RESOURCES DEFINITION
   #
-  # 'daemonic processes are not allowed to have children' arises when running the sidecar.cli
-  # because multi-processing library is used by the sidecar and the nanny does not like it
-  # setting --no-nanny fixes this: see https://github.com/dask/distributed/issues/2142
+  # Following resources are defined on a dask-sidecar for computational tasks:
+  # CPU: number of CPUs available (= num of processing units - DASK_SIDECAR_NUM_NON_USABLE_CPUS)
+  # GPU: number GPUs available (= num of GPUs if a nvidia-smi can be run inside a docker container)
+  # RAM: amount of RAM available (= CPU/nproc * total virtual memory given by python psutil - DASK_SIDECAR_NON_USABLE_RAM)
+  # MPI: backwards-compatibility (deprecated if core_number = TARGET_MPI_NODE_CPU_COUNT set to 1)
+
+  # CPUs
   num_cpus=$(($(nproc) - ${DASK_SIDECAR_NUM_NON_USABLE_CPUS:-2}))
   if [ "$num_cpus" -le 0 ]; then
     # ensure the minimal amount of cpus is 1 in case the system is a dual-core or less (CI case for instance)
     num_cpus=1
   fi
+
+  # GPUs
   num_gpus=$(python -c "from simcore_service_sidecar.utils import num_available_gpus; print(num_available_gpus());")
-  # the amount of RAM is computed similarly as the default dask-sidecar computation
-  ram=$(python -c "import psutil; print(psutil.virtual_memory().total * $num_cpus/$(nproc))")
+
+  # RAM (is computed similarly as the default dask-sidecar computation)
+  ram=$(($(python -c "import psutil; print(psutil.virtual_memory().total * $num_cpus/$(nproc))") - ${DASK_SIDECAR_NON_USABLE_RAM:-0}))
+
+  # overall resources available on the dask-sidecar (CPUs and RAM are always available)
   resources="CPU=$num_cpus,RAM=$ram"
+
+  # add the GPUs if there are any
   if [ "$num_gpus" -gt 0 ]; then
     resources="$resources,GPU=$num_gpus"
   fi
+
+  # add the MPI if possible
   if [ ${TARGET_MPI_NODE_CPU_COUNT+x} ]; then
     if [ "$(nproc)" -eq "${TARGET_MPI_NODE_CPU_COUNT}" ]; then
       resources="$resources,MPI=1"
     fi
   fi
+  #
+  # DASK RESOURCES DEFINITION --------------------------------- END
+  #
+
+  #
+  # 'daemonic processes are not allowed to have children' arises when running the sidecar.cli
+  # because multi-processing library is used by the sidecar and the nanny does not like it
+  # setting --no-nanny fixes this: see https://github.com/dask/distributed/issues/2142
   echo "$INFO" "Starting as a ${DASK_WORKER_VERSION} -> ${DASK_SCHEDULER_ADDRESS} ..."
   echo "$INFO" "Worker resources set as: $resources"
   if [ "${SC_BOOT_MODE}" = "debug-ptvsd" ]; then
-
     exec watchmedo auto-restart --recursive --pattern="*.py" -- \
       dask-worker "${DASK_SCHEDULER_ADDRESS}" \
       --local-directory /tmp/dask-sidecar \
@@ -91,9 +108,7 @@ else
       --dashboard-address 8787 \
       --memory-limit "$ram" \
       --resources "$resources"
-
   else
-
     exec dask-worker "${DASK_SCHEDULER_ADDRESS}" \
       --local-directory /tmp/dask-sidecar \
       --preload simcore_service_dask_sidecar.tasks \
@@ -103,6 +118,5 @@ else
       --dashboard-address 8787 \
       --memory-limit "$ram" \
       --resources "$resources"
-
   fi
 fi
