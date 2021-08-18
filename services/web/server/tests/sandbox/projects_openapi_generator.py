17a2a7f456a4581cbfc57b43d43a8ee699c66d53
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-08-18 22:23:41 +0200

    Updates generator

--- services/web/server/tests/sandbox/projects_openapi_generator.py
+++ services/web/server/tests/sandbox/projects_openapi_generator.py
@@ -2,20 +2,21 @@
 # Assists on the creation of project's OAS
 #
 
+import json
+
 # pylint: disable=redefined-outer-name
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
-
-
-import json
-from datetime import datetime
+import logging
 from typing import Any, Callable, Dict, List, Optional, Union
 from uuid import UUID, uuid3
 
+import simcore_service_webserver.snapshots_api_handlers as api_handlers
 from fastapi import Depends, FastAPI
 from fastapi import Path as PathParam
 from fastapi import Query, Request, status
 from fastapi.exceptions import HTTPException
+from fastapi.routing import APIRoute, APIRouter
 from models_library.services import PROPERTY_KEY_RE
 from pydantic import (
     BaseModel,
@@ -27,8 +28,13 @@ from pydantic import (
     constr,
 )
 from pydantic.networks import AnyUrl
+from simcore_service_webserver.snapshots_models import (
+    SnapshotPatchBody,
+    SnapshotResource,
+)
 
-app = FastAPI()
+logging.basicConfig(level=logging.DEBUG)
+
 
 error_responses = {
     status.HTTP_400_BAD_REQUEST: {},
@@ -79,28 +85,9 @@ class Parameter(BaseModel):
     output_id: OutputID
 
 
-class Snapshot(BaseModel):
-    id: PositiveInt = Field(..., description="Unique snapshot identifier")
-    label: Optional[str] = Field(None, description="Unique human readable display name")
-    created_at: datetime = Field(
-        default_factory=datetime.utcnow,
-        description="Timestamp of the time snapshot was taken from parent. Notice that parent might change with time",
-    )
-
-    parent_uuid: UUID = Field(..., description="Parent's project uuid")
-    project_uuid: UUID = Field(..., description="Current project's uuid")
-
-
-class ParameterApiModel(Parameter):
+class ParameterResource(Parameter):
     url: AnyUrl
     # url_output: AnyUrl
-
-
-class SnapshotApiModel(Snapshot):
-    url: AnyUrl
-    url_parent: AnyUrl
-    url_project: AnyUrl
-    url_parameters: Optional[AnyUrl] = None
 
 
 ####################################################################
@@ -127,13 +114,15 @@ def get_valid_id(project_id: UUID = PathParam(...)) -> UUID:
 
 ####################################################################
 
+project_routes = APIRouter(tags=["project"])
 
-@app.get("/projects/{project_id}", response_model=Project, tags=["project"])
+
+@project_routes.get("/projects/{project_id}", response_model=Project, tags=["project"])
 def get_project(pid: UUID = Depends(get_valid_id)):
     return _PROJECTS[pid]
 
 
-@app.post(
+@project_routes.post(
     "/projects/{project_id}", status_code=status.HTTP_201_CREATED, tags=["project"]
 )
 def create_project(project: Project):
@@ -142,37 +131,37 @@ def create_project(project: Project):
     _PROJECTS[project.id] = project
 
 
-@app.put("/projects/{project_id}", tags=["project"])
+@project_routes.put("/projects/{project_id}", tags=["project"])
 def replace_project(project: Project, pid: UUID = Depends(get_valid_id)):
     _PROJECTS[pid] = project
 
 
-@app.patch("/projects/{project_id}", tags=["project"])
+@project_routes.patch("/projects/{project_id}", tags=["project"])
 def update_project(project: Project, pid: UUID = Depends(get_valid_id)):
     raise NotImplementedError()
 
 
-@app.delete("/projects/{project_id}", tags=["project"])
+@project_routes.delete("/projects/{project_id}", tags=["project"])
 def delete_project(pid: UUID = Depends(get_valid_id)):
     del _PROJECTS[pid]
 
 
-@app.post("/projects/{project_id}:open", tags=["project"])
+@project_routes.post("/projects/{project_id}:open", tags=["project"])
 def open_project(pid: UUID = Depends(get_valid_id)):
     pass
 
 
-@app.post("/projects/{project_id}:start", tags=["project"])
+@project_routes.post("/projects/{project_id}:start", tags=["project"])
 def start_project(use_cache: bool = True, pid: UUID = Depends(get_valid_id)):
     pass
 
 
-@app.post("/projects/{project_id}:stop", tags=["project"])
+@project_routes.post("/projects/{project_id}:stop", tags=["project"])
 def stop_project(pid: UUID = Depends(get_valid_id)):
     pass
 
 
-@app.post("/projects/{project_id}:close", tags=["project"])
+@project_routes.post("/projects/{project_id}:close", tags=["project"])
 def close_project(pid: UUID = Depends(get_valid_id)):
     pass
 
@@ -182,11 +171,12 @@ def close_project(pid: UUID = Depends(get_valid_id)):
 #  - takes a snapshot of the current state of the project
 #  -
 
+snapshot_routes = APIRouter(tags=["project", "snapshot"])
 
-@app.get(
+
+@snapshot_routes.get(
     "/projects/{project_id}/snapshots",
-    response_model=List[SnapshotApiModel],
-    tags=["project"],
+    response_model=List[SnapshotResource],
 )
 async def list_snapshots(
     pid: UUID = Depends(get_valid_id),
@@ -198,70 +188,101 @@ async def list_snapshots(
     ),
     url_for: Callable = Depends(get_reverse_url_mapper),
 ):
-    """ Lists all snapshots taken from a given project """
+    """Lists all snapshots taken from a given project"""
 
 
-@app.post(
+@snapshot_routes.post(
     "/projects/{project_id}/snapshots",
-    response_model=SnapshotApiModel,
+    response_model=SnapshotResource,
     status_code=status.HTTP_201_CREATED,
-    tags=["project"],
 )
 async def create_snapshot(
     pid: UUID = Depends(get_valid_id),
     snapshot_label: Optional[str] = None,
     url_for: Callable = Depends(get_reverse_url_mapper),
 ):
-    """ Takes a snapshot of the project at this time """
+    """Takes a snapshot of the project at this time"""
     # - hash parent_project as a mechanism to check changes
     # -
 
 
-@app.get(
+@snapshot_routes.get(
     "/projects/{project_id}/snapshots/{snapshot_id}",
-    response_model=SnapshotApiModel,
-    tags=["project"],
+    response_model=SnapshotResource,
 )
 async def get_snapshot(
     snapshot_id: PositiveInt,
     pid: UUID = Depends(get_valid_id),
     url_for: Callable = Depends(get_reverse_url_mapper),
 ):
-    """ Gets commit info for a given snapshot """
+    """Gets commit info for a given snapshot"""
 
 
-@app.delete(
+@snapshot_routes.delete(
     "/projects/{project_id}/snapshots/{snapshot_id}",
     status_code=status.HTTP_204_NO_CONTENT,
-    tags=["project"],
 )
 async def delete_snapshot(
     snapshot_id: PositiveInt,
     pid: UUID = Depends(get_valid_id),
 ):
-    """ Deletes both the commit and the project itself """
+    """Deletes both the commit and the project itself"""
     # delete a snapshot -> project deleted?
     # delete a project-snapshot -> delete snapshot
 
 
-@app.get(
-    "/projects/{project_id}/snapshots/{snapshot_id}/parameters",
-    response_model=List[ParameterApiModel],
-    tags=["project"],
+@snapshot_routes.patch(
+    "/projects/{project_id}/snapshots/{snapshot_id}",
+)
+async def update_snapshot(
+    snapshot_id: PositiveInt,
+    update: SnapshotPatchBody,
+    pid: UUID = Depends(get_valid_id),
+):
+    """Updates label/name of a snapshot"""
+
+
+# project parametrization
+
+parameter_routes = APIRouter(tags=["project"])
+
+
+@parameter_routes.get(
+    "/projects/{project_id}/parameters",
+    response_model=List[ParameterResource],
 )
 async def list_snapshot_parameters(
     snapshot_id: str,
     pid: UUID = Depends(get_valid_id),
     url_for: Callable = Depends(get_reverse_url_mapper),
 ):
-    pass
+    """Lists all parameters in a project"""
 
 
 ## workflow compiler #######################################
 
+
+def redifine_operation_id_in_router(router: APIRouter, operation_id_prefix: str):
+    for route in router.routes:
+        if isinstance(route, APIRoute):
+            route.operation_id = f"{operation_id_prefix}.{route.endpoint.__name__}"
+
+
+def include_router(app: FastAPI, router: APIRouter, operation_id_prefix: str):
+    redifine_operation_id_in_router(router, operation_id_prefix)
+    app.include_router(snapshot_routes)
+
+
+app = FastAPI()
+
+
+# app.include_router(project_routes)
+include_router(app, snapshot_routes, api_handlers.__name__)
+# app.include_router(parameter_routes)
+
+
 # print(yaml.safe_dump(app.openapi()))
 # print("-"*100)
-
 
 with open("openapi-ignore.json", "wt") as f:
     json.dump(app.openapi(), f, indent=2)
