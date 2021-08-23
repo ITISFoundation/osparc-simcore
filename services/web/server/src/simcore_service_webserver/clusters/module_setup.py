b078bd9a04c26253c12632726a5d9e594f82bda3
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-08-23 17:46:59 +0200

    adding webserver clusters api

---
+++ services/web/server/src/simcore_service_webserver/clusters/module_setup.py
@@ -0,0 +1,39 @@
+""" clusters app module setup
+
+    Allows a user to manage clusters depending on user group(s) access rights:
+        - create, modify, delete clusters
+        - monitor clusters
+        - send computational jobs to clusters
+
+"""
+import logging
+
+from aiohttp import web
+from servicelib.application_setup import (
+    ModuleCategory,
+    SkipModuleSetup,
+    app_module_setup,
+)
+
+from ..constants import APP_SETTINGS_KEY
+from ..settings import ApplicationSettings
+from . import handlers
+
+log = logging.getLogger(__file__)
+
+
+@app_module_setup(
+    __name__,
+    ModuleCategory.ADDON,
+    depends=["simcore_service_webserver.groups"],
+    logger=log,
+)
+def setup_clusters(app: web.Application):
+    settings: ApplicationSettings = app[APP_SETTINGS_KEY]
+    if not settings.WEBSERVER_DEV_FEATURES_ENABLED:
+        raise SkipModuleSetup(reason="Development feature")
+
+    app.add_routes(handlers.routes)
+
+
+__all__ = ["setup_clusters"]
