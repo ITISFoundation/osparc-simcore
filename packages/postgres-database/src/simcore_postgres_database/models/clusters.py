61e778f2cd16c0920003b633b1f7a3c07f83cdfe
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-08-30 11:37:56 +0200

    fix foreign key name

--- packages/postgres-database/src/simcore_postgres_database/models/clusters.py
+++ packages/postgres-database/src/simcore_postgres_database/models/clusters.py
@@ -36,7 +36,7 @@ clusters = sa.Table(
         sa.BigInteger,
         sa.ForeignKey(
             "groups.gid",
-            name="fk_services_meta_data_gid_groups",
+            name="fk_clusters_gid_groups",
             onupdate="CASCADE",
             ondelete="RESTRICT",
         ),
