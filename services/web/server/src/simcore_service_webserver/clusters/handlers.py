b1de1067ade130a8a8af7a4ee0961f1d770b87f2
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-08-30 18:15:11 +0200

    added exceptions for clusters

--- services/web/server/src/simcore_service_webserver/clusters/handlers.py
+++ services/web/server/src/simcore_service_webserver/clusters/handlers.py
@@ -5,6 +5,7 @@ from aiohttp import web
 from models_library.users import GroupID, UserID
 from servicelib.rest_utils import extract_and_validate
 from simcore_postgres_database.models.clusters import ClusterType
+from simcore_service_webserver.clusters.exceptions import ClusterNotFoundError
 from simcore_service_webserver.clusters.models import Cluster, ClusterCreate
 from simcore_service_webserver.groups_api import list_user_groups
 from simcore_service_webserver.security_decorators import permission_required
@@ -67,7 +68,20 @@ async def create_cluster_handler(request: web.Request) -> web.Response:
 @login_required
 @permission_required("clusters.read")
 async def get_cluster_handler(request: web.Request) -> web.Response:
-    raise web.HTTPNotImplemented(reason="not yet implemented")
+    path, _, _ = await extract_and_validate(request)
+    user_id: UserID = request[RQT_USERID_KEY]
+    primary_group, std_groups, all_group = await list_user_groups(request.app, user_id)
+
+    clusters_repo = ClustersRepository(request)
+    try:
+        cluster = clusters_repo.get_cluster(
+            GroupID(primary_group["gid"]),
+            [GroupID(g["gid"]) for g in std_groups],
+            GroupID(all_group["gid"]),
+            path["id"],
+        )
+    except ClusterNotFoundError as exc:
+        raise web.HTTPNotFound(reason=exc.msg)
 
 
 @routes.patch(f"/{api_version_prefix}/clusters/{{id}}", name="update_cluster_handler")
