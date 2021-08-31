8a89675d44a512814a915e79432bb1ebe4860209
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-08-31 15:28:30 +0200

    Adds extra requirements

--- packages/service-library/setup.py
+++ packages/service-library/setup.py
@@ -17,6 +17,11 @@ install_requirements = read_reqs(
     here / "requirements" / "_base.in"
 )  # WEAK requirements
 
+aiohttp_requirements = read_reqs(
+    here / "requirements" / "_aiohttp.in"
+)  # WEAK requirements
+
+
 test_requirements = read_reqs(
     here / "requirements" / "_test.txt"
 )  # STRONG requirements
@@ -44,6 +49,6 @@ setup(
     include_package_data=True,
     test_suite="tests",
     tests_require=test_requirements,
-    extras_require={"test": test_requirements},
+    extras_require={"test": test_requirements, "aiohttp": aiohttp_requirements},
     zip_safe=False,
 )
