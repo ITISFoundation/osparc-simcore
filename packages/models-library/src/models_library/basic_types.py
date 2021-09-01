65753195eeee4149c7b765c944fe6a014a52f3db
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-09-01 10:08:51 +0200

    @GitHK review: minor doc and asserts

--- packages/models-library/src/models_library/basic_types.py
+++ packages/models-library/src/models_library/basic_types.py
@@ -2,10 +2,10 @@ from enum import Enum
 
 from pydantic import conint, constr
 
-# url port
+# port number range
 PortInt = conint(gt=0, lt=65535)
 
-#
+# e.g. 'v5'
 VersionTag = constr(regex=r"^v\d$")
 
 # checksums
--- packages/models-library/src/models_library/utils/misc.py
+++ packages/models-library/src/models_library/utils/misc.py
@@ -1,19 +1,31 @@
-from typing import Any, Dict, List, Type
+from typing import Any, Dict, List, Type, Union
 
 from pydantic import BaseModel
+from pydantic.main import SchemaExtraCallable
 
 
 def extract_examples(model_cls: Type[BaseModel]) -> List[Dict[str, Any]]:
-    """ Extracts examples from pydantic classes"""
+    """Extracts examples from pydantic classes"""
 
     examples = []
 
-    schema_extra = model_cls.__config__.schema_extra
-    if schema_extra and isinstance(schema_extra, dict):
+    schema_extra: Union[
+        Dict[str, Any], SchemaExtraCallable
+    ] = model_cls.__config__.schema_extra
+
+    if isinstance(schema_extra, dict):
+        # NOTE: Sometimes an example (singular) mistaken
+        # by exampleS. The assertions below should
+        # help catching this error while running tests
+
         examples = schema_extra.get("examples", [])
+        assert isinstance(examples, list)  # nosec
+
         if example := schema_extra.get("example"):
+            assert not isinstance(example, list)  # nosec
             examples.append(example)
 
-    # TODO: extract as well from the fields?
+    # TODO: treat SchemaExtraCallable case (so far we only have one example)
+    # TODO: extract examples from single fields and compose model?
 
     return examples
