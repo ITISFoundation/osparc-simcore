bb79889326130a7e91b8084d66d28b56a3680f82
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-09-10 11:50:55 +0200

    fix unit tests move tests playing with the swarm out of the parallel tests

--- services/director-v2/tests/unit/with_swarm/test_routes_dynamic_services.py
+++ services/director-v2/tests/unit/with_swarm/test_routes_dynamic_services.py
@@ -46,6 +46,7 @@ def minimal_config(project_env_devel_environment, monkeypatch) -> None:
     monkeypatch.setenv("REGISTRY_ENABLED", "0")
     monkeypatch.setenv("DIRECTOR_V2_DASK_SCHEDULER_ENABLED", "0")
     monkeypatch.setenv("DIRECTOR_V2_CELERY_SCHEDULER_ENABLED", "0")
+    monkeypatch.setenv("DIRECTOR_V2_DASK_CLIENT_ENABLED", "0")
 
 
 @pytest.fixture(scope="session")
