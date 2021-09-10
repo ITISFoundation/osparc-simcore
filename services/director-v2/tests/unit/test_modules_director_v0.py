8ad0932873786e8873a0b009df81bd36e4c42369
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-09-10 11:07:15 +0200

    disable dask scheduler for testing

--- services/director-v2/tests/unit/test_modules_director_v0.py
+++ services/director-v2/tests/unit/test_modules_director_v0.py
@@ -39,6 +39,8 @@ def minimal_director_config(project_env_devel_environment, monkeypatch):
     monkeypatch.setenv("DIRECTOR_V2_POSTGRES_ENABLED", "0")
     monkeypatch.setenv("DIRECTOR_V2_CELERY_ENABLED", "0")
     monkeypatch.setenv("DIRECTOR_V2_CELERY_SCHEDULER_ENABLED", "0")
+    monkeypatch.setenv("DIRECTOR_V2_DASK_CLIENT_ENABLED", "0")
+    monkeypatch.setenv("DIRECTOR_V2_DASK_SCHEDULER_ENABLED", "0")
 
 
 @pytest.fixture
