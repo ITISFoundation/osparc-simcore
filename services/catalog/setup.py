b5ff3f4240e338a710301954ec750ef7bddb3a8f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-09-22 17:25:35 +0200

    catalog service: split reqs in base and packages

--- services/catalog/setup.py
+++ services/catalog/setup.py
@@ -3,6 +3,7 @@
 import re
 import sys
 from pathlib import Path
+from typing import List
 
 from setuptools import find_packages, setup
 
@@ -15,19 +16,34 @@ if not (sys.version_info.major == 3 and sys.version_info.minor == 8):
 current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
 
-def read_reqs(reqs_path: Path):
-    return re.findall(
-        r"(^[^#\n-][\w\[,\]]+[-~>=<.\w]*)", reqs_path.read_text(), re.MULTILINE
+def read_reqs(reqs_path: Path) -> List[str]:
+    return list(
+        set(
+            [
+                r
+                for r in re.findall(
+                    r"(^[^#\n-][\w\[,\]]+[-~>=<.\w]*)",
+                    reqs_path.read_text(),
+                    re.MULTILINE,
+                )
+                if isinstance(r, str)
+            ]
+        )
     )
 
 
 readme = (current_dir / "README.md").read_text()
 version = (current_dir / "VERSION").read_text().strip()
 
-install_requirements = read_reqs(current_dir / "requirements" / "_base.txt") + [
-    "simcore-models-library",
-    "simcore-postgres-database",
-]
+install_requirements = (
+    read_reqs(current_dir / "requirements" / "_base.txt")
+    + read_reqs(current_dir / "requirements" / "_packages.txt")
+    + [
+        "simcore-models-library",
+        "simcore-postgres-database",
+        "simcore-service-library[aiohttp]",
+    ]
+)
 
 test_requirements = read_reqs(current_dir / "requirements" / "_test.txt")
 
@@ -56,7 +72,7 @@ setup(
         ],
     },
     include_package_data=True,
-    install_requires=install_requirements,
+    # install_requires=install_requirements,
     test_suite="tests",
     tests_require=test_requirements,
     extras_require={"test": test_requirements},
