97470c3f5278b5d6aa577b17f591a01475900e09
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-09-23 18:05:08 +0200

    patches properly exclusive* field discrepancies between openapi and jsonschema

--- packages/service-library/src/servicelib/fastapi/openapi.py
+++ packages/service-library/src/servicelib/fastapi/openapi.py
@@ -28,27 +28,39 @@ def redefine_operation_id_in_router(router: APIRouter, operation_id_prefix: str)
             )
 
 
+# https://swagger.io/docs/specification/data-models/data-types/#numbers
+SCHEMA_TO_PYTHON_TYPES = {"integer": int, "number": float}
+
+SKIP = (
+    "examples",
+    # SEE openapi-standard: https://swagger.io/docs/specification/adding-examples/
+    # - exampleS are Dicts and not Lists
+    "patternProperties"
+    # SEE Unsupported openapi-standard: https://swagger.io/docs/specification/data-models/keywords/?sbsearch=patternProperties
+    # SEE https://github.com/OAI/OpenAPI-Specification/issues/687
+    # SEE https://json-schema.org/understanding-json-schema/reference/object.html#pattern-properties
+)
+
+
 def _patch_openapi_specs(app_openapi: Dict[str, Any]):
     """Patches app.openapi with some fixes and osparc conventions"""
 
     def _patch(node):
         if isinstance(node, Dict):
             for key in list(node.keys()):
-                # SEE fastapi issue: https://github.com/tiangolo/fastapi/issues/240
+                # SEE fastapi ISSUE: https://github.com/tiangolo/fastapi/issues/240 (test_openap.py::test_exclusive_min_openapi_issue )
                 # SEE openapi-standard: https://swagger.io/docs/specification/data-models/data-types/#range
                 if key == "exclusiveMinimum":
-                    node[key] = bool(node[key])
+                    cast_to_python = SCHEMA_TO_PYTHON_TYPES[node["type"]]
+                    node["minimum"] = cast_to_python(node[key])
+                    node["exclusiveMinimum"] = True
 
-                SKIP = (
-                    "examples",
-                    # SEE openapi-standard: https://swagger.io/docs/specification/adding-examples/
-                    # - exampleS are Dicts and not Lists
-                    "patternProperties"
-                    # SEE Unsupported openapi-standard: https://swagger.io/docs/specification/data-models/keywords/?sbsearch=patternProperties
-                    # SEE https://github.com/OAI/OpenAPI-Specification/issues/687
-                    # SEE https://json-schema.org/understanding-json-schema/reference/object.html#pattern-properties
-                )
-                if key in SKIP:
+                elif key == "exclusiveMaximum":
+                    cast_to_python = SCHEMA_TO_PYTHON_TYPES[node["type"]]
+                    node["maximum"] = cast_to_python(node[key])
+                    node["exclusiveMaximum"] = True
+
+                elif key in SKIP:
                     node.pop(key)
                     continue
 
--- packages/service-library/tests/fastapi/conftest.py
+++ packages/service-library/tests/fastapi/conftest.py
@@ -6,7 +6,9 @@ from datetime import datetime
 
 import pytest
 from fastapi import FastAPI
+from fastapi.params import Query
 from fastapi.routing import APIRouter
+from pydantic.types import PositiveFloat
 
 
 @pytest.fixture
@@ -15,8 +17,12 @@ def app() -> FastAPI:
     api_router = APIRouter()
 
     @api_router.get("/")
-    def root():
+    def _get_root():
         return {"name": __name__, "timestamp": datetime.utcnow().isoformat()}
+
+    @api_router.get("/data")
+    def _get_data(x: PositiveFloat, y: int = Query(..., gt=3, lt=4)):
+        pass
 
     _app = FastAPI()
     _app.include_router(api_router)
