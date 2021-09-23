541ce810fccf96b421210e57f1e891ae1e3ab8f2
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-09-23 12:46:55 +0200

    doc

--- packages/service-library/src/servicelib/fastapi/openapi.py
+++ packages/service-library/src/servicelib/fastapi/openapi.py
@@ -34,15 +34,17 @@ def _patch_openapi_specs(app_openapi: Dict[str, Any]):
     def _patch(node):
         if isinstance(node, Dict):
             for key in list(node.keys()):
-                # SEE https://github.com/tiangolo/fastapi/issues/240
+                # SEE fastapi issue: https://github.com/tiangolo/fastapi/issues/240
+                # SEE openapi-standard: https://swagger.io/docs/specification/data-models/data-types/#range
                 if key == "exclusiveMinimum":
                     node[key] = bool(node[key])
 
                 SKIP = (
                     "examples",
-                    # SEE https://swagger.io/docs/specification/adding-examples/
-                    # - exampleS are dicts in openapi
+                    # SEE openapi-standard: https://swagger.io/docs/specification/adding-examples/
+                    # - exampleS are Dicts and not Lists
                     "patternProperties"
+                    # SEE Unsupported openapi-standard: https://swagger.io/docs/specification/data-models/keywords/?sbsearch=patternProperties
                     # SEE https://github.com/OAI/OpenAPI-Specification/issues/687
                     # SEE https://json-schema.org/understanding-json-schema/reference/object.html#pattern-properties
                 )
