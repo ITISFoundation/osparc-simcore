ce2c32fc91b7cf4978f3c2f56a15203afba08887
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-09-23 11:04:54 +0200

    updates service-lib setup

--- packages/service-library/setup.py
+++ packages/service-library/setup.py
@@ -1,59 +1,64 @@
 import re
 import sys
 from pathlib import Path
-from typing import Any, List
+from typing import Set
 
 from setuptools import find_packages, setup
 
-here = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+CURRENT_DIR = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
 
-def read_reqs(reqs_path: Path) -> List[Any]:
-    return re.findall(
-        r"(^[^#\n-][\w\[,\]]+[-~>=<.\w]*)", reqs_path.read_text(), re.MULTILINE
+def read_reqs(reqs_path: Path) -> Set[str]:
+    return {
+        r
+        for r in re.findall(
+            r"(^[^#\n-][\w\[,\]]+[-~>=<.\w]*)",
+            reqs_path.read_text(),
+            re.MULTILINE,
+        )
+        if isinstance(r, str)
+    }
+
+
+# WEAK requirements (see requirements/python-dependencies.md)
+install_requirements = read_reqs(CURRENT_DIR / "requirements" / "_base.in")
+aiohttp_requirements = read_reqs(CURRENT_DIR / "requirements" / "_aiohttp.in")
+fastapi_requirements = read_reqs(CURRENT_DIR / "requirements" / "_fastapi.in")
+
+# STRONG requirements (see requirements/python-dependencies.md)
+test_requirements = read_reqs(CURRENT_DIR / "requirements" / "_test.txt")
+
+
+if __name__ == "__main__":
+
+    setup(
+        name="simcore-service-library",
+        version="1.0.0",
+        author="Pedro Crespo (pcrespov)",
+        description="Core service library for simcore (or servicelib)",
+        classifiers=[
+            "Development Status :: 2 - Pre-Alpha",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Programming Language :: Python :: 3.8",
+        ],
+        long_description=Path(CURRENT_DIR / "README.rst").read_text(),
+        license="MIT license",
+        python_requires="~=3.8",
+        install_requires=list(install_requirements),
+        packages=find_packages(where="src"),
+        package_dir={"": "src"},
+        include_package_data=True,
+        test_suite="tests",
+        tests_require=list(test_requirements),
+        extras_require={
+            "test": list(test_requirements),
+            "aiohttp": list(aiohttp_requirements),
+            "fastapi": list(fastapi_requirements),
+            "all": list(
+                install_requirements | aiohttp_requirements | fastapi_requirements
+            ),
+        },
+        zip_safe=False,
     )
-
-
-install_requirements = read_reqs(
-    here / "requirements" / "_base.in"
-)  # WEAK requirements
-
-
-test_requirements = read_reqs(
-    here / "requirements" / "_test.txt"
-)  # STRONG requirements
-
-aiohttp_requirements = read_reqs(here / "requirements" / "_aiohttp.in")
-fastapi_requirements = read_reqs(here / "requirements" / "_fastapi.in")
-
-readme = Path(here / "README.rst").read_text()
-
-setup(
-    name="simcore-service-library",
-    version="1.0.0",
-    author="Pedro Crespo (pcrespov)",
-    description="Core service library for simcore (or servicelib)",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.8",
-    ],
-    long_description=readme,
-    license="MIT license",
-    python_requires="~=3.8",
-    install_requires=install_requirements,
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    test_suite="tests",
-    tests_require=test_requirements,
-    extras_require={
-        "test": test_requirements,
-        "aiohttp": aiohttp_requirements,
-        "fastapi": fastapi_requirements,
-        "all": aiohttp_requirements + fastapi_requirements,
-    },
-    zip_safe=False,
-)
