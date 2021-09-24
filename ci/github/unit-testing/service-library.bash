4b6e38fd843e3d540a13da71f55216c7d601a57f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-09-24 13:51:39 +0200

    renamed job

--- ci/github/unit-testing/service-library.bash
+++ ci/github/unit-testing/service-library.bash
@@ -5,23 +5,13 @@ set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 IFS=$'\n\t'
 
-install() {
-    bash ci/helpers/ensure_python_pip.bash
-    pushd packages/service-library; pip3 install -r requirements/ci.txt; popd;
-    pip list -v
-}
 
+# NOTE: notice that the CI uses [all]
+# TODO: add STEPS where pip-sync individual extras and test separately
 install_all() {
     bash ci/helpers/ensure_python_pip.bash
     pushd packages/service-library; pip3 install -r "requirements/ci[all].txt"; popd;
     pip list -v
-}
-
-test() {
-    pytest --cov=servicelib --durations=10 --cov-append \
-          --color=yes --cov-report=term-missing --cov-report=xml --cov-config=.coveragerc \
-          -v -m "not travis" --ignore=packages/service-library/tests/aiohttp \
-          packages/service-library/tests
 }
 
 test_all() {
