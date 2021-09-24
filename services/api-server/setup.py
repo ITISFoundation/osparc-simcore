63f8d92de2a5341e2ccd3588f4b3fb0e28b3692b
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-09-24 18:49:29 +0200

    adds simcore-service-library[fastapi] to api-server reqs

--- services/api-server/setup.py
+++ services/api-server/setup.py
@@ -3,6 +3,7 @@
 import re
 import sys
 from pathlib import Path
+from typing import Set
 
 from setuptools import find_packages, setup
 
@@ -12,54 +13,62 @@ if not (sys.version_info.major == 3 and sys.version_info.minor == 8):
         % str(sys.version_info)
     )
 
-current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+
+def read_reqs(reqs_path: Path) -> Set[str]:
+    return {
+        r
+        for r in re.findall(
+            r"(^[^#\n-][\w\[,\]]+[-~>=<.\w]*)",
+            reqs_path.read_text(),
+            re.MULTILINE,
+        )
+        if isinstance(r, str)
+    }
 
 
-def read_reqs(reqs_path: Path):
-    return re.findall(
-        r"(^[^#\n-][\w\[,\]]+[-~>=<.\w]*)", reqs_path.read_text(), re.MULTILINE
-    )
+CURRENT_DIR = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
+VERSION = (CURRENT_DIR / "VERSION").read_text().strip()
 
-readme = (current_dir / "README.md").read_text()
-version = (current_dir / "VERSION").read_text().strip()
-
-install_requirements = read_reqs(current_dir / "requirements" / "_base.txt") + [
-    "simcore-models-library",
-    "simcore-postgres-database",
-    "simcore-sdk",
-    "simcore-service-library",
-]
-
-test_requirements = read_reqs(current_dir / "requirements" / "_test.txt")
-
-
-setup(
-    name="simcore-service-api-server",
-    version=version,
-    author="Pedro Crespo (pcrespov)",
-    description="Platform's API Server for external clients",
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.8",
-    ],
-    long_description=readme,
-    license="MIT license",
-    python_requires="~=3.8",
-    packages=find_packages(where="src"),
-    package_dir={
-        "": "src",
-    },
-    include_package_data=True,
-    package_data={
-        "": [
-            "mocks/*.y*ml",
-        ]
-    },
-    install_requires=install_requirements,
-    test_suite="tests",
-    tests_require=test_requirements,
-    extras_require={"test": test_requirements},
+PROD_REQUIREMENTS = list(
+    read_reqs(CURRENT_DIR / "requirements" / "_base.txt")
+    | {
+        "simcore-models-library",
+        "simcore-postgres-database",
+        "simcore-sdk",
+        "simcore-service-library[fastapi]",
+    }
 )
+
+TEST_REQUIREMENTS = list(read_reqs(CURRENT_DIR / "requirements" / "_test.txt"))
+
+if __name__ == "__main__":
+    setup(
+        name="simcore-service-api-server",
+        version=VERSION,
+        author="Pedro Crespo (pcrespov)",
+        description="Platform's API Server for external clients",
+        classifiers=[
+            "Development Status :: 1 - Planning",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Programming Language :: Python :: 3.8",
+        ],
+        long_description=(CURRENT_DIR / "README.md").read_text(),
+        license="MIT license",
+        python_requires="~=3.8",
+        packages=find_packages(where="src"),
+        package_dir={
+            "": "src",
+        },
+        include_package_data=True,
+        package_data={
+            "": [
+                "mocks/*.y*ml",
+            ]
+        },
+        install_requires=PROD_REQUIREMENTS,
+        test_suite="tests",
+        tests_require=TEST_REQUIREMENTS,
+        extras_require={"test": TEST_REQUIREMENTS},
+    )
