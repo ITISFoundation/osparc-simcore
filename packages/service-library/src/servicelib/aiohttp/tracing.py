f74c5a961593649a1310fb76eab044dcc90d2b18
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-09-27 17:48:01 +0200

    updates servicelib.tracing

--- packages/service-library/src/servicelib/aiohttp/tracing.py
+++ packages/service-library/src/servicelib/aiohttp/tracing.py
@@ -1,39 +1,45 @@
 """ Adds aiohttp middleware for tracing using zipkin server instrumentation.
 
 """
-import asyncio
 import logging
-from typing import Dict, Optional
+from typing import Union
 
 import aiozipkin as az
-import trafaret as T
 from aiohttp import web
-from pydantic import AnyHttpUrl, BaseSettings
+from yarl import URL
 
 log = logging.getLogger(__name__)
 
 
+DEFAULT_JAEGER_BASE_URL = "http://jaeger:9411"
+
+
 def setup_tracing(
-    app: web.Application, app_name: str, host: str, port: int, config: Dict
+    app: web.Application,
+    *,
+    service_name: str,
+    host: str,
+    port: int,
+    jaeger_base_url: Union[URL, str],
 ) -> bool:
-    zipkin_address = f"{config['zipkin_endpoint']}/api/v2/spans"
-    endpoint = az.create_endpoint(app_name, ipv4=host, port=port)
-    loop = asyncio.get_event_loop()
-    tracer = loop.run_until_complete(
-        az.create(zipkin_address, endpoint, sample_rate=1.0)
+    """
+    Sets up this service for a distributed tracing system
+    using zipkin (https://zipkin.io/) and Jaeger (https://www.jaegertracing.io/)
+    """
+    zipkin_address = URL(f"{jaeger_base_url}") / "api/v2/spans"
+
+    log.debug(
+        "Settings up tracing for %s(%s:%d) -> %s",
+        service_name,
+        host,
+        port,
+        zipkin_address,
     )
-    az.setup(app, tracer)
+
+    async def _on_startup(app: web.Application):
+        endpoint = az.create_endpoint(service_name, ipv4=host, port=port)
+        tracer = await az.create(f"{zipkin_address}", endpoint, sample_rate=1.0)
+        az.setup(app, tracer)
+
+    app.on_startup.append(_on_startup)
     return True
-
-
-schema = T.Dict(
-    {
-        T.Key("enabled", default=True, optional=True): T.Or(T.Bool(), T.ToInt),
-        T.Key("zipkin_endpoint", default="http://jaeger:9411"): T.String(),
-    }
-)
-
-
-class TracingSettings(BaseSettings):
-    enabled: Optional[bool] = True
-    zipkin_endpoint: AnyHttpUrl = "http://jaeger:9411"
