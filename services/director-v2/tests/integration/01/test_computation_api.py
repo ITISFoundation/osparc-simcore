72ba3808b35922857285dccb746851436d81b9c2
Andrei Neagu <neagu@itis.swiss>
2021-09-29 14:07:54 +0200

    refactor

--- services/director-v2/tests/integration/01/test_computation_api.py
+++ services/director-v2/tests/integration/01/test_computation_api.py
@@ -30,8 +30,6 @@ from shared_comp_utils import (
     assert_pipeline_status,
     create_pipeline,
 )
-from simcore_postgres_database.models.comp_tasks import comp_tasks
-from simcore_postgres_database.models.projects import projects
 from simcore_service_director_v2.models.schemas.comp_tasks import ComputationTaskOut
 from starlette import status
 from starlette.testclient import TestClient
@@ -87,34 +85,6 @@ def minimal_configuration(
     simcore_services: Dict[str, URL],
 ) -> None:
     pass
-
-
-@pytest.fixture
-def update_project_workbench_with_comp_tasks(postgres_db: sa.engine.Engine) -> Callable:
-    def updator(project_uuid: str):
-        with postgres_db.connect() as con:
-            result = con.execute(
-                projects.select().where(projects.c.uuid == project_uuid)
-            )
-            prj_row = result.first()
-            prj_workbench = prj_row.workbench
-
-            result = con.execute(
-                comp_tasks.select().where(comp_tasks.c.project_id == project_uuid)
-            )
-            # let's get the results and run_hash
-            for task_row in result:
-                # pass these to the project workbench
-                prj_workbench[task_row.node_id]["outputs"] = task_row.outputs
-                prj_workbench[task_row.node_id]["runHash"] = task_row.run_hash
-
-            con.execute(
-                projects.update()
-                .values(workbench=prj_workbench)
-                .where(projects.c.uuid == project_uuid)
-            )
-
-    yield updator
 
 
 @pytest.fixture(scope="session")
