9d7232a1fe1c087679b214c5bf1654d1c92e6e2c
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-09-29 11:37:49 +0200

    adds smpale of functional service [skip ci]

--- services/web/server/src/simcore_service_webserver/meta_core.py
+++ services/web/server/src/simcore_service_webserver/meta_core.py
@@ -1,17 +1,106 @@
-# itag: iteration tag
-from dataclasses import dataclass
-from functools import cached_property
-from typing import Callable, Iterator
-from uuid import UUID
+import inspect
+from typing import Any, Callable, Optional, Type
 
-from models_library.frontend_services_catalog import (
-    _create_data_iterator_int_range,
-    is_iterator_consumer_service,
-    is_iterator_service,
-)
-from models_library.services import ServiceDockerData, ServiceInputs
-from pydantic import create_model
-from pydantic.types import PositiveInt
+from models_library.frontend_services_catalog import FRONTEND_SERVICE_KEY_PREFIX
+from pydantic.fields import Field
+from pydantic.main import BaseModel
+
+PROPTYPE_2_PYTYPE = {"number": float, "boolean": bool, "integer": int, "string": str}
+PYTYPE_2_PROPTYPE = {v: k for k, v in PROPTYPE_2_PYTYPE.items()}
+
+
+class Info(BaseModel):
+    key: str
+    version: str
+    name: str
+    description: Optional[str] = None
+
+
+class FunctionalMixin:
+    Inputs: Type[BaseModel]
+    Outputs: Type[BaseModel]
+    _impl: Callable
+
+    @classmethod
+    def run(cls, **kwargs):
+        assert cls.Inputs
+        assert cls._impl
+        assert cls.Outputs
+
+        _inputs = cls.Inputs.parse_obj(kwargs)
+        return cls.run_with_model(_inputs)
+
+    @classmethod
+    def collect_outputs(cls, returned: Any):
+        if not isinstance(returned, tuple):
+            returned = (returned,)
+        obj = dict(zip(cls.Outputs.__fields__.keys(), returned))
+        return cls.Outputs.parse_obj(obj)
+
+    @classmethod
+    def run_with_model(cls, inputs: BaseModel):
+        _returned = cls._impl(**inputs.dict())
+        outputs = cls.collect_outputs(_returned)
+        return inputs, outputs
+
+
+# Concrete Functional Services -------
+class SumDiffDef(FunctionalMixin):
+    info = Info(
+        name="sum-diff",
+        key=f"{FRONTEND_SERVICE_KEY_PREFIX}/def/sum-diff",
+        version="1.0.0",
+        description="Sum and different of two numbers",
+    )
+
+    # NOTE: INputs/outputs should be easily parsed from signatures
+    class Inputs(BaseModel):
+        x: float
+        y: float = 3
+
+    class Outputs(BaseModel):
+        sum: float = Field(..., description="Sum of all inputs")
+        diff: float = Field(..., description="Difference between inputs")
+
+    @staticmethod
+    def _impl(x, y):
+        return x + y, x - y
+
+
+class SumDiffData(BaseModel):
+    info: Info = SumDiffDef.info
+    inputs: SumDiffDef.Inputs
+    outputs: Optional[SumDiffDef.Outputs] = None
+
+    @classmethod
+    def from_io(cls, inputs, outputs) -> "SumDiffData":
+        return cls(inputs=inputs, outputs=outputs)
+
+
+##############################################
+
+
+def extract_ios(fun: Callable):
+    # TODO:
+    sig = inspect.signature(fun)
+
+    # param -> field
+    fields = []
+    for param in sig.parameters.values():
+        if param.kind == param.POSITIONAL_ONLY:
+            raise ValueError(f"{param.name}: positional only not allowed")
+
+        if param.annotation == param.empty:
+            raise ValueError(f"{param.name} was not annotated")
+
+        pydantic_type = param.annotation
+
+        field_args = {}
+        if param.default != param.empty:
+            field_args["default"] = param.default
+
+    # if not issubclass(sig.return_annotation, tuple):
+
 
 # /projects/{project_uuid}/workbench:start
 
@@ -34,25 +123,3 @@ from pydantic.types import PositiveInt
 
 # convert ServiceDockerData into functions??
 #
-def _linspace(
-    linspace_start: int = 0, linspace_stop: int = 1, linspace_step: int = 1
-) -> Iterator[int]:
-    # TODO: horrible argument names
-
-    for value in range(linspace_start, linspace_stop, linspace_step):
-        yield value
-
-
-#######################
-
-PROPTYPE_2_PYTYPE = {"number": float, "boolean": bool, "integer": int, "string": str}
-PYTYPE_2_PROPTYPE = {v: k for k, v in PROPTYPE_2_PYTYPE}
-
-
-def to_pydantic_model(service_inputs: ServiceInputs):
-    # input_defs to pydantic model, then they can be parsed
-
-    for name, input_def in service_inputs.items():
-        input_def.property_type
-        input_def.default_value
-        input_def.description
