20cde41c752fa4a8bcb8c878393193413f7dbf40
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-09-29 21:46:33 +0200

    typo

--- services/api-server/tests/unit/test_core_settings.py
+++ services/api-server/tests/unit/test_core_settings.py
@@ -18,7 +18,7 @@ def test_min_environ_for_settings(project_env_devel_environment, monkeypatch):
     print("captured settings: \n", settings.json(indent=2))
 
     assert settings.SC_BOOT_MODE == BootModeEnum.PRODUCTION
-    assert settings.loglevel == logging.DEBUG
+    assert settings.log_level == logging.DEBUG
 
     assert URL(settings.API_SERVER_POSTGRES.dsn) == URL(
         "postgresql://test:test@127.0.0.1:5432/test"
