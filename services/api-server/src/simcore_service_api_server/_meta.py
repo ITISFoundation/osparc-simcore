3fc5855e1bc426e930040b64b6ebcd99cf87c799
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2021-09-30 23:06:41 +0200

    ♻️ update settings api server (#2563)
    - ♻️ refactors api-server to use settings-library (based on pattern presented in http://github.com/pcrespov/sample-app-settings)
    - ♻️ formatted meta constants to uppercase in both api-server and director-v2
    - ✨adds CLI command to resolve and print envfiles
    - 🐛 Added a retry policy to retrieve index from static web server since i noticed that unhandled exception shut down the web-server in one of the tests
    
    
    * Add settings-library to requirements
    
    * directorv2: formatted all constants in meta in upper-case
    
    * api-server: formatted all constants in meta in upper-case
    
    * fixes reqs
    
    * api-server changed to new settings
    
    * add cli
    
    * missing setting
    
    * FIX: disable scicrunch unreliable test
    
    * FIX: Retries connection web->static services

--- services/api-server/src/simcore_service_api_server/_meta.py
+++ services/api-server/src/simcore_service_api_server/_meta.py
@@ -5,26 +5,26 @@ from contextlib import suppress
 
 import pkg_resources
 
-current_distribution = pkg_resources.get_distribution("simcore_service_api_server")
+_current_distribution = pkg_resources.get_distribution("simcore_service_api_server")
 
-project_name: str = current_distribution.project_name
+PROJECT_NAME: str = _current_distribution.project_name
 
-api_version: str = current_distribution.version
-major, minor, patch = current_distribution.version.split(".")
-api_vtag: str = f"v{major}"
+API_VERSION: str = _current_distribution.version
+MAJOR, MINOR, PATCH = _current_distribution.version.split(".")
+API_VTAG: str = f"v{MAJOR}"
 
-__version__ = current_distribution.version
+__version__ = _current_distribution.version
 
 
 def get_summary() -> str:
     with suppress(Exception):
         try:
-            metadata = current_distribution.get_metadata_lines("METADATA")
+            metadata = _current_distribution.get_metadata_lines("METADATA")
         except FileNotFoundError:
-            metadata = current_distribution.get_metadata_lines("PKG-INFO")
+            metadata = _current_distribution.get_metadata_lines("PKG-INFO")
 
         return next(x.split(":") for x in metadata if x.startswith("Summary:"))[-1]
     return ""
 
 
-summary: str = get_summary()
+SUMMARY: str = get_summary()
