7789141c058cf4d61ed38787bcead5a41b2c7791
Andrei Neagu <neagu@itis.swiss>
2021-10-01 08:28:49 +0200

    mocked api removed, no requests are fowarded here

--- services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/api/_routing.py
+++ services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/api/_routing.py
@@ -5,12 +5,10 @@ from fastapi import APIRouter
 from .._meta import api_vtag
 from .containers import containers_router
 from .health import health_router
-from .mocked import mocked_router
 
 # setup and register all routes here form different modules
 main_router = APIRouter()
 main_router.include_router(health_router)
 main_router.include_router(containers_router, prefix=f"/{api_vtag}")
-main_router.include_router(mocked_router)
 
 __all__ = ["main_router"]
