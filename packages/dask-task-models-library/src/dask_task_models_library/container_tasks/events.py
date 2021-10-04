088d7bee98e403e9626aa2671a9072bb51999b4b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-10-04 15:14:18 +0200

    added TaskProgressEvent

--- packages/dask-task-models-library/src/dask_task_models_library/container_tasks/events.py
+++ packages/dask-task-models-library/src/dask_task_models_library/container_tasks/events.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from distributed.worker import get_worker
 from models_library.projects_state import RunningState
-from pydantic import BaseModel, Extra
+from pydantic import BaseModel, Extra, NonNegativeFloat
 
 
 class TaskEvent(BaseModel, ABC):
@@ -43,6 +43,32 @@ class TaskStateEvent(TaskEvent):
                     "job_id": "simcore/services/comp/sleeper:1.1.0:projectid_ec7e595a-63ee-46a1-a04a-901b11b649f8:nodeid_39467d89-b659-4914-9359-c40b1b6d1d6d:uuid_5ee5c655-450d-4711-a3ec-32ffe16bc580",
                     "msg": "some unexpected error happened",
                     "state": RunningState.FAILED.value,
+                },
+            ]
+        }
+
+
+class TaskProgressEvent(TaskEvent):
+    progress: NonNegativeFloat
+
+    @staticmethod
+    def topic_name() -> str:
+        return "task_progress"
+
+    @classmethod
+    def from_dask_worker(cls, progress: float) -> "TaskProgressEvent":
+        return cls(job_id=get_worker().get_current_task(), progress=progress)
+
+    class Config(TaskEvent.Config):
+        schema_extra = {
+            "examples": [
+                {
+                    "job_id": "simcore/services/comp/sleeper:1.1.0:projectid_ec7e595a-63ee-46a1-a04a-901b11b649f8:nodeid_39467d89-b659-4914-9359-c40b1b6d1d6d:uuid_5ee5c655-450d-4711-a3ec-32ffe16bc580",
+                    "progress": 0,
+                },
+                {
+                    "job_id": "simcore/services/comp/sleeper:1.1.0:projectid_ec7e595a-63ee-46a1-a04a-901b11b649f8:nodeid_39467d89-b659-4914-9359-c40b1b6d1d6d:uuid_5ee5c655-450d-4711-a3ec-32ffe16bc580",
+                    "progress": 1.0,
                 },
             ]
         }
