526cd97e5dfaf2000c51c60afcb13a4d37bfee6e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-10-04 07:58:53 +0200

    mypy

--- services/dask-sidecar/src/simcore_service_dask_sidecar/dask_utils.py
+++ services/dask-sidecar/src/simcore_service_dask_sidecar/dask_utils.py
@@ -2,7 +2,7 @@ from dask_task_models_library.container_tasks.events import TaskEvent
 from distributed.worker import get_client
 
 
-def publish_event(event: TaskEvent):
+def publish_event(event: TaskEvent) -> None:
     get_client().log_event(
         event.topic_name(),
         event.json(),
