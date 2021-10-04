b0342a5a7941bdd95e324e20b685dad3735b6ec4
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2021-10-04 11:31:33 +0200

    Nodes Tree UI/UX: Show/hide action buttons (#2565)

--- services/web/client/source/class/osparc/component/widget/NodesTree.js
+++ services/web/client/source/class/osparc/component/widget/NodesTree.js
@@ -43,7 +43,6 @@ qx.Class.define("osparc.component.widget.NodesTree", {
 
     this._setLayout(new qx.ui.layout.VBox());
 
-    this.__toolBar = this._createChildControlImpl("toolbar");
     this.__tree = this._createChildControlImpl("tree");
 
     this.__attachEventHandlers();
@@ -85,22 +84,12 @@ qx.Class.define("osparc.component.widget.NodesTree", {
   },
 
   members: {
-    __toolBar: null,
     __tree: null,
-    __exportButton: null,
-    __openButton: null,
-    __renameButton: null,
-    __deleteButton: null,
     __currentNodeId: null,
-    __toolbarInitMinWidth: null,
 
     _createChildControlImpl: function(id) {
       let control;
       switch (id) {
-        case "toolbar":
-          control = this.__buildToolbar();
-          this._add(control);
-          break;
         case "tree":
           control = this.__buildTree();
           this._add(control, {
@@ -118,66 +107,6 @@ qx.Class.define("osparc.component.widget.NodesTree", {
 
     setCurrentNodeId: function(nodeId) {
       this.__currentNodeId = nodeId;
-    },
-
-    __getToolbarButtons: function(toolbar) {
-      const toolBarChildren = toolbar.getChildren();
-      return toolBarChildren.filter(toolBarChild => toolBarChild instanceof qx.ui.toolbar.Button);
-    },
-
-    __buildToolbar: function() {
-      const iconSize = 14;
-      const toolbar = this.__toolBar = new qx.ui.toolbar.ToolBar();
-
-      toolbar.addSpacer();
-
-      if (osparc.data.Permissions.getInstance().canDo("study.node.export")) {
-        const exportButton = this.__exportButton = new qx.ui.toolbar.Button(this.tr("Export"), "@FontAwesome5Solid/share/"+iconSize);
-        exportButton.addListener("execute", () => {
-          this.__exportDAG();
-        }, this);
-        osparc.utils.Utils.setIdToWidget(exportButton, "exportServicesBtn");
-        toolbar.add(exportButton);
-      }
-
-      const openButton = this.__openButton = new qx.ui.toolbar.Button(this.tr("Open"), "@FontAwesome5Solid/edit/"+iconSize);
-      openButton.addListener("execute", e => {
-        const selectedItem = this.__getSelection();
-        if (selectedItem) {
-          const nodeId = selectedItem ? selectedItem.getNodeId() : this.getStudy().getUuid();
-          this.__openItem(nodeId);
-        }
-      }, this);
-      osparc.utils.Utils.setIdToWidget(openButton, "openServiceBtn");
-      toolbar.add(openButton);
-
-      const renameButton = this.__renameButton = new qx.ui.toolbar.Button(this.tr("Rename"), "@FontAwesome5Solid/i-cursor/"+iconSize);
-      renameButton.addListener("execute", e => {
-        this.__openItemRenamer();
-      }, this);
-      osparc.utils.Utils.setIdToWidget(renameButton, "renameServiceBtn");
-      toolbar.add(renameButton);
-
-      const deleteButton = this.__deleteButton = new qx.ui.toolbar.Button(this.tr("Delete"), "@FontAwesome5Solid/trash/"+iconSize).set({
-        enabled: false
-      });
-      deleteButton.addListener("execute", e => {
-        this.__deleteNode();
-      }, this);
-      osparc.utils.Utils.setIdToWidget(deleteButton, "deleteServiceBtn");
-      toolbar.add(deleteButton);
-
-      const toolBarBtns = this.__getToolbarButtons(toolbar);
-      let btnsWidth = 11;
-      toolBarBtns.forEach(toolBarBtn => {
-        const pad = 5;
-        const spa = 10;
-        const width = toolBarBtn.getSizeHint().width + pad + spa;
-        btnsWidth += width;
-      });
-      this.__toolbarInitMinWidth = btnsWidth;
-
-      return toolbar;
     },
 
     __getOneSelectedRow: function() {
@@ -214,7 +143,17 @@ qx.Class.define("osparc.component.widget.NodesTree", {
         study.bind("name", newModel, "label");
         this.__tree.setModel(newModel);
         this.__tree.setDelegate({
-          createItem: () => new osparc.component.widget.NodeTreeItem(),
+          createItem: () => {
+            const nodeTreeItem = new osparc.component.widget.NodeTreeItem(study);
+            nodeTreeItem.addListener("openNode", e => this.__openItem(e.getData()));
+            nodeTreeItem.addListener("renameNode", e => this.__openItemRenamer(e.getData()));
+            nodeTreeItem.addListener("deleteNode", e => this.__deleteNode(e.getData()));
+            const readOnly = study.isReadOnly();
+            // disable rename and delete button if the study is read only
+            nodeTreeItem.getChildControl("rename-btn").setEnabled(!readOnly);
+            nodeTreeItem.getChildControl("delete-btn").setEnabled(!readOnly);
+            return nodeTreeItem;
+          },
           bindItem: (c, item, id) => {
             c.bindDefaultProperties(item, id);
             c.bindProperty("nodeId", "nodeId", null, item, id);
@@ -261,6 +200,11 @@ qx.Class.define("osparc.component.widget.NodesTree", {
       return selectedItem;
     },
 
+    __selectedItem: function(item) {
+      const nodeId = item.getModel().getNodeId();
+      this.fireDataEvent("changeSelectedNode", nodeId);
+    },
+
     __exportDAG: function() {
       const selectedItem = this.__getSelection();
       if (selectedItem) {
@@ -279,29 +223,24 @@ qx.Class.define("osparc.component.widget.NodesTree", {
       }
     },
 
-    __selectedItem: function(item) {
-      const nodeId = item.getModel().getNodeId();
-      this.fireDataEvent("changeSelectedNode", nodeId);
-    },
-
-    __openItemRenamer: function() {
-      const selectedItem = this.__getSelection();
-      if (selectedItem) {
-        const treeItemRenamer = new osparc.component.widget.Renamer(selectedItem.getLabel());
+    __openItemRenamer: function(nodeId) {
+      const renameItem = nodeId === undefined ? this.__getSelection() : this.__getNodeInTree(this.__tree.getModel(), nodeId);
+      if (renameItem) {
+        const treeItemRenamer = new osparc.component.widget.Renamer(renameItem.getLabel());
         treeItemRenamer.addListener("labelChanged", e => {
           const {
             newLabel
           } = e.getData();
-          const nodeId = selectedItem.getNodeId();
+          const selectedNodeId = renameItem.getNodeId();
           const study = this.getStudy();
-          if (nodeId === study.getUuid() && osparc.data.Permissions.getInstance().canDo("study.update", true)) {
+          if (selectedNodeId === study.getUuid() && osparc.data.Permissions.getInstance().canDo("study.update", true)) {
             const params = {
               name: newLabel
             };
             study.updateStudy(params);
           } else if (osparc.data.Permissions.getInstance().canDo("study.node.rename", true)) {
-            selectedItem.setLabel(newLabel);
-            const node = study.getWorkbench().getNode(nodeId);
+            renameItem.setLabel(newLabel);
+            const node = study.getWorkbench().getNode(selectedNodeId);
             if (node) {
               node.renameNode(newLabel);
             }
@@ -314,12 +253,16 @@ qx.Class.define("osparc.component.widget.NodesTree", {
       }
     },
 
-    __deleteNode: function() {
-      const selectedItem = this.__getSelection();
-      if (selectedItem === null) {
-        return;
+    __deleteNode: function(nodeId) {
+      if (nodeId === undefined) {
+        const selectedItem = this.__getSelection();
+        if (selectedItem === null) {
+          return;
+        }
+        this.fireDataEvent("removeNode", selectedItem.getNodeId());
+      } else {
+        this.fireDataEvent("removeNode", nodeId);
       }
-      this.fireDataEvent("removeNode", selectedItem.getNodeId());
     },
 
     nodeSelected: function(nodeId) {
@@ -328,25 +271,6 @@ qx.Class.define("osparc.component.widget.NodesTree", {
       if (item) {
         this.__tree.openNodeAndParents(item);
         this.__tree.setSelection(new qx.data.Array([item]));
-
-        this.__updateButtons(nodeId, item);
-      }
-    },
-
-    __updateButtons: function(nodeId, item) {
-      const studyId = this.getStudy().getUuid();
-      const readOnly = this.getStudy().isReadOnly();
-      if (this.__exportButton) {
-        this.__exportButton.setEnabled(studyId !== nodeId && item.getIsContainer());
-      }
-      if (this.__openButton) {
-        this.__openButton.setEnabled(this.__currentNodeId !== nodeId);
-      }
-      if (this.__renameButton) {
-        this.__renameButton.setEnabled(!readOnly);
-      }
-      if (this.__deleteButton) {
-        this.__deleteButton.setEnabled(!readOnly && studyId !== nodeId && this.__currentNodeId !== nodeId);
       }
     },
 
@@ -360,24 +284,6 @@ qx.Class.define("osparc.component.widget.NodesTree", {
       this.addListener("keypress", function(keyEvent) {
         if (keyEvent.getKeyIdentifier() === "F2") {
           this.__openItemRenamer();
-        }
-      }, this);
-
-      this.__toolBar.addListener("resize", () => {
-        const toolBarBtns = this.__getToolbarButtons(this.__toolBar);
-        if (this.__toolBar.getBounds().width < this.__toolbarInitMinWidth) {
-          // Hide Label
-          toolBarBtns.forEach(toolBarBtn => {
-            const label = toolBarBtn.getChildControl("label");
-            label.exclude();
-            toolBarBtn.setToolTipText(label.getValue());
-          });
-        } else {
-          // Show Label
-          toolBarBtns.forEach(toolBarBtn => {
-            toolBarBtn.getChildControl("label").show();
-            toolBarBtn.setToolTipText(null);
-          });
         }
       }, this);
     }
