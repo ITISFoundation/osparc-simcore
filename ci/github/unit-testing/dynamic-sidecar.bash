271655a67f7a758e5337f2b63d396dab7cd7996c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-10-04 10:29:26 +0200

    fix calling codestyle

--- ci/github/unit-testing/dynamic-sidecar.bash
+++ ci/github/unit-testing/dynamic-sidecar.bash
@@ -1,29 +1,32 @@
 #!/bin/bash
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -o errexit   # abort on nonzero exitstatus
-set -o nounset   # abort on unbound variable
-set -o pipefail  # don't hide errors within pipes
+set -o errexit  # abort on nonzero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
 IFS=$'\n\t'
 
 install() {
-    bash ci/helpers/ensure_python_pip.bash;
-    pushd services/dynamic-sidecar; pip3 install -r requirements/ci.txt -r requirements/_tools.txt; popd;
-    pip list -v
+  bash ci/helpers/ensure_python_pip.bash
+  pushd services/dynamic-sidecar
+  pip3 install -r requirements/ci.txt -r requirements/_tools.txt
+  popd
+  pip list -v
 }
 
-codestyle(){
-    pushd services/dynamic-sidecar; make codestyle ci simcore_service_dynamic_sidecar; popd;
+codestyle() {
+  pushd services/dynamic-sidecar
+  make codestyle-ci
+  popd
 }
 
 test() {
-    pytest --cov=simcore_service_dynamic_sidecar --durations=10 --cov-append \
-          --color=yes --cov-report=term-missing --cov-report=xml --cov-config=.coveragerc \
-          -v -m "not travis" services/dynamic-sidecar/tests/unit
+  pytest --cov=simcore_service_dynamic_sidecar --durations=10 --cov-append \
+    --color=yes --cov-report=term-missing --cov-report=xml --cov-config=.coveragerc \
+    -v -m "not travis" services/dynamic-sidecar/tests/unit
 }
 
 # Check if the function exists (bash specific)
-if declare -f "$1" > /dev/null
-then
+if declare -f "$1" >/dev/null; then
   # call arguments verbatim
   "$@"
 else
