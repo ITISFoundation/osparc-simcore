40abbf41a3e2c4bdd6fecd66bc285519bb63ff65
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-10-05 22:12:02 +0200

    Adds utils to parse/compose identifiers

---
+++ services/web/server/src/simcore_service_webserver/version_control_tags.py
@@ -0,0 +1,23 @@
+import re
+from typing import Any, Dict
+from uuid import UUID, uuid3
+
+from models_library.basic_regex import UUID_RE
+from models_library.projects import ProjectID
+
+from .version_control_models import CommitID
+
+
+def compose_wcopy_project_id(repo_project_uuid: ProjectID, commit_id: CommitID) -> UUID:
+    """Produces a unique identifier for a working copy of a given repo_project_uuid@commit_id"""
+    return uuid3(repo_project_uuid, f"{commit_id}")
+
+
+def compose_wcopy_project_tag_name(wcopy_project_id: UUID) -> str:
+    return f"project:{wcopy_project_id}"
+
+
+def parse_wcopy_project_tag_name(name: str) -> Dict[str, Any]:
+    if m := re.match(rf"^project:(?P<wcopy_project_id>{UUID_RE})$", name):
+        return m.groupdict()
+    return {}
