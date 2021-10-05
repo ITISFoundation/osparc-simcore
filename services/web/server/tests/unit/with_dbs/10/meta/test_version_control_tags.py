b25ba1b4be79fd752e9fec53f523100f375b5f2e
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-10-05 22:19:54 +0200

    adds tests for vc_tags

---
+++ services/web/server/tests/unit/with_dbs/10/meta/test_version_control_tags.py
@@ -0,0 +1,18 @@
+from faker import Faker
+from simcore_service_webserver.version_control_tags import (
+    compose_wcopy_project_id,
+    compose_wcopy_project_tag_name,
+    parse_wcopy_project_tag_name,
+)
+
+
+def test_parse_and_compose_tag_names(faker: Faker):
+
+    wcopy_project_id = compose_wcopy_project_id(
+        repo_project_uuid=faker.uuid4(), commit_id=faker.pyint()
+    )
+
+    tag = compose_wcopy_project_tag_name(wcopy_project_id)
+    assert parse_wcopy_project_tag_name(tag)["wcopy_project_id"] == str(
+        wcopy_project_id
+    )
