23b047754ad86a0179078fd814a89e1bf1025d40
odeimaiz <maiz@itis.swiss>
2021-10-06 13:41:23 +0200

    snapshot buttons added to StudyMenu

--- services/web/client/source/class/osparc/navigation/StudyMenu.js
+++ services/web/client/source/class/osparc/navigation/StudyMenu.js
@@ -33,7 +33,9 @@ qx.Class.define("osparc.navigation.StudyMenu", {
     "slidesGuidedStart": "qx.event.type.Event",
     "slidesAppStart": "qx.event.type.Event",
     "slidesStop": "qx.event.type.Event",
-    "slidesEdit": "qx.event.type.Event"
+    "slidesEdit": "qx.event.type.Event",
+    "takeSnapshot": "qx.event.type.Event",
+    "showSnapshots": "qx.event.type.Event"
   },
 
   properties: {
@@ -49,6 +51,8 @@ qx.Class.define("osparc.navigation.StudyMenu", {
     __startAppButton: null,
     __editSlidesButton: null,
     __stopSlidesButton: null,
+    __takeSnapshotButton: null,
+    __showSnapshotsButton: null,
 
     __populateMenu: function() {
       const studyButtonMenu = new qx.ui.menu.Menu();
@@ -75,17 +79,30 @@ qx.Class.define("osparc.navigation.StudyMenu", {
       const stopSlidesBtn = this.__stopSlidesButton = new qx.ui.menu.Button(this.tr("Stop Slideshow"));
       stopSlidesBtn.addListener("execute", () => this.fireEvent("slidesStop"), this);
       studyButtonMenu.add(stopSlidesBtn);
+
+      studyButtonMenu.addSeparator();
+
+      const takeSnapshotBtn = this.__takeSnapshotButton = new qx.ui.menu.Button(this.tr("Take Snapshot"));
+      takeSnapshotBtn.addListener("execute", () => this.fireEvent("takeSnapshot"), this);
+      studyButtonMenu.add(takeSnapshotBtn);
+
+      const showSnapshotsBtn = this.__showSnapshotsButton = new qx.ui.menu.Button(this.tr("Show Snapshots"));
+      showSnapshotsBtn.addListener("execute", () => this.fireEvent("showSnapshots"), this);
+      studyButtonMenu.add(showSnapshotsBtn);
     },
 
     _applyStudy: function(study) {
       if (study) {
-        study.getWorkbench().addListener("pipelineChanged", () => this.evalButtonsVisibiilty());
-        study.getUi().getSlideshow().addListener("changeSlideshow", () => this.evalButtonsVisibiilty());
-        this.evalButtonsVisibiilty();
+        study.getWorkbench().addListener("pipelineChanged", () => this.evalSlidesButtons());
+        study.getUi().getSlideshow().addListener("changeSlideshow", () => this.evalSlidesButtons());
+        this.evalSlidesButtons();
+
+        study.getWorkbench().addListener("nNodesChanged", this.evalSnapshotsButtons());
+        this.evalSnapshotsButtons();
       }
     },
 
-    evalButtonsVisibiilty: function(editorContext) {
+    evalSlidesButtons: function(editorContext) {
       const slidesBtnsVisible = ["workbench", "guided", "app"].includes(editorContext);
       if (slidesBtnsVisible) {
         const study = this.getStudy();
@@ -104,6 +121,16 @@ qx.Class.define("osparc.navigation.StudyMenu", {
         }
       } else {
         this.__stopSlidesButton.setEnabled(false);
+      }
+    },
+
+    evalSnapshotsButtons: async function() {
+      const study = this.getStudy();
+      if (study) {
+        this.__takeSnapshotButton.setEnabled(osparc.data.Permissions.getInstance().canDo("study.snapshot.create"));
+
+        const hasSnapshots = await study.hasSnapshots();
+        this.__showSnapshotsButton.setEnabled(hasSnapshots);
       }
     }
   }
