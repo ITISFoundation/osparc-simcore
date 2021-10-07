609289eb98e9cd3642f10e416916c87cf0ffc19f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-10-07 10:58:09 +0200

    fixes tests

--- services/web/server/src/simcore_service_webserver/version_control_tags.py
+++ services/web/server/src/simcore_service_webserver/version_control_tags.py
@@ -1,5 +1,5 @@
 import re
-from typing import Any, Dict
+from typing import Any, Dict, Union
 from uuid import UUID, uuid3
 
 from models_library.basic_regex import UUID_RE
@@ -8,12 +8,16 @@ from models_library.projects import ProjectID
 from .version_control_models import CommitID
 
 
-def compose_wcopy_project_id(repo_project_uuid: ProjectID, commit_id: CommitID) -> UUID:
+def compose_wcopy_project_id(
+    repo_project_uuid: Union[ProjectID, str], commit_id: CommitID
+) -> UUID:
     """Produces a unique identifier for a working copy of a given repo_project_uuid@commit_id"""
+    if isinstance(repo_project_uuid, str):
+        repo_project_uuid = UUID(repo_project_uuid)
     return uuid3(repo_project_uuid, f"{commit_id}")
 
 
-def compose_wcopy_project_tag_name(wcopy_project_id: UUID) -> str:
+def compose_wcopy_project_tag_name(wcopy_project_id: ProjectID) -> str:
     return f"project:{wcopy_project_id}"
 
 
--- services/web/server/tests/unit/with_dbs/10/meta/test_version_control_tags.py
+++ services/web/server/tests/unit/with_dbs/10/meta/test_version_control_tags.py
@@ -9,7 +9,7 @@ from simcore_service_webserver.version_control_tags import (
 def test_parse_and_compose_tag_names(faker: Faker):
 
     wcopy_project_id = compose_wcopy_project_id(
-        repo_project_uuid=faker.uuid4(), commit_id=faker.pyint()
+        repo_project_uuid=faker.uuid4(cast_to=None), commit_id=faker.pyint()
     )
 
     tag = compose_wcopy_project_tag_name(wcopy_project_id)
