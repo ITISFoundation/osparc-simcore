0207497202efcdcc22e159c5e5d00d5d3be46e50
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-10-08 16:06:22 +0200

    temp print os environ

--- services/catalog/tests/unit/with_dbs/test_entrypoint_dags.py
+++ services/catalog/tests/unit/with_dbs/test_entrypoint_dags.py
@@ -22,6 +22,9 @@ pytest_simcore_ops_services_selection = [
 def test_read_healthcheck(
     director_mockup: MockRouter, app: FastAPI, client: TestClient
 ):
+    import os
+
+    print(os.environ)
     response = client.get("/")
     assert response.status_code == 200
     assert response.text == '":-)"'
