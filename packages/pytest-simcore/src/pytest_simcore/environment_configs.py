b94556c727e2da8fbc601a422e3b362c32cdfe4a
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-10-08 18:13:28 +0200

    adding type annotations

--- packages/pytest-simcore/src/pytest_simcore/environment_configs.py
+++ packages/pytest-simcore/src/pytest_simcore/environment_configs.py
@@ -5,14 +5,15 @@
 
 from copy import deepcopy
 from pathlib import Path
-from typing import Dict, Union
+from typing import Dict, Optional
 
 import dotenv
 import pytest
+from _pytest.monkeypatch import MonkeyPatch
 
 
 @pytest.fixture(scope="session")
-def env_devel_dict(env_devel_file: Path) -> Dict[str, Union[str, None]]:
+def env_devel_dict(env_devel_file: Path) -> Dict[str, Optional[str]]:
     assert env_devel_file.exists()
     assert env_devel_file.name == ".env-devel"
     environ = dotenv.dotenv_values(env_devel_file, verbose=True, interpolate=True)
@@ -20,7 +21,10 @@ def env_devel_dict(env_devel_file: Path) -> Dict[str, Union[str, None]]:
 
 
 @pytest.fixture(scope="function")
-def mock_env_devel_environment(env_devel_dict, monkeypatch):
+def mock_env_devel_environment(
+    env_devel_dict: Dict[str, Optional[str]], monkeypatch: MonkeyPatch
+) -> Dict[str, Optional[str]]:
     for key, value in env_devel_dict.items():
-        monkeypatch.setenv(key, value)
+        if value:
+            monkeypatch.setenv(key, value)
     return deepcopy(env_devel_dict)
