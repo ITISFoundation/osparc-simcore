0e9e69b7ae22b6ea3671a50c79e757ab34580106
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-10-13 20:30:27 +0200

    adds in web-server

--- services/web/server/tests/unit/isolated/test__package.py
+++ services/web/server/tests/unit/isolated/test__package.py
@@ -1,6 +1,9 @@
-# pylint:disable=unused-variable
-# pylint:disable=unused-argument
-# pylint:disable=redefined-outer-name
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+
+from pathlib import Path
+
 import pytest
 from pytest_simcore.helpers.utils_pylint import (
     assert_no_pdb_in_code,
@@ -9,17 +12,17 @@ from pytest_simcore.helpers.utils_pylint import (
 from simcore_service_webserver.cli import main
 
 
-def test_run_pylint(pylintrc, package_dir):
+def test_run_pylint(pylintrc: Path, package_dir: Path):
     # fixtures in pytest_simcore.environs
     assert_pylint_is_passing(pylintrc=pylintrc, package_dir=package_dir)
 
 
-def test_no_pdbs_in_place(package_dir):
+def test_no_pdbs_in_place(package_dir: Path):
     # fixtures in pytest_simcore.environs
     assert_no_pdb_in_code(code_dir=package_dir)
 
 
-def test_main(here):  # pylint: disable=unused-variable
+def test_main(here):
     with pytest.raises(SystemExit) as excinfo:
         main("--help".split())
 
