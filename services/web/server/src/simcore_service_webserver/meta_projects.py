388752f62d6a757a5a68fcea5c9ec974d148368c
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-10-15 11:26:35 +0200

    redirects and returns repo uuid

--- services/web/server/src/simcore_service_webserver/meta_projects.py
+++ services/web/server/src/simcore_service_webserver/meta_projects.py
@@ -7,16 +7,21 @@
 #
 
 
+import json
 import logging
-from typing import List, Tuple
+import re
+from typing import Callable, Dict, List, Optional, Tuple
 
 from aiohttp import web
 from aiohttp.web_middlewares import _Handler
+from models_library.basic_regex import UUID_RE
 from models_library.projects import ProjectID
+from servicelib.json_serialization import json_dumps
 
 from ._meta import api_version_prefix as VTAG
 from .director_v2_api import AbstractProjectRunPolicy
 from .meta_iterations import get_or_create_runnable_projects, get_runnable_projects_ids
+from .projects.projects_handlers import RQ_REQUESTED_REPO_PROJECT_UUID_KEY
 from .version_control_db import VersionControlRepositoryInternalAPI
 from .version_control_models import CommitID
 
@@ -27,44 +32,38 @@ log = logging.getLogger(__name__)
 #
 #  https://github.com/ITISFoundation/osparc-simcore/blob/master/services/web/server/src/simcore_service_webserver/api/v0/openapi.yaml#L8563
 #
-PROJECTS_API_PREFIX = f"/{VTAG}/projects/"
+URL_PATTERN = re.compile(rf"^\/{VTAG}\/projects\/({UUID_RE})[\/.]{1}")
 
 
 @web.middleware
 async def projects_redirection_middleware(request: web.Request, handler: _Handler):
     """Intercepts /project/{project_id}* requests and redirect them to the copy @HEAD"""
-    if str(request.rel_url).startswith(PROJECTS_API_PREFIX):
 
-        try:
-            project_id = ProjectID(request.match_info["project_id"])
+    if match := URL_PATTERN.match(str(request.rel_url)):
+        assert match.group(1) == request.match_info["project_id"]  # nosec
 
-            # find current working copy
-            vc_repo = VersionControlRepositoryInternalAPI(request)
+        project_id = ProjectID(request.match_info["project_id"])
 
+        vc_repo = VersionControlRepositoryInternalAPI(request)
+        if repo_id := await vc_repo.get_repo_id(project_id):
+            # Changes resolved project_id parameter with working copy instead
+            #
             # TODO: optimize db calls
-            if repo_id := await vc_repo.get_repo_id(project_id):
-                #
-                # Changes resolved project_id parameter with working copy instead
-                #
-                wcopy_project_id = await vc_repo.get_wcopy_project_id(repo_id)
-                request.match_info["project_id"] = str(wcopy_project_id)
+            wcopy_project_id = await vc_repo.get_wcopy_project_id(repo_id)
+            request.match_info["project_id"] = str(wcopy_project_id)
 
+            if wcopy_project_id != project_id:
+                request[RQ_REQUESTED_REPO_PROJECT_UUID_KEY] = wcopy_project_id
                 log.debug(
                     "Redirecting project '%s' to working copy '%s'",
                     project_id,
                     wcopy_project_id,
                 )
-        except KeyError as err:
-            log.debug("Skips redirection of %s: %s", request, err)
 
     response = await handler(request)
 
-    #
-    # FIXME: any reference to project_id in the response has to be replaced by wcopy_project_id?
-    # Asks for project X and returns project Y !! should instead be composed
-    #
+    # TODO: A solution would be to have project/*/workbench  instead?
 
-    # A solution would be to have project/*/workbench ?
     return response
 
 
