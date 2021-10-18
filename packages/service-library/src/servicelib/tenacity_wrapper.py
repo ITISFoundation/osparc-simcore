919d38c6f420b8bc3d79eb702b8b2a3db91d236c
Andrei Neagu <neagu@itis.swiss>
2021-10-18 16:21:27 +0200

    refactored to offer some extra parameters

--- packages/service-library/src/servicelib/tenacity_wrapper.py
+++ packages/service-library/src/servicelib/tenacity_wrapper.py
@@ -1,51 +1,88 @@
+import inspect
 import logging
 import sys
-from typing import Any, Callable, Dict, Union
+from typing import Any, Callable, Dict, Tuple, Type, Union
 
-from tenacity import RetryCallState
-from tenacity import Retrying as TenacityRetrying
-from tenacity import WrappedFn
-from tenacity import retry as tenacity_retry
-from tenacity._asyncio import AsyncRetrying as TenacityAsyncRetrying
-from tenacity.after import after_log
-from tenacity.before import before_log
+from tenacity import RetryCallState, _utils
+from tenacity.retry import retry_base
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def _get_before_logger() -> Callable[[RetryCallState], None]:
-    return before_log(logger, logging.DEBUG)
+def _log_if_code_succeeds(retry_state: RetryCallState) -> None:
+    message = (
+        f"Execution succeeded for '{_utils.get_callback_name(retry_state.fn)}', "
+        f"this is the {_utils.to_ordinal(retry_state.attempt_number)} time calling it."
+    )
+    logger.debug(message)
 
 
-def _get_after_logger() -> Callable[[RetryCallState], None]:
-    return after_log(logger, logging.DEBUG)
+class retry_if_exception_type_and_log_success(retry_base):
+    """
+    Like default policy plus logging.
+    Retries if an exception has been raised for one or more types.
+    """
+
+    def __init__(
+        self,
+        exception_types: Union[
+            Type[BaseException],
+            Tuple[Type[BaseException], ...],
+        ] = Exception,
+    ) -> None:
+        self.predicate: Callable[[BaseException], bool] = lambda e: isinstance(
+            e, exception_types
+        )
+
+    def __call__(self, retry_state: RetryCallState) -> bool:
+        if retry_state.outcome.failed:
+            return self.predicate(retry_state.outcome.exception())
+
+        _log_if_code_succeeds(retry_state)
+        return False
 
 
-def _inject_loggers_if_missing(kwargs: Dict[str, Any]) -> None:
-    if "before" not in kwargs:
-        kwargs["before"] = _get_before_logger()
-    if "after" not in kwargs:
-        kwargs["after"] = _get_after_logger()
+def _set_parameter(
+    overwrite_retry_paramenter: bool, kwargs: Dict[str, Any], key: str, value: Any
+) -> None:
+    if key in kwargs:
+        frame = inspect.stack()[2]
+        file_name, line_number, function_name = frame[1], frame[2], frame[3]
+        if overwrite_retry_paramenter:
+            message = (
+                f"Parementer '{key}={kwargs[key]}' set to '{value}' "
+                f"at '{file_name}::{function_name}:{line_number}'"
+            )
+
+        else:
+            message = (
+                f"Parementer '{key}={kwargs[key]}' WILL NOT BE SET TO '{value}' "
+                f"at '{file_name}::{function_name}:{line_number}'"
+            )
+        logging.warning(message)
+
+        if overwrite_retry_paramenter:
+            return
+
+    kwargs[key] = value
 
 
-class AsyncRetrying(TenacityAsyncRetrying):
-    def __init__(self, **kwargs: Any) -> None:
-        _inject_loggers_if_missing(kwargs)
-        super().__init__(**kwargs)
-
-
-class Retrying(TenacityRetrying):
-    def __init__(self, **kwargs: Any):
-        _inject_loggers_if_missing(kwargs)
-        super().__init__(**kwargs)
-
-
-def retry(
-    *dargs: Any, **dkw: Any
-) -> Union[WrappedFn, Callable[[WrappedFn], WrappedFn]]:
-    _inject_loggers_if_missing(dkw)
-    return tenacity_retry(*dargs, **dkw)
-
-
-__all__ = ["AsyncRetrying", "Retrying", "retry"]
+def add_defaults(
+    overwrite_retry_paramenter: bool = False, **kwargs: Any
+) -> Dict[str, Any]:
+    """Adds some extra parameters to make help with debugging"""
+    _set_parameter(
+        overwrite_retry_paramenter=overwrite_retry_paramenter,
+        kwargs=kwargs,
+        key="retry",
+        value=retry_if_exception_type_and_log_success(),
+    )
+    # always reraise the original exception
+    _set_parameter(
+        overwrite_retry_paramenter=overwrite_retry_paramenter,
+        kwargs=kwargs,
+        key="reraise",
+        value=True,
+    )
+    return kwargs
--- packages/service-library/tests/test_tenacity_wrapper.py
+++ packages/service-library/tests/test_tenacity_wrapper.py
@@ -1,10 +1,24 @@
 # pylint: disable=redefined-outer-name
 
+import logging
+from collections import namedtuple
+from typing import Type
+
 import pytest
-from servicelib.tenacity_wrapper import AsyncRetrying, Retrying, retry
-from tenacity import RetryError
+from servicelib.tenacity_wrapper import add_defaults
+from tenacity import AsyncRetrying, RetryError, Retrying, retry
 from tenacity.stop import stop_after_attempt
 from tenacity.wait import wait_fixed
+
+logger = logging.getLogger(__name__)
+
+
+TestParams = namedtuple(
+    "TestParams", "expected_error, reraise, overwrite_retry_paramenter"
+)
+
+
+# UTILS
 
 
 class CustomTestException(Exception):
@@ -14,6 +28,9 @@ class CustomTestException(Exception):
 def _print_and_raise() -> None:
     print(">>>>> my code runs here")
     raise CustomTestException("expected error")
+
+
+# FIXTURES
 
 
 @pytest.fixture
@@ -26,13 +43,41 @@ def wait_interval() -> float:
     return 0.01
 
 
-async def test_async_retrying(max_retires: int, wait_interval: float) -> None:
+# TESTS
+
+
+@pytest.mark.parametrize(
+    "expected_error, reraise, overwrite_retry_paramenter",
+    [
+        TestParams(
+            expected_error=CustomTestException,
+            reraise=True,
+            overwrite_retry_paramenter=True,
+        ),
+        TestParams(
+            expected_error=RetryError,
+            reraise=False,
+            overwrite_retry_paramenter=True,
+        ),
+    ],
+)
+async def test_async_retrying(
+    expected_error: Type[Exception],
+    reraise: bool,
+    overwrite_retry_paramenter: bool,
+    max_retires: int,
+    wait_interval: float,
+) -> None:
     counter = 0
 
-    with pytest.raises(RetryError):
+    with pytest.raises(expected_error):
         async for attempt in AsyncRetrying(
-            stop=stop_after_attempt(max_retires),
-            wait=wait_fixed(wait_interval),
+            **add_defaults(
+                overwrite_retry_paramenter=overwrite_retry_paramenter,
+                stop=stop_after_attempt(max_retires),
+                wait=wait_fixed(wait_interval),
+                reraise=reraise,
+            )
         ):
             with attempt:
                 counter += 1
@@ -44,10 +89,11 @@ async def test_async_retrying(max_retires: int, wait_interval: float) -> None:
 def test_retrying(max_retires: int, wait_interval: float) -> None:
     counter = 0
 
-    with pytest.raises(RetryError):
+    with pytest.raises(CustomTestException):
         for attempt in Retrying(
-            stop=stop_after_attempt(max_retires),
-            wait=wait_fixed(wait_interval),
+            **add_defaults(
+                stop=stop_after_attempt(max_retires), wait=wait_fixed(wait_interval)
+            )
         ):
             with attempt:
                 counter += 1
@@ -61,12 +107,16 @@ def test_try_decorator_callable(max_retires: int, wait_interval: float) -> None:
     # in nested function's scope
     counter = [0]
 
-    @retry(stop=stop_after_attempt(max_retires), wait=wait_fixed(wait_interval))
+    @retry(
+        **add_defaults(
+            stop=stop_after_attempt(max_retires), wait=wait_fixed(wait_interval)
+        )
+    )
     def failing_callable() -> None:
         counter[0] += 1
         _print_and_raise()
 
-    with pytest.raises(RetryError):
+    with pytest.raises(CustomTestException):
         failing_callable()
 
     assert counter[0] == max_retires
@@ -77,12 +127,40 @@ async def test_try_decorator_coroutine(max_retires: int, wait_interval: float) -
     # in nested function's scope
     counter = [0]
 
-    @retry(stop=stop_after_attempt(max_retires), wait=wait_fixed(wait_interval))
+    @retry(
+        **add_defaults(
+            stop=stop_after_attempt(max_retires), wait=wait_fixed(wait_interval)
+        )
+    )
     async def failing_coroutine() -> None:
         counter[0] += 1
         _print_and_raise()
 
-    with pytest.raises(RetryError):
+    with pytest.raises(CustomTestException):
         await failing_coroutine()
 
     assert counter[0] == max_retires
+
+
+async def test_no_fail_after_first_time(max_retires: int, wait_interval: float) -> None:
+    # variables normally passed by reference are not accessible
+    # in nested function's scope
+    counter = [0]
+
+    @retry(
+        **add_defaults(
+            stop=stop_after_attempt(max_retires), wait=wait_fixed(wait_interval)
+        )
+    )
+    async def failing_coroutine() -> None:
+        counter[0] += 1
+
+        logger.debug("[try=%s] before code", counter[0])
+        if counter[0] == 1:
+            logger.debug("[try=%s] will raise error", counter[0])
+            raise CustomTestException("expected error")
+        logger.debug("[try=%s] after code", counter[0])
+
+    await failing_coroutine()
+
+    assert counter[0] == 2
