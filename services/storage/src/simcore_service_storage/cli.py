e50507017f411616e48bb68ec6041c28e06d00b0
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-10-20 21:00:36 +0200

    lower level of very verbose loggers

--- services/storage/src/simcore_service_storage/cli.py
+++ services/storage/src/simcore_service_storage/cli.py
@@ -6,9 +6,11 @@ from settings_library.cli_utils import create_settings_command
 from . import application
 from .settings import Settings
 
-log = logging.getLogger(__name__)
-main = typer.Typer(name="simcore-service-storage service")
+LOG_LEVEL_STEP = logging.CRITICAL - logging.ERROR
 
+log = logging.getLogger(__name__)
+
+main = typer.Typer(name="simcore-service-storage service")
 
 main.command()(create_settings_command(settings_cls=Settings, logger=log))
 
@@ -22,6 +24,15 @@ def run():
 
     logging.basicConfig(level=settings_obj.log_level)
     logging.root.setLevel(settings_obj.log_level)
+
+    # keep mostly quiet noisy loggers
+    quiet_level: int = max(
+        min(logging.root.level + LOG_LEVEL_STEP, logging.CRITICAL), logging.WARNING
+    )
+    logging.getLogger("engineio").setLevel(quiet_level)
+    logging.getLogger("openapi_spec_validator").setLevel(quiet_level)
+    logging.getLogger("sqlalchemy").setLevel(quiet_level)
+    logging.getLogger("sqlalchemy.engine").setLevel(quiet_level)
 
     typer.secho("Starting app ... ")
     application.run(settings_obj)
