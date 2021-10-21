01a111cce6d00f33c2bb0f1e6b7ffe3c4109c4bd
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-10-21 14:43:38 +0200

    oneliners

--- services/director-v2/docker/boot.sh
+++ services/director-v2/docker/boot.sh
@@ -36,14 +36,18 @@ fi
 if [ "${SC_BOOT_MODE}" = "debug-ptvsd" ]; then
   # NOTE: ptvsd is programmatically enabled inside of the service
   # this way we can have reload in place as well
+  reload_dir_packages=$(find packages -maxdepth 3 -type d -path "*/src/*" ! -path "*.*" -exec echo --reload-dir {} \;)
+  reload_dir_packages_concat=""
+  for dir in $reload_dir_packages
+  do
+      # the + returns the fallback if the variable is set (inverse to MYVAR:-default)
+      reload_dir_packages_concat="${reload_dir_packages_concat:+${reload_dir_packages_concat} }${dir}"
+  done
+
   exec uvicorn simcore_service_director_v2.main:the_app \
     --host 0.0.0.0 \
     --reload \
-    --reload-dir packages/models-library/src/models_library \
-    --reload-dir packages/postgres-database/src/simcore_postgres_database \
-    --reload-dir packages/service-library/src/servicelib \
-    --reload-dir packages/settings-library/src/settings_library \
-    --reload-dir packages/simcore-sdk/src/simcore_sdk \
+    "$reload_dir_packages_concat" \
     --reload-dir services/director-v2/src/simcore_service_director_v2
 else
   exec uvicorn simcore_service_director_v2.main:the_app \
