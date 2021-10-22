4b07b3d8ccfb75962c8d4c9981c33bac2784012e
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-10-22 04:25:28 +0200

    adds migration check in catalog

--- services/catalog/src/simcore_service_catalog/db/events.py
+++ services/catalog/src/simcore_service_catalog/db/events.py
@@ -3,7 +3,11 @@ from io import StringIO
 
 from aiopg.sa import Engine, create_engine
 from fastapi import FastAPI
-from tenacity import before_sleep_log, retry, stop_after_attempt, wait_fixed
+from simcore_postgres_database.utils_migration import get_current_head
+from tenacity import retry
+from tenacity.before_sleep import before_sleep_log
+from tenacity.stop import stop_after_attempt
+from tenacity.wait import wait_fixed
 
 from ..core.settings import PostgresSettings
 
@@ -32,6 +36,7 @@ def _compose_info_on_engine(app: FastAPI) -> str:
 
 @retry(**pg_retry_policy)
 async def connect_to_db(app: FastAPI) -> None:
+    logger.debug("Connecting db ...")
     cfg: PostgresSettings = app.state.settings.CATALOG_POSTGRES
     engine: Engine = await create_engine(
         str(cfg.dsn),
@@ -40,6 +45,27 @@ async def connect_to_db(app: FastAPI) -> None:
         maxsize=cfg.POSTGRES_MAXSIZE,
     )
     logger.debug("Connected to %s", engine.dsn)
+
+    logger.debug("Checking db migrationn ...")
+    try:
+        async with engine.acquire() as conn:
+            version_num = await conn.scalar(
+                'SELECT "version_num" FROM "alembic_version"'
+            )
+            head_version_num = get_current_head()
+            if version_num != head_version_num:
+                raise RuntimeError(
+                    f"Migration is incomplete, expected {head_version_num} and got {version_num}"
+                )
+
+    except Exception:
+        # WARNING: engine must be closed because retry will create a new engine
+        engine.close()
+        await engine.wait_closed()
+        raise
+    else:
+        logger.debug("Migration up-to-date")
+
     app.state.engine = engine
 
     logger.debug(_compose_info_on_engine(app))
