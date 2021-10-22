0e958b0783d36f84a78aef0464bc8bb890557c3f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-10-22 03:26:46 +0200

    separated utils and added get_current_head

--- packages/postgres-database/src/simcore_postgres_database/cli.py
+++ packages/postgres-database/src/simcore_postgres_database/cli.py
@@ -1,25 +1,19 @@
 """ command line interface for migration
 
 """
-# pylint: disable=broad-except
-# pylint: disable=wildcard-import,unused-wildcard-import
+# pylint: disable=wildcard-import
+# pylint: disable=unused-wildcard-import
 
 import json
 import json.decoder
 import logging
 import os
-import sys
-from copy import deepcopy
-from functools import wraps
 from logging.config import fileConfig
-from pathlib import Path
-from typing import Callable, Dict, Final, Optional
+from typing import Dict, Optional
 
 import alembic.command
 import click
-import docker
 from alembic import __version__ as __alembic_version__
-from alembic.config import Config as AlembicConfig
 from simcore_postgres_database.models import *
 from simcore_postgres_database.utils import (
     build_url,
@@ -27,18 +21,22 @@ from simcore_postgres_database.utils import (
     hide_url_pass,
     raise_if_not_responsive,
 )
+from simcore_postgres_database.utils_migration import (
+    DEFAULT_INI,
+    DISCOVERED_CACHE,
+    get_alembic_config_from_cache,
+    get_service_published_port,
+    load_cache,
+    reset_cache,
+)
 from tenacity import Retrying
 from tenacity.after import after_log
 from tenacity.wait import wait_fixed
 
 ALEMBIC_VERSION = tuple(int(v) for v in __alembic_version__.split(".")[0:3])
-
-CURRENT_DIR: Final = Path(
-    sys.argv[0] if __name__ == "__main__" else __file__
-).parent.resolve()
-DEFAULT_INI: Final = CURRENT_DIR / "alembic.ini"
-MIGRATION_DIR: Final = CURRENT_DIR / "migration"
-DISCOVERED_CACHE: Final = os.path.expanduser("~/.simcore_postgres_database_cache.json")
+DEFAULT_HOST = "postgres"
+DEFAULT_PORT = 5432
+DEFAULT_DB = "simcoredb"
 
 log = logging.getLogger("root")
 
@@ -46,111 +44,6 @@ if __name__ == "__main__":
     # swallows up all log messages from tests
     # only enable it during cli invocation
     fileConfig(DEFAULT_INI)
-
-
-def safe(if_fails_return=False):
-    def _decorator(func: Callable):
-        @wraps(func)
-        def wrapper(*args, **kargs):
-            try:
-                res = func(*args, **kargs)
-                return res
-            except RuntimeError as err:
-                log.info(
-                    "%s failed:  %s",
-                    func.__name__,
-                    str(err),
-                    exc_info=True,
-                    stack_info=True,
-                )
-            except Exception:
-                log.info(
-                    "%s failed unexpectedly",
-                    func.__name__,
-                    exc_info=True,
-                    stack_info=True,
-                )
-            return deepcopy(if_fails_return)  # avoid issues with default mutables
-
-        return wrapper
-
-    return _decorator
-
-
-@safe(if_fails_return=None)
-def _get_service_published_port(service_name: str) -> int:
-    client = docker.from_env()
-    services = [x for x in client.services.list() if service_name in x.name]
-    if not services:
-        raise RuntimeError(
-            "Cannot find published port for service '%s'. Probably services still not up"
-            % service_name
-        )
-    service_endpoint = services[0].attrs["Endpoint"]
-
-    if "Ports" not in service_endpoint or not service_endpoint["Ports"]:
-        raise RuntimeError(
-            "Cannot find published port for service '%s' in endpoint. Probably services still not up"
-            % service_name
-        )
-
-    published_port = service_endpoint["Ports"][0]["PublishedPort"]
-    return int(published_port)
-
-
-def _get_alembic_config_from_cache(
-    force_cfg: Optional[Dict] = None,
-) -> Optional[AlembicConfig]:
-    """
-    Creates alembic config from cfg or cache
-
-    Returns None if cannot build url (e.g. if user requires a cache that does not exists)
-    """
-
-    # build url
-    try:
-        if force_cfg:
-            cfg = force_cfg
-        else:
-            cfg = _load_cache(raise_if_error=True)
-
-        url = build_url(**cfg)
-    except Exception:
-        log.debug(
-            "Cannot open cache or cannot build URL", exc_info=True, stack_info=True
-        )
-        click.echo("Invalid database config, please run discover first", err=True)
-        _reset_cache()
-        return None
-
-    # build config
-    config = AlembicConfig(file_=str(DEFAULT_INI))
-    config.set_main_option("script_location", str(MIGRATION_DIR))
-    config.set_main_option("sqlalchemy.url", str(url))
-    return config
-
-
-def _load_cache(*, raise_if_error=False) -> Dict:
-    try:
-        with open(DISCOVERED_CACHE) as fh:
-            cfg = json.load(fh)
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
-        if raise_if_error:
-            raise
-        return {}
-    return cfg
-
-
-def _reset_cache():
-    if os.path.exists(DISCOVERED_CACHE):
-        os.remove(DISCOVERED_CACHE)
-        click.echo("Removed %s" % DISCOVERED_CACHE)
-
-
-# CLI -----------------------------------------------
-DEFAULT_HOST = "postgres"
-DEFAULT_PORT = 5432
-DEFAULT_DB = "simcoredb"
 
 
 @click.group()
@@ -176,7 +69,7 @@ def discover(**cli_inputs) -> Optional[Dict]:
 
     def _test_cached() -> Dict:
         """Tests cached configuration"""
-        cfg = _load_cache(raise_if_error=True)
+        cfg = load_cache(raise_if_error=True)
         cfg.update(cli_cfg)  # overrides
         return cfg
 
@@ -196,7 +89,7 @@ def discover(**cli_inputs) -> Optional[Dict]:
         """Tests published port in swarm from host"""
         cfg = _test_env()
         cfg["host"] = "127.0.0.1"
-        cfg["port"] = _get_service_published_port(cli_cfg.get("host", DEFAULT_HOST))
+        cfg["port"] = get_service_published_port(cli_cfg.get("host", DEFAULT_HOST))
         cfg.setdefault("database", DEFAULT_DB)
         return cfg
 
@@ -226,11 +119,11 @@ def discover(**cli_inputs) -> Optional[Dict]:
 
             return cfg
 
-        except Exception as err:
+        except Exception as err:  # pylint: disable=broad-except
             inline_msg = str(err).replace("\n", ". ")
             click.echo(f"<- {test.__name__} failed : {inline_msg}")
 
-    _reset_cache()
+    reset_cache()
     click.secho("Sorry, database not found !!", blink=False, bold=True, fg="red")
     return None
 
@@ -240,9 +133,9 @@ def info():
     """Displays discovered config and other alembic infos"""
     click.echo("Using alembic {}.{}.{}".format(*ALEMBIC_VERSION))
 
-    cfg = _load_cache()
+    cfg = load_cache()
     click.echo(f"Saved config: {hide_dict_pass(cfg)} @ {DISCOVERED_CACHE}")
-    config = _get_alembic_config_from_cache(cfg)
+    config = get_alembic_config_from_cache(cfg)
     if config:
         click.echo("Revisions history ------------")
         alembic.command.history(config)
@@ -253,7 +146,7 @@ def info():
 @main.command()
 def clean():
     """Clears discovered database"""
-    _reset_cache()
+    reset_cache()
 
 
 @main.command()
@@ -270,13 +163,13 @@ def upgrade_and_close():
         info.callback()
         upgrade.callback(revision="head")
         info.callback()
-    except Exception:
+    except Exception:  # pylint: disable=broad-except
         log.exception("Unable to upgrade")
 
     click.echo("I did my job here. Bye!")
 
 
-# Bypasses alembic CLI into a reduced version  ------------
+# Overrides Alembic CLI  ------------
 
 
 @main.command()
@@ -285,7 +178,7 @@ def review(message):
     """Auto-generates a new revison. Equivalent to `alembic revision --autogenerate -m "first tables"`"""
     click.echo("Auto-generates revision based on changes ")
 
-    config = _get_alembic_config_from_cache()
+    config = get_alembic_config_from_cache()
     if config:
         alembic.command.revision(
             config,
@@ -319,7 +212,7 @@ def upgrade(revision):
 
     """
     click.echo(f"Upgrading database to {revision} ...")
-    config = _get_alembic_config_from_cache()
+    config = get_alembic_config_from_cache()
     if config:
         alembic.command.upgrade(config, revision, sql=False, tag=None)
     else:
@@ -343,7 +236,7 @@ def downgrade(revision):
     """
     # https://click.palletsprojects.com/en/3.x/arguments/#argument-like-options
     click.echo(f"Downgrading database to current-{revision} ...")
-    config = _get_alembic_config_from_cache()
+    config = get_alembic_config_from_cache()
     if config:
         alembic.command.downgrade(config, str(revision), sql=False, tag=None)
     else:
@@ -355,7 +248,7 @@ def downgrade(revision):
 def stamp(revision):
     """Stamps the database with a given revision; does not run any migration"""
     click.echo(f"Stamps db to {revision} ...")
-    config = _get_alembic_config_from_cache()
+    config = get_alembic_config_from_cache()
     if config:
         alembic.command.stamp(config, revision, sql=False, tag=None)
     else:
