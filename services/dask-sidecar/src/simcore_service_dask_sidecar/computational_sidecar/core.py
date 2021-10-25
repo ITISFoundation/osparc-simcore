8d993684c9fcaef791eabfe6b0efd6e0e456303d
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-10-25 16:46:13 +0200

    added a bit of logs

--- services/dask-sidecar/src/simcore_service_dask_sidecar/computational_sidecar/core.py
+++ services/dask-sidecar/src/simcore_service_dask_sidecar/computational_sidecar/core.py
@@ -29,7 +29,7 @@ from pydantic.networks import AnyUrl
 from yarl import URL
 
 from ..boot_mode import BootMode
-from ..dask_utils import create_dask_worker_logger, publish_event
+from ..dask_utils import create_dask_worker_logger, publish_event, publish_task_logs
 from ..file_utils import copy_file_to_remote
 from ..settings import Settings
 from .docker_utils import (
@@ -143,14 +143,30 @@ class ComputationalSidecar:  # pylint: disable=too-many-instance-attributes
         publish_event(
             self._state_pub, TaskStateEvent.from_dask_worker(state=RunningState.STARTED)
         )
+        publish_event(
+            self._logs_pub,
+            TaskLogEvent.from_dask_worker(
+                log=f"[sidecar] Starting task for {self.service_key}:{self.service_version}..."
+            ),
+        )
 
         settings = Settings.create_from_envs()
         run_id = f"{uuid4()}"
         async with Docker() as docker_client, TaskSharedVolumes(
             Path(f"{settings.SIDECAR_COMP_SERVICES_SHARED_FOLDER}/{run_id}")
         ) as task_volumes:
+            publish_event(
+                self._logs_pub,
+                TaskLogEvent.from_dask_worker(
+                    log=f"[sidecar] Pulling image for {self.service_key}:{self.service_version}..."
+                ),
+            )
             await pull_image(
                 docker_client, self.docker_auth, self.service_key, self.service_version
+            )
+            publish_event(
+                self._logs_pub,
+                TaskLogEvent.from_dask_worker(log="[sidecar] Image pulled."),
             )
             integration_version = await get_integration_version(
                 docker_client, self.docker_auth, self.service_key, self.service_version
@@ -171,6 +187,10 @@ class ComputationalSidecar:  # pylint: disable=too-many-instance-attributes
 
             # set up the inputs
             await self._write_input_data(task_volumes, integration_version)
+            publish_event(
+                self._logs_pub,
+                TaskLogEvent.from_dask_worker(log="[sidecar] Configuration done."),
+            )
 
             # run the image
             async with managed_container(docker_client, config) as container:
@@ -184,6 +204,12 @@ class ComputationalSidecar:  # pylint: disable=too-many-instance-attributes
                     task_volumes=task_volumes,
                     log_file_url=self.log_file_url,
                 ):
+                    publish_event(
+                        self._logs_pub,
+                        TaskLogEvent.from_dask_worker(
+                            log="[sidecar] Starting container..."
+                        ),
+                    )
                     # run the container
                     await container.start()
                     # get the logs
@@ -198,6 +224,12 @@ class ComputationalSidecar:  # pylint: disable=too-many-instance-attributes
                             TaskStateEvent.from_dask_worker(
                                 state=RunningState.FAILED,
                                 msg=f"error while running container {container.id} for {self.service_key}:{self.service_version}",
+                            ),
+                        )
+                        publish_event(
+                            self._logs_pub,
+                            TaskLogEvent.from_dask_worker(
+                                log=f"[sidecar] task failed with exit code{container_data['State']['ExitCode']}"
                             ),
                         )
 
@@ -217,7 +249,16 @@ class ComputationalSidecar:  # pylint: disable=too-many-instance-attributes
                     )
 
             # get the outputs
-            return await self._retrieve_output_data(task_volumes, integration_version)
+            results = await self._retrieve_output_data(
+                task_volumes, integration_version
+            )
+            publish_event(
+                self._logs_pub,
+                TaskLogEvent.from_dask_worker(
+                    log="[sidecar] task completed successfully"
+                ),
+            )
+            return results
 
     async def __aenter__(self) -> "ComputationalSidecar":
         return self
