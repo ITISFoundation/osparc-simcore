1daa42dac5871749ad3c9382d5c30c256cb46a77
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-10-26 08:46:07 +0200

    updates api-server

--- services/api-server/src/simcore_service_api_server/db/events.py
+++ services/api-server/src/simcore_service_api_server/db/events.py
@@ -3,7 +3,13 @@ from io import StringIO
 
 from aiopg.sa import Engine, create_engine
 from fastapi import FastAPI
-from tenacity import before_sleep_log, retry, stop_after_attempt, wait_fixed
+from servicelib.common_aiopg_utils import (
+    ENGINE_ATTRS,
+    PostgresRetryPolicyUponInitialization,
+    close_engine,
+    raise_if_migration_not_ready,
+)
+from tenacity import retry
 
 from .._meta import PROJECT_NAME
 from ..core.settings import PostgresSettings
@@ -11,27 +17,7 @@ from ..core.settings import PostgresSettings
 logger = logging.getLogger(__name__)
 
 
-ENGINE_ATTRS = "closed driver dsn freesize maxsize minsize name size timeout".split()
-
-
-pg_retry_policy = dict(
-    wait=wait_fixed(5),
-    stop=stop_after_attempt(20),
-    before_sleep=before_sleep_log(logger, logging.WARNING),
-    reraise=True,
-)
-
-
-def _compose_info_on_engine(app: FastAPI) -> str:
-    engine = app.state.engine
-    stm = StringIO()
-    print("Setup engine:", end=" ", file=stm)
-    for attr in ENGINE_ATTRS:
-        print(f"{attr}={getattr(engine, attr)}", end="; ", file=stm)
-    return stm.getvalue()
-
-
-@retry(**pg_retry_policy)
+@retry(**PostgresRetryPolicyUponInitialization(logger).kwargs)
 async def connect_to_db(app: FastAPI) -> None:
     logger.debug("Connecting db ...")
 
@@ -44,15 +30,28 @@ async def connect_to_db(app: FastAPI) -> None:
         maxsize=cfg.POSTGRES_MAXSIZE,
     )
     logger.debug("Connected to %s", engine.dsn)
-    app.state.engine = engine
 
-    logger.debug(_compose_info_on_engine(app))
+    logger.debug("Checking db migrationn ...")
+    try:
+        await raise_if_migration_not_ready(engine)
+    except Exception:
+        # NOTE: engine must be closed because retry will create a new engine
+        await close_engine(engine)
+        raise
+    else:
+        logger.debug("Migration up-to-date")
+
+    app.state.engine = engine
+    logger.debug(
+        "Setup engine: %s",
+        " ".join(f"{attr}={getattr(engine, attr)}" for attr in ENGINE_ATTRS),
+    )
 
 
 async def close_db_connection(app: FastAPI) -> None:
     logger.debug("Disconnecting db ...")
 
-    engine: Engine = app.state.engine
-    engine.close()
-    await engine.wait_closed()
+    if engine := app.state.engine:
+        await close_engine(engine)
+
     logger.debug("Disconnected from %s", engine.dsn)
