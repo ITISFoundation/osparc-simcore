a527f4667d20ffd0c383a6783ff820cdad7ecb49
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-10-26 13:52:37 +0200

    allow getting/setting value link without downloading/uploading directly

--- packages/simcore-sdk/src/simcore_sdk/node_ports_v2/port.py
+++ packages/simcore-sdk/src/simcore_sdk/node_ports_v2/port.py
@@ -4,11 +4,18 @@ from pprint import pformat
 from typing import Any, Dict, Optional, Tuple, Type
 
 from models_library.services import PROPERTY_KEY_RE, ServiceProperty
-from pydantic import Field, PrivateAttr, validator
+from pydantic import AnyUrl, Field, PrivateAttr, validator
 
 from ..node_ports_common.exceptions import InvalidItemTypeError
 from . import port_utils
-from .links import DataItemValue, DownloadLink, FileLink, ItemConcreteValue, PortLink
+from .links import (
+    DataItemValue,
+    DownloadLink,
+    FileLink,
+    ItemConcreteValue,
+    ItemValue,
+    PortLink,
+)
 
 log = logging.getLogger(__name__)
 
@@ -66,6 +73,35 @@ class Port(ServiceProperty):
             self.value = self.default_value
             self._used_default_value = True
 
+    async def get_value(self) -> Optional[ItemValue]:
+        """returns the value of the link after resolving the port links"""
+        log.debug(
+            "getting value of %s[%s] containing '%s'",
+            self.key,
+            self.property_type,
+            pformat(self.value),
+        )
+
+        if isinstance(self.value, PortLink):
+            # this is a link to another node
+            return await port_utils.get_value_link_from_port_link(
+                # pylint: disable=protected-access
+                self.value,
+                self._node_ports._node_ports_creator_cb,
+            )
+        if isinstance(self.value, FileLink):
+            # let's get the download/upload link from storage
+            return await port_utils.get_download_link_from_storage(
+                user_id=self._node_ports.user_id,
+                value=self.value,
+            )
+
+        if isinstance(self.value, DownloadLink):
+            # this is a downloadable link
+            return self.value.download_link
+
+        return self.value
+
     async def get(self) -> Optional[ItemConcreteValue]:
         log.debug(
             "getting %s[%s] with value %s",
@@ -82,20 +118,25 @@ class Port(ServiceProperty):
             # this is a link to another node
             value = await port_utils.get_value_from_link(
                 # pylint: disable=protected-access
-                self.key,
-                self.value,
-                self.file_to_key_map,
-                self._node_ports._node_ports_creator_cb,
+                key=self.key,
+                value=self.value,
+                fileToKeyMap=self.file_to_key_map,
+                node_port_creator=self._node_ports._node_ports_creator_cb,
             )
         elif isinstance(self.value, FileLink):
             # this is a link from storage
             value = await port_utils.pull_file_from_store(
-                self._node_ports.user_id, self.key, self.file_to_key_map, self.value
+                user_id=self._node_ports.user_id,
+                key=self.key,
+                fileToKeyMap=self.file_to_key_map,
+                value=self.value,
             )
         elif isinstance(self.value, DownloadLink):
             # this is a downloadable link
             value = await port_utils.pull_file_from_download_link(
-                self.key, self.file_to_key_map, self.value
+                key=self.key,
+                fileToKeyMap=self.file_to_key_map,
+                value=self.value,
             )
         else:
             # this is directly the value
@@ -108,7 +149,10 @@ class Port(ServiceProperty):
 
     async def _set(self, new_value: ItemConcreteValue) -> None:
         log.debug(
-            "setting %s[%s] with value %s", self.key, self.property_type, new_value
+            "setting %s[%s] with concrete value %s",
+            self.key,
+            self.property_type,
+            new_value,
         )
         final_value: Optional[DataItemValue] = None
         if new_value is not None:
@@ -119,10 +163,10 @@ class Port(ServiceProperty):
                 if not converted_value.exists() or not converted_value.is_file():
                     raise InvalidItemTypeError(self.property_type, str(new_value))
                 final_value = await port_utils.push_file_to_store(
-                    converted_value,
-                    self._node_ports.user_id,
-                    self._node_ports.project_id,
-                    self._node_ports.node_uuid,
+                    file=converted_value,
+                    user_id=self._node_ports.user_id,
+                    project_id=self._node_ports.project_id,
+                    node_id=self._node_ports.node_uuid,
                 )
             else:
                 final_value = converted_value
@@ -133,4 +177,26 @@ class Port(ServiceProperty):
     async def set(self, new_value: ItemConcreteValue) -> None:
         """sets a value to the port, by default it is also stored in the database"""
         await self._set(new_value)
+        await self._node_ports.save_to_db_cb(self._node_ports)
+
+    async def set_value(self, new_value: Optional[ItemValue]) -> None:
+        """set the value on the port using a value (e.g. link for the file)"""
+        log.debug(
+            "setting %s[%s] with value %s", self.key, self.property_type, new_value
+        )
+        final_value: Optional[DataItemValue] = None
+        if port_utils.is_file_type(self.property_type) and new_value is not None:
+            if not isinstance(new_value, AnyUrl):
+                raise InvalidItemTypeError(self.property_type, f"{new_value}")
+            final_value = await port_utils.get_file_link_from_url(
+                new_value,
+                self._node_ports.user_id,
+                self._node_ports.project_id,
+                self._node_ports.node_uuid,
+            )
+        else:
+            final_value = self._py_value_converter(new_value)
+
+        self.value = final_value
+        self._used_default_value = False
         await self._node_ports.save_to_db_cb(self._node_ports)
