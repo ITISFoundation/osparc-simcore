5ef4cd985f98f1ca4ee116659624748c5bf683a8
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2021-10-27 13:39:38 +0200

    ✨ postgres readiness check includes migration completed (#2600)
    * separated utils and added get_current_head
    
    * adds some test to ensure single head
    
    * retries until migration is ready
    
    * adds migration check in catalog
    * adds migration check in web-server
    * moved to common aiopg
    
    * updates catalog
    * updates director-v2
    * updates storage
    * updates api-server
    * updates simcore-sdk
    * updates sidecar
    
    * removes responsive check and  use only migration check
    * splits utils to avoid import docker
    * adds alembic in postgres base reqs
    * adds aiopg utils and errors to pg-lib
    * refactor servicelib: splits retry policies, removes attrs
    * moving from servicelib -> pglib
    * adapting to new imports
    * using pglib.errors
    * Review @sanderegg: changes log level

---
+++ packages/service-library/src/servicelib/retry_policies.py
@@ -0,0 +1,30 @@
+""" Common retry policies to access services
+
+    Other than tenacity, this module SHOULD NOT have other dependencies
+"""
+
+import logging
+from typing import Optional
+
+from tenacity.before_sleep import before_sleep_log
+from tenacity.stop import stop_after_attempt
+from tenacity.wait import wait_fixed
+
+log = logging.getLogger(__name__)
+
+
+class PostgresRetryPolicyUponInitialization:
+    """Retry policy upon service initialization"""
+
+    WAIT_SECS = 5
+    ATTEMPTS_COUNT = 20
+
+    def __init__(self, logger: Optional[logging.Logger] = None):
+        logger = logger or log
+
+        self.kwargs = dict(
+            wait=wait_fixed(self.WAIT_SECS),
+            stop=stop_after_attempt(self.ATTEMPTS_COUNT),
+            before_sleep=before_sleep_log(logger, logging.WARNING),
+            reraise=True,
+        )
--- services/director-v2/src/simcore_service_director_v2/modules/db/events.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/events.py
@@ -1,62 +1,60 @@
 import logging
-from io import StringIO
 from typing import Any
 
 import orjson
 from aiopg.sa import Engine, create_engine
 from aiopg.sa.engine import get_dialect
 from fastapi import FastAPI
+from servicelib.retry_policies import PostgresRetryPolicyUponInitialization
 from settings_library.postgres import PostgresSettings
-from tenacity import before_sleep_log, retry, stop_after_attempt, wait_fixed
+from simcore_postgres_database.utils_aiopg import (
+    close_engine,
+    get_pg_engine_info,
+    raise_if_migration_not_ready,
+)
+from tenacity import retry
 
 logger = logging.getLogger(__name__)
-
-
-ENGINE_ATTRS = "closed driver dsn freesize maxsize minsize name size timeout".split()
-
-
-pg_retry_policy = dict(
-    wait=wait_fixed(5),
-    stop=stop_after_attempt(20),
-    before_sleep=before_sleep_log(logger, logging.WARNING),
-    reraise=True,
-)
-
-
-def _compose_info_on_engine(app: FastAPI) -> str:
-    engine = app.state.engine
-    stm = StringIO()
-    print("Setup engine:", end=" ", file=stm)
-    for attr in ENGINE_ATTRS:
-        print(f"{attr}={getattr(engine, attr)}", end="; ", file=stm)
-    return stm.getvalue()
 
 
 def json_serializer(o: Any) -> str:
     return str(orjson.dumps(o), "utf-8")
 
 
-@retry(**pg_retry_policy)
+@retry(**PostgresRetryPolicyUponInitialization(logger).kwargs)
 async def connect_to_db(app: FastAPI, settings: PostgresSettings) -> None:
+    """
+    Creates an engine to communicate to the db and retries until
+    the db is ready
+    """
     logger.debug("Connecting db ...")
-    aiopg_dialect = get_dialect(json_serializer=json_serializer)
     engine: Engine = await create_engine(
         str(settings.dsn),
         application_name=f"{__name__}_{id(app)}",  # unique identifier per app
         minsize=settings.POSTGRES_MINSIZE,
         maxsize=settings.POSTGRES_MAXSIZE,
-        dialect=aiopg_dialect,
+        dialect=get_dialect(json_serializer=json_serializer),
     )
     logger.debug("Connected to %s", engine.dsn)
-    app.state.engine = engine
 
-    logger.debug(_compose_info_on_engine(app))
+    logger.debug("Checking db migrationn ...")
+    try:
+        await raise_if_migration_not_ready(engine)
+    except Exception:
+        # NOTE: engine must be closed because retry will create a new engine
+        await close_engine(engine)
+        raise
+    else:
+        logger.debug("Migration up-to-date")
+
+    app.state.engine = engine
+    logger.debug("Setup engine: %s", get_pg_engine_info(engine))
 
 
 async def close_db_connection(app: FastAPI) -> None:
     logger.debug("Disconnecting db ...")
 
-    engine: Engine = app.state.engine
-    engine.close()
-    await engine.wait_closed()
+    if engine := app.state.engine:
+        await close_engine(engine)
+
     logger.debug("Disconnected from %s", engine.dsn)
