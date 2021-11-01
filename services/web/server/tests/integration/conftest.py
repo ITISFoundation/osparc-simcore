3f6390f422f754590228a96b19ea1376f0c36eb9
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-11-01 11:06:11 +0100

    missing imports

--- services/web/server/tests/integration/conftest.py
+++ services/web/server/tests/integration/conftest.py
@@ -45,6 +45,7 @@ pytest_plugins = [
     "pytest_simcore.docker_compose",
     "pytest_simcore.docker_registry",
     "pytest_simcore.docker_swarm",
+    "pytest_simcore.monkeypatch_extra",
     "pytest_simcore.postgres_service",
     "pytest_simcore.rabbit_service",
     "pytest_simcore.redis_service",
