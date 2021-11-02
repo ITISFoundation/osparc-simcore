23988c67246f4ee3cbd76075246d051b87960f20
Andrei Neagu <neagu@itis.swiss>
2021-11-02 10:56:12 +0100

    renamed and removed thins which are not strictly required

--- services/director-v2/tests/unit/test_modules_dynamic_sidecar_docker_service_specs_sidecar.py
+++ services/director-v2/tests/unit/test_modules_dynamic_sidecar_docker_service_specs_sidecar.py
@@ -1,6 +1,4 @@
-import json
 from typing import Dict
-from uuid import UUID
 
 from _pytest.monkeypatch import MonkeyPatch
 from simcore_service_director_v2.core.settings import (
@@ -71,49 +69,7 @@ def test_dynamic_sidecar_env_vars(
     dynamic_sidecar_env_vars = _get_dy_sidecar_env_vars(
         scheduler_data_from_http_request, app_settings, dynamic_sidecar_settings
     )
-    registry_settings = dynamic_sidecar_settings.REGISTRY
-    rabbit_settings = app_settings.CELERY.CELERY_RABBIT
     print("dynamic_sidecar_env_vars:", dynamic_sidecar_env_vars)
 
     assert len(dynamic_sidecar_env_vars) == len(EXPECTED_DYNAMIC_SIDECAR_ENV_VAR_NAMES)
     assert set(dynamic_sidecar_env_vars) == EXPECTED_DYNAMIC_SIDECAR_ENV_VAR_NAMES
-
-    assert dynamic_sidecar_env_vars["REGISTRY_AUTH"] == str(
-        registry_settings.REGISTRY_AUTH
-    )
-    assert dynamic_sidecar_env_vars["REGISTRY_PATH"] == str(
-        registry_settings.REGISTRY_PATH
-    )
-    assert dynamic_sidecar_env_vars["REGISTRY_URL"] == str(
-        registry_settings.REGISTRY_URL
-    )
-    assert dynamic_sidecar_env_vars["REGISTRY_USER"] == str(
-        registry_settings.REGISTRY_USER
-    )
-    assert dynamic_sidecar_env_vars["REGISTRY_PW"] == str(
-        registry_settings.REGISTRY_PW.get_secret_value()
-    )
-    assert dynamic_sidecar_env_vars["REGISTRY_SSL"] == str(
-        registry_settings.REGISTRY_SSL
-    )
-
-    assert registry_settings.REGISTRY_PW.get_secret_value() == MOCKED_PASSWORD
-
-    assert dynamic_sidecar_env_vars["RABBIT_HOST"] == str(rabbit_settings.RABBIT_HOST)
-    assert dynamic_sidecar_env_vars["RABBIT_PORT"] == str(rabbit_settings.RABBIT_PORT)
-    assert dynamic_sidecar_env_vars["RABBIT_USER"] == str(rabbit_settings.RABBIT_USER)
-    assert str(rabbit_settings.RABBIT_PASSWORD) == "**********"
-    assert dynamic_sidecar_env_vars["RABBIT_PASSWORD"] == str(
-        rabbit_settings.RABBIT_PASSWORD.get_secret_value()
-    )
-    assert dynamic_sidecar_env_vars["RABBIT_CHANNELS"] == json.dumps(
-        rabbit_settings.RABBIT_CHANNELS
-    )
-
-    assert int(dynamic_sidecar_env_vars["USER_ID"]) >= 0
-    assert UUID(dynamic_sidecar_env_vars["PROJECT_ID"])
-    assert UUID(dynamic_sidecar_env_vars["NODE_ID"])
-
-    assert int(dynamic_sidecar_env_vars["DY_SIDECAR_USER_ID"]) >= 0
-    assert UUID(dynamic_sidecar_env_vars["DY_SIDECAR_PROJECT_ID"])
-    assert UUID(dynamic_sidecar_env_vars["DY_SIDECAR_NODE_ID"])
