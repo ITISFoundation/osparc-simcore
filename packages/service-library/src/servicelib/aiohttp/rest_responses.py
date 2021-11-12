8a96383bf2faf9de6a65fb8436bf0f357f36dad9
odeimaiz <maiz@itis.swiss>
2021-11-12 16:04:33 +0100

    @sanderegg fix DatasetMetaData is not JSON serializable

--- packages/service-library/src/servicelib/aiohttp/rest_responses.py
+++ packages/service-library/src/servicelib/aiohttp/rest_responses.py
@@ -8,6 +8,7 @@ import attr
 from aiohttp import web, web_exceptions
 from aiohttp.web_exceptions import HTTPError, HTTPException
 
+from ..json_serialization import json_dumps
 from .rest_codecs import json, jsonify
 from .rest_models import ErrorItemType, ErrorType, LogMessageType
 
@@ -77,7 +78,7 @@ def create_data_response(
         else:
             payload = data
 
-        response = web.json_response(payload, dumps=jsonify)
+        response = web.json_response(payload, dumps=json_dumps)
     except (TypeError, ValueError) as err:
         response = create_error_response(
             [
