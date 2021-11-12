9987ceb8873c125cb40ae2362ca39c85e847e77c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-11-12 15:50:06 +0100

    fix jsonify

--- packages/service-library/src/servicelib/aiohttp/rest_responses.py
+++ packages/service-library/src/servicelib/aiohttp/rest_responses.py
@@ -8,7 +8,7 @@ import attr
 from aiohttp import web, web_exceptions
 from aiohttp.web_exceptions import HTTPError, HTTPException
 
-from .rest_codecs import json, jsonify
+from ..json_serialization import json_dumps
 from .rest_models import ErrorItemType, ErrorType, LogMessageType
 
 ENVELOPE_KEYS = ("data", "error")
@@ -77,7 +77,7 @@ def create_data_response(
         else:
             payload = data
 
-        response = web.json_response(payload, dumps=jsonify)
+        response = web.json_response(payload, dumps=json_dumps)
     except (TypeError, ValueError) as err:
         response = create_error_response(
             [
