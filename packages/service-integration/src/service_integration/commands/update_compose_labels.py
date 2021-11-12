a3461e3b4579979595badda9404abad22e2c70f0
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-11-12 15:14:24 +0100

    converts files into compose-spec labels

--- packages/service-integration/src/service_integration/commands/update_compose_labels.py
+++ packages/service-integration/src/service_integration/commands/update_compose_labels.py
@@ -8,25 +8,34 @@ import yaml
 from ..models import ComposeSpecDict
 
 
-def get_compose_file(compose_file: Path) -> ComposeSpecDict:
+def load_compose_spec(compose_file: Path) -> ComposeSpecDict:
     # TODO: auto-generate minimal: catch FileNotFoundError and deduce minimal from model
     with compose_file.open() as fp:
         # TODO: validate with docker-compose config?
         return yaml.safe_load(fp)
 
 
-def get_metadata_file(metadata_file: Path) -> Dict:
+def load_io_specs(metadata_file: Path) -> Dict:
     with metadata_file.open() as fp:
         return yaml.safe_load(fp)
         # TODO: validate using pydantic model
 
 
-def stringify_metadata(metadata: Dict) -> Dict[str, str]:
-    jsons = {}
-    for key, value in metadata.items():
-        # TODO: connect this with models
-        jsons[f"io.simcore.{key}"] = json.dumps({key: value}, sort_keys=False)
-    return jsons
+def to_labels(
+    data: Dict, *, prefix_key: str = "io.simcore", trim_key_head: bool = False
+) -> Dict[str, str]:
+    # TODO: connect this with models
+    # FIXME: null is loaded as 'null' string value? is that correct? json -> None upon deserialization?
+    labels = {}
+    for key, value in data.items():
+        if trim_key_head:
+            label = json.dumps(value, sort_keys=False)
+        else:
+            label = json.dumps({key: value}, sort_keys=False)
+
+        labels[f"{prefix_key}.{key}"] = label
+
+    return labels
 
 
 def update_compose_labels(
@@ -60,13 +69,14 @@ def update_compose_labels(
 @click.option(
     "--metadata",
     "--metadata-file",
-    "metadata_file_path",
+    "io_specs_path",
     help="The metadata yaml file",
     type=Path,
     required=False,
     default="metadata/metadata.yml",
 )
-def main(compose_file_path: Path, metadata_file_path: Path):
+@click.option("--service-specs", "service_specs_path", type=Path)
+def main(compose_file_path: Path, io_specs_path: Path, service_specs_path: Path):
     """Update a docker-compose file with json files in a path
 
     Usage: python update_compose_labels --c docker-compose.yml -f folder/path
@@ -74,17 +84,30 @@ def main(compose_file_path: Path, metadata_file_path: Path):
     """
 
     click.echo("Updating component labels")
-    # get available jsons
-    compose_spec = get_compose_file(compose_file_path)
-    metadata = get_metadata_file(metadata_file_path)
-    json_metadata = stringify_metadata(metadata)
+
+    # load if exists
+    compose_spec = {}
+    if compose_file_path.exists():
+        compose_spec = load_compose_spec(compose_file_path)
+
+    # specs
+    io_spec = load_io_specs(io_specs_path)
+    service_spec = yaml.safe_load(service_specs_path.read_text())
+
+    # specs -> labels
+    io_labels = to_labels(io_spec, prefix_key="io.simcore", trim_key_head=False)
+    service_labels = to_labels(
+        service_spec, prefix_key="simcore.service", trim_key_head=True
+    )
+
+    compose_labels = {**io_labels, **service_labels}
 
     # TODO: require key ends with service-name, i.e. the one listed in docker-compose.yml::services
-    service_name = metadata["key"].split("/")[-1]
+    service_name = io_spec["key"].split("/")[-1]
 
-    if update_compose_labels(compose_spec, json_metadata, service_name):
+    if update_compose_labels(compose_spec, compose_labels, service_name):
         click.echo(
-            f"Updating {compose_file_path} using labels in {metadata_file_path}",
+            f"Updating {compose_file_path} using labels in {io_specs_path}",
         )
         # write the file back
         with compose_file_path.open("w") as fp:
