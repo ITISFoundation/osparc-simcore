5a6cbd05865df19dff92813b747ae95f40033e37
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-11-14 23:42:19 +0100

    linter and arranging new tests

--- packages/service-integration/tests/test_oci_image_spec.py
+++ packages/service-integration/tests/test_oci_image_spec.py
@@ -2,21 +2,33 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
+from pathlib import Path
+
 from service_integration.oci_image_spec import (
     LabelSchemaAnnotations,
     OCIImageSpecAnnotations,
 )
+from service_integration.osparc_config import IOSpecification
 
 
-def test_create_annotations_from_metadata():
+def test_create_annotations_from_metadata(tests_data_dir: Path):
     # metadata -> produce annotations
     # inject as docker labels
     # recover from docker labels
     #
+
+    io_spec = IOSpecification.from_yaml(tests_data_dir / "metadata.yml")
+
+    # map io_spec to OCI image-spec
+    oic_image_spec = OCIImageSpecAnnotations(
+        authors=", ".join([f"{a.name} ({a.email})" for a in io_spec.authors])
+    )
+
+    # TODO: convert oic to ls
+    ls_spec = LabelSchemaAnnotations()
 
     # a base image could contain already annotations
     # retrieve from labels
     # override option or merge/append?
     # NOTE: license has a merge language
     #
-    raise NotImplementedError
