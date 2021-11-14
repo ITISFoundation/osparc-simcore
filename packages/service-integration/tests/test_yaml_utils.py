f0c72b7fb21dc8b2e8cdd7f8abf92d3e732ae236
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-11-14 19:27:24 +0100

    fixes extended yaml

--- packages/service-integration/tests/test_yaml_utils.py
+++ packages/service-integration/tests/test_yaml_utils.py
@@ -3,17 +3,66 @@
 # pylint: disable=unused-variable
 
 
+import os
+import shutil
 from pathlib import Path
 
+import pytest
 import yaml
 from service_integration.yaml_utils import yaml_safe_load
 
+#
+# NOTE: cannot create a yaml file because the pre-commit hooks fail
+# to recongnize '!include'  keyword when running YAML checkers
+#
+YAML_BODY = """\
+compose-spec: !include {0}
+paths-mapping:
+  inputs_path: "/config/workspace/inputs"
+  outputs_path: "/config/workspace/outputs"
+  state_paths:
+    - "/config"
+settings:
+  - name: resources
+    type: Resources
+    value:
+      mem_limit: 17179869184
+      cpu_limit: 1000000000
+  - name: ports
+    type: int
+    value: 8443
+  - name: constraints
+    type: string
+    value:
+      - node.platform.os == linux
+"""
 
-def test_include_file_in_yaml(tests_data_dir: Path):
 
-    expected = yaml.safe_load((tests_data_dir / "compose-spec.yml").read_text())
+@pytest.fixture
+def compose_spec_path(tests_data_dir: Path, tmp_path: Path):
+    dst = tmp_path / "compose-spec.yml"
+    shutil.copyfile(tests_data_dir / "compose-spec.yml", dst)
+    assert dst.exists()
+    return dst
 
-    with open(tests_data_dir / "service.yml") as fh:
+
+@pytest.fixture
+def service_config_path(tmp_path: Path, compose_spec_path):
+    dirpath = tmp_path / "labels"
+    dirpath.mkdir(parents=True, exist_ok=True)
+
+    filepath = dirpath / "service-with-include.yml"
+    filepath.write_text(
+        YAML_BODY.format(os.path.relpath(compose_spec_path.resolve(), filepath.parent))
+    )
+    return filepath
+
+
+def test_include_file_in_yaml(compose_spec_path: Path, service_config_path: Path):
+
+    expected = yaml.safe_load(compose_spec_path.read_text())
+
+    with open(service_config_path) as fh:
         data = yaml_safe_load(fh)
 
     # compose-spec: !include ./compose-spec.yml
