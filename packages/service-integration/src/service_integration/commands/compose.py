d316770ff34bc1ef36adba4f6c056b548bfad57c
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-11-14 22:53:12 +0100

    fast arrangement to revert operation

--- packages/service-integration/src/service_integration/commands/compose.py
+++ packages/service-integration/src/service_integration/commands/compose.py
@@ -1,10 +1,14 @@
+import json
 from contextlib import suppress
 from pathlib import Path
-from typing import IO, Dict
+from typing import Dict
 
 import click
 import yaml
+from pydantic import ValidationError
+from pydantic.main import BaseModel
 
+from ..compose_spec_model import ComposeSpecification
 from ..labels_annotations import to_labels
 from ..models import ComposeSpecDict
 from ..oci_image_spec import LS_LABEL_PREFIX, OCI_LABEL_PREFIX
@@ -12,62 +16,10 @@ from ..osparc_config import IOSpecification, ServiceSpecification
 from ..osparc_image_specs import create_image_spec
 
 
-def load_compose_spec(compose_file: Path) -> ComposeSpecDict:
-    # TODO: auto-generate minimal: catch FileNotFoundError and deduce minimal from model
-    with compose_file.open() as fp:
-        # TODO: validate with docker-compose config?
-        return yaml.safe_load(fp)
-
-
-def update_compose_labels(
-    compose_spec: ComposeSpecDict, labels: Dict[str, str], service_name: str
-) -> bool:
-    current_labels = compose_spec["services"][service_name]["build"]["labels"]
-    updated_labels = {key: current_labels[key] for key in sorted(current_labels.keys())}
-
-    changed: bool = list(current_labels.keys()) != list(updated_labels.keys())
-    for key, value in labels.items():
-        if key in updated_labels:
-            if updated_labels[key] == value:
-                continue
-        updated_labels[key] = value
-        changed = True
-
-    if changed:
-        compose_spec["services"][service_name]["build"]["labels"] = updated_labels
-
-    return changed
-
-
-@click.command()
-@click.option(
-    "--spec-file",
-    "-f",
-    "compose_spec_path",
-    help="The compose file where labels shall be updated",
-    type=Path,
-    default=Path("docker-compose.yml"),
-)
-@click.option(
-    "--metadata",
-    "--io-specs",
-    "io_specs_path",
-    help="info and i/o specs",
-    type=Path,
-    required=False,
-    default=".osparc/labels/metadata.yml",
-)
-@click.option(
-    "--service-specs",
-    "service_specs_path",
-    help="specs for the sidecar",
-    type=Path,
-    default=".osparc/labels/simcore.service.yml",
-)
-def main(compose_spec_path: Path, io_specs_path: Path, service_specs_path: Path):
-    """Updates or creates a docker-compose file with json files in a path"""
-
-    click.echo("Updating component labels")
+def create_docker_compose_image_spec(
+    compose_spec_path, io_specs_path, service_specs_path
+):
+    click.echo(f"Creating docker-compose build  -> {compose_spec_path}")
 
     # specs -> labels
 
@@ -105,17 +57,101 @@ def main(compose_spec_path: Path, io_specs_path: Path, service_specs_path: Path)
 
     compose_spec = create_image_spec(io_spec, service_spec, extra_labels=extra_labels)
 
-    with compose_spec_path.open("w") as fh:
+    with compose_spec_path.open("wt") as fh:
         yaml.safe_dump(
             compose_spec.dict(exclude_unset=True),
             fh,
             default_flow_style=False,
             sort_keys=False,
         )
-        click.echo("Update completed")
+
+    click.echo("Compose-spec completed")
+
+
+def create_osparc_specs(
+    compose_spec_path: Path,
+    io_specs_path: Path = Path("metadata.yml"),
+    service_specs_path: Path = Path("simcore.service.yml"),
+):
+    """Creates io and service specs out of existing compose-spec"""
+
+    click.echo(f"Creating osparc config files from {compose_spec_path}")
+
+    compose_spec = ComposeSpecification.parse_obj(
+        yaml.safe_load(compose_spec_path.read_text())
+    )
+
+    if compose_spec.services:
+
+        def _save(filename: Path, model: BaseModel):
+            with filename.open("wt") as fh:
+                data = json.loads(
+                    model.json(exclude_unset=True, by_alias=True, exclude_none=True)
+                )
+                yaml.safe_dump(data, fh, sort_keys=False)
+
+        for service_name in compose_spec.services:
+            try:
+                labels = compose_spec.services[service_name].build.labels
+                if labels:
+                    if isinstance(labels, list):
+                        labels: Dict[str, str] = dict(
+                            item.strip().split("=") for item in labels
+                        )
+                    assert isinstance(labels.__root__, dict)
+                    labels = labels.__root__
+
+                io_spec = IOSpecification.from_labels_annotations(labels)
+
+                _save(
+                    io_specs_path.with_name(f"{io_specs_path.stem}-{service_name}.yml"),
+                    io_spec,
+                )
+
+                service_spec = ServiceSpecification.from_labels_annotations(labels)
+                _save(
+                    service_specs_path.with_name(
+                        f"{service_specs_path.stem}-{service_name}.yml"
+                    ),
+                    service_spec,
+                )
+
+            except (AttributeError, ValidationError, TypeError) as err:
+                click.echo(
+                    f"WARNING: failure producing specs for {service_name}: {err}"
+                )
+
+        click.echo("osparc config files created")
 
 
 def _tmp(compose_spec_path, io_spec, service_name, compose_labels, io_specs_path):
+    def load_compose_spec(compose_file: Path) -> ComposeSpecDict:
+        # TODO: auto-generate minimal: catch FileNotFoundError and deduce minimal from model
+        with compose_file.open() as fp:
+            # TODO: validate with docker-compose config?
+            return yaml.safe_load(fp)
+
+    def update_compose_labels(
+        compose_spec: ComposeSpecDict, labels: Dict[str, str], service_name: str
+    ) -> bool:
+        current_labels = compose_spec["services"][service_name]["build"]["labels"]
+        updated_labels = {
+            key: current_labels[key] for key in sorted(current_labels.keys())
+        }
+
+        changed: bool = list(current_labels.keys()) != list(updated_labels.keys())
+        for key, value in labels.items():
+            if key in updated_labels:
+                if updated_labels[key] == value:
+                    continue
+            updated_labels[key] = value
+            changed = True
+
+        if changed:
+            compose_spec["services"][service_name]["build"]["labels"] = updated_labels
+
+        return changed
+
     # load if exists
     if compose_spec_path.exists():
         compose_spec = load_compose_spec(compose_spec_path)
@@ -134,6 +170,52 @@ def _tmp(compose_spec_path, io_spec, service_name, compose_labels, io_specs_path
             click.echo("Update completed")
     else:
         click.echo("No update necessary")
+
+
+####### TMP -----
+
+
+@click.command()
+@click.option(
+    "--spec-file",
+    "-f",
+    "compose_spec_path",
+    help="The compose file where labels shall be updated",
+    type=Path,
+    default=Path("docker-compose.yml"),
+)
+@click.option(
+    "--metadata",
+    "--io-specs",
+    "io_specs_path",
+    help="info and i/o specs",
+    type=Path,
+    required=False,
+    default=".osparc/labels/metadata.yml",
+)
+@click.option(
+    "--service-specs",
+    "service_specs_path",
+    help="specs for the sidecar",
+    type=Path,
+    default=".osparc/labels/simcore.service.yml",
+)
+@click.option("--revert/--no-revert", default=False)
+def main(
+    compose_spec_path: Path,
+    io_specs_path: Path,
+    service_specs_path: Path,
+    revert: bool = False,
+):
+    """Updates or creates a docker-compose file with json files in a path"""
+
+    if revert:
+        create_osparc_specs(compose_spec_path, io_specs_path, service_specs_path)
+        return
+
+    create_docker_compose_image_spec(
+        compose_spec_path, io_specs_path, service_specs_path
+    )
 
 
 if __name__ == "__main__":
