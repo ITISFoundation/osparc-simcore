3b962b877612945451882e9edf4638381d1b0445
odeimaiz <maiz@itis.swiss>
2021-11-16 18:35:54 +0100

    refactoring

--- services/web/client/source/class/osparc/desktop/CollapseWithUserMenu.js
+++ services/web/client/source/class/osparc/desktop/CollapseWithUserMenu.js
@@ -33,65 +33,85 @@ qx.Class.define("osparc.desktop.CollapseWithUserMenu", {
   },
 
   members: {
+    _createChildControlImpl: function(id) {
+      let control;
+      switch (id) {
+        case "separator":
+          control = new qx.ui.toolbar.Separator().set({
+            padding: 0,
+            margin: 0,
+            backgroundColor: "contrasted-background++"
+          });
+          this._add(control);
+          break;
+        case "back-to-dashboard-button":
+          control = new osparc.ui.form.FetchButton(this.tr("Dashboard"), "@FontAwesome5Solid/arrow-left/16").set({
+            font: "text-14",
+            backgroundColor: "contrasted-background+"
+          });
+          osparc.utils.Utils.setIdToWidget(control, "dashboardBtn");
+          control.addListener("execute", () => this.fireEvent("backToDashboardPressed"));
+          this._add(control);
+          break;
+        case "user-menu-button":
+          control = new osparc.navigation.UserMenuButton().set({
+            backgroundColor: "contrasted-background+"
+          });
+          osparc.io.WatchDog.getInstance().bind("online", control, "backgroundColor", {
+            converter: on => on ? "contrasted-background+" : "red"
+          });
+          control.getChildControl("label").exclude();
+          control.getMenu().set({
+            backgroundColor: "contrasted-background+"
+          });
+          control.populateExtendedMenu();
+          this._add(control);
+          break;
+        case "collapse-expand-stack": {
+          control = new qx.ui.container.Stack();
+
+          const collapseNavBarBtn = new qx.ui.form.Button(null, "@FontAwesome5Solid/chevron-up/14").set({
+            backgroundColor: "contrasted-background+"
+          });
+          control.add(collapseNavBarBtn);
+          collapseNavBarBtn.addListener("execute", () => {
+            this.getChildControl("separator").show();
+            this.getChildControl("back-to-dashboard-button").show();
+            this.getChildControl("user-menu-button").show();
+            control.setSelection([control.getSelectables()[1]]);
+            this.fireEvent("collapseNavBar");
+          });
+
+          const expandNavBarBtn = new qx.ui.form.Button(null, "@FontAwesome5Solid/chevron-down/14").set({
+            backgroundColor: "contrasted-background+"
+          });
+          control.add(expandNavBarBtn);
+          expandNavBarBtn.addListener("execute", () => {
+            this.getChildControl("separator").exclude();
+            this.getChildControl("back-to-dashboard-button").exclude();
+            this.getChildControl("user-menu-button").exclude();
+            control.setSelection([control.getSelectables()[0]]);
+            this.fireEvent("expandNavBar");
+          });
+
+          this._add(control);
+          break;
+        }
+      }
+      return control || this.base(arguments, id);
+    },
+
     __buildLayout: function() {
-      const separator = new qx.ui.toolbar.Separator().set({
-        padding: 0,
-        margin: 0,
-        backgroundColor: "contrasted-background++"
-      });
+      const separator = this.getChildControl("separator");
       separator.exclude();
-      this._add(separator);
 
-      const closeStudyButton = new osparc.ui.form.FetchButton(this.tr("Dashboard"), "@FontAwesome5Solid/arrow-left/16").set({
-        font: "text-14",
-        backgroundColor: "contrasted-background+"
-      });
-      osparc.utils.Utils.setIdToWidget(closeStudyButton, "dashboardBtn");
-      closeStudyButton.addListener("execute", () => this.fireEvent("backToDashboardPressed"));
-      closeStudyButton.exclude();
-      this._add(closeStudyButton);
-      const userMenuButton = new osparc.navigation.UserMenuButton().set({
-        backgroundColor: "contrasted-background+"
-      });
-      osparc.io.WatchDog.getInstance().bind("online", userMenuButton, "backgroundColor", {
-        converter: on => on ? "contrasted-background+" : "red"
-      });
-      userMenuButton.getChildControl("label").exclude();
-      userMenuButton.getMenu().set({
-        backgroundColor: "contrasted-background+"
-      });
-      userMenuButton.populateExtendedMenu();
+      const backToDashboardBtn = this.getChildControl("back-to-dashboard-button");
+      backToDashboardBtn.exclude();
+
+      const userMenuButton = this.getChildControl("user-menu-button");
       userMenuButton.exclude();
-      this._add(userMenuButton);
 
-
-      const collapseExpandNavBarStack = new qx.ui.container.Stack();
-
-      const collapseNavBarBtn = new qx.ui.form.Button(null, "@FontAwesome5Solid/chevron-up/14").set({
-        backgroundColor: "contrasted-background+"
-      });
-      collapseExpandNavBarStack.add(collapseNavBarBtn);
-      collapseNavBarBtn.addListener("execute", () => {
-        separator.show();
-        closeStudyButton.show();
-        userMenuButton.show();
-        collapseExpandNavBarStack.setSelection([collapseExpandNavBarStack.getSelectables()[1]]);
-        this.fireEvent("collapseNavBar");
-      });
-
-      const expandNavBarBtn = new qx.ui.form.Button(null, "@FontAwesome5Solid/chevron-down/14").set({
-        backgroundColor: "contrasted-background+"
-      });
-      collapseExpandNavBarStack.add(expandNavBarBtn);
-      expandNavBarBtn.addListener("execute", () => {
-        separator.exclude();
-        closeStudyButton.exclude();
-        userMenuButton.exclude();
-        collapseExpandNavBarStack.setSelection([collapseExpandNavBarStack.getSelectables()[0]]);
-        this.fireEvent("expandNavBar");
-      });
-
-      this._add(collapseExpandNavBarStack);
+      this.getChildControl("collapse-expand-stack");
     }
   }
 });
