772bc9b0b5ffd27e42d770d00cee7d0ebcc37791
odeimaiz <maiz@itis.swiss>
2021-11-18 17:27:29 +0100

    [skip ci] dragging on FileDrop

--- services/web/client/source/class/osparc/file/FileDrop.js
+++ services/web/client/source/class/osparc/file/FileDrop.js
@@ -25,6 +25,15 @@ qx.Class.define("osparc.file.FileDrop", {
     this.set({
       droppable: true
     });
+    [
+      "dragover", // on target (pointer over)
+      "dragleave" // on target (pointer out)
+    ].forEach(signalName => {
+      this.addListener(signalName, e => {
+        const dragging = signalName !== "dragleave";
+        this.__dragging(e, dragging);
+      }, this);
+    });
 
     this.getContentElement().setStyles(this.self().getBorderStyle());
 
@@ -37,13 +46,14 @@ qx.Class.define("osparc.file.FileDrop", {
       return {
         "border-radius": "20px",
         "border-color": qx.theme.manager.Color.getInstance().resolve("contrasted-background+"),
-        // "border-color": qx.theme.manager.Color.getInstance().resolve("blue"),
         "border-style": "dotted"
       };
     }
   },
 
   members: {
+    __dropHint: null,
+
     _createChildControlImpl: function(id) {
       let control = null;
       switch (id) {
@@ -69,13 +79,55 @@ qx.Class.define("osparc.file.FileDrop", {
             left: 0
           });
           break;
-        case "drop-me":
-          control = new qx.ui.basic.Label(this.tr("Drop me")).set({
-            font: "title-14"
-          });
-          this._add(control);
-          break;
       }
+    },
+
+    __getLeftOffset: function() {
+      const leftOffset = window.innerWidth - this.getInnerSize().width;
+      return leftOffset;
+    },
+
+    __getTopOffset: function() {
+      const topOffset = window.innerHeight - this.getInnerSize().height;
+      return topOffset;
+    },
+
+    __pointerEventToScreenPos: function(e) {
+      return {
+        x: e.getDocumentLeft() - this.__getLeftOffset(),
+        y: e.getDocumentTop() - this.__getTopOffset()
+      };
+    },
+
+    __dragging: function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      let posX = 0;
+      let posY = 0;
+      if ("offsetX" in e && "offsetY" in e) {
+        posX = e.offsetX + 2;
+        posY = e.offsetY + 2;
+      } else {
+        const pos = this.__pointerEventToScreenPos(e);
+        posX = pos.x;
+        posY = pos.y;
+      }
+
+      if (this.__dropHint === null) {
+        const dropHint = this.__dropHint = new qx.ui.basic.Label(this.tr("Drop me")).set({
+          font: "title-14",
+          textAlign: "center"
+        });
+        this._add(dropHint);
+        const svgLayer = this.getChildControl("svg-layer");
+        dropHint.rect = svgLayer.drawDashedRect(150, 80, posX, posY);
+      }
+      this.__dropHint.setLayoutProperties({
+        left: posX,
+        top: posY
+      });
+      osparc.component.workbench.SvgWidget.updateRect(this.__dropHint.rect, posX, posY);
     }
   }
 });
