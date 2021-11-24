b1ebaa3153845781cf934ac607bdd11b0cdb917f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-11-24 16:40:14 +0100

    adds checpoints/HEAD entrypoint

--- services/web/server/tests/unit/with_dbs/10/meta/test_version_control.py
+++ services/web/server/tests/unit/with_dbs/10/meta/test_version_control.py
@@ -7,6 +7,7 @@ from typing import Any, Dict
 import pytest
 from models_library.projects import Workbench
 from openapi_core.schema.specs.models import Spec as OpenApiSpecs
+from pydantic import BaseModel
 from simcore_service_webserver._meta import API_VTAG as VX
 from simcore_service_webserver.version_control import version_control_handlers
 from simcore_service_webserver.version_control_db import compute_workbench_checksum
