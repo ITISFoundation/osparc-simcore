e4d3c5b9108a4e326d1c580683bd59c634e88f8c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-11-26 09:25:53 +0100

    adding test for dask client acquisition

--- services/director-v2/tests/unit/test_modules_dask_clients_pool.py
+++ services/director-v2/tests/unit/test_modules_dask_clients_pool.py
@@ -3,11 +3,21 @@
 # pylint:disable=redefined-outer-name
 
 
-from typing import Any, Dict
+from random import choice
+from typing import Any, Callable, Dict, List
 
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
+from faker import Faker
+from models_library.clusters import (
+    Cluster,
+    JupyterHubTokenAuthentication,
+    KerberosAuthentication,
+    NoAuthentication,
+    SimpleAuthentication,
+)
 from pytest_mock.plugin import MockerFixture
+from simcore_postgres_database.models.clusters import ClusterType
 from simcore_service_director_v2.core.application import init_app
 from simcore_service_director_v2.core.errors import ConfigurationError
 from simcore_service_director_v2.core.settings import AppSettings
@@ -62,3 +72,64 @@ def test_dask_clients_pool_properly_setup_and_deleted(
     with TestClient(app, raise_server_exceptions=True) as client:
         mocked_dask_clients_pool.create.assert_called_once()
     mocked_dask_clients_pool.delete.assert_called_once()
+
+
+@pytest.fixture
+def fake_clusters(faker: Faker) -> Callable[[int], List[Cluster]]:
+    def creator(num_clusters: int) -> List[Cluster]:
+        fake_clusters = []
+        for n in range(num_clusters):
+            fake_clusters.append(
+                Cluster.parse_obj(
+                    {
+                        "id": faker.pyint(),
+                        "name": faker.name(),
+                        "type": ClusterType.ON_PREMISE,
+                        "owner": faker.pyint(),
+                        "endpoint": faker.uri(),
+                        "authentication": choice(
+                            [
+                                NoAuthentication(),
+                                SimpleAuthentication(
+                                    username=faker.user_name(),
+                                    password=faker.password(),
+                                ),
+                                KerberosAuthentication(),
+                                JupyterHubTokenAuthentication(api_token=faker.uuid4()),
+                            ]
+                        ),
+                    }
+                )
+            )
+        return fake_clusters
+
+    return creator
+
+
+async def test_dask_clients_pool_acquisition_creates_client_on_demand(
+    minimal_dask_config: None,
+    mocker: MockerFixture,
+    client: TestClient,
+    fake_clusters: Callable[[int], List[Cluster]],
+):
+    mocked_dask_client = mocker.patch(
+        "simcore_service_director_v2.modules.dask_clients_pool.DaskClient",
+        autospec=True,
+    )
+    clients_pool = DaskClientsPool.instance(client.app)
+    mocked_dask_client.assert_not_called()
+
+    clusters = fake_clusters(30)
+    for cluster in clusters:
+        async with clients_pool.acquire(cluster) as dask_client:
+            # on start it is created
+            mocked_dask_client.create.assert_called_once_with(
+                app=client.app, settings=client.app.state.settings.DASK_SCHEDULER
+            )
+
+        import pdb
+
+        pdb.set_trace()
+        async with clients_pool.acquire(cluster) as dask_client:
+            # the connection already exists
+            mocked_dask_client.create.assert_not_called()
