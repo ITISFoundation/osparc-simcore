3a3467aab60de7ffc94dfcab04f056cebab6afaa
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-12-02 18:05:23 +0100

    updates dask-sidecar

--- services/dask-sidecar/setup.py
+++ services/dask-sidecar/setup.py
@@ -1,34 +1,32 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import re
 import sys
 from pathlib import Path
+from typing import Set
 
 from setuptools import find_packages, setup
 
-if sys.version_info.major != 3 and sys.version_info.minor != 6:
-    raise RuntimeError(
-        "Expected ~=3.6, got %s. Did you forget to activate virtualenv?"
-        % str(sys.version_info)
-    )
 
-current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
-
-
-def read_reqs(reqs_path: Path):
-    return re.findall(r"(^[^#-][\w]+[-~>=<.\w]+)", reqs_path.read_text(), re.MULTILINE)
-
-
-readme = (current_dir / "README.md").read_text()
-version = (current_dir / "VERSION").read_text().strip()
-
-install_requirements = read_reqs(current_dir / "requirements" / "_base.txt")
-test_requirements = read_reqs(current_dir / "requirements" / "_test.txt")
+def read_reqs(reqs_path: Path) -> Set[str]:
+    return {
+        r
+        for r in re.findall(
+            r"(^[^#\n-][\w\[,\]]+[-~>=<.\w]*)",
+            reqs_path.read_text(),
+            re.MULTILINE,
+        )
+        if isinstance(r, str)
+    }
 
 
-setup(
+CURRENT_DIR = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+
+INSTALL_REQUIREMENTS = tuple(read_reqs(CURRENT_DIR / "requirements" / "_base.txt"))
+TEST_REQUIREMENTS = tuple(read_reqs(CURRENT_DIR / "requirements" / "_test.txt"))
+
+
+SETUP = dict(
     name="simcore-service-dask-sidecar",
-    version=version,
+    version=(CURRENT_DIR / "VERSION").read_text().strip(),
     author="Pedro Crespo-Valero (pcrespov)",
     description="A dask-worker that runs as a sidecar",
     classifiers=[
@@ -37,15 +35,19 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3.8",
     ],
-    long_description=readme,
+    long_description=(CURRENT_DIR / "README.md").read_text(),
     license="MIT license",
     python_requires="~=3.8",
     packages=find_packages(where="src"),
     package_dir={
         "": "src",
     },
-    install_requires=install_requirements,
+    install_requires=INSTALL_REQUIREMENTS,
     test_suite="tests",
-    tests_require=test_requirements,
-    extras_require={"test": test_requirements},
+    tests_require=TEST_REQUIREMENTS,
+    extras_require={"test": TEST_REQUIREMENTS},
 )
+
+
+if __name__ == "__main__":
+    setup(**SETUP)
