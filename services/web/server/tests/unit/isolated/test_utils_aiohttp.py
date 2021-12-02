5feb4ed41f2a8c61aa688784466fa035dcc56285
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-12-02 17:23:01 +0100

    WIP

--- services/web/server/tests/unit/isolated/test_utils_aiohttp.py
+++ services/web/server/tests/unit/isolated/test_utils_aiohttp.py
@@ -2,7 +2,8 @@ from simcore_service_webserver.utils_aiohttp import enveloped_json_response
 
 
 def test_enveloped_response():
-    raise NotImplemented
+
+    raise NotImplementedError()
     # TODO: implement Envelop with generics
     # TODO: replace all envelope functionality form packages/service-library/src/servicelib/aiohttp/rest_responses.py
     # TODO: create decorator instead of middleware to envelope handler responses
