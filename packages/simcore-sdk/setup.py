7f2d926ab80126812a41b5cef9449ffd18628de4
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-12-02 17:52:04 +0100

    simcoresdk setup

--- packages/simcore-sdk/setup.py
+++ packages/simcore-sdk/setup.py
@@ -1,33 +1,44 @@
 import re
 import sys
 from pathlib import Path
+from typing import Set
 
 from setuptools import find_packages, setup
 
-here = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+CURRENT_DIR = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
 
-def read_reqs(reqs_path: Path):
-    return re.findall(
-        r"(^[^#\n-][\w\[,\]]+[-~>=<.\w]*)", reqs_path.read_text(), re.MULTILINE
-    )
+def read_reqs(reqs_path: Path) -> Set[str]:
+    return {
+        r
+        for r in re.findall(
+            r"(^[^#\n-][\w\[,\]]+[-~>=<.\w]*)",
+            reqs_path.read_text(),
+            re.MULTILINE,
+        )
+        if isinstance(r, str)
+    }
 
 
-install_requirements = read_reqs(here / "requirements" / "_base.in")
-test_requirements = read_reqs(here / "requirements" / "_test.txt") + [
+INSTALL_REQUIREMENTS = read_reqs(CURRENT_DIR / "requirements" / "_base.in")
+TEST_REQUIREMENTS = read_reqs(CURRENT_DIR / "requirements" / "_test.txt") | {
     "simcore-postgres-database",
     "simcore-service-library",
     "simcore-models-library",
-]
+}
 
-setup(
+SETUP = dict(
     name="simcore-sdk",
     version="1.0.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     python_requires=">=3.6",
-    install_requires=install_requirements,
-    tests_require=test_requirements,
-    extras_require={"test": test_requirements},
+    install_requires=INSTALL_REQUIREMENTS,
+    tests_require=TEST_REQUIREMENTS,
+    extras_require={"test": TEST_REQUIREMENTS},
     test_suite="tests",
 )
+
+
+if __name__ == "__main__":
+    setup(**SETUP)
