b0403252d26687b06ceea6da6932c152669502d5
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-12-06 21:17:46 +0100

    mutations: drafts tests

--- packages/models-library/tests/test_utils_models_mutations_factory.py
+++ packages/models-library/tests/test_utils_models_mutations_factory.py
@@ -4,56 +4,130 @@
 
 from typing import Dict, Optional
 
-from models_library.utils.models_mutations_factory import extract_fields
-from pydantic import BaseModel, Field, create_model
+from models_library.generics import Envelope
+from models_library.rest_pagination import Page
+from models_library.utils.models_mutations_factory import (
+    create_model_for_replace_as,
+    create_model_for_update_as,
+    extract_fields,
+)
+from pydantic import BaseModel, validator
+from pydantic.types import PositiveInt
+
+# HELPERS -----
 
 
 def copy_dict(obj: Dict, *, exclude={}):
     return {k: v for k, v in obj.items() if k not in exclude}
 
 
+def name_must_contain_space(v):
+    if " " not in v:
+        raise ValueError("must contain a space")
+    return v.title()
+
+
+def passwords_match(v, values, **kwargs):
+    password_ref = values.get("password")
+    if password_ref is None:
+        raise ValueError("reference password missing")
+    elif v != password_ref:
+        raise ValueError("passwords do not match")
+    return v
+
+
+def username_alphanumeric(v):
+    assert v.isalnum(), "must be alphanumeric"
+    return v
+
+
 class User(BaseModel):
-    user_id: int = Field(...)
-    username: Optional[str] = "Anonymous"
-    password: Optional[str]
+    """Domain model"""
 
+    id: PositiveInt
+    display_name: str
+    username: str
+    password: str
 
-def test_create_reduced_model():
-    class UserOut_expected(BaseModel):
-        user_id: int = Field(...)
-        username: Optional[str] = "Anonymous"
-        password: Optional[str]
-
-    print(UserOut_expected.schema_json(indent=1))
-
-    UserOut = create_model("UserOut", **extract_fields(User, exclude={"password"}))
-    print(UserOut.schema_json(indent=1))
-
-    assert not issubclass(UserOut, BaseModel)
-
-    assert copy_dict(UserOut.schema(), exclude={"title"}) == copy_dict(
-        UserOut.schema(), exclude={"title"}
+    # validators when model created in code
+    _name_must_contain_space = validator("display_name", allow_reuse=True)(
+        name_must_contain_space
     )
-    assert not issubclass(UserOut, User)
+    _username_alphanumeric = validator("username", allow_reuse=True)(
+        username_alphanumeric
+    )
 
 
-# UserOut = create_model_for_replace_as(User, exclude={"password"})
-# class UserOut(User):
-#     password: str = Field(..., exclude=True)
+class UserCreate(BaseModel):
+    """in -> Model for body of POST /users"""
+
+    display_name: str
+    username: str
+    password: str
+    password2: str
+
+    # parses json-body from Create request
+    _name_must_contain_space = validator("display_name", allow_reuse=True)(
+        name_must_contain_space
+    )
+    _username_alphanumeric = validator("username", allow_reuse=True)(
+        username_alphanumeric
+    )
+    _passwords_match = validator("password2", allow_reuse=True)(passwords_match)
 
 
-# class UserInCreate(User):
-#     user_id: str = Field(..., exclude=True)  # this is decided on the server-side
-#     repeat_password: str = Field(...)
+class UserUpdate(BaseModel):
+    """in -> Model for body of PATCH /users/{id}"""
+
+    display_name: Optional[str]
+    username: Optional[str]
+    password: Optional[str]
+    password2: Optional[str]
+
+    # parses json-body from Update request
+    _name_must_contain_space = validator("display_name", allow_reuse=True)(
+        name_must_contain_space
+    )
+    _username_alphanumeric = validator("username", allow_reuse=True)(
+        username_alphanumeric
+    )
+    _passwords_match = validator("password2", allow_reuse=True)(passwords_match)
 
 
-# UserInUpdate = create_model_for_update_as(UserInCreate, exclude=None)
+# Model for body of PUT /users/{id}
+UserReplace = UserCreate
 
 
-# class UserInUpdate(User):
-#     username: Optional[str]
-#     password: Optional[str]
-#     repeat_password: str = Field(...)
+class UserGet(BaseModel):
+    """<- out Detailed model for response in GET /users/{id}"""
+
+    id: int
+    display_name: str
+    username: str
+
+    # parses from User (i.e. validated domain model)
 
 
-# print(UserOut.__fields__)
+UserGetEnveloped = Envelope[UserGet]
+
+
+class UserListItem(BaseModel):
+    """<- out Item model for response in GET /users"""
+
+    id: int
+    username: str
+
+    # parses from User
+
+
+UsersList = Page[UserListItem]
+
+
+# TESTS ----------------------
+
+
+def test_mutations_factories():
+
+    _UserCreate = create_model_for_replace_as(User, exclude={"id"})
+
+    assert UserCreate == _UserCreate
