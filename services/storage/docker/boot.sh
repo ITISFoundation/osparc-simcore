0159b0aab591325fb04522c7e0399e019a0d09a3
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-12-06 12:52:13 +0100

    Extend ignore pattern for auto-restart in webserver and storage

--- services/storage/docker/boot.sh
+++ services/storage/docker/boot.sh
@@ -25,7 +25,7 @@ if [ "${SC_BUILD_TARGET}" = "development" ]; then
   pip list | sed 's/^/    /'
 
   echo "$INFO" "Setting entrypoint to use watchmedo autorestart..."
-  entrypoint='watchmedo auto-restart --recursive --pattern="*.py" --'
+  entrypoint='watchmedo auto-restart --recursive --pattern="*.py;*/src/*" --ignore-patterns="*test*" --ignore-directories --directory /devel/services/storage/src --directory /devel/packages --'
 
 elif [ "${SC_BUILD_TARGET}" = "production" ]; then
   entrypoint=""
