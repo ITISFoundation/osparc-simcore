983c4d58a1a99d603c677e674b764a4fe88146d4
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-12-06 20:19:25 +0100

    pagination: keeps only utils in servicelib

--- packages/service-library/src/servicelib/rest_pagination_utils.py
+++ packages/service-library/src/servicelib/rest_pagination_utils.py
@@ -1,198 +1,76 @@
 from math import ceil
-from typing import Any, List, Optional
+from typing import Any, Dict, List, Protocol, TypedDict, Union
 
-from pydantic import (
-    AnyHttpUrl,
-    BaseModel,
-    Extra,
-    Field,
-    NonNegativeInt,
-    PositiveInt,
-    validator,
-)
+from models_library.rest_pagination import PageLinks, PageMetaInfoLimitOffset
 from yarl import URL
 
-DEFAULT_NUMBER_OF_ITEMS_PER_PAGE = 20
+
+class _StarletteURL(Protocol):
+    # Convenience protocol to avoid including starlette in requirements
+    #
+    # SEE starlette.data_structures.URL
+    #  in https://github.com/encode/starlette/blob/master/starlette/datastructures.py#L130
+    #
+
+    def replace_query_params(self, **kwargs: Any) -> "_StarletteURL":
+        ...
 
 
-def monkey_patch_pydantic_url_regex() -> None:
-    # waiting for PR https://github.com/samuelcolvin/pydantic/pull/2512 to be released into
-    # pydantic main codebase
-    import pydantic
-
-    if pydantic.VERSION > "1.8.2":
-        raise RuntimeError(
-            (
-                "Please check that PR https://github.com/samuelcolvin/pydantic/pull/2512 "
-                "was merged AND added in this version."
-                "If already present in this version, remove this monkey_patch"
-            )
-        )
-
-    import re
-    from typing import Pattern
-
-    from pydantic import networks
-
-    def url_regex() -> Pattern[str]:
-        _url_regex_cache = networks._url_regex_cache  # pylint: disable=protected-access
-        if _url_regex_cache is None:
-            _url_regex_cache = re.compile(
-                r"(?:(?P<scheme>[a-z][a-z0-9+\-.]+)://)?"  # scheme https://tools.ietf.org/html/rfc3986#appendix-A
-                r"(?:(?P<user>[^\s:/]*)(?::(?P<password>[^\s/]*))?@)?"  # user info
-                r"(?:"
-                r"(?P<ipv4>(?:\d{1,3}\.){3}\d{1,3})(?=$|[/:#?])|"  # ipv4
-                r"(?P<ipv6>\[[A-F0-9]*:[A-F0-9:]+\])(?=$|[/:#?])|"  # ipv6
-                r"(?P<domain>[^\s/:?#]+)"  # domain, validation occurs later
-                r")?"
-                r"(?::(?P<port>\d+))?"  # port
-                r"(?P<path>/[^\s?#]*)?"  # path
-                r"(?:\?(?P<query>[^\s#]+))?"  # query
-                r"(?:#(?P<fragment>\S+))?",  # fragment
-                re.IGNORECASE,
-            )
-        return _url_regex_cache
-
-    networks.url_regex = url_regex
+URLType = Union[URL, _StarletteURL]
 
 
-monkey_patch_pydantic_url_regex()
+def _replace_query(url: URLType, query: Dict[str, Any]):
+    """In this repo we use two type of URL-like data structures:
+        - from yarl (aiohttp-style) and
+        - from starlette (fastapi-style).
+
+    This helper function ensures query replacement works with both
+    """
+    if isinstance(url, URL):
+        # yarl URL
+        new_url = url.update_query(query)
+    else:
+        new_url = url.replace_query_params(**query)
+    return f"{new_url}"
 
 
-class PageMetaInfoLimitOffset(BaseModel):
-    limit: PositiveInt = DEFAULT_NUMBER_OF_ITEMS_PER_PAGE
-    total: NonNegativeInt
-    offset: NonNegativeInt = 0
-    count: NonNegativeInt
-
-    @validator("offset")
-    @classmethod
-    def check_offset(cls, v, values):
-        if v > 0 and v >= values["total"]:
-            raise ValueError(
-                f"offset {v} cannot be equal or bigger than total {values['total']}, please check"
-            )
-        return v
-
-    @validator("count")
-    @classmethod
-    def check_count(cls, v, values):
-        if v > values["limit"]:
-            raise ValueError(
-                f"count {v} bigger than limit {values['limit']}, please check"
-            )
-        if v > values["total"]:
-            raise ValueError(
-                f"count {v} bigger than expected total {values['total']}, please check"
-            )
-        if "offset" in values and (values["offset"] + v) > values["total"]:
-            raise ValueError(
-                f"offset {values['offset']} + count {v} is bigger than allowed total {values['total']}, please check"
-            )
-        return v
-
-    class Config:
-        extra = Extra.forbid
-
-        schema_extra = {
-            "examples": [
-                {"total": 7, "count": 4, "limit": 4, "offset": 0},
-            ]
-        }
-
-
-class PageLinks(BaseModel):
-    self: AnyHttpUrl
-    first: AnyHttpUrl
-    prev: Optional[AnyHttpUrl]
-    next: Optional[AnyHttpUrl]
-    last: AnyHttpUrl
-
-    class Config:
-        extra = Extra.forbid
-
-
-class PageResponseLimitOffset(BaseModel):
-    meta: PageMetaInfoLimitOffset = Field(alias="_meta")
-    links: PageLinks = Field(alias="_links")
+class PageDict(TypedDict):
+    _meta: Any
+    _links: Any
     data: List[Any]
 
-    @validator("data", pre=True)
-    @classmethod
-    def convert_none_to_empty_list(cls, v):
-        if v is None:
-            v = []
-        return v
 
-    @validator("data")
-    @classmethod
-    def check_data_compatible_with_meta(cls, v, values):
-        if "meta" not in values:
-            # if the validation failed in meta this happens
-            raise ValueError("meta not in values")
-        if len(v) != values["meta"].count:
-            raise ValueError(
-                f"container size [{len(v)}] must be equal to count [{values['meta'].count}]"
+def paginate_data(
+    data: List[Any],
+    request_url: URLType,
+    total: int,
+    limit: int,
+    offset: int,
+) -> PageDict:
+    """Helper to build page-like objects to feed to Page[X] pydantic model class"""
+    last_page = ceil(total / limit) - 1
+
+    return PageDict(
+        _meta=PageMetaInfoLimitOffset(
+            total=total, count=len(data), limit=limit, offset=offset
+        ),
+        _links=PageLinks(
+            self=_replace_query(request_url, {"offset": offset, "limit": limit}),
+            first=_replace_query(request_url, {"offset": 0, "limit": limit}),
+            prev=_replace_query(
+                request_url, {"offset": max(offset - limit, 0), "limit": limit}
             )
-        return v
-
-    @classmethod
-    def paginate_data(
-        cls,
-        data: List[Any],
-        request_url: URL,
-        total: int,
-        limit: int,
-        offset: int,
-    ) -> "PageResponseLimitOffset":
-        last_page = ceil(total / limit) - 1
-
-        return PageResponseLimitOffset(
-            _meta=PageMetaInfoLimitOffset(
-                total=total, count=len(data), limit=limit, offset=offset
+            if offset > 0
+            else None,
+            next=_replace_query(
+                request_url,
+                {"offset": min(offset + limit, last_page * limit), "limit": limit},
+            )
+            if offset < (last_page * limit)
+            else None,
+            last=_replace_query(
+                request_url, {"offset": last_page * limit, "limit": limit}
             ),
-            _links=PageLinks(
-                self=f"{request_url.update_query({'offset': offset, 'limit': limit})}",
-                first=f"{request_url.update_query({'offset': 0, 'limit': limit})}",
-                prev=f"{request_url.update_query({'offset': max(offset - limit, 0), 'limit': limit})}"
-                if offset > 0
-                else None,
-                next=f"{request_url.update_query({'offset': min(offset + limit, last_page * limit), 'limit': limit})}"
-                if offset < (last_page * limit)
-                else None,
-                last=f"{request_url.update_query({'offset': last_page * limit, 'limit': limit})}",
-            ),
-            data=data,
-        )
-
-    class Config:
-        extra = Extra.forbid
-
-        schema_extra = {
-            "examples": [
-                # first page
-                {
-                    "_meta": {"total": 7, "count": 4, "limit": 4, "offset": 0},
-                    "_links": {
-                        "self": "http://osparc.io/v2/listing?offset=0&limit=4",
-                        "first": "http://osparc.io/v2/listing?offset=0&limit=4",
-                        "prev": None,
-                        "next": "http://osparc.io/v2/listing?offset=1&limit=4",
-                        "last": "http://osparc.io/v2/listing?offset=1&limit=4",
-                    },
-                    "data": ["data 1", "data 2", "data 3", "data 4"],
-                },
-                # second and last page
-                {
-                    "_meta": {"total": 7, "count": 3, "limit": 4, "offset": 1},
-                    "_links": {
-                        "self": "http://osparc.io/v2/listing?offset=1&limit=4",
-                        "first": "http://osparc.io/v2/listing?offset=0&limit=4",
-                        "prev": "http://osparc.io/v2/listing?offset=0&limit=4",
-                        "next": None,
-                        "last": "http://osparc.io/v2/listing?offset=1&limit=4",
-                    },
-                    "data": ["data 5", "data 6", "data 7"],
-                },
-            ]
-        }
+        ),
+        data=data,
+    )
--- packages/service-library/tests/test_rest_pagination_utils.py
+++ packages/service-library/tests/test_rest_pagination_utils.py
@@ -1,60 +1,7 @@
-from copy import deepcopy
-
 import pytest
-from pydantic.main import BaseModel
-from servicelib.rest_pagination_utils import (
-    PageLinks,
-    PageMetaInfoLimitOffset,
-    PageResponseLimitOffset,
-)
+from models_library.rest_pagination import Page, PageLinks, PageMetaInfoLimitOffset
+from servicelib.rest_pagination_utils import PageDict, paginate_data
 from yarl import URL
-
-
-@pytest.mark.parametrize(
-    "cls_model", [PageResponseLimitOffset, PageMetaInfoLimitOffset]
-)
-def test_page_response_limit_offset_models(cls_model: BaseModel):
-    examples = cls_model.Config.schema_extra["examples"]
-
-    for index, example in enumerate(examples):
-        print(f"{index:-^10}:\n", example)
-
-        model_instance = cls_model(**example)
-        assert model_instance
-
-
-def test_invalid_offset():
-    with pytest.raises(ValueError):
-        PageMetaInfoLimitOffset(limit=6, total=5, offset=5, count=2)
-
-
-@pytest.mark.parametrize(
-    "count, offset",
-    [
-        pytest.param(7, 0, id="count bigger than limit"),
-        pytest.param(6, 0, id="count bigger than total"),
-        pytest.param(5, 1, id="count + offset bigger than total"),
-    ],
-)
-def test_invalid_count(count: int, offset: int):
-    with pytest.raises(ValueError):
-        PageMetaInfoLimitOffset(limit=6, total=5, offset=offset, count=count)
-
-
-def test_data_size_does_not_fit_count():
-    example = deepcopy(PageResponseLimitOffset.Config.schema_extra["examples"][0])
-    example["_meta"]["count"] = len(example["data"]) - 1
-    with pytest.raises(ValueError):
-        PageResponseLimitOffset(**example)
-
-
-def test_empty_data_is_converted_to_list():
-    example = deepcopy(PageResponseLimitOffset.Config.schema_extra["examples"][0])
-    example["data"] = None
-    example["_meta"]["count"] = 0
-    model_instance = PageResponseLimitOffset(**example)
-    assert model_instance
-    assert model_instance.data == []
 
 
 @pytest.mark.parametrize(
@@ -77,14 +24,16 @@ def test_paginating_data(base_url):
     total_number_of_data = 29
     limit = 9
     offset = 0
-    partial_data = [range(9)]
+    partial_data = list(range(9))
     request_url = URL(f"{base_url}?some=1&random=4&query=true")
 
     # first "call"
-    model_instance: PageResponseLimitOffset = PageResponseLimitOffset.paginate_data(
+    data_obj: PageDict = paginate_data(
         partial_data, request_url, total_number_of_data, limit, offset
     )
+    assert data_obj
 
+    model_instance = Page[int].parse_obj(data_obj)
     assert model_instance
     assert model_instance.meta == PageMetaInfoLimitOffset(
         total=total_number_of_data, count=len(partial_data), limit=limit, offset=offset
@@ -114,8 +63,10 @@ def test_paginating_data(base_url):
     )
 
     # next "call"s
-    for i in [1, 2]:
-        model_instance: PageResponseLimitOffset = PageResponseLimitOffset.paginate_data(
+    for i in (1, 2):
+        assert model_instance.links.next is not None
+
+        data_obj: PageDict = paginate_data(
             partial_data,
             URL(model_instance.links.next),
             total_number_of_data,
@@ -123,6 +74,7 @@ def test_paginating_data(base_url):
             offset + i * limit,
         )
 
+        model_instance = Page[int].parse_obj(data_obj)
         assert model_instance
         assert model_instance.meta == PageMetaInfoLimitOffset(
             total=total_number_of_data,
@@ -159,15 +111,19 @@ def test_paginating_data(base_url):
         )
 
     # last "call"
-    model_instance: PageResponseLimitOffset = PageResponseLimitOffset.paginate_data(
+    assert model_instance.links.next is not None
+    data_obj: PageDict = paginate_data(
         partial_data,
         URL(model_instance.links.next),
         total_number_of_data,
         limit,
         offset + 3 * limit,
     )
+    assert data_obj
 
+    model_instance = Page[int].parse_obj(data_obj)
     assert model_instance
+
     assert model_instance.meta == PageMetaInfoLimitOffset(
         total=total_number_of_data,
         count=len(partial_data),
