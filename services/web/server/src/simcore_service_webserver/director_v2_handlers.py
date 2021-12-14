e7b5066fe19ab05a8afea9481db12504c614d2bb
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-12-14 11:56:59 +0100

    maintenance: cleanup

--- services/web/server/src/simcore_service_webserver/director_v2_handlers.py
+++ services/web/server/src/simcore_service_webserver/director_v2_handlers.py
@@ -62,7 +62,10 @@ async def start_pipeline(request: web.Request) -> web.Response:
             project_vc_commits,
         ) = await run_policy.get_or_create_runnable_projects(request, project_id)
         log.debug(
-            "Project %s will start %d variants", project_id, len(running_project_ids)
+            "Project %s will start %d variants: %s",
+            f"{project_id=}",
+            len(running_project_ids),
+            f"{running_project_ids=}",
         )
 
         assert running_project_ids  # nosec
