aa4f37fc7175fda3826907dd9d8e26ab3f849b4e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-12-17 16:47:14 +0100

    added more informative logs

--- services/web/server/src/simcore_service_webserver/computation_subscribe.py
+++ services/web/server/src/simcore_service_webserver/computation_subscribe.py
@@ -28,9 +28,9 @@ from .computation_config import get_settings as get_computation_settings
 from .projects import projects_api
 from .projects.projects_exceptions import NodeNotFoundError, ProjectNotFoundError
 from .socketio.events import (
+    SOCKET_IO_EVENT,
     SOCKET_IO_LOG_EVENT,
     SOCKET_IO_NODE_UPDATED_EVENT,
-    SOCKET_IO_EVENT,
     SocketMessageDict,
     send_messages,
 )
@@ -191,8 +191,9 @@ async def setup_rabbitmq_consumer(app: web.Application) -> AsyncIterator[None]:
                 raise
             except Exception:  # pylint: disable=broad-except
                 log.warning(
-                    "unexpected error in consumer for %s, restarting",
+                    "unexpected error in consumer for %s, %s",
                     exchange_name,
+                    "restarting..." if consumer_running else "stopping",
                     exc_info=True,
                 )
 
@@ -233,9 +234,8 @@ async def setup_rabbitmq_consumer(app: web.Application) -> AsyncIterator[None]:
     consumer_running = False
     for task in consumer_tasks:
         task.cancel()
-    with contextlib.suppress(asyncio.CancelledError):
-        for task in consumer_tasks:
-            await task
+    await asyncio.gather(*consumer_tasks, return_exceptions=True)
+
     log.info("Closing connections...")
     await channel_pool.close()
     await connection_pool.close()
