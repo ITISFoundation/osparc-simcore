8cbf97395027b553b4cbdf0a462c761975110464
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-12-20 09:49:44 +0100

    use source method instead of mimicking

--- services/web/server/tests/unit/with_dbs/01/test_comp_tasks_listening_task.py
+++ services/web/server/tests/unit/with_dbs/01/test_comp_tasks_listening_task.py
@@ -3,28 +3,35 @@
 # pylint:disable=redefined-outer-name
 # pylint:disable=no-value-for-parameter
 
-import asyncio
 import json
 import logging
-from typing import Any, Dict, List
+from typing import Any, AsyncIterator, Dict, List
+from unittest import mock
 
 import aiopg.sa
 import pytest
-import tenacity
 from aiopg.sa.result import RowProxy
+from pytest_mock.plugin import MockerFixture
 from servicelib.aiohttp.application_keys import APP_DB_ENGINE_KEY
 from simcore_postgres_database.models.comp_pipeline import StateType
 from simcore_postgres_database.models.comp_tasks import NodeClass, comp_tasks
 from simcore_service_webserver.computation_comp_tasks_listening_task import (
-    comp_tasks_listening_task,
+    setup_comp_tasks_listening_task,
 )
 from sqlalchemy.sql.elements import literal_column
+from tenacity._asyncio import AsyncRetrying
+from tenacity.before_sleep import before_sleep_log
+from tenacity.retry import retry_if_exception_type
+from tenacity.stop import stop_after_delay
+from tenacity.wait import wait_fixed
 
 logger = logging.getLogger(__name__)
 
 
 @pytest.fixture
-async def mock_project_subsystem(mocker) -> Dict:
+async def mock_project_subsystem(
+    mocker: MockerFixture,
+) -> AsyncIterator[Dict[str, mock.MagicMock]]:
     mocked_project_calls = {
         "_get_project_owner": mocker.patch(
             "simcore_service_webserver.computation_comp_tasks_listening_task._get_project_owner",
@@ -42,28 +49,14 @@ async def mock_project_subsystem(mocker) -> Dict:
     yield mocked_project_calls
 
 
-async def test_mock_project_api(loop, mock_project_subsystem: Dict, mocker):
-    from simcore_service_webserver.computation_comp_tasks_listening_task import (
-        _get_project_owner,
-        _update_project_outputs,
-        _update_project_state,
-    )
-
-    assert isinstance(_get_project_owner, mocker.AsyncMock)
-    assert isinstance(_update_project_state, mocker.AsyncMock)
-    assert isinstance(_update_project_outputs, mocker.AsyncMock)
-
-
 @pytest.fixture
 async def comp_task_listening_task(
     loop, mock_project_subsystem: Dict, client
-) -> asyncio.Task:
-    listening_task = loop.create_task(comp_tasks_listening_task(client.app))
-
-    yield listening_task
-
-    listening_task.cancel()
-    await listening_task
+) -> AsyncIterator:
+    await setup_comp_tasks_listening_task(client.app).__anext__()
+    yield
+    # cleanup
+    await setup_comp_tasks_listening_task(client.app).__anext__()
 
 
 @pytest.mark.parametrize(
@@ -98,7 +91,7 @@ async def comp_task_listening_task(
 )
 async def test_listen_comp_tasks_task(
     mock_project_subsystem: Dict,
-    comp_task_listening_task: asyncio.Task,
+    comp_task_listening_task: None,
     client,
     update_values: Dict[str, Any],
     expected_calls: List[str],
@@ -125,20 +118,15 @@ async def test_listen_comp_tasks_task(
         # tests whether listener gets hooked calls executed
         for call_name, mocked_call in mock_project_subsystem.items():
             if call_name in expected_calls:
-                async for attempt in _async_retry_if_fails():
+                async for attempt in AsyncRetrying(
+                    wait=wait_fixed(1),
+                    stop=stop_after_delay(10),
+                    retry=retry_if_exception_type(AssertionError),
+                    before_sleep=before_sleep_log(logger, logging.INFO),
+                    reraise=True,
+                ):
                     with attempt:
                         mocked_call.assert_awaited()
 
             else:
                 mocked_call.assert_not_called()
-
-
-def _async_retry_if_fails():
-    # Helper that retries to account for some uncontrolled delays
-    return tenacity.AsyncRetrying(
-        wait=tenacity.wait_fixed(1),
-        stop=tenacity.stop_after_delay(10),
-        retry=tenacity.retry_if_exception_type(AssertionError),
-        before=tenacity.before_log(logger, logging.INFO),
-        reraise=True,
-    )
