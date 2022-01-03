2c760b8b775fd795813bf7754869e171d9943453
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-01-03 11:04:23 +0100

    added some documentation and name of app

--- services/web/server/docker/boot.sh
+++ services/web/server/docker/boot.sh
@@ -32,22 +32,27 @@ fi
 # RUNNING application ----------------------------------------
 echo "$INFO" "Selected config $APP_CONFIG"
 
+# NOTE: the number of workers ```(2 x $num_cores) + 1``` is the official recommendation [https://docs.gunicorn.org/en/latest/design.html#how-many-workers]
+
 if [ "${SC_BOOT_MODE}" = "debug-ptvsd" ]; then
   # NOTE: needs ptvsd installed
   echo "$INFO" "PTVSD Debugger initializing in port 3000 with ${APP_CONFIG}"
   # exec python3 -m ptvsd --host 0.0.0.0 --port 3000 -m \
-  exec gunicorn simcore_service_webserver.cli:app_factory \
-      --bind 0.0.0.0:8080 \
-      --worker-class aiohttp.GunicornWebWorker \
-      --workers="${WEBSERVER_GUNICORN_WORKERS:-1}" \
-      --reload
 
-    # simcore_service_webserver --config $APP_CONFIG
+  exec gunicorn simcore_service_webserver.cli:app_factory \
+    --bind 0.0.0.0:8080 \
+    --worker-class aiohttp.GunicornWebWorker \
+    --workers="${WEBSERVER_GUNICORN_WORKERS:-1}" \
+    --name="webserver_$(hostname)_$(date +'%Y-%m-%d_%T')_$$" \
+    --reload
+
+  # simcore_service_webserver --config $APP_CONFIG
 
 else
   exec gunicorn simcore_service_webserver.cli:app_factory \
     --bind 0.0.0.0:8080 \
     --worker-class aiohttp.GunicornWebWorker \
-    --workers="${WEBSERVER_GUNICORN_WORKERS:-1}"
+    --workers="${WEBSERVER_GUNICORN_WORKERS:-1}" \
+    --name="webserver_$(hostname)_$(date +'%Y-%m-%d_%T')_$$"
   # exec simcore-service-webserver --config $APP_CONFIG
 fi
