76ea431b5dc47f8d99f56d1b50a9f0cfe570b313
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-05 09:14:50 +0100

    WIP [skip ci]

--- packages/settings-library/src/settings_library/utils_cli.py
+++ packages/settings-library/src/settings_library/utils_cli.py
@@ -6,6 +6,7 @@ from typing import Callable, Optional, Type
 import typer
 from pydantic import ValidationError
 from pydantic.env_settings import BaseSettings
+from pydantic.main import BaseModel
 
 from ._constants import HEADER_STR
 from .base import BaseCustomSettings
@@ -36,6 +37,13 @@ def print_as_json(settings_obj, *, compact=False):
     typer.echo(settings_obj.json(indent=None if compact else 2))
 
 
+def inject_fake_defaults_fields(model_cls: Type[BaseModel]):
+    # copy
+    # fake if not default. fake can be determined by schema and faker
+
+    raise NotImplementedError
+
+
 def create_settings_command(
     settings_cls: Type[BaseCustomSettings], logger: Optional[logging.Logger] = None
 ) -> Callable:
@@ -52,8 +60,11 @@ def create_settings_command(
         as_json_schema: bool = False,
         compact: bool = typer.Option(False, help="Print compact form"),
         verbose: bool = False,
+        fake_missing_envs: bool = typer.Option(
+            False, help="Adds fake value if required field is not captured in the envs"
+        ),
     ):
-        """Resolves settings and prints envfile"""
+        """Resolves settings (i.e. parses envs) and echoes them in different formats"""
 
         if as_json_schema:
             typer.echo(settings_cls.schema_json(indent=0 if compact else 2))
@@ -64,6 +75,17 @@ def create_settings_command(
 
         except ValidationError as err:
             settings_schema = settings_cls.schema_json(indent=2)
+
+            if fake_missing_envs:
+                # take schema
+                # create isolated environ? subprocess??
+                # add missing variable reported in Validationerror using schema info
+                # transfer back json instance
+                # create settings_obj using constructor or
+                import pdb
+
+                pdb.set_trace()
+                raise NotImplementedError()
 
             assert logger is not None  # nosec
             logger.error(
