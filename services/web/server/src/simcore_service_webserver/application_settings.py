87dc5ab45c01a7f4d84409ec47502a52703e21d1
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-07 19:24:58 +0100

    Converted application_settings.py

--- services/web/server/src/simcore_service_webserver/application_settings.py
+++ services/web/server/src/simcore_service_webserver/application_settings.py
@@ -1,13 +1,10 @@
-"""
-TODO: pydantic settings comming soon and replacing trafaret
-
-"""
 import logging
-from typing import Dict, Optional
+from typing import Any, Dict, Optional
 
 from aiohttp import web
 from models_library.basic_types import BootModeEnum, BuildTargetEnum
-from pydantic import BaseSettings, Field
+from pydantic import Field
+from settings_library.base import BaseCustomSettings
 
 from ._constants import APP_SETTINGS_KEY
 from ._meta import API_VERSION, APP_NAME
@@ -16,74 +13,79 @@ from .utils import snake_to_camel
 log = logging.getLogger(__name__)
 
 
-class ApplicationSettings(BaseSettings):
+class ApplicationSettings(BaseCustomSettings):
     # CODE STATICS ---
     # settings defined by the code
 
-    app_name: str = APP_NAME
-    api_version: str = API_VERSION
+    APP_NAME: str = APP_NAME
+    API_VERSION: str = API_VERSION
 
     # IMAGE BUILD ---
     # settings defined when docker image is built
     #
+    SC_VCS_URL: Optional[str] = None
+    SC_VCS_REF: Optional[str] = None
+    SC_BUILD_DATE: Optional[str] = None
+    SC_BUILD_TARGET: Optional[BuildTargetEnum] = None
 
-    vcs_url: Optional[str] = Field(None, env="SC_VCS_URL")
-    vcs_ref: Optional[str] = Field(None, env="SC_VCS_REF")
-    build_date: Optional[str] = Field(None, env="SC_BUILD_DATE")
-    build_target: Optional[BuildTargetEnum] = Field(
-        BuildTargetEnum.PRODUCTION, env="SC_BUILD_TARGET"
-    )
+    # DOCKER
+    SC_BOOT_MODE: Optional[BootModeEnum]
 
-    boot_mode: Optional[BootModeEnum] = Field(
-        BootModeEnum.PRODUCTION, env="SC_BOOT_MODE"
-    )
-    user_name: Optional[str] = Field(None, env="SC_USER_NAME")
-    user_id: Optional[int] = Field(None, env="SC_USER_ID")
+    SC_USER_NAME: Optional[str] = None
+    SC_USER_ID: Optional[int] = None
 
-    heathcheck_retry: Optional[int] = Field(None, env="SC_HEATHCHECK_RETRY")
-    heatcheck_inteval: Optional[int] = Field(None, env="SC_HEATCHECK_INTEVAL")
+    SC_HEATHCHECK_RETRY: Optional[int] = None
+    SC_HEATCHECK_INTEVAL: Optional[int] = None
 
     # stack name defined upon deploy (see main Makefile)
-    swarm_stack_name: Optional[str] = Field(
-        None, alias="stackName", env="SWARM_STACK_NAME"
-    )
+    SWARM_STACK_NAME: Optional[str] = None
 
     # CONTAINER RUN  ---
     # settings defined from environs defined when container runs
 
     WEBSERVER_DEV_FEATURES_ENABLED: bool = Field(
         False,
-        env="WEBSERVER_DEV_FEATURES_ENABLED",
         description="Enables development features. WARNING: make sure it is disabled in production .env file!",
     )
 
-    class Config:
-        env_prefix = "WEBSERVER_"
-        case_sensitive = False
-        alias_generator = snake_to_camel
+    class Config(BaseCustomSettings.Config):
+        fields = {
+            "SC_VCS_URL": "vcs_url",
+            "SC_VCS_REF": "vcs_ref",
+            "SC_BUILD_DATE": "build_date",
+            "SWARM_STACK_NAME": "stack_name",
+        }
+        alias_generator = lambda s: s.lower()
 
-    # ---
-
-    def public_dict(self) -> Dict:
+    def public_dict(self) -> Dict[str, Any]:
         """Data publicaly available"""
         return self.dict(
-            include={"app_name", "api_version", "vcs_url", "vcs_ref", "build_date"},
-            exclude_none=True,
-        )
-
-    def to_client_statics(self) -> Dict:
-        return self.dict(
             include={
-                "app_name",
-                "api_version",
-                "vcs_url",
-                "vcs_ref",
-                "build_date",
-                "swarm_stack_name",
+                "APP_NAME",
+                "API_VERSION",
+                "SC_VCS_URL",
+                "SC_VCS_REF",
+                "SC_BUILD_DATE",
             },
             exclude_none=True,
             by_alias=True,
         )
+
+    def to_client_statics(self) -> Dict[str, Any]:
+        data = self.dict(
+            include={
+                "APP_NAME",
+                "API_VERSION",
+                "SC_VCS_URL",
+                "SC_VCS_REF",
+                "SC_BUILD_DATE",
+                "SWARM_STACK_NAME",
+            },
+            exclude_none=True,
+            by_alias=True,
+        )
+        # Alias MUST be camelcase here
+        return {snake_to_camel(k): v for k, v in data.items()}
 
 
 def setup_settings(app: web.Application):
