3a640aedc8ee91649383e2092b2b398e0d490cfc
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-08 14:11:18 +0100

    webserver.catalog: split schema

--- services/web/server/src/simcore_service_webserver/catalog_config.py
+++ services/web/server/src/simcore_service_webserver/catalog_config.py
@@ -1,35 +1,11 @@
-""" catalog's subsystem configuration
-
-    - config-file schema
-    - settings
-"""
-import os
 from typing import Dict
 
-import trafaret as T
 from aiohttp import web
 from models_library.basic_types import PortInt, VersionTag
 from pydantic import BaseSettings, Field
 from servicelib.aiohttp.application_keys import APP_CONFIG_KEY
 
 CONFIG_SECTION_NAME = "catalog"
-
-
-_default_values = {
-    "host": os.environ.get("CATALOG_HOST", "catalog"),
-    "port": int(os.environ.get("CATALOG_PORT", 8000)),
-}
-
-schema = T.Dict(
-    {
-        T.Key("enabled", default=True, optional=True): T.Bool(),
-        T.Key("host", default=_default_values["host"]): T.String(),
-        T.Key("port", default=_default_values["port"]): T.ToInt(),
-        T.Key("version", default="v0"): T.Regexp(
-            regexp=r"^v\d+"
-        ),  # catalog API version basepath
-    }
-)
 
 
 class CatalogSettings(BaseSettings):
