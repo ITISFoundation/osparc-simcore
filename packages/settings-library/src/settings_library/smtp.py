a9d735a168812265ab742aeee8bc133583e7db17
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-08 22:01:40 +0100

    settingslib: new stmp settings

---
+++ packages/settings-library/src/settings_library/smtp.py
@@ -0,0 +1,16 @@
+from pydantic.fields import Field
+from pydantic.types import SecretStr
+
+from .base import BaseCustomSettings
+from .basic_types import PortInt
+
+
+class SmtpSettings(BaseCustomSettings):
+    """Simple Mail Transfer Protocol"""
+
+    SMTP_HOST: str
+    SMTP_PORT: PortInt
+
+    SMTP_TLS_ENABLED: bool = Field(description="Enables Secure Mode")
+    SMTP_USERNAME: str
+    SMTP_PASSWORD: SecretStr
