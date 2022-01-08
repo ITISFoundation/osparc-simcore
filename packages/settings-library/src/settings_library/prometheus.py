2a4cc4bac82e1b671bd2b5e0f864e4a518ac316c
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-08 21:36:29 +0100

    settingslib: new prometheous settings

---
+++ packages/settings-library/src/settings_library/prometheus.py
@@ -0,0 +1,21 @@
+from functools import cached_property
+
+from models_library.basic_types import PortInt, VersionTag
+from settings_library.base import BaseCustomSettings
+from settings_library.utils_service import MixinServiceSettings
+from yarl import URL
+
+
+class PrometheusSettings(BaseCustomSettings, MixinServiceSettings):
+    PROMETHEUS_HOST: str = "prometheus"
+    PROMETHEUS_PORT: PortInt = 9090
+    PROMETHEUS_VTAG: VersionTag = "v1"
+
+    @cached_property
+    def base_url(self) -> URL:
+        return URL.build(
+            scheme="http",
+            host=self.PROMETHEUS_HOST,
+            port=self.PROMETHEUS_PORT,
+            path=f"/api/{self.PROMETHEUS_VTAG}/query",
+        )
