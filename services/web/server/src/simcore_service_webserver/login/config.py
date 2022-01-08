2deb387f12ace7577b9a1acf8063aa31b97b1fe9
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-08 14:07:07 +0100

    login: split schema

--- services/web/server/src/simcore_service_webserver/login/config.py
+++ services/web/server/src/simcore_service_webserver/login/config.py
@@ -5,10 +5,8 @@
 """
 from typing import Dict, Optional
 
-import trafaret as T
 from aiohttp.web import Application
 from pydantic import BaseSettings
-
 from servicelib.aiohttp.application_keys import APP_CONFIG_KEY
 
 from ..email_config import CONFIG_SECTION_NAME as SMTP_SECTION
@@ -16,21 +14,6 @@ from .cfg import DEFAULTS
 from .storage import AsyncpgStorage
 
 CONFIG_SECTION_NAME = "login"
-
-# TODO: merge with cfg.py
-schema = T.Dict(
-    {
-        T.Key("enabled", default=True, optional=True): T.Bool(),
-        T.Key(
-            "registration_confirmation_required",
-            default=DEFAULTS["REGISTRATION_CONFIRMATION_REQUIRED"],
-            optional=True,
-        ): T.Or(T.Bool, T.ToInt),
-        T.Key("registration_invitation_required", default=False, optional=True): T.Or(
-            T.Bool, T.ToInt
-        ),
-    }
-)
 
 
 class LoginSettings(BaseSettings):
