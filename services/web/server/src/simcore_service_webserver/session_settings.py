ff3d7061409d196d7fbdcae6ded1c17cfdfe6a69
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-13 17:38:33 +0100

    webserver.session settings

--- services/web/server/src/simcore_service_webserver/session_settings.py
+++ services/web/server/src/simcore_service_webserver/session_settings.py
@@ -6,9 +6,14 @@
 from typing import Dict
 
 from aiohttp.web import Application
+from pydantic import BaseSettings, constr
 from servicelib.aiohttp.application_keys import APP_CONFIG_KEY
 
-CONFIG_SECTION_NAME = "session"
+from .session_config import CONFIG_SECTION_NAME
+
+
+class SessionSettings(BaseSettings):
+    secret_key: constr(strip_whitespace=True, min_length=32)
 
 
 def assert_valid_config(app: Application) -> Dict:
