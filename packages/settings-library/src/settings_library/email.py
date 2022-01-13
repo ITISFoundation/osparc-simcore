089ed3a5710d404d436432b77549a7829edaa718
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-13 09:40:01 +0100

    adds email settings

---
+++ packages/settings-library/src/settings_library/email.py
@@ -0,0 +1,18 @@
+from pydantic.fields import Field
+from pydantic.types import SecretStr
+
+from .base import BaseCustomSettings
+from .basic_types import PortInt
+
+
+class SmtpSettings(BaseCustomSettings):
+    """SMPT = Simple Mail Transfer Protocol"""
+
+    SMTP_SENDER: str = "OSPARC support <support@osparc.io>"
+
+    SMTP_HOST: str
+    SMTP_PORT: PortInt
+
+    SMTP_TLS_ENABLED: bool = Field(description="Enables Secure Mode")
+    SMTP_USERNAME: str
+    SMTP_PASSWORD: SecretStr
