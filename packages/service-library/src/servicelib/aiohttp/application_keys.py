73cb97cfb05085804e87509bc6fb965b0fd301bb
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-13 10:02:42 +0100

    refactor constants

--- packages/service-library/src/servicelib/aiohttp/application_keys.py
+++ packages/service-library/src/servicelib/aiohttp/application_keys.py
@@ -8,25 +8,24 @@ All keys are constants with a unique name convention:
 
  See https://aiohttp.readthedocs.io/en/stable/web_advanced.html#data-sharing-aka-no-singletons-please
 """
+from typing import Final
 
 # REQUIREMENTS:
 # - guarantees all keys are unique
 # - one place for all common keys
 # - hierarchical classification
-# TODO: should be read-only (frozen?)
 
 #
 # web.Application keys, i.e. app[APP_*_KEY]
 #
+APP_CONFIG_KEY: Final[str] = f"{__name__ }.config"
+APP_JSONSCHEMA_SPECS_KEY: Final[str] = f"{__name__ }.jsonschema_specs"
+APP_OPENAPI_SPECS_KEY: Final[str] = f"{__name__ }.openapi_specs"
+APP_SETTINGS_KEY: Final[str] = f"{__name__ }.settings"
 
-APP_CONFIG_KEY = f"{__name__ }.config"
-APP_SETTINGS_KEY = f"{__name__ }.settings"
-APP_OPENAPI_SPECS_KEY = f"{__name__ }.openapi_specs"
-APP_JSONSCHEMA_SPECS_KEY = f"{__name__ }.jsonschema_specs"
+APP_DB_ENGINE_KEY: Final[str] = f"{__name__ }.db_engine"
 
-APP_DB_ENGINE_KEY = f"{__name__ }.db_engine"
-
-APP_CLIENT_SESSION_KEY = f"{__name__ }.session"
+APP_CLIENT_SESSION_KEY: Final[str] = f"{__name__ }.session"
 
 #
 # web.Response keys, i.e. app[RSP_*_KEY]
