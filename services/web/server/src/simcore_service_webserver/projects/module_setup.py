b6bfff468d072703177a19a45ad3b1d77a1511f1
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-13 19:12:21 +0100

    fixes project

--- services/web/server/src/simcore_service_webserver/projects/module_setup.py
+++ services/web/server/src/simcore_service_webserver/projects/module_setup.py
@@ -19,9 +19,10 @@ from servicelib.aiohttp.rest_routing import (
 from .._constants import APP_OPENAPI_SPECS_KEY
 from .._resources import resources
 from . import projects_handlers, projects_nodes_handlers, projects_tags_handlers
-from .config import CONFIG_SECTION_NAME, assert_valid_config
+from .config import CONFIG_SECTION_NAME
 from .projects_access import setup_projects_access
 from .projects_db import setup_projects_db
+from .settings import assert_valid_config
 
 logger = logging.getLogger(__name__)
 
