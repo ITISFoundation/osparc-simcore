fd5238b98a95c2347f92dff4ad73b78c282aa615
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-13 17:41:41 +0100

    fix webserver.session settings

--- services/web/server/src/simcore_service_webserver/session_config.py
+++ services/web/server/src/simcore_service_webserver/session_config.py
@@ -3,15 +3,5 @@
     - config-file schema
     - settings
 """
-from typing import Dict
-
-from aiohttp.web import Application
-from servicelib.aiohttp.application_keys import APP_CONFIG_KEY
 
 CONFIG_SECTION_NAME = "session"
-
-
-def assert_valid_config(app: Application) -> Dict:
-    cfg = app[APP_CONFIG_KEY][CONFIG_SECTION_NAME]
-    _settings = SessionSettings(**cfg)
-    return cfg
