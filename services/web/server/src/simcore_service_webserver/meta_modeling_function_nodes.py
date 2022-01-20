bdec0c8ffa9dce1fabf3763cd5e1ab3d33fae85a
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-20 02:28:36 +0100

    WIP: adds sensitivity iterator

--- services/web/server/src/simcore_service_webserver/meta_modeling_function_nodes.py
+++ services/web/server/src/simcore_service_webserver/meta_modeling_function_nodes.py
@@ -12,10 +12,12 @@ An example of meta-function is the "Integers Iterator" node.
 """
 
 from copy import deepcopy
-from typing import Callable, Dict, Iterator, Tuple
+from typing import Callable, Dict, Iterator, List, Tuple
 
 from models_library.frontend_services_catalog import (
     FRONTEND_SERVICE_KEY_PREFIX,
+    OM,
+    ServiceType,
     iter_service_docker_data,
 )
 from models_library.projects_nodes import Node, OutputTypes
@@ -34,11 +36,110 @@ def _linspace_func(
         yield value
 
 
-def linspace_generator(**kwargs) -> Iterator[Dict[str, OutputTypes]]:
+def _linspace_generator(**kwargs) -> Iterator[Dict[str, OutputTypes]]:
     # maps generator with iterable outputs. can have non-iterable outputs
     # as well
     for value in _linspace_func(**kwargs):
         yield {"out_1": value}
+
+
+# TODO: extend InputTypes/OutputTypes to List[T]
+# TODO: pydantic map to json-schema
+
+# TODO: inputs need to match _func definition!
+# TODO: _sensitivity_meta needs to go in frontend_service_cantalog and registered in there so that
+# the catalog can identify this service
+
+# TODO: inject in database (including code??) If so, the definition can even change??
+
+
+def _sensitivity_meta() -> ServiceDockerData:
+    return ServiceDockerData(
+        key=f"{FRONTEND_SERVICE_KEY_PREFIX}/data-iterator/sensitivity",
+        version="1.0.0",
+        type=ServiceType.FRONTEND,
+        name="Sensitivity iterator",
+        description="Increases/decreases one dimension of the reference parameters at every iteration",
+        authors=[
+            OM,
+        ],
+        contact=OM.email,
+        **_sensitivity_schema(),
+    )
+
+
+def _sensitivity_schema():
+    return {
+        "inputs": {
+            "in_1": {
+                "label": "paramrefs",
+                "description": "reference parameters",
+                "type": List[
+                    float
+                ],  # TODO: check how pydantic maps with python with jsonschema
+            },
+            "in_2": {
+                "label": "paramdiff",
+                "description": "diff parameters",
+                "type": List[float],
+            },
+            "in_3": {
+                "label": "diff_or_fact",
+                "description": "Applies difference (true) or factor (false)",
+                "type": bool,
+            },
+        },
+        "outputs": {
+            "out_1": {
+                "label": "i",
+                "description": "dimension index that was modified",
+                "type": int,
+            },
+            "out_2": {
+                "label": "paramtestplus",
+                "description": "increased parameter",
+                "type": List[float],
+            },
+            "out_3": {
+                "label": "paramtestminus",
+                "description": "decreased parameter",
+                "type": List[float],
+            },
+        },
+    }
+
+
+def _sensitivity_func(
+    paramrefs: List[float],
+    paramdiff: List[float],
+    diff_or_fact: bool,
+):
+    # This code runs in the backend
+    assert len(paramrefs) == len(paramdiff)
+
+    n_dims = len(paramrefs)
+
+    for i in range(n_dims):
+        paramtestplus = deepcopy(paramrefs)
+        paramtestminus = deepcopy(paramrefs)
+
+        # inc/dec one dimension at a time
+        if diff_or_fact:
+            paramtestplus[i] += paramdiff[i]
+        else:
+            paramtestplus[i] *= paramdiff[i]
+
+        if diff_or_fact:
+            paramtestminus[i] -= paramdiff[i]
+        else:
+            paramtestminus[i] /= paramdiff[i]  # check that not zero
+
+        yield (i, paramtestplus, paramtestminus)
+
+
+def _sensitivity_generator(**kwargs) -> Iterator[Dict[str, OutputTypes]]:
+    for i, paramtestplus, paramtestminus in _sensitivity_func(**kwargs):
+        yield {"out_1": i, "out_2": paramtestplus, "out_3": paramtestminus}
 
 
 # META-FUNCTIONS ---------------------------------------------------
@@ -55,7 +156,11 @@ FRONTEND_SERVICE_TO_CALLABLE: Dict[_ServiceKeyVersionPair, Callable] = {
     (
         f"{FRONTEND_SERVICE_KEY_PREFIX}/data-iterator/int-range",
         "1.0.0",
-    ): linspace_generator,
+    ): _linspace_generator,
+    (
+        f"{FRONTEND_SERVICE_KEY_PREFIX}/data-iterator/sensitivity",
+        "1.0.0",
+    ): _sensitivity_generator,
 }
 
 
--- services/web/server/tests/unit/with_dbs/10/meta_modeling/test_meta_modeling_function_nodes.py
+++ services/web/server/tests/unit/with_dbs/10/meta_modeling/test_meta_modeling_function_nodes.py
@@ -1,3 +1,20 @@
 # pylint: disable=redefined-outer-name
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
+
+
+def create(iterable_func, input_values):
+
+    # TODO: process input to iterable_func inputs
+    def iter_node():
+        for output_values in iterable_func(**input_values):
+            # map each func output into a node output
+            yield {f"out_{i}": value for i, value in enumerate(output_values, start=1)}
+
+    return iter_node
+
+
+# given a function signature -> inputs and outputs for ServiceDockerData (schema)
+# of the service
+#
+#
