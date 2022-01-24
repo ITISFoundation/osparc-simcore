d37270b86b111b5d0208109366a386ae6964ba98
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-24 14:43:03 +0100

    extra doc [skip CI]

--- packages/pytest-simcore/src/pytest_simcore/helpers/typing_env.py
+++ packages/pytest-simcore/src/pytest_simcore/helpers/typing_env.py
@@ -3,10 +3,11 @@ from typing import Dict, Optional
 EnvVarsDict = Dict[str, Optional[str]]
 #
 # NOTE: that this means that env vars do not require a value. If that happens a None is assigned
-#   For instance, a env file as
+#   For instance, a valid env file is
 #
 #   NAME=foo
+#   INDEX=33
 #   ONLY_NAME=
 #
-# will return env: EnvVarsDict = {"NAME": "foo", "ONLY_NAME": None}
+# will return env: EnvVarsDict = {"NAME": "foo", "INDEX": 33, "ONLY_NAME": None}
 #
