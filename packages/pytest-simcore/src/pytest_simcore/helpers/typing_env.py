552088227184805a2e69720ddf669fc81ed56883
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-24 14:40:35 +0100

    @sanderegg review: envvarsdict type and doc [skip CI]

--- packages/pytest-simcore/src/pytest_simcore/helpers/typing_env.py
+++ packages/pytest-simcore/src/pytest_simcore/helpers/typing_env.py
@@ -1,3 +1,12 @@
-from typing import Dict, Union
+from typing import Dict, Optional
 
-EnvVarsDict = Dict[str, Union[str, None]]
+EnvVarsDict = Dict[str, Optional[str]]
+#
+# NOTE: that this means that env vars do not require a value. If that happens a None is assigned
+#   For instance, a env file as
+#
+#   NAME=foo
+#   ONLY_NAME=
+#
+# will return env: EnvVarsDict = {"NAME": "foo", "ONLY_NAME": None}
+#
