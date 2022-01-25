ee4ac846a7b8b6d5ff060ab368427debfcc18274
Andrei Neagu <neagu@itis.swiss>
2022-01-25 15:36:17 +0100

    reverting changes

--- packages/models-library/src/models_library/services.py
+++ packages/models-library/src/models_library/services.py
@@ -16,11 +16,9 @@ from pydantic import (
     StrictFloat,
     StrictInt,
     constr,
-    root_validator,
     validator,
 )
 from pydantic.types import PositiveInt
-from typing_extensions import TypedDict
 
 from .basic_regex import VERSION_RE
 from .boot_options import BootOption, BootOptions
@@ -41,13 +39,11 @@ SERVICE_NETWORK_RE = r"^([a-zA-Z0-9_-]+)$"
 
 PROPERTY_TYPE_RE = r"^(number|integer|boolean|string|data:([^/\s,]+/[^/\s,]+|\[[^/\s,]+/[^/\s,]+(,[^/\s]+/[^/,\s]+)*\]))$"
 PROPERTY_KEY_RE = r"^[-_a-zA-Z0-9]+$"
-ENV_VAR_KEY_RE = r"[a-zA-Z][a-azA-Z0-9_]*"
 
 FILENAME_RE = r".+"
 
 PropertyName = constr(regex=PROPERTY_KEY_RE)
 FileName = constr(regex=FILENAME_RE)
-EnvVarKey = constr(regex=ENV_VAR_KEY_RE)
 GroupId = PositiveInt
 
 ServiceKey = constr(regex=KEY_RE)
@@ -342,68 +338,6 @@ class ServiceCommonData(BaseModel):
 
 ServiceInputs = Dict[PropertyName, ServiceInput]
 ServiceOutputs = Dict[PropertyName, ServiceOutput]
-
-
-class BootOptionItem(TypedDict):
-    label: str
-    description: str
-
-
-class BootOptionMode(BaseModel):
-    label: str
-    description: str
-    default: str
-    items: Dict[str, BootOptionItem]
-
-    @root_validator
-    @classmethod
-    def ensure_default_is_present(cls, values: Dict) -> Dict:
-        default = values["default"]
-        items = values["items"]
-        if default not in items:
-            raise ValueError(
-                f"Expected default={default} to be present a key of items={items}"
-            )
-        return values
-
-    class Config:
-        schema_extra = {
-            "examples": [
-                {
-                    "label": "Boot mode",
-                    "description": "Start it in web page mode",
-                    "default": "0",
-                    "items": {
-                        "0": {
-                            "label": "Non Voila",
-                            "description": "Tooltip for non Voila boot mode",
-                        },
-                        "1": {
-                            "label": "Voila",
-                            "description": "Tooltip for Voila boot mode",
-                        },
-                    },
-                },
-                {
-                    "label": "Application theme",
-                    "description": "Select a theme for the application",
-                    "default": "b",
-                    "items": {
-                        "a": {
-                            "label": "Clear",
-                            "description": "Using white background",
-                        },
-                        "b": {
-                            "label": "Dark",
-                            "description": "Using black and gray tones",
-                        },
-                    },
-                },
-            ]
-        }
-
-
-BootOptions = Optional[Dict[EnvVarKey, BootOptionMode]]
 
 
 class ServiceDockerData(ServiceKeyVersion, ServiceCommonData):
