d5737e9e3468321adde7701e240412d0d639a5fd
Andrei Neagu <neagu@itis.swiss>
2022-01-28 08:37:56 +0100

    adds rclone installation for CI

---
+++ ci/github/helpers/install_rclone.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#  Installs the latest version of rclone plugin
+#
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
+IFS=$'\n\t'
+
+
+# Installation instructions from https://rclone.org/docker/
+
+apt-get -y install fuse
+mkdir -p /var/lib/docker-plugins/rclone/config
+mkdir -p /var/lib/docker-plugins/rclone/cache
+docker plugin install rclone/docker-volume-rclone:amd64 args="-v" --alias rclone --grant-all-permissions
+docker plugin list
+docker plugin inspect rclone:latest
