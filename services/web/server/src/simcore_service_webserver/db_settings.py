5ebd54bea59e30a03d18f2d4a6c0785e679567d4
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-02-02 22:27:57 +0100

    rm db.postgres.entrypoint

--- services/web/server/src/simcore_service_webserver/db_settings.py
+++ services/web/server/src/simcore_service_webserver/db_settings.py
@@ -7,7 +7,6 @@ from .db_config import CONFIG_SECTION_NAME
 
 
 def assert_valid_config(app: Application):
-    import json
 
     cfg = app[APP_CONFIG_KEY][CONFIG_SECTION_NAME]
 
@@ -15,11 +14,15 @@ def assert_valid_config(app: Application):
     if app_settings := app.get(APP_SETTINGS_KEY):
         assert cfg_enabled == (app_settings.WEBSERVER_DB is not None)  # nosec
 
+    # TODO: DEPRECATE: "endpoint": f"{WEBSERVER_POSTGRES.POSTGRES_HOST}:{WEBSERVER_POSTGRES.POSTGRES_PORT}",
+    # NOTE: found inconsistencies between values passed as host and the entrypoint.
+    # Remove and use instead WEBSERVER_POSTGRES.dsn
+    cfg.get("postgres", {}).pop("entrypoint", None)
+
     WEBSERVER_POSTGRES = PostgresSettings()
     got = {  # nosec
         "postgres": {
             "database": WEBSERVER_POSTGRES.POSTGRES_DB,
-            "endpoint": f"{WEBSERVER_POSTGRES.POSTGRES_HOST}:{WEBSERVER_POSTGRES.POSTGRES_PORT}",
             "host": WEBSERVER_POSTGRES.POSTGRES_HOST,
             "maxsize": WEBSERVER_POSTGRES.POSTGRES_MAXSIZE,
             "minsize": WEBSERVER_POSTGRES.POSTGRES_MINSIZE,
@@ -28,5 +31,5 @@ def assert_valid_config(app: Application):
             "user": WEBSERVER_POSTGRES.POSTGRES_USER,
         },
     }
-    assert cfg == got, json.dumps(got) + "!=" + json.dumps(cfg)  # nosec
+    assert cfg == got, f"{got}!={cfg}"  # nosec
     return cfg, PostgresSettings
