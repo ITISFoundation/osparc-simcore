5bfec5c38bdc1c00baafc05c842bc71e399937aa
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2022-02-11 11:35:03 +0100

    Enable/disable Export and Duplicate (#2820)

---
+++ services/web/client/source/class/osparc/utils/DisabledPlugins.js
@@ -0,0 +1,55 @@
+/* ************************************************************************
+
+   osparc - the simcore frontend
+
+   https://osparc.io
+
+   Copyright:
+     2022 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+/**
+ * Collection of methods for studies
+ */
+
+qx.Class.define("osparc.utils.DisabledPlugins", {
+  type: "static",
+
+  statics: {
+    EXPORT: "WEBSERVER_EXPORTER",
+    DUPLICATE: "WEBSERVER_EXPORTER",
+    SCICRUNCH: "WEBSERVER_SCICRUNCH",
+
+    isExportDisabled: function() {
+      return this.self().isPluginDisabled(this.self().EXPORT);
+    },
+
+    isDuplicateDisabled: function() {
+      return this.self().isPluginDisabled(this.self().EXPORT);
+    },
+
+    isScicrunchDisabled: function() {
+      return this.self().isPluginDisabled(this.self().SCICRUNCH);
+    },
+
+    isPluginDisabled: function(key) {
+      return new Promise((resolve, reject) => {
+        osparc.data.Resources.get("statics")
+          .then(statics => {
+            if ("pluginsDisabled" in statics) {
+              resolve(statics["pluginsDisabled"].includes(key));
+            }
+            resolve(false);
+          })
+          .catch(err => reject(err));
+      });
+    }
+  }
+});
