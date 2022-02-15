daa0412137868de2654599f84336f947417f2254
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-02-15 15:55:20 +0100

    director

--- services/web/server/src/simcore_service_webserver/director/module_setup.py
+++ services/web/server/src/simcore_service_webserver/director/module_setup.py
@@ -7,14 +7,8 @@ import logging
 
 from aiohttp import web
 from servicelib.aiohttp.application_setup import ModuleCategory, app_module_setup
-from servicelib.aiohttp.rest_routing import (
-    iter_path_operations,
-    map_handlers_with_operations,
-)
 
-from .._constants import APP_OPENAPI_SPECS_KEY
-from .config import APP_DIRECTOR_API_KEY
-from .settings import assert_valid_config
+from .settings import get_plugin_settings
 
 logger = logging.getLogger(__name__)
 
@@ -25,41 +19,6 @@ logger = logging.getLogger(__name__)
     depends=[],
     logger=logger,
 )
-def setup_director(
-    app: web.Application, *, disable_login: bool = False, disable_routes: bool = False
-):
-    """Sets up director's app module
-
-    disable_* options are for testing purpuses
-    """
-    # ----------------------------------------------
-    # TODO: temporary, just to check compatibility between
-    # trafaret and pydantic schemas
-    _, settings = assert_valid_config(app)
-    # ---------------------------------------------
-
-    # director service API base url, e.g. http://director:8081/v0
-    app[APP_DIRECTOR_API_KEY] = str(settings.base_url)
-
-    # setup routes ------------
-    if not disable_routes:
-        specs = app[APP_OPENAPI_SPECS_KEY]
-
-        def include_path(tup_object):
-            _method, path, _operation_id, _tags = tup_object
-            return any(tail in path for tail in ["/running_interactive_services"])
-
-        handlers_dict = {}
-
-        # Disables login_required decorator for testing purposes
-        if disable_login:
-            for name, hnds in handlers_dict.items():
-                if hasattr(hnds, "__wrapped__"):
-                    handlers_dict[name] = hnds.__wrapped__
-
-        routes = map_handlers_with_operations(
-            handlers_dict,
-            filter(include_path, iter_path_operations(specs)),
-            strict=True,
-        )
-        app.router.add_routes(routes)
+def setup_director(app: web.Application):
+    get_plugin_settings(app)
+    # TODO: init some client
--- services/web/server/src/simcore_service_webserver/director/settings.py
+++ services/web/server/src/simcore_service_webserver/director/settings.py
@@ -7,6 +7,7 @@ from settings_library.base import BaseCustomSettings
 from settings_library.utils_service import DEFAULT_AIOHTTP_PORT, MixinServiceSettings
 from yarl import URL
 
+from .._constants import APP_SETTINGS_KEY
 from .config import get_config
 
 APP_DIRECTOR_API_KEY = __name__ + ".director_api"
@@ -20,6 +21,12 @@ class DirectorSettings(BaseCustomSettings, MixinServiceSettings):
     @cached_property
     def base_url(self) -> URL:
         return URL(self._build_api_base_url(prefix="DIRECTOR"))
+
+
+def get_plugin_settings(app: Application) -> DirectorSettings:
+    settings = app[APP_SETTINGS_KEY].WEBSERVER_DIRECTOR
+    assert settings
+    return settings
 
 
 def assert_valid_config(app: Application) -> Tuple[Dict, DirectorSettings]:
