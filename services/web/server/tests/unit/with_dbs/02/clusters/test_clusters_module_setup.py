9c9b12d04501f62e4e4c204a697d6f39566f7696
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-02-16 21:04:26 +0100

    cluster tests

--- services/web/server/tests/unit/with_dbs/02/clusters/test_clusters_module_setup.py
+++ services/web/server/tests/unit/with_dbs/02/clusters/test_clusters_module_setup.py
@@ -3,22 +3,21 @@
 # pylint:disable=redefined-outer-name
 
 from aiohttp.test_utils import TestClient
-from servicelib.aiohttp.application_keys import APP_CONFIG_KEY
+from servicelib.aiohttp.application_keys import APP_SETTINGS_KEY
+from simcore_service_webserver.application_settings import ApplicationSettings
 
 
 def test_module_setup_skipped_when_no_dev_features_enabled(client: TestClient):
-    cfg = client.app[APP_CONFIG_KEY]
-    assert "clusters" in cfg
-    assert cfg["clusters"]["enabled"] == True
+    settings: ApplicationSettings = client.app[APP_SETTINGS_KEY]
 
+    assert not settings.WEBSERVER_CLUSTERS
     assert "list_clusters_handler" not in client.app.router
 
 
 def test_module_setup_applied_when_dev_features_enabled(
     enable_dev_features: None, client: TestClient
 ):
-    cfg = client.app[APP_CONFIG_KEY]
-    assert "clusters" in cfg
-    assert cfg["clusters"]["enabled"] == True
+    settings: ApplicationSettings = client.app[APP_SETTINGS_KEY]
 
+    assert settings.WEBSERVER_CLUSTERS
     assert "list_clusters_handler" in client.app.router
