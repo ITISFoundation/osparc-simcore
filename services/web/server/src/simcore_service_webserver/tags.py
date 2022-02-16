00674d1493d175066f405321e32a788d7d81ac40
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-02-16 17:27:24 +0100

    tags

--- services/web/server/src/simcore_service_webserver/tags.py
+++ services/web/server/src/simcore_service_webserver/tags.py
@@ -4,6 +4,7 @@
 import logging
 
 from aiohttp import web
+from servicelib.aiohttp.application_keys import APP_SETTINGS_KEY
 from servicelib.aiohttp.application_setup import ModuleCategory, app_module_setup
 from servicelib.aiohttp.rest_routing import (
     get_handlers_from_namespace,
@@ -24,7 +25,7 @@ logger = logging.getLogger(__name__)
     logger=logger,
 )
 def setup_tags(app: web.Application):
-
+    assert app[APP_SETTINGS_KEY].WEBSERVER_TAGS  # nosec
     # routes
     specs = app[APP_OPENAPI_SPECS_KEY]
     routes = map_handlers_with_operations(
