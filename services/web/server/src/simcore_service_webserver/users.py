358315417bc47bd47d4d452a56605d644ccd0e99
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-02-16 17:47:50 +0100

    users settings

--- services/web/server/src/simcore_service_webserver/users.py
+++ services/web/server/src/simcore_service_webserver/users.py
@@ -4,6 +4,7 @@
 import logging
 
 from aiohttp import web
+from servicelib.aiohttp.application_keys import APP_SETTINGS_KEY
 from servicelib.aiohttp.application_setup import ModuleCategory, app_module_setup
 from servicelib.aiohttp.rest_routing import (
     get_handlers_from_namespace,
@@ -24,6 +25,7 @@ logger = logging.getLogger(__name__)
     logger=logger,
 )
 def setup_users(app: web.Application):
+    assert app[APP_SETTINGS_KEY].WEBSERVER_USERS  # nosec
 
     # routes related with users
     specs = app[APP_OPENAPI_SPECS_KEY]
