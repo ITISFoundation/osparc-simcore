3308dbf8434f8744d8cb11da9176df56b37d9cc7
Andrei Neagu <neagu@itis.swiss>
2022-02-17 10:42:36 +0100

    refactor

--- services/web/server/src/simcore_service_webserver/director_v2_api.py
+++ services/web/server/src/simcore_service_webserver/director_v2_api.py
@@ -11,27 +11,31 @@ from .director_v2_abc import (
     set_project_run_policy,
 )
 from .director_v2_core import (
-    DirectorServiceError,
+    attach_network_to_dynamic_sidecar,
     create_or_update_pipeline,
     delete_pipeline,
+    detach_network_from_dynamic_sidecar,
+    DirectorServiceError,
     get_computation_task,
     get_service_state,
     get_services,
     is_healthy,
     request_retrieve_dyn_service,
+    requires_dynamic_sidecar,
     restart,
     retrieve,
     start_service,
     stop_service,
     stop_services,
-    attach_networks_to_containers
 )
 
 # director-v2 module internal API
 __all__: Tuple[str, ...] = (
     "AbstractProjectRunPolicy",
+    "attach_network_to_dynamic_sidecar",
     "create_or_update_pipeline",
     "delete_pipeline",
+    "detach_network_from_dynamic_sidecar",
     "DirectorServiceError",
     "get_computation_task",
     "get_project_run_policy",
@@ -39,6 +43,7 @@ __all__: Tuple[str, ...] = (
     "get_services",
     "is_healthy",
     "request_retrieve_dyn_service",
+    "requires_dynamic_sidecar",
     "restart",
     "retrieve",
     "set_project_run_policy",
