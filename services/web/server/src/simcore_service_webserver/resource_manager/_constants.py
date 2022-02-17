112a33e5cfa8bbb6a4c75b32824270e57ee46253
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-02-17 22:24:51 +0100

    rm resource manager schema

--- services/web/server/src/simcore_service_webserver/resource_manager/_constants.py
+++ services/web/server/src/simcore_service_webserver/resource_manager/_constants.py
@@ -1,5 +1,2 @@
-CONFIG_SECTION_NAME = "resource_manager"
-
-
 APP_CLIENT_SOCKET_REGISTRY_KEY = f"{__name__}.resource_manager.registry"
 APP_RESOURCE_MANAGER_TASKS_KEY = f"{__name__}.resource_manager.tasks.key"
