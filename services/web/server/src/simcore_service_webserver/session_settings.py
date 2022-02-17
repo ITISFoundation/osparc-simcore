71d77d47f0fa86f00453d956201f7b180e4ee75f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-02-17 11:47:22 +0100

    Fixes session key in api-server

--- services/web/server/src/simcore_service_webserver/session_settings.py
+++ services/web/server/src/simcore_service_webserver/session_settings.py
@@ -1,16 +1,14 @@
-import base64
-import binascii
-
 from aiohttp import web
 from pydantic.class_validators import validator
 from pydantic.fields import Field
 from pydantic.types import SecretStr
 from settings_library.base import BaseCustomSettings
+from settings_library.utils_session import MixinSessionSettings
 
 from ._constants import APP_SETTINGS_KEY
 
 
-class SessionSettings(BaseCustomSettings):
+class SessionSettings(BaseCustomSettings, MixinSessionSettings):
 
     SESSION_SECRET_KEY: SecretStr = Field(
         ...,
@@ -23,21 +21,7 @@ class SessionSettings(BaseCustomSettings):
     @validator("SESSION_SECRET_KEY")
     @classmethod
     def check_valid_fernet_key(cls, v):
-        """Ensures it is a URL-safe base64-encoded 32-byte valid secret key"""
-        # NOTE: was difficult to adjust! See test_session.py::test_session_settings
-        value = v.get_secret_value()
-        try:
-            # SEE https://github.com/pyca/cryptography/blob/main/src/cryptography/fernet.py#L26-L164
-            # NOTE: len(v) cannot be 1 more than a multiple of 4
-            key_b64decode = base64.urlsafe_b64decode(value)
-        except binascii.Error as exc:
-            raise ValueError(f"Invalid session key {value=}: {exc}") from exc
-        if len(key_b64decode) != 32:
-            raise ValueError(
-                f"Invalid session secret {value=} must be 32 url-safe base64-encoded bytes, got {len(key_b64decode)=}."
-                'TIP: create new key with python3 -c "from cryptography.fernet import *; print(Fernet.generate_key())"'
-            )
-        return v
+        return cls.do_check_valid_fernet_key(v)
 
 
 def get_plugin_settings(app: web.Application) -> SessionSettings:
