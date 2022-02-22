c2383476ed3a18eee6ff41be32ca44e9064b4b53
odeimaiz <maiz@itis.swiss>
2022-02-22 12:50:18 +0100

    cleanup

--- services/web/client/source/class/osparc/ui/list/CollaboratorListItem.js
+++ services/web/client/source/class/osparc/ui/list/CollaboratorListItem.js
@@ -65,7 +65,6 @@ qx.Class.define("osparc.ui.list.CollaboratorListItem", {
             icon: "@FontAwesome5Solid/ellipsis-v/"+(iconSize-11),
             focusable: false
           });
-          osparc.utils.Utils.setIdToWidget(control, "studyItemMenuButton");
           this._add(control, {
             row: 0,
             column: 3,
