fc9e100a26d4666b17aecad3ead14632dd19e17e
Andrei Neagu <neagu@itis.swiss>
2022-02-23 16:12:03 +0100

    fixed f-strings

--- services/director-v2/tests/unit/with_swarm/test_modules_dynamic_sidecar_docker_api.py
+++ services/director-v2/tests/unit/with_swarm/test_modules_dynamic_sidecar_docker_api.py
@@ -319,8 +319,8 @@ async def test_get_swarm_network_missing_network(
     with pytest.raises(DynamicSidecarError) as excinfo:
         await docker_api.get_swarm_network(dynamic_sidecar_settings)
     assert str(excinfo.value) == (
-        f"Swarm network name (searching for '*test_network_name*') is not configured."
-        f"Found following networks: []"
+        "Swarm network name (searching for '*test_network_name*') is not configured."
+        "Found following networks: []"
     )
 
 
