ea388df49585598933b4446df895f305bfcce427
odeimaiz <maiz@itis.swiss>
2022-03-03 15:01:43 +0100

    tip logos

--- services/web/client/source/class/osparc/utils/Utils.js
+++ services/web/client/source/class/osparc/utils/Utils.js
@@ -206,9 +206,13 @@ qx.Class.define("osparc.utils.Utils", {
         case "s4l":
           logoPath = "osparc/s4l_logo.png";
           break;
-        case "tis":
-          logoPath = "osparc/ti-tp.svg";
+        case "tis": {
+          const colorManager = qx.theme.manager.Color.getInstance();
+          const textColor = colorManager.resolve("text");
+          const luminance = this.getColorLuminance(textColor);
+          logoPath = (luminance > 0.3) ? "osparc/tip-white.svg" : "osparc/tip-black.svg";
           break;
+        }
         default: {
           const colorManager = qx.theme.manager.Color.getInstance();
           const textColor = colorManager.resolve("text");
