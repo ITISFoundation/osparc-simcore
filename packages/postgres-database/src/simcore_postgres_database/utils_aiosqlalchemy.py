2613623c94a2ae9b7d9e085b4431721f92066445
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-03-04 16:14:36 +0100

    added utils for async sqlalchemy

--- packages/postgres-database/src/simcore_postgres_database/utils_aiosqlalchemy.py
+++ packages/postgres-database/src/simcore_postgres_database/utils_aiosqlalchemy.py
@@ -1,29 +1,36 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 
-
+import sqlalchemy as sa
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 from .utils_migration import get_current_head
 
-_ENGINE_ATTRS = "closed driver dsn freesize maxsize minsize name size timeout".split()
+
+async def _get_connections(engine, db_name: str) -> List[Dict]:
+    """Return information about connections"""
+    sql = sa.DDL(
+        f"""
+    SELECT
+        pid,
+        state
+    FROM pg_stat_activity
+    WHERE datname = '{db_name}'
+    AND query NOT LIKE '%%FROM pg_stat_activity%%'
+    """
+    )
+    async with engine.connect() as conn:
+        result = await conn.execute(sql)
+
+        connections = [{"pid": r[0], "state": r[1]} for r in result.fetchall()]
+
+    return connections
 
 
-def get_pg_engine_info(engine: AsyncEngine) -> Dict[str, Any]:
-    return {attr: getattr(engine, attr, None) for attr in _ENGINE_ATTRS}
-
-
-from sqlalchemy.pool import Pool
-
-
-def get_pg_engine_stateinfo(engine: AsyncEngine) -> Dict[str, Any]:
-    engine_pool: Pool = engine.pool
-    return engine_pool.info
-    # return {
-    #     "size": engine.size,
-    #     "acquired": engine.size - engine.freesize,
-    #     "free": engine.freesize,
-    #     "reserved": {"min": engine.minsize, "max": engine.maxsize},
-    # }
+async def get_pg_engine_stateinfo(engine: AsyncEngine, db_name: str) -> Dict[str, Any]:
+    return {
+        "pgserver stats": f"{await _get_connections(engine, db_name)}",
+        "current pool connections": f"{engine.pool.checkedin()=},{engine.pool.checkedout()=}",
+    }
 
 
 async def close_engine(engine: AsyncEngine) -> None:
@@ -41,7 +48,9 @@ async def raise_if_migration_not_ready(engine: AsyncEngine):
     :raises
     """
     async with engine.connect() as conn:
-        version_num = await conn.scalar('SELECT "version_num" FROM "alembic_version"')
+        version_num = await conn.scalar(
+            sa.DDL('SELECT "version_num" FROM "alembic_version"')
+        )
         head_version_num = get_current_head()
         if version_num != head_version_num:
             raise DBMigrationError(
