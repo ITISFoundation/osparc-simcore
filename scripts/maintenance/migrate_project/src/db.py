2a1487aa60ee059168af99bbed0ac7f5ae29f34b
Andrei Neagu <neagu@itis.swiss>
2022-03-08 08:25:47 +0100

    fixed formatting and added colors

--- scripts/maintenance/migrate_project/src/db.py
+++ scripts/maintenance/migrate_project/src/db.py
@@ -3,6 +3,7 @@ from contextlib import contextmanager
 from typing import Any, Deque, Dict, Iterator, Optional, Tuple
 from uuid import UUID
 
+import typer
 from models import DBConfig
 from simcore_postgres_database.models.file_meta_data import file_meta_data
 from simcore_postgres_database.models.projects import projects
@@ -63,6 +64,27 @@ def _get_file_meta_data_without_soft_links(
     return connection.execute(statemet)
 
 
+def _format_message(message: str, color: str, bold: bool = False) -> None:
+    formatted_message = typer.style(message, fg=color, bold=bold)
+    typer.echo(formatted_message)
+
+
+def _red_message(message: str) -> None:
+    _format_message(message, typer.colors.RED, bold=True)
+
+
+def _green_message(message: str) -> None:
+    _format_message(message, typer.colors.GREEN)
+
+
+def _project_summary(project: Dict) -> str:
+    return f"PROJECT: {project['uuid']} {project['name']}"
+
+
+def _file_summary(file_meta_data: Dict) -> str:
+    return f"FILE: {file_meta_data['file_uuid']}"
+
+
 def get_project_and_files_to_migrate(
     project_uuid: UUID,
     hidden_projects_for_user: Optional[int],
@@ -83,9 +105,9 @@ def get_project_and_files_to_migrate(
     project_id = project["uuid"]
 
     if _project_uuid_exists_in_destination(dst_conn, project_id):
-        print(f"\n\n>>>>>>>>>SKIPPING, sync for {project=}\n\n")
-        skipped_projects.append(project)
-        raise Exception("could not continue, main project already exists on remote!")
+        error_message = f"main project {project['uuid']} already exists at destination!"
+        _red_message(error_message)
+        raise Exception(error_message)
 
     projects_to_migrate.append(project)
 
@@ -95,7 +117,7 @@ def get_project_and_files_to_migrate(
         for hidden_result in hidden_projects_cursor:
             hidden_project = dict(hidden_result.items())
             if _project_uuid_exists_in_destination(dst_conn, hidden_project["uuid"]):
-                print(f"\n\n>>>>>>>>>SKIPPING, sync for {project=}\n\n")
+                _red_message(f"SKIPPING, sync for {_project_summary(project)}")
                 skipped_projects.append(project)
                 continue
 
@@ -114,31 +136,29 @@ def get_project_and_files_to_migrate(
             file_uuid = file_meta_data["file_uuid"]
 
             if _meta_data_exists_in_destination(dst_conn, file_uuid):
-                print(f"\n\n>>>>>>>>>SKIPPING, sync for {file_meta_data=}\n\n")
+                _red_message(f"SKIPPING, sync for {_file_summary(file_meta_data)}")
                 skipped_files_meta_data.append(file_meta_data)
                 continue
 
             files_meta_data_to_migrate.append(file_meta_data)
 
     if len(skipped_projects) > 0:
-        print("SKIPPED projects", len(skipped_projects))
+        _red_message("SKIPPED projects count %s" % len(skipped_projects))
     if len(skipped_files_meta_data) > 0:
-        print("SKIPPED files", len(skipped_files_meta_data))
+        _red_message("SKIPPED files count %s" % len(skipped_files_meta_data))
 
-    print("Projects to move", len(projects_to_migrate))
-    print("Files to move", len(files_meta_data_to_migrate))
-    print("Projects to SKIP", len(skipped_projects))
-    print("Files to SKIP", len(skipped_files_meta_data))
+    _green_message("Projects to move %s" % len(projects_to_migrate))
+    _green_message("Files to move %s" % len(files_meta_data_to_migrate))
 
     # if files and projects already exist
     if len(skipped_files_meta_data) > 0 or len(skipped_projects) > 0:
-        print(
-            "Projects skipped uuid(primary keys) listing:",
-            [x["uuid"] for x in skipped_projects],
+        _red_message(
+            "Projects skipped uuid(primary keys) listing: %s"
+            % [x["uuid"] for x in skipped_projects],
         )
-        print(
-            "File meta data skipped file_uuid(primary keys) listing:",
-            [x["file_uuid"] for x in skipped_files_meta_data],
+        _red_message(
+            "File meta data skipped file_uuid(primary keys) listing: %s"
+            % [x["file_uuid"] for x in skipped_files_meta_data],
         )
         raise Exception(
             "Could not continue migration, some projects or files already exist."
