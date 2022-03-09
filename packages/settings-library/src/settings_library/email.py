4ce694eb753c1b17e158641b3dfd778ead2cf794
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-03-09 11:11:20 +0100

    fixes case of zero lenght password

--- packages/settings-library/src/settings_library/email.py
+++ packages/settings-library/src/settings_library/email.py
@@ -1,6 +1,6 @@
 from typing import Optional
-from pydantic import root_validator
 
+from pydantic import root_validator
 from pydantic.fields import Field
 from pydantic.types import SecretStr
 
@@ -11,14 +11,15 @@ from .basic_types import PortInt
 class SMTPSettings(BaseCustomSettings):
     """Simple Mail Transfer Protocol"""
 
+    # TODO: NameEmail or EmailStr
     SMTP_SENDER: str = "@".join(["O2SPARC support <support", "osparc.io>"])
 
     SMTP_HOST: str
     SMTP_PORT: PortInt
 
     SMTP_TLS_ENABLED: bool = Field(False, description="Enables Secure Mode")
-    SMTP_USERNAME: Optional[str]
-    SMTP_PASSWORD: Optional[SecretStr]
+    SMTP_USERNAME: Optional[str] = Field(None, min_length=1)
+    SMTP_PASSWORD: Optional[SecretStr] = Field(None, min_length=1)
 
     @root_validator
     @classmethod
