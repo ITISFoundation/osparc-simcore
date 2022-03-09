11b0a1bfd42732493dac730ee1cbe4ece3803fc1
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-03-09 17:42:15 +0100

    organize api

--- services/web/server/src/simcore_service_webserver/projects/projects_api.py
+++ services/web/server/src/simcore_service_webserver/projects/projects_api.py
@@ -27,17 +27,17 @@ from ._core_notify import (
 from ._core_services import remove_project_dynamic_services
 
 __all__: Tuple[str, ...] = (
-    "get_project_for_user",
-    "remove_project_dynamic_services",
-    "retrieve_and_notify_project_locked_state",
-    "validate_project",
-    "notify_project_state_update",
-    "update_project_node_outputs",
-    "post_trigger_connected_service_retrieve",
-    "update_project_node_progress",
     "delete_project",
+    "get_project_for_user",
     "get_workbench_node_ids_from_project_uuid",
     "is_node_id_present_in_any_project_workbench",
-    "update_project_node_state",
     "notify_project_node_update",
+    "notify_project_state_update",
+    "post_trigger_connected_service_retrieve",
+    "remove_project_dynamic_services",
+    "retrieve_and_notify_project_locked_state",
+    "update_project_node_outputs",
+    "update_project_node_progress",
+    "update_project_node_state",
+    "validate_project",
 )
