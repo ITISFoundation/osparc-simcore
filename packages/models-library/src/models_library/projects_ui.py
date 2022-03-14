fd2d095389e6014966ec327c0edee863162d57c4
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2022-03-14 20:26:07 +0100

    Update projects_ui.py

--- packages/models-library/src/models_library/projects_ui.py
+++ packages/models-library/src/models_library/projects_ui.py
@@ -2,9 +2,9 @@
     Models Front-end UI
 """
 
-from typing import Dict, Optional
+from typing import Dict, Literal, Optional
 
-from pydantic import BaseModel, Extra, Field, Literal
+from pydantic import BaseModel, Extra, Field
 from pydantic.color import Color
 
 from .projects_nodes_io import NodeID, NodeIDStr
