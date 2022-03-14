fd28dc4aba45cde02a119d74cd02c6907b5e8c3d
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-03-14 15:30:03 +0100

    adds  tool

--- requirements/tools/check_changes.py
+++ requirements/tools/check_changes.py
@@ -1,9 +1,21 @@
+import os
 import re
 import subprocess
 from collections import Counter, defaultdict
+from contextlib import contextmanager
 from pathlib import Path
+from typing import Dict, List, Literal, NamedTuple
 
 from packaging.version import Version
+
+
+@contextmanager
+def printing_table(columns: List[str]):
+    print("|" + "|".join(columns) + "|")
+    print("|" + "|".join(["-" * len(c) for c in columns]) + "|")
+    yield
+    print("|" + "|".join(["-" * len(c) for c in columns]) + "|")
+
 
 BEFORE_PATTERN = re.compile(r"^-([\w-]+)==([0-9\.post]+)")
 AFTER_PATTERN = re.compile(r"^\+([\w-]+)==([0-9\.post]+)")
@@ -38,7 +50,7 @@ def parse_changes(filename: Path):
     return before, after, Counter(changes)
 
 
-def main():
+def main_changes_stats():
 
     filepath = Path("changes.ignore.keep.log")
     if not filepath.exists():
@@ -54,34 +66,119 @@ def main():
 
     COLUMNS = ["#", "name", "before", "after", "upgrade", " count"]
 
-    print("|" + "|".join(COLUMNS) + "|")
-    print("|" + "|".join(["-" * len(c) for c in COLUMNS]) + "|")
-    for i, name in enumerate(sorted(before.keys()), start=1):
-        # TODO: where are these libraries?
-        # TODO: are they first dependencies?
-        # TODO: if major, get link to release notes
-        from_versions = set(str(v) for v in before[name])
-        to_versions = set(str(v) for v in after[name])
+    with printing_table(COLUMNS):
 
-        print(
-            "|",
-            f"{i:2d}",
-            "|",
-            f"{name:25s}",
-            "|",
-            f'{", ".join(from_versions):15s}',
-            "|",
-            f'{",".join(to_versions) if to_versions else "removed":10s}',
-            "|",
-            # how big the version change is
-            f"{tag_upgrade(sorted(set(before[name]))[-1], sorted(set(after[name]))[-1]):10s}"
-            if to_versions
-            else "",
-            "|",
-            counts[name],
-            "|",
+        for i, name in enumerate(sorted(before.keys()), start=1):
+            # TODO: where are these libraries?
+            # TODO: are they first dependencies?
+            # TODO: if major, get link to release notes
+            from_versions = set(str(v) for v in before[name])
+            to_versions = set(str(v) for v in after[name])
+
+            print(
+                "|",
+                f"{i:2d}",
+                "|",
+                f"{name:25s}",
+                "|",
+                f'{", ".join(from_versions):15s}',
+                "|",
+                f'{",".join(to_versions) if to_versions else "removed":10s}',
+                "|",
+                # how big the version change is
+                f"{tag_upgrade(sorted(set(before[name]))[-1], sorted(set(after[name]))[-1]):10s}"
+                if to_versions
+                else "",
+                "|",
+                counts[name],
+                "|",
+            )
+
+
+## Stats on installed packages (i.e. defined in txt files)
+DEPENDENCY = re.compile(r"([\w_-]+)==([0-9\.-]+)")
+
+
+def parse_dependencies_in_reqfile(reqfile: Path) -> Dict[str, Version]:
+    name2version = {}
+    for name, version in DEPENDENCY.findall(reqfile.read_text()):
+        # TODO: typing-extensions==4.0.1 ; python_version < "3.9" might intro multiple versions
+        assert name not in name2version, f"{name} more than once in {reqfile}"
+        name2version[name] = Version(version)
+    return name2version
+
+
+class ReqFile(NamedTuple):
+    path: Path
+    target: Literal["base", "test", "tool", "other"]
+    dependencies: Dict[str, Version]
+
+
+def parse_dependencies(repodir: Path) -> List[ReqFile]:
+    reqs = []
+    for reqfile in repodir.rglob("**/requirements/_*.txt"):
+        try:
+            t = {"_base.txt": "base", "_test.txt": "test", "_tools.txt": "tool"}[
+                reqfile.name
+            ]
+        except KeyError:
+            if "test" in f"{reqfile.parent}":
+                t = "test"
+            else:
+                t = "other"
+
+        reqs.append(
+            ReqFile(
+                path=reqfile,
+                target=t,
+                dependencies=parse_dependencies_in_reqfile(reqfile),
+            )
         )
+    return reqs
+
+
+def main_dep_stats():
+    repodir = Path(os.environ.get("REPODIR", "."))
+    reqs = parse_dependencies(repodir)
+
+    # format
+    print("Stats")
+    print("- #reqs files parsed:", len(reqs))
+    print()
+
+    deps = defaultdict(lambda: defaultdict(list))
+    for r in reqs:
+        for name, version in r.dependencies.items():
+            deps[name]["name"] = name
+            deps[name][r.target].append(version)
+
+    with printing_table(
+        columns=["#", "name", "versions-base", "versions-test", "versons-tool"]
+    ):
+        for i, name in enumerate(sorted(deps.keys()), start=1):
+
+            def _norm(thing):
+                return [f"{v}" for v in sorted(list(set(thing)))]
+
+            bases = _norm(deps[name]["base"])
+            tests = _norm(deps[name]["test"])
+            tools = _norm(deps[name]["tool"])
+
+            print(
+                "|",
+                f"{i:2d}",
+                "|",
+                f"{name:25s}",
+                "|",
+                f'{", ".join(bases):25s}',
+                "|",
+                f'{", ".join(tests):25s}',
+                "|",
+                f'{", ".join(tools):25s}',
+                "|",
+            )
 
 
 if __name__ == "__main__":
-    main()
+    main_changes_stats()
+    main_dep_stats()
