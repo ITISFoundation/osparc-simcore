d7ea76877cfc0a97029eec6b1c535492c884f654
Dustin Kaiser <kaiser@itis.swiss>
2022-03-15 10:08:56 +0100

    Add self-hosted regsitries with IP and Port to Regex Settings validation

--- services/director-v2/src/simcore_service_director_v2/models/schemas/constants.py
+++ services/director-v2/src/simcore_service_director_v2/models/schemas/constants.py
@@ -8,8 +8,6 @@ ClusterID = NonNegativeInt
 DYNAMIC_SIDECAR_SERVICE_PREFIX = "dy-sidecar"
 DYNAMIC_PROXY_SERVICE_PREFIX = "dy-proxy"
 
-DYNAMIC_SIDECAR_DOCKER_IMAGE_RE = (
-    r"(^(local|itisfoundation)/)?(dynamic-sidecar):([\w]+)"
-)
+DYNAMIC_SIDECAR_DOCKER_IMAGE_RE = r"(^(local|itisfoundation|((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+))/)?(dynamic-sidecar):([\w]+)"
 REGEX_DY_SERVICE_SIDECAR = fr"^{DYNAMIC_SIDECAR_SERVICE_PREFIX}_[a-zA-Z0-9-_]*"
 REGEX_DY_SERVICE_PROXY = fr"^{DYNAMIC_PROXY_SERVICE_PREFIX}_[a-zA-Z0-9-_]*"
