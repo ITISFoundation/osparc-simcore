4029fb18f585e819a5dcd70aa2c25686dc335103
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-03-17 19:29:48 +0100

    adds missing plugin dependency

--- services/web/server/src/simcore_service_webserver/garbage_collector.py
+++ services/web/server/src/simcore_service_webserver/garbage_collector.py
@@ -5,6 +5,7 @@ from servicelib.aiohttp.application_setup import ModuleCategory, app_module_setu
 
 from .garbage_collector_task import run_background_task
 from .projects.plugin import setup_projects_db, setup_projects_model_schema
+from .socketio.plugin import setup_socketio_server
 
 logger = logging.getLogger(__name__)
 
@@ -17,10 +18,14 @@ logger = logging.getLogger(__name__)
 )
 def setup_garbage_collector(app: web.Application):
 
+    # TODO: review these partial inits! project-api is code smell!!
+
     ## needs a partial init of projects plugin since this plugin uses projects-api
     # - project-api needs access to db
     setup_projects_db(app)
     # - projects-api needs access to schema
     setup_projects_model_schema(app)
+    # - project needs access to socketio via notify_project_state_update
+    setup_socketio_server(app)
 
     app.cleanup_ctx.append(run_background_task)
