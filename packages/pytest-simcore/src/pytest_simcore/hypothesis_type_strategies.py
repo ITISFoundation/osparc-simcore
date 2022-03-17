47842be1e2df98c3807137edcd9be43442075363
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-03-17 13:53:59 +0100

    added hypothesis common code

---
+++ packages/pytest-simcore/src/pytest_simcore/hypothesis_type_strategies.py
@@ -0,0 +1,8 @@
+from hypothesis import provisional
+from hypothesis import strategies as st
+from pydantic import AnyUrl, HttpUrl
+
+# FIXME: For now it seems the pydantic hypothesis plugin does not provide strategies for these types.
+# therefore we currently provide it
+st.register_type_strategy(AnyUrl, provisional.urls())
+st.register_type_strategy(HttpUrl, provisional.urls())
