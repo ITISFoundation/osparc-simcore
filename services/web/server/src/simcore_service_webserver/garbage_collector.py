7925a64073354a7fe880e51d5bbc64b02f68fc92
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2022-03-18 00:18:27 +0100

    🚑️ GC needs dependency to login storage (#2904)

--- services/web/server/src/simcore_service_webserver/garbage_collector.py
+++ services/web/server/src/simcore_service_webserver/garbage_collector.py
@@ -4,6 +4,7 @@ from aiohttp import web
 from servicelib.aiohttp.application_setup import ModuleCategory, app_module_setup
 
 from .garbage_collector_task import run_background_task
+from .login.plugin import setup_login_storage
 from .projects.plugin import setup_projects_db, setup_projects_model_schema
 from .socketio.plugin import setup_socketio_server
 
@@ -27,5 +28,7 @@ def setup_garbage_collector(app: web.Application):
     setup_projects_model_schema(app)
     # - project needs access to socketio via notify_project_state_update
     setup_socketio_server(app)
+    # - project needs access to user-api that is connected to login plugin
+    setup_login_storage(app)
 
     app.cleanup_ctx.append(run_background_task)
