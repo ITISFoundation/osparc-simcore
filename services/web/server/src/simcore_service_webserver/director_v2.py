1dea3f53b402cd227bc6a89053d6521e7374a83e
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2022-03-17 14:29:42 +0100

    ♻️  Is2757/gc as a service implementation  (⚠️⚠️ devops) (#2834)

--- services/web/server/src/simcore_service_webserver/director_v2.py
+++ services/web/server/src/simcore_service_webserver/director_v2.py
@@ -2,7 +2,11 @@ import logging
 
 from aiohttp import web
 from servicelib.aiohttp.application_keys import APP_SETTINGS_KEY
-from servicelib.aiohttp.application_setup import ModuleCategory, app_module_setup
+from servicelib.aiohttp.application_setup import (
+    ModuleCategory,
+    app_module_setup,
+    is_setup_completed,
+)
 from servicelib.aiohttp.rest_routing import (
     iter_path_operations,
     map_handlers_with_operations,
@@ -26,23 +30,31 @@ log = logging.getLogger(__name__)
 def setup_director_v2(app: web.Application):
     assert app[APP_SETTINGS_KEY].WEBSERVER_DIRECTOR_V2  # nosec
 
-    # dependencies
-    setup_rest(app)
-
-    # client
+    # client to communicate with director-v2 service
     set_client(app, DirectorV2ApiClient(app))
 
-    # routes
-    set_project_run_policy(app, DefaultProjectRunPolicy())
+    # routes at the web-server app
+    setup_rest(app)
 
-    specs = app[APP_OPENAPI_SPECS_KEY]
-    routes = map_handlers_with_operations(
-        {
-            "start_pipeline": director_v2_handlers.start_pipeline,
-            "stop_pipeline": director_v2_handlers.stop_pipeline,
-        },
-        filter(lambda o: "computation" in o[1], iter_path_operations(specs)),
-        strict=True,
-    )
-    # TODO:  app.router.add_routes(director_v2_handlers.routes) and corresponding tests
-    app.router.add_routes(routes)
+    if is_setup_completed(setup_rest.metadata()["module_name"], app):
+        log.debug("Adding routes")
+
+        set_project_run_policy(app, DefaultProjectRunPolicy())
+
+        specs = app[APP_OPENAPI_SPECS_KEY]
+        routes = map_handlers_with_operations(
+            {
+                "start_pipeline": director_v2_handlers.start_pipeline,
+                "stop_pipeline": director_v2_handlers.stop_pipeline,
+            },
+            filter(lambda o: "computation" in o[1], iter_path_operations(specs)),
+            strict=True,
+        )
+        # TODO:  app.router.add_routes(director_v2_handlers.routes) and corresponding tests
+        app.router.add_routes(routes)
+
+    else:
+
+        log.warning(
+            "Skipping computation routes since WEBSERVER_REST plugin is disabled (i.e. service w/o http API)"
+        )
