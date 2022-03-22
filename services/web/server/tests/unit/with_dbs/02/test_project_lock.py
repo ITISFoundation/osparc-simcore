b729e95e08b1029ddebc7de3300d01307da015f9
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-03-22 18:51:04 +0100

    test value

--- services/web/server/tests/unit/with_dbs/02/test_project_lock.py
+++ services/web/server/tests/unit/with_dbs/02/test_project_lock.py
@@ -1,6 +1,9 @@
 from faker import Faker
 from models_library.projects_state import ProjectState, ProjectStatus
-from simcore_service_webserver.projects.project_lock import lock_project
+from simcore_service_webserver.projects.project_lock import (
+    PROJECT_REDIS_LOCK_KEY,
+    lock_project,
+)
 from aiohttp.test_utils import TestClient
 from typing import Dict, Any
 
@@ -20,10 +23,14 @@ def project_uuid(faker: Faker) -> ProjectID:
     return faker.uuid4(cast_to=None)
 
 
+import aioredis
+
+
 async def test_lock_project(
     client: TestClient,
     user_id: UserID,
     project_uuid: ProjectID,
+    redis_client: aioredis.Redis,
 ):
     assert client.app
     async with lock_project(
@@ -33,4 +40,7 @@ async def test_lock_project(
         user_id=user_id,
         user_name={"first_name": "pytest", "last_name": "smith"},
     ):
-        ...
+        redis_value = await redis_client.get(
+            PROJECT_REDIS_LOCK_KEY.format(project_uuid)
+        )
+        assert redis_value
