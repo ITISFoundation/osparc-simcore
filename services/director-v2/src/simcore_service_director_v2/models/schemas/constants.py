1c911285388067448a3ef90ea934695d144c52d4
Dustin Kaiser <kaiser@itis.swiss>
2022-03-23 11:41:48 +0100

    Change regex

--- services/director-v2/src/simcore_service_director_v2/models/schemas/constants.py
+++ services/director-v2/src/simcore_service_director_v2/models/schemas/constants.py
@@ -12,7 +12,9 @@ DYNAMIC_PROXY_SERVICE_PREFIX = "dy-proxy"
 # - local
 # - itisfoundation
 # - 10.0.0.0:8473 (IP & Port)
-DYNAMIC_SIDECAR_DOCKER_IMAGE_RE = r"(^(local|itisfoundation|((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+))/)?(dynamic-sidecar):([\w]+)"
+DYNAMIC_SIDECAR_DOCKER_IMAGE_RE = (
+    r"(^([a-zA-Z0-9:.-]+)/)?(dynamic-sidecar):([a-zA-Z0-9.-]+$)"
+)
 
 REGEX_DY_SERVICE_SIDECAR = fr"^{DYNAMIC_SIDECAR_SERVICE_PREFIX}_[a-zA-Z0-9-_]*"
 REGEX_DY_SERVICE_PROXY = fr"^{DYNAMIC_PROXY_SERVICE_PREFIX}_[a-zA-Z0-9-_]*"
