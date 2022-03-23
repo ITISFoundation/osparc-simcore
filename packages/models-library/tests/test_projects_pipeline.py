e0b00f0b64cc073be65eb219829ff69e4f9399dc
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-03-23 18:33:50 +0100

    minor

--- packages/models-library/tests/test_projects_pipeline.py
+++ packages/models-library/tests/test_projects_pipeline.py
@@ -3,16 +3,20 @@
 # pylint:disable=redefined-outer-name
 
 from pprint import pformat
+from typing import Dict
 
 import pytest
 from models_library.projects_pipeline import ComputationTask
+from pydantic import BaseModel
 
 
 @pytest.mark.parametrize(
     "model_cls",
     (ComputationTask,),
 )
-def test_computation_task_model_examples(model_cls, model_cls_examples):
+def test_computation_task_model_examples(
+    model_cls: BaseModel, model_cls_examples: Dict[str, Dict]
+):
     for name, example in model_cls_examples.items():
         print(name, ":", pformat(example))
         model_instance = model_cls(**example)
