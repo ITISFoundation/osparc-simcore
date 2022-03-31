6528ab8aadcf944ace05b6b62e423cc496b5fe3f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-03-31 21:06:39 +0200

    setup hadolint correctly

--- scripts/hadolint.bash
+++ scripts/hadolint.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-# dockerfile linter tool
-# - https://github.com/hadolint/hadolint
-exec docker run --rm --interactive hadolint/hadolint -< "$@"
+
+docker run --rm -i hadolint/hadolint hadolint \
+  - < "$@"
