bb014a476357cf12291f575c98d86c5eb73c1a02
Andrei Neagu <neagu@itis.swiss>
2022-04-06 08:29:31 +0200

    added test to ensure compare works

--- services/director-v2/tests/unit/test_schemas_dynamic_services_scheduler.py
+++ services/director-v2/tests/unit/test_schemas_dynamic_services_scheduler.py
@@ -1,11 +1,22 @@
 # pylint: disable=redefined-outer-name
 
+from copy import deepcopy
 from pathlib import Path
+from attr import attr
 import pytest
+from typing import Any, Callable, Any
 import json
 from simcore_service_director_v2.models.schemas.dynamic_services.scheduler import (
     SchedulerData,
 )
+from dataclasses import dataclass
+
+
+@dataclass
+class Change:
+    target_prop: Callable[[SchedulerData], Any]
+    attr_name: str
+    value: Any
 
 
 @pytest.fixture(
@@ -27,5 +38,41 @@ def fake_scheduler_data(mocks_dir: Path, fake_data_file_name: str) -> str:
 
 
 async def test_parse_saved_fake_scheduler_data(fake_scheduler_data: str) -> None:
+    assert SchedulerData.parse_obj(json.loads(fake_scheduler_data))
+
+
+@pytest.mark.parametrize(
+    "change",
+    [
+        Change(
+            target_prop=lambda x: x.paths_mapping,
+            attr_name="inputs_path",
+            value=Path("/tmp"),
+        ),
+        Change(target_prop=lambda x: x, attr_name="version", value="2.0.5"),
+        Change(target_prop=lambda x: x.dynamic_sidecar, attr_name="port", value=33333),
+        Change(
+            target_prop=lambda x: x.dynamic_sidecar.status,
+            attr_name="info",
+            value="some info",
+        ),
+        Change(
+            target_prop=lambda x: x.dynamic_sidecar,
+            attr_name="containers_inspect",
+            value=[],
+        ),
+        Change(
+            target_prop=lambda x: x.dynamic_sidecar.service_removal_state,
+            attr_name="was_removed",
+            value=True,
+        ),
+    ],
+)
+async def test_nested_compare(fake_scheduler_data: str, change: Change) -> None:
     scheduler_data = SchedulerData.parse_obj(json.loads(fake_scheduler_data))
-    assert scheduler_data
+    original_scheduler_data = deepcopy(scheduler_data)
+
+    # apply change
+    change.target_prop(scheduler_data).__setattr__(change.attr_name, change.value)
+
+    assert original_scheduler_data != scheduler_data
