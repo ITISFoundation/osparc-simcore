8ad33394780a1958e281434b22dd8b81bbbd7f50
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-04-06 21:20:44 +0200

    url of stop is stop

--- services/director-v2/src/simcore_service_director_v2/api/routes/computations.py
+++ services/director-v2/src/simcore_service_director_v2/api/routes/computations.py
@@ -2,7 +2,6 @@
 
 import contextlib
 import logging
-import re
 from typing import Any, List, Optional
 
 import networkx as nx
@@ -335,7 +334,7 @@ async def stop_computation_project(
             pipeline_details=await compute_pipeline_details(
                 complete_dag, pipeline_dag, tasks
             ),
-            url=parse_obj_as(AnyHttpUrl, re.sub(r":stop", "", f"{request.url}")),
+            url=parse_obj_as(AnyHttpUrl, f"{request.url}"),
             stop_url=None,
             iteration=last_run.iteration if last_run else None,
             cluster_id=last_run.cluster_id if last_run else None,
