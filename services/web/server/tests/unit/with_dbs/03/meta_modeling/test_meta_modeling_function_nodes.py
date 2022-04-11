952a79cd11aea7177534c33e3c9ddcd1e8da464f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-04-11 13:43:56 +0200

    cleanup usage of function services

--- services/web/server/tests/unit/with_dbs/03/meta_modeling/test_meta_modeling_function_nodes.py
+++ services/web/server/tests/unit/with_dbs/03/meta_modeling/test_meta_modeling_function_nodes.py
@@ -7,19 +7,19 @@ import collections.abc
 import inspect
 from typing import get_origin
 
-from simcore_service_webserver.meta_modeling_function_nodes import (
-    FUNCTION_SERVICE_TO_CALLABLE,
-)
+from simcore_service_webserver.meta_modeling_function_nodes import catalog
 
 # TODO: test i/o schemas on FRONTEND_SERVICES_CATALOG fit the  _fun Callable
 
 
 def test_frontend_service_to_callable_registry():
 
-    print(f"\n{len(FUNCTION_SERVICE_TO_CALLABLE)=}")
-    for (node_key, node_version), node_call in FUNCTION_SERVICE_TO_CALLABLE.items():
-        print(" -", node_key, node_version, node_call.__name__)
-        assert (
-            get_origin(inspect.signature(node_call).return_annotation)
-            is collections.abc.Iterator
-        ), f"Expected iterable nodes only {(node_key, node_version)=}"
+    print(f"\n{len(catalog)=}")
+
+    for (node_key, node_version), func in catalog.iter_items():
+        if node_call := func.implementation:
+            print(" -", node_key, node_version, node_call.__name__)
+            assert (
+                get_origin(inspect.signature(node_call).return_annotation)
+                is collections.abc.Iterator
+            ), f"Expected iterable nodes only {(node_key, node_version)=}"
