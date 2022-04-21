c5335d65449fbd35b53fa28744f6d7258504f690
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-04-21 10:08:56 +0200

    fixed unit test for getting node resources

--- services/web/server/tests/unit/with_dbs/02/test_projects_nodes_handler.py
+++ services/web/server/tests/unit/with_dbs/02/test_projects_nodes_handler.py
@@ -3,13 +3,50 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
+import re
 from typing import Any, Type
 
 import pytest
 from aiohttp import web
 from aiohttp.test_utils import TestClient
 from pytest_simcore.helpers.utils_assert import assert_status
+from pytest_simcore.helpers.utils_login import UserInfoDict
+from simcore_service_webserver.catalog_settings import (
+    CatalogSettings,
+    get_plugin_settings,
+)
 from simcore_service_webserver.db_models import UserRole
+
+
+@pytest.fixture
+def mock_catalog_service_api_responses(client, aioresponses_mocker):
+    settings: CatalogSettings = get_plugin_settings(client.app)
+    url_pattern = re.compile(f"^{settings.base_url}+/.*$")
+
+    aioresponses_mocker.get(
+        url_pattern,
+        payload={"data": {}},
+        repeat=True,
+    )
+    aioresponses_mocker.post(
+        url_pattern,
+        payload={"data": {}},
+        repeat=True,
+    )
+    aioresponses_mocker.put(
+        url_pattern,
+        payload={"data": {}},
+        repeat=True,
+    )
+    aioresponses_mocker.patch(
+        url_pattern,
+        payload={"data": {}},
+        repeat=True,
+    )
+    aioresponses_mocker.delete(
+        url_pattern,
+        repeat=True,
+    )
 
 
 @pytest.mark.parametrize(
@@ -23,8 +60,9 @@ from simcore_service_webserver.db_models import UserRole
 )
 async def test_get_node_resources(
     client: TestClient,
-    logged_user: dict[str, Any],
+    logged_user: UserInfoDict,
     user_project: dict[str, Any],
+    mock_catalog_service_api_responses: None,
     expected: Type[web.HTTPException],
 ):
     assert client.app
@@ -48,8 +86,9 @@ async def test_get_node_resources(
 )
 async def test_replace_node_resources(
     client: TestClient,
-    logged_user: dict[str, Any],
+    logged_user: UserInfoDict,
     user_project: dict[str, Any],
+    mock_catalog_service_api_responses: None,
     expected: Type[web.HTTPException],
 ):
     assert client.app
