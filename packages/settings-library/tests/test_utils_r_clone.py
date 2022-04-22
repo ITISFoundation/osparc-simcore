1789bc17adfe6bfd6c724ffc2dd4c85238f75d56
Andrei Neagu <neagu@itis.swiss>
2022-04-22 14:31:26 +0200

    fix tests

--- packages/settings-library/tests/test_utils_r_clone.py
+++ packages/settings-library/tests/test_utils_r_clone.py
@@ -7,7 +7,13 @@ from settings_library.utils_r_clone import _COMMON_ENTRIES, get_r_clone_config
 
 @pytest.fixture(params=list(S3Provider))
 def r_clone_settings(request, monkeypatch) -> RCloneSettings:
-    monkeypatch.setenv("R_CLONE_S3_PROVIDER", request.param)
+    monkeypatch.setenv("R_CLONE_PROVIDER", request.param)
+    monkeypatch.setenv("R_CLONE_STORAGE_ENDPOINT", "storage_endpoint")
+    monkeypatch.setenv("S3_ENDPOINT", "endpoint")
+    monkeypatch.setenv("S3_ACCESS_KEY", "access_key")
+    monkeypatch.setenv("S3_SECRET_KEY", "secret_key")
+    monkeypatch.setenv("S3_BUCKET_NAME", "bucket_name")
+    monkeypatch.setenv("S3_SECURE", False)
     return RCloneSettings()
 
 
