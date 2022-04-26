14be36c1008f42b26e6a854e13a31ca98114de0e
Andrei Neagu <neagu@itis.swiss>
2022-04-26 09:38:49 +0200

    moved tests

--- services/director-v2/tests/unit/test_modules_dynamic_sidecar_docker_api.py
+++ services/director-v2/tests/unit/test_modules_dynamic_sidecar_docker_api.py
@@ -406,6 +406,12 @@ def test_valid_network_names(
     monkeypatch.setenv("TRAEFIK_SIMCORE_ZONE", "test_traefik_zone")
     monkeypatch.setenv("SWARM_STACK_NAME", "test_swarm_name")
     monkeypatch.setenv("R_CLONE_PROVIDER", "MINIO")
+    monkeypatch.setenv("S3_ENDPOINT", "endpoint")
+    monkeypatch.setenv("S3_ACCESS_KEY", "access_key")
+    monkeypatch.setenv("S3_SECRET_KEY", "secret_key")
+    monkeypatch.setenv("S3_BUCKET_NAME", "bucket_name")
+    monkeypatch.setenv("S3_SECURE", "false")
+    monkeypatch.setenv("R_CLONE_STORAGE_ENDPOINT", "storage_endpoint")
     dynamic_sidecar_settings = DynamicSidecarSettings.create_from_envs()
     assert dynamic_sidecar_settings
 
