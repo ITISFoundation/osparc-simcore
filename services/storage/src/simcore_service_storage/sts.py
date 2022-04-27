ffe669657faf51dd2aa2b64520ae1699866d62b9
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-04-27 09:19:57 +0200

    storage implements API to return default S3 settings

---
+++ services/storage/src/simcore_service_storage/sts.py
@@ -0,0 +1,11 @@
+from aiohttp import web
+from models_library.users import UserID
+from servicelib.aiohttp.application_keys import APP_CONFIG_KEY
+from settings_library.s3 import S3Settings
+
+
+async def get_or_create_temporary_token_for_user(
+    app: web.Application, user_id: UserID
+) -> S3Settings:
+    app_settings = app[APP_CONFIG_KEY]
+    return app_settings.STORAGE_S3
--- services/storage/tests/unit/test_route_s3_access.py
+++ services/storage/tests/unit/test_route_s3_access.py
@@ -10,6 +10,7 @@ import pytest
 from aiohttp import web
 from aiohttp.test_utils import TestClient
 from pytest_simcore.helpers.utils_assert import assert_status
+from settings_library.s3 import S3Settings
 from simcore_service_storage.application import create
 from simcore_service_storage.settings import Settings
 
@@ -37,12 +38,16 @@ def client(
     )
 
 
-async def test_simcore_s3_access(client: TestClient):
+async def test_simcore_s3_access_returns_default(client: TestClient):
     assert client.app
     url = (
         client.app.router["get_or_create_temporary_s3_access"]
         .url_for()
         .with_query(user_id=1)
     )
-    response = await client.get(f"{url}")
-    await assert_status(response, web.HTTPOk)
+    response = await client.post(f"{url}")
+    data, error = await assert_status(response, web.HTTPOk)
+    assert not error
+    assert data
+    received_settings = S3Settings.parse_obj(data)
+    assert received_settings
