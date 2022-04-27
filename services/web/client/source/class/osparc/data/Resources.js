b580bb609065f0d27b62f5f519363dfff4c1bdfe
odeimaiz <maiz@itis.swiss>
2022-04-27 07:56:05 +0200

    getNodeErrors to Resources

--- services/web/client/source/class/osparc/data/Resources.js
+++ services/web/client/source/class/osparc/data/Resources.js
@@ -142,6 +142,11 @@ qx.Class.define("osparc.data.Resources", {
             method: "DELETE",
             url: statics.API + "/projects/{studyId}/nodes/{nodeId}"
           },
+          getNodeErrors: {
+            useCache: false,
+            method: "GET",
+            url: statics.API + "/projects/{studyId}/nodes/{nodeId}/errors"
+          },
           addTag: {
             useCache: false,
             method: "PUT",
