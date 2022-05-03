d1fbac06bee4d0bd2d45dd7eab56a9a6031b758e
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2022-05-03 16:59:10 +0200

    ♻️ maintenance: fixes scicrunch tests and set as integration tests (#3018)
    * split scicrunch into unit and integration tests
    
    * doc
    
    * adapts tests to pass

--- services/web/server/tests/integration/02/scicrunch/test_scicrunch__resolver.py
+++ services/web/server/tests/integration/02/scicrunch/test_scicrunch__resolver.py
@@ -10,6 +10,7 @@ from aiohttp.client import ClientTimeout
 # FIXME: PC check the CELL_LINE_CITATIONS test please
 from pytest_simcore.helpers.utils_scrunch_citations import (  # CELL_LINE_CITATIONS,
     ANTIBODY_CITATIONS,
+    CELL_LINE_CITATIONS,
     ORGANISM_CITATIONS,
     PLAMID_CITATIONS,
     TOOL_CITATIONS,
@@ -20,18 +21,27 @@ from simcore_service_webserver.scicrunch.settings import SciCrunchSettings
 
 @pytest.mark.parametrize(
     "name,rrid",
-    TOOL_CITATIONS + ANTIBODY_CITATIONS + PLAMID_CITATIONS + ORGANISM_CITATIONS,
+    TOOL_CITATIONS
+    + ANTIBODY_CITATIONS
+    + PLAMID_CITATIONS
+    + ORGANISM_CITATIONS
+    + CELL_LINE_CITATIONS,
 )
 async def test_scicrunch_resolves_all_valid_rrids(
     name: str, rrid: str, settings: SciCrunchSettings
 ):
+    # NOTE: this test run against https://scicrunch.org/resolver/{SCR_018997}.json
+    # which is an open API (no auth required). Any change in the responses of that
+    # service might cause a failure on this test
+    # This tests checks some of the structure "deduced" from the responses so far.
+
     async with ClientSession(timeout=ClientTimeout(total=30)) as client:
-        resolved = await resolve_rrid(rrid, client, settings)
+        resolved = await resolve_rrid(identifier=rrid, client=client, settings=settings)
 
         assert resolved
         assert isinstance(resolved, ResolvedItem)
 
-        if resolved.is_unique:
+        if resolved.is_unique and name:
             assert name in resolved.proper_citation
 
         assert rrid in resolved.proper_citation
@@ -42,7 +52,7 @@ async def test_scicrunch_resolves_all_valid_rrids(
             # only rrid with a prefix
             assert resolved.proper_citation == f"RRID:{rrid}"
         else:
-            # includes name and rrid
+            # proper_citation includes both 'name' and 'rrid' but in different formats!
 
             #
             # NOTE: why CELL_LINE_CITATIONS are removed from test parametrization ?
