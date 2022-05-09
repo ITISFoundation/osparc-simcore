e3a05e809e8f05bfdb01dddbb608b356b589e878
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-05-09 10:06:37 +0200

    separate services entrypoint tests

--- services/catalog/src/simcore_service_catalog/api/root.py
+++ services/catalog/src/simcore_service_catalog/api/root.py
@@ -8,5 +8,5 @@ router.include_router(health.router)
 # API
 router.include_router(meta.router, tags=["meta"], prefix="/meta")
 router.include_router(dags.router, tags=["DAG"], prefix="/dags")
-router.include_router(services.router, tags=["services"], prefix="/services")
 router.include_router(services_resources.router, tags=["services"], prefix="/services")
+router.include_router(services.router, tags=["services"], prefix="/services")
