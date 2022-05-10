ac6015cd775d1c31b9eb17fe837721b1672db126
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-05-10 15:13:55 +0200

    code smell

--- services/catalog/src/simcore_service_catalog/api/root.py
+++ services/catalog/src/simcore_service_catalog/api/root.py
@@ -15,9 +15,15 @@ router.include_router(health.router)
 # API
 router.include_router(meta.router, tags=["meta"], prefix="/meta")
 router.include_router(dags.router, tags=["DAG"], prefix="/dags")
-router.include_router(services_resources.router, tags=["services"], prefix="/services")
+
+# /services
+services_prefix = "/services"
+services_tags = ["services"]
 router.include_router(
-    services_specifications.router, tags=["services"], prefix="/services"
+    services_resources.router, tags=services_tags, prefix=services_prefix
+)
+router.include_router(
+    services_specifications.router, tags=services_tags, prefix=services_prefix
 )
 # note that this router must come after resources/specifications
-router.include_router(services.router, tags=["services"], prefix="/services")
+router.include_router(services.router, tags=services_tags, prefix=services_prefix)
