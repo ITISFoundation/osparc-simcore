fc68f6fa3ca997198503ca58ad47501517bb7cb1
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-05-12 15:29:02 +0200

    renamed and optimized merge method

--- services/director-v2/src/simcore_service_director_v2/utils/dict_utils.py
+++ services/director-v2/src/simcore_service_director_v2/utils/dict_utils.py
@@ -4,15 +4,12 @@ from typing import Any
 from toolz.dicttoolz import get_in, update_in
 
 
-def merge_extend(
-    a: dict[str, Any],
-    b: dict[str, Any],
-    *,
-    extendable_array_keys: tuple[list[str], ...],
-    extendable_dict_keys: tuple[list[str], ...],
+def nested_update(
+    a: dict[str, Any], b: dict[str, Any], *, include: tuple[list[str], ...]
 ) -> dict[str, Any]:
-    """returns dict which results of merging a with b by extending
-    the fields defined in extendable_array_keys and extendable_dict_keys
+    """returns dict which results of updating a with b on selected key paths
+    by merging dictionaries and extending arrays with what is found in b
+    Only the fields defined in
 
     example:
     a={"labels"{"subentry":[1,2]}}
@@ -21,19 +18,23 @@ def merge_extend(
     --> result={"labels"{"subentry":[1,2,3,6]}}
 
     """
+
+    def _merge_fct(a, b):
+        if isinstance(b, list):
+            return (a or []) + b
+        if isinstance(b, dict):
+            return (a or {}) | b
+        return b
+
     merged_dict = deepcopy(a)
-    for keys_path in extendable_array_keys:
+    for keys_path in include:
+        b_value = get_in(keys_path, b)
+        if b_value is None:
+            # skip the merge if there is no value in b
+            continue
+        assert b_value is not None  # nosec
         merged_dict = update_in(
-            merged_dict,
-            keys_path,
-            lambda value, keys_path=keys_path, b=b: (value or [])
-            + get_in(keys_path, b, []),
+            merged_dict, keys_path, lambda x, b_value=b_value: _merge_fct(x, b_value)
         )
-    for keys_path in extendable_dict_keys:
-        merged_dict = update_in(
-            merged_dict,
-            keys_path,
-            lambda value, keys_path=keys_path, b=b: (value or {})
-            | get_in(keys_path, b, {}),
-        )
+
     return merged_dict
--- services/director-v2/tests/unit/test_utils_dict_utils.py
+++ services/director-v2/tests/unit/test_utils_dict_utils.py
@@ -2,38 +2,97 @@ from dataclasses import dataclass
 from typing import Any
 
 import pytest
-from simcore_service_director_v2.utils.dict_utils import merge_extend
+from simcore_service_director_v2.utils.dict_utils import nested_update
 
 
 @dataclass(
     frozen=True,
 )
 class MergeStructureTestParams:
-    struct_a: dict[str, Any]
-    struct_b: dict[str, Any]
-    extendable_arrays: tuple[list[str], ...]
-    extendable_dicts: tuple[list[str], ...]
-    expected_result_struct: dict[str, Any]
+    a: dict[str, Any]
+    b: dict[str, Any]
+    include: tuple[list[str], ...]
+    expected_dict: dict[str, Any]
 
 
 @pytest.mark.parametrize(
     "test_params",
     [
         MergeStructureTestParams(
-            {"labels": ["1", "2"]},
-            {"labels": ["3", "2"]},
-            (["labels"],),
-            (),
-            {"labels": ["1", "2", "3", "2"]},
-        )
+            a={},
+            b={},
+            include=(["a", "key", "path"], ["another"]),
+            expected_dict={},
+        ),
+        MergeStructureTestParams(
+            a={"labels": ["1", "2"]},
+            b={"labels": ["3", "2"]},
+            include=(["labels"],),
+            expected_dict={"labels": ["1", "2", "3", "2"]},
+        ),
+        MergeStructureTestParams(
+            a={"labels": {"my_label": "label_value1"}},
+            b={"labels": {"my_label": "label_value2"}},
+            include=(["labels"],),
+            expected_dict={"labels": {"my_label": "label_value2"}},
+        ),
+        MergeStructureTestParams(
+            a={"labels": {"my_label1": "label_value1"}},
+            b={"labels": {"my_label2": "label_value2"}},
+            include=(["labels"],),
+            expected_dict={
+                "labels": {"my_label1": "label_value1", "my_label2": "label_value2"}
+            },
+        ),
+        MergeStructureTestParams(
+            a={
+                "labels": {"my_label1": "label_value1"},
+                "entry1": {"entry2": {"entry3": "value"}},
+            },
+            b={"labels": {"my_label2": "label_value2"}},
+            include=(["labels"], ["entry1", "entry2"]),
+            expected_dict={
+                "labels": {"my_label1": "label_value1", "my_label2": "label_value2"},
+                "entry1": {"entry2": {"entry3": "value"}},
+            },
+        ),
+        MergeStructureTestParams(
+            a={
+                "labels": {"my_label1": "label_value1"},
+                "entry1": {"entry2": {"entry3": "value"}},
+            },
+            b={
+                "labels": {"my_label2": "label_value2"},
+                "entry1": {"entry2": {"entry4": "value"}},
+            },
+            include=(["labels"], ["entry1", "entry2"]),
+            expected_dict={
+                "labels": {"my_label1": "label_value1", "my_label2": "label_value2"},
+                "entry1": {"entry2": {"entry3": "value", "entry4": "value"}},
+            },
+        ),
+        MergeStructureTestParams(
+            a={
+                "labels": {"my_label1": "label_value1"},
+                "entry1": {"entry2": {"entry3": 3}},
+            },
+            b={
+                "labels": {"my_label2": "label_value2"},
+                "entry1": {"entry2": {"entry3": 5}},
+            },
+            include=(["labels"], ["entry1", "entry2", "entry3"]),
+            expected_dict={
+                "labels": {"my_label1": "label_value1", "my_label2": "label_value2"},
+                "entry1": {"entry2": {"entry3": 5}},
+            },
+        ),
     ],
 )
 def test_merge_complex_structures(test_params: MergeStructureTestParams):
-    returned_struct = merge_extend(
-        test_params.struct_a,
-        test_params.struct_b,
-        extendable_array_keys=test_params.extendable_arrays,
-        extendable_dict_keys=test_params.extendable_dicts,
+    returned_struct = nested_update(
+        test_params.a,
+        test_params.b,
+        include=test_params.include,
     )
-    assert returned_struct
-    assert returned_struct == test_params.expected_result_struct
+    assert returned_struct is not None
+    assert returned_struct == test_params.expected_dict
