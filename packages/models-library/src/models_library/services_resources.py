2661cc9733ab53f4a1190c0fddda9e0ed1ba50b1
Andrei Neagu <neagu@itis.swiss>
2022-05-12 15:29:52 +0200

    extended resoruces

--- packages/models-library/src/models_library/services_resources.py
+++ packages/models-library/src/models_library/services_resources.py
@@ -1,9 +1,27 @@
-from typing import Union
+import logging
+from typing import Any, Union
 
 from models_library.generics import DictModel
-from pydantic import BaseModel, StrictFloat, StrictInt, root_validator
+from pydantic import (
+    BaseModel,
+    ByteSize,
+    Field,
+    StrictFloat,
+    StrictInt,
+    constr,
+    root_validator,
+)
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_RESOURCES: dict[str, Any] = {
+    "CPU": {"limit": 0.1, "reservation": 0.1},
+    "RAM": {"limit": ByteSize(2 * 1024**3), "reservation": ByteSize(2 * 1024**3)},
+}
 
 ResourceName = str
+
+ComposeImage = constr(regex=r"[\w/-]+:[\w.@]+")
 
 
 class ResourceValue(BaseModel):
@@ -22,4 +40,83 @@ class ResourceValue(BaseModel):
         return values
 
 
-ServiceResources = DictModel[ResourceName, ResourceValue]
+Resources = DictModel[ResourceName, ResourceValue]
+
+
+class ImageResources(BaseModel):
+    image: ComposeImage = Field(..., description="used by the fro")
+    resources: Resources
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "image": "simcore/service/dynamic/pretty-intense:1.0.0",
+                "resources": {
+                    "CPU": {"limit": 4, "reservation": 0.1},
+                    "RAM": {"limit": 103079215104, "reservation": 536870912},
+                    "VRAM": {"limit": 1, "reservation": 1},
+                    "AIRAM": {"limit": 1, "reservation": 1},
+                    "ANY_resource": {
+                        "limit": "some_value",
+                        "reservation": "some_value",
+                    },
+                },
+            }
+        }
+
+
+class ServiceResources(DictModel[str, ImageResources]):
+    @classmethod
+    def from_resources(cls, resource: Resources, image: str) -> "ServiceResources":
+        service_resources = cls.parse_obj(
+            {"container": {"image": image, "resources": resource}}
+        )
+        logger.debug("%s", f"{service_resources}")
+        return service_resources
+
+    class Config:
+        schema_extra = {
+            "examples": [
+                # no compose spec (majority of services)
+                {
+                    # NOTE: Try to do this for
+                    "container": {
+                        "image": "simcore/services/dynamic/jupyter-math:2.0.5",
+                        "resources": DEFAULT_RESOURCES,
+                    },
+                },
+                # service with a compose spec
+                {
+                    "rt-web-dy": {
+                        "image": "simcore/services/dynamic/sim4life-dy:3.0.0",
+                        "resources": {
+                            "CPU": {"limit": 0.3, "reservation": 0.3},
+                            "RAM": {"limit": 53687091232, "reservation": 53687091232},
+                        },
+                    },
+                    "s4l-core": {
+                        "image": "simcore/services/dynamic/s4l-core-dy:3.0.0",
+                        "resources": {
+                            "CPU": {"limit": 4.0, "reservation": 0.1},
+                            "RAM": {"limit": 17179869184, "reservation": 536870912},
+                            "VRAM": {"limit": 1, "reservation": 1},
+                        },
+                    },
+                    "sym-server": {
+                        "image": "simcore/services/dynamic/sym-server:3.0.0",
+                        "resources": DEFAULT_RESOURCES,
+                    },
+                },
+                # compose spec with image outside the platform
+                {
+                    "jupyter-lab": {
+                        "image": "simcore/services/dynamic/jupyter-math:4.0.0",
+                        "resources": DEFAULT_RESOURCES,
+                    },
+                    "proxy": {
+                        "image": "traefik:v2.6.6",
+                        "resources": DEFAULT_RESOURCES,
+                    },
+                },
+            ]
+        }
---
+++ packages/models-library/tests/test_service_resources.py
@@ -0,0 +1,57 @@
+# pylint:disable=unused-variable
+# pylint:disable=unused-argument
+# pylint:disable=redefined-outer-name
+
+from pprint import pformat
+from typing import Type
+
+import pytest
+from models_library.services_resources import (
+    ComposeImage,
+    ImageResources,
+    Resources,
+    ServiceResources,
+)
+from pydantic import BaseModel, parse_obj_as
+
+
+@pytest.mark.parametrize(
+    "model_cls",
+    (
+        ImageResources,
+        ServiceResources,
+    ),
+)
+def test_computation_task_model_examples(
+    model_cls: Type[BaseModel], model_cls_examples: dict[str, dict]
+):
+    for name, example in model_cls_examples.items():
+        print(name, ":", pformat(example))
+        model_instance = model_cls.parse_obj(example)
+        assert model_instance, f"Failed with {name}"
+
+
+@pytest.mark.parametrize(
+    "example",
+    (
+        "simcore/services/dynamic/the:latest",
+        "simcore/services/dynamic/nice-service:v1.0.0",
+        "a/docker-hub/image:1.0.0",
+        "traefik:v1.0.0",
+        "traefik:v1.0.0@somehash",
+    ),
+)
+def test_compose_image(example: str) -> None:
+    parse_obj_as(ComposeImage, example)
+
+
+@pytest.fixture
+def resources() -> Resources:
+    return Resources.parse_obj(
+        ImageResources.Config.schema_extra["example"]["resources"]
+    )
+
+
+def test_service_resoruces_from_resources(resources: Resources) -> None:
+    image = "asdsad:asdsadsa"
+    assert ServiceResources.from_resources(resources, image)
