0553697e9424f4bc3e831882f225eb3f36bc280a
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-05-13 00:26:39 +0200

    finally runs!

--- services/director-v2/src/simcore_service_director_v2/modules/dynamic_sidecar/scheduler/events.py
+++ services/director-v2/src/simcore_service_director_v2/modules/dynamic_sidecar/scheduler/events.py
@@ -165,17 +165,18 @@ class CreateSidecars(DynamicSchedulerEvent):
         )
 
         catalog_client = CatalogClient.instance(app)
-        user_specific_service_spec: dict[
-            str, Any
-        ] = await catalog_client.get_service_specifications(
+        user_specific_service_spec = await catalog_client.get_service_specifications(
             scheduler_data.user_id, scheduler_data.key, scheduler_data.version
         )
+        user_specific_service_spec = AioDockerServiceSpec.parse_obj(
+            user_specific_service_spec.get("sidecar", {})
+        )
+        # NOTE: since user_specific_service_spec follows Docker Service Spec and not Aio
+        # we do not use aliases when exporting dynamic_sidecar_service_spec_base
         dynamic_sidecar_service_final_spec = AioDockerServiceSpec.parse_obj(
             nested_update(
-                jsonable_encoder(
-                    dynamic_sidecar_service_spec_base, by_alias=True, exclude_unset=True
-                ),
-                user_specific_service_spec.get("sidecar", {}),
+                jsonable_encoder(dynamic_sidecar_service_spec_base, exclude_unset=True),
+                jsonable_encoder(user_specific_service_spec, exclude_unset=True),
                 include=DYNAMIC_SIDECAR_SERVICE_EXTENDABLE_SPECS,
             )
         )
