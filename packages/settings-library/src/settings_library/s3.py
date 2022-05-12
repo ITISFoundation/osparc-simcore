1fd7bc8b14801a2513beb12504b8a6d62e3478b4
Andrei Neagu <neagu@itis.swiss>
2022-05-12 16:35:16 +0200

    fix validator order

--- packages/settings-library/src/settings_library/s3.py
+++ packages/settings-library/src/settings_library/s3.py
@@ -5,12 +5,12 @@ from pydantic import validator
 
 
 class S3Settings(BaseCustomSettings):
+    S3_SECURE: bool = False
     S3_ENDPOINT: str
     S3_ACCESS_KEY: str
     S3_SECRET_KEY: str
     S3_ACCESS_TOKEN: Optional[str] = None
     S3_BUCKET_NAME: str
-    S3_SECURE: bool = False
     S3_REGION: str = "us-east-1"
 
     @validator("S3_ENDPOINT", pre=True)
