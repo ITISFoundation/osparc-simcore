387af9bbadbcf03586003e6e1ef9771d0a2f3384
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-05-13 16:24:30 +0200

    sonarcloud

--- services/catalog/src/simcore_service_catalog/api/routes/services_resources.py
+++ services/catalog/src/simcore_service_catalog/api/routes/services_resources.py
@@ -26,6 +26,25 @@ router = APIRouter()
 logger = logging.getLogger(__name__)
 
 
+def _parse_generic_resource(
+    generic_resources: list[Any], service_resources: ServiceResources
+) -> None:
+    for res in generic_resources:
+        if not isinstance(res, dict):
+            continue
+
+        if named_resource_spec := res.get("NamedResourceSpec"):
+            service_resources.setdefault(
+                named_resource_spec["Kind"],
+                ResourceValue(limit=0, reservation=named_resource_spec["Value"]),
+            ).reservation = named_resource_spec["Value"]
+        if discrete_resource_spec := res.get("DiscreteResourceSpec"):
+            service_resources.setdefault(
+                discrete_resource_spec["Kind"],
+                ResourceValue(limit=0, reservation=discrete_resource_spec["Value"]),
+            ).reservation = discrete_resource_spec["Value"]
+
+
 def _from_service_settings(
     settings: list[SimcoreServiceSettingLabelEntry],
     service_key: ServiceKey,
@@ -39,11 +58,12 @@ def _from_service_settings(
     for entry in resource_entries:
         if not isinstance(entry.value, dict):
             logger.warning(
-                "resource %s for %s got invalid type",
+                "resource %s for %s got invalid type, skipping it",
                 f"{entry.dict()!r}",
                 f"{service_key}:{service_version}",
             )
             continue
+
         if nano_cpu_limit := entry.value.get("Limits", {}).get("NanoCPUs"):
             service_resources["CPU"].limit = nano_cpu_limit / 1.0e09
         if nano_cpu_reservation := entry.value.get("Reservations", {}).get("NanoCPUs"):
@@ -53,27 +73,10 @@ def _from_service_settings(
         if ram_reservation := entry.value.get("Reservations", {}).get("MemoryBytes"):
             service_resources["RAM"].reservation = ram_reservation
 
-        if generic_resources := entry.value.get("Reservations", {}).get(
-            "GenericResources", []
-        ):
-            for res in generic_resources:
-                if not isinstance(res, dict):
-                    continue
-
-                if named_resource_spec := res.get("NamedResourceSpec"):
-                    service_resources.setdefault(
-                        named_resource_spec["Kind"],
-                        ResourceValue(
-                            limit=0, reservation=named_resource_spec["Value"]
-                        ),
-                    ).reservation = named_resource_spec["Value"]
-                if discrete_resource_spec := res.get("DiscreteResourceSpec"):
-                    service_resources.setdefault(
-                        discrete_resource_spec["Kind"],
-                        ResourceValue(
-                            limit=0, reservation=discrete_resource_spec["Value"]
-                        ),
-                    ).reservation = discrete_resource_spec["Value"]
+        _parse_generic_resource(
+            entry.value.get("Reservations", {}).get("GenericResources", []),
+            service_resources,
+        )
 
     return service_resources
 
