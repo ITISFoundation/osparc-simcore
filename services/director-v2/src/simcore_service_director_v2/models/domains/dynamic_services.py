783e21715c7a874be7171dd5e14836aa1a823d1a
Andrei Neagu <neagu@itis.swiss>
2022-05-13 10:22:00 +0200

    update example

--- services/director-v2/src/simcore_service_director_v2/models/domains/dynamic_services.py
+++ services/director-v2/src/simcore_service_director_v2/models/domains/dynamic_services.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from models_library.services import PropertyName
+from models_library.services_resources import ServiceResources
 from pydantic import BaseModel, ByteSize, Field
 
 from ..schemas.dynamic_services import RunningDynamicServiceDetails, ServiceDetails
@@ -31,5 +32,23 @@ class RetrieveDataOutEnveloped(BaseModel):
         schema_extra = {"examples": [{"data": {"size_bytes": 42}}]}
 
 
-DynamicServiceCreate = ServiceDetails
+class DynamicServiceCreate(ServiceDetails):
+    service_resources: ServiceResources
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "key": "simcore/services/dynamic/3dviewer",
+                "version": "2.4.5",
+                "user_id": 234,
+                "project_id": "dd1d04d9-d704-4f7e-8f0f-1ca60cc771fe",
+                "node_uuid": "75c7f3f4-18f9-4678-8610-54a2ade78eaa",
+                "basepath": "/x/75c7f3f4-18f9-4678-8610-54a2ade78eaa",
+                "service_resources": ServiceResources.Config.schema_extra["examples"][
+                    0
+                ],
+            }
+        }
+
+
 DynamicServiceOut = RunningDynamicServiceDetails
