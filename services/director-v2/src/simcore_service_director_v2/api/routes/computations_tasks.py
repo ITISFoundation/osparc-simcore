c4bca060a24956f043e3caec12f8a541f022629d
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-05-17 19:37:36 +0200

    drafts new api entry

---
+++ services/director-v2/src/simcore_service_director_v2/api/routes/computations_tasks.py
@@ -0,0 +1,36 @@
+""" CRUD operations on a computation's tasks sub-resource
+
+A task is computation sub-resource that respresents a running computational service in the pipeline described above
+Therefore,
+ - the task ID is the same as the associated node uuid
+
+"""
+# pylint: disable=too-many-arguments
+
+
+import logging
+
+from fastapi import APIRouter
+from models_library.projects import ProjectID
+from models_library.projects_nodes_io import NodeID
+from models_library.users import UserID
+from starlette import status
+
+from ...models.schemas.comp_tasks import ComputationGet
+
+log = logging.getLogger(__name__)
+
+router = APIRouter()
+
+
+@router.get(
+    "/{project_id}/tasks/{node_uuid}/log",
+    summary="Returns a computation pipeline state",
+    response_model=ComputationGet,
+    status_code=status.HTTP_200_OK,
+)
+async def get_computation_task_log(
+    user_id: UserID, project_id: ProjectID, node_uuid: NodeID
+) -> ComputationGet:
+    # TODO: as close as possible to https://docs.docker.com/engine/api/v1.41/#operation/ContainerTop
+    raise NotImplementedError()
