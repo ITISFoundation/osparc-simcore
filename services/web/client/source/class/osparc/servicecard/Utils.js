597e2d9d0c40b734928e3ea024cac3ee97398f2c
odeimaiz <maiz@itis.swiss>
2022-05-19 11:58:05 +0200

    undo minor

--- services/web/client/source/class/osparc/servicecard/Utils.js
+++ services/web/client/source/class/osparc/servicecard/Utils.js
@@ -199,7 +199,7 @@ qx.Class.define("osparc.servicecard.Utils", {
       }));
 
       const label = new qx.ui.basic.Label(qx.locale.Manager.tr("Resources")).set({
-        font: "title-11"
+        font: "title-12"
       });
       resourcesLayout.add(label);
 
