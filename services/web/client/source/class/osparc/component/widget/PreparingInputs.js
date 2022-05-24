0ed2050e8d588cbcc853a93ad565c75e18ba2e4e
odeimaiz <maiz@itis.swiss>
2022-05-24 17:48:36 +0200

    listening to prepringNodes

--- services/web/client/source/class/osparc/component/widget/PreparingInputs.js
+++ services/web/client/source/class/osparc/component/widget/PreparingInputs.js
@@ -32,14 +32,34 @@ qx.Class.define("osparc.component.widget.PreparingInputs", {
     this._add(title);
 
     this.__preparingNodes = preparingNodes;
-    this.__preparingNodes.forEach(node => this._add(new qx.ui.basic.Label("- " + node.getLabel())));
+    const list = this.__monitoredNodesList = new qx.ui.container.Composite(new qx.ui.layout.VBox(5));
+    this._add(list);
+    this.__updateMonitoredNodesList();
+    preparingNodes.forEach(preparingNode => {
+      [
+        "changeRunning",
+        "changeOutput"
+      ].forEach(changeEvent => preparingNode.getStatus().addListener(changeEvent, () => this.__updateMonitoredNodesList()));
+    });
 
     const loggerView = this.__loggerView = new osparc.component.widget.logger.LoggerView();
     loggerView.getChildControl("pin-node").exclude();
-    this._add(loggerView);
+    this._add(loggerView, {
+      flex: 1
+    });
   },
 
   members: {
-    __preparingNodes: null
+    __preparingNodes: null,
+    __monitoredNodesList: null,
+
+    __updateMonitoredNodesList: function() {
+      this.__monitoredNodesList.removeAll();
+      this.__preparingNodes.forEach(node => {
+        if (!osparc.data.model.NodeStatus.isCompNodeReady(node)) {
+          this.__monitoredNodesList.add(new qx.ui.basic.Label("- " + node.getLabel()));
+        }
+      });
+    }
   }
 });
