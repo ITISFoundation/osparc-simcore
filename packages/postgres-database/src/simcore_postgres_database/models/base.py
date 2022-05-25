b1c0dcac17237e9d41f1d3cfd2dc2c2b935ee5eb
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-05-25 14:27:22 +0200

    Fixed MovedIn20Warning:
    The ``declarative_base()`` function is now available as sqlalchemy.orm.declarative_base(). (deprecated since: 1.4)
    
    Background on SQLAlchemy 2.0 at: https://sqlalche.me/e/b8d9

--- packages/postgres-database/src/simcore_postgres_database/models/base.py
+++ packages/postgres-database/src/simcore_postgres_database/models/base.py
@@ -3,10 +3,10 @@
     - Collects all table's schemas
     - Metadata object needed to explicitly define table schemas
 """
-from sqlalchemy.ext.declarative import declarative_base
+import sqlalchemy.orm
 
 #  DO NOT inheriting from _base. Use instead explicit table definitions
 #  See https://docs.sqlalchemy.org/en/latest/orm/mapping_styles.html#classical-mappings
-Base = declarative_base()
+Base = sqlalchemy.orm.declarative_base()
 
 metadata = Base.metadata
