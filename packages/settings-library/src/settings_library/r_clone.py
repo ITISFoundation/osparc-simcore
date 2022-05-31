10723913f249e70afc60186c3ba04771413d7243
Andrei Neagu <neagu@itis.swiss>
2022-05-31 10:07:51 +0200

    expanded rclone settings

--- packages/settings-library/src/settings_library/r_clone.py
+++ packages/settings-library/src/settings_library/r_clone.py
@@ -1,9 +1,11 @@
 from enum import Enum
 
-from pydantic import Field
+from pydantic import Field, constr
 
 from .base import BaseCustomSettings
 from .s3 import S3Settings
+
+MemoryStr = constr(regex=r"^[1-9][0-9]*[bkmg]")
 
 
 class S3Provider(str, Enum):
@@ -15,3 +17,7 @@ class S3Provider(str, Enum):
 class RCloneSettings(BaseCustomSettings):
     R_CLONE_S3: S3Settings = Field(auto_default_from_env=True)
     R_CLONE_PROVIDER: S3Provider
+    R_CLONE_VERSION: str = "1.58.1"
+    R_CLONE_MEMORY_RESERVATION: MemoryStr = "100m"
+    R_CLONE_MEMORY_LIMIT: MemoryStr = "1g"
+    R_CLONE_MAX_CPU_USAGE: float = 0.5
