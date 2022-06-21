8cc35694e4b25ba34e5f5d3dfa6c322048fc0912
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-06-21 13:32:57 +0200

    cleanup fixtures

--- services/dynamic-sidecar/tests/conftest.py
+++ services/dynamic-sidecar/tests/conftest.py
@@ -1,13 +1,12 @@
-# pylint: disable=unused-argument
 # pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
 
 import asyncio
 import json
 import os
 import random
 import sys
-import tempfile
-import uuid
 from pathlib import Path
 from typing import Any, AsyncGenerator, AsyncIterable, Iterator, List
 from unittest.mock import AsyncMock, Mock
@@ -16,6 +15,7 @@ import aiodocker
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
 from async_asgi_testclient import TestClient
+from faker import Faker
 from fastapi import FastAPI
 from pytest_mock.plugin import MockerFixture
 from simcore_service_dynamic_sidecar.core import utils
@@ -32,97 +32,103 @@ pytest_plugins = [
     "pytest_simcore.docker_swarm",
     "pytest_simcore.monkeypatch_extra",
     "pytest_simcore.pytest_global_environs",
+    "pytest_simcore.repository_paths",
 ]
 
-
-@pytest.fixture(scope="session")
-def mock_dy_volumes() -> Iterator[Path]:
-    with tempfile.TemporaryDirectory() as temp_dir:
-        yield Path(temp_dir)
+CURRENT_DIR = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
 
 @pytest.fixture(scope="session")
-def io_temp_dir() -> Iterator[Path]:
-    with tempfile.TemporaryDirectory() as temp_dir:
-        yield Path(temp_dir)
+def project_slug_dir() -> Path:
+    folder = CURRENT_DIR.parent
+    assert folder.exists()
+    assert any(folder.glob("src/simcore_service_dynamic_sidecar"))
+    return folder
 
 
-@pytest.fixture(scope="session")
-def compose_namespace() -> str:
-    return f"dy-sidecar_{uuid.uuid4()}"
+@pytest.fixture
+def mock_dy_volumes(tmp_path: Path) -> Iterator[Path]:
+    return tmp_path / "dy-volumes"
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
+def io_temp_dir(tmp_path: Path) -> Path:
+    return tmp_path / "io"
+
+
+@pytest.fixture
+def compose_namespace(faker: Faker) -> str:
+    return f"dy-sidecar_{faker.uuid4()}"
+
+
+@pytest.fixture
 def inputs_dir(io_temp_dir: Path) -> Path:
     return io_temp_dir / "inputs"
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def outputs_dir(io_temp_dir: Path) -> Path:
     return io_temp_dir / "outputs"
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def state_paths_dirs(io_temp_dir: Path) -> List[Path]:
-    return [io_temp_dir / f"dir_{x}" for x in range(4)]
+    return [io_temp_dir / f"dir_{i}" for i in range(4)]
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def state_exclude_dirs(io_temp_dir: Path) -> List[Path]:
-    return [io_temp_dir / f"dir_exclude_{x}" for x in range(4)]
+    return [io_temp_dir / f"dir_exclude_{i}" for i in range(4)]
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def mock_environment(
-    monkeypatch_module: MonkeyPatch,
+    monkeypatch: MonkeyPatch,
     mock_dy_volumes: Path,
     compose_namespace: str,
     inputs_dir: Path,
     outputs_dir: Path,
     state_paths_dirs: List[Path],
     state_exclude_dirs: List[Path],
+    faker: Faker,
 ) -> None:
-    monkeypatch_module.setenv("SC_BOOT_MODE", "production")
-    monkeypatch_module.setenv("DYNAMIC_SIDECAR_COMPOSE_NAMESPACE", compose_namespace)
-    monkeypatch_module.setenv("REGISTRY_AUTH", "false")
-    monkeypatch_module.setenv("REGISTRY_USER", "test")
-    monkeypatch_module.setenv("REGISTRY_PW", "test")
-    monkeypatch_module.setenv("REGISTRY_SSL", "false")
-    monkeypatch_module.setenv("DY_SIDECAR_USER_ID", "1")
-    monkeypatch_module.setenv("DY_SIDECAR_PROJECT_ID", f"{uuid.uuid4()}")
-    monkeypatch_module.setenv("DY_SIDECAR_NODE_ID", f"{uuid.uuid4()}")
-    monkeypatch_module.setenv("DY_SIDECAR_RUN_ID", f"{uuid.uuid4()}")
-    monkeypatch_module.setenv("DY_SIDECAR_PATH_INPUTS", str(inputs_dir))
-    monkeypatch_module.setenv("DY_SIDECAR_PATH_OUTPUTS", str(outputs_dir))
-    monkeypatch_module.setenv(
-        "DY_SIDECAR_STATE_PATHS", json.dumps([str(x) for x in state_paths_dirs])
+    monkeypatch.setenv("SC_BOOT_MODE", "production")
+    monkeypatch.setenv("DYNAMIC_SIDECAR_COMPOSE_NAMESPACE", compose_namespace)
+    monkeypatch.setenv("REGISTRY_AUTH", "false")
+    monkeypatch.setenv("REGISTRY_USER", "test")
+    monkeypatch.setenv("REGISTRY_PW", "test")
+    monkeypatch.setenv("REGISTRY_SSL", "false")
+    monkeypatch.setenv("DY_SIDECAR_USER_ID", "1")
+    monkeypatch.setenv("DY_SIDECAR_PROJECT_ID", f"{faker.uuid4()}")
+    monkeypatch.setenv("DY_SIDECAR_NODE_ID", f"{faker.uuid4()}")
+    monkeypatch.setenv("DY_SIDECAR_RUN_ID", f"{faker.uuid4()}")
+    monkeypatch.setenv("DY_SIDECAR_PATH_INPUTS", f"{inputs_dir}")
+    monkeypatch.setenv("DY_SIDECAR_PATH_OUTPUTS", f"{outputs_dir}")
+    monkeypatch.setenv(
+        "DY_SIDECAR_STATE_PATHS", json.dumps([f"{x}" for x in state_paths_dirs])
     )
-    monkeypatch_module.setenv(
-        "DY_SIDECAR_STATE_EXCLUDE", json.dumps([str(x) for x in state_exclude_dirs])
+    monkeypatch.setenv(
+        "DY_SIDECAR_STATE_EXCLUDE", json.dumps([f"{x}" for x in state_exclude_dirs])
     )
-    monkeypatch_module.setenv("RABBIT_SETTINGS", "null")
+    monkeypatch.setenv("RABBIT_SETTINGS", "null")
 
-    monkeypatch_module.setenv("S3_ENDPOINT", "endpoint")
-    monkeypatch_module.setenv("S3_ACCESS_KEY", "access_key")
-    monkeypatch_module.setenv("S3_SECRET_KEY", "secret_key")
-    monkeypatch_module.setenv("S3_BUCKET_NAME", "bucket_name")
-    monkeypatch_module.setenv("S3_SECURE", "false")
-    monkeypatch_module.setenv("R_CLONE_PROVIDER", "MINIO")
+    monkeypatch.setenv("S3_ENDPOINT", "endpoint")
+    monkeypatch.setenv("S3_ACCESS_KEY", "access_key")
+    monkeypatch.setenv("S3_SECRET_KEY", "secret_key")
+    monkeypatch.setenv("S3_BUCKET_NAME", "bucket_name")
+    monkeypatch.setenv("S3_SECURE", "false")
+    monkeypatch.setenv("R_CLONE_PROVIDER", "MINIO")
 
-    monkeypatch_module.setattr(mounted_fs, "DY_VOLUMES", mock_dy_volumes)
-
-
-@pytest.fixture(scope="module")
-def disable_registry_check(monkeypatch_module: MockerFixture) -> None:
-    async def _mock_is_registry_reachable(*args, **kwargs) -> None:
-        pass
-
-    monkeypatch_module.setattr(
-        utils, "_is_registry_reachable", _mock_is_registry_reachable
-    )
+    monkeypatch.setattr(mounted_fs, "DY_VOLUMES", mock_dy_volumes)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
+def disable_registry_check(monkeypatch: MonkeyPatch) -> None:
+    mock = Mock(return_value=None)
+    monkeypatch.setattr(utils, "_is_registry_reachable", mock)
+
+
+@pytest.fixture
 def app(mock_environment: None, disable_registry_check: None) -> FastAPI:
     app = assemble_application()
     app.state.rabbitmq = AsyncMock()
@@ -175,10 +181,11 @@ async def test_client(app: FastAPI) -> AsyncIterable[TestClient]:
         yield client
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=True)  # <----- TODO: PC->ANE!!
 async def cleanup_containers(
     app: FastAPI, ensure_external_volumes: None
 ) -> AsyncGenerator[None, None]:
+
     yield
     # run docker compose down here
 
@@ -215,11 +222,6 @@ def mock_containers_get(mocker: MockerFixture) -> int:
     mocker.patch("aiodocker.containers.DockerContainers.get", side_effect=mock_get)
 
     return mock_status_code
-
-
-@pytest.fixture
-def tests_dir() -> Path:
-    return Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
 
 @pytest.fixture
