fd088938c6b31c455a41bb2334a62f0e4410dbb1
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-06-21 17:40:18 +0200

    deprecation warning on monkeypatch scope>function

--- packages/pytest-simcore/src/pytest_simcore/monkeypatch_extra.py
+++ packages/pytest-simcore/src/pytest_simcore/monkeypatch_extra.py
@@ -2,12 +2,18 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
+import warnings
 from typing import Iterator
 
 import pytest
 from _pytest.fixtures import FixtureRequest
 from _pytest.monkeypatch import MonkeyPatch
 
+warnings.warn(
+    f"{__name__} is deprecated, we highly recommend to use pytest.monkeypatch at function-scope level."
+    "Large scopes lead to complex problems during tests",
+    DeprecationWarning,
+)
 # Some extras to overcome https://github.com/pytest-dev/pytest/issues/363
 # SEE https://github.com/pytest-dev/pytest/issues/363#issuecomment-289830794
 
