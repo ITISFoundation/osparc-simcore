b9baea770f0e82c89b6fbaf3ba67564bb6bde7e3
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-06-21 15:39:03 +0200

    revert changes

--- packages/pytest-simcore/src/pytest_simcore/docker_swarm.py
+++ packages/pytest-simcore/src/pytest_simcore/docker_swarm.py
@@ -154,8 +154,12 @@ def keep_docker_up(request) -> bool:
     return request.config.getoption("--keep-docker-up")
 
 
-def _docker_swarm_initialized(docker_client: docker.client.DockerClient) -> Generator:
+@pytest.fixture(scope="module")
+def docker_swarm(
+    docker_client: docker.client.DockerClient, keep_docker_up: Iterator[bool]
+) -> Iterator[None]:
     """inits docker swarm"""
+
     for attempt in Retrying(
         wait=wait_fixed(2), stop=stop_after_delay(15), reraise=True
     ):
@@ -166,44 +170,15 @@ def _docker_swarm_initialized(docker_client: docker.client.DockerClient) -> Gene
                 print("--> docker swarm initialized.")
             # if still not in swarm, raise an error to try and initialize again
             _in_docker_swarm(docker_client, raise_error=True)
+
     assert _in_docker_swarm(docker_client) is True
-    yield
 
-    # cleanup
-    print("<-- leaving docker swarm...")
-    assert docker_client.swarm.leave(force=True)
-    print("<-- docker swarm left.")
-    assert _in_docker_swarm(docker_client) is False
-
-
-@pytest.fixture(scope="module")
-def docker_swarm(
-    docker_client: docker.client.DockerClient, keep_docker_up: bool
-) -> Iterator[None]:
-    g = _docker_swarm_initialized(docker_client)
-    next(g, None)
     yield
 
     if not keep_docker_up:
-        # cleanup
-        next(g, None)
-
-    assert _in_docker_swarm(docker_client) is keep_docker_up
-
-
-@pytest.fixture(scope="session")
-def session_docker_swarm(
-    docker_client: docker.client.DockerClient, keep_docker_up: bool
-) -> Iterator[None]:
-    g = _docker_swarm_initialized(docker_client)
-    next(g, None)
-    yield
-
-    if not keep_docker_up:
-        # cleanup
-        next(g, None)
-
-    assert _in_docker_swarm(docker_client) is keep_docker_up
+        print("<-- leaving docker swarm...")
+        assert docker_client.swarm.leave(force=True)
+        print("<-- docker swarm left.")
 
 
 @pytest.fixture(scope="module")
