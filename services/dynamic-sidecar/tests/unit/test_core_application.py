b766aee55743b7ba455af86e1a556df5e672c06c
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-06-23 04:19:50 +0200

    fixes test-core-app

--- services/dynamic-sidecar/tests/unit/test_core_application.py
+++ services/dynamic-sidecar/tests/unit/test_core_application.py
@@ -1,14 +1,20 @@
 # pylint: disable=redefined-outer-name
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
-
+import inspect
 
 from pytest_simcore.helpers.typing_env import EnvVarsDict
-from simcore_service_dynamic_sidecar.core.application import create_app
+from simcore_service_dynamic_sidecar.core.application import AppState, create_app
 from simcore_service_dynamic_sidecar.core.settings import DynamicSidecarSettings
 
 
-def test_init_appication(mock_environment_with_envdevel: EnvVarsDict):
-
+def test_create_app(mock_environment_with_envdevel: EnvVarsDict):
     app = create_app()
     assert isinstance(app.state.settings, DynamicSidecarSettings)
+
+    # ensure exposed properties are init after creation
+    app_state = AppState(app)
+    properties = inspect.getmembers(AppState, lambda o: isinstance(o, property))
+
+    for prop_name, _ in properties:
+        assert getattr(app_state, prop_name) == getattr(app.state, prop_name)
