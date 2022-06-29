653b9548cdbf64f1cc514a4e89e55edc00374d7d
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-06-29 13:59:55 +0200

    added doc

--- packages/service-library/src/servicelib/fastapi/requests_decorators.py
+++ packages/service-library/src/servicelib/fastapi/requests_decorators.py
@@ -11,6 +11,13 @@ logger = logging.getLogger(__name__)
 
 _DEFAULT_CHECK_INTERVAL_S: float = 0.5
 
+HTTP_499_CLIENT_CLOSED_REQUEST = 499
+# A non-standard status code introduced by nginx for the case when a client
+# closes the connection while nginx is processing the request.
+# SEE https://www.webfx.com/web-development/glossary/http-status-codes/what-is-a-499-status-code/
+
+_FastAPIHandlerCallable = Callable[..., Coroutine[Any, Any, Optional[Any]]]
+
 
 async def _cancel_task_if_client_disconnected(
     request: Request, task: asyncio.Task, interval: float = _DEFAULT_CHECK_INTERVAL_S
@@ -32,38 +39,49 @@ async def _cancel_task_if_client_disconnected(
         logger.debug("task completed")
 
 
-def cancellable_request(handler: Callable[..., Coroutine[Any, Any, Optional[Any]]]):
-    """this decorator periodically checks if the client disconnected and then will cancel the request and return a 499 code (a la nginx).
-    Usage:
+def cancellable_request(handler_fun: _FastAPIHandlerCallable):
+    """This decorator periodically checks if the client disconnected and
+    then will cancel the request and return a HTTP_499_CLIENT_CLOSED_REQUEST code (a la nginx).
 
-    decorate the cancellable route and add request: Request as an argument
+    Usage: decorate the cancellable route and add request: Request as an argument
 
-    @cancellable_request
-    async def route(
-        _request: Request,
-        ...
-    )
+        @cancellable_request
+        async def route(
+            _request: Request,
+            ...
+        )
     """
 
-    @wraps(handler)
+    @wraps(handler_fun)
     async def wrapper(*args, **kwargs) -> Optional[Any]:
         request: Request = kwargs["_request"]
+
+        # Intercepts handler call and creates a task out of it
         handler_task = asyncio.create_task(
-            handler(*args, **kwargs),
-            name=f"cancellable_request/handler/{handler.__name__}",
+            handler_fun(*args, **kwargs),
+            name=f"cancellable_request/handler/{handler_fun.__name__}",
         )
+        # An extra task to monitor when the client disconnects so it can
+        # cancel 'handler_task'
         auto_cancel_task = asyncio.create_task(
             _cancel_task_if_client_disconnected(request, handler_task),
-            name=f"cancellable_request/auto_cancel/{handler.__name__}",
+            name=f"cancellable_request/auto_cancel/{handler_fun.__name__}",
         )
+
         try:
             return await handler_task
         except CancelledError:
             logger.warning(
-                "request %s was cancelled by client %s!", request.url, request.client
+                "Request %s was cancelled since client %s disconnected !",
+                f"{request.url}",
+                request.client,
             )
-            return Response("Oh No!", status_code=499)
+            return Response(
+                "Request cancelled because client disconnected",
+                status_code=HTTP_499_CLIENT_CLOSED_REQUEST,
+            )
         finally:
+            # TODO: is this called when return ??? Should it stop when responded?
             auto_cancel_task.cancel()
             with suppress(CancelledError):
                 await auto_cancel_task
