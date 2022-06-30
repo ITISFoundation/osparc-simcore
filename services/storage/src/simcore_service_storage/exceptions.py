84142e9f552f07879ba5ad8fc24ae674c6f26f24
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-06-30 11:38:35 +0200

    @GitHK review: unify exceptions

--- services/storage/src/simcore_service_storage/exceptions.py
+++ services/storage/src/simcore_service_storage/exceptions.py
@@ -1,41 +1,50 @@
 from pydantic.errors import PydanticErrorMixin
 
 
-class FileMetaDataNotFoundError(PydanticErrorMixin, RuntimeError):
+class StorageRuntimeError(PydanticErrorMixin, RuntimeError):
+    ...
+
+
+class DatabaseAccessError(StorageRuntimeError):
+    code = "database.access_error"
+    msg_template: str = "Unexpected error while accessing database backend"
+
+
+class FileMetaDataNotFoundError(DatabaseAccessError):
     code = "filemetadata.not_found_error"
     msg_template: str = "The file meta data for {file_id} was not found"
 
 
-class FileAccessRightError(PydanticErrorMixin, RuntimeError):
+class FileAccessRightError(DatabaseAccessError):
     code = "file.access_right_error"
     msg_template: str = "Insufficient access rights to {access_right} {file_id}"
 
 
-class ProjectAccessRightError(PydanticErrorMixin, RuntimeError):
+class ProjectAccessRightError(DatabaseAccessError):
     code = "file.access_right_error"
     msg_template: str = "Insufficient access rights to {access_right} {project_id}"
 
 
-class ProjectNotFoundError(PydanticErrorMixin, RuntimeError):
+class ProjectNotFoundError(DatabaseAccessError):
     code = "project.not_found_error"
     msg_template: str = "Project {project_id} was not found"
 
 
-class LinkAlreadyExistsError(PydanticErrorMixin, RuntimeError):
+class LinkAlreadyExistsError(DatabaseAccessError):
     code = "link.already_exists_error"
     msg_template: str = "The link {file_id} already exists"
 
 
-class S3AccessError(PydanticErrorMixin, RuntimeError):
+class S3AccessError(StorageRuntimeError):
     code = "s3_access.error"
     msg_template: str = "Unexpected error while accessing S3 backend"
 
 
-class S3BucketInvalidError(PydanticErrorMixin, RuntimeError):
+class S3BucketInvalidError(S3AccessError):
     code = "s3_bucket.invalid_error"
     msg_template: str = "The {bucket} is invalid"
 
 
-class S3KeyNotFoundError(PydanticErrorMixin, RuntimeError):
+class S3KeyNotFoundError(S3AccessError):
     code = "s3_key.not_found_error"
     msg_template: str = "The file {key}  in {bucket} was not found"
