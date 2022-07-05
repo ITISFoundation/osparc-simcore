b0fa8d1b693e0e5ac03fc51b7959e22b5906d55e
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-07-06 01:10:56 +0200

    doc:

--- services/dynamic-sidecar/tests/conftest.py
+++ services/dynamic-sidecar/tests/conftest.py
@@ -25,7 +25,7 @@ pytest_plugins = [
     "pytest_simcore.docker_compose",
     "pytest_simcore.docker_registry",
     "pytest_simcore.docker_swarm",
-    "pytest_simcore.monkeypatch_extra",
+    "pytest_simcore.monkeypatch_extra",  # TODO: remove this dependency
     "pytest_simcore.pytest_global_environs",
     "pytest_simcore.rabbit_service",
     "pytest_simcore.repository_paths",
