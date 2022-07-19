524aafef110120f6bbf9622263a928ab563d67ce
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-07-19 20:07:02 +0200

    reproduces issue in legacy jupyters

--- services/storage/tests/unit/test__legacy_storage_sdk_compatibility.py
+++ services/storage/tests/unit/test__legacy_storage_sdk_compatibility.py
@@ -7,8 +7,12 @@ Keeps compatibility with old services/storage/client-sdk/python client (simcore_
 
 """
 
+from pathlib import Path
+
+import aiohttp
 import pytest
 from aiohttp.test_utils import TestClient
+from faker import Faker
 from models_library.projects_nodes_io import LocationID, SimcoreS3FileID
 from models_library.users import UserID
 from simcore_service_storage.simcore_s3_dsm import SimcoreS3DataManager
@@ -46,12 +50,12 @@ def file_id(simcore_file_id: SimcoreS3FileID) -> str:
 
 
 @pytest.fixture
-def store_id() -> LocationID:
+def location_id() -> LocationID:
     return SimcoreS3DataManager.get_location_id()
 
 
 @pytest.fixture
-def store_name() -> str:
+def location_name() -> str:
     return SimcoreS3DataManager.get_location_name()
 
 
@@ -59,8 +63,10 @@ async def test_storage_client_used_in_simcore_sdk_0_3_2(
     client: TestClient,
     file_id: str,
     user_id: str,
-    store_id: int,
-    store_name: str,
+    location_id: int,
+    location_name: str,
+    tmp_path: Path,
+    faker: Faker,
 ):
     """
     This test has calls to 'simcore_service_storage_sdk' used
@@ -77,57 +83,88 @@ async def test_storage_client_used_in_simcore_sdk_0_3_2(
 
     assert client.app
     assert client.server
+    # --------
     cfg = Configuration()
     cfg.host = f"http://{client.host}:{client.port}/v0"
     cfg.debug = True
 
     assert cfg.host == f'{client.make_url("/v0")}'
-
     print(f"{cfg=}")
     print(f"{cfg.to_debug_report()=}")
 
+    # --------
     api_client = ApiClient(cfg)
     print(f"{api_client=}")
     print(f"{api_client.default_headers=}")
 
+    # --------
     try:
         api = UsersApi(api_client)
         print(f"{api=}")
 
+        (
+            response_payload,
+            status_code,
+            response_headers,
+        ) = await api.get_storage_locations_with_http_info(user_id)
+        print(f"{response_payload=}")
+        print(f"{status_code=}")
+        print(f"{response_headers=}")
+
+        assert status_code == 200
+
+        # _get_upload_link
+        # https://github.com/ITISFoundation/osparc-simcore/blob/cfdf4f86d844ebb362f4f39e9c6571d561b72897/packages/simcore-sdk/src/simcore_sdk/node_ports/filemanager.py#L132
+        resp_model = await api.upload_file(
+            location_id=location_id,
+            user_id=user_id,
+            file_id=file_id,
+            _request_timeout=1000,
+        )
+        assert resp_model.error is None
+        assert resp_model.data.link is not None
+
+        # dumps file & upload
+        file = tmp_path / Path(file_id).name
+        file.write_text(faker.text())
+
+        async with aiohttp.ClientSession() as session:
+            with file.open("rb") as fh:
+                async with session.put(resp_model.data.link, data=fh) as resp_model:
+                    print(resp_model.status)
+                    print(await resp_model.text())
+
         # entry_exists
         # https://github.com/ITISFoundation/osparc-simcore/blob/cfdf4f86d844ebb362f4f39e9c6571d561b72897/packages/simcore-sdk/src/simcore_sdk/node_ports/filemanager.py#L322
-        # A bug in the response of this call was preventing downloading data with the new storage API
-        resp = await api.get_file_metadata(file_id, store_id, user_id)
-        print(f"{resp=}")
-        assert resp.data.object_name is not None
+        #
+        # NOTE: jupyter-smash 2.5.9 was failing to download because
+        #   this call was not returning any 'object_name'
+        #   A bug in the response of this call was preventing downloading data
+        #   with the new storage API
+        #
+        resp_model = await api.get_file_metadata(file_id, location_id, user_id)
+        print(type(resp_model), ":\n", resp_model)
+        assert resp_model.data.object_name is not None
+        assert resp_model.error is None
 
         # _get_location_id_from_location_name
         # https://github.com/ITISFoundation/osparc-simcore/blob/cfdf4f86d844ebb362f4f39e9c6571d561b72897/packages/simcore-sdk/src/simcore_sdk/node_ports/filemanager.py#L89
-        resp = await api.get_storage_locations(user_id=user_id)
-        print(f"{resp=}")
-        for location in resp.data:
-            assert location["name"] == store_name
+        resp_model = await api.get_storage_locations(user_id=user_id)
+        print(f"{resp_model=}")
+        for location in resp_model.data:
+            assert location["name"] == location_name
             assert location["id"]
 
         # _get_download_link
         # https://github.com/ITISFoundation/osparc-simcore/blob/cfdf4f86d844ebb362f4f39e9c6571d561b72897/packages/simcore-sdk/src/simcore_sdk/node_ports/filemanager.py#L123
-        download_link = await api.download_file(
-            location_id=store_id,
+        resp_model = await api.download_file(
+            location_id=location_id,
             user_id=user_id,
             file_id=file_id,
             _request_timeout=1000,
         )
-        assert download_link
-
-        # _get_upload_link
-        # https://github.com/ITISFoundation/osparc-simcore/blob/cfdf4f86d844ebb362f4f39e9c6571d561b72897/packages/simcore-sdk/src/simcore_sdk/node_ports/filemanager.py#L132
-        upload_link = await api.upload_file(
-            location_id=store_id,
-            user_id=user_id,
-            file_id=file_id,
-            _request_timeout=1000,
-        )
-        assert upload_link
+        print(f"{resp_model=}")
+        assert resp_model.error is None
 
     finally:
         del api_client
