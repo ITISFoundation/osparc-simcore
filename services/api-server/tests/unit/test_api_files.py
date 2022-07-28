334e57da875667e3a78848bc5f8a8234dfd44af0
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-07-28 11:05:43 +0200

    test fully works and tests different sizes

--- services/api-server/tests/unit/test_api_files.py
+++ services/api-server/tests/unit/test_api_files.py
@@ -11,9 +11,11 @@ import respx
 from faker import Faker
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from pydantic import ByteSize
+from pydantic import ByteSize, parse_obj_as
 from pytest_mock import MockerFixture
+from pytest_simcore.helpers.utils_parametrizations import byte_size_ids
 from requests.auth import HTTPBasicAuth
+from simcore_sdk.node_ports_common.filemanager import UploadableFileObject
 from simcore_service_api_server._meta import API_VTAG
 from simcore_service_api_server.core.settings import ApplicationSettings
 from simcore_service_api_server.models.schemas.files import File
@@ -60,21 +62,31 @@ async def test_list_files(
     assert resp.status_code == status.HTTP_200_OK, resp.text
 
 
+@pytest.mark.parametrize(
+    "file_size",
+    [
+        (parse_obj_as(ByteSize, "1Mib")),
+        (parse_obj_as(ByteSize, "500Mib")),
+        pytest.param(parse_obj_as(ByteSize, "7Gib"), marks=pytest.mark.heavy_load),
+    ],
+    ids=byte_size_ids,
+)
 async def test_upload_file(
     mocker: MockerFixture,
     app: FastAPI,
     auth: HTTPBasicAuth,
     create_file_of_size: Callable[[ByteSize], Path],
+    file_size: ByteSize,
     faker: Faker,
 ):
     fake_checksum = faker.md5()
-    mocker.patch(
+    mocked_upload_file = mocker.patch(
         "simcore_service_api_server.api.routes.files.storage_upload_file",
         autospec=True,
         return_value=(faker.pyint(min_value=0), fake_checksum),
     )
 
-    path = create_file_of_size(ByteSize(500))
+    path = create_file_of_size(file_size)
 
     # NOTE: I was unable to make this work with the async client,
     # if someones knows better feel free to fix it
@@ -89,3 +101,10 @@ async def test_upload_file(
     received_file = File.parse_obj(resp.json())
     assert received_file.checksum == fake_checksum
     assert received_file.filename == path.name
+    mocked_upload_file.assert_called_once()
+    assert mocked_upload_file.call_args.kwargs
+    assert "file_to_upload" in mocked_upload_file.call_args.kwargs
+    uploadable_file_object = mocked_upload_file.call_args.kwargs["file_to_upload"]
+    assert isinstance(uploadable_file_object, UploadableFileObject)
+    assert uploadable_file_object.file_name == path.name
+    assert uploadable_file_object.file_size == path.stat().st_size
