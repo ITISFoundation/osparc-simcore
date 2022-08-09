be83c50988c55c666341dc356ff6abee778a6213
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-08-09 18:46:49 +0200

    precommit removes imports

--- packages/service-library/src/servicelib/fastapi/long_running_tasks/client.py
+++ packages/service-library/src/servicelib/fastapi/long_running_tasks/client.py
@@ -2,6 +2,16 @@
 Provides a convenient way to return the result given a TaskId.
 """
 
+from ...long_running_tasks._errors import TaskClientResultError
+from ...long_running_tasks._models import (
+    CancelResult,
+    ProgressCallback,
+    ProgressMessage,
+    ProgressPercent,
+)
+from ...long_running_tasks._task import TaskId, TaskResult
+from ._client import Client, setup
+from ._context_manager import periodic_task_result
 
 __all__: tuple[str, ...] = (
     "CancelResult",
