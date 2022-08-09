7de02fa20ff9fea0cdd112493a54583f49ef19ff
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-08-09 14:12:23 +0200

    added imports

--- packages/service-library/src/servicelib/aiohttp/long_running_tasks/server.py
+++ packages/service-library/src/servicelib/aiohttp/long_running_tasks/server.py
@@ -1,9 +1,9 @@
 """
 Extension helping to deal with the long running tasks.
 Sets up all the infrastructure required to define long running tasks
-in a FastAPI application.
+in a AIOHTTP application.
 The server only has to return a `TaskId` in the handler creating the long
-running task. The client will take care of recovering the result from it.
+running task.
 """
 
 from ...long_running_tasks._task import (
--- packages/service-library/src/servicelib/fastapi/long_running_tasks/server.py
+++ packages/service-library/src/servicelib/fastapi/long_running_tasks/server.py
@@ -6,14 +6,29 @@ The server only has to return a `TaskId` in the handler creating the long
 running task. The client will take care of recovering the result from it.
 """
 
+from ...long_running_tasks._task import (
+    TaskAlreadyRunningError,
+    TaskCancelledError,
+    TaskId,
+    TaskManager,
+    TaskProgress,
+    TaskResult,
+    TaskStatus,
+    start_task,
+)
+from ._dependencies import get_task_manager
+from ._server import setup
+
 
 __all__: tuple[str, ...] = (
     "get_task_manager",
     "setup",
     "start_task",
     "TaskAlreadyRunningError",
+    "TaskCancelledError",
     "TaskId",
     "TaskManager",
     "TaskProgress",
+    "TaskResult",
     "TaskStatus",
 )
