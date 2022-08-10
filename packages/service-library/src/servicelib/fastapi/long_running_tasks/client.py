b3f917b9e8f5fd6d1e0ed174f63fdbba85a1d7fa
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-08-10 10:35:05 +0200

    refactoring

--- packages/service-library/src/servicelib/fastapi/long_running_tasks/client.py
+++ packages/service-library/src/servicelib/fastapi/long_running_tasks/client.py
@@ -4,7 +4,6 @@ Provides a convenient way to return the result given a TaskId.
 
 from ...long_running_tasks._errors import TaskClientResultError
 from ...long_running_tasks._models import (
-    CancelResult,
     ProgressCallback,
     ProgressMessage,
     ProgressPercent,
@@ -14,7 +13,6 @@ from ._client import Client, setup
 from ._context_manager import periodic_task_result
 
 __all__: tuple[str, ...] = (
-    "CancelResult",
     "Client",
     "periodic_task_result",
     "ProgressCallback",
