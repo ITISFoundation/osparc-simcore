dd7091156be94d78840ae7bc38f303d601b8674f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-08-11 20:29:07 +0200

    WIP

--- services/web/server/src/simcore_service_webserver/login/_2fa.py
+++ services/web/server/src/simcore_service_webserver/login/_2fa.py
@@ -10,13 +10,14 @@ log = logging.getLogger(__name__)
 async def send_sms_code(phone_number, code):
     def sender():
         log.info(
-            "sending sms code %s to %s",
-            code,
-            phone_number,
+            "sending sms code to %s",
+            f"{phone_number=}",
         )
+
         account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
         auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
         messaging_service_sid = os.environ.get("TWILIO_MESSAGING_SID")
+
         client = Client(account_sid, auth_token)
         message = client.messages.create(
             messaging_service_sid=messaging_service_sid,
@@ -25,9 +26,10 @@ async def send_sms_code(phone_number, code):
                 code
             ),
         )
-        log.info(
-            "message id %s",
-            message.sid,
+
+        log.debug(
+            "Got twilio client %s",
+            f"{message=}",
         )
 
     await asyncio.get_event_loop().run_in_executor(None, sender)
