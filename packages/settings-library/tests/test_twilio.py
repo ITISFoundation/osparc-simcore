1620808331582f13d961ec956cf1771579b632aa
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-08-15 15:26:05 +0200

    rms all 2fa options

--- packages/settings-library/tests/test_twilio.py
+++ packages/settings-library/tests/test_twilio.py
@@ -1,10 +1,29 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+
+import pytest
+from pydantic import ValidationError
+from pytest import MonkeyPatch
+from pytest_simcore.helpers.utils_envs import setenvs_from_dict
 from settings_library.twilio import TwilioSettings
 
 
-def test_twilio_settings_within_envdevel(mock_env_devel_environment: dict[str, str]):
-    settings = TwilioSettings.create_from_envs()
+def test_twilio_settings_within_envdevel(
+    mock_env_devel_environment: dict[str, str], monkeypatch: MonkeyPatch
+):
+    # in .env-devel these are for the oment undefined
+    with pytest.raises(ValidationError):
+        TwilioSettings.create_from_envs()
 
-    assert (
-        settings.TWILIO_MESSAGING_SID
-        == mock_env_devel_environment["TWILIO_MESSAGING_SID"]
-    )
+    # adds twilio credentials
+    with monkeypatch.context() as patch:
+        setenvs_from_dict(
+            patch,
+            {
+                "TWILIO_ACCOUNT_SID": "fake-account",
+                "TWILIO_AUTH_TOKEN": "fake-token",
+                "TWILIO_MESSAGING_SID": "x" * 34,
+            },
+        )
+        TwilioSettings.create_from_envs()
