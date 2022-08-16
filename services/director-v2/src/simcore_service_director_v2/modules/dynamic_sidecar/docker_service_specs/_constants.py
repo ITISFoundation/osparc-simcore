d7c4cbda4735736d6edb756106ec9fe24267b431
Andrei Neagu <neagu@itis.swiss>
2022-08-16 15:37:57 +0200

    removed comment

--- services/director-v2/src/simcore_service_director_v2/modules/dynamic_sidecar/docker_service_specs/_constants.py
+++ services/director-v2/src/simcore_service_director_v2/modules/dynamic_sidecar/docker_service_specs/_constants.py
@@ -2,7 +2,7 @@ from typing import Final
 
 _NANO: Final[int] = int(1e9)
 
-# task_template->ContainerSpec ...
+
 DOCKER_CONTAINER_SPEC_RESTART_POLICY_DEFAULTS = {
     "Condition": "on-failure",
     "Delay": 100,
