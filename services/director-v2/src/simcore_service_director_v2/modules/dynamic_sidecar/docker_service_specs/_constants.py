ec037866c4169f9c5d1fc4237e508bfc3af3968c
Andrei Neagu <neagu@itis.swiss>
2022-08-17 11:36:14 +0200

    fix policy

--- services/director-v2/src/simcore_service_director_v2/modules/dynamic_sidecar/docker_service_specs/_constants.py
+++ services/director-v2/src/simcore_service_director_v2/modules/dynamic_sidecar/docker_service_specs/_constants.py
@@ -1,10 +1,9 @@
 from typing import Final
 
-_GIGA: Final[int] = int(1e9)
-_DELAY_NS: Final[int] = 5 * _GIGA
+_SECOND: Final[int] = 1_000_000_000  # expressed in nano-seconds
 
 DOCKER_CONTAINER_SPEC_RESTART_POLICY_DEFAULTS = {
     "Condition": "on-failure",
-    "MaxAttempts": 100,
-    "Delay": _DELAY_NS,
+    "MaxAttempts": 0,
+    "Delay": 5 * _SECOND,
 }
