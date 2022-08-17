7147b7cd83508a9cda1567827d281180f1a12742
Odei Maiz <maiz@itis.swiss>
2022-08-17 12:30:21 +0200

    minor

--- services/web/client/source/class/osparc/navigation/NavigationBar.js
+++ services/web/client/source/class/osparc/navigation/NavigationBar.js
@@ -196,7 +196,6 @@ qx.Class.define("osparc.navigation.NavigationBar", {
         case "tasks-button":
           control = new osparc.component.task.TasksButton();
           this.getChildControl("right-items").add(control);
-          this._add(control);
           break;
         case "manual":
           control = this.__createManualMenuBtn();
