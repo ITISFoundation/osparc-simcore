2971dff47a791f68ab579e37e836d84db272ad1e
Odei Maiz <maiz@itis.swiss>
2022-08-20 16:27:29 +0200

    ti-plan test

---
+++ tests/e2e/tutorials/ti-plan.js
@@ -0,0 +1,47 @@
+// node ti-plan.js [url] [user] [password] [--demo]
+
+const utils = require('../utils/utils');
+const tutorialBase = require('./tutorialBase');
+
+const args = process.argv.slice(2);
+const {
+  url,
+  user,
+  pass,
+  newUser,
+  startTimeout,
+  enableDemoMode
+} = utils.parseCommandLineArguments(args)
+
+const studyName = "New Plan";
+
+async function runTutorial() {
+  const tutorial = new tutorialBase.TutorialBase(url, studyName, user, pass, newUser, enableDemoMode);
+  let studyId
+  try {
+    await tutorial.start();
+
+    // check that the "New Study" is "New Plan"
+    await tutorial.checkFirstStudyId("newPlanBtn");
+  }
+  catch (err) {
+    tutorial.setTutorialFailed(true);
+    console.log('Tutorial error: ' + err);
+  }
+  finally {
+    // await tutorial.toDashboard()
+    // await tutorial.removeStudy(studyId, 20000);
+    await tutorial.logOut();
+    await tutorial.close();
+  }
+
+  if (tutorial.getTutorialFailed()) {
+    throw "Tutorial Failed";
+  }
+}
+
+runTutorial()
+  .catch(error => {
+    console.log('Puppeteer error: ' + error);
+    process.exit(1);
+  });
