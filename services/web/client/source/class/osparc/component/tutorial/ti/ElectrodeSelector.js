a9b6ad6932356c3005e27e2d830f32be3d00553a
Odei Maiz <maiz@itis.swiss>
2022-08-25 16:12:27 +0200

    ElectrodeSelector

--- services/web/client/source/class/osparc/component/tutorial/ti/ElectrodeSelector.js
+++ services/web/client/source/class/osparc/component/tutorial/ti/ElectrodeSelector.js
@@ -26,7 +26,47 @@ qx.Class.define("osparc.component.tutorial.ti.ElectrodeSelector", {
   members: {
     _populateCard: function() {
       this._add(new qx.ui.basic.Label().set({
-        value: "asdf",
+        value: this.tr("\
+        After pressing New Plan, three panels will be shown\
+        "),
+        rich: true,
+        wrap: true,
+        font: "text-14"
+      }));
+
+      this._add(new qx.ui.basic.Label().set({
+        value: this.tr("\
+        In a first step, the relevant species, stimulation target, and potential electrode locations \
+        (currently required to narrow down the huge exposure configuration search space) are selected.\
+        "),
+        rich: true,
+        wrap: true,
+        font: "text-14"
+      }));
+
+      const image = new qx.ui.basic.Image("osparc/tutorial/ti/ElectrodeSelector.gif").set({
+        alignX: "center",
+        scale: true,
+        width: 737,
+        height: 540
+      });
+      this._add(image);
+
+      this._add(new qx.ui.basic.Label().set({
+        value: this.tr("\
+        After finishing the set up, click on the big button on the top right to submit the configuration.\
+        "),
+        rich: true,
+        wrap: true,
+        font: "text-14"
+      }));
+
+      this._add(new qx.ui.basic.Label().set({
+        value: this.tr("\
+        Now the Arrow that says 'Next' can be pushed and the optimization will start.\
+        "),
+        rich: true,
+        wrap: true,
         font: "text-14"
       }));
     }
