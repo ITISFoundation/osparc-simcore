547a5f33fb7575dd4d4f6cabcb20d6c34157a8e5
Andrei Neagu <neagu@itis.swiss>
2022-08-30 10:12:23 +0200

    moved in a more appropriate place

--- services/director-v2/src/simcore_service_director_v2/core/settings.py
+++ services/director-v2/src/simcore_service_director_v2/core/settings.py
@@ -301,6 +301,12 @@ class DynamicSidecarSettings(BaseCustomSettings):
         description="exposes the proxy on localhost for debuging and testing",
     )
 
+    # NOTE regarding locking resources
+    # A node can end up with all the services from a single study.
+    # When the study is closed/opened all the services will try to
+    # upload/download their data. This causes a lot of disk
+    # and network stress (especially for low power nodes like in AWS).
+    # Some nodes collapse under load or behave unexpectedly.
     DYNAMIC_SIDECAR_DOCKER_NODE_RESOURCE_LIMITS_ENABLED: bool = Field(
         False,
         description=(
