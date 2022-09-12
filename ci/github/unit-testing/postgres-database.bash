198c1904d9e9804f7812bc948097199f7fad0785
Sylvain <35365065+sanderegg@users.noreply.github.com>
2022-09-12 17:14:36 +0200

    ✨Do not auto-cancel study copy if the user disconnects (#3345)

--- ci/github/unit-testing/postgres-database.bash
+++ ci/github/unit-testing/postgres-database.bash
@@ -6,27 +6,24 @@ set -o pipefail  # don't hide errors within pipes
 IFS=$'\n\t'
 
 install() {
-    bash ci/helpers/ensure_python_pip.bash
-    pushd packages/postgres-database; pip3 install -r requirements/ci.txt; popd;
-    pip list -v
+  bash ci/helpers/ensure_python_pip.bash
+  make devenv
+  # shellcheck source=/dev/null
+  source .venv/bin/activate
+  pushd packages/postgres-database
+  make install-ci
+  popd
+  .venv/bin/pip list --verbose
 }
 
 test() {
-    pytest \
-      --asyncio-mode=auto \
-      --color=yes \
-      --cov-append \
-      --cov-config=.coveragerc \
-      --cov-report=term-missing \
-      --cov-report=xml \
-      --cov=simcore_postgres_database \
-      --log-date-format="%Y-%m-%d %H:%M:%S" \
-      --log-format="%(asctime)s %(levelname)s %(message)s" \
-      --durations=10 \
-      --verbose \
-      -m "not heavy_load" \
-      packages/postgres-database/tests
+  # shellcheck source=/dev/null
+  source .venv/bin/activate
+  pushd packages/postgres-database
+  make tests-ci
+  popd
 }
+
 
 # Check if the function exists (bash specific)
 if declare -f "$1" > /dev/null
