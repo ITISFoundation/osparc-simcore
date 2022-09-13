c083e8ca7189e0bbea00f04d6a40a78a861eba20
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-09-13 23:58:42 +0200

    use 2.0.4 version of the dy-static-file-server

--- services/director-v2/tests/integration/02/utils.py
+++ services/director-v2/tests/integration/02/utils.py
@@ -344,11 +344,10 @@ async def assert_all_services_running(
                 )
             )
 
-            # check that no service has failed
-            for service_state in service_states:
-                assert service_state != "failed"
+            all_services = await docker_client.services.list()
 
             assert all(x == "running" for x in service_states)
+            print("--> all services are up and running!")
 
 
 async def assert_retrieve_service(
