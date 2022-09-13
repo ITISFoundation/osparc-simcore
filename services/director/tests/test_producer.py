dc09dc720579cd3d8a7ff2837234c5f4940d2da0
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-09-13 08:50:02 +0200

    use object target

--- services/director/tests/test_producer.py
+++ services/director/tests/test_producer.py
@@ -20,8 +20,8 @@ from tenacity.wait import wait_fixed
 
 @pytest.fixture
 def ensure_service_runs_in_ci(monkeypatch):
-    monkeypatch.setattr("config.DEFAULT_MAX_MEMORY", 250 * pow(1024, 2))
-    monkeypatch.setattr("config.DEFAULT_MAX_NANO_CPUS", 0.5 * pow(10, 9))
+    monkeypatch.setattr(config, "DEFAULT_MAX_MEMORY", 250 * pow(1024, 2))
+    monkeypatch.setattr(config, "DEFAULT_MAX_NANO_CPUS", 0.5 * pow(10, 9))
 
 
 @pytest.fixture
