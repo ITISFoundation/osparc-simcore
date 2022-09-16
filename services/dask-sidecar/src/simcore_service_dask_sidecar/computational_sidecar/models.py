029a403f870987fd16af73afe03477377d3c9160
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-09-16 11:31:12 +0200

    add versions with changelog

--- services/dask-sidecar/src/simcore_service_dask_sidecar/computational_sidecar/models.py
+++ services/dask-sidecar/src/simcore_service_dask_sidecar/computational_sidecar/models.py
@@ -1,13 +1,27 @@
-from typing import Dict, List
-
 from packaging import version
 from pydantic import BaseModel, ByteSize, Field
 
 LEGACY_INTEGRATION_VERSION = version.Version("0")
 
+# CHANGELOG:
+# INPUT_FOLDER/input.json --> INPUT_FOLDER/inputs.json
+# OUTPUT_FOLDER/output.json --> OUTPUT_FOLDER/outputs.json
+# logs folder removed since docker already provides a logging system
+VERSION_1_0_0 = version.Version("1.0.0")
+
+# CHANGELOG:
+# removes flat filesystem
+# /inputs/inputs.json --> contains non file data (strings, numbers, ...)
+# /inputs/input_1/... -> contains file data for input_1
+# /inputs/input_2/... -> contains file data for input_2
+# /outputs/outputs.json --> contains non file data
+# /outputs/output_1 --> contains non file data for output_1
+# /outputs/output_2 --> contains non file data for output_2
+VERSION_1_1_0 = version.Version("1.1.0")
+
 
 class ContainerHostConfig(BaseModel):
-    binds: List[str] = Field(
+    binds: list[str] = Field(
         ..., alias="Binds", description="A list of volume bindings for this container"
     )
     init: bool = Field(
@@ -22,8 +36,8 @@ class ContainerHostConfig(BaseModel):
 
 
 class DockerContainerConfig(BaseModel):
-    env: List[str] = Field(..., alias="Env")
-    cmd: List[str] = Field(..., alias="Cmd")
+    env: list[str] = Field(..., alias="Env")
+    cmd: list[str] = Field(..., alias="Cmd")
     image: str = Field(..., alias="Image")
-    labels: Dict[str, str] = Field(..., alias="Labels")
+    labels: dict[str, str] = Field(..., alias="Labels")
     host_config: ContainerHostConfig = Field(..., alias="HostConfig")
