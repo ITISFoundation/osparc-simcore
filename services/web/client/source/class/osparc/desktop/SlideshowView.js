40e04c5ee85b8df0b6bfee8e08c4d6cc3bab1fae
Odei Maiz <maiz@itis.swiss>
2022-09-16 14:18:26 +0200

    minor

--- services/web/client/source/class/osparc/desktop/SlideshowView.js
+++ services/web/client/source/class/osparc/desktop/SlideshowView.js
@@ -174,7 +174,7 @@ qx.Class.define("osparc.desktop.SlideshowView", {
     __getNotReadyDependencies: function(node) {
       const dependencies = node.getStatus().getDependencies() || [];
       const wb = this.getStudy().getWorkbench();
-      const upstreamNodeIds = wb.getUpstreamNodes(node, true);
+      const upstreamNodeIds = wb.getUpstreamCompNodes(node, true);
       upstreamNodeIds.forEach(upstreamNodeId => {
         if (!this.__isNodeReady(upstreamNodeId)) {
           dependencies.push(upstreamNodeId);
