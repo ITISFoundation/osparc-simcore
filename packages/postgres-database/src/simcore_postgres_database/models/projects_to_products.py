1723cd2f2563fac34b1701d0e54ca8db99af1b0d
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-09-20 08:55:30 +0200

    add table projects to products

---
+++ packages/postgres-database/src/simcore_postgres_database/models/projects_to_products.py
@@ -0,0 +1,47 @@
+import sqlalchemy as sa
+
+from .base import metadata
+
+cluster_to_groups = sa.Table(
+    "projects_to_products",
+    metadata,
+    sa.Column(
+        "project_uuid",
+        sa.String,
+        sa.ForeignKey(
+            "projects.uuid",
+            name="fk_projects_to_products_project_uuid_projects",
+            onupdate="CASCADE",
+            ondelete="CASCADE",
+        ),
+        doc="Project unique ID",
+    ),
+    sa.Column(
+        "product_name",
+        sa.String,
+        sa.ForeignKey(
+            "products.name",
+            name="fk_projects_to_products_name_products",
+            onupdate="CASCADE",
+            ondelete="CASCADE",
+        ),
+        doc="Products unique name",
+    ),
+    # -----
+    sa.Column(
+        "created",
+        sa.DateTime(),
+        nullable=False,
+        server_default=sa.sql.func.now(),
+        doc="Timestamp auto-generated upon creation",
+    ),
+    sa.Column(
+        "modified",
+        sa.DateTime(),
+        nullable=False,
+        server_default=sa.sql.func.now(),
+        onupdate=sa.sql.func.now(),
+        doc="Timestamp with last row update",
+    ),
+    sa.UniqueConstraint("project_uuid", "product_name"),
+)
