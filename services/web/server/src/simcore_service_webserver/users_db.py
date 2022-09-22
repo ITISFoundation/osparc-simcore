29e2e4872003f817c14ae108863ae82074996b24
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-09-22 19:19:38 +0200

    tuning update_expired users

--- services/web/server/src/simcore_service_webserver/users_db.py
+++ services/web/server/src/simcore_service_webserver/users_db.py
@@ -1,16 +1,15 @@
 import logging
-from datetime import datetime
 
 from aiopg.sa.engine import Engine
 from aiopg.sa.result import ResultProxy
 from models_library.basic_types import IdInt
 from simcore_postgres_database.models.users import UserStatus, users
+from sqlalchemy.sql import func
 
 logger = logging.getLogger(__name__)
 
 
 async def update_expired_users(engine: Engine) -> list[IdInt]:
-    now = datetime.utcnow()
     async with engine.acquire() as conn:
         result: ResultProxy = await conn.execute(
             users.update()
@@ -18,7 +17,7 @@ async def update_expired_users(engine: Engine) -> list[IdInt]:
             .where(
                 (users.c.expires_at != None)
                 & (users.c.status == UserStatus.ACTIVE)
-                & (users.c.expires_at < now)
+                & (users.c.expires_at < func.now())
             )
             .returning(users.c.id)
         )
