1d07f88089c576680cbd312434308a5819e8b74d
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-09-26 18:40:33 +0200

    fixes swarm

--- services/autoscaling/tests/unit/test_utils_docker.py
+++ services/autoscaling/tests/unit/test_utils_docker.py
@@ -2,15 +2,22 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
+import pytest
+from aiodocker import DockerError
+from fastapi import status
 from simcore_service_autoscaling.utils_docker import (
-    ClusterResources,
     eval_cluster_resources,
     need_resources,
 )
 
 
 async def test_eval_cluster_resource_without_swarm():
-    assert not await need_resources()
+    with pytest.raises(DockerError) as exc_info:
+        await need_resources()
 
-    resources: ClusterResources = await eval_cluster_resources()
-    assert not resources.nodes_ids
+    assert exc_info.value.status == status.HTTP_503_SERVICE_UNAVAILABLE
+
+    with pytest.raises(DockerError) as exc_info:
+        await eval_cluster_resources()
+
+    assert exc_info.value.status == status.HTTP_503_SERVICE_UNAVAILABLE
