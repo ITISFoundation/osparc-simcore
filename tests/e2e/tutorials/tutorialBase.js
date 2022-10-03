31359f446ac9043b9483ebc63cb845f95fa6d962
Odei Maiz <maiz@itis.swiss>
2022-10-03 11:25:29 +0200

    :open

--- tests/e2e/tutorials/tutorialBase.js
+++ tests/e2e/tutorials/tutorialBase.js
@@ -208,7 +208,7 @@ class TutorialBase {
     this.__responsesQueue.addResponseListener(":open");
     let resp = null;
     try {
-      resp = await this.__responsesQueue.waitUntilResponse("open");
+      resp = await this.__responsesQueue.waitUntilResponse(":open");
     }
     catch (err) {
       console.error(this.__templateName, "could not be started", err);
@@ -225,7 +225,7 @@ class TutorialBase {
       await this.waitFor(2000);
       await auto.dashboardNewPlan(this.__page);
       await this.__responsesQueue.waitUntilResponse("projects?from_study=");
-      resp = await this.__responsesQueue.waitUntilResponse("open");
+      resp = await this.__responsesQueue.waitUntilResponse(":open");
       const studyId = resp["data"]["uuid"];
       console.log("Study ID:", studyId);
     }
@@ -265,7 +265,7 @@ class TutorialBase {
       const templateFound = await auto.dashboardOpenFirstTemplate(this.__page, this.__templateName);
       assert(templateFound, "Expected template, got nothing. TIP: did you inject templates in database??")
       await this.__responsesQueue.waitUntilResponse("projects?from_study=");
-      resp = await this.__responsesQueue.waitUntilResponse("open");
+      resp = await this.__responsesQueue.waitUntilResponse(":open");
       const studyId = resp["data"]["uuid"];
       console.log("Study ID:", studyId);
     }
@@ -285,7 +285,7 @@ class TutorialBase {
     try {
       const serviceFound = await auto.dashboardOpenService(this.__page, this.__templateName);
       assert(serviceFound, "Expected service, got nothing. TIP: is it available??");
-      resp = await this.__responsesQueue.waitUntilResponse("open");
+      resp = await this.__responsesQueue.waitUntilResponse(":open");
       const studyId = resp["data"]["uuid"];
       console.log("Study ID:", studyId);
     }
