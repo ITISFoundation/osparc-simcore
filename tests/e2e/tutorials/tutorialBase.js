43351d99d4f1d92a2054fde541b21b6064f60939
Odei Maiz <maiz@itis.swiss>
2022-10-04 07:57:40 +0200

    cleanup

--- tests/e2e/tutorials/tutorialBase.js
+++ tests/e2e/tutorials/tutorialBase.js
@@ -74,20 +74,6 @@ class TutorialBase {
     // eslint-disable-next-line no-undef
     const commit = await this.__page.evaluate(() => qx.core.Environment.get("osparc.vcsRef"));
     console.log("commit", commit);
-
-    await this.__page.evaluate((user, newUser) => {
-      let testerUserName = "";
-      if (newUser) {
-        testerUserName = "registered";
-      }
-      else if (user === null) {
-        testerUserName = "anonymous";
-      }
-      else {
-        testerUserName = user;
-      }
-      osparc.auth.Data.getInstance().setTesterUserName(testerUserName);
-    }, this.__user, this.__newUser);
   }
 
   async __printMe() {
