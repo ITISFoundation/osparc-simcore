e531c43d82067d4f4ffe94098dc581299c96f731
Andrei Neagu <neagu@itis.swiss>
2022-10-21 15:27:05 +0200

    ap now restarts failed coroutines

--- services/simcore-agent/src/simcore_service_simcore_agent/app.py
+++ services/simcore-agent/src/simcore_service_simcore_agent/app.py
@@ -1,22 +1,27 @@
 import asyncio
 import logging
 import signal
+import traceback
 from contextlib import suppress
-from typing import Coroutine
+from typing import Any, Callable, Coroutine, Final, Optional
+
+from pydantic import PositiveFloat, PositiveInt
+from servicelib.logging_utils import config_all_loggers
 
 from ._meta import APP_FINISHED_BANNER_MSG, APP_STARTED_BANNER_MSG
 from .settings import ApplicationSettings
 from .volumes_cleanup import backup_and_remove_volumes
-from servicelib.logging_utils import config_all_loggers
 
 logger = logging.getLogger(__name__)
+
+DEFAULT_TASK_WAIT_ON_ERROR: Final[PositiveInt] = 10
 
 
 class Application:
     HANDLED_EXIT_SIGNALS: set[signal.Signals] = {signal.SIGTERM, signal.SIGINT}
 
-    def __init__(self):
-        self._loop = asyncio.get_event_loop()
+    def __init__(self, loop: Optional[asyncio.AbstractEventLoop] = None):
+        self._loop = asyncio.get_event_loop() if loop is None else loop
         for sig_name in self.HANDLED_EXIT_SIGNALS:
             self._loop.add_signal_handler(
                 sig_name,
@@ -37,8 +42,44 @@ class Application:
         logger.info(APP_FINISHED_BANNER_MSG)
         loop.stop()
 
-    def add_coroutine(self, coroutine: Coroutine) -> None:
-        self._coroutines.add(coroutine)
+    def add_job(
+        self,
+        target: Callable,
+        *args: Any,
+        repeat_interval_s: Optional[PositiveFloat] = None
+    ) -> None:
+        async def _task_runner() -> None:
+            while True:
+                coroutine: Coroutine = target(*args)
+                logger.info("Running '%s'", coroutine.__name__)
+                try:
+                    await coroutine
+                except Exception as e:  # pylint: disable=broad-except
+
+                    logger.error(
+                        "Had an error while running '%s':\n%s",
+                        coroutine.__name__,
+                        "\n".join(traceback.format_tb(e.__traceback__)),
+                    )
+
+                if repeat_interval_s is None:
+                    logger.warning(
+                        "Unexpected termination of '%s'; it will be restarted",
+                        coroutine.__name__,
+                    )
+
+                logger.info(
+                    "Will run '%s' again in %s seconds",
+                    coroutine.__name__,
+                    repeat_interval_s,
+                )
+                await asyncio.sleep(
+                    DEFAULT_TASK_WAIT_ON_ERROR
+                    if repeat_interval_s is None
+                    else repeat_interval_s
+                )
+
+        self._coroutines.add(_task_runner())
 
     def run(self) -> None:
         logger.info(APP_STARTED_BANNER_MSG)
@@ -64,6 +105,10 @@ def create_application() -> Application:
     settings = ApplicationSettings.create_from_envs()
     setup_logger(settings)
 
-    app.add_coroutine(backup_and_remove_volumes(settings))
+    app.add_job(
+        backup_and_remove_volumes,
+        settings,
+        repeat_interval_s=settings.SIMCORE_AGENT_INTERVAL_VOLUMES_CLEANUP_S,
+    )
 
     return app
