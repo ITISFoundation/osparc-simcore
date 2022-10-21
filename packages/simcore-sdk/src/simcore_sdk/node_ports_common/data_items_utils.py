48d22c80c6c2cf3a8e8c08ca434ea8c102b4185f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-10-21 08:21:03 +0200

    allow for creating long file ids

--- packages/simcore-sdk/src/simcore_sdk/node_ports_common/data_items_utils.py
+++ packages/simcore-sdk/src/simcore_sdk/node_ports_common/data_items_utils.py
@@ -1,14 +1,25 @@
+import contextlib
 import tempfile
 import threading
 from pathlib import Path
+from typing import Optional
 
 from models_library.projects_nodes_io import SimcoreS3FileID
 
 
 def create_simcore_file_id(
-    file_path: Path, project_id: str, node_id: str
+    file_path: Path,
+    project_id: str,
+    node_id: str,
+    *,
+    file_base_path: Optional[Path] = None,
 ) -> SimcoreS3FileID:
-    return SimcoreS3FileID(f"{project_id}/{node_id}/{file_path.name}")
+    s3_file_name = file_path.name
+    if file_base_path:
+        with contextlib.suppress(ValueError):
+            s3_file_name = file_path.relative_to(file_base_path)
+
+    return SimcoreS3FileID(f"{project_id}/{node_id}/{s3_file_name}")
 
 
 _INTERNAL_DIR = Path(tempfile.gettempdir(), "simcorefiles")
