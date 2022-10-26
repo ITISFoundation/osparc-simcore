aa879450f8e6b48e7a4dcc742176737725a5217e
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-10-26 15:29:47 +0200

    adds example script

---
+++ packages/service-integration/scripts/ooil.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+
+set -o errexit
+set -o nounset
+set -o pipefail
+IFS=$'\n\t'
+
+IMAGE_NAME="itisfoundation/service-integration:master-github-latest"
+WORKDIR="$(pwd)"
+
+
+run() {
+  docker run \
+    --it \
+    --rm \
+    --volume="/etc/group:/etc/group:ro" \
+    --volume="/etc/passwd:/etc/passwd:ro" \
+    --user="$(id --user "$USER")":"$(id --group "$USER")" \
+    --volume "$WORKDIR":/src \
+    --workdir=/src \
+    "$IMAGE_NAME" \
+    "$@"
+}
+
+# ----------------------------------------------------------------------
+# MAIN
+#
+# USAGE
+#    ./scripts/ooil.bash --help
+run "$@"
+echo "DONE"
+# ----------------------------------------------------------------------
