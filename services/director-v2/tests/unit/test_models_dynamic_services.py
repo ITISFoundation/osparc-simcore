7d34c018fca68de65d815e82ab7677ddb55a1a10
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-10-28 13:40:12 +0200

    @pcrespov review: re-enable test

--- services/director-v2/tests/unit/test_models_dynamic_services.py
+++ services/director-v2/tests/unit/test_models_dynamic_services.py
@@ -148,12 +148,12 @@ def test_running_service_details_make_status(
     assert running_service_details_dict == expected_running_service_details
 
 
-# def test_all_states_are_mapped():
-#     service_state_defined: set[ServiceState] = _all_states()
-#     comparison_mapped: set[ServiceState] = set(ServiceState.comparison_order().keys())
-#     assert (
-#         service_state_defined == comparison_mapped
-#     ), "entries from _COMPARISON_ORDER do not match all states in ServiceState"
+def test_all_states_are_mapped():
+    service_state_defined: set[ServiceState] = _all_states()
+    comparison_mapped: set[ServiceState] = set(ServiceState.comparison_order().keys())
+    assert (
+        service_state_defined == comparison_mapped
+    ), "entries from _COMPARISON_ORDER do not match all states in ServiceState"
 
 
 def test_equality():
