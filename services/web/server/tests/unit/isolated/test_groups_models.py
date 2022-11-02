ce0efa0e454290eae5be9e5e8cb586cb222ce9bb
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2022-11-02 11:34:17 +0100

    🐛 Fix/sanitize old data for usergroups.thumbnail (#3498)

--- services/web/server/tests/unit/isolated/test_groups_models.py
+++ services/web/server/tests/unit/isolated/test_groups_models.py
@@ -48,3 +48,31 @@ def test_group_models_examples(
 
         assert model_enveloped.error is None
         assert model_array_enveloped.error is None
+
+
+def test_sanitize_legacy_data():
+    users_group_1 = UsersGroup.parse_obj(
+        {
+            "gid": "27",
+            "label": "A user",
+            "description": "A very special user",
+            "thumbnail": "",  # <--- empty strings
+            "accessRights": {"read": True, "write": False, "delete": False},
+        }
+    )
+
+    assert users_group_1.thumbnail is None
+
+    users_group_2 = UsersGroup.parse_obj(
+        {
+            "gid": "27",
+            "label": "A user",
+            "description": "A very special user",
+            "thumbnail": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAPgAAADMCAMAAABp5J",  # <--- encoded thumbnail are discarded
+            "accessRights": {"read": True, "write": False, "delete": False},
+        }
+    )
+
+    assert users_group_2.thumbnail is None
+
+    assert users_group_1 == users_group_2
