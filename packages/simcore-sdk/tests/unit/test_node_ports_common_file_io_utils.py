f787c8dcb085a1adbb297aaedfac96c9c1605ebf
Andrei Neagu <neagu@itis.swiss>
2022-11-08 15:07:37 +0100

    refactor tests

--- packages/simcore-sdk/tests/unit/test_node_ports_common_file_io_utils.py
+++ packages/simcore-sdk/tests/unit/test_node_ports_common_file_io_utils.py
@@ -1,105 +1,82 @@
 # pylint: disable=redefined-outer-name
 # pylint: disable=unused-argument
 
-from asyncio import BaseEventLoop
+from typing import AsyncIterable, Iterable
 
 import pytest
-from aiohttp import web
-from aiohttp.test_utils import TestClient
-from pytest import FixtureRequest
+from aiohttp import ClientResponse, ClientSession
+from aioresponses import aioresponses
 from simcore_sdk.node_ports_common.file_io_utils import (
     ExtendedClientResponseError,
     _check_for_aws_http_errors,
     _raise_for_status,
 )
 
-
-def _get_test_client(
-    mocked_response: web.Response,
-    loop: BaseEventLoop,
-    aiohttp_client,
-) -> TestClient:
-    async def raise_error(_: web.Request) -> web.Response:
-        return mocked_response
-
-    app = web.Application()
-    app.router.add_get("/", raise_error)
-    return loop.run_until_complete(aiohttp_client(app))
-
-
-TEST_ERROR = "OPSIE there was an error here"
+A_TEST_ROUTE = "http://127.0.0.1:1249/test-route"
 
 
 @pytest.fixture
-def test_client(loop: BaseEventLoop, aiohttp_client) -> TestClient:
-    return _get_test_client(
-        mocked_response=web.Response(
-            body=TEST_ERROR, status=web.HTTPBadRequest.status_code
+def mock_service() -> Iterable[aioresponses]:
+    with aioresponses() as mock:
+        yield mock
+
+
+@pytest.fixture
+async def aiohttp_client() -> AsyncIterable[ClientSession]:
+    async with ClientSession() as session:
+        yield session
+
+
+async def test_raise_for_status(
+    mock_service: aioresponses, aiohttp_client: ClientSession
+):
+    mock_service.get(A_TEST_ROUTE, body="OPSIE there was an error here", status=400)
+
+    async with aiohttp_client.get(A_TEST_ROUTE) as resp:
+        assert isinstance(resp, ClientResponse)
+
+        with pytest.raises(ExtendedClientResponseError) as exe_info:
+            await _raise_for_status(resp)
+        assert "OPSIE there was an error here" in f"{exe_info.value}"
+
+
+@pytest.mark.parametrize(
+    "status, body",
+    [
+        (
+            400,
+            '<?xml version="1.0" encoding="UTF-8"?><Error><Code>RequestTimeout</Code>'
+            "<Message>Your socket connection to the server was not read from or written to within "
+            "the timeout period. Idle connections will be closed.</Message>"
+            "<RequestId>7EE901348D6C6812</RequestId><HostId>"
+            "FfQE7jdbUt39E6mcQq/"
+            "ZeNR52ghjv60fccNT4gCE4IranXjsGLG+L6FUyiIxx1tAuXL9xtz2NAY7ZlbzMTm94fhY3TBiCBmf"
+            "</HostId></Error>",
         ),
-        loop=loop,
-        aiohttp_client=aiohttp_client,
-    )
-
-
-async def test_raise_for_status(test_client: TestClient):
-    resp = await test_client.get("/")
-    with pytest.raises(ExtendedClientResponseError) as exe_info:
-        await _raise_for_status(resp)
-    assert TEST_ERROR in f"{exe_info.value}"
-
-
-@pytest.fixture(
-    params=[
-        web.Response(
-            status=400,
-            body=(
-                '<?xml version="1.0" encoding="UTF-8"?><Error><Code>RequestTimeout</Code>'
-                "<Message>Your socket connection to the server was not read from or written to within "
-                "the timeout period. Idle connections will be closed.</Message>"
-                "<RequestId>7EE901348D6C6812</RequestId><HostId>"
-                "FfQE7jdbUt39E6mcQq/"
-                "ZeNR52ghjv60fccNT4gCE4IranXjsGLG+L6FUyiIxx1tAuXL9xtz2NAY7ZlbzMTm94fhY3TBiCBmf"
-                "</HostId></Error>"
-            ),
-        ),
-        web.Response(status=500),
-        web.Response(status=503),
-    ]
+        (500, ""),
+        (503, ""),
+    ],
 )
-def test_client_2(
-    request: FixtureRequest, loop: BaseEventLoop, aiohttp_client
-) -> TestClient:
-    return _get_test_client(
-        mocked_response=request.param, loop=loop, aiohttp_client=aiohttp_client
-    )
+async def testtest_check_for_aws_http_errors_true_raise_for_status(
+    mock_service: aioresponses, aiohttp_client: ClientSession, status: int, body: str
+):
+    mock_service.get(A_TEST_ROUTE, body=body, status=status)
+
+    async with aiohttp_client.get(A_TEST_ROUTE) as resp:
+        try:
+            await _raise_for_status(resp)
+        except ExtendedClientResponseError as exception:
+            assert _check_for_aws_http_errors(exception) is True
 
 
-async def test__check_for_aws_http_errors_true(test_client_2: TestClient):
-    resp = await test_client_2.get("/")
-    try:
-        await _raise_for_status(resp)
-    except ExtendedClientResponseError as exception:
-        assert _check_for_aws_http_errors(exception) is True
+@pytest.mark.parametrize("status", [400, 200, 399])
+async def test_check_for_aws_http_errors_false(
+    mock_service: aioresponses, aiohttp_client: ClientSession, status: int
+):
+    mock_service.get(A_TEST_ROUTE, status=status)
 
-
-@pytest.fixture(
-    params=[
-        web.Response(status=400),
-        web.Response(status=200),
-        web.Response(status=399),
-    ]
-)
-def test_client_3(
-    request: FixtureRequest, loop: BaseEventLoop, aiohttp_client
-) -> TestClient:
-    return _get_test_client(
-        mocked_response=request.param, loop=loop, aiohttp_client=aiohttp_client
-    )
-
-
-async def test_check_for_aws_http_errors_false(test_client_3: TestClient):
-    resp = await test_client_3.get("/")
-    try:
-        await _raise_for_status(resp)
-    except ExtendedClientResponseError as exception:
-        assert _check_for_aws_http_errors(exception) is False
+    async with aiohttp_client.get(A_TEST_ROUTE) as resp:
+        try:
+            await _raise_for_status(resp)
+        except ExtendedClientResponseError as exception:
+            assert _check_for_aws_http_errors(exception) is False
