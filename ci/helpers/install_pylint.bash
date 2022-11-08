4b5a94b359ca3203ec8229c3d8b6222c93301236
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-11-08 11:27:27 +0100

    Revert "⬆️ Maintenance/upgrades tests+tooling requirements (#3524)"
    This reverts commit 9274b8e7c56c4a4e2a3545a8a7e68832a33d1780.

--- ci/helpers/install_pylint.bash
+++ ci/helpers/install_pylint.bash
@@ -4,18 +4,20 @@
 #
 
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -o errexit  # abort on nonzero exitstatus
-set -o nounset  # abort on unbound variable
-set -o pipefail # don't hide errors within pipes
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
 IFS=$'\n\t'
 
-CURDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-REQUIREMENTS=packages/service-library/requirements/_tools.txt
+
+REQUIREMENTS=packages/service-library/requirements/_test.txt
 PYLINT_VERSION="$(grep pylint== $REQUIREMENTS | awk '{print $1}')"
 pip3 install "$PYLINT_VERSION"
 
 # Minimal packages to pass linter
 pip install -r "$CURDIR/requirements.txt"
+
 
 echo "INFO:" "$(pylint --version)" "@" "$(command -v pylint)"
