210d213fd074f2d3252197d23e26b90c5e1ca9fd
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-11-10 00:42:34 +0100

    created fixture to pull data if needed

--- packages/models-library/tests/test_utils_service_io.py
+++ packages/models-library/tests/test_utils_service_io.py
@@ -6,14 +6,17 @@
 
 import itertools
 import json
+import sys
 from pathlib import Path
 from typing import Union
 
 import pytest
-from models_library.services import ServiceInput, ServiceOutput
+from models_library.services import ServiceInput, ServiceOutput, ServicePortKey
 from models_library.utils.json_schema import jsonschema_validate_schema
 from models_library.utils.services_io import get_service_io_json_schema
 from pydantic import parse_obj_as
+from pytest_simcore.helpers.typing_env import EnvVarsDict
+from pytest_simcore.helpers.utils_docker_registry import download_all_registry_metadata
 
 example_inputs_labels = [
     e for e in ServiceInput.Config.schema_extra["examples"] if e["label"]
@@ -50,32 +53,48 @@ def test_get_schema_from_port(service_port: Union[ServiceInput, ServiceOutput]):
         jsonschema_validate_schema(schema)
 
 
-def test_it(project_tests_dir: Path):
-    data_folder = project_tests_dir / "data"
-
-    for path in data_folder.rglob("metadata*.json"):
-        print(path)
-        meta = json.loads(path.read_text())
-
-        inputs = parse_obj_as(list[ServiceInput], meta["inputs"])
-        outputs = parse_obj_as(list[ServiceOutput], meta["outputs"])
-
-        for port in itertools.chain(inputs, outputs):
-            schema = get_service_io_json_schema(port)
-
-            if port.property_type.startswith("data"):
-                assert not schema
-            else:
-                assert schema
-                # check valid jsons-schema
-                jsonschema_validate_schema(schema)
+CURRENT_DIR = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+TEST_DATA_FOLDER = CURRENT_DIR / "data"
 
 
-# TODO: test against all image labels in master registry! and write down tests that
-# fail right now or edge cases
-#
-#
-# Build an audit tool as well to keep track of changes in models-Library inputs.outpust schemas
-#
+@pytest.fixture(scope="module")
+def metadata_test_data_dir(
+    project_tests_dir: Path, mock_env_devel_environment: EnvVarsDict
+):
+    """
+    Downloads all metadata from services to registry defined in env vars
+    in
+    """
+    assert project_tests_dir == CURRENT_DIR
+    test_data_dir = TEST_DATA_FOLDER / ".metadata-cache.keep.ignore"
+    download_all_registry_metadata(dest_dir=test_data_dir)
+    return test_data_dir
 
-# curl -u  admin:adminadmin -X GET https://registry.osparc-master.speag.com/v2/simcore/services/comp/ascent-runner/manifests/1.0.0 | jq ".history[0].v1Compatibility"
+
+@pytest.mark.parametrize(
+    "meta_path",
+    TEST_DATA_FOLDER.rglob("metadata*.json"),
+    ids=lambda p: f"{p.parent.name}/{p.name}",
+)
+def test_against_service_metadata_configs(
+    metadata_test_data_dir: Path, meta_path: Path
+):
+    """
+    This tests can be used as well to validate all metadata in a given registry
+    """
+    assert metadata_test_data_dir in meta_path.parents
+
+    meta = json.loads(meta_path.read_text())
+
+    inputs = parse_obj_as(dict[ServicePortKey, ServiceInput], meta["inputs"])
+    outputs = parse_obj_as(dict[ServicePortKey, ServiceOutput], meta["outputs"])
+
+    for port in itertools.chain(inputs.values(), outputs.values()):
+        schema = get_service_io_json_schema(port)
+
+        if port.property_type.startswith("data"):
+            assert not schema
+        else:
+            assert schema
+            # check valid jsons-schema
+            jsonschema_validate_schema(schema)
