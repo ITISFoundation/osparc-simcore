36ef697d323c200827298e0d0580df1258750c5f
Andrei Neagu <neagu@itis.swiss>
2022-11-09 13:41:29 +0100

    fixed broken tests

--- services/dynamic-sidecar/tests/unit/test_cli.py
+++ services/dynamic-sidecar/tests/unit/test_cli.py
@@ -2,8 +2,10 @@
 # pylint: disable=redefined-outer-name
 
 import os
+import traceback
 
 import pytest
+from click.testing import Result
 from pytest_mock.plugin import MockerFixture
 from pytest_simcore.helpers.typing_env import EnvVarsDict
 from simcore_service_dynamic_sidecar.cli import main
@@ -11,7 +13,17 @@ from typer.testing import CliRunner
 
 
 @pytest.fixture
-def cli_runner(mock_environment: EnvVarsDict) -> CliRunner:
+def mock_node_ports_v2_ports(mocker: MockerFixture) -> None:
+    mocker.patch(
+        "simcore_service_dynamic_sidecar.modules.outputs_manager.node_ports_v2.ports",
+        spec=True,
+    )
+
+
+@pytest.fixture
+def cli_runner(
+    mock_environment: EnvVarsDict, mock_node_ports_v2_ports: None
+) -> CliRunner:
     return CliRunner()
 
 
@@ -36,11 +48,17 @@ def mock_nodeports(mocker: MockerFixture) -> None:
     )
 
 
+def _format_cli_error(result: Result) -> str:
+    assert result.exception
+    tb_message = "".join(traceback.format_tb(result.exception.__traceback__))
+    return f"Below exception was raised by the cli:\n{tb_message}"
+
+
 def test_list_state_dirs(
     cli_runner: CliRunner, mock_rabbitmq: None, mock_data_manager: None
 ):
     result = cli_runner.invoke(main, ["state-list-dirs"])
-    assert result.exit_code == os.EX_OK, result.stdout
+    assert result.exit_code == os.EX_OK, _format_cli_error(result)
     assert result.stdout.strip() == "\n".join(
         [f"Entries in /data/state_dir{i}: []" for i in range(4)]
     )
@@ -50,7 +68,7 @@ def test_outputs_save_interface(
     cli_runner: CliRunner, mock_rabbitmq: None, mock_data_manager: None
 ):
     result = cli_runner.invoke(main, ["state-save"])
-    assert result.exit_code == os.EX_OK, result.stdout
+    assert result.exit_code == os.EX_OK, _format_cli_error(result)
     assert result.stdout == "state save finished successfully\n"
 
 
@@ -58,5 +76,5 @@ def test_state_push_outputs(
     cli_runner: CliRunner, mock_rabbitmq: None, mock_nodeports: None
 ):
     result = cli_runner.invoke(main, ["outputs-push"])
-    assert result.exit_code == os.EX_OK, result.stdout
+    assert result.exit_code == os.EX_OK, _format_cli_error(result)
     assert result.stdout == "output ports push finished successfully\n"
