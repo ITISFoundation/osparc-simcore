dcbeee0423d68b1719b7a0846ffe35c8d740c3bc
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-11-11 10:38:26 +0100

    cleanup

--- services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/core/rabbitmq.py
+++ services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/core/rabbitmq.py
@@ -1,7 +1,6 @@
-from __future__ import annotations
-
 import logging
 from dataclasses import dataclass
+from typing import Union
 
 from fastapi import FastAPI
 from models_library.rabbitmq_messages import (
@@ -15,6 +14,33 @@ from servicelib.rabbitmq import RabbitMQClient as BaseRabbitMQClient
 from ..core.settings import ApplicationSettings
 
 log = logging.getLogger(__file__)
+
+
+@dataclass
+class RabbitMQ(BaseRabbitMQClient):
+    app_settings: ApplicationSettings
+
+    async def post_log_message(self, log_msg: Union[str, list[str]]) -> None:
+        if isinstance(log_msg, str):
+            log_msg = [log_msg]
+
+        data = LoggerRabbitMessage(
+            node_id=self.app_settings.DY_SIDECAR_NODE_ID,
+            user_id=self.app_settings.DY_SIDECAR_USER_ID,
+            project_id=self.app_settings.DY_SIDECAR_PROJECT_ID,
+            messages=log_msg,
+        )
+
+        await self.publish(self.settings.RABBIT_CHANNELS["log"], data.json())
+
+    async def send_event_reload_iframe(self) -> None:
+        data = EventRabbitMessage(
+            node_id=self.app_settings.DY_SIDECAR_NODE_ID,
+            user_id=self.app_settings.DY_SIDECAR_USER_ID,
+            project_id=self.app_settings.DY_SIDECAR_PROJECT_ID,
+            action=RabbitEventMessageType.RELOAD_IFRAME,
+        )
+        await self.publish(self.settings.RABBIT_CHANNELS["events"], data.json())
 
 
 async def send_message(rabbitmq: RabbitMQ, msg: str) -> None:
@@ -38,30 +64,3 @@ def setup_rabbitmq(app: FastAPI) -> None:
 
     app.add_event_handler("startup", on_startup)
     app.add_event_handler("shutdown", on_shutdown)
-
-
-@dataclass
-class RabbitMQ(BaseRabbitMQClient):
-    app_settings: ApplicationSettings
-
-    async def post_log_message(self, log_msg: str | list[str]) -> None:
-        if isinstance(log_msg, str):
-            log_msg = [log_msg]
-
-        data = LoggerRabbitMessage(
-            node_id=self.app_settings.DY_SIDECAR_NODE_ID,
-            user_id=self.app_settings.DY_SIDECAR_USER_ID,
-            project_id=self.app_settings.DY_SIDECAR_PROJECT_ID,
-            messages=log_msg,
-        )
-
-        await self.publish(self.settings.RABBIT_CHANNELS["log"], data.json())
-
-    async def send_event_reload_iframe(self) -> None:
-        data = EventRabbitMessage(
-            node_id=self.app_settings.DY_SIDECAR_NODE_ID,
-            user_id=self.app_settings.DY_SIDECAR_USER_ID,
-            project_id=self.app_settings.DY_SIDECAR_PROJECT_ID,
-            action=RabbitEventMessageType.RELOAD_IFRAME,
-        )
-        await self.publish(self.settings.RABBIT_CHANNELS["events"], data.json())
