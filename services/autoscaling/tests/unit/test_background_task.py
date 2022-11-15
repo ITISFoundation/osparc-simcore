631b1f18fe4f808eea68aab635af29db44289d38
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-11-15 11:05:04 +0100

    check cancellation

--- services/autoscaling/tests/unit/test_background_task.py
+++ services/autoscaling/tests/unit/test_background_task.py
@@ -56,3 +56,14 @@ async def test_background_task_raises_restarts(
     await asyncio.sleep(3 * _FAST_POLL_INTERVAL)
     mock_background_task.assert_called()
     assert mock_background_task.call_count > 1
+
+
+async def test_background_task_correctly_cancels(
+    app_environment: EnvVarsDict,
+    mock_background_task: mock.Mock,
+    initialized_app: FastAPI,
+):
+    mock_background_task.side_effect = asyncio.CancelledError
+    await asyncio.sleep(3 * _FAST_POLL_INTERVAL)
+    # the task will be called once, and then stop
+    mock_background_task.assert_called_once()
