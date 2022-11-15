1e782ecac050fc59c4b85a6982ce6e5b2c37d058
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-11-15 11:50:17 +0100

    initial test

---
+++ services/autoscaling/tests/unit/test_dynamic_scaling_core.py
@@ -0,0 +1,34 @@
+# pylint: disable=no-value-for-parameter
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+
+
+from typing import Iterator
+
+import pytest
+from fastapi import FastAPI
+from pytest_mock.plugin import MockerFixture
+from simcore_service_autoscaling.dynamic_scaling_core import check_dynamic_resources
+
+
+@pytest.fixture
+def disable_dynamic_service_background_task(mocker: MockerFixture) -> Iterator[None]:
+    mocker.patch(
+        "simcore_service_autoscaling.dynamic_scaling.start_background_task",
+        autospec=True,
+    )
+
+    mocker.patch(
+        "simcore_service_autoscaling.dynamic_scaling.stop_background_task",
+        autospec=True,
+    )
+
+    yield
+
+
+async def test_check_dynamic_resources(
+    disable_dynamic_service_background_task: None,
+    initialized_app: FastAPI,
+):
+    await check_dynamic_resources(initialized_app)
