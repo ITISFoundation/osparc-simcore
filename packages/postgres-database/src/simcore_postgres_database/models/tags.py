36bf5d98a4e8695cff8292946539c0eca254fa00
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-11-17 19:54:08 +0100

    adds tag_to_group table and docs

--- packages/postgres-database/src/simcore_postgres_database/models/tags.py
+++ packages/postgres-database/src/simcore_postgres_database/models/tags.py
@@ -2,6 +2,123 @@ import sqlalchemy as sa
 
 from .base import metadata
 
+#
+# tags: a way to mark any entity (e.g. a project, ...)
+#       this can be used to perform operations as filter, select, compare, etc
+#
+tags = sa.Table(
+    "tags",
+    metadata,
+    sa.Column(
+        "id",
+        sa.BigInteger(),
+        nullable=False,
+        primary_key=True,
+    ),
+    sa.Column(
+        "user_id",
+        sa.BigInteger(),
+        sa.ForeignKey("users.id", onupdate="CASCADE", ondelete="CASCADE"),
+        nullable=False,
+        doc="DEPRECATED: should be replaced by assigning a tag_to_group instead",
+    ),
+    sa.Column(
+        "name",
+        sa.String(),
+        nullable=False,
+        doc="display name",
+    ),
+    sa.Column(
+        "description",
+        sa.String(),
+        nullable=True,
+        doc="description displayed",
+    ),
+    sa.Column(
+        "color",
+        sa.String(),
+        nullable=False,
+        doc="Hex color (see https://www.color-hex.com/)",
+    ),
+)
+
+
+#
+# tag_to_group: Defines the level of access of a group (group_id) to a given tag (tag_id)
+#
+tag_to_group = sa.Table(
+    "tag_to_group",
+    metadata,
+    sa.Column(
+        "tag_id",
+        sa.String(),
+        sa.ForeignKey(
+            tags.c.id,
+            onupdate="CASCADE",
+            ondelete="CASCADE",
+            name="fk_tag_to_group_tag_id",
+        ),
+        nullable=False,
+        doc="Tag unique ID",
+    ),
+    sa.Column(
+        "group_id",
+        sa.String(),
+        sa.ForeignKey(
+            "groups.gid",
+            onupdate="CASCADE",
+            ondelete="CASCADE",
+            name="fk_tag_to_group_group_id",
+        ),
+        nullable=False,
+        doc="Group unique ID",
+    ),
+    # ACCESS RIGHTS ---
+    sa.Column(
+        "read",
+        sa.Boolean(),
+        nullable=False,
+        server_default=sa.sql.expression.true(),
+        doc="If true, group can see the tag listed and can assign it to a project."
+        "This column can be used to set the tag invisible",
+    ),
+    sa.Column(
+        "write",
+        sa.Boolean(),
+        nullable=False,
+        server_default=sa.sql.expression.false(),
+        doc="If true, group can modify the tag attributes, e.g. name, color etc",
+    ),
+    sa.Column(
+        "delete",
+        sa.Boolean(),
+        nullable=False,
+        server_default=sa.sql.expression.false(),
+        doc="If true, group can delete the tag",
+    ),
+    # TIME STAMPS ----
+    sa.Column(
+        "created",
+        sa.DateTime(),
+        nullable=False,
+        server_default=sa.sql.func.now(),
+        doc="Timestamp auto-generated upon creation",
+    ),
+    sa.Column(
+        "modified",
+        sa.DateTime(),
+        nullable=False,
+        server_default=sa.sql.func.now(),
+        onupdate=sa.sql.func.now(),
+        doc="Timestamp with last row update",
+    ),
+    sa.UniqueConstraint("tag_id", "group_id"),
+)
+
+
+#
+# study_tags: projects marked with tags
+#
 study_tags = sa.Table(
     "study_tags",
     metadata,
@@ -18,19 +135,4 @@ study_tags = sa.Table(
         nullable=False,
     ),
     sa.UniqueConstraint("study_id", "tag_id"),
-)
-
-tags = sa.Table(
-    "tags",
-    metadata,
-    sa.Column("id", sa.BigInteger, nullable=False, primary_key=True),
-    sa.Column(
-        "user_id",
-        sa.BigInteger,
-        sa.ForeignKey("users.id", onupdate="CASCADE", ondelete="CASCADE"),
-        nullable=False,
-    ),
-    sa.Column("name", sa.String, nullable=False),
-    sa.Column("description", sa.String, nullable=True),
-    sa.Column("color", sa.String, nullable=False),
 )
