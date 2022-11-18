d49d0667077050252f1d846aa37f15003f116f44
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-11-18 17:33:59 +0100

    test_tags with repo prototype

--- packages/postgres-database/tests/test_models_tags.py
+++ packages/postgres-database/tests/test_models_tags.py
@@ -1,101 +1,243 @@
-from typing import Any, Callable
+from dataclasses import dataclass
+from typing import Any, Callable, Iterator, NamedTuple, TypedDict
 
 import pytest
 import sqlalchemy as sa
 from aiopg.sa.connection import SAConnection
 from aiopg.sa.engine import Engine
 from aiopg.sa.result import RowProxy
-from simcore_postgres_database.models.groups import GroupType, groups, user_to_groups
+from simcore_postgres_database.models.groups import groups, user_to_groups
 from simcore_postgres_database.models.tags import tags, tags_to_groups
 from simcore_postgres_database.models.users import UserRole, UserStatus, users
 
 
+class TestData(NamedTuple):
+    user: dict
+    group: dict
+
+
 @pytest.fixture
-async def setup_tables(
-    pg_engine: Engine,
-    create_fake_group: Callable[[SAConnection, Any], RowProxy],
+async def connection(pg_engine: Engine) -> Iterator[SAConnection]:
+    async with pg_engine.acquire() as _conn:
+        yield _conn
+
+
+@pytest.fixture
+async def group(
+    create_fake_group: Callable[[SAConnection, Any], RowProxy], connection: SAConnection
+) -> RowProxy:
+    group_ = await create_fake_group(connection, name="some_group")
+    assert group_
+    return group_
+
+
+@pytest.fixture
+async def user(
     create_fake_user: Callable[[SAConnection, RowProxy, Any], RowProxy],
-):
-    # have some tags with user_id
-    async with pg_engine.acquire() as conn:
-        group = await create_fake_group(conn, name="some_group")
-        user = await create_fake_user(
-            conn,
-            group=group,
-            name="pcrespov",
-            status=UserStatus.ACTIVE,
-            role=UserRole.USER,
+    group: RowProxy,
+    connection: SAConnection,
+) -> RowProxy:
+    user_ = await create_fake_user(
+        connection,
+        group=group,
+        name="pcrespov",
+        status=UserStatus.ACTIVE,
+        role=UserRole.USER,
+    )
+
+    return user_
+
+
+# ----------------------
+# Prototype for tags repo layer
+
+
+class TagDict(TypedDict):
+    id: int
+    name: int
+    description: str
+    color: str
+
+
+class NotFoundError(RuntimeError):
+    pass
+
+
+class NotAllowedError(RuntimeError):  # maps to AccessForbidden
+    pass
+
+
+@dataclass
+class TagsRepo:
+    user_id: int
+
+    async def list_(self, conn: SAConnection):
+        # select read tags in user's groups
+        j_user_read_tags = (
+            tags.join(
+                tags_to_groups,
+                (tags.c.tag_id == tags_to_groups.c.id)
+                & (tags_to_groups.c.read == True),
+            )
+            .join(groups)
+            .join(user_to_groups, user_to_groups.c.uid == self.user_id)
+        )
+        select_stmt = (
+            sa.select([tags.c.id, tags.c.name, tags.c.description, tags.c.color])
+            .distinct(tags.c.id)
+            .select_from(j_user_read_tags)
+            .order_by(tags.c.name)
+            .limit(50)
         )
 
-        for data in [
-            {"name": "tag1", "description": "description1", "color": "#f00"},
-            {"name": "tag2", "description": "description2", "color": "#00f"},
-        ]:
-            await conn.scalar(
-                tags.insert().values(user_id=user.id, **data).returning(tags.c.id)
+        # pylint: disable=not-an-iterable
+        tags = []
+        async for row in conn.execute(select_stmt):
+            row_dict = TagDict(row.items())
+            tags.append(row_dict)
+        return tags
+
+    async def update(self, conn: SAConnection, tag_id: int, tag_update: TagDict):
+        # select write tags in user's groups
+        j_user_write_tags = (
+            tags.join(
+                tags_to_groups,
+                (tags.c.id == tag_id) & (tags_to_groups.c.write == True),
+            )
+            .join(groups)
+            .join(user_to_groups, (user_to_groups.c.uid == self.user_id))
+        )
+
+        update_stmt = (
+            tags.update()
+            .values(
+                **{
+                    k: tag_update[k]
+                    for k in ("name", "description", "color")
+                    if k in tag_update
+                }
+            )
+            .where((tags.c.id == tag_id) & (tags.c.user_id == self.user_id))
+            .returning(tags.c.id, tags.c.name, tags.c.description, tags.c.color)
+        )
+
+        can_update = await conn.scalar(
+            sa.select([tags_to_groups.write]).select_from(j_user_write_tags).distinct()
+        )
+        if not can_update:
+            raise NotAllowedError()
+
+        result = await conn.execute(update_stmt)
+        if row_proxy := await result.first():
+            return TagDict(row_proxy.items())
+
+        raise NotFoundError
+
+    async def create(self, conn: SAConnection, tag_create: TagDict) -> int:
+        # repository
+        insert_tag_stmt = (
+            tags.insert()
+            .values(
+                name=tag_create["name"],
+                description=tag_create["description"],
+                color=tag_create["color"],
+            )
+            .returning(tags.c.id, tags.c.name, tags.c.description, tags.c.color)
+        )
+
+        async with conn.begin():
+            # get primary gid
+            primary_gid = await conn.scalar(
+                sa.select([users.c.primary_gid]).where(users.c.id == self.user_id)
             )
 
+            # insert new tag
+            result = await conn.execute(insert_tag_stmt)
+            if tag := await result.first():
+                # take tag ownership
+                await conn.execute(
+                    tags_to_groups.insert().values(
+                        tag_id=tag.id,
+                        group_id=primary_gid,
+                        read=True,
+                        write=True,
+                        delete=True,
+                    )
+                )
+                return TagDict(tag.items())
 
-def foo():
-    # we find group ids associated to this user
-
-    j = user_to_groups.join(
-        groups,
-        (groups.c.gid == user_to_groups.c.gid) & (groups.c.type == GroupType.PRIMARY),
-    )
-
-    s = sa.select([user_to_groups.c.uid, user_to_groups.c.gid]).select_from(j)
-
-    s = sa.select([tags.c.id, tags.c.name, user_to_groups.c.gid]).select_from(j)
-
-
-def insert_():
-    j = (
-        tags.join(users)
-        .join(user_to_groups)
-        .join(
-            groups,
-            (groups.c.gid == user_to_groups.c.gid)
-            & (groups.c.type == GroupType.PRIMARY),
+    async def get(self, conn: SAConnection, tag_id: int) -> TagDict:
+        j_user_read_tags = (
+            tags.join(
+                tags_to_groups,
+                (tags.c.tag_id == tags_to_groups.c.id)
+                & (tags_to_groups.c.read == True),
+            )
+            .join(groups)
+            .join(user_to_groups, user_to_groups.c.uid == self.user_id)
         )
-    )
-    select_stmt = sa.select([tags.c.id, user_to_groups.c.gid]).select_from(j)
-    insert_stmt = tags_to_groups.insert().from_select(
-        [tags_to_groups.c.tag_id, tags_to_groups.c.group_id], select_stmt
-    )
+        select_stmt = (
+            sa.select([tags.c.id, tags.c.name, tags.c.description, tags.c.color])
+            .distinct(tags.c.id)
+            .select_from(j_user_read_tags)
+            .where(tags.c.id == tag_id)
+        )
 
-    update_stmt = tags_to_groups.update().values(write=True, delete=True)
+        result = await conn.execute(select_stmt)
+        row = await result.first()
+        if not row:
+            raise NotFoundError
+        return TagDict(row.items())
 
-    print(insert_stmt)
-    print(update_stmt)
-    print()
+    async def delete(self, conn: SAConnection, tag_id: int):
+        # select delete tags in user's groups
+        j_user_delete_tags = (
+            tags.join(
+                tags_to_groups,
+                (tags.c.id == tag_id) & (tags_to_groups.c.delete == True),
+            )
+            .join(groups)
+            .join(user_to_groups, (user_to_groups.c.uid == self.user_id))
+        )
+
+        # pylint: disable=no-value-for-parameter
+        can_delete = await conn.scalar(
+            sa.select([tags_to_groups.c.delete])
+            .select_from(j_user_delete_tags)
+            .distinct()
+        )
+
+        if not can_delete:
+            raise NotAllowedError()
+
+        assert can_delete  # nosec
+        await conn.execute(tags.delete().where(tags.c.id == tag_id))
 
 
-async def test_it3(pg_engine: Engine, setup_tables):
+# ----------------------
+
+
+async def test_tags_repo(pg_engine: Engine, user: RowProxy, group: RowProxy):
 
     async with pg_engine.acquire() as conn:
+        repo = TagsRepo(user_id=user.id)
 
-        await conn.execute(insert_stmt)
+        # create & own
+        tag1 = await repo.create(conn, {"name": "t1", "color": "blue"})
+        tag2 = await repo.create(conn, {"name": "t2", "color": "red"})
 
-        # async for data in conn.execute():
-        #     print(data)
+        assert await repo.list_(conn) == [tag1, tag2]
 
-        #     await conn.execute(
-        #         tag_to_group.insert().values(
-        #             tag_id=data["id"],
-        #             group_id=data["gid"],
-        #             read=True,
-        #             write=True,
-        #             delete=True,
-        #         )
-        #     )
+        tag1_updated = {**tag1, **{"name": "new t1"}}
 
-    # transform them to a row in tag_to_group
+        assert (
+            await repo.update(conn, tag_id=tag1["id"], tag_update={"name": "new t1"})
+            == tag1_updated
+        )
 
-    # def test_list_tags():
-    # a tag in my group
+        assert await repo.get(conn, tag1["id"]) == tag1_updated
 
-    # a tag in another group
+        assert await repo.delete(conn, tag1["id"])
 
-    # def test_update_tags():
-    # a group in
+        with pytest.raises(NotFoundError):
+            await repo.get(conn, tag1["id"])
