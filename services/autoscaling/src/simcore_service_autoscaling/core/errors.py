98615d0eebd87d69867a43f86d324a6b54f1337a
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-11-21 15:03:19 +0100

    added error when too many instances are started

--- services/autoscaling/src/simcore_service_autoscaling/core/errors.py
+++ services/autoscaling/src/simcore_service_autoscaling/core/errors.py
@@ -8,3 +8,10 @@ class AutoscalingRuntimeError(PydanticErrorMixin, RuntimeError):
 class Ec2InstanceNotFoundError(AutoscalingRuntimeError):
     code = "autoscaling.ec2_instance_not_found_error"
     msg_template: str = "Needed instance was not found"
+
+
+class Ec2TooManyInstancesError(AutoscalingRuntimeError):
+    code = "autoscaling.ec2_too_many_instances_error"
+    msg_template: str = (
+        "The maximum amount of instances {num_instances} is already reached!"
+    )
