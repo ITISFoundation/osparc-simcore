f1b518b4e4ee0d12c3815a09779c11569909b2cb
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-11-27 22:42:19 +0100

    added new entry in services_specifications table

--- services/catalog/src/simcore_service_catalog/models/schemas/services_specifications.py
+++ services/catalog/src/simcore_service_catalog/models/schemas/services_specifications.py
@@ -1,6 +1,9 @@
 from typing import Optional
 
 from models_library.generated_models.docker_rest_api import (
+    Resources1 as DockerServiceTaskResources,
+)
+from models_library.generated_models.docker_rest_api import (
     ServiceSpec as DockerServiceSpec,
 )
 from pydantic import BaseModel, Field
@@ -10,6 +13,10 @@ class ServiceSpecifications(BaseModel):
     sidecar: Optional[DockerServiceSpec] = Field(
         default=None,
         description="schedule-time specifications for the service sidecar (follows Docker Service creation API, see https://docs.docker.com/engine/api/v1.25/#operation/ServiceCreate)",
+    )
+    service: Optional[DockerServiceTaskResources] = Field(
+        default=None,
+        description="schedule-time resources specifications for the service (follows Docker Service creation API (specifically only the Resources part), see https://docs.docker.com/engine/api/v1.41/#tag/Service/operation/ServiceCreate",
     )
 
 
