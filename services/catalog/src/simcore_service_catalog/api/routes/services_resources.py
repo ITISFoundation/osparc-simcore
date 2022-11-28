2f7d814cad8806b3a31c1a49ed3fe63cf03e6dd2
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-11-28 09:33:38 +0100

    first draft of merge for single services

--- services/catalog/src/simcore_service_catalog/api/routes/services_resources.py
+++ services/catalog/src/simcore_service_catalog/api/routes/services_resources.py
@@ -132,7 +132,7 @@ def _get_service_settings(
     logger.debug("received %s", f"{service_settings=}")
     return service_settings
 
-
+from models_library.generated_models.docker_rest_api import ServiceSpec
 @router.get(
     "/{service_key:path}/{service_version}/resources",
     response_model=ServiceResourcesDict,
@@ -184,6 +184,31 @@ async def get_service_resources(
             tuple(user_groups),
             allow_use_latest_service_version=True,
         )
+        if user_specific_service_specs:
+            def _merge_service_resources_with_user_specs(service_resources: ResourcesDict, user_specific_resources: ServiceSpec) -> ResourcesDict:
+                merged_resources = deepcopy(service_resources)
+                if user_specific_resources.TaskTemplate and user_specific_resources.TaskTemplate.Resources:
+                    user_specific_resources = user_specific_resources.TaskTemplate.Resources
+                    if user_specific_resources.Limits:
+                        if user_specific_resources.Limits.NanoCPUs:
+                            merged_resources["CPU"].limit = user_specific_resources.Limits.NanoCPUs / 10**9
+                        if user_specific_resources.Limits.MemoryBytes:
+                            merged_resources["RAM"].limit = user_specific_resources.Limits.MemoryBytes
+                    if user_specific_resources.Reservations:
+                        if user_specific_resources.Reservations.NanoCPUs:
+                            merged_resources["CPU"].reservation = user_specific_resources.Reservations.NanoCPUs / 10**9
+                        if user_specific_resources.Reservations.MemoryBytes:
+                            merged_resources["RAM"].reservation = user_specific_resources.Reservations.MemoryBytes
+                        if user_specific_resources.Reservations.GenericResources:
+                            for generic_resource in user_specific_resources.Reservations.GenericResources.__root__:
+                                if generic_resource.DiscreteResourceSpec and generic_resource.DiscreteResourceSpec.Kind and generic_resource.DiscreteResourceSpec.Value:
+                                    merged_resources[generic_resource.DiscreteResourceSpec.Kind] = ResourceValue(limit=generic_resource.DiscreteResourceSpec.Value, reservation=generic_resource.DiscreteResourceSpec.Value)
+                                if generic_resource.NamedResourceSpec and generic_resource.NamedResourceSpec.Kind and generic_resource.NamedResourceSpec.Value:
+                                    merged_resources[generic_resource.NamedResourceSpec.Kind] = ResourceValue(limit=generic_resource.NamedResourceSpec.Value, reservation=generic_resource.NamedResourceSpec.Value)
+
+
+                return merged_resources
+            service_resources = _merge_service_resources_with_user_specs(service_resources, user_specific_service_specs.service)
 
         # TODO: merge with user specific settings here
         return ServiceResourcesDictHelpers.create_from_single_service(
