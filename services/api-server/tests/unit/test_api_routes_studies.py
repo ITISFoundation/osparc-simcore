b29adcb1d4b482386dd7ff5c714ac4a10a36180b
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-11-30 15:20:18 +0100

    updates tests

--- services/api-server/tests/unit/test_api_routes_studies.py
+++ services/api-server/tests/unit/test_api_routes_studies.py
@@ -13,6 +13,9 @@ import pytest
 import respx
 from faker import Faker
 from fastapi import FastAPI, status
+from pytest_simcore.helpers.faker_webserver import (
+    PROJECTS_METADATA_PORTS_RESPOSE_BODY_DATA,
+)
 from respx import MockRouter
 from simcore_service_api_server.core.settings import ApplicationSettings
 from simcore_service_api_server.models.schemas.studies import StudyID
@@ -37,57 +40,10 @@ def study_id(faker: Faker) -> StudyID:
 
 
 @pytest.fixture
-def fake_study_ports(faker: Faker) -> list[dict[str, Any]]:
-    # same as the fake data used in services/web/server/tests/unit/with_dbs/02/test_projects_ports_handlers.py
-    return [
-        {
-            "key": "38a0d401-af4b-4ea7-ab4c-5005c712a546",
-            "kind": "input",
-            "content_schema": {
-                "description": "Parameter of type integer",
-                "title": "X",
-                "type": "integer",
-            },
-        },
-        {
-            "key": "fc48252a-9dbb-4e07-bf9a-7af65a18f612",
-            "kind": "input",
-            "content_schema": {
-                "description": "Parameter of type integer",
-                "title": "Z",
-                "type": "integer",
-            },
-        },
-        {
-            "key": "7bf0741f-bae4-410b-b662-fc34b47c27c9",
-            "kind": "input",
-            "content_schema": {
-                "description": "Parameter of type boolean",
-                "title": "on",
-                "type": "boolean",
-            },
-        },
-        {
-            "key": "09fd512e-0768-44ca-81fa-0cecab74ec1a",
-            "kind": "output",
-            "content_schema": {
-                "default": 0,
-                "description": "Captures integer values attached to it",
-                "title": "Random sleep interval_2",
-                "type": "integer",
-            },
-        },
-        {
-            "key": "76f607b4-8761-4f96-824d-cab670bc45f5",
-            "kind": "output",
-            "content_schema": {
-                "default": 0,
-                "description": "Captures integer values attached to it",
-                "title": "Random sleep interval",
-                "type": "integer",
-            },
-        },
-    ]
+def fake_study_ports() -> list[dict[str, Any]]:
+    # NOTE: Reuses fakes used to test web-server API responses of /projects/{project_id}/metadata/ports
+    # as reponses in this mock. SEE services/web/server/tests/unit/with_dbs/02/test_projects_ports_handlers.py
+    return deepcopy(PROJECTS_METADATA_PORTS_RESPOSE_BODY_DATA)
 
 
 @pytest.fixture
