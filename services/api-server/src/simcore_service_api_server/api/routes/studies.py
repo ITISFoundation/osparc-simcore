59005afda4e401fa54341a8c21ad1c9cdfc7e754
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-11-30 15:27:49 +0100

    adds entrypoints and defines handlers

--- services/api-server/src/simcore_service_api_server/api/routes/studies.py
+++ services/api-server/src/simcore_service_api_server/api/routes/studies.py
@@ -2,13 +2,35 @@
 
 
 """
+import logging
 
+from fastapi import APIRouter, Depends
+
+from ...core.settings import BasicSettings
+from ...models.schemas.studies import StudyPort
+from ..dependencies.webserver import AuthSession, get_webserver_session
+
+logger = logging.getLogger(__name__)
+router = APIRouter()
+settings = BasicSettings.create_from_envs()
 
 #
 # studies/{project_id}
 #
 
 
-def list_study_ports():
+@router.get(
+    "/{study_id}/ports",
+    response_model=list[StudyPort],
+    include_in_schema=settings.API_SERVER_DEV_FEATURES_ENABLED,
+)
+def list_study_ports(
+    client: AuthSession = Depends(get_webserver_session),
+):
+    """Lists metadata on ports of a given study
+
+    New in *version 0.5.0* (only with API_SERVER_DEV_FEATURES_ENABLED=1)
+    """
     # GET /projects/{project_id}/metadata/ports
-    pass
+
+    raise NotImplementedError()
--- services/api-server/src/simcore_service_api_server/models/schemas/studies.py
+++ services/api-server/src/simcore_service_api_server/models/schemas/studies.py
@@ -1,3 +1,15 @@
 from models_library.projects import ProjectID
+from models_library.projects_nodes_io import NodeID
+from pydantic import Field
+
+from .solvers import SolverPort
 
 StudyID = ProjectID
+
+
+class StudyPort(SolverPort):
+    key: NodeID = Field(
+        ...,
+        description="port identifier name",
+        title="Key name",
+    )
