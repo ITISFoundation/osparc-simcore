a1dfec7f1f3d56e31e5078c7fd6b6c16f57f3d85
Odei Maiz <maiz@itis.swiss>
2022-11-30 14:46:21 +0100

    not min css

--- services/static-webserver/client/source/class/osparc/wrapper/IntlTelInput.js
+++ services/static-webserver/client/source/class/osparc/wrapper/IntlTelInput.js
@@ -21,7 +21,7 @@
  * @asset(intl-tel-input/js/intlTelInput.min.js)
  * @asset(intl-tel-input/js/data.min.js)
  * @asset(intl-tel-input/js/utils.js)
- * @asset(intl-tel-input/css/intlTelInput.min.css)
+ * @asset(intl-tel-input/css/intlTelInput.css)
  * @asset(intl-tel-input/img/flags.png)
  * @asset(intl-tel-input/img/flags@2x.png)
  * @ignore(intlTelInput)
@@ -60,7 +60,7 @@ qx.Class.define("osparc.wrapper.IntlTelInput", {
       let intlTelInputPath = "intl-tel-input/js/intlTelInput.min.js";
       let dataPath = "intl-tel-input/js/data.min.js";
       let utilsPath = "intl-tel-input/js/utils.js";
-      let intlTelInputCss = "intl-tel-input/css/intlTelInput.min.css";
+      let intlTelInputCss = "intl-tel-input/css/intlTelInput.css";
       let intlTelInputCssUri = qx.util.ResourceManager.getInstance().toUri(intlTelInputCss);
       qx.module.Css.includeStylesheet(intlTelInputCssUri);
       let dynLoader = new qx.util.DynamicScriptLoader([
