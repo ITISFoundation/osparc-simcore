c1d30ff355d2ee8d82f92f92895a1d2f7f2f4744
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2022-12-01 08:21:34 +0100

    ✨ Is686/webserver api `0.12`: project ports metadata (#3621)

--- services/web/server/src/simcore_service_webserver/projects/projects_ports_handlers.py
+++ services/web/server/src/simcore_service_webserver/projects/projects_ports_handlers.py
@@ -5,14 +5,15 @@
 
 import functools
 import logging
-from typing import Any
+from typing import Any, Literal, Optional
 
 from aiohttp import web
 from models_library.projects import ProjectID
 from models_library.projects_nodes import Node, NodeID
 from models_library.users import UserID
 from models_library.utils.fastapi_encoders import jsonable_encoder
-from pydantic import BaseModel, Field, parse_obj_as
+from models_library.utils.services_io import JsonSchemaDict
+from pydantic import BaseModel, Extra, Field, parse_obj_as
 from servicelib.aiohttp.requests_validation import (
     parse_request_body_as,
     parse_request_path_parameters_as,
@@ -82,14 +83,30 @@ async def _get_validated_workbench_model(
     return workbench
 
 
-#
-# projects/*/inputs COLLECTION -------------------------
-#
-
 routes = web.RouteTableDef()
 
 
-class ProjectPort(BaseModel):
+class _InputSchema:
+    class Config:
+        allow_population_by_field_name = False
+        extra = Extra.forbid
+        allow_mutations = False
+
+
+class _OutputSchema:
+    class Config:
+        allow_population_by_field_name = True
+        extra = Extra.ignore
+        allow_mutations = False
+
+        #
+
+
+# projects/*/inputs COLLECTION -------------------------
+#
+
+
+class _ProjectIOBase(BaseModel):
     key: NodeID = Field(
         ...,
         description="Project port's unique identifer. Same as the UUID of the associated port node",
@@ -97,7 +114,15 @@ class ProjectPort(BaseModel):
     value: Any = Field(..., description="Value assigned to this i/o port")
 
 
-class ProjectPortGet(ProjectPort):
+class ProjectInputUpdate(_InputSchema, _ProjectIOBase):
+    ...
+
+
+class ProjectInputGet(_OutputSchema, _ProjectIOBase):
+    label: str
+
+
+class ProjectOutputGet(_OutputSchema, _ProjectIOBase):
     label: str
 
 
@@ -118,7 +143,7 @@ async def get_project_inputs(request: web.Request) -> web.Response:
 
     return _web_json_response_enveloped(
         data={
-            node_id: ProjectPortGet(
+            node_id: ProjectInputGet(
                 key=node_id, label=workbench[node_id].label, value=value
             )
             for node_id, value in inputs.items()
@@ -134,7 +159,7 @@ async def update_project_inputs(request: web.Request) -> web.Response:
     db: ProjectDBAPI = ProjectDBAPI.get_from_app_context(request.app)
     req_ctx = RequestContext.parse_obj(request)
     path_params = parse_request_path_parameters_as(ProjectPathParams, request)
-    inputs_updates = await parse_request_body_as(list[ProjectPort], request)
+    inputs_updates = await parse_request_body_as(list[ProjectInputUpdate], request)
 
     assert request.app  # nosec
 
@@ -168,7 +193,7 @@ async def update_project_inputs(request: web.Request) -> web.Response:
 
     return _web_json_response_enveloped(
         data={
-            node_id: ProjectPortGet(
+            node_id: ProjectInputGet(
                 key=node_id, label=workbench[node_id].label, value=value
             )
             for node_id, value in inputs.items()
@@ -198,9 +223,55 @@ async def get_project_outputs(request: web.Request) -> web.Response:
 
     return _web_json_response_enveloped(
         data={
-            node_id: ProjectPortGet(
+            node_id: ProjectOutputGet(
                 key=node_id, label=workbench[node_id].label, value=value
             )
             for node_id, value in outputs.items()
         }
+    )
+
+
+#
+# projects/*/metadata/ports sub-collection -------------------------
+#
+
+
+class ProjectMetadataPortGet(BaseModel):
+    key: NodeID = Field(
+        ...,
+        description="Project port's unique identifer. Same as the UUID of the associated port node",
+    )
+    kind: Literal["input", "output"]
+    content_schema: Optional[JsonSchemaDict] = Field(
+        None,
+        description="jsonschema for the port's value. SEE https://json-schema.org/understanding-json-schema/",
+    )
+
+
+@routes.get(
+    f"/{VTAG}/projects/{{project_id}}/metadata/ports",
+    name="list_project_metadata_ports",
+)
+@login_required
+@permission_required("project.read")
+@_handle_project_exceptions
+async def list_project_metadata_ports(request: web.Request) -> web.Response:
+    req_ctx = RequestContext.parse_obj(request)
+    path_params = parse_request_path_parameters_as(ProjectPathParams, request)
+
+    assert request.app  # nosec
+
+    workbench = await _get_validated_workbench_model(
+        app=request.app, project_id=path_params.project_id, user_id=req_ctx.user_id
+    )
+
+    return _web_json_response_enveloped(
+        data=[
+            ProjectMetadataPortGet(
+                key=port.node_id,
+                kind=port.kind,
+                content_schema=port.get_schema(),
+            )
+            for port in _ports.iter_project_ports(workbench)
+        ]
     )
--- services/web/server/tests/unit/with_dbs/02/test_projects_ports_handlers.py
+++ services/web/server/tests/unit/with_dbs/02/test_projects_ports_handlers.py
@@ -208,6 +208,66 @@ async def test_io_workflow(
 
     project_id = user_project["uuid"]
 
+    # list_project_metadata_ports
+    expected_url = client.app.router["list_project_metadata_ports"].url_for(
+        project_id=project_id
+    )
+    assert URL(f"/v0/projects/{project_id}/metadata/ports") == expected_url
+
+    resp = await client.get(f"/v0/projects/{project_id}/metadata/ports")
+    ports_meta, error = await assert_status(resp, expected_cls=expected)
+
+    if not error:
+        assert ports_meta == [
+            {
+                "key": "38a0d401-af4b-4ea7-ab4c-5005c712a546",
+                "kind": "input",
+                "content_schema": {
+                    "description": "Parameter of type integer",
+                    "title": "X",
+                    "type": "integer",
+                },
+            },
+            {
+                "key": "fc48252a-9dbb-4e07-bf9a-7af65a18f612",
+                "kind": "input",
+                "content_schema": {
+                    "description": "Parameter of type integer",
+                    "title": "Z",
+                    "type": "integer",
+                },
+            },
+            {
+                "key": "7bf0741f-bae4-410b-b662-fc34b47c27c9",
+                "kind": "input",
+                "content_schema": {
+                    "description": "Parameter of type boolean",
+                    "title": "on",
+                    "type": "boolean",
+                },
+            },
+            {
+                "key": "09fd512e-0768-44ca-81fa-0cecab74ec1a",
+                "kind": "output",
+                "content_schema": {
+                    "default": 0,
+                    "description": "Captures integer values attached to it",
+                    "title": "Random sleep interval_2",
+                    "type": "integer",
+                },
+            },
+            {
+                "key": "76f607b4-8761-4f96-824d-cab670bc45f5",
+                "kind": "output",
+                "content_schema": {
+                    "default": 0,
+                    "description": "Captures integer values attached to it",
+                    "title": "Random sleep interval",
+                    "type": "integer",
+                },
+            },
+        ]
+
     # get_project_inputs
     expected_url = client.app.router["get_project_inputs"].url_for(
         project_id=project_id
