719bf1c4acdde751f0824f7011846904c3106bb0
Sylvain <35365065+sanderegg@users.noreply.github.com>
2022-12-01 15:29:03 +0100

    Revert "⬆️ Maintenance: Upgrade python-socketio + flakyness (#3622)" (#3630)
    This reverts commit db03a28713d14aa64698ba6deffa339e95ed2803.

--- services/web/server/src/simcore_service_webserver/socketio/server.py
+++ services/web/server/src/simcore_service_webserver/socketio/server.py
@@ -1,6 +1,4 @@
-import asyncio
 import logging
-from typing import AsyncIterator
 
 from aiohttp import web
 from socketio import AsyncServer
@@ -15,40 +13,14 @@ def get_socket_server(app: web.Application) -> AsyncServer:
     return app[APP_CLIENT_SOCKET_SERVER_KEY]
 
 
-async def _socketio_server_cleanup_ctx(_app: web.Application) -> AsyncIterator[None]:
-    yield
-    # NOTE: this is ugly. It seems though that python-enginio does not
-    # cleanup its background tasks properly.
-    # https://github.com/miguelgrinberg/python-socketio/discussions/1092
-    current_tasks = asyncio.tasks.all_tasks()
-    cancelled_tasks = []
-    for task in current_tasks:
-        coro = task.get_coro()
-        if any(
-            coro_name in coro.__qualname__  # type: ignore
-            for coro_name in [
-                "AsyncServer._service_task",
-                "AsyncSocket.schedule_ping",
-            ]
-        ):
-            task.cancel()
-            cancelled_tasks.append(task)
-    await asyncio.gather(*cancelled_tasks, return_exceptions=True)
-
-
 def setup_socketio_server(app: web.Application):
     if app.get(APP_CLIENT_SOCKET_SERVER_KEY) is None:
         # SEE https://github.com/miguelgrinberg/python-socketio/blob/v4.6.1/docs/server.rst#aiohttp
         # TODO: ujson to speed up?
         # TODO: client_manager= to socketio.AsyncRedisManager/AsyncAioPikaManager for horizontal scaling (shared sessions)
-        sio = AsyncServer(
-            async_mode="aiohttp",
-            logger=log,  # type: ignore
-            engineio_logger=False,
-        )
+        sio = AsyncServer(async_mode="aiohttp", logger=log, engineio_logger=False)
         sio.attach(app)
 
         app[APP_CLIENT_SOCKET_SERVER_KEY] = sio
-        app.cleanup_ctx.append(_socketio_server_cleanup_ctx)
 
     return get_socket_server(app)
