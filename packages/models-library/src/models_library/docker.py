b18126fbb869c6cf4155092811bed8148f3e10d3
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-12-05 14:41:47 +0100

    refactor

--- packages/models-library/src/models_library/docker.py
+++ packages/models-library/src/models_library/docker.py
@@ -1,8 +1,11 @@
 import re
-from typing import Optional
+from typing import Final, Optional
 
 from pydantic import ConstrainedStr, constr
 
+_DOCKER_LABEL_KEY_REGEX: Final[re.Pattern] = re.compile(
+    r"^(?!(\.|\-|com.docker\.|io.docker\.|org.dockerproject\.))(?!.*(--|\.\.))[a-z0-9\.-]+$"
+)
 DOCKER_IMAGE_KEY_RE = r"[\w/-]+"
 DOCKER_IMAGE_VERSION_RE = r"[\w/.]+"
 
@@ -13,6 +16,4 @@ DockerImageVersion = constr(regex=DOCKER_IMAGE_VERSION_RE)
 class DockerLabelKey(ConstrainedStr):
     # NOTE: https://docs.docker.com/config/labels-custom-metadata/#key-format-recommendations
     # good practice: use reverse DNS notation
-    regex: Optional[re.Pattern[str]] = re.compile(
-        r"^(?!(\.|\-|com.docker\.|io.docker\.|org.dockerproject\.))(?!.*(--|\.\.))[a-z0-9\.-]+$"
-    )
+    regex: Optional[re.Pattern[str]] = _DOCKER_LABEL_KEY_REGEX
