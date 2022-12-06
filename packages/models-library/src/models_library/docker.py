5c424d03e1946ae60c5781eae667815b5351318c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-12-06 09:13:41 +0100

    @pcrespov review: add tests on regex

--- packages/models-library/src/models_library/docker.py
+++ packages/models-library/src/models_library/docker.py
@@ -1,13 +1,13 @@
 import re
-from typing import Final, Optional
+from typing import Optional
 
 from pydantic import ConstrainedStr, constr
 
-_DOCKER_LABEL_KEY_REGEX: Final[re.Pattern] = re.compile(
-    r"^(?!(\.|\-|com.docker\.|io.docker\.|org.dockerproject\.))(?!.*(--|\.\.))[a-z0-9\.-]+$"
+from .basic_regex import (
+    DOCKER_IMAGE_KEY_RE,
+    DOCKER_IMAGE_VERSION_RE,
+    DOCKER_LABEL_KEY_REGEX,
 )
-DOCKER_IMAGE_KEY_RE = r"[\w/-]+"
-DOCKER_IMAGE_VERSION_RE = r"[\w/.]+"
 
 DockerImageKey = constr(regex=DOCKER_IMAGE_KEY_RE)
 DockerImageVersion = constr(regex=DOCKER_IMAGE_VERSION_RE)
@@ -16,4 +16,4 @@ DockerImageVersion = constr(regex=DOCKER_IMAGE_VERSION_RE)
 class DockerLabelKey(ConstrainedStr):
     # NOTE: https://docs.docker.com/config/labels-custom-metadata/#key-format-recommendations
     # good practice: use reverse DNS notation
-    regex: Optional[re.Pattern[str]] = _DOCKER_LABEL_KEY_REGEX
+    regex: Optional[re.Pattern[str]] = DOCKER_LABEL_KEY_REGEX
