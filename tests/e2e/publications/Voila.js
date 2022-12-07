988675da863add59e2f59ec2550d890bb4b6f849
Odei Maiz <maiz@itis.swiss>
2022-12-07 16:43:58 +0100

    look for rendered_cells

--- tests/e2e/publications/Voila.js
+++ tests/e2e/publications/Voila.js
@@ -12,7 +12,7 @@ const {
 } = utils.parseCommandLineArgumentsTemplate(args);
 
 const anonURL = urlPrefix + templateUuid;
-const screenshotPrefix = "Voila_";
+const screenshotPrefix = "Voila";
 
 
 async function runTutorial () {
@@ -25,19 +25,29 @@ async function runTutorial () {
     const workbenchData = utils.extractWorkbenchData(studyData["data"]);
     console.log("Workbench Data:", workbenchData);
     const voilaIdViewer = workbenchData["nodeIds"][0];
-    await tutorial.waitForServices(workbenchData["studyId"], [voilaIdViewer], startTimeout);
+    await tutorial.waitForServices(
+      workbenchData["studyId"],
+      [voilaIdViewer],
+      startTimeout,
+      false
+    );
 
     await tutorial.waitFor(40000, 'Some time for starting the iframe');
     await utils.takeScreenshot(page, screenshotPrefix + 'service_started');
 
     const iframe = await tutorial.getIframe(voilaIdViewer);
-    // look for rendered_cells
-    const rendered_cells = iframe.querySelector('#rendered_cells');
-    console.log(rendered_cells);
     await utils.takeScreenshot(page, screenshotPrefix + 'iFrame');
+
+    // look for "rendered_cells" which is the root div of a Voila document
+    await utils.takeScreenshot(page, screenshotPrefix + 'iFrame');
+    await iframe.waitForSelector('#rendered_cells', {
+      timeout: 5000
+    })
+      .then(() => console.log("Voila started successfully"))
+      .catch(() => console.log("Voila page not found"));
   }
   catch(err) {
-    await tutorial.setTutorialFailed(true);
+    await tutorial.setTutorialFailed(true, false);
     console.log('Tutorial error: ' + err);
   }
   finally {
