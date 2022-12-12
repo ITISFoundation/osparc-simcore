4df7818f28491be42f6a101ab7289464471649f9
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-12-12 17:22:57 +0100

    ensure proper testing

--- services/autoscaling/tests/unit/test_models.py
+++ services/autoscaling/tests/unit/test_models.py
@@ -8,11 +8,47 @@ from typing import Any, Awaitable, Callable, Mapping
 import aiodocker
 import pytest
 from models_library.generated_models.docker_rest_api import Task
-from pydantic import ValidationError, parse_obj_as
-from simcore_service_autoscaling.models import SimcoreServiceDockerLabelKeys
+from pydantic import ByteSize, ValidationError, parse_obj_as
+from simcore_service_autoscaling.models import Resources, SimcoreServiceDockerLabelKeys
 
 
-async def test_task_ownership_from_task_with_missing_labels_raises(
+@pytest.mark.parametrize(
+    "a,b,a_greater_or_equal_than_b",
+    [
+        (
+            Resources(cpus=0.2, ram=ByteSize(0)),
+            Resources(cpus=0.1, ram=ByteSize(0)),
+            True,
+        ),
+        (
+            Resources(cpus=0.1, ram=ByteSize(0)),
+            Resources(cpus=0.1, ram=ByteSize(0)),
+            True,
+        ),
+        (
+            Resources(cpus=0.1, ram=ByteSize(1)),
+            Resources(cpus=0.1, ram=ByteSize(0)),
+            True,
+        ),
+        (
+            Resources(cpus=0.05, ram=ByteSize(1)),
+            Resources(cpus=0.1, ram=ByteSize(0)),
+            False,
+        ),
+        (
+            Resources(cpus=0.1, ram=ByteSize(0)),
+            Resources(cpus=0.1, ram=ByteSize(1)),
+            False,
+        ),
+    ],
+)
+def test_resources_ge_operator(
+    a: Resources, b: Resources, a_greater_or_equal_than_b: bool
+):
+    assert (a >= b) is a_greater_or_equal_than_b
+
+
+async def test_get_simcore_service_docker_labels_from_task_with_missing_labels_raises(
     async_docker_client: aiodocker.Docker,
     create_service: Callable[
         [dict[str, Any], dict[str, Any], str], Awaitable[Mapping[str, Any]]
@@ -40,7 +76,7 @@ def test_osparc_docker_label_keys_to_docker_labels(
     assert parse_obj_as(SimcoreServiceDockerLabelKeys, exported_dict)
 
 
-async def test_task_ownership_from_task(
+async def test_get_simcore_service_docker_labels(
     async_docker_client: aiodocker.Docker,
     create_service: Callable[
         [dict[str, Any], dict[str, str], str], Awaitable[Mapping[str, Any]]
