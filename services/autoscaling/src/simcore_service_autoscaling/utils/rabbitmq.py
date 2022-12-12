7fe8340a2910afc99b566010411889d1e5fcc5fc
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-12-12 19:19:32 +0100

    removed unused code

--- services/autoscaling/src/simcore_service_autoscaling/utils/rabbitmq.py
+++ services/autoscaling/src/simcore_service_autoscaling/utils/rabbitmq.py
@@ -1,40 +1,14 @@
 import logging
 
 from fastapi import FastAPI
-from models_library.generated_models.docker_rest_api import Node, Task
-from models_library.rabbitmq_messages import (
-    AutoscalingStatus,
-    LoggerRabbitMessage,
-    RabbitAutoscalingMessage,
-)
+from models_library.generated_models.docker_rest_api import Task
+from models_library.rabbitmq_messages import LoggerRabbitMessage
 from servicelib.logging_utils import log_catch
 
-from ..models import Resources, SimcoreServiceDockerLabelKeys
+from ..models import SimcoreServiceDockerLabelKeys
 from ..modules.rabbitmq import post_message
 
 logger = logging.getLogger(__name__)
-
-
-async def post_state_message(
-    app: FastAPI,
-    monitored_nodes: list[Node],
-    cluster_total_resources: Resources,
-    cluster_used_resources: Resources,
-    pending_tasks: list[Task],
-) -> None:
-    with log_catch(logger, reraise=False):
-        message = RabbitAutoscalingMessage(
-            origin=app.title,
-            number_monitored_nodes=len(monitored_nodes),
-            cluster_total_resources=cluster_total_resources.dict(),
-            cluster_used_resources=cluster_used_resources.dict(),
-            number_pending_tasks_without_resources=len(pending_tasks),
-            status=AutoscalingStatus.SCALING_UP
-            if pending_tasks
-            else AutoscalingStatus.IDLE,
-        )
-        logger.debug("autoscaling state: %s", message)
-        await post_message(app, message)
 
 
 async def post_log_message(app: FastAPI, task: Task, log: str, level: int):
