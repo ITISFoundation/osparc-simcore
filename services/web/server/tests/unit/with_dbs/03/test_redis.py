965253e3cdaecb6faf55186b822477ce598ecbcd
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-12-13 23:19:47 +0100

    test auto extend lock

--- services/web/server/tests/unit/with_dbs/03/test_redis.py
+++ services/web/server/tests/unit/with_dbs/03/test_redis.py
@@ -3,11 +3,14 @@
 # pylint:disable=redefined-outer-name
 
 import asyncio
+import datetime
 
 import pytest
 import redis
 import redis.asyncio as aioredis
 from faker import Faker
+from redis.asyncio.lock import Lock
+from servicelib.background_task import periodic_task
 
 
 async def test_aioredis(redis_client: aioredis.Redis, faker: Faker):
@@ -19,12 +22,29 @@ async def test_aioredis(redis_client: aioredis.Redis, faker: Faker):
 
 
 async def test_lock_acquisition(redis_client: aioredis.Redis, faker: Faker):
-    lock = redis_client.lock(faker.pystr())
+    lock_name = faker.pystr()
+    lock = redis_client.lock(lock_name)
     assert not await lock.locked()
 
     # Try to acquire the lock:
     lock_acquired = await lock.acquire(blocking=False)
-    assert lock_acquired, "Lock not acquired"
+    assert lock_acquired is True
+    assert await lock.locked() is True
+    assert await lock.owned() is True
+    with pytest.raises(redis.exceptions.LockError):
+        # a lock with no timeout cannot be reacquired
+        await lock.reacquire()
+    with pytest.raises(redis.exceptions.LockError):
+        # a lock with no timeout cannot be extended
+        await lock.extend(2)
+
+    # try to acquire the lock a second time
+    same_lock = redis_client.lock(lock_name)
+    assert await same_lock.locked() is True
+    assert await same_lock.owned() is False
+    assert await same_lock.acquire(blocking=False) is False
+
+    # now release the lock
     await lock.release()
     assert not await lock.locked()
     assert not await lock.owned()
@@ -38,9 +58,14 @@ async def test_lock_context_manager(redis_client: aioredis.Redis, faker: Faker):
     async with lock:
         assert await lock.locked()
         assert await lock.owned()
-        # a lock with no timeout cannot be extended
         with pytest.raises(redis.exceptions.LockError):
+            # a lock with no timeout cannot be reacquired
+            await lock.reacquire()
+
+        with pytest.raises(redis.exceptions.LockError):
+            # a lock with no timeout cannot be extended
             await lock.extend(2)
+
         # try to acquire the lock a second time
         same_lock = redis_client.lock(lock_name, blocking_timeout=1)
         assert await same_lock.locked()
@@ -52,8 +77,15 @@ async def test_lock_context_manager(redis_client: aioredis.Redis, faker: Faker):
     assert not await lock.locked()
 
 
-async def test_lock_with_ttl(redis_client: aioredis.Redis, faker: Faker):
-    ttl_lock = redis_client.lock(faker.pystr(), timeout=2, blocking_timeout=1)
+@pytest.fixture
+def lock_timeout() -> datetime.timedelta:
+    return datetime.timedelta(seconds=2)
+
+
+async def test_lock_with_ttl(
+    redis_client: aioredis.Redis, faker: Faker, lock_timeout: datetime.timedelta
+):
+    ttl_lock = redis_client.lock(faker.pystr(), timeout=lock_timeout.total_seconds())
     assert not await ttl_lock.locked()
 
     with pytest.raises(redis.exceptions.LockNotOwnedError):
@@ -61,5 +93,27 @@ async def test_lock_with_ttl(redis_client: aioredis.Redis, faker: Faker):
         async with ttl_lock:
             assert await ttl_lock.locked()
             assert await ttl_lock.owned()
-            await asyncio.sleep(3)
+            await asyncio.sleep(2 * lock_timeout.total_seconds())
             assert not await ttl_lock.locked()
+
+
+async def test_lock_with_auto_extent(
+    redis_client: aioredis.Redis, faker: Faker, lock_timeout: datetime.timedelta
+):
+    ttl_lock = redis_client.lock(faker.pystr(), timeout=lock_timeout.total_seconds())
+    assert not await ttl_lock.locked()
+
+    async def _auto_extend_lock(lock: Lock) -> None:
+        assert await lock.reacquire() is True
+
+    async with ttl_lock, periodic_task(
+        _auto_extend_lock,
+        interval=0.6 * lock_timeout,
+        task_name=f"{ttl_lock.name}_auto_extend",
+        lock=ttl_lock,
+    ):
+        assert await ttl_lock.locked()
+        assert await ttl_lock.owned()
+        await asyncio.sleep(5 * lock_timeout.total_seconds())
+        assert await ttl_lock.locked()
+        assert await ttl_lock.owned()
