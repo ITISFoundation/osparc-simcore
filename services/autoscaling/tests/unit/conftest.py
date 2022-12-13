bb5bcd86f43bb1040a5534042a13cd896783d96b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-12-13 21:16:05 +0100

    @pcrespov review: use decorator

--- services/autoscaling/tests/unit/conftest.py
+++ services/autoscaling/tests/unit/conftest.py
@@ -45,7 +45,6 @@ from simcore_service_autoscaling.models import SimcoreServiceDockerLabelKeys
 from simcore_service_autoscaling.modules.docker import AutoscalingDocker
 from simcore_service_autoscaling.modules.ec2 import AutoscalingEC2, EC2InstanceData
 from tenacity import retry
-from tenacity._asyncio import AsyncRetrying
 from tenacity.retry import retry_if_exception_type
 from tenacity.stop import stop_after_delay
 from tenacity.wait import wait_fixed
@@ -347,46 +346,43 @@ async def create_service(
     await asyncio.sleep(0)
 
 
+SUCCESS_STABLE_TIME_S: Final[float] = 3
+WAIT_TIME: Final[float] = 0.5
+
+
+@retry(
+    retry=retry_if_exception_type(AssertionError),
+    reraise=True,
+    wait=wait_fixed(WAIT_TIME),
+    stop=stop_after_delay(10 * SUCCESS_STABLE_TIME_S),
+)
 async def assert_for_service_state(
     async_docker_client: aiodocker.Docker, service: Service, expected_states: list[str]
 ) -> None:
-    SUCCESS_STABLE_TIME_S: Final[float] = 3
-    WAIT_TIME: Final[float] = 0.5
     number_of_success = 0
-    async for attempt in AsyncRetrying(
-        retry=retry_if_exception_type(AssertionError),
-        reraise=True,
-        wait=wait_fixed(WAIT_TIME),
-        stop=stop_after_delay(10 * SUCCESS_STABLE_TIME_S),
-    ):
-        with attempt:
-            print(
-                f"--> waiting for service {service.ID} to become {expected_states}..."
-            )
-            services = await async_docker_client.services.list(
-                filters={"id": service.ID}
-            )
-            assert services, f"no service with {service.ID}!"
-            assert len(services) == 1
-            found_service = services[0]
+    print(f"--> waiting for service {service.ID} to become {expected_states}...")
+    services = await async_docker_client.services.list(filters={"id": service.ID})
+    assert services, f"no service with {service.ID}!"
+    assert len(services) == 1
+    found_service = services[0]
 
-            tasks = await async_docker_client.tasks.list(
-                filters={"service": found_service["Spec"]["Name"]}
-            )
-            assert tasks, f"no tasks available for {found_service['Spec']['Name']}"
-            assert len(tasks) == 1
-            service_task = tasks[0]
-            assert (
-                service_task["Status"]["State"] in expected_states
-            ), f"service {found_service['Spec']['Name']}'s task is {service_task['Status']['State']}"
-            print(
-                f"<-- service {found_service['Spec']['Name']} is now {service_task['Status']['State']} {'.'*number_of_success}"
-            )
-            number_of_success += 1
-            assert (number_of_success * WAIT_TIME) >= SUCCESS_STABLE_TIME_S
-            print(
-                f"<-- service {found_service['Spec']['Name']} is now {service_task['Status']['State']} after {SUCCESS_STABLE_TIME_S} seconds"
-            )
+    tasks = await async_docker_client.tasks.list(
+        filters={"service": found_service["Spec"]["Name"]}
+    )
+    assert tasks, f"no tasks available for {found_service['Spec']['Name']}"
+    assert len(tasks) == 1
+    service_task = tasks[0]
+    assert (
+        service_task["Status"]["State"] in expected_states
+    ), f"service {found_service['Spec']['Name']}'s task is {service_task['Status']['State']}"
+    print(
+        f"<-- service {found_service['Spec']['Name']} is now {service_task['Status']['State']} {'.'*number_of_success}"
+    )
+    number_of_success += 1
+    assert (number_of_success * WAIT_TIME) >= SUCCESS_STABLE_TIME_S
+    print(
+        f"<-- service {found_service['Spec']['Name']} is now {service_task['Status']['State']} after {SUCCESS_STABLE_TIME_S} seconds"
+    )
 
 
 @pytest.fixture(scope="module")
