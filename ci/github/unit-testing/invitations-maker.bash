9b5d0c860fabd56504ff0b2e65fdf99004276f71
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-12-19 21:20:04 +0100

    CI

---
+++ ci/github/unit-testing/invitations-maker.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -o errexit  # abort on nonzero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
+IFS=$'\n\t'
+
+install() {
+  bash ci/helpers/ensure_python_pip.bash
+  make devenv
+  # shellcheck source=/dev/null
+  source .venv/bin/activate
+  pushd packages/invitations-maker
+  make install-ci
+  popd
+  .venv/bin/pip list --verbose
+}
+
+test() {
+  # shellcheck source=/dev/null
+  source .venv/bin/activate
+  pushd packages/invitations-maker
+  make tests-ci
+  popd
+}
+
+typecheck() {
+  pushd packages/invitations-maker
+  make mypy
+  popd
+}
+
+# Check if the function exists (bash specific)
+if declare -f "$1" >/dev/null; then
+  # call arguments verbatim
+  "$@"
+else
+  # Show a helpful error
+  echo "'$1' is not a known function name" >&2
+  exit 1
+fi
