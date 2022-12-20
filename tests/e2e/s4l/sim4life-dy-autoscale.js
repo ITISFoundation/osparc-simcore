6f597c0810954ee28cfc2b83d7588227bbd10292
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-12-20 17:17:45 +0100

    initial test

---
+++ tests/e2e/s4l/sim4life-dy-autoscale.js
@@ -0,0 +1,57 @@
+// node sim4life.js [url] [user] [password] [timeout] [--demo]
+
+const utils = require('../utils/utils');
+const tutorialBase = require('../tutorials/tutorialBase');
+
+const args = process.argv.slice(2);
+const {
+  url,
+  user,
+  pass,
+  newUser,
+  startTimeout,
+  enableDemoMode
+} = utils.parseCommandLineArguments(args)
+
+const serviceName = "sim4life-dy";
+
+async function runTutorial() {
+  const tutorial = new tutorialBase.TutorialBase(url, serviceName, user, pass, newUser, enableDemoMode);
+  let studyId;
+  try {
+    await tutorial.start();
+
+    // start sim4life-dy service
+    const studyData = await tutorial.openService(1000);
+    studyId = studyData["data"]["uuid"];
+
+    const workbenchData = utils.extractWorkbenchData(studyData["data"]);
+    console.log(workbenchData);
+    const s4lNodeId = workbenchData["nodeIds"][0];
+    await tutorial.waitForServices(
+      workbenchData["studyId"],
+      [s4lNodeId],
+      startTimeout,
+      false
+    );
+
+    // await tutorial.testS4L(s4lNodeId);
+  }
+  catch (err) {
+    await tutorial.setTutorialFailed(true);
+    console.log('Tutorial error: ' + err);
+  }
+  finally {
+    await tutorial.leave(studyId);
+  }
+
+  if (tutorial.getTutorialFailed()) {
+    throw "Tutorial Failed";
+  }
+}
+
+runTutorial()
+  .catch(error => {
+    console.log('Puppeteer error: ' + error);
+    process.exit(1);
+  });
