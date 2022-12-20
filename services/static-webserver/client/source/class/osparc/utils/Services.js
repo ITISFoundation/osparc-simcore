311abe46eb2e711093c8bb37a1ead73ade885543
Odei Maiz <maiz@itis.swiss>
2022-12-20 18:49:46 +0100

    [skip ci] minor

--- services/static-webserver/client/source/class/osparc/utils/Services.js
+++ services/static-webserver/client/source/class/osparc/utils/Services.js
@@ -191,10 +191,10 @@ qx.Class.define("osparc.utils.Services", {
       return null;
     },
 
-    canIWrite: function(serviceData) {
+    canIWrite: function(serviceAccessRights) {
       const orgIDs = osparc.auth.Data.getInstance().getOrgIds();
       orgIDs.push(osparc.auth.Data.getInstance().getGroupId());
-      return osparc.component.permissions.Service.canGroupsWrite(serviceData["accessRights"], orgIDs);
+      return osparc.component.permissions.Service.canGroupsWrite(serviceAccessRights, orgIDs);
     },
 
     /**
