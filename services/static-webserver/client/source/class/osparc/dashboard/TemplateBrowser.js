b7b180ccc52bd3d245aaa5f7640d717895f2c9e5
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2022-12-21 14:26:05 +0100

    🐛 Bugfixes Lock on Study card & Update card (#3720)

--- services/static-webserver/client/source/class/osparc/dashboard/TemplateBrowser.js
+++ services/static-webserver/client/source/class/osparc/dashboard/TemplateBrowser.js
@@ -50,8 +50,7 @@ qx.Class.define("osparc.dashboard.TemplateBrowser", {
     __attachEventHandlers: function() {
       const socket = osparc.wrapper.WebSocket.getInstance();
       const slotName = "projectStateUpdated";
-      socket.removeSlot(slotName);
-      socket.on(slotName, function(jsonString) {
+      socket.on(slotName, jsonString => {
         const data = JSON.parse(jsonString);
         if (data) {
           const templateId = data["project_uuid"];
