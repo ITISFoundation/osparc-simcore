a5a54f278d438350d7fd209042d8df7e1faaf886
Odei Maiz <maiz@itis.swiss>
2023-01-06 17:53:12 +0100

    getProductEveryone on share icon

--- services/static-webserver/client/source/class/osparc/dashboard/CardBase.js
+++ services/static-webserver/client/source/class/osparc/dashboard/CardBase.js
@@ -553,6 +553,107 @@ qx.Class.define("osparc.dashboard.CardBase", {
       moreOpts.openUpdateServices();
     },
 
+    // groups -> [orgMembs, orgs, [productEveryone], [everyone]];
+    _evaluateShareIcon: function(shareIcon, accessRights) {
+      shareIcon.addListener("tap", e => {
+        e.stopPropagation();
+        this._openAccessRights();
+      }, this);
+      shareIcon.addListener("pointerdown", e => e.stopPropagation());
+
+      const store = osparc.store.Store.getInstance();
+      Promise.all([
+        store.getGroupEveryone(),
+        store.getProductEveryone(),
+        store.getVisibleMembers(),
+        store.getGroupsOrganizations()
+      ])
+        .then(values => {
+          const everyone = values[0] ? [values[0]] : [];
+          const productEveryone = values[1] ? [values[1]] : [];
+          const orgMembs = [];
+          const orgMembers = values[2];
+          for (const gid of Object.keys(orgMembers)) {
+            orgMembs.push(orgMembers[gid]);
+          }
+          const orgs = values.length === 4 ? values[3] : [];
+          const groups = [orgMembs, orgs, productEveryone, everyone];
+          this.__setIconAndTooltip(shareIcon, accessRights, groups);
+        });
+
+      if (this.isResourceType("study")) {
+        this._setStudyPermissions(accessRights);
+      }
+    },
+
+    // groups -> [orgMembs, orgs, [productEveryone], [everyone]];
+    __setIconAndTooltip: function(shareIcon, accessRights, groups) {
+      if (osparc.data.model.Study.canIWrite(accessRights)) {
+        shareIcon.set({
+          source: osparc.dashboard.CardBase.SHARE_ICON,
+          toolTipText: this.tr("Share")
+        });
+      }
+      let sharedGrps = [];
+      const myGroupId = osparc.auth.Data.getInstance().getGroupId();
+      for (let i=0; i<groups.length; i++) {
+        if (groups[i].length === 0) {
+          // user has no read access to the productEveryone
+          continue;
+        }
+        const sharedGrp = [];
+        const gids = Object.keys(accessRights);
+        for (let j=0; j<gids.length; j++) {
+          const gid = parseInt(gids[j]);
+          if (this.isResourceType("study") && (gid === myGroupId)) {
+            continue;
+          }
+          const grp = groups[i].find(group => group["gid"] === gid);
+          if (grp) {
+            sharedGrp.push(grp);
+          }
+        }
+        if (sharedGrp.length === 0) {
+          continue;
+        } else {
+          sharedGrps = sharedGrps.concat(sharedGrp);
+        }
+        switch (i) {
+          case 0:
+            shareIcon.setSource(osparc.dashboard.CardBase.SHARED_USER);
+            break;
+          case 1:
+            shareIcon.setSource(osparc.dashboard.CardBase.SHARED_ORGS);
+            break;
+          case 2:
+          case 3:
+            shareIcon.setSource(osparc.dashboard.CardBase.SHARED_ALL);
+            break;
+        }
+      }
+
+      // tooltip
+      if (sharedGrps.length === 0) {
+        return;
+      }
+      const sharedGrpLabels = [];
+      const maxItems = 6;
+      for (let i=0; i<sharedGrps.length; i++) {
+        if (i > maxItems) {
+          sharedGrpLabels.push("...");
+          break;
+        }
+        const sharedGrpLabel = sharedGrps[i]["label"];
+        if (!sharedGrpLabels.includes(sharedGrpLabel)) {
+          sharedGrpLabels.push(sharedGrpLabel);
+        }
+      }
+      const hintText = sharedGrpLabels.join("<br>");
+      const hint = new osparc.ui.hint.Hint(shareIcon, hintText);
+      shareIcon.addListener("mouseover", () => hint.show(), this);
+      shareIcon.addListener("mouseout", () => hint.exclude(), this);
+    },
+
     /**
      * Event handler for the pointer over event.
      */
