93b8c57095b088fdae39aedc0a45557da3cd3015
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-01-10 14:44:03 +0100

    adds tests

---
+++ services/invitations/tests/unit/test_cli.py
@@ -0,0 +1,78 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+# pylint: disable=too-many-arguments
+
+import json
+import os
+
+from faker import Faker
+from pytest import MonkeyPatch
+from pytest_simcore.helpers.utils_envs import load_dotenv, setenvs_from_dict
+from simcore_service_invitations._meta import API_VERSION
+from simcore_service_invitations.cli import app
+from simcore_service_invitations.invitations import InvitationData
+from simcore_service_invitations.settings import WebApplicationSettings
+from typer.testing import CliRunner
+
+
+def test_cli_help_and_version(cli_runner: CliRunner):
+    # invitations-maker --help
+    result = cli_runner.invoke(app, "--help")
+    assert result.exit_code == os.EX_OK, result.output
+
+    result = cli_runner.invoke(app, "--version")
+    assert result.exit_code == os.EX_OK, result.output
+    assert result.stdout.strip() == API_VERSION
+
+
+def test_invite_user_and_check_invitation(
+    cli_runner: CliRunner, faker: Faker, invitation_data: InvitationData
+):
+    # invitations-maker generate-key
+    result = cli_runner.invoke(app, "generate-key")
+    assert result.exit_code == os.EX_OK, result.output
+
+    # export INVITATIONS_MAKER_SECRET_KEY=$(invitations-maker generate-key)
+    environs = dict(
+        INVITATIONS_MAKER_SECRET_KEY=result.stdout.strip(),
+        INVITATIONS_MAKER_OSPARC_URL=faker.url(),
+    )
+
+    # invitations-maker invite guest@email.com --issuer=me --trial-account-days=3
+    trial_account = ""
+    if invitation_data.trial_account_days:
+        trial_account = f"--trial-account-days={invitation_data.trial_account_days}"
+
+    result = cli_runner.invoke(
+        app,
+        f"invite {invitation_data.guest} --issuer={invitation_data.issuer} {trial_account}",
+        env=environs,
+    )
+    assert result.exit_code == os.EX_OK, result.output
+
+    invitation_url = result.stdout
+    print(invitation_url)
+
+    # invitations-maker check https://foo#invitation=123
+    result = cli_runner.invoke(
+        app,
+        f"check {invitation_url}",
+        env=environs,
+    )
+    assert result.exit_code == os.EX_OK, result.output
+    assert invitation_data.dict() == json.loads(result.stdout)
+
+
+def test_generate_dotenv(cli_runner: CliRunner, monkeypatch: MonkeyPatch):
+    # invitations-maker --generate-dotenv
+    result = cli_runner.invoke(app, "generate-dotenv --auto-password")
+    assert result.exit_code == os.EX_OK, result.output
+
+    environs = load_dotenv(result.stdout)
+
+    envs = setenvs_from_dict(monkeypatch, environs)
+    settings_from_obj = WebApplicationSettings.parse_obj(envs)
+    settings_from_envs = WebApplicationSettings()
+
+    assert settings_from_envs == settings_from_obj
---
+++ services/invitations/tests/unit/test_settings.py
@@ -0,0 +1,30 @@
+from pytest import MonkeyPatch
+
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+# pylint: disable=too-many-arguments
+from pytest_simcore.helpers.typing_env import EnvVarsDict
+from pytest_simcore.helpers.utils_envs import setenvs_from_dict
+from simcore_service_invitations.settings import (
+    DesktopApplicationSettings,
+    WebApplicationSettings,
+)
+
+
+def test_valid_cli_application_settings(monkeypatch: MonkeyPatch, secret_key: str):
+    setenvs_from_dict(
+        monkeypatch,
+        {
+            "INVITATIONS_MAKER_SECRET_KEY": secret_key,
+            "INVITATIONS_MAKER_OSPARC_URL": "https://myosparc.org",
+        },
+    )
+
+    settings = DesktopApplicationSettings()
+    assert settings
+
+
+def test_valid_web_application_settings(app_environment: EnvVarsDict):
+    settings = WebApplicationSettings()
+    assert settings
