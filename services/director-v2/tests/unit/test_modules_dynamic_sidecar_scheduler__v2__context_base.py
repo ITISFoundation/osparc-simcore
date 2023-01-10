4ec7b909246c33902a9d029e229e8b48b5405522
Andrei Neagu <neagu@itis.swiss>
2023-01-10 10:40:05 +0100

    no async required

--- services/director-v2/tests/unit/test_modules_dynamic_sidecar_scheduler__v2__context_base.py
+++ services/director-v2/tests/unit/test_modules_dynamic_sidecar_scheduler__v2__context_base.py
@@ -3,7 +3,7 @@ from simcore_service_director_v2.modules.dynamic_sidecar.scheduler._v2._context_
 )
 
 
-async def test_reserved_context_keys():
+def test_reserved_context_keys():
     user_defined_keys: set[str] = set()
     for key_name, value in ReservedContextKeys.__dict__.items():
         if isinstance(value, str) and not key_name.startswith("_"):
