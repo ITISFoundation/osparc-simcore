5a73a59e070723741617ee9dc848d37497ab545f
Andrei Neagu <neagu@itis.swiss>
2023-01-11 13:24:20 +0100

    refactor error bases

--- services/director-v2/src/simcore_service_director_v2/modules/dynamic_sidecar/scheduler/_v2/_errors.py
+++ services/director-v2/src/simcore_service_director_v2/modules/dynamic_sidecar/scheduler/_v2/_errors.py
@@ -1,11 +1,11 @@
 from pydantic.errors import PydanticErrorMixin
 
 
-class BaseV2SchedulerException(PydanticErrorMixin, RuntimeError):
+class BaseSchedulerException(PydanticErrorMixin, RuntimeError):
     """base for all exceptions here"""
 
 
-class BaseContextException(BaseV2SchedulerException):
+class BaseContextException(BaseSchedulerException):
     """use as base for all context related errors"""
 
 
@@ -39,26 +39,30 @@ class NotAllowedContextKeyError(BaseContextException):
     )
 
 
-class BaseStepException(BaseV2SchedulerException):
-    """use as base for all context related errors"""
-
-
-class UnexpectedStepReturnTypeError(BaseStepException):
+class UnexpectedStepReturnTypeError(BaseSchedulerException):
     code = "dynamic_sidecar.scheduler.v2.unexpected_step_return_type"
     msg_template = "Step should always return `dict[str, Any]`, returning: {type}"
 
 
-class WorkflowAlreadyRunningException(BaseStepException):
+class BaseWorkflowException(BaseSchedulerException):
+    """use as base for all workflow related errors"""
+
+
+class WorkflowAlreadyRunningException(BaseWorkflowException):
     code = "dynamic_sidecar.scheduler.v2.workflow_already_running"
     msg_template = "Another workflow named '{workflow_name}' is already running"
 
 
-class WorkflowNotFoundException(BaseStepException):
+class WorkflowNotFoundException(BaseWorkflowException):
     code = "dynamic_sidecar.scheduler.v2.workflow_not_found"
     msg_template = "Workflow '{workflow_name}' not found"
 
 
-class ActionNotRegisteredException(BaseStepException):
+class BaseActionException(BaseSchedulerException):
+    """use as base for all action related errors"""
+
+
+class ActionNotRegisteredException(BaseActionException):
     code = "dynamic_sidecar.scheduler.v2.action_not_registered"
     msg_template = (
         "Trying to start action '{action_name}' but these are the only"
@@ -66,7 +70,7 @@ class ActionNotRegisteredException(BaseStepException):
     )
 
 
-class OnErrorActionNotInWorkflowException(BaseStepException):
+class OnErrorActionNotInWorkflowException(BaseActionException):
     code = "dynamic_sidecar.scheduler.v2.on_error_action_not_in_workflow"
     msg_template = (
         "Action '{action_name}' defines an on_error_action '{on_error_action}'"
@@ -74,7 +78,7 @@ class OnErrorActionNotInWorkflowException(BaseStepException):
     )
 
 
-class NextActionNotInWorkflowException(BaseStepException):
+class NextActionNotInWorkflowException(BaseActionException):
     code = "dynamic_sidecar.scheduler.v2.next_action_not_in_workflow"
     msg_template = (
         "Action '{action_name}' defines an next_action '{next_action}'"
