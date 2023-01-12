d416d726a755390e64a72bb5004acdc5fcce9b14
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-01-12 15:08:17 +0100

    @GitHK review: naming

--- services/invitations/src/simcore_service_invitations/api/_dependencies.py
+++ services/invitations/src/simcore_service_invitations/api/_dependencies.py
@@ -34,19 +34,19 @@ def get_current_username(
     credentials: HTTPBasicCredentials = Depends(get_basic_credentials),
     settings: WebApplicationSettings = Depends(get_settings),
 ) -> str:
+    def _is_valid(current: str, expected: str) -> bool:
+        return secrets.compare_digest(current.encode("utf8"), expected.encode("utf8"))
 
-    # username
-    current: bytes = credentials.username.encode("utf8")
-    expected: bytes = settings.INVITATIONS_USERNAME.encode("utf8")
-    is_correct_username = secrets.compare_digest(current, expected)
-
-    # password
-    current = credentials.password.encode("utf8")
-    expected = settings.INVITATIONS_PASSWORD.get_secret_value().encode("utf8")
-    is_correct_password = secrets.compare_digest(current, expected)
-
-    # check
-    if not (is_correct_username and is_correct_password):
+    if not (
+        _is_valid(
+            credentials.username,
+            expected=settings.INVITATIONS_USERNAME,
+        )
+        and _is_valid(
+            credentials.password,
+            expected=settings.INVITATIONS_PASSWORD,
+        )
+    ):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect email or password",
