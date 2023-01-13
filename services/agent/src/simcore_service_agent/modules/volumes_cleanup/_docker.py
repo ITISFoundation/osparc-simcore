62cd501be94443552a056ee4bd9b33f714fc3a82
Andrei Neagu <neagu@itis.swiss>
2023-01-13 11:56:26 +0100

    fix broken code

--- services/agent/src/simcore_service_agent/modules/volumes_cleanup/_docker.py
+++ services/agent/src/simcore_service_agent/modules/volumes_cleanup/_docker.py
@@ -18,10 +18,10 @@ async def get_dyv_volumes(docker: Docker, target_swarm_stack_name: str) -> list[
     dyv_volumes: deque[dict] = deque()
     volumes = await docker.volumes.list()
     for volume in volumes["Volumes"]:
+        volume_labels = volume.get("Labels", {}) or {}
         if (
             volume["Name"].startswith(f"{PREFIX_DYNAMIC_SIDECAR_VOLUMES}_")
-            and volume.get("Labels", {}).get("swarm_stack_name")
-            == target_swarm_stack_name
+            and volume_labels.get("swarm_stack_name") == target_swarm_stack_name
         ):
             dyv_volumes.append(volume)
     return list(dyv_volumes)
