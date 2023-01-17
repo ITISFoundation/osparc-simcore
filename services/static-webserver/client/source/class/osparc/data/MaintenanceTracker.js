0277468d147f32f56b0667dbaaa8d5802c4b3089
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2023-01-17 17:38:08 +0100

    Update MaintenanceTracker.js

--- services/static-webserver/client/source/class/osparc/data/MaintenanceTracker.js
+++ services/static-webserver/client/source/class/osparc/data/MaintenanceTracker.js
@@ -40,7 +40,7 @@ qx.Class.define("osparc.data.MaintenanceTracker", {
   },
 
   statics: {
-    CHECK_INTERVAL: 5*1000, // Check hourly
+    CHECK_INTERVAL: 60*60*1000, // Check hourly
     WARN_IN_ADVANCE: 20*60*1000 // Show Flash Message 20' in advance
   },
 
