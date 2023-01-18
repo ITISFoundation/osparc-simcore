598495d4a06bd75d7675b28f8c2c9fdb1191b18e
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-01-18 16:33:58 +0100

    new access decorators

--- services/web/server/src/simcore_service_webserver/login/_constants.py
+++ services/web/server/src/simcore_service_webserver/login/_constants.py
@@ -37,6 +37,9 @@ MSG_UNAUTHORIZED_LOGIN_2FA: Final[
 MSG_UNAUTHORIZED_REGISTER_PHONE: Final[
     str
 ] = "Unauthorized: you cannot register the phone anymore, please restart."
+MSG_UNAUTHORIZED_PHONE_CONFIRMATION: Final[
+    str
+] = "Unauthorized: you cannot submit the code anymore, please restart."
 MSG_UNKNOWN_EMAIL: Final[str] = "This email is not registered"
 MSG_USER_BANNED: Final[
     str
@@ -58,3 +61,8 @@ CODE_2FA_CODE_REQUIRED = "SMS_CODE_REQUIRED"
 # App keys for login plugin
 # Naming convention: APP_LOGIN_...KEY
 APP_LOGIN_SETTINGS_PER_PRODUCT_KEY = f"{__name__}.LOGIN_SETTINGS_PER_PRODUCT"
+
+
+#
+MAX_RESEND_CODE = 5
+MAX_CODE_TRIALS = MAX_RESEND_CODE + 1
--- services/web/server/src/simcore_service_webserver/session_access.py
+++ services/web/server/src/simcore_service_webserver/session_access.py
@@ -3,6 +3,7 @@ import time
 from typing import Optional, TypedDict
 
 from aiohttp import web
+from pydantic import PositiveInt, validate_arguments
 from servicelib.aiohttp.typing_extension import Handler
 
 from .session import get_session
@@ -11,7 +12,7 @@ from .session import get_session
 # NOTE: dataclass cannot be serialized in the cookie
 class RouteTrace(TypedDict):
     route_name: str
-    timestamp: int
+    timestamp: float
 
 
 # session keys
@@ -41,7 +42,7 @@ def session_access_trace(route_name: str):
 def session_access_constraint(
     allow_access_after: list[str],
     max_number_of_access: int = 1,
-    unauthorized_reason: str = None,
+    unauthorized_reason: Optional[str] = None,
 ):
     """
     allow_access_after: grants access if any of the listed names was accessed before
@@ -85,6 +86,82 @@ def session_access_constraint(
 
             response = await handler(request)
             return response
+
+        return _wrapper
+
+    return _decorator
+
+
+SESSION_GRANTED_ACCESS_TOKENS_KEY = f"{__name__}.SESSION_GRANTED_ACCESS_TOKENS_KEY"
+
+
+@validate_arguments
+def on_success_grant_session_access_to(
+    name: str,
+    max_access_count: PositiveInt = 1,
+):
+    """Creates access token if handle suceeds with 2XX"""
+
+    def _decorator(handler: Handler):
+        @functools.wraps(handler)
+        async def _wrapper(request: web.Request):
+
+            response = await handler(request)
+
+            # success 2XX ---
+            session = await get_session(request)
+            # TODO: what if raises non-error?
+            routes_granted_acess = session.setdefault(
+                SESSION_GRANTED_ACCESS_TOKENS_KEY, {}
+            )
+            # NOTE: does NOT add up access counts but resets to max_access_count
+            routes_granted_acess[name] = max_access_count
+            # ----
+
+            return response
+
+        return _wrapper
+
+    return _decorator
+
+
+def session_access_required(
+    name: str,
+    unauthorized_reason: Optional[str] = None,
+    one_time_access: bool = True,
+):
+    def _decorator(handler: Handler):
+        @functools.wraps(handler)
+        async def _wrapper(request: web.Request):
+
+            session = await get_session(request)
+            granted_access_tokens = session.get(SESSION_GRANTED_ACCESS_TOKENS_KEY, {})
+
+            try:
+                access_count: int = granted_access_tokens.get(name, 0)
+                access_count -= 1  # consume access count
+                if access_count < 0:
+                    raise web.HTTPUnauthorized(reason=unauthorized_reason)
+
+                # update and keep for future accesses (e.g. retry this route)
+                granted_access_tokens[name] = access_count
+
+                # Access granted to this handler
+                response = await handler(request)
+
+                if one_time_access:
+                    # avoids future accesses by clearing all tokens
+                    granted_access_tokens.pop(name, None)
+
+                return response
+
+            finally:
+                # prunes
+                session[SESSION_GRANTED_ACCESS_TOKENS_KEY] = {
+                    name: access_count
+                    for name, access_count in granted_access_tokens.items()
+                    if access_count > 0
+                }
 
         return _wrapper
 
