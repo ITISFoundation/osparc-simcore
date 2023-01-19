80be38995cfe9d8145b7d527c61eba2dd3248177
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-01-19 22:42:59 +0100

    remove docker, the registry is anyway not compatible

--- services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/core/docker_utils.py
+++ services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/core/docker_utils.py
@@ -1,14 +1,12 @@
-import asyncio
-import json
 import logging
 from contextlib import asynccontextmanager
-from typing import Any, AsyncGenerator, Final
+from typing import Any, AsyncGenerator
 
 import aiodocker
 import yaml
 from aiodocker.utils import clean_filters
 from models_library.services import RunID
-from pydantic import ByteSize, PositiveInt, parse_obj_as
+from pydantic import PositiveInt
 
 from .errors import UnexpectedDockerError, VolumeNotFoundError
 
@@ -64,60 +62,3 @@ def get_docker_service_images(compose_spec_yaml: str) -> set[str]:
         service_data["image"]
         for service_data in docker_compose_spec["services"].values()
     }
-
-
-DEFAULT_DOCKER_MANIFEST_CMD_TIMEOUT_S: Final[int] = 30
-
-
-async def get_image_size_on_remote(image_name: str) -> ByteSize:
-    async with aiodocker.Docker() as docker:
-        docker_platform_info = await docker.system.info()
-    local_arch = docker_platform_info.get("Architecture", "amd64")
-    local_os = docker_platform_info.get("OSType", "linux")
-    if local_arch == "x86_64":
-        local_arch = "amd64"
-    logger.debug("getting image size on remote using %s", f"{local_arch=}, {local_os=}")
-    command = ["docker", "manifest", "inspect", "--verbose", f"{image_name}"]
-    process = await asyncio.create_subprocess_exec(
-        *command,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    stdout, stderr = await process.communicate()
-    await asyncio.wait_for(
-        process.wait(), timeout=DEFAULT_DOCKER_MANIFEST_CMD_TIMEOUT_S
-    )
-    assert process.returncode is not None  # nosec
-    logger.debug(
-        "getting image size on remote using %s, completed process with %s",
-        f"{local_arch=}, {local_os=}",
-        f"{process.returncode=}",
-    )
-    if process.returncode > 0:
-        raise RuntimeError(
-            f"unexpected error running '{' '.join(command)}': {stderr.decode()}"
-        )
-
-    all_manifests = json.loads(stdout.decode())
-    local_arch_manifest = next(
-        iter(
-            filter(
-                lambda x: (
-                    x.get("Descriptor", {}).get("platform", {}).get("architecture")
-                    == local_arch
-                )
-                and (x.get("Descriptor", {}).get("platform", {}).get("os") == local_os),
-                all_manifests,
-            )
-        )
-    )
-    logger.debug("found %s", f"{local_arch_manifest=}")
-    image_layers = local_arch_manifest.get("SchemaV2Manifest", {}).get("layers", [])
-    layer_to_sizes = {
-        layer["digest"]: layer["size"]
-        for layer in image_layers
-        if all(x in layer for x in ["digest", "size"])
-    }
-    logger.debug("found %s", f"{layer_to_sizes=}")
-    image_size = parse_obj_as(ByteSize, sum(layer_to_sizes.values()))
-    return image_size
