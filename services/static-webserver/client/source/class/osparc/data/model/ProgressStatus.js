f5bbdeb17412462f4309097077511c7b1b21c02c
Odei Maiz <maiz@itis.swiss>
2023-01-23 15:16:17 +0100

    progressTypes as props

--- services/static-webserver/client/source/class/osparc/data/model/ProgressStatus.js
+++ services/static-webserver/client/source/class/osparc/data/model/ProgressStatus.js
@@ -38,16 +38,86 @@ qx.Class.define("osparc.data.model.ProgressStatus", {
     this.__sequenceLoadingPage = new qx.ui.container.Composite(new qx.ui.layout.VBox(5));
   },
 
+  properties: {
+    clusterUpScaling: {
+      check: "Number",
+      init: 0,
+      nullable: false,
+      apply: "__applyClusterUpScaling"
+    },
+
+    sidecarPulling: {
+      check: "Number",
+      init: 0,
+      nullable: false,
+      apply: "__applySidecarPulling"
+    },
+
+    inputsPulling: {
+      check: "Number",
+      init: 0,
+      nullable: false,
+      apply: "__applyInputsPulling"
+    },
+
+    outputsPulling: {
+      check: "Number",
+      init: 0,
+      nullable: false,
+      apply: "__applyOutputsPulling"
+    },
+
+    statePulling: {
+      check: "Number",
+      init: 0,
+      nullable: false,
+      apply: "__applyStatePulling"
+    },
+
+    imagesPulling: {
+      check: "Number",
+      init: null,
+      nullable: false,
+      apply: "__applyImagesPulling"
+    }
+  },
+
   members: {
     __sequenceLoadingPage: null,
+
+    getSequenceForLoadingPage: function() {
+      const label = new qx.ui.basic.Label("Hey there");
+      return label;
+    },
 
     addProgressMessage: function(progressType, progress) {
       console.log(progressType, progress);
     },
 
-    getSequenceForLoadingPage: function() {
-      const label = new qx.ui.basic.Label("Hey there");
-      return label;
+    __applyClusterUp: function(value) {
+
+    },
+
+    __applySidecarPulling: function(value) {
+      this.setClusterUpScaling(1);
+    },
+
+    __applyInputsPulling: function(value) {
+      this.setSidecarPulling(1);
+    },
+
+    __applyOutputsPulling: function(value) {
+      this.setSidecarPulling(1);
+    },
+
+    __applyStatePulling: function(value) {
+      this.setSidecarPulling(1);
+    },
+
+    __applyImagesPulling: function(value) {
+      this.setInputsPulling(1);
+      this.setOutputsPulling(1);
+      this.setStatePulling(1);
     }
   }
 });
