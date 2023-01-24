8cb1f8256cc363f510296b71015e7a9b049c7f18
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-01-24 21:33:38 +0100

    clean

--- services/dynamic-sidecar/tests/unit/test_core_docker_utils.py
+++ services/dynamic-sidecar/tests/unit/test_core_docker_utils.py
@@ -169,26 +169,3 @@ async def test_issue_3793_pulling_images_raises_error():
             progress_cb=_print_progress,
             log_cb=_print_log,
         )
-
-    # This is how
-
-    # {'status': 'Pulling fs layer', 'progressDetail': {}, 'id': '6e3729cf69e0'}
-    # {'status': 'Downloading', 'progressDetail': {'current': 309633, 'total': 30428708}, 'progress': '[>                                                  ]  309.6kB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Downloading', 'progressDetail': {'current': 4663681, 'total': 30428708}, 'progress': '[=======>                                           ]  4.664MB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Downloading', 'progressDetail': {'current': 5912961, 'total': 30428708}, 'progress': '[=========>                                         ]  5.913MB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Downloading', 'progressDetail': {'current': 8718721, 'total': 30428708}, 'progress': '[==============>                                    ]  8.719MB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Downloading', 'progressDetail': {'current': 11204993, 'total': 30428708}, 'progress': '[==================>                                ]   11.2MB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Downloading', 'progressDetail': {'current': 15563137, 'total': 30428708}, 'progress': '[=========================>                         ]  15.56MB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Downloading', 'progressDetail': {'current': 23361921, 'total': 30428708}, 'progress': '[======================================>            ]  23.36MB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Verifying Checksum', 'progressDetail': {}, 'id': '6e3729cf69e0'}
-    # {'status': 'Download complete', 'progressDetail': {}, 'id': '6e3729cf69e0'}
-    # {'status': 'Extracting', 'progressDetail': {'current': 327680, 'total': 30428708}, 'progress': '[>                                                  ]  327.7kB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Extracting', 'progressDetail': {'current': 7864320, 'total': 30428708}, 'progress': '[============>                                      ]  7.864MB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Extracting', 'progressDetail': {'current': 13434880, 'total': 30428708}, 'progress': '[======================>                            ]  13.43MB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Extracting', 'progressDetail': {'current': 21626880, 'total': 30428708}, 'progress': '[===================================>               ]  21.63MB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Extracting', 'progressDetail': {'current': 26542080, 'total': 30428708}, 'progress': '[===========================================>       ]  26.54MB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Extracting', 'progressDetail': {'current': 30146560, 'total': 30428708}, 'progress': '[=================================================> ]  30.15MB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Extracting', 'progressDetail': {'current': 30428708, 'total': 30428708}, 'progress': '[==================================================>]  30.43MB/30.43MB', 'id': '6e3729cf69e0'}
-    # {'status': 'Pull complete', 'progressDetail': {}, 'id': '6e3729cf69e0'}
-    # {'status': 'Digest: sha256:27cb6e6ccef575a4698b66f5de06c7ecd61589132d5a91d098f7f3f9285415a9'}
-    # {'status': 'Status: Downloaded newer image for ubuntu:latest'}
