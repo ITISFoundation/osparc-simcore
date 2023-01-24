e03937c761efd7bc52ec1fbe2d09da5a9e7b5359
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-01-24 10:40:47 +0100

    added test

--- services/autoscaling/tests/unit/test_utils_dynamic_scaling.py
+++ services/autoscaling/tests/unit/test_utils_dynamic_scaling.py
@@ -9,7 +9,7 @@ from typing import Callable
 
 import pytest
 from faker import Faker
-from models_library.generated_models.docker_rest_api import Node, NodeDescription
+from models_library.generated_models.docker_rest_api import Node
 from simcore_service_autoscaling.core.errors import Ec2InvalidDnsNameError
 from simcore_service_autoscaling.modules.ec2 import EC2InstanceData
 from simcore_service_autoscaling.utils.dynamic_scaling import (
@@ -41,10 +41,15 @@ def ec2_instance_data(faker: Faker) -> Callable[..., EC2InstanceData]:
 def node(faker: Faker) -> Callable[..., Node]:
     def _creator(**overrides) -> Node:
         return Node(
-            ID=faker.uuid4(),
-            CreatedAt=f"{faker.date_time()}",
-            UpdatedAt=f"{faker.date_time()}",
-            Description=NodeDescription(Hostname=faker.pystr()),
+            **(
+                {
+                    "ID": faker.uuid4(),
+                    "CreatedAt": f"{faker.date_time()}",
+                    "UpdatedAt": f"{faker.date_time()}",
+                    "Description": {"Hostname": faker.pystr()},
+                }
+                | overrides
+            )
         )
 
     return _creator
@@ -89,3 +94,34 @@ async def test_associate_ec2_instances_with_nodes_with_no_correspondence(
     assert not associated_instances
     assert non_associated_instances
     assert len(non_associated_instances) == len(ec2_instances)
+
+
+async def test_associate_ec2_instances_with_corresponding_nodes(
+    ec2_instance_data: Callable[..., EC2InstanceData],
+    node: Callable[..., Node],
+):
+    nodes = []
+    ec2_instances = []
+    for n in range(10):
+        host_name = f"ip-10-12-32-{n+1}"
+        nodes.append(node(Description={"Hostname": host_name}))
+        ec2_instances.append(
+            ec2_instance_data(aws_private_dns=f"{host_name}.internal-data")
+        )
+
+    (
+        associated_instances,
+        non_associated_instances,
+    ) = await associate_ec2_instances_with_nodes(nodes, ec2_instances)
+
+    assert associated_instances
+    assert not non_associated_instances
+    assert len(associated_instances) == len(ec2_instances)
+    assert len(associated_instances) == len(nodes)
+    for associated_instance in associated_instances:
+        assert associated_instance.node.Description
+        assert associated_instance.node.Description.Hostname
+        assert (
+            associated_instance.node.Description.Hostname
+            in associated_instance.ec2_instance.aws_private_dns
+        )
