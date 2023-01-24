56060dd180d410f5b278b35cdf67831dc8509bdc
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-01-24 15:26:17 +0100

    fix test

--- services/autoscaling/tests/unit/test_dynamic_scaling_core.py
+++ services/autoscaling/tests/unit/test_dynamic_scaling_core.py
@@ -472,6 +472,7 @@ async def test_cluster_scaling_up_starts_multiple_instances(
             "io.simcore.autoscaling.version",
             "io.simcore.autoscaling.monitored_nodes_labels",
             "io.simcore.autoscaling.monitored_services_labels",
+            "Name",
         ]
         for tag_dict in instance["Tags"]:
             assert "Key" in tag_dict
