4c29d8692d1910b0c89e0fd6c51a4d81a425f8f3
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-01-26 13:31:11 +0100

    ensure allowed instances are sorted the same

--- services/autoscaling/src/simcore_service_autoscaling/dynamic_scaling_core.py
+++ services/autoscaling/src/simcore_service_autoscaling/dynamic_scaling_core.py
@@ -354,6 +354,14 @@ async def _scale_up_cluster(
         )
     )
 
+    def _sort_according_to_allowed_types(instance_type: EC2Instance) -> int:
+        assert app_settings.AUTOSCALING_EC2_INSTANCES  # nosec
+        return app_settings.AUTOSCALING_EC2_INSTANCES.EC2_INSTANCES_ALLOWED_TYPES.index(
+            instance_type.name
+        )
+
+    allowed_instance_types.sort(key=_sort_according_to_allowed_types)
+
     # let's start these
     if needed_ec2_instances := await _find_needed_instances(
         app,
