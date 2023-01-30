b9c8091c0a2fec741bcf3e9d2274e2f5ba73e1ee
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-01-30 11:47:26 +0100

    renaming

--- packages/service-library/src/servicelib/progress_bar.py
+++ packages/service-library/src/servicelib/progress_bar.py
@@ -1,41 +1,48 @@
 import asyncio
-import contextlib
 from dataclasses import dataclass, field
 from typing import Optional
 
 
 @dataclass
-class ProgressBar:
+class ProgressData:
     steps: int
-    progress: float = 0
+    _continuous_progress: float = 0
     _children: list = field(default_factory=list)
-    _parent: Optional["ProgressBar"] = None
+    _parent: Optional["ProgressData"] = None
     _lock: asyncio.Lock = field(init=False)
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         self._lock = asyncio.Lock()
 
-    async def __aenter__(self):
+    async def __aenter__(self) -> "ProgressData":
         await self.start()
         return self
 
-    async def __aexit__(self, exc_type, exc_value, traceback):
+    async def __aexit__(self, exc_type, exc_value, traceback) -> None:
         await self.finish()
 
-    async def start(self):
+    async def start(self) -> None:
         pass
 
-    async def update(self, value: float = 1):
+    async def update(self, value: float = 1) -> None:
         async with self._lock:
-            self.progress += value
+            new_progress_value = self._continuous_progress + value
+            if round(new_progress_value) > self.steps:
+                raise ValueError(
+                    f"Progress cannot be updated by {value} as it cannot be higher than {self.steps}"
+                )
+            self._continuous_progress += value
             if self._parent:
                 await self._parent.update(value / self.steps)
 
-    async def finish(self):
+    async def finish(self) -> None:
         pass
 
-    @contextlib.asynccontextmanager
-    async def sub_progress(self, steps):
-        child = ProgressBar(steps=steps, _parent=self)
+    def sub_progress(self, steps) -> "ProgressData":
+        if len(self._children) == self.steps:
+            raise RuntimeError(
+                "Too many sub progresses created already. Wrong usage of the progress bar"
+            )
+        child = ProgressData(steps=steps, _parent=self)
         self._children.append(child)
-        yield child
+        return child
--- packages/service-library/tests/test_progress_bar.py
+++ packages/service-library/tests/test_progress_bar.py
@@ -1,63 +1,66 @@
+# pylint: disable=broad-except
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+# pylint: disable=protected-access
+
 import asyncio
 
 import pytest
-import tqdm
-from servicelib.progress_bar import ProgressBar
-
-
-async def do_something(root: ProgressBar):
-    async with root.sub_progress(steps=50) as sub:
-        for n in range(50):
-            await sub.update()
-
-
-async def do_something_else(root: ProgressBar):
-    async with root.sub_progress(steps=34) as sub:
-        for n in range(17):
-            await sub.update(2)
+from servicelib.progress_bar import ProgressData
 
 
 async def test_progress_bar():
-    async with ProgressBar(steps=2) as root:
-        assert root.progress == 0
+    async with ProgressData(steps=2) as root:
+        assert root._continuous_progress == pytest.approx(0)
         assert root.steps == 2
-        await do_something(root)
-        assert root.progress == pytest.approx(1)
+        async with root.sub_progress(steps=50) as sub:
+            for _ in range(50):
+                await sub.update()
+        assert root._continuous_progress == pytest.approx(1)
         assert root.steps == 2
-        await do_something_else(root)
-        assert root.progress == pytest.approx(2)
+        async with root.sub_progress(steps=50) as sub:
+            for _ in range(50):
+                await sub.update()
+        assert root._continuous_progress == pytest.approx(2)
         assert root.steps == 2
 
 
-async def do_something_tqdm():
-    with tqdm.tqdm(total=100, desc="1st sub loop") as pbar:
-        for n in range(100):
-            await asyncio.sleep(0.01)
-            pbar.update()
+async def test_concurrent_progress_bar():
+    async def do_something(root: ProgressData):
+        async with root.sub_progress(steps=50) as sub:
+            assert sub.steps == 50
+            assert sub._continuous_progress == 0
+            for n in range(50):
+                await sub.update()
+                assert sub._continuous_progress == (n + 1)
+
+    async with ProgressData(steps=12) as root:
+        assert root._continuous_progress == pytest.approx(0)
+        assert root.steps == 12
+        await asyncio.gather(*[do_something(root) for n in range(12)])
+        assert root._continuous_progress == pytest.approx(12)
 
 
-async def do_something_else_tqdm():
-    with tqdm.tqdm(total=100, desc="2nd sub loop") as sub:
-        for n in range(100):
-            await asyncio.sleep(0.01)
-            sub.update(2)
+async def test_too_many_sub_progress_bars_raises():
+    async with ProgressData(steps=2) as root:
+        assert root.steps == 2
+        async with root.sub_progress(steps=50) as sub:
+            for _ in range(50):
+                await sub.update()
+        async with root.sub_progress(steps=50) as sub:
+            for _ in range(50):
+                await sub.update()
+        with pytest.raises(RuntimeError):
+            async with root.sub_progress(steps=50) as sub:
+                for _ in range(50):
+                    await sub.update()
 
 
-async def test_tqdm():
-    # for i in tqdm.trange(4, desc="1st loop"):
-    #     for j in tqdm.trange(5, desc="2nd loop"):
-    #         for k in tqdm.trange(50, desc="3rd loop", leave=False):
-    #             sleep(0.01)
-    with tqdm.tqdm(total=2, desc="1st loop", position=0) as pbar:
-        # await do_something_tqdm()
-        with tqdm.tqdm(total=100, desc="1st sub loop", position=1, leave=False) as sub:
-            for n in range(100):
-                await asyncio.sleep(0.01)
-                sub.update()
-                pbar.update(1 / 100)
-        # await do_something_else_tqdm()
-        with tqdm.tqdm(total=100, desc="2nd sub loop", position=1, leave=False) as sub:
-            for n in range(100):
-                await asyncio.sleep(0.01)
-                sub.update()
-                pbar.update(1 / 100)
+async def test_too_many_updates_raises():
+    async with ProgressData(steps=2) as root:
+        assert root.steps == 2
+        await root.update()
+        await root.update()
+        with pytest.raises(ValueError):
+            await root.update()
