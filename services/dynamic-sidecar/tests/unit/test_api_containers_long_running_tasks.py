878afb2373e2bc556d758416530995c5412322c8
Andrei Neagu <neagu@itis.swiss>
2023-01-30 10:04:21 +0100

    test now requires swarm

--- services/dynamic-sidecar/tests/unit/test_api_containers_long_running_tasks.py
+++ services/dynamic-sidecar/tests/unit/test_api_containers_long_running_tasks.py
@@ -349,6 +349,7 @@ async def _debug_progress(message: str, percent: float, task_id: TaskId) -> None
 
 
 async def test_create_containers_task(
+    docker_swarm: None,
     httpx_async_client: AsyncClient,
     client: Client,
     compose_spec: str,
