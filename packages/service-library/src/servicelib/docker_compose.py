ad0faae4be2c990a0837c41cc36e6355c8226d20
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-01-31 17:08:11 +0100

    uses new setup in old functions

--- packages/service-library/src/servicelib/docker_compose.py
+++ packages/service-library/src/servicelib/docker_compose.py
@@ -1,4 +1,5 @@
 import yaml
+from models_library.utils.string_substitution import SubstitutionsDict, TemplateText
 
 # Notes on below env var names:
 # - SIMCORE_REGISTRY will be replaced by the url of the simcore docker registry
@@ -7,6 +8,7 @@ import yaml
 # to which this compos spec is attached
 # Example usage in docker compose:
 #   image: ${SIMCORE_REGISTRY}/${DOCKER_IMAGE_NAME}-dynamic-sidecar-compose-spec:${SERVICE_VERSION}
+
 MATCH_SERVICE_VERSION = "${SERVICE_VERSION}"
 MATCH_SIMCORE_REGISTRY = "${SIMCORE_REGISTRY}"
 MATCH_IMAGE_START = f"{MATCH_SIMCORE_REGISTRY}/"
@@ -24,16 +26,18 @@ def replace_env_vars_in_compose_spec(
     returns a stringified version
     """
 
-    stringified_service_spec = yaml.safe_dump(service_spec)
+    content: str = yaml.safe_dump(service_spec)
 
-    # NOTE: could not use `string.Template` here because the test will
-    # fail since `${DISPLAY}` cannot be replaced, and we do not want
-    # it to be replaced at this time. If this method is changed
-    # the test suite should always pass without changes.
-    stringified_service_spec = stringified_service_spec.replace(
-        MATCH_SIMCORE_REGISTRY, replace_simcore_registry
+    template = TemplateText(content)
+    substitutions = SubstitutionsDict(
+        {
+            "SERVICE_VERSION": replace_service_version,
+            "SIMCORE_REGISTRY": replace_simcore_registry,
+        }
     )
-    stringified_service_spec = stringified_service_spec.replace(
-        MATCH_SERVICE_VERSION, replace_service_version
-    )
-    return stringified_service_spec
+    resolved_content: str = template.safe_substitute(substitutions)
+
+    # should all be used!??
+    assert not substitutions.unused  # nosec
+
+    return resolved_content
