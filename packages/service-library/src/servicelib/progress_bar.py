f7701f0c0a09776a922683297981359b60d70111
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-01-31 15:24:09 +0100

    added option to add an external hook

--- packages/service-library/src/servicelib/progress_bar.py
+++ packages/service-library/src/servicelib/progress_bar.py
@@ -1,15 +1,28 @@
 import asyncio
+import logging
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Optional, Protocol, runtime_checkable
+
+from servicelib.logging_utils import log_catch
+
+logger = logging.getLogger(__name__)
+
+
+@runtime_checkable
+class AsyncReportCB(Protocol):
+    async def __call__(self, progress_value: float) -> None:
+        ...
 
 
 @dataclass
 class ProgressBarData:
     steps: int
+    progress_report_cb: Optional[AsyncReportCB] = None
     _continuous_progress: float = 0
     _children: list = field(default_factory=list)
     _parent: Optional["ProgressBarData"] = None
     _lock: asyncio.Lock = field(init=False)
+    _last_report_value: float = 0
 
     def __post_init__(self) -> None:
         self._lock = asyncio.Lock()
@@ -21,22 +34,41 @@ class ProgressBarData:
     async def __aexit__(self, exc_type, exc_value, traceback) -> None:
         await self.finish()
 
+    async def _update_parent(self, value: float) -> None:
+        if self._parent:
+            await self._parent.update(value / self.steps)
+
+    async def _report_external(self, value: float, force: bool = False) -> None:
+        if not self.progress_report_cb:
+            return
+
+        with log_catch(logger, reraise=False):
+            # NOTE: only report if at least a percent was increased
+            if (force and value != self._last_report_value) or (
+                (value - self._last_report_value) / self.steps > 0.01
+            ):
+                await self.progress_report_cb(value / self.steps)
+                self._last_report_value = value
+
     async def start(self) -> None:
-        pass
+        await self._report_external(0, force=True)
 
     async def update(self, value: float = 1) -> None:
         async with self._lock:
             new_progress_value = self._continuous_progress + value
-            if round(new_progress_value) > self.steps:
+            if new_progress_value > self.steps:
+                new_progress_value = round(new_progress_value)
+            if new_progress_value > self.steps:
                 raise ValueError(
                     f"Progress cannot be updated by {value} as it cannot be higher than {self.steps}"
                 )
-            self._continuous_progress += value
-            if self._parent:
-                await self._parent.update(value / self.steps)
+            self._continuous_progress = new_progress_value
+        await self._update_parent(value)
+        await self._report_external(new_progress_value)
 
     async def finish(self) -> None:
-        pass
+        await self.update(self.steps - self._continuous_progress)
+        await self._report_external(self.steps, force=True)
 
     def sub_progress(self, steps) -> "ProgressBarData":
         if len(self._children) == self.steps:
