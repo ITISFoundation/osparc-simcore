60e0c848b730c5666167f85909397950e38799f9
Andrei Neagu <5694077+GitHK@users.noreply.github.com>
2023-02-03 14:15:00 +0100

    🐛 issues with docker networking and DNS resolution (#3826)

--- services/director-v2/src/simcore_service_director_v2/core/settings.py
+++ services/director-v2/src/simcore_service_director_v2/core/settings.py
@@ -162,7 +162,7 @@ class DynamicSidecarProxySettings(BaseCustomSettings):
 
 class DynamicSidecarEgressSettings(BaseCustomSettings):
     DYNAMIC_SIDECAR_ENVOY_IMAGE: DockerGenericTag = Field(
-        "envoyproxy/envoy:v1.24-latest",
+        "envoyproxy/envoy:v1.25-latest",
         description="envoy image to use",
     )
     DYNAMIC_SIDECAR_ENVOY_LOG_LEVEL: EnvoyLogLevel = Field(
@@ -479,7 +479,6 @@ class ComputationalBackendSettings(BaseCustomSettings):
 
 
 class AppSettings(BaseCustomSettings, MixinLoggingSettings):
-
     # docker environs
     SC_BOOT_MODE: BootModeEnum
     SC_BOOT_TARGET: Optional[BuildTargetEnum]
