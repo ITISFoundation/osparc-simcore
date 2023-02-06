882fe7f101a4a216195b4fb6c8b2f81d666ae1c7
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-02-06 17:24:56 +0100

    fixes column name

--- packages/postgres-database/tests/test_services_environments.py
+++ packages/postgres-database/tests/test_services_environments.py
@@ -3,31 +3,39 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
-from typing import AsyncIterator, Callable
+from typing import Callable
 
-import pytest
 from aiopg.sa.connection import SAConnection
-from aiopg.sa.engine import Engine
 from simcore_postgres_database.models.services_environments import services_environments
-from sqlalchemy.sql import select, text
+from sqlalchemy.sql import select
 
 
-@pytest.fixture
-async def connection(pg_engine: Engine) -> AsyncIterator[SAConnection]:
-    async with pg_engine.acquire() as _conn:
-        yield _conn
+async def test_services_environments_table(
+    connection: SAConnection, create_fake_group: Callable
+):
 
-
-async def test_it(connection: SAConnection, create_fake_group: Callable):
-
-    group = await create_fake_group(connection, name="Product-A")
+    vendor_services_prefix = "simcore/services/dynamic/vendor-x"
+    product_group = await create_fake_group(connection, name="product_a")
 
     await connection.execute(
+        # a vendor exposes these environs to its services to everybody
         services_environments.insert().values(
-            service_key="sim4life",
-            gid=group["gid"],  # product's group
+            service_key_prefix=vendor_services_prefix,
+            # everybody
             osparc_environments={
-                "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_HOST": "foo",
+                "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_HOST": "everybody",
+                "OSPARC_ENVIRONMENT_VENDOR_LICENSE_FILE": "license-everybody.txt",
+            },
+        )
+    )
+
+    await connection.execute(
+        # same vendor exposes different environs to its services for a given product
+        services_environments.insert().values(
+            service_key_prefix=vendor_services_prefix,
+            gid=product_group["gid"],  # product's group
+            osparc_environments={
+                "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_HOST": "product_a-server",
                 "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_PRIMARY_PORT": 1,
                 "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_SECONDARY_PORT": 2,
                 "OSPARC_ENVIRONMENT_VENDOR_LICENSE_DNS_RESOLVER_IP": "1.1.1.1",
@@ -39,12 +47,17 @@ async def test_it(connection: SAConnection, create_fake_group: Callable):
 
     substitutions = await connection.scalar(
         select([services_environments.c.osparc_environments]).where(
-            services_environments.c.service_key.like(text("sim4life%"))
-            & (services_environments.c.gid == group["gid"])
+            (
+                services_environments.c.gid == product_group["gid"]
+            )  # current product's group
+            & services_environments.c.service_key_prefix.like(
+                f"{vendor_services_prefix}%"
+            )
         )
     )
+
     assert substitutions == {
-        "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_HOST": "foo",
+        "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_HOST": "product_a-server",
         "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_PRIMARY_PORT": 1,
         "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_SECONDARY_PORT": 2,
         "OSPARC_ENVIRONMENT_VENDOR_LICENSE_DNS_RESOLVER_IP": "1.1.1.1",
