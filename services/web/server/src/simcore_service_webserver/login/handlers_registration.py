a51e5ad59c0d3ef7d80b5b27ea96fefc98838c0c
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-02-08 19:49:09 +0100

    drafts handlers

--- services/web/server/src/simcore_service_webserver/login/handlers_registration.py
+++ services/web/server/src/simcore_service_webserver/login/handlers_registration.py
@@ -10,10 +10,11 @@ from servicelib.error_codes import create_error_code
 from servicelib.mimetype_constants import MIMETYPE_APPLICATION_JSON
 
 from ..groups_api import auto_add_user_to_groups, auto_add_user_to_product_group
+from ..invitations import is_service_invitation_code
 from ..products import Product, get_current_product
 from ..security_api import encrypt_password
 from ..session_access import on_success_grant_session_access_to, session_access_required
-from ..utils_aiohttp import NextPage
+from ..utils_aiohttp import NextPage, envelope_json_response
 from ._2fa import create_2fa_code, mask_phone_number, send_sms_code
 from ._confirmation import make_confirmation_link
 from ._constants import (
@@ -25,7 +26,11 @@ from ._constants import (
     MSG_UNAUTHORIZED_REGISTER_PHONE,
 )
 from ._models import InputSchema, check_confirm_password_match
-from ._registration import check_and_consume_invitation, check_other_registrations
+from ._registration import (
+    check_and_consume_invitation,
+    check_other_registrations,
+    extract_email_from_invitation,
+)
 from ._security import login_granted_response
 from .settings import (
     LoginOptions,
@@ -54,6 +59,35 @@ def _get_user_name(email: str) -> str:
 
 
 routes = RouteTableDef()
+
+
+class InvitationCheck(InputSchema):
+    invitation: str = Field(..., description="Invitation code")
+
+
+class InvitationInfo(InputSchema):
+    email: Optional[EmailStr] = Field(
+        None, description="Email associated to invitation or None"
+    )
+
+
+@routes.post("/v0/invitations:check", name="auth_register")
+async def check_invitation(request: web.Request):
+    """
+    Decrypts invitation and extracts associated email or
+    returns None if is not an encrypted invitation (might be a database invitation).
+
+    raises HTTPForbidden, HTTPServiceUnavailable
+    """
+    check = await parse_request_body_as(InvitationCheck, request)
+
+    if not is_service_invitation_code(code=check.invitation):
+        return envelope_json_response(InvitationInfo(email=None))
+
+    email = await extract_email_from_invitation(
+        request.app, invitation_code=check.invitation
+    )
+    return envelope_json_response(InvitationInfo(email=email))
 
 
 class RegisterBody(InputSchema):
