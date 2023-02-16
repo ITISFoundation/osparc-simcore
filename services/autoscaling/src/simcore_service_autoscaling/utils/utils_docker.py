65c37313eae4641a205980eba33eda5e8ba1c032
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-02-16 17:16:54 +0100

    ensure we do not have crappy login credentials

--- services/autoscaling/src/simcore_service_autoscaling/utils/utils_docker.py
+++ services/autoscaling/src/simcore_service_autoscaling/utils/utils_docker.py
@@ -329,7 +329,10 @@ async def get_docker_swarm_join_bash_command() -> str:
 def get_docker_login_on_start_bash_command(registry_settings: RegistrySettings) -> str:
     return " ".join(
         [
+            # NOTE: this is to ensure the image does not have some crap login credentials
             "docker",
+            "logout",
+            "&&" "docker",
             "login",
             "--username",
             registry_settings.REGISTRY_USER,
