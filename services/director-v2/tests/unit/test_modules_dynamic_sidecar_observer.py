a774b1d34692d7a271a65f6a655e6f5736e54b68
Andrei Neagu <5694077+GitHK@users.noreply.github.com>
2023-02-20 12:02:45 +0100

    ♻️ making groups_extra_properties depend on product (#3883)

--- services/director-v2/tests/unit/test_modules_dynamic_sidecar_observer.py
+++ services/director-v2/tests/unit/test_modules_dynamic_sidecar_observer.py
@@ -117,7 +117,7 @@ def _is_observation_task_present(
     )
 
 
-@pytest.mark.parametrize("can_save", [None, False, False])
+@pytest.mark.parametrize("can_save", [None, False, True])
 async def test_regression_break_endless_loop_cancellation_edge_case(
     disable_observation: None,
     mock_are_sidecar_and_proxy_services_present: None,
