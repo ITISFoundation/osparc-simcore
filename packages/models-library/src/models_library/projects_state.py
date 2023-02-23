fbdb25306089da78663ed6ba4f8aac5b9ab233d0
Andrei Neagu <neagu@itis.swiss>
2023-02-23 10:00:58 +0100

    removed

--- packages/models-library/src/models_library/projects_state.py
+++ packages/models-library/src/models_library/projects_state.py
@@ -50,7 +50,6 @@ class ProjectStatus(str, Enum):
     EXPORTING = "EXPORTING"
     OPENING = "OPENING"
     OPENED = "OPENED"
-    CHECK_IF_LOCKED = "CHECK_IF_LOCKED"
 
 
 class ProjectLocked(BaseModel):
