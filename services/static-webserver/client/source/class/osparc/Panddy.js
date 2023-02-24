b96de76cc382e0cd705dc66ba6421d3c4e7392c4
Odei Maiz <maiz@itis.swiss>
2023-02-24 15:19:41 +0100

    Hey there

--- services/static-webserver/client/source/class/osparc/Panddy.js
+++ services/static-webserver/client/source/class/osparc/Panddy.js
@@ -23,17 +23,53 @@ qx.Class.define("osparc.Panddy", {
     this.base(arguments);
 
     this._setLayout(new qx.ui.layout.Canvas());
+    this.getChildControl("panddy");
 
-    const pandiSize = 100;
-    const pandi = new qx.ui.basic.Image("osparc/panda.gif").set({
-      width: pandiSize,
-      height: pandiSize,
-      scale: true
-    });
+    setTimeout(() => {
+      this.getChildControl("bubble-text").setValue(osparc.Panddy.INTRO_TEXT);
+    }, 1000);
+  },
 
-    this._add(pandi, {
-      bottom: 0,
-      right: 0
-    });
+  statics: {
+    INTRO_TEXT: qx.locale.Manager.tr("Hey there!<br>This is Panddy. I'm here to give you hints on how to use oSPARC.")
+  },
+
+  members: {
+    _createChildControlImpl: function(id) {
+      const pandiSize = 100;
+      let control;
+      switch (id) {
+        case "panddy": {
+          control = new qx.ui.basic.Image("osparc/panda.gif").set({
+            width: pandiSize,
+            height: pandiSize,
+            scale: true,
+            cursor: "pointer"
+          });
+          this._add(control, {
+            bottom: 0,
+            right: 0
+          });
+          break;
+        }
+        case "bubble-text":
+          control = new qx.ui.basic.Label().set({
+            font: "text-14",
+            backgroundColor: "c05",
+            padding: 10,
+            rich: true,
+            maxWidth: 300
+          });
+          control.getContentElement().setStyles({
+            "border-radius": "8px"
+          });
+          this._add(control, {
+            bottom: pandiSize-20,
+            right: pandiSize-20
+          });
+          break;
+      }
+      return control || this.base(arguments, id);
+    }
   }
 });
