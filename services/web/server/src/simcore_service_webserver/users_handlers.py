baf106eebca028e4c562646ce6064c1ad56dd1fd
Odei Maiz <maiz@itis.swiss>
2023-03-07 16:19:50 +0100

    cleanup

--- services/web/server/src/simcore_service_webserver/users_handlers.py
+++ services/web/server/src/simcore_service_webserver/users_handlers.py
@@ -168,10 +168,8 @@ async def update_user_notification(request: web.Request):
         notif = json.loads(notif_str)
         # body includes a dict with the changes to make
         body = await request.json()
-        print("old notif", notif)
         for k, v in body.items():
             notif[k] = str(v)
-        print("new notif", notif)
         await redis_client.set(notif_hash_key, value=json.dumps(notif))
         response = web.json_response(status=web.HTTPNoContent.status_code)
         return response
