93f7ab54175ba363c44b30f6df01ae05cf31a5d9
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-03-07 15:05:03 +0100

    constraints

--- packages/postgres-database/src/simcore_postgres_database/utils_services_specifications.py
+++ packages/postgres-database/src/simcore_postgres_database/utils_services_specifications.py
@@ -10,11 +10,16 @@ from .models.services_environments import (
 async def get_vendor_environments(
     connection: DBConnection, vendor_service_key: str
 ) -> OsparcEnvironmentsDict:
-
-    environments = await connection.scalar(
+    # we know it is unique
+    identifiers_map = await connection.scalar(
         select([services_vendor_environments.c.identifiers_map]).where(
             services_vendor_environments.c.service_key == vendor_service_key
         )
     )
+    environments = {}
+    if identifiers_map is not None:
+        environments = dict(identifiers_map)
+
+    assert all(key.startswith("OSPARC_ENVIRONMENT_") for key in environments)  # nosec
 
     return environments
