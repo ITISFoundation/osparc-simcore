ab051854fc310270f2b2475163599ff88fc79f6d
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-03-07 14:00:09 +0100

    refactors vendor environs

--- packages/postgres-database/tests/test_services_environments.py
+++ packages/postgres-database/tests/test_services_environments.py
@@ -3,17 +3,16 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
-from typing import Callable
-
 from aiopg.sa.connection import SAConnection
 from simcore_postgres_database.models.services import services_meta_data
-from simcore_postgres_database.models.services_environments import services_environments
+from simcore_postgres_database.models.services_environments import (
+    OsparcEnvironmentsDict,
+    services_vendor_environments,
+)
 from sqlalchemy.sql import select
 
 
-async def test_services_environments_table(
-    connection: SAConnection, create_fake_group: Callable
-):
+async def test_services_environments_table(connection: SAConnection):
 
     vendor_service = "simcore/services/dynamic/vendor/some_service"
 
@@ -22,47 +21,42 @@ async def test_services_environments_table(
             key=vendor_service,
             version="1.0.0",
             name="some-service",
-            description="Vendor's Some Service",
+            description="Some service from a vendor",
         )
     )
 
-    product_group = await create_fake_group(connection, name="product_a")
+    await connection.execute(
+        services_meta_data.insert().values(
+            key="simcore/services/dynamic/vendor/other_service",
+            version="1.0.0",
+            name="some-service",
+            description="Some service from a vendor",
+        )
+    )
 
     await connection.execute(
         # a vendor exposes these environs to its services to everybody
-        services_environments.insert().values(
+        services_vendor_environments.insert().values(
             service_key=vendor_service,
-            # everybody
-            osparc_environments={
-                "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_HOST": "everybody",
-                "OSPARC_ENVIRONMENT_VENDOR_LICENSE_FILE": "license-everybody.txt",
-            },
-        )
-    )
-
-    await connection.execute(
-        # same vendor exposes different environs to its services for a given product
-        services_environments.insert().values(
-            service_key=vendor_service,
-            gid=product_group["gid"],  # product's group
-            osparc_environments={
-                "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_HOST": "product_a-server",
-                "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_PRIMARY_PORT": 1,
-                "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_SECONDARY_PORT": 2,
-                "OSPARC_ENVIRONMENT_VENDOR_LICENSE_DNS_RESOLVER_IP": "1.1.1.1",
-                "OSPARC_ENVIRONMENT_VENDOR_LICENSE_DNS_RESOLVER_PORT": 21,
-                "OSPARC_ENVIRONMENT_VENDOR_LICENSE_FILE": "license.txt",
-            },
+            identifiers_map=OsparcEnvironmentsDict(
+                {
+                    "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_HOST": "product_a-server",
+                    "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_PRIMARY_PORT": 1,
+                    "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_SECONDARY_PORT": 2,
+                    "OSPARC_ENVIRONMENT_VENDOR_LICENSE_DNS_RESOLVER_IP": "1.1.1.1",
+                    "OSPARC_ENVIRONMENT_VENDOR_LICENSE_DNS_RESOLVER_PORT": "21",
+                    "OSPARC_ENVIRONMENT_VENDOR_LICENSE_FILE": "license.txt",
+                    "OSPARC_ENVIRONMENT_VENDOR_LICENSE_FILE_PRODUCT1": "license-p1.txt",
+                    "OSPARC_ENVIRONMENT_VENDOR_LICENSE_FILE_PRODUCT2": "license-p2.txt",
+                    "OSPARC_ENVIRONMENT_VENDOR_LIST": [1, 2, 3],
+                }
+            ),
         )
     )
 
     substitutions = await connection.scalar(
-        select([services_environments.c.osparc_environments]).where(
-            (
-                services_environments.c.gid == product_group["gid"]
-            )  # current product's group
-            & (services_environments.c.service_key == vendor_service)
-            # & services_environments.c.service_key.like(f"{vendor_service}%")
+        select([services_vendor_environments.c.identifiers_map]).where(
+            services_vendor_environments.c.service_key == vendor_service
         )
     )
 
@@ -71,6 +65,19 @@ async def test_services_environments_table(
         "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_PRIMARY_PORT": 1,
         "OSPARC_ENVIRONMENT_VENDOR_LICENSE_SERVER_SECONDARY_PORT": 2,
         "OSPARC_ENVIRONMENT_VENDOR_LICENSE_DNS_RESOLVER_IP": "1.1.1.1",
-        "OSPARC_ENVIRONMENT_VENDOR_LICENSE_DNS_RESOLVER_PORT": 21,
+        "OSPARC_ENVIRONMENT_VENDOR_LICENSE_DNS_RESOLVER_PORT": "21",
         "OSPARC_ENVIRONMENT_VENDOR_LICENSE_FILE": "license.txt",
+        "OSPARC_ENVIRONMENT_VENDOR_LICENSE_FILE_PRODUCT1": "license-p1.txt",
+        "OSPARC_ENVIRONMENT_VENDOR_LICENSE_FILE_PRODUCT2": "license-p2.txt",
+        "OSPARC_ENVIRONMENT_VENDOR_LIST": [1, 2, 3],
     }
+
+    vendor_substitutions = await connection.execute(
+        select([services_vendor_environments.c.identifiers_map]).where(
+            services_vendor_environments.c.service_key.like("%/vendor/%")
+        )
+    )
+
+    assert [row.identifiers_map for row in await vendor_substitutions.fetchall()] == [
+        substitutions
+    ]
