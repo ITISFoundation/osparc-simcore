01b661b564d8b7e96430072bdf0eb3b828aa0315
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-03-10 11:08:18 +0100

    linter

--- services/osparc-gateway-server/config/default_config.py
+++ services/osparc-gateway-server/config/default_config.py
@@ -1,8 +1,12 @@
-# defines the backend to use with the gateway
+# pylint: disable=undefined-variable
 
-c.DaskGateway.backend_class = "osparc_gateway_server.backend.osparc.OsparcBackend"
+# NOTE: this configuration is used by the dask-gateway-server
+# it follows [traitlets](https://traitlets.readthedocs.io/en/stable/config.html) configuration files
+
+# defines the backend to use with the gateway
+c.DaskGateway.backend_class = "osparc_gateway_server.backend.osparc.OsparcBackend"  # type: ignore
 # defines the password for 'simple' authentication
-c.Authenticator.password = "asdf"
+c.Authenticator.password = "asdf"  # type: ignore
 # defines log levels
-c.DaskGateway.log_level = "WARN"
-c.Proxy.log_level = "WARN"
+c.DaskGateway.log_level = "WARN"  # type: ignore
+c.Proxy.log_level = "WARN"  # type: ignore
