1852f5f4690d292309cf4d8195aab98f03c2643b
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-03-10 11:50:53 +0100

    constants

---
+++ services/web/server/src/simcore_service_webserver/studies_dispatcher/_constants.py
@@ -0,0 +1,15 @@
+from typing import Final
+
+MSG_PROJECT_NOT_FOUND: Final[
+    str
+] = "This link does not reference any public study ({project_id})."
+
+
+MSG_PROJECT_NOT_PUBLISHED: Final[
+    str
+] = "Requested study has not been published as a public study ({project_id})."
+
+
+MSG_UNEXPECTED_ERROR: Final[
+    str
+] = "Opps this is embarrasing! Something went wrong {hint}"
