0d150e1f86ebe4115e3e752ce4ccdfd7f7eabb23
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-03-14 22:22:43 +0100

    fixes in expired_at

--- services/web/server/src/simcore_service_webserver/studies_dispatcher/_studies_access.py
+++ services/web/server/src/simcore_service_webserver/studies_dispatcher/_studies_access.py
@@ -12,7 +12,7 @@ TODO: Refactor to reduce modules coupling! See all TODO: .``from ...`` comments
 """
 import functools
 import logging
-from datetime import datetime, timezone
+from datetime import datetime
 from functools import lru_cache
 from uuid import UUID, uuid5
 
@@ -87,7 +87,7 @@ async def _create_temporary_user(request: web.Request):
     random_uname = get_random_string(min_len=5)
     email = random_uname + "@guest-at-osparc.io"
     password = get_random_string(min_len=12)
-    expires_at = datetime.now(timezone.utc) + settings.STUDIES_GUEST_ACCOUNT_LIFETIME
+    expires_at = datetime.utcnow() + settings.STUDIES_GUEST_ACCOUNT_LIFETIME
 
     # GUEST_USER_RC_LOCK:
     #
