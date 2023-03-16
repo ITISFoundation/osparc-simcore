e6fc9a05cfed225eec4c38d69012a7eface73056
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-03-16 18:24:59 +0100

    mypy

--- services/catalog/src/simcore_service_catalog/models/schemas/meta.py
+++ services/catalog/src/simcore_service_catalog/models/schemas/meta.py
@@ -1,19 +1,23 @@
-from typing import Dict, Optional
+import re
+from typing import Optional
 
-from pydantic import BaseModel, Field, constr
+from pydantic import BaseModel, ConstrainedStr, Field
 
 # TODO: review this RE
 # use https://www.python.org/dev/peps/pep-0440/#version-scheme
 # or https://www.python.org/dev/peps/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions
 #
 VERSION_RE = r"^(0|[1-9]\d*)(\.(0|[1-9]\d*)){2}(-(0|[1-9]\d*|\d*[-a-zA-Z][-\da-zA-Z]*)(\.(0|[1-9]\d*|\d*[-a-zA-Z][-\da-zA-Z]*))*)?(\+[-\da-zA-Z]+(\.[-\da-zA-Z-]+)*)?$"
-VersionStr = constr(regex=VERSION_RE)
+
+
+class VersionStr(ConstrainedStr):
+    regex = re.compile(VERSION_RE)
 
 
 class Meta(BaseModel):
     name: str
     version: VersionStr
-    released: Optional[Dict[str, VersionStr]] = Field(
+    released: Optional[dict[str, VersionStr]] = Field(
         None, description="Maps every route's path tag with a released version"
     )
 
