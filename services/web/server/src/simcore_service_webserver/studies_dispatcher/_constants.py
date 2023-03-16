fd87bbc5b19ce0488651464d9242fbf3044d4f8d
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-03-16 17:28:04 +0100

    minor

--- services/web/server/src/simcore_service_webserver/studies_dispatcher/_constants.py
+++ services/web/server/src/simcore_service_webserver/studies_dispatcher/_constants.py
@@ -1,15 +1,13 @@
 from typing import Final
 
-MSG_PROJECT_NOT_FOUND: Final[
-    str
-] = "This link does not reference any public study ({project_id})."
+MSG_PROJECT_NOT_FOUND: Final[str] = "Link with ID '{project_id}' cannot be found."
 
 
 MSG_PROJECT_NOT_PUBLISHED: Final[
     str
-] = "Requested study has not been published as a public study ({project_id})."
+] = "Link with ID '{project_id}' does not match any published study."
 
 
 MSG_UNEXPECTED_ERROR: Final[
     str
-] = "Opps this is embarrasing! Something went wrong {hint}"
+] = "Opps this is embarrasing! Something went really wrong {hint}"
