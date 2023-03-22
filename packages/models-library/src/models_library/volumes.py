f10037cae4be6856c04a23509931bb1252d35881
Andrei Neagu <neagu@itis.swiss>
2023-03-22 14:39:18 +0100

    added docstring

--- packages/models-library/src/models_library/volumes.py
+++ packages/models-library/src/models_library/volumes.py
@@ -4,6 +4,17 @@ from .utils.enums import StrAutoEnum
 
 
 class VolumeCategory(StrAutoEnum):
+    """
+    These uniquely identify volumes which are mounted by
+    the dynamic-sidecar and user services.
+
+    This is primarily used to keep track of the status of
+    each individual volume on the volumes.
+
+    The status is ingested by the agent and processed
+    when the volume is removed.
+    """
+
     OUTPUTS = auto()
     INPUTS = auto()
     STATES = auto()
