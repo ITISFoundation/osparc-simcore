2ff375c8abe51bcf3151affe187cba1945af3af6
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-03-24 16:53:52 +0100

    adds new query trials

--- packages/postgres-database/tests/test_services__meta_data.py
+++ packages/postgres-database/tests/test_services__meta_data.py
@@ -4,22 +4,63 @@
 # pylint: disable=too-many-arguments
 
 
+from typing import NamedTuple
+
 import pytest
 import sqlalchemy as sa
 from faker import Faker
 from packaging.version import Version
+from pytest_simcore.helpers.rawdata_fakers import random_group
+from simcore_postgres_database.models.groups import GroupType, groups
+from simcore_postgres_database.models.products import products
 from simcore_postgres_database.models.services import (
+    services_access_rights,
     services_latest,
     services_meta_data,
 )
 from sqlalchemy.dialects.postgresql import insert as pg_insert
 
 
-@pytest.mark.testit
-def test_it(faker: Faker, pg_sa_engine: sa.engine.Engine):
+class ServicesFixture(NamedTuple):
+    expected_latest: dict
+    num_services: int
+
+
+@pytest.fixture
+def services_fixture(faker: Faker, pg_sa_engine: sa.engine.Engine) -> ServicesFixture:
+    # fake metadata from image
+    # emulate background
+    #  - inject to database
+    #  - create permissions
+
     expected_latest = {}
 
     with pg_sa_engine.connect() as conn:
+        # need PRODUCT
+        product_name = conn.execute(
+            products.insert()
+            .values(
+                name="osparc",
+                display_name="Product Osparc",
+                short_name="osparc",
+                host_regex=r"^osparc.",
+                priority=0,
+            )
+            .returning(products.c.name)
+        ).scalar()
+
+        # need GROUPS
+        product_gid = conn.execute(
+            groups.insert()
+            .values(**random_group(type=GroupType.STANDARD, name="osparc group"))
+            .returning(groups.c.gid)
+        ).scalar()
+        everyone_gid = conn.execute(
+            sa.select(groups.c.gid).where(groups.c.type == GroupType.EVERYONE)
+        ).scalar()
+
+        assert product_gid != everyone_gid
+
         # fill w/ different versions
         num_services = 3
 
@@ -31,7 +72,7 @@ def test_it(faker: Faker, pg_sa_engine: sa.engine.Engine):
 
             num_versions = 4
             for _ in range(num_versions):
-                version = faker.numerify("##.##.###")  # might hit same versioN!
+                version = faker.numerify("##.##.###")
                 if Version(expected_latest[key]) < Version(version):
                     expected_latest[key] = version
 
@@ -45,11 +86,29 @@ def test_it(faker: Faker, pg_sa_engine: sa.engine.Engine):
                     if service_index % 2
                     else [],
                 )
-
                 conn.execute(query)
 
+                # all
+                query = services_access_rights.insert().values(
+                    key=key,
+                    version=version,
+                    gid=everyone_gid,
+                    execute_access=True,
+                    write_access=False,
+                    product_name=product_name,
+                )
+
+                conn.execute(query)
+    return ServicesFixture(
+        expected_latest=expected_latest,
+        num_services=num_services,
+    )
+
+
+@pytest.mark.testit
+def test_it(services_fixture: ServicesFixture, pg_sa_engine: sa.engine.Engine):
     with pg_sa_engine.connect() as conn:
-        # select query to find latest
+        # Select query for latest
         latest_select_query = sa.select(
             services_meta_data.c.key,
             sa.text(
@@ -59,8 +118,12 @@ def test_it(faker: Faker, pg_sa_engine: sa.engine.Engine):
         ).group_by(services_meta_data.c.key)
 
         print(latest_select_query)
+        rows: list = conn.execute(latest_select_query).fetchall()
 
-        # Insert from select query
+        assert len(rows) == services_fixture.num_services
+        assert set(services_fixture.expected_latest.items()) == set(rows)
+
+        # Insert from select query (kept for reference)
         def _insert_latest():
             ins = services_latest.insert().from_select(
                 [services_latest.c.key, services_latest.c.version], latest_select_query
@@ -70,11 +133,7 @@ def test_it(faker: Faker, pg_sa_engine: sa.engine.Engine):
             result = conn.execute(ins)  # fills services_latest the first time
             print(result)
 
-        rows: list = conn.execute(latest_select_query).fetchall()
-
-        assert len(rows) == num_services
-        assert set(expected_latest.items()) == set(rows)
-
+        # Upsert from fetched value (alternative 1 - kept for reference)
         def _upsert_with_fetched_values():
             for row in rows:
                 data = dict(row.items())
@@ -91,10 +150,7 @@ def test_it(faker: Faker, pg_sa_engine: sa.engine.Engine):
 
                 conn.execute(upsert_query)
 
-            latest_values = conn.execute(services_latest.select()).fetchall()
-            assert latest_values == rows
-
-        # upsert from select of latest
+        # Upsert from subquery (alternative 2)
         query = pg_insert(services_latest).from_select(
             [services_latest.c.key, services_latest.c.version], latest_select_query
         )
@@ -104,29 +160,42 @@ def test_it(faker: Faker, pg_sa_engine: sa.engine.Engine):
             ],
             set_=dict(version=query.excluded.version),
         )
-
         conn.execute(upsert_query)
 
         latest_values = conn.execute(services_latest.select()).fetchall()
         assert latest_values == rows
 
+    # list latest services
     with pg_sa_engine.connect() as conn:
-        # list latest services
-        query = sa.select(services_meta_data).select_from(
+        query = sa.select(services_meta_data, services_access_rights).select_from(
             services_latest.join(
                 services_meta_data,
                 (services_meta_data.c.key == services_latest.c.key)
                 & (services_meta_data.c.version == services_latest.c.version),
+            ).join(
+                services_access_rights,
+                (services_meta_data.c.key == services_access_rights.c.key)
+                & (services_meta_data.c.version == services_access_rights.c.version),
             )
         )
+        print(query)
+
         query1 = query.where(services_meta_data.c.classifiers.contains(["osparc"]))
+
         query2 = query.where(
             sa.func.array_length(services_meta_data.c.classifiers, 1) > 0
         )
 
-        print(query)
+        # list services with gid=1 (x=1, w=0) and with type dynamic and classifier include osparc
+        query3 = query.where(
+            services_latest.c.key.like("simcore/services/dynamic/%%")
+            & services_meta_data.c.classifiers.contains(["osparc"])
+            & (services_access_rights.c.gid == 1)
+            & (services_access_rights.c.execute_access == True)
+        )
 
-        for stmt in (query1, query2):
+        for n, stmt in enumerate([query1, query2, query3]):
+            print("query", n, "-----")
             rows = conn.execute(stmt).fetchall()
-            assert len(rows) <= num_services
+            assert len(rows) <= services_fixture.num_services
             print(rows)
