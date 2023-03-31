e4902c712403f04b9451aecda67a09bda5a64306
Odei Maiz <maiz@itis.swiss>
2023-03-31 13:35:39 +0200

    panddy sequence

---
+++ services/static-webserver/client/source/class/osparc/product/panddy/osparc/Sequence.js
@@ -0,0 +1,23 @@
+/* ************************************************************************
+
+   osparc - the simcore frontend
+
+   https://osparc.io
+
+   Copyright:
+     2023 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+qx.Class.define("osparc.product.panddy.osparc.Sequence", {
+  type: "static",
+
+  statics: {
+  }
+});
