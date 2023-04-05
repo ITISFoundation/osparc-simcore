5dcdc5d1acc47855649452ec26803373b24e363e
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-04-05 17:50:25 +0200

    test oas

--- services/web/server/tests/unit/isolated/test_projects_oas_schemas.py
+++ services/web/server/tests/unit/isolated/test_projects_oas_schemas.py
@@ -1,31 +1,35 @@
 import pytest
+from models_library.generics import Envelope
 from models_library.rest_pagination import Page
 from pydantic import parse_obj_as
 from pytest_simcore.simcore_webserver_projects_rest_api import (
+    GET_PROJECT,
     LIST_PROJECTS,
-    SESSION_WORKFLOW,
+    NEW_PROJECT,
+    REPLACE_PROJECT,
+    REPLACE_PROJECT_ON_MODIFIED,
     HttpApiCallCapture,
 )
 from simcore_service_webserver.projects.oas_schemas import (
     ProjectCreate,
+    ProjectGet,
     ProjectListItem,
+    ProjectReplace,
 )
 
 
 @pytest.mark.parametrize(
     "api_call",
-    (
-        capture
-        for capture in SESSION_WORKFLOW
-        if capture.method == "POST" and capture.path == "/v0/projects"
-    ),
+    (NEW_PROJECT,),
     ids=lambda c: c.name,
 )
-def test_project_create_model(api_call: HttpApiCallCapture):
+def test_create_project_schemas(api_call: HttpApiCallCapture):
 
-    model = ProjectCreate.parse_obj(api_call.request_payload)
+    request_payload = ProjectCreate.parse_obj(api_call.request_payload)
+    assert request_payload
 
-    assert model
+    response_body = parse_obj_as(Envelope[ProjectGet], api_call.response_body)
+    assert response_body
 
 
 @pytest.mark.parametrize(
@@ -33,8 +37,32 @@ def test_project_create_model(api_call: HttpApiCallCapture):
     (LIST_PROJECTS,),
     ids=lambda c: c.name,
 )
-def test_list_project_model(api_call: HttpApiCallCapture):
+def test_list_project_schemas(api_call: HttpApiCallCapture):
 
-    model = parse_obj_as(Page[ProjectListItem], api_call.request_payload)
+    request_payload = parse_obj_as(Page[ProjectListItem], api_call.request_payload)
+    assert request_payload
 
-    assert model
+
+@pytest.mark.parametrize(
+    "api_call",
+    (GET_PROJECT,),
+    ids=lambda c: c.name,
+)
+def test_get_project_schemas(api_call: HttpApiCallCapture):
+
+    response_body = parse_obj_as(Envelope[ProjectGet], api_call.response_body)
+    assert response_body
+
+
+@pytest.mark.parametrize(
+    "api_call",
+    (REPLACE_PROJECT, REPLACE_PROJECT_ON_MODIFIED),
+    ids=lambda c: c.name,
+)
+def test_replace_project_schemas(api_call: HttpApiCallCapture):
+
+    request_payload = parse_obj_as(ProjectReplace, api_call.request_payload)
+    assert request_payload
+
+    response_body = parse_obj_as(Envelope[ProjectGet], api_call.response_body)
+    assert response_body
