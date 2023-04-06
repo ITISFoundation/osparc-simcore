21209b2648c4a2047639c2a3ae2a67979f36f0a7
Odei Maiz <maiz@itis.swiss>
2023-04-06 16:38:04 +0200

    footer

--- services/static-webserver/client/source/class/osparc/product/landingPage/s4llite/Footer.js
+++ services/static-webserver/client/source/class/osparc/product/landingPage/s4llite/Footer.js
@@ -26,10 +26,10 @@ qx.Class.define("osparc.product.landingPage.s4llite.Footer", {
     }));
 
     this.set({
-      padding: 100,
+      padding: 50,
       paddingLeft: 200,
       paddingRight: 200,
-      backgroundColor: "background-main-3"
+      backgroundColor: "background-main-2"
     });
 
     this.buildLayout();
@@ -44,10 +44,8 @@ qx.Class.define("osparc.product.landingPage.s4llite.Footer", {
         flex: 1
       });
 
-      /*
       const memberOfLayout = this.__createMemberOf();
       this._add(memberOfLayout);
-      */
     },
 
     __createSocialNetworks: function() {
@@ -55,19 +53,20 @@ qx.Class.define("osparc.product.landingPage.s4llite.Footer", {
 
       const createSociaNetworkIcon = (image, link) => {
         const sociaNetworkIcon = new qx.ui.basic.Image(image).set({
+          alignY: "middle",
           cursor: "pointer"
         });
         sociaNetworkIcon.addListener("tap", () => window.open(link, "_blank"));
         return sociaNetworkIcon;
       };
       [{
-        image: "@FontAwesome5Solid/paw/20",
+        image: "@FontAwesome5Solid/facebook/28",
         link: "https://www.facebook.com/itisfoundation/"
       }, {
-        image: "@FontAwesome5Solid/paw/20",
+        image: "@FontAwesome5Solid/youtube/28",
         link: "https://www.youtube.com/@zurichmedtechag2809"
       }, {
-        image: "@FontAwesome5Solid/paw/20",
+        image: "@FontAwesome5Solid/linked/28",
         link: "https://ch.linkedin.com/company/itis-foundation"
       }].forEach(template => socialNetworksLayout.add(createSociaNetworkIcon(template.image, template.link)));
 
@@ -75,25 +74,23 @@ qx.Class.define("osparc.product.landingPage.s4llite.Footer", {
     },
 
     __createMemberOf: function() {
-      const socialNetworksLayout = new qx.ui.container.Composite(new qx.ui.layout.HBox(10));
+      const socialNetworksLayout = new qx.ui.container.Composite(new qx.ui.layout.HBox(10).set({
+        alignY: "middle"
+      }));
+      const label = new qx.ui.basic.Label("Member of:").set({
+        font: "text-16",
+        alignY: "middle"
+      });
+      socialNetworksLayout.add(label);
 
-      const createSociaNetworkIcon = (image, link) => {
-        const sociaNetworkIcon = new qx.ui.basic.Image(image).set({
-          cursor: "pointer"
-        });
-        sociaNetworkIcon.addListener("tap", () => window.open(link, "_blank"));
-        return sociaNetworkIcon;
-      };
-      [{
-        image: "@FontAwesome5Solid/paw/20",
-        link: "https://www.facebook.com/itisfoundation/"
-      }, {
-        image: "@FontAwesome5Solid/paw/20",
-        link: "https://www.youtube.com/@zurichmedtechag2809"
-      }, {
-        image: "@FontAwesome5Solid/paw/20",
-        link: "https://ch.linkedin.com/company/itis-foundation"
-      }].forEach(template => socialNetworksLayout.add(createSociaNetworkIcon(template.image, template.link)));
+      const image = new qx.ui.basic.Image("https://www.z43.swiss/z43_logo_white.svg").set({
+        width: 100,
+        height: 100,
+        cursor: "pointer",
+        scale: true
+      });
+      image.addListener("tap", () => window.open("https://www.z43.swiss/", "_blank"));
+      socialNetworksLayout.add(image);
 
       return socialNetworksLayout;
     }
