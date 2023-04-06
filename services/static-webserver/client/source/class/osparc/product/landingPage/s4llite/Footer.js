d7cfa9fe6b606f6b2e6b5a234f14bb071d5dc6f0
Odei Maiz <maiz@itis.swiss>
2023-04-06 14:14:58 +0200

    Footer

--- services/static-webserver/client/source/class/osparc/product/landingPage/s4llite/Footer.js
+++ services/static-webserver/client/source/class/osparc/product/landingPage/s4llite/Footer.js
@@ -25,16 +25,77 @@ qx.Class.define("osparc.product.landingPage.s4llite.Footer", {
       alignY: "middle"
     }));
 
-    this._setLayout(new qx.ui.layout.VBox(20).set({
-      alignY: "middle",
-      alignX: "center"
-    }));
+    this.set({
+      padding: 100,
+      paddingLeft: 200,
+      paddingRight: 200,
+      backgroundColor: "background-main-3"
+    });
 
     this.buildLayout();
   },
 
   members: {
     buildLayout: function() {
+      const socialNetworksLayout = this.__createSocialNetworks();
+      this._add(socialNetworksLayout);
+
+      this._add(new qx.ui.core.Spacer(), {
+        flex: 1
+      });
+
+      /*
+      const memberOfLayout = this.__createMemberOf();
+      this._add(memberOfLayout);
+      */
+    },
+
+    __createSocialNetworks: function() {
+      const socialNetworksLayout = new qx.ui.container.Composite(new qx.ui.layout.HBox(10));
+
+      const createSociaNetworkIcon = (image, link) => {
+        const sociaNetworkIcon = new qx.ui.basic.Image(image).set({
+          cursor: "pointer"
+        });
+        sociaNetworkIcon.addListener("tap", () => window.open(link, "_blank"));
+        return sociaNetworkIcon;
+      };
+      [{
+        image: "@FontAwesome5Solid/paw/20",
+        link: "https://www.facebook.com/itisfoundation/"
+      }, {
+        image: "@FontAwesome5Solid/paw/20",
+        link: "https://www.youtube.com/@zurichmedtechag2809"
+      }, {
+        image: "@FontAwesome5Solid/paw/20",
+        link: "https://ch.linkedin.com/company/itis-foundation"
+      }].forEach(template => socialNetworksLayout.add(createSociaNetworkIcon(template.image, template.link)));
+
+      return socialNetworksLayout;
+    },
+
+    __createMemberOf: function() {
+      const socialNetworksLayout = new qx.ui.container.Composite(new qx.ui.layout.HBox(10));
+
+      const createSociaNetworkIcon = (image, link) => {
+        const sociaNetworkIcon = new qx.ui.basic.Image(image).set({
+          cursor: "pointer"
+        });
+        sociaNetworkIcon.addListener("tap", () => window.open(link, "_blank"));
+        return sociaNetworkIcon;
+      };
+      [{
+        image: "@FontAwesome5Solid/paw/20",
+        link: "https://www.facebook.com/itisfoundation/"
+      }, {
+        image: "@FontAwesome5Solid/paw/20",
+        link: "https://www.youtube.com/@zurichmedtechag2809"
+      }, {
+        image: "@FontAwesome5Solid/paw/20",
+        link: "https://ch.linkedin.com/company/itis-foundation"
+      }].forEach(template => socialNetworksLayout.add(createSociaNetworkIcon(template.image, template.link)));
+
+      return socialNetworksLayout;
     }
   }
 });
--- services/static-webserver/client/source/class/osparc/product/landingPage/s4llite/Page.js
+++ services/static-webserver/client/source/class/osparc/product/landingPage/s4llite/Page.js
@@ -34,12 +34,11 @@ qx.Class.define("osparc.product.landingPage.s4llite.Page", {
     const scrollContainer = new qx.ui.container.Scroll();
 
     const content = new osparc.product.landingPage.s4llite.Content();
+    content.setMinHeight(1000);
     scrollContainer.add(content);
 
-    /*
     const footer = new osparc.product.landingPage.s4llite.Footer();
     scrollContainer.add(footer);
-    */
 
     this._add(scrollContainer, {
       flex: 1
