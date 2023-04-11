1fc446c5c5517d3ef9d53adb5001d3e44dcdd40a
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-04-11 21:24:57 +0200

    fixes Returning Any from function declared to return

--- services/api-server/src/simcore_service_api_server/api/dependencies/application.py
+++ services/api-server/src/simcore_service_api_server/api/dependencies/application.py
@@ -6,15 +6,18 @@ from simcore_postgres_database.utils_products import get_default_product_name
 from ...core.settings import ApplicationSettings
 
 
-def get_reverse_url_mapper(request: Request) -> Callable:
-    def reverse_url_mapper(name: str, **path_params: Any) -> str:
-        return request.url_for(name, **path_params)
+def get_reverse_url_mapper(request: Request) -> Callable[..., str]:
+    def _reverse_url_mapper(name: str, **path_params: Any) -> str:
+        url: str = request.url_for(name, **path_params)
+        return url
 
-    return reverse_url_mapper
+    return _reverse_url_mapper
 
 
 def get_settings(request: Request) -> ApplicationSettings:
-    return request.app.state.settings
+    settings = request.app.state.settings
+    assert isinstance(settings, ApplicationSettings)  # nosec
+    return settings
 
 
 def get_app(request: Request) -> FastAPI:
@@ -27,4 +30,6 @@ async def get_product_name(app: FastAPI = Depends(get_app)) -> str:
         async with app.state.engine.acquire() as conn:
             app.state.default_product_name = await get_default_product_name(conn)
 
-    return app.state.default_product_name
+    default_product_name = app.state.default_product_name
+    assert isinstance(default_product_name, str)  # nosec
+    return default_product_name
