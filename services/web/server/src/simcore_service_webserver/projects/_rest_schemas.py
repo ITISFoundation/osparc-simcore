db20695774681ee21048ee848835a59f06a3f182
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-04-12 16:41:59 +0200

    cleanup

--- services/web/server/src/simcore_service_webserver/projects/_rest_schemas.py
+++ services/web/server/src/simcore_service_webserver/projects/_rest_schemas.py
@@ -5,7 +5,6 @@ SEE rationale in https://fastapi.tiangolo.com/tutorial/extra-models/#multiple-mo
 
 """
 
-import uuid
 from typing import Any, TypeAlias
 
 from models_library.emails import LowerCaseEmailStr
@@ -20,56 +19,31 @@ from servicelib.aiohttp.long_running_tasks.server import TaskGet
 from ..rest_schemas_base import InputSchema, OutputSchema
 
 # TODO: review creation  policies with OM (e.g. NO uuid!)
-# ProjectCreate = copy_model(
-#     Project,
-#     name="ProjectCreate",
-#     include={
-#         "uuid",  # TODO: review with OM
-#         "name",
-#         "description",
-#         "creation_date",
-#         "last_change_date",
-#         "workbench",
-#         "prj_owner",
-#         "access_rights",  # TODO: review with OM
-#     },
-#     __base__=BaseOutputSchemaModel,
-# )
 
 
-class ProjectCreate(OutputSchema):
+class ProjectCreate(InputSchema):
     uuid: ProjectID
     name: str
     description: str
-    creation_date: DateTimeStr
-    last_change_date: DateTimeStr
+    thumbnail: HttpUrlWithCustomMinLength | None
     workbench: NodesDict
     prj_owner: LowerCaseEmailStr
     access_rights: dict[GroupIDStr, AccessRights]
+    tags: list[int] | None = []
+    classifiers: list[ClassifierID] | None = Field(default_factory=list)
+    state: ProjectState | None = None
+    ui: StudyUI | None = None
+    quality: dict[str, Any] = Field(default_factory=dict)
+    dev: dict | None = None
 
 
-# ProjectGet: type[BaseModel] = copy_model(
-#     Project,
-#     name="ProjectGet",
-#     include={
-#         "uuid",
-#         "name",
-#         "description",
-#         "thumbnail",
-#         "creation_date",
-#         "last_change_date",
-#         "workbench",
-#         "prj_owner",
-#         "access_rights",
-#         "tags",
-#         "classifiers",
-#         "state",
-#         "ui",
-#         "quality",
-#         "dev",
-#     },
-#     __base__=BaseOutputSchemaModel,
-# )
+# NOTE: based on OVERRIDABLE_DOCUMENT_KEYS
+class ProjectCopyOverride(InputSchema):
+    name: str
+    description: str
+    thumbnail: HttpUrlWithCustomMinLength | None
+    prj_owner: LowerCaseEmailStr
+    access_rights: dict[GroupIDStr, AccessRights]
 
 
 class ProjectGet(OutputSchema):
@@ -83,14 +57,10 @@ class ProjectGet(OutputSchema):
     prj_owner: LowerCaseEmailStr
     access_rights: dict[GroupIDStr, AccessRights]
     tags: list[int] | None = []
-    classifiers: list[ClassifierID] | None = Field(
-        default_factory=list,
-    )
+    classifiers: list[ClassifierID] | None = Field(default_factory=list)
     state: ProjectState | None = None
     ui: StudyUI | None = None
-    quality: dict[str, Any] = Field(
-        default_factory=dict,
-    )
+    quality: dict[str, Any] = Field(default_factory=dict)
     dev: dict | None = None
 
 
@@ -101,27 +71,6 @@ TaskProjectGet: TypeAlias = TaskGet
 # TODO: review with OM. with option to get it lighter??
 class ProjectListItem(ProjectGet):
     ...
-
-
-# ProjectReplace = copy_model(
-#     Project,
-#     name="ProjectReplace",
-#     include={
-#         "name",
-#         "description",
-#         "thumbnail",
-#         "creation_date",
-#         "last_change_date",
-#         "workbench",
-#         "access_rights",
-#         "tags",
-#         "classifiers",
-#         "ui",
-#         "quality",
-#         "dev",
-#     },
-#     __base__=BaseInputSchemaModel,
-# )
 
 
 class ProjectReplace(InputSchema):
@@ -144,47 +93,22 @@ class ProjectReplace(InputSchema):
     dev: dict | None = None
 
 
-# ProjectUpdate = copy_model(
-#     Project,
-#     name="ProjectUpdate",
-#     include={
-#         "name",
-#         "description",
-#         "thumbnail",
-#         "creation_date",
-#         "last_change_date",
-#         "workbench",
-#         "access_rights",
-#         "tags",
-#         "classifiers",
-#         "ui",
-#         "quality",
-#         "dev",
-#     },
-#     as_update_model=True,
-#     __base__=BaseInputSchemaModel,
-# )
-
-
 class ProjectUpdate(InputSchema):
-    name: str = None  # type: ignore
-    description: str = None  # type: ignore
+    name: str = Field(default=None)
+    description: str = Field(default=None)
     thumbnail: HttpUrlWithCustomMinLength | None = None
-    workbench: NodesDict = None  # type: ignore
-    access_rights: dict[GroupIDStr, AccessRights] = None  # type: ignore
+    workbench: NodesDict = Field(default=None)
+    access_rights: dict[GroupIDStr, AccessRights] = Field(default=None)
     tags: list[int] | None = []
-    classifiers: list[ClassifierID] | None = Field(
-        default_factory=list,
-    )
+    classifiers: list[ClassifierID] | None = Field(default_factory=list)
     ui: StudyUI | None = None
-    quality: dict[str, Any] = Field(
-        default_factory=dict,
-    )
+    quality: dict[str, Any] = Field(default_factory=dict)
     dev: dict | None = None
 
 
 __all__: tuple[str, ...] = (
     "ProjectCreate",
+    "ProjectCopyOverride",
     "ProjectGet",
     "ProjectListItem",
     "ProjectReplace",
