32c21e78fd9ffecd26c2d14be069f82a43c42086
Andrei Neagu <neagu@itis.swiss>
2023-04-14 15:28:25 +0200

    refactor

--- packages/service-library/src/servicelib/redis.py
+++ packages/service-library/src/servicelib/redis.py
@@ -53,11 +53,10 @@ class RedisClientSDK:
         return self._client
 
     def __post_init__(self):
-        # Run 3 retries with exponential backoff strategy source: https://redis.readthedocs.io/en/stable/backoff.html
-        tenacity_retry = Retry(ExponentialBackoff(cap=0.512, base=0.008), retries=3)
         self._client = aioredis.from_url(
             self.redis_dsn,
-            retry=tenacity_retry,
+            # Run 3 retries with exponential backoff strategy source: https://redis.readthedocs.io/en/stable/backoff.html
+            retry=Retry(ExponentialBackoff(cap=0.512, base=0.008), retries=3),
             retry_on_error=[
                 redis.exceptions.BusyLoadingError,
                 redis.exceptions.ConnectionError,
