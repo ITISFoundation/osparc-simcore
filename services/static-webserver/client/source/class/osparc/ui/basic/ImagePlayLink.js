58de6894814d92398c74a496f91903698e710526
Odei Maiz <maiz@itis.swiss>
2023-04-16 14:38:17 +0200

    two images

--- services/static-webserver/client/source/class/osparc/ui/basic/ImagePlayLink.js
+++ services/static-webserver/client/source/class/osparc/ui/basic/ImagePlayLink.js
@@ -25,7 +25,11 @@ qx.Class.define("osparc.ui.basic.ImagePlayLink", {
       cursor: "pointer"
     });
 
-    this.addListener("pointerover", this.__showPlayLink);
+    if (link) {
+      this.setLink(link);
+    }
+
+    this.addListener("pointerover", this.__showPlayLink, this);
   },
 
   properties: {
@@ -37,9 +41,10 @@ qx.Class.define("osparc.ui.basic.ImagePlayLink", {
   },
 
   members: {
-    __hoverImage: null,
+    __hoverCanvas: null,
+    __hoverPlay: null,
 
-    __createHoverImage: function() {
+    __createHoverCanvas: function() {
       const element = this.getContentElement().getDomElement();
       const {
         width,
@@ -47,13 +52,34 @@ qx.Class.define("osparc.ui.basic.ImagePlayLink", {
       } = qx.bom.element.Dimension.getSize(element);
 
       const image = new qx.ui.basic.Image().set({
-        source: "@FontAwesome5Solid/play/96",
+        backgroundColor: "text",
+        opacity: 0.5,
+        height,
+        width,
+        cursor: "pointer"
+      });
+      image.addListener("pointerout", this.__hidePlayLink, this);
+      image.addListener("tap", this.__openPlayLink, this);
+      return image;
+    },
+
+    __createHoverPlay: function() {
+      const element = this.getContentElement().getDomElement();
+      const {
+        width,
+        height
+      } = qx.bom.element.Dimension.getSize(element);
+
+      const playSize = 96;
+      const image = new qx.ui.basic.Image().set({
+        source: "@FontAwesome5Solid/play/" + playSize,
         textColor: "strong-main",
         height,
         width,
         alignX: "center",
         alignY: "middle",
-        cursor: "pointer"
+        cursor: "pointer",
+        paddingTop: parseInt(height/2 - playSize/2)
       });
       image.addListener("pointerout", this.__hidePlayLink, this);
       image.addListener("tap", this.__openPlayLink, this);
@@ -67,8 +93,9 @@ qx.Class.define("osparc.ui.basic.ImagePlayLink", {
     },
 
     __showPlayLink: function() {
-      if (this.__hoverImage === null) {
-        const hoverCanvas = this.__hoverImage = this.__createHoverImage();
+      if (this.__hoverCanvas === null) {
+        const hoverCanvas = this.__hoverCanvas = this.__createHoverCanvas();
+        const hoverPlay = this.__hoverPlay = this.__createHoverPlay();
         const element = this.getContentElement().getDomElement();
         const {
           top,
@@ -79,15 +106,25 @@ qx.Class.define("osparc.ui.basic.ImagePlayLink", {
           top,
           left
         });
+        root.add(hoverPlay, {
+          top,
+          left
+        });
       }
     },
 
     __hidePlayLink: function() {
-      if (this.__hoverImage) {
+      if (this.__hoverCanvas) {
         const root = qx.core.Init.getApplication().getRoot();
-        root.remove(this.__hoverImage);
-        this.__hoverImage.exclude();
-        this.__hoverImage = null;
+        root.remove(this.__hoverCanvas);
+        this.__hoverCanvas.exclude();
+        this.__hoverCanvas = null;
+      }
+      if (this.__hoverPlay) {
+        const root = qx.core.Init.getApplication().getRoot();
+        root.remove(this.__hoverPlay);
+        this.__hoverPlay.exclude();
+        this.__hoverPlay = null;
       }
     }
   }
