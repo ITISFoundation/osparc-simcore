df7ddc3de203e463b108d551b746b09857ab818a
matusdrobuliak66 <drobuliak66@gmail.com>
2023-04-18 19:24:32 +0200

    autoscaling - normalize logging

--- services/autoscaling/src/simcore_service_autoscaling/main.py
+++ services/autoscaling/src/simcore_service_autoscaling/main.py
@@ -4,12 +4,14 @@
 import logging
 
 from fastapi import FastAPI
+from servicelib.logging_utils import config_all_loggers
 from simcore_service_autoscaling.core.application import create_app
 from simcore_service_autoscaling.core.settings import ApplicationSettings
 
 the_settings = ApplicationSettings.create_from_envs()
 logging.basicConfig(level=the_settings.log_level)
 logging.root.setLevel(the_settings.log_level)
+config_all_loggers()
 
 # SINGLETON FastAPI app
 the_app: FastAPI = create_app(the_settings)
