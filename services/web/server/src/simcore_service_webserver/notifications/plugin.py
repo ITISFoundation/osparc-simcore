df1df79c9bc0451c7de3425e9c42b076e494a175
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-05-03 16:54:45 +0200

    ensures setup_diagnostics

--- services/web/server/src/simcore_service_webserver/notifications/plugin.py
+++ services/web/server/src/simcore_service_webserver/notifications/plugin.py
@@ -6,26 +6,28 @@ import logging
 
 from aiohttp import web
 from servicelib.aiohttp.application_setup import ModuleCategory, app_module_setup
+from simcore_service_webserver.diagnostics.plugin import setup_diagnostics
 
 from ..db import setup_db
+from ..diagnostics.plugin import setup_diagnostics
 from ..rabbitmq import setup_rabbitmq
 from ..socketio.plugin import setup_socketio
 from ._db_comp_tasks_listening_task import create_comp_tasks_listening_task
 from ._rabbitmq_consumers import setup_rabbitmq_consumers
 
-log = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
 
 @app_module_setup(
     __name__,
     ModuleCategory.ADDON,
     settings_name="WEBSERVER_NOTIFICATIONS",
-    logger=log,
-    depends=[
-        "simcore_service_webserver.diagnostics",
-    ],  # depends on diagnostics for setting the instrumentation
+    logger=_logger,
 )
 def setup_notifications(app: web.Application):
+    # depends on diagnostics for setting the instrumentation
+    setup_diagnostics(app)
+
     setup_rabbitmq(app)
     setup_socketio(app)
     # Subscribe to rabbit upon startup for logs, progress and other
