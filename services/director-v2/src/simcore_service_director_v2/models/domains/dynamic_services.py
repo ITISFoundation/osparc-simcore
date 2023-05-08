e904a702f2d6e1b98e8ec52b9ee95685097096cd
Andrei Neagu <neagu@itis.swiss>
2023-05-08 11:59:34 +0200

    added missing

--- services/director-v2/src/simcore_service_director_v2/models/domains/dynamic_services.py
+++ services/director-v2/src/simcore_service_director_v2/models/domains/dynamic_services.py
@@ -51,6 +51,7 @@ class DynamicServiceCreate(ServiceDetails):
                 "node_uuid": "75c7f3f4-18f9-4678-8610-54a2ade78eaa",
                 "basepath": "/x/75c7f3f4-18f9-4678-8610-54a2ade78eaa",
                 "product_name": "osparc",
+                "can_save": True,
                 "service_resources": ServiceResourcesDictHelpers.Config.schema_extra[
                     "examples"
                 ][0],
