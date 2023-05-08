6e74600d09326cab9f35d527e08273a0e0beec43
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-05-08 11:49:39 +0200

    basic test

---
+++ services/migration/tests/integration/test_migration_service.py
@@ -0,0 +1,8 @@
+pytest_simcore_core_services_selection = ["postgres", "migration"]
+pytest_simcore_ops_services_selection = [
+    "adminer",
+]
+
+
+def test_migration_service_runs_correctly(docker_stack: dict):
+    ...
