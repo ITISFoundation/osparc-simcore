b9b31bc089f8fea3c41b40864a07549f88537915
Elisabetta Iavarone <18575092+elisabettai@users.noreply.github.com>
2023-05-10 10:15:45 +0200

    ✨ New p2e test: BIOS VNS calibrator (#4208)
    * First attempt to get rid of authentication pop-up when container dies
    
    * fixed regexp
    
    * ensure priority is the same as for the webserver
    
    * add some docs
    
    * New p2e test for BIOS VNC Calibrator template
    
    ---------
    
    Co-authored-by: Dustin Kaiser <Dustin.Kaiser@gmx.net>
    Co-authored-by: Sylvain <35365065+sanderegg@users.noreply.github.com>

---
+++ tests/e2e/portal/BIOS_VNS_Calibrator.js
@@ -0,0 +1,60 @@
+// node BIOS_VNS_Calibrator.js [url_prefix] [template_uuid] [start_timeout] [--demo]
+
+const tutorialBase = require('../tutorials/tutorialBase');
+const utils = require('../utils/utils');
+
+const args = process.argv.slice(2);
+const {
+  urlPrefix,
+  templateUuid,
+  startTimeout,
+  basicauthUsername,
+  basicauthPassword,
+  enableDemoMode
+} = utils.parseCommandLineArgumentsAnonymous(args);
+
+const anonURL = urlPrefix + templateUuid;
+const screenshotPrefix = "BIOS_";
+
+
+async function runTutorial () {
+  const tutorial = new tutorialBase.TutorialBase(anonURL, screenshotPrefix, null, null, null, basicauthUsername, basicauthPassword, enableDemoMode);
+
+  try {
+    const page = await tutorial.beforeScript();
+    const studyData = await tutorial.openStudyLink();
+
+    const workbenchData = utils.extractWorkbenchData(studyData["data"]);
+    console.log("Workbench Data:", workbenchData);
+    const BIOSIdViewer = workbenchData["nodeIds"][0];
+    await tutorial.waitForServices(workbenchData["studyId"],
+     [BIOSIdViewer],
+     startTimeout,
+     false
+     );
+
+    await tutorial.waitFor(5000, 'Some time for starting the service');
+    await utils.takeScreenshot(page, screenshotPrefix + 'service_started');
+
+    // This study opens in fullscreen mode
+    await tutorial.restoreIFrame();
+  }
+  catch(err) {
+    await tutorial.setTutorialFailed(true);
+    console.log('Tutorial error: ' + err);
+  }
+  finally {
+    await tutorial.logOut();
+    await tutorial.close();
+  }
+
+  if (tutorial.getTutorialFailed()) {
+    throw "Tutorial Failed";
+  }
+}
+
+runTutorial()
+  .catch(error => {
+    console.log('Puppeteer error: ' + error);
+    process.exit(1);
+  });
