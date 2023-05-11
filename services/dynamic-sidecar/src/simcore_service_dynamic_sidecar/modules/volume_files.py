b8c6d9326538883691cdd866ac2d83611556cfda
Andrei Neagu <neagu@itis.swiss>
2023-05-11 16:08:58 +0200

    toggle immutability

--- services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/modules/volume_files.py
+++ services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/modules/volume_files.py
@@ -1,13 +1,16 @@
 import asyncio
 import os
 import stat
+from contextlib import asynccontextmanager
 from dataclasses import dataclass
 from functools import partial
 from pathlib import Path
+from typing import AsyncIterator
 
 import aiofiles
 from aiofiles import os as aiofiles_os
 from models_library.volumes import VolumeCategory
+from pydantic import PositiveFloat
 from servicelib.file_constants import AGENT_FILE_NAME, HIDDEN_FILE_NAME
 from servicelib.volumes_utils import VolumeState, VolumeStatus, save_volume_state
 
@@ -42,7 +45,14 @@ class _MountedVolumesLocalPaths:
         return list(result)
 
 
-async def _create_file_with_restricted_permissions(file: Path) -> None:
+async def _toggle_file_immutability(
+    file_path: Path, set_immutable: bool, timeout: PositiveFloat = 5
+) -> None:
+    immutable: str = "+i" if set_immutable else "-i"
+    await async_command(f"chattr {immutable} {file_path}", timeout=timeout)
+
+
+async def _create_file_with_restricted_permissions(file_path: Path) -> None:
     """only allows the user who created the files to change it"""
 
     # NOTE: the `stat.S_IWGRP`, group write permission, should not be here.
@@ -52,14 +62,23 @@ async def _create_file_with_restricted_permissions(file: Path) -> None:
     # write back the created file any longer.
 
     file_touch = partial(
-        file.touch,
+        file_path.touch,
         mode=(stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH),
         exist_ok=True,
     )
     await asyncio.get_event_loop().run_in_executor(None, file_touch)
 
     # File is set immutable (it will only be changed by this process)
-    await async_command(f"chattr +i {file}", timeout=5)
+    await _toggle_file_immutability(file_path=file_path, set_immutable=True)
+
+
+@asynccontextmanager
+async def _disable_file_immutability(file_path: Path) -> AsyncIterator[None]:
+    await _toggle_file_immutability(file_path=file_path, set_immutable=True)
+    try:
+        yield None
+    finally:
+        await _toggle_file_immutability(file_path=file_path, set_immutable=False)
 
 
 async def create_hidden_file_on_all_volumes(mounted_volumes: MountedVolumes) -> None:
@@ -98,21 +117,22 @@ async def create_agent_file_on_all_volumes(mounted_volumes: MountedVolumes) -> N
     for path in (volumes_local_paths.inputs, volumes_local_paths.shared_store):
         agent_file_path = path / AGENT_FILE_NAME
         await _create_file_with_restricted_permissions(agent_file_path)
-
-        await save_volume_state(
-            agent_file_path=agent_file_path,
-            volume_state=VolumeState(status=VolumeStatus.CONTENT_NO_SAVE_REQUIRED),
-        )
+        async with _disable_file_immutability(agent_file_path):
+            await save_volume_state(
+                agent_file_path=agent_file_path,
+                volume_state=VolumeState(status=VolumeStatus.CONTENT_NO_SAVE_REQUIRED),
+            )
 
     # volumes which require saving
     for path in volumes_local_paths.states + (volumes_local_paths.outputs,):
         agent_file_path = path / AGENT_FILE_NAME
         await _create_file_with_restricted_permissions(agent_file_path)
 
-        await save_volume_state(
-            agent_file_path=agent_file_path,
-            volume_state=VolumeState(status=VolumeStatus.CONTENT_NEEDS_TO_BE_SAVED),
-        )
+        async with _disable_file_immutability(agent_file_path):
+            await save_volume_state(
+                agent_file_path=agent_file_path,
+                volume_state=VolumeState(status=VolumeStatus.CONTENT_NEEDS_TO_BE_SAVED),
+            )
 
 
 async def set_volume_state(
@@ -127,7 +147,8 @@ async def set_volume_state(
         volume_category
     )
     for volume_path in volume_paths:
-        await save_volume_state(
-            agent_file_path=volume_path / AGENT_FILE_NAME,
-            volume_state=VolumeState(status=status),
-        )
+        agent_file_path = volume_path / AGENT_FILE_NAME
+        async with _disable_file_immutability(agent_file_path):
+            await save_volume_state(
+                agent_file_path=agent_file_path, volume_state=VolumeState(status=status)
+            )
