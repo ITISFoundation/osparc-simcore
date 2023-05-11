19f7a8fab9b51b5e7f9f85b8367c8d7cad9c49b5
Odei Maiz <maiz@itis.swiss>
2023-05-11 17:06:18 +0200

    pricing modes

--- services/static-webserver/client/source/class/osparc/product/landingPage/s4llite/Pricing.js
+++ services/static-webserver/client/source/class/osparc/product/landingPage/s4llite/Pricing.js
@@ -35,6 +35,37 @@ qx.Class.define("osparc.product.landingPage.s4llite.Pricing", {
     "backToContent": "qx.event.type.Event"
   },
 
+  statics: {
+    createToolbarRadioButton: function(label, icon, pos) {
+      const rButton = new qx.ui.toolbar.RadioButton().set({
+        label,
+        icon,
+        font: "text-18",
+        padding: 15,
+        gap: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        margin: 0
+      });
+      rButton.getContentElement().setStyles({
+        "border-radius": "0px"
+      });
+      const radius = "12px";
+      if (pos === "left") {
+        rButton.getContentElement().setStyles({
+          "border-top-left-radius": radius,
+          "border-bottom-left-radius": radius
+        });
+      } else if (pos === "right") {
+        rButton.getContentElement().setStyles({
+          "border-top-right-radius": radius,
+          "border-bottom-right-radius": radius
+        });
+      }
+      return rButton;
+    }
+  },
+
   members: {
     buildLayout: function() {
       const backToContentButton = new qx.ui.form.Button(this.tr("Back")).set({
@@ -48,6 +79,33 @@ qx.Class.define("osparc.product.landingPage.s4llite.Pricing", {
       });
       backToContentButton.addListener("execute", () => this.fireEvent("backToContent"));
       this._add(backToContentButton);
+
+
+      const toolbar = new qx.ui.toolbar.ToolBar().set({
+        backgroundColor: "transparent"
+      });
+      const modePartLayout = new qx.ui.toolbar.Part();
+      const cloudBtn = this.self().createToolbarRadioButton(this.tr("Cloud"), "@FontAwesome5Solid/cloud/36", "left");
+      cloudBtn.addListener("execute", () => console.log("cloud"));
+
+      const desktopBtn = this.self().createToolbarRadioButton(this.tr("Desktop"), "@FontAwesome5Solid/desktop/36", "right");
+      desktopBtn.addListener("execute", () => console.log("desktop"));
+
+      const radioGroup = new qx.ui.form.RadioGroup();
+      [
+        cloudBtn,
+        desktopBtn
+      ].forEach(btn => {
+        modePartLayout.add(btn);
+        radioGroup.add(btn);
+      });
+      radioGroup.setAllowEmptySelection(false);
+      toolbar.addSpacer();
+      toolbar.add(modePartLayout);
+      toolbar.addSpacer();
+
+      this._add(toolbar);
+
 
       const title1 = osparc.product.landingPage.Utils.largeTitle(this.tr("Pricing & Plans")).set({
         paddingTop: 20
