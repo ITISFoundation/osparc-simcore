72a09f4c1a7c032e2cd8eec1d674b5b1c81b54c1
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2023-05-12 18:50:27 +0200

    🎨 Frontend: Landing Page PoC III (#4227)

--- services/static-webserver/client/source/class/osparc/product/landingPage/s4llite/Pricing.js
+++ services/static-webserver/client/source/class/osparc/product/landingPage/s4llite/Pricing.js
@@ -31,30 +31,67 @@ qx.Class.define("osparc.product.landingPage.s4llite.Pricing", {
     this.buildLayout();
   },
 
-  events: {
-    "backToContent": "qx.event.type.Event"
+  statics: {
+    createToolbarRadioButton: function(label, icon, pos) {
+      const rButton = new qx.ui.toolbar.RadioButton().set({
+        label,
+        icon,
+        font: "text-18",
+        padding: 15,
+        gap: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        margin: 0
+      });
+      rButton.getContentElement().setStyles({
+        "border-radius": "0px"
+      });
+      const radius = "12px";
+      if (pos === "left") {
+        rButton.getContentElement().setStyles({
+          "border-top-left-radius": radius,
+          "border-bottom-left-radius": radius
+        });
+      } else if (pos === "right") {
+        rButton.getContentElement().setStyles({
+          "border-top-right-radius": radius,
+          "border-bottom-right-radius": radius
+        });
+      }
+      return rButton;
+    }
   },
 
   members: {
     buildLayout: function() {
-      const backToContentButton = new qx.ui.form.Button(this.tr("Back")).set({
-        icon: "@FontAwesome5Solid/arrow-left/14",
-        font: "text-14",
-        alignX: "left",
-        alignY: "middle",
-        allowGrowX: false,
-        padding: 5,
-        width: 90
+      const toolbar = new qx.ui.toolbar.ToolBar().set({
+        backgroundColor: "transparent"
       });
-      backToContentButton.addListener("execute", () => this.fireEvent("backToContent"));
-      this._add(backToContentButton);
+      const modePartLayout = new qx.ui.toolbar.Part();
+      const cloudBtn = this.self().createToolbarRadioButton(this.tr("Cloud"), "@FontAwesome5Solid/cloud/36", "left");
+      const desktopBtn = this.self().createToolbarRadioButton(this.tr("Desktop"), "@FontAwesome5Solid/desktop/36", "right");
+      const radioGroup = new qx.ui.form.RadioGroup();
+      [
+        cloudBtn,
+        desktopBtn
+      ].forEach(btn => {
+        modePartLayout.add(btn);
+        radioGroup.add(btn);
+      });
+      radioGroup.setAllowEmptySelection(false);
+      toolbar.addSpacer();
+      toolbar.add(modePartLayout);
+      toolbar.addSpacer();
 
-      const title1 = osparc.product.landingPage.Utils.largeTitle(this.tr("Pricing & Plans")).set({
+      this._add(toolbar);
+
+
+      const title1 = osparc.product.landingPage.Utils.largeTitle(this.tr("Cloud: Pricing & Plans")).set({
         paddingTop: 20
       });
       this._add(title1);
 
-      const img1 = new qx.ui.basic.Image("osparc/landingPage/diru1.png");
+      const img1 = new qx.ui.basic.Image("osparc/landingPage/diru3.png");
       img1.getContentElement().setStyles({
         "border-radius": "6px"
       });
@@ -65,11 +102,22 @@ qx.Class.define("osparc.product.landingPage.s4llite.Pricing", {
       });
       this._add(title2);
 
-      const img2 = new qx.ui.basic.Image("osparc/landingPage/diru2.png");
+      const img2 = new qx.ui.basic.Image("osparc/landingPage/diru4.png");
       img2.getContentElement().setStyles({
         "border-radius": "6px"
       });
       this._add(img2);
+
+      cloudBtn.addListener("execute", () => {
+        title1.setValue("Cloud: Pricing & Plans");
+        img1.setSource("osparc/landingPage/diru3.png");
+        img2.setSource("osparc/landingPage/diru4.png");
+      });
+      desktopBtn.addListener("execute", () => {
+        title1.setValue("Desktop: Pricing & Plans");
+        img1.setSource("osparc/landingPage/diru1.png");
+        img2.setSource("osparc/landingPage/diru2.png");
+      });
     }
   }
 });
