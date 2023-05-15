172399332f297543d04741f13b6f97ccb77a291e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-05-15 09:19:55 +0200

    added test

--- packages/service-library/tests/test_logging_utils.py
+++ packages/service-library/tests/test_logging_utils.py
@@ -2,11 +2,10 @@
 
 import logging
 from threading import Thread
-from typing import Optional
 
 import pytest
 from pytest import LogCaptureFixture
-from servicelib.logging_utils import log_decorator
+from servicelib.logging_utils import guess_message_log_level, log_decorator
 from servicelib.utils import logged_gather
 
 logger = logging.getLogger(__name__)
@@ -15,7 +14,7 @@ logger = logging.getLogger(__name__)
 @pytest.mark.parametrize("logger", [None, logger])
 @pytest.mark.parametrize("log_traceback", [True, False])
 async def test_error_regression_async_def(
-    caplog: LogCaptureFixture, logger: Optional[logging.Logger], log_traceback: bool
+    caplog: LogCaptureFixture, logger: logging.Logger | None, log_traceback: bool
 ):
     @log_decorator(logger, log_traceback=log_traceback)
     async def _raising_error() -> None:
@@ -34,7 +33,7 @@ async def test_error_regression_async_def(
 @pytest.mark.parametrize("logger", [None, logger])
 @pytest.mark.parametrize("log_traceback", [True, False])
 async def test_error_regression_def(
-    caplog: LogCaptureFixture, logger: Optional[logging.Logger], log_traceback: bool
+    caplog: LogCaptureFixture, logger: logging.Logger | None, log_traceback: bool
 ):
     @log_decorator(logger, log_traceback=log_traceback)
     def _raising_error() -> None:
@@ -50,3 +49,26 @@ async def test_error_regression_def(
         assert "Traceback" in caplog.text
     else:
         assert "Traceback" not in caplog.text
+
+
+@pytest.mark.parametrize(
+    "message, expected_log_level",
+    [
+        ("", logging.INFO),
+        ("Error: this is an error", logging.ERROR),
+        ("[Error] this is an error", logging.ERROR),
+        ("[Error]: this is an error", logging.ERROR),
+        ("[Err] this is an error", logging.ERROR),
+        ("[Err]: this is an error", logging.ERROR),
+        ("Err: this is an error", logging.ERROR),
+        ("Warning: this is an warning", logging.WARNING),
+        ("[Warning] this is an warning", logging.WARNING),
+        ("[Warning]: this is an warning", logging.WARNING),
+        ("[Warn] this is an warning", logging.WARNING),
+        ("[Warn]: this is an warning", logging.WARNING),
+        ("Warn: this is an warning", logging.WARNING),
+        ("Not a Warn: this is an warning", logging.INFO),
+    ],
+)
+def test_guess_message_log_level(message: str, expected_log_level: int):
+    assert guess_message_log_level(message) == expected_log_level
