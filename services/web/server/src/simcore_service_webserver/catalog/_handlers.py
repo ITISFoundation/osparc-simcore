232d549211e48458c6f6f9387d40275b37cb9cd4
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-05-15 17:16:19 +0200

    rm context

--- services/web/server/src/simcore_service_webserver/catalog/_handlers.py
+++ services/web/server/src/simcore_service_webserver/catalog/_handlers.py
@@ -7,8 +7,6 @@
 
 """
 import logging
-from contextlib import contextmanager
-from dataclasses import dataclass
 from typing import Any, Iterator
 
 import orjson
@@ -19,8 +17,14 @@ from models_library.services_resources import (
     ServiceResourcesDict,
     ServiceResourcesDictHelpers,
 )
+from models_library.users import UserID
 from pint import UnitRegistry
-from pydantic import ValidationError
+from pydantic import BaseModel, Extra, Field
+from servicelib.aiohttp.requests_validation import (
+    handle_validation_as_http_error,
+    parse_request_path_parameters_as,
+    parse_request_query_parameters_as,
+)
 from servicelib.rest_constants import RESPONSE_MODEL_POLICY
 
 from .._constants import RQ_PRODUCT_KEY, RQT_USERID_KEY
@@ -36,73 +40,59 @@ from ._schemas import (
     ServiceOutputGet,
     ServiceOutputKey,
     ServiceVersion,
-    json_dumps,
     replace_service_input_outputs,
 )
 from ._units import can_connect
 
-_logger = logging.getLogger(__name__)
-
-
 VTAG = f"/{api_version_prefix}"
 
+_logger = logging.getLogger(__name__)
 
 routes = RouteTableDef()
 
 
-@dataclass
-class _RequestContext:
+class _RequestContext(BaseModel):
     app: web.Application
-    user_id: int
+    user_id: UserID
     product_name: str
     unit_registry: UnitRegistry
 
     @classmethod
     def create(cls, request: Request) -> "_RequestContext":
-        return cls(
-            app=request.app,
-            user_id=request[RQT_USERID_KEY],
-            product_name=request[RQ_PRODUCT_KEY],
-            unit_registry=request.app[UnitRegistry.__name__],
-        )
+        with handle_validation_as_http_error(
+            error_msg_template="Invalid request",
+            resource_name=request.rel_url.path,
+            use_error_v1=True,
+        ):
+            return cls(
+                app=request.app,
+                user_id=request[RQT_USERID_KEY],
+                product_name=request[RQ_PRODUCT_KEY],
+                unit_registry=request.app[UnitRegistry.__name__],
+            )
 
 
-@contextmanager
-def parameters_validation(request: web.Request):
-    """Context manager to wrap match, parsing and validation of
-    request parameters (both path and queries)
-    """
-    try:
-        try:
-            context = _RequestContext.create(request)
-        except KeyError as err:
-            raise web.HTTPBadRequest(reason="Invalid headers") from err
+class _ServicePathParams(BaseModel):
+    service_key: ServiceKey
+    service_version: ServiceVersion
 
-        yield context
-
-        #
-        # wraps match, parse and validate
-        # For instance
-        #   service_key: ServiceKey = request.match_info["service_key"]
-        #   from_service_version: ServiceVersion = request.query["fromVersion"]
-        #   body = await request.json()
-        #
-    except ValidationError as err:
-        raise web.HTTPUnprocessableEntity(
-            text=json_dumps({"error": err.errors()}), content_type="application/json"
-        ) from err
-
-    except KeyError as err:
-        raise web.HTTPBadRequest(reason=f"Expected parameter {err}") from err
+    class Config:
+        allow_population_by_field_name = True
+        extra = Extra.forbid
 
 
 @routes.get(f"{VTAG}/catalog/services")
 @login_required
 @permission_required("services.catalog.*")
 async def list_services_handler(request: Request):
-    with parameters_validation(request) as ctx:
-        # match, parse and validate
-        data_array = await list_services(ctx)
+    req_ctx = _RequestContext.create(request)
+
+    data_array = await list_services(
+        request.app,
+        user_id=req_ctx.user_id,
+        product_name=req_ctx.product_name,
+        unit_registry=req_ctx.unit_registry,
+    )
 
     return envelope_json_response(data_array)
 
@@ -111,13 +101,10 @@ async def list_services_handler(request: Request):
 @login_required
 @permission_required("services.catalog.*")
 async def get_service_handler(request: Request):
-    with parameters_validation(request) as ctx:
-        # match, parse and validate
-        service_key: ServiceKey = request.match_info["service_key"]
-        service_version: ServiceVersion = request.match_info["service_version"]
+    ctx = _RequestContext.create(request)
+    path_params = parse_request_path_parameters_as(_ServicePathParams, request)
 
-    # Evaluate and return validated model
-    data = await get_service(service_key, service_version, ctx)
+    data = await get_service(path_params.service_key, path_params.service_version, ctx)
 
     return envelope_json_response(data)
 
@@ -126,14 +113,14 @@ async def get_service_handler(request: Request):
 @login_required
 @permission_required("services.catalog.*")
 async def update_service_handler(request: Request):
-    with parameters_validation(request) as ctx:
-        # match, parse and validate
-        service_key: ServiceKey = request.match_info["service_key"]
-        service_version: ServiceVersion = request.match_info["service_version"]
-        update_data: dict[str, Any] = await request.json(loads=orjson.loads)
+    ctx = _RequestContext.create(request)
+    path_params = parse_request_path_parameters_as(_ServicePathParams, request)
+    update_data: dict[str, Any] = await request.json(loads=orjson.loads)
 
     # Evaluate and return validated model
-    data = await update_service(service_key, service_version, update_data, ctx)
+    data = await update_service(
+        path_params.service_key, path_params.service_version, update_data, ctx
+    )
 
     return envelope_json_response(data)
 
@@ -142,16 +129,20 @@ async def update_service_handler(request: Request):
 @login_required
 @permission_required("services.catalog.*")
 async def list_service_inputs_handler(request: Request):
-    with parameters_validation(request) as ctx:
-        # match, parse and validate
-        service_key: ServiceKey = request.match_info["service_key"]
-        service_version: ServiceVersion = request.match_info["service_version"]
+    ctx = _RequestContext.create(request)
+    path_params = parse_request_path_parameters_as(_ServicePathParams, request)
 
     # Evaluate and return validated model
-    response_model = await list_service_inputs(service_key, service_version, ctx)
+    response_model = await list_service_inputs(
+        path_params.service_key, path_params.service_version, ctx
+    )
 
     data = [m.dict(**RESPONSE_MODEL_POLICY) for m in response_model]
     return envelope_json_response(data)
+
+
+class _ServiceInputsPathParams(_ServicePathParams):
+    input_key: ServiceInputKey
 
 
 @routes.get(
@@ -160,40 +151,39 @@ async def list_service_inputs_handler(request: Request):
 @login_required
 @permission_required("services.catalog.*")
 async def get_service_input_handler(request: Request):
-    with parameters_validation(request) as ctx:
-        # match, parse and validate
-        service_key: ServiceKey = request.match_info["service_key"]
-        service_version: ServiceVersion = request.match_info["service_version"]
-        input_key: ServiceInputKey = request.match_info["input_key"]
+    ctx = _RequestContext.create(request)
+    path_params = parse_request_path_parameters_as(_ServiceInputsPathParams, request)
 
     # Evaluate and return validated model
     response_model = await get_service_input(
-        service_key, service_version, input_key, ctx
+        path_params.service_key, path_params.service_version, path_params.input_key, ctx
     )
 
     data = response_model.dict(**RESPONSE_MODEL_POLICY)
     return envelope_json_response(data)
 
 
+class _FromServiceOutputParams(BaseModel):
+    from_service_key: ServiceKey = Field(..., alias="fromService")
+    from_service_version: ServiceVersion = Field(..., alias="fromVersion")
+    from_output_key: ServiceOutputKey = Field(..., alias="fromOutput")
+
+
 @routes.get(f"{VTAG}/catalog/services/{{service_key}}/{{service_version}}/inputs:match")
 @login_required
 @permission_required("services.catalog.*")
 async def get_compatible_inputs_given_source_output_handler(request: Request):
-    with parameters_validation(request) as ctx:
-        # match, parse and validate
-        service_key: ServiceKey = request.match_info["service_key"]
-        service_version: ServiceVersion = request.match_info["service_version"]
-        from_service_key: ServiceKey = request.query["fromService"]
-        from_service_version: ServiceVersion = request.query["fromVersion"]
-        from_output_key: ServiceOutputKey = request.query["fromOutput"]
+    ctx = _RequestContext.create(request)
+    path_params = parse_request_path_parameters_as(_ServicePathParams, request)
+    query_params = parse_request_query_parameters_as(_FromServiceOutputParams, request)
 
     # Evaluate and return validated model
     data = await get_compatible_inputs_given_source_output(
-        service_key,
-        service_version,
-        from_service_key,
-        from_service_version,
-        from_output_key,
+        path_params.service_key,
+        path_params.service_version,
+        query_params.from_service_key,
+        query_params.from_service_version,
+        query_params.from_output_key,
         ctx,
     )
 
@@ -206,16 +196,20 @@ async def get_compatible_inputs_given_source_output_handler(request: Request):
 @login_required
 @permission_required("services.catalog.*")
 async def list_service_outputs_handler(request: Request):
-    with parameters_validation(request) as ctx:
-        # match, parse and validate
-        service_key: ServiceKey = request.match_info["service_key"]
-        service_version: ServiceVersion = request.match_info["service_version"]
+    ctx = _RequestContext.create(request)
+    path_params = parse_request_path_parameters_as(_ServicePathParams, request)
 
     # Evaluate and return validated model
-    response_model = await list_service_outputs(service_key, service_version, ctx)
+    response_model = await list_service_outputs(
+        path_params.service_key, path_params.service_version, ctx
+    )
 
     data = [m.dict(**RESPONSE_MODEL_POLICY) for m in response_model]
     return envelope_json_response(data)
+
+
+class _ServiceOutputsPathParams(_ServicePathParams):
+    output_key: ServiceOutputKey
 
 
 @routes.get(
@@ -224,19 +218,25 @@ async def list_service_outputs_handler(request: Request):
 @login_required
 @permission_required("services.catalog.*")
 async def get_service_output_handler(request: Request):
-    with parameters_validation(request) as ctx:
-        # match, parse and validate
-        service_key: ServiceKey = request.match_info["service_key"]
-        service_version: ServiceVersion = request.match_info["service_version"]
-        output_key: ServiceOutputKey = request.match_info["output_key"]
+    ctx = _RequestContext.create(request)
+    path_params = parse_request_path_parameters_as(_ServiceOutputsPathParams, request)
 
     # Evaluate and return validated model
     response_model = await get_service_output(
-        service_key, service_version, output_key, ctx
+        path_params.service_key,
+        path_params.service_version,
+        path_params.output_key,
+        ctx,
     )
 
     data = response_model.dict(**RESPONSE_MODEL_POLICY)
     return envelope_json_response(data)
+
+
+class _ToServiceInputsParams(BaseModel):
+    to_service_key: ServiceKey = Field(..., alias="toService")
+    to_service_version: ServiceVersion = Field(..., alias="toVersion")
+    to_input_key: ServiceInputKey = Field(..., alias="toInput")
 
 
 @routes.get(
@@ -250,21 +250,16 @@ async def get_compatible_outputs_given_target_input_handler(request: Request):
 
     Returns compatible output port of a connected node for a given input
     """
-    with parameters_validation(request) as ctx:
-        # match, parse and validate
-        service_key: ServiceKey = request.match_info["service_key"]
-        service_version: ServiceVersion = request.match_info["service_version"]
-        to_service_key: ServiceKey = request.query["toService"]
-        to_service_version: ServiceVersion = request.query["toVersion"]
-        to_input_key: ServiceInputKey = request.query["toInput"]
+    ctx = _RequestContext.create(request)
+    path_params = parse_request_path_parameters_as(_ServicePathParams, request)
+    query_params = parse_request_query_parameters_as(_ToServiceInputsParams, request)
 
-    # Evaluate and return validated model
     data = await get_compatible_outputs_given_target_input(
-        service_key,
-        service_version,
-        to_service_key,
-        to_service_version,
-        to_input_key,
+        path_params.service_key,
+        path_params.service_version,
+        query_params.to_service_key,
+        query_params.to_service_version,
+        query_params.to_input_key,
         ctx,
     )
 
@@ -280,16 +275,14 @@ async def get_service_resources_handler(request: Request):
 
     Returns compatible output port of a connected node for a given input
     """
-    with parameters_validation(request) as ctx:
-        # match, parse and validate
-        service_key: ServiceKey = request.match_info["service_key"]
-        service_version: ServiceVersion = request.match_info["service_version"]
+    ctx = _RequestContext.create(request)
+    path_params = parse_request_path_parameters_as(_ServicePathParams, request)
 
     service_resources: ServiceResourcesDict = await client.get_service_resources(
         request.app,
         user_id=ctx.user_id,
-        service_key=service_key,
-        service_version=service_version,
+        service_key=path_params.service_key,
+        service_version=path_params.service_version,
     )
 
     data = ServiceResourcesDictHelpers.create_jsonable(service_resources)
@@ -299,14 +292,19 @@ async def get_service_resources_handler(request: Request):
 # IMPLEMENTATION --------------------------------------------------------------------------------
 
 
-async def list_services(ctx: _RequestContext):
+async def list_services(
+    app: web.Application,
+    user_id: UserID,
+    product_name: str,
+    unit_registry: UnitRegistry,
+):
     services = await client.get_services_for_user_in_product(
-        ctx.app, ctx.user_id, ctx.product_name, only_key_versions=False
+        app, user_id, product_name, only_key_versions=False
     )
     for service in services:
         try:
             replace_service_input_outputs(
-                service, unit_registry=ctx.unit_registry, **RESPONSE_MODEL_POLICY
+                service, unit_registry=unit_registry, **RESPONSE_MODEL_POLICY
             )
         except KeyError:
             # This will limit the effect of a any error in the formatting of
@@ -357,7 +355,7 @@ async def update_service(
 
 async def list_service_inputs(
     service_key: ServiceKey, service_version: ServiceVersion, ctx: _RequestContext
-) -> ServiceOutputGet:
+) -> list[ServiceOutputGet]:
 
     service = await client.get_service(
         ctx.app, ctx.user_id, service_key, service_version, ctx.product_name
