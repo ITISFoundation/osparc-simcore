9e316227d6b4ddd31ac66eea89108c0ee548b448
matusdrobuliak66 <drobuliak66@gmail.com>
2023-05-17 15:52:30 +0200

    adding UserWithoutServiceAccess pydantic model

--- packages/models-library/src/models_library/services.py
+++ packages/models-library/src/models_library/services.py
@@ -627,3 +627,9 @@ class ServiceMetaData(_BaseServiceCommonDataModel):
                 },
             }
         }
+
+
+class UserWithoutServiceAccess(BaseModel):
+    user_id: int
+    service_key: str
+    service_version: str
