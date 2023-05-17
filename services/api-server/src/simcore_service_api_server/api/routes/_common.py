95944b27cc2b5a6d068782da4358b0b46d4acfbb
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-05-17 11:15:52 +0200

    api

---
+++ services/api-server/src/simcore_service_api_server/api/routes/_common.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+from fastapi import status
+
+JOB_OUTPUT_LOGFILE_RESPONSES: dict[int, dict[str, Any]] = {
+    status.HTTP_200_OK: {
+        "content": {
+            "application/octet-stream": {
+                "schema": {"type": "string", "format": "binary"}
+            },
+            "application/zip": {"schema": {"type": "string", "format": "binary"}},
+            "text/plain": {"schema": {"type": "string"}},
+        },
+        "description": "Returns a log file",
+    },
+    status.HTTP_404_NOT_FOUND: {"description": "Log not found"},
+}
