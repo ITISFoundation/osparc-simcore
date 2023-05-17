cdddfb78e6cd2cd021cb1c6899fcf2595ebb9744
Odei Maiz <maiz@itis.swiss>
2023-05-17 09:16:43 +0200

    study and template

--- services/static-webserver/client/source/class/osparc/product/Utils.js
+++ services/static-webserver/client/source/class/osparc/product/Utils.js
@@ -33,15 +33,24 @@ qx.Class.define("osparc.product.Utils", {
     },
 
     getStudyAlias: function(options = {}) {
-      let alias = qx.locale.Manager.tr("study");
-      if (this.isProduct("s4llite")) {
-        if (options.plural) {
-          alias = qx.locale.Manager.tr("projects");
-        } else {
-          alias = qx.locale.Manager.tr("project");
-        }
-      } else if (options.plural) {
-        alias = qx.locale.Manager.tr("studies");
+      let alias = null;
+      const product = this.getProductName();
+      switch (product) {
+        case "s4l":
+        case "s4llite":
+          if (options.plural) {
+            alias = qx.locale.Manager.tr("projects");
+          } else {
+            alias = qx.locale.Manager.tr("project");
+          }
+          break;
+        default:
+          if (options.plural) {
+            alias = qx.locale.Manager.tr("studies");
+          } else {
+            alias = qx.locale.Manager.tr("study");
+          }
+          break;
       }
 
       if (options.firstUpperCase) {
@@ -54,15 +63,24 @@ qx.Class.define("osparc.product.Utils", {
     },
 
     getTemplateAlias: function(options = {}) {
-      let alias = qx.locale.Manager.tr("template");
-      if (this.isProduct("s4llite")) {
-        if (options.plural) {
-          alias = qx.locale.Manager.tr("tutorials");
-        } else {
-          alias = qx.locale.Manager.tr("tutorial");
-        }
-      } else if (options.plural) {
-        alias = qx.locale.Manager.tr("templates");
+      let alias = null;
+      const product = this.getProductName();
+      switch (product) {
+        case "s4l":
+        case "s4llite":
+          if (options.plural) {
+            alias = qx.locale.Manager.tr("tutorials");
+          } else {
+            alias = qx.locale.Manager.tr("tutorial");
+          }
+          break;
+        default:
+          if (options.plural) {
+            alias = qx.locale.Manager.tr("templates");
+          } else {
+            alias = qx.locale.Manager.tr("template");
+          }
+          break;
       }
 
       if (options.firstUpperCase) {
