ac2487fe04b641d00ef9471ad0d557e8d5982bbe
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-05-17 11:23:19 +0200

    cleanup

--- services/resource-usage-tracker/tests/unit/api/conftest.py
+++ services/resource-usage-tracker/tests/unit/api/conftest.py
@@ -3,46 +3,39 @@
 # pylint: disable=unused-variable
 # pylint: disable=too-many-arguments
 
-import json
-from typing import Iterator
+from typing import AsyncIterator, Iterator
 
 import httpx
 import pytest
+from asgi_lifespan import LifespanManager
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from pytest import FixtureRequest
 from pytest_simcore.helpers.typing_env import EnvVarsDict
 from simcore_service_resource_usage_tracker.core.application import create_app
+from simcore_service_resource_usage_tracker.core.settings import ApplicationSettings
+
+
+@pytest.fixture
+async def initialized_app(app_environment: EnvVarsDict) -> AsyncIterator[FastAPI]:
+    settings = ApplicationSettings.create_from_envs()
+    app = create_app(settings)
+    async with LifespanManager(app):
+        yield app
 
 
 @pytest.fixture
 def client(app_environment: EnvVarsDict) -> Iterator[TestClient]:
-    print(f"app_environment={json.dumps(app_environment)}")
-
-    app = create_app()
-    print("settings:\n", app.state.settings.json(indent=1))
+    settings = ApplicationSettings.create_from_envs()
+    app = create_app(settings)
     with TestClient(app, base_url="http://testserver.test") as client:
         yield client
 
 
-@pytest.fixture(params=["username", "password", "both", None])
-def invalid_basic_auth(
-    request: FixtureRequest, fake_user_name: str, fake_password: str
-) -> httpx.BasicAuth | None:
-    invalid_case = request.param
-
-    if invalid_case is None:
-        return None
-
-    kwargs = {"username": fake_user_name, "password": fake_password}
-
-    if invalid_case == "both":
-        kwargs = {key: "wrong" for key in kwargs}
-    else:
-        kwargs[invalid_case] = "wronggg"
-
-    return httpx.BasicAuth(**kwargs)
-
-
 @pytest.fixture
-def basic_auth(fake_user_name: str, fake_password: str) -> httpx.BasicAuth:
-    return httpx.BasicAuth(username=fake_user_name, password=fake_password)
+async def async_client(initialized_app: FastAPI) -> AsyncIterator[httpx.AsyncClient]:
+    async with httpx.AsyncClient(
+        app=initialized_app,
+        base_url=f"http://{initialized_app.title}.testserver.io",
+        headers={"Content-Type": "application/json"},
+    ) as client:
+        yield client
