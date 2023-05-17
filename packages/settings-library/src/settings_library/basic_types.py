d7b0d8e9ab8b4642cf4be031d9eafed44a459773
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-05-17 19:33:14 +0200

    fix

--- packages/settings-library/src/settings_library/basic_types.py
+++ packages/settings-library/src/settings_library/basic_types.py
@@ -3,6 +3,7 @@
 #       This is a minor evil to avoid the maintenance burden that creates
 #       an extra dependency to a larger models_library (intra-repo library)
 
+import re
 from enum import Enum
 
 from pydantic import ConstrainedInt, ConstrainedStr
@@ -16,7 +17,7 @@ class PortInt(ConstrainedInt):
 
 # e.g. 'v5'
 class VersionTag(ConstrainedStr):
-    regex = r"^v\d$"
+    regex = re.compile(r"^v\d$")
 
 
 class LogLevel(str, Enum):
