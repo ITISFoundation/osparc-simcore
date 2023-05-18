a34cffa290a7dc974b45f0735991e1c7af66d53c
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-05-18 13:39:29 +0200

    migration

--- packages/postgres-database/src/simcore_postgres_database/migration/versions/ef817566287e_new_services_vendor_secrets_table.py
+++ packages/postgres-database/src/simcore_postgres_database/migration/versions/ef817566287e_new_services_vendor_secrets_table.py
@@ -1,8 +1,8 @@
-"""new services_vendor_environments table
+"""new services_vendor_secrets table
 
-Revision ID: 4e48c058077a
+Revision ID: ef817566287e
 Revises: 0c084cb1091c
-Create Date: 2023-05-18 10:48:19.234075+00:00
+Create Date: 2023-05-18 11:38:51.671239+00:00
 
 """
 import sqlalchemy as sa
@@ -10,7 +10,7 @@ from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = "4e48c058077a"
+revision = "ef817566287e"
 down_revision = "0c084cb1091c"
 branch_labels = None
 depends_on = None
@@ -18,10 +18,10 @@ depends_on = None
 
 def upgrade():
     op.create_table(
-        "services_vendor_environments",
+        "services_vendor_secrets",
         sa.Column("service_key", sa.String(), nullable=False),
         sa.Column(
-            "identifiers_map",
+            "secrets_map",
             postgresql.JSONB(astext_type=sa.Text()),
             server_default=sa.text("'{}'::jsonb"),
             nullable=False,
@@ -33,11 +33,11 @@ def upgrade():
             "modified", sa.DateTime(), server_default=sa.text("now()"), nullable=False
         ),
         sa.PrimaryKeyConstraint(
-            "service_key", name="services_vendor_environments_service_key_pk"
+            "service_key", name="services_vendor_secrets_service_key_pk"
         ),
     )
     op.create_unique_constraint(
-        "services_specifications_service_key_unique",
+        "services_vendor_secrets_service_key_unique",
         "services_specifications",
         ["service_key"],
     )
@@ -45,8 +45,8 @@ def upgrade():
 
 def downgrade():
     op.drop_constraint(
-        "services_specifications_service_key_unique",
+        "services_vendor_secrets_service_key_unique",
         "services_specifications",
         type_="unique",
     )
-    op.drop_table("services_vendor_environments")
+    op.drop_table("services_vendor_secrets")
--- packages/postgres-database/src/simcore_postgres_database/models/services_environments.py
+++ packages/postgres-database/src/simcore_postgres_database/models/services_environments.py
@@ -8,19 +8,19 @@ from .base import metadata
 
 VendorSecretsDict: TypeAlias = dict[str, Any]
 
-# NOTE: this prefix intentionally stresses that the value is secret to avoid leaking this value
-#       on a public domain
+# Intentionally includes the term "SECRET" to avoid leaking this value on a public domain
 VENDOR_SECRET_PREFIX: Final[str] = "OSPARC_VENDOR_SECRET_"
-
-
-#
-#  Service vendor secret environments
-#    - a vendor can associate secrets (e.g. a license code) to one of its services
-#    - values could be encrypted
 
 
 services_vendor_secrets = sa.Table(
     "services_vendor_secrets",
+    #
+    # - A secret is an environment value passed to the service at runtime
+    # - A vendor can associate secrets (e.g. a license code) to any of the services it owns
+    # - secrets_map
+    #   - keys should be prefixed with OSPARC_VENDOR_SECRET_ (can still normalize on read)
+    #   - values might be encrypted
+    #
     metadata,
     sa.Column(
         "service_key",
@@ -32,14 +32,13 @@ services_vendor_secrets = sa.Table(
         JSONB,
         nullable=False,
         server_default=sa.text("'{}'::jsonb"),
-        doc="Maps OSPARC_VENDOR_SECRET_* identifiers to a secret value that can be replaced at runtime in compose specs",
+        doc="Maps OSPARC_VENDOR_SECRET_* identifiers to a secret value (could be encrypted) "
+        "that can be replaced at runtime if found in the compose-specs",
     ),
     # TIME STAMPS ----
     column_created_datetime(),
     column_modified_datetime(),
-    #
     # CONSTRAINTS --
-    #
     sa.PrimaryKeyConstraint(
         "service_key", name="services_vendor_secrets_service_key_pk"
     ),
