6381002fd3b241de15c25022b461a7e37ff77122
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-05-19 15:55:21 +0200

    fixes

--- services/director-v2/src/simcore_service_director_v2/modules/db/repositories/services_environments.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/repositories/services_environments.py
@@ -1,4 +1,6 @@
 from models_library.services import ServiceKey
+from models_library.users import UserID
+from pydantic import EmailStr
 from simcore_postgres_database.utils_services_environments import (
     VENDOR_SECRET_PREFIX,
     VendorSecret,
@@ -14,12 +16,12 @@ class ServicesEnvironmentsRepository(BaseRepository):
     ) -> dict[str, VendorSecret]:
         """Fetches vendor secrets for a service using normalized names"""
         async with self.db_engine.acquire() as conn:
-            vendor_secrets = await get_vendor_secrets(
+            vendor_secrets: dict[str, VendorSecret] = await get_vendor_secrets(
                 conn, vendor_service_key=service_key, normalize_names=True
             )
 
             # NOTE: normalize_names = True
-            assert all(
+            assert all(  # nosec
                 self.is_vendor_secret_identifier(key) for key in vendor_secrets
             )  # nosec
 
@@ -27,3 +29,9 @@ class ServicesEnvironmentsRepository(BaseRepository):
 
     def is_vendor_secret_identifier(self, identifier: str) -> bool:
         return identifier.startswith(VENDOR_SECRET_PREFIX)
+
+    async def get_user_role(self, user_id: UserID):
+        ...
+
+    async def get_user_email(self, user_id: UserID) -> EmailStr:
+        ...
