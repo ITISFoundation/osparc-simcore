5fc5597715b52cba422b9ecb460bab2c3def61d8
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-05-19 20:52:43 +0200

    pylint

--- packages/postgres-database/src/simcore_postgres_database/utils_users.py
+++ packages/postgres-database/src/simcore_postgres_database/utils_users.py
@@ -18,14 +18,16 @@ class UserNotFoundInRepoError(BaseUserRepoError):
 
 
 class UsersRepo:
-    async def get_role(self, conn: SAConnection, user_id: UserID) -> UserRole:
+    @staticmethod
+    async def get_role(conn: SAConnection, user_id: UserID) -> UserRole:
         if value := await conn.scalar(
             sa.select(users.c.role).where(users.c.id == user_id)
         ):
             return UserRole(value)
         raise UserNotFoundInRepoError()
 
-    async def get_email(self, conn: SAConnection, user_id: UserID) -> str:
+    @staticmethod
+    async def get_email(conn: SAConnection, user_id: UserID) -> str:
         if value := await conn.scalar(
             sa.select(users.c.email).where(users.c.id == user_id)
         ):
