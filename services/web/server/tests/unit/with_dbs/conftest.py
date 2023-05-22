8031e39a1b23e2d2464cc1a39269fe6a02052358
matusdrobuliak66 <drobuliak66@gmail.com>
2023-05-22 14:52:16 +0200

    fix after failing tests

--- services/web/server/tests/unit/with_dbs/conftest.py
+++ services/web/server/tests/unit/with_dbs/conftest.py
@@ -226,7 +226,7 @@ def catalog_subsystem_mock(
 
     for namespace in (
         "simcore_service_webserver.projects._crud_read_utils.get_services_for_user_in_product",
-        "simcore_service_webserver.projects.projects_handlers_crud.get_services_for_user_in_product",
+        "simcore_service_webserver.projects._handlers_crud.get_services_for_user_in_product",
     ):
         mock = mocker.patch(
             namespace,
