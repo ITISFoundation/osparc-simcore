86604d7b3100e2f12857c53ebb419ab7a44ba230
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-05-23 14:32:11 +0200

    @GitHK review: rename study_id

--- services/api-server/src/simcore_service_api_server/api/routes/studies.py
+++ services/api-server/src/simcore_service_api_server/api/routes/studies.py
@@ -18,30 +18,30 @@ settings = BasicSettings.create_from_envs()
     response_model=list[Study],
     include_in_schema=settings.API_SERVER_DEV_FEATURES_ENABLED,
 )
-async def list_studies(study_uid: StudyID):
+async def list_studies(study_id: StudyID):
     raise NotImplementedError(
-        f"list_studies {study_uid=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
+        f"list_studies {study_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
     )
 
 
 @router.get(
-    "/{study_uid}",
+    "/{study_id}",
     response_model=Study,
     include_in_schema=settings.API_SERVER_DEV_FEATURES_ENABLED,
 )
-async def get_study(study_uid: StudyID):
+async def get_study(study_id: StudyID):
     raise NotImplementedError(
-        f"get_study {study_uid=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
+        f"get_study {study_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
     )
 
 
 @router.get(
-    "/{study_uid}/ports",
+    "/{study_id}/ports",
     response_model=list[StudyPort],
     include_in_schema=settings.API_SERVER_DEV_FEATURES_ENABLED,
 )
 async def list_study_ports(
-    study_uid: StudyID,
+    study_id: StudyID,
     webserver_api: AuthSession = Depends(get_webserver_session),
 ):
     """Lists metadata on ports of a given study
@@ -50,5 +50,5 @@ async def list_study_ports(
     """
     project_ports: list[
         dict[str, Any]
-    ] = await webserver_api.get_project_metadata_ports(project_id=study_uid)
+    ] = await webserver_api.get_project_metadata_ports(project_id=study_id)
     return project_ports
--- services/api-server/src/simcore_service_api_server/api/routes/studies_jobs.py
+++ services/api-server/src/simcore_service_api_server/api/routes/studies_jobs.py
@@ -24,115 +24,115 @@ JobID: TypeAlias = UUID
 
 
 @router.get(
-    "/studies/{study_uid:uuid}/jobs",
+    "/studies/{study_id:uuid}/jobs",
     response_model=list[Job],
     include_in_schema=settings.API_SERVER_DEV_FEATURES_ENABLED,
 )
-async def list_study_jobs(study_uid: StudyID):
+async def list_study_jobs(study_id: StudyID):
     raise NotImplementedError(
-        f"list study jobs {study_uid=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
+        f"list study jobs {study_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
     )
 
 
 @router.post(
-    "/studies/{study_uid:uuid}/jobs",
+    "/studies/{study_id:uuid}/jobs",
     response_model=Job,
     include_in_schema=settings.API_SERVER_DEV_FEATURES_ENABLED,
 )
-async def create_study_job(study_uid: StudyID):
+async def create_study_job(study_id: StudyID):
     raise NotImplementedError(
-        f"create study job {study_uid=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
+        f"create study job {study_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
     )
 
 
 @router.get(
-    "/studies/{study_uid:uuid}/jobs/{job_id:uuid}",
+    "/studies/{study_id:uuid}/jobs/{job_id:uuid}",
     response_model=Job,
     include_in_schema=settings.API_SERVER_DEV_FEATURES_ENABLED,
 )
 async def get_study_job(
-    study_uid: StudyID,
+    study_id: StudyID,
     job_id: JobID,
 ):
     raise NotImplementedError(
-        f"get study job {study_uid=} {job_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
+        f"get study job {study_id=} {job_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
     )
 
 
 @router.delete(
-    "/studies/{study_uid:uuid}/jobs/{job_id:uuid}",
+    "/studies/{study_id:uuid}/jobs/{job_id:uuid}",
     status_code=status.HTTP_204_NO_CONTENT,
     include_in_schema=settings.API_SERVER_DEV_FEATURES_ENABLED,
 )
-async def delete_study_job(study_uid: StudyID, job_id: JobID):
+async def delete_study_job(study_id: StudyID, job_id: JobID):
     raise NotImplementedError(
-        f"delete study job {study_uid=} {job_id=}.  SEE https://github.com/ITISFoundation/osparc-simcore/issues/4111"
+        f"delete study job {study_id=} {job_id=}.  SEE https://github.com/ITISFoundation/osparc-simcore/issues/4111"
     )
 
 
 @router.post(
-    "/studies/{study_uid:uuid}/jobs/{job_id:uuid}:start",
+    "/studies/{study_id:uuid}/jobs/{job_id:uuid}:start",
     response_model=JobStatus,
     include_in_schema=settings.API_SERVER_DEV_FEATURES_ENABLED,
 )
 async def start_study_job(
-    study_uid: StudyID,
+    study_id: StudyID,
     job_id: JobID,
 ):
     raise NotImplementedError(
-        f"start study job {study_uid=} {job_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
+        f"start study job {study_id=} {job_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
     )
 
 
 @router.post(
-    "/studies/{study_uid:uuid}/jobs/{job_id:uuid}:stop",
+    "/studies/{study_id:uuid}/jobs/{job_id:uuid}:stop",
     response_model=Job,
     include_in_schema=settings.API_SERVER_DEV_FEATURES_ENABLED,
 )
 async def stop_study_job(
-    study_uid: StudyID,
+    study_id: StudyID,
     job_id: JobID,
 ):
     raise NotImplementedError(
-        f"stop study job {study_uid=} {job_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
+        f"stop study job {study_id=} {job_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
     )
 
 
 @router.post(
-    "/studies/{study_uid}/job/{job_id}:inspect",
+    "/studies/{study_id}/job/{job_id}:inspect",
     response_model=JobStatus,
     include_in_schema=settings.API_SERVER_DEV_FEATURES_ENABLED,
 )
 async def inspect_study_job(
-    study_uid: StudyID,
+    study_id: StudyID,
     job_id: JobID,
 ):
     raise NotImplementedError(
-        f"inspect study job {study_uid=} {job_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
+        f"inspect study job {study_id=} {job_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
     )
 
 
 @router.post(
-    "/studies/{study_uid}/job/{job_id}/outputs",
+    "/studies/{study_id}/job/{job_id}/outputs",
     response_model=JobOutputs,
     include_in_schema=settings.API_SERVER_DEV_FEATURES_ENABLED,
 )
 async def get_study_job_outputs(
-    study_uid: StudyID,
+    study_id: StudyID,
     job_id: JobID,
 ):
     raise NotImplementedError(
-        f"get study job outputs {study_uid=} {job_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
+        f"get study job outputs {study_id=} {job_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
     )
 
 
 @router.post(
-    "/studies/{study_uid}/job/{job_id}/outputs/logfile",
+    "/studies/{study_id}/job/{job_id}/outputs/logfile",
     response_class=RedirectResponse,
     include_in_schema=settings.API_SERVER_DEV_FEATURES_ENABLED,
     responses=JOB_OUTPUT_LOGFILE_RESPONSES,
 )
-async def get_study_job_output_logfile(study_uid: StudyID, job_id: JobID):
+async def get_study_job_output_logfile(study_id: StudyID, job_id: JobID):
     raise NotImplementedError(
-        f"get study job output logfile {study_uid=} {job_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
+        f"get study job output logfile {study_id=} {job_id=}. SEE https://github.com/ITISFoundation/osparc-simcore/issues/4177"
     )
