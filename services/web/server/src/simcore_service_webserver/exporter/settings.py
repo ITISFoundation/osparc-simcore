4e4fb495c8508cfc9a7c578a0e239c05352ca22c
Andrei Neagu <neagu@itis.swiss>
2023-05-23 11:21:25 +0200

    refactor settings

--- services/web/server/src/simcore_service_webserver/exporter/settings.py
+++ services/web/server/src/simcore_service_webserver/exporter/settings.py
@@ -1,10 +1,15 @@
 from aiohttp.web import Application
+from pydantic import Field
 from servicelib.aiohttp.application_keys import APP_SETTINGS_KEY
 from settings_library.base import BaseCustomSettings
 
 
 class ExporterSettings(BaseCustomSettings):
-    ...
+    # NOTE: class will be renamed to `SDSSettings` in the next refactoring
+    # that's why the env var is indexed with `SDS_`
+    SDS_ENABLED: bool = Field(
+        False, description="disabled by default since it is half finished"
+    )
 
 
 def get_plugin_settings(app: Application) -> ExporterSettings:
