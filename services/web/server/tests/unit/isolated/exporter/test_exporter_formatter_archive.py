827ebc454cd99eed0054a997df4c530776f50fbc
Andrei Neagu <neagu@itis.swiss>
2023-05-24 09:19:40 +0200

    refactor tests

--- services/web/server/tests/unit/isolated/exporter/test_exporter_formatter_archive.py
+++ services/web/server/tests/unit/isolated/exporter/test_exporter_formatter_archive.py
@@ -11,11 +11,6 @@ from simcore_service_webserver.exporter.exceptions import SDSException
 
 
 @pytest.fixture
-def temp_dir(tmpdir) -> Path:
-    return Path(tmpdir)
-
-
-@pytest.fixture
 def temp_dir2() -> Iterator[Path]:
     with tempfile.TemporaryDirectory() as temp_dir:
         temp_dir_path = Path(temp_dir)
@@ -37,16 +32,16 @@ def project_id(faker: Faker):
     return faker.uuid4()
 
 
-def temp_dir_with_existing_archive(temp_dir, project_id) -> Path:
-    nested_dir = temp_dir / "nested"
+def temp_dir_with_existing_archive(tmp_path, project_id) -> Path:
+    nested_dir = tmp_path / "nested"
     nested_dir.mkdir(parents=True, exist_ok=True)
     nested_file = nested_dir / f"sds_{project_id}.zip"
     nested_file.write_text("some_data")
     return nested_dir
 
 
-async def test_archive_already_exists(temp_dir, project_id):
-    tmp_dir_to_compress = temp_dir_with_existing_archive(temp_dir, project_id)
+async def test_archive_already_exists(tmp_path, project_id):
+    tmp_dir_to_compress = temp_dir_with_existing_archive(tmp_path, project_id)
     with pytest.raises(SDSException) as exc_info:
         await _compress_dir(
             folder_to_zip=tmp_dir_to_compress,
@@ -57,5 +52,5 @@ async def test_archive_already_exists(temp_dir, project_id):
     assert exc_info.type is SDSException
     assert (
         exc_info.value.args[0]
-        == f"Cannot archive '{temp_dir}/nested' because '{temp_dir}/nested/sds_{project_id}.zip' already exists"
+        == f"Cannot archive '{tmp_path}/nested' because '{tmp_path}/nested/sds_{project_id}.zip' already exists"
     )
