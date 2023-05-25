7aba725f3418f7cf31935bfd4e92cbc2debd2d12
matusdrobuliak66 <drobuliak66@gmail.com>
2023-05-25 10:17:20 +0200

    fixture is not needed

--- packages/pytest-simcore/src/pytest_simcore/pytest_global_environs.py
+++ packages/pytest-simcore/src/pytest_simcore/pytest_global_environs.py
@@ -4,8 +4,3 @@ import pytest
 @pytest.fixture(autouse=True)
 def sqlalchemy_2_0_warnings(monkeypatch):
     monkeypatch.setenv("SQLALCHEMY_WARN_20", "1")
-
-
-@pytest.fixture(autouse=True)
-def patch_log_format_local_dev_enabled(monkeypatch):
-    monkeypatch.setenv("LOG_FORMAT_LOCAL_DEV_ENABLED", "1")
