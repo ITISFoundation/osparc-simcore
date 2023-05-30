1d049d2d00b0d2306ab45e8f2f19ebfcf75a59b8
Andrei Neagu <neagu@itis.swiss>
2023-05-30 10:44:16 +0200

    using correct imports

--- services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/models/shared_store.py
+++ services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/models/shared_store.py
@@ -4,11 +4,10 @@ from typing import Final, TypeAlias
 
 import aiofiles
 from fastapi import FastAPI
-from models_library.volumes import VolumeCategory, VolumeStatus
+from models_library.volumes import VolumeCategory, VolumeState, VolumeStatus
 from pydantic import BaseModel, Field, PrivateAttr
 
 from ..core.settings import ApplicationSettings
-from .volumes import VolumeState
 
 ContainerNameStr: TypeAlias = str
 
