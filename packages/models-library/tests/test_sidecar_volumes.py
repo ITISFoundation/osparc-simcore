14a4d762f67d7f517dedb2eb8045d7ddce3c3aa5
Andrei Neagu <neagu@itis.swiss>
2023-05-30 13:03:29 +0200

    update test name and notes

--- packages/models-library/tests/test_sidecar_volumes.py
+++ packages/models-library/tests/test_sidecar_volumes.py
@@ -10,7 +10,9 @@ def status(request: FixtureRequest) -> VolumeStatus:
     return request.param
 
 
-def test_volume_state_equality(status: VolumeStatus):
+def test_volume_state_equality_does_not_use_last_changed(status: VolumeStatus):
+    # NOTE: `last_changed` is initialized with the utc datetime
+    #  at the moment of the creation of the object.
     assert VolumeState(status=status) == VolumeState(status=status)
     schema_property_count = len(VolumeState.schema()["properties"])
     assert len(VolumeState(status=status).dict()) == schema_property_count
