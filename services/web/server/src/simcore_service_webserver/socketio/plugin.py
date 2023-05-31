d14b5676463408b580c93b4ad87079cdcf61a3ba
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-05-31 10:56:36 +0200

    setup of socketio now done as event. ensure PubSub server is properly stopped for tests

--- services/web/server/src/simcore_service_webserver/socketio/plugin.py
+++ services/web/server/src/simcore_service_webserver/socketio/plugin.py
@@ -9,9 +9,7 @@ from aiohttp import web
 from servicelib.aiohttp.application_setup import ModuleCategory, app_module_setup
 
 from .._constants import APP_SETTINGS_KEY
-from . import _handlers
 from ._observer import setup_socketio_observer_events
-from ._utils import register_socketio_handlers
 from .server import setup_socketio_server
 
 _logger = logging.getLogger(__name__)
@@ -28,7 +26,7 @@ def setup_socketio(app: web.Application):
     assert app[APP_SETTINGS_KEY].WEBSERVER_SOCKETIO  # nosec
 
     setup_socketio_server(app)
-    register_socketio_handlers(app, _handlers)
+
     setup_socketio_observer_events(app)
 
 
--- services/web/server/src/simcore_service_webserver/socketio/server.py
+++ services/web/server/src/simcore_service_webserver/socketio/server.py
@@ -6,13 +6,30 @@ from aiohttp import web
 from socketio import AsyncAioPikaManager, AsyncServer
 
 from ..rabbitmq_settings import get_plugin_settings as get_rabbitmq_settings
-from ._utils import APP_CLIENT_SOCKET_SERVER_KEY, get_socket_server
+from . import _handlers
+from ._utils import APP_CLIENT_SOCKET_SERVER_KEY, register_socketio_handlers
 
 _logger = logging.getLogger(__name__)
 
 
-async def _socketio_server_cleanup_ctx(_app: web.Application) -> AsyncIterator[None]:
+async def _socketio_server_cleanup_ctx(app: web.Application) -> AsyncIterator[None]:
+    # SEE https://github.com/miguelgrinberg/python-socketio/blob/v4.6.1/docs/server.rst#aiohttp
+    server_manager = AsyncAioPikaManager(
+        url=get_rabbitmq_settings(app).dsn, logger=_logger
+    )
+    sio_server = AsyncServer(
+        async_mode="aiohttp",
+        logger=_logger,  # type: ignore
+        engineio_logger=False,
+        client_manager=server_manager,
+    )
+    sio_server.attach(app)
+
+    app[APP_CLIENT_SOCKET_SERVER_KEY] = sio_server
+
+    register_socketio_handlers(app, _handlers)
     yield
+    # cast(AsyncPubSubManager, server_manager).thread.cancel()
     # NOTE: this is ugly. It seems though that python-enginio does not
     # cleanup its background tasks properly.
     # https://github.com/miguelgrinberg/python-socketio/discussions/1092
@@ -25,6 +42,7 @@ async def _socketio_server_cleanup_ctx(_app: web.Application) -> AsyncIterator[N
             for coro_name in [
                 "AsyncServer._service_task",
                 "AsyncSocket.schedule_ping",
+                "AsyncPubSubManager._thread",
             ]
         ):
             task.cancel()
@@ -32,31 +50,9 @@ async def _socketio_server_cleanup_ctx(_app: web.Application) -> AsyncIterator[N
     await asyncio.gather(*cancelled_tasks, return_exceptions=True)
 
 
-def setup_socketio_server(app: web.Application) -> AsyncServer:
-    if app.get(APP_CLIENT_SOCKET_SERVER_KEY) is None:
-        # SEE https://github.com/miguelgrinberg/python-socketio/blob/v4.6.1/docs/server.rst#aiohttp
-        server_manager = AsyncAioPikaManager(
-            url=get_rabbitmq_settings(app).dsn, logger=_logger
-        )
-        sio_server = AsyncServer(
-            async_mode="aiohttp",
-            logger=_logger,  # type: ignore
-            engineio_logger=False,
-            client_manager=server_manager,
-        )
-        sio_server.attach(app)
+def setup_socketio_server(app: web.Application) -> None:
+    app.cleanup_ctx.append(_socketio_server_cleanup_ctx)
 
-        app[APP_CLIENT_SOCKET_SERVER_KEY] = sio_server
-
-        if _socketio_server_cleanup_ctx not in app.cleanup_ctx:
-            app.cleanup_ctx.append(_socketio_server_cleanup_ctx)
-
-        assert sio_server == get_socket_server(app)  # nosec
-
-    return get_socket_server(app)
-
-
-assert get_socket_server  # nosec
 
 __all__: tuple[str, ...] = (
     "get_socket_server",
