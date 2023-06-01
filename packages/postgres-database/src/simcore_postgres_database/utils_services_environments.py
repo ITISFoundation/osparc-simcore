6be5a1f215a57bce778466a97bb295fc93ea8c01
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-06-01 15:05:32 +0200

    @sanderegg review: adds version policy

--- packages/postgres-database/src/simcore_postgres_database/utils_services_environments.py
+++ packages/postgres-database/src/simcore_postgres_database/utils_services_environments.py
@@ -1,6 +1,7 @@
-from typing import TypeAlias
+from typing import Final, TypeAlias
 
-from sqlalchemy.sql import select
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import ARRAY, INTEGER
 
 from ._protocols import DBConnection
 from .models.services_environments import VENDOR_SECRET_PREFIX, services_vendor_secrets
@@ -8,32 +9,65 @@ from .models.services_environments import VENDOR_SECRET_PREFIX, services_vendor_
 # This constraint is to avoid deserialization issues after substitution!
 VendorSecret: TypeAlias = bool | int | float | str
 
+LATEST: Final[str] = "latest"
+
 
 async def get_vendor_secrets(
     conn: DBConnection,
     vendor_service_key: str,  # NOTE: ServiceKey is defined in model_library
+    vendor_service_version: str = LATEST,  # NOTE: ServiceVersion is defined in model_library
     *,
     normalize_names: bool = True,
 ) -> dict[str, VendorSecret]:
+
+    select_stmt = sa.select(services_vendor_secrets.c.secrets_map)
+    where_stmt = services_vendor_secrets.c.service_key == vendor_service_key
+
+    def _version(column_or_value):
+        # converts version value string to array[integer] that can be compared
+        return sa.func.string_to_array(column_or_value, ".").cast(ARRAY(INTEGER))
+
+    if vendor_service_version == LATEST:
+        latest_version = sa.select(
+            sa.func.array_to_string(
+                sa.func.max(_version(services_vendor_secrets.c.service_from_version)),
+                ".",
+            )
+        ).where(services_vendor_secrets.c.service_key == vendor_service_key)
+
+        where_stmt &= (
+            services_vendor_secrets.c.service_from_version == latest_version.subquery()
+        )
+
+        query = select_stmt.where(where_stmt)
+    else:
+        assert len([int(p) for p in vendor_service_version.split(".")]) == 3  # nosec
+
+        where_stmt &= _version(
+            services_vendor_secrets.c.service_from_version
+        ) <= _version(vendor_service_version)
+
+        query = (
+            select_stmt.where(where_stmt)
+            .order_by(_version(services_vendor_secrets.c.service_from_version).desc())
+            .limit(1)
+        )
+
+    secrets_map = await conn.scalar(query)
     secrets: dict[str, VendorSecret] = {}
 
-    secrets_map = await conn.scalar(
-        select(services_vendor_secrets.c.secrets_map).where(
-            services_vendor_secrets.c.service_key == vendor_service_key
-        )
-    )
     if secrets_map is not None:
         secrets = dict(secrets_map)
 
-    if normalize_names:
+    if secrets_map and normalize_names:
         for key in list(secrets.keys()):
             if not key.startswith(VENDOR_SECRET_PREFIX):
                 secrets[VENDOR_SECRET_PREFIX + key.upper()] = secrets.pop(key)
 
         assert all(key.startswith(VENDOR_SECRET_PREFIX) for key in secrets)  # nosec
 
-    assert all(  # nosec
-        isinstance(value, (bool, int, str, float)) for value in secrets.values()
-    )
+        assert all(  # nosec
+            isinstance(value, (bool, int, str, float)) for value in secrets.values()
+        )
 
     return secrets
--- packages/postgres-database/tests/test_utils_services_environments.py
+++ packages/postgres-database/tests/test_utils_services_environments.py
@@ -4,7 +4,7 @@
 # pylint: disable=unused-variable
 
 import random
-from typing import Any
+from typing import Any, NamedTuple
 
 import pytest
 from aiopg.sa.connection import SAConnection
@@ -17,23 +17,20 @@ from simcore_postgres_database.utils_services_environments import get_vendor_sec
 
 
 @pytest.fixture
-async def vendor_service() -> str:
+def vendor_service() -> str:
     return "simcore/services/dynamic/vendor/some_service"
+
+
+class ExpectedSecrets(NamedTuple):
+    old_secrets: dict[str, Any]
+    vendor_secrets: dict[str, Any]
 
 
 @pytest.fixture
 async def expected_secrets(
     connection: SAConnection, vendor_service: str
-) -> dict[str, Any]:
-    await connection.execute(
-        services_meta_data.insert().values(
-            key=vendor_service,
-            version="1.0.0",
-            name="some-service",
-            description="Some service from a vendor",
-        )
-    )
-
+) -> ExpectedSecrets:
+    # other-service
     await connection.execute(
         services_meta_data.insert().values(
             key="simcore/services/dynamic/vendor/other_service",
@@ -43,10 +40,38 @@ async def expected_secrets(
         )
     )
 
-    vendor_secrets = {
+    # Three versions of 'some_service'
+    for version, description in [
+        ("0.0.1", "First version from a vendor"),  # has no secrets
+        ("0.0.2", "Second version from a vendor"),  # has old secrets
+        ("1.0.0", "Third version from a vendor"),  # has secrets
+        ("1.2.0", "Lastest version from a vendor"),
+    ]:
+        await connection.execute(
+            services_meta_data.insert().values(
+                key=vendor_service,
+                version=version,
+                name="some-service",
+                description=description,
+            )
+        )
+
+    old_secrets = {
         VENDOR_SECRET_PREFIX + "LICENSE_SERVER_HOST": "product_a-server",
         VENDOR_SECRET_PREFIX + "LICENSE_SERVER_PRIMARY_PORT": 1,
         VENDOR_SECRET_PREFIX + "LICENSE_SERVER_SECONDARY_PORT": 2,
+    }
+
+    await connection.execute(
+        services_vendor_secrets.insert().values(
+            service_key=vendor_service,
+            service_from_version="0.0.2",
+            secrets_map=old_secrets,
+        )
+    )
+
+    vendor_secrets = {
+        **old_secrets,
         VENDOR_SECRET_PREFIX + "LICENSE_DNS_RESOLVER_IP": "1.1.1.1",
         VENDOR_SECRET_PREFIX + "LICENSE_DNS_RESOLVER_PORT": "21",
         VENDOR_SECRET_PREFIX + "LICENSE_FILE": "license.txt",
@@ -59,6 +84,7 @@ async def expected_secrets(
         # a vendor exposes these environs to its services to everybody
         services_vendor_secrets.insert().values(
             service_key=vendor_service,
+            service_from_version="1.0.0",  # valid from
             secrets_map={
                 (
                     key.removeprefix(VENDOR_SECRET_PREFIX)
@@ -70,14 +96,43 @@ async def expected_secrets(
         )
     )
 
-    return vendor_secrets
+    return ExpectedSecrets(old_secrets, vendor_secrets)
 
 
 def test_vendor_secret_prefix_must_end_with_underscore():
     assert VENDOR_SECRET_PREFIX.endswith("_")  # should allow
 
 
-async def test_get_vendor_secrets(
-    connection: SAConnection, vendor_service: str, expected_secrets: dict[str, Any]
+async def test_get_latest_service_vendor_secrets(
+    connection: SAConnection, vendor_service: str, expected_secrets: ExpectedSecrets
 ):
-    assert await get_vendor_secrets(connection, vendor_service) == expected_secrets
+    # latest i.e. 1.2.0
+    assert (
+        await get_vendor_secrets(connection, vendor_service)
+        == expected_secrets.vendor_secrets
+    )
+
+
+@pytest.mark.parametrize(
+    "service_version,expected_result",
+    [("0.0.1", "Empty"), ("0.0.2", "Old"), ("1.0.0", "Latest"), ("1.2.0", "Latest")],
+)
+async def test_get_service_vendor_secrets(
+    connection: SAConnection,
+    vendor_service: str,
+    expected_secrets: ExpectedSecrets,
+    service_version: str,
+    expected_result: str,
+):
+    match expected_result:
+        case "Empty":
+            expected = {}
+        case "Old":
+            expected = expected_secrets.old_secrets
+        case "Latest":
+            expected = expected_secrets.vendor_secrets
+
+    assert (
+        await get_vendor_secrets(connection, vendor_service, service_version)
+        == expected
+    )
