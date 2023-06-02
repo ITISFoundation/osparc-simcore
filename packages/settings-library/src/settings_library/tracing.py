2d23ce4c8b5d632470307384a312167eebca4ebf
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-06-02 21:57:35 +0200

    tolerate http non-secuire

--- packages/settings-library/src/settings_library/tracing.py
+++ packages/settings-library/src/settings_library/tracing.py
@@ -7,11 +7,11 @@ UNDEFINED_CLIENT_NAME = "undefined-tracing-client-name"
 
 class TracingSettings(BaseCustomSettings):
     TRACING_ZIPKIN_ENDPOINT: AnyUrl = Field(
-        default=parse_obj_as(AnyUrl, "http://jaeger:9411"),
+        default=parse_obj_as(AnyUrl, "http://jaeger:9411"),  # NOSONAR
         description="Zipkin compatible endpoint",
     )
     TRACING_THRIFT_COMPACT_ENDPOINT: AnyUrl = Field(
-        default=parse_obj_as(AnyUrl, "http://jaeger:5775"),
+        default=parse_obj_as(AnyUrl, "http://jaeger:5775"),  # NOSONAR
         description="accept zipkin.thrift over compact thrift protocol (deprecated, used by legacy clients only)",
     )
     TRACING_CLIENT_NAME: str = Field(
