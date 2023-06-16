a474c206b07ad6f79ed373215ac691cf9e841bb3
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-06-16 18:14:41 +0200

    adds new interface

--- api/specs/webserver/scripts/openapi_projects_nodes.py
+++ api/specs/webserver/scripts/openapi_projects_nodes.py
@@ -19,9 +19,12 @@ from _common import (
 from fastapi import FastAPI
 from models_library.generics import Envelope
 from models_library.projects import ProjectID
+from models_library.projects_nodes_io import NodeID
 from models_library.users import GroupID
+from pydantic import BaseModel, Field, HttpUrl
 from simcore_service_webserver.projects._handlers_crud import ProjectPathParams
 from simcore_service_webserver.projects._handlers_project_nodes import (
+    _NodePathParams,
     _ProjectGroupAccess,
 )
 
@@ -53,6 +56,44 @@ assert_handler_signature_against_model(
 )
 
 
-if __name__ == "__main__":
+class _HomePageScreenshot(BaseModel):
+    thumbnail_url: HttpUrl
+    file_url: HttpUrl
 
+
+class _ProjectNodeHomePage(BaseModel):
+    project_id: ProjectID
+    node_id: NodeID
+    screenshots: list[_HomePageScreenshot] = Field(default_factory=list)
+
+
+@app.get(
+    "/projects/{project_id}/nodes/-/homepage",
+    response_model=Envelope[list[_ProjectNodeHomePage]],
+    tags=TAGS,
+    operation_id="list_project_node_homepages",
+    summary="Lists info with all node homepages info",
+)
+async def list_project_node_homepages(project_id: ProjectID):
+    ...
+
+
+assert_handler_signature_against_model(list_project_node_homepages, ProjectPathParams)
+
+
+@app.get(
+    "/projects/{project_id}/nodes/{node_id}/homepage",
+    response_model=Envelope[_ProjectNodeHomePage],
+    tags=TAGS,
+    operation_id="get_project_node_homepage",
+    summary="Gets info on a given node's homepage",
+)
+async def get_project_node_homepage(project_id: ProjectID, node_id: NodeID):
+    ...
+
+
+assert_handler_signature_against_model(get_project_node_homepage, _NodePathParams)
+
+
+if __name__ == "__main__":
     create_openapi_specs(app, CURRENT_DIR.parent / "openapi-projects-pydantic.yaml")
