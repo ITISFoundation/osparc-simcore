33e466f794edd29824405b18b36d7dc59b54e94f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-06-24 18:10:20 +0200

    fixing test

--- services/api-server/tests/unit/api_solvers/test_api_routers_solvers_jobs_read.py
+++ services/api-server/tests/unit/api_solvers/test_api_routers_solvers_jobs_read.py
@@ -7,10 +7,10 @@ from typing import TypedDict
 
 import httpx
 import pytest
-from models_library.basic_regex import UUID_RE_BASE
-from pydantic import parse_file_as
+from pydantic import parse_file_as, parse_obj_as
 from respx import MockRouter
-from simcore_service_api_server.models.schemas.jobs import Job, JobInputs
+from simcore_service_api_server.models.pagination import LimitOffsetPage
+from simcore_service_api_server.models.schemas.jobs import Job
 from simcore_service_api_server.utils.http_calls_capture import HttpApiCallCaptureModel
 from starlette import status
 
@@ -32,11 +32,11 @@ def solver_version() -> str:
 
 @pytest.fixture
 def mocked_backend_services_apis_to_read_solver_jobs(
-    mocked_webserver_service_api: MockRouter,
-    mocked_catalog_service_api: MockRouter,
+    mocked_webserver_service_api_base: MockRouter,
+    mocked_catalog_service_api_base: MockRouter,
     project_tests_dir: Path,
 ) -> MockedBackendApiDict:
-    mock_name = "on_create_job.json"
+    mock_name = "on_list_jobs.json"
 
     # fixture
     captures = parse_file_as(
@@ -47,68 +47,68 @@ def mocked_backend_services_apis_to_read_solver_jobs(
     capture = captures[0]
     assert capture.host == "catalog"
     assert capture.method == "GET"
-    mocked_catalog_service_api.request(
-        method=capture.method, path=capture.path, name="get_service"  # GET service
+    mocked_catalog_service_api_base.request(
+        method=capture.method, path=capture.path, name="get_services"
     ).respond(status_code=capture.status_code, json=capture.response_body)
 
-    capture = captures[-1]
+    capture = captures[1]
     assert capture.host == "webserver"
-    assert capture.method == "DELETE"
-
-    mocked_webserver_service_api.delete(
-        path__regex=rf"/projects/(?P<project_id>{UUID_RE_BASE})$",
-        name="delete_project",
+    assert capture.method == "GET"
+    assert capture.name == "list_projects"
+    mocked_webserver_service_api_base.request(
+        method=capture.method,
+        path=capture.path,
+        name=capture.name,
+        params__contains={
+            "show_hidden": "true",
+            "offset": "0",
+            "search": "solvers%2Fsimcore%252Fservices%252Fcomp%252Fitis%252Fsleeper%2Freleases%2F2.0.0",
+        },
     ).respond(status_code=capture.status_code, json=capture.response_body)
 
     return MockedBackendApiDict(
-        catalog=mocked_catalog_service_api,
-        webserver=mocked_webserver_service_api,
+        catalog=mocked_catalog_service_api_base,
+        webserver=mocked_webserver_service_api_base,
     )
 
 
 @pytest.mark.acceptance_test(
-    "For https://github.com/ITISFoundation/osparc-simcore/issues/4111"
+    "https://github.com/ITISFoundation/osparc-simcore/issues/4110"
 )
 async def test_list_solver_jobs(
     auth: httpx.BasicAuth,
     client: httpx.AsyncClient,
     solver_key: str,
     solver_version: str,
-    mocked_backend_services_apis_for_create_and_delete_solver_job: MockedBackendApiDict,
+    mocked_backend_services_apis_to_read_solver_jobs: MockedBackendApiDict,
 ):
-    # list Jobs
+
+    # list jobs (w/o pagination)
+    resp = await client.get(
+        f"/v0/solvers/{solver_key}/releases/{solver_version}/jobs", auth=auth
+    )
+    assert resp.status_code == status.HTTP_200_OK
+    jobs = parse_obj_as(list[Job], resp.json())
+
+    # list jobs (w/ pagination)
     resp = await client.get(
         f"/v0/solvers/{solver_key}/releases/{solver_version}/jobs",
         auth=auth,
-        json=JobInputs(
-            values={
-                "x": 3.14,
-                "n": 42,
-            }
-        ).dict(),
-    )
-    assert resp.status_code == status.HTTP_200_OK
-    job = Job.parse_obj(resp.json())
-
-    # Delete Job after creation
-    resp = await client.delete(
-        f"/v0/solvers/{solver_key}/releases/{solver_version}/jobs/{job.id}",
-        auth=auth,
+        params={"limits": 20},
     )
     assert resp.status_code == status.HTTP_204_NO_CONTENT
 
-    mock_webserver_router = (
-        mocked_backend_services_apis_for_create_and_delete_solver_job["webserver"]
-    )
-    assert mock_webserver_router
-    assert mock_webserver_router["delete_project"].called
+    jobs_page = parse_obj_as(LimitOffsetPage[Job], resp.json())
 
-    mock_catalog_router = mocked_backend_services_apis_for_create_and_delete_solver_job[
-        "catalog"
+    assert jobs_page.items == jobs
+
+    # check calls to the deep-backend services
+    mock_webserver_router = mocked_backend_services_apis_to_read_solver_jobs[
+        "webserver"
     ]
+    assert mock_webserver_router
+    assert mock_webserver_router["list_projects"].called
+
+    mock_catalog_router = mocked_backend_services_apis_to_read_solver_jobs["catalog"]
     assert mock_catalog_router
     assert mock_catalog_router["get_service"].called
-
-    # NOTE: ideas for further tests
-    # Run job and try to delete while running
-    # Run a job and delete when finished
