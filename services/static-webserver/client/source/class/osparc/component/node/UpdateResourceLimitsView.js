0ad84758a92cfce751c1939ca6a4d0429029fb7f
Odei Maiz <maiz@itis.swiss>
2023-06-26 11:32:22 +0200

    PUT new limits

--- services/static-webserver/client/source/class/osparc/component/node/UpdateResourceLimitsView.js
+++ services/static-webserver/client/source/class/osparc/component/node/UpdateResourceLimitsView.js
@@ -23,6 +23,9 @@ qx.Class.define("osparc.component.node.UpdateResourceLimitsView", {
   },
 
   members: {
+    __resourceFields: null,
+    __saveBtn: null,
+
     _applyNode: function(node) {
       if (node.isComputational() || node.isDynamic()) {
         this.__populateLayout();
@@ -30,6 +33,7 @@ qx.Class.define("osparc.component.node.UpdateResourceLimitsView", {
     },
 
     __populateLayout: function() {
+      this.__resourceFields = [];
       this._removeAll();
 
       this._add(new qx.ui.basic.Label(this.tr("Update Service Limits")).set({
@@ -37,6 +41,7 @@ qx.Class.define("osparc.component.node.UpdateResourceLimitsView", {
       }));
 
       const resourcesLayout = osparc.info.ServiceUtils.createResourcesInfo(false);
+      resourcesLayout.exclude();
       this._add(resourcesLayout);
 
       const node = this.getNode();
@@ -84,6 +89,14 @@ qx.Class.define("osparc.component.node.UpdateResourceLimitsView", {
                       singleStep: 0.1,
                       enabled: !(resourceKey === "VRAM")
                     });
+                    const nf = new qx.util.format.NumberFormat();
+                    nf.setMinimumFractionDigits(1);
+                    nf.setMaximumFractionDigits(1);
+                    spinner.setNumberFormat(nf);
+
+                    spinner.imageName = imageName;
+                    spinner.resourceKey = resourceKey;
+                    this.__resourceFields.push(spinner);
                     layout.add(spinner, {
                       row,
                       column
@@ -95,10 +108,57 @@ qx.Class.define("osparc.component.node.UpdateResourceLimitsView", {
               });
             }
           });
+
+          const buttonsLayout = new qx.ui.container.Composite(new qx.ui.layout.HBox(5));
+          this._add(buttonsLayout);
+
+          const resetBtn = new qx.ui.form.Button(this.tr("Reset"));
+          resetBtn.addListener("execute", () => this.__populateLayout(), this);
+          buttonsLayout.add(resetBtn);
+
+          const saveBtn = this.__saveBtn = new osparc.ui.form.FetchButton(this.tr("Save")).set({
+            center: true,
+            appearance: "strong-button"
+          });
+          saveBtn.addListener("execute", () => this.__saveValues(serviceResources), this);
+          buttonsLayout.add(saveBtn);
         })
         .catch(err => console.error(err));
 
       return resourcesLayout;
+    },
+
+    __saveValues: function(serviceResources) {
+      this.__saveBtn.setFetching(true);
+      const updatedResources = osparc.utils.Utils.deepCloneObject(serviceResources);
+      this.__resourceFields.forEach(resourceField => {
+        if (
+          resourceField.imageName in updatedResources &&
+          resourceField.resourceKey in updatedResources[resourceField.imageName].resources
+        ) {
+          updatedResources[resourceField.imageName].resources[resourceField.resourceKey] = resourceField.getValue();
+        }
+      });
+      const params = {
+        url: {
+          studyId: this.getStudyId(),
+          nodeId: this.getNodeId()
+        },
+        data: updatedResources
+      };
+      osparc.data.Resources.fetch("nodesInStudyResources", params)
+        .then(updatedStudy => {
+          osparc.component.message.FlashMessenger.getInstance().logAs(this.tr("Limits successfully updated"));
+          this.fireEvent("limitsChanged");
+        })
+        .catch(err => {
+          osparc.component.message.FlashMessenger.getInstance().logAs(this.tr("Something went wrong Updating the limits"), "ERROR");
+          console.error(err);
+        })
+        .finally(() => {
+          this.__saveBtn.setFetching(false);
+          this.__populateLayout();
+        });
     }
   }
 });
