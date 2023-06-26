3d68a7a18fb18d088f77a8ead274af4cb69b8669
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-06-26 17:18:02 +0200

    projects metadata table

---
+++ packages/postgres-database/src/simcore_postgres_database/models/projects_metadata.py
@@ -0,0 +1,53 @@
+"""
+    These tables were designed to be controled by projects-plugin in
+    the webserver's service
+"""
+
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import JSONB
+
+from ._common import (
+    column_created_datetime,
+    column_modified_datetime,
+    register_modified_datetime_auto_update_trigger,
+)
+from .base import metadata
+from .projects import projects
+
+projects_metadata = sa.Table(
+    "projects_metadata",
+    #
+    # Holds runtime metadata on a project.
+    #
+    # Things like 'stars', 'quality', 'classifiers' etc (or any kind of stats)
+    # should be moved here.
+    #
+    metadata,
+    sa.Column(
+        "project_uuid",
+        sa.String,
+        sa.ForeignKey(
+            projects.c.uuid,
+            onupdate="CASCADE",
+            ondelete="CASCADE",
+            name="fk_projects_metadata_project_uuid",
+        ),
+        nullable=False,
+        primary_key=True,
+        doc="The project unique identifier is also used to identify the associated job",
+    ),
+    sa.Column(
+        "user_metadata",
+        JSONB,
+        nullable=False,
+        server_default=sa.text("'{}'::jsonb"),
+        doc="Free json for user to store her metadata",
+    ),
+    # TIME STAMPS ----
+    column_created_datetime(timezone=True),
+    column_modified_datetime(timezone=True),
+    sa.PrimaryKeyConstraint("project_uuid"),
+)
+
+
+register_modified_datetime_auto_update_trigger(projects_metadata)
