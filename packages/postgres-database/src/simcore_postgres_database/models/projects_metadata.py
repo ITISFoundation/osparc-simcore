f064d2952ad735eab5feed1eef31fdffc613a728
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-06-27 19:08:44 +0200

    all tables together

--- packages/postgres-database/src/simcore_postgres_database/models/projects_metadata.py
+++ packages/postgres-database/src/simcore_postgres_database/models/projects_metadata.py
@@ -51,3 +51,52 @@ projects_metadata = sa.Table(
 
 
 register_modified_datetime_auto_update_trigger(projects_metadata)
+
+
+projects_jobs_metadata = sa.Table(
+    "projects_jobs_metadata",
+    #
+    # Every job is mapped to a project and has an ancestor (see job_parent_name)
+    # but not every project is associated to a job.
+    #
+    # This table
+    #   - holds all projects associated to jobs
+    #   - stores jobs ancestry relations and metadata
+    #
+    metadata,
+    sa.Column(
+        "project_uuid",
+        sa.String,
+        sa.ForeignKey(
+            projects.c.uuid,
+            onupdate="CASCADE",
+            ondelete="CASCADE",
+            name="fk_projects_jobs_metadata_project_uuid",
+        ),
+        nullable=False,
+        primary_key=True,
+        doc="The project unique identifier is also used to identify the associated job",
+    ),
+    sa.Column(
+        "parent_name",
+        sa.String,
+        nullable=False,
+        doc="Project's ancestor when create as a job. A project can be created as a"
+        " - solver job: solver name (e.g. /v0/solvers/{id}/releases/{version})"
+        " - study job: study name (e.g. /v0/studies/{id})",
+    ),
+    sa.Column(
+        "job_metadata",
+        JSONB,
+        nullable=True,
+        server_default=sa.text("'{}'::jsonb"),
+        doc="Job can store here metadata. "
+        "Preserves class information during serialization/deserialization",
+    ),
+    # TIME STAMPS ----
+    column_created_datetime(timezone=True),
+    column_modified_datetime(timezone=True),
+    sa.PrimaryKeyConstraint("project_uuid"),
+)
+
+register_modified_datetime_auto_update_trigger(projects_jobs_metadata)
