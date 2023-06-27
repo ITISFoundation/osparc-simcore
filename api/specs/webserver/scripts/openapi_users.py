e6660a85e073f5e9174242c1aaf5555f21365adb
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-06-27 08:32:47 +0200

    initial openapi

---
+++ api/specs/webserver/scripts/openapi_users.py
@@ -0,0 +1,132 @@
+""" Helper script to generate OAS automatically
+"""
+
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+# pylint: disable=too-many-arguments
+
+
+from enum import Enum
+
+from fastapi import FastAPI, status
+from models_library.generics import Envelope
+from simcore_service_webserver.users._handlers import (
+    PermissionGet,
+    UserNotificationsGet,
+)
+from simcore_service_webserver.users.schemas import ProfileGet, Token
+
+# from simcore_service_webserver.users._handlers import
+
+app = FastAPI(redoc_url=None)
+
+TAGS: list[str | Enum] = [
+    "user",
+]
+
+
+@app.get(
+    "/me",
+    response_model=Envelope[ProfileGet],
+    tags=TAGS,
+    operation_id="get_my_profile",
+)
+async def get_user_profile():
+    ...
+
+
+@app.get(
+    "/me/tokens",
+    response_model=Envelope[list[Token]],
+    tags=TAGS,
+    operation_id="list_tokens",
+)
+async def list_tokens():
+    ...
+
+
+@app.post(
+    "/me/tokens",
+    response_model=Envelope[Token],
+    tags=TAGS,
+    operation_id="create_token",
+)
+async def create_token():
+    ...
+
+
+@app.get(
+    "/me/tokens/{token_id}",
+    response_model=Envelope[Token],
+    tags=TAGS,
+    operation_id="get_token",
+)
+async def get_token():
+    ...
+
+
+@app.put(
+    "/me/tokens/{token_id}",
+    response_model=Envelope[Token],
+    tags=TAGS,
+    operation_id="update_token",
+)
+async def update_token():
+    ...
+
+
+@app.delete(
+    "/me/tokens/{token_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    tags=TAGS,
+    operation_id="delete_token",
+)
+async def delete_token():
+    ...
+
+
+@app.get(
+    "/me/notifications",
+    response_model=Envelope[list[UserNotificationsGet]],
+    tags=TAGS,
+    operation_id="get_user_notifications",
+)
+async def get_user_notifications():
+    ...
+
+
+@app.post(
+    "/me/notifications",
+    status_code=status.HTTP_204_NO_CONTENT,
+    tags=TAGS,
+    operation_id="post_user_notification",
+)
+async def post_user_notification():
+    ...
+
+
+@app.patch(
+    "/me/notifications/{notification_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    tags=TAGS,
+    operation_id="update_user_notification",
+)
+async def update_user_notification():
+    ...
+
+
+@app.get(
+    "/me/permissions",
+    response_model=Envelope[list[PermissionGet]],
+    tags=TAGS,
+    operation_id="list_user_permissions",
+)
+async def list_user_permissions():
+    ...
+
+
+if __name__ == "__main__":
+    from _common import CURRENT_DIR, create_openapi_specs
+
+    create_openapi_specs(app, CURRENT_DIR.parent / "openapi-user.yaml")
