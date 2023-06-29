f6f8d963300f141648ea91e92b741345a4617e70
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-06-29 23:08:18 +0200

    updates

--- services/api-server/src/simcore_service_api_server/db/tables.py
+++ services/api-server/src/simcore_service_api_server/db/tables.py
@@ -1,16 +1,26 @@
+from typing import TypeAlias
+
 from simcore_postgres_database.models.api_keys import api_keys
 from simcore_postgres_database.models.groups import GroupType, groups, user_to_groups
 from simcore_postgres_database.models.users import UserRole, UserStatus, users
 
-metadata = api_keys.metadata
+metadata: TypeAlias = api_keys.metadata
 
-__all__ = [
+assert api_keys  # nosec
+assert groups  # nosec
+assert GroupType  # nosec
+assert user_to_groups  # nosec
+assert UserRole  # nosec
+assert users  # nosec
+assert UserStatus  # nosec
+
+__all__: tuple[str, ...] = (
     "api_keys",
-    "users",
     "groups",
-    "user_to_groups",
-    "metadata",
-    "UserStatus",
-    "UserRole",
     "GroupType",
-]
+    "metadata",
+    "user_to_groups",
+    "UserRole",
+    "users",
+    "UserStatus",
+)
