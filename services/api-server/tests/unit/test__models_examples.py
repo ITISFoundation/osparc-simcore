365785663ad297e0c882554fb0b375cd7a974a8f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-06-30 00:27:28 +0200

    cleanup tests

--- services/api-server/tests/unit/test__models_examples.py
+++ services/api-server/tests/unit/test__models_examples.py
@@ -7,11 +7,9 @@ import json
 from typing import Any
 
 import pytest
-import simcore_service_api_server.models.domain
+import simcore_service_api_server.models
 from pydantic import BaseModel
 from pytest_simcore.pydantic_models import walk_model_examples_in_package
-
-assert simcore_service_api_server.models  # nosec
 
 
 @pytest.mark.parametrize(
@@ -21,6 +19,6 @@ assert simcore_service_api_server.models  # nosec
 def test_api_server_model_examples(
     model_cls: type[BaseModel], example_name: int, example_data: Any
 ):
-    print(example_name, ":", json.dumps(example_data))
-    assert model_cls.parse_obj(example_data)
-    assert model_cls.parse_obj(example_data)
+    assert model_cls.parse_obj(
+        example_data
+    ), f"Failed {example_name} : {json.dumps(example_data)}"
