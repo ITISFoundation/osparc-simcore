ee5a60ed27491e825b898e13d1df9aa1eb297359
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-07-03 15:19:45 +0200

    ruff

--- services/web/server/src/simcore_service_webserver/users/_db.py
+++ services/web/server/src/simcore_service_webserver/users/_db.py
@@ -10,7 +10,6 @@ from simcore_postgres_database.utils_groups_extra_properties import (
     GroupExtraPropertiesNotFound,
     GroupExtraPropertiesRepo,
 )
-from sqlalchemy.sql import func
 
 from ..db.models import user_to_groups
 from ..db.plugin import get_database_engine
@@ -24,13 +23,12 @@ async def do_update_expired_users(conn: SAConnection) -> list[UserID]:
         .where(
             (users.c.expires_at.is_not(None))
             & (users.c.status == UserStatus.ACTIVE)
-            & (users.c.expires_at < func.now())
+            & (users.c.expires_at < sa.sql.func.now())
         )
         .returning(users.c.id)
     )
     if rows := await result.fetchall():
-        expired = [r.id for r in rows]
-        return expired
+        return [r.id for r in rows]
     return []
 
 
