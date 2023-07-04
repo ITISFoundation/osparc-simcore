0a25f51fe60fe9dbd90035a2a19eb4ee16960522
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-07-04 21:11:29 +0200

    only one table

--- packages/postgres-database/src/simcore_postgres_database/models/projects_metadata.py
+++ packages/postgres-database/src/simcore_postgres_database/models/projects_metadata.py
@@ -17,10 +17,14 @@ from .projects import projects
 projects_metadata = sa.Table(
     "projects_metadata",
     #
-    # Holds **runtime** metadata on a project
+    # Keeps "third-party" metadata attached to a project
     #
-    # Things like 'stars', 'quality', 'classifiers' etc (or any kind of stats)
-    # should be moved here.
+    # These SHOULD NOT be actual properties of the project (e.g. uuid, name etc)
+    # but rather information attached by third-parties that "decorate" or qualify
+    # a project resource
+    #
+    # Things like 'stars', 'quality', 'classifiers', 'dev', etc (or any kind of stats)
+    # should be moved here
     #
     metadata,
     sa.Column(
@@ -41,9 +45,9 @@ projects_metadata = sa.Table(
         JSONB,
         nullable=False,
         server_default=sa.text("'{}'::jsonb"),
-        doc="Free json for user to store her metadata.",
+        doc="Unstructured free json for user to store metadata",
     ),
-    # TIME STAMPS ----
+    # TIME STAMPS ----ß
     column_created_datetime(timezone=True),
     column_modified_datetime(timezone=True),
     sa.PrimaryKeyConstraint("project_uuid"),
@@ -51,52 +55,3 @@ projects_metadata = sa.Table(
 
 
 register_modified_datetime_auto_update_trigger(projects_metadata)
-
-
-projects_jobs_metadata = sa.Table(
-    "projects_jobs_metadata",
-    #
-    # Every job is mapped to a project and has an ancestor (see job_parent_name)
-    # but not every project is associated to a job.
-    #
-    # This table contains specific metadata on projects/jobs
-    #   - holds all projects associated to jobs
-    #   - stores jobs ancestry relations and metadata
-    #
-    metadata,
-    sa.Column(
-        "project_uuid",
-        sa.String,
-        sa.ForeignKey(
-            projects.c.uuid,
-            onupdate="CASCADE",
-            ondelete="CASCADE",
-            name="fk_projects_jobs_metadata_project_uuid",
-        ),
-        nullable=False,
-        primary_key=True,
-        doc="The project unique identifier is also used to identify the associated job",
-    ),
-    sa.Column(
-        "parent_name",
-        sa.String,
-        nullable=False,
-        doc="Project's ancestor when create as a job. A project can be created as a"
-        " - solver job: solver name (e.g. /v0/solvers/{id}/releases/{version})"
-        " - study job: study name (e.g. /v0/studies/{id})",
-    ),
-    sa.Column(
-        "job_metadata",
-        JSONB,
-        nullable=True,
-        server_default=sa.text("'{}'::jsonb"),
-        doc="Job can store here metadata. "
-        "Preserves class information during serialization/deserialization",
-    ),
-    # TIME STAMPS ----
-    column_created_datetime(timezone=True),
-    column_modified_datetime(timezone=True),
-    sa.PrimaryKeyConstraint("project_uuid"),
-)
-
-register_modified_datetime_auto_update_trigger(projects_jobs_metadata)
