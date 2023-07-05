f7266dad249cbb96c7785f1424011a2d6d073b50
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-07-05 18:37:45 +0200

    project metadata repo ready

--- packages/postgres-database/src/simcore_postgres_database/utils_projects_metadata.py
+++ packages/postgres-database/src/simcore_postgres_database/utils_projects_metadata.py
@@ -5,10 +5,12 @@ from typing import Any
 
 import sqlalchemy as sa
 from aiopg.sa.connection import SAConnection
-from aiopg.sa.results import ResultProxy
-from simcore_postgres_database.models.projects_metadata import projects_jobs_metadata
+from aiopg.sa.result import ResultProxy, RowProxy
+from simcore_postgres_database.models.projects_metadata import projects_metadata
+from sqlalchemy.dialects.postgresql import insert as pg_insert
 
 from .errors import ForeignKeyViolation
+from .models.projects import projects
 from .utils_models import FromRowMixin
 
 #
@@ -20,26 +22,16 @@ class ProjectNotFoundError(Exception):
     code = "projects.not_found"
 
 
-class BaseProjectJobMetadataError(Exception):
-    ...
-
-
-class ProjectJobMetadataNotFoundError(BaseProjectJobMetadataError):
-    code = "projects.job_metadata.not_found"
-
-
 #
 # Data
 #
 
 
 @dataclass(frozen=True, slots=True, kw_only=True)
-class ProjectJobMetadata(FromRowMixin):
-    project_uuid: uuid.UUID
-    parent_name: str
-    job_metadata: dict[str, Any] = {}
-    created: datetime.datetime
-    modified: datetime.datetime
+class ProjectMetadata(FromRowMixin):
+    custom_metadata: dict[str, Any] | None
+    created: datetime.datetime | None
+    modified: datetime.datetime | None
 
 
 #
@@ -47,106 +39,57 @@ class ProjectJobMetadata(FromRowMixin):
 #
 
 
-@dataclass(frozen=True, slots=True, kw_only=True)
-class ProjectJobMetadataRepo:
-    api_vtag: str = "v0"
-
-    def _get_parent_name(
-        self,
-        service_key: str,
-        service_version: str,
-    ):
-        return f"/{self.api_vtag}/solvers/{service_key}/releases/{service_version}"
-
-    async def create_solver_job(
-        self,
-        connection: SAConnection,
-        project_uuid: uuid.UUID,  # pk
-        service_key: str,
-        service_version: str,
-        job_metadata: dict[str, Any] | None = None,
-    ) -> ProjectJobMetadata:
-
-        values: dict[str, Any] = {
-            "project_uuid": project_uuid,
-            "parent_name": self._get_parent_name(service_key, service_version),
-        }
-        if job_metadata:
-            values["job_metadata"] = job_metadata
-
-        try:
-            insert_stmt = (
-                projects_jobs_metadata.insert()
-                .values(**values)
-                .returning(*[projects_jobs_metadata.columns.keys()])
+class ProjectMetadataRepo:
+    @staticmethod
+    async def get(connection: SAConnection, project_uuid: uuid.UUID) -> ProjectMetadata:
+        # JOIN LEFT OUTER
+        get_stmt = (
+            sa.select(
+                projects.c.uuid,
+                projects_metadata.c.custom_metadata,
+                projects_metadata.c.created,
+                projects_metadata.c.modified,
             )
-            result: ResultProxy = await connection.execute(insert_stmt)
-            row = await result.first()
-
-            return ProjectJobMetadata.from_row(row)
-
-        except ForeignKeyViolation as exc:
-            msg = f"Cannot create metadata without a valid project {project_uuid=}"
-            raise ProjectNotFoundError(msg) from exc
-
-    async def list_solver_jobs(
-        self,
-        connection: SAConnection,
-        service_key: str,
-        service_version: str,
-        limit: int,
-        offset: int,
-    ) -> list[ProjectJobMetadata]:
-        assert limit > 0  # nosec
-        assert offset >= 0  # nosec
-
-        parent_name = self._get_parent_name(service_key, service_version)
-        list_stmt = (
-            sa.select(projects_jobs_metadata)
-            .where(projects_jobs_metadata.c.parent_name == parent_name)
-            .order_by(projects_jobs_metadata.c.created_at)
-            .offset(offset)
-            .limit(limit)
-        )
-        result: ResultProxy = await connection.execute(list_stmt)
-        rows = await result.fetchall()
-        return [ProjectJobMetadata.from_row(row) for row in rows]
-
-    async def get(
-        self, connection: SAConnection, project_uuid: uuid.UUID
-    ) -> ProjectJobMetadata:
-        get_stmt = sa.select(projects_jobs_metadata).where(
-            projects_jobs_metadata.c.project_uuid == f"{project_uuid}"
+            .select_from(
+                sa.join(
+                    projects,
+                    projects_metadata,
+                    projects.c.uuid == projects_metadata.c.project_uuid,
+                    isouter=True,
+                )
+            )
+            .where(projects.c.uuid == f"{project_uuid}")
         )
         result: ResultProxy = await connection.execute(get_stmt)
-        if row := await result.first():
-            return ProjectJobMetadata.from_row(row)
-        raise ProjectJobMetadataNotFoundError
+        row: RowProxy | None = await result.first()
+        if row is None:
+            msg = f"Project project_uuid={project_uuid!r} not found"
+            raise ProjectNotFoundError(msg)
+        return ProjectMetadata.from_row(row)
 
-    async def update(
-        self,
+    @staticmethod
+    async def upsert(
         connection: SAConnection,
         *,
         project_uuid: uuid.UUID,
-        job_metadata: dict[str, Any],
-    ) -> ProjectJobMetadata:
+        custom_metadata: dict[str, Any],
+    ) -> ProjectMetadata:
+        data = {
+            "project_uuid": f"{project_uuid}",
+            "custom_metadata": custom_metadata,
+        }
 
-        update_stmt = (
-            projects_jobs_metadata.update()
-            .where(projects_jobs_metadata.c.project_uuid == project_uuid)
-            .values(projects_jobs_metadata.c.job_metadata == job_metadata)
-            .returning(*list(projects_jobs_metadata.columns))
-        )
-        result = await connection.execute(update_stmt)
-        if row := await result.first():
-            return ProjectJobMetadata.from_row(row)
-        raise ProjectJobMetadataNotFoundError
+        try:
+            insert_stmt = pg_insert(projects_metadata).values(**data)
+            upsert_stmt = insert_stmt.on_conflict_do_update(
+                index_elements=[projects_metadata.c.project_uuid],
+                set_=data,
+            ).returning(sa.literal_column("*"))
 
-    async def delete(self, connection: SAConnection, project_uuid: uuid.UUID) -> None:
-        delete_stmt = sa.delete(projects_jobs_metadata).where(
-            projects_jobs_metadata.c.project_uuid == f"{project_uuid}"
-        )
-        result = await connection.execute(delete_stmt)
-        if result.rowcount:
-            msg = f"Could not delete non-existing metadata of project_uuid={project_uuid!r}"
-            raise ProjectJobMetadataNotFoundError(msg)
+            result: ResultProxy = await connection.execute(upsert_stmt)
+            row: RowProxy | None = await result.first()
+            assert row  # nosec
+            return ProjectMetadata.from_row(row)
+
+        except ForeignKeyViolation as err:
+            raise ProjectNotFoundError(project_uuid) from err
--- packages/postgres-database/tests/test_utils_projects_metadata.py
+++ packages/postgres-database/tests/test_utils_projects_metadata.py
@@ -4,19 +4,15 @@
 # pylint: disable=too-many-arguments
 
 from collections.abc import Awaitable, Callable
-from typing import Any
-from uuid import UUID
 
 import pytest
-import sqlalchemy as sa
 from aiopg.sa.connection import SAConnection
-from aiopg.sa.result import ResultProxy, RowProxy
-from simcore_postgres_database.models.projects import projects
-from simcore_postgres_database.models.projects_metadata import (
-    projects_jobs_metadata,
-    projects_metadata,
+from aiopg.sa.result import RowProxy
+from faker import Faker
+from simcore_postgres_database.utils_projects_metadata import (
+    ProjectMetadataRepo,
+    ProjectNotFoundError,
 )
-from sqlalchemy.dialects.postgresql import insert as pg_insert
 
 
 @pytest.fixture
@@ -38,99 +34,50 @@ async def fake_project(
     return project
 
 
-async def test_jobs_workflow(
+async def test_projects_metadata_repository(
     connection: SAConnection,
-    create_fake_project: Callable[..., Awaitable[RowProxy]],
     create_fake_user: Callable[..., Awaitable[RowProxy]],
+    create_fake_project: Callable[..., Awaitable[RowProxy]],
+    faker: Faker,
 ):
     user: RowProxy = await create_fake_user(connection)
+    project: RowProxy = await create_fake_project(connection, user, hidden=True)
 
-    async def _create_solver_job(service_key: str, service_version: str, n: int):
-        parent_name = f"/v0/solvers/{service_key}/releases/{service_version}"
-        project: RowProxy = await create_fake_project(connection, user, hidden=True)
+    # subresource is attached to parent
+    user_metadata = {"float": 3.14, "int": 42, "string": "foo", "bool": True}
 
-        query = projects_jobs_metadata.insert().values(
-            project_uuid=project.uuid,
-            parent_name=parent_name,
-            job_metadata={
-                "__type__": "JobMeta",
-                "inputs_checksum": f"{n}2bfd4885aa1daf5c16fdd39b9118f652c4977c4021c900794dc125cf123718e",
-                "created_at": f"2022-06-01T15:{n}:56.807441",
-            },
+    with pytest.raises(ProjectNotFoundError):
+        await ProjectMetadataRepo.get(connection, project_uuid=faker.uuid4())
+
+    with pytest.raises(ProjectNotFoundError):
+        await ProjectMetadataRepo.upsert(
+            connection, project_uuid=faker.uuid4(), custom_metadata=user_metadata
         )
-        result: ResultProxy = await connection.execute(query)
-        assert result
-        return project.uuid
 
-    # some project from the UI
-    project_study = await create_fake_project(connection, user, hidden=True)
+    pm = await ProjectMetadataRepo.get(connection, project_uuid=project["uuid"])
+    assert pm is not None
+    assert pm.custom_metadata is None
 
-    # CREATE
-    # some solver-job projects
-    created_jobs: list[UUID] = [
-        await _create_solver_job(
-            service_key="simcore/comp/itis/sleeper", service_version="2.0.0", n=n
-        )
-        for n in range(3)
-    ]
-
-    assert project_study.uuid not in set(created_jobs)
-
-    # READ
-    async def _list_solver_jobs(service_key: str, service_version: str):
-        # list jobs of a solver
-        parent_name = f"/v0/solvers/{service_key}/releases/{service_version}"
-
-        j = projects.join(
-            projects_jobs_metadata,
-            (projects.c.uuid == projects_jobs_metadata.c.project_uuid),
-        )
-        query = (
-            sa.select(projects_jobs_metadata, projects.c.hidden)
-            .select_from(j)
-            .where(projects_jobs_metadata.c.parent_name == parent_name)
-        )
-        jobs = await (await connection.execute(query)).fetchall()
-        assert jobs
-        return jobs
-
-    got_jobs = await _list_solver_jobs(
-        service_key="simcore/comp/itis/sleeper", service_version="2.0.0"
+    got = await ProjectMetadataRepo.upsert(
+        connection, project_uuid=project["uuid"], custom_metadata=user_metadata
     )
-    assert {j.project_uuid for j in got_jobs} == set(created_jobs)
-    assert all(j.hidden for j in got_jobs)
+    assert got.custom_metadata
+    assert user_metadata == got.custom_metadata
+
+    pm = await ProjectMetadataRepo.get(connection, project_uuid=project["uuid"])
+    assert pm is not None
+    assert pm == got
+
+    got2 = await ProjectMetadataRepo.upsert(
+        connection, project_uuid=project["uuid"], custom_metadata={}
+    )
+    assert got2.custom_metadata == {}
+    assert got.modified
+    assert got2.modified
+    assert got.modified < got2.modified
 
     # list jobs of a study
     # list all jobs of a user
     # list projects that are non-jobs
 
-    async def _upsert_custom_metadata(project_uuid, metadata: dict[str, Any]):
-        params = dict(
-            project_uuid=f"{project_uuid}",
-            custom_metadata=metadata,
-        )
-        insert_stmt = pg_insert(projects_metadata).values(**params)
-        on_update_stmt = insert_stmt.on_conflict_do_update(
-            index_elements=[
-                projects_metadata.c.project_uuid,
-            ],
-            set_=params,
-        )
-        await connection.execute(on_update_stmt)
-
-    # UPDATE custom - meta
-    await _upsert_custom_metadata(project_study.uuid, metadata={"my data": "foo"})
-    await _upsert_custom_metadata(
-        got_jobs[0].project_uuid, metadata={"jobs data": "bar"}
-    )
-
     # DELETE job by deleting project
-
-
-# test create a job
-#
-#    - from a study
-#    - from a solver
-# - search jobs from a study, from a solver, etc
-# - list studies  -> projects uuids that are not jobs
-# - list study jobs -> projects uuids that are Jodbs
