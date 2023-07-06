f2d8c2ccdfa2670b1a389576da7d4759c207b127
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-07-06 15:21:54 +0200

    fixes deletion in test

--- services/web/server/tests/unit/with_dbs/02/test_projects_metadata_handlers.py
+++ services/web/server/tests/unit/with_dbs/02/test_projects_metadata_handlers.py
@@ -16,7 +16,9 @@ from models_library.api_schemas_webserver.projects_metadata import (
 from pydantic import parse_obj_as
 from pytest_simcore.helpers.utils_assert import assert_status
 from pytest_simcore.helpers.utils_login import UserInfoDict
+from pytest_simcore.helpers.utils_webserver_unit_with_db import MockedStorageSubsystem
 from simcore_postgres_database.models.users import UserRole
+from simcore_service_webserver.projects import _crud_delete_utils
 from simcore_service_webserver.projects.models import ProjectDict
 
 
@@ -27,7 +29,10 @@ from simcore_service_webserver.projects.models import ProjectDict
     ],
 )
 async def test_custom_metadata_handlers(
-    mocked_director_v2_api: None,  # for deletion
+    # for deletion
+    mocked_director_v2_api: None,
+    storage_subsystem_mock: MockedStorageSubsystem,
+    #
     client: TestClient,
     faker: Faker,
     logged_user: UserInfoDict,
@@ -78,6 +83,14 @@ async def test_custom_metadata_handlers(
     url = client.app.router["delete_project"].url_for(project_id=user_project["uuid"])
     response = await client.delete(f"{url}")
     await assert_status(response, expected_cls=web.HTTPNoContent)
+
+    async def _wait_until_deleted():
+        tasks = _crud_delete_utils.get_scheduled_tasks(
+            project_uuid=user_project["uuid"], user_id=logged_user["id"]
+        )
+        await tasks[0]
+
+    await _wait_until_deleted()
 
     # no metadata -> project not found
     url = client.app.router["get_project_custom_metadata"].url_for(
