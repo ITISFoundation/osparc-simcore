45fbed376186dec3db52f74956fcfe752f231db5
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-07-11 14:45:04 +0200

    tests models

--- services/web/server/tests/unit/with_dbs/02/test_announcements.py
+++ services/web/server/tests/unit/with_dbs/02/test_announcements.py
@@ -1,15 +1,21 @@
 # pylint: disable=redefined-outer-name
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
+# pylint: disable=too-many-arguments
 
+import json
+from typing import Any
 
 import pytest
+import simcore_service_webserver.announcements._models
 from aiohttp import web
 from aiohttp.test_utils import TestClient
+from pydantic import BaseModel
 from pytest import MonkeyPatch
 from pytest_simcore.helpers.typing_env import EnvVarsDict
 from pytest_simcore.helpers.utils_assert import assert_status
 from pytest_simcore.helpers.utils_envs import setenvs_from_dict
+from pytest_simcore.pydantic_models import iter_model_examples_in_module
 
 
 @pytest.fixture
@@ -21,6 +27,18 @@ def app_environment(app_environment: EnvVarsDict, monkeypatch: MonkeyPatch):
             "WEBSERVER_ANNOUNCEMENTS": "1",
         },
     )
+
+
+@pytest.mark.parametrize(
+    "model_cls, example_name, example_data",
+    iter_model_examples_in_module(simcore_service_webserver.announcements._models),
+)
+def test_model_examples(
+    model_cls: type[BaseModel], example_name: int, example_data: Any
+):
+    assert model_cls.parse_obj(
+        example_data
+    ), f"Failed {example_name} : {json.dumps(example_data)}"
 
 
 async def test_list_announcements(client: TestClient):
