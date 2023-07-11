9955dd719aef57a65f24ec2779529532b7eb858b
Odei Maiz <maiz@itis.swiss>
2023-07-11 14:08:19 +0200

    fakeAnnouncements

--- services/static-webserver/client/source/class/osparc/AnnouncementsTracker.js
+++ services/static-webserver/client/source/class/osparc/AnnouncementsTracker.js
@@ -28,7 +28,28 @@ qx.Class.define("osparc.AnnouncementsTracker", {
     __announcements: null,
 
     startTracker: function() {
+      const fakeAnnouncements = [{
+        "id": "Student_Competition_2023",
+        "products": ["s4llite"],
+        "start": "2023-06-22T15:00:00.000Z",
+        "end": "2023-11-01T02:00:00.000Z",
+        "title": "Student Competition 2023",
+        "description": "For more information click <a href='https://zmt.swiss/news-and-events/news/sim4life/s4llite-student-competition-2023/' style='color: white' target='_blank'>here</a>",
+        "link": "https://zmt.swiss/news-and-events/news/sim4life/s4llite-student-competition-2023/",
+        "widgets": ["login", "ribbon"]
+      },
+      {
+        "id": "TIP_v2",
+        "products": ["tis"],
+        "start": "2023-07-22T15:00:00.000Z",
+        "end": "2023-08-01T02:00:00.000Z",
+        "title": "TIP v2",
+        "description": "For more information click <a href='https://itis.swiss/tools-and-systems/ti-planning/' style='color: white' target='_blank'>here</a>",
+        "link": "https://itis.swiss/tools-and-systems/ti-planning/",
+        "widgets": ["login", "ribbon", "user-menu"]
+      }];
       const checkAnnouncements = () => {
+        this.__setAnnouncements(fakeAnnouncements);
         osparc.data.Resources.get("announcements")
           .then(announcements => {
             if (announcements && announcements.length) {
