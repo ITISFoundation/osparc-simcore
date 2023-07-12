c7fdefeb2271728f9c547ee79725cf07fd58ec7b
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2023-07-12 14:45:01 +0200

    ✨ Frontend: Announcements (#4489)

--- services/static-webserver/client/source/class/osparc/component/notification/RibbonNotifications.js
+++ services/static-webserver/client/source/class/osparc/component/notification/RibbonNotifications.js
@@ -27,7 +27,6 @@ qx.Class.define("osparc.component.notification.RibbonNotifications", {
     this.__notifications = new qx.data.Array();
 
     this.set({
-      backgroundColor: "warning-yellow-s4l",
       alignX: "center",
       alignY: "middle",
       visibility: "excluded"
@@ -40,7 +39,7 @@ qx.Class.define("osparc.component.notification.RibbonNotifications", {
     __notifications: null,
 
     /**
-     * @param {osparc.component.notification.Notification} notification
+     * @param {osparc.component.notification.RibbonNotification} notification
      */
     addNotification: function(notification) {
       this.__notifications.push(notification);
@@ -48,7 +47,7 @@ qx.Class.define("osparc.component.notification.RibbonNotifications", {
     },
 
     /**
-     * @param {osparc.component.notification.Notification} notification
+     * @param {osparc.component.notification.RibbonNotification} notification
      */
     removeNotification: function(notification) {
       if (this.__notifications.indexOf(notification) > -1) {
@@ -57,44 +56,76 @@ qx.Class.define("osparc.component.notification.RibbonNotifications", {
       this.__updateRibbon();
     },
 
+    /**
+     * @param {osparc.component.notification.RibbonNotification} notification
+     */
+    __createNotificationUI: function(notification) {
+      const notificationLayout = new qx.ui.container.Composite(new qx.ui.layout.HBox(5, "center")).set({
+        backgroundColor: notification.getType() === "announcement" ? "strong-main" : "warning-yellow-s4l",
+        allowGrowX: true
+      });
+
+      const notificationAtom = new qx.ui.basic.Atom().set({
+        label: notification.getFullText(),
+        icon: notification.getType() === "announcement" ? null : "@FontAwesome5Solid/exclamation-triangle/14",
+        padding: notification.getType() === "announcement" ? 6 : 2,
+        height: notification.getType() === "announcement" ? 30 : 20,
+        center: true,
+        gap: 10
+      });
+      notificationAtom.getChildControl("label").set({
+        textColor: notification.getType() === "announcement" ? "white" : "black",
+        font: "text-14",
+        rich: true,
+        wrap: true,
+        selectable: true
+      });
+      notificationAtom.getChildControl("icon").set({
+        textColor: "black"
+      });
+      notificationLayout.add(notificationAtom);
+
+      if (notification.getClosable()) {
+        const closeButton = new qx.ui.form.Button(null, "@FontAwesome5Solid/times/12").set({
+          backgroundColor: "transparent",
+          textColor: notification.getType() === "announcement" ? "white" : "black",
+          alignY: "middle"
+        });
+        closeButton.addListener("tap", () => this.removeNotification(notification), this);
+        notificationLayout.add(closeButton);
+      }
+
+      if (notification.getType() === "announcement") {
+        const dontShowButton = new qx.ui.form.Button(this.tr("Don't show again")).set({
+          backgroundColor: "transparent",
+          textColor: "strong-text",
+          alignY: "middle",
+          padding: 4,
+          allowGrowX: false,
+          allowGrowY: false,
+          marginLeft: 15
+        });
+        osparc.utils.Utils.addBorder(dontShowButton, 1, qx.theme.manager.Color.getInstance().resolve("strong-text"));
+        dontShowButton.addListener("tap", () => {
+          this.removeNotification(notification);
+          osparc.utils.Utils.localCache.setDontShowAnnouncement(notification.announcementId);
+        });
+        notificationLayout.add(dontShowButton);
+      }
+
+      return notificationLayout;
+    },
+
     __updateRibbon: function() {
       this._removeAll();
       const notifications = this.__notifications;
+      // sort notifications
+      notifications.sort((a, _) => (a.getType() === "announcement" ? 1 : -1));
       if (notifications.length) {
         this.show();
         notifications.forEach(notification => {
-          const text = notification.getFullText();
-          const notificationAtom = new qx.ui.basic.Atom().set({
-            label: text,
-            icon: "@FontAwesome5Solid/exclamation-triangle/14",
-            center: true,
-            padding: 2,
-            gap: 10,
-            height: 20
-          });
-          notificationAtom.getChildControl("label").set({
-            textColor: "black",
-            font: "text-14",
-            rich: true,
-            wrap: true,
-            selectable: true
-          });
-          notificationAtom.getChildControl("icon").set({
-            textColor: "black"
-          });
-          if (notification.getClosable()) {
-            const closableLayout = new qx.ui.container.Composite(new qx.ui.layout.HBox(5, "center"));
-            closableLayout.add(notificationAtom);
-            const closeButton = new qx.ui.form.Button(null, "@FontAwesome5Solid/times/12").set({
-              backgroundColor: "transparent",
-              textColor: "black"
-            });
-            closeButton.addListener("tap", () => this.removeNotification(notification), this);
-            closableLayout.add(closeButton);
-            this._add(closableLayout);
-          } else {
-            this._add(notificationAtom);
-          }
+          const notificationUI = this.__createNotificationUI(notification);
+          this._add(notificationUI);
         });
       } else {
         this.exclude();
