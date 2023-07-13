8465b1cd18c191c1ca091fe24e2067c6d0574074
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-07-13 21:47:06 +0200

    rm openapi submodule

--- packages/service-library/tests/aiohttp/conftest.py
+++ packages/service-library/tests/aiohttp/conftest.py
@@ -1,20 +1,2 @@
 # pylint: disable=redefined-outer-name
 # pylint: disable=unused-argument
-
-from pathlib import Path
-
-import pytest
-from servicelib.aiohttp.openapi import OpenApiSpec, create_openapi_specs
-
-
-@pytest.fixture
-def petstore_spec_file(here) -> Path:
-    filepath = here / "data/oas3/petstore.yaml"
-    assert filepath.exists()
-    return filepath
-
-
-@pytest.fixture
-async def petstore_specs(petstore_spec_file) -> OpenApiSpec:
-    specs = await create_openapi_specs(petstore_spec_file)
-    return specs
