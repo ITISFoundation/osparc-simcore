65361c3a19cb1f5e67eb6d94fd5811f34ddf9a42
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2023-07-13 14:10:29 +0200

    🎨 Guest users warning message (#4492)

--- services/static-webserver/client/source/class/osparc/Application.js
+++ services/static-webserver/client/source/class/osparc/Application.js
@@ -362,7 +362,10 @@ qx.Class.define("osparc.Application", {
         .then(profile => {
           this.__connectWebSocket();
 
-          if ("expirationDate" in profile) {
+          if (osparc.auth.Data.getInstance().isGuest()) {
+            osparc.utils.Utils.createAccountMessage()
+              .then(msg => osparc.component.message.FlashMessenger.getInstance().logAs(msg, "WARNING"));
+          } else if ("expirationDate" in profile) {
             const now = new Date();
             const today = new Date(now.toISOString().slice(0, 10));
             const expirationDay = new Date(profile["expirationDate"]);
