f8a85170f4ff142e31aa05b7500ac2fce71d28a9
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-07-14 21:51:45 +0200

    fixes required_login

--- services/web/server/src/simcore_service_webserver/login/decorators.py
+++ services/web/server/src/simcore_service_webserver/login/decorators.py
@@ -1,38 +1,44 @@
 import functools
+import inspect
 
 from aiohttp import web
 from aiohttp_security.api import check_authorized
-from servicelib.aiohttp.typing_extension import Handler
+from servicelib.aiohttp.typing_extension import HandlerAnyReturn
 from servicelib.request_keys import RQT_USERID_KEY
 
 
-def login_required(handler: Handler) -> Handler:
-    """Decorator that restrict access only for authorized users.
+def login_required(handler: HandlerAnyReturn) -> HandlerAnyReturn:
+    """Decorator that restrict access only for authorized users
 
     - User is considered authorized if check_authorized(request) raises no exception
     - If authorized, it injects user_id in request[RQT_USERID_KEY]
 
-    WARNING: Apply below @router.get(... ), e.g.
+    WARNING: Add always @router. decorator FIRST, e.g.
 
-    @router.get("/foo")
-    @login_required
-    def get_foo(request: web.Request):
-        ...
-
+        @router.get("/foo")
+        @login_required
+        async def get_foo(request: web.Request):
+            ...
 
     and NOT as
 
-    @login_required
-    @router.get("/foo")
-    def get_foo(request: web.Request):
-        ...
+        @login_required
+        @router.get("/foo")
+        async def get_foo(request: web.Request):
+            ...
 
-    since the latter will register in `router` get_foo **without** the `login_required`
+    since the latter will register in `router` get_foo **without** `login_required`
     """
+    assert set(inspect.signature(handler).parameters.values()) == {  # nosec
+        inspect.Parameter(
+            name="request",
+            kind=inspect.Parameter.POSITIONAL_OR_KEYWORD,
+            annotation=web.Request,
+        )
+    }, f"Expected {handler.__name__} with request as signature, got {handler.__annotations__}"
 
     @functools.wraps(handler)
-    async def _wrapper(request: web.Request) -> web.StreamResponse:
-        assert isinstance(request, web.Request)  # nosec
+    async def _wrapper(request: web.Request):
         # WARNING: note that check_authorized is patched in some tests.
         # Careful when changing the function signature
         request[RQT_USERID_KEY] = await check_authorized(request)
