d4be45f0b9676200c0e5015fc4f0dbf2e97496c8
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-07-14 19:44:01 +0200

    fixes required_login

--- services/web/server/src/simcore_service_webserver/login/decorators.py
+++ services/web/server/src/simcore_service_webserver/login/decorators.py
@@ -9,17 +9,33 @@ from servicelib.request_keys import RQT_USERID_KEY
 def login_required(handler: Handler) -> Handler:
     """Decorator that restrict access only for authorized users.
 
-    User is considered authorized if check_authorized(request) raises no exception
+    - User is considered authorized if check_authorized(request) raises no exception
+    - If authorized, it injects user_id in request[RQT_USERID_KEY]
 
-    Keeps userid in request[RQT_USERID_KEY]
+    WARNING: Apply below @router.get(... ), e.g.
+
+    @router.get("/foo")
+    @login_required
+    def get_foo(request: web.Request):
+        ...
+
+
+    and NOT as
+
+    @login_required
+    @router.get("/foo")
+    def get_foo(request: web.Request):
+        ...
+
+    since the latter will register in `router` get_foo **without** the `login_required`
     """
 
     @functools.wraps(handler)
-    async def wrapped(request: web.Request) -> web.StreamResponse:
+    async def _wrapper(request: web.Request) -> web.StreamResponse:
         assert isinstance(request, web.Request)  # nosec
         # WARNING: note that check_authorized is patched in some tests.
         # Careful when changing the function signature
         request[RQT_USERID_KEY] = await check_authorized(request)
         return await handler(request)
 
-    return wrapped
+    return _wrapper
