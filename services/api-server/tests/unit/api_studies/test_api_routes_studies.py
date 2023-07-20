67b16785b7119096576f9856edfc700b99a53cda
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-07-20 22:54:36 +0200

    updates tests

--- services/api-server/tests/unit/api_studies/test_api_routes_studies.py
+++ services/api-server/tests/unit/api_studies/test_api_routes_studies.py
@@ -3,39 +3,71 @@
 # pylint: disable=unused-variable
 
 
-from typing import Any
+from pathlib import Path
+from typing import Any, TypedDict
 
 import httpx
 import pytest
 from fastapi import status
-from pydantic import parse_obj_as
+from pydantic import parse_file_as, parse_obj_as
 from respx import MockRouter
-from simcore_service_api_server.models.schemas.studies import Study, StudyID
+from simcore_service_api_server.models.schemas.errors import ErrorGet
+from simcore_service_api_server.models.schemas.studies import Study, StudyID, StudyPort
+from simcore_service_api_server.utils.http_calls_capture import HttpApiCallCaptureModel
 
 
-async def test_list_study_ports(
-    client: httpx.AsyncClient,
-    auth: httpx.BasicAuth,
+class MockedBackendApiDict(TypedDict):
+    catalog: MockRouter | None
+    webserver: MockRouter | None
+
+
+@pytest.fixture
+def mocked_backend(
     mocked_webserver_service_api: MockRouter,
-    fake_study_ports: list[dict[str, Any]],
-    study_id: StudyID,
-):
-    # list_study_ports
-    resp = await client.get(f"/v0/studies/{study_id}/ports", auth=auth)
-    assert resp.status_code == status.HTTP_200_OK
-    assert resp.json() == {"items": fake_study_ports, "total": len(fake_study_ports)}
+    project_tests_dir: Path,
+) -> MockedBackendApiDict:
+    mock_name = "for_test_api_routes_studies.json"
+
+    captures = {
+        c.name: c
+        for c in parse_file_as(
+            list[HttpApiCallCaptureModel], project_tests_dir / "mocks" / mock_name
+        )
+    }
+
+    for name in (
+        "get_me",
+        "list_projects",
+        "get_project",
+        "get_invalid_project",
+        "get_project_ports",
+        "get_invalid_project_ports",
+    ):
+        capture = captures[name]
+        assert capture.host == "webserver"
+
+        route = mocked_webserver_service_api.request(
+            method=capture.method,
+            path__regex=capture.path.removeprefix("/v0") + "$",
+            name=capture.name,
+        ).respond(
+            status_code=capture.status_code,
+            json=capture.response_body,
+        )
+        print(route)
+    return MockedBackendApiDict(webserver=mocked_webserver_service_api, catalog=None)
 
 
-# @pytest.mark.xfail(reason="Still not implemented")
 @pytest.mark.acceptance_test(
     "Implements https://github.com/ITISFoundation/osparc-simcore/issues/4177"
 )
 async def test_studies_workflow(
     client: httpx.AsyncClient,
     auth: httpx.BasicAuth,
-    mocked_webserver_service_api: MockRouter,
-    study_id: StudyID,
+    mocked_backend: MockRouter,
 ):
+    study_id = StudyID("25531b1a-2565-11ee-ab43-02420a000031")
+
     # list_studies
     resp = await client.get("/v0/studies", auth=auth)
     assert resp.status_code == status.HTTP_200_OK
@@ -46,7 +78,7 @@ async def test_studies_workflow(
 
     # create_study doest NOT exist -> needs to be done via GUI
     resp = await client.post("/v0/studies", auth=auth)
-    assert resp.status_code == status.HTTP_404_NOT_FOUND
+    assert resp.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
 
     # get_study
     resp = await client.get(f"/v0/studies/{study_id}", auth=auth)
@@ -55,28 +87,44 @@ async def test_studies_workflow(
     study = parse_obj_as(Study, resp.json())
     assert study.uid == study_id
 
-    # get_study metadata
-    resp = await client.get(f"/v0/studies/{study_id}/metadata", auth=auth)
+    # get ports
+    resp = await client.get(f"/v0/studies/{study_id}/ports", auth=auth)
     assert resp.status_code == status.HTTP_200_OK
-    assert resp.json()["metadata"] == {}
 
-    # update_study metadata
-    custom_metadata = {"number": 3.14, "string": "str", "boolean": False}
-    resp = await client.put(
-        f"/v0/studies/{study_id}/metadata",
-        auth=auth,
-        json=custom_metadata,
-    )
-    assert resp.status_code == status.HTTP_200_OK
-    assert resp.json()["metadata"] == custom_metadata
+    ports = parse_obj_as(list[StudyPort], resp.json()["items"])
+    assert len(ports) == (resp.json()["total"])
 
-    # other type
-    new_metadata = custom_metadata.copy()
-    new_metadata["other"] = custom_metadata.copy()  # or use json.dumps
-    resp = await client.put(
-        f"/v0/studies/{study_id}/metadata",
-        auth=auth,
-        json=custom_metadata,
+    # get_study with non-existing uuid
+    inexistent_study_id = StudyID("15531b1a-2565-11ee-ab43-02420a000031")
+    resp = await client.get(f"/v0/studies/{inexistent_study_id}", auth=auth)
+    assert resp.status_code == status.HTTP_404_NOT_FOUND
+    error = parse_obj_as(ErrorGet, resp.json())
+    assert f"{inexistent_study_id}" in error.errors[0]
+
+    resp = await client.get(f"/v0/studies/{inexistent_study_id}/ports", auth=auth)
+    assert resp.status_code == status.HTTP_404_NOT_FOUND
+    error = parse_obj_as(ErrorGet, resp.json())
+    assert f"{inexistent_study_id}" in error.errors[0]
+
+
+async def test_list_study_ports(
+    client: httpx.AsyncClient,
+    auth: httpx.BasicAuth,
+    mocked_webserver_service_api: MockRouter,
+    fake_study_ports: list[dict[str, Any]],
+    study_id: StudyID,
+):
+    # Mocks /projects/{*}/metadata/ports
+
+    mocked_webserver_service_api.get(
+        path__regex=r"/projects/(?P<project_id>[\w-]+)/metadata/ports$",
+        name="list_project_metadata_ports",
+    ).respond(
+        200,
+        json={"data": fake_study_ports},
     )
+
+    # list_study_ports
+    resp = await client.get(f"/v0/studies/{study_id}/ports", auth=auth)
     assert resp.status_code == status.HTTP_200_OK
-    assert resp.json()["metadata"]["other"] == str(new_metadata["other"])
+    assert resp.json() == {"items": fake_study_ports, "total": len(fake_study_ports)}
