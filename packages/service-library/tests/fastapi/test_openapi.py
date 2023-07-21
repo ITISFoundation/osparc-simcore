59d8474da1ff48dd2430e40a3f274b94e00ae168
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2023-07-21 13:00:51 +0200

    ⬆️  Maintenance: updates tools and tests requirements (#4480)

--- packages/service-library/tests/fastapi/test_openapi.py
+++ packages/service-library/tests/fastapi/test_openapi.py
@@ -2,7 +2,7 @@ import pytest
 import starlette.routing
 from fastapi.applications import FastAPI
 from fastapi.routing import APIRouter
-from openapi_spec_validator import validate_spec
+from openapi_spec_validator import openapi_v31_spec_validator, validate_spec
 from openapi_spec_validator.exceptions import OpenAPISpecValidatorError
 from servicelib.fastapi.openapi import (
     override_fastapi_openapi_method,
@@ -21,12 +21,16 @@ def test_naming_operation_id(app: FastAPI):
             assert isinstance(route, starlette.routing.Route)
 
 
-@pytest.mark.xfail(reason="Waiting for review of new OAS update by PC")
+@pytest.mark.xfail(
+    reason="fastapi unresolved issue. Waiting for review of new OAS update by PC"
+)
 def test_exclusive_min_openapi_issue(app: FastAPI):
     # Tests patched issues is still unresolved https://github.com/tiangolo/fastapi/issues/240
     # When this test fails, remove patch
+    # NOTE: With the latest update of openapi_spec_validator, now passes validation 3.1 but
+    # does not seem resolved. It was moved to https://github.com/tiangolo/fastapi/discussions/9140
     with pytest.raises(OpenAPISpecValidatorError):
-        validate_spec(app.openapi())
+        validate_spec(app.openapi(), validator=openapi_v31_spec_validator)
 
 
 def test_overriding_openapi_method(app: FastAPI):
@@ -43,8 +47,10 @@ def test_overriding_openapi_method(app: FastAPI):
     assert openapi
     assert isinstance(openapi, dict)
 
-    validate_spec(openapi)
+    validate_spec(openapi, validator=openapi_v31_spec_validator)
 
+    # NOTE: https://github.com/tiangolo/fastapi/issues/240 now passes validation 3.1 but
+    # does not seem resolved. It was moved to https://github.com/tiangolo/fastapi/discussions/9140
     params = openapi["paths"]["/data"]["get"]["parameters"]
     assert params == [
         {
