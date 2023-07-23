9f6fe4bc765d858f1ecc91fb663a2742622e1f3a
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-07-23 16:12:53 +0200

    cleanup projects crud

--- api/specs/webserver/scripts/openapi_projects_crud.py
+++ api/specs/webserver/scripts/openapi_projects_crud.py
@@ -9,12 +9,10 @@ This OAS are the source of truth
 # pylint: disable=too-many-arguments
 
 
-from _common import (
-    CURRENT_DIR,
-    assert_handler_signature_against_model,
-    create_and_save_openapi_specs,
-)
-from fastapi import APIRouter, FastAPI, Query, status
+from typing import Annotated
+
+from _common import CURRENT_DIR, create_and_save_openapi_specs
+from fastapi import APIRouter, Depends, FastAPI, status
 from models_library.api_schemas_webserver.projects import (
     ProjectCopyOverride,
     ProjectCreateNew,
@@ -26,12 +24,7 @@ from models_library.api_schemas_webserver.projects import (
 )
 from models_library.generics import Envelope
 from models_library.projects import ProjectID
-from models_library.rest_pagination import (
-    DEFAULT_NUMBER_OF_ITEMS_PER_PAGE,
-    MAXIMUM_NUMBER_OF_ITEMS_PER_PAGE,
-    Page,
-)
-from pydantic import NonNegativeInt
+from models_library.rest_pagination import Page
 from servicelib.aiohttp.long_running_tasks.server import TaskGet
 from simcore_service_webserver._meta import API_VTAG
 from simcore_service_webserver.projects._crud_handlers import (
@@ -63,29 +56,10 @@ router = APIRouter(
     operation_id="create_project",
 )
 async def create_project(
-    create: ProjectCreateNew | ProjectCopyOverride,
-    from_study: ProjectID
-    | None = Query(
-        None,
-        description="Option to create a project from existing template or study: from_study={study_uuid}",
-    ),
-    as_template: bool = Query(
-        False,
-        description="Option to create a template from existing project: as_template=true",
-    ),
-    copy_data: bool = Query(
-        True,
-        description="Option to copy data when creating from an existing template or as a template, defaults to True",
-    ),
-    hidden: bool = Query(
-        False,
-        description="Enables/disables hidden flag. Hidden projects are by default unlisted",
-    ),
+    _params: Annotated[_ProjectCreateParams, Depends()],
+    _create: ProjectCreateNew | ProjectCopyOverride,
 ):
     ...
-
-
-assert_handler_signature_against_model(create_project, _ProjectCreateParams)
 
 
 @router.get(
@@ -93,44 +67,8 @@ assert_handler_signature_against_model(create_project, _ProjectCreateParams)
     response_model=Page[ProjectListItem],
     operation_id="list_projects",
 )
-async def list_projects(
-    limit: int = Query(
-        default=DEFAULT_NUMBER_OF_ITEMS_PER_PAGE,
-        description="maximum number of items to return (pagination)",
-        ge=1,
-        lt=MAXIMUM_NUMBER_OF_ITEMS_PER_PAGE,
-    ),
-    offset: NonNegativeInt = Query(
-        default=0, description="index to the first item to return (pagination)"
-    ),
-    project_type: ProjectTypeAPI = Query(default=ProjectTypeAPI.all, alias="type"),
-    show_hidden: bool = Query(
-        default=False, description="includes projects marked as hidden in the listing"
-    ),
-    order_by: str
-    | None = Query(
-        default=None,
-        description="Comma separated list of fields for ordering. The default sorting order is ascending. To specify descending order for a field, users append a 'desc' suffix",
-        example="foo desc, bar",
-    ),
-    filters: str
-    | None = Query(
-        default=None,
-        description="Filters to process on the projects list, encoded as JSON",
-        example='{"tags": [1, 5], "classifiers": ["foo", "bar"]}',
-    ),
-    search: str = Query(
-        default=None,
-        description="Multi column full text search",
-        max_length=25,
-        example="My project",
-    ),
-):
+async def list_projects(_params: Annotated[_ProjectListParams, Depends()]):
     ...
-
-
-# NOTE: filters and order_by are not yet implemented
-assert_handler_signature_against_model(list_projects, _ProjectListParams)
 
 
 @router.get(
@@ -142,9 +80,6 @@ async def get_active_project(client_session_id: str):
     ...
 
 
-assert_handler_signature_against_model(get_active_project, _ProjectActiveParams)
-
-
 @router.get(
     "/projects/{project_id}",
     response_model=Envelope[ProjectGet],
@@ -154,19 +89,13 @@ async def get_project(project_id: ProjectID):
     ...
 
 
-assert_handler_signature_against_model(get_project, ProjectPathParams)
-
-
 @router.put(
     "/projects/{project_id}",
     response_model=Envelope[ProjectGet],
     operation_id="replace_project",
 )
-async def replace_project(project_id: ProjectID, replace: ProjectReplace):
+async def replace_project(project_id: ProjectID, _replace: ProjectReplace):
     """Replaces (i.e. full update) a project resource"""
-
-
-assert_handler_signature_against_model(replace_project, ProjectPathParams)
 
 
 @router.patch(
@@ -178,9 +107,6 @@ async def update_project(project_id: ProjectID, update: ProjectUpdate):
     """Partial update of a project resource"""
 
 
-assert_handler_signature_against_model(update_project, ProjectPathParams)
-
-
 @router.delete(
     "/projects/{project_id}",
     operation_id="delete_project",
@@ -188,9 +114,6 @@ assert_handler_signature_against_model(update_project, ProjectPathParams)
 )
 async def delete_project(project_id: ProjectID):
     ...
-
-
-assert_handler_signature_against_model(delete_project, ProjectPathParams)
 
 
 if __name__ == "__main__":
