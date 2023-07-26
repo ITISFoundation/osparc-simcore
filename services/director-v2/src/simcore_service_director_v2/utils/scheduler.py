a7e4c6b6f38b0d0e92b87f77b8ba957235ee8dfb
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-07-26 08:06:49 +0200

    cleanup

--- services/director-v2/src/simcore_service_director_v2/utils/scheduler.py
+++ services/director-v2/src/simcore_service_director_v2/utils/scheduler.py
@@ -1,8 +1,5 @@
-from aiopg.sa.engine import Engine
 from models_library.projects_state import RunningState
 from pydantic import PositiveInt
-
-from ..api.dependencies.database import RepoType
 
 SCHEDULED_STATES: set[RunningState] = {
     RunningState.PUBLISHED,
@@ -23,6 +20,7 @@ PROCESSING_STATES: set[RunningState] = {
     RunningState.PENDING,
     RunningState.WAITING_FOR_RESOURCES,
     RunningState.STARTED,
+    RunningState.RETRY,
 }
 
 COMPLETED_STATES: set[RunningState] = {
@@ -31,10 +29,6 @@ COMPLETED_STATES: set[RunningState] = {
     RunningState.FAILED,
     RunningState.UNKNOWN,
 }
-
-
-def get_repository(db_engine: Engine, repo_cls: type[RepoType]) -> RepoType:
-    return repo_cls(db_engine=db_engine)
 
 
 Iteration = PositiveInt
