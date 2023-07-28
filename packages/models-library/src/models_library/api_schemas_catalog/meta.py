d54039322c0479c1db31bb5bb74ebbc19a3c0377
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-07-28 14:55:58 +0200

    sonarcloud

--- packages/models-library/src/models_library/api_schemas_catalog/meta.py
+++ packages/models-library/src/models_library/api_schemas_catalog/meta.py
@@ -1,17 +1,8 @@
-import re
 from typing import Any, ClassVar
 
-from pydantic import BaseModel, ConstrainedStr, Field
+from pydantic import BaseModel, Field
 
-# TODO: review this RE
-# use https://www.python.org/dev/peps/pep-0440/#version-scheme
-# or https://www.python.org/dev/peps/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions
-#
-VERSION_RE = r"^(0|[1-9]\d*)(\.(0|[1-9]\d*)){2}(-(0|[1-9]\d*|\d*[-a-zA-Z][-\da-zA-Z]*)(\.(0|[1-9]\d*|\d*[-a-zA-Z][-\da-zA-Z]*))*)?(\+[-\da-zA-Z]+(\.[-\da-zA-Z]+)*)?$"
-
-
-class VersionStr(ConstrainedStr):
-    regex = re.compile(VERSION_RE)
+from ..basic_types import VersionStr
 
 
 class Meta(BaseModel):
--- packages/models-library/src/models_library/api_schemas_catalog/services.py
+++ packages/models-library/src/models_library/api_schemas_catalog/services.py
@@ -1,7 +1,6 @@
 from typing import Any, ClassVar
 
 from pydantic import Extra
-from pydantic.main import BaseModel
 
 from ..emails import LowerCaseEmailStr
 from ..services import ServiceDockerData, ServiceMetaData
@@ -74,7 +73,7 @@ class ServiceGet(
             "example": {
                 "name": "File Picker",
                 "thumbnail": None,
-                "description": "File Picker",
+                "description": "description",
                 "classifiers": [],
                 "quality": {},
                 "accessRights": {
@@ -106,34 +105,6 @@ class ServiceGet(
                     }
                 },
                 "owner": "redpandas@wonderland.com",
-            }
-        }
-
-
-# TODO: prototype for next iteration
-# Items are non-detailed version of resources listed
-class ServiceItem(BaseModel):
-    class Config:
-        extra = Extra.ignore
-        schema_extra: ClassVar[dict[str, Any]] = {
-            "example": {
-                "title": "File Picker",  # NEW: rename 'name' as title (so it is not confused with an identifier!)
-                "thumbnail": None,  # optional
-                "description": "File Picker",
-                "classifiers_url": "https://catalog:8080/services/a8f5a503-01d5-40bc-b416-f5b7cc5d1fa4/classifiers",
-                "quality": "https://catalog:8080/services/a8f5a503-01d5-40bc-b416-f5b7cc5d1fa4/quality",
-                "access_rights_url": "https://catalog:8080/services/a8f5a503-01d5-40bc-b416-f5b7cc5d1fa4/access_rights",
-                "key_id": "simcore/services/frontend/file-picker",  # NEW: renames key -> key_id
-                "version": "1.0.0",
-                "id": "a8f5a503-01d5-40bc-b416-f5b7cc5d1fa4",  # NEW: alternative identifier to key_id:version
-                "integration-version": "1.0.0",
-                "type": "dynamic",
-                "badges_url": "https://catalog:8080/services/a8f5a503-01d5-40bc-b416-f5b7cc5d1fa4/badges",
-                "authors_url": "https://catalog:8080/services/a8f5a503-01d5-40bc-b416-f5b7cc5d1fa4/authors",
-                "inputs_url": "https://catalog:8080/services/a8f5a503-01d5-40bc-b416-f5b7cc5d1fa4/inputs",
-                "outputs_url": "https://catalog:8080/services/a8f5a503-01d5-40bc-b416-f5b7cc5d1fa4/outputs",
-                "owner": "maiz@itis.swiss",  #  NEW, replaces "contact": "maiz@itis.swiss"
-                "url": "https://catalog:8080/services/a8f5a503-01d5-40bc-b416-f5b7cc5d1fa4",  # NEW self
             }
         }
 
