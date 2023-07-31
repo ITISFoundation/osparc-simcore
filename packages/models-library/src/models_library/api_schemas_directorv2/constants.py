706dcfc1c67e18d116117713bbd2c643ed41f610
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-07-31 09:23:52 +0200

    directorv2 models -> models_library

---
+++ packages/models-library/src/models_library/api_schemas_directorv2/constants.py
@@ -0,0 +1,22 @@
+from typing import Final
+
+# dynamic services
+
+DYNAMIC_SIDECAR_SERVICE_PREFIX: Final[str] = "dy-sidecar"
+DYNAMIC_PROXY_SERVICE_PREFIX: Final[str] = "dy-proxy"
+DYNAMIC_VOLUME_REMOVER_PREFIX: Final[str] = "dy-volrm"
+
+# label storing scheduler_data to allow service
+# monitoring recovery after director-v2 reboots
+DYNAMIC_SIDECAR_SCHEDULER_DATA_LABEL: Final[str] = "io.simcore.scheduler-data"
+
+# This matches registries by:
+# - local
+# - itisfoundation
+# - 10.0.0.0:8473 (IP & Port)
+DYNAMIC_SIDECAR_DOCKER_IMAGE_RE = (
+    r"(^([_a-zA-Z0-9:.-]+)/)?(dynamic-sidecar):([_a-zA-Z0-9.-]+$)"
+)
+
+REGEX_DY_SERVICE_SIDECAR = rf"^{DYNAMIC_SIDECAR_SERVICE_PREFIX}_[a-zA-Z0-9-_]*"
+REGEX_DY_SERVICE_PROXY = rf"^{DYNAMIC_PROXY_SERVICE_PREFIX}_[a-zA-Z0-9-_]*"
---
+++ packages/models-library/src/models_library/api_schemas_directorv2/meta.py
@@ -0,0 +1,27 @@
+import re
+from typing import Any, ClassVar
+
+from pydantic import BaseModel, ConstrainedStr, Field
+
+from ..basic_regex import VERSION_RE
+
+
+class VersionStr(ConstrainedStr):
+    regex = re.compile(VERSION_RE)
+
+
+class Meta(BaseModel):
+    name: str
+    version: VersionStr
+    released: dict[str, VersionStr] | None = Field(
+        None, description="Maps every route's path tag with a released version"
+    )
+
+    class Config:
+        schema_extra: ClassVar[dict[str, Any]] = {
+            "example": {
+                "name": "simcore_service_foo",
+                "version": "2.4.45",
+                "released": {"v1": "1.3.4", "v2": "2.4.45"},
+            }
+        }
