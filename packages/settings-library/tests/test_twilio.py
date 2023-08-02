4faea9690ed18361a075f1a6c1fb9d70c0b84d01
Yury Hrytsuk <50014626+YuryHrytsuk@users.noreply.github.com>
2023-08-02 14:09:30 +0200

    🐛 Fix webserver twilio env (#4566)

--- packages/settings-library/tests/test_twilio.py
+++ packages/settings-library/tests/test_twilio.py
@@ -2,8 +2,6 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
-import pytest
-from pydantic import ValidationError
 from pytest import MonkeyPatch
 from pytest_simcore.helpers.utils_envs import setenvs_from_dict
 from settings_library.twilio import TwilioSettings
@@ -12,10 +10,6 @@ from settings_library.twilio import TwilioSettings
 def test_twilio_settings_within_envdevel(
     mock_env_devel_environment: dict[str, str], monkeypatch: MonkeyPatch
 ):
-    # in .env-devel these are for the oment undefined
-    with pytest.raises(ValidationError):
-        TwilioSettings.create_from_envs()
-
     # adds twilio credentials
     with monkeypatch.context() as patch:
         setenvs_from_dict(
