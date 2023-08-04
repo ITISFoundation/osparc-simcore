986daf0ebfcc57f5d40e463b9415f666dd8a26b8
Sylvain <35365065+sanderegg@users.noreply.github.com>
2023-08-04 17:13:43 +0200

    🐛Uploading files works again (#4577)

--- services/web/server/src/simcore_service_webserver/projects/lock.py
+++ services/web/server/src/simcore_service_webserver/projects/lock.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 from asyncio.log import logger
 from contextlib import asynccontextmanager
 from typing import Final
@@ -9,10 +10,13 @@ from models_library.projects import ProjectID
 from models_library.projects_state import Owner, ProjectLocked, ProjectStatus
 from redis.asyncio.lock import Lock
 from servicelib.background_task import periodic_task
+from servicelib.logging_utils import log_context
 
 from ..redis import get_redis_lock_manager_client
 from ..users.api import UserNameDict
 from .exceptions import ProjectLockError
+
+_logger = logging.getLogger(__name__)
 
 PROJECT_REDIS_LOCK_KEY: str = "project_lock:{}"
 PROJECT_LOCK_TIMEOUT: Final[datetime.timedelta] = datetime.timedelta(seconds=10)
@@ -57,16 +61,22 @@ async def lock_project(
                 status=status,
             ).json(),
         ):
-            raise ProjectLockError(
-                f"Lock for project {project_uuid!r} user {user_id!r} could not be acquired"
-            )
-        async with periodic_task(
-            _auto_extend_project_lock,
-            interval=0.6 * PROJECT_LOCK_TIMEOUT,
-            task_name=f"{PROJECT_REDIS_LOCK_KEY.format(project_uuid)}_lock_auto_extend",
-            project_lock=redis_lock,
+            msg = f"Lock for project {project_uuid!r} user {user_id!r} could not be acquired"
+            raise ProjectLockError(msg)
+
+        with log_context(
+            _logger,
+            logging.DEBUG,
+            msg=f"with lock for {user_id=}:{user_name=}:{project_uuid=}:{status=}",
         ):
-            yield
+            async with periodic_task(
+                _auto_extend_project_lock,
+                interval=0.6 * PROJECT_LOCK_TIMEOUT,
+                task_name=f"{PROJECT_REDIS_LOCK_KEY.format(project_uuid)}_lock_auto_extend",
+                project_lock=redis_lock,
+            ):
+                yield
+
     finally:
         # let's ensure we release that stuff
         try:
