39d0164093618b10a3b542fb64c70ccd460b2d9a
matusdrobuliak66 <drobuliak66@gmail.com>
2023-08-08 09:26:35 +0200

    review @GitHK

--- services/resource-usage-tracker/tests/unit/modules/test_rabbitmq.py
+++ services/resource-usage-tracker/tests/unit/modules/test_rabbitmq.py
@@ -3,12 +3,8 @@
 # pylint:disable=redefined-outer-name
 
 
-from unittest import mock
-
-import pytest
 from fastapi import FastAPI
 from settings_library.rabbit import RabbitSettings
-from simcore_service_resource_usage_tracker.core.errors import ConfigurationError
 from simcore_service_resource_usage_tracker.modules.rabbitmq import get_rabbitmq_client
 
 # Selection of core and tool services started in this swarm fixture (integration)
@@ -19,25 +15,10 @@ pytest_simcore_core_services_selection = [
 pytest_simcore_ops_services_selection = []
 
 
-def test_rabbitmq_does_not_initialize_if_deactivated(
-    disabled_prometheus: None,
-    disabled_database: None,
-    disabled_rabbitmq: None,
-    mocked_setup_rabbitmq: mock.Mock,
-    mocked_redis_server: None,
-    initialized_app: FastAPI,
-):
-    assert hasattr(initialized_app.state, "rabbitmq_client")
-    assert initialized_app.state.rabbitmq_client == None
-    with pytest.raises(ConfigurationError):
-        get_rabbitmq_client(initialized_app)
-
-
 def test_rabbitmq_initializes(
     disabled_prometheus: None,
     disabled_database: None,
     enabled_rabbitmq: RabbitSettings,
-    mocked_setup_rabbitmq: mock.Mock,
     mocked_redis_server: None,
     initialized_app: FastAPI,
 ):
