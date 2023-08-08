fc7a10678db718b1abb9a50d36ac6aba2b685468
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-08-08 15:21:10 +0200

    remove totality

--- services/director-v2/src/simcore_service_director_v2/models/comp_runs.py
+++ services/director-v2/src/simcore_service_director_v2/models/comp_runs.py
@@ -7,13 +7,13 @@ from models_library.projects import ProjectID
 from models_library.projects_nodes_io import NodeID
 from models_library.projects_state import RunningState
 from models_library.users import UserID
-from pydantic import BaseModel, PositiveInt, validator
+from pydantic import BaseModel, Field, PositiveInt, validator
 from simcore_postgres_database.models.comp_pipeline import StateType
 
 from ..utils.db import DB_TO_RUNNING_STATE
 
 
-class RunMetadataDict(TypedDict):
+class RunMetadataDict(TypedDict, total=False):
     node_id_names_map: dict[NodeID, str]
     project_name: str
     product_name: str
@@ -34,7 +34,7 @@ class CompRunsAtDB(BaseModel):
     modified: datetime.datetime
     started: datetime.datetime | None
     ended: datetime.datetime | None
-    metadata: RunMetadataDict
+    metadata: RunMetadataDict = Field(default=dict)
 
     @validator("result", pre=True)
     @classmethod
@@ -89,7 +89,10 @@ class CompRunsAtDB(BaseModel):
                     "started": "2021-03-01 8:07:34.19161",
                     "ended": "2021-03-01 13:07:34.10",
                     "metadata": {
+                        "node_id_names_map": {},
                         "product_name": "osparc",
+                        "project_name": "my awesome project",
+                        "simcore_user_agent": "undefined",
                         "some-other-metadata-which-is-an-array": [1, 3, 4],
                     },
                 },
