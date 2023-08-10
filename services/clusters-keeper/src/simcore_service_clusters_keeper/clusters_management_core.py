1e4aa90b84bbd6ea4cc027ddfb430a43e3e2ca92
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-08-10 16:32:46 +0200

    ensure termination of instances with the correct tags

--- services/clusters-keeper/src/simcore_service_clusters_keeper/clusters_management_core.py
+++ services/clusters-keeper/src/simcore_service_clusters_keeper/clusters_management_core.py
@@ -1,21 +1,63 @@
-from dataclasses import dataclass
+import datetime
+import logging
 
 from fastapi import FastAPI
 
+from .core.settings import get_application_settings
+from .models import EC2InstanceData
+from .modules.ec2 import get_ec2_client
+from .utils import ec2 as ec2_utils
 
-@dataclass
-class OSparcGateway:
-    ...
-
-
-async def _analyze_available_gateways(app: FastAPI) -> list[OSparcGateway]:
-    return []
+_logger = logging.getLogger(__name__)
 
 
-async def _remove_unused_gateways(app: FastAPI, gateways: list[OSparcGateway]) -> None:
-    ...
+async def _find_terminateable_instances(
+    app: FastAPI, gateways: list[EC2InstanceData]
+) -> list[EC2InstanceData]:
+    app_settings = get_application_settings(app)
+    assert app_settings.CLUSTERS_KEEPER_EC2_INSTANCES  # nosec
+
+    # get the corresponding ec2 instance data
+    terminateable_instances: list[EC2InstanceData] = []
+
+    for instance in gateways:
+        # NOTE: AWS price is hourly based (e.g. same price for a machine used 2 minutes or 1 hour, so we wait until 55 minutes)
+        elapsed_time_since_launched = (
+            datetime.datetime.now(datetime.timezone.utc) - instance.launch_time
+        )
+        elapsed_time_since_full_hour = elapsed_time_since_launched % datetime.timedelta(
+            hours=1
+        )
+        if (
+            elapsed_time_since_full_hour
+            >= app_settings.CLUSTERS_KEEPER_EC2_INSTANCES.EC2_INSTANCES_TIME_BEFORE_TERMINATION
+        ):
+            # let's terminate that one
+            terminateable_instances.append(instance)
+
+    if terminateable_instances:
+        _logger.info(
+            "the following nodes were found to be terminateable: '%s'",
+            f"{[instance.id for instance in terminateable_instances]}",
+        )
+    return terminateable_instances
+
+
+async def _get_running_gateways(app: FastAPI) -> list[EC2InstanceData]:
+    app_settings = get_application_settings(app)
+    assert app_settings.CLUSTERS_KEEPER_EC2_INSTANCES  # nosec
+    return await get_ec2_client(app).get_instances(
+        app_settings.CLUSTERS_KEEPER_EC2_INSTANCES,
+        ec2_utils.get_ec2_tags_for_listing(),
+        state_names=["running"],
+    )
+
+
+async def _terminate_gateways(app: FastAPI, gateways: list[EC2InstanceData]) -> None:
+    await get_ec2_client(app).terminate_instances(gateways)
 
 
 async def check_clusters(app: FastAPI) -> None:
-    gateways = await _analyze_available_gateways(app)
-    await _remove_unused_gateways(app, gateways)
+    gateways = await _get_running_gateways(app)
+    if terminateable_gateways := await _find_terminateable_instances(app, gateways):
+        await _terminate_gateways(app, terminateable_gateways)
