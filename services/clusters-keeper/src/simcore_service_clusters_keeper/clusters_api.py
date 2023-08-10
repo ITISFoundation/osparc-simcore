11eb5267cbf2b3f2c315ec34126e6ae6fe61bb39
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-08-10 12:43:58 +0200

    test crating instances over rpc

--- services/clusters-keeper/src/simcore_service_clusters_keeper/clusters_api.py
+++ services/clusters-keeper/src/simcore_service_clusters_keeper/clusters_api.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
+from models_library.users import UserID
+from models_library.wallets import WalletID
 
 from .core.settings import get_application_settings
 from .modules.ec2 import get_ec2_client
+from .utils.ec2 import get_ec2_tags
 
 
-async def create_cluster(app: FastAPI):
+async def create_cluster(app: FastAPI, *, user_id: UserID, wallet_id: WalletID):
     ec2_client = get_ec2_client(app)
     app_settings = get_application_settings(app)
     assert app_settings.CLUSTERS_KEEPER_EC2_INSTANCES  # nosec
-    instances_data = await ec2_client.start_aws_instance(
+    ec2_instances = await ec2_client.start_aws_instance(
         app_settings.CLUSTERS_KEEPER_EC2_INSTANCES,
         instance_type="t2.micro",
-        tags={},
+        tags=get_ec2_tags(app_settings, user_id=user_id, wallet_id=wallet_id),
         startup_script="",
         number_of_instances=1,
     )
+    return ec2_instances
--- services/clusters-keeper/tests/unit/test_workflow_create_cluster.py
+++ services/clusters-keeper/tests/unit/test_workflow_create_cluster.py
@@ -8,10 +8,15 @@ from collections.abc import Callable
 from typing import Final
 
 import pytest
+from faker import Faker
 from fastapi import FastAPI
+from models_library.users import UserID
+from models_library.wallets import WalletID
+from parse import Result, search
 from pydantic import parse_obj_as
 from servicelib.rabbitmq import RabbitMQClient
 from servicelib.rabbitmq_utils import RPCMethodName, RPCNamespace
+from types_aiobotocore_ec2 import EC2Client
 
 pytest_simcore_core_services_selection = [
     "rabbit",
@@ -35,6 +40,16 @@ CLUSTERS_KEEPER_NAMESPACE: Final[RPCNamespace] = parse_obj_as(
 )
 
 
+@pytest.fixture
+def user_id(faker: Faker) -> UserID:
+    return faker.pyint(min_value=1)
+
+
+@pytest.fixture
+def wallet_id(faker: Faker) -> WalletID:
+    return faker.pyint(min_value=1)
+
+
 async def test_create_cluster(
     docker_swarm: None,
     enabled_rabbitmq: None,
@@ -45,10 +60,39 @@ async def test_create_cluster(
     mocked_redis_server: None,
     clusters_keeper_rabbitmq_rpc_client: RabbitMQClient,
     initialized_app: FastAPI,
+    ec2_client: EC2Client,
+    user_id: UserID,
+    wallet_id: WalletID,
 ):
     # send rabbitmq rpc to create_cluster
-    await clusters_keeper_rabbitmq_rpc_client.rpc_request(
-        CLUSTERS_KEEPER_NAMESPACE, RPCMethodName("create_cluster")
+    rpc_response = await clusters_keeper_rabbitmq_rpc_client.rpc_request(
+        CLUSTERS_KEEPER_NAMESPACE,
+        RPCMethodName("create_cluster"),
+        kwargs={
+            "user_id": user_id,
+            "wallet_id": wallet_id,
+        },
     )
+    assert rpc_response
     # wait for response
     # check we do have a new machine in AWS
+    instances = await ec2_client.describe_instances()
+    assert len(instances["Reservations"]) == 1
+    assert "Instances" in instances["Reservations"][0]
+    assert len(instances["Reservations"][0]["Instances"]) == 1
+    assert "Tags" in instances["Reservations"][0]["Instances"][0]
+    assert len(instances["Reservations"][0]["Instances"][0]["Tags"]) == 2
+    assert "Key" in instances["Reservations"][0]["Instances"][0]["Tags"][0]
+    assert (
+        instances["Reservations"][0]["Instances"][0]["Tags"][0]["Key"]
+        == "io.simcore.clusters-keeper.version"
+    )
+    assert "Key" in instances["Reservations"][0]["Instances"][0]["Tags"][1]
+    assert instances["Reservations"][0]["Instances"][0]["Tags"][1]["Key"] == "Name"
+    assert "Value" in instances["Reservations"][0]["Instances"][0]["Tags"][1]
+    instance_name = instances["Reservations"][0]["Instances"][0]["Tags"][1]["Value"]
+
+    parse_result = search("user_id:{user_id:d}-wallet_id:{wallet_id:d}", instance_name)
+    assert isinstance(parse_result, Result)
+    assert parse_result["user_id"] == user_id
+    assert parse_result["wallet_id"] == wallet_id
