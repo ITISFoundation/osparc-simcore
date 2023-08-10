e0afe686ad5fe97633450615473a0528e5b70d55
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-08-10 16:32:13 +0200

    better logs

--- services/clusters-keeper/src/simcore_service_clusters_keeper/clusters_api.py
+++ services/clusters-keeper/src/simcore_service_clusters_keeper/clusters_api.py
@@ -1,21 +1,28 @@
+import logging
+from dataclasses import asdict
+
 from fastapi import FastAPI
 from models_library.users import UserID
 from models_library.wallets import WalletID
+from servicelib.logging_utils import log_context
 
 from .core.settings import get_application_settings
 from .modules.ec2 import get_ec2_client
 from .utils.ec2 import get_ec2_tags
 
+_logger = logging.getLogger(__name__)
+
 
 async def create_cluster(app: FastAPI, *, user_id: UserID, wallet_id: WalletID):
-    ec2_client = get_ec2_client(app)
-    app_settings = get_application_settings(app)
-    assert app_settings.CLUSTERS_KEEPER_EC2_INSTANCES  # nosec
-    ec2_instances = await ec2_client.start_aws_instance(
-        app_settings.CLUSTERS_KEEPER_EC2_INSTANCES,
-        instance_type="t2.micro",
-        tags=get_ec2_tags(app_settings, user_id=user_id, wallet_id=wallet_id),
-        startup_script="",
-        number_of_instances=1,
-    )
-    return ec2_instances
+    with log_context(_logger, logging.DEBUG, msg="create_cluster"):
+        ec2_client = get_ec2_client(app)
+        app_settings = get_application_settings(app)
+        assert app_settings.CLUSTERS_KEEPER_EC2_INSTANCES  # nosec
+        ec2_instances = await ec2_client.start_aws_instance(
+            app_settings.CLUSTERS_KEEPER_EC2_INSTANCES,
+            instance_type="t2.micro",
+            tags=get_ec2_tags(app_settings, user_id=user_id, wallet_id=wallet_id),
+            startup_script="",
+            number_of_instances=1,
+        )
+    return [asdict(i) for i in ec2_instances]
