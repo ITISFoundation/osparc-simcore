4483019cc91c10a7430ff61d184ee835b48d086d
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-08-14 16:32:08 +0200

    change of plan

--- services/clusters-keeper/tests/unit/test_clusters_rpc.py
+++ services/clusters-keeper/tests/unit/test_clusters_rpc.py
@@ -5,6 +5,8 @@
 
 import asyncio
 import datetime
+from collections.abc import Callable
+from typing import Final
 
 import arrow
 import pytest
@@ -13,13 +15,33 @@ from fastapi import FastAPI
 from models_library.users import UserID
 from models_library.wallets import WalletID
 from parse import Result, search
-from simcore_service_clusters_keeper.clusters_api import (
-    cluster_heartbeat,
-    create_cluster,
-)
+from pydantic import parse_obj_as
+from servicelib.rabbitmq import RabbitMQClient
+from servicelib.rabbitmq_utils import RPCMethodName, RPCNamespace
 from simcore_service_clusters_keeper.core.errors import Ec2InstanceNotFoundError
 from simcore_service_clusters_keeper.utils.ec2 import HEARTBEAT_TAG_KEY
 from types_aiobotocore_ec2 import EC2Client
+
+pytest_simcore_core_services_selection = [
+    "rabbit",
+]
+
+pytest_simcore_ops_services_selection = []
+
+
+@pytest.fixture
+async def clusters_keeper_rabbitmq_rpc_client(
+    rabbitmq_client: Callable[[str], RabbitMQClient]
+) -> RabbitMQClient:
+    rpc_client = rabbitmq_client("pytest_clusters_keeper_rpc_client")
+    assert rpc_client
+    await rpc_client.rpc_initialize()
+    return rpc_client
+
+
+CLUSTERS_KEEPER_NAMESPACE: Final[RPCNamespace] = parse_obj_as(
+    RPCNamespace, "clusters-keeper"
+)
 
 
 @pytest.fixture
@@ -35,12 +57,13 @@ def wallet_id(faker: Faker) -> WalletID:
 @pytest.fixture
 def _base_configuration(
     docker_swarm: None,
-    disabled_rabbitmq: None,
+    enabled_rabbitmq: None,
     aws_subnet_id: str,
     aws_security_group_id: str,
     aws_ami_id: str,
     aws_allowed_ec2_instance_type_names: list[str],
     mocked_redis_server: None,
+    initialized_app: FastAPI,
 ) -> None:
     ...
 
@@ -77,25 +100,34 @@ async def _assert_cluster_instance_created(
 
 
 async def _create_cluster(
-    app: FastAPI,
+    clusters_keeper_rabbitmq_rpc_client: RabbitMQClient,
     ec2_client: EC2Client,
     user_id: UserID,
     wallet_id: WalletID,
 ) -> None:
-    created_clusters = await create_cluster(app, user_id=user_id, wallet_id=wallet_id)
-    assert len(created_clusters) == 1
+    # send rabbitmq rpc to create_cluster
+    rpc_response = await clusters_keeper_rabbitmq_rpc_client.rpc_request(
+        CLUSTERS_KEEPER_NAMESPACE,
+        RPCMethodName("create_cluster"),
+        user_id=user_id,
+        wallet_id=wallet_id,
+    )
+    assert rpc_response
+    # wait for response
     # check we do have a new machine in AWS
     await _assert_cluster_instance_created(ec2_client, user_id, wallet_id)
 
 
 async def test_create_cluster(
     _base_configuration: None,
+    clusters_keeper_rabbitmq_rpc_client: RabbitMQClient,
     ec2_client: EC2Client,
     user_id: UserID,
     wallet_id: WalletID,
-    initialized_app: FastAPI,
 ):
-    await _create_cluster(initialized_app, ec2_client, user_id, wallet_id)
+    await _create_cluster(
+        clusters_keeper_rabbitmq_rpc_client, ec2_client, user_id, wallet_id
+    )
 
 
 async def _assert_cluster_heartbeat_on_instance(
@@ -119,31 +151,52 @@ async def _assert_cluster_heartbeat_on_instance(
     return this_heartbeat_time
 
 
+async def _send_cluster_heartbeat(
+    clusters_keeper_rabbitmq_rpc_client: RabbitMQClient,
+    ec2_client: EC2Client,
+    user_id: UserID,
+) -> datetime.datetime:
+    rpc_response = await clusters_keeper_rabbitmq_rpc_client.rpc_request(
+        CLUSTERS_KEEPER_NAMESPACE,
+        RPCMethodName("cluster_heartbeat"),
+        user_id=user_id,
+    )
+    assert rpc_response is None
+
+    return await _assert_cluster_heartbeat_on_instance(ec2_client)
+
+
 async def test_cluster_heartbeat(
     _base_configuration: None,
+    clusters_keeper_rabbitmq_rpc_client: RabbitMQClient,
     ec2_client: EC2Client,
     user_id: UserID,
     wallet_id: WalletID,
-    initialized_app: FastAPI,
 ):
-    await _create_cluster(initialized_app, ec2_client, user_id, wallet_id)
+    await _create_cluster(
+        clusters_keeper_rabbitmq_rpc_client, ec2_client, user_id, wallet_id
+    )
 
-    await cluster_heartbeat(initialized_app, user_id=user_id)
-    first_heartbeat_time = await _assert_cluster_heartbeat_on_instance(ec2_client)
+    first_heartbeat_time = await _send_cluster_heartbeat(
+        clusters_keeper_rabbitmq_rpc_client, ec2_client, user_id
+    )
 
     await asyncio.sleep(1)
 
-    await cluster_heartbeat(initialized_app, user_id=user_id)
-    next_heartbeat_time = await _assert_cluster_heartbeat_on_instance(ec2_client)
+    next_heartbeat_time = await _send_cluster_heartbeat(
+        clusters_keeper_rabbitmq_rpc_client, ec2_client, user_id
+    )
 
     assert next_heartbeat_time > first_heartbeat_time
 
 
 async def test_cluster_heartbeat_on_non_existing_cluster_raises(
     _base_configuration: None,
+    clusters_keeper_rabbitmq_rpc_client: RabbitMQClient,
     ec2_client: EC2Client,
     user_id: UserID,
-    initialized_app: FastAPI,
 ):
     with pytest.raises(Ec2InstanceNotFoundError):
-        await cluster_heartbeat(initialized_app, user_id=user_id)
+        await _send_cluster_heartbeat(
+            clusters_keeper_rabbitmq_rpc_client, ec2_client, user_id
+        )
