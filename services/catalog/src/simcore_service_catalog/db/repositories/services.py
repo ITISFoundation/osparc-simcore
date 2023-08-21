1ce793b78010dd1c8e72c267facc47be6976121a
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-08-21 17:41:25 +0200

    sql 2.0

--- services/catalog/src/simcore_service_catalog/db/repositories/services.py
+++ services/catalog/src/simcore_service_catalog/db/repositories/services.py
@@ -46,7 +46,7 @@ def _make_list_services_query(
         )
         query = (
             sa.select(
-                [services_meta_data],
+                services_meta_data,
             )
             .distinct(services_meta_data.c.key, services_meta_data.c.version)
             .select_from(services_meta_data.join(services_access_rights))
