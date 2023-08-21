8114deac4b38d1964e386fe3eaa61797dc21c030
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-08-21 10:17:21 +0200

    remove test for docker-compose v1

--- tests/environment-setup/test_used_docker_compose.py
+++ tests/environment-setup/test_used_docker_compose.py
@@ -29,8 +29,9 @@ def docker_compose_in_requirement_files(
     return reqs_path_version_tuples
 
 
-@pytest.fixture(scope="module")
-def docker_compose_in_ci_scripts(osparc_simcore_root_dir: Path) -> tuple[Path, str]:
+def test_no_docker_compose_v1_in_ci_scripts(
+    osparc_simcore_root_dir: Path,
+):
     ci_workflows_path = osparc_simcore_root_dir / ".github/workflows"
     versions_in_workflow_files: set[str] = set()
     for workflow_file in ci_workflows_path.rglob("ci-*.yml"):
@@ -41,47 +42,19 @@ def docker_compose_in_ci_scripts(osparc_simcore_root_dir: Path) -> tuple[Path, s
             )
         }
         assert (
-            len(versions_in_file) == 1
-        ), f"found different docker_compose versions in {workflow_file}, versions found {versions_in_file}, please check!"
+            len(versions_in_file) == 0
+        ), f"found docker_compose versions in {workflow_file}, versions found {versions_in_file}, please check!"
 
         versions_in_workflow_files.update(versions_in_file)
     assert (
-        len(versions_in_workflow_files) == 1
+        len(versions_in_workflow_files) == 0
     ), f"found different docker_compose versions in workflow files: {versions_in_workflow_files}, please check {list(ci_workflows_path.rglob('ci-*.yml'))}!"
-    return (ci_workflows_path, versions_in_workflow_files.pop())
 
 
 def test_there_are_no_docker_compose_v1_anywhere(
     docker_compose_in_requirement_files,
-    docker_compose_in_ci_scripts,
 ):
     assert not docker_compose_in_requirement_files
-    assert not docker_compose_in_ci_scripts
-
-
-def test_all_docker_compose_have_same_version(
-    docker_compose_in_requirement_files,
-    docker_compose_in_ci_scripts,
-    osparc_simcore_root_dir,
-):
-    previous = docker_compose_in_requirement_files[0]
-    for req_path, docker_compose_version in docker_compose_in_requirement_files:
-        print(
-            str(req_path.relative_to(osparc_simcore_root_dir)),
-            "->",
-            docker_compose_version,
-        )
-        assert docker_compose_version == previous[1]
-        previous = (req_path, docker_compose_version)
-
-    print(
-        str(docker_compose_in_ci_scripts[0].relative_to(osparc_simcore_root_dir)),
-        "->",
-        docker_compose_in_ci_scripts[1],
-    )
-    assert (
-        docker_compose_in_ci_scripts[1] == previous[1]
-    ), f"CI installs {docker_compose_in_ci_scripts[1]}=!{previous[1]} (see {docker_compose_in_ci_scripts[0]}"
 
 
 @pytest.fixture
@@ -160,28 +133,3 @@ def test_validate_compose_file(
 
     # About versioning https://docs.docker.com/compose/compose-file/compose-file-v3/
     assert compose["version"] == "3.8"
-
-
-def test_installed_docker_compose(docker_compose_in_ci_scripts):
-    setup_ci_path, ci_version = docker_compose_in_ci_scripts
-
-    which = subprocess.run(
-        "which docker-compose",
-        shell=True,
-        stdout=subprocess.PIPE,
-        encoding="utf8",
-        check=True,
-    ).stdout.strip()
-
-    p = subprocess.run(
-        ["docker-compose", "--version"],
-        stdout=subprocess.PIPE,
-        encoding="utf8",
-        check=True,
-    )
-    found = re.search(r"\d+\.\d+\.\d+", p.stdout)
-    assert found
-    installed_version = found.group()
-    assert (
-        ci_version == installed_version
-    ), f"Use {setup_ci_path} to install version {ci_version} of docker-compose in your system {which}"
