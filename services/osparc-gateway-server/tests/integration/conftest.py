bf1901db37bb4c1f50213fc60741da5f1c1ffbaf
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-08-22 18:35:08 +0200

    sql 2.0

--- services/osparc-gateway-server/tests/integration/conftest.py
+++ services/osparc-gateway-server/tests/integration/conftest.py
@@ -4,7 +4,7 @@
 
 import asyncio
 import json
-from typing import Any, AsyncIterator, Awaitable, Callable, Union
+from typing import Any, AsyncIterator, Awaitable, Callable
 
 import aiodocker
 import dask_gateway
@@ -20,6 +20,7 @@ from osparc_gateway_server.backend.utils import (
     OSPARC_SCHEDULER_DASHBOARD_PORT,
 )
 from pytest_simcore.helpers.utils_docker import get_localhost_ip
+from sqlalchemy import exc as sa_exceptions
 from tenacity._asyncio import AsyncRetrying
 from tenacity.wait import wait_fixed
 
@@ -56,7 +57,7 @@ def gateway_password(faker: Faker) -> str:
     return faker.password()
 
 
-def _convert_to_dict(c: Union[traitlets.config.Config, dict]) -> dict[str, Any]:
+def _convert_to_dict(c: traitlets.config.Config | dict) -> dict[str, Any]:
     converted_dict = {}
     for x, y in c.items():
         if isinstance(y, (dict, traitlets.config.Config)):
@@ -102,7 +103,8 @@ async def local_dask_gateway_server(
     dask_gateway_server = DaskGateway(config=c)
     dask_gateway_server.initialize([])  # that is a shitty one!
     print("--> local dask gateway server initialized")
-    await dask_gateway_server.setup()
+    with pytest.warns(sa_exceptions.Base20DeprecationWarning):
+        await dask_gateway_server.setup()
     await dask_gateway_server.backend.proxy._proxy_contacted  # pylint: disable=protected-access
     print("--> local dask gateway server setup completed")
     yield DaskGatewayServer(
