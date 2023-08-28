f54c9b949eb7d53cb1cc3d24634765df8785bab0
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-08-28 09:05:48 +0200

    WIP: fastapi/rabbitmq

---
+++ packages/service-library/src/servicelib/fastapi/rabbitmq.py
@@ -0,0 +1,81 @@
+import logging
+
+from fastapi import FastAPI
+from settings_library.rabbit import RabbitSettings
+
+from ..rabbitmq import RabbitMQClient
+from ..rabbitmq_utils import wait_till_rabbitmq_responsive
+from .errors import ApplicationStateError
+
+_logger = logging.getLogger(__name__)
+
+
+def _create_client(app: FastAPI):
+    app.state.rabbitmq_client = RabbitMQClient(
+        client_name=app.state.rabbitmq_client_name,
+        settings=app.state.rabbitmq_settings,
+    )
+
+
+async def _remove_client(app: FastAPI):
+    await app.state.rabbitmq_client.close()
+    app.state.rabbitmq_client = None
+
+
+async def connect(app: FastAPI):
+    assert app.state.rabbitmq_settings  # nosec
+    await wait_till_rabbitmq_responsive(app.state.rabbitmq_settings.dsn)
+    _create_client(app)
+
+
+async def disconnect(app: FastAPI):
+    if app.state.rabbitmq_client:
+        await _remove_client(app)
+
+
+async def reconnect(app: FastAPI):
+    await disconnect(app)
+    await connect(app)
+
+
+def setup_rabbit(
+    app: FastAPI,
+    *,
+    settings: RabbitSettings,
+    name: str,
+) -> None:
+    """Sets up rabbit in a given app
+
+    - Inits app.states for rabbitmq
+    - Creates a client to communicate with rabbitmq
+
+    Arguments:
+        app -- fastapi app
+        settings -- Rabbit settings or if None, the connection to rabbit is not done upon startup
+        name -- name for the rmq client name
+    """
+    app.state.rabbitmq_client = None  # RabbitMQClient | None
+    app.state.rabbitmq_client_name = name
+    app.state.rabbitmq_settings = settings
+
+    if settings is None:
+        return
+
+    async def on_startup() -> None:
+        await connect(app)
+
+    app.add_event_handler("startup", on_startup)
+
+    async def on_shutdown() -> None:
+        await disconnect(app)
+
+    app.add_event_handler("shutdown", on_shutdown)
+
+
+def get_rabbitmq_client(app: FastAPI) -> RabbitMQClient:
+    if not app.state.rabbitmq_client:
+        raise ApplicationStateError(
+            state="rabbitmq_client",
+            msg="Rabbitmq service unavailable. Check app settings",
+        )
+    return app.state.rabbitmq_client
