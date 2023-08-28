700931756eccd7751e491efe445de760181b69db
Odei Maiz <maiz@itis.swiss>
2023-08-28 17:08:52 +0200

    star

--- services/static-webserver/client/source/class/osparc/desktop/credits/Overview.js
+++ services/static-webserver/client/source/class/osparc/desktop/credits/Overview.js
@@ -39,28 +39,34 @@ qx.Class.define("osparc.desktop.credits.Overview", {
     _createChildControlImpl: function(id) {
       let control;
       switch (id) {
-        case "wallets-card":
-          control = this.__createOverviewCard("Wallet", "toWallets");
+        case "wallets-card": {
+          const content = this.__createWalletsView();
+          control = this.__createOverviewCard("Wallets", content, "toWallets");
           this._add(control, {
             column: 0,
             row: 0
           });
           break;
-        case "transactions-card":
-          control = this.__createOverviewCard("Transactions", "toTransactions");
+        }
+        case "transactions-card": {
+          const content = this.__createWalletsView();
+          control = this.__createOverviewCard("Transactions", content, "toTransactions");
           this._add(control, {
             column: 1,
             row: 0
           });
           break;
-        case "usage-card":
-          control = this.__createOverviewCard("Usage", "toUsageOverview");
+        }
+        case "usage-card": {
+          const content = this.__createWalletsView();
+          control = this.__createOverviewCard("Usage", content, "toUsageOverview");
           this._add(control, {
             column: 0,
             row: 1,
             colSpan: 2
           });
           break;
+        }
       }
       return control || this.base(arguments, id);
     },
@@ -71,7 +77,7 @@ qx.Class.define("osparc.desktop.credits.Overview", {
       this.getChildControl("usage-card");
     },
 
-    __createOverviewCard: function(cardName, signalName) {
+    __createOverviewCard: function(cardName, content, signalName) {
       const layout = new qx.ui.container.Composite(new qx.ui.layout.VBox(10)).set({
         minWidth: 200,
         minHeight: 200,
@@ -82,11 +88,15 @@ qx.Class.define("osparc.desktop.credits.Overview", {
         "border-radius": "4px"
       });
 
-      const label = new qx.ui.basic.Label().set({
+      const title = new qx.ui.basic.Label().set({
         value: cardName,
         font: "text-14"
       });
-      layout.add(label);
+      layout.add(title);
+
+      layout.add(content, {
+        flex: 1
+      });
 
       const goToButton = new qx.ui.form.Button().set({
         label: this.tr("Go to ") + cardName,
@@ -96,6 +106,50 @@ qx.Class.define("osparc.desktop.credits.Overview", {
       });
       goToButton.addListener("execute", () => this.fireEvent(signalName), this);
       layout.add(goToButton);
+
+      return layout;
+    },
+
+    __createWalletsView: function() {
+      const grid = new qx.ui.layout.Grid(5, 5);
+      grid.setColumnFlex(1, 1);
+      const layout = new qx.ui.container.Composite(grid);
+
+      const wallets = osparc.store.Store.getInstance().getWallets();
+      const maxIndicators = 5;
+      for (let i=0; i<wallets.length && i<maxIndicators; i++) {
+        const wallet = wallets[i];
+
+        // shared or not shared
+
+        // indicator
+        const progressBar = new osparc.desktop.credits.CreditsIndicatorWText(wallet, "horizontal").set({
+          allowShrinkY: true
+        });
+        progressBar.getChildControl("credits-indicator").set({
+          minWidth: 100
+        });
+        layout.add(progressBar, {
+          column: 1,
+          row: i
+        });
+
+        // favourite
+        const starImage = new qx.ui.basic.Image().set({
+          backgroundColor: "transparent",
+          alignY: "middle"
+        });
+        wallet.bind("defaultWallet", starImage, "source", {
+          converter: isDefault => isDefault ? "@FontAwesome5Solid/star/20" : "@FontAwesome5Regular/star/20"
+        });
+        wallet.bind("defaultWallet", starImage, "textColor", {
+          converter: isDefault => isDefault ? "strong-main" : null
+        });
+        layout.add(starImage, {
+          column: 2,
+          row: i
+        });
+      }
 
       return layout;
     }
