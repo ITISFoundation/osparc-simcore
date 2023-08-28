45d21673a50e05c87c07c81dde7c3fe4f5c3af35
matusdrobuliak66 <drobuliak66@gmail.com>
2023-08-28 16:41:51 +0200

    @pcrespov review

--- services/web/server/src/simcore_service_webserver/wallets/_api.py
+++ services/web/server/src/simcore_service_webserver/wallets/_api.py
@@ -3,6 +3,7 @@ import logging
 from aiohttp import web
 from models_library.api_schemas_webserver.wallets import (
     WalletGet,
+    WalletGetPermissions,
     WalletGetWithAvailableCredits,
 )
 from models_library.users import UserID
@@ -109,3 +110,42 @@ async def delete_wallet(
         )
 
     raise NotImplementedError
+
+
+async def get_wallet_by_user(
+    app: web.Application,
+    user_id: UserID,
+    wallet_id: WalletID,
+) -> WalletGet:
+    wallet: UserWalletDB = await db.get_wallet_for_user(
+        app=app, user_id=user_id, wallet_id=wallet_id
+    )
+    if wallet.read is False:
+        raise WalletAccessForbiddenError(
+            reason=f"User {user_id} does not have read permission on wallet {wallet_id}"
+        )
+
+    wallet_api: WalletGet = WalletGet(
+        wallet_id=wallet.wallet_id,
+        name=wallet.name,
+        description=wallet.description,
+        owner=wallet.owner,
+        thumbnail=wallet.thumbnail,
+        status=wallet.status,
+        created=wallet.created,
+        modified=wallet.modified,
+    )
+    return wallet_api
+
+
+async def get_wallet_with_permissions_by_user(
+    app: web.Application,
+    user_id: UserID,
+    wallet_id: WalletID,
+) -> WalletGetPermissions:
+    wallet: UserWalletDB = await db.get_wallet_for_user(
+        app=app, user_id=user_id, wallet_id=wallet_id
+    )
+
+    output = WalletGetPermissions.construct(**wallet.dict())
+    return output
