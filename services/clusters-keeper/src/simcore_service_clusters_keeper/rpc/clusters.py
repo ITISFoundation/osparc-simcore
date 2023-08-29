ea4e659fb6d00fe844561080fa99640616ca2c9d
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-08-29 09:01:52 +0200

    add doc and use ping

--- services/clusters-keeper/src/simcore_service_clusters_keeper/rpc/clusters.py
+++ services/clusters-keeper/src/simcore_service_clusters_keeper/rpc/clusters.py
@@ -5,6 +5,7 @@ from models_library.wallets import WalletID
 from ..core.errors import Ec2InstanceNotFoundError
 from ..models import ClusterGet, EC2InstanceData
 from ..modules import clusters
+from ..modules.dask import ping_gateway
 from .rpc_router import RPCRouter
 
 router = RPCRouter()
@@ -14,6 +15,11 @@ router = RPCRouter()
 async def get_or_create_cluster(
     app: FastAPI, *, user_id: UserID, wallet_id: WalletID
 ) -> ClusterGet:
+    """Get or create cluster for user_id and wallet_id
+    This function will create a new instance on AWS if needed or return the already running one.
+    It will also check that the underlying computational backend is up and running.
+    Calling several time will always return the same cluster.
+    """
     ec2_instance = None
     try:
         ec2_instance = await clusters.get_cluster(
@@ -29,11 +35,12 @@ async def get_or_create_cluster(
         ec2_instance = new_ec2_instances[0]
     assert ec2_instance is not None  # nosec
 
-    # if ec2_instance.state == "running":
-    #     # check gateway is up?
-    #     gateway.ping(ec2_instance)
+    cluster_get = ClusterGet.from_ec2_instance_data(ec2_instance, user_id, wallet_id)
 
-    return ClusterGet.from_ec2_instance_data(ec2_instance, user_id, wallet_id)
+    if ec2_instance.state == "running" and await ping_gateway(ec2_instance):
+        ...
+
+    return cluster_get
 
 
 @router.expose()
