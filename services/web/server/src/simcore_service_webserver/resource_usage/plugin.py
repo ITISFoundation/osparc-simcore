61f2f616eea2f3544ed19d217bc9db8a4e50d5cf
matusdrobuliak66 <drobuliak66@gmail.com>
2023-08-29 09:04:53 +0200

    @pcrespov review

--- services/web/server/src/simcore_service_webserver/resource_usage/plugin.py
+++ services/web/server/src/simcore_service_webserver/resource_usage/plugin.py
@@ -7,6 +7,7 @@ from aiohttp import web
 from servicelib.aiohttp.application_keys import APP_SETTINGS_KEY
 from servicelib.aiohttp.application_setup import ModuleCategory, app_module_setup
 
+from ..wallets.plugin import setup_wallets
 from . import _service_runs_handlers
 
 _logger = logging.getLogger(__name__)
@@ -21,4 +22,7 @@ _logger = logging.getLogger(__name__)
 )
 def setup_resource_tracker(app: web.Application):
     assert app[APP_SETTINGS_KEY].WEBSERVER_RESOURCE_USAGE_TRACKER  # nosec
+
+    setup_wallets(app)
+
     app.router.add_routes(_service_runs_handlers.routes)
--- services/web/server/tests/unit/with_dbs/03/resource_usage/conftest.py
+++ services/web/server/tests/unit/with_dbs/03/resource_usage/conftest.py
@@ -2,25 +2,10 @@
 # pylint:disable=unused-argument
 # pylint:disable=redefined-outer-name
 
-from copy import deepcopy
-from typing import Callable
 
 import pytest
-from servicelib.aiohttp.application import create_safe_application
-from simcore_service_webserver.application_settings import setup_settings
-from simcore_service_webserver.db.plugin import setup_db
-from simcore_service_webserver.director_v2.plugin import setup_director_v2
-from simcore_service_webserver.login.plugin import setup_login
-from simcore_service_webserver.products.plugin import setup_products
-from simcore_service_webserver.projects.plugin import setup_projects
-from simcore_service_webserver.resource_manager.plugin import setup_resource_manager
-from simcore_service_webserver.resource_usage.plugin import setup_resource_tracker
-from simcore_service_webserver.rest.plugin import setup_rest
-from simcore_service_webserver.security.plugin import setup_security
-from simcore_service_webserver.session.plugin import setup_session
-from simcore_service_webserver.socketio.plugin import setup_socketio
-from simcore_service_webserver.tags.plugin import setup_tags
-from simcore_service_webserver.wallets.plugin import setup_wallets
+from pytest_simcore.helpers.typing_env import EnvVarsDict
+from pytest_simcore.helpers.utils_envs import setenvs_from_dict
 
 API_VERSION = "v0"
 RESOURCE_NAME = "projects"
@@ -31,53 +16,14 @@ DEFAULT_GARBAGE_COLLECTOR_INTERVAL_SECONDS: int = 3
 DEFAULT_GARBAGE_COLLECTOR_DELETION_TIMEOUT_SECONDS: int = 3
 
 
-@pytest.fixture
-def client(
-    event_loop,
-    aiohttp_client,
-    app_cfg,
-    postgres_db,
-    mocked_director_v2_api,
-    mock_orphaned_services,
-    redis_client,  # this ensure redis is properly cleaned
-    monkeypatch_setenv_from_app_config: Callable,
-):
-    # config app
-    cfg = deepcopy(app_cfg)
-    port = cfg["main"]["port"]
-    cfg["projects"]["enabled"] = True
-    cfg["director"]["enabled"] = True
-    cfg["resource_manager"][
-        "garbage_collection_interval_seconds"
-    ] = DEFAULT_GARBAGE_COLLECTOR_INTERVAL_SECONDS  # increase speed of garbage collection
-    cfg["resource_manager"][
-        "resource_deletion_timeout_seconds"
-    ] = DEFAULT_GARBAGE_COLLECTOR_DELETION_TIMEOUT_SECONDS  # reduce deletion delay
+def app_environment(app_environment: EnvVarsDict, monkeypatch: pytest.MonkeyPatch):
+    # print( ApplicationSettings.create_from_envs().json(indent=1 )
 
-    monkeypatch_setenv_from_app_config(cfg)
-
-    app = create_safe_application(cfg)
-
-    assert setup_settings(app)
-
-    # setup app
-    setup_db(app)
-    setup_session(app)
-    setup_security(app)
-    setup_rest(app)
-    setup_login(app)  # needed for login_utils fixtures
-    setup_resource_manager(app)
-    setup_socketio(app)
-    setup_director_v2(app)
-    setup_tags(app)
-    assert setup_projects(app)
-    setup_products(app)
-    setup_wallets(app)
-    setup_resource_tracker(app)
-
-    # server and client
-    yield event_loop.run_until_complete(
-        aiohttp_client(app, server_kwargs={"port": port, "host": "localhost"})
+    return app_environment | setenvs_from_dict(
+        monkeypatch,
+        {
+            "WEBSERVER_DB_LISTENER": "0",
+            "WEBSERVER_GARBAGE_COLLECTOR": "null",
+            "WEBSERVER_ANNOUNCEMENTS": "1",
+        },
     )
-
-    # teardown here ...
