4dbf950bc6c5525351781072c44fa6510297b620
matusdrobuliak66 <drobuliak66@gmail.com>
2023-08-30 12:56:30 +0200

    fix code smell

--- services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/resource_tracker.py
+++ services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/resource_tracker.py
@@ -46,10 +46,11 @@ def on_app_startup(app: FastAPI) -> Callable[[], Awaitable[None]]:
 
 
 def on_app_shutdown(
-    app: FastAPI,  # pylint: disable=unused-argument
+    _app: FastAPI,
 ) -> Callable[[], Awaitable[None]]:
     async def _stop() -> None:
         # NOTE: We want to have persistent queue, therefore we will not unsubscribe
+        assert _app  # nosec
         return None
 
     return _stop
