5d3166de37e77973e7a511f2302b50b3570d6416
matusdrobuliak66 <drobuliak66@gmail.com>
2023-08-30 11:37:23 +0200

    disable unused argument for now

--- services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/resource_tracker.py
+++ services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/resource_tracker.py
@@ -45,7 +45,9 @@ def on_app_startup(app: FastAPI) -> Callable[[], Awaitable[None]]:
     return _startup
 
 
-def on_app_shutdown(app: FastAPI) -> Callable[[], Awaitable[None]]:
+def on_app_shutdown(
+    app: FastAPI,
+) -> Callable[[], Awaitable[None]]:  # pylint: disable=unused-argument
     async def _stop() -> None:
         # NOTE: We want to have persistent queue, therefore we will not unsubscribe
         return None
