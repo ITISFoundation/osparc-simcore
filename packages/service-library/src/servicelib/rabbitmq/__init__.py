621605e0ae5bc8db4f222849a7b5d3095fc6102c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-08-30 10:35:59 +0200

    provide wait_till_rabbitmq_responsive

--- packages/service-library/src/servicelib/rabbitmq/__init__.py
+++ packages/service-library/src/servicelib/rabbitmq/__init__.py
@@ -4,7 +4,7 @@ from ._constants import BIND_TO_ALL_TOPICS
 from ._errors import RemoteMethodNotRegisteredError, RPCNotInitializedError
 from ._models import RPCMethodName, RPCNamespace
 from ._rpc_router import RPCRouter
-from ._utils import rpc_register_entries
+from ._utils import rpc_register_entries, wait_till_rabbitmq_responsive
 
 __all__: tuple[str, ...] = (
     "BIND_TO_ALL_TOPICS",
@@ -16,4 +16,5 @@ __all__: tuple[str, ...] = (
     "RPCNamespace",
     "RPCNotInitializedError",
     "RPCRouter",
+    "wait_till_rabbitmq_responsive",
 )
