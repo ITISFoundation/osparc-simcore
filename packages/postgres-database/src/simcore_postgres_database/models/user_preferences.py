3e54f9d69df05371cbb6aa621d1a49cc49e40b38
Andrei Neagu <neagu@itis.swiss>
2023-08-31 15:46:28 +0200

    refactor tables to store preferences

--- packages/postgres-database/src/simcore_postgres_database/models/user_preferences.py
+++ packages/postgres-database/src/simcore_postgres_database/models/user_preferences.py
@@ -1,33 +1,82 @@
 import sqlalchemy as sa
 
 from .base import metadata
+from .products import products
 from .users import users
 
-user_preferences = sa.Table(
-    "user_preferences",
-    metadata,
-    sa.Column(
-        "user_preference_name",
-        sa.String,
-        nullable=False,
-        primary_key=True,
-        doc="preference name which also includes the user_id",
-    ),
-    sa.Column(
+
+def _user_id_column(fk_name: str) -> sa.Column:
+    return sa.Column(
         "user_id",
         sa.BigInteger,
         sa.ForeignKey(
             users.c.id,
-            name="fk_user_preferences_id_users",
+            name=fk_name,
             ondelete="CASCADE",
             onupdate="CASCADE",
         ),
         nullable=False,
+    )
+
+
+def _product_name_column(fk_name: str) -> sa.Column:
+    return sa.Column(
+        "product_name",
+        sa.String,
+        sa.ForeignKey(
+            products.c.name,
+            name=fk_name,
+            ondelete="CASCADE",
+            onupdate="CASCADE",
+        ),
+        nullable=False,
+    )
+
+
+def _preference_name_column() -> sa.Column:
+    return sa.Column(
+        "preference_name",
+        sa.String,
+        nullable=False,
+    )
+
+
+user_preferences_frontend = sa.Table(
+    "user_preferences_frontend",
+    metadata,
+    _user_id_column("fk_user_preferences_frontend_id_users"),
+    _product_name_column("fk_user_preferences_frontend_name_products"),
+    _preference_name_column(),
+    sa.Column(
+        "payload",
+        sa.JSON,
+        nullable=False,
+        doc="preference content encoded as json",
     ),
+    sa.PrimaryKeyConstraint(
+        "user_id",
+        "product_name",
+        "preference_name",
+        name="user_preferences_frontend_pk",
+    ),
+)
+
+user_preferences_user_services = sa.Table(
+    "user_preferences_user_services",
+    metadata,
+    _user_id_column("fk_user_preferences_user_services_id_users"),
+    _product_name_column("fk_user_preferences_user_services_name_products"),
+    _preference_name_column(),
     sa.Column(
         "payload",
         sa.LargeBinary,
         nullable=False,
-        doc="property content encoded as bytes",
+        doc="preference content encoded as bytes",
+    ),
+    sa.PrimaryKeyConstraint(
+        "user_id",
+        "product_name",
+        "preference_name",
+        name="user_preferences_user_services_pk",
     ),
 )
