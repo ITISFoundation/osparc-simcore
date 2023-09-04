77ea2d493c0d6b0788bc80921bb474f5ee47578b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-09-04 11:22:26 +0200

    moved cluster model into rpc_schemas

---
+++ packages/models-library/src/models_library/rpc_schemas_clusters_keeper/clusters.py
@@ -0,0 +1,23 @@
+from enum import auto
+
+from pydantic import AnyUrl, BaseModel
+
+from ..clusters import ClusterAuthentication
+from ..users import UserID
+from ..utils.enums import StrAutoEnum
+from ..wallets import WalletID
+
+
+class ClusterState(StrAutoEnum):
+    STARTED = auto()
+    RUNNING = auto()
+    STOPPED = auto()
+
+
+class ComputationalCluster(BaseModel):
+    endpoint: AnyUrl
+    authentication: ClusterAuthentication
+    state: ClusterState
+    user_id: UserID
+    wallet_id: WalletID
+    gateway_ready: bool = False
