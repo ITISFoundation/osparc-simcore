59bb8522ab56c5b96dde1dbd0697b3a4594c55ba
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-09-05 20:12:36 +0200

    updates OAS

--- services/payments/scripts/fake_payment_gateway.py
+++ services/payments/scripts/fake_payment_gateway.py
@@ -2,14 +2,14 @@ import datetime
 import types
 from datetime import datetime
 from decimal import Decimal
-from typing import Literal, TypeAlias
+from typing import Annotated, Literal, TypeAlias
 from uuid import UUID
 
 import uvicorn
-from fastapi import APIRouter, FastAPI, status
+from fastapi import APIRouter, Depends, FastAPI, Header, status
 from fastapi.responses import HTMLResponse
 from fastapi.routing import APIRoute
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 def set_operation_id_as_handler_function_name(router: APIRouter):
@@ -44,8 +44,12 @@ def create_payment_router():
 
     # payment
     @router.post("/init", response_model=PaymentInitiated)
-    def init_payment(payment: InitPayment):
+    def init_payment(
+        payment: InitPayment,
+        auth: Annotated[int, Depends(auth_session)],
+    ):
         assert payment  # nosec
+        assert auth  # nosec
 
     @router.get("/pay", response_class=HTMLResponse)
     def get_form_payment(id: PaymentID):
@@ -79,8 +83,11 @@ class GetPaymentMethod(BaseModel):
     expiration_year: int
 
 
-def create_payment_method_router():
+def auth_session(x_init_api_secret: Annotated[str | None, Header()] = None):
+    return 1
 
+
+def create_payment_method_router():
     router = APIRouter(
         prefix="/payment-methods",
         tags=[
@@ -90,8 +97,12 @@ def create_payment_method_router():
 
     # payment-methods
     @router.post(":init", response_model=PaymentMethodInitiated)
-    def init_payment_method(payment_method: InitPaymentMethod):
+    def init_payment_method(
+        payment_method: InitPaymentMethod,
+        auth: Annotated[int, Depends(auth_session)],
+    ):
         assert payment_method  # nosec
+        assert auth  # nosec
 
     @router.get("/form", response_class=HTMLResponse)
     def get_form_payment_method(id: PaymentMethodID):
@@ -99,20 +110,34 @@ def create_payment_method_router():
 
     # CRUD payment-methods
     @router.get("", response_model=list[GetPaymentMethod])
-    def list_payment_methods():
-        ...
+    def list_payment_methods(
+        auth: Annotated[int, Depends(auth_session)],
+    ):
+        assert auth  # nosec
 
     @router.get("/{id}", response_class=HTMLResponse)
-    def get_payment_method(id: PaymentMethodID):
+    def get_payment_method(
+        id: PaymentMethodID,
+        auth: Annotated[int, Depends(auth_session)],
+    ):
         assert id  # nosec
+        assert auth  # nosec
 
     @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-    def delete_payment_method(id: PaymentMethodID):
+    def delete_payment_method(
+        id: PaymentMethodID,
+        auth: Annotated[int, Depends(auth_session)],
+    ):
         assert id  # nosec
+        assert auth  # nosec
 
     @router.post("/{id}:pay", response_model=PaymentInitiated)
-    def pay_with_payment_method(payment: InitPayment):
+    def pay_with_payment_method(
+        payment: InitPayment,
+        auth: Annotated[int, Depends(auth_session)],
+    ):
         assert payment  # nosec
+        assert auth  # nosec
 
     return router
 
@@ -120,17 +145,16 @@ def create_payment_method_router():
 class AckPayment(BaseModel):
     idr: PaymentID
     success: bool
-    message: str = None
+    message: str = Field(default=None)
 
 
 class AckPaymentMethod(BaseModel):
     idr: PaymentMethodID
     success: bool
-    message: str = None
+    message: str = Field(default=None)
 
 
 def create_payment_service_ack_routes():
-
     router = APIRouter(
         tags=[
             "Ack (our payment service)",
@@ -139,12 +163,12 @@ def create_payment_service_ack_routes():
 
     @router.post("/payments:ack", status_code=status.HTTP_200_OK)
     def ack_payment(ack: AckPayment):
-        """ACK payment created by /pay"""
+        """ACK payment created by `/pay`"""
         assert ack  # nosec
 
     @router.post("/payments-method:ack", status_code=status.HTTP_200_OK)
     def ack_payment_method(ack: AckPaymentMethod):
-        """ACK payment method added by /paymeth-methods:init"""
+        """ACK payment method added by `/paymeth-methods:init`"""
         assert ack  # nosec
 
     return router
