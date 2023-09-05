6d07bb7c35b2d408927330b7c2d983272bc3670d
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-09-05 20:41:54 +0200

    placeholder for rpc

---
+++ services/payments/src/simcore_service_payments/rpc/__init__.py
@@ -0,0 +1,13 @@
+#
+# server interfaces (called by web-server)
+#   - init_payment
+#   - init_create_payment_method
+
+
+#
+#
+# client interfaces (targets web-server)
+#   - ack_payment
+#   - ack_create_payment_method
+#
+#
