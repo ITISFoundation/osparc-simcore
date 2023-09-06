a045f1d4b2b78c38b7dec1b6477a2dfd3ae252f4
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2023-09-06 14:30:06 +0200

    ✨ Frontend: Connect payments and transactions (#4704)

--- services/static-webserver/client/source/class/osparc/desktop/credits/WalletsMiniViewer.js
+++ services/static-webserver/client/source/class/osparc/desktop/credits/WalletsMiniViewer.js
@@ -62,11 +62,10 @@ qx.Class.define("osparc.desktop.credits.WalletsMiniViewer", {
 
     __reloadLayout: function() {
       const activeWallet = this.getActiveWallet();
-      if (activeWallet) {
-        this.__showOneWallet(activeWallet);
-      } else if (osparc.store.Store.getInstance().getWallets().find(wallet => wallet.isDefaultWallet())) {
-        const found = osparc.store.Store.getInstance().getWallets().find(wallet => wallet.isDefaultWallet());
-        this.__showOneWallet(found);
+      const preferredWallet = osparc.desktop.credits.Utils.getFavouriteWallet();
+      const oneWallet = activeWallet ? activeWallet : preferredWallet;
+      if (oneWallet) {
+        this.__showOneWallet(oneWallet);
       } else if (osparc.store.Store.getInstance().getWallets().length) {
         this.__showAllWallets();
       } else {
