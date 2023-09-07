4ff89c3ff4e1434c48e7e9013ea57280cead130d
Odei Maiz <maiz@itis.swiss>
2023-09-07 17:26:34 +0200

    status with color

--- services/static-webserver/client/source/class/osparc/desktop/credits/Transactions.js
+++ services/static-webserver/client/source/class/osparc/desktop/credits/Transactions.js
@@ -58,6 +58,33 @@ qx.Class.define("osparc.desktop.credits.Transactions", {
       }
     },
 
+    addColorTag: function(status) {
+      const color = this.getLevelColor(status);
+      status = osparc.utils.Utils.onlyFirstsUp(status);
+      return ("<font color=" + color +">" + status + "</font>");
+    },
+
+    getLevelColor: function(status) {
+      const colorManager = qx.theme.manager.Color.getInstance();
+      let logLevel = null;
+      switch (status) {
+        case "SUCCESS":
+          logLevel = "info";
+          break;
+        case "PENDING":
+          logLevel = "warning";
+          break;
+        case "CANCELED":
+        case "FAILED":
+          logLevel = "error";
+          break;
+        default:
+          console.error("completedStatus unknown");
+          break;
+      }
+      return colorManager.resolve("logger-"+logLevel+"-message");
+    },
+
     createPdfIconWithLink: function(link) {
       return `<a href='${link}' target='_blank'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/833px-PDF_file_icon.svg.png' alt='Invoice' width='16' height='20'></a>`;
     },
@@ -78,7 +105,7 @@ qx.Class.define("osparc.desktop.credits.Transactions", {
           }
           newData[cols["wallet"].pos] = walletName;
           if (data["completedStatus"]) {
-            newData[cols["status"].pos] = osparc.utils.Utils.onlyFirstsUp(data["completedStatus"]);
+            newData[cols["status"].pos] = this.addColorTag(data["completedStatus"]);
           }
           newData[cols["comment"].pos] = data["comment"];
           newData[cols["invoice"].pos] = this.createPdfIconWithLink(data["invoiceUrl"] ? data["invoiceUrl"] : "https://assets.website-files.com/63206faf68ab2dc3ee3e623b/634ea60a9381021f775e7a28_Placeholder%20PDF.pdf");
@@ -101,8 +128,9 @@ qx.Class.define("osparc.desktop.credits.Transactions", {
       const table = this.__table = new osparc.ui.table.Table(tableModel, {
         tableColumnModel: obj => new qx.ui.table.columnmodel.Resize(obj)
       });
-      const imageRenderer = new qx.ui.table.cellrenderer.Html();
-      table.getTableColumnModel().setDataCellRenderer(cols.invoice.pos, imageRenderer);
+      const htmlRenderer = new qx.ui.table.cellrenderer.Html();
+      table.getTableColumnModel().setDataCellRenderer(cols.status.pos, htmlRenderer);
+      table.getTableColumnModel().setDataCellRenderer(cols.invoice.pos, htmlRenderer);
       table.setColumnWidth(cols.invoice.pos, 50);
       table.makeItLoose();
       this._add(table);
