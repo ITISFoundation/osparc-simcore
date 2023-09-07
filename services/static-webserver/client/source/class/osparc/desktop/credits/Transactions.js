9165564f3cf5aba6f59a675937c5980d0955f534
Odei Maiz <maiz@itis.swiss>
2023-09-07 13:41:08 +0200

    refactoring

--- services/static-webserver/client/source/class/osparc/desktop/credits/Transactions.js
+++ services/static-webserver/client/source/class/osparc/desktop/credits/Transactions.js
@@ -56,6 +56,34 @@ qx.Class.define("osparc.desktop.credits.Transactions", {
         pos: 6,
         title: qx.locale.Manager.tr("Invoice")
       }
+    },
+
+    createPdfIconWithLink: function(link) {
+      return `<a href='${link}' target='_blank'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/833px-PDF_file_icon.svg.png' alt='Invoice' width='16' height='20'></a>`;
+    },
+
+    respDataToTableData: function(datas) {
+      const newDatas = [];
+      if (datas) {
+        const cols = this.COLUMNS;
+        datas.forEach(data => {
+          const newData = [];
+          newData[cols["date"].pos] = osparc.utils.Utils.formatDateAndTime(new Date(data["createdAt"]));
+          newData[cols["price"].pos] = data["priceDollars"] ? data["priceDollars"] : 0;
+          newData[cols["credits"].pos] = data["osparcCredits"] ? data["osparcCredits"] : 0;
+          let walletName = "Unknown";
+          const found = osparc.desktop.credits.Utils.getWallet(data["walletId"]);
+          if (found) {
+            walletName = found.getName();
+          }
+          newData[cols["wallet"].pos] = walletName;
+          newData[cols["status"].pos] = data["success"];
+          newData[cols["comment"].pos] = data["comment"];
+          newData[cols["invoice"].pos] = this.createPdfIconWithLink("https://assets.website-files.com/63206faf68ab2dc3ee3e623b/634ea60a9381021f775e7a28_Placeholder%20PDF.pdf");
+          newDatas.push(newData);
+        });
+      }
+      return newDatas;
     }
   },
 
@@ -81,51 +109,18 @@ qx.Class.define("osparc.desktop.credits.Transactions", {
     },
 
     refetchData: function() {
-      this.__rawData = [];
-
+      this.__table.setData([]);
       osparc.data.Resources.fetch("payments", "get")
         .then(transactions => {
           if ("data" in transactions) {
-            transactions["data"].forEach(transaction => {
-              this.__addRow(
-                transaction["createdAt"],
-                transaction["priceDollars"],
-                transaction["osparcCredits"],
-                transaction["walletId"],
-                transaction["success"],
-                transaction["comment"],
-                "https://assets.website-files.com/63206faf68ab2dc3ee3e623b/634ea60a9381021f775e7a28_Placeholder%20PDF.pdf"
-              );
-            });
+            const newDatas = this.self().respDataToTableData(transactions["data"]);
+            this.__table.setData(newDatas);
           }
         })
         .catch(err => {
           osparc.component.message.FlashMessenger.getInstance().logAs(err.message, "ERROR");
           console.error(err);
         });
-    },
-
-    __createPdfIconWithLink: function(link) {
-      return `<a href='${link}' target='_blank'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/833px-PDF_file_icon.svg.png' alt='Invoice' width='16' height='20'></a>`;
-    },
-
-    __addRow: function(createdAt, price, nCredits, walletId, status, comment, invoiceUrl) {
-      let walletName = "Unknown";
-      const found = osparc.desktop.credits.Utils.getWallet(walletId);
-      if (found) {
-        walletName = found.getName();
-      }
-      const newData = [
-        osparc.utils.Utils.formatDateAndTime(new Date(createdAt)),
-        price ? price : 0,
-        nCredits ? nCredits : 0,
-        walletName,
-        comment ? comment : "",
-        status,
-        invoiceUrl ? this.__createPdfIconWithLink(invoiceUrl) : null
-      ];
-      this.__rawData.push(newData);
-      this.__table.setData(this.__rawData);
     }
   }
 });
