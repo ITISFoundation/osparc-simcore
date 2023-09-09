bccf735ef449975caf0495be4df72122a4c4612c
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-09-09 18:03:46 +0200

    gateway api

--- services/payments/src/simcore_service_payments/services/payments_gateway.py
+++ services/payments/src/simcore_service_payments/services/payments_gateway.py
@@ -5,12 +5,17 @@
 # one-time-payment
 #   - init()
 #   - execute()
-
 # payment-method
 #    - init()
 #    - execute()
 #    - get(), list()
 #    - delete()
+
+
+import asyncio
+import logging
+from dataclasses import dataclass
+from uuid import uuid4
 
 import httpx
 from fastapi import FastAPI
@@ -27,67 +32,94 @@ from ..models.payments_gateway import (
     PaymentMethodInitiated,
 )
 
-#
-# payment
-#
+_logger = logging.getLogger(__name__)
 
 
-async def init_payment(payment: InitPayment, auth) -> PaymentInitiated:
-    raise NotImplementedError
+@dataclass
+class PaymentGatewayApi:
+    client: httpx.AsyncClient
 
+    #
+    # service diagnostics
+    #
 
-async def get_form_payment_url(id_: PaymentID) -> URL:
-    raise NotImplementedError
+    async def ping(self) -> bool:
+        """Check whether server is reachable"""
+        try:
+            await self.client.get("/")
+            return True
+        except httpx.RequestError:
+            return False
 
+    async def is_healhy(self):
+        ...
 
-#
-# payment method
-#
+    #
+    # payment
+    #
 
+    async def init_payment(self, payment: InitPayment) -> PaymentInitiated:
+        _logger.debug("FAKE: init %s", f"{payment}")
+        await asyncio.sleep(2)
+        return PaymentInitiated(payment_id=uuid4())
 
-async def init_payment_method(
-    payment_method: InitPaymentMethod,
-    auth,
-) -> PaymentMethodInitiated:
-    raise NotImplementedError
+    def get_form_payment_url(self, id_: PaymentID) -> URL:
+        return self.client.base_url.copy_with(path="/pay", params={"id": f"{id_}"})
 
+    #
+    # payment method
+    #
 
-async def get_form_payment_method(id_: PaymentMethodID) -> URL:
-    raise NotImplementedError
+    async def init_payment_method(
+        self,
+        payment_method: InitPaymentMethod,
+    ) -> PaymentMethodInitiated:
+        raise NotImplementedError
 
+    async def get_form_payment_method(self, id_: PaymentMethodID) -> URL:
+        raise NotImplementedError
 
-async def list_payment_methods(auth) -> list[GetPaymentMethod]:
-    raise NotImplementedError
+    async def list_payment_methods(self) -> list[GetPaymentMethod]:
+        raise NotImplementedError
 
+    async def get_payment_method(
+        self,
+        id_: PaymentMethodID,
+    ) -> GetPaymentMethod:
+        raise NotImplementedError
 
-async def get_payment_method(
-    id_: PaymentMethodID,
-    auth,
-) -> GetPaymentMethod:
-    raise NotImplementedError
+    async def delete_payment_method(self, id_: PaymentMethodID) -> None:
+        raise NotImplementedError
 
+    async def pay_with_payment_method(self, payment: InitPayment) -> PaymentInitiated:
+        raise NotImplementedError
 
-async def delete_payment_method(id_: PaymentMethodID, auth) -> None:
-    raise NotImplementedError
+    #
+    # setup in app
+    #
 
+    @classmethod
+    def create_and_save_in_state(cls, app: FastAPI):
+        assert app.state  # nosec
+        assert not hasattr(app.state, "payment_gateway_api")  # nosec
 
-async def pay_with_payment_method(payment: InitPayment, auth) -> PaymentInitiated:
-    raise NotImplementedError
+        app_settings: ApplicationSettings = app.state.settings
 
+        app.state.payment_gateway_api = cls(
+            client=httpx.AsyncClient(
+                auth=(
+                    app_settings.PAYMENT_GATEWAY_API_KEY.get_secret_value(),
+                    app_settings.PAYMENT_GATEWAY_API_SECRET.get_secret_value(),
+                ),
+                base_url=app_settings.PAYMENTS_GATEWAY_URL,
+            )
+        )
 
-#
-# setup client
-#
+    @classmethod
+    def get_from_state(cls, app: FastAPI) -> "PaymentGatewayApi":
+        return app.state.payment_gateway_api
 
 
 def setup_payments_gateway(app: FastAPI):
     assert app.state  # nosec
-    app_settings: ApplicationSettings = app.state.settings
-
-    auth = None  # TODO: add auth
-
-    # TODO: create client
-    # TODO: use base client
-    app.state.payments_client = httpx.AsyncClient(
-        auth=auth, base_url=app_settings.PAYMENTS_GATEWAY_URL
-    )
+    PaymentGatewayApi.create_and_save_in_state(app)
