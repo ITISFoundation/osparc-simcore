2a82edf6a898508c85bc6ed0d7ada2f99b0d5342
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-09-11 21:05:56 +0200

    reverted

--- services/web/server/src/simcore_service_webserver/director_v2/_handlers.py
+++ services/web/server/src/simcore_service_webserver/director_v2/_handlers.py
@@ -88,7 +88,6 @@ async def start_computation(request: web.Request) -> web.Response:
         "force_restart": force_restart,
         "cluster_id": cluster_id,
         "simcore_user_agent": simcore_user_agent,
-        # "use_on_demand_clusters": True,  # NOTE: this will remain here until next PR
     }
 
     try:
