f03ee5b6e2d749a90fd7a97befc85d6ce6b9e6bc
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-09-11 13:36:50 +0200

    tmp repo

---
+++ services/payments/src/simcore_service_payments/db/payments_transactions_repo.py
@@ -0,0 +1,9 @@
+class PaymentsTransactionsRepo:
+    #
+    # Next PRs should move most of the implementations in
+    # services/web/server/src/simcore_service_webserver/payments/_db.py
+    # here.
+    # The transition should put all the implementations in the simcore_postgres_database first
+    # so it is usable temporarily in both the webserver and here
+    #
+    ...
