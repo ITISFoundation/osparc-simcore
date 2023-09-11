d58036af7bf7be7dc8478209f040be0e606f63ff
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-09-11 14:56:45 +0200

    without wallet

--- services/director-v2/src/simcore_service_director_v2/api/routes/computations.py
+++ services/director-v2/src/simcore_service_director_v2/api/routes/computations.py
@@ -232,7 +232,7 @@ async def create_computation(  # noqa: C901, PLR0912
                     project_name=project.name,
                     simcore_user_agent=computation.simcore_user_agent,
                     user_email=await users_repo.get_user_email(computation.user_id),
-                    wallet_id=computation.wallet_id,
+                    wallet_id=None,
                     wallet_name=None,
                     pricing_plan_id=None,
                     pricing_detail_id=None,
