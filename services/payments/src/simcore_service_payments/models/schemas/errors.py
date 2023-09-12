61a8d80846af94d4a057190f4002e598345ae2f1
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-09-12 10:33:03 +0200

    @GitHK @sanderegg review: rename

--- services/payments/src/simcore_service_payments/models/schemas/errors.py
+++ services/payments/src/simcore_service_payments/models/schemas/errors.py
@@ -4,7 +4,7 @@ from models_library.utils.pydantic_tools_extension import NOT_REQUIRED
 from pydantic import BaseModel, Field
 
 
-class Error(BaseModel):
+class PaymentsError(BaseModel):
     error: str = Field(..., description="Standarized error name")
     message: str = Field(..., description="Human readable error message")
     details: Any = Field(NOT_REQUIRED, description="Further details")
