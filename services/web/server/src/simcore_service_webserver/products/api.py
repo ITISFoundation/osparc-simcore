ee4277feaef48d359f9afa4948bd66a4f951188b
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-09-13 00:50:52 +0200

    fixesmypy

--- services/web/server/src/simcore_service_webserver/products/api.py
+++ services/web/server/src/simcore_service_webserver/products/api.py
@@ -1,10 +1,12 @@
+from models_library.products import ProductName
+
 from ._api import (
     get_current_product,
     get_product_name,
     get_product_template_path,
     list_products,
 )
-from ._model import Product, ProductName
+from ._model import Product
 
 __all__: tuple[str, ...] = (
     "get_current_product",
