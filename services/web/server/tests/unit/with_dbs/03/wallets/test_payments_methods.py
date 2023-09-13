e5a61ec4ee869bc6ae4eb9c37b9edfc439c1e6df
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-09-13 18:12:04 +0200

    adds test for cancelation

--- services/web/server/tests/unit/with_dbs/03/wallets/test_payments_methods.py
+++ services/web/server/tests/unit/with_dbs/03/wallets/test_payments_methods.py
@@ -19,9 +19,7 @@ from pydantic import parse_obj_as
 from pytest_mock import MockerFixture
 from pytest_simcore.helpers.utils_assert import assert_status
 from pytest_simcore.helpers.utils_login import UserInfoDict
-from simcore_postgres_database.models.payments_transactions import (
-    PaymentTransactionState,
-)
+from simcore_postgres_database.models.payments_methods import InitPromptAckFlowState
 from simcore_service_webserver.db.models import UserRole
 from simcore_service_webserver.payments._api import (
     complete_create_of_wallet_payment_method,
@@ -72,7 +70,7 @@ async def logged_user_wallet(
     return await create_new_wallet()
 
 
-async def test_add_payment_method_worfklow(
+async def test_payment_method_worfklow(
     client: TestClient,
     logged_user_wallet: WalletGet,
     mocker: MockerFixture,
@@ -85,9 +83,9 @@ async def test_add_payment_method_worfklow(
     send_message = mocker.patch(
         "simcore_service_webserver.payments._socketio.send_messages", autospec=True
     )
-
     wallet = logged_user_wallet
 
+    # init Create
     response = await client.post(
         f"/v0/wallets/{wallet.wallet_id}/payments-method:init",
     )
@@ -99,26 +97,24 @@ async def test_add_payment_method_worfklow(
     assert init.payment_method_form_url.query
     assert init.payment_method_form_url.query.endswith(init.payment_method_id)
 
-    # if I try to get the payment method here, it should fail with not completed!
+    # Get: if I try to get the payment method here, it should fail since the flow is NOT acked!
     response = await client.get(
         f"/v0/wallets/{wallet.wallet_id}/payments-method/{init.payment_method_id}"
     )
     data, error = await assert_status(response, web.HTTPConflict)
     assert "complete" in error["errors"][0]["details"]
 
-    # Complete
+    # Ack
     await complete_create_of_wallet_payment_method(
         client.app,
         payment_method_id=init.payment_method_id,
-        completion_state=PaymentTransactionState.SUCCESS,
-        message="set in test_add_payment_method_worfklow",
+        completion_state=InitPromptAckFlowState.SUCCESS,
+        message="ACKED by test_add_payment_method_worfklow",
     )
-
-    # check notification
     assert send_message.called
     send_message.assert_called_once()
 
-    # get payment-method for this wallet
+    # Get
     response = await client.get(
         f"/v0/wallets/{wallet.wallet_id}/payments-method/{init.payment_method_id}"
     )
@@ -126,14 +122,14 @@ async def test_add_payment_method_worfklow(
     payment_method = PaymentMethodGet(**data)
     assert payment_method.idr == init.payment_method_id
 
-    # list all payment-methods for this wallet
+    # List
     response = await client.get(f"/v0/wallets/{wallet.wallet_id}/payments-method")
     data, _ = await assert_status(response, web.HTTPOk)
 
     wallet_payments_methods = parse_obj_as(list[PaymentMethodGet], data)
     assert wallet_payments_methods == [payment_method]
 
-    # delete
+    # Delete
     response = await client.delete(
         f"/v0/wallets/{wallet.wallet_id}/payments-method/{init.payment_method_id}"
     )
@@ -145,3 +141,31 @@ async def test_add_payment_method_worfklow(
 
     # TODO: if you like to the new naming of entrypoints and models for the flow, make it uniform with payments
     # TODO:
+
+
+async def test_init_and_cancel_payment_method(
+    client: TestClient,
+    logged_user_wallet: WalletGet,
+    mocker: MockerFixture,
+):
+    wallet = logged_user_wallet
+
+    # init Create
+    response = await client.post(
+        f"/v0/wallets/{wallet.wallet_id}/payments-method:init",
+    )
+    data, error = await assert_status(response, web.HTTPCreated)
+    assert error is None
+    init = CreatePaymentMethodInitiated.parse_obj(data)
+
+    # cancel Create
+    response = await client.post(
+        f"/v0/wallets/{wallet.wallet_id}/payments-method/{init.payment_method_id}:cancel",
+    )
+    await assert_status(response, web.HTTPNoContent)
+
+    # Get -> not found
+    response = await client.get(
+        f"/v0/wallets/{wallet.wallet_id}/payments-method/{init.payment_method_id}"
+    )
+    await assert_status(response, web.HTTPNotFound)
