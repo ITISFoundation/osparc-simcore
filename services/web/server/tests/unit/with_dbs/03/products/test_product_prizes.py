8ae1ae707429edd641f023279f84d2663c6e5c94
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-09-14 21:18:04 +0200

    drafts tests

---
+++ services/web/server/tests/unit/with_dbs/03/products/test_product_prizes.py
@@ -0,0 +1,26 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+# pylint: disable=too-many-arguments
+
+
+import pytest
+from aiohttp import web
+from aiohttp.test_utils import TestClient
+from pytest_simcore.helpers.utils_assert import assert_status
+from pytest_simcore.helpers.utils_login import UserInfoDict
+from simcore_service_webserver.db.models import UserRole
+
+
+@pytest.mark.parametrize(
+    "user_role,expected",
+    [
+        (role, web.HTTPOk if role >= UserRole.USER else web.HTTPForbidden)
+        for role in UserRole
+    ],
+)
+async def test_get_product_prize(client: TestClient, logged_user: UserInfoDict):
+    response = await client.get("/v0/prize")
+    data, _ = await assert_status(response, web.HTTPOk)
+
+    assert data["dollarsPerCredit"] >= 0
