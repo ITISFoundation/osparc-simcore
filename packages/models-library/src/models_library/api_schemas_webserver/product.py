e367ffcbc71b92090cb9c93e532a093f82a5a1c8
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-09-19 21:43:13 +0200

    @sanderegg review: constraint

--- packages/models-library/src/models_library/api_schemas_webserver/product.py
+++ packages/models-library/src/models_library/api_schemas_webserver/product.py
@@ -1,31 +1,23 @@
-from decimal import Decimal
 from typing import Any, ClassVar
 
-from pydantic import Field, validator
+from pydantic import Field
 
+from ..basic_types import NonNegativeDecimal
 from ._base import OutputSchema
 
 
 class ProductPriceGet(OutputSchema):
     product_name: str
-    usd_per_credit: Decimal | None = Field(
+    usd_per_credit: NonNegativeDecimal | None = Field(
         ...,
         description="Price of a credit in USD. "
         "If None, then this product's price is UNDEFINED",
     )
 
-    @validator("usd_per_credit")
-    @classmethod
-    def non_negative(cls, v):
-        if v < 0:
-            msg = "Must be non-negative value, got {v}"
-            raise ValueError(msg)
-        return v
-
     class Config:
         schema_extra: ClassVar[dict[str, Any]] = {
             "examples": [
-                {"productName": "osparc", "usdPerCredit": "null"},
+                {"productName": "osparc", "usdPerCredit": None},
                 {"productName": "osparc", "usdPerCredit": "10"},
             ]
         }
