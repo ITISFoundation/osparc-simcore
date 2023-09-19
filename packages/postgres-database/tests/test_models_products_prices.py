b22905a3d05d905def7fcb2c81c370c6ba325a83
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-09-19 13:47:35 +0200

    adds more tests to db

--- packages/postgres-database/tests/test_models_products_prices.py
+++ packages/postgres-database/tests/test_models_products_prices.py
@@ -4,28 +4,36 @@
 # pylint: disable=too-many-arguments
 
 
+import pytest
 import sqlalchemy as sa
 from aiopg.sa.connection import SAConnection
+from aiopg.sa.result import RowProxy
 from pytest_simcore.helpers.rawdata_fakers import random_product
+from simcore_postgres_database.errors import CheckViolation, ForeignKeyViolation
 from simcore_postgres_database.models.products import products
 from simcore_postgres_database.models.products_prices import products_prices
 
 
-async def test_creating_product_prices(connection: SAConnection):
-    # a product
+@pytest.fixture
+async def fake_product(connection: SAConnection) -> RowProxy:
     result = await connection.execute(
         products.insert()
         .values(random_product(group_id=None))
         .returning(sa.literal_column("*"))
     )
     product = await result.first()
-    assert product
+    assert product is not None
+    return product
 
+
+async def test_creating_product_prices(
+    connection: SAConnection, fake_product: RowProxy
+):
     # a price per product
     result = await connection.execute(
         products_prices.insert()
         .values(
-            product_name=product.name,
+            product_name=fake_product.name,
             usd_per_credit=100,
             authorized_by="PO Mr X",
         )
@@ -34,4 +42,83 @@ async def test_creating_product_prices(connection: SAConnection):
     product_prices = await result.first()
     assert product_prices
 
-    # check if the user is PO when the price is setup
+
+async def test_non_negative_price_not_allowed(
+    connection: SAConnection, fake_product: RowProxy
+):
+    # negative price not allowed
+    with pytest.raises(CheckViolation) as exc_info:
+        await connection.execute(
+            products_prices.insert().values(
+                product_name=fake_product.name,
+                usd_per_credit=-100,  # <----- NEGATIVE
+                authorized_by="PO Mr X",
+            )
+        )
+
+    assert exc_info.value
+
+    # zero price is allowed
+    await connection.execute(
+        products_prices.insert().values(
+            product_name=fake_product.name,
+            usd_per_credit=0,  # <----- ZERO
+            authorized_by="PO Mr X",
+        )
+    )
+
+
+async def test_price_history_of_a_product(
+    connection: SAConnection, fake_product: RowProxy
+):
+    # initial price
+    await connection.execute(
+        products_prices.insert().values(
+            product_name=fake_product.name,
+            usd_per_credit=1,
+            authorized_by="PO Mr X",
+        )
+    )
+
+    # new price
+    await connection.execute(
+        products_prices.insert().values(
+            product_name=fake_product.name,
+            usd_per_credit=2,
+            authorized_by="Update by Mr X",
+        )
+    )
+
+    result = await connection.execute(
+        products_prices.select()
+        .where(products_prices.c.product_name == fake_product.name)
+        .order_by(sa.desc(products_prices.c.created))
+    )
+    rows = await result.fetchall()
+    assert rows
+    assert len(rows) == 2
+    assert rows[0].usd_per_credit == 2, "Newest first"
+    assert rows[1].usd_per_credit == 1
+
+
+async def test_delete_price_constraints(
+    connection: SAConnection, fake_product: RowProxy
+):
+    # products_prices
+    await connection.execute(
+        products_prices.insert().values(
+            product_name=fake_product.name,
+            usd_per_credit=10,
+            authorized_by="PO Mr X",
+        )
+    )
+
+    # should not be able to delete a product w/o deleting price first
+    with pytest.raises(ForeignKeyViolation) as exc_info:
+        await connection.execute(products.delete())
+
+    assert "delete" in f"{exc_info.value}"
+
+    # this is the correct way to delete
+    await connection.execute(products_prices.delete())
+    await connection.execute(products.delete())
