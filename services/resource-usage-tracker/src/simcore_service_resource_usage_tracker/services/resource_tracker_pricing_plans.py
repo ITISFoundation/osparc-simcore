a741c0f180b0ebb5108cb622e2c0269f2c5dbf19
matusdrobuliak66 <drobuliak66@gmail.com>
2023-09-19 10:06:55 +0200

    refactor duplication

--- services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/services/resource_tracker_pricing_plans.py
+++ services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/services/resource_tracker_pricing_plans.py
@@ -12,81 +12,6 @@ from ..api.dependencies import get_repository
 from ..modules.db.repositories.resource_tracker import ResourceTrackerRepository
 
 
-async def _list_pricing_plans_by_product(
-    resource_tracker_repo: ResourceTrackerRepository, product_name: ProductName
-) -> list[PricingPlanGet]:
-    list_of_pricing_plan_get = []
-    active_pricing_plans = (
-        await resource_tracker_repo.list_active_pricing_plans_by_product(product_name)
-    )
-    for active_pricing_plan in active_pricing_plans:
-        list_of_pricing_detail_db = (
-            await resource_tracker_repo.list_pricing_details_by_pricing_plan(
-                active_pricing_plan.pricing_plan_id
-            )
-        )
-        list_of_pricing_plan_get.append(
-            PricingPlanGet(
-                pricing_plan_id=active_pricing_plan.pricing_plan_id,
-                name=active_pricing_plan.name,
-                description=active_pricing_plan.description,
-                classification=active_pricing_plan.classification,
-                created_at=active_pricing_plan.created,
-                details=[
-                    PricingDetailMinimalGet(
-                        pricing_detail_id=detail.pricing_detail_id,
-                        unit_name=detail.unit_name,
-                        cost_per_unit=detail.cost_per_unit,
-                        valid_from=detail.valid_from,
-                        simcore_default=detail.simcore_default,
-                    )
-                    for detail in list_of_pricing_detail_db
-                ],
-            )
-        )
-    return list_of_pricing_plan_get
-
-
-async def _list_pricing_plans_by_product_and_service(
-    resource_tracker_repo: ResourceTrackerRepository,
-    product_name: ProductName,
-    service_key: ServiceKey,
-    service_version: ServiceVersion,
-) -> list[PricingPlanGet]:
-    list_of_pricing_plan_get = []
-    active_pricing_plans = (
-        await resource_tracker_repo.list_active_pricing_plans_by_product_and_service(
-            product_name, service_key, service_version
-        )
-    )
-    for active_pricing_plan in active_pricing_plans:
-        list_of_pricing_detail_db = (
-            await resource_tracker_repo.list_pricing_details_by_pricing_plan(
-                active_pricing_plan.pricing_plan_id
-            )
-        )
-        list_of_pricing_plan_get.append(
-            PricingPlanGet(
-                pricing_plan_id=active_pricing_plan.pricing_plan_id,
-                name=active_pricing_plan.name,
-                description=active_pricing_plan.description,
-                classification=active_pricing_plan.classification,
-                created_at=active_pricing_plan.created,
-                details=[
-                    PricingDetailMinimalGet(
-                        pricing_detail_id=detail.pricing_detail_id,
-                        unit_name=detail.unit_name,
-                        cost_per_unit=detail.cost_per_unit,
-                        valid_from=detail.valid_from,
-                        simcore_default=detail.simcore_default,
-                    )
-                    for detail in list_of_pricing_detail_db
-                ],
-            )
-        )
-    return list_of_pricing_plan_get
-
-
 async def list_pricing_plans(
     product_name: ProductName,
     resource_tracker_repo: Annotated[
@@ -96,7 +21,40 @@ async def list_pricing_plans(
     service_version: ServiceVersion = Query(None),
 ) -> list[PricingPlanGet]:
     if service_key is None and service_version is None:
-        return await _list_pricing_plans_by_product(resource_tracker_repo, product_name)
-    return await _list_pricing_plans_by_product_and_service(
-        resource_tracker_repo, product_name, service_key, service_version
-    )
+        active_pricing_plans = (
+            await resource_tracker_repo.list_active_pricing_plans_by_product(
+                product_name
+            )
+        )
+    else:
+        active_pricing_plans = await resource_tracker_repo.list_active_pricing_plans_by_product_and_service(
+            product_name, service_key, service_version
+        )
+
+    list_of_pricing_plan_get = []
+    for active_pricing_plan in active_pricing_plans:
+        list_of_pricing_detail_db = (
+            await resource_tracker_repo.list_pricing_details_by_pricing_plan(
+                active_pricing_plan.pricing_plan_id
+            )
+        )
+        list_of_pricing_plan_get.append(
+            PricingPlanGet(
+                pricing_plan_id=active_pricing_plan.pricing_plan_id,
+                name=active_pricing_plan.name,
+                description=active_pricing_plan.description,
+                classification=active_pricing_plan.classification,
+                created_at=active_pricing_plan.created,
+                details=[
+                    PricingDetailMinimalGet(
+                        pricing_detail_id=detail.pricing_detail_id,
+                        unit_name=detail.unit_name,
+                        cost_per_unit=detail.cost_per_unit,
+                        valid_from=detail.valid_from,
+                        simcore_default=detail.simcore_default,
+                    )
+                    for detail in list_of_pricing_detail_db
+                ],
+            )
+        )
+    return list_of_pricing_plan_get
