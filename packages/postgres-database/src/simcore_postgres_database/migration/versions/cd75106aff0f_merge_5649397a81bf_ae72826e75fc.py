78095bad82eb8290b998a9a82cd75cb49b5de1a0
Mads Bisgaard <bisgaard@itis.swiss>
2023-09-20 14:47:03 +0200

    minor change

---
+++ packages/postgres-database/src/simcore_postgres_database/migration/versions/cd75106aff0f_merge_5649397a81bf_ae72826e75fc.py
@@ -0,0 +1,22 @@
+"""merge 5649397a81bf  ae72826e75fc
+
+Revision ID: cd75106aff0f
+Revises: 5649397a81bf, ae72826e75fc
+Create Date: 2023-09-20 12:46:23.679965+00:00
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "cd75106aff0f"
+down_revision = ("5649397a81bf", "ae72826e75fc")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
