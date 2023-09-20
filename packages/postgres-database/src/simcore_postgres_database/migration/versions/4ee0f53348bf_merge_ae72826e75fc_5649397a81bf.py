aa58067c08faa13539b766ff14715ad236ef4408
Mads Bisgaard <bisgaard@itis.swiss>
2023-09-20 14:23:47 +0200

    resolve alembic merge conflict and add make recipe for automatically resolving such conflicts in the future

---
+++ packages/postgres-database/src/simcore_postgres_database/migration/versions/4ee0f53348bf_merge_ae72826e75fc_5649397a81bf.py
@@ -0,0 +1,22 @@
+"""merge ae72826e75fc  5649397a81bf
+
+Revision ID: 4ee0f53348bf
+Revises: ae72826e75fc, 5649397a81bf
+Create Date: 2023-09-20 12:22:11.950264+00:00
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "4ee0f53348bf"
+down_revision = ("ae72826e75fc", "5649397a81bf")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
