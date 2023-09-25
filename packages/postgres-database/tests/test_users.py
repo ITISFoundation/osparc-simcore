a97cff1d4e7aa7a54ba2a9a20e77b13352e446ad
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-09-25 16:55:30 +0200

    migration and tests

--- packages/postgres-database/tests/test_users.py
+++ packages/postgres-database/tests/test_users.py
@@ -3,7 +3,7 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import pytest
 import sqlalchemy as sa
@@ -26,37 +26,61 @@ def test_user_role_to_level_map_in_sync():
     assert set(_USER_ROLE_TO_LEVEL.keys()) == set(UserRole.__members__.keys())
 
 
-def test_user_role_comparison():
+def test_user_roles_compares_to_admin():
     assert UserRole.ANONYMOUS < UserRole.ADMIN
     assert UserRole.GUEST < UserRole.ADMIN
     assert UserRole.USER < UserRole.ADMIN
     assert UserRole.TESTER < UserRole.ADMIN
+    assert UserRole.PRODUCT_OWNER < UserRole.ADMIN
     assert UserRole.ADMIN == UserRole.ADMIN
 
+
+def test_user_roles_compares_to_product_owner():
+    assert UserRole.ANONYMOUS < UserRole.PRODUCT_OWNER
+    assert UserRole.GUEST < UserRole.PRODUCT_OWNER
+    assert UserRole.USER < UserRole.PRODUCT_OWNER
+    assert UserRole.TESTER < UserRole.PRODUCT_OWNER
+    assert UserRole.PRODUCT_OWNER == UserRole.PRODUCT_OWNER
+    assert UserRole.ADMIN > UserRole.PRODUCT_OWNER
+
+
+def test_user_roles_compares_to_tester():
     assert UserRole.ANONYMOUS < UserRole.TESTER
     assert UserRole.GUEST < UserRole.TESTER
     assert UserRole.USER < UserRole.TESTER
     assert UserRole.TESTER == UserRole.TESTER
+    assert UserRole.PRODUCT_OWNER > UserRole.TESTER
     assert UserRole.ADMIN > UserRole.TESTER
 
+
+def test_user_roles_compares_to_user():
     assert UserRole.ANONYMOUS < UserRole.USER
     assert UserRole.GUEST < UserRole.USER
     assert UserRole.USER == UserRole.USER
     assert UserRole.TESTER > UserRole.USER
+    assert UserRole.PRODUCT_OWNER > UserRole.USER
     assert UserRole.ADMIN > UserRole.USER
 
+
+def test_user_roles_compares_to_guest():
     assert UserRole.ANONYMOUS < UserRole.GUEST
     assert UserRole.GUEST == UserRole.GUEST
     assert UserRole.USER > UserRole.GUEST
     assert UserRole.TESTER > UserRole.GUEST
+    assert UserRole.PRODUCT_OWNER > UserRole.GUEST
     assert UserRole.ADMIN > UserRole.GUEST
 
+
+def test_user_roles_compares_to_anonymous():
     assert UserRole.ANONYMOUS == UserRole.ANONYMOUS
     assert UserRole.GUEST > UserRole.ANONYMOUS
     assert UserRole.USER > UserRole.ANONYMOUS
     assert UserRole.TESTER > UserRole.ANONYMOUS
+    assert UserRole.PRODUCT_OWNER > UserRole.ANONYMOUS
     assert UserRole.ADMIN > UserRole.ANONYMOUS
 
+
+def test_user_roles_compares():
     # < and >
     assert UserRole.TESTER < UserRole.ADMIN
     assert UserRole.ADMIN > UserRole.TESTER
@@ -74,7 +98,7 @@ async def test_trial_accounts(pg_engine: Engine):
 
     async with pg_engine.acquire() as conn:
         # creates trial user
-        client_now = datetime.utcnow()
+        client_now = datetime.now(tz=timezone.utc)
         user_id: int | None = await conn.scalar(
             users.insert()
             .values(
