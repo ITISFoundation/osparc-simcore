9e53b79fd447351828be6d82b0fb82e827a050b9
Odei Maiz <maiz@itis.swiss>
2023-09-26 10:37:47 +0200

    larger caret

--- services/static-webserver/client/source/class/osparc/tours/Step.js
+++ services/static-webserver/client/source/class/osparc/tours/Step.js
@@ -19,7 +19,7 @@ qx.Class.define("osparc.tours.Step", {
   extend: osparc.ui.basic.FloatingHelper,
 
   construct: function(element, title, message) {
-    this.base(arguments, element);
+    this.base(arguments, element, "large");
 
     this.setLayout(new qx.ui.layout.VBox(8));
 
