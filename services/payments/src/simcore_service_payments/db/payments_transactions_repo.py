b071b74c83ca4e2753080fca6968e35d2f1cdc5d
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-09-27 19:14:57 +0200

    repo

--- services/payments/src/simcore_service_payments/db/payments_transactions_repo.py
+++ services/payments/src/simcore_service_payments/db/payments_transactions_repo.py
@@ -1,3 +1,16 @@
+import datetime
+from decimal import Decimal
+
+from models_library.api_schemas_webserver.wallets import PaymentID
+from models_library.users import UserID
+from models_library.wallets import WalletID
+from simcore_postgres_database.models.payments_transactions import (
+    PaymentTransactionState,
+)
+
+from ..models.db import PaymentsTransactionsDB
+
+
 class PaymentsTransactionsRepo:
     #
     # Next PRs should move most of the implementations in
@@ -6,4 +19,24 @@ class PaymentsTransactionsRepo:
     # The transition should put all the implementations in the simcore_postgres_database first
     # so it is usable temporarily in both the webserver and here
     #
-    ...
+    async def init_payment_transaction(
+        self,
+        payment_id: str,
+        price_dollars: Decimal,
+        osparc_credits: Decimal,
+        product_name: str,
+        user_id: UserID,
+        user_email: str,
+        wallet_id: WalletID,
+        comment: str | None,
+        initiated_at: datetime.datetime,
+    ):
+        raise NotImplementedError
+
+    async def ack_payment_transaction(
+        self,
+        payment_id: PaymentID,
+        completion_state: PaymentTransactionState,
+        state_message: str | None,
+    ) -> PaymentsTransactionsDB:
+        raise NotImplementedError
---
+++ services/payments/src/simcore_service_payments/models/db.py
@@ -0,0 +1,52 @@
+import datetime
+from decimal import Decimal
+from typing import Any, ClassVar
+
+from models_library.api_schemas_webserver.wallets import PaymentID
+from models_library.emails import LowerCaseEmailStr
+from models_library.products import ProductName
+from models_library.users import UserID
+from models_library.wallets import WalletID
+from pydantic import BaseModel
+from simcore_postgres_database.models.payments_transactions import (
+    PaymentTransactionState,
+)
+
+
+#
+# NOTE: this will be moved to the payments service
+# NOTE: with https://sqlmodel.tiangolo.com/ we would only define this once!
+class PaymentsTransactionsDB(BaseModel):
+    payment_id: PaymentID
+    price_dollars: Decimal  # accepts negatives
+    osparc_credits: Decimal  # accepts negatives
+    product_name: ProductName
+    user_id: UserID
+    user_email: LowerCaseEmailStr
+    wallet_id: WalletID
+    comment: str | None
+    initiated_at: datetime.datetime
+    completed_at: datetime.datetime | None
+    state: PaymentTransactionState
+    state_message: str | None
+
+    class Config:
+        orm_mode = True
+        schema_extra: ClassVar[dict[str, Any]] = {
+            "examples": [
+                {
+                    "payment_id": "12345",
+                    "price_dollars": 10.99,
+                    "osparc_credits": 5.0,
+                    "product_name": "Sample Product",
+                    "user_id": 123,
+                    "user_email": "user@example.com",
+                    "wallet_id": "wallet567",
+                    "comment": "This is a test comment.",
+                    "initiated_at": "2023-09-27T10:00:00",
+                    "completed_at": "2023-09-27T10:00:10",
+                    "state": "SUCCESS",
+                    "state_message": "Payment completed successfully",
+                }
+            ]
+        }
