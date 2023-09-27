dd3b96aa20782fd0b071acdc01df2bb0a50d9f08
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-09-28 01:15:12 +0200

    minor

--- services/web/server/src/simcore_service_webserver/products/_invitations_handlers.py
+++ services/web/server/src/simcore_service_webserver/products/_invitations_handlers.py
@@ -40,7 +40,7 @@ async def generate_invitation(request: web.Request):
 
     _, user_email = await get_user_name_and_email(request.app, user_id=req_ctx.user_id)
 
-    # TODO: check if invitations are activated in this product or raise
+    # NOTE: check if invitations are activated in this product or raise
 
     generated = await api.generate_invitation(
         request.app,
