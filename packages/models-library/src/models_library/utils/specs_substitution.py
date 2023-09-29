5238d9e4cdea635574713239ce3706e7433fe346
Andrei Neagu <neagu@itis.swiss>
2023-09-29 14:23:58 +0200

    refactor to work with more cases

--- packages/models-library/src/models_library/utils/specs_substitution.py
+++ packages/models-library/src/models_library/utils/specs_substitution.py
@@ -1,6 +1,6 @@
-from typing import Any, TypeAlias, cast
+import json
+from typing import Any, Final, TypeAlias, cast
 
-import yaml
 from pydantic import StrictBool, StrictFloat, StrictInt
 
 from .string_substitution import (
@@ -13,6 +13,16 @@ from .string_substitution import (
 # deserialization issues on the TextTemplate substitution!
 SubstitutionValue: TypeAlias = StrictBool | StrictInt | StrictFloat | str
 
+_EXPECTED_PARTS: Final[int] = 2
+
+
+def _serializer(data: dict[str, Any]) -> str:
+    return json.dumps(data)
+
+
+def _deserializer(str_data: str) -> dict[str, Any]:
+    return json.loads(str_data)
+
 
 class SpecsSubstitutionsResolver:
     """
@@ -22,7 +32,7 @@ class SpecsSubstitutionsResolver:
 
     """
 
-    def __init__(self, specs: dict[str, Any], upgrade: bool):
+    def __init__(self, specs: dict[str, Any], *, upgrade: bool):
         self._template = self._create_text_template(specs, upgrade=upgrade)
         self._substitutions: SubstitutionsDict = SubstitutionsDict()
 
@@ -31,7 +41,7 @@ class SpecsSubstitutionsResolver:
         cls, specs: dict[str, Any], *, upgrade: bool
     ) -> TextTemplate:
         # convert to yaml (less symbols as in json)
-        service_spec_str: str = yaml.safe_dump(specs)
+        service_spec_str: str = _serializer(specs)
 
         if upgrade:  # legacy
             service_spec_str = substitute_all_legacy_identifiers(service_spec_str)
@@ -55,22 +65,40 @@ class SpecsSubstitutionsResolver:
         return self._substitutions
 
     def set_substitutions(
-        self, environs: dict[str, SubstitutionValue]
+        self, mappings: dict[str, SubstitutionValue] | None = None
     ) -> SubstitutionsDict:
-        """NOTE: ONLY targets identifiers declared in the specs"""
-        identifiers_needed = self.get_identifiers()
+        """
+        NOTE: ONLY targets identifiers declared in the specs
+        NOTE:`${identifier:-a_default_value}` will replace the identifier with `a_default_value`
+        if not provided
+        """
+        if mappings is None:
+            mappings = {}
 
+        needed_identifiers = self.get_identifiers()
+
+        needed_identifiers_with_defaults: dict[str, str | None] = {}
+        for identifier in needed_identifiers:
+            parts = identifier.split(":-")
+            if len(parts) == _EXPECTED_PARTS or ":-" in identifier:
+                default = parts[1]
+                needed_identifiers_with_defaults[identifier] = default
+            else:
+                needed_identifiers_with_defaults[identifier] = None
+
+        resolved_identifiers: dict[str, str] = {}
+        for identifier in needed_identifiers_with_defaults:
+            if identifier in mappings:
+                resolved_identifiers[identifier] = mappings[identifier]
+            elif needed_identifiers_with_defaults[identifier] is not None:
+                resolved_identifiers[identifier] = needed_identifiers_with_defaults[
+                    identifier
+                ]
         # picks only needed for substitution
-        self._substitutions = SubstitutionsDict(
-            {
-                identifier: environs[identifier]
-                for identifier in identifiers_needed
-                if identifier in environs
-            }
-        )
+        self._substitutions = SubstitutionsDict(resolved_identifiers)
         return self._substitutions
 
     def run(self) -> dict[str, Any]:
         new_specs_txt: str = self._template.safe_substitute(self._substitutions)
-        new_specs = yaml.safe_load(new_specs_txt)
+        new_specs = _deserializer(new_specs_txt)
         return cast(dict[str, Any], new_specs)
