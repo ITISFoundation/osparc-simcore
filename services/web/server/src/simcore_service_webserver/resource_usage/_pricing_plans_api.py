6f7be19f8fa933583a38f8769abaca66e8428ae7
matusdrobuliak66 <drobuliak66@gmail.com>
2023-10-01 15:46:37 +0200

    fix mypy

--- services/web/server/src/simcore_service_webserver/resource_usage/_pricing_plans_api.py
+++ services/web/server/src/simcore_service_webserver/resource_usage/_pricing_plans_api.py
@@ -17,11 +17,13 @@ async def get_default_service_pricing_plan(
     service_key: ServiceKey,
     service_version: ServiceVersion,
 ) -> webserver_api_schemas.ServicePricingPlanGet:
-    data: rut_api_schemas.ServicePricingPlanGet = await resource_tracker_client.get_default_service_pricing_plan(  # type: ignore[no-redef]
-        app=app,
-        product_name=product_name,
-        service_key=service_key,
-        service_version=service_version,
+    data: rut_api_schemas.ServicePricingPlanGet = (
+        await resource_tracker_client.get_default_service_pricing_plan(
+            app=app,
+            product_name=product_name,
+            service_key=service_key,
+            service_version=service_version,
+        )
     )
 
     return parse_obj_as(webserver_api_schemas.ServicePricingPlanGet, data)
@@ -33,11 +35,13 @@ async def get_pricing_plan_unit(
     pricing_plan_id: PricingPlanId,
     pricing_unit_id: PricingUnitId,
 ) -> webserver_api_schemas.PricingUnitGet:
-    data: rut_api_schemas.PricingUnitGet = await resource_tracker_client.get_pricing_plan_unit(  # type: ignore[no-redef]
-        app=app,
-        product_name=product_name,
-        pricing_plan_id=pricing_plan_id,
-        pricing_unit_id=pricing_unit_id,
+    data: rut_api_schemas.PricingUnitGet = (
+        await resource_tracker_client.get_pricing_plan_unit(
+            app=app,
+            product_name=product_name,
+            pricing_plan_id=pricing_plan_id,
+            pricing_unit_id=pricing_unit_id,
+        )
     )
 
     return webserver_api_schemas.PricingUnitGet(
