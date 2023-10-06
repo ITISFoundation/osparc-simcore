a57cb98490c8d3ddc0779eb45766951a6ce123eb
Odei Maiz <maiz@itis.swiss>
2023-10-06 15:25:11 +0200

    Atom to Widget

--- services/static-webserver/client/source/class/osparc/desktop/credits/CreditsIndicator.js
+++ services/static-webserver/client/source/class/osparc/desktop/credits/CreditsIndicator.js
@@ -16,33 +16,38 @@
 ************************************************************************ */
 
 qx.Class.define("osparc.desktop.credits.CreditsIndicator", {
-  extend: qx.ui.basic.Atom,
+  extend: qx.ui.core.Widget,
 
   construct: function(wallet) {
     this.base(arguments);
 
-    this.set({
-      font: "text-16",
-      allowGrowX: false
+    this._setLayout(new qx.ui.layout.VBox());
+
+    const atom = new qx.ui.basic.Atom().set({
+      alignX: "right",
+      font: "text-16"
     });
+    this._add(atom);
+
+    const progressBar = this.__progressBar = new qx.ui.core.Widget().set({
+      allowGrowX: true,
+      height: 4
+    });
+    this._add(progressBar);
 
     if (wallet) {
       this.setWallet(wallet);
     }
 
-    this.bind("creditsAvailable", this, "label", {
+    this.addListener("changeCreditsAvailable", () => this.__recomputeProgressBar());
+    this.__recomputeProgressBar();
+
+    this.bind("creditsAvailable", atom, "label", {
       converter: () => this.__recomputeLabel()
     });
 
-    this.bind("creditsAvailable", this, "textColor", {
-      converter: val => {
-        if (val > 20) {
-          return "text";
-        } else if (val > 0.1) {
-          return "warning-yellow";
-        }
-        return "danger-red";
-      }
+    this.bind("creditsAvailable", atom, "textColor", {
+      converter: credits => this.self().creditsToColor(credits, "text")
     });
   },
 
@@ -64,19 +69,43 @@ qx.Class.define("osparc.desktop.credits.CreditsIndicator", {
   },
 
   statics: {
+    creditsToColor: function(credits, defaultColor = "text") {
+      let color = defaultColor;
+      if (credits < 0) {
+        color = "danger-red";
+      } else if (credits < 20) {
+        color = "warning-yellow";
+      }
+      return color;
+    },
+
     normalizeCredits: function(credits) {
       const logBase = (n, base) => Math.log(n) / Math.log(base);
 
       let normalized = logBase(credits, 10000) + 0.01;
       normalized = Math.min(Math.max(normalized, 0), 1);
-      return normalized;
+      return normalized * 100;
     }
   },
 
   members: {
+    __progressBar: null,
+
     __applyWallet: function(wallet) {
       if (wallet) {
         wallet.bind("creditsAvailable", this, "creditsAvailable");
+      }
+    },
+
+    __recomputeProgressBar: function() {
+      const credits = this.getCreditsAvailable();
+      if (credits !== null) {
+        const bgColor = this.self().creditsToColor(credits, "strong-main");
+        const progress = this.self().normalizeCredits(credits);
+        this.__progressBar.setBackgroundColor(bgColor);
+        this.__progressBar.getContentElement().setStyles({
+          width: progress + "%"
+        });
       }
     },
 
