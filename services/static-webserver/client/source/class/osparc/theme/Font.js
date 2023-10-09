72a67571ef0613369396f5962d2702a9bb2556fa
Odei Maiz <maiz@itis.swiss>
2023-10-09 12:24:13 +0200

    unused fonts

--- services/static-webserver/client/source/class/osparc/theme/Font.js
+++ services/static-webserver/client/source/class/osparc/theme/Font.js
@@ -38,20 +38,6 @@ qx.Theme.define("osparc.theme.Font", {
       size: 13
     },
 
-    "text-30": {
-      include: "defaults",
-      size: 30
-    },
-
-    "text-26": {
-      include: "defaults",
-      size: 24
-    },
-
-    "text-22": {
-      include: "defaults",
-      size: 22
-    },
 
     "title-20": {
       include: "defaults",
@@ -86,11 +72,6 @@ qx.Theme.define("osparc.theme.Font", {
       size: 16
     },
 
-    "text-15": {
-      include: "defaults",
-      size: 15
-    },
-
     "title-14": {
       include: "defaults",
       size: 14,
@@ -117,12 +98,6 @@ qx.Theme.define("osparc.theme.Font", {
     "text-12": {
       include: "defaults",
       size: 12
-    },
-
-    "text-12-italic": {
-      include: "defaults",
-      size: 12,
-      italic: true
     },
 
     "link-label-12": {
