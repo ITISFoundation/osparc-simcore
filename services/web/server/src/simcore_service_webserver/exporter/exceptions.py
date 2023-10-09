ef973dbb296fe21a11b22da622f19b3c025fafe7
Andrei Neagu <5694077+GitHK@users.noreply.github.com>
2023-10-09 13:25:02 +0100

    ♻️ CIMIS export content update (#4825)

--- services/web/server/src/simcore_service_webserver/exporter/exceptions.py
+++ services/web/server/src/simcore_service_webserver/exporter/exceptions.py
@@ -2,6 +2,7 @@ from aiohttp.web import HTTPBadRequest
 
 
 class SDSException(HTTPBadRequest):  # pylint: disable=too-many-ancestors
+    # NOTE: all these errors will displayed to the user
     """Basic exception for errors raised inside the module"""
 
     def __init__(self, message: str):
