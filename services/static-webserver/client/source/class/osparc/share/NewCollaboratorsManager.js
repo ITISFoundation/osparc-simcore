f76919eed2860dd2a8370414dce358817ca7e7c4
Odei Maiz <maiz@itis.swiss>
2023-10-10 15:47:56 +0200

    minor

--- services/static-webserver/client/source/class/osparc/share/NewCollaboratorsManager.js
+++ services/static-webserver/client/source/class/osparc/share/NewCollaboratorsManager.js
@@ -130,12 +130,15 @@ qx.Class.define("osparc.share.NewCollaboratorsManager", {
         if (this.__resourceData["accessRights"]) {
           // study/template/wallet
           if (this.__resourceData["resourceType"] === "wallet") {
+            // array of objects
             existingCollabs = this.__resourceData["accessRights"].map(collab => collab["gid"]);
           } else {
+            // object
             existingCollabs = Object.keys(this.__resourceData["accessRights"]);
           }
         } else if (this.__resourceData["access_rights"]) {
           // service
+          // object
           existingCollabs = Object.keys(this.__resourceData["access_rights"]);
         }
       }
