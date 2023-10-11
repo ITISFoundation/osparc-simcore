d479a8ff91fd3830fa01910bf5dd11f5e230b145
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2023-10-11 22:19:31 +0200

    @bisgaard-itis undefined -> unlimited

--- services/static-webserver/client/source/class/osparc/desktop/credits/AutoRecharge.js
+++ services/static-webserver/client/source/class/osparc/desktop/credits/AutoRecharge.js
@@ -182,7 +182,7 @@ qx.Class.define("osparc.desktop.credits.AutoRecharge", {
       layout.add(paymentAmountField);
 
       const nTopUpsLabel = new qx.ui.basic.Label().set({
-        value: this.tr("Number of Top ups left (-1 undefined):"),
+        value: this.tr("Number of Top ups left (-1 unlimited):"),
         font: "text-14"
       });
       layout.add(nTopUpsLabel);
