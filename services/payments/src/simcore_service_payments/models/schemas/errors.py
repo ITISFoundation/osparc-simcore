da5fa6bdd2fdd945396f212feb0c6cd54e84560c
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-10-14 18:41:30 +0200

    errors from code

--- services/payments/src/simcore_service_payments/models/schemas/errors.py
+++ services/payments/src/simcore_service_payments/models/schemas/errors.py
@@ -1,5 +1,7 @@
+import http
 from typing import Any
 
+import httpx
 from models_library.utils.pydantic_tools_extension import NOT_REQUIRED
 from pydantic import BaseModel, Field
 
@@ -10,3 +12,13 @@ class DefaultApiError(BaseModel):
         NOT_REQUIRED, description="Human readable error message"
     )
     detail: Any | None = Field(NOT_REQUIRED, description="Further details")
+
+    @classmethod
+    def from_status_code(cls, code: int) -> "DefaultApiError":
+        assert httpx.codes.is_error(code)  # nosec
+        httplib_code = http.HTTPStatus(code)
+        return DefaultApiError(
+            name=httplib_code.phrase,
+            message=httplib_code.description or httplib_code.phrase,
+            detail=httpx.codes.get_reason_phrase(code),
+        )
---
+++ services/payments/tests/unit/test_models_schemas.py
@@ -0,0 +1,20 @@
+# pylint: disable=protected-access
+# pylint: disable=redefined-outer-name
+# pylint: disable=too-many-arguments
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+
+
+import http
+
+import pytest
+from simcore_service_payments.models.schemas.errors import DefaultApiError
+
+
+@pytest.mark.parametrize("code", [e.value for e in http.HTTPStatus if e.value >= 400])
+def test_default_api_error_model(code: int):
+
+    error = DefaultApiError.from_status_code(code)
+    assert error.name
+    assert error.message
+    assert error.detail
