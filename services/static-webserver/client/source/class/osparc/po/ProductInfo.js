9b01b7c47f6f53eff6ae75d5fba4d85c6a7b96e3
Odei Maiz <maiz@itis.swiss>
2023-10-18 17:34:54 +0200

    ProductInfo page

---
+++ services/static-webserver/client/source/class/osparc/po/ProductInfo.js
@@ -0,0 +1,47 @@
+/* ************************************************************************
+
+   osparc - the simcore frontend
+
+   https://osparc.io
+
+   Copyright:
+     2023 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+qx.Class.define("osparc.po.ProductInfo", {
+  extend: qx.ui.core.Widget,
+
+  construct: function() {
+    this.base(arguments);
+
+    this._setLayout(new qx.ui.layout.VBox(10));
+
+    this.__fetchInfo();
+  },
+
+  members: {
+    __fetchInfo: function() {
+      const params = {
+        url: {
+          productName: osparc.product.Utils.getProductName()
+        }
+      };
+      osparc.data.Resources.fetch("productMetadata", "get", params)
+        .then(respData => {
+          const invitationRespViewer = new osparc.ui.basic.JsonTreeWidget(respData, "product-metadata");
+          const container = new qx.ui.container.Scroll();
+          container.add(invitationRespViewer);
+          this._add(container, {
+            flex: 1
+          });
+        });
+    }
+  }
+});
