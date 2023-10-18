16ae16898aea205b96f18cae3c31373ae5097377
matusdrobuliak66 <drobuliak66@gmail.com>
2023-10-18 14:18:12 +0200

    fix

--- services/director-v2/src/simcore_service_director_v2/api/dependencies/rut_client.py
+++ services/director-v2/src/simcore_service_director_v2/api/dependencies/rut_client.py
@@ -1,12 +1,7 @@
-from typing import cast
-
 from fastapi import Request
 
 from ...modules.resource_usage_tracker_client import ResourceUsageTrackerClient
 
 
 def get_rut_client(request: Request) -> ResourceUsageTrackerClient:
-    return cast(
-        ResourceUsageTrackerClient,
-        ResourceUsageTrackerClient.get_from_state(request.app),
-    )
+    return ResourceUsageTrackerClient.get_from_state(request.app)
