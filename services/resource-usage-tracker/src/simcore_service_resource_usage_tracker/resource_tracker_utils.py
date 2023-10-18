20445f03f95552cf3580493dfd9b374e2b2dd34f
matusdrobuliak66 <drobuliak66@gmail.com>
2023-10-18 16:53:13 +0200

    introducing rabbit exchange for reaching 0 credits

--- services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/resource_tracker_utils.py
+++ services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/resource_tracker_utils.py
@@ -1,9 +1,23 @@
+import asyncio
 import logging
 from datetime import datetime, timezone
+from decimal import Decimal
 
+from models_library.api_schemas_resource_usage_tracker.credit_transactions import (
+    WalletTotalCredits,
+)
 from models_library.products import ProductName
-from models_library.rabbitmq_messages import WalletCreditsMessage
+from models_library.projects import ProjectID
+from models_library.projects_nodes_io import NodeID
+from models_library.rabbitmq_messages import (
+    CreditsLimit,
+    WalletCreditsLimitReachedMessage,
+    WalletCreditsMessage,
+)
+from models_library.resource_tracker import ServiceRunId, ServiceRunStatus
+from models_library.users import UserID
 from models_library.wallets import WalletID
+from pydantic import PositiveInt
 from servicelib.rabbitmq import RabbitMQClient
 
 from .modules.db.repositories.resource_tracker import ResourceTrackerRepository
@@ -20,7 +34,7 @@ async def sum_credit_transactions_and_publish_to_rabbitmq(
     rabbitmq_client: RabbitMQClient,
     product_name: ProductName,
     wallet_id: WalletID,
-):
+) -> WalletTotalCredits:
     wallet_total_credits = (
         await resource_tracker_repo.sum_credit_transactions_by_product_and_wallet(
             product_name,
@@ -33,3 +47,74 @@ async def sum_credit_transactions_and_publish_to_rabbitmq(
         credits=wallet_total_credits.available_osparc_credits,
     )
     await rabbitmq_client.publish(publish_message.channel_name, publish_message)
+    return wallet_total_credits
+
+
+_BATCH_SIZE = 20
+
+
+async def _publish_to_rabbitmq_wallet_credits_limit_reached(
+    rabbitmq_client: RabbitMQClient,
+    service_run_id: ServiceRunId,
+    user_id: UserID,
+    project_id: ProjectID,
+    node_id: NodeID,
+    wallet_id: WalletID,
+    credits: Decimal,
+    credits_limit: CreditsLimit,
+):
+    publish_message = WalletCreditsLimitReachedMessage(
+        service_run_id=service_run_id,
+        user_id=user_id,
+        project_id=project_id,
+        node_id=node_id,
+        wallet_id=wallet_id,
+        credits=credits,
+        credits_limit=credits_limit,
+    )
+    await rabbitmq_client.publish(publish_message.channel_name, publish_message)
+
+
+async def publish_to_rabbitmq_wallet_credits_limit_reached(
+    resource_tracker_repo: ResourceTrackerRepository,
+    rabbitmq_client: RabbitMQClient,
+    product_name: ProductName,
+    wallet_id: WalletID,
+    credits: Decimal,
+    credits_limit: CreditsLimit,
+):
+    # Get all current running services for that wallet
+    total_count: PositiveInt = (
+        await resource_tracker_repo.total_service_runs_by_product_and_user_and_wallet(
+            product_name,
+            user_id=None,
+            wallet_id=wallet_id,
+            service_run_status=ServiceRunStatus.RUNNING,
+        )
+    )
+
+    for offset in range(0, total_count, _BATCH_SIZE):
+        batch_services = await resource_tracker_repo.list_service_runs_by_product_and_user_and_wallet(
+            product_name,
+            user_id=None,
+            wallet_id=wallet_id,
+            offset=offset,
+            limit=_BATCH_SIZE,
+            service_run_status=ServiceRunStatus.RUNNING,
+        )
+
+        await asyncio.gather(
+            *(
+                _publish_to_rabbitmq_wallet_credits_limit_reached(
+                    rabbitmq_client=rabbitmq_client,
+                    service_run_id=service.service_run_id,
+                    user_id=service.user_id,
+                    project_id=service.project_id,
+                    node_id=service.node_id,
+                    wallet_id=wallet_id,
+                    credits=credits,
+                    credits_limit=credits_limit,
+                )
+                for service in batch_services
+            )
+        )
