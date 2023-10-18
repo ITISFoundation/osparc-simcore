790fab6517e24ecfd5e1ddeb61c3ec90c506f413
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-10-18 14:42:58 +0200

    naming rpc

--- services/payments/src/simcore_service_payments/api/rpc/routes.py
+++ services/payments/src/simcore_service_payments/api/rpc/routes.py
@@ -3,7 +3,7 @@ import logging
 from fastapi import FastAPI
 from models_library.api_schemas_payments import PAYMENTS_RPC_NAMESPACE
 
-from ...services.rabbitmq import get_rabbitmq_rpc_client, is_rabbitmq_enabled
+from ...services.rabbitmq import get_rabbitmq_rpc_server
 from . import _payments
 
 _logger = logging.getLogger(__name__)
@@ -11,10 +11,7 @@ _logger = logging.getLogger(__name__)
 
 def setup_rpc_api_routes(app: FastAPI) -> None:
     async def _on_startup() -> None:
-        if is_rabbitmq_enabled(app):
-            rpc_client = get_rabbitmq_rpc_client(app)
-            await rpc_client.register_router(
-                _payments.router, PAYMENTS_RPC_NAMESPACE, app
-            )
+        rpc_server = get_rabbitmq_rpc_server(app)
+        await rpc_server.register_router(_payments.router, PAYMENTS_RPC_NAMESPACE, app)
 
     app.add_event_handler("startup", _on_startup)
