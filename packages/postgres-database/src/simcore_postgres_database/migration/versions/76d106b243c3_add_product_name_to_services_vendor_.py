1345658929874eb440a4e68e3ba5e9b62778689e
Andrei Neagu <neagu@itis.swiss>
2023-10-19 16:29:57 +0200

    regenerate migration

--- packages/postgres-database/src/simcore_postgres_database/migration/versions/76d106b243c3_add_product_name_to_services_vendor_.py
+++ packages/postgres-database/src/simcore_postgres_database/migration/versions/76d106b243c3_add_product_name_to_services_vendor_.py
@@ -1,16 +1,16 @@
 """add product_name to services_vendor_secrets
 
-Revision ID: 9dfb8c889469
-Revises: 5c62b190e124
-Create Date: 2023-10-18 15:31:36.949489+00:00
+Revision ID: 76d106b243c3
+Revises: 3810966d1534
+Create Date: 2023-10-19 14:28:50.637834+00:00
 
 """
 import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "9dfb8c889469"
-down_revision = "5c62b190e124"
+revision = "76d106b243c3"
+down_revision = "3810966d1534"
 branch_labels = None
 depends_on = None
 
@@ -22,7 +22,7 @@ def upgrade():
         sa.Column("product_name", sa.String(), server_default="osparc", nullable=False),
     )
     op.create_foreign_key(
-        "fk_services_name_products2",
+        "fk_services_name_products",
         "services_vendor_secrets",
         "products",
         ["product_name"],
@@ -31,6 +31,7 @@ def upgrade():
         ondelete="CASCADE",
     )
     # ### end Alembic commands ###
+
     op.drop_constraint(
         "services_vendor_secrets_pk", "services_vendor_secrets", type_="primary"
     )
