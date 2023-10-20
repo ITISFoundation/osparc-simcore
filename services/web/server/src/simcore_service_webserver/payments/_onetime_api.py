cb67c1e8e951d585a706c799cb5b7dbaca537d4c
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2023-10-20 19:40:35 +0200

    🎨 Read-only permissions to wallets do not give access to wallet-payment info (#4897)

--- services/web/server/src/simcore_service_webserver/payments/_onetime_api.py
+++ services/web/server/src/simcore_service_webserver/payments/_onetime_api.py
@@ -26,18 +26,30 @@ from ._socketio import notify_payment_completed
 _logger = logging.getLogger(__name__)
 
 
-async def check_wallet_permissions(
+MSG_WALLET_NO_ACCESS_ERROR = "User {user_id} does not have necessary permissions to do a payment into wallet {wallet_id}"
+
+
+async def raise_for_wallet_payments_permissions(
     app: web.Application,
+    *,
     user_id: UserID,
     wallet_id: WalletID,
     product_name: ProductName,
 ):
+    """
+    NOTE: payments can only be done to owned wallets therefore
+    we cannot allow users with read-only access to even read any
+    payment information associated to this wallet.
+    SEE some context about this in https://github.com/ITISFoundation/osparc-simcore/pull/4897
+    """
     permissions = await get_wallet_with_permissions_by_user(
         app, user_id=user_id, wallet_id=wallet_id, product_name=product_name
     )
     if not permissions.read or not permissions.write:
         raise WalletAccessForbiddenError(
-            reason=f"User {user_id} does not have necessary permissions to do a payment into wallet {wallet_id}"
+            reason=MSG_WALLET_NO_ACCESS_ERROR.format(
+                user_id=user_id, wallet_id=wallet_id
+            )
         )
 
 
@@ -84,7 +96,7 @@ async def init_creation_of_wallet_payment(
     """
 
     # wallet: check permissions
-    await check_wallet_permissions(
+    await raise_for_wallet_payments_permissions(
         app, user_id=user_id, wallet_id=wallet_id, product_name=product_name
     )
     user_wallet = await get_wallet_by_user(
@@ -168,7 +180,7 @@ async def cancel_payment_to_wallet(
     wallet_id: WalletID,
     product_name: ProductName,
 ) -> PaymentTransaction:
-    await check_wallet_permissions(
+    await raise_for_wallet_payments_permissions(
         app, user_id=user_id, wallet_id=wallet_id, product_name=product_name
     )
 
