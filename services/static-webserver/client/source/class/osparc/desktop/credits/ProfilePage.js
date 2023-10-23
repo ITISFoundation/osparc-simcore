dc059f924f0a005b4bf9c070a11dde3a1a9c1ecd
Odei Maiz <maiz@itis.swiss>
2023-10-23 13:52:14 +0200

    minor

--- services/static-webserver/client/source/class/osparc/desktop/credits/ProfilePage.js
+++ services/static-webserver/client/source/class/osparc/desktop/credits/ProfilePage.js
@@ -51,11 +51,6 @@ qx.Class.define("osparc.desktop.credits.ProfilePage", {
         maxWidth: 500
       });
 
-      const email = new qx.ui.form.TextField().set({
-        tabIndex: 1,
-        placeholder: this.tr("Email")
-      });
-
       const firstName = new qx.ui.form.TextField().set({
         tabIndex: 2,
         placeholder: this.tr("First Name")
@@ -66,10 +61,15 @@ qx.Class.define("osparc.desktop.credits.ProfilePage", {
         placeholder: this.tr("Last Name")
       });
 
+      const email = new qx.ui.form.TextField().set({
+        tabIndex: 1,
+        placeholder: this.tr("Email")
+      });
+
       const form = new qx.ui.form.Form();
-      form.add(email, "Email", null, "email");
       form.add(firstName, "First Name", null, "firstName");
       form.add(lastName, "Last Name", null, "lastName");
+      form.add(email, "Email", null, "email");
       box.add(new qx.ui.form.renderer.Single(form));
 
       const expirationLayout = new qx.ui.container.Composite(new qx.ui.layout.HBox(5)).set({
