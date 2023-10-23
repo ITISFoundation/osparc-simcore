b86bcdd45ff6d173a2bace42952442f39202247e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-10-23 09:02:47 +0200

    add fixture for ck docker-compose

--- services/clusters-keeper/tests/unit/test_data_docker_compose.py
+++ services/clusters-keeper/tests/unit/test_data_docker_compose.py
@@ -2,9 +2,15 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
+from pathlib import Path
 from typing import Any
 
 import pytest
+
+
+@pytest.fixture
+def clusters_keeper_docker_compose_file() -> Path:
+    ...
 
 
 @pytest.fixture
@@ -33,3 +39,19 @@ def test_redis_version_same_as_main_docker_compose(
     )
 
     assert simcore_redis["image"] == clusters_keeper_redis["image"]
+
+
+def test_all_services_run_on_manager_but_dask_sidecar(
+    clusters_keeper_docker_compose: dict[str, Any]
+):
+    for service_name, service_config in clusters_keeper_docker_compose[
+        "services"
+    ].items():
+        assert "deploy" in service_config
+        assert "placement" in service_config["deploy"]
+        assert "constraints" in service_config["deploy"]["placement"]
+        assert service_config["deploy"]["placement"]["constraints"] == [
+            "node_role==worker"
+            if service_name == "dask-sidecar"
+            else "node_role==manager"
+        ]
