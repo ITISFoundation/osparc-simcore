5912c82a0643a0e72a4fffa254c8bf613536e4ca
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-10-23 10:29:47 +0200

    added test for get_cluster_name

--- services/clusters-keeper/tests/unit/test_utils_ec2.py
+++ services/clusters-keeper/tests/unit/test_utils_ec2.py
@@ -12,6 +12,7 @@ from simcore_service_clusters_keeper.utils.ec2 import (
     all_created_ec2_instances_filter,
     compose_user_data,
     creation_ec2_tags,
+    get_cluster_name,
 )
 
 
@@ -23,6 +24,31 @@ def user_id(faker: Faker) -> UserID:
 @pytest.fixture
 def wallet_id(faker: Faker) -> WalletID:
     return faker.pyint(min_value=1)
+
+
+def test_get_cluster_name(
+    disabled_rabbitmq: None,
+    disabled_ec2: None,
+    mocked_redis_server: None,
+    app_settings: ApplicationSettings,
+    user_id: UserID,
+    wallet_id: WalletID,
+):
+    assert app_settings.SWARM_STACK_NAME
+    # manager
+    assert (
+        get_cluster_name(
+            app_settings, user_id=user_id, wallet_id=wallet_id, manager=True
+        )
+        == f"osparc-computational-cluster-manager-{app_settings.SWARM_STACK_NAME}-user_id:{user_id}-wallet_id:{wallet_id}"
+    )
+    # worker
+    assert (
+        get_cluster_name(
+            app_settings, user_id=user_id, wallet_id=wallet_id, manager=False
+        )
+        == f"osparc-computational-cluster-worker-{app_settings.SWARM_STACK_NAME}-user_id:{user_id}-wallet_id:{wallet_id}"
+    )
 
 
 def test_creation_ec2_tags(
