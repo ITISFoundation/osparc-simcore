91024ca2359cbdce3e5373110d3482a1851eefd4
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-10-23 22:04:05 +0200

    fixes tests

--- services/web/server/tests/unit/with_dbs/03/login/test_login_unregister.py
+++ services/web/server/tests/unit/with_dbs/03/login/test_login_unregister.py
@@ -5,12 +5,14 @@
 # pylint: disable=unused-variable
 
 
+from unittest.mock import MagicMock
+
 import pytest
-from aiohttp import web
+from aiohttp import ClientResponseError, web
 from aiohttp.test_utils import TestClient
 from pytest_mock import MockerFixture
 from pytest_simcore.helpers.utils_assert import assert_status
-from pytest_simcore.helpers.utils_login import UserInfoDict
+from pytest_simcore.helpers.utils_login import NewUser, UserInfoDict
 from simcore_postgres_database.models.users import UserRole
 from simcore_service_webserver.login._constants import MSG_USER_DELETED
 
@@ -29,35 +31,47 @@ async def test_unregister_account_access_rights(
         },
     )
 
-    with pytest.raises((web.HTTPUnauthorized, web.HTTPForbidden)):
+    with pytest.raises(ClientResponseError) as err_info:
         response.raise_for_status()
+
+    error = err_info.value
+    assert error.status in (
+        web.HTTPUnauthorized.status_code,
+        web.HTTPConflict.status_code,
+    ), f"{error}"
+
+
+@pytest.fixture
+def mocked_send_email(mocker: MockerFixture) -> MagicMock:
+    # OVERRIDES services/web/server/tests/unit/with_dbs/conftest.py:mocked_send_email fixture
+    return mocker.patch(
+        "simcore_service_webserver.email._core._do_send_mail",
+        spec=True,
+    )
 
 
 @pytest.mark.parametrize(
     "user_role", [role for role in UserRole if role >= UserRole.USER]
 )
 async def test_unregister_account(
-    client: TestClient, logged_user: UserInfoDict, mocker: MockerFixture
+    client: TestClient, logged_user: UserInfoDict, mocked_send_email: MagicMock
 ):
-    mock = mocker.patch(
-        "simcore_service_webserver.email._core._do_send_mail",
-        spec=True,
-    )
+    assert client.app
 
     # is logged in
     response = await client.get("/v0/me")
     await assert_status(response, web.HTTPOk)
 
     # failed check to delete another account
-    # FIXME: add here another valid user
-    response = await client.post(
-        "/v0/auth/unregister",
-        json={
-            "email": "other_user@email.com",
-            "password": logged_user["raw_password"],
-        },
-    )
-    await assert_status(response, web.HTTPConflict)
+    async with NewUser(app=client.app) as other_user:
+        response = await client.post(
+            "/v0/auth/unregister",
+            json={
+                "email": other_user["email"],
+                "password": other_user["raw_password"],
+            },
+        )
+        await assert_status(response, web.HTTPConflict)
 
     # success to request deletion of account
     response = await client.post(
@@ -70,7 +84,7 @@ async def test_unregister_account(
     await assert_status(response, web.HTTPOk)
 
     # sent email?
-    mimetext = mock.call_args[1]["message"]
+    mimetext = mocked_send_email.call_args[1]["message"]
     assert mimetext["Subject"]
     assert mimetext["To"] == logged_user["email"]
 
