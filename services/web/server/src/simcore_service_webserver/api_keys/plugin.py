d25480d0498a210c2708a1fa42c799bb7cc7a4fc
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-10-25 00:06:22 +0200

    adds plugin in app

--- services/web/server/src/simcore_service_webserver/api_keys/plugin.py
+++ services/web/server/src/simcore_service_webserver/api_keys/plugin.py
@@ -3,8 +3,10 @@ import logging
 from aiohttp import web
 from servicelib.aiohttp.application_setup import ModuleCategory, app_module_setup
 
+from .._constants import APP_SETTINGS_KEY
 from ..db.plugin import setup_db
 from ..products.plugin import setup_products
+from ..rabbitmq import setup_rabbitmq
 from ..rest.plugin import setup_rest
 from . import _handlers
 
@@ -18,6 +20,8 @@ _logger = logging.getLogger(__name__)
     logger=_logger,
 )
 def setup_api_keys(app: web.Application):
+    assert app[APP_SETTINGS_KEY].WEBSERVER_API_KEYS  # nosec
+
     setup_db(app)
     setup_products(app)
     setup_rest(app)
@@ -25,4 +29,4 @@ def setup_api_keys(app: web.Application):
     # routes
     app.router.add_routes(_handlers.routes)
 
-    return True
+    setup_rabbitmq(app)
