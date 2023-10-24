dd77339982249ccb62ce8b0275e40cf0c0cc1daa
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-10-24 10:04:23 +0200

    @matusdrobuliak66 review: doc

--- packages/postgres-database/src/simcore_postgres_database/constants.py
+++ packages/postgres-database/src/simcore_postgres_database/constants.py
@@ -3,4 +3,7 @@ from typing import Final
 
 # NOTE: this is sync with DECIMAL_PLACES@ packages/models-library/src/models_library/basic_types.py using test_postgres_and_models_library_same_decimal_places_constant
 DECIMAL_PLACES: Final = 2
+
+# NOTE: Constant used in the exp argument of quantize to convert decimals as: Decimal().quantize(QUANTIZE_EXP_ARG)
+# See https://docs.python.org/3/library/decimal.html#decimal.Decimal.quantize
 QUANTIZE_EXP_ARG: Final = Decimal(f"1e-{DECIMAL_PLACES}")
