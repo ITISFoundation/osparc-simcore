54abe42da360ef05704b17b779258f3ec1c1efa9
Odei Maiz <maiz@itis.swiss>
2023-10-25 17:08:53 +0200

    make it yellow with warning

--- services/static-webserver/client/source/class/osparc/navigation/CreditsMenuButton.js
+++ services/static-webserver/client/source/class/osparc/navigation/CreditsMenuButton.js
@@ -30,34 +30,17 @@ qx.Class.define("osparc.navigation.CreditsMenuButton", {
       backgroundColor: "transparent"
     });
 
-    const store = osparc.store.Store.getInstance();
     const preferencesSettings = osparc.Preferences.getInstance();
+    this.__computeVisibility();
+    preferencesSettings.addListener("changeWalletIndicatorVisibility", () => this.__computeVisibility());
+    preferencesSettings.addListener("changeCreditsWarningThreshold", () => {
+      this.__computeVisibility();
+      this.__updateCredits();
+    });
 
-    const computeVisibility = () => {
-      if (preferencesSettings.getWalletIndicatorVisibility() === "warning") {
-        const wallet = store.getContextWallet();
-        if (wallet) {
-          this.setVisibility(wallet.getCreditsAvailable() <= preferencesSettings.getCreditsWarningThreshold() ? "visible" : "excluded");
-        }
-      } else if (preferencesSettings.getWalletIndicatorVisibility() === "always") {
-        this.setVisibility("visible");
-      }
-    };
-    computeVisibility();
-    preferencesSettings.addListener("changeWalletIndicatorVisibility", () => computeVisibility());
-    preferencesSettings.addListener("changeCreditsWarningThreshold", () => computeVisibility());
-
-    const updateContextWallet = () => {
-      const wallet = store.getContextWallet();
-      if (wallet) {
-        wallet.bind("creditsAvailable", this, "label", {
-          converter: credits => osparc.desktop.credits.Utils.creditsToFixed(credits) + " credits"
-        });
-      }
-    };
-
-    updateContextWallet(store.getContextWallet());
-    store.addListener("changeContextWallet", () => updateContextWallet());
+    const store = osparc.store.Store.getInstance();
+    this.__contextWalletChanged(store.getContextWallet());
+    store.addListener("changeContextWallet", () => this.__contextWalletChanged());
 
 
     const preferencesButton = new qx.ui.menu.Button(this.tr("Preferences"));
@@ -75,5 +58,41 @@ qx.Class.define("osparc.navigation.CreditsMenuButton", {
     menu.add(billingCenterButton);
 
     osparc.utils.Utils.prettifyMenu(menu);
+  },
+
+  members: {
+    __contextWalletChanged: function() {
+      const store = osparc.store.Store.getInstance();
+      const wallet = store.getContextWallet();
+      if (wallet) {
+        this.__updateCredits();
+        wallet.addListener("changeCreditsAvailable", () => this.__updateCredits());
+      }
+    },
+
+    __updateCredits: function() {
+      const store = osparc.store.Store.getInstance();
+      const wallet = store.getContextWallet();
+      if (wallet) {
+        const credits = wallet.getCreditsAvailable();
+        this.set({
+          label: credits === null ? "-" : osparc.desktop.credits.Utils.creditsToFixed(credits) + this.tr(" credits"),
+          textColor: osparc.desktop.credits.Utils.creditsToColor(credits, "text")
+        });
+      }
+    },
+
+    __computeVisibility: function() {
+      const store = osparc.store.Store.getInstance();
+      const preferencesSettings = osparc.Preferences.getInstance();
+      if (preferencesSettings.getWalletIndicatorVisibility() === "warning") {
+        const wallet = store.getContextWallet();
+        if (wallet) {
+          this.setVisibility(wallet.getCreditsAvailable() <= preferencesSettings.getCreditsWarningThreshold() ? "visible" : "excluded");
+        }
+      } else if (preferencesSettings.getWalletIndicatorVisibility() === "always") {
+        this.setVisibility("visible");
+      }
+    }
   }
 });
