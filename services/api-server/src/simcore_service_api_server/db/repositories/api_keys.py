7485390d22f56acd1b9f01ef4f9b4a1069022394
Mads Bisgaard <bisgaard@itis.swiss>
2023-10-27 13:47:59 +0200

    add User model

--- services/api-server/src/simcore_service_api_server/db/repositories/api_keys.py
+++ services/api-server/src/simcore_service_api_server/db/repositories/api_keys.py
@@ -1,6 +1,7 @@
 import logging
 
 import sqlalchemy as sa
+from pydantic import BaseModel
 from pydantic.types import PositiveInt
 from simcore_postgres_database.errors import DatabaseError
 
@@ -10,19 +11,26 @@ from ._base import BaseRepository
 _logger = logging.getLogger(__name__)
 
 
+class User(BaseModel):
+    user_id: PositiveInt | None = None
+    product_id: PositiveInt | None = None
+
+
 class ApiKeysRepository(BaseRepository):
-    async def get_user_id(self, api_key: str, api_secret: str) -> PositiveInt | None:
-        stmt = sa.select(tbl.api_keys.c.user_id).where(
+    async def get_user(self, api_key: str, api_secret: str) -> User:
+        stmt = sa.select(tbl.api_keys.c.user_id, tbl.api_keys.c.product_name).where(
             (tbl.api_keys.c.api_key == api_key)
             & (tbl.api_keys.c.api_secret == api_secret),
         )
-
+        user: User = User()
         try:
             async with self.db_engine.acquire() as conn:
-                user_id: PositiveInt | None = await conn.scalar(stmt)
+                result = await conn.execute(stmt)
+                row = await result.fetchone()
+                if row:
+                    user = User(user_id=row.user_id, product_id=row.product_name)
 
         except DatabaseError as err:
             _logger.debug("Failed to get user id: %s", err)
-            user_id = None
 
-        return user_id
+        return user
