273bd1fc8b14d7248c800770764fa85e6f3f323a
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-10-30 21:41:32 +0100

    linters

--- packages/service-library/src/servicelib/rabbitmq/rpc_interfaces/clusters_keeper/ec2_instances.py
+++ packages/service-library/src/servicelib/rabbitmq/rpc_interfaces/clusters_keeper/ec2_instances.py
@@ -8,8 +8,9 @@ from ....rabbitmq import RabbitMQRPCClient
 async def get_instance_type_details(
     client: RabbitMQRPCClient, *, instance_type_names: set[str]
 ) -> list[EC2InstanceType]:
-    return await client.request(
+    instance_types: list[EC2InstanceType] = await client.request(
         CLUSTERS_KEEPER_RPC_NAMESPACE,
         RPCMethodName("get_instance_type_details"),
         instance_type_names=instance_type_names,
     )
+    return instance_types
