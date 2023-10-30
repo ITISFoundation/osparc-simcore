1f87aa4c48f1149587e87a7b27aece1ac7e812bb
Andrei Neagu <neagu@itis.swiss>
2023-10-30 13:50:01 +0100

    services state is no recovered via API calls

--- services/director-v2/src/simcore_service_director_v2/modules/service_status_observer/_monitor.py
+++ services/director-v2/src/simcore_service_director_v2/modules/service_status_observer/_monitor.py
@@ -1,36 +1,74 @@
 from asyncio import Task
+from contextlib import suppress
 from dataclasses import dataclass
 from datetime import timedelta
 from typing import Final
 
-from fastapi import FastAPI
-from pydantic import NonNegativeFloat
+from fastapi import FastAPI, status
+from httpx import AsyncClient, HTTPError
+from models_library.api_schemas_directorv2.dynamic_services_service import (
+    RunningDynamicServiceDetails,
+)
+from models_library.projects_nodes_io import NodeID
+from pydantic import NonNegativeFloat, NonNegativeInt, ValidationError
 from servicelib.background_task import start_periodic_task, stop_periodic_task
+from servicelib.osparc_resource_manager import (
+    OsparcResourceManager,
+    OsparcResourceType,
+    ResourceIdentifier,
+)
+from servicelib.utils import logged_gather
 
+from ...meta import API_VTAG
+from ..osparc_resource_tracker import get_osparc_resource_manager
 from ._store import update_status_cache
 
 _WAIT_FOR_TASK_TO_STOP: Final[NonNegativeFloat] = 5
 
 SERVICES_STATUS_POLL_INTERVAL: Final[timedelta] = timedelta(seconds=60)
+_PARALLEL_REQUESTS: Final[NonNegativeInt] = 5
 
 
 @dataclass
 class StatusesMonitor:
     app: FastAPI
     task: Task | None = None
+    httpx_client = AsyncClient(base_url=f"http://localhost:8000/{API_VTAG}", timeout=5)
 
-    async def _task(self) -> None:
-        """run at periodic intervals"""
-        # recover the information about all the running services in the system!
-        # director-v0 legacy service are only polled
-        # director-v2 new style services are both polled and can push updates
+    async def _periodic_task(self) -> None:
+        osparc_resoruce_manager: OsparcResourceManager = get_osparc_resource_manager(
+            self.app
+        )
 
-        # TODO: fetch all service statuses here and then propagate downstream
-        _ = update_status_cache
+        tracked_dynamic_services: set[
+            ResourceIdentifier
+        ] = await osparc_resoruce_manager.get_resources(
+            OsparcResourceType.DYNAMIC_SERVICE
+        )
+
+        await logged_gather(
+            *(self._update_status_cache(NodeID(x)) for x in tracked_dynamic_services),
+            reraise=False,
+            max_concurrency=_PARALLEL_REQUESTS,
+        )
+
+    async def _update_status_cache(self, node_id: NodeID) -> None:
+        try:
+            response = await self.httpx_client.get(f"/dynamic_services/{node_id}")
+        except HTTPError:
+            return
+
+        if response.status_code != status.HTTP_200_OK:
+            return
+
+        with suppress(ValidationError):
+            await update_status_cache(
+                self.app, node_id, RunningDynamicServiceDetails.parse_raw(response.text)
+            )
 
     async def startup(self) -> None:
         self.task = start_periodic_task(
-            self._task,
+            self._periodic_task,
             interval=SERVICES_STATUS_POLL_INTERVAL,
             task_name="statuses_monitor",
         )
