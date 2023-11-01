5c6e6c8d28ed3dc643d9617b9fce45ad924bfe8f
Mads Bisgaard <bisgaard@itis.swiss>
2023-11-01 14:53:44 +0100

    propagate type

--- services/api-server/src/simcore_service_api_server/db/repositories/users.py
+++ services/api-server/src/simcore_service_api_server/db/repositories/users.py
@@ -1,17 +1,18 @@
 import sqlalchemy as sa
-from pydantic import EmailStr
+from models_library.emails import LowerCaseEmailStr
+from pydantic import parse_obj_as
 
 from ..tables import users
 from ._base import BaseRepository
 
 
 class UsersRepository(BaseRepository):
-    async def get_email_from_user_id(self, user_id: int) -> EmailStr | None:
+    async def get_email_from_user_id(self, user_id: int) -> LowerCaseEmailStr | None:
         stmt = sa.select(
             [
                 users.c.email,
             ]
         ).where(users.c.id == user_id)
         async with self.db_engine.acquire() as conn:
-            email: EmailStr | None = await conn.scalar(stmt)
-        return email
+            email: str | None = await conn.scalar(stmt)
+        return parse_obj_as(LowerCaseEmailStr, email) if email is not None else None
