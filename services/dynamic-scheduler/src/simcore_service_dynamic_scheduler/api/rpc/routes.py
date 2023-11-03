7fe0d6c45bb34fc40c323bae81f3cc59d2d58367
Andrei Neagu <neagu@itis.swiss>
2023-11-03 07:34:47 +0100

    fix mypy

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/api/rpc/routes.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/api/rpc/routes.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
-from models_library.api_schemas_dynamic_schduler import DYNAMIC_SCHEDULER_RPC_NAMESPACE
+from models_library.api_schemas_dynamic_scheduler import DYNAMIC_SCHEDULER_RPC_NAMESPACE
+from servicelib.rabbitmq import RPCRouter
 
 from ...services.rabbitmq import get_rabbitmq_rpc_server
+
+ROUTERS: list[RPCRouter] = []
 
 
 def setup_rpc_api_routes(app: FastAPI) -> None:
     async def _on_startup() -> None:
         rpc_server = get_rabbitmq_rpc_server(app)
-        for router in ():
+        for router in ROUTERS:
             await rpc_server.register_router(
                 router, DYNAMIC_SCHEDULER_RPC_NAMESPACE, app
             )
