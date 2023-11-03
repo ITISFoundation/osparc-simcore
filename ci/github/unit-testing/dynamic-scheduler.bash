64a3ced0c901eb5452538856b9034f9c33d8a7ad
Andrei Neagu <neagu@itis.swiss>
2023-11-03 07:27:25 +0100

    fixed typo

--- ci/github/unit-testing/dynamic-scheduler.bash
+++ ci/github/unit-testing/dynamic-scheduler.bash
@@ -10,7 +10,7 @@ install() {
   make devenv
   # shellcheck source=/dev/null
   source .venv/bin/activate
-  pushd services/dynamic-schduler
+  pushd services/dynamic-scheduler
   make install-ci
   popd
   .venv/bin/pip list --verbose
@@ -19,13 +19,13 @@ install() {
 test() {
   # shellcheck source=/dev/null
   source .venv/bin/activate
-  pushd services/dynamic-schduler
+  pushd services/dynamic-scheduler
   make test-ci-unit
   popd
 }
 
 typecheck() {
-  pushd services/dynamic-schduler
+  pushd services/dynamic-scheduler
   make mypy
   popd
 }
