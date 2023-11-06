2ec60d2e506b481d5156eda5d9d3457b96a54ef4
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-11-06 10:17:03 +0100

    errors and exception utils

---
+++ packages/settings-library/src/settings_library/image_build.py
@@ -0,0 +1,30 @@
+from pydantic import Field, PositiveInt, parse_obj_as
+
+
+class ImageBuildSettings:
+    """Base settings of any osparc service's app"""
+
+    # CODE STATICS ---------------------------------------------------------
+    API_VERSION: str = API_VERSION
+    APP_NAME: str = PROJECT_NAME
+    API_VTAG: VersionTag = parse_obj_as(VersionTag, API_VTAG)
+
+
+class ImageBuildSettings:
+    # IMAGE BUILDTIME ------------------------------------------------------
+    # @Makefile
+    SC_BUILD_DATE: str | None = None
+    SC_BUILD_TARGET: BuildTargetEnum | None = None
+    SC_VCS_REF: str | None = None
+    SC_VCS_URL: str | None = None
+
+    # @Dockerfile
+    SC_BOOT_TARGET: BuildTargetEnum | None = None
+    SC_HEALTHCHECK_TIMEOUT: PositiveInt | None = Field(
+        default=None,
+        description="If a single run of the check takes longer than timeout seconds "
+        "then the check is considered to have failed."
+        "It takes retries consecutive failures of the health check for the container to be considered unhealthy.",
+    )
+    SC_USER_ID: int | None = None
+    SC_USER_NAME: str | None = None
