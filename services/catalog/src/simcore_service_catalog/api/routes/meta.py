1f92ca72e2f367e39852c10d053ead9b8d33e307
Andrei Neagu <5694077+GitHK@users.noreply.github.com>
2023-11-06 07:13:58 +0000

    ✨adding `dynamic-scheduler` service (⚠️ devops) (#4973)
    Co-authored-by: Andrei Neagu <neagu@itis.swiss>

--- services/catalog/src/simcore_service_catalog/api/routes/meta.py
+++ services/catalog/src/simcore_service_catalog/api/routes/meta.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from models_library.api_schemas_catalog.meta import Meta, VersionStr
+from models_library.api_schemas__common.meta import BaseMeta
+from models_library.basic_types import VersionStr
 from pydantic import parse_obj_as
 
 from ..._meta import API_VERSION, API_VTAG
@@ -7,9 +8,9 @@ from ..._meta import API_VERSION, API_VTAG
 router = APIRouter()
 
 
-@router.get("", response_model=Meta)
+@router.get("", response_model=BaseMeta)
 async def get_service_metadata():
-    return Meta(
+    return BaseMeta(
         name=__name__.split(".")[0],
         version=parse_obj_as(VersionStr, API_VERSION),
         released={API_VTAG: parse_obj_as(VersionStr, API_VERSION)},
---
+++ services/dynamic-scheduler/tests/unit/api_rest/test_api_rest__meta.py
@@ -0,0 +1,23 @@
+# pylint:disable=redefined-outer-name
+# pylint:disable=unused-argument
+
+
+import pytest
+from fastapi import status
+from httpx import AsyncClient
+from pytest_simcore.helpers.typing_env import EnvVarsDict
+from simcore_service_dynamic_scheduler._meta import API_VTAG
+from simcore_service_dynamic_scheduler.models.schemas.meta import Meta
+
+
+@pytest.fixture
+def app_environment(
+    disable_rabbitmq_setup: None, app_environment: EnvVarsDict
+) -> EnvVarsDict:
+    return app_environment
+
+
+async def test_health(client: AsyncClient):
+    response = await client.get(f"/{API_VTAG}/meta")
+    assert response.status_code == status.HTTP_200_OK
+    assert Meta.parse_raw(response.text)
