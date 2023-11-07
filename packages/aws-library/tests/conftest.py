56cf0c59f3644e154796bc38d76d252c9ec294ca
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-11-07 17:09:31 +0100

    added skeleton

---
+++ packages/aws-library/tests/conftest.py
@@ -0,0 +1,20 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-import
+from pathlib import Path
+
+import aws_library
+import pytest
+
+pytest_plugins = [
+    "pytest_simcore.repository_paths",
+    "pytest_simcore.pydantic_models",
+    "pytest_simcore.pytest_global_environs",
+]
+
+
+@pytest.fixture(scope="session")
+def package_dir() -> Path:
+    pdir = Path(aws_library.__file__).resolve().parent
+    assert pdir.exists()
+    return pdir
