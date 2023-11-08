aa8f72d054655a712a82b064a15e42cacc2ca57b
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-11-08 22:55:47 +0100

    adds test

---
+++ tests/environment-setup/test_requirements.py
@@ -0,0 +1,21 @@
+import re
+import sys
+from pathlib import Path
+
+import pytest
+
+CURRENT_DIR = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+REPO_DIR = CURRENT_DIR.parent.parent
+assert any(REPO_DIR.glob(".git"))
+
+SERVICES_DIR = REPO_DIR / "services"
+assert SERVICES_DIR.exists()
+
+
+@pytest.mark.parametrize("exclude", ["pytest", "respx", "aioresponses"])
+@pytest.mark.parametrize("base_path", SERVICES_DIR.rglob("_base.txt"))
+def test_libraries_are_not_allowed_in_base_requirements(base_path: Path, exclude: str):
+    requirements_text = re.sub(
+        r"(^|\s)(#.*|\n)", "", base_path.read_text(), flags=re.MULTILINE
+    )
+    assert exclude not in requirements_text
