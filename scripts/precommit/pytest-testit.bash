42adfc9e80024994cdf99ccb1c5a455712480758
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-11-10 08:25:20 +0100

    another trial

--- scripts/precommit/pytest-testit.bash
+++ scripts/precommit/pytest-testit.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Check for the presence of @pytest.mark.testit in staged files
-if git diff --cached | grep -q '@pytest\.mark\.testit'; then
-    echo "Error: Your commit contains '@pytest.mark.testit'. Please remove it before committing."
-    exit 1
-fi
+git diff --cached --name-only | while IFS= read -r file; do
+    if grep -q '@pytest\.mark\.testit' "$file"; then
+        echo "Error: Your commit contains '@pytest.mark.testit' in file '$file'. Please remove it before committing."
+        exit 1
+    fi
+done
