4059e89fc0995f8f3f81b3351979c1a29606dacb
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-11-13 15:10:21 +0100

    ruff

--- services/clusters-keeper/src/simcore_service_clusters_keeper/rpc/ec2_instances.py
+++ services/clusters-keeper/src/simcore_service_clusters_keeper/rpc/ec2_instances.py
@@ -11,4 +11,7 @@ router = RPCRouter()
 async def get_instance_type_details(
     app: FastAPI, *, instance_type_names: set[str]
 ) -> list[EC2InstanceType]:
-    return await get_ec2_client(app).get_ec2_instance_capabilities(instance_type_names)
+    instance_capabilities: list[EC2InstanceType] = await get_ec2_client(
+        app
+    ).get_ec2_instance_capabilities(instance_type_names)
+    return instance_capabilities
