c6a31e12b65b8c36f4d94e08170f4fd70896927d
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-11-14 08:17:19 +0100

    @pcrespov review: add examples

--- packages/settings-library/src/settings_library/ec2.py
+++ packages/settings-library/src/settings_library/ec2.py
@@ -1,3 +1,5 @@
+from typing import Any, ClassVar
+
 from pydantic import Field
 
 from .base import BaseCustomSettings
@@ -10,3 +12,15 @@ class EC2Settings(BaseCustomSettings):
     )
     EC2_REGION_NAME: str = "us-east-1"
     EC2_SECRET_ACCESS_KEY: str
+
+    class Config(BaseCustomSettings.Config):
+        schema_extra: ClassVar[dict[str, Any]] = {
+            "examples": [
+                {
+                    "EC2_ACCESS_KEY_ID": "my_access_key_id",
+                    "EC2_ENDPOINT": "http://my_ec2_endpoint.com",
+                    "EC2_REGION_NAME": "us-east-1",
+                    "EC2_SECRET_ACCESS_KEY": "my_secret_access_key",
+                }
+            ],
+        }
