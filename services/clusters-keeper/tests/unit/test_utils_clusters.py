cd6cea93baab50b38a6f07187bb624eede57b503
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-11-15 23:31:11 +0100

    updated autoscaling docker-compose

--- services/clusters-keeper/tests/unit/test_utils_clusters.py
+++ services/clusters-keeper/tests/unit/test_utils_clusters.py
@@ -80,7 +80,6 @@ def test_create_startup_script(
     # check lists have \" written in them
     list_settings = [
         "WORKERS_EC2_INSTANCES_ALLOWED_TYPES",
-        "WORKERS_EC2_INSTANCES_CUSTOM_BOOT_SCRIPTS",
         "WORKERS_EC2_INSTANCES_SECURITY_GROUP_IDS",
     ]
     assert all(
