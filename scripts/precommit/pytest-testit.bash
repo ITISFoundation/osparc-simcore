c678d3532ce822401aa500cae4235fea8da460b7
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2023-11-15 17:39:52 +0100

    revert

--- scripts/precommit/pytest-testit.bash
+++ scripts/precommit/pytest-testit.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Check for the presence of @pytest.mark.testit in staged files
 git diff --cached --name-only | while IFS= read -r file; do
     if grep -n '@pytest\.mark\.testit' "$file"; then
         sed -i '/@pytest\.mark\.testit/d' "$file"
+        echo "Removed @pytest.mark.testit from file '$file'"
         exit 1
     fi
 done
