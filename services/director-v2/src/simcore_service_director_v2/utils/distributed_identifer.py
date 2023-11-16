6be44563463be9d6b00c834678b94c0a4beb3b31
Andrei Neagu <neagu@itis.swiss>
2023-11-16 15:57:45 +0100

    refactored distributed identifier

--- services/director-v2/src/simcore_service_director_v2/utils/distributed_identifer.py
+++ services/director-v2/src/simcore_service_director_v2/utils/distributed_identifer.py
@@ -1,29 +1,109 @@
 import logging
 from abc import ABC, abstractmethod
-from typing import Generic, TypeVar
+from asyncio import Task
+from datetime import timedelta
+from typing import Final, Generic, TypeVar
 
+from pydantic import NonNegativeInt
+from servicelib.background_task import start_periodic_task, stop_periodic_task
 from servicelib.logging_utils import log_catch, log_context
+from servicelib.redis import RedisClientSDK, RedisDatabase
+from servicelib.utils import logged_gather
 
 _logger = logging.getLogger(__name__)
+
+_REDIS_MAX_CONCURRENCY: Final[NonNegativeInt] = 10
+_DEFAULT_CLEANUP_INTERVAL: Final[timedelta] = timedelta(minutes=1)
 
 Ident = TypeVar("Ident")
 Res = TypeVar("Res")
 
+# Provided at the moment of creation.
+# Can be used inside ``is_present`` and ``remove``.
+CleanupContext = TypeVar("CleanupContext")
 
-class BaseDistributedIdentifierManager(ABC, Generic[Ident, Res]):
-    """Common interface used to manage the lifecycle of osparc resources.
 
-    An osparc resource can be anything that needs to be created and then removed
-    during the runtime of the platform.
+class BaseDistributedIdentifierManager(ABC, Generic[Ident, Res, CleanupContext]):
+    # TODO: add docstring here
 
-    Safe remove the resource.
+    @classmethod
+    @abstractmethod
+    def _deserialize_identifier(cls, raw: str) -> Ident:
+        """User provided deserialization for the identifier
 
-    For usage check ``packages/service-library/tests/test_osparc_generic_resource.py``
-    """
+        Arguments:
+            raw -- stream to be deserialized
+
+        Returns:
+            an identifier object
+        """
+
+    @classmethod
+    @abstractmethod
+    def _serialize_identifier(cls, identifier: Ident) -> str:
+        """User provided serialization for the identifier
+
+        Arguments:
+            cleanup_context -- user defined identifier object
+
+        Returns:
+            object encoded as string
+        """
+
+    @classmethod
+    @abstractmethod
+    def _deserialize_cleanup_context(cls, raw: str) -> CleanupContext:
+        """User provided deserialization for the context
+
+        Arguments:
+            raw -- stream to be deserialized
+
+        Returns:
+            an object of the type chosen by the user
+        """
+
+    @classmethod
+    @abstractmethod
+    def _serialize_cleanup_context(cls, cleanup_context: CleanupContext) -> str:
+        """User provided serialization for the context
+
+        Arguments:
+            cleanup_context -- user defined cleanup context object
+
+        Returns:
+            object encoded as string
+        """
+
+    @abstractmethod
+    async def is_used(self, identifier: Ident, cleanup_context: CleanupContext) -> bool:
+        """Check if the resource associated to the ``identifier`` is
+        still being used.
+        # NOTE: a resource can be created but not in use.
+
+        Arguments:
+            identifier -- user chosen identifier for the resource
+            cleanup_context -- user defined CleanupContext object
+
+        Returns:
+            True if ``identifier`` is still being used
+        """
+
+    @abstractmethod
+    async def _create(self, **extra_kwargs) -> tuple[Ident, Res]:
+        """Used INTERNALLY for creating the resources.
+        # NOTE: should not be used directly, use the public
+        version ``create`` instead.
+
+        Arguments:
+            **extra_kwargs -- can be overloaded by the user
+
+        Returns:
+            tuple[identifier for the resource, resource object]
+        """
 
     @abstractmethod
     async def get(self, identifier: Ident, **extra_kwargs) -> Res | None:
-        """Returns a resource if exists.
+        """If exists, returns the resource.
 
         Arguments:
             identifier -- user chosen identifier for the resource
@@ -34,36 +114,152 @@ class BaseDistributedIdentifierManager(ABC, Generic[Ident, Res]):
         """
 
     @abstractmethod
-    async def create(self, **extra_kwargs) -> tuple[Ident, Res]:
+    async def _destroy(
+        self, identifier: Ident, cleanup_context: CleanupContext
+    ) -> None:
+        """Used to destroy an existing resource
+        # NOTE: should not be used directly, use the public
+        version ``remove`` instead.
+
+        Arguments:
+            identifier -- user chosen identifier for the resource
+            cleanup_context -- user defined CleanupContext object
+        """
+
+    # PUBLIC
+
+    async def create(
+        self, *, cleanup_context: CleanupContext, **extra_kwargs
+    ) -> tuple[Ident, Res]:
         """Used for creating the resources
 
         Arguments:
+            cleanup_context -- user defined CleanupContext object
             **extra_kwargs -- can be overloaded by the user
 
         Returns:
             tuple[identifier for the resource, resource object]
         """
+        identifier, result = await self._create(**extra_kwargs)
+        await self.redis_client_sdk.redis.set(
+            self._to_redis_key(identifier),
+            self._serialize_cleanup_context(cleanup_context),
+        )
+        return identifier, result
 
-    @abstractmethod
-    async def destroy(self, identifier: Ident, **extra_kwargs) -> None:
-        """Used to destroy an existing resource
-
-        Arguments:
-            identifier -- user chosen identifier for the resource
-            **extra_kwargs -- can be overloaded by the user
-        """
-
-    async def remove(
-        self, identifier: Ident, *, reraise: bool = False, **extra_kwargs
-    ) -> None:
+    async def remove(self, identifier: Ident, *, reraise: bool = False) -> None:
         """Attempts to remove the resource, if an error occurs it is logged.
 
         Arguments:
             identifier -- user chosen identifier for the resource
             reraise -- when True raises any exception raised by ``destroy`` (default: {False})
-            **extra_kwargs -- can be overloaded by the user
         """
+
+        cleanup_context = await self._get_identifier_context(identifier)
+        if cleanup_context is None:
+            _logger.warning(
+                "Something went wrong, did not find any context for %s", identifier
+            )
+            return
+
         with log_context(
             _logger, logging.DEBUG, f"{self.__class__}: removing {identifier}"
         ), log_catch(_logger, reraise=reraise):
-            await self.destroy(identifier, **extra_kwargs)
+            await self._destroy(identifier, cleanup_context)
+
+    async def setup(self) -> None:
+        self._cleanup_task = start_periodic_task(
+            self._cleanup_unused_identifiers,
+            interval=self.cleanup_interval,
+            task_name="cleanup_unused_identifiers_task",
+        )
+
+    async def shutdown(self) -> None:
+        if self._cleanup_task:
+            await stop_periodic_task(self._cleanup_task, timeout=5)
+
+    # UTILS
+
+    @classmethod
+    @property
+    def class_path(cls) -> str:
+        return f"{cls.__module__}.{cls.__name__}"
+
+    @classmethod
+    @property
+    def _redis_key_prefix(cls) -> str:
+        return f"{cls.class_path}:"
+
+    @classmethod
+    def _to_redis_key(cls, identifier: Ident) -> str:
+        return f"{cls._redis_key_prefix}{cls._serialize_identifier(identifier)}"
+
+    @classmethod
+    def _from_redis_key(cls, redis_key: str) -> Ident:
+        return cls._deserialize_identifier(
+            redis_key.removeprefix(cls._redis_key_prefix)
+        )
+
+    # CORE
+
+    def __init__(
+        self,
+        redis_client_sdk: RedisClientSDK,
+        *,
+        cleanup_interval: timedelta = _DEFAULT_CLEANUP_INTERVAL,
+    ) -> None:
+        # TODO: add docstring
+
+        if not redis_client_sdk.redis_dsn.endswith(
+            f"{RedisDatabase.DISTRIBUTED_IDENTIFIERS}"
+        ):
+            msg = (
+                f"Redis endpoint {redis_client_sdk.redis_dsn} contains the wrong database."
+                f"Expected {RedisDatabase.DISTRIBUTED_IDENTIFIERS}"
+            )
+            raise TypeError(msg)
+
+        self.redis_client_sdk = redis_client_sdk
+        self.cleanup_interval = cleanup_interval
+
+        self._cleanup_task: Task | None = None
+
+    async def _get_identifier_context(self, identifier: Ident) -> CleanupContext | None:
+        raw: bytes | None = await self.redis_client_sdk.redis.get(
+            self._to_redis_key(identifier)
+        )
+        return self._deserialize_cleanup_context(raw) if raw else None
+
+    async def _get_tracked(self) -> dict[Ident, CleanupContext]:
+        identifiers: list[Ident] = [
+            self._from_redis_key(redis_key)
+            for redis_key in await self.redis_client_sdk.redis.keys(
+                f"{self._redis_key_prefix}*"
+            )
+        ]
+
+        cleanup_contexts: list[CleanupContext | None] = await logged_gather(
+            *(self._get_identifier_context(identifier) for identifier in identifiers),
+            max_concurrency=_REDIS_MAX_CONCURRENCY,
+        )
+
+        return {
+            identifier: cleanup_context
+            for identifier, cleanup_context in zip(
+                identifiers, cleanup_contexts, strict=True
+            )
+            # NOTE: cleanup_context will be None if the key was removed before
+            # recovering all the cleanup_contexts
+            if cleanup_context is not None
+        }
+
+    async def _cleanup_unused_identifiers(self) -> None:
+        # removes no longer used identifiers
+        tracked_data: dict[Ident, CleanupContext] = await self._get_tracked()
+        _logger.info("Will remove unused  %s", list(tracked_data.keys()))
+
+        for identifier, cleanup_context in tracked_data.items():
+            if await self.is_used(identifier, cleanup_context):
+                continue
+
+            await self.remove(identifier)
--- services/director-v2/tests/unit/test_utils_distributed_identifier.py
+++ services/director-v2/tests/unit/test_utils_distributed_identifier.py
@@ -1,75 +1,200 @@
+# pylint:disable=protected-access
 # pylint:disable=redefined-outer-name
 
-import random
+import asyncio
 import string
-from typing import Any
+from collections.abc import AsyncIterable, AsyncIterator
+from dataclasses import dataclass
+from secrets import choice
+from typing import Final
 from uuid import UUID, uuid4
 
 import pytest
+from pydantic import BaseModel, NonNegativeInt, StrBytes
 from pytest_mock import MockerFixture
+from servicelib.redis import RedisClientSDK
+from servicelib.utils import logged_gather
+from settings_library.redis import RedisDatabase, RedisSettings
 from simcore_service_director_v2.utils.distributed_identifer import (
     BaseDistributedIdentifierManager,
 )
 
+pytest_simcore_core_services_selection = [
+    "redis",
+]
 
-# define a custom type of ID for the API
+pytest_simcore_ops_services_selection = [
+    # "redis-commander",
+]
+
+# if this goes too high, max open file limit is reached
+_MAX_REDIS_CONCURRENCY: Final[NonNegativeInt] = 1000
+
+
 class UserDefinedID:
+    # define a custom type of ID for the API
+    # by choice it is hard to serialize/deserialize
+
     def __init__(self, uuid: UUID | None = None) -> None:
         self._id = uuid if uuid else uuid4()
 
     def __eq__(self, other: "UserDefinedID") -> bool:
         return self._id == other._id
 
+    # only necessary for nice looking IDs in the logs
+    def __repr__(self) -> str:
+        return f"<id={self._id}>"
+
+    # only necessary for RandomTextAPI
     def __hash__(self):
         return hash(str(self._id))
+
+
+class RandomTextEntry(BaseModel):
+    text: str
+
+    @classmethod
+    def create(cls, length: int) -> "RandomTextEntry":
+        letters_and_digits = string.ascii_letters + string.digits
+        text = "".join(choice(letters_and_digits) for _ in range(length))
+        return cls(text=text)
 
 
 # mocked api interface
 class RandomTextAPI:
     def __init__(self) -> None:
-        self._created: dict[UserDefinedID, Any] = {}
+        self._created: dict[UserDefinedID, RandomTextEntry] = {}
 
-    @staticmethod
-    def _random_string(length: int) -> str:
-        letters_and_digits = string.ascii_letters + string.digits
-        return "".join(
-            random.choice(letters_and_digits) for _ in range(length)  # noqa: S311
-        )
-
-    def create(self, length: int) -> tuple[UserDefinedID, Any]:
+    def create(self, length: int) -> tuple[UserDefinedID, RandomTextEntry]:
         identifier = UserDefinedID(uuid4())
-        self._created[identifier] = self._random_string(length)
+        self._created[identifier] = RandomTextEntry.create(length)
         return identifier, self._created[identifier]
 
     def delete(self, identifier: UserDefinedID) -> None:
         del self._created[identifier]
 
-    def get(self, identifier: UserDefinedID) -> Any | None:
+    def get(self, identifier: UserDefinedID) -> RandomTextEntry | None:
         return self._created.get(identifier, None)
 
 
+@dataclass
+class ComponentUsingRandomText:
+    # this one is tracking if the above resources are bring used
+
+    _in_use: bool = True
+
+    def is_used(self, an_id: UserDefinedID) -> bool:
+        _ = an_id
+        return self._in_use
+
+    def toggle_is_present(self, in_use: bool) -> None:
+        self._in_use = in_use
+
+
+class RandomTextCleanupContext(BaseModel):
+    # when cleaning up, extra parameters might be required
+    # in this case there aren't any since they are not required.
+    ...
+
+
 # define a custom manager using the custom user defined identifiers
-# NOTE: note that the generic uses `[UserDefinedID, Any]`
+# NOTE: note that the generic uses `[UserDefinedID, RandomTextEntry, RandomTextCleanupContext]`
 # which enforces typing constraints on the overloaded abstract methods
-class RandomTextResourcesManager(BaseDistributedIdentifierManager[UserDefinedID, Any]):
-    # pylint:disable=arguments-differ
-
-    def __init__(self) -> None:
+class RandomTextResourcesManager(
+    BaseDistributedIdentifierManager[
+        UserDefinedID, RandomTextEntry, RandomTextCleanupContext
+    ]
+):
+    def __init__(
+        self,
+        redis_client_sdk: RedisClientSDK,
+        component_using_random_text: ComponentUsingRandomText,
+    ) -> None:
+        # THESE two systems would normally come stored in the `app` context
         self.api = RandomTextAPI()
+        self.component_using_random_text = component_using_random_text
 
-    async def get(self, identifier: UserDefinedID, **_) -> Any | None:
-        return self.api.get(identifier)
+        super().__init__(redis_client_sdk)
 
-    async def create(self, length: int) -> tuple[UserDefinedID, Any]:
+    @classmethod
+    def _deserialize_identifier(cls, raw: str) -> UserDefinedID:
+        return UserDefinedID(UUID(raw))
+
+    @classmethod
+    def _serialize_identifier(cls, identifier: UserDefinedID) -> str:
+        return f"{identifier._id}"  # noqa: SLF001
+
+    @classmethod
+    def _deserialize_cleanup_context(cls, raw: StrBytes) -> RandomTextCleanupContext:
+        return RandomTextCleanupContext.parse_raw(raw)
+
+    @classmethod
+    def _serialize_cleanup_context(
+        cls, cleanup_context: RandomTextCleanupContext
+    ) -> str:
+        return cleanup_context.json()
+
+    async def is_used(
+        self, identifier: UserDefinedID, cleanup_context: RandomTextCleanupContext
+    ) -> bool:
+        _ = cleanup_context
+        return self.component_using_random_text.is_used(identifier)
+
+    async def _create(  # pylint:disable=arguments-differ # type:ignore [override]
+        self, length: int
+    ) -> tuple[UserDefinedID, RandomTextEntry]:
         return self.api.create(length)
 
-    async def destroy(self, identifier: UserDefinedID) -> None:
+    async def get(self, identifier: UserDefinedID, **_) -> RandomTextEntry | None:
+        return self.api.get(identifier)
+
+    async def _destroy(
+        self, identifier: UserDefinedID, _: RandomTextCleanupContext
+    ) -> None:
         self.api.delete(identifier)
 
 
 @pytest.fixture
-def manager() -> RandomTextResourcesManager:
-    return RandomTextResourcesManager()
+async def redis_client_sdk(
+    redis_service: RedisSettings,
+) -> AsyncIterator[RedisClientSDK]:
+    redis_resources_dns = redis_service.build_redis_dsn(
+        RedisDatabase.DISTRIBUTED_IDENTIFIERS
+    )
+
+    client = RedisClientSDK(redis_resources_dns)
+    assert client
+    assert client.redis_dsn == redis_resources_dns
+    await client.setup()
+    # cleanup, previous run's leftovers
+    await client.redis.flushall()
+
+    yield client
+    # cleanup, properly close the clients
+    await client.redis.flushall()
+    await client.shutdown()
+
+
+@pytest.fixture
+def component_using_random_text() -> ComponentUsingRandomText:
+    return ComponentUsingRandomText()
+
+
+@pytest.fixture
+async def manager_with_no_cleanup_task(
+    redis_client_sdk: RedisClientSDK,
+    component_using_random_text: ComponentUsingRandomText,
+) -> RandomTextResourcesManager:
+    return RandomTextResourcesManager(redis_client_sdk, component_using_random_text)
+
+
+@pytest.fixture
+async def manager(
+    manager_with_no_cleanup_task: RandomTextResourcesManager,
+) -> AsyncIterable[RandomTextResourcesManager]:
+    await manager_with_no_cleanup_task.setup()
+    yield manager_with_no_cleanup_task
+    await manager_with_no_cleanup_task.shutdown()
 
 
 async def test_resource_is_missing(manager: RandomTextResourcesManager):
@@ -77,29 +202,19 @@ async def test_resource_is_missing(manager: RandomTextResourcesManager):
     assert await manager.get(missing_identifier) is None
 
 
-async def test_manual_workflow(manager: RandomTextResourcesManager):
-    # creation
-    identifier, _ = await manager.create(length=1)
-    assert await manager.get(identifier) is not None
-
-    # removal
-    await manager.destroy(identifier)
-
-    # resource no longer exists
-    assert await manager.get(identifier) is None
-
-
 @pytest.mark.parametrize("delete_before_removal", [True, False])
-async def test_automatic_cleanup_workflow(
+async def test_full_workflow(
     manager: RandomTextResourcesManager, delete_before_removal: bool
 ):
     # creation
-    identifier, _ = await manager.create(length=1)
+    identifier, _ = await manager.create(
+        cleanup_context=RandomTextCleanupContext(), length=1
+    )
     assert await manager.get(identifier) is not None
 
     # optional removal
     if delete_before_removal:
-        await manager.destroy(identifier)
+        await manager.remove(identifier)
 
     is_still_present = not delete_before_removal
     assert (await manager.get(identifier) is not None) is is_still_present
@@ -121,10 +236,12 @@ async def test_remove_raises_error(
     caplog.clear()
 
     error_message = "mock error during resource destroy"
-    mocker.patch.object(manager, "destroy", side_effect=RuntimeError(error_message))
+    mocker.patch.object(manager, "_destroy", side_effect=RuntimeError(error_message))
 
     # after creation object is present
-    identifier, _ = await manager.create(length=1)
+    identifier, _ = await manager.create(
+        cleanup_context=RandomTextCleanupContext(), length=1
+    )
     assert await manager.get(identifier) is not None
 
     if reraise:
@@ -135,3 +252,98 @@ async def test_remove_raises_error(
     # check logs in case of error
     assert "Unhandled exception:" in caplog.text
     assert error_message in caplog.text
+
+
+async def _create_resources(
+    manager: RandomTextResourcesManager, count: int
+) -> list[UserDefinedID]:
+    creation_results: list[tuple[UserDefinedID, RandomTextEntry]] = await logged_gather(
+        *[
+            manager.create(cleanup_context=RandomTextCleanupContext(), length=1)
+            for _ in range(count)
+        ],
+        max_concurrency=_MAX_REDIS_CONCURRENCY,
+    )
+    return [x[0] for x in creation_results]
+
+
+async def _assert_all_resources(
+    manager: RandomTextResourcesManager,
+    identifiers: list[UserDefinedID],
+    *,
+    exist: bool,
+) -> None:
+    get_results: list[RandomTextEntry | None] = await logged_gather(
+        *[manager.get(identifier) for identifier in identifiers],
+        max_concurrency=_MAX_REDIS_CONCURRENCY,
+    )
+    if exist:
+        assert all(x is not None for x in get_results)
+    else:
+        assert all(x is None for x in get_results)
+
+
+@pytest.mark.parametrize("count", [1000])
+async def test_parallel_create_remove(manager: RandomTextResourcesManager, count: int):
+    # create resources
+    identifiers: list[UserDefinedID] = await _create_resources(manager, count)
+    await _assert_all_resources(manager, identifiers, exist=True)
+
+    # safe remove the resources, they do not exist any longer
+    await asyncio.gather(*[manager.remove(identifier) for identifier in identifiers])
+    await _assert_all_resources(manager, identifiers, exist=False)
+
+
+async def test_background_removal_of_unused_resources(
+    manager_with_no_cleanup_task: RandomTextResourcesManager,
+    component_using_random_text: ComponentUsingRandomText,
+):
+    # create resources
+    identifiers: list[UserDefinedID] = await _create_resources(
+        manager_with_no_cleanup_task, 10_000
+    )
+    await _assert_all_resources(manager_with_no_cleanup_task, identifiers, exist=True)
+
+    # call cleanup, all resources still exist
+    await manager_with_no_cleanup_task._cleanup_unused_identifiers()  # noqa: SLF001
+    await _assert_all_resources(manager_with_no_cleanup_task, identifiers, exist=True)
+
+    # make resources unused in external system
+    component_using_random_text.toggle_is_present(in_use=False)
+    await manager_with_no_cleanup_task._cleanup_unused_identifiers()  # noqa: SLF001
+    await _assert_all_resources(manager_with_no_cleanup_task, identifiers, exist=False)
+
+
+async def test_no_redis_key_overlap_when_inheriting(
+    redis_client_sdk: RedisClientSDK,
+    component_using_random_text: ComponentUsingRandomText,
+):
+    class ChildRandomTextResourcesManager(RandomTextResourcesManager):
+        ...
+
+    parent_manager = RandomTextResourcesManager(
+        redis_client_sdk, component_using_random_text
+    )
+    child_manager = ChildRandomTextResourcesManager(
+        redis_client_sdk, component_using_random_text
+    )
+
+    # create an entry in the child and one in the parent
+
+    parent_identifier, _ = await parent_manager.create(
+        cleanup_context=RandomTextCleanupContext(), length=1
+    )
+    child_identifier, _ = await child_manager.create(
+        cleanup_context=RandomTextCleanupContext(), length=1
+    )
+    assert parent_identifier != child_identifier
+
+    keys = await redis_client_sdk.redis.keys("*")
+    assert len(keys) == 2
+
+    # check keys contain the correct prefixes
+    key_prefixes: set[str] = {k.split(":")[0] for k in keys}
+    assert key_prefixes == {
+        RandomTextResourcesManager.class_path,
+        ChildRandomTextResourcesManager.class_path,
+    }
