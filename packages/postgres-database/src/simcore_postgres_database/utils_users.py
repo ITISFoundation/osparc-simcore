f3bd7158e51d21972d826f3efe9fe159b7edd188
Andrei Neagu <neagu@itis.swiss>
2023-11-20 08:18:50 +0100

    refactor

--- packages/postgres-database/src/simcore_postgres_database/utils_users.py
+++ packages/postgres-database/src/simcore_postgres_database/utils_users.py
@@ -2,12 +2,11 @@
     i.e. models.users main table and all its relations
 """
 
-from typing import Any
 
 import sqlalchemy as sa
 from aiopg.sa.connection import SAConnection
 
-from .models.users import UserRole, users
+from .models.users import users
 
 
 class BaseUserRepoError(Exception):
@@ -20,15 +19,15 @@ class UserNotFoundInRepoError(BaseUserRepoError):
 
 class UsersRepo:
     @staticmethod
-    async def get_role(conn: SAConnection, user_id: int) -> Any:
+    async def get_role(conn: SAConnection, user_id: int) -> str:
         if value := await conn.scalar(
             sa.select(users.c.role).where(users.c.id == user_id)
         ):
-            return UserRole(value)
+            return value
         raise UserNotFoundInRepoError
 
     @staticmethod
-    async def get_email(conn: SAConnection, user_id: int) -> Any:
+    async def get_email(conn: SAConnection, user_id: int) -> str:
         if value := await conn.scalar(
             sa.select(users.c.email).where(users.c.id == user_id)
         ):
