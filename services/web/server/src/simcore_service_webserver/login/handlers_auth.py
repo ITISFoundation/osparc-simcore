4cbfb590650ee4c6c20099c9e7311c8bd2932f50
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2023-11-21 16:45:43 +0100

    typo

--- services/web/server/src/simcore_service_webserver/login/handlers_auth.py
+++ services/web/server/src/simcore_service_webserver/login/handlers_auth.py
@@ -98,7 +98,6 @@ async def login(request: web.Request):
         product=product,
     )
 
-    # By default expects all suers to have 2fa enabled
     skip_2fa = not user.get("two_factor_enabled", True)
     if skip_2fa or not settings.LOGIN_2FA_REQUIRED:
         return await login_granted_response(request, user=user)
