c5220c17766abc574a7ed7db1700b3cf5ee554d9
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2023-11-21 14:35:03 +0100

    WIP: sio

--- services/payments/tests/unit/test_services_socketio.py
+++ services/payments/tests/unit/test_services_socketio.py
@@ -1,6 +1,12 @@
+from collections.abc import Callable
+
+import pytest
+import socketio
+from aiohttp import web
 from faker import Faker
 from fastapi import FastAPI
 from simcore_service_payments.services.socketio import emit_to_frontend
+from socketio import AsyncAioPikaManager, AsyncServer
 
 
 async def test_socketio_setup():
@@ -8,13 +14,38 @@ async def test_socketio_setup():
     ...
 
 
-async def test_emit_socketio_event_to_front_end(app: FastAPI, faker: Faker):
-    # create a client
-    sid = faker.uuid4()
+@pytest.fixture
+async def socketio_aiohttp_server(aiohttp_server: Callable):
+    aiohttp_app = web.Application()
+    server = await aiohttp_server(aiohttp_app)
 
+    # Emulates simcore_service_webserver/socketio/server.py
+    server_manager = AsyncAioPikaManager(url=get_rabbitmq_settings(app).dsn)
+    sio_server = AsyncServer(
+        async_mode="aiohttp",
+        engineio_logger=True,
+        client_manager=server_manager,
+    )
+    sio_server.attach(aiohttp_app)
+
+
+async def test_emit_socketio_event_to_front_end(app: FastAPI, faker: Faker, sio_server):
     # create a server
 
-    # emit from external
-    await emit_to_frontend(app, event_name="event", data={"foo": "bar"}, to=sid)
+    # create a client
+    async with socketio.AsyncSimpleClient(logger=True, engineio_logger=True) as sio:
 
-    # client receives it
+        # connect to a server
+        await sio.connect(server_url, transports=["websocket"])
+        session_client_id = sio.sid
+
+        # emit from external
+        await emit_to_frontend(
+            app, event_name="event", data={"foo": "bar"}, to=session_client_id
+        )
+
+        # client receives it
+        event: list = await sio.receive(timeout=5)
+        event_name, *event_kwargs = event
+
+        assert event_name == "event"
