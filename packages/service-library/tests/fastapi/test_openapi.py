79dbbef451bcbd28ddfe7522ec796789112d4cc9
Mads Bisgaard <bisgaard@itis.swiss>
2023-11-21 14:14:18 +0100

    attempt to make tests pass

--- packages/service-library/tests/fastapi/test_openapi.py
+++ packages/service-library/tests/fastapi/test_openapi.py
@@ -1,8 +1,14 @@
+# pylint: disable=protected-access
+# pylint: disable=redefined-outer-name
+# pylint: disable=too-many-arguments
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+
 import pytest
 import starlette.routing
 from fastapi.applications import FastAPI
 from fastapi.routing import APIRouter
-from openapi_spec_validator import openapi_v3_spec_validator, validate_spec
+from openapi_spec_validator import validate_spec
 from openapi_spec_validator.exceptions import OpenAPISpecValidatorError
 from servicelib.fastapi.openapi import (
     override_fastapi_openapi_method,
@@ -31,7 +37,7 @@ def test_exclusive_min_openapi_issue(app: FastAPI):
     # NOTE: With the latest update of openapi_spec_validator, now passes validation 3.1 but
     # does not seem resolved. It was moved to https://github.com/tiangolo/fastapi/discussions/9140
     with pytest.raises(OpenAPISpecValidatorError):
-        validate_spec(app.openapi(), validator=openapi_v3_spec_validator)
+        validate_spec(app.openapi())
 
 
 def test_overriding_openapi_method(app: FastAPI):
@@ -48,7 +54,7 @@ def test_overriding_openapi_method(app: FastAPI):
     assert openapi
     assert isinstance(openapi, dict)
 
-    validate_spec(openapi, validator=openapi_v3_spec_validator)
+    validate_spec(openapi)
 
     # NOTE: https://github.com/tiangolo/fastapi/issues/240 now passes validation 3.1 but
     # does not seem resolved. It was moved to https://github.com/tiangolo/fastapi/discussions/9140
