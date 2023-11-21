b313f9fc3b107a206a87cc165ab2356520106aeb
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2023-11-21 17:18:31 +0100

    removed commented code

--- services/static-webserver/client/source/class/osparc/desktop/StudyEditor.js
+++ services/static-webserver/client/source/class/osparc/desktop/StudyEditor.js
@@ -635,7 +635,6 @@ qx.Class.define("osparc.desktop.StudyEditor", {
         .catch(error => {
           if ("status" in error && error.status === 409) {
             console.log("Flash message blocked") // Workaround for osparc-issues #1189
-            // osparc.FlashMessenger.getInstance().logAs(error.message, "ERROR");
           } else {
             console.error(error);
             osparc.FlashMessenger.getInstance().logAs(this.tr("Error saving the study"), "ERROR");
