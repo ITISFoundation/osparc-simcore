bd7d978f559c50f69d9a729aedcff969777fb71c
Mads Bisgaard <bisgaard@itis.swiss>
2023-11-21 13:51:22 +0100

    remove tests which are not yet fully implemented

--- packages/service-library/tests/fastapi/test_rabbitmq.py
+++ packages/service-library/tests/fastapi/test_rabbitmq.py
@@ -57,102 +57,104 @@ def rabbit_message(
     }[request.param]
 
 
-def test_rabbitmq_does_not_initialize_if_deactivated(
-    disabled_rabbitmq: None,
-    initialized_app: FastAPI,
-):
-    assert hasattr(initialized_app.state, "rabbitmq_client")
-    assert initialized_app.state.rabbitmq_client is None
-    with pytest.raises(InvalidConfig):
-        get_rabbitmq_client(initialized_app)
+if False:
+    # https://github.com/ITISFoundation/osparc-simcore/issues/5059
+    def test_rabbitmq_does_not_initialize_if_deactivated(
+        disabled_rabbitmq: None,
+        initialized_app: FastAPI,
+    ):
+        assert hasattr(initialized_app.state, "rabbitmq_client")
+        assert initialized_app.state.rabbitmq_client is None
+        with pytest.raises(InvalidConfig):
+            get_rabbitmq_client(initialized_app)
 
+    def test_rabbitmq_initializes(
+        enabled_rabbitmq: RabbitSettings,
+        initialized_app: FastAPI,
+    ):
+        assert hasattr(initialized_app.state, "rabbitmq_client")
+        assert initialized_app.state.rabbitmq_client is not None
+        assert (
+            get_rabbitmq_client(initialized_app)
+            == initialized_app.state.rabbitmq_client
+        )
 
-def test_rabbitmq_initializes(
-    enabled_rabbitmq: RabbitSettings,
-    initialized_app: FastAPI,
-):
-    assert hasattr(initialized_app.state, "rabbitmq_client")
-    assert initialized_app.state.rabbitmq_client is not None
-    assert get_rabbitmq_client(initialized_app) == initialized_app.state.rabbitmq_client
+    async def test_post_message(
+        enabled_rabbitmq: RabbitSettings,
+        initialized_app: FastAPI,
+        rabbit_message: RabbitMessageBase,
+        create_rabbitmq_client: Callable[[str], RabbitMQClient],
+        mocker: MockerFixture,
+    ):
+        mocked_message_handler = mocker.AsyncMock(return_value=True)
+        consumer_rmq = create_rabbitmq_client("pytest_consumer")
+        await consumer_rmq.subscribe(
+            rabbit_message.channel_name,
+            mocked_message_handler,
+            topics=[BIND_TO_ALL_TOPICS] if rabbit_message.routing_key() else None,
+        )
 
+        producer_rmq = get_rabbitmq_client(initialized_app)
+        assert producer_rmq is not None
+        await producer_rmq.publish(rabbit_message.channel_name, rabbit_message)
 
-async def test_post_message(
-    enabled_rabbitmq: RabbitSettings,
-    initialized_app: FastAPI,
-    rabbit_message: RabbitMessageBase,
-    create_rabbitmq_client: Callable[[str], RabbitMQClient],
-    mocker: MockerFixture,
-):
-    mocked_message_handler = mocker.AsyncMock(return_value=True)
-    consumer_rmq = create_rabbitmq_client("pytest_consumer")
-    await consumer_rmq.subscribe(
-        rabbit_message.channel_name,
-        mocked_message_handler,
-        topics=[BIND_TO_ALL_TOPICS] if rabbit_message.routing_key() else None,
-    )
+        async for attempt in AsyncRetrying(**_TENACITY_RETRY_PARAMS):
+            with attempt:
+                print(
+                    f"--> checking for message in rabbit exchange {rabbit_message.channel_name}, {attempt.retry_state.retry_object.statistics}"
+                )
+                mocked_message_handler.assert_called_once_with(
+                    rabbit_message.json().encode()
+                )
+                print("... message received")
 
-    producer_rmq = get_rabbitmq_client(initialized_app)
-    assert producer_rmq is not None
-    await producer_rmq.publish(rabbit_message.channel_name, rabbit_message)
+    async def test_post_message_with_disabled_rabbit_does_not_raise(
+        disabled_rabbitmq: None,
+        disabled_ec2: None,
+        mocked_redis_server: None,
+        initialized_app: FastAPI,
+        rabbit_message: RabbitMessageBase,
+    ):
+        await post_message(initialized_app, message=rabbit_message)
 
-    async for attempt in AsyncRetrying(**_TENACITY_RETRY_PARAMS):
-        with attempt:
+    async def _switch_off_rabbit_mq_instance(
+        async_docker_client: aiodocker.Docker,
+    ) -> None:
+        # remove the rabbit MQ instance
+        rabbit_services = [
+            s
+            for s in await async_docker_client.services.list()
+            if "rabbit" in s["Spec"]["Name"]
+        ]
+        await asyncio.gather(
+            *(async_docker_client.services.delete(s["ID"]) for s in rabbit_services)
+        )
+
+        @retry(**_TENACITY_RETRY_PARAMS)
+        async def _check_service_task_gone(service: Mapping[str, Any]) -> None:
             print(
-                f"--> checking for message in rabbit exchange {rabbit_message.channel_name}, {attempt.retry_state.retry_object.statistics}"
+                f"--> checking if service {service['ID']}:{service['Spec']['Name']} is really gone..."
             )
-            mocked_message_handler.assert_called_once_with(
-                rabbit_message.json().encode()
+            list_of_tasks = await async_docker_client.containers.list(
+                all=True,
+                filters={
+                    "label": [f"com.docker.swarm.service.id={service['ID']}"],
+                },
             )
-            print("... message received")
+            assert not list_of_tasks
+            print(f"<-- service {service['ID']}:{service['Spec']['Name']} is gone.")
 
+        await asyncio.gather(*(_check_service_task_gone(s) for s in rabbit_services))
 
-async def test_post_message_with_disabled_rabbit_does_not_raise(
-    disabled_rabbitmq: None,
-    disabled_ec2: None,
-    mocked_redis_server: None,
-    initialized_app: FastAPI,
-    rabbit_message: RabbitMessageBase,
-):
-    await post_message(initialized_app, message=rabbit_message)
+    async def test_post_message_when_rabbit_disconnected(
+        enabled_rabbitmq: RabbitSettings,
+        disabled_ec2: None,
+        mocked_redis_server: None,
+        initialized_app: FastAPI,
+        rabbit_log_message: LoggerRabbitMessage,
+        async_docker_client: aiodocker.Docker,
+    ):
+        await _switch_off_rabbit_mq_instance(async_docker_client)
 
-
-async def _switch_off_rabbit_mq_instance(async_docker_client: aiodocker.Docker) -> None:
-    # remove the rabbit MQ instance
-    rabbit_services = [
-        s
-        for s in await async_docker_client.services.list()
-        if "rabbit" in s["Spec"]["Name"]
-    ]
-    await asyncio.gather(
-        *(async_docker_client.services.delete(s["ID"]) for s in rabbit_services)
-    )
-
-    @retry(**_TENACITY_RETRY_PARAMS)
-    async def _check_service_task_gone(service: Mapping[str, Any]) -> None:
-        print(
-            f"--> checking if service {service['ID']}:{service['Spec']['Name']} is really gone..."
-        )
-        list_of_tasks = await async_docker_client.containers.list(
-            all=True,
-            filters={
-                "label": [f"com.docker.swarm.service.id={service['ID']}"],
-            },
-        )
-        assert not list_of_tasks
-        print(f"<-- service {service['ID']}:{service['Spec']['Name']} is gone.")
-
-    await asyncio.gather(*(_check_service_task_gone(s) for s in rabbit_services))
-
-
-async def test_post_message_when_rabbit_disconnected(
-    enabled_rabbitmq: RabbitSettings,
-    disabled_ec2: None,
-    mocked_redis_server: None,
-    initialized_app: FastAPI,
-    rabbit_log_message: LoggerRabbitMessage,
-    async_docker_client: aiodocker.Docker,
-):
-    await _switch_off_rabbit_mq_instance(async_docker_client)
-
-    # now posting should not raise out
-    await post_message(initialized_app, message=rabbit_log_message)
+        # now posting should not raise out
+        await post_message(initialized_app, message=rabbit_log_message)
