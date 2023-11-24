09778b84b77c32c3a9c1bf82a7bf8ee170b7268d
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-11-24 18:03:33 +0100

    still not quite right

--- packages/service-library/tests/test_progress_bar.py
+++ packages/service-library/tests/test_progress_bar.py
@@ -11,23 +11,38 @@ import pytest
 from pytest_mock import MockerFixture
 from servicelib.progress_bar import ProgressBarData
 
-quit
+
+@pytest.fixture
+def mocked_progress_bar_cb(mocker: MockerFixture) -> mock.Mock:
+    return mocker.Mock()
 
 
-async def test_progress_bar():
-    async with ProgressBarData(steps=2) as root:
+async def test_progress_bar(mocked_progress_bar_cb: mock.Mock):
+    async with ProgressBarData(
+        steps=2, progress_report_cb=mocked_progress_bar_cb
+    ) as root:
         assert root._continuous_progress_value == pytest.approx(0)  # noqa: SLF001
+        mocked_progress_bar_cb.assert_called_once_with(pytest.approx(0))
+        mocked_progress_bar_cb.reset_mock()
         assert root.steps == 2
         assert root.step_weights is None
         async with root.sub_progress(steps=50) as sub:
             for _ in range(50):
                 await sub.update()
         assert root._continuous_progress_value == pytest.approx(1)  # noqa: SLF001
+        mocked_progress_bar_cb.assert_called()
+        assert mocked_progress_bar_cb.call_count == 43
+        assert mocked_progress_bar_cb.call_args_list[43] == 2
+        mocked_progress_bar_cb.reset_mock()
         assert root.steps == 2
         async with root.sub_progress(steps=50) as sub:
             for _ in range(50):
                 await sub.update()
         assert root._continuous_progress_value == pytest.approx(2)  # noqa: SLF001
+        mocked_progress_bar_cb.assert_called()
+        assert mocked_progress_bar_cb.call_count == 49
+        assert mocked_progress_bar_cb.call_args_list[49] == 2
+        mocked_progress_bar_cb.reset_mock()
         assert root.steps == 2
 
 
@@ -94,17 +109,14 @@ async def test_too_many_updates_does_not_raise_but_show_warning_with_stack(
         assert "TIP:" in caplog.messages[0]
 
 
-@pytest.fixture
-def mocked_progress_bar_cb(mocker: MockerFixture) -> mock.Mock:
-    return mocker.AsyncMock()
-
-
 async def test_weighted_progress_bar(mocked_progress_bar_cb: mock.Mock):
     async with ProgressBarData(
         steps=3,
         step_weights=[1, 3, 1],
         progress_report_cb=mocked_progress_bar_cb,
     ) as root:
+        mocked_progress_bar_cb.assert_called_once_with(pytest.approx(0))
+        mocked_progress_bar_cb.reset_mock()
         assert root.step_weights == [1 / 5, 3 / 5, 1 / 5, 0]
         await root.update()
         mocked_progress_bar_cb.assert_called_once_with(pytest.approx(1 / 5 * 3))
