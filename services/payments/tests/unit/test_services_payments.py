c12370bcc7a7c6ffb74762cb05f67feb81fecd5d
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2023-11-29 15:29:33 +0100

    tsts fix

--- services/payments/tests/unit/test_services_payments.py
+++ services/payments/tests/unit/test_services_payments.py
@@ -14,6 +14,8 @@ from models_library.users import UserID
 from models_library.wallets import WalletID
 from pydantic import EmailStr
 from pytest_mock import MockerFixture
+from pytest_simcore.helpers.typing_env import EnvVarsDict
+from pytest_simcore.helpers.utils_envs import setenvs_from_dict
 from respx import MockRouter
 from simcore_service_payments.db.payments_methods_repo import PaymentsMethodsRepo
 from simcore_service_payments.db.payments_transactions_repo import (
@@ -26,8 +28,37 @@ from simcore_service_payments.services.resource_usage_tracker import (
     ResourceUsageTrackerApi,
 )
 
+pytest_simcore_core_services_selection = [
+    "postgres",
+]
+pytest_simcore_ops_services_selection = [
+    "adminer",
+]
 
-async def test_failure_in_pay_with_payment_method(
+
+@pytest.fixture
+def app_environment(
+    monkeypatch: pytest.MonkeyPatch,
+    app_environment: EnvVarsDict,
+    with_disabled_rabbitmq_and_rpc: None,
+    postgres_env_vars_dict: EnvVarsDict,
+    wait_for_postgres_ready_and_db_migrated: None,
+):
+    # set environs
+    monkeypatch.delenv("PAYMENTS_RABBITMQ", raising=False)
+    monkeypatch.delenv("PAYMENTS_POSTGRES", raising=False)
+
+    return setenvs_from_dict(
+        monkeypatch,
+        {
+            **app_environment,
+            **postgres_env_vars_dict,
+            "POSTGRES_CLIENT_NAME": "payments-service-pg-client",
+        },
+    )
+
+
+async def test_fails_to_pay_with_payment_method_without_funds(
     app: FastAPI,
     create_fake_payment_method_in_db: Callable[
         [PaymentMethodID, WalletID, UserID], Awaitable[PaymentsMethodsDB]
@@ -53,7 +84,7 @@ async def test_failure_in_pay_with_payment_method(
         user_id=user_id,
     )
 
-    rut = (ResourceUsageTrackerApi.get_from_app_state(app),)
+    rut = ResourceUsageTrackerApi.get_from_app_state(app)
     rut_create_credit_transaction = mocker.spy(rut, "create_credit_transaction")
 
     payment = await payments.pay_with_payment_method(
