474d364cde439aa2f319b9dbe58bfa7c6ace9b73
matusdrobuliak66 <drobuliak66@gmail.com>
2023-11-29 16:40:33 +0100

    improving tests

--- tests/e2e-playwright/tests/sim4life.py
+++ tests/e2e-playwright/tests/sim4life.py
@@ -6,9 +6,13 @@
 # pylint: disable=unnecessary-lambda
 
 import os
-
+import re
 import pytest
-from playwright.sync_api import BrowserContext, Page
+from playwright.sync_api import BrowserContext, Page, APIRequestContext
+from tenacity import Retrying
+from tenacity.retry import retry_if_exception_type
+from tenacity.stop import stop_after_attempt
+from tenacity.wait import wait_fixed
 
 PRODUCT_URL = os.environ["PRODUCT_URL"]
 USER_NAME = os.environ["USER_NAME"]
@@ -25,15 +29,25 @@ def on_web_socket(ws):
 
 
 @pytest.fixture
-def log_in_and_out(osparc_test_id_attribute: None, context: BrowserContext, page: Page):
+def api_request_context(context: BrowserContext):
+
+    yield context.request
+
+
+@pytest.fixture
+def log_in_and_out(osparc_test_id_attribute: None, api_request_context: APIRequestContext, page: Page):
     print("Before test: Logging in starts")
     page.goto(PRODUCT_URL)
 
-    # In case the accept cookies window shows up, we accept (ex. after new release)
-    page.wait_for_timeout(5000)
+    # In case the accept cookies or new release window shows up, we accept
+    page.wait_for_timeout(2000)
     acceptCookiesBtnLocator = page.get_by_test_id("acceptCookiesBtn")
     if acceptCookiesBtnLocator.is_visible():
         acceptCookiesBtnLocator.click()
+        page.wait_for_timeout(1000)
+        newReleaseCloseBtnLocator = page.get_by_test_id("newReleaseCloseBtn")
+        if newReleaseCloseBtnLocator.is_visible():
+            newReleaseCloseBtnLocator.click()
 
     _user_email_box = page.get_by_test_id("loginUserEmailFld")
     _user_email_box.click()
@@ -46,13 +60,18 @@ def log_in_and_out(osparc_test_id_attribute: None, context: BrowserContext, page
     yield
 
     print("After test cleaning: Logging out starts")
-    api_request_context = context.request
     api_request_context.post(f"{PRODUCT_URL}v0/auth/logout")
 
-
-def test_billable_sim4life(page: Page, log_in_and_out):
+@pytest.mark.testit
+def test_billable_sim4life(page: Page, log_in_and_out: None, api_request_context: APIRequestContext):
     # connect and listen to websocket
     page.on("websocket", on_web_socket)
+
+    # Welcome to Sim4Life
+    page.wait_for_timeout(2000)
+    welcomeToSim4LifeLocator = page.get_by_text("Welcome to Sim4Life")
+    if welcomeToSim4LifeLocator.is_visible():
+        page.get_by_text("").nth(1).click()  # There is missing osparc-test-id for this button
 
     # open services tab and filter for sim4life service
     page.get_by_test_id("servicesTabBtn").click()
@@ -61,24 +80,41 @@ def test_billable_sim4life(page: Page, log_in_and_out):
     _textbox.press("Enter")
     page.get_by_test_id(SERVICE_TEST_ID).click()
 
-    # Project detail view pop-ups shows
-    page.get_by_test_id("openResource").click()
-    # Open project with default resources
-    page.get_by_test_id("openWithResources").click()
-    page.wait_for_timeout(5000)
+    with page.expect_response(re.compile(r'/projects/')) as response_info:
+        # Project detail view pop-ups shows
+        page.get_by_test_id("openResource").click()
+        # Open project with default resources
+        page.get_by_test_id("openWithResources").click()
+        page.wait_for_timeout(1000)
+
+    # Get project uuid, will be used to delete this project in the end
+    uuid_pattern = re.compile(r'/projects/([0-9a-fA-F-]+)')
+    match = uuid_pattern.search(response_info.value.url)
+    extracted_uuid = match.group(1)
 
     # Wait until grid is shown and click on the similation button
     page.frame_locator(".qx-main-dark").get_by_role("img", name="Remote render").click(
         button="right", timeout=600000
     )
     page.frame_locator(".qx-main-dark").get_by_text("Simulation").click()
-    page.wait_for_timeout(5000)
+    page.wait_for_timeout(1000)
 
     # Going back to dashboard
     page.get_by_test_id("dashboardBtn").click()
     page.get_by_test_id("confirmDashboardBtn").click()
-    page.wait_for_timeout(5000)
+    page.wait_for_timeout(1000)
 
     # Going back to projecs/studies view (In Sim4life projects:=studies)
     page.get_by_test_id("studiesTabBtn").click()
-    page.wait_for_timeout(5000)
+    page.wait_for_timeout(1000)
+
+    # The project is closing, wait until it is closed and delete it (currently waits max=5 minutes)
+    for attempt in Retrying(
+        wait=wait_fixed(5),
+        stop=stop_after_attempt(60),  # 5*60= 300 seconds
+        retry=retry_if_exception_type(AssertionError),
+        reraise=True,
+    ):
+        with attempt:
+            resp = api_request_context.delete(f"{PRODUCT_URL}v0/projects/{extracted_uuid}")
+            assert resp.status == 204
