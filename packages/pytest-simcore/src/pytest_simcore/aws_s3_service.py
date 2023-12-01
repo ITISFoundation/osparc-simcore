84a479e8b05bf1f0ef7dc7de49b95e9638615bc9
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-12-01 08:51:37 +0100

    s3 client fixture

--- packages/pytest-simcore/src/pytest_simcore/aws_s3_service.py
+++ packages/pytest-simcore/src/pytest_simcore/aws_s3_service.py
@@ -4,27 +4,31 @@ import typing
 import aioboto3
 import pytest
 from aiobotocore.session import ClientCreatorContext
+from botocore.client import Config
+from settings_library.s3 import S3Settings
 from types_aiobotocore_s3 import S3Client
 
 
 @pytest.fixture
 async def s3_client(
-    mocked_ec2_server_settings: EC2Settings,
+    mocked_s3_server_settings: S3Settings,
 ) -> typing.AsyncIterator[S3Client]:
     session = aioboto3.Session()
     exit_stack = contextlib.AsyncExitStack()
     session_client = session.client(
-        "ec2",
-        endpoint_url=mocked_ec2_server_settings.EC2_ENDPOINT,
-        aws_access_key_id=mocked_ec2_server_settings.EC2_ACCESS_KEY_ID,
-        aws_secret_access_key=mocked_ec2_server_settings.EC2_SECRET_ACCESS_KEY,
-        region_name=mocked_ec2_server_settings.EC2_REGION_NAME,
+        "s3",
+        endpoint_url=mocked_s3_server_settings.S3_ENDPOINT,
+        aws_access_key_id=mocked_s3_server_settings.S3_ACCESS_KEY,
+        aws_secret_access_key=mocked_s3_server_settings.S3_SECRET_KEY,
+        aws_session_token=mocked_s3_server_settings.S3_ACCESS_TOKEN,
+        region_name=mocked_s3_server_settings.S3_REGION,
+        config=Config(signature_version="s3v4"),
     )
     assert isinstance(session_client, ClientCreatorContext)
-    ec2_client = typing.cast(
+    s3_client = typing.cast(
         S3Client, await exit_stack.enter_async_context(session_client)
     )
 
-    yield ec2_client
+    yield s3_client
 
     await exit_stack.aclose()
