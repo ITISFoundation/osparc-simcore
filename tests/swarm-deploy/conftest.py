33cd6548d22da24042324b3c312880eb6933a612
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-12-02 23:42:30 +0100

    remove monkeypatch_extra

--- tests/swarm-deploy/conftest.py
+++ tests/swarm-deploy/conftest.py
@@ -20,14 +20,13 @@ pytest_plugins = [
     "pytest_simcore.docker_compose",
     "pytest_simcore.docker_registry",
     "pytest_simcore.docker_swarm",
-    "pytest_simcore.monkeypatch_extra",
     "pytest_simcore.postgres_service",
+    "pytest_simcore.pytest_global_environs",
     "pytest_simcore.rabbit_service",
     "pytest_simcore.repository_paths",
     "pytest_simcore.simcore_webserver_service",
     "pytest_simcore.tmp_path_extra",
     "pytest_simcore.traefik_service",
-    "pytest_simcore.pytest_global_environs",
 ]
 
 log = logging.getLogger(__name__)
