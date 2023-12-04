feb546fbcf2fc0004e2c5879e94b2cddce5ee6db
matusdrobuliak66 <drobuliak66@gmail.com>
2023-12-04 11:59:36 +0100

    fix

--- services/web/server/src/simcore_service_webserver/notifications/plugin.py
+++ services/web/server/src/simcore_service_webserver/notifications/plugin.py
@@ -4,8 +4,6 @@
 """
 import logging
 
-import _rabbitmq_exclusive_queue_consumers
-import _rabbitmq_nonexclusive_queue_consumers
 from aiohttp import web
 from servicelib.aiohttp.application_setup import ModuleCategory, app_module_setup
 
@@ -13,6 +11,10 @@ from ..diagnostics.plugin import setup_diagnostics
 from ..rabbitmq import setup_rabbitmq
 from ..socketio.plugin import setup_socketio
 from ..wallets.plugin import setup_wallets
+from . import (
+    _rabbitmq_exclusive_queue_consumers,
+    _rabbitmq_nonexclusive_queue_consumers,
+)
 
 _logger = logging.getLogger(__name__)
 
