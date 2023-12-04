504398842c518388ef1bc53d50a852ed3d4fd7c6
Mads Bisgaard <bisgaard@itis.swiss>
2023-12-04 15:11:52 +0100

    cast

--- services/api-server/src/simcore_service_api_server/api/dependencies/rabbitmq.py
+++ services/api-server/src/simcore_service_api_server/api/dependencies/rabbitmq.py
@@ -22,4 +22,4 @@ def get_max_log_check_seconds(
     app: Annotated[FastAPI, Depends(get_app)]
 ) -> NonNegativeInt:
     assert app.state.settings  # nosec
-    return app.state.settings.API_SERVER_MAX_LOG_CHECK_SECONDS
+    return cast(NonNegativeInt, app.state.settings.API_SERVER_MAX_LOG_CHECK_SECONDS)
