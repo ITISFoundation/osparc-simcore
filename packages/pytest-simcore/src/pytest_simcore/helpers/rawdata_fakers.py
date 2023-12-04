e856919b245080ec380f2e0011704ec8cba415a0
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2023-12-04 10:19:57 +0100

    @GitHK review: import

--- packages/pytest-simcore/src/pytest_simcore/helpers/rawdata_fakers.py
+++ packages/pytest-simcore/src/pytest_simcore/helpers/rawdata_fakers.py
@@ -23,6 +23,7 @@ import faker
 from faker import Faker
 from simcore_postgres_database.models.api_keys import api_keys
 from simcore_postgres_database.models.comp_pipeline import StateType
+from simcore_postgres_database.models.groups import groups
 from simcore_postgres_database.models.payments_methods import InitPromptAckFlowState
 from simcore_postgres_database.models.payments_transactions import (
     PaymentTransactionState,
@@ -101,7 +102,6 @@ def random_project(**overrides) -> dict[str, Any]:
 
 
 def random_group(**overrides) -> dict[str, Any]:
-    from simcore_postgres_database.models.groups import groups
 
     data = {
         "name": FAKE.company(),
