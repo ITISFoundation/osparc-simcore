1af0bd0b2ffafe7c3eb7888780aa269f5b55cf50
Andrei Neagu <neagu@itis.swiss>
2023-12-05 15:02:56 +0100

    exposing  via RPC

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/api/rest/_dependencies.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/api/rest/_dependencies.py
@@ -1,14 +1,7 @@
-from fastapi import Request
 from servicelib.fastapi.dependencies import get_app, get_reverse_url_mapper
-
-from ...services.director_v2 import DirectorV2Client
 
 assert get_app  # nosec
 assert get_reverse_url_mapper  # nosec
-
-
-def get_director_v2_client(request: Request) -> DirectorV2Client:
-    return DirectorV2Client.get_from_app_state(get_app(request))
 
 
 __all__: tuple[str, ...] = (
---
+++ services/dynamic-scheduler/tests/unit/api_rpc/test_api_rpc__services.py
@@ -0,0 +1,77 @@
+# pylint:disable=redefined-outer-name
+# pylint:disable=unused-argument
+
+from collections.abc import Awaitable, Callable, Iterator
+
+import pytest
+import respx
+from faker import Faker
+from fastapi import FastAPI, status
+from models_library.api_schemas_directorv2.dynamic_services import DynamicServiceGet
+from models_library.api_schemas_dynamic_scheduler import DYNAMIC_SCHEDULER_RPC_NAMESPACE
+from models_library.projects_nodes_io import NodeID
+from models_library.rabbitmq_basic_types import RPCMethodName
+from pytest_simcore.helpers.typing_env import EnvVarsDict
+from servicelib.rabbitmq import RabbitMQRPCClient
+from settings_library.rabbit import RabbitSettings
+
+pytest_simcore_core_services_selection = [
+    "rabbit",
+]
+
+
+@pytest.fixture
+def node_id(faker: Faker) -> NodeID:
+    return faker.uuid4(cast_to=None)
+
+
+@pytest.fixture
+def service_state() -> DynamicServiceGet:
+    return DynamicServiceGet.parse_obj(
+        DynamicServiceGet.Config.schema_extra["examples"][1]
+    )
+
+
+@pytest.fixture
+def mock_director_v2(
+    node_id: NodeID, service_state: DynamicServiceGet
+) -> Iterator[None]:
+    with respx.mock(
+        base_url="http://director-v2:8000/v1",
+        assert_all_called=False,
+        assert_all_mocked=True,  # IMPORTANT: KEEP always True!
+    ) as mock:
+        mock.get(f"/dynamic-services/{node_id}").respond(
+            status.HTTP_200_OK, text=service_state.json()
+        )
+        yield None
+
+
+@pytest.fixture
+def app_environment(
+    app_environment: EnvVarsDict, rabbit_service: RabbitSettings
+) -> EnvVarsDict:
+    return app_environment
+
+
+@pytest.fixture
+async def rpc_client(
+    app_environment: EnvVarsDict,
+    mock_director_v2: None,
+    app: FastAPI,
+    rabbitmq_rpc_client: Callable[[str], Awaitable[RabbitMQRPCClient]],
+) -> RabbitMQRPCClient:
+    return await rabbitmq_rpc_client("client")
+
+
+async def test_get_state(
+    rpc_client: RabbitMQRPCClient,
+    node_id: NodeID,
+    service_state: DynamicServiceGet,
+):
+    result = await rpc_client.request(
+        DYNAMIC_SCHEDULER_RPC_NAMESPACE,
+        RPCMethodName("get_service_status"),
+        node_id=node_id,
+    )
+    assert result == service_state
