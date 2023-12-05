714ebeefee1c54596e2af9b5d571d481cbfeefe7
Andrei Neagu <neagu@itis.swiss>
2023-12-05 15:13:05 +0100

    refactor

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/api/rpc/routes.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/api/rpc/routes.py
@@ -5,7 +5,9 @@ from servicelib.rabbitmq import RPCRouter
 from ...services.rabbitmq import get_rabbitmq_rpc_server
 from . import _services
 
-ROUTERS: list[RPCRouter] = [_services.router]
+ROUTERS: list[RPCRouter] = [
+    _services.router,
+]
 
 
 def setup_rpc_api_routes(app: FastAPI) -> None:
