1073b4cd30747b1db5df13c5a4bce10ddecfee26
Andrei Neagu <neagu@itis.swiss>
2023-12-06 09:36:37 +0100

    refactor

--- services/web/server/src/simcore_service_webserver/dynamic_scheduler/_rpc.py
+++ services/web/server/src/simcore_service_webserver/dynamic_scheduler/_rpc.py
@@ -3,6 +3,7 @@ import logging
 from aiohttp import web
 from models_library.api_schemas_directorv2.dynamic_services import DynamicServiceGet
 from models_library.api_schemas_dynamic_scheduler import DYNAMIC_SCHEDULER_RPC_NAMESPACE
+from models_library.api_schemas_webserver.projects_nodes import NodeGet, NodeGetIdle
 from models_library.projects_nodes_io import NodeID
 from models_library.rabbitmq_basic_types import RPCMethodName
 from pydantic import parse_obj_as
@@ -16,7 +17,7 @@ _logger = logging.getLogger(__name__)
 @log_decorator(_logger, level=logging.DEBUG)
 async def get_service_status(
     app: web.Application, *, node_id: NodeID
-) -> DynamicServiceGet:
+) -> NodeGetIdle | DynamicServiceGet | NodeGet:
     rpc_client = get_rabbitmq_rpc_client(app)
 
     result = await rpc_client.request(
@@ -24,5 +25,5 @@ async def get_service_status(
         parse_obj_as(RPCMethodName, "get_service_status"),
         node_id=node_id,
     )
-    assert isinstance(result, DynamicServiceGet)  # nosec
+    assert isinstance(result, NodeGetIdle | DynamicServiceGet | NodeGet)  # nosec
     return result
