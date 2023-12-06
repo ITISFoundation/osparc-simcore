a0e1fbd9e49651531d4376bc52106c7d688f6830
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-12-06 08:35:20 +0100

    use new environment style

--- services/clusters-keeper/src/simcore_service_clusters_keeper/modules/clusters.py
+++ services/clusters-keeper/src/simcore_service_clusters_keeper/modules/clusters.py
@@ -2,7 +2,12 @@ import datetime
 import logging
 from typing import cast
 
-from aws_library.ec2.models import EC2InstanceConfig, EC2InstanceData, EC2InstanceType
+from aws_library.ec2.models import (
+    EC2InstanceBootSpecific,
+    EC2InstanceConfig,
+    EC2InstanceData,
+    EC2InstanceType,
+)
 from fastapi import FastAPI
 from models_library.users import UserID
 from models_library.wallets import WalletID
@@ -10,7 +15,7 @@ from servicelib.logging_utils import log_context
 from types_aiobotocore_ec2.literals import InstanceTypeType
 
 from ..core.errors import Ec2InstanceNotFoundError
-from ..core.settings import get_application_settings
+from ..core.settings import ApplicationSettings, get_application_settings
 from ..utils.clusters import create_startup_script
 from ..utils.ec2 import (
     HEARTBEAT_TAG_KEY,
@@ -24,25 +29,38 @@ from .ec2 import get_ec2_client
 _logger = logging.getLogger(__name__)
 
 
+def _get_primary_ec2_params(
+    app_settings: ApplicationSettings,
+) -> tuple[InstanceTypeType, EC2InstanceBootSpecific]:
+    assert app_settings.CLUSTERS_KEEPER_PRIMARY_EC2_INSTANCES  # nosec
+    assert (
+        len(
+            app_settings.CLUSTERS_KEEPER_PRIMARY_EC2_INSTANCES.PRIMARY_EC2_INSTANCES_ALLOWED_TYPES
+        )
+        == 1
+    )  # nosec
+    ec2_type_name, ec2_boot_specs = next(
+        iter(
+            app_settings.CLUSTERS_KEEPER_PRIMARY_EC2_INSTANCES.PRIMARY_EC2_INSTANCES_ALLOWED_TYPES.items()
+        )
+    )
+    return cast(InstanceTypeType, ec2_type_name), ec2_boot_specs
+
+
 async def create_cluster(
     app: FastAPI, *, user_id: UserID, wallet_id: WalletID | None
 ) -> list[EC2InstanceData]:
     ec2_client = get_ec2_client(app)
     app_settings = get_application_settings(app)
     assert app_settings.CLUSTERS_KEEPER_PRIMARY_EC2_INSTANCES  # nosec
+
+    ec2_instance_type_name, ec2_instance_boot_specs = _get_primary_ec2_params(
+        app_settings
+    )
     ec2_instance_types: list[
         EC2InstanceType
     ] = await ec2_client.get_ec2_instance_capabilities(
-        instance_type_names={
-            cast(
-                InstanceTypeType,
-                next(
-                    iter(
-                        app_settings.CLUSTERS_KEEPER_PRIMARY_EC2_INSTANCES.PRIMARY_EC2_INSTANCES_ALLOWED_TYPES
-                    )
-                ),
-            )
-        }
+        instance_type_names=[ec2_instance_type_name]
     )
     assert len(ec2_instance_types) == 1  # nosec
     instance_config = EC2InstanceConfig(
@@ -54,7 +72,7 @@ async def create_cluster(
                 app_settings, user_id=user_id, wallet_id=wallet_id, is_manager=False
             ),
         ),
-        ami_id=app_settings.CLUSTERS_KEEPER_PRIMARY_EC2_INSTANCES.PRIMARY_EC2_INSTANCES_AMI_ID,
+        ami_id=ec2_instance_boot_specs.ami_id,
         key_name=app_settings.CLUSTERS_KEEPER_PRIMARY_EC2_INSTANCES.PRIMARY_EC2_INSTANCES_KEY_NAME,
         security_group_ids=app_settings.CLUSTERS_KEEPER_PRIMARY_EC2_INSTANCES.PRIMARY_EC2_INSTANCES_SECURITY_GROUP_IDS,
         subnet_id=app_settings.CLUSTERS_KEEPER_PRIMARY_EC2_INSTANCES.PRIMARY_EC2_INSTANCES_SUBNET_ID,
