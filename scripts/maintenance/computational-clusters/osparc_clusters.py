bc8c09d3ecea286ccb5c5631317f7710f817a3e6
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-12-12 10:27:12 +0100

    improve layout

--- scripts/maintenance/computational-clusters/osparc_clusters.py
+++ scripts/maintenance/computational-clusters/osparc_clusters.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python3
 
+import datetime
+from dataclasses import dataclass
+from enum import Enum
 from pathlib import Path
 
 import arrow
@@ -7,9 +10,9 @@ import boto3
 import parse
 import typer
 from dotenv import dotenv_values
-from mypy_boto3_ec2.service_resource import ServiceResourceInstancesCollection
+from mypy_boto3_ec2.service_resource import Instance, ServiceResourceInstancesCollection
 from rich import print
-from rich.table import Table
+from rich.table import Column, Table
 
 app = typer.Typer()
 
@@ -24,12 +27,17 @@ def _get_instance_name(instance) -> str:
     return "unknown"
 
 
-def _get_last_heartbeat_since_now(instance) -> str:
+def _get_last_heartbeat(instance) -> datetime.datetime | None:
     for tag in instance.tags:
         if tag["Key"] == "last_heartbeat":
-            time_diff = arrow.get(tag["Value"]) - arrow.utcnow()
-            return f"{int(time_diff.total_seconds() / 60)}:{time_diff.total_seconds() % 60}"
-    return "999999"
+            return arrow.get(tag["Value"]).datetime
+    return None
+
+
+def _timedelta_formatting(time_diff: datetime.timedelta) -> str:
+    formatted_time_diff = f"{time_diff.days} days, " if time_diff.days else ""
+    formatted_time_diff += f"{time_diff.seconds // 3600:02}:{(time_diff.seconds // 60) % 60:02}:{time_diff.seconds % 60:02}"
+    return formatted_time_diff
 
 
 computational_parser = parse.compile(
@@ -37,69 +45,182 @@ computational_parser = parse.compile(
 )
 
 
-def _custom_sort_key(instance):
-    # Custom key function for sorting instances
+@dataclass(frozen=True, slots=True, kw_only=True)
+class AutoscaledInstance:
+    name: str
+    ec2_instance: Instance
+
+
+class InstanceRole(str, Enum):
+    manager = "manager"
+    worker = "worker"
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class ComputationalInstance(AutoscaledInstance):
+    role: InstanceRole
+    user_id: int
+    wallet_id: int
+    last_heartbeat: datetime.datetime | None
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class DynamicInstance(AutoscaledInstance):
+    ...
+
+
+def _parse_computational(instance: Instance) -> ComputationalInstance | None:
     name = _get_instance_name(instance)
-    result = computational_parser.parse(name)
-
-    if result:
-        return (
-            0 if result["role"] == "manager" else 1,
-            result["user_id"],
-            result["wallet_id"],
+    if result := computational_parser.search(name):
+        assert isinstance(result, parse.Result)
+        last_heartbeat = _get_last_heartbeat(instance)
+        return ComputationalInstance(
+            role=InstanceRole(result["role"]),
+            user_id=result["user_id"],
+            wallet_id=result["wallet_id"],
+            name=name,
+            last_heartbeat=last_heartbeat,
+            ec2_instance=instance,
         )
-    return name
+
+    return None
 
 
-def _print_instances_details(
-    instances: ServiceResourceInstancesCollection,
-    *,
-    with_name: str,
-    computational_row: bool,
-    **rich_table_kwargs,
-):
-    comp_specific_rows = []
-    if computational_row:
-        comp_specific_rows = ["DaskSchedulerUI", "last heartbeat"]
+dynamic_parser = parse.compile("osparc-dynamic-autoscaled-worker-{key_name}")
+
+
+def _parse_dynamic(instance: Instance) -> DynamicInstance | None:
+    name = _get_instance_name(instance)
+    if result := dynamic_parser.search(name):
+        assert isinstance(result, parse.Result)
+        return DynamicInstance(name=name, ec2_instance=instance)
+    return None
+
+
+def _print_dynamic_instances(instances: list[DynamicInstance]) -> None:
+    time_now = arrow.utcnow()
     table = Table(
         "Instance",
-        "publicIP",
-        "privateIP",
+        Column("Type", justify="right"),
+        Column("publicIP", justify="right"),
+        Column("privateIP", justify="right"),
         "Name",
-        "Created",
+        Column("Created since", justify="right"),
         "State",
-        *comp_specific_rows,
-        **rich_table_kwargs,
     )
-    filtered_instances = [
-        instance for instance in instances if with_name in _get_instance_name(instance)
-    ]
-
-    for instance in sorted(
-        filtered_instances,
-        key=_custom_sort_key,
-    ):
-        specific = []
-        if computational_row:
-            specific = [
-                f"http://{instance.public_ip_address}:8787",
-                f"{_get_last_heartbeat_since_now(instance)}",
-            ]
+    for instance in instances:
+        instance_state = (
+            f"[green]{instance.ec2_instance.state['Name']}[/green]"
+            if instance.ec2_instance.state["Name"] == "running"
+            else f"[yellow]{instance.ec2_instance.state['Name']}[/yellow]"
+        )
         table.add_row(
-            instance.id,
-            instance.public_ip_address,
-            instance.private_ip_address,
-            _get_instance_name(instance),
-            f"{instance.launch_time}",
-            typer.style(
-                instance.state["Name"],
-                fg=typer.colors.GREEN
-                if instance.state["Name"] == "running"
-                else typer.colors.YELLOW,
+            instance.ec2_instance.id,
+            instance.ec2_instance.instance_type,
+            instance.ec2_instance.public_ip_address,
+            instance.ec2_instance.private_ip_address,
+            instance.name,
+            _timedelta_formatting(
+                time_now - arrow.get(instance.ec2_instance.launch_time)
             ),
-            *specific,
+            instance_state,
         )
     print(table)
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class ComputationalCluster:
+    primary: ComputationalInstance
+    workers: list[ComputationalInstance]
+
+
+def _print_computational_clusters(clusters: list[ComputationalCluster]) -> None:
+    time_now = arrow.utcnow()
+    table = Table(title="computational clusters")
+    table = Table(
+        "Instance",
+        Column("Type", justify="right"),
+        Column("publicIP", justify="right"),
+        Column("privateIP", justify="right"),
+        "Name",
+        Column("Created since", justify="right"),
+        "State",
+        "DaskSchedulerUI",
+        "last heartbeat since",
+    )
+
+    for cluster in clusters:
+        # first print primary machine info
+        instance_state = (
+            f"[green]{cluster.primary.ec2_instance.state['Name']}[/green]"
+            if cluster.primary.ec2_instance.state["Name"] == "running"
+            else f"[yellow]{cluster.primary.ec2_instance.state['Name']}[/yellow]"
+        )
+        assert cluster.primary.last_heartbeat
+        table.add_row(
+            cluster.primary.ec2_instance.id,
+            cluster.primary.ec2_instance.instance_type,
+            cluster.primary.ec2_instance.public_ip_address,
+            cluster.primary.ec2_instance.private_ip_address,
+            cluster.primary.name,
+            _timedelta_formatting(
+                time_now - arrow.get(cluster.primary.ec2_instance.launch_time)
+            ),
+            instance_state,
+            f"http://{cluster.primary.ec2_instance.public_ip_address}:8787",
+            _timedelta_formatting(time_now - arrow.get(cluster.primary.last_heartbeat)),
+        )
+        # now add the workers
+        for worker in cluster.workers:
+            instance_state = (
+                f"[green]{worker.ec2_instance.state['Name']}[/green]"
+                if worker.ec2_instance.state["Name"] == "running"
+                else f"[yellow]{worker.ec2_instance.state['Name']}[/yellow]"
+            )
+
+            table.add_row(
+                worker.ec2_instance.id,
+                worker.ec2_instance.instance_type,
+                worker.ec2_instance.public_ip_address,
+                worker.ec2_instance.private_ip_address,
+                worker.name,
+                _timedelta_formatting(
+                    time_now - arrow.get(worker.ec2_instance.launch_time)
+                ),
+                instance_state,
+                "",
+                "",
+            )
+        table.add_row(end_section=True)
+    print(table)
+
+
+def _detect_instances(
+    instances: ServiceResourceInstancesCollection,
+) -> tuple[list[DynamicInstance], list[ComputationalCluster]]:
+    dynamic_instances = []
+    computational_instances = []
+    for instance in instances:
+        if comp_instance := _parse_computational(instance):
+            computational_instances.append(comp_instance)
+        elif dyn_instance := _parse_dynamic(instance):
+            dynamic_instances.append(dyn_instance)
+    computational_clusters = [
+        ComputationalCluster(primary=instance, workers=[])
+        for instance in computational_instances
+        if instance.role is InstanceRole.manager
+    ]
+    for instance in computational_instances:
+        if instance.role is InstanceRole.worker:
+            # assign the worker to correct cluster
+            for cluster in computational_clusters:
+                if (
+                    cluster.primary.user_id == instance.user_id
+                    and cluster.primary.wallet_id == instance.wallet_id
+                ):
+                    cluster.workers.append(instance)
+
+    return dynamic_instances, computational_clusters
 
 
 @app.command()
@@ -123,24 +244,10 @@ def summary(env_file: Path) -> None:
         ]
     )
 
-    # TODO: once we have the EC2 custom tags, we shall use these instead
+    dynamic_autoscaled_instances, computational_clusters = _detect_instances(instances)
 
-    # print the dynamic ones
-    assert env_key_values["EC2_INSTANCES_NAME_PREFIX"]
-    _print_instances_details(
-        instances,
-        with_name=env_key_values["EC2_INSTANCES_NAME_PREFIX"],
-        title="dynamic autoscaled nodes",
-        computational_row=False,
-    )
-
-    # print the computational ones
-    _print_instances_details(
-        instances,
-        with_name="osparc-computational",
-        title="computational clusters",
-        computational_row=True,
-    )
+    _print_dynamic_instances(dynamic_autoscaled_instances)
+    _print_computational_clusters(computational_clusters)
 
 
 if __name__ == "__main__":
