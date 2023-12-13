50a4ad4c86b87e3e7a8699238d73cc367c815079
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-12-13 17:34:18 +0100

    refactor

--- tests/e2e-playwright/tests/conftest.py
+++ tests/e2e-playwright/tests/conftest.py
@@ -4,9 +4,12 @@
 # pylint: disable=too-many-arguments
 # pylint: disable=too-many-statements
 
+import os
+from collections.abc import Iterator
+
 import pytest
-from playwright.sync_api import BrowserContext, Page, APIRequestContext
-from typing import Iterator
+from playwright.sync_api import APIRequestContext, BrowserContext, Page
+from pydantic import URL, parse_obj_as
 
 
 @pytest.fixture
@@ -17,14 +20,50 @@ def osparc_test_id_attribute(playwright):
 
 @pytest.fixture
 def api_request_context(context: BrowserContext):
-
-    yield context.request
+    return context.request
 
 
 @pytest.fixture
-def log_in_and_out(osparc_test_id_attribute: None, api_request_context: APIRequestContext, page: Page, product_and_user: tuple) -> Iterator[None]:
-    print("Before test: Logging in starts")
-    page.goto(product_and_user[0])
+def product_url() -> URL:
+    return parse_obj_as(URL, os.environ["PRODUCT_URL"])
+
+
+@pytest.fixture
+def user_name() -> str:
+    return os.environ["USER_NAME"]
+
+
+@pytest.fixture
+def user_password() -> str:
+    return os.environ["USER_PASSWORD"]
+
+
+@pytest.fixture
+def product_billable() -> bool:
+    return parse_obj_as(bool, os.environ["PRODUCT_BILLABLE"])
+
+
+@pytest.fixture
+def service_test_id() -> str:
+    return os.environ["SERVICE_TEST_ID"]
+
+
+@pytest.fixture
+def service_key() -> str:
+    return os.environ["SERVICE_KEY"]
+
+
+@pytest.fixture
+def log_in_and_out(
+    osparc_test_id_attribute: None,
+    api_request_context: APIRequestContext,
+    page: Page,
+    product_url: URL,
+    user_name: str,
+    user_password: str,
+) -> Iterator[None]:
+    print(f"------> Logging in {product_url=} using {user_name=}/{user_password=}")
+    page.goto(product_url)
 
     # In case the accept cookies or new release window shows up, we accept
     page.wait_for_timeout(2000)
@@ -38,23 +77,30 @@ def log_in_and_out(osparc_test_id_attribute: None, api_request_context: APIReque
 
     _user_email_box = page.get_by_test_id("loginUserEmailFld")
     _user_email_box.click()
-    _user_email_box.fill(product_and_user[1])
+    _user_email_box.fill(user_name)
     _user_password_box = page.get_by_test_id("loginPasswordFld")
     _user_password_box.click()
-    _user_password_box.fill(product_and_user[2])
+    _user_password_box.fill(user_password)
     page.get_by_test_id("loginSubmitBtn").click()
 
     # Welcome to Sim4Life
     page.wait_for_timeout(5000)
     welcomeToSim4LifeLocator = page.get_by_text("Welcome to Sim4Life")
     if welcomeToSim4LifeLocator.is_visible():
-        page.get_by_text("").nth(1).click()  # There is missing osparc-test-id for this button
+        page.get_by_text("").nth(
+            1
+        ).click()  # There is missing osparc-test-id for this button
     # Quick start window
     quickStartWindowCloseBtnLocator = page.get_by_test_id("quickStartWindowCloseBtn")
     if quickStartWindowCloseBtnLocator.is_visible():
         quickStartWindowCloseBtnLocator.click()
+    print(
+        f"------> Successfully logged in {product_url=} using {user_name=}/{user_password=}"
+    )
 
     yield
 
-    print("After test cleaning: Logging out starts")
-    api_request_context.post(f"{product_and_user[0]}v0/auth/logout")
+    # TODO: this is not the UI way of logging out
+    print(f"<------ Logging out of {product_url=} using {user_name=}/{user_password=}")
+    api_request_context.post(f"{product_url}v0/auth/logout")
+    print(f"<------ Logged out of {product_url=} using {user_name=}/{user_password=}")
