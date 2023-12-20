0518c305f7c368c6592f542fa8d7f700de8dc177
matusdrobuliak66 <60785969+matusdrobuliak66@users.noreply.github.com>
2023-12-20 14:14:14 +0100

    🐛 Bugfix/adding rabbitmq checks (#5190)

--- services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/api/_health.py
+++ services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/api/_health.py
@@ -1,8 +1,13 @@
 import datetime
 import logging
+from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.responses import PlainTextResponse
+from models_library.errors import RABBITMQ_CLIENT_UNHEALTHY_MSG
+from servicelib.rabbitmq import RabbitMQClient
+
+from ..modules.rabbitmq import get_rabbitmq_client_from_request
 
 logger = logging.getLogger(__name__)
 
@@ -13,6 +18,17 @@ logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
+class HealthCheckError(RuntimeError):
+    """Failed a health check"""
+
+
 @router.get("/", response_class=PlainTextResponse)
-async def healthcheck() -> str:
+async def healthcheck(
+    rabbitmq_client: Annotated[
+        RabbitMQClient, Depends(get_rabbitmq_client_from_request)
+    ],
+) -> str:
+    if not rabbitmq_client.healthy:
+        raise HealthCheckError(RABBITMQ_CLIENT_UNHEALTHY_MSG)
+
     return f"{__name__}@{datetime.datetime.now(datetime.timezone.utc).isoformat()}"
--- services/resource-usage-tracker/tests/unit/api/test_api_meta.py
+++ services/resource-usage-tracker/tests/unit/api/test_api_meta.py
@@ -5,9 +5,13 @@
 
 from unittest import mock
 
+import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
+from pytest_mock import MockerFixture
+from servicelib.rabbitmq import RabbitMQClient
 from simcore_service_resource_usage_tracker._meta import API_VTAG
+from simcore_service_resource_usage_tracker.api._health import HealthCheckError
 from simcore_service_resource_usage_tracker.api._meta import _Meta
 
 
@@ -18,12 +22,40 @@ def test_healthcheck(
     mocked_redis_server: None,
     mocked_setup_rabbitmq: mock.MagicMock,
     client: TestClient,
+    mocker: MockerFixture,
 ):
+    rabbitmq_mock = mocker.Mock(spec=RabbitMQClient)
+    rabbitmq_mock.healthy = True
+    mocker.patch(
+        "simcore_service_resource_usage_tracker.modules.rabbitmq.get_rabbitmq_client",
+        return_value=rabbitmq_mock,
+    )
+
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
     assert response.text.startswith(
         "simcore_service_resource_usage_tracker.api._health@"
     )
+
+
+def test_healthcheck__unhealthy(
+    disabled_database: None,
+    disabled_prometheus: None,
+    disabled_rabbitmq: None,
+    mocked_redis_server: None,
+    mocked_setup_rabbitmq: mock.MagicMock,
+    client: TestClient,
+    mocker: MockerFixture,
+):
+    rabbitmq_mock = mocker.Mock(spec=RabbitMQClient)
+    rabbitmq_mock.healthy = False
+    mocker.patch(
+        "simcore_service_resource_usage_tracker.modules.rabbitmq.get_rabbitmq_client",
+        return_value=rabbitmq_mock,
+    )
+
+    with pytest.raises(HealthCheckError):
+        client.get("/")
 
 
 def test_meta(
