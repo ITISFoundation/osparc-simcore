e30fd6e454a670bce1d03ad65dcf32158ed23108
Mads Bisgaard <bisgaard@itis.swiss>
2023-12-20 12:41:29 +0100

    don't add endpoint to schema

--- services/api-server/src/simcore_service_api_server/core/_prometheus_instrumentation.py
+++ services/api-server/src/simcore_service_api_server/core/_prometheus_instrumentation.py
@@ -4,4 +4,4 @@ from prometheus_fastapi_instrumentator import Instrumentator
 
 def instrument_app(app: FastAPI):
 
-    Instrumentator().instrument(app).expose(app)
+    Instrumentator().instrument(app).expose(app, include_in_schema=False)
