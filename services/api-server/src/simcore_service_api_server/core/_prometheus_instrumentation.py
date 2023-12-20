c9f0d9f8cee5ae3c2c7cab1d2ebaa1d76cb81cc8
Mads Bisgaard <bisgaard@itis.swiss>
2023-12-20 13:15:13 +0100

    explicitely add endpoint to add credentials

--- services/api-server/src/simcore_service_api_server/core/_prometheus_instrumentation.py
+++ services/api-server/src/simcore_service_api_server/core/_prometheus_instrumentation.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from prometheus_client import generate_latest
 from prometheus_fastapi_instrumentator import Instrumentator
 
 
 def instrument_app(app: FastAPI):
 
-    Instrumentator().instrument(app).expose(app, include_in_schema=False)
+    Instrumentator().instrument(app)
+
+    @app.get(
+        "/metrics",
+        response_class=PlainTextResponse,
+        include_in_schema=True,
+    )
+    def _metrics():
+        metrics_data = generate_latest()
+        return PlainTextResponse(metrics_data)
