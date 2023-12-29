063330b8a10d2dc4dd8c2d117f89da9085564862
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2023-12-29 19:28:40 +0100

    fixes tests

--- packages/service-library/tests/fastapi/test_httpx_utils.py
+++ packages/service-library/tests/fastapi/test_httpx_utils.py
@@ -115,5 +115,5 @@ async def test_to_httpx_command(client: AsyncClient):
     print(cmd_short)
     assert (
         cmd_short
-        == 'httpx -m POST -c \'{"y": 12}\' -h "host" "test_base_http_api" -h "accept" "*/*" -h "accept-encoding" "gzip, deflate" -h "connection" "keep-alive" -h "user-agent" "python-httpx/0.25.0" -h "x-secret" "*****" -h "content-length" "9" -h "content-type" "application/json" https://test_base_http_api/foo?x=3'
+        == f'httpx -m POST -c \'{{"y": 12}}\' -h "host" "test_base_http_api" -h "accept" "*/*" -h "accept-encoding" "gzip, deflate" -h "connection" "keep-alive" -h "user-agent" "python-httpx/0.25.0" -h "x-secret" "{_PLACEHOLDER}" -h "content-length" "9" -h "content-type" "application/json" https://test_base_http_api/foo?x=3'
     )
