2a1362cba90f0c00001cd111ad7ebeb0e7621f5b
Andrei Neagu <neagu@itis.swiss>
2024-01-04 12:44:10 +0100

    add missing

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/services_tracker/api.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/services_tracker/api.py
@@ -1,3 +1,13 @@
+# nopycln: file
+
+from ._resource_manager import (
+    get_service_status,
+    run_dynamic_service,
+    stop_dynamic_service,
+)
+from ._setup import get_services_tracker, setup_services_tracker
+from ._tracker import ServicesTracker
+
 __all__: tuple[str, ...] = (
     "get_service_status",
     "get_services_tracker",
