e1a3302522fb3daa5a3804a13228a97857f9b307
Mads Bisgaard <bisgaard@itis.swiss>
2024-01-07 14:47:47 +0100

    move fastapi-prometheus-instrumentator to servicelib

---
+++ packages/service-library/src/servicelib/fastapi/prometheus_instrumentation.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
+
+def instrument_app(app: FastAPI):
+
+    Instrumentator(
+        should_instrument_requests_inprogress=True, inprogress_labels=True
+    ).instrument(app).expose(app)
