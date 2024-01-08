bf5dcc892843bdd73d82b9052dd8482057ad3016
Mads Bisgaard <bisgaard@itis.swiss>
2024-01-08 11:14:19 +0100

    no inprogress labels

--- packages/service-library/src/servicelib/fastapi/prometheus_instrumentation.py
+++ packages/service-library/src/servicelib/fastapi/prometheus_instrumentation.py
@@ -5,5 +5,5 @@ from prometheus_fastapi_instrumentator import Instrumentator
 def instrument_app(app: FastAPI):
 
     Instrumentator(
-        should_instrument_requests_inprogress=True, inprogress_labels=True
+        should_instrument_requests_inprogress=True, inprogress_labels=False
     ).instrument(app).expose(app)
