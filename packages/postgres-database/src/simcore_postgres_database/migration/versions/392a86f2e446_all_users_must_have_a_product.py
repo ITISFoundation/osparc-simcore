0b26acd3b14ded43771341eaa8c5b2de6b18e9a0
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-01-09 16:27:49 +0100

    migration script

---
+++ packages/postgres-database/src/simcore_postgres_database/migration/versions/392a86f2e446_all_users_must_have_a_product.py
@@ -0,0 +1,50 @@
+"""all users must have a product
+
+Revision ID: 392a86f2e446
+Revises: 0ad000429e3d
+Create Date: 2024-01-09 15:14:11.504329+00:00
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "392a86f2e446"
+down_revision = "0ad000429e3d"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # If a user has NO associated product groups, assign them a default product
+    migration_query = sa.text(
+        """
+        INSERT INTO users_to_groups (uid, gid)
+        SELECT u.uid, (
+            SELECT p.group_id
+            FROM products p
+            WHERE p.group_id IS NOT NULL
+            ORDER BY p.priority ASC
+            LIMIT 1
+        ) AS default_group_id
+        FROM users u
+        WHERE u.uid NOT IN (
+            SELECT utg.uid
+            FROM users_to_groups utg
+            WHERE utg.gid IN (
+                SELECT p.group_id
+                FROM products p
+            )
+        )
+        AND u.state = 'ACTIVE';
+    """
+    )
+
+    # Execute the migration query
+    conn = op.get_bind()
+    conn.execute(migration_query)
+
+
+def downgrade():
+    # Define the downgrade logic if needed
+    pass
