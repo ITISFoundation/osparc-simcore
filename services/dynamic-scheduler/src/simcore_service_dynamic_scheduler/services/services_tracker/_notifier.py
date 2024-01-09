ae6ab62cd24dd83bf7ccfbe98f1d68a88d075ed6
Andrei Neagu <neagu@itis.swiss>
2024-01-09 13:33:54 +0100

    refactor module

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/services_tracker/_notifier.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/services_tracker/_notifier.py
@@ -1,18 +1,10 @@
-# use the Notifer pattern used in payments as PC pointed out
-# requires the user_id to properly target for the status
-
-
-# NOTE: messages will be sent to all the open tabs, they need to be filtered by the fronted
-# in order to figure out if they require to update the project. should also include project_id
-# to be sure about all the possible issues
-
-
 import logging
 
 from fastapi import FastAPI
 from models_library.api_schemas_directorv2.dynamic_services import DynamicServiceGet
 from models_library.api_schemas_webserver.projects_nodes import NodeGet, NodeGetIdle
 from models_library.projects_nodes_io import NodeID
+from models_library.users import GroupID
 
 _logger = logging.getLogger(__name__)
 
@@ -21,8 +13,11 @@ async def publish_message(
     app: FastAPI,
     *,
     node_id: NodeID,
-    service_status: NodeGet | DynamicServiceGet | NodeGetIdle
+    service_status: NodeGet | DynamicServiceGet | NodeGetIdle,
+    primary_group_id: GroupID
 ) -> None:
     _ = app
-    _logger.debug("Publishing message for %s -> %s", node_id, service_status)
-    # TODO: finish here
+    _logger.debug(
+        "Publishing message for %s(%s) -> %s", node_id, primary_group_id, service_status
+    )
+    # NOTE: implementation is coming in a followup PR
