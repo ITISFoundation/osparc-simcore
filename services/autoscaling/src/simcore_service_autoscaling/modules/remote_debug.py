c7bcabbf3b70f541cbeec28afc1ecdfd43ff80cc
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-01-09 20:53:19 +0000

    use debugpy

--- services/autoscaling/src/simcore_service_autoscaling/modules/remote_debug.py
+++ services/autoscaling/src/simcore_service_autoscaling/modules/remote_debug.py
@@ -1,32 +1,26 @@
-""" Setup remote debugger with Python Tools for Visual Studio (PTVSD)
+""" Setup remote debugger with debugpy - a debugger for Python
+    https://github.com/microsoft/debugpy
 
 """
 import logging
 
 from fastapi import FastAPI
-from simcore_service_autoscaling.core.settings import get_application_settings
 
-logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
+_REMOTE_DEBUGGING_PORT = 3000
 
 
-def setup(app: FastAPI) -> None:
-    remote_debug_port = get_application_settings(app).AUTOSCALING_REMOTE_DEBUG_PORT
-
+def setup_remote_debugging(app: FastAPI) -> None:
     def on_startup() -> None:
         try:
-            logger.debug("Enabling attach ptvsd ...")
-            #
-            # SEE https://github.com/microsoft/ptvsd#enabling-debugging
-            #
-            import ptvsd
+            _logger.info("Attaching debugpy on %s...", _REMOTE_DEBUGGING_PORT)
 
-            ptvsd.enable_attach(
-                address=("0.0.0.0", remote_debug_port),  # nosec  # noqa: S104
-            )  # nosec
-        except ImportError as err:
-            msg = "Cannot enable remote debugging. Please install ptvsd first"
+            import debugpy
+
+            debugpy.listen(("0.0.0.0", _REMOTE_DEBUGGING_PORT))  # nosec  # noqa: S104
+
+        except ImportError as err:  # pragma: no cover
+            msg = "Cannot enable remote debugging. Please install debugpy first"
             raise RuntimeError(msg) from err
-
-        logger.info("Remote debugging enabled: listening port %s", remote_debug_port)
 
     app.add_event_handler("startup", on_startup)
