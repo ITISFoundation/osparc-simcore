57720a051c48b7c4436c5448f96ea6e43aad9877
Andrei Neagu <neagu@itis.swiss>
2024-01-11 14:55:17 +0100

    added start_exclusive_periodic_task

--- packages/service-library/src/servicelib/redis_utils.py
+++ packages/service-library/src/servicelib/redis_utils.py
@@ -1,20 +1,22 @@
+import asyncio
 import functools
 import logging
-from typing import Optional, Union
+from collections.abc import Awaitable, Callable
+from datetime import timedelta
 
-from .redis import RedisClientSDK
+import arrow
 
-log = logging.getLogger(__file__)
+from .background_task import start_periodic_task
+from .redis import CouldNotAcquireLockError, RedisClientSDK
+
+_logger = logging.getLogger(__file__)
 
 
 def exclusive(
-    redis: RedisClientSDK,
-    *,
-    lock_key: str,
-    lock_value: Optional[Union[bytes, str]] = None
+    redis: RedisClientSDK, *, lock_key: str, lock_value: bytes | str | None = None
 ):
     """
-    Define a method to run exclusively accross
+    Define a method to run exclusively across
     processes by leveraging a Redis Lock.
 
     parameters:
@@ -32,3 +34,62 @@ def exclusive(
         return wrapper
 
     return decorator
+
+
+async def _exclusive_task_starter(
+    redis: RedisClientSDK,
+    usr_tsk_task: Callable[..., Awaitable[None]],
+    *,
+    usr_tsk_interval: timedelta,
+    usr_tsk_task_name: str,
+    **kwargs,
+) -> None:
+    lock_key = f"lock:exclusive_task_starter:{usr_tsk_task_name}"
+    lock_value = f"locked since {arrow.utcnow().format()}"
+
+    try:
+        await exclusive(redis, lock_key=lock_key, lock_value=lock_value)(
+            start_periodic_task
+        )(
+            usr_tsk_task,
+            interval=usr_tsk_interval,
+            task_name=usr_tsk_task_name,
+            **kwargs,
+        )
+    except CouldNotAcquireLockError:
+        _logger.debug(
+            "Could not acquire lock '%s' with value '%s'", lock_key, lock_value
+        )
+    except Exception as e:
+        _logger.exception(e)  # noqa: TRY401
+        raise
+
+
+def start_exclusive_periodic_task(
+    redis: RedisClientSDK,
+    task: Callable[..., Awaitable[None]],
+    *,
+    interval: timedelta,
+    task_name: str,
+    **kwargs,
+) -> asyncio.Task:
+    """
+    Ensures that only 1 instance of `task` is periodically ran at all times across
+    multiple processes.
+    If one process dies out another process will run the `task`.
+
+    Creates a background task that periodically tries to start the user `task`.
+    Before the `task` is scheduled for periodic background execution, it acquires a lock.
+    Subsequent calls to `start_exclusive_periodic_task` will not allow the same `task`
+    to run since the lock will prevent the scheduling.
+    """
+    return start_periodic_task(
+        _exclusive_task_starter,
+        interval=interval,
+        task_name=f"exclusive_task_starter_{task_name}",
+        redis=redis,
+        usr_tsk_task=task,
+        usr_tsk_interval=timedelta(seconds=1),
+        usr_tsk_task_name=task_name,
+        **kwargs,
+    )
