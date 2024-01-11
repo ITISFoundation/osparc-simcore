204ecdaa82c60b6b2838ad5fde4040687842d195
Mads Bisgaard <bisgaard@itis.swiss>
2024-01-11 10:37:07 +0100

    try to please pylint

--- packages/service-library/src/servicelib/fastapi/prometheus_instrumentation.py
+++ packages/service-library/src/servicelib/fastapi/prometheus_instrumentation.py
@@ -1,3 +1,6 @@
+# pylint: disable=protected-access
+
+
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
 
@@ -13,9 +16,7 @@ def instrument_app(app: FastAPI):
     )
 
     def _unregister():
-        for collector in list(
-            instrumentator.registry._collector_to_names.keys()
-        ):  # pylint: disable=protected-access
+        for collector in list(instrumentator.registry._collector_to_names.keys()):
             instrumentator.registry.unregister(collector)
 
     # avoid registering collectors multiple times when running unittests consecutively (https://stackoverflow.com/a/62489287)
