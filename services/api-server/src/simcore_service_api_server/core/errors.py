e82078844825eeacd61dbff27131bc80267f1eee
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-01-12 16:31:28 +0100

    errors

---
+++ services/api-server/src/simcore_service_api_server/core/errors.py
@@ -0,0 +1,53 @@
+from enum import auto
+
+import httpx
+from models_library.utils.enums import StrAutoEnum
+from pydantic.errors import PydanticErrorMixin
+from servicelib.fastapi.httpx_utils import to_httpx_command
+
+
+class _BaseAppError(PydanticErrorMixin, ValueError):
+    @classmethod
+    def get_full_class_name(cls) -> str:
+        # Can be used as unique code identifier
+        return f"{cls.__module__}.{cls.__name__}"
+
+
+class BackendEnum(StrAutoEnum):
+    CATALOG = auto()
+    DIRECTOR = auto()
+    STORAGE = auto()
+    WEBSERVER = auto()
+
+
+class BackendServiceError(_BaseAppError):
+    http_status_error: httpx.HTTPStatusError | None = None
+    service: BackendEnum
+
+    msg_template = "{service} error"
+
+    @classmethod
+    def from_httpx_status_error(
+        cls, service: BackendEnum, error: httpx.HTTPStatusError, **ctx
+    ) -> "BackendServiceError":
+        return cls(http_status_error=error, service=service, **ctx)
+
+    def get_debug_message(self) -> str:
+        msg = f"{self}"
+        if http_status_error := getattr(self, "http_status_error", None):
+            resp = http_status_error.response
+            # request
+            msg += f"\n\t'{to_httpx_command(http_status_error.request)}'"
+            msg += f"\n\t'{resp.text}'"
+            # status, latency
+            msg += f"\n\t{resp.status_code}, {resp.elapsed.total_seconds()*1E6}us"
+            # response
+        return msg
+
+
+class DirectorError(BackendServiceError):
+    service = BackendEnum.DIRECTOR
+
+
+class WebServerError(BackendServiceError):
+    service = BackendEnum.WEBSERVER
---
+++ services/api-server/tests/unit/test_core_errors.py
@@ -0,0 +1,63 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+# pylint: disable=too-many-arguments
+
+from collections.abc import AsyncIterator, Iterator
+
+import httpx
+import pytest
+import respx
+from fastapi import status
+from httpx import AsyncClient
+from simcore_service_api_server.core.errors import BackendEnum, BackendServiceError
+
+
+@pytest.fixture
+def base_url() -> str:
+    return f"https://{__name__}"
+
+
+@pytest.fixture
+def mock_server_api(base_url: str) -> Iterator[respx.MockRouter]:
+    with respx.mock(
+        base_url=base_url,
+        assert_all_called=False,
+        assert_all_mocked=True,  # IMPORTANT: KEEP always True!
+    ) as mock:
+
+        mock.get("/ok").respond(status.HTTP_200_OK)
+        mock.post(path__startswith="/fail").respond(
+            status.HTTP_500_INTERNAL_SERVER_ERROR, text="FAILURE"
+        )
+
+        yield mock
+
+
+@pytest.fixture
+async def client(
+    mock_server_api: respx.MockRouter, base_url: str
+) -> AsyncIterator[AsyncClient]:
+    async with httpx.AsyncClient(base_url=base_url) as cli:
+        yield cli
+
+
+async def test_backend_error(client: AsyncClient):
+
+    try:
+        response = await client.post("/fail", params={"id": 3}, json={"x": 2})
+        response.raise_for_status()
+
+    except httpx.HTTPStatusError as err:
+        service_error = BackendServiceError.from_httpx_status_error(
+            service=BackendEnum.DIRECTOR, error=err
+        )
+
+        assert hasattr(service_error, "http_status_error")  # auto-injected as context
+
+        assert service_error.get_debug_message()
+        assert service_error.service == BackendEnum.DIRECTOR
+        assert (
+            service_error.get_full_class_name()
+            == "simcore_service_api_server.core.errors.BackendServiceError"
+        )
