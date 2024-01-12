726d07c336ac9378ee09c4e0bd07935ba2c368a9
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-01-12 16:25:11 +0000

    typo

--- services/autoscaling/src/simcore_service_autoscaling/utils/computational_scaling.py
+++ services/autoscaling/src/simcore_service_autoscaling/utils/computational_scaling.py
@@ -89,6 +89,6 @@ def try_assigning_task_to_instance_types(
         if assigned_tasks_to_instance_type.has_resources_for_task(
             task_required_resources
         ):
-            assigned_tasks_to_instance_type.assign_task(task)
+            assigned_tasks_to_instance_type.assign_task(task, task_required_resources)
             return True
     return False
