78f7e5cce9a259f62314e1c8c5cff996eda72c72
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-01-19 15:25:10 +0100

    ruff models-library

--- packages/models-library/tests/test_user_preferences.py
+++ packages/models-library/tests/test_user_preferences.py
@@ -92,9 +92,7 @@ def test_user_service_preferences(value: Any, mock_file_path: Path):
 def unregister_defined_classes() -> Iterator[None]:
     yield
     # pylint: disable=protected-access
-    _AutoRegisterMeta.registered_user_preference_classes.pop(  # noqa: SLF001
-        "Pref1", None
-    )
+    _AutoRegisterMeta.registered_user_preference_classes.pop("Pref1", None)
 
 
 def test__frontend__user_preference(value: Any, unregister_defined_classes: None):
