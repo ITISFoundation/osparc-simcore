7603d776e8e1808607bd06eeda9ec991abbff287
Andrei Neagu <neagu@itis.swiss>
2024-01-22 13:28:25 +0100

    refactor validation

--- services/director/src/simcore_service_director/config.py
+++ services/director/src/simcore_service_director/config.py
@@ -5,6 +5,7 @@ import json
 import logging
 import os
 from distutils.util import strtobool
+from typing import Dict, Optional
 
 from servicelib.client_session import (  # pylint: disable=no-name-in-module
     APP_CLIENT_SESSION_KEY,
@@ -61,9 +62,9 @@ DIRECTOR_SERVICES_CUSTOM_CONSTRAINTS: str = os.environ.get(
 )
 
 
-def _parse_placement_replacements() -> dict[str, str] | None:
-    result: dict[str, str] | None = None
-    str_env_var: str | None = os.environ.get(
+def _parse_placement_replacements() -> Optional[Dict[str, str]]:
+    result: Optional[Dict[str, str]] = None
+    str_env_var: Optional[str] = os.environ.get(
         "DIRECTOR_PLACEMENT_CONSTRAINTS_REPLACEMENTS_FOR_GENERIC_RESOURCES", None
     )
     if str_env_var:
@@ -78,9 +79,9 @@ def _parse_placement_replacements() -> dict[str, str] | None:
     return result
 
 
-DIRECTOR_PLACEMENT_CONSTRAINTS_REPLACEMENTS_FOR_GENERIC_RESOURCES: None | (
-    dict[str, str]
-) = _parse_placement_replacements()
+DIRECTOR_PLACEMENT_CONSTRAINTS_REPLACEMENTS_FOR_GENERIC_RESOURCES: Optional[
+    Dict[str, str]
+] = _parse_placement_replacements()
 
 # for passing self-signed certificate to spawned services
 DIRECTOR_SELF_SIGNED_SSL_SECRET_ID: str = os.environ.get(
@@ -121,7 +122,7 @@ REGISTRY_SSL: bool = strtobool(os.environ.get("REGISTRY_SSL", "True"))
 EXTRA_HOSTS_SUFFIX: str = os.environ.get("EXTRA_HOSTS_SUFFIX", "undefined")
 
 # these are the envs passed to the dynamic services by default
-SERVICES_DEFAULT_ENVS: dict[str, str] = {
+SERVICES_DEFAULT_ENVS: Dict[str, str] = {
     "POSTGRES_ENDPOINT": os.environ.get(
         "POSTGRES_ENDPOINT", "undefined postgres endpoint"
     ),
@@ -146,7 +147,7 @@ NODE_SCHEMA_LOCATION: str = os.environ.get(
     "NODE_SCHEMA_LOCATION", f"{API_ROOT}/{API_VERSION}/schemas/node-meta-v0.0.1.json"
 )
 # used to find the right network name
-SIMCORE_SERVICES_NETWORK_NAME: str | None = os.environ.get(
+SIMCORE_SERVICES_NETWORK_NAME: Optional[str] = os.environ.get(
     "SIMCORE_SERVICES_NETWORK_NAME"
 )
 # useful when developing with an alternative registry namespace
