0b4926ffd04ede893f673ec45a1ad9332c3ef146
Mads Bisgaard <bisgaard@itis.swiss>
2024-01-24 09:19:30 +0100

    Add OEC @pcrespov

--- services/api-server/src/simcore_service_api_server/api/errors/custom_errors.py
+++ services/api-server/src/simcore_service_api_server/api/errors/custom_errors.py
@@ -1,7 +1,11 @@
+import logging
 from urllib.request import Request
 
 from fastapi import status
+from servicelib.error_codes import create_error_code
 from starlette.responses import JSONResponse
+
+_logger = logging.getLogger(__name__)
 
 
 class CustomBaseError(Exception):
@@ -25,7 +29,9 @@ async def custom_error_handler(_: Request, exc: CustomBaseError):
         return JSONResponse(
             status_code=status.HTTP_424_FAILED_DEPENDENCY, content=f"{exc}"
         )
+    error_code = create_error_code(exc)
+    _logger.error("Unexpected error", extra={"error_cose": error_code})
     return JSONResponse(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        content="Encountered unspecified error",
+        content=f"Unexpected server error [OEC: {error_code}]",
     )
