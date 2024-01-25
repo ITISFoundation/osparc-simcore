b92bd67fcb719279adc8597a68335d1d9efee676
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-01-25 14:04:52 +0100

    use authentication to connect with scheduler

--- services/clusters-keeper/src/simcore_service_clusters_keeper/rpc/clusters.py
+++ services/clusters-keeper/src/simcore_service_clusters_keeper/rpc/clusters.py
@@ -10,7 +10,7 @@ from ..modules import clusters
 from ..modules.dask import ping_scheduler
 from ..modules.redis import get_redis_client
 from ..utils.clusters import create_cluster_from_ec2_instance
-from ..utils.dask import get_scheduler_url
+from ..utils.dask import get_scheduler_auth, get_scheduler_url
 
 router = RPCRouter()
 
@@ -50,6 +50,8 @@ async def get_or_create_cluster(
         wallet_id,
         dask_scheduler_ready=bool(
             ec2_instance.state == "running"
-            and await ping_scheduler(url=get_scheduler_url(ec2_instance))
+            and await ping_scheduler(
+                get_scheduler_url(ec2_instance), get_scheduler_auth(app)
+            )
         ),
     )
--- services/clusters-keeper/src/simcore_service_clusters_keeper/utils/dask.py
+++ services/clusters-keeper/src/simcore_service_clusters_keeper/utils/dask.py
@@ -1,7 +1,17 @@
 from aws_library.ec2.models import EC2InstanceData
+from fastapi import FastAPI
+from models_library.clusters import InternalClusterAuthentication
 from pydantic import AnyUrl, parse_obj_as
+
+from ..core.settings import get_application_settings
 
 
 def get_scheduler_url(ec2_instance: EC2InstanceData) -> AnyUrl:
     url: AnyUrl = parse_obj_as(AnyUrl, f"tcp://{ec2_instance.aws_public_ip}:8786")
     return url
+
+
+def get_scheduler_auth(app: FastAPI) -> InternalClusterAuthentication:
+    return get_application_settings(
+        app
+    ).CLUSTERS_KEEPER_COMPUTATIONAL_BACKEND_DEFAULT_CLUSTER_AUTH
