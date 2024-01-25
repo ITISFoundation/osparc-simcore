b794af9efe47399b6bc0ea804893ed22fa4a447d
Andrei Neagu <neagu@itis.swiss>
2024-01-25 15:09:43 +0100

    fixed test

--- services/payments/tests/unit/test_services_notifier.py
+++ services/payments/tests/unit/test_services_notifier.py
@@ -18,6 +18,7 @@ from fastapi import FastAPI
 from models_library.api_schemas_payments.socketio import (
     SOCKET_IO_PAYMENT_COMPLETED_EVENT,
 )
+from models_library.api_schemas_webserver.socketio import SocketIORoom
 from models_library.api_schemas_webserver.wallets import PaymentTransaction
 from models_library.users import GroupID, UserID
 from pydantic import parse_obj_as
@@ -95,8 +96,7 @@ def socketio_server_events(
     mocker: MockerFixture,
     user_primary_group_id: GroupID,
 ) -> dict[str, AsyncMock]:
-
-    user_room_name = f"{user_primary_group_id}"
+    user_room_name = SocketIORoom.from_group_id(user_primary_group_id)
 
     # handlers
     async def connect(sid: str, environ):
