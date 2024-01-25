a867ee525af2918df8ab0e58b25de07d858971e9
Andrei Neagu <neagu@itis.swiss>
2024-01-25 17:13:16 +0100

    disable async support

--- packages/service-library/src/servicelib/aiohttp/monitor_slow_callbacks.py
+++ packages/service-library/src/servicelib/aiohttp/monitor_slow_callbacks.py
@@ -1,19 +1,10 @@
 import asyncio.events
 import sys
 import time
-from typing import ClassVar
 
 from pyinstrument import Profiler
 
 from .incidents import LimitedOrderedStack, SlowCallback
-
-
-class ProfilerBorg:
-    _shared_state: ClassVar[dict] = {}
-
-    def __init__(self, interval: float) -> None:
-        self.__dict__ = self._shared_state
-        self.profiler = Profiler(interval=interval)
 
 
 def enable(
@@ -34,7 +25,7 @@ def enable(
         # unsetting profiler, helps with development mode and tests
         sys.setprofile(None)
 
-        with self.profiler_borg.profiler:
+        with Profiler(interval=slow_duration_secs, async_mode="disabled") as profiler:
             t0 = time.monotonic()
 
             retval = _run(self)
@@ -45,7 +36,7 @@ def enable(
             # the indentation is correct, the profiler needs to be stopped when
             # printing the output, the profiler is started and stopped by the
             # contextmanger
-            profiler_result = self.profiler_borg.profiler.output_text(
+            profiler_result = profiler.output_text(
                 unicode=True, color=False, show_all=True
             )
             incidents.append(SlowCallback(msg=profiler_result, delay_secs=dt))
@@ -56,4 +47,3 @@ def enable(
         return retval
 
     asyncio.events.Handle._run = instrumented  # type: ignore[method-assign]
-    asyncio.events.Handle.profiler_borg = ProfilerBorg(interval=slow_duration_secs)  # type: ignore[method-assign]
