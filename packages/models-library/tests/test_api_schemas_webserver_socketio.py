a2c35db66ec5961e2afe00545fd323257385be15
Andrei Neagu <neagu@itis.swiss>
2024-01-25 17:05:29 +0100

    fixeed failing test

--- packages/models-library/tests/test_api_schemas_webserver_socketio.py
+++ packages/models-library/tests/test_api_schemas_webserver_socketio.py
@@ -23,5 +23,5 @@ def socket_id(faker: Faker) -> str:
 
 def test_socketio_room(user_id: UserID, group_id: GroupID, socket_id: str):
     assert SocketIORoom.from_user_id(user_id) == f"user:{user_id}"
-    assert SocketIORoom.from_group_id(group_id) == f"{group_id}"
+    assert SocketIORoom.from_group_id(group_id) == f"group:{group_id}"
     assert SocketIORoom.from_socket_id(socket_id) == socket_id
