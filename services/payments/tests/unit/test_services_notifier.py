737eb17a0d764ba0c19177373ca412be14f2b39f
Andrei Neagu <neagu@itis.swiss>
2024-01-25 15:12:55 +0100

    fixed issue

--- services/payments/tests/unit/test_services_notifier.py
+++ services/payments/tests/unit/test_services_notifier.py
@@ -96,12 +96,12 @@ def socketio_server_events(
     mocker: MockerFixture,
     user_primary_group_id: GroupID,
 ) -> dict[str, AsyncMock]:
-    user_room_name = SocketIORoom.from_group_id(user_primary_group_id)
+    room_name = SocketIORoom.from_group_id(user_primary_group_id)
 
     # handlers
     async def connect(sid: str, environ):
         print("connecting", sid)
-        await socketio_server.enter_room(sid, user_room_name)
+        await socketio_server.enter_room(sid, room_name)
 
     async def on_check(sid, data):
         print("check", sid, data)
@@ -111,7 +111,7 @@ def socketio_server_events(
 
     async def disconnect(sid: str):
         print("disconnecting", sid)
-        await socketio_server.leave_room(sid, user_room_name)
+        await socketio_server.leave_room(sid, room_name)
 
     # spies
     spy_connect = mocker.AsyncMock(wraps=connect)
