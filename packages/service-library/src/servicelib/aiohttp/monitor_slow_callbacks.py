0805fdca69d5a5106bf82d6d90b6aa9dc97cdbf6
Andrei Neagu <neagu@itis.swiss>
2024-01-25 17:02:23 +0100

    rename and using shared instance

--- packages/service-library/src/servicelib/aiohttp/monitor_slow_callbacks.py
+++ packages/service-library/src/servicelib/aiohttp/monitor_slow_callbacks.py
@@ -1,10 +1,19 @@
 import asyncio.events
 import sys
 import time
+from typing import ClassVar
 
 from pyinstrument import Profiler
 
 from .incidents import LimitedOrderedStack, SlowCallback
+
+
+class ProfilerBorg:
+    _shared_state: ClassVar[dict] = {}
+
+    def __init__(self, interval: float) -> None:
+        self.__dict__ = self._shared_state
+        self.profiler = Profiler(interval=interval, async_mode="strict")
 
 
 def enable(
@@ -21,14 +30,11 @@ def enable(
     aio_debug_logger = get_logger(__name__)
     _run = asyncio.events.Handle._run
 
-    # sharing instance
-    profiler = Profiler(interval=slow_duration_secs)
-
     def instrumented(self):
         # unsetting profiler, helps with development mode and tests
         sys.setprofile(None)
 
-        with profiler:
+        with self.profiler_borg.profiler:
             t0 = time.monotonic()
 
             retval = _run(self)
@@ -39,7 +45,7 @@ def enable(
             # the indentation is correct, the profiler needs to be stopped when
             # printing the output, the profiler is started and stopped by the
             # contextmanger
-            profiler_result = profiler.output_text(
+            profiler_result = self.profiler_borg.profiler.output_text(
                 unicode=True, color=False, show_all=True
             )
             incidents.append(SlowCallback(msg=profiler_result, delay_secs=dt))
@@ -50,3 +56,4 @@ def enable(
         return retval
 
     asyncio.events.Handle._run = instrumented  # type: ignore[method-assign]
+    asyncio.events.Handle.profiler_borg = ProfilerBorg(interval=slow_duration_secs)  # type: ignore[method-assign]
