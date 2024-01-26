64d735e31db08103320d1817e3fb42986135da97
Andrei Neagu <neagu@itis.swiss>
2024-01-26 11:06:54 +0100

    using proper validation

--- packages/models-library/src/models_library/api_schemas_webserver/socketio.py
+++ packages/models-library/src/models_library/api_schemas_webserver/socketio.py
@@ -1,3 +1,5 @@
+from pydantic import parse_obj_as
+
 from ..basic_types import IDStr
 from ..users import GroupID, UserID
 
@@ -5,12 +7,12 @@ from ..users import GroupID, UserID
 class SocketIORoomStr(IDStr):
     @classmethod
     def from_socket_id(cls, socket_id: str) -> "SocketIORoomStr":
-        return cls(socket_id)
+        return parse_obj_as(cls, socket_id)
 
     @classmethod
     def from_group_id(cls, group_id: GroupID) -> "SocketIORoomStr":
-        return cls(f"group:{group_id}")
+        return parse_obj_as(cls, f"group:{group_id}")
 
     @classmethod
     def from_user_id(cls, user_id: UserID) -> "SocketIORoomStr":
-        return cls(f"user:{user_id}")
+        return parse_obj_as(cls, f"user:{user_id}")
