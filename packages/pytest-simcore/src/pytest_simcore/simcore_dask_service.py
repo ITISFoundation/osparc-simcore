86df492bcab7ac6827d48e41c6069beaef9b3323
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-01-27 15:51:23 +0100

    it is now tls

--- packages/pytest-simcore/src/pytest_simcore/simcore_dask_service.py
+++ packages/pytest-simcore/src/pytest_simcore/simcore_dask_service.py
@@ -11,7 +11,7 @@ from pydantic import AnyUrl
 from .helpers.utils_docker import get_service_published_port
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 async def dask_scheduler_service(simcore_services_ready, monkeypatch) -> str:
     # the dask scheduler has a UI for the dashboard and a secondary port for the API
     # simcore_services fixture already ensure the dask-scheduler is up and running
@@ -20,17 +20,16 @@ async def dask_scheduler_service(simcore_services_ready, monkeypatch) -> str:
     )
     # override the port
     monkeypatch.setenv("DASK_SCHEDULER_PORT", f"{dask_scheduler_api_port}")
-    return AnyUrl.build(scheme="tcp", host="127.0.0.1", port=dask_scheduler_api_port)
+    return AnyUrl.build(scheme="tls", host="127.0.0.1", port=dask_scheduler_api_port)
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def dask_client(dask_scheduler_service: str) -> Iterator[Client]:
-
     client = Client(dask_scheduler_service)
     yield client
     client.close()
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def dask_sidecar_service(dask_client: Client) -> None:
     dask_client.wait_for_workers(n_workers=1, timeout=30)
