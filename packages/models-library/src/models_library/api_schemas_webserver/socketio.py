5897de1c4be4c4d0313a7f0fbce4602689f4eacb
Andrei Neagu <neagu@itis.swiss>
2024-01-30 08:20:43 +0100

    remove parsing

--- packages/models-library/src/models_library/api_schemas_webserver/socketio.py
+++ packages/models-library/src/models_library/api_schemas_webserver/socketio.py
@@ -1,5 +1,3 @@
-from pydantic import parse_obj_as
-
 from ..basic_types import IDStr
 from ..users import GroupID, UserID
 
@@ -7,12 +5,12 @@ from ..users import GroupID, UserID
 class SocketIORoomStr(IDStr):
     @classmethod
     def from_socket_id(cls, socket_id: str) -> "SocketIORoomStr":
-        return parse_obj_as(cls, socket_id)
+        return cls(socket_id)
 
     @classmethod
     def from_group_id(cls, group_id: GroupID) -> "SocketIORoomStr":
-        return parse_obj_as(cls, f"group:{group_id}")
+        return cls(f"group:{group_id}")
 
     @classmethod
     def from_user_id(cls, user_id: UserID) -> "SocketIORoomStr":
-        return parse_obj_as(cls, f"user:{user_id}")
+        return cls(f"user:{user_id}")
