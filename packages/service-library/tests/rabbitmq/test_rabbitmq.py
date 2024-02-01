c16abb7790c62dc1d8b9fc148b9bbcffc58cd30c
Andrei Neagu <neagu@itis.swiss>
2024-02-01 14:35:01 +0100

    refactor

--- packages/service-library/tests/rabbitmq/test_rabbitmq.py
+++ packages/service-library/tests/rabbitmq/test_rabbitmq.py
@@ -29,7 +29,10 @@ pytest_simcore_core_services_selection = [
 ]
 
 _ON_ERROR_DELAY_S: Final[float] = 0.1
-_TIMEOUT_IF_STUCK: Final[float] = 20
+# NOTE: if running the tests individually consider raising
+# this value to avoid failures due to timeouts
+# when ran in batch the rabbit service is already up and running
+_TIMEOUT_IF_STUCK: Final[float] = 10
 
 
 @pytest.fixture
