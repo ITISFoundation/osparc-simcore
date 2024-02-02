5650028dcaddd1f9f06b48fe206f8612de0f212d
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-02-02 11:29:28 +0100

    tests send and attach

--- services/payments/tests/unit/test_services_notifier_email.py
+++ services/payments/tests/unit/test_services_notifier_email.py
@@ -3,9 +3,10 @@
 # pylint: disable=unused-variable
 # pylint: disable=too-many-arguments
 
+import mimetypes
+from email.headerregistry import Address
 from email.message import EmailMessage
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from email.utils import make_msgid
 from pathlib import Path
 
 import aiosmtplib
@@ -22,7 +23,17 @@ def tmp_environment(
     return setenvs_from_envfile(monkeypatch, osparc_simcore_root_dir / ".secrets")
 
 
-async def test_it(tmp_environment: EnvVarsDict):
+def guess_file_type(file_path: Path) -> tuple[str, str]:
+    assert file_path.is_file()
+    mimetype, _encoding = mimetypes.guess_type(file_path)
+    if mimetype:
+        maintype, subtype = mimetype.split("/", maxsplit=1)
+    else:
+        maintype, subtype = "application", "octet-stream"
+    return maintype, subtype
+
+
+async def test_it(tmp_environment: EnvVarsDict, osparc_simcore_root_dir: Path):
 
     settings = SMTPSettings.create_from_envs()
 
@@ -45,33 +56,82 @@ async def test_it(tmp_environment: EnvVarsDict):
                 settings.SMTP_PASSWORD.get_secret_value(),
             )
 
-        # Session ready to send email
+        def compose_email(msg: EmailMessage, text_body, html_body) -> EmailMessage:
+            # Text version
+            msg.set_content(
+                f"""\
+                {text_body}
 
-        def compose_simple():
-            # compose simple
-            msg = EmailMessage()
-            msg["From"] = "support@osparc.io"
-            msg["To"] = "crespo@speag.com"
-            msg["Subject"] = "Hello World!"
-            msg.set_content("Sent via aiosmtplib")
-            return msg
-
-        def compose_multipart():
-            # multipart
-            msg = MIMEMultipart("alternative")
-            msg["From"] = "support@osparc.io"
-            msg["To"] = "crespo@speag.com"
-            msg["Subject"] = "Hello World!"
-
-            plain_text_message = MIMEText("Sent via aiosmtplib", "plain", "utf-8")
-            html_message = MIMEText(
-                "<html><body><h1>Sent via aiosmtplib</h1></body></html>",
-                "html",
-                "utf-8",
+                Done with love at Z43
+            """
             )
-            msg.attach(plain_text_message)
-            msg.attach(html_message)
+
+            # HTML version
+            logo_cid = make_msgid()
+            msg.add_alternative(
+                f"""\
+            <html>
+            <head></head>
+            <body>
+                {html_body}
+                Done with love at <img src="cid:{logo_cid[1:-1]}" width=30/>
+            </body>
+            </html>
+            """,
+                subtype="html",
+            )
+
+            assert msg.is_multipart()
+
+            logo_path = (
+                osparc_simcore_root_dir
+                / "services/static-webserver/client/source/resource/osparc/z43-logo.png"
+            )
+            maintype, subtype = guess_file_type(logo_path)
+            msg.get_payload(1).add_related(
+                logo_path.read_bytes(),
+                maintype=maintype,
+                subtype=subtype,
+                cid=logo_cid,
+            )
+
+            # Attach file
+            pdf_path = osparc_simcore_root_dir / "ignore.pdf"
+            maintype, subtype = guess_file_type(pdf_path)
+            msg.add_attachment(
+                pdf_path.read_bytes(),
+                filename=pdf_path.name,
+                maintype=maintype,
+                subtype=subtype,
+            )
             return msg
 
-        await smtp.send_message(compose_simple())
-        await smtp.send_message(compose_multipart())
+        msg = EmailMessage()
+        msg["From"] = Address(
+            display_name="osparc support", addr_spec="support@osparc.io"
+        )
+        msg["To"] = Address(
+            display_name="Pedro Crespo-Valero", addr_spec="crespo@speag.com"
+        )
+        msg["Subject"] = "Payment invoice"
+
+        text_body = """\
+        Hi there,
+
+        This is your invoice.
+        """
+
+        html_body = """\
+        <p>Hi there!</p>
+        <p>This is your
+            <a href="http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718">
+                invoice
+            </a>.
+        </p>
+        """
+        msg = compose_email(msg, text_body, html_body)
+
+        await smtp.send_message(msg)
+
+        # render a template
+        # common CSS+HTML
