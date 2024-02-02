be3e0009a11d46957022678e494481883d4aade5
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-02-02 16:03:44 +0100

    🐛 🚑️ Fixes internal server error in port matching (#5292)

--- services/web/server/src/simcore_service_webserver/catalog/_models.py
+++ services/web/server/src/simcore_service_webserver/catalog/_models.py
@@ -62,8 +62,12 @@ _CACHE_MAXSIZE: Final = (
 _CACHE_TTL: Final = 60  # secs
 
 
-def _hash_inputs(service: dict[str, Any], input_key: str, ureg):
-    assert ureg  # nosec
+def _hash_inputs(
+    service: dict[str, Any],
+    input_key: str,
+    *args,  # noqa: ARG001 # pylint: disable=unused-argument
+    **kwargs,  # noqa: ARG001 # pylint: disable=unused-argument
+):
     return f"{service['key']}/{service['version']}/{input_key}"
 
 
@@ -92,8 +96,12 @@ class ServiceInputGetFactory:
         return port
 
 
-def _hash_outputs(service: dict[str, Any], output_key: str, ureg):
-    assert ureg  # nosec
+def _hash_outputs(
+    service: dict[str, Any],
+    output_key: str,
+    *args,  # noqa: ARG001 # pylint: disable=unused-argument
+    **kwargs,  # noqa: ARG001 # pylint: disable=unused-argument
+):
     return f"{service['key']}/{service['version']}/{output_key}"
 
 
