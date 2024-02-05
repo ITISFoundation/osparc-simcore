13455e47c05ca4574d0fce12cd7222b3fec346ee
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-02-05 17:36:44 +0100

    new exceptions

--- services/storage/src/simcore_service_storage/s3_utils.py
+++ services/storage/src/simcore_service_storage/s3_utils.py
@@ -1,19 +1,36 @@
 import functools
 import logging
 from dataclasses import dataclass
-from typing import Final, Optional
+from typing import Final
 
-from botocore import exceptions as botocore_exc
+import botocore.exceptions
+import tenacity
 from pydantic import ByteSize, parse_obj_as
 from servicelib.aiohttp.long_running_tasks.server import (
     ProgressMessage,
     ProgressPercent,
     TaskProgress,
 )
+from tenacity.before_sleep import before_sleep_log
+from tenacity.retry import retry_if_exception_type
+from tenacity.stop import stop_after_attempt
+from tenacity.wait import wait_exponential
 
-from .exceptions import S3AccessError, S3BucketInvalidError, S3KeyNotFoundError
+from .exceptions import (
+    S3AccessError,
+    S3BucketInvalidError,
+    S3KeyNotFoundError,
+    S3ReadTimeoutError,
+)
 
-logger = logging.getLogger(__name__)
+#
+# Retry policies
+#
+# NOTE: these are retries on the S3* exceptions mapped in s3_exception_handler
+#
+
+
+_logger = logging.getLogger(__name__)
 
 # this is artifically defined, if possible we keep a maximum number of requests for parallel
 # uploading. If that is not possible then we create as many upload part as the max part size allows
@@ -42,63 +59,83 @@ def compute_num_file_chunks(file_size: ByteSize) -> tuple[int, ByteSize]:
         num_upload_links = int(file_size / chunk) + (1 if file_size % chunk > 0 else 0)
         if num_upload_links < _MULTIPART_MAX_NUMBER_OF_PARTS:
             return (num_upload_links, chunk)
+    msg = f"Could not determine number of upload links for {file_size=}"
     raise ValueError(
-        f"Could not determine number of upload links for {file_size=}",
+        msg,
     )
 
 
 def s3_exception_handler(log: logging.Logger):
-    """converts typical aiobotocore/boto exceptions to storage exceptions
+    """Converts typical aiobotocore/boto exceptions to storage exceptions
     NOTE: this is a work in progress as more exceptions might arise in different
     use-cases
+
+    SEE https://boto3.amazonaws.com/v1/documentation/api/latest/guide/error-handling.html
     """
 
-    def decorator(func):
+    def _decorator(func):
         @functools.wraps(func)
-        async def wrapper(self, *args, **kwargs):
+        async def _wrapper(self, *args, **kwargs):
+
             try:
                 response = await func(self, *args, **kwargs)
-            except self.client.exceptions.NoSuchBucket as exc:
-                raise S3BucketInvalidError(
-                    bucket=exc.response.get("Error", {}).get("BucketName", "undefined")
-                ) from exc
-            except botocore_exc.ClientError as exc:
-                if exc.response.get("Error", {}).get("Code") == "404":
-                    if exc.operation_name == "HeadObject":
-                        raise S3KeyNotFoundError(bucket=args[0], key=args[1]) from exc
-                    if exc.operation_name == "HeadBucket":
-                        raise S3BucketInvalidError(bucket=args[0]) from exc
-                if exc.response.get("Error", {}).get("Code") == "403":
-                    if exc.operation_name == "HeadBucket":
-                        raise S3BucketInvalidError(bucket=args[0]) from exc
-                raise S3AccessError from exc
-            except botocore_exc.EndpointConnectionError as exc:
-                raise S3AccessError from exc
 
-            except botocore_exc.BotoCoreError as exc:
+            except self.client.exceptions.NoSuchBucket as err:
+                raise S3BucketInvalidError(
+                    bucket=err.response.get("Error", {}).get("BucketName", "undefined")
+                ) from err
+
+            except botocore.exceptions.ReadTimeoutError as err:
+                raise S3ReadTimeoutError(error=err) from err
+
+            except botocore.exceptions.ClientError as err:
+                # AWS services error responses
+                error_code: str | None = err.response.get("Error", {}).get("Code", None)
+                if error_code == "404":
+                    if err.operation_name == "HeadObject":
+                        raise S3KeyNotFoundError(bucket=args[0], key=args[1]) from err
+                    if err.operation_name == "HeadBucket":
+                        raise S3BucketInvalidError(bucket=args[0]) from err
+                if error_code == "403" and err.operation_name == "HeadBucket":
+                    raise S3BucketInvalidError(bucket=args[0]) from err
+                raise S3AccessError from err
+
+            except botocore.exceptions.EndpointConnectionError as err:
+                raise S3AccessError from err
+
+            except botocore.exceptions.BotoCoreError as err:
                 log.exception("Unexpected error in s3 client: ")
-                raise S3AccessError from exc
+                raise S3AccessError from err
 
             return response
 
-        return wrapper
+        return _wrapper
 
-    return decorator
+    return _decorator
+
+
+on_timeout_retry_with_exponential_backoff = tenacity.retry(
+    retry=retry_if_exception_type(S3ReadTimeoutError),
+    reraise=True,
+    stop=stop_after_attempt(5),
+    wait=wait_exponential(multiplier=1, min=4, max=60),
+    before_sleep=before_sleep_log(_logger, logging.WARNING),
+)
 
 
 def update_task_progress(
-    task_progress: Optional[TaskProgress],
-    message: Optional[ProgressMessage] = None,
-    progress: Optional[ProgressPercent] = None,
+    task_progress: TaskProgress | None,
+    message: ProgressMessage | None = None,
+    progress: ProgressPercent | None = None,
 ) -> None:
-    logger.debug("%s [%s]", message or "", progress or "n/a")
+    _logger.debug("%s [%s]", message or "", progress or "n/a")
     if task_progress:
         task_progress.update(message=message, percent=progress)
 
 
 @dataclass
 class S3TransferDataCB:
-    task_progress: Optional[TaskProgress]
+    task_progress: TaskProgress | None
     total_bytes_to_transfer: ByteSize
     task_progress_message_prefix: str = ""
     _total_bytes_copied: int = 0
