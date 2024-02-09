5a9dcb35819637f561822254f9ef53cdcede27c1
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-02-09 17:54:03 +0100

    defining api errors

--- api/specs/web-server/_projects_states.py
+++ api/specs/web-server/_projects_states.py
@@ -10,8 +10,10 @@ from fastapi import APIRouter, Body, Depends
 from models_library.api_schemas_webserver.projects import ProjectGet
 from models_library.generics import Envelope
 from models_library.projects_state import ProjectState
+from pydantic import ValidationError
 from servicelib.aiohttp import status
 from simcore_service_webserver._meta import API_VTAG
+from simcore_service_webserver.director_v2.exceptions import DirectorServiceError
 from simcore_service_webserver.projects._states_handlers import (
     ProjectPathParams,
     _OpenProjectQuery,
@@ -32,22 +34,36 @@ router = APIRouter(
 )
 
 
+def to_desc(exceptions: set[type[Exception]] | type[Exception]):
+    exc_classes = {exceptions} if not isinstance(exceptions, set) else exceptions
+    return ", ".join(f"{cls.__name__}" for cls in exc_classes)
+
+
 @router.post(
     "/projects/{project_id}:open",
     response_model=Envelope[ProjectGet],
     responses={
-        status.HTTP_400_BAD_REQUEST: {"description": "TODO"},
+        status.HTTP_400_BAD_REQUEST: {"description": to_desc({ValidationError})},
         status.HTTP_402_PAYMENT_REQUIRED: {
-            "description": f"{WalletNotEnoughCreditsError}"
+            "description": to_desc({WalletNotEnoughCreditsError})
         },
-        status.HTTP_403_FORBIDDEN: {"description": f"{ProjectInvalidRightsError}"},
+        status.HTTP_403_FORBIDDEN: {
+            "description": to_desc({ProjectInvalidRightsError})
+        },
         status.HTTP_404_NOT_FOUND: {
-            "description": f"{ProjectNotFoundError}, {UserDefaultWalletNotFoundError}"
+            "description": to_desc(
+                {ProjectNotFoundError, UserDefaultWalletNotFoundError}
+            )
         },
         status.HTTP_409_CONFLICT: {
-            "description": f"{ProjectTooManyProjectOpenedError}"
+            "description": to_desc({ProjectTooManyProjectOpenedError}),
         },
-        status.HTTP_422_UNPROCESSABLE_ENTITY: {"description": "request validation"},
+        status.HTTP_422_UNPROCESSABLE_ENTITY: {
+            "description": to_desc({ValidationError})
+        },
+        status.HTTP_503_SERVICE_UNAVAILABLE: {
+            "description": to_desc({DirectorServiceError})
+        },
     },
 )
 def open_project(
