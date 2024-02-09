09620afb704a5190175157d2849c1a7503439b5c
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-02-09 15:58:34 +0100

    WIP

--- api/specs/web-server/_projects_states.py
+++ api/specs/web-server/_projects_states.py
@@ -6,15 +6,23 @@
 
 from typing import Annotated
 
-from fastapi import APIRouter, Body, Depends, status
+from fastapi import APIRouter, Body, Depends
 from models_library.api_schemas_webserver.projects import ProjectGet
 from models_library.generics import Envelope
 from models_library.projects_state import ProjectState
+from servicelib import status
 from simcore_service_webserver._meta import API_VTAG
 from simcore_service_webserver.projects._states_handlers import (
     ProjectPathParams,
     _OpenProjectQuery,
 )
+from simcore_service_webserver.projects.exceptions import (
+    ProjectInvalidRightsError,
+    ProjectNotFoundError,
+    ProjectTooManyProjectOpenedError,
+)
+from simcore_service_webserver.users.exceptions import UserDefaultWalletNotFoundError
+from simcore_service_webserver.wallets.errors import WalletNotEnoughCreditsError
 
 router = APIRouter(
     prefix=f"/{API_VTAG}",
@@ -27,6 +35,20 @@ router = APIRouter(
 @router.post(
     "/projects/{project_id}:open",
     response_model=Envelope[ProjectGet],
+    responses={
+        status.HTTP_400_BAD_REQUEST: {"description": "TODO"},
+        status.HTTP_402_PAYMENT_REQUIRED: {
+            "description": f"{WalletNotEnoughCreditsError}"
+        },
+        status.HTTP_403_FORBIDDEN: {"description": f"{ProjectInvalidRightsError}"},
+        status.HTTP_404_NOT_FOUND: {
+            "description": f"{ProjectNotFoundError}, {UserDefaultWalletNotFoundError}"
+        },
+        status.HTTP_409_CONFLICT: {
+            "description": f"{ProjectTooManyProjectOpenedError}"
+        },
+        status.HTTP_422_UNPROCESSABLE_ENTITY: {"description": "request validation"},
+    },
 )
 def open_project(
     client_session_id: Annotated[str, Body(...)],
