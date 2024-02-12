8a806bf2888d3c686278411a1de4209888ddf3ee
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-02-12 09:35:49 +0100

    @sanderegg review: doc from previous PR and minor cleanup

--- packages/service-library/src/servicelib/aiohttp/client_session.py
+++ packages/service-library/src/servicelib/aiohttp/client_session.py
@@ -23,7 +23,6 @@ async def persistent_client_session(app: web.Application) -> AsyncGenerator[None
     # ANE: it is important to have fast connection handshakes
     # also requests should be as fast as possible
     # some services are not that fast to  reply
-    # Setting the time of a request using this client session to 5 seconds totals
     timeout_settings = ClientTimeout(
         total=get_http_client_request_total_timeout(),
         connect=get_http_client_request_aiohttp_connect_timeout(),
