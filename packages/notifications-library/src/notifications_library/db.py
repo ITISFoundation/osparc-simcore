6740b31f891f7fcc21fd3e8783acbbaa24b79f4b
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-02-14 16:15:32 +0100

    adds db repo

---
+++ packages/notifications-library/src/notifications_library/db.py
@@ -0,0 +1,76 @@
+import sqlalchemy as sa
+from models_library.api_schemas_webserver.wallets import PaymentID
+from models_library.products import ProductName
+from models_library.users import GroupID, UserID
+from simcore_postgres_database.models.jinja2_templates import jinja2_templates
+from simcore_postgres_database.models.payments_transactions import payments_transactions
+from simcore_postgres_database.models.products import products
+from simcore_postgres_database.models.users import users
+from sqlalchemy.ext.asyncio import AsyncEngine
+
+
+class TemplatesRepo:
+    def __init__(self, db_engine: AsyncEngine):
+        assert db_engine is not None  # nosec
+        self.db_engine = db_engine
+
+    async def _get(self, query):
+        async with self.db_engine.begin() as conn:
+            result = await conn.execute(query)
+            return result.first()
+
+    async def get_primary_group_id(self, user_id: UserID) -> GroupID:
+        if row := await self._get(
+            sa.select(
+                users.c.primary_gid,
+            ).where(users.c.id == user_id)
+        ):
+            return GroupID(row.primary_gid)
+
+        msg = f"{user_id=} not found"
+        raise ValueError(msg)
+
+    async def get_notification_data(self, user_id: UserID, payment_id: PaymentID):
+        """Retrives data that will be injected in a notification for the user on this payment"""
+        if row := await self._get(
+            sa.select(
+                payments_transactions.c.payment_id,
+                users.c.first_name,
+                users.c.last_name,
+                users.c.email,
+                products.c.name.label("product_name"),
+                products.c.display_name,
+                products.c.vendor,
+                products.c.support_email,
+            )
+            .select_from(
+                sa.join(
+                    sa.join(
+                        payments_transactions,
+                        users,
+                        payments_transactions.c.user_id == users.c.id,
+                    ),
+                    products,
+                    payments_transactions.c.product_name == products.c.name,
+                )
+            )
+            .where(
+                (payments_transactions.c.payment_id == payment_id)
+                & (payments_transactions.c.user_id == user_id)
+            )
+        ):
+            return row
+
+        msg = f"{payment_id=} for {user_id=} was not found"
+        raise ValueError(msg)
+
+    async def get_email_templates(self, names: set[str], product: ProductName):
+        # TODO: create products_to_template table and add a join here
+        async with self.db_engine.begin() as conn:
+            result = await conn.execute(
+                sa.select(
+                    jinja2_templates.c.name,
+                    jinja2_templates.c.content,
+                ).where(jinja2_templates.c.name.in_(names))
+            )
+            return result.fetchall()
