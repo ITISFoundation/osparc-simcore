456a4e52f295f7a3e25685b5c5afd52d987367e0
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2024-02-14 17:24:41 +0100

    resilient and ugly universal fix for paymentMethodAck reception

--- services/static-webserver/client/source/class/osparc/desktop/paymentMethods/PaymentMethods.js
+++ services/static-webserver/client/source/class/osparc/desktop/paymentMethods/PaymentMethods.js
@@ -96,7 +96,7 @@ qx.Class.define("osparc.desktop.paymentMethods.PaymentMethods", {
           .then(data => {
             const gatewayWindow = this.__popUpPaymentGateway(data.paymentMethodId, data.paymentMethodFormUrl);
             osparc.wrapper.WebSocket.getInstance().getSocket().once("paymentMethodAcknowledged", wsData => {
-              const {paymentMethodId} = JSON.parse(wsData);
+              const { paymentMethodId } = typeof wsData === 'string' ? JSON.parse(wsData) : wsData
               if (paymentMethodId === data.paymentMethodId) {
                 gatewayWindow.close();
                 this.__fetchPaymentMethods();
