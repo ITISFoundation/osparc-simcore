5724a49475ddd3599aeb917061f2ef3e296790b3
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-02-14 12:13:15 +0100

    skip

--- services/web/server/tests/unit/with_dbs/03/test_socketio.py
+++ services/web/server/tests/unit/with_dbs/03/test_socketio.py
@@ -54,6 +54,9 @@ def app_environment(app_environment: EnvVarsDict, monkeypatch: pytest.MonkeyPatc
     return app_environment | overrides
 
 
+@pytest.mark.skip(
+    reason="Pending https://github.com/ITISFoundation/osparc-simcore/issues/5332"
+)
 @pytest.mark.parametrize("user_role", (UserRole.USER,))
 async def test_socketio_session_client_to_server(
     logged_user: UserInfoDict,
