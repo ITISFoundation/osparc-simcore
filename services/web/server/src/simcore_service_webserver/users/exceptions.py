1a981e113c170d33fdfbf44600157e725daee1cc
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-02-22 00:10:50 +0100

    users

--- services/web/server/src/simcore_service_webserver/users/exceptions.py
+++ services/web/server/src/simcore_service_webserver/users/exceptions.py
@@ -1,33 +1,28 @@
-"""Defines the different exceptions that may arise in the projects subpackage"""
+from ..errors import WebServerBaseError
 
 
-class UsersException(Exception):
-    """Basic exception for errors raised in projects"""
-
-    def __init__(self, msg: str = None):
-        super().__init__(msg or "Unexpected error occured in projects subpackage")
+class UsersBaseError(WebServerBaseError):
+    ...
 
 
-class UserNotFoundError(UsersException):
-    """User in group was not found in DB"""
-
+class UserNotFoundError(UsersBaseError):
     def __init__(self, *, uid: int | None = None, email: str | None = None):
-        super().__init__(
-            f"User id {uid} not found" if uid else f"User with email {email} not found"
-        )
         self.uid = uid
         self.email = email
+        self.msg_template = (
+            "User id {uid} not found" if uid else f"User with email {email} not found"
+        )
 
 
-class TokenNotFoundError(UsersException):
-    """Token was not found in DB"""
+class TokenNotFoundError(UsersBaseError):
+    msg_template = "Token for service {service_id} not found"
 
     def __init__(self, service_id: str):
-        super().__init__(f"Token for service {service_id} not found")
         self.service_id = service_id
 
 
-class UserDefaultWalletNotFoundError(UsersException):
+class UserDefaultWalletNotFoundError(UsersBaseError):
+    msg_template = "Default wallet for user {uid} not found"
+
     def __init__(self, uid: int | None = None):
-        super().__init__(f"Default wallet for user {uid} not found")
         self.uid = uid
