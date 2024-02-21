b19f77bcd4c3576071783f294ce8b19f03789c15
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-02-21 23:12:04 +0100

    groups

--- services/web/server/src/simcore_service_webserver/groups/exceptions.py
+++ services/web/server/src/simcore_service_webserver/groups/exceptions.py
@@ -1,32 +1,22 @@
 """Defines the different exceptions that may arise in the projects subpackage"""
 
+from ..errors import WebServerError
 
-class GroupsError(Exception):
-    """Basic exception for errors raised in projects"""
+
+class GroupsError(WebServerError):
+    msg_template = "{msg}"
 
     def __init__(self, msg: str = None):
-        super().__init__(msg or "Unexpected error occured in projects subpackage")
+        super().__init__(msg=msg or "Unexpected error occured in projects subpackage")
 
 
 class GroupNotFoundError(GroupsError):
-    """Group was not found in DB"""
-
-    def __init__(self, gid: int):
-        super().__init__(f"Group with id {gid} not found")
-        self.gid = gid
+    msg_template = "Group with id {gid} not found"
 
 
 class UserInsufficientRightsError(GroupsError):
-    """User has not sufficient rights"""
-
-    def __init__(self, msg: str):
-        super().__init__(msg)
+    ...
 
 
 class UserInGroupNotFoundError(GroupsError):
-    """User in group was not found in DB"""
-
-    def __init__(self, gid: int, uid: int):
-        super().__init__(f"User id {uid} in Group {gid} not found")
-        self.gid = gid
-        self.uid = uid
+    msg_template = "User id {uid} in Group {gid} not found"
