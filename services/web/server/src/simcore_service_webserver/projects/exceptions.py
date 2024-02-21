2b25131491ce89d1d9b2b7f93a1d230318975586
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-02-21 23:27:23 +0100

    projects

--- services/web/server/src/simcore_service_webserver/projects/exceptions.py
+++ services/web/server/src/simcore_service_webserver/projects/exceptions.py
@@ -5,16 +5,18 @@ import redis.exceptions
 from models_library.projects import ProjectID
 from models_library.users import UserID
 
+from ..errors import WebServerError
 
-class BaseProjectError(Exception):
-    """Basic exception for errors raised in projects"""
+
+class BaseProjectError(WebServerError):
+    msg_template = "{msg}"
 
     def __init__(self, msg=None):
-        super().__init__(msg or "Unexpected error occured in projects submodule")
+        self.msg = msg or "Unexpected error occured in projects submodule"
 
-    def detailed_message(self):
+    def debug_message(self):
         # Override in subclass
-        return f"{type(self)}: {self}"
+        return f"{self.code}: {self}"
 
 
 class ProjectInvalidUsageError(BaseProjectError):
@@ -22,34 +24,31 @@ class ProjectInvalidUsageError(BaseProjectError):
 
 
 class ProjectInvalidRightsError(BaseProjectError):
-    """Invalid rights to access project"""
+    msg_template = (
+        "User {user_id} has no rights to access project with uuid {project_uuid}"
+    )
 
     def __init__(self, user_id, project_uuid):
-        super().__init__(
-            f"User {user_id} has no rights to access project with uuid {project_uuid}"
-        )
         self.user_id = user_id
         self.project_uuid = project_uuid
 
 
 class ProjectOwnerNotFoundError(BaseProjectError):
-    """Project owner was not found"""
+    msg_template = "Project with uuid {project_uuid} has no project owner"
 
     def __init__(self, project_uuid):
-        super().__init__(f"Project with uuid {project_uuid} has no project owner")
         self.project_uuid = project_uuid
 
 
 class ProjectNotFoundError(BaseProjectError):
-    """Project was not found in DB"""
+    msg_template = "Project with uuid {project_uuid} not found."
 
     def __init__(self, project_uuid, *, search_context: Any | None = None):
-        super().__init__(f"Project with uuid {project_uuid} not found.")
         self.project_uuid = project_uuid
         self.search_context_msg = f"{search_context}"
 
-    def detailed_message(self):
-        msg = f"Project with uuid {self.project_uuid}"
+    def debug_message(self):
+        msg = f"{self.code}: Project with uuid {self.project_uuid}"
         if self.search_context_msg:
             msg += f" and {self.search_context_msg}"
         msg += " was not found"
@@ -57,16 +56,17 @@ class ProjectNotFoundError(BaseProjectError):
 
 
 class ProjectDeleteError(BaseProjectError):
+    msg_template = "Failed to complete deletion of {project_uuid=}: {reason}"
+
     def __init__(self, project_uuid, reason):
-        super().__init__(f"Failed to complete deletion of {project_uuid=}: {reason}")
         self.project_uuid = project_uuid
+        self.reason = reason
 
 
 class NodeNotFoundError(BaseProjectError):
-    """Node was not found in project"""
+    msg_template = "Node {node_uuid} not found in project {project_uuid}"
 
     def __init__(self, project_uuid: str, node_uuid: str):
-        super().__init__(f"Node {node_uuid} not found in project {project_uuid}")
         self.node_uuid = node_uuid
         self.project_uuid = project_uuid
 
@@ -75,21 +75,18 @@ ProjectLockError = redis.exceptions.LockError
 
 
 class ProjectStartsTooManyDynamicNodesError(BaseProjectError):
-    """user tried to start too many nodes concurrently"""
+    msg_template = "The maximal amount of concurrently running dynamic services was reached. Please manually stop a service and retry."
 
     def __init__(self, user_id: UserID, project_uuid: ProjectID):
-        super().__init__(
-            "The maximal amount of concurrently running dynamic services was reached. Please manually stop a service and retry."
-        )
         self.user_id = user_id
         self.project_uuid = project_uuid
 
 
 class ProjectTooManyProjectOpenedError(BaseProjectError):
+    msg_template = "You cannot open more than {max_num_projects} stud{'y' if max_num_projects == 1 else 'ies'} at once. Please close another study and retry."
+
     def __init__(self, max_num_projects: int):
-        super().__init__(
-            f"You cannot open more than {max_num_projects} stud{'y' if max_num_projects == 1 else 'ies'} at once. Please close another study and retry."
-        )
+        self.max_num_projects = max_num_projects
 
 
 class PermalinkNotAllowedError(BaseProjectError):
@@ -109,12 +106,11 @@ class ProjectNodeResourcesInsufficientRightsError(BaseProjectError):
 
 
 class DefaultPricingUnitNotFoundError(BaseProjectError):
-    """Node was not found in project"""
+    msg_template = (
+        "Default pricing unit not found for node {node_uuid} in project {project_uuid}"
+    )
 
     def __init__(self, project_uuid: str, node_uuid: str):
-        super().__init__(
-            f"Default pricing unit not found for node {node_uuid} in project {project_uuid}"
-        )
         self.node_uuid = node_uuid
         self.project_uuid = project_uuid
 
