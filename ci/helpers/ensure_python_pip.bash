19b126f5b7720ed56a117d2b9019e5ecb11929b2
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-02-21 12:28:28 +0100

    revert

--- ci/helpers/ensure_python_pip.bash
+++ ci/helpers/ensure_python_pip.bash
@@ -20,7 +20,7 @@ python -m ensurepip
 
 echo "INFO:" "$(pip --version)" "@" "$(command -v pip)"
 
-uv pip install --upgrade \
+pip install --upgrade \
   pip~=$PIP_VERSION \
   wheel \
   setuptools
