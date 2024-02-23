95e03479fcc5b91adbe9c29ada85fa8174776608
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-02-23 17:19:22 +0100

    use uv on devel containers

--- services/datcore-adapter/docker/boot.sh
+++ services/datcore-adapter/docker/boot.sh
@@ -24,7 +24,8 @@ if [ "${SC_BUILD_TARGET}" = "development" ]; then
   command -v python | sed 's/^/    /'
 
   cd services/datcore-adapter || exit 1
-  pip --quiet --no-cache-dir install -r requirements/dev.txt
+  pip install uv
+  uv pip --quiet --no-cache-dir install -r requirements/dev.txt
   cd - || exit 1
   echo "$INFO" "PIP :"
   pip list | sed 's/^/    /'
