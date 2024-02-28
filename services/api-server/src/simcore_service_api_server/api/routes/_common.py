c6fea8451e4ed136fe1af7e95643f00eab459731
Mads Bisgaard <bisgaard@itis.swiss>
2024-02-28 11:42:30 +0100

    removed not needed dict

--- services/api-server/src/simcore_service_api_server/api/routes/_common.py
+++ services/api-server/src/simcore_service_api_server/api/routes/_common.py
@@ -1,17 +1,7 @@
-from typing import Any, Final
-
-from fastapi import status
-from simcore_service_api_server.models.basic_types import HTTPExceptionModel
+from typing import Final
 
 from ...core.settings import BasicSettings
 
 API_SERVER_DEV_FEATURES_ENABLED: Final[
     bool
 ] = BasicSettings.create_from_envs().API_SERVER_DEV_FEATURES_ENABLED
-
-NOT_IMPLEMENTED_STATUS_CODES: dict[int | str, dict[str, Any]] = {
-    status.HTTP_501_NOT_IMPLEMENTED: {
-        "description": "Not implemented",
-        "model": HTTPExceptionModel,
-    }
-}
