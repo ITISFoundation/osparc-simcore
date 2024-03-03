5ed5168e7e7be90de1e0b9d5c58818520478955c
matusdrobuliak66 <drobuliak66@gmail.com>
2024-03-03 14:49:19 +0100

    fix test

--- services/payments/tests/unit/api/test__one_time_payment_workflows.py
+++ services/payments/tests/unit/api/test__one_time_payment_workflows.py
@@ -10,6 +10,7 @@ from faker import Faker
 from fastapi import FastAPI, status
 from models_library.api_schemas_webserver.wallets import WalletPaymentInitiated
 from models_library.basic_types import IDStr
+from models_library.products import StripePriceID, StripeTaxRateID
 from models_library.rabbitmq_basic_types import RPCMethodName
 from models_library.users import UserID
 from models_library.wallets import WalletID
@@ -70,6 +71,8 @@ async def test_successful_one_time_payment_workflow(
     user_id: UserID,
     user_name: IDStr,
     user_email: EmailStr,
+    product_price_stripe_price_id: StripePriceID,
+    product_price_stripe_tax_rate_id: StripeTaxRateID,
     auth_headers: dict[str, str],
     payments_clean_db: None,
     mocker: MockerFixture,
@@ -94,6 +97,8 @@ async def test_successful_one_time_payment_workflow(
         user_id=user_id,
         user_name=user_name,
         user_email=user_email,
+        product_price_stripe_price_id=product_price_stripe_price_id,
+        product_price_stripe_tax_rate_id=product_price_stripe_tax_rate_id,
         timeout_s=None if is_pdb_enabled else RPC_REQUEST_DEFAULT_TIMEOUT_S,
     )
 
