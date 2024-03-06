4ba591b49c5256237d0a16045de9cbfa6220a38d
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-03-06 13:16:17 +0100

    teampltes

--- packages/notifications-library/tests/test__templates.py
+++ packages/notifications-library/tests/test__templates.py
@@ -1,9 +1,13 @@
+import pytest
 from notifications_library._templates import get_email_templates
 
 
-def test_templates_organization():
-    on_payed_templates = get_email_templates(event_name="on_payed")
-    assert set(on_payed_templates) == {
-        f"on_payed.email.{suffix}"
+@pytest.mark.parametrize("event_name", ["on_registered", "on_payed"])
+def test_get_email_templates(event_name: str):
+
+    event_templates = get_email_templates(event_name=event_name)
+
+    assert set(event_templates) == {
+        f"{event_name}.email.{suffix}"
         for suffix in ["subject.txt", "content.html", "content.txt"]
     }
