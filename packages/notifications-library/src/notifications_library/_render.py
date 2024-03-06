5b934d2a3cdd4471f0d8d2549b3ff3872610f7dd
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-03-06 12:30:05 +0100

    render

--- packages/notifications-library/src/notifications_library/_render.py
+++ packages/notifications-library/src/notifications_library/_render.py
@@ -1,9 +1,10 @@
 import logging
 from dataclasses import dataclass
 from email.headerregistry import Address
-from typing import Any
+from typing import Any, NamedTuple
 
 from jinja2 import Environment
+from jinja2.exceptions import TemplateNotFound
 from models_library.products import ProductName
 
 _logger = logging.getLogger(__name__)
@@ -24,6 +25,14 @@ class ProductData:
     support_email: str
 
 
+class EmailPartsTuple(NamedTuple):
+    from_: Address
+    to: Address
+    suject: str
+    text_content: str
+    html_content: str | None
+
+
 def render_email_parts(
     env: Environment,
     event_name: str,
@@ -31,7 +40,7 @@ def render_email_parts(
     user: UserData,
     product: ProductData,
     extra: dict[str, Any] | None = None,
-) -> tuple:
+) -> EmailPartsTuple:
     from_ = Address(
         display_name=f"{product.display_name} support",
         addr_spec=product.support_email,
@@ -50,7 +59,11 @@ def render_email_parts(
     text_template = env.get_template(f"{event_name}.email.txt")
     text_content = text_template.render(data)
 
-    html_template = env.get_template(f"{event_name}.email.html")
-    html_content = html_template.render(data)
+    try:
+        html_template = env.get_template(f"{event_name}.email.html")
+        html_content = html_template.render(data)
+    except TemplateNotFound as err:
+        _logger.debug("Event %s has no html template: %s", event_name, err)
+        html_content = None
 
-    return (from_, to, subject, text_content, html_content)
+    return EmailPartsTuple(from_, to, subject, text_content, html_content)
