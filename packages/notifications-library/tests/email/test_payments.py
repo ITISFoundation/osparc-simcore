761d70be13e117229da0f7afbb64252066c69474
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-03-07 18:47:09 +0100

    tests on_register

--- packages/notifications-library/tests/email/test_payments.py
+++ packages/notifications-library/tests/email/test_payments.py
@@ -3,6 +3,7 @@
 # pylint: disable=unused-variable
 # pylint: disable=too-many-arguments
 
+from email.message import EmailMessage
 from pathlib import Path
 from unittest.mock import AsyncMock, MagicMock
 
@@ -22,6 +23,20 @@ from pytest_simcore.helpers.typing_env import EnvVarsDict
 from settings_library.email import SMTPSettings
 
 
+async def _send_and_assert(msg: EmailMessage, smtp_mock_or_none: MagicMock | None):
+    settings = SMTPSettings.create_from_envs()
+
+    async with create_email_session(settings) as smtp:
+        await smtp.send_message(msg)
+
+    # check email was sent
+    if smtp_mock_or_none:
+        assert smtp_mock_or_none.called
+        assert isinstance(smtp, AsyncMock)
+        assert smtp.login.called
+        assert smtp.send_message.called
+
+
 async def test_on_payed_event(
     app_environment: EnvVarsDict,
     tmp_path: Path,
@@ -39,14 +54,11 @@ async def test_on_payed_event(
         > pytest --external-user-email=me@email.me --external-envfile=.myenv -k test_send_email_workflow  --pdb tests/unit
     """
 
-    settings = SMTPSettings.create_from_envs()
-    env = create_render_env_from_package()
-
     assert user_data.email == user_email
     assert product_data.product_name == product_name
 
     parts = render_email_parts(
-        env,
+        env=create_render_env_from_package(),
         event_name="on_payed",
         user=user_data,
         product=product_data,
@@ -63,12 +75,28 @@ async def test_on_payed_event(
     attachment.write_text(faker.text())
     add_attachments(msg, [attachment])
 
-    async with create_email_session(settings) as smtp:
-        await smtp.send_message(msg)
+    await _send_and_assert(msg, smtp_mock_or_none)
 
-    # check email was sent
-    if smtp_mock_or_none:
-        assert smtp_mock_or_none.called
-        assert isinstance(smtp, AsyncMock)
-        assert smtp.login.called
-        assert smtp.send_message.called
+
+async def test_on_registered_event(
+    app_environment: EnvVarsDict,
+    tmp_path: Path,
+    faker: Faker,
+    user_email: EmailStr,
+    product_name: ProductName,
+    smtp_mock_or_none: MagicMock | None,
+    user_data: UserData,
+    product_data: ProductData,
+):
+
+    parts = render_email_parts(
+        env=create_render_env_from_package(),
+        event_name="on_registered",
+        user=user_data,
+        product=product_data,
+        # extras
+        host=faker.url(),
+        link=faker.url(),
+    )
+
+    await _send_and_assert(compose_email(*parts), smtp_mock_or_none)
