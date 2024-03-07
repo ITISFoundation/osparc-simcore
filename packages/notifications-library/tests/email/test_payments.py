601f20f10f2758e0f156e75ed02c1b888a783ebc
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-03-07 18:14:26 +0100

    refactor tests

--- packages/notifications-library/tests/email/test_payments.py
+++ packages/notifications-library/tests/email/test_payments.py
@@ -6,7 +6,6 @@
 from pathlib import Path
 from unittest.mock import AsyncMock, MagicMock
 
-import pytest
 from faker import Faker
 from models_library.products import ProductName
 from notifications_library._email import (
@@ -19,38 +18,11 @@ from notifications_library._models import ProductData, UserData
 from notifications_library._render import create_render_env_from_package
 from notifications_library.payments import PaymentData
 from pydantic import EmailStr
-from pytest_mock import MockerFixture
 from pytest_simcore.helpers.typing_env import EnvVarsDict
-from pytest_simcore.helpers.utils_envs import setenvs_from_dict
 from settings_library.email import SMTPSettings
 
 
-@pytest.fixture
-def app_environment(
-    monkeypatch: pytest.MonkeyPatch,
-    env_devel_dict: EnvVarsDict,
-    external_environment: EnvVarsDict,
-) -> EnvVarsDict:
-    return setenvs_from_dict(
-        monkeypatch,
-        {
-            **env_devel_dict,
-            **external_environment,
-        },
-    )
-
-
-@pytest.fixture
-def smtp_mock_or_none(
-    mocker: MockerFixture, external_user_email: EmailStr | None
-) -> MagicMock | None:
-    if not external_user_email:
-        return mocker.patch("notifications_library._email.SMTP")
-    print("🚨 Emails might be sent to", external_user_email)
-    return None
-
-
-async def test_send_email_workflow(
+async def test_on_payed_event(
     app_environment: EnvVarsDict,
     tmp_path: Path,
     faker: Faker,
@@ -94,6 +66,7 @@ async def test_send_email_workflow(
     async with create_email_session(settings) as smtp:
         await smtp.send_message(msg)
 
+    # check email was sent
     if smtp_mock_or_none:
         assert smtp_mock_or_none.called
         assert isinstance(smtp, AsyncMock)
