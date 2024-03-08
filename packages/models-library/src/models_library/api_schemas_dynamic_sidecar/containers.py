ba0d4b6bdf5d3b0835b5a756f72f9456fc61f817
Andrei Neagu <neagu@itis.swiss>
2024-03-08 09:28:29 +0100

    refactor

--- packages/models-library/src/models_library/api_schemas_dynamic_sidecar/containers.py
+++ packages/models-library/src/models_library/api_schemas_dynamic_sidecar/containers.py
@@ -1,10 +1,18 @@
-from typing import TypeAlias
+from typing import Any, ClassVar, TypeAlias
 
 from pydantic import BaseModel, NonNegativeFloat
 
 
 class ActivityInfo(BaseModel):
     seconds_inactive: NonNegativeFloat
+
+    class Config:
+        schema_extra: ClassVar[dict[str, Any]] = {
+            "examples": [
+                {"seconds_inactive": 0},
+                {"seconds_inactive": 100},
+            ]
+        }
 
 
 ActivityInfoOrNone: TypeAlias = ActivityInfo | None
