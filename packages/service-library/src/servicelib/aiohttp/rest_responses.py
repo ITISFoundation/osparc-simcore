38bbfb697c85cac8a251fb3851cc554ffe14f43a
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-03-14 14:58:10 +0100

    minor rename

--- packages/service-library/src/servicelib/aiohttp/rest_responses.py
+++ packages/service-library/src/servicelib/aiohttp/rest_responses.py
@@ -13,7 +13,7 @@ from servicelib.aiohttp.status import HTTP_200_OK
 
 from ..json_serialization import json_dumps
 from ..mimetype_constants import MIMETYPE_APPLICATION_JSON
-from .rest_models import ErrorItem, ResponseErrorBody
+from .rest_models import ErrorDetail, ResponseErrorBody
 
 _ENVELOPE_KEYS = ("data", "error")
 
@@ -74,7 +74,7 @@ def create_data_response(data: Any, *, status=HTTP_200_OK) -> web.Response:
 
 
 def create_error_response(
-    errors: list[Exception] | Exception,
+    errors: Exception | list[Exception] | None,
     message: str | None = None,
     http_error_cls: type[HTTPError] = web.HTTPInternalServerError,
 ) -> HTTPError:
@@ -83,18 +83,17 @@ def create_error_response(
     - Can skip internal details when 500 status e.g. to avoid transmitting server
     exceptions to the client in production
     """
-    if not isinstance(errors, list):
+    if isinstance(errors, Exception):
         errors = [errors]
 
-    if message is None:
-        message = HTTPStatus(http_error_cls.status_code).description
+    errors = errors or []
 
     text: str | None = None
     if not http_error_cls.empty_body:
         error = ResponseErrorBody(
             status=http_error_cls.status_code,
-            message=message,
-            errors=[ErrorItem.from_error(e) for e in errors],
+            message=message or HTTPStatus(http_error_cls.status_code).description,
+            errors=[ErrorDetail.from_exception(e) for e in errors],
         )
         text = json_dumps(wrap_as_envelope(error=asdict(error)))
 
