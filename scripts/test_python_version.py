e4165cb0ae01715eeb9edf74ba180b934f6ac22f
Werner Van Geit <werner.vangeit@epfl.ch>
2024-03-18 15:56:25 +0100

    Get python version from the requirements file in test directory

--- scripts/test_python_version.py
+++ scripts/test_python_version.py
@@ -1,5 +1,6 @@
 #!/bin/env python
 
+import pathlib as pl
 import platform
 import sys
 
@@ -9,14 +10,29 @@ def main():
 
     print(f"Found python version: {major_v}.{minor_v}.{patch_v}")
 
+    min_major_v, min_minor_v = to_version(
+        (pl.Path(__file__).parent.parent / "requirements" / "PYTHON_VERSION")
+        .read_text()
+        .strip()
+    )
+
     exit_code = (
-        1 if int(major_v) < 2 or (int(major_v) == 3 and int(minor_v) < 10) else 0
+        1
+        if int(major_v) < min_major_v
+        or (int(major_v) == min_major_v and int(minor_v) < min_minor_v)
+        else 0
     )
 
     if exit_code > 0:
-        print("Wrong python version, osparc compilation needs at least Python 3.10")
+        print(
+            f"Wrong python version, osparc compilation needs at least Python {min_major_v}.{min_minor_v}"
+        )
 
     sys.exit(exit_code)
+
+
+def to_version(version):
+    return tuple(int(v) for v in version.split("."))
 
 
 if __name__ == "__main__":
