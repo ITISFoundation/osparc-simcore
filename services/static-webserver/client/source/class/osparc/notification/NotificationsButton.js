3369ff06fe916d9adcb6936a9d143e43b2441888
Odei Maiz <maiz@itis.swiss>
2024-03-18 16:35:17 +0100

    minor

--- services/static-webserver/client/source/class/osparc/notification/NotificationsButton.js
+++ services/static-webserver/client/source/class/osparc/notification/NotificationsButton.js
@@ -85,7 +85,6 @@ qx.Class.define("osparc.notification.NotificationsButton", {
       const notifications = osparc.notification.Notifications.getInstance().getNotifications();
       notifications.forEach(notification => notification.addListener("changeRead", () => this.__updateButton(), this));
 
-
       const nUnreadNotifications = notifications.filter(notification => notification.getRead() === false).length;
       const icon = this.getChildControl("icon");
       icon.set({
