49b37fc89946cfaf88af379d88ea721ee477a000
Odei Maiz <maiz@itis.swiss>
2024-03-21 16:55:12 +0100

    update_routes too

--- services/static-webserver/client/scripts/post-compile.py
+++ services/static-webserver/client/scripts/post-compile.py
@@ -1,53 +1,20 @@
 import os
+import json
 
+
+output_folders = [
+    "source-output", # dev output
+    "build-output",  # default production output
+    "build-client"   # I believe we create the production outputs here
+]
 
 def update_apps_metadata():
-    applications = [
-        {
-            "application": "osparc",
-             "replacements": {
-                "replace_me_og_title": "oSPARC",
-                "replace_me_og_description": "open online simulations for Stimulating Peripheral Activity to Relieve Conditions",
-                "replace_me_og_image": "https://raw.githubusercontent.com/ITISFoundation/osparc-simcore/master/services/static-webserver/client/source/resource/osparc/favicon-osparc.png"
-            },
-        }, {
-            "application": "s4l",
-            "replacements": {
-                "replace_me_og_title": "Sim4Life",
-                "replace_me_og_description": "Computational life sciences platform that combines computable human phantoms, powerful physics solvers and advanced tissue models.",
-                "replace_me_og_image": "https://raw.githubusercontent.com/ZurichMedTech/s4l-assets/main/app/full/background-images/S4L/Sim4Life-head-default.png"
-            }
-        }, {
-            "application": "s4lacad",
-            "replacements": {
-                "replace_me_og_title": "Sim4Life Science",
-                "replace_me_og_description": "Sim4Life for Science - Computational life sciences platform that combines computable human phantoms, powerful physics solvers and advanced tissue models.",
-                "replace_me_og_image": "https://raw.githubusercontent.com/ZurichMedTech/s4l-assets/main/app/full/background-images/S4L/Sim4Life-head-academy.png"
-            }
-        }, {
-            "application": "s4llite",
-            "replacements": {
-                "replace_me_og_title": "S4L Lite",
-                "replace_me_og_description": "Sim4Life for Students - Computational life sciences platform that combines computable human phantoms, powerful physics solvers and advanced tissue models.",
-                "replace_me_og_image": "https://raw.githubusercontent.com/ZurichMedTech/s4l-assets/main/app/full/background-images/S4L/Sim4Life-head-lite.png"
-            }
-        }, {
-            "application": "tis",
-            "replacements": {
-                "replace_me_og_title": "TI Plan - IT'IS",
-                "replace_me_og_description": "A tool powered by o²S²PARC technology that reduces optimization of targeted neurostimulation protocols.",
-                "replace_me_og_image": "https://raw.githubusercontent.com/ITISFoundation/osparc-simcore/master/services/static-webserver/client/source/resource/osparc/tip_splitimage.png"
-            }
-        }
-    ]
-
-    output_folders = [
-        "source-output", # dev output
-        "build-output",  # default production output
-        "build-client"   # I believe we create the production outputs here
-    ]
-
     dirname = os.path.dirname(__file__)
+    meta_filename = os.path.join(dirname, "apps_metadata.json")
+    f = open(meta_filename)
+    metadata = json.load(f)
+    applications = metadata["applications"]
+
     for i in applications:
         application = i.get("application")
         for output_folder in output_folders:
@@ -59,12 +26,35 @@ def update_apps_metadata():
                 replacements = i.get("replacements")
                 for key in replacements:
                     replace_text = replacements[key]
-                    data = data.replace(key, replace_text) 
-
+                    data = data.replace(key, replace_text)
             with open(filename, "w") as file: 
                 print(f"Updating app metadata: {filename}")
                 file.write(data)
 
 
+def update_routes():
+    dirname = os.path.dirname(__file__)
+    meta_filename = os.path.join(dirname, "apps_metadata.json")
+    f = open(meta_filename)
+    metadata = json.load(f)
+    applications = metadata["applications"]
+
+    for i in applications:
+        application = i.get("application")
+        for output_folder in output_folders:
+            filename = os.path.join(dirname, '..', output_folder, application, "index.html")
+            if not os.path.isfile(filename):
+                continue
+            with open(filename, "r") as file:
+                data = file.read()
+                # fixes relative paths
+                data = data.replace(f'href="../../resource/{application}', f'href="../resource/{application}')
+                data = data.replace('src="boot.js"', f'src="{application}/boot.js"')
+            with open(filename, "w") as file: 
+                print(f"Updating routes: {filename}")
+                file.write(data)
+
+
 if __name__ == "__main__":
     update_apps_metadata()
+    update_routes()
