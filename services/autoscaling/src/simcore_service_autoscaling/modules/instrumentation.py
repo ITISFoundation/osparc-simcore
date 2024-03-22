eac86eed6c0b7c7204c66c4d2de569ac62c368b1
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-03-22 08:23:46 +0100

    use app name and sybsystem

--- services/autoscaling/src/simcore_service_autoscaling/modules/instrumentation.py
+++ services/autoscaling/src/simcore_service_autoscaling/modules/instrumentation.py
@@ -7,10 +7,12 @@ from servicelib.fastapi.prometheus_instrumentation import (
     setup_prometheus_instrumentation as setup_rest_instrumentation,
 )
 
+from .._meta import APP_NAME
 from ..core.errors import ConfigurationError
 from ..core.settings import get_application_settings
 from ..models import AssociatedInstance, Cluster, NonAssociatedInstance
 
+METRICS_NAMESPACE: Final[str] = APP_NAME
 EC2_INSTANCE_LABELS: Final[tuple[str]] = ("instance_type",)
 
 
@@ -27,6 +29,7 @@ def _update_gauge(
 @dataclass(slots=True, kw_only=True)
 class AutoscalingInstrumentation:
     registry: CollectorRegistry
+    subsystem: str
     _active_nodes: Gauge = field(init=False)
     _pending_nodes: Gauge = field(init=False)
     _drained_nodes: Gauge = field(init=False)
@@ -41,40 +44,56 @@ class AutoscalingInstrumentation:
             "active_nodes",
             "Number of EC2-backed docker nodes which are active and ready to run tasks",
             labelnames=EC2_INSTANCE_LABELS,
+            namespace=METRICS_NAMESPACE,
+            subsystem=self.subsystem,
         )
         self._pending_nodes = Gauge(
             "pending_nodes",
             "Number of EC2-backed docker nodes which are active and NOT ready to run tasks",
             labelnames=EC2_INSTANCE_LABELS,
+            namespace=METRICS_NAMESPACE,
+            subsystem=self.subsystem,
         )
         self._drained_nodes = Gauge(
             "drained_nodes",
             "Number of EC2-backed docker nodes which are drained",
             labelnames=EC2_INSTANCE_LABELS,
+            namespace=METRICS_NAMESPACE,
+            subsystem=self.subsystem,
         )
         self._buffer_drained_nodes = Gauge(
             "buffer_drained_nodes",
             "Number of EC2-backed docker nodes which are drained and in buffer/reserve",
             labelnames=EC2_INSTANCE_LABELS,
+            namespace=METRICS_NAMESPACE,
+            subsystem=self.subsystem,
         )
         self._pending_ec2s = Gauge(
             "pending_ec2s",
             "Number of EC2 instance not yet part of the cluster",
             labelnames=EC2_INSTANCE_LABELS,
+            namespace=METRICS_NAMESPACE,
+            subsystem=self.subsystem,
         )
         self._disconnected_nodes = Gauge(
             "disconnected_nodes",
             "Number of docker node not backed by a running EC2 instance",
+            namespace=METRICS_NAMESPACE,
+            subsystem=self.subsystem,
         )
         self._started_instances = Counter(
             "started_instances_total",
             "Number of EC2 instances that were started",
             labelnames=EC2_INSTANCE_LABELS,
+            namespace=METRICS_NAMESPACE,
+            subsystem=self.subsystem,
         )
         self._terminated_instances = Counter(
             "terminated_ec2_instances_total",
             "Number of EC2 instances that were terminated",
             labelnames=EC2_INSTANCE_LABELS,
+            namespace=METRICS_NAMESPACE,
+            subsystem=self.subsystem,
         )
 
     def update_from_cluster(self, cluster: Cluster) -> None:
@@ -101,8 +120,11 @@ def setup(app: FastAPI) -> None:
     instrumentator = setup_rest_instrumentation(app)
 
     async def on_startup() -> None:
+        metrics_subsystem = (
+            "dynamic" if app_settings.AUTOSCALING_NODES_MONITORING else "computational"
+        )
         app.state.instrumentation = AutoscalingInstrumentation(
-            registry=instrumentator.registry
+            registry=instrumentator.registry, subsystem=metrics_subsystem
         )
 
     async def on_shutdown() -> None:
