d93cba7ed96ae1a765fb7c01728ac2e2c4791e21
Odei Maiz <maiz@itis.swiss>
2024-03-22 11:28:13 +0100

    extend metadata test

--- tests/e2e/tests/title.test.js
+++ tests/e2e/tests/title.test.js
@@ -1,8 +1,24 @@
+const appMetadata = require('../../../services/static-webserver/client/scripts/apps_metadata.json')
+
 beforeAll(async () => {
   await page.goto(url);
 }, ourTimeout);
 
 test('Check site title', async () => {
+  // osparc ([¨0]) is the product served by default
+  const replacements = appMetadata["applications"][0]["replacements"];
+
   const title = await page.title();
-  expect(title).toBe('oSPARC');
+  expect(title).toBe(replacements["replace_me_og_title"]);
+
+  const desc = document.querySelectorAll("head > meta[name='description']")[0].content;
+  expect(desc).toBe(replacements["replace_me_og_description"]);
+
+  // Open Graph metadata
+  const ogTitle = document.querySelectorAll("head > meta[property='og:title']")[0].content;
+  expect(ogTitle).toBe(replacements["replace_me_og_title"]);
+
+  const ogDescription = document.querySelectorAll("head > meta[property='og:description']")[0].content;
+  expect(ogDescription).toBe(replacements["replace_me_og_description"]);
+
 }, 20000);
