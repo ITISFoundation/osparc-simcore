e76c15ec246fa2d529d3b9f7ab3de5b1e008f604
Odei Maiz <maiz@itis.swiss>
2024-03-22 18:14:07 +0100

    fix tests

--- tests/e2e/tests/title.test.js
+++ tests/e2e/tests/title.test.js
@@ -1,7 +1,3 @@
-/**
- * @jest-environment jsdom
- */
-
 const appMetadata = require('../../../services/static-webserver/client/scripts/apps_metadata.json')
 
 beforeAll(async () => {
@@ -10,19 +6,25 @@ beforeAll(async () => {
 
 test('Check site title', async () => {
   const title = await page.title();
-  expect(title).toBe("oSPARC");
+  expect(title).toBe("oSPARC (master)");
   
   // oSPARC ([0]) is the product served by default
   const replacements = appMetadata["applications"][0]["replacements"];
 
-  const description = document.querySelectorAll("head > meta[name='description']")[0].content;
+  const description = await page.$$eval("head > meta[name='description']", descriptions => {
+    return descriptions[0].content;
+  });
   expect(description).toBe(replacements["replace_me_og_description"]);
 
   // Open Graph metadata
-  const ogTitle = document.querySelectorAll("head > meta[property='og:title']")[0].content;
+  const ogTitle = await page.$$eval("head > meta[property='og:title']", ogTitles => {
+    return ogTitles[0].content;
+  });
   expect(ogTitle).toBe(replacements["replace_me_og_title"]);
 
-  const ogDescription = document.querySelectorAll("head > meta[property='og:description']")[0].content;
+  const ogDescription = await page.$$eval("head > meta[property='og:description']", ogDescriptions => {
+    return ogDescriptions[0].content;
+  });
   expect(ogDescription).toBe(replacements["replace_me_og_description"]);
 
 }, 20000);
