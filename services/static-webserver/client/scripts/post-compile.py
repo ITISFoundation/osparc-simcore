c02e78ea77c8d382ccb75534837a7b2487e9a938
Odei Maiz <maiz@itis.swiss>
2024-03-22 12:20:35 +0100

    undo

--- services/static-webserver/client/scripts/post-compile.py
+++ services/static-webserver/client/scripts/post-compile.py
@@ -37,25 +37,5 @@ def update_apps_metadata():
                 file.write(data)
 
 
-def update_routes():
-    dirname = os.path.dirname(__file__)
-    applications = read_json_file("apps_metadata.json")
-    for i in applications:
-        application = i.get("application")
-        for output_folder in output_folders:
-            filename = os.path.join(dirname, '..', output_folder, application, "index.html")
-            if not os.path.isfile(filename):
-                continue
-            with open(filename, "r") as file:
-                data = file.read()
-                # fixes relative paths
-                data = data.replace(f'href="../../resource/{application}', f'href="../resource/{application}')
-                data = data.replace('src="boot.js"', f'src="{application}/boot.js"')
-            with open(filename, "w") as file: 
-                print(f"Updating routes: {filename}")
-                file.write(data)
-
-
 if __name__ == "__main__":
     update_apps_metadata()
-    update_routes()
