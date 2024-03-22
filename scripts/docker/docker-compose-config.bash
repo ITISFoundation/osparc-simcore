423b04c73ce1e5804cd9dbac20c0a74b1a295347
kaiser <kaiser@itis.swiss>
2024-03-22 16:35:04 +0100

    Phase out docker compose v1, update docker-compose-configs.bash

--- scripts/docker/docker-compose-config.bash
+++ scripts/docker/docker-compose-config.bash
@@ -17,15 +17,11 @@ show_error() {
 
 
 env_file=".env"
-project_directory=""
 # Parse command line arguments
-while getopts ":e:p:" opt; do
+while getopts ":e:" opt; do
   case $opt in
     e)
       env_file="$OPTARG"
-      ;;
-    p)
-      project_directory="$OPTARG"
       ;;
     \?)
       show_error "Invalid option: -$OPTARG"
@@ -61,55 +57,29 @@ if docker compose version --short | grep --quiet "^2\." ; then
   if [[ -z "$version" ]]; then
     version="3.9"  # Default to 3.9 if version is not found in file
   fi
-
+# shellcheck disable=SC2002
   docker_command="\
-docker \
---log-level=ERROR \
-compose \
---env-file ${env_file}"
-
-  if [ "$project_directory" ]; then
-    docker_command+=" --project-directory ${project_directory}"
-  fi
+set -o allexport && \
+. ${env_file} && set +o allexport && \
+docker stack config"
 
   for compose_file_path in "$@"
   do
-    docker_command+=" --file=${compose_file_path}"
+    docker_command+=" --compose-file ${compose_file_path}"
   done
-  docker_command+="\
- config \
+
+  #docker_command+=" --skip-interpolation"
+
+  docker_command+=" \
 | sed '/published:/s/\"//g' \
 | sed '/size:/s/\"//g' \
-| sed '1 { /name:.*/d ; }' \
-| sed '1 i version: \"${version}\"' \
-| sed --regexp-extended 's/cpus: ([0-9\\.]+)/cpus: \"\\1\"/'"
+| sed --regexp-extended 's/cpus: ([0-9\\.]+)/cpus: \"\\1\"/' \
+| sed 's/\\\$/\\\$\\\$/g'" # mitigates https://stackoverflow.com/questions/40619582/how-can-i-escape-a-dollar-sign-in-a-docker-compose-file
 
   # Execute the command
   show_info "Executing Docker command: ${docker_command}"
-  eval ${docker_command}
+  eval "${docker_command}"
 else
-  show_warning "docker compose V2 is not available, trying V1 instead... please update your docker engine."
-  if docker-compose version --short | grep --quiet "^1\." ; then
-    show_info "Running compose V1"
-    docker_command="\
-docker-compose \
---log-level=ERROR \
---env-file ${env_file}"
-    for compose_file_path in "$@"
-    do
-      docker_command+=" --file=${compose_file_path} "
-    done
-    if [ "$project_directory" ]; then
-      docker_command+=" --project-directory ${project_directory}"
-    fi
-    docker_command+=" \
-config \
-| sed --regexp-extended 's/cpus: ([0-9\\.]+)/cpus: \"\\1\"/'"
-    # Execute the command
-    show_info "Executing Docker command: ${docker_command}"
-    eval ${docker_command}
-  else
-    show_error "docker-compose V1 is not available. It is impossible to run this script!"
-    exit 1
-  fi
+  show_warning "docker compose V2 is not available... please update your docker engine."
+  exit 1
 fi
