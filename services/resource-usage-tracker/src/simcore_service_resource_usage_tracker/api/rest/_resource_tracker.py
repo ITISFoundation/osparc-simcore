19e80327eb324333a329883951cd4cd789faaf86
matusdrobuliak66 <drobuliak66@gmail.com>
2024-03-26 15:48:38 +0100

    tools+tests+base upgrades in resource-usage-tracker

--- services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/api/rest/_resource_tracker.py
+++ services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/api/rest/_resource_tracker.py
@@ -1,8 +1,7 @@
 import logging
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Query, status
-from fastapi_pagination.api import create_page
+from fastapi import APIRouter, Depends, status
 from models_library.api_schemas_resource_usage_tracker.credit_transactions import (
     CreditTransactionCreated,
     WalletTotalCredits,
@@ -11,72 +10,18 @@ from models_library.api_schemas_resource_usage_tracker.pricing_plans import (
     PricingPlanGet,
     PricingUnitGet,
 )
-from models_library.api_schemas_resource_usage_tracker.service_runs import (
-    ServiceRunGet,
-    ServiceRunPage,
-)
-from models_library.products import ProductName
 from models_library.resource_tracker import CreditTransactionId
-from models_library.users import UserID
-from models_library.wallets import WalletID
-from simcore_service_resource_usage_tracker.api.rest.dependencies import get_repository
-from simcore_service_resource_usage_tracker.modules.db.repositories.resource_tracker import (
-    ResourceTrackerRepository,
-)
 
-from ...models.pagination import LimitOffsetPage, LimitOffsetParamsWithDefault
 from ...services import (
     resource_tracker_credit_transactions,
     resource_tracker_pricing_plans,
     resource_tracker_pricing_units,
-    resource_tracker_service_runs,
 )
 
 _logger = logging.getLogger(__name__)
 
 
 router = APIRouter()
-
-
-########
-# USAGE
-########
-
-
-@router.get(
-    "/services/-/usages",
-    response_model=LimitOffsetPage[ServiceRunGet],
-    operation_id="list_usage_services",
-    description="Returns a list of tracked containers for a given user and product",
-    tags=["usages"],
-)
-async def list_usage_services(
-    page_params: Annotated[LimitOffsetParamsWithDefault, Depends()],
-    user_id: UserID,
-    product_name: ProductName,
-    resource_tracker_repo: Annotated[
-        ResourceTrackerRepository, Depends(get_repository(ResourceTrackerRepository))
-    ],
-    wallet_id: Annotated[WalletID | None, Query()] = None,
-    access_all_wallet_usage: Annotated[bool, Query()] = False,
-):
-    usage_services_page: ServiceRunPage = (
-        await resource_tracker_service_runs.list_service_runs(
-            user_id=user_id,
-            product_name=product_name,
-            resource_tracker_repo=resource_tracker_repo,
-            limit=page_params.limit,
-            offset=page_params.offset,
-            wallet_id=wallet_id,
-            access_all_wallet_usage=access_all_wallet_usage,
-        )
-    )
-
-    return create_page(
-        usage_services_page.items,
-        total=usage_services_page.total,
-        params=page_params,
-    )
 
 
 ######################
