ee6fd1507d92709384e18f12886d58c637ab2034
Odei Maiz <maiz@itis.swiss>
2024-03-28 16:00:50 +0100

    minor

--- services/static-webserver/client/source/class/osparc/data/Permissions.js
+++ services/static-webserver/client/source/class/osparc/data/Permissions.js
@@ -285,15 +285,15 @@ qx.Class.define("osparc.data.Permissions", {
     },
 
     isTester: function() {
-      return ["tester", "product_owner", "admin"].includes(this.getRole());
+      return ["admin", "product_owner", "tester"].includes(this.getRole());
     },
 
     isProductOwner: function() {
-      return this.getRole() === "product_owner";
+      return ["admin", "product_owner"].includes(this.getRole());
     },
 
     isAdmin: function() {
-      return this.getRole() === "admin";
+      return ["admin"].includes(this.getRole());
     },
   }
 });
