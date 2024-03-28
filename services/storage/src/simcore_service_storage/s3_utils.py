08199c03b6fc223c387e070acb9601285c9631ef
matusdrobuliak66 <drobuliak66@gmail.com>
2024-03-28 14:03:40 +0100

    removing commented code

--- services/storage/src/simcore_service_storage/s3_utils.py
+++ services/storage/src/simcore_service_storage/s3_utils.py
@@ -43,45 +43,6 @@ def compute_num_file_chunks(file_size: ByteSize) -> tuple[int, ByteSize]:
     )
 
 
-# def s3_exception_handler(log: logging.Logger):
-#     """converts typical aiobotocore/boto exceptions to storage exceptions
-#     NOTE: this is a work in progress as more exceptions might arise in different
-#     use-cases
-#     """
-
-#     def decorator(func):
-#         @functools.wraps(func)
-#         async def wrapper(self, *args, **kwargs):
-#             try:
-#                 response = await func(self, *args, **kwargs)
-#             except self.client.exceptions.NoSuchBucket as exc:
-#                 raise S3BucketInvalidError(
-#                     bucket=exc.response.get("Error", {}).get("BucketName", "undefined")
-#                 ) from exc
-#             except botocore_exc.ClientError as exc:
-#                 if exc.response.get("Error", {}).get("Code") == "404":
-#                     if exc.operation_name == "HeadObject":
-#                         raise S3KeyNotFoundError(bucket=args[0], key=args[1]) from exc
-#                     if exc.operation_name == "HeadBucket":
-#                         raise S3BucketInvalidError(bucket=args[0]) from exc
-#                 if exc.response.get("Error", {}).get("Code") == "403":
-#                     if exc.operation_name == "HeadBucket":
-#                         raise S3BucketInvalidError(bucket=args[0]) from exc
-#                 raise S3AccessError from exc
-#             except botocore_exc.EndpointConnectionError as exc:
-#                 raise S3AccessError from exc
-
-#             except botocore_exc.BotoCoreError as exc:
-#                 log.exception("Unexpected error in s3 client: ")
-#                 raise S3AccessError from exc
-
-#             return response
-
-#         return wrapper
-
-#     return decorator
-
-
 def update_task_progress(
     task_progress: TaskProgress | None,
     message: ProgressMessage | None = None,
