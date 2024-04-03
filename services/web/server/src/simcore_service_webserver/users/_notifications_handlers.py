ab7416e74291d83e75d5a0ff8aad81e111378bd8
Odei Maiz <maiz@itis.swiss>
2024-04-03 18:34:55 +0200

    filter by product

--- services/web/server/src/simcore_service_webserver/users/_notifications_handlers.py
+++ services/web/server/src/simcore_service_webserver/users/_notifications_handlers.py
@@ -11,6 +11,7 @@ from servicelib.mimetype_constants import MIMETYPE_APPLICATION_JSON
 
 from .._meta import API_VTAG
 from ..login.decorators import login_required
+from ..products.api import get_product_name
 from ..redis import get_redis_user_notifications_client
 from ..security.decorators import permission_required
 from ..utils_aiohttp import envelope_json_response
@@ -33,13 +34,24 @@ routes = web.RouteTableDef()
 
 
 async def _get_user_notifications(
-    redis_client: aioredis.Redis, user_id: int
+    redis_client: aioredis.Redis, user_id: int, product_name: str
 ) -> list[UserNotification]:
     """returns a list of notifications where the latest notification is at index 0"""
     raw_notifications: list[str] = await redis_client.lrange(
         get_notification_key(user_id), -1 * MAX_NOTIFICATIONS_FOR_USER_TO_SHOW, -1
     )
-    return [UserNotification.parse_raw(x) for x in raw_notifications]
+    notifications = [UserNotification.parse_raw(x) for x in raw_notifications]
+    # filter by product
+    print(product_name)
+    print(notifications)
+    def filter_by_product(n):
+        if "product" in n:
+            return n["product"] == product_name
+        else:
+            return True
+    filtered_notifications = filter(filter_by_product, notifications)
+    print(filtered_notifications)
+    return filtered_notifications
 
 
 @routes.get(f"/{API_VTAG}/me/notifications", name="list_user_notifications")
@@ -48,7 +60,8 @@ async def _get_user_notifications(
 async def list_user_notifications(request: web.Request) -> web.Response:
     redis_client = get_redis_user_notifications_client(request.app)
     req_ctx = UsersRequestContext.parse_obj(request)
-    notifications = await _get_user_notifications(redis_client, req_ctx.user_id)
+    product_name = get_product_name(request)
+    notifications = await _get_user_notifications(redis_client, req_ctx.user_id, product_name)
     return envelope_json_response(notifications)
 
 
