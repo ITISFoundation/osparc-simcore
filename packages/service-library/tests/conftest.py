dc2528fdb244c0724ccb6c19ac68b9396e31c910
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-04-11 14:51:20 +0200

    add option to external env file

--- packages/service-library/tests/conftest.py
+++ packages/service-library/tests/conftest.py
@@ -10,6 +10,8 @@ from typing import Any
 import pytest
 import servicelib
 from faker import Faker
+from pytest_simcore.helpers.typing_env import EnvVarsDict
+from pytest_simcore.helpers.utils_envs import load_dotenv
 
 pytest_plugins = [
     "pytest_simcore.docker_compose",
@@ -59,3 +61,38 @@ def fake_data_dict(faker: Faker) -> dict[str, Any]:
     }
     data["object"] = deepcopy(data)
     return data
+
+
+def pytest_addoption(parser: pytest.Parser):
+    group = parser.getgroup(
+        "external_environment",
+        description="Replaces mocked services with real ones by passing actual environs and connecting directly to external services",
+    )
+    group.addoption(
+        "--external-envfile",
+        action="store",
+        type=Path,
+        default=None,
+        help="Path to an env file. Consider passing a link to repo configs, i.e. `ln -s /path/to/osparc-ops-config/repo.config`",
+    )
+
+
+@pytest.fixture(scope="session")
+def external_environment(request: pytest.FixtureRequest) -> EnvVarsDict:
+    """
+    If a file under test folder prefixed with `.env-secret` is present,
+    then this fixture captures it.
+
+    This technique allows reusing the same tests to check against
+    external development/production servers
+    """
+    envs = {}
+    if envfile := request.config.getoption("--external-envfile"):
+        print("🚨 EXTERNAL `envfile` option detected. Loading", envfile, "...")
+
+        assert isinstance(envfile, Path)
+        assert envfile.is_file()
+
+        envs = load_dotenv(envfile)
+
+    return envs
