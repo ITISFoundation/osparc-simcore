b2d5b0c722a41202704c9e1f20e671e568b9463e
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-04-12 16:41:20 +0200

    fixes url problems

--- services/api-server/src/simcore_service_api_server/api/routes/studies_jobs.py
+++ services/api-server/src/simcore_service_api_server/api/routes/studies_jobs.py
@@ -84,6 +84,8 @@ async def create_study_job(
     url_for: Annotated[Callable, Depends(get_reverse_url_mapper)],
 ) -> Job:
     project = await webserver_api.clone_project(project_id=study_id, hidden=True)
+    # TODO: revert if fails
+
     job = create_job_from_study(
         study_key=study_id, project=project, job_inputs=job_inputs
     )
@@ -124,6 +126,7 @@ async def create_study_job(
         project_inputs, file_inputs, job_inputs
     )
 
+    # TODO: error if mismatch between job inputs and project inputs
     for node_label, file_link in new_project_file_inputs.items():
         node_id = file_param_nodes[node_label]
 
