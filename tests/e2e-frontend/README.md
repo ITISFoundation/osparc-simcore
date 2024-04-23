d356568d64d28ad3ace701f0912524e2856c2acd
Odei Maiz <maiz@itis.swiss>
2024-04-23 14:05:53 +0200

    init

---
+++ tests/e2e-frontend/README.md
@@ -0,0 +1,2 @@
+Run test locally with headed mode
+npx playwright test --ui
---
+++ tests/e2e-frontend/playwright.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+
+// @ts-check
+const {
+  defineConfig,
+  devices
+} = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:9081',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [{
+    name: 'chromium',
+    use: {
+      ...devices['Desktop Chrome']
+    },
+  }, {
+    name: 'firefox',
+    use: {
+      ...devices['Desktop Firefox']
+    },
+  }, {
+    name: 'webkit',
+    use: {
+      ...devices['Desktop Safari']
+    },
+  }],
+});
