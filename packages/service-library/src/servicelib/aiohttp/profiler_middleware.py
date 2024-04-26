0f2b8a8b88e776bc6c12b652bab5abf9f764178d
Mads Bisgaard <bisgaard@itis.swiss>
2024-04-26 21:32:15 +0200

    return exceptions in profiling middleware as 500 status codes

--- packages/service-library/src/servicelib/aiohttp/profiler_middleware.py
+++ packages/service-library/src/servicelib/aiohttp/profiler_middleware.py
@@ -1,5 +1,6 @@
 from aiohttp.web import Request, StreamResponse, middleware
 from pyinstrument import Profiler
+from servicelib.aiohttp.status import HTTP_500_INTERNAL_SERVER_ERROR
 
 from .._utils_profiling_middleware import append_profile
 
@@ -14,25 +15,28 @@ def create_profiling_middleware(app_name: str):
 
         response = await handler(request)
 
-        if isinstance(profiler, Profiler):
-            assert response.content_type == "application/json"
-            stream_response = StreamResponse(
-                status=response.status,
-                reason=response.reason,
-                headers=response.headers,
+        if profiler is None:
+            return response
+        if response.content_type != "application/json":
+            return StreamResponse(
+                status=HTTP_500_INTERNAL_SERVER_ERROR,
+                reason=f"Profiling middleware is not compatible with {response.content_type=}",
+                headers={},
             )
-            stream_response.content_type = "application/x-ndjson"
-            await stream_response.prepare(request)
-            await stream_response.write(response.body)
-            profiler.stop()
-            await stream_response.write(
-                append_profile(
-                    "", profiler.output_text(unicode=True, color=True)
-                ).encode()
-            )
-            await stream_response.write_eof()
-            return stream_response
 
-        return response
+        stream_response = StreamResponse(
+            status=response.status,
+            reason=response.reason,
+            headers=response.headers,
+        )
+        stream_response.content_type = "application/x-ndjson"
+        await stream_response.prepare(request)
+        await stream_response.write(response.body)
+        profiler.stop()
+        await stream_response.write(
+            append_profile("", profiler.output_text(unicode=True, color=True)).encode()
+        )
+        await stream_response.write_eof()
+        return stream_response
 
     return profiling_middleware
