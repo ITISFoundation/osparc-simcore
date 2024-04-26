328321d4b9345e9d1ab8abe48b648c79b93dbabd
Mads Bisgaard <bisgaard@itis.swiss>
2024-04-26 13:43:39 +0200

    further improvements

--- services/storage/src/simcore_service_storage/application.py
+++ services/storage/src/simcore_service_storage/application.py
@@ -10,6 +10,7 @@ from aiohttp import web
 from servicelib.aiohttp.application import APP_CONFIG_KEY, create_safe_application
 from servicelib.aiohttp.dev_error_logger import setup_dev_error_logger
 from servicelib.aiohttp.monitoring import setup_monitoring
+from servicelib.aiohttp.profiler_middleware import create_profiling_middleware
 from servicelib.aiohttp.tracing import setup_tracing
 
 from ._meta import APP_NAME, APP_STARTED_BANNER_MSG, VERSION
@@ -71,6 +72,7 @@ def create(settings: Settings) -> web.Application:
         setup_dsm_cleaner(app)
 
     app.middlewares.append(dsm_exception_handler)
+    app.middlewares.append(create_profiling_middleware("storage"))
 
     if settings.LOG_LEVEL == "DEBUG":
         setup_dev_error_logger(app)
