75a02d863652ebe3c15ad5bb2231fb9e12ac1824
Odei Maiz <maiz@itis.swiss>
2024-04-29 16:53:14 +0200

    Role centers

--- tests/e2e-frontend/tests/navigationBar/navigationBar.spec.js
+++ tests/e2e-frontend/tests/navigationBar/navigationBar.spec.js
@@ -5,12 +5,27 @@ const { test, expect } = require('@playwright/test');
 import products from '../products.json';
 import users from '../users.json';
 
-const poweredByOsparcIcon = {
-  "osparc": false,
-  "s4l": true,
-  "s4lacad": true,
-  "s4llite": true,
-  "tis": true
+const userMenuButtonsPerRole = {
+  "user": {
+    "My Account": true,
+    "PO Center": false,
+    "Admin Center": false,
+  },
+  "tester": {
+    "My Account": true,
+    "PO Center": false,
+    "Admin Center": false,
+  },
+  "po_owner": {
+    "My Account": true,
+    "PO Center": true,
+    "Admin Center": false,
+  },
+  "admin": {
+    "My Account": true,
+    "PO Center": true,
+    "Admin Center": true,
+  },
 };
 
 for (const product in products) {
@@ -19,25 +34,33 @@ for (const product in products) {
     test.describe(`Navigation Bar ${product}`, () => {
       for (const user of productUsers) {
 
-        test.beforeAll('Log in', async ({ page }) => {
+        test.beforeAll('Log in', async () => {
+          const browser = await chromium.launch();
+          const page = await browser.newPage();
+
           await page.goto(products[product]);
-          await page.get_by_test_id("loginUserEmailFld").fill(user.email);
-          await page.get_by_test_id("loginPasswordFld").fill(user.password);
+          await page.getByTestId("loginUserEmailFld").fill(user.email);
+          await page.getByTestId("loginPasswordFld").fill(user.password);
 
           const responsePromise = page.waitForResponse('**/me');
-          await page.get_by_test_id("loginSubmitBtn").click();
+          await page.getByTestId("loginSubmitBtn").click();
 
           const response = await responsePromise;
           const statics = await response.json();
           expect(statics["data"]["role"]).toBe(user.role);
         });
 
-        test(`Items`, async ({ page }) => {
-          expect(poweredByOsparcIcon[product]).toBeDefined();
+        test(`Role Centers`, async ({ page }) => {
+          expect(userMenuButtonsPerRole[user.role]).toBeDefined();
 
-          const expected = poweredByOsparcIcon[product];
-          const isVisible = await page.getByTestId("poweredByOsparc").isVisible();
-          expect(isVisible).toEqual(expected);
+          const buttons = userMenuButtonsPerRole[user.role];
+          for (const button in buttons) {
+            const expected = buttons[button];
+            const isVisible = await page.getByRole("button", {
+              name: button
+            }).isVisible();
+            expect(isVisible).toEqual(expected);
+          }
         });
       }
     });
