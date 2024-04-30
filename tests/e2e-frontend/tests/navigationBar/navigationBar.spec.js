257a87cd331d6b56821a7b2f22a168bb279d5071
Odei Maiz <maiz@itis.swiss>
2024-04-30 11:36:26 +0200

    test.use

--- tests/e2e-frontend/tests/navigationBar/navigationBar.spec.js
+++ tests/e2e-frontend/tests/navigationBar/navigationBar.spec.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-undef */
 
-const { test, expect } = require('@playwright/test');
+const {
+  test,
+  expect,
+  base
+} = require('@playwright/test');
 
-import LoginPage from '../fixtures/loginPage';
+import { LoginPage } from '../fixtures/loginPage';
 
 import products from '../products.json';
 import users from '../users.json';
@@ -35,20 +39,13 @@ for (const product in products) {
     const productUsers = users[product];
     test.describe(`Navigation Bar ${product}`, () => {
       for (const user of productUsers) {
-
-        test.beforeAll('Log in', async () => {
+        test.beforeAll(async () => {
           const browser = await chromium.launch();
           const page = await browser.newPage();
 
-          const loginPage = LoginPage(page, products[product]);
+          const loginPage = test.use(new LoginPage(page, products[product]));
           await loginPage.goto();
-
-          const responsePromise = page.waitForResponse('**/me');
           await loginPage.login(user.email, user.password);
-
-          const response = await responsePromise;
-          const statics = await response.json();
-          expect(statics["data"]["role"]).toBe(user.role);
         });
 
         test(`Options per Role ${user.role}`, async ({ page }) => {
