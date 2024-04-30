274f1a795a63e4406c95175478d183adfc9937de
Odei Maiz <maiz@itis.swiss>
2024-04-30 11:36:17 +0200

    ts -> js

--- tests/e2e-frontend/tests/fixtures/loginPage.js
+++ tests/e2e-frontend/tests/fixtures/loginPage.js
@@ -6,12 +6,6 @@ export class LoginPage {
   constructor(page, productUrl) {
     this.page = page;
     this.productUrl = productUrl;
-
-    this.usernameField = this.page.getByTestId("loginUserEmailFld");
-    this.passwordField = this.page.getByTestId("loginPasswordFld");
-    this.submitButton = this.page.getByTestId("loginSubmitBtn");
-    this.navUserMenuButton = this.page.getByTestId("userMenuBtn");
-    this.navLogoutButton = this.page.getByTestId("userMenuLogoutBtn");
   }
 
   async goto() {
@@ -23,13 +17,20 @@ export class LoginPage {
    * @param {string} password
    */
   async login(email, password) {
+    this.usernameField = this.page.getByTestId("loginUserEmailFld");
+    this.passwordField = this.page.getByTestId("loginPasswordFld");
+    this.submitButton = this.page.getByTestId("loginSubmitBtn");
+
     await this.usernameField.fill(email);
     await this.passwordField.fill(password);
     await this.submitButton.click();
   }
 
   async logout() {
+    this.navUserMenuButton = this.page.getByTestId("userMenuBtn");
     await this.navUserMenuButton.click();
+
+    this.navLogoutButton = this.page.getByTestId("userMenuLogoutBtn");
     await this.navLogoutButton.click();
   }
 }
