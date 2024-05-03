478f38fd6e23b16496a73265f6623683fd62a612
Andrei Neagu <neagu@itis.swiss>
2024-05-03 10:00:16 +0200

    added base test with restart capacity

--- packages/service-library/tests/deferred_tasks/example_app.py
+++ packages/service-library/tests/deferred_tasks/example_app.py
@@ -1,22 +1,139 @@
 import asyncio
 import json
 from dataclasses import dataclass
-from typing import Any
+from datetime import timedelta
+from typing import Any, Final
 from uuid import uuid4
 
+from pydantic import NonNegativeInt
+from servicelib.deferred_tasks import (
+    BaseDeferredHandler,
+    DeferredManager,
+    FullStartContext,
+    TaskUID,
+    UserStartContext,
+)
+from servicelib.redis import RedisClientSDK, RedisClientSDKHealthChecked
 from settings_library.rabbit import RabbitSettings
-from settings_library.redis import RedisSettings
+from settings_library.redis import RedisDatabase, RedisSettings
+
+_CONSTANT_RESULT: Final[str] = "always_the_same"
+
+
+class ExampleDeferredHandler(BaseDeferredHandler[str]):
+    @classmethod
+    async def get_timeout(cls, start_context: FullStartContext) -> timedelta:
+        return timedelta(seconds=60)
+
+    @classmethod
+    async def start_deferred(cls, sleep_duration: float) -> UserStartContext:
+        return {"sleep_duration": sleep_duration}
+
+    @classmethod
+    async def on_deferred_created(
+        cls, task_uid: TaskUID, start_context: FullStartContext
+    ) -> None:
+        distributed_lists: DistributedLists = start_context["distributed_lists"]
+        await distributed_lists.append_to("scheduled", task_uid)
+
+    @classmethod
+    async def run_deferred(cls, start_context: FullStartContext) -> str:
+        sleep_duration: float = start_context["sleep_duration"]
+        await asyncio.sleep(sleep_duration)
+        return _CONSTANT_RESULT
+
+    @classmethod
+    async def on_deferred_result(
+        cls, result: str, start_context: FullStartContext
+    ) -> None:
+        distributed_lists: DistributedLists = start_context["distributed_lists"]
+        await distributed_lists.append_to("results", result)
+
+
+class DistributedLists:
+    def __init__(self, redis_settings: RedisSettings) -> None:
+        self.redis_sdk = RedisClientSDK(
+            redis_settings.build_redis_dsn(RedisDatabase.DEFERRED_TASKS)
+        )
+
+    def _get_queue_name(self, queue_name: str) -> str:
+        return f"distributed_lists::{queue_name}"
+
+    async def append_to(self, queue_name: str, value: Any) -> None:
+        await self.redis_sdk.redis.rpush(self._get_queue_name(queue_name), value)  # type: ignore
+
+    async def get_all_from(self, queue_name: str) -> list:
+        return await self.redis_sdk.redis.lrange(
+            self._get_queue_name(queue_name), 0, -1
+        )  # type: ignore
+
+
+class ExampleApp:
+    def __init__(
+        self,
+        rabbit_settings: RabbitSettings,
+        redis_settings: RedisSettings,
+        distributed_lists: DistributedLists,
+        max_workers: NonNegativeInt,
+    ) -> None:
+        self._redis_client = RedisClientSDKHealthChecked(
+            redis_settings.build_redis_dsn(RedisDatabase.DEFERRED_TASKS)
+        )
+        self._manager = DeferredManager(
+            rabbit_settings,
+            self._redis_client,
+            globals_for_start_context={"distributed_lists": distributed_lists},
+            max_workers=max_workers,
+        )
+
+    async def setup(self) -> None:
+        await self._redis_client.setup()
+        await self._manager.setup()
 
 
 @dataclass
 class Context:
     redis_settings: RedisSettings | None = None
     rabbit_settings: RabbitSettings | None = None
+    example_app: ExampleApp | None = None
+    distributed_lists: DistributedLists | None = None
 
 
-async def _commands_handler(context: Context, command: dict[str, Any]) -> None:
+async def _commands_handler(
+    context: Context, command: str, payload: dict[str, Any]
+) -> Any:
     """Handles all commands send by remote party"""
-    return command
+    if command == "init-context":
+        context.redis_settings = RedisSettings.parse_raw(payload["redis"])
+        context.rabbit_settings = RabbitSettings.parse_raw(payload["rabbit"])
+        # using the same db as the deferred tasks with different keys
+        context.distributed_lists = DistributedLists(context.redis_settings)
+
+        context.example_app = ExampleApp(
+            context.rabbit_settings,
+            context.redis_settings,
+            context.distributed_lists,
+            payload["max-workers"],
+        )
+        await context.example_app.setup()
+
+        print(f"Initialized context {context=}")
+
+        return None
+
+    if command == "start":
+        await ExampleDeferredHandler.start_deferred(**payload)
+        return None
+
+    if command == "get-scheduled":
+        assert context.distributed_lists
+        return await context.distributed_lists.get_all_from("scheduled")
+
+    if command == "get-results":
+        assert context.distributed_lists
+        return await context.distributed_lists.get_all_from("results")
+
+    return None
 
 
 class AsyncTCPServer:
@@ -27,12 +144,17 @@ class AsyncTCPServer:
         self.port = port
         self.buff_size = buff_size
 
+        self._scheduled: list = []
+        self._results: list = []
+
         self._context = Context()
 
     async def _handle_request(self, command: Any) -> Any:
         unique_request_id = uuid4()
         print(f"[{unique_request_id}] request:  {command=}")
-        response = await _commands_handler(self._context, command)
+        response = await _commands_handler(
+            self._context, command["command"], command["payload"]
+        )
         print(f"[{unique_request_id}] response: {response=}")
         return response
 
@@ -49,13 +171,15 @@ class AsyncTCPServer:
         writer.close()
 
     async def run(self):
-        server = await asyncio.start_server(self._handle_client, self.host, self.port)
-
-        addr = server.sockets[0].getsockname()
+        # disable
+        tcp_server = await asyncio.start_server(
+            self._handle_client, self.host, self.port
+        )
+        addr = tcp_server.sockets[0].getsockname()
         print(f"Serving on {addr}")
 
-        async with server:
-            await server.serve_forever()
+        async with tcp_server:
+            await tcp_server.serve_forever()
 
 
 if __name__ == "__main__":
--- packages/service-library/tests/deferred_tasks/test_deferred_tasks.py
+++ packages/service-library/tests/deferred_tasks/test_deferred_tasks.py
@@ -5,23 +5,14 @@ import asyncio
 import json
 import random
 import sys
-from datetime import timedelta
 from pathlib import Path
-from typing import Any, AsyncIterable, Final
-from unittest.mock import Mock
+from typing import Any, AsyncIterable, AsyncIterator
 
 import psutil
 import pytest
-from pydantic import NonNegativeFloat, NonNegativeInt
-from servicelib.background_task import cancel_task
-from servicelib.deferred_tasks import (
-    BaseDeferredHandler,
-    DeferredManager,
-    FullStartContext,
-    TaskUID,
-    UserStartContext,
-)
-from servicelib.redis import RedisClientSDKHealthChecked
+from pydantic import NonNegativeFloat, NonNegativeInt, SecretStr
+from pydantic.json import pydantic_encoder
+from servicelib.redis import RedisClientSDK
 from settings_library.rabbit import RabbitSettings
 from settings_library.redis import RedisDatabase, RedisSettings
 from tenacity._asyncio import AsyncRetrying
@@ -38,229 +29,6 @@ pytest_simcore_ops_services_selection = [
 ]
 
 
-_CONSTANT_RESULT: Final[str] = "always_the_same"
-
-
-class ExampleDeferredHandler(BaseDeferredHandler[str]):
-    @classmethod
-    async def get_timeout(cls, start_context: FullStartContext) -> timedelta:
-        return timedelta(seconds=60)
-
-    @classmethod
-    async def start_deferred(cls, sleep_duration: float) -> UserStartContext:
-        return {"sleep_duration": sleep_duration}
-
-    @classmethod
-    async def on_deferred_created(
-        cls, task_uid: TaskUID, start_context: FullStartContext
-    ) -> None:
-        scheduled_queue: asyncio.Queue = start_context["scheduled_queue"]
-        await scheduled_queue.put(task_uid)
-
-    @classmethod
-    async def run_deferred(cls, start_context: FullStartContext) -> str:
-        sleep_duration: float = start_context["sleep_duration"]
-        await asyncio.sleep(sleep_duration)
-        return _CONSTANT_RESULT
-
-    @classmethod
-    async def on_deferred_result(
-        cls, result: str, start_context: FullStartContext
-    ) -> None:
-        result_queue: asyncio.Queue = start_context["result_queue"]
-        await result_queue.put(result)
-
-
-class ExampleApp:
-    def __init__(
-        self,
-        rabbit_settings: RabbitSettings,
-        redis_settings: RedisSettings,
-        result_queue: asyncio.Queue,
-        scheduled_queue: asyncio.Queue,
-        max_workers: NonNegativeInt,
-    ) -> None:
-        self._redis_client = RedisClientSDKHealthChecked(
-            redis_settings.build_redis_dsn(RedisDatabase.DEFERRED_TASKS)
-        )
-        self._manager = DeferredManager(
-            rabbit_settings,
-            self._redis_client,
-            globals_for_start_context={
-                "result_queue": result_queue,
-                "scheduled_queue": scheduled_queue,
-            },
-            max_workers=max_workers,
-        )
-
-    async def setup(self) -> None:
-        await self._redis_client.setup()
-        await self._manager.setup()
-
-    async def shutdown(self) -> None:
-        await self._manager.shutdown()
-        await self._redis_client.shutdown()
-
-
-class _AppLifecycleManager:
-    def __init__(
-        self,
-        rabbit_settings: RabbitSettings,
-        redis_settings: RedisSettings,
-        max_workers: NonNegativeInt,
-        results_mock: Mock,
-        scheduled_mock: Mock,
-    ) -> None:
-        self._result_queue: asyncio.Queue = asyncio.Queue()
-        self._scheduled_queue: asyncio.Queue = asyncio.Queue()
-        self._commands_queue: asyncio.Queue = asyncio.Queue()
-        self.results_mock = results_mock
-        self.scheduled_mock = scheduled_mock
-
-        self._app = ExampleApp(
-            rabbit_settings,
-            redis_settings,
-            self._result_queue,
-            self._scheduled_queue,
-            max_workers,
-        )
-
-        self._commands_task: asyncio.Task | None = None
-        self._results_task: asyncio.Task | None = None
-        self._scheduled_task: asyncio.Task | None = None
-
-    async def _commands_worker(self) -> None:
-        while True:
-            command = await self._commands_queue.get()
-
-            if command is None:
-                break
-
-            await ExampleDeferredHandler.start_deferred(**command)
-
-    async def _results_worker(self) -> None:
-        while True:
-            result = await self._result_queue.get()
-            self.results_mock(result)
-            print("App Lifecycle -> JOB DONE")
-
-    async def _scheduled_worker(self) -> None:
-        while True:
-            task_uid = await self._scheduled_queue.get()
-            self.scheduled_mock(task_uid)
-            print("App Lifecycle -> JOB SCHEDULED")
-
-    async def start(self) -> None:
-        assert self._commands_task is None
-        assert self._results_task is None
-        assert self._scheduled_task is None
-
-        await self._app.setup()
-
-        self._commands_task = asyncio.create_task(self._commands_worker())
-        self._results_task = asyncio.create_task(self._results_worker())
-        self._scheduled_task = asyncio.create_task(self._scheduled_worker())
-
-        print("App Lifecycle -> STARTED")
-
-    async def stop(self) -> None:
-        assert self._commands_task is not None
-        assert self._results_task is not None
-        assert self._scheduled_task is not None
-
-        await self._app.shutdown()
-
-        # graceful shut down of deferred_manager
-        await self._commands_queue.put(None)
-        await asyncio.sleep(0.1)  # wait for manager shutdown
-
-        await cancel_task(self._commands_task, timeout=1)
-        self._commands_task = None
-
-        await cancel_task(self._results_task, timeout=1)
-        self._results_task = None
-
-        await cancel_task(self._scheduled_task, timeout=1)
-        self._scheduled_task = None
-
-        print("App Lifecycle -> STOPPED")
-
-    async def start_deferred_task(self, sleep_duration: float) -> None:
-        await self._commands_queue.put({"sleep_duration": sleep_duration})
-
-    async def __aenter__(self) -> "_AppLifecycleManager":
-        await self.start()
-        return self
-
-    async def __aexit__(self, *args):
-        await self.stop()
-        # wait for all events to be consumed
-        await asyncio.sleep(0.1)
-
-
-async def _assert_mock_has_calls(
-    mock: Mock, *, count: NonNegativeInt, timeout: float = 10
-) -> None:
-    async for attempt in AsyncRetrying(
-        wait=wait_fixed(0.01),
-        stop=stop_after_delay(timeout),
-        reraise=True,
-        retry=retry_if_exception_type(AssertionError),
-    ):
-        with attempt:
-            assert len(mock.call_args_list) == count
-
-
-@pytest.mark.parametrize("max_workers", [10])
-@pytest.mark.parametrize(
-    "deferred_tasks_to_start",
-    [
-        # 1,
-        2,
-        # 100,
-    ],
-)
-@pytest.mark.parametrize(
-    "start_stop_cycles",
-    [
-        # 0
-        1,
-    ],
-)
-async def test_run_lots_of_jobs_interrupted(
-    rabbit_service: RabbitSettings,
-    redis_service: RedisSettings,
-    max_workers: NonNegativeInt,
-    deferred_tasks_to_start: NonNegativeInt,
-    start_stop_cycles: NonNegativeInt,
-):
-    scheduled_mock = Mock()
-    results_mock = Mock()
-
-    async with _AppLifecycleManager(
-        rabbit_service, redis_service, max_workers, results_mock, scheduled_mock
-    ) as manager:
-
-        # start all in parallel
-        await asyncio.gather(
-            *[manager.start_deferred_task(0.1) for _ in range(deferred_tasks_to_start)]
-        )
-        # makes sure tasks have been scheduled
-        await _assert_mock_has_calls(scheduled_mock, count=deferred_tasks_to_start)
-
-        # if this fails all scheduled tasks have already finished
-        assert len(results_mock.call_args_list) < deferred_tasks_to_start
-
-        # emulate issues with processing start & stop DeferredManager
-        for _ in range(start_stop_cycles):
-            await manager.stop()
-            radom_wait = random.uniform(0.1, 0.2)  # noqa: S311
-            await asyncio.sleep(radom_wait)
-            await manager.start()
-
-        await _assert_mock_has_calls(results_mock, count=deferred_tasks_to_start)
-
-
 class _RemoteProcess:
     def __init__(self, shell_command):
         self.shell_command = shell_command
@@ -275,9 +43,10 @@ class _RemoteProcess:
         self.pid = self.process.pid
         print(f"Process started {self.pid}")
 
-    async def stop(self):
-        assert self.process is not None
-        assert self.pid is not None
+    async def stop(self, *, graceful: bool = False):
+        if not graceful:
+            assert self.process is not None
+            assert self.pid is not None
 
         parent = psutil.Process(self.pid)
         children = parent.children(recursive=True)
@@ -293,7 +62,17 @@ class _RemoteProcess:
 
 
 @pytest.fixture
-async def remote_process() -> AsyncIterable[None]:
+async def cleanup_redis(redis_service: RedisSettings) -> AsyncIterator[None]:
+    redis_client_sdk = RedisClientSDK(
+        redis_service.build_redis_dsn(RedisDatabase.DEFERRED_TASKS)
+    )
+    await redis_client_sdk.redis.flushall()
+    yield
+    await redis_client_sdk.redis.flushall()
+
+
+@pytest.fixture
+async def remote_process(cleanup_redis: None) -> AsyncIterable[_RemoteProcess]:
     python_interpreter = sys.executable
     current_module_path = (
         Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
@@ -302,15 +81,15 @@ async def remote_process() -> AsyncIterable[None]:
     assert app_to_start.exists()
 
     process = _RemoteProcess(f"{python_interpreter} {app_to_start}")
-    await process.start()
 
-    yield None
+    yield process
 
-    await process.stop()
+    await process.stop(graceful=True)
 
 
 async def _tcp_command(
-    command: dict[str, Any],
+    command: str,
+    payload: dict[str, Any],
     *,
     host: str = "127.0.0.1",
     port: int = 3562,
@@ -325,7 +104,7 @@ async def _tcp_command(
         with attempt:
             reader, writer = await asyncio.open_connection(host, port)
 
-    writer.write(json.dumps(command).encode())
+    writer.write(json.dumps({"command": command, "payload": payload}).encode())
     await writer.drain()
     response = await reader.read(buff_size)
     decoded_response = response.decode()
@@ -333,14 +112,139 @@ async def _tcp_command(
     return json.loads(decoded_response)
 
 
-async def test_with_remote_process(remote_process: None):
+def _get_serialization_options() -> dict[str, Any]:
+    def _show_secrets_encoder(obj):
+        if isinstance(obj, SecretStr):
+            return obj.get_secret_value()
 
-    request = {"hello": "bc-d"}
-    response = await _tcp_command(request)
-    assert response == request
+        return pydantic_encoder(obj)
+
+    return {
+        "encoder": _show_secrets_encoder,
+        "exclude_defaults": True,
+        "exclude_none": True,
+        "exclude_unset": True,
+    }
 
 
-# THIS could be the issue: ERROR    servicelib.deferred_tasks._utils:_utils.py:29 Error detected in user code. Aborting message retry. Please check code at: 'servicelib.deferred_tasks._deferred_manager._fs_handle_worker'
-# It's in ERROR_RESULT when the thing was cancelled, should be in worker retry,
-# I guess something went wrong? But why is the status in the DB wrong?
-# Figure out from here
+class _RemoteProcessLifecycleManager:
+    def __init__(
+        self,
+        remote_process: _RemoteProcess,
+        rabbit_service: RabbitSettings,
+        redis_service: RedisSettings,
+        max_workers: int,
+    ) -> None:
+        self.remote_process = remote_process
+        self.rabbit_service = rabbit_service
+        self.redis_service = redis_service
+        self.max_workers = max_workers
+
+    async def __aenter__(self) -> "_RemoteProcessLifecycleManager":
+        await self.start()
+        return self
+
+    async def __aexit__(self, *args):
+        await self.stop()
+        # wait for all events to be consumed
+        await asyncio.sleep(0.1)
+
+    async def start(self) -> None:
+        await self.remote_process.start()
+
+        response = await _tcp_command(
+            "init-context",
+            {
+                "rabbit": self.rabbit_service.json(**_get_serialization_options()),
+                "redis": self.redis_service.json(**_get_serialization_options()),
+                "max-workers": self.max_workers,
+            },
+        )
+        assert response is None
+
+    async def stop(self) -> None:
+        await self.remote_process.stop()
+
+    async def start_deferred_task(self, sleep_duration: float) -> None:
+        response = await _tcp_command("start", {"sleep_duration": sleep_duration})
+        assert response is None
+
+    async def get_results(self) -> list[str]:
+        response = await _tcp_command("get-results", {})
+        assert isinstance(response, list)
+        return response
+
+    async def get_scheduled(self) -> list[str]:
+        response = await _tcp_command("get-scheduled", {})
+        assert isinstance(response, list)
+        return response
+
+
+async def _assert_has_entries(
+    manager: _RemoteProcessLifecycleManager,
+    list_name: str,
+    *,
+    count: NonNegativeInt,
+    timeout: float = 10,
+) -> None:
+    async for attempt in AsyncRetrying(
+        wait=wait_fixed(0.01),
+        stop=stop_after_delay(timeout),
+        reraise=True,
+        retry=retry_if_exception_type(AssertionError),
+    ):
+        with attempt:
+            if list_name == "get-results":
+                assert len(await manager.get_results()) == count
+            if list_name == "get-scheduled":
+                assert len(await manager.get_scheduled()) == count
+
+
+@pytest.mark.parametrize("max_workers", [10])
+@pytest.mark.parametrize(
+    "deferred_tasks_to_start",
+    [
+        # 1,
+        # 2,
+        40,
+    ],
+)
+@pytest.mark.parametrize(
+    "start_stop_cycles",
+    [
+        # 0,
+        1,
+    ],
+)
+async def test_with_remote_process(
+    remote_process: _RemoteProcess,
+    rabbit_service: RabbitSettings,
+    redis_service: RedisSettings,
+    max_workers: int,
+    deferred_tasks_to_start: NonNegativeInt,
+    start_stop_cycles: NonNegativeInt,
+):
+    async with _RemoteProcessLifecycleManager(
+        remote_process, rabbit_service, redis_service, max_workers
+    ) as manager:
+
+        # start all in parallel
+        await asyncio.gather(
+            *[manager.start_deferred_task(0.1) for _ in range(deferred_tasks_to_start)]
+        )
+        # makes sure tasks have been scheduled
+        await _assert_has_entries(
+            manager, "get-scheduled", count=deferred_tasks_to_start
+        )
+
+        # if this fails all scheduled tasks have already finished
+        assert len(await manager.get_results()) < deferred_tasks_to_start
+
+        # emulate issues with processing start & stop DeferredManager
+        for _ in range(start_stop_cycles):
+            await manager.stop()
+            radom_wait = random.uniform(0.1, 0.2)  # noqa: S311
+            await asyncio.sleep(radom_wait)
+            await manager.start()
+
+        await _assert_has_entries(manager, "get-results", count=deferred_tasks_to_start)
