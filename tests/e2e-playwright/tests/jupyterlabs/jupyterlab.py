e2c5977d32c02a17075225dc615052b84d6e2725
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-05-07 17:28:53 +0200

    simplify stuff

--- tests/e2e-playwright/tests/jupyterlabs/jupyterlab.py
+++ tests/e2e-playwright/tests/jupyterlabs/jupyterlab.py
@@ -6,17 +6,11 @@
 # pylint: disable=unnecessary-lambda
 
 import re
-from http import HTTPStatus
-from typing import Final
+from typing import Callable, Final
 
 from playwright.sync_api import APIRequestContext, Page
 from pydantic import AnyUrl
-from pytest_simcore.logging_utils import test_logger
-from pytest_simcore.playwright_utils import on_web_socket_default_handler
-from tenacity import Retrying
-from tenacity.retry import retry_if_exception_type
-from tenacity.stop import stop_after_attempt
-from tenacity.wait import wait_fixed
+from pytest_simcore.playwright_utils import RunningState, on_web_socket_default_handler
 
 _PROJECTS_UUID_PATTERN: Final[re.Pattern] = re.compile(
     r"/projects/([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})"
@@ -26,11 +20,11 @@ _PROJECTS_UUID_PATTERN: Final[re.Pattern] = re.compile(
 def test_jupyterlab(
     page: Page,
     log_in_and_out: None,
+    create_new_project_and_delete: Callable[..., None],
     api_request_context: APIRequestContext,
     product_url: AnyUrl,
     product_billable: bool,
     service_key: str,
-    service_test_id: str,
 ):
     # connect and listen to websocket
     page.on("websocket", on_web_socket_default_handler)
@@ -40,71 +34,29 @@ def test_jupyterlab(
     _textbox = page.get_by_role("textbox", name="search")
     _textbox.fill(service_key)
     _textbox.press("Enter")
-    page.get_by_test_id(service_test_id).click()
+    page.get_by_test_id(
+        f"studyBrowserListItem_simcore/services/dynamic/{service_key}"
+    ).click()
 
-    with page.expect_response(_PROJECTS_UUID_PATTERN) as response_info:
-        # Project detail view pop-ups shows
-        page.get_by_test_id("openResource").click()
-        if product_billable:
-            # Open project with default resources
-            page.get_by_test_id("openWithResources").click()
-        page.wait_for_timeout(1000)
-
-    # Get project uuid, will be used to delete this project in the end
-    test_logger.info(
-        "projects uuid endpoint captured: %s", f"{response_info.value.url}"
-    )
-    match = _PROJECTS_UUID_PATTERN.search(response_info.value.url)
-    assert match
-    extracted_uuid = match.group(1)
+    create_new_project_and_delete(expected_states=(RunningState.UNKNOWN,))
 
     # Wait until iframe is shown and create new notebook with print statement
     page.frame_locator(".qx-main-dark").get_by_role(
         "button", name="New Launcher"
     ).click(timeout=600000)
-    if "jupyter-octave-python-math" in service_key or "jupyter-math" in service_key:
-        # Python Math service
-        # NOTE MD: as currently jupyter python math UI test is flaky i will comment it out until it is improved
-        ...
-    elif "jupyter-smash" in service_key:
-        # Jupyter smash service
-        page.frame_locator(".qx-main-dark").locator(
-            ".jp-LauncherCard-icon"
-        ).first.click()
-        page.wait_for_timeout(3000)
-        page.frame_locator(".qx-main-dark").get_by_role(
-            "tab", name="Untitled.ipynb"
-        ).click()
-        _jupyterlab_ui = (
-            page.frame_locator(".qx-main-dark")
-            .get_by_label("Untitled.ipynb")
-            .get_by_role("textbox")
-        )
-        _jupyterlab_ui.fill("print('test')")
-        _jupyterlab_ui.press("Shift+Enter")
-    else:
-        msg = "Not supported service key"
-        raise ValueError(msg)
-    page.wait_for_timeout(1000)
 
-    # Going back to dashboard
-    page.get_by_test_id("dashboardBtn").click()
-    page.get_by_test_id("confirmDashboardBtn").click()
-    page.wait_for_timeout(1000)
+    # Jupyter smash service
+    page.frame_locator(".qx-main-dark").locator(".jp-LauncherCard-icon").first.click()
+    page.wait_for_timeout(3000)
+    page.frame_locator(".qx-main-dark").get_by_role(
+        "tab", name="Untitled.ipynb"
+    ).click()
+    _jupyterlab_ui = (
+        page.frame_locator(".qx-main-dark")
+        .get_by_label("Untitled.ipynb")
+        .get_by_role("textbox")
+    )
+    _jupyterlab_ui.fill("print('test')")
+    _jupyterlab_ui.press("Shift+Enter")
 
-    # Going back to projecs/studies view (In Sim4life projects:=studies)
-    page.get_by_test_id("studiesTabBtn").click()
     page.wait_for_timeout(1000)
-
-    # The project is closing, wait until it is closed and delete it (currently waits max=5 minutes)
-    for attempt in Retrying(
-        wait=wait_fixed(5),
-        stop=stop_after_attempt(60),  # 5*60= 300 seconds
-        retry=retry_if_exception_type(AssertionError),
-        reraise=True,
-    ):
-        with attempt:
-            resp = api_request_context.delete(
-                f"{product_url}v0/projects/{extracted_uuid}"
-            )
-            assert resp.status == HTTPStatus.NO_CONTENT
