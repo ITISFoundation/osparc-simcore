70c9abb4501030102cbac1001f8cfd26264c0995
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-05-07 19:03:35 +0200

    getting there

--- tests/e2e-playwright/tests/jupyterlabs/conftest.py
+++ tests/e2e-playwright/tests/jupyterlabs/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from pydantic import ByteSize
+from pydantic import ByteSize, TypeAdapter
 
 
 def pytest_addoption(parser: pytest.Parser) -> None:
@@ -7,17 +7,19 @@ def pytest_addoption(parser: pytest.Parser) -> None:
         "oSparc e2e options", description="oSPARC-e2e specific parameters"
     )
     group.addoption(
-        "--file-size",
+        "--large-file-size",
         action="store",
         type=str,
-        default="20Gib",
+        default=None,
         help="large file size in human readable form",
     )
 
 
 @pytest.fixture(scope="session")
 def large_file_size(request: pytest.FixtureRequest) -> ByteSize:
-    file_size = request.config.getoption("--file-size")
+    file_size = request.config.getoption("--large-file-size")
+    if not file_size:
+        return ByteSize(0)
     assert file_size is not None
     assert isinstance(file_size, str)
-    return ByteSize(file_size)
+    return TypeAdapter(ByteSize).validate_python(file_size)
