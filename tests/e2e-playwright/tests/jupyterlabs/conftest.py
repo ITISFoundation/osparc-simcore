06e22cdae8e7020c788e49f06ac88bb57f898cfe
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-05-08 08:41:58 +0200

    add validations

--- tests/e2e-playwright/tests/jupyterlabs/conftest.py
+++ tests/e2e-playwright/tests/jupyterlabs/conftest.py
@@ -1,3 +1,10 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-statements
+# pylint: disable=no-name-in-module
+
 import pytest
 from pydantic import ByteSize, TypeAdapter
 
@@ -16,10 +23,21 @@ def pytest_addoption(parser: pytest.Parser) -> None:
 
 
 @pytest.fixture(scope="session")
-def large_file_size(request: pytest.FixtureRequest) -> ByteSize:
+def large_file_block_size() -> ByteSize:
+    return TypeAdapter(ByteSize).validate_python("64Mib")
+
+
+@pytest.fixture(scope="session")
+def large_file_size(
+    request: pytest.FixtureRequest, large_file_block_size: ByteSize
+) -> ByteSize:
     file_size = request.config.getoption("--large-file-size")
     if not file_size:
         return ByteSize(0)
     assert file_size is not None
     assert isinstance(file_size, str)
-    return TypeAdapter(ByteSize).validate_python(file_size)
+    validated_file_size = TypeAdapter(ByteSize).validate_python(file_size)
+    assert (
+        validated_file_size >= large_file_block_size
+    ), f"{validated_file_size.human_readable()} must be larger than {large_file_block_size.human_readable()}!"
+    return validated_file_size
