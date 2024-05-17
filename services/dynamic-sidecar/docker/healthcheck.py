26a7f76327d0ad1f7bf58a08aa1bbdfbe0ed0851
Andrei Neagu <neagu@itis.swiss>
2024-05-17 10:22:08 +0200

    stripped old comment

--- services/dynamic-sidecar/docker/healthcheck.py
+++ services/dynamic-sidecar/docker/healthcheck.py
@@ -1,16 +1,6 @@
 #!/bin/python
 """ Healthcheck script to run inside docker
 
-Example of usage in a Dockerfile
-```
-    COPY --chown=scu:scu docker/healthcheck.py docker/healthcheck.py
-    HEALTHCHECK --interval=30s \
-                --timeout=30s \
-                --start-period=1s \
-                --retries=3 \
-                CMD python3 docker/healthcheck.py http://localhost:8000/
-```
-
 Q&A:
     1. why not to use curl instead of a python script?
         - SEE https://blog.sixeyed.com/docker-healthchecks-why-not-to-use-curl-or-iwr/
