284136416993ede456a7e20f836180823080beac
Odei Maiz <maiz@itis.swiss>
2024-05-28 11:56:59 +0200

    more patching

--- services/static-webserver/client/source/class/osparc/metadata/ClassifiersEditor.js
+++ services/static-webserver/client/source/class/osparc/metadata/ClassifiersEditor.js
@@ -18,13 +18,13 @@
 qx.Class.define("osparc.metadata.ClassifiersEditor", {
   extend: qx.ui.core.Widget,
 
-  construct: function(studyData) {
+  construct: function(resourceData) {
     this.base(arguments);
 
-    if (osparc.utils.Resources.isService(studyData)) {
-      this.__studyData = osparc.utils.Utils.deepCloneObject(studyData);
+    if (osparc.utils.Resources.isService(resourceData)) {
+      this.__resourceData = osparc.utils.Utils.deepCloneObject(resourceData);
     } else {
-      this.__studyData = osparc.data.model.Study.deepCloneStudyObject(studyData);
+      this.__resourceData = osparc.data.model.Study.deepCloneStudyObject(resourceData);
     }
 
     this._setLayout(new qx.ui.layout.VBox(10));
@@ -37,7 +37,7 @@ qx.Class.define("osparc.metadata.ClassifiersEditor", {
   },
 
   members: {
-    __studyData: null,
+    __resourceData: null,
     __classifiersTree: null,
 
     _createChildControlImpl: function(id) {
@@ -89,17 +89,17 @@ qx.Class.define("osparc.metadata.ClassifiersEditor", {
         flex: 1
       });
 
-      const addRRIDClassfierBtn = new osparc.ui.form.FetchButton(this.tr("Add Classifier"));
-      addRRIDClassfierBtn.addListener("execute", () => {
-        this.__addRRIDClassfier(textField.getValue(), addRRIDClassfierBtn);
+      const addRRIDClassifierBtn = new osparc.ui.form.FetchButton(this.tr("Add Classifier"));
+      addRRIDClassifierBtn.addListener("execute", () => {
+        this.__addRRIDClassifier(textField.getValue(), addRRIDClassifierBtn);
       }, this);
-      rridLayout.add(addRRIDClassfierBtn);
+      rridLayout.add(addRRIDClassifierBtn);
 
       return rridLayout;
     },
 
     __createClassifiersTree: function() {
-      const studyData = this.__studyData;
+      const studyData = this.__resourceData;
       const classifiers = studyData.classifiers && studyData.classifiers ? studyData.classifiers : [];
       const classifiersTree = this.__classifiersTree = new osparc.filter.ClassifiersFilter("classifiersEditor", "searchBarFilter", classifiers);
       osparc.store.Store.getInstance().addListener("changeClassifiers", e => {
@@ -120,7 +120,7 @@ qx.Class.define("osparc.metadata.ClassifiersEditor", {
       return buttons;
     },
 
-    __addRRIDClassfier: function(rrid, btn) {
+    __addRRIDClassifier: function(rrid, btn) {
       rrid = rrid.replace("RRID:", "");
       const params = {
         url: {
@@ -144,32 +144,26 @@ qx.Class.define("osparc.metadata.ClassifiersEditor", {
     __saveClassifiers: function(saveBtn) {
       saveBtn.setFetching(true);
 
-      if (osparc.utils.Resources.isStudy(this._serializedData) || osparc.utils.Resources.isTemplate(this._serializedData)) {
-        this.__studyData["classifiers"] = this.__classifiersTree.getCheckedClassifierIDs();
-        const params = {
-          url: {
-            "studyId": this.__studyData["uuid"]
-          },
-          data: this.__studyData
-        };
-        osparc.data.Resources.fetch("studies", "put", params)
-          .then(updatedStudy => {
+      const newClassifiers = this.__classifiersTree.getCheckedClassifierIDs();
+      if (osparc.utils.Resources.isStudy(this.__resourceData) || osparc.utils.Resources.isTemplate(this.__resourceData)) {
+        osparc.info.StudyUtils.patchStudyData(this.__resourceData, "classifiers", newClassifiers)
+          .then(() => {
             osparc.FlashMessenger.getInstance().logAs(this.tr("Classifiers successfully edited"));
             saveBtn.setFetching(false);
-            this.fireDataEvent("updateClassifiers", updatedStudy);
+            this.fireDataEvent("updateClassifiers", this.__resourceData);
           })
           .catch(err => {
-            osparc.FlashMessenger.getInstance().logAs(this.tr("Something went wrong editing Classifiers"), "ERROR");
             console.error(err);
+            osparc.FlashMessenger.getInstance().logAs(this.tr("Something went wrong editing Classifiers"), "ERROR");
           });
       } else {
         const params = {
           url: osparc.data.Resources.getServiceUrl(
-            this.__studyData["key"],
-            this.__studyData["version"]
+            this.__resourceData["key"],
+            this.__resourceData["version"]
           ),
           data: {
-            "classifiers": this.__classifiersTree.getCheckedClassifierIDs()
+            "classifiers": newClassifiers
           }
         };
         osparc.data.Resources.fetch("services", "patch", params)
