55c74372ccb3a14bd7af8b5e5b4a482e611c87a7
matusdrobuliak66 <drobuliak66@gmail.com>
2024-05-30 15:07:08 +0200

    review @prespo

--- services/efs-guardian/src/simcore_service_efs_guardian/core/errors.py
+++
@@ -1,9 +0,0 @@
-from pydantic.errors import PydanticErrorMixin
-
-
-class EfsGuardianRuntimeError(PydanticErrorMixin, RuntimeError):
-    msg_template: str = "efs-guardian unexpected error"
-
-
-class ConfigurationError(EfsGuardianRuntimeError):
-    msg_template: str = "Application misconfiguration: {msg}"
