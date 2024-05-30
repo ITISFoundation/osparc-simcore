d115102eb71abc08f15ded388e3c34fc5909ea06
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-05-30 11:35:43 +0200

    more

--- packages/postgres-database/src/simcore_postgres_database/models/projects_metadata.py
+++ packages/postgres-database/src/simcore_postgres_database/models/projects_metadata.py
@@ -34,6 +34,8 @@ projects_metadata = sa.Table(
     #   in cases 2., 3. the parent_project_uuid is the direct parent project, and parent_node_id is the direct node parent as
     #   a specific node is defined by a project AND a node (since node IDs are non unique)
     #
+    #   in cases 2., 3. the root_parent_project_uuid is the very first parent project, and root_parent_node_id is the very first parent node
+    #
     metadata,
     sa.Column(
         "project_uuid",
@@ -67,12 +69,31 @@ projects_metadata = sa.Table(
         nullable=True,
         doc="If applicable the parent node UUID of this project (the node that ran the public API to start this project_uuid lives in a node with ID parent_node_id)",
     ),
+    sa.Column(
+        "root_parent_project_uuid",
+        sa.String,
+        nullable=True,
+        doc="If applicable the root parent project UUID of this project (the root project UUID in which the root node created the very first child)",
+    ),
+    sa.Column(
+        "root_parent_node_id",
+        sa.String,
+        nullable=True,
+        doc="If applicable the root parent node UUID of this project (the root node ID of the node that created the very first child)",
+    ),
     # TIME STAMPS ----ß
     column_created_datetime(timezone=True),
     column_modified_datetime(timezone=True),
     sa.PrimaryKeyConstraint("project_uuid"),
     sa.ForeignKeyConstraint(
         ("parent_project_uuid", "parent_node_id"),
+        (projects_nodes.c.project_uuid, projects_nodes.c.node_id),
+        onupdate="CASCADE",
+        ondelete="SET NULL",
+        name="fk_projects_metadata_parent_node_id",
+    ),
+    sa.ForeignKeyConstraint(
+        ("root_parent_project_uuid", "root_parent_node_id"),
         (projects_nodes.c.project_uuid, projects_nodes.c.node_id),
         onupdate="CASCADE",
         ondelete="SET NULL",
