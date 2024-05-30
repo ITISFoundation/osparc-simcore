ef2b005175f322a514033a409c8c368eaef4043f
matusdrobuliak66 <drobuliak66@gmail.com>
2024-05-30 14:54:22 +0200

    review @prespov & @GitHK

--- services/efs-guardian/tests/unit/conftest.py
+++ services/efs-guardian/tests/unit/conftest.py
@@ -2,15 +2,18 @@
 # pylint:disable=unused-argument
 # pylint:disable=redefined-outer-name
 
+import re
+from collections.abc import AsyncIterator
 from pathlib import Path
 from typing import AsyncIterator
 
 import httpx
 import pytest
+import simcore_service_efs_guardian
+import yaml
 from asgi_lifespan import LifespanManager
-from faker import Faker
 from fastapi import FastAPI
-from pytest import MonkeyPatch
+from httpx import ASGITransport
 from pytest_simcore.helpers.utils_envs import EnvVarsDict, setenvs_from_dict
 from simcore_service_efs_guardian.core.application import create_app
 from simcore_service_efs_guardian.core.settings import ApplicationSettings
@@ -25,25 +28,69 @@ pytest_plugins = [
 @pytest.fixture(scope="session")
 def project_slug_dir(osparc_simcore_root_dir: Path) -> Path:
     # fixtures in pytest_simcore.environs
-    service_folder = osparc_simcore_root_dir / "services" / "resource-usage-tracker"
+    service_folder = osparc_simcore_root_dir / "services" / "efs_guardian"
     assert service_folder.exists()
-    assert any(service_folder.glob("src/simcore_service_resource_usage_tracker"))
+    assert any(service_folder.glob("src/simcore_service_efs_guardian"))
     return service_folder
 
 
+@pytest.fixture(scope="session")
+def installed_package_dir() -> Path:
+    dirpath = Path(simcore_service_efs_guardian.__file__).resolve().parent
+    assert dirpath.exists()
+    return dirpath
+
+
 @pytest.fixture
-def app_environment(monkeypatch: MonkeyPatch, faker: Faker) -> EnvVarsDict:
-    envs = setenvs_from_dict(
-        monkeypatch,
-        {
-            "POSTGRES_HOST": faker.domain_name(),
-            "POSTGRES_USER": faker.user_name(),
-            "POSTGRES_PASSWORD": faker.password(),
-            "POSTGRES_DB": faker.pystr(),
-        },
-    )
+def env_devel_dict(env_devel_dict: EnvVarsDict) -> EnvVarsDict:
+    return env_devel_dict
+
+
+@pytest.fixture
+def docker_compose_service_efs_guardian_env_vars(
+    services_docker_compose_file: Path,
+    env_devel_dict: EnvVarsDict,
+) -> EnvVarsDict:
+    """env vars injected at the docker-compose"""
+
+    payments = yaml.safe_load(services_docker_compose_file.read_text())["services"][
+        "efs-guardian"
+    ]
+
+    def _substitute(key, value):
+        if m := re.match(r"\${([^{}:-]\w+)", value):
+            expected_env_var = m.group(1)
+            try:
+                # NOTE: if this raises, then the RHS env-vars in the docker-compose are
+                # not defined in the env-devel
+                if value := env_devel_dict[expected_env_var]:
+                    return key, value
+            except KeyError:
+                pytest.fail(
+                    f"{expected_env_var} is not defined in .env-devel but used in docker-compose services[{payments}].environment[{key}]"
+                )
+        return None
+
+    envs: EnvVarsDict = {}
+    for key, value in payments.get("environment", {}).items():
+        if found := _substitute(key, value):
+            _, new_value = found
+            envs[key] = new_value
 
     return envs
+
+
+@pytest.fixture
+def app_environment(
+    monkeypatch: pytest.MonkeyPatch,
+    docker_compose_service_efs_guardian_env_vars: EnvVarsDict,
+) -> EnvVarsDict:
+    return setenvs_from_dict(
+        monkeypatch,
+        {
+            **docker_compose_service_efs_guardian_env_vars,
+        },
+    )
 
 
 @pytest.fixture
@@ -53,17 +100,22 @@ def app_settings(app_environment: EnvVarsDict) -> ApplicationSettings:
 
 
 @pytest.fixture
-async def initialized_app(app_settings: ApplicationSettings) -> AsyncIterator[FastAPI]:
-    app = create_app(app_settings)
-    async with LifespanManager(app):
-        yield app
+async def app(app_settings: ApplicationSettings) -> AsyncIterator[FastAPI]:
+    the_test_app = create_app(app_settings)
+    async with LifespanManager(
+        the_test_app,
+    ):
+        yield the_test_app
 
 
 @pytest.fixture
-async def async_client(initialized_app: FastAPI) -> AsyncIterator[httpx.AsyncClient]:
+async def client(app: FastAPI) -> AsyncIterator[httpx.AsyncClient]:
+    # - Needed for app to trigger start/stop event handlers
+    # - Prefer this client instead of fastapi.testclient.TestClient
     async with httpx.AsyncClient(
-        app=initialized_app,
-        base_url=f"http://{initialized_app.title}.testserver.io",
+        app=app,
+        base_url="http://efs-guardian.testserver.io",
         headers={"Content-Type": "application/json"},
     ) as client:
+        assert isinstance(client._transport, ASGITransport)
         yield client
--- services/efs-guardian/tests/unit/test_api_health.py
+++ services/efs-guardian/tests/unit/test_api_health.py
@@ -3,24 +3,11 @@
 # pylint:disable=redefined-outer-name
 
 import httpx
-import pytest
-from pytest_simcore.helpers.utils_envs import EnvVarsDict
 from starlette import status
 
-pytest_simcore_core_services_selection = []
 
-pytest_simcore_ops_services_selection = []
-
-
-@pytest.fixture
-def app_environment(
-    app_environment: EnvVarsDict,
-) -> EnvVarsDict:
-    return app_environment
-
-
-async def test_healthcheck(async_client: httpx.AsyncClient):
-    response = await async_client.get("/")
+async def test_healthcheck(client: httpx.AsyncClient):
+    response = await client.get("/")
     response.raise_for_status()
     assert response.status_code == status.HTTP_200_OK
     assert "simcore_service_efs_guardian" in response.text
