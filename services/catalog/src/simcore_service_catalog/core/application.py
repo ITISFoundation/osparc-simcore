0b87d64e0e27dfb2de2aa1299a117cc71069adce
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-05-31 18:10:26 +0200

    moves exceptions

--- services/catalog/src/simcore_service_catalog/core/application.py
+++ services/catalog/src/simcore_service_catalog/core/application.py
@@ -3,7 +3,6 @@ import time
 from collections.abc import Callable
 
 from fastapi import FastAPI, Request
-from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.gzip import GZipMiddleware
 from models_library.basic_types import BootModeEnum
 from servicelib.fastapi.openapi import override_fastapi_openapi_method
@@ -12,8 +11,6 @@ from servicelib.fastapi.prometheus_instrumentation import (
     setup_prometheus_instrumentation,
 )
 from servicelib.logging_utils import config_all_loggers
-from starlette import status
-from starlette.exceptions import HTTPException
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from .._meta import (
@@ -24,13 +21,9 @@ from .._meta import (
     PROJECT_NAME,
     SUMMARY,
 )
-from ..api.errors.http_error import (
-    http_error_handler,
-    make_http_error_handler_for_exception,
-)
-from ..api.errors.validation_error import http422_error_handler
 from ..api.root import router as api_router
 from ..api.routes.health import router as health_router
+from ..exceptions.handlers import setup_exception_handlers
 from ..services.function_services import setup_function_services
 from .events import create_start_app_handler, create_stop_app_handler
 from .settings import ApplicationSettings
@@ -73,10 +66,10 @@ def init_app(settings: ApplicationSettings | None = None) -> FastAPI:
 
     # EVENTS
     async def _on_startup() -> None:
-        print(APP_STARTED_BANNER_MSG, flush=True)
+        print(APP_STARTED_BANNER_MSG, flush=True)  # noqa: T201
 
     async def _on_shutdown() -> None:
-        print(APP_FINISHED_BANNER_MSG, flush=True)
+        print(APP_FINISHED_BANNER_MSG, flush=True)  # noqa: T201
 
     app.add_event_handler("startup", _on_startup)
     app.add_event_handler("startup", create_start_app_handler(app))
@@ -84,27 +77,12 @@ def init_app(settings: ApplicationSettings | None = None) -> FastAPI:
     app.add_event_handler("shutdown", create_stop_app_handler(app))
     app.add_event_handler("shutdown", _on_shutdown)
 
-    # ERROR HANDLERS
-    app.add_exception_handler(HTTPException, http_error_handler)
-    app.add_exception_handler(RequestValidationError, http422_error_handler)
-    # SEE https://docs.python.org/3/library/exceptions.html#exception-hierarchy
-    app.add_exception_handler(
-        NotImplementedError,
-        make_http_error_handler_for_exception(
-            status.HTTP_501_NOT_IMPLEMENTED, NotImplementedError
-        ),
-    )
-    app.add_exception_handler(
-        Exception,
-        make_http_error_handler_for_exception(
-            status.HTTP_500_INTERNAL_SERVER_ERROR, Exception
-        ),
-    )
+    # EXCEPTIONS
+    setup_exception_handlers(app)
 
-    # ROUTING
+    # ROUTES
     # healthcheck at / and at /v0/
     app.include_router(health_router)
-
     # api under /v*
     app.include_router(api_router, prefix=f"/{API_VTAG}")
 
@@ -121,7 +99,6 @@ def init_app(settings: ApplicationSettings | None = None) -> FastAPI:
 
         app.add_middleware(BaseHTTPMiddleware, dispatch=_add_process_time_header)
 
-    # gzip middleware
     app.add_middleware(GZipMiddleware)
 
     return app
