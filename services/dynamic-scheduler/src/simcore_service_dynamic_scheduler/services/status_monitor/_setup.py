0cd023f5bb0f47d83ccca35c9a305e22da0c5e8a
Andrei Neagu <neagu@itis.swiss>
2024-05-31 10:20:48 +0200

    added deferred manager and setup for status_monitor

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/status_monitor/_setup.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/status_monitor/_setup.py
@@ -1,12 +1,20 @@
+from datetime import timedelta
+
 from fastapi import FastAPI
+
+from ._monitor import Monitor
 
 
 def setup_status_monitor(app: FastAPI) -> None:
     async def on_startup() -> None:
-        pass
+        app.state.status_monitor = monitor = Monitor(
+            app, check_threshold=timedelta(seconds=1)
+        )
+        await monitor.setup()
 
     async def on_shutdown() -> None:
-        pass
+        monitor: Monitor = app.state.status_monitor
+        await monitor.shutdown()
 
     app.add_event_handler("startup", on_startup)
     app.add_event_handler("shutdown", on_shutdown)
