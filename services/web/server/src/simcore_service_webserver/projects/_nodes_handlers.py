d8afcefbab336ac6c30df34aad9bce73b7655c12
Andrei Neagu <neagu@itis.swiss>
2024-06-03 12:46:47 +0200

    fix serialization

--- services/web/server/src/simcore_service_webserver/projects/_nodes_handlers.py
+++ services/web/server/src/simcore_service_webserver/projects/_nodes_handlers.py
@@ -107,7 +107,7 @@ def _handle_project_nodes_exceptions(handler: Handler):
         except ClustersKeeperNotAvailableError as exc:
             raise web.HTTPServiceUnavailable(reason=f"{exc}") from exc
         except ProjectNodeRequiredInputsNotSetError as exc:
-            raise web.HTTPConflict(reason=f"{exc}", text=f"{exc}") from exc
+            raise web.HTTPConflict(reason=f"{exc}") from exc
 
     return wrapper
 
