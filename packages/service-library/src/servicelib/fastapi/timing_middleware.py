5d4290a2fb817afafed585d8336ea80b830a4dc0
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-06-04 14:40:10 +0200

    🔨 New pytest-simcore `environment_config` fixtures and `utils_postgres` helpers (#5909)

--- packages/service-library/src/servicelib/fastapi/timing_middleware.py
+++ packages/service-library/src/servicelib/fastapi/timing_middleware.py
@@ -1,14 +1,15 @@
-import time
 import logging
+import time
+
 from fastapi import Request
 
-logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
 
 async def add_process_time_header(request: Request, call_next):
     start_time = time.time()
     response = await call_next(request)
     process_time = time.time() - start_time
-    logger.debug("time to process %.2fs", process_time)
+    _logger.debug("time to process %.2fs", process_time)
     response.headers["X-Process-Time"] = str(process_time)
     return response
