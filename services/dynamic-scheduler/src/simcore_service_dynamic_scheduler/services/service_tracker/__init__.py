ebf75f9859f3ebcfee823e664b97baaafd826129
Andrei Neagu <neagu@itis.swiss>
2024-06-05 11:18:39 +0200

    refactor

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/service_tracker/__init__.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/service_tracker/__init__.py
@@ -1,9 +1,10 @@
 from ._api import (
+    can_notify_frontend,
     get_all_tracked,
     get_tracked,
     remove_tracked,
     set_check_status_after_to,
-    set_new_status,
+    set_if_status_changed,
     set_request_as_running,
     set_request_as_stopped,
     set_service_status_task_uid,
@@ -12,11 +13,12 @@ from ._models import TrackedServiceModel
 from ._setup import setup_service_tracker
 
 __all__: tuple[str, ...] = (
+    "can_notify_frontend",
     "get_all_tracked",
     "get_tracked",
     "remove_tracked",
     "set_check_status_after_to",
-    "set_new_status",
+    "set_if_status_changed",
     "set_request_as_running",
     "set_request_as_stopped",
     "set_service_status_task_uid",
