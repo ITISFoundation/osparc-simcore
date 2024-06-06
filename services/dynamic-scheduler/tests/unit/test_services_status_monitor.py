71ca28779a4b3e7e46df4ad1c92aa99668dcd8a4
Andrei Neagu <neagu@itis.swiss>
2024-06-06 11:03:10 +0200

    basic working test

--- services/dynamic-scheduler/tests/unit/test_services_status_monitor.py
+++ services/dynamic-scheduler/tests/unit/test_services_status_monitor.py
@@ -5,6 +5,7 @@ import json
 import re
 from collections.abc import AsyncIterable
 from copy import deepcopy
+from datetime import timedelta
 from typing import Any
 from unittest.mock import AsyncMock
 from uuid import uuid4
@@ -23,8 +24,10 @@ from pytest_simcore.helpers.typing_env import EnvVarsDict
 from settings_library.rabbit import RabbitSettings
 from settings_library.redis import RedisSettings
 from simcore_service_dynamic_scheduler.services.service_tracker import (
+    _api,
     set_request_as_running,
 )
+from simcore_service_dynamic_scheduler.services.status_monitor import _monitor
 from simcore_service_dynamic_scheduler.services.status_monitor._deferred_get_status import (
     DeferredGetStatus,
 )
@@ -50,9 +53,6 @@ def app_environment(
 ) -> EnvVarsDict:
     return app_environment
 
-
-# create service pattern for start & stop with the appropriate type of message types
-# including idle and the ones for legacy services
 
 _DEFAULT_NODE_ID: NodeID = uuid4()
 
@@ -144,6 +144,54 @@ class _ResponseTimeline:
         return status
 
 
+async def _assert_call_to(
+    deferred_status_spies: dict[str, AsyncMock], *, method: str, count: NonNegativeInt
+) -> None:
+    async for attempt in AsyncRetrying(
+        reraise=True,
+        stop=stop_after_delay(1),
+        wait=wait_fixed(0.01),
+        retry=retry_if_exception_type(AssertionError),
+    ):
+        with attempt:
+            call_count = deferred_status_spies[method].call_count
+            assert (
+                call_count == count
+            ), f"Received calls {call_count} != {count} (expected) to '{method}'"
+
+
+async def _assert_result(
+    deferred_status_spies: dict[str, AsyncMock],
+    *,
+    timeline: list[NodeGet | DynamicServiceGet | NodeGetIdle],
+) -> None:
+    async for attempt in AsyncRetrying(
+        reraise=True,
+        stop=stop_after_delay(1),
+        wait=wait_fixed(0.01),
+        retry=retry_if_exception_type(AssertionError),
+    ):
+        with attempt:
+
+            assert deferred_status_spies["on_result"].call_count == len(timeline)
+            assert [
+                x.args[0] for x in deferred_status_spies["on_result"].call_args_list
+            ] == timeline
+
+
+async def _assert_notification_count(
+    mock: AsyncMock, expected_count: NonNegativeInt
+) -> None:
+    async for attempt in AsyncRetrying(
+        reraise=True,
+        stop=stop_after_delay(1),
+        wait=wait_fixed(0.01),
+        retry=retry_if_exception_type(AssertionError),
+    ):
+        with attempt:
+            assert mock.call_count == expected_count
+
+
 @pytest.fixture
 async def mock_director_v2_status(
     app: FastAPI, response_timeline: _ResponseTimeline
@@ -185,7 +233,9 @@ def monitor(mock_director_v2_status: None, app: FastAPI) -> Monitor:
 def deferred_status_spies(mocker: MockerFixture) -> dict[str, AsyncMock]:
     results: dict[str, AsyncMock] = {}
     for method_name in (
+        "start",
         "on_result",
+        "on_created",
         "run",
         "on_finished_with_error",
     ):
@@ -196,74 +246,67 @@ def deferred_status_spies(mocker: MockerFixture) -> dict[str, AsyncMock]:
     return results
 
 
-async def _assert_call_to(
-    deferred_status_spies: dict[str, AsyncMock], *, method: str, count: NonNegativeInt
-) -> None:
-    async for attempt in AsyncRetrying(
-        reraise=True,
-        stop=stop_after_delay(5),
-        wait=wait_fixed(0.01),
-        retry=retry_if_exception_type(AssertionError),
-    ):
-        with attempt:
-            assert deferred_status_spies[method].call_count == count
-
-
-async def _assert_result(
-    deferred_status_spies: dict[str, AsyncMock],
-    *,
-    timeline: list[NodeGet | DynamicServiceGet | NodeGetIdle],
-) -> None:
-    async for attempt in AsyncRetrying(
-        reraise=True,
-        stop=stop_after_delay(5),
-        wait=wait_fixed(0.01),
-        retry=retry_if_exception_type(AssertionError),
-    ):
-        with attempt:
-
-            assert deferred_status_spies["on_result"].call_count == len(timeline)
-            assert [
-                x.args[0] for x in deferred_status_spies["on_result"].call_args_list
-            ] == timeline
-
-
 @pytest.fixture
 def node_id() -> NodeID:
     return _DEFAULT_NODE_ID
 
 
+@pytest.fixture
+def mocked_notify_frontend(mocker: MockerFixture) -> AsyncMock:
+    return mocker.patch(
+        "simcore_service_dynamic_scheduler.services.status_monitor._deferred_get_status.notify_frontend"
+    )
+
+
+@pytest.fixture
+def mock_poll_rate_intervals(mocker: MockerFixture) -> None:
+    mocker.patch.object(_api, "_LOW_RATE_POLL_INTERVAL", timedelta(seconds=0.1))
+    mocker.patch.object(_api, "NORMAL_RATE_POLL_INTERVAL", timedelta(seconds=0.2))
+    mocker.patch.object(_monitor, "NORMAL_RATE_POLL_INTERVAL", timedelta(seconds=0.2))
+
+
 @pytest.mark.parametrize(
-    "response_timeline",
+    "response_timeline, expected_notification_count",
     [
-        _ResponseTimeline([_get_node_get_with("running")]),
-        _ResponseTimeline(
-            [
-                __get_dynamic_service_get_legacy_with("running"),
-                __get_dynamic_service_get_legacy_with("running"),
-            ]
+        # TODO: below
+        # create service pattern for start & stop with the appropriate type of message types
+        # including idle and the ones for legacy services
+        (_ResponseTimeline([_get_node_get_with("running")]), 1),
+        (
+            _ResponseTimeline(
+                [__get_dynamic_service_get_legacy_with("running") for _ in range(10)]
+            ),
+            1,
         ),
-        _ResponseTimeline([__get_dynamic_service_get_new_style_with("running")]),
-        _ResponseTimeline([__get_node_get_idle()]),
+        (_ResponseTimeline([__get_dynamic_service_get_new_style_with("running")]), 1),
+        (_ResponseTimeline([__get_node_get_idle()]), 1),
     ],
 )
-async def test_basic_examples(
+async def test_expected_calls_to_notify_frontend(
+    mock_poll_rate_intervals: None,
+    mocked_notify_frontend: AsyncMock,
     deferred_status_spies: dict[str, AsyncMock],
     app: FastAPI,
     monitor: Monitor,
-    response_timeline: _ResponseTimeline,
     node_id: NodeID,
+    response_timeline: _ResponseTimeline,
+    expected_notification_count: NonNegativeInt,
 ):
     await set_request_as_running(app, node_id)
-
-    # ADD some service to monitor, then mock the API to director-v2 to returns different
-    # statuses based on the times when it is called
 
     entries_in_timeline = len(response_timeline)
 
     for i in range(entries_in_timeline):
-        await monitor._worker_start_get_status_requests()
-        await _assert_call_to(deferred_status_spies, method="on_result", count=i + 1)
+        async for attempt in AsyncRetrying(
+            reraise=True, stop=stop_after_delay(10), wait=wait_fixed(0.1)
+        ):
+            with attempt:
+                # pylint:disable=protected-access
+                await monitor._worker_start_get_status_requests()  # noqa: SLF001
+                for method in ("start", "on_created", "on_result"):
+                    await _assert_call_to(
+                        deferred_status_spies, method=method, count=i + 1
+                    )
 
     await _assert_call_to(
         deferred_status_spies, method="run", count=entries_in_timeline
@@ -273,3 +316,7 @@ async def test_basic_examples(
     )
 
     await _assert_result(deferred_status_spies, timeline=response_timeline.entries)
+
+    await _assert_notification_count(
+        mocked_notify_frontend, expected_notification_count
+    )
