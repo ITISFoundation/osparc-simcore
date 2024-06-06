16ad846994c31823d721118d571b2e18b07e07ce
matusdrobuliak66 <drobuliak66@gmail.com>
2024-06-06 11:25:16 +0200

    modifying settings

---
+++ packages/settings-library/src/settings_library/efs.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from models_library.users import UserID
+from pydantic import Field
+
+from .base import BaseCustomSettings
+
+
+class AwsEfsSettings(BaseCustomSettings):
+    EFS_DNS_NAME: str = Field(
+        description="AWS Elastic File System DNS name",
+        example="fs-xxx.efs.us-east-1.amazonaws.com",
+    )
+    EFS_PROJECT_SPECIFIC_DATA_DIRECTORY: str = Field(default="project-specific-data")
+    EFS_MOUNTED_PATH: Path = Field(
+        default=Path("/data/efs"),
+        description="This is the path where EFS is mounted to the EC2 machine",
+    )
+    EFS_ENABLED_FOR_USERS: list[UserID] = Field(
+        description="This is temporary solution so we can enable it for specific users for testing purpose",
+        example=[1],
+    )
