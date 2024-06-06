17b0ad9a192b70da5af0141760f753f37d23c7aa
Andrei Neagu <neagu@itis.swiss>
2024-06-06 10:44:14 +0200

    basic working version of monitor

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/status_monitor/_monitor.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/status_monitor/_monitor.py
@@ -9,26 +9,30 @@ from models_library.projects_nodes_io import NodeID
 from pydantic import NonNegativeFloat, NonNegativeInt
 from servicelib.utils import logged_gather
 
-from ..service_tracker import (
-    TrackedServiceModel,
-    get_all_tracked,
-    set_check_status_after_to,
-)
+from ..service_tracker import TrackedServiceModel, get_all_tracked, set_scheduled_to_run
 from ._deferred_get_status import DeferredGetStatus
 
 _logger = logging.getLogger(__name__)
 
 _MAX_CONCURRENCY: Final[NonNegativeInt] = 10
+_NEXT_STATUS_CHECK_AFTER: Final[timedelta] = timedelta(seconds=0.1)
+
+
+async def _start_get_status_deferred(
+    app: FastAPI, node_id: NodeID, *, next_check_delay: timedelta
+) -> None:
+    await set_scheduled_to_run(app, node_id, next_check_delay)
+    await DeferredGetStatus.start(node_id=node_id)
 
 
 class Monitor:
-    def __init__(self, app: FastAPI, check_threshold: timedelta) -> None:
+    def __init__(self, app: FastAPI, status_worker_interval: timedelta) -> None:
         self.app = app
-        self.check_threshold = check_threshold
+        self.status_worker_interval = status_worker_interval
 
     @cached_property
-    def check_threshold_seconds(self) -> NonNegativeFloat:
-        return self.check_threshold.total_seconds()
+    def status_worker_interval_seconds(self) -> NonNegativeFloat:
+        return self.status_worker_interval.total_seconds()
 
     async def _worker_start_get_status_requests(self) -> None:
         # NOTE: this worker runs on only once across all instances of the scheduler
@@ -36,43 +40,44 @@ class Monitor:
         models: dict[NodeID, TrackedServiceModel] = await get_all_tracked(self.app)
 
         to_start: list[NodeID] = []
-        to_set_check_status_after: list[NodeID] = []
 
         current_timestamp = arrow.utcnow().timestamp()
 
         for node_id, model in models.items():
-            if (
-                model.check_status_after is None
-                or model.check_status_after > current_timestamp
-            ):
-                # status fetching is required
-                if model.service_status_task_uid is None:
-                    to_start.append(node_id)
-                else:
-                    _logger.info(
-                        "Skipping status check for %s, since already running. Will check later",
-                        node_id,
-                    )
-            if model.check_status_after is None:
-                to_set_check_status_after.append(node_id)
 
-        # for services where the check never ran, make sure we are nto able to start the check while it's running
+            job_not_running = not (
+                model.scheduled_to_run
+                and model.service_status_task_uid is not None
+                and await DeferredGetStatus.is_present(model.service_status_task_uid)
+            )
+            wait_period_finished = current_timestamp > model.check_status_after
+            if job_not_running and wait_period_finished:
+                to_start.append(node_id)
+            else:
+                _logger.info(
+                    "Skipping status check for %s, because: %s or %s",
+                    node_id,
+                    f"{job_not_running=}",
+                    (
+                        f"{wait_period_finished=}"
+                        if wait_period_finished
+                        else f"can_start_in={model.check_status_after - current_timestamp}"
+                    ),
+                )
+
+        _logger.debug("DeferredGetStatus to start: '%s'", to_start)
         await logged_gather(
             *(
-                set_check_status_after_to(self.app, node_id, timedelta(seconds=5))
-                for node_id in to_set_check_status_after
+                _start_get_status_deferred(
+                    self.app, node_id, next_check_delay=_NEXT_STATUS_CHECK_AFTER
+                )
+                for node_id in to_start
             ),
             max_concurrency=_MAX_CONCURRENCY,
         )
 
-        await logged_gather(
-            *(DeferredGetStatus.start(node_id=node_id) for node_id in to_start),
-            max_concurrency=_MAX_CONCURRENCY,
-        )
-
     async def setup(self) -> None:
-        # TODO: start uniquely running task
-        # NOTE: THIS needs to be distributed only 1 at a time
+        # TODO: run uniquely across all processes
         pass
 
     async def shutdown(self) -> None:
