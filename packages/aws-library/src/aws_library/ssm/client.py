f111968a9db57e8e56dea61d80a3ac7469cceb17
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-06-07 17:11:29 +0200

    added commands

--- packages/aws-library/src/aws_library/ssm/client.py
+++ packages/aws-library/src/aws_library/ssm/client.py
@@ -7,8 +7,17 @@ import aioboto3
 from aiobotocore.session import ClientCreatorContext
 from settings_library.ssm import SSMSettings
 from types_aiobotocore_ssm import SSMClient
+from types_aiobotocore_ssm.literals import CommandStatusType
 
 _logger = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True)
+class SSMCommand:
+    name: str
+    command_id: str
+    instance_id: str
+    status: CommandStatusType
 
 
 @dataclass(frozen=True)
@@ -43,3 +52,45 @@ class SimcoreSSMAPI:
             return True
         except Exception:  # pylint: disable=broad-except
             return False
+
+    # a function to send a command via ssm
+    async def send_command(
+        self, instance_id: str, command: str, command_name: str
+    ) -> SSMCommand:
+        # TODO: evaluate using Targets instead of instances as this is limited to 50 instances
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm.html#SSM.Client.send_command
+        response = await self.client.send_command(
+            InstanceIds=tuple(
+                instance_id,
+            ),
+            DocumentName="AWS-RunShellScript",
+            Comment=command_name,
+            Parameters={"commands": [command]},
+        )
+        assert response["Command"]  # nosec
+        assert "Comment" in response["Command"]  # nosec
+        assert "CommandId" in response["Command"]  # nosec
+        assert "Status" in response["Command"]  # nosec
+
+        return SSMCommand(
+            name=response["Command"]["Comment"],
+            command_id=response["Command"]["CommandId"],
+            status=response["Command"]["Status"],
+            instance_id=instance_id,
+        )
+
+    # a function to list commands on an instance
+    async def list_commands_on_instance(self, instance_id: str) -> list[SSMCommand]:
+        response = await self.client.list_commands(
+            InstanceId=instance_id, MaxResults=100
+        )
+        return [
+            SSMCommand(
+                name=command["Comment"],
+                command_id=command["CommandId"],
+                status=command["Status"],
+                instance_id=instance_id,
+            )
+            for command in response["Commands"]
+            if all(_ in command for _ in ("Comment", "CommandId", "Status"))
+        ]
