e3192cef06ce406e22191f740845632b842fcc0f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-06-10 22:11:04 +0200

    missing fixture

--- services/autoscaling/tests/unit/test_modules_buffer_machine_core.py
+++ services/autoscaling/tests/unit/test_modules_buffer_machine_core.py
@@ -61,6 +61,7 @@ async def test_monitor_buffer_machines(
     disabled_rabbitmq: None,
     mocked_redis_server: None,
     enabled_dynamic_mode: EnvVarsDict,
+    mocked_ec2_instances_envs: EnvVarsDict,
     mocked_ec2_server_envs: EnvVarsDict,
     enabled_buffer_pools: EnvVarsDict,
     mocked_ssm_server_envs: EnvVarsDict,
