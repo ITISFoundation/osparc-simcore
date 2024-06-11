57afee48e58a0761a6f3bbcba8e91e3e602f1470
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-06-11 11:19:34 +0200

    added test that check for moto version

--- packages/pytest-simcore/src/pytest_simcore/helpers/utils_moto.py
+++ packages/pytest-simcore/src/pytest_simcore/helpers/utils_moto.py
@@ -1,3 +1,6 @@
+import warnings
+from copy import deepcopy
+
 import aiobotocore.client
 
 # NOTE: send_command is not completely patched by moto, therefore we need this specific mock
@@ -7,27 +10,36 @@ import aiobotocore.client
 orig = aiobotocore.client.AioBaseClient._make_api_call
 
 
-# Mocked botocore _make_api_call function
-def mock_make_api_call(self, operation_name, kwarg):
+# Mocked aiobotocore _make_api_call function
+async def patched_aiobotocore_make_api_call(self, operation_name, api_params):
     # For example for the Access Analyzer service
     # As you can see the operation_name has the list_analyzers snake_case form but
     # we are using the ListAnalyzers form.
     # Rationale -> https://github.com/boto/botocore/blob/develop/botocore/client.py#L810:L816
-    print("I AM HERE")
-    print(operation_name)
-    if operation_name == "ListAnalyzers":
-        return {
-            "analyzers": [
-                {
-                    "arn": "ARN",
-                    "name": "Test Analyzer",
-                    "status": "Enabled",
-                    "findings": 0,
-                    "tags": "",
-                    "type": "ACCOUNT",
-                    "region": "eu-west-1",
-                }
-            ]
-        }
+    if operation_name == "SendCommand":
+        warnings.warn(
+            "moto is missing SendCommand mock with InstanceIds as Targets, therefore it is manually mocked."
+            " TIP: periodically check if it gets updated https://docs.getmoto.org/en/latest/docs/services/ssm.html#ssm",
+            UserWarning,
+            stacklevel=1,
+        )
+
+        assert "Targets" in api_params, "Targets is missing in the API call"
+        assert (
+            len(api_params["Targets"]) == 1
+        ), "Targets for patched SendCommand should have only one item"
+        target_data = api_params["Targets"][0]
+        assert "Key" in target_data
+        assert "Values" in target_data
+        target_key = target_data["Key"]
+        assert (
+            target_key == "InstanceIds"
+        ), "Targets for patched SendCommand should have InstanceIds as key"
+        instance_ids = target_data["Values"]
+        new_api_params = deepcopy(api_params)
+        new_api_params.pop("Targets")
+        new_api_params["InstanceIds"] = instance_ids
+        return await orig(self, operation_name, new_api_params)
+
     # If we don't want to patch the API call
-    return orig(self, operation_name, kwarg)
+    return await orig(self, operation_name, api_params)
