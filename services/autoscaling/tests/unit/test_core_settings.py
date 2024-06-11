ddc0488665f6970c52a4cc4d81870bea5f4e80c2
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-06-11 18:11:37 +0200

    simplify

--- services/autoscaling/tests/unit/test_core_settings.py
+++ services/autoscaling/tests/unit/test_core_settings.py
@@ -26,7 +26,7 @@ def instance_type_with_invalid_boot_script(
     mock_env_devel_environment: EnvVarsDict,
     monkeypatch: pytest.MonkeyPatch,
     faker: Faker,
-    ec2_instances: list[InstanceTypeType],
+    aws_allowed_ec2_instance_type_names: list[InstanceTypeType],
 ) -> EnvVarsDict:
     return setenvs_from_dict(
         monkeypatch,
@@ -37,7 +37,7 @@ def instance_type_with_invalid_boot_script(
                         "ami_id": faker.pystr(),
                         "custom_boot_scripts": ['ls"'],
                     }
-                    for ec2_type_name in ec2_instances
+                    for ec2_type_name in aws_allowed_ec2_instance_type_names
                 }
             ),
         },
