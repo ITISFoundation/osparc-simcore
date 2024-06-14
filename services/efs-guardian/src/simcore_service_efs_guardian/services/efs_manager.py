c33a6741b07dcfe1a9dfefcb2ac3da922562a92a
matusdrobuliak66 <drobuliak66@gmail.com>
2024-06-14 13:28:52 +0200

    efs guardian adding specific user

--- services/efs-guardian/src/simcore_service_efs_guardian/services/efs_manager.py
+++ services/efs-guardian/src/simcore_service_efs_guardian/services/efs_manager.py
@@ -1,9 +1,13 @@
+import os
 from dataclasses import dataclass
 from pathlib import Path
 
 from fastapi import FastAPI
 from models_library.projects import ProjectID
 from models_library.projects_nodes_io import NodeID
+
+_EFS_LINUX_USER = 8006
+_EFS_GROUP_LINUX_GROUP = 8106
 
 
 @dataclass(frozen=True)
@@ -26,6 +30,13 @@ class EfsManager:
         _dir_path = self._efs_mounted_path / self._project_specific_data_base_directory
         Path.mkdir(_dir_path, parents=True, exist_ok=True)
 
+        # Temporary
+        await self.create_project_specific_data_dir(
+            project_id="24acabca-e57e-428b-aa69-7aaf33c5425f",
+            node_id="24acabca-e57e-428b-aa69-7aaf33c5425f",
+            storage_directory_name="matus",
+        )
+
     async def create_project_specific_data_dir(
         self, project_id: ProjectID, node_id: NodeID, storage_directory_name: str
     ) -> Path:
@@ -36,5 +47,12 @@ class EfsManager:
             / f"{node_id}"
             / f"{storage_directory_name}"
         )
+        # Ensure the directory exists with the right parents
         Path.mkdir(_dir_path, parents=True, exist_ok=True)
+        # Change the owner to user id 8006(efs) and group id 8106(efs-group)
+        os.chown(_dir_path, _EFS_LINUX_USER, _EFS_GROUP_LINUX_GROUP)
+        # Set directory permissions to allow group write access
+        os.chmod(
+            _dir_path, 0o770
+        )  # This gives rwx permissions to user and group, and nothing to others
         return _dir_path
