942d32448195c94aa5eed7f3840952bc7aea1446
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-06-24 15:25:47 +0200

    improvements

--- packages/pytest-simcore/src/pytest_simcore/file_extra.py
+++ packages/pytest-simcore/src/pytest_simcore/file_extra.py
@@ -1,6 +1,5 @@
-import random
+from collections.abc import Callable
 from pathlib import Path
-from typing import Callable
 
 import pytest
 from faker import Faker
@@ -27,52 +26,43 @@ def create_file_of_size(tmp_path: Path, faker: Faker) -> Callable[[ByteSize], Pa
 
 @pytest.fixture
 def create_folder_of_size_with_multiple_files(
-    tmp_path: Path,
+    tmp_path: Path, faker: Faker
 ) -> Callable[[ByteSize, ByteSize], Path]:
     def _create_folder_of_size_with_multiple_files(
         directory_size: ByteSize, file_max_size: ByteSize
     ) -> Path:
         # Helper function to create random files and directories
 
-        def create_random_content(
-            current_dir: Path, remaining_size: ByteSize, path_name_suffix: int
-        ) -> ByteSize:
+        def create_random_content(base_dir: Path, remaining_size: ByteSize) -> ByteSize:
             if remaining_size <= 0:
                 return remaining_size
 
             # Decide to create a file or a subdirectory
-            if (
-                random.choice([True, False]) or remaining_size < 512
-            ):  # 512 bytes threshold for files
-                # Create a file
-                file_size = random.randint(
-                    1, min(remaining_size, file_max_size)
-                )  # max file size 1MB
-                file_path = current_dir / f"file_{path_name_suffix}.txt"
-                with file_path.open(file_path, "wb") as fp:
-                    fp.write(f"I am a {file_size.human_readable()} file".encode())
-                    fp.truncate(file_size)
-                remaining_size -= file_size
-            else:
-                # Create a subdirectory
-                sub_dir_path = current_dir / f"dir_{path_name_suffix}"
-                sub_dir_path.mkdir()
-                remaining_size = create_random_content(
-                    sub_dir_path, remaining_size, path_name_suffix
-                )
+            # Create a file
+            file_size = ByteSize(
+                faker.pyint(min_value=1, max_value=min(remaining_size, file_max_size))
+            )  # max file size 1MB
+            file_path = base_dir / f"{faker.file_path(absolute=False)}"
+            file_path.parent.mkdir(parents=True, exist_ok=True)
+            assert not file_path.exists()
+            with file_path.open("wb") as fp:
+                fp.write(f"I am a {file_size.human_readable()} file".encode())
+                fp.truncate(file_size)
+            assert file_path.exists()
 
-            path_name_suffix += 1
-
-            return remaining_size
+            return ByteSize(remaining_size - file_size)
 
         # Recursively create content in the temporary directory
         remaining_size = directory_size
-        path_name_suffix = 1
+        print(
+            f"--> about to create a directory of {directory_size.human_readable()} random files writen in {tmp_path}"
+        )
         while remaining_size > 0:
-            remaining_size = create_random_content(
-                tmp_path, remaining_size, path_name_suffix
-            )
-
+            remaining_size = create_random_content(tmp_path, remaining_size)
+            print(f"<-- still {remaining_size.human_readable()} to write...")
+        print(
+            f"<-- directory of {directory_size.human_readable()} random files writen in {tmp_path}"
+        )
         return tmp_path
 
     return _create_folder_of_size_with_multiple_files
