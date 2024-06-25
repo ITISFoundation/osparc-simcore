d91e7f84cece5e88d34af634be85f4bc029c2633
Odei Maiz <maiz@itis.swiss>
2024-06-25 14:43:22 +0200

    converters

--- services/static-webserver/client/source/class/osparc/ui/basic/SVGImage.js
+++ services/static-webserver/client/source/class/osparc/ui/basic/SVGImage.js
@@ -62,8 +62,39 @@ qx.Class.define("osparc.ui.basic.SVGImage", {
   statics: {
     rgbToCSSFilter: function(rgb) {
       const [r, g, b] = rgb.split(",").map(Number);
-      // Values below are based on approximations and may not be perfect
-      return `invert(${100 - r/2.55}%) sepia(${g/2.55}%) saturate(${b/2.55}%) hue-rotate(${r - g}deg)`;
+
+      let [rf, gf, bf] = [r / 255, g / 255, b / 255];
+      let [mi, ma] = [Math.min(rf, gf, bf), Math.max(rf, gf, bf)];
+      let [h, s, l] = [0, 0, (mi + ma) / 2];
+
+      if (mi !== ma) {
+        s = l < 0.5 ? (ma - mi) / (ma + mi) : (ma - mi) / (2 - ma - mi);
+        switch (ma) {
+          case rf:
+            h = (gf - bf) / (ma - mi);
+            break;
+          case gf:
+            h = 2 + (bf - rf) / (ma - mi);
+            break;
+          case bf:
+            h = 4 + (rf - gf) / (ma - mi);
+            break;
+        }
+      }
+
+      h = Math.round(h * 60);
+      if (h < 0) {
+        h += 360;
+      }
+      s = Math.round(s * 100);
+      l = Math.round(l * 100);
+
+      const invertValue = l2 => 100 - l2;
+      const sepiaValue = s2 => s2;
+      const saturateValue = s3 => s3;
+      const brightnessValue = l3 => l3;
+      const contrastValue = l4 => l4 > 50 ? 50 : l4;
+      return `invert(${invertValue(l)}%) sepia(${sepiaValue(s)}%) saturate(${saturateValue(s)}%) hue-rotate(${h}deg) brightness(${brightnessValue(l)}%) contrast(${contrastValue(l)}%)`;
     }
   },
 
