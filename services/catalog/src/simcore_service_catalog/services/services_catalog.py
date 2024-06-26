0f02616344227ed24a98e285de0642657212a30e
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-06-26 16:12:39 +0200

    connecting rpc interface

--- services/catalog/src/simcore_service_catalog/services/services_catalog.py
+++ services/catalog/src/simcore_service_catalog/services/services_catalog.py
@@ -1,13 +1,24 @@
+from models_library.api_schemas_catalog.services import DEVServiceGet
+from models_library.products import ProductName
+from models_library.rest_pagination import PageLimitInt
+from models_library.users import UserID
+from pydantic import NonNegativeInt
+
 from ..db.repositories.services import ServicesRepository
 
 
-async def list_services_with_history(
-    services_repo: ServicesRepository,
-    limit: int | None = None,
-    offset: int | None = None,
-):
+async def list_services_paginated(
+    repo: ServicesRepository,
+    product_name: ProductName,
+    user_id: UserID,
+    limit: PageLimitInt | None,
+    offset: NonNegativeInt = 0,
+) -> tuple[NonNegativeInt, list[DEVServiceGet]]:
+
     # TODO: add access-rights needed
 
     # user_id
-    items = await services_repo.list_services_with_history(limit=limit, offset=offset)
+    items = await repo.list_services_with_history(
+        product_name=product_name, user_id=user_id, limit=limit, offset=offset
+    )
     # TODO: add more info on latest version!
