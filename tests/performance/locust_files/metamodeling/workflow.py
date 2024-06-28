0e7353b53853df332ab1aaaf39d88156fb974ab9
Mads Bisgaard <bisgaard@itis.swiss>
2024-06-28 16:59:06 +0200

    catch username and password from env

--- tests/performance/locust_files/metamodeling/workflow.py
+++ tests/performance/locust_files/metamodeling/workflow.py
@@ -1,9 +1,19 @@
-from locust import HttpUser
+from locust import HttpUser, task
+from pydantic import Field
+from pydantic_settings import BaseSettings
+from requests.auth import HTTPBasicAuth
 from urllib3 import PoolManager, Retry
+
+
+class UserSettings(BaseSettings):
+    username: str = Field(default=...)
+    password: str = Field(default=...)
 
 
 class MetaModelingUser(HttpUser):
     def __init__(self, *args, **kwargs):
+        config = UserSettings()
+        self._auth = HTTPBasicAuth(username=config.username, password=config.password)
         retry_strategy = Retry(
             total=4,
             backoff_factor=4.0,
@@ -25,3 +35,11 @@ class MetaModelingUser(HttpUser):
         super().__init__(
             *args, **kwargs, pool_manager=PoolManager(key_retries=retry_strategy)
         )
+
+    def on_start(self) -> None:
+        response = self.client.get("/v0/me", auth=self._auth)
+        response.raise_for_status()
+
+    @task
+    def create_and_run_job(self):
+        self.client.get("/v0/me", auth=self._auth)
