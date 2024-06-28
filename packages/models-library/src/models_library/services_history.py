245a659f736c1638c52e36e978a33b7eb707828c
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-06-28 11:12:17 +0200

    @sanderegg review: rename fields

--- packages/models-library/src/models_library/services_history.py
+++ packages/models-library/src/models_library/services_history.py
@@ -19,9 +19,15 @@ class ServiceRelease(BaseModel):
     # from ServiceMetaDataPublished
     version: ServiceVersion
     version_display: str | None = Field(default=None)
-    released_at: datetime | None = Field(default=None)
-    deprecated_at: datetime | None = Field(
-        default=None, description="If deprecated, it sets the deprecation date"
+    released: datetime | None = Field(
+        default=None, description="When provided, it indicates the release timestamp"
+    )
+    retired: datetime | None = Field(
+        default=None,
+        description="whether this service is planned to be retired. "
+        "If None, the service is still active. "
+        "If now<retired then the service is deprecated. "
+        "If retired<now then the service is retired and should not be used. ",
     )
 
     # computed compatibility
@@ -38,8 +44,8 @@ class ServiceRelease(BaseModel):
                 {
                     "version": "0.9.1",
                     "version_display": "Matterhorn",
-                    "released_at": "2024-06-20T18:49:17",
-                    "deprecated_at": "2034-06-20T00:00:00",
+                    "released": "2024-06-20T18:49:17",
+                    "retired": "2034-06-20T00:00:00",
                     "compatibility": {"can_update_to": "0.9.10"},
                 },
             ]
