b425a5f1f83cebd103203bc8cd084d23c6f250f7
Mads Bisgaard <bisgaard@itis.swiss>
2024-06-28 16:28:30 +0200

    add initial file for metamodeling load test

---
+++ tests/performance/locust_files/metamodeling/workflow.py
@@ -0,0 +1,26 @@
+from locust import HttpUser
+from urllib3 import PoolManager, Retry
+
+
+class MetaModelingUser(HttpUser):
+    def __init__(self, *args, **kwargs):
+        retry_strategy = Retry(
+            total=4,
+            backoff_factor=4.0,
+            status_forcelist={429, 503, 504},
+            allowed_methods={
+                "DELETE",
+                "GET",
+                "HEAD",
+                "OPTIONS",
+                "PUT",
+                "TRACE",
+                "POST",
+                "PATCH",
+                "CONNECT",
+            },
+            respect_retry_after_header=True,
+            raise_on_status=True,
+        )
+        pool_manager = PoolManager(key_retries=retry_strategy)
+        super().__init__(*args, **kwargs, pool_manager=pool_manager)
