689b94db9a96711a9fc446d9ecc460d049e74837
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-07-01 11:58:24 +0200

    cleanup

--- packages/pytest-simcore/src/pytest_simcore/helpers/s3.py
+++ packages/pytest-simcore/src/pytest_simcore/helpers/s3.py
@@ -9,10 +9,11 @@ from aiohttp import ClientSession
 from aws_library.s3 import MultiPartUploadLinks
 from models_library.api_schemas_storage import ETag, FileUploadSchema, UploadedPart
 from pydantic import AnyUrl, ByteSize, parse_obj_as
-from pytest_simcore.helpers.logging import log_context
 from servicelib.aiohttp import status
 from servicelib.utils import limited_as_completed, logged_gather
 from types_aiobotocore_s3 import S3Client
+
+from .logging import log_context
 
 _SENDER_CHUNK_SIZE: Final[int] = parse_obj_as(ByteSize, "16Mib")
 
