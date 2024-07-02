829a1cbfa8bfd85c6aa654d5e5a05f05b7ad91f4
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-07-02 18:20:14 +0200

    minor

--- services/catalog/src/simcore_service_catalog/api/rpc/routes.py
+++ services/catalog/src/simcore_service_catalog/api/rpc/routes.py
@@ -12,7 +12,6 @@ _logger = logging.getLogger(__name__)
 def setup_rpc_api_routes(app: FastAPI) -> None:
     async def _on_startup() -> None:
         rpc_server = get_rabbitmq_rpc_server(app)
-        for router in (_services.router,):
-            await rpc_server.register_router(router, CATALOG_RPC_NAMESPACE, app)
+        await rpc_server.register_router(_services.router, CATALOG_RPC_NAMESPACE, app)
 
     app.add_event_handler("startup", _on_startup)
