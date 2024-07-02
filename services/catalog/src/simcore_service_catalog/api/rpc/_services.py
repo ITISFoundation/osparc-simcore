34abc1e391a3592a86227bac267f496da11181a8
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-07-02 16:21:13 +0200

    validator

--- services/catalog/src/simcore_service_catalog/api/rpc/_services.py
+++ services/catalog/src/simcore_service_catalog/api/rpc/_services.py
@@ -10,19 +10,17 @@ from models_library.rpc_pagination import (
 )
 from models_library.services_types import ServiceKey, ServiceVersion
 from models_library.users import UserID
-from pydantic import NonNegativeInt, ValidationError, parse_obj_as, validate_arguments
+from pydantic import NonNegativeInt, ValidationError, parse_obj_as
 from servicelib.logging_utils import log_decorator
 from servicelib.rabbitmq import RPCRouter
 
 _logger = logging.getLogger(__name__)
-_config = {"arbitrary_types_allowed": True}
 
 router = RPCRouter()
 
 
 @router.expose(reraise_if_error_type=(ValidationError,))
 @log_decorator(_logger, level=logging.DEBUG)
-@validate_arguments(config=_config)
 async def list_services_paginated(
     app: FastAPI,
     *,
@@ -50,7 +48,6 @@ async def list_services_paginated(
 
 @router.expose(reraise_if_error_type=(ValidationError,))
 @log_decorator(_logger, level=logging.DEBUG)
-@validate_arguments(config=_config)
 async def get_service(
     app: FastAPI,
     *,
@@ -73,7 +70,6 @@ async def get_service(
 
 @router.expose(reraise_if_error_type=(ValidationError,))
 @log_decorator(_logger, level=logging.DEBUG)
-@validate_arguments(config=_config)
 async def update_service(
     app: FastAPI,
     *,
