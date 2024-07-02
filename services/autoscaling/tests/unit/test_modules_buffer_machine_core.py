ced1c2d206448e95e13d101d78e9ce035ea16398
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-07-02 19:00:19 +0200

    make fixtures usable with external env

--- services/autoscaling/tests/unit/test_modules_buffer_machine_core.py
+++ services/autoscaling/tests/unit/test_modules_buffer_machine_core.py
@@ -5,11 +5,12 @@
 
 import json
 import logging
-from typing import Any
+from typing import Any, Sequence, cast
 from unittest import mock
 
 import pytest
 import tenacity
+from aws_library.ec2.models import EC2InstanceBootSpecific
 from faker import Faker
 from fastapi import FastAPI
 from models_library.docker import DockerGenericTag
@@ -21,7 +22,7 @@ from pytest_simcore.helpers.aws_ec2 import (
 from pytest_simcore.helpers.logging import log_context
 from pytest_simcore.helpers.monkeypatch_envs import EnvVarsDict, setenvs_from_dict
 from pytest_simcore.helpers.moto import patched_aiobotocore_make_api_call
-from simcore_service_autoscaling.core.settings import get_application_settings
+from simcore_service_autoscaling.core.settings import EC2InstancesSettings
 from simcore_service_autoscaling.modules.auto_scaling_mode_dynamic import (
     DynamicAutoscaling,
 )
@@ -35,11 +36,6 @@ from types_aiobotocore_ec2.type_defs import FilterTypeDef
 
 
 @pytest.fixture
-def buffer_count(faker: Faker) -> int:
-    return faker.pyint(min_value=1, max_value=9)
-
-
-@pytest.fixture
 def pre_pull_images() -> list[DockerGenericTag]:
     return parse_obj_as(
         list[DockerGenericTag],
@@ -50,21 +46,6 @@ def pre_pull_images() -> list[DockerGenericTag]:
             "asd",
         ],
     )
-
-
-@pytest.fixture
-def ec2_instances_allowed_types(
-    faker: Faker,
-    pre_pull_images: list[DockerGenericTag],
-    buffer_count: int,
-) -> dict[InstanceTypeType, Any]:
-    return {
-        "t2.micro": {
-            "ami_id": faker.pystr(),
-            "pre_pull_images": pre_pull_images,
-            "buffer_count": buffer_count,
-        }
-    }
 
 
 @pytest.fixture
@@ -154,6 +135,7 @@ async def test_monitor_buffer_machines(
     initialized_app: FastAPI,
     ec2_client: EC2Client,
     ec2_instances_allowed_types: dict[InstanceTypeType, Any],
+    ec2_instance_custom_tags: dict[str, str],
     buffer_count: int,
 ):
     # 0. we have no instances now
@@ -170,7 +152,9 @@ async def test_monitor_buffer_machines(
         expected_num_instances=buffer_count,
         expected_instance_type=next(iter(ec2_instances_allowed_types)),
         expected_instance_state="running",
-        expected_additional_tag_keys=[],
+        expected_additional_tag_keys=[
+            *ec2_instance_custom_tags.keys(),
+        ],
         instance_filters=None,
     )
 
@@ -184,7 +168,11 @@ async def test_monitor_buffer_machines(
         expected_num_instances=buffer_count,
         expected_instance_type=next(iter(ec2_instances_allowed_types)),
         expected_instance_state="running",
-        expected_additional_tag_keys=["pulling", "ssm-command-id"],
+        expected_additional_tag_keys=[
+            "pulling",
+            "ssm-command-id",
+            *ec2_instance_custom_tags.keys(),
+        ],
         instance_filters=None,
     )
 
@@ -198,7 +186,9 @@ async def test_monitor_buffer_machines(
         expected_num_instances=buffer_count,
         expected_instance_type=next(iter(ec2_instances_allowed_types)),
         expected_instance_state="stopped",
-        expected_additional_tag_keys=[],
+        expected_additional_tag_keys=[
+            *ec2_instance_custom_tags.keys(),
+        ],
         instance_filters=None,
     )
 
@@ -206,6 +196,41 @@ async def test_monitor_buffer_machines(
 @pytest.fixture
 def ec2_pytest_tag_key() -> str:
     return "pytest"
+
+
+@pytest.fixture
+def ec2_instances_allowed_types(
+    faker: Faker,
+    pre_pull_images: list[DockerGenericTag],
+    buffer_count: int,
+    external_ec2_instances_allowed_types: None | dict[str, EC2InstanceBootSpecific],
+) -> dict[InstanceTypeType, Any]:
+    if not external_ec2_instances_allowed_types:
+        return {
+            "t2.micro": {
+                "ami_id": faker.pystr(),
+                "pre_pull_images": pre_pull_images,
+                "buffer_count": buffer_count,
+            }
+        }
+
+    allowed_ec2_types = external_ec2_instances_allowed_types
+    allowed_ec2_types_with_buffer_defined = dict(
+        filter(
+            lambda instance_type_and_settings: instance_type_and_settings[
+                1
+            ].buffer_count
+            > 0,
+            allowed_ec2_types.items(),
+        )
+    )
+    assert (
+        len(allowed_ec2_types_with_buffer_defined) == 1
+    ), "more than one type with buffer is disallowed in this test!"
+    return {
+        cast(InstanceTypeType, k): v
+        for k, v in allowed_ec2_types_with_buffer_defined.items()
+    }
 
 
 @pytest.fixture
@@ -221,26 +246,45 @@ def with_ec2_pytest_tag_key_as_custom_tag(
     return app_environment
 
 
-async def test_monitor_buffer_machines_against_aws(
-    disabled_rabbitmq: None,
-    mocked_redis_server: None,
-    external_envfile_dict: EnvVarsDict,
-    with_ec2_pytest_tag_key_as_custom_tag: EnvVarsDict,
-    ec2_client: EC2Client,
-    ec2_pytest_tag_key: str,
-    initialized_app: FastAPI,
-):
-    if not external_envfile_dict:
-        pytest.skip(
-            "This test is only for use directly with AWS server, please define --external-envfile"
-        )
+@pytest.fixture
+def instance_type_filters(
+    with_ec2_pytest_tag_key_as_custom_tag: EnvVarsDict, ec2_pytest_tag_key: str
+) -> Sequence[FilterTypeDef]:
+    return [
+        FilterTypeDef(
+            Name="tag-key",
+            Values=[
+                ec2_pytest_tag_key,
+            ],
+        ),
+        FilterTypeDef(
+            Name="instance-state-name",
+            Values=["pending", "running", "stopped"],
+        ),
+    ]
 
-    app_settings = get_application_settings(initialized_app)
-    assert app_settings.AUTOSCALING_EC2_INSTANCES
-    allowed_ec2_types = (
-        app_settings.AUTOSCALING_EC2_INSTANCES.EC2_INSTANCES_ALLOWED_TYPES
-    )
-    # in this test we consider only one type to have buffers
+
+@pytest.fixture
+def external_ec2_instances_allowed_types(
+    external_envfile_dict: EnvVarsDict, monkeypatch: pytest.MonkeyPatch
+) -> None | dict[str, EC2InstanceBootSpecific]:
+    if not external_envfile_dict:
+        return None
+    with monkeypatch.context() as patch:
+        setenvs_from_dict(patch, {**external_envfile_dict})
+        settings = EC2InstancesSettings.create_from_envs()
+    return settings.EC2_INSTANCES_ALLOWED_TYPES
+
+
+@pytest.fixture
+def buffer_count(
+    faker: Faker,
+    external_ec2_instances_allowed_types: None | dict[str, EC2InstanceBootSpecific],
+) -> int:
+    if not external_ec2_instances_allowed_types:
+        return faker.pyint(min_value=1, max_value=9)
+
+    allowed_ec2_types = external_ec2_instances_allowed_types
     allowed_ec2_types_with_buffer_defined = dict(
         filter(
             lambda instance_type_and_settings: instance_type_and_settings[
@@ -253,56 +297,120 @@ async def test_monitor_buffer_machines_against_aws(
     assert (
         len(allowed_ec2_types_with_buffer_defined) == 1
     ), "more than one type with buffer is disallowed in this test!"
+    return next(iter(allowed_ec2_types_with_buffer_defined.values())).buffer_count
 
-    buffer_count = next(
-        iter(allowed_ec2_types_with_buffer_defined.values())
-    ).buffer_count
+
+async def test_monitor_buffer_machines_against_aws(
+    disabled_rabbitmq: None,
+    mocked_redis_server: None,
+    external_envfile_dict: EnvVarsDict,
+    buffer_count: int,
+    ec2_instances_allowed_types: dict[InstanceTypeType, Any],
+    instance_type_filters: Sequence[FilterTypeDef],
+    ec2_client: EC2Client,
+    ec2_pytest_tag_key: str,
+    initialized_app: FastAPI,
+):
+    if not external_envfile_dict:
+        pytest.skip(
+            "This test is only for use directly with AWS server, please define --external-envfile"
+        )
+
+    # in this test we consider only one type to have buffers
 
     # 0. we have no instances now
-    instance_type_filters = [
-        FilterTypeDef(
-            Name="tag-key",
-            Values=[
-                ec2_pytest_tag_key,
-            ],
-        ),
-        FilterTypeDef(
-            Name="instance-state-name",
-            Values=["pending", "running", "stopped"],
-        ),
-    ]
     all_instances = await ec2_client.describe_instances(Filters=instance_type_filters)
     assert not all_instances["Reservations"]
 
     # 1. run, this will create as many buffer machines as needed
-    await monitor_buffer_machines(
-        initialized_app, auto_scaling_mode=DynamicAutoscaling()
-    )
-    # this takes awhile, so we need to wait for the instances to be created
-    async for attempt in AsyncRetrying(
-        wait=tenacity.wait_fixed(5),
-        stop=tenacity.stop_after_delay(120),
-        retry=tenacity.retry_if_exception_type(AssertionError),
-        reraise=True,
-    ):
-        with attempt, log_context(
-            logging.INFO,
-            f"waiting for {buffer_count} buffer instances to be running (trial: {attempt.retry_state.attempt_number})",
+    with log_context(logging.INFO, "create buffer machines"):
+        await monitor_buffer_machines(
+            initialized_app, auto_scaling_mode=DynamicAutoscaling()
+        )
+        with log_context(
+            logging.INFO, f"waiting for {buffer_count} buffer instances to be running"
         ) as ctx:
-            await assert_autoscaled_dynamic_warm_pools_ec2_instances(
-                ec2_client,
-                expected_num_reservations=1,
-                expected_num_instances=buffer_count,
-                expected_instance_type=next(
-                    iter(allowed_ec2_types_with_buffer_defined)
-                ),
-                expected_instance_state="running",
-                expected_additional_tag_keys=[ec2_pytest_tag_key],
-                instance_filters=instance_type_filters,
-            )
-            ctx.logger.info(
-                "%s",
-                f"{buffer_count} buffer now running after {attempt.retry_state.retry_object.statistics}",
-            )
+            async for attempt in AsyncRetrying(
+                wait=tenacity.wait_fixed(5),
+                stop=tenacity.stop_after_delay(120),
+                retry=tenacity.retry_if_exception_type(AssertionError),
+                reraise=True,
+                before_sleep=tenacity.before_sleep_log(ctx.logger, logging.INFO),
+                after=tenacity.after_log(ctx.logger, logging.INFO),
+            ):
+                with attempt:
+                    await assert_autoscaled_dynamic_warm_pools_ec2_instances(
+                        ec2_client,
+                        expected_num_reservations=1,
+                        expected_num_instances=buffer_count,
+                        expected_instance_type=next(iter(ec2_instances_allowed_types)),
+                        expected_instance_state="running",
+                        expected_additional_tag_keys=[ec2_pytest_tag_key],
+                        instance_filters=instance_type_filters,
+                    )
+                    ctx.logger.info(
+                        "%s",
+                        f"{buffer_count} buffer now running after {attempt.retry_state.retry_object.statistics}",
+                    )
 
-    ctx.logger.info("we are done waiting!")
+    # 2. this should now run a SSM command for pulling
+    with log_context(logging.INFO, "run SSM commands for pulling") as ctx:
+        async for attempt in AsyncRetrying(
+            wait=tenacity.wait_fixed(5),
+            stop=tenacity.stop_after_delay(120),
+            retry=tenacity.retry_if_exception_type(AssertionError),
+            reraise=True,
+            before_sleep=tenacity.before_sleep_log(ctx.logger, logging.INFO),
+            after=tenacity.after_log(ctx.logger, logging.INFO),
+        ):
+            with attempt:
+                await monitor_buffer_machines(
+                    initialized_app, auto_scaling_mode=DynamicAutoscaling()
+                )
+                await assert_autoscaled_dynamic_warm_pools_ec2_instances(
+                    ec2_client,
+                    expected_num_reservations=1,
+                    expected_num_instances=buffer_count,
+                    expected_instance_type=next(iter(ec2_instances_allowed_types)),
+                    expected_instance_state="running",
+                    expected_additional_tag_keys=[
+                        "pulling",
+                        "ssm-command-id",
+                        ec2_pytest_tag_key,
+                    ],
+                    instance_filters=instance_type_filters,
+                )
+                ctx.logger.info(
+                    "%s",
+                    f"command set after {attempt.retry_state.retry_object.statistics}",
+                )
+
+    # 3. is the command finished?
+    with log_context(logging.INFO, "wait for SSM commands for to finish") as ctx:
+        async for attempt in AsyncRetrying(
+            wait=tenacity.wait_fixed(5),
+            stop=tenacity.stop_after_delay(120),
+            retry=tenacity.retry_if_exception_type(AssertionError),
+            reraise=True,
+            before_sleep=tenacity.before_sleep_log(ctx.logger, logging.INFO),
+            after=tenacity.after_log(ctx.logger, logging.INFO),
+        ):
+            with attempt:
+                await monitor_buffer_machines(
+                    initialized_app, auto_scaling_mode=DynamicAutoscaling()
+                )
+                await assert_autoscaled_dynamic_warm_pools_ec2_instances(
+                    ec2_client,
+                    expected_num_reservations=1,
+                    expected_num_instances=buffer_count,
+                    expected_instance_type=next(iter(ec2_instances_allowed_types)),
+                    expected_instance_state="stopped",
+                    expected_additional_tag_keys=[
+                        ec2_pytest_tag_key,
+                    ],
+                    instance_filters=instance_type_filters,
+                )
+                ctx.logger.info(
+                    "%s",
+                    f"command set after {attempt.retry_state.retry_object.statistics}",
+                )
