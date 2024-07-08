a0108868f64b797a454ed9a46ebd936dc59cf6ee
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-07-08 11:36:25 +0200

    bad merge

--- packages/aws-library/src/aws_library/ec2/client.py
+++ packages/aws-library/src/aws_library/ec2/client.py
@@ -2,7 +2,7 @@ import contextlib
 import logging
 from collections.abc import Iterable, Sequence
 from dataclasses import dataclass
-from typing import Sequence, cast
+from typing import cast
 
 import aioboto3
 import botocore.exceptions
--- packages/pytest-simcore/src/pytest_simcore/helpers/moto.py
+++ packages/pytest-simcore/src/pytest_simcore/helpers/moto.py
@@ -55,42 +55,6 @@ def _patch_describe_instance_information(
     return {"InstanceInformationList": [{"PingStatus": "Online"}]}
 
 
-def _patch_send_command(self, operation_name, api_params):
-    warnings.warn(
-        "moto is missing SendCommand mock with InstanceIds as Targets, therefore it is manually mocked."
-        " TIP: periodically check if it gets updated https://docs.getmoto.org/en/latest/docs/services/ssm.html#ssm",
-        UserWarning,
-        stacklevel=1,
-    )
-
-    assert "Targets" in api_params, "Targets is missing in the API call"
-    assert (
-        len(api_params["Targets"]) == 1
-    ), "Targets for patched SendCommand should have only one item"
-    target_data = api_params["Targets"][0]
-    assert "Key" in target_data
-    assert "Values" in target_data
-    target_key = target_data["Key"]
-    assert (
-        target_key == "InstanceIds"
-    ), "Targets for patched SendCommand should have InstanceIds as key"
-    instance_ids = target_data["Values"]
-    new_api_params = deepcopy(api_params)
-    new_api_params.pop("Targets")
-    new_api_params["InstanceIds"] = instance_ids
-    return orig(self, operation_name, new_api_params)
-
-
-def _patch_describe_instance_information(self, operation_name, api_params):
-    warnings.warn(
-        "moto is missing the describe_instance_information function, therefor it is manually mocked."
-        "TIP: periodically check if it gets updated https://docs.getmoto.org/en/latest/docs/services/ssm.html#ssm",
-        UserWarning,
-        stacklevel=1,
-    )
-    return {"InstanceInformationList": [{"PingStatus": "Online"}]}
-
-
 # Mocked aiobotocore _make_api_call function
 async def patched_aiobotocore_make_api_call(self, operation_name, api_params):
     # For example for the Access Analyzer service
