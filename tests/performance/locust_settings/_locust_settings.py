99f3fff3e13d23ba02a22a3fb9583a9c6df31a38
Mads Bisgaard <bisgaard@itis.swiss>
2024-07-09 11:03:59 +0200

    fix serialization

--- tests/performance/locust_settings/_locust_settings.py
+++ tests/performance/locust_settings/_locust_settings.py
@@ -1,7 +1,15 @@
 from datetime import timedelta
 
 from parse import Result, parse
-from pydantic import AnyHttpUrl, Field, PositiveFloat, PositiveInt, field_validator
+from pydantic import (
+    AnyHttpUrl,
+    Field,
+    PositiveFloat,
+    PositiveInt,
+    SerializationInfo,
+    field_serializer,
+    field_validator,
+)
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from ._dump_dotenv import dump_dotenv
@@ -14,10 +22,13 @@ def _timedelta_serializer(td: timedelta) -> str:
     return f"{hours}h{minutes}m{seconds}s"
 
 
+def _remove_trailing_backslash(url: AnyHttpUrl):
+    s = f"{url}"
+    return s.rstrip("/")
+
+
 class LocustSettings(BaseSettings):
-    model_config = SettingsConfigDict(
-        cli_parse_args=True, json_encoders={timedelta: _timedelta_serializer}
-    )
+    model_config = SettingsConfigDict(cli_parse_args=True)
 
     LOCUST_HOST: AnyHttpUrl = Field(
         default=..., examples=["https://api.osparc-master.speag.com"]
@@ -32,7 +43,7 @@ class LocustSettings(BaseSettings):
 
     @field_validator("LOCUST_RUN_TIME", mode="before")
     @classmethod
-    def validate_run_time(cls, v: str):
+    def validate_run_time(cls, v: str) -> str | timedelta:
         result = parse("{hour:d}h{min:d}m{sec:d}s", v)
         if not isinstance(result, Result):
             return v
@@ -42,6 +53,19 @@ class LocustSettings(BaseSettings):
         if hour is None or min is None or sec is None:
             raise ValueError("Could not parse time")
         return timedelta(hours=hour, minutes=min, seconds=sec)
+
+    @field_serializer("LOCUST_RUN_TIME")
+    def serialize_run_time(self, td: timedelta, info: SerializationInfo) -> str:
+        total_seconds = int(td.total_seconds())
+        hours, remainder = divmod(total_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{hours}h{minutes}m{seconds}s"
+
+    @field_serializer("LOCUST_HOST")
+    def serialize_host(self, url: AnyHttpUrl, info: SerializationInfo) -> str:
+        # added as a temporary fix for https://github.com/pydantic/pydantic/issues/7186
+        s = f"{url}"
+        return s.rstrip("/")
 
 
 if __name__ == "__main__":
