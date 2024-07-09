4b21c93e4cd2dcf0d3aa6e7ce085e996cc54a3e4
Odei Maiz <maiz@itis.swiss>
2024-07-09 09:47:15 +0200

    Load existing html

--- services/static-webserver/client/source/class/osparc/auth/BlurredLoginPageS4LEngineering.js
+++ services/static-webserver/client/source/class/osparc/auth/BlurredLoginPageS4LEngineering.js
@@ -16,44 +16,21 @@
 ************************************************************************ */
 
 qx.Class.define("osparc.auth.BlurredLoginPageS4LEngineering", {
-  extend: qx.ui.core.Widget,
+  extend: qx.ui.embed.Html,
+
+  construct: function() {
+    this.base(arguments);
+
+    this._setLayout(new qx.ui.layout.Grow());
+
+    this.__loadHtml();
+  },
 
   members: {
-    // overridden
-    _reloadLayout: function() {
-      const layout = new qx.ui.layout.HBox();
-      this._setLayout(layout);
-
-      this.setBackgroundColor("primary-background-color");
-
-      this._removeAll();
-
-      this.__setBackgroundImage();
-
-    },
-
-    __setBackgroundImage: function() {
-      let backgroundImage = "";
-
-      const defaultBG = `url(${osparc.product.Utils.getProductBackgroundUrl("Sim4Life-head-default.png")}), url(${osparc.product.Utils.getProductBackgroundUrl("clouds_11.png")})`;
-      const liteBG = `url(${osparc.product.Utils.getProductBackgroundUrl("Sim4Life-head-lite.png")}), url(${osparc.product.Utils.getProductBackgroundUrl("clouds_11.png")})`;
-      const academyBG = `url(${osparc.product.Utils.getProductBackgroundUrl("Sim4Life-head-academy.png")}), url(${osparc.product.Utils.getProductBackgroundUrl("clouds_11.png")})`;
-
-      switch (osparc.product.Utils.getProductName()) {
-        case "s4llite":
-          backgroundImage = liteBG;
-          break;
-        case "s4lacad":
-        case "s4ldesktopacad":
-          backgroundImage = academyBG;
-          break;
-        case "s4ldesktop":
-        case "s4l":
-        default:
-          backgroundImage = defaultBG;
-          break;
-      }
-      this._setBackgroundImage(backgroundImage);
+    __loadHtml: function() {
+      const iframe = new qx.ui.embed.Html();
+      iframe.setSource("osparc/S4LEngine_ComingSoon.html");
+      this._add(iframe);
     }
   }
 });
