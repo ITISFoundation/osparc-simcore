23a9140df32589ddda0c35d512ec05db07f78081
Odei Maiz <maiz@itis.swiss>
2024-07-12 11:57:07 +0200

    AddCollaborators widget

---
+++ services/static-webserver/client/source/class/osparc/share/AddCollaborators.js
@@ -0,0 +1,84 @@
+/* ************************************************************************
+
+   osparc - the simcore frontend
+
+   https://osparc.io
+
+   Copyright:
+     2024 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+/**
+ *   Widget that offers the "Share with..." button to add collaborators to a resource.
+ *   It also provides the "Check Organization..." direct access.
+ *   As output, once the user select n gid in the NewCollaboratorsManager pop up window,
+ * an event is fired with the list of collaborators.
+ */
+
+qx.Class.define("osparc.share.AddCollaborators", {
+  extend: qx.ui.core.Widget,
+
+  /**
+    * @param resourceType {String} resourceType
+    * @param serializedDataCopy {Object} Object containing the Serialized Data
+    */
+  construct: function(resourceType, serializedDataCopy) {
+    this.base(arguments);
+
+    this.__resourceType = resourceType;
+    this.__serializedDataCopy = serializedDataCopy;
+
+    this._setLayout(new qx.ui.layout.VBox(5));
+
+    this.__buildLayout();
+  },
+
+  events: {
+    "addCollaborators": "qx.event.type.Data"
+  },
+
+  members: {
+    __resourceType: null,
+    __serializedDataCopy: null,
+
+    __buildLayout: function() {
+      if (this.__resourceType === "service") {
+        // service
+        this.setVisibility(this._canIWrite() ? "visible" : "excluded");
+      } else {
+        // study or template
+        this.setVisibility(this._canIDelete() ? "visible" : "excluded");
+      }
+
+      const label = new qx.ui.basic.Label(this.tr("Select from the list below and click Share"));
+      this._add(label);
+
+      const addCollaboratorBtn = new qx.ui.form.Button(this.tr("Share with...")).set({
+        appearance: "form-button",
+        alignX: "left",
+        allowGrowX: false
+      });
+      addCollaboratorBtn.addListener("execute", () => {
+        const collaboratorsManager = new osparc.share.NewCollaboratorsManager(this.__serializedDataCopy);
+        collaboratorsManager.addListener("addCollaborators", e => this.fireDataEvent("addCollaborators", e.getData()), this);
+      }, this);
+      this._add(addCollaboratorBtn);
+
+      const organizations = new qx.ui.form.Button(this.tr("Check Organizations...")).set({
+        appearance: "form-button-outlined",
+        allowGrowY: false,
+        allowGrowX: false,
+        icon: osparc.dashboard.CardBase.SHARED_ORGS
+      });
+      organizations.addListener("execute", () => osparc.desktop.organizations.OrganizationsWindow.openWindow(), this);
+      this._add(organizations);
+    }
+  }
+});
