f61ef169c0a06c352624779a3697147a0d6b3b01
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-07-16 15:12:09 +0200

    wip

--- services/catalog/src/simcore_service_catalog/services/compatibility.py
+++ services/catalog/src/simcore_service_catalog/services/compatibility.py
@@ -1,7 +1,9 @@
 """ Manages service compatibility policies
 
 """
-from models_library.services_history import ServiceRelease
+from copy import deepcopy
+
+from models_library.services_history import Compatibility, ServiceRelease
 from models_library.services_types import ServiceVersion
 from packages.version import Version
 from simcore_service_catalog.utils.versioning import as_version
@@ -16,9 +18,15 @@ def get_latest_compatible_version(
 ) -> ServiceVersion | None:
     current_version = as_version(version)
     versions = [as_version(item.version) for item in history if not item.retired]
-    latest_patch_release = sorted(
+    patches = sorted(
         v for v in versions if current_version < v and _is_patch(current_version, v)
     )
-    return (
-        ServiceVersion(f"{latest_patch_release[0]}") if latest_patch_release else None
-    )
+    return ServiceVersion(f"{patches[-1]}") if patches else None
+
+
+def update_compatibility(history: list[ServiceRelease]) -> list[ServiceRelease]:
+    updated_history = deepcopy(history)
+    for item in updated_history:
+        if v := get_latest_compatible_version(item.version, history):
+            item.compatibility = Compatibility(can_update_to=v)
+    return updated_history
