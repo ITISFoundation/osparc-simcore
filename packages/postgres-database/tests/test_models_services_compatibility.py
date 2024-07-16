79987a2edd3d4a4376d6bc3b51e5dbb55e742e93
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-07-16 19:54:13 +0200

    drafts tests

---
+++ packages/postgres-database/tests/test_models_services_compatibility.py
@@ -0,0 +1,22 @@
+import pytest
+from simcore_postgres_database.models.services_compatibility import (
+    services_compatibility,
+)
+
+assert services_compatibility
+
+
+def test_insert_services_compatibility():
+    # TODO: use this test to produce complete fakes for other tests
+    pytest.fail(reason="Not implemented")
+
+
+def test_update_services_compatibility():
+    # what can change and what not?
+    pytest.fail(reason="Not implemented")
+
+
+def test_delete_services_compatibility():
+    # TODO: how it deletes? casacade/null policies?
+    # TODO: pk dependencies?
+    pytest.fail(reason="Not implemented")
