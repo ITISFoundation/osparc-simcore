6e0ec5b906dc55038d24fbb4fb84115bbe095ba6
Odei Maiz <maiz@itis.swiss>
2024-07-24 18:11:49 +0200

    comment

--- services/static-webserver/client/source/class/osparc/share/PublishTemplate.js
+++ services/static-webserver/client/source/class/osparc/share/PublishTemplate.js
@@ -46,6 +46,7 @@ qx.Class.define("osparc.share.PublishTemplate", {
     __selectedCollabs: null,
 
     __buildLayout: function() {
+      // mark it us template, so that testers can share it with product everyone
       this.__potentialTemplateData["resourceType"] = "template";
       const addCollaborators = new osparc.share.AddCollaborators(this.__potentialTemplateData);
       addCollaborators.getChildControl("intro-text").set({
