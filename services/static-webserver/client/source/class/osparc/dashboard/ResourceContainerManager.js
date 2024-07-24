cc4fa870d9a5982733912fccaeaf109fe424609f
Odei Maiz <maiz@itis.swiss>
2024-07-24 12:32:08 +0200

    Only visible in master until it's connected to the backend

--- services/static-webserver/client/source/class/osparc/dashboard/ResourceContainerManager.js
+++ services/static-webserver/client/source/class/osparc/dashboard/ResourceContainerManager.js
@@ -36,6 +36,8 @@ qx.Class.define("osparc.dashboard.ResourceContainerManager", {
     const foldersContainer = this.__foldersContainer = new osparc.dashboard.ToggleButtonContainer();
     folders.add(foldersContainer);
     this._add(folders);
+    // Only visible in master until it's connected to the backend
+    folders.setVisibility(osparc.utils.Utils.isDevelopmentPlatform() ? "visible" : "excluded");
 
     const nonGroupedContainer = this.__nonGroupedContainer = new osparc.dashboard.ToggleButtonContainer();
     [
