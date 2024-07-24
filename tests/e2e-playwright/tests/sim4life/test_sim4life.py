37ab7569231f6fcdad55e2884543b8da82ad7ef6
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-07-24 14:58:41 +0200

    ongoing

--- tests/e2e-playwright/tests/sim4life/test_sim4life.py
+++ tests/e2e-playwright/tests/sim4life/test_sim4life.py
@@ -7,14 +7,26 @@
 # pylint: disable=unused-variable
 
 
+import logging
 import re
 from typing import Any, Callable, Final
 
-from playwright.sync_api import Page
-from pytest_simcore.helpers.playwright import ServiceType
+from playwright.sync_api import Page, WebSocket
+from pytest_simcore.helpers.logging_tools import log_context
+from pytest_simcore.helpers.playwright import (
+    MINUTE,
+    ServiceType,
+    wait_for_service_running,
+)
 
 projects_uuid_pattern: Final[re.Pattern] = re.compile(
     r"/projects/([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})"
+)
+_EC2_STARTUP_MAX_WAIT_TIME: Final[int] = 1 * MINUTE
+_S4L_MAX_STARTUP_TIME: Final[int] = 1 * MINUTE
+_S4L_DOCKER_PULLING_MAX_TIME: Final[int] = 10 * MINUTE
+_S4L_AUTOSCALED_MAX_STARTUP_TIME: Final[int] = (
+    _EC2_STARTUP_MAX_WAIT_TIME + _S4L_DOCKER_PULLING_MAX_TIME + _S4L_MAX_STARTUP_TIME
 )
 
 
@@ -23,12 +35,39 @@ def test_sim4life(
     create_project_from_service_dashboard: Callable[
         [ServiceType, str, str | None], dict[str, Any]
     ],
+    log_in_and_out: WebSocket,
     service_key: str,
+    autoscaled: bool,
 ):
-    create_project_from_service_dashboard(ServiceType.DYNAMIC, service_key, None)
-
-    # Wait until grid is shown
-    page.frame_locator(".qx-main-dark").get_by_role("img", name="Remote render").click(
-        button="right", timeout=600000
+    project_data = create_project_from_service_dashboard(
+        ServiceType.DYNAMIC, service_key, None
     )
-    page.wait_for_timeout(1000)
+    assert "workbench" in project_data, "Expected workbench to be in project data!"
+    assert isinstance(
+        project_data["workbench"], dict
+    ), "Expected workbench to be a dict!"
+    node_ids: list[str] = list(project_data["workbench"])
+    assert len(node_ids) == 1, "Expected 1 node in the workbench!"
+
+    with log_context(logging.INFO, "launch S4l"):
+        s4l_iframe = wait_for_service_running(
+            page=page,
+            node_id=node_ids[0],
+            websocket=log_in_and_out,
+            timeout=(
+                _S4L_AUTOSCALED_MAX_STARTUP_TIME
+                if autoscaled
+                else _S4L_MAX_STARTUP_TIME
+            ),
+            press_start_button=False,
+        )
+
+    # NOTE: here the startup screen is up there. from the test it seems the default timeout is enough.
+    # Wait until grid is shown
+    with log_context(logging.INFO, "Interact with S4l"):
+        s4l_iframe.get_by_test_id("tree-item-Grid").nth(0).click()
+        page.wait_for_timeout(30000)
+        s4l_iframe.get_by_role("img", name="Remote render").click(button="right")
+
+    with log_context(logging.INFO, "Check videostreaming works"):
+        ...
