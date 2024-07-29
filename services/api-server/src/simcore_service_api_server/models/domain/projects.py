4d93e2a3c1decd6809587d2c84430e96f67e9cfc
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-07-29 10:43:28 +0200

    mypy

--- services/api-server/src/simcore_service_api_server/models/domain/projects.py
+++ services/api-server/src/simcore_service_api_server/models/domain/projects.py
@@ -1,6 +1,14 @@
+# mypy: disable-error-code=truthy-function
 from models_library.projects import AccessRights, Node, StudyUI
 from models_library.projects_nodes import InputTypes, OutputTypes
 from models_library.projects_nodes_io import SimCoreFileLink
+
+assert AccessRights  # nosec
+assert InputTypes  # nosec
+assert Node  # nosec
+assert OutputTypes  # nosec
+assert SimCoreFileLink  # nosec
+assert StudyUI  # nosec
 
 __all__: tuple[str, ...] = (
     "AccessRights",
@@ -10,5 +18,3 @@ __all__: tuple[str, ...] = (
     "SimCoreFileLink",
     "StudyUI",
 )
-
-# nopycln: file
