ba8cb1ab5ec8e4b7aa623911a367334152defb9b
Andrei Neagu <neagu@itis.swiss>
2024-08-01 14:16:06 +0200

    fixed imports

--- services/web/server/src/simcore_service_webserver/security/_identity_policy.py
+++ services/web/server/src/simcore_service_webserver/security/_identity_policy.py
@@ -1,5 +1,5 @@
-from aiohttp_security.session_identity import (
-    SessionIdentityPolicy,  # type: ignore[import-untyped]
+from aiohttp_security.session_identity import (  # type: ignore[import-untyped]
+    SessionIdentityPolicy,
 )
 
 assert SessionIdentityPolicy  # nosec
