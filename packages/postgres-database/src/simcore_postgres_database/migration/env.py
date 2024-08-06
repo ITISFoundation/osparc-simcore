89a3d38d131d35f3bb4e673e65eea630801e21f6
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-08-06 08:19:40 +0200

    revert import

--- packages/postgres-database/src/simcore_postgres_database/migration/env.py
+++ packages/postgres-database/src/simcore_postgres_database/migration/env.py
@@ -1,9 +1,8 @@
 from logging.config import fileConfig
 
 from alembic import context
+from simcore_postgres_database.settings import target_metadatas
 from sqlalchemy import engine_from_config, pool
-
-from ..settings import target_metadatas
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
