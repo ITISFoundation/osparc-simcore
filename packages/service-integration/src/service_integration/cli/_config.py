dded2d68c7e2b1f11b12a5bcd8f3aa7daa1289da
Andrei Neagu <neagu@itis.swiss>
2024-08-06 11:40:42 +0200

    revert

--- packages/service-integration/src/service_integration/cli/_config.py
+++ packages/service-integration/src/service_integration/cli/_config.py
@@ -135,9 +135,7 @@ def init_config(
     template: Annotated[
         str, typer.Option(help="Github repo or path to the template")
     ] = _COOKIECUTTER_GITHUB_URL,
-    checkout: Annotated[
-        str | None, typer.Option(help="Branch if different from main")
-    ] = None,
+    checkout: Annotated[str, typer.Option(help="Branch if different from main")] = None,  # type: ignore[assignment]
 ):
     """runs cookie-cutter"""
     from cookiecutter.main import cookiecutter  # type: ignore[import-untyped]
