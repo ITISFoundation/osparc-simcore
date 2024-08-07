321ea307d241f920e4592e114768d4951cd81404
Odei Maiz <maiz@itis.swiss>
2024-08-07 19:58:11 +0200

    with spinning wheel

--- services/static-webserver/client/source/class/osparc/desktop/credits/CreditsPerService.js
+++ services/static-webserver/client/source/class/osparc/desktop/credits/CreditsPerService.js
@@ -38,19 +38,28 @@ qx.Class.define("osparc.desktop.credits.CreditsPerService", {
   members: {
     __populateList: function(nDays) {
       this._removeAll();
-
-      [{
-        service: "simcore/services/comp/itis/sleeper",
-        credits: 140,
-        percentage: 70
-      }, {
-        service: "simcore/services/dynamic/sim4life-8-0-0-dy",
-        credits: 60,
-        percentage: 30
-      }].forEach(entry => {
-        const uiEntry = new osparc.desktop.credits.CreditsServiceListItem(entry.service, entry.credits, entry.percentage);
-        this._add(uiEntry);
+      const loadingImage = new qx.ui.basic.Image("@FontAwesome5Solid/circle-notch/26").set({
+        alignX: "center",
+        padding: 6
       });
+      loadingImage.getContentElement().addClass("rotate");
+      this._add(loadingImage);
+
+      setTimeout(() => {
+        this._removeAll();
+        [{
+          service: "simcore/services/comp/itis/sleeper",
+          credits: nDays*30,
+          percentage: 66
+        }, {
+          service: "simcore/services/dynamic/sim4life-8-0-0-dy",
+          credits: nDays*20,
+          percentage: 33
+        }].forEach(entry => {
+          const uiEntry = new osparc.desktop.credits.CreditsServiceListItem(entry.service, entry.credits, entry.percentage);
+          this._add(uiEntry);
+        });
+      }, 1000);
     }
   }
 });
