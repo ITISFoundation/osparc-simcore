e08318d8572e52636724396cd6231dfbfe12ac8a
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2024-08-08 16:11:23 +0200

    ✨ [Frontend] Credits per service summary (#6146)

---
+++ services/static-webserver/client/source/class/osparc/desktop/credits/CreditsSummary.js
@@ -0,0 +1,156 @@
+/* ************************************************************************
+
+   osparc - the simcore frontend
+
+   https://osparc.io
+
+   Copyright:
+     2024 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+qx.Class.define("osparc.desktop.credits.CreditsSummary", {
+  extend: qx.ui.core.Widget,
+
+  construct: function() {
+    this.base(arguments);
+
+    this._setLayout(new qx.ui.layout.VBox(10));
+
+    this.set({
+      appearance: "floating-menu",
+      padding: 8
+    });
+    osparc.utils.Utils.setIdToWidget(this, "creditsSummary");
+
+    this.__buildLayout();
+
+    const root = qx.core.Init.getApplication().getRoot();
+    root.add(this, {
+      top: 0,
+      right: 0
+    });
+  },
+
+  statics: {
+    BILLING_CENTER_BUTTON_SIZE: 26,
+    WIDTH: 300,
+    TIME_RANGES: [{
+      key: 1,
+      label: "Today"
+    }, {
+      key: 7,
+      label: "Last week"
+    }, {
+      key: 30,
+      label: "Last month"
+    }]
+  },
+
+  members: {
+    _createChildControlImpl: function(id) {
+      let control;
+      switch (id) {
+        case "top-layout":
+          control = new qx.ui.container.Composite(new qx.ui.layout.HBox(10));
+          this._add(control);
+          break;
+        case "top-left-spacer": {
+          const buttonSize = this.self().BILLING_CENTER_BUTTON_SIZE;
+          control = new qx.ui.core.Spacer(buttonSize, buttonSize);
+          const topLayout = this.getChildControl("top-layout");
+          topLayout.add(control);
+          break;
+        }
+        case "credits-indicator": {
+          control = new osparc.desktop.credits.CreditsIndicator();
+          control.getChildControl("credits-bar").exclude();
+          const store = osparc.store.Store.getInstance();
+          store.bind("contextWallet", control, "wallet");
+          const topLayout = this.getChildControl("top-layout");
+          topLayout.add(control, {
+            flex: 1
+          });
+          break;
+        }
+        case "billing-center-button": {
+          const buttonSize = this.self().BILLING_CENTER_BUTTON_SIZE;
+          control = new qx.ui.form.Button().set({
+            appearance: "form-button-outlined",
+            width: buttonSize,
+            height: buttonSize,
+            alignX: "center",
+            alignY: "middle",
+            center: true,
+            icon: "@FontAwesome5Solid/ellipsis-v/12"
+          });
+          // make it circular
+          control.getContentElement().setStyles({
+            "border-radius": `${buttonSize / 2}px`
+          });
+          control.addListener("execute", () => {
+            osparc.desktop.credits.BillingCenterWindow.openWindow();
+            this.exclude();
+          });
+          osparc.utils.Utils.setIdToWidget(control, "billingCenterButton");
+          const topLayout = this.getChildControl("top-layout");
+          topLayout.add(control);
+          break;
+        }
+        case "time-range-sb":
+          control = new qx.ui.form.SelectBox().set({
+            allowGrowX: false,
+            alignX: "center",
+            backgroundColor: "transparent"
+          });
+          this.self().TIME_RANGES.forEach(tr => {
+            const trItem = new qx.ui.form.ListItem(tr.label, null, tr.key);
+            control.add(trItem);
+          });
+          this._add(control);
+          break;
+        case "services-consumption":
+          control = new osparc.desktop.credits.CreditsPerService();
+          this._add(control);
+          break;
+      }
+      return control || this.base(arguments, id);
+    },
+
+    setPosition: function(x, y) {
+      this.setLayoutProperties({
+        left: x - this.self().WIDTH,
+        top: y
+      });
+    },
+
+    __buildLayout: function() {
+      this.getChildControl("top-left-spacer");
+      this.getChildControl("credits-indicator");
+      this.getChildControl("billing-center-button");
+      this.__buildConsumptionSummary();
+
+      this.set({
+        maxWidth: this.self().WIDTH
+      })
+    },
+
+    __buildConsumptionSummary: function() {
+      const timeRangeSB = this.getChildControl("time-range-sb");
+      const servicesConsumption = this.getChildControl("services-consumption");
+
+      timeRangeSB.addListener("changeSelection", e => {
+        const selection = e.getData();
+        if (selection.length) {
+          servicesConsumption.setDaysRange(selection[0].getModel());
+        }
+      });
+    }
+  }
+});
