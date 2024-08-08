ed5235441770b0163e056dc60deeea9f2af11dca
Andrei Neagu <neagu@itis.swiss>
2024-08-08 10:51:03 +0200

    refactor

--- services/web/server/src/simcore_service_webserver/email/_core.py
+++ services/web/server/src/simcore_service_webserver/email/_core.py
@@ -66,13 +66,13 @@ async def _do_send_mail(
         finally:
             await smtp.quit()
     else:
-        async with _create_smtp_client(settings) as aio_smtp:
+        async with _create_smtp_client(settings) as smtp2:
             if settings.SMTP_USERNAME and settings.SMTP_PASSWORD:
                 _logger.info("Login email server ...")
-                await aio_smtp.login(
+                await smtp2.login(
                     settings.SMTP_USERNAME, settings.SMTP_PASSWORD.get_secret_value()
                 )
-            await aio_smtp.send_message(message)  # type: ignore[attr-defined]
+            await smtp2.send_message(message)  # type: ignore[attr-defined]
 
 
 MIMEMessage = Union[MIMEText, MIMEMultipart]
