fd7768882ebe44981879d35ff9d9837d3c2e8e5b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-08-09 17:22:10 +0200

    refactored imports

--- packages/aws-library/src/aws_library/ec2/__init__.py
+++ packages/aws-library/src/aws_library/ec2/__init__.py
@@ -1,11 +1,15 @@
 from ._client import SimcoreEC2API
 from ._errors import EC2AccessError, EC2NotConnectedError, EC2RuntimeError
+from ._models import EC2InstanceConfig, EC2InstanceData, Resources
 
 __all__: tuple[str, ...] = (
     "SimcoreEC2API",
     "EC2AccessError",
     "EC2NotConnectedError",
     "EC2RuntimeError",
+    "EC2InstanceData",
+    "EC2InstanceConfig",
+    "Resources",
 )
 
 # nopycln: file
