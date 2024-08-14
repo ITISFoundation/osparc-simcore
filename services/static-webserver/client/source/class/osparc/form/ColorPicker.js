cb71d74678e18e8ce7a8f71a94037a370990ae9a
Odei Maiz <maiz@itis.swiss>
2024-08-14 12:31:17 +0200

    ColorPicker is a form item

--- services/static-webserver/client/source/class/osparc/form/ColorPicker.js
+++ services/static-webserver/client/source/class/osparc/form/ColorPicker.js
@@ -8,55 +8,74 @@
 
 qx.Class.define("osparc.form.ColorPicker", {
   extend: qx.ui.core.Widget,
+  include : [
+    qx.ui.form.MForm
+  ],
+  implement : [
+    qx.ui.form.IStringForm,
+    qx.ui.form.IForm
+  ],
 
   construct: function() {
     this.base(arguments);
 
-    this._setLayout(new qx.ui.layout.HBox());
+    const layout = new qx.ui.layout.HBox();
+    this._setLayout(layout);
 
-    this._add(this.getChildControl("random-button"));
-    this._add(this.getChildControl("selector-button"));
-    this._add(this.getChildControl("color-input"));
+    this.getChildControl("random-button");
+    this.getChildControl("selector-button");
+    this.getChildControl("color-input");
   },
 
   properties: {
-    color: {
+    value: {
       check: "Color",
-      event: "changeColor",
+      event: "changeValue",
       init: "#303030"
     }
   },
 
+  // eslint-disable-next-line qx-rules/no-refs-in-members
   members: {
+    _forwardStates: {
+      focused : true,
+      invalid : true
+    },
+
     _createChildControlImpl: function(id) {
       let control;
       switch (id) {
         case "random-button":
           control = new qx.ui.form.Button(null, "@FontAwesome5Solid/sync-alt/12");
-          control.addListener("execute", () => this.setColor(osparc.utils.Utils.getRandomColor()), this);
-          this.bind("color", control, "backgroundColor");
-          this.bind("color", control, "textColor", {
+          control.addListener("execute", () => this.setValue(osparc.utils.Utils.getRandomColor()), this);
+          this.bind("value", control, "backgroundColor");
+          this.bind("value", control, "textColor", {
             converter: value => qx.theme.manager.Color.getInstance().resolve(osparc.utils.Utils.getContrastedTextColor(value))
           });
+          this._add(control);
           break;
         case "selector-button":
           control = new qx.ui.form.Button(null, "@FontAwesome5Solid/eye-dropper/12");
           control.addListener("execute", () => this.__openColorSelector(), this);
-          this.bind("color", control, "backgroundColor");
-          this.bind("color", control, "textColor", {
+          this.bind("value", control, "backgroundColor");
+          this.bind("value", control, "textColor", {
             converter: value => qx.theme.manager.Color.getInstance().resolve(osparc.utils.Utils.getContrastedTextColor(value))
           });
+          this._add(control);
           break;
         case "color-input":
           control = new qx.ui.form.TextField().set({
             width: 80,
             required: true
           });
-          this.bind("color", control, "value");
+          this._add(control, {
+            flex: 1
+          });
+          this.bind("value", control, "value");
           control.addListener("changeValue", e => {
             const newColor = e.getData();
             if (osparc.utils.Validators.hexColor(newColor, control)) {
-              this.setColor(newColor);
+              this.setValue(newColor);
             }
           });
           break;
@@ -66,12 +85,30 @@ qx.Class.define("osparc.form.ColorPicker", {
 
     __openColorSelector: function() {
       const colorSelector = new qx.ui.control.ColorSelector();
-      const rgb = qx.util.ColorUtil.hexStringToRgb(this.getColor());
+      const rgb = qx.util.ColorUtil.hexStringToRgb(this.getValue());
       colorSelector.setRed(rgb[0]);
       colorSelector.setGreen(rgb[1]);
       colorSelector.setBlue(rgb[2]);
       osparc.ui.window.Window.popUpInWindow(colorSelector, this.tr("Pick a color"), 590, 380);
-      colorSelector.addListener("changeValue", e => this.setColor(e.getData()));
-    }
+      colorSelector.addListener("changeValue", e => this.setValue(e.getData()));
+    },
+
+    // overridden
+    resetValue: function() {
+      this.getChildControl("color-input").resetValue();
+    },
+
+    // overridden
+    focus: function() {
+      this.base(arguments);
+      this.getChildControl("color-input").getFocusElement().focus();
+    },
+
+    // overridden
+    tabFocus: function() {
+      const field = this.getChildControl("color-input");
+      field.getFocusElement().focus();
+      field.selectAllText();
+    },
   }
 });
