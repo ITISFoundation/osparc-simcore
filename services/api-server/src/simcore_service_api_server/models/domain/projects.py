54dcca19b1f859c7863f5a305caf53676f30b399
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-08-14 09:53:37 +0200

    @sanderegg review: doc

--- services/api-server/src/simcore_service_api_server/models/domain/projects.py
+++ services/api-server/src/simcore_service_api_server/models/domain/projects.py
@@ -1,8 +1,8 @@
+# mypy: disable-error-code=truthy-function
+
 from models_library.projects_access import AccessRights
 from models_library.projects_nodes import InputTypes, Node, OutputTypes
 from models_library.projects_nodes_io import SimCoreFileLink
-
-# mypy: disable-error-code=truthy-function
 from models_library.projects_ui import StudyUI
 
 assert AccessRights  # nosec
