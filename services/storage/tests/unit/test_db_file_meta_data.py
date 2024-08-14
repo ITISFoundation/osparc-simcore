088ab6a30c137ea66d89b60482e09f63c2ed0968
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-08-14 18:10:43 +0200

    adds extra checks

--- services/storage/tests/unit/test_db_file_meta_data.py
+++ services/storage/tests/unit/test_db_file_meta_data.py
@@ -6,6 +6,7 @@
 
 from uuid import uuid4
 
+from faker import Faker
 from simcore_postgres_database.utils import as_postgres_sql_query_str
 from simcore_service_storage.db_file_meta_data import (
     _list_filter_with_partial_file_id_stmt,
@@ -13,7 +14,7 @@ from simcore_service_storage.db_file_meta_data import (
 from simcore_service_storage.models import UserOrProjectFilter
 
 
-def test_building_sql_statements():
+def test_building_sql_statements(faker: Faker):
     def _check(func_smt, **kwargs):
         print()
         print(f"{func_smt.__name__:*^100}")
@@ -69,3 +70,25 @@ def test_building_sql_statements():
         offset=1,
     )
     # (file_meta_data.user_id = '42' OR file_meta_data.project_id IN ('3cd9704db' ...)) AND file_meta_data.is_directory IS false ORDER BY file_meta_data.created_at ASC LIMIT 10 OFFSET 1
+
+    # As used in SimcoreS3DataManager.list_files
+    _check(
+        _list_filter_with_partial_file_id_stmt,
+        user_or_project_filter=UserOrProjectFilter(user_id=42, project_ids=[]),
+        file_id_prefix=None,
+        partial_file_id="{project_id}/",
+        sha256_checksum=None,
+        only_files=False,
+    )
+
+    # As used in SimcoreS3DataManager.search_owned_files
+    _check(
+        _list_filter_with_partial_file_id_stmt,
+        user_or_project_filter=UserOrProjectFilter(user_id=42, project_ids=[]),
+        file_id_prefix="api/",
+        partial_file_id=None,
+        sha256_checksum=faker.sha256(),
+        only_files=True,
+        limit=10,
+        offset=0,
+    )
