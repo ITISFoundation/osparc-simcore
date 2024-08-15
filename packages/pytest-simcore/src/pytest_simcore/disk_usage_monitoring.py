77f94b14fd9e667ba07bcc9e8eecda0b35db9168
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-08-15 19:09:04 +0200

    disk usage plugin

---
+++ packages/pytest-simcore/src/pytest_simcore/disk_usage_monitoring.py
@@ -0,0 +1,83 @@
+import logging
+import shutil
+
+import pytest
+
+_logger = logging.getLogger(__name__)
+
+
+def pytest_addoption(parser):
+    simcore_group = parser.getgroup("simcore")
+    simcore_group.addoption(
+        "--disk-usage", action="store_true", help="Enable disk usage monitoring"
+    )
+    simcore_group.addoption(
+        "--disk-usage-threshold",
+        action="store",
+        type=float,
+        default=0.0,
+        help="Set the threshold for disk usage increase as a percentage. No warning if increase is below this percentage.",
+    )
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    """Check if the disk usage monitoring is enabled and register the plugin."""
+    if config.getoption("--disk-usage"):
+        config.pluginmanager.register(DiskUsagePlugin(config), "disk_usage_plugin")
+
+
+class DiskUsagePlugin:
+    def __init__(self, config):
+        self.threshold = config.getoption("--disk-usage-threshold")
+
+    def _get_disk_usage(self):
+        return shutil.disk_usage("/").used
+
+    def _log_disk_usage_increase(
+        self, initial_usage: int, final_usage: int, scope_name: str
+    ):
+        if final_usage > initial_usage:
+            increase = final_usage - initial_usage
+            increase_percent = (increase / initial_usage) * 100
+
+            if increase_percent >= self.threshold:
+                _logger.warning(
+                    "Disk usage increased by %s bytes during %s.", increase, scope_name
+                )
+
+    @pytest.fixture(scope="session", autouse=True)
+    def monitor_session_disk_usage(self):
+        """SESSION-level fixture to monitor overall disk usage."""
+        initial_usage = self._get_disk_usage()
+
+        yield
+
+        final_usage = self._get_disk_usage()
+        self._log_disk_usage_increase(initial_usage, final_usage, "the session")
+
+    @pytest.fixture(scope="module", autouse=True)
+    def monitor_module_disk_usage(self, request):
+        """MODULE-level fixture to monitor disk usage before and after each module."""
+        initial_usage = self._get_disk_usage()
+
+        yield
+
+        final_usage = self._get_disk_usage()
+        module_name = request.module.__name__
+        self._log_disk_usage_increase(
+            initial_usage, final_usage, f"the module {module_name}"
+        )
+
+    @pytest.fixture(autouse=True)
+    def monitor_test_disk_usage(self, request):
+        """FUNCTION-level fixture to monitor disk usage before and after each test."""
+        initial_usage = self._get_disk_usage()
+
+        yield
+
+        final_usage = self._get_disk_usage()
+        test_name = request.node.name
+        self._log_disk_usage_increase(
+            initial_usage, final_usage, f"the test {test_name}"
+        )
