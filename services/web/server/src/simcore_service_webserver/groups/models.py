29146ae4572409e646d2699993007456160455f4
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-08-19 08:16:48 +0200

    @GitHK review: top of the file

--- services/web/server/src/simcore_service_webserver/groups/models.py
+++ services/web/server/src/simcore_service_webserver/groups/models.py
@@ -1,7 +1,6 @@
+# mypy: disable-error-code=truthy-function
 from ._utils import convert_groups_db_to_schema
 
-# Avoids pycln to remove import
-# mypy: disable-error-code=truthy-function
 assert convert_groups_db_to_schema  # nosec
 
 __all__: tuple[str, ...] = ("convert_groups_db_to_schema",)
