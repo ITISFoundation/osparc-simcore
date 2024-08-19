d23ba53fd1bec6adbc6248055a3e42b6a816038f
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-08-19 11:41:55 +0200

    minor

--- tests/performance/locust_settings.py
+++ tests/performance/locust_settings.py
@@ -33,14 +33,15 @@ class LocustSettings(BaseSettings):
     LOCUST_CHECK_FAIL_RATIO: PositiveFloat = Field(default=0.01, ge=0.0, le=1.0)
     LOCUST_HEADLESS: bool = Field(default=True)
     LOCUST_HOST: AnyHttpUrl = Field(
-        default=..., examples=["https://api.osparc-master.speag.com"]
+        default=...,
+        examples=["https://api.osparc-master.speag.com"],
     )
     LOCUST_LOCUSTFILE: Path = Field(
         default=...,
         description="Test file. Path should be relative to `locust_files` dir",
     )
     LOCUST_PRINT_STATS: bool = Field(default=True)
-    LOCUST_RUN_TIME: timedelta = Field(default=...)
+    LOCUST_RUN_TIME: timedelta
     LOCUST_SPAWN_RATE: PositiveInt = Field(default=20)
     LOCUST_TIMESCALE: NonNegativeInt = Field(
         default=1,
@@ -59,7 +60,7 @@ class LocustSettings(BaseSettings):
 
     @field_validator("LOCUST_RUN_TIME", mode="before")
     @classmethod
-    def validate_run_time(cls, v: str) -> str | timedelta:
+    def _validate_run_time(cls, v: str) -> str | timedelta:
         result = parse("{hour:d}h{min:d}m{sec:d}s", v)
         if not isinstance(result, Result):
             return v
@@ -67,28 +68,31 @@ class LocustSettings(BaseSettings):
         _min = result.named.get("min")
         sec = result.named.get("sec")
         if hour is None or _min is None or sec is None:
-            raise ValueError("Could not parse time")
+            msg = "Could not parse time"
+            raise ValueError(msg)
         return timedelta(hours=hour, minutes=_min, seconds=sec)
 
     @field_validator("LOCUST_LOCUSTFILE", mode="after")
     @classmethod
-    def validate_locust_file(cls, v: Path) -> Path:
+    def _validate_locust_file(cls, v: Path) -> Path:
         v = v.resolve()
         if not v.is_file():
-            raise ValueError(f"{v} must be an existing file")
+            msg = f"{v} must be an existing file"
+            raise ValueError(msg)
         if not v.is_relative_to(_LOCUST_FILES_DIR):
-            raise ValueError(f"{v} must be a test file relative to {_LOCUST_FILES_DIR}")
+            msg = f"{v} must be a test file relative to {_LOCUST_FILES_DIR}"
+            raise ValueError(msg)
         return v.relative_to(_TEST_DIR)
 
     @field_serializer("LOCUST_RUN_TIME")
-    def serialize_run_time(self, td: timedelta, info: SerializationInfo) -> str:
+    def _serialize_run_time(self, td: timedelta, info: SerializationInfo) -> str:
         total_seconds = int(td.total_seconds())
         hours, remainder = divmod(total_seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
         return f"{hours}h{minutes}m{seconds}s"
 
     @field_serializer("LOCUST_HOST")
-    def serialize_host(self, url: AnyHttpUrl, info: SerializationInfo) -> str:
+    def _serialize_host(self, url: AnyHttpUrl, info: SerializationInfo) -> str:
         # added as a temporary fix for https://github.com/pydantic/pydantic/issues/7186
         s = f"{url}"
         return s.rstrip("/")
