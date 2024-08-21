8fb5a5724016893bee1a45e8b423e9abddd2ad79
Odei Maiz <maiz@itis.swiss>
2024-08-21 11:54:32 +0200

    tooltip timeouts

--- services/static-webserver/client/source/class/osparc/theme/Appearance.js
+++ services/static-webserver/client/source/class/osparc/theme/Appearance.js
@@ -1124,8 +1124,8 @@ qx.Theme.define("osparc.theme.Appearance", {
         padding: [5, 10],
         // showTimeout is themeable so it can be tuned
         // it was defaulted to 700 which was too short
-        showTimeout: 3000,
-        hideTimeout: 6000,
+        showTimeout: 2000,
+        hideTimeout: 5000,
       })
     },
 
