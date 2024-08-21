b40fa06a46e7a4ed5f4b8305f6b1aff8a10b45e8
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-08-21 14:26:30 +0200

    wrong import

--- packages/service-library/src/servicelib/aiohttp/typing_extension.py
+++ packages/service-library/src/servicelib/aiohttp/typing_extension.py
@@ -2,24 +2,13 @@ from collections.abc import AsyncIterator, Awaitable, Callable
 from typing import Any, TypeAlias
 
 from aiohttp import web
+from aiohttp.typedefs import Handler
 
 try:
-    import aiohttp.typedefs
-
-    assert aiohttp.typedefs is not None  # nosec
-    has_typedefs = True
+    from aiohttp.typedefs import Middleware
 except ImportError:
-    has_typedefs = False
-
-
-if has_typedefs:
-    from aiohttp.typedefs import Handler, Middleware
-
-else:
     # For older versions
     # Taken from aiohttp.web_middlewares import _Handler, _Middleware
-
-    Handler: TypeAlias = Callable[[web.Request], Awaitable[web.StreamResponse]]  # type: ignore[no-redef, misc]
     Middleware: TypeAlias = Callable[  # type: ignore[no-redef]
         [web.Request, Handler], Awaitable[web.StreamResponse]
     ]
