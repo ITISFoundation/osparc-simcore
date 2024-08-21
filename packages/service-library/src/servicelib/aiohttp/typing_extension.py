8ecf8c5fea74d9fd47a6ea280adc3f6c148da6cb
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-08-21 13:48:37 +0200

    mypy

--- packages/service-library/src/servicelib/aiohttp/typing_extension.py
+++ packages/service-library/src/servicelib/aiohttp/typing_extension.py
@@ -3,10 +3,33 @@ from typing import Any, TypeAlias
 
 from aiohttp import web
 
-# Taken from aiohttp.web_middlewares import _Handler, _Middleware
-Handler: TypeAlias = Callable[[web.Request], Awaitable[web.StreamResponse]]
+try:
+    import aiohttp.typedefs
+
+    assert aiohttp.typedefs is not None  # nosec
+    has_typedefs = True
+except ImportError:
+    has_typedefs = False
+
+
+if has_typedefs:
+    from aiohttp.typedefs import Handler, Middleware
+
+else:
+    # For older versions
+    # Taken from aiohttp.web_middlewares import _Handler, _Middleware
+
+    Handler: TypeAlias = Callable[[web.Request], Awaitable[web.StreamResponse]]  # type: ignore[no-redef, misc]
+    Middleware: TypeAlias = Callable[  # type: ignore[no-redef]
+        [web.Request, Handler], Awaitable[web.StreamResponse]
+    ]
+
+
+__all__: tuple[str, ...] = (
+    "Handler",
+    "Middleware",
+)
+
+
 HandlerAnyReturn: TypeAlias = Callable[[web.Request], Awaitable[Any]]
-Middleware: TypeAlias = Callable[[web.Request, Handler], Awaitable[web.StreamResponse]]
-
-
 CleanupContextFunc: TypeAlias = Callable[[web.Application], AsyncIterator[None]]
