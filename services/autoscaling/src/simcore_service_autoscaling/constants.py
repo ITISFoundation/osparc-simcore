8cfffaa3b28736052170d95689d9876ff2e80cef
Sylvain <35365065+sanderegg@users.noreply.github.com>
2024-08-26 17:53:51 +0200

    🎨Autoscaling: chunk prepulled image AWS EC2 tags (#6232)

---
+++ services/autoscaling/src/simcore_service_autoscaling/constants.py
@@ -0,0 +1,34 @@
+import re
+from typing import Final
+
+from aws_library.ec2._models import AWSTagKey, AWSTagValue, EC2Tags
+from pydantic import parse_obj_as
+
+BUFFER_MACHINE_PULLING_EC2_TAG_KEY: Final[AWSTagKey] = parse_obj_as(
+    AWSTagKey, "pulling"
+)
+BUFFER_MACHINE_PULLING_COMMAND_ID_EC2_TAG_KEY: Final[AWSTagKey] = parse_obj_as(
+    AWSTagKey, "ssm-command-id"
+)
+PREPULL_COMMAND_NAME: Final[str] = "docker images pulling"
+
+DOCKER_PULL_COMMAND: Final[
+    str
+] = "docker compose -f /docker-pull.compose.yml -p buffering pull"
+
+PRE_PULLED_IMAGES_EC2_TAG_KEY: Final[AWSTagKey] = parse_obj_as(
+    AWSTagKey, "io.simcore.autoscaling.pre_pulled_images"
+)
+
+BUFFER_MACHINE_TAG_KEY: Final[AWSTagKey] = parse_obj_as(
+    AWSTagKey, "io.simcore.autoscaling.buffer_machine"
+)
+DEACTIVATED_BUFFER_MACHINE_EC2_TAGS: Final[EC2Tags] = {
+    BUFFER_MACHINE_TAG_KEY: parse_obj_as(AWSTagValue, "true")
+}
+ACTIVATED_BUFFER_MACHINE_EC2_TAGS: Final[EC2Tags] = {
+    BUFFER_MACHINE_TAG_KEY: parse_obj_as(AWSTagValue, "false")
+}
+PRE_PULLED_IMAGES_RE: Final[re.Pattern] = re.compile(
+    rf"{PRE_PULLED_IMAGES_EC2_TAG_KEY}_\((\d+)\)"
+)
