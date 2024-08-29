8db69aeec51ccc9d7510a87bfe0ce5b0f1135b13
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-08-29 09:16:33 +0200

    test

--- services/autoscaling/tests/unit/test_modules_instrumentation_models.py
+++ services/autoscaling/tests/unit/test_modules_instrumentation_models.py
@@ -1,3 +1,5 @@
+from dataclasses import is_dataclass
+
 import pytest
 from simcore_service_autoscaling.models import BufferPool, Cluster
 from simcore_service_autoscaling.modules.instrumentation._models import (
@@ -11,5 +13,7 @@ from simcore_service_autoscaling.modules.instrumentation._models import (
     [(Cluster, ClusterMetrics), (BufferPool, BufferPoolsMetrics)],
 )
 def test_models_are_in_sync(class_name: type, metrics_class_name: type):
+    assert is_dataclass(class_name)
+    assert is_dataclass(metrics_class_name)
     for field in class_name.__dataclass_fields__:
         assert hasattr(metrics_class_name, field)
