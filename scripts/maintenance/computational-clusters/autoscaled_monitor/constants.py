99de33da661355910d482b18be3709b3f290e003
Sylvain <35365065+sanderegg@users.noreply.github.com>
2024-09-06 10:10:58 +0200

    🎨Autoscaling monitoring tool: compatible with TIP, small improvements (#6311)

--- scripts/maintenance/computational-clusters/autoscaled_monitor/constants.py
+++ scripts/maintenance/computational-clusters/autoscaled_monitor/constants.py
@@ -19,7 +19,9 @@ DEFAULT_COMPUTATIONAL_EC2_FORMAT_WORKERS: Final[
     str
 ] = r"osparc-computational-cluster-{role}-{swarm_stack_name}-user_id:{user_id:d}-wallet_id:{wallet_id:wallet_id_spec}-{key_name}"
 DEFAULT_DYNAMIC_EC2_FORMAT: Final[str] = r"osparc-dynamic-autoscaled-worker-{key_name}"
-DEPLOY_SSH_KEY_PARSER: Final[parse.Parser] = parse.compile(r"osparc-{random_name}.pem")
+DEPLOY_SSH_KEY_PARSER: Final[parse.Parser] = parse.compile(
+    r"{prefix}-{random_name}.pem"
+)
 
 MINUTE: Final[int] = 60
 HOUR: Final[int] = 60 * MINUTE
