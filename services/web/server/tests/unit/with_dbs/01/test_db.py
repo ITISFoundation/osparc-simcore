8c542bac636e7caf2887f156474ece211dc514f6
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-09-09 15:14:46 +0200

    adds testsing

--- services/web/server/tests/unit/with_dbs/01/test_db.py
+++ services/web/server/tests/unit/with_dbs/01/test_db.py
@@ -7,19 +7,25 @@ from pathlib import Path
 
 import yaml
 from aiohttp.test_utils import TestServer
+from simcore_service_webserver.application_settings import (
+    ApplicationSettings,
+    get_application_settings,
+)
 from simcore_service_webserver.db.plugin import (
+    get_async_engine,
     is_service_enabled,
     is_service_responsive,
 )
+from sqlalchemy.ext.asyncio import AsyncEngine
 
 
 def test_uses_same_postgres_version(
     docker_compose_file: Path, osparc_simcore_root_dir: Path
 ):
-    with open(docker_compose_file) as fh:
+    with Path.open(docker_compose_file) as fh:
         fixture = yaml.safe_load(fh)
 
-    with open(osparc_simcore_root_dir / "services" / "docker-compose.yml") as fh:
+    with Path.open(osparc_simcore_root_dir / "services" / "docker-compose.yml") as fh:
         expected = yaml.safe_load(fh)
 
     assert (
@@ -32,3 +38,16 @@ async def test_responsive(web_server: TestServer):
     app = web_server.app
     assert is_service_enabled(app)
     assert await is_service_responsive(app)
+
+
+def test_async_engine(web_server: TestServer):
+    app = web_server.app
+    assert app
+    settings: ApplicationSettings = get_application_settings(app)
+
+    # same as host
+    assert settings.WEBSERVER_DB
+    assert settings.WEBSERVER_DB.POSTGRES_CLIENT_NAME
+
+    engine: AsyncEngine = get_async_engine(app)
+    assert engine
