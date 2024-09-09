96c8447986eb906c72b4f403e5509bf0b887e37e
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-09-09 14:26:28 +0200

    service layer

--- services/web/server/src/simcore_service_webserver/tags/_handlers.py
+++ services/web/server/src/simcore_service_webserver/tags/_handlers.py
@@ -2,7 +2,6 @@ import functools
 
 from aiohttp import web
 from pydantic import parse_obj_as
-from servicelib.aiohttp.db_asyncpg_engine import get_async_engine
 from servicelib.aiohttp.requests_validation import (
     parse_request_body_as,
     parse_request_path_parameters_as,
@@ -12,17 +11,15 @@ from servicelib.mimetype_constants import MIMETYPE_APPLICATION_JSON
 from simcore_postgres_database.utils_tags import (
     TagNotFoundError,
     TagOperationNotAllowedError,
-    TagsRepo,
 )
-from sqlalchemy.ext.asyncio import AsyncEngine
 
 from .._meta import API_VTAG as VTAG
 from ..login.decorators import login_required
 from ..security.decorators import permission_required
 from ..utils_aiohttp import envelope_json_response
+from . import _api
 from .schemas import (
     TagCreate,
-    TagGet,
     TagGroupCreate,
     TagGroupGet,
     TagGroupPathParams,
@@ -56,20 +53,13 @@ routes = web.RouteTableDef()
 @_handle_tags_exceptions
 async def create_tag(request: web.Request):
     assert request.app  # nosec
-    engine: AsyncEngine = get_async_engine(request.app)
     req_ctx = TagRequestContext.parse_obj(request)
     new_tag = await parse_request_body_as(TagCreate, request)
 
-    repo = TagsRepo(engine)
-    tag = await repo.create(
-        user_id=req_ctx.user_id,
-        read=True,
-        write=True,
-        delete=True,
-        **new_tag.dict(exclude_unset=True),
+    created = await _api.create_tag(
+        request.app, user_id=req_ctx.user_id, new_tag=new_tag
     )
-    model = TagGet.from_db(tag)
-    return envelope_json_response(model)
+    return envelope_json_response(created)
 
 
 @routes.get(f"/{VTAG}/tags", name="list_tags")
@@ -77,12 +67,10 @@ async def create_tag(request: web.Request):
 @permission_required("tag.crud.*")
 @_handle_tags_exceptions
 async def list_tags(request: web.Request):
-    engine: AsyncEngine = get_async_engine(request.app)
-    req_ctx = TagRequestContext.parse_obj(request)
 
-    repo = TagsRepo(engine)
-    tags = await repo.list_all(user_id=req_ctx.user_id)
-    return envelope_json_response([TagGet.from_db(t).dict(by_alias=True) for t in tags])
+    req_ctx = TagRequestContext.parse_obj(request)
+    got = await _api.list_tags(request.app, user_id=req_ctx.user_id)
+    return envelope_json_response(got)
 
 
 @routes.patch(f"/{VTAG}/tags/{{tag_id}}", name="update_tag")
@@ -90,19 +78,17 @@ async def list_tags(request: web.Request):
 @permission_required("tag.crud.*")
 @_handle_tags_exceptions
 async def update_tag(request: web.Request):
-    engine: AsyncEngine = get_async_engine(request.app)
     req_ctx = TagRequestContext.parse_obj(request)
     path_params = parse_request_path_parameters_as(TagPathParams, request)
     tag_updates = await parse_request_body_as(TagUpdate, request)
 
-    repo = TagsRepo(engine)
-    tag = await repo.update(
+    updated = await _api.update_tag(
+        request.app,
         user_id=req_ctx.user_id,
         tag_id=path_params.tag_id,
-        **tag_updates.dict(exclude_unset=True),
+        tag_updates=tag_updates,
     )
-    model = TagGet.from_db(tag)
-    return envelope_json_response(model)
+    return envelope_json_response(updated)
 
 
 @routes.delete(f"/{VTAG}/tags/{{tag_id}}", name="delete_tag")
@@ -110,12 +96,12 @@ async def update_tag(request: web.Request):
 @permission_required("tag.crud.*")
 @_handle_tags_exceptions
 async def delete_tag(request: web.Request):
-    engine: AsyncEngine = get_async_engine(request.app)
     req_ctx = TagRequestContext.parse_obj(request)
     path_params = parse_request_path_parameters_as(TagPathParams, request)
 
-    repo = TagsRepo(engine)
-    await repo.delete(user_id=req_ctx.user_id, tag_id=path_params.tag_id)
+    await _api.delete_tag(
+        request.app, user_id=req_ctx.user_id, tag_id=path_params.tag_id
+    )
 
     raise web.HTTPNoContent(content_type=MIMETYPE_APPLICATION_JSON)
 
