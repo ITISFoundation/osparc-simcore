bc87e67040293182a4002eb3e28a10e992751d60
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-09-10 18:16:17 +0200

    added retired nodes

--- services/autoscaling/src/simcore_service_autoscaling/models.py
+++ services/autoscaling/src/simcore_service_autoscaling/models.py
@@ -112,6 +112,7 @@ class Cluster:  # pylint: disable=too-many-instance-attributes
             or self.drained_nodes
             or self.pending_ec2s
             or self.terminating_nodes
+            or self.retired_nodes
         )
 
     def total_number_of_machines(self) -> int:
@@ -124,6 +125,7 @@ class Cluster:  # pylint: disable=too-many-instance-attributes
             + len(self.pending_ec2s)
             + len(self.broken_ec2s)
             + len(self.terminating_nodes)
+            + len(self.retired_nodes)
         )
 
     def __repr__(self) -> str:
@@ -142,6 +144,7 @@ class Cluster:  # pylint: disable=too-many-instance-attributes
             f"buffer-ec2s: count={len(self.buffer_ec2s)} {_get_instance_ids(self.buffer_ec2s)}, "
             f"disconnected-nodes: count={len(self.disconnected_nodes)}, "
             f"terminating-nodes: count={len(self.terminating_nodes)} {_get_instance_ids(self.terminating_nodes)}, "
+            f"retired-nodes: count={len(self.retired_nodes)} {_get_instance_ids(self.retired_nodes)}, "
             f"terminated-ec2s: count={len(self.terminated_instances)} {_get_instance_ids(self.terminated_instances)})"
         )
 
