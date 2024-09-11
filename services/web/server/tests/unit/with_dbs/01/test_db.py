032e4546c7545c4433c1dd38d49189bc09b4816b
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-09-11 20:36:58 +0200

    improved test

--- services/web/server/tests/unit/with_dbs/01/test_db.py
+++ services/web/server/tests/unit/with_dbs/01/test_db.py
@@ -6,6 +6,8 @@
 from pathlib import Path
 
 import aiopg.sa
+import asyncpg
+import sqlalchemy as sa
 import yaml
 from aiohttp.test_utils import TestServer
 from simcore_service_webserver.application_settings import (
@@ -18,25 +20,50 @@ from simcore_service_webserver.db.plugin import (
     is_service_enabled,
     is_service_responsive,
 )
+from simcore_service_webserver.login.storage import AsyncpgStorage, get_plugin_storage
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 
-def test_engines_in_app_state(web_server: TestServer):
+async def test_all_pg_engines_in_app(web_server: TestServer):
     app = web_server.app
     assert app
-    settings: ApplicationSettings = get_application_settings(app)
 
-    # same as host
+    settings: ApplicationSettings = get_application_settings(app)
     assert settings.WEBSERVER_DB
     assert settings.WEBSERVER_DB.POSTGRES_CLIENT_NAME
 
-    engine: AsyncEngine = get_asyncpg_engine(app)
-    assert engine
-    assert isinstance(engine, AsyncEngine)
-
+    # (1) aiopg engine  (deprecated)
     aiopg_engine = get_aiopg_engine(app)
     assert aiopg_engine
     assert isinstance(aiopg_engine, aiopg.sa.Engine)
+
+    # (2) asyncpg engine via sqlalchemy.ext.asyncio  (new)
+    asyncpg_engine: AsyncEngine = get_asyncpg_engine(app)
+    assert asyncpg_engine
+    assert isinstance(asyncpg_engine, AsyncEngine)
+
+    # (3) low-level asyncpg Pool (deprecated)
+    # Will be replaced by (2)
+    login_storage: AsyncpgStorage = get_plugin_storage(app)
+    assert login_storage.pool
+    assert isinstance(login_storage.pool, asyncpg.Pool)
+
+    # they ALL point to the SAME database
+    assert aiopg_engine.dsn
+    assert asyncpg_engine.url
+
+    query = sa.text('SELECT "version_num" FROM "alembic_version"')
+    async with login_storage.pool.acquire() as conn:
+        result_pool = await conn.fetchval(str(query))
+
+    async with asyncpg_engine.connect() as conn:
+        result_asyncpg = (await conn.execute(query)).scalar_one_or_none()
+
+    async with aiopg_engine.acquire() as conn:
+        result_aiopg = await (await conn.execute(query)).scalar()
+
+    assert result_pool == result_asyncpg
+    assert result_pool == result_aiopg
 
 
 def test_uses_same_postgres_version(
