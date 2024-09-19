2ba3130e80f09fbc55612e95454c87a97aa4f544
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-09-19 13:42:59 +0200

    properly setup

--- packages/service-library/src/servicelib/fastapi/prometheus_instrumentation.py
+++ packages/service-library/src/servicelib/fastapi/prometheus_instrumentation.py
@@ -7,18 +7,20 @@ from prometheus_fastapi_instrumentator import Instrumentator
 
 def setup_prometheus_instrumentation(app: FastAPI) -> Instrumentator:
 
-    instrumentator = (
-        Instrumentator(
-            should_instrument_requests_inprogress=True, inprogress_labels=False
-        )
-        .instrument(app)
-        .expose(app, include_in_schema=False)
-    )
+    instrumentator = Instrumentator(
+        should_instrument_requests_inprogress=True, inprogress_labels=False
+    ).instrument(app)
 
-    def _unregister():
-        for collector in list(instrumentator.registry._collector_to_names.keys()):
+    async def on_startup(app: FastAPI) -> None:
+        instrumentator.expose(app, include_in_schema=False)
+
+    def _unregister() -> None:
+        # NOTE: avoid registering collectors multiple times when running unittests consecutively (https://stackoverflow.com/a/62489287)
+        for collector in list(
+            instrumentator.registry._collector_to_names.keys()  # noqa: SLF001
+        ):
             instrumentator.registry.unregister(collector)
 
-    # avoid registering collectors multiple times when running unittests consecutively (https://stackoverflow.com/a/62489287)
+    app.add_event_handler("startup", on_startup)
     app.add_event_handler("shutdown", _unregister)
     return instrumentator
