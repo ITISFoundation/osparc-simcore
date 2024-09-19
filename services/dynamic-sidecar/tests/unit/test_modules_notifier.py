6bd6d15e0e9fc9a2e9eb3600fdada559d470c02a
Andrei Neagu <neagu@itis.swiss>
2024-09-19 14:04:23 +0200

    added notifications tests

--- services/dynamic-sidecar/tests/unit/test_modules_notifier.py
+++ services/dynamic-sidecar/tests/unit/test_modules_notifier.py
@@ -4,6 +4,7 @@
 from collections.abc import AsyncIterable, Callable
 from contextlib import AsyncExitStack, _AsyncGeneratorContextManager
 from pathlib import Path
+from typing import Final
 from unittest.mock import AsyncMock
 
 import pytest
@@ -11,8 +12,16 @@ import socketio
 from asgi_lifespan import LifespanManager
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
+from models_library.api_schemas_dynamic_sidecar.ports import (
+    InputPortSatus,
+    InputStatus,
+    OutputPortStatus,
+    OutputStatus,
+)
 from models_library.api_schemas_dynamic_sidecar.socketio import (
     SOCKET_IO_SERVICE_DISK_USAGE_EVENT,
+    SOCKET_IO_STATE_INPUT_PORTS_EVENT,
+    SOCKET_IO_STATE_OUTPUT_PORTS_EVENT,
 )
 from models_library.api_schemas_dynamic_sidecar.telemetry import (
     DiskUsage,
@@ -20,6 +29,7 @@ from models_library.api_schemas_dynamic_sidecar.telemetry import (
 )
 from models_library.api_schemas_webserver.socketio import SocketIORoomStr
 from models_library.projects_nodes_io import NodeID
+from models_library.services_types import ServicePortKey
 from models_library.users import UserID
 from pydantic import ByteSize, NonNegativeInt, parse_obj_as
 from pytest_mock import MockerFixture
@@ -28,7 +38,10 @@ from servicelib.utils import logged_gather
 from settings_library.rabbit import RabbitSettings
 from simcore_service_dynamic_sidecar.core.application import create_app
 from simcore_service_dynamic_sidecar.core.settings import ApplicationSettings
-from simcore_service_dynamic_sidecar.modules.notifications import publish_disk_usage
+from simcore_service_dynamic_sidecar.modules.notifications import (
+    PortNotifier,
+    publish_disk_usage,
+)
 from simcore_service_dynamic_sidecar.modules.system_monitor._disk_usage import (
     DiskUsageMonitor,
 )
@@ -40,6 +53,8 @@ from tenacity.wait import wait_fixed
 pytest_simcore_core_services_selection = [
     "rabbit",
 ]
+
+_NUMBER_OF_CLIENTS: Final[NonNegativeInt] = 10
 
 
 @pytest.fixture
@@ -104,20 +119,6 @@ def room_name(user_id: UserID) -> SocketIORoomStr:
     return SocketIORoomStr.from_user_id(user_id)
 
 
-def _get_on_service_disk_usage_event(
-    socketio_client: socketio.AsyncClient,
-) -> AsyncMock:
-    # emulates front-end receiving message
-
-    async def on_service_status(data):
-        assert parse_obj_as(ServiceDiskUsage, data) is not None
-
-    on_event_spy = AsyncMock(wraps=on_service_status)
-    socketio_client.on(SOCKET_IO_SERVICE_DISK_USAGE_EVENT, on_event_spy)
-
-    return on_event_spy
-
-
 async def _assert_call_count(mock: AsyncMock, *, call_count: int) -> None:
     async for attempt in AsyncRetrying(
         wait=wait_fixed(0.1), stop=stop_after_delay(5), reraise=True
@@ -135,6 +136,20 @@ def _get_mocked_disk_usage(byte_size_str: str) -> DiskUsage:
     )
 
 
+def _get_on_service_disk_usage_spy(
+    socketio_client: socketio.AsyncClient,
+) -> AsyncMock:
+    # emulates front-end receiving message
+
+    async def on_service_status(data):
+        assert parse_obj_as(ServiceDiskUsage, data) is not None
+
+    on_event_spy = AsyncMock(wraps=on_service_status)
+    socketio_client.on(SOCKET_IO_SERVICE_DISK_USAGE_EVENT, on_event_spy)
+
+    return on_event_spy
+
+
 @pytest.mark.parametrize(
     "usage",
     [
@@ -149,7 +164,7 @@ def _get_mocked_disk_usage(byte_size_str: str) -> DiskUsage:
         ),
     ],
 )
-async def test_notifier_publish_message(
+async def test_notifier_publish_disk_usage(
     disk_usage_monitor: DiskUsageMonitor,
     socketio_server_events: dict[str, AsyncMock],
     app: FastAPI,
@@ -165,15 +180,14 @@ async def test_notifier_publish_message(
     server_disconnect = socketio_server_events["disconnect"]
     server_on_check = socketio_server_events["on_check"]
 
-    number_of_clients: NonNegativeInt = 10
     async with AsyncExitStack() as socketio_frontend_clients:
         frontend_clients: list[socketio.AsyncClient] = await logged_gather(
             *[
                 socketio_frontend_clients.enter_async_context(socketio_client_factory())
-                for _ in range(number_of_clients)
+                for _ in range(_NUMBER_OF_CLIENTS)
             ]
         )
-        await _assert_call_count(server_connect, call_count=number_of_clients)
+        await _assert_call_count(server_connect, call_count=_NUMBER_OF_CLIENTS)
 
         # client emits and check it was received
         await logged_gather(
@@ -182,11 +196,11 @@ async def test_notifier_publish_message(
                 for frontend_client in frontend_clients
             ]
         )
-        await _assert_call_count(server_on_check, call_count=number_of_clients)
+        await _assert_call_count(server_on_check, call_count=_NUMBER_OF_CLIENTS)
 
         # attach spy to client
         on_service_disk_usage_events: list[AsyncMock] = [
-            _get_on_service_disk_usage_event(c) for c in frontend_clients
+            _get_on_service_disk_usage_spy(c) for c in frontend_clients
         ]
 
         # server publishes a message
@@ -199,7 +213,175 @@ async def test_notifier_publish_message(
                 jsonable_encoder(ServiceDiskUsage(node_id=node_id, usage=usage))
             )
 
-    await _assert_call_count(server_disconnect, call_count=number_of_clients)
+    await _assert_call_count(server_disconnect, call_count=_NUMBER_OF_CLIENTS)
 
 
-# TODO: add tests for the two new types of messages
+@pytest.fixture
+def port_key() -> ServicePortKey:
+    return ServicePortKey("test_port")
+
+
+def _get_on_input_port_spy(
+    socketio_client: socketio.AsyncClient,
+) -> AsyncMock:
+    # emulates front-end receiving message
+
+    async def on_service_status(data):
+        assert parse_obj_as(ServiceDiskUsage, data) is not None
+
+    on_event_spy = AsyncMock(wraps=on_service_status)
+    socketio_client.on(SOCKET_IO_STATE_INPUT_PORTS_EVENT, on_event_spy)
+
+    return on_event_spy
+
+
+@pytest.mark.parametrize("input_status", InputStatus)
+async def test_notifier_send_input_port_status(
+    socketio_server_events: dict[str, AsyncMock],
+    app: FastAPI,
+    user_id: UserID,
+    node_id: NodeID,
+    port_key: ServicePortKey,
+    socketio_client_factory: Callable[
+        [], _AsyncGeneratorContextManager[socketio.AsyncClient]
+    ],
+    input_status: InputStatus,
+):
+    # web server spy events
+    server_connect = socketio_server_events["connect"]
+    server_disconnect = socketio_server_events["disconnect"]
+    server_on_check = socketio_server_events["on_check"]
+
+    async with AsyncExitStack() as socketio_frontend_clients:
+        frontend_clients: list[socketio.AsyncClient] = await logged_gather(
+            *[
+                socketio_frontend_clients.enter_async_context(socketio_client_factory())
+                for _ in range(_NUMBER_OF_CLIENTS)
+            ]
+        )
+        await _assert_call_count(server_connect, call_count=_NUMBER_OF_CLIENTS)
+
+        # client emits and check it was received
+        await logged_gather(
+            *[
+                frontend_client.emit("check", data="an_event")
+                for frontend_client in frontend_clients
+            ]
+        )
+        await _assert_call_count(server_on_check, call_count=_NUMBER_OF_CLIENTS)
+
+        # attach spy to client
+        on_input_port_events: list[AsyncMock] = [
+            _get_on_input_port_spy(c) for c in frontend_clients
+        ]
+
+        port_notifier = PortNotifier(app, user_id, node_id)
+
+        # server publishes a message
+        match input_status:
+            case InputStatus.UPLOAD_STARTED:
+                await port_notifier.send_input_port_upload_started(port_key)
+            case InputStatus.UPLOAD_WAS_ABORTED:
+                await port_notifier.send_input_port_upload_was_aborted(port_key)
+            case InputStatus.UPLOAD_FINISHED_SUCCESSFULLY:
+                await port_notifier.send_input_port_upload_finished_succesfully(
+                    port_key
+                )
+            case InputStatus.UPLOAD_FINISHED_WITH_ERRROR:
+                await port_notifier.send_input_port_upload_finished_with_error(port_key)
+
+        # check that all clients received it
+        for on_input_port_event in on_input_port_events:
+            await _assert_call_count(on_input_port_event, call_count=1)
+            on_input_port_event.assert_awaited_once_with(
+                jsonable_encoder(
+                    InputPortSatus(
+                        node_id=node_id, port_key=port_key, status=input_status
+                    )
+                )
+            )
+
+    await _assert_call_count(server_disconnect, call_count=_NUMBER_OF_CLIENTS)
+
+
+def _get_on_output_port_spy(
+    socketio_client: socketio.AsyncClient,
+) -> AsyncMock:
+    # emulates front-end receiving message
+
+    async def on_service_status(data):
+        assert parse_obj_as(ServiceDiskUsage, data) is not None
+
+    on_event_spy = AsyncMock(wraps=on_service_status)
+    socketio_client.on(SOCKET_IO_STATE_OUTPUT_PORTS_EVENT, on_event_spy)
+
+    return on_event_spy
+
+
+@pytest.mark.parametrize("output_status", OutputStatus)
+async def test_notifier_send_output_port_status(
+    socketio_server_events: dict[str, AsyncMock],
+    app: FastAPI,
+    user_id: UserID,
+    node_id: NodeID,
+    port_key: ServicePortKey,
+    socketio_client_factory: Callable[
+        [], _AsyncGeneratorContextManager[socketio.AsyncClient]
+    ],
+    output_status: OutputStatus,
+):
+    # web server spy events
+    server_connect = socketio_server_events["connect"]
+    server_disconnect = socketio_server_events["disconnect"]
+    server_on_check = socketio_server_events["on_check"]
+
+    async with AsyncExitStack() as socketio_frontend_clients:
+        frontend_clients: list[socketio.AsyncClient] = await logged_gather(
+            *[
+                socketio_frontend_clients.enter_async_context(socketio_client_factory())
+                for _ in range(_NUMBER_OF_CLIENTS)
+            ]
+        )
+        await _assert_call_count(server_connect, call_count=_NUMBER_OF_CLIENTS)
+
+        # client emits and check it was received
+        await logged_gather(
+            *[
+                frontend_client.emit("check", data="an_event")
+                for frontend_client in frontend_clients
+            ]
+        )
+        await _assert_call_count(server_on_check, call_count=_NUMBER_OF_CLIENTS)
+
+        # attach spy to client
+        on_output_port_events: list[AsyncMock] = [
+            _get_on_output_port_spy(c) for c in frontend_clients
+        ]
+
+        port_notifier = PortNotifier(app, user_id, node_id)
+
+        # server publishes a message
+        match output_status:
+            case OutputStatus.DOWNLOAD_STARTED:
+                await port_notifier.send_output_port_download_sarted(port_key)
+            case OutputStatus.DOWNLOAD_FINISHED_SUCCESSFULLY:
+                await port_notifier.send_output_port_download_finished_successfully(
+                    port_key
+                )
+            case OutputStatus.DOWNLOAD_FINISHED_WITH_ERRROR:
+                await port_notifier.send_output_port_download_finished_with_error(
+                    port_key
+                )
+
+        # check that all clients received it
+        for on_output_port_event in on_output_port_events:
+            await _assert_call_count(on_output_port_event, call_count=1)
+            on_output_port_event.assert_awaited_once_with(
+                jsonable_encoder(
+                    OutputPortStatus(
+                        node_id=node_id, port_key=port_key, status=output_status
+                    )
+                )
+            )
+
+    await _assert_call_count(server_disconnect, call_count=_NUMBER_OF_CLIENTS)
