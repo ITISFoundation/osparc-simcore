4dda24d05927087cc4c5138f39af7e60b6be510c
Giancarlo Romeo <giancarloromeo@gmail.com>
2024-09-27 22:29:28 +0200

    run bump-pydantic

--- services/efs-guardian/src/simcore_service_efs_guardian/core/settings.py
+++ services/efs-guardian/src/simcore_service_efs_guardian/core/settings.py
@@ -8,7 +8,7 @@ from models_library.basic_types import (
     LogLevel,
     VersionTag,
 )
-from pydantic import Field, PositiveInt, validator
+from pydantic import AliasChoices, Field, PositiveInt, field_validator
 from settings_library.base import BaseCustomSettings
 from settings_library.efs import AwsEfsSettings
 from settings_library.rabbit import RabbitSettings
@@ -59,28 +59,35 @@ class ApplicationSettings(BaseCustomSettings, MixinLoggingSettings):
 
     # RUNTIME  -----------------------------------------------------------
     EFS_GUARDIAN_DEBUG: bool = Field(
-        default=False, description="Debug mode", env=["EFS_GUARDIAN_DEBUG", "DEBUG"]
+        default=False,
+        description="Debug mode",
+        validation_alias=AliasChoices("EFS_GUARDIAN_DEBUG", "DEBUG"),
     )
     EFS_GUARDIAN_LOGLEVEL: LogLevel = Field(
-        LogLevel.INFO, env=["EFS_GUARDIAN_LOGLEVEL", "LOG_LEVEL", "LOGLEVEL"]
+        LogLevel.INFO,
+        validation_alias=AliasChoices("EFS_GUARDIAN_LOGLEVEL", "LOG_LEVEL", "LOGLEVEL"),
     )
     EFS_GUARDIAN_LOG_FORMAT_LOCAL_DEV_ENABLED: bool = Field(
         default=False,
-        env=[
+        validation_alias=AliasChoices(
             "EFS_GUARDIAN_LOG_FORMAT_LOCAL_DEV_ENABLED",
             "LOG_FORMAT_LOCAL_DEV_ENABLED",
-        ],
+        ),
         description="Enables local development log format. WARNING: make sure it is disabled if you want to have structured logs!",
     )
 
-    EFS_GUARDIAN_AWS_EFS_SETTINGS: AwsEfsSettings = Field(auto_default_from_env=True)
-    EFS_GUARDIAN_RABBITMQ: RabbitSettings = Field(auto_default_from_env=True)
+    EFS_GUARDIAN_AWS_EFS_SETTINGS: AwsEfsSettings = Field(
+        json_schema_extra={"auto_default_from_env": True}
+    )
+    EFS_GUARDIAN_RABBITMQ: RabbitSettings = Field(
+        json_schema_extra={"auto_default_from_env": True}
+    )
 
     @cached_property
     def LOG_LEVEL(self) -> LogLevel:  # noqa: N802
         return self.EFS_GUARDIAN_LOGLEVEL
 
-    @validator("EFS_GUARDIAN_LOGLEVEL")
+    @field_validator("EFS_GUARDIAN_LOGLEVEL")
     @classmethod
     def valid_log_level(cls, value: str) -> str:
         return cls.validate_log_level(value)
