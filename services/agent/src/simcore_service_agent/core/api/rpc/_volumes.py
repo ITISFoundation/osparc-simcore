ac24306b5d7fc2b7b154427a4269471b6d46bf3b
Andrei Neagu <neagu@itis.swiss>
2024-09-27 11:55:13 +0200

    reanme

--- services/agent/src/simcore_service_agent/core/api/rpc/_volumes.py
+++ services/agent/src/simcore_service_agent/core/api/rpc/_volumes.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from models_library.projects_nodes_io import NodeID
 from servicelib.rabbitmq import RPCRouter
-from simcore_service_agent.services.volume_manager import get_volume_manager
+from simcore_service_agent.services.volumes_manager import get_volumes_manager
 
 router = RPCRouter()
 
@@ -10,9 +10,12 @@ router = RPCRouter()
 async def remove_volumes_without_backup_for_service(
     app: FastAPI, *, node_id: NodeID
 ) -> None:
-    await get_volume_manager(app).remove_service_volumes(node_id)
+    await get_volumes_manager(app).remove_service_volumes(node_id)
 
 
 @router.expose()
 async def backup_and_remove_volumes_for_all_services(app: FastAPI) -> None:
-    await get_volume_manager(app).remove_all_volumes()
+    await get_volumes_manager(app).remove_all_volumes()
+
+
+# TODO: add tests for these
