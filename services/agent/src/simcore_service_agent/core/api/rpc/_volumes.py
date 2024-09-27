7f9fb2e16f54b2c7bcdcfb56efbd0c7b0bdf1176
Andrei Neagu <neagu@itis.swiss>
2024-09-27 09:13:16 +0200

    added impmenetation

--- services/agent/src/simcore_service_agent/core/api/rpc/_volumes.py
+++ services/agent/src/simcore_service_agent/core/api/rpc/_volumes.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from models_library.projects_nodes_io import NodeID
 from servicelib.rabbitmq import RPCRouter
+from simcore_service_agent.services.volume_manager import get_volume_manager
 
 router = RPCRouter()
 
@@ -9,10 +10,9 @@ router = RPCRouter()
 async def remove_volumes_without_backup_for_service(
     app: FastAPI, *, node_id: NodeID
 ) -> None:
-    _ = app
-    _ = node_id
+    await get_volume_manager(app).remove_service_volumes(node_id)
 
 
 @router.expose()
-async def backup_and_remove_all_service_volumes(app: FastAPI) -> None:
-    _ = app
+async def backup_and_remove_volumes_for_all_services(app: FastAPI) -> None:
+    await get_volume_manager(app).remove_all_volumes()
