edcff82c12f9730296bb0c456fe5e543b48ba46f
Odei Maiz <maiz@itis.swiss>
2024-09-27 14:48:30 +0200

    minor

--- services/static-webserver/client/source/class/osparc/dashboard/StudyBrowser.js
+++ services/static-webserver/client/source/class/osparc/dashboard/StudyBrowser.js
@@ -516,7 +516,7 @@ qx.Class.define("osparc.dashboard.StudyBrowser", {
     },
 
     __showMoveToWorkspaceWarningMessage: function() {
-      const msg = this.tr("The permissions will be taken from the new workspace?");
+      const msg = this.tr("The permissions will be taken from the new workspace.");
       const win = new osparc.ui.window.Confirmation(msg).set({
         confirmText: this.tr("Move"),
       });
