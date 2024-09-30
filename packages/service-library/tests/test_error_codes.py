64f59e5828ece7da2d66bce5a6487cfcf96b6f23
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-09-30 16:30:12 +0200

    formatted message for troubleshooting

--- packages/service-library/tests/test_error_codes.py
+++ packages/service-library/tests/test_error_codes.py
@@ -5,50 +5,50 @@
 
 import logging
 
+import pytest
 from servicelib.error_codes import create_error_code, parse_error_code
 
 logger = logging.getLogger(__name__)
 
 
-def test_error_code_use_case(caplog):
+def test_error_code_use_case(caplog: pytest.LogCaptureFixture):
     """use case for error-codes"""
-    try:
+    with pytest.raises(RuntimeError) as exc_info:
         raise RuntimeError("Something unexpected went wrong")
-    except Exception as err:
-        # 1. Unexpected ERROR
 
-        # 2. create error-code
-        error_code = create_error_code(err)
+    # 1. Unexpected ERROR
+    err = exc_info.value
 
-        # 3. log all details in service
-        caplog.clear()
+    # 2. create error-code
+    error_code = create_error_code(err)
 
-        # Can add a formatter that prefix error-codes
-        syslog = logging.StreamHandler()
-        syslog.setFormatter(
-            logging.Formatter("%(asctime)s %(error_code)s : %(message)s")
-        )
-        logger.addHandler(syslog)
+    # 3. log all details in service
+    caplog.clear()
 
-        logger.error("Fake Unexpected error", extra={"error_code": error_code})
+    # Can add a formatter that prefix error-codes
+    syslog = logging.StreamHandler()
+    syslog.setFormatter(logging.Formatter("%(asctime)s %(error_code)s : %(message)s"))
+    logger.addHandler(syslog)
 
-        # logs something like E.g. 2022-07-06 14:31:13,432 OEC:140350117529856 : Fake Unexpected error
-        assert parse_error_code(
-            f"2022-07-06 14:31:13,432 {error_code} : Fake Unexpected error"
-        ) == {
-            error_code,
-        }
+    logger.exception("Fake Unexpected error", extra={"error_code": error_code})
 
-        assert caplog.records[0].error_code == error_code
-        assert caplog.records[0]
+    # logs something like E.g. 2022-07-06 14:31:13,432 OEC:140350117529856 : Fake Unexpected error
+    assert parse_error_code(
+        f"2022-07-06 14:31:13,432 {error_code} : Fake Unexpected error"
+    ) == {
+        error_code,
+    }
 
-        logger.error("Fake without error_code")
+    assert caplog.records[0].error_code == error_code
+    assert caplog.records[0]
 
-        # 4. inform user (e.g. with new error or sending message)
-        user_message = (
-            f"This is a user-friendly message to inform about an error. [{error_code}]"
-        )
+    logger.exception("Fake without error_code")
 
-        assert parse_error_code(user_message) == {
-            error_code,
-        }
+    # 4. inform user (e.g. with new error or sending message)
+    user_message = (
+        f"This is a user-friendly message to inform about an error. [{error_code}]"
+    )
+
+    assert parse_error_code(user_message) == {
+        error_code,
+    }
