5076541e4491fb457f8935b146929c1fff517496
Odei Maiz <maiz@itis.swiss>
2024-10-03 13:05:35 +0200

    keep ready

--- services/static-webserver/client/source/class/osparc/data/model/IframeHandler.js
+++ services/static-webserver/client/source/class/osparc/data/model/IframeHandler.js
@@ -309,7 +309,7 @@ qx.Class.define("osparc.data.model.IframeHandler", {
     __serviceReadyIn: function(srvUrl) {
       const node = this.getNode();
       node.setServiceUrl(srvUrl);
-      this.__setCustomInteractive("ready");
+      node.getStatus().setInteractive("ready");
     },
 
     __setCustomInteractive: function(customStatus) {
@@ -396,7 +396,7 @@ qx.Class.define("osparc.data.model.IframeHandler", {
       const node = this.getNode();
       const status = node.getStatus().getInteractive();
       // it might have been stopped
-      if (status === "running") {
+      if (["running", "ready"].includes(status)) {
         this.getIFrame().resetSource();
         this.getIFrame().setSource(node.getServiceUrl());
 
