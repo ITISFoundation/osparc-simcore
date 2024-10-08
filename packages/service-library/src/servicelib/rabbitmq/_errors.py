cd00aec5a01c18fb438f59a6debbeb7c66fb37da
Giancarlo Romeo <giancarloromeo@gmail.com>
2024-10-08 09:24:43 +0200

    ignore assignment

--- packages/service-library/src/servicelib/rabbitmq/_errors.py
+++ packages/service-library/src/servicelib/rabbitmq/_errors.py
@@ -10,12 +10,12 @@ class BaseRPCError(OsparcErrorMixin, RuntimeError):
 
 
 class RPCNotInitializedError(BaseRPCError):
-    code = f"{_ERROR_PREFIX}.not_started"
+    code = f"{_ERROR_PREFIX}.not_started"   # type: ignore[assignment]
     msg_template = "Please check that the RabbitMQ RPC backend was initialized!"
 
 
 class RemoteMethodNotRegisteredError(BaseRPCError):
-    code = f"{_ERROR_PREFIX}.remote_not_registered"
+    code = f"{_ERROR_PREFIX}.remote_not_registered" # type: ignore[assignment]
     msg_template = (
         "Could not find a remote method named: '{method_name}'. "
         "Message from remote server was returned: {incoming_message}. "
