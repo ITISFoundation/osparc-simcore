f7d71e433a3ebf61e2893171fba46c5232a15bac
Andrei Neagu <neagu@itis.swiss>
2024-10-08 07:47:02 +0200

    remove out of date comment

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/cli.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/cli.py
@@ -31,13 +31,6 @@ def echo_dotenv(ctx: typer.Context, *, minimal: bool = True):
     """
     assert ctx  # nosec
 
-    # NOTE: we normally DO NOT USE `os.environ` to capture env vars but this is a special case
-    # The idea here is to have a command that can generate a **valid** `.env` file that can be used
-    # to initialized the app. For that reason we fill required fields of the `ApplicationSettings` with
-    # "fake" but valid values (e.g. generating a password or adding tags as `replace-with-api-key).
-    # Nonetheless, if the caller of this CLI has already some **valid** env vars in the environment we want to use them ...
-    # and that is why we use `os.environ`.
-
     settings = ApplicationSettings.create_from_envs()
 
     print_as_envfile(
