7663205ee85ec18044baed3ee3c9e20db39d9707
Giancarlo Romeo <giancarloromeo@gmail.com>
2024-10-09 09:32:45 +0200

    fix networks types

--- packages/common-library/src/common_library/pydantic_networks_extension.py
+++ packages/common-library/src/common_library/pydantic_networks_extension.py
@@ -1,27 +1,25 @@
 from typing import Annotated, TypeAlias
 
 from pydantic import AfterValidator, AnyHttpUrl, AnyUrl, HttpUrl
+from pydantic_core import Url
 
 
-def _strip_last_slash(url: str) -> str:
-    return url.rstrip("/")
+def _strip_last_slash(url: Url) -> str:
+    return f"{url}".rstrip("/")
 
 
 AnyUrlLegacy: TypeAlias = Annotated[
     AnyUrl,
-    AfterValidator(str),
     AfterValidator(_strip_last_slash),
 ]
 
 AnyHttpUrlLegacy: TypeAlias = Annotated[
     AnyHttpUrl,
-    AfterValidator(str),
     AfterValidator(_strip_last_slash),
 ]
 
 
 HttpUrlLegacy: TypeAlias = Annotated[
     HttpUrl,
-    AfterValidator(str),
     AfterValidator(_strip_last_slash),
 ]
--- packages/common-library/tests/test_pydantic_networks_extension.py
+++ packages/common-library/tests/test_pydantic_networks_extension.py
@@ -1,6 +1,11 @@
+import pytest
 from common_library.pydantic_networks_extension import AnyHttpUrlLegacy
-from pydantic import AnyHttpUrl, TypeAdapter
+from pydantic import AnyHttpUrl, BaseModel, TypeAdapter, ValidationError
 from pydantic_core import Url
+
+
+class A(BaseModel):
+    url: AnyHttpUrlLegacy
 
 
 def test_any_http_url():
@@ -9,12 +14,26 @@ def test_any_http_url():
     )
 
     assert isinstance(url, Url)
-    assert f"{url}" == "http://backgroud.testserver.io/"    # NOTE: trailing '/' added in Pydantic v2
+    assert (
+        f"{url}" == "http://backgroud.testserver.io/"
+    )  # trailing slash added (in Pydantic v2)
+
 
 def test_any_http_url_legacy():
     url = TypeAdapter(AnyHttpUrlLegacy).validate_python(
-            "http://backgroud.testserver.io",
+        "http://backgroud.testserver.io",
     )
 
     assert isinstance(url, str)
-    assert url == "http://backgroud.testserver.io"
+    assert url == "http://backgroud.testserver.io"  # no trailing slash was added
+
+
+def test_valid_any_http_url_legacy_field():
+    a = A(url="http://backgroud.testserver.io")  # type: ignore
+
+    assert a.url == "http://backgroud.testserver.io"  # no trailing slash was added
+
+
+def test_not_valid_any_http_url_legacy_field():
+    with pytest.raises(ValidationError):
+        A(url="htttttp://backgroud.testserver.io")  # type: ignore
