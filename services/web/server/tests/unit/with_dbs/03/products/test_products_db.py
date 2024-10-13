74088cbf70013f2d1cea9f346049e6d31c4d1843
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-10-13 12:00:15 +0200

    default app

--- services/web/server/tests/unit/with_dbs/03/products/test_products_db.py
+++ services/web/server/tests/unit/with_dbs/03/products/test_products_db.py
@@ -148,8 +148,6 @@ async def test_product_repository_get_product(
     assert await product_repository.get_product(product.name) == product
 
     # tests definitions of default from utle_products and web-server.products are in sync
-    mock_request = mocker.MagicMock()
-    mock_request.app = app
     async with product_repository.engine.acquire() as conn:
         default_product = await utils_products.get_default_product_name(conn)
-        assert default_product == _get_default_product_name(mock_request)
+        assert default_product == _get_default_product_name(app)
