84eb8df7a1310c566bc2a8818194581e17709efd
Andrei Neagu <neagu@itis.swiss>
2024-10-18 11:20:15 +0200

    added missing import

--- services/payments/tests/unit/api/test__payment_method_workflows.py
+++ services/payments/tests/unit/api/test__payment_method_workflows.py
@@ -18,7 +18,7 @@ from models_library.api_schemas_webserver.wallets import (
 from models_library.rabbitmq_basic_types import RPCMethodName
 from models_library.users import UserID
 from models_library.wallets import WalletID
-from pydantic import EmailStr
+from pydantic import EmailStr, TypeAdapter
 from pytest_mock import MockerFixture
 from pytest_simcore.helpers.monkeypatch_envs import setenvs_from_dict
 from pytest_simcore.helpers.typing_env import EnvVarsDict
