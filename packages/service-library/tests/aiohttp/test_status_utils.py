322cc7da12a3c5da1b3ecbe8b05055a5dccdb23b
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-10-18 16:02:07 +0200

    adds tests

--- packages/service-library/tests/aiohttp/test_status_utils.py
+++ packages/service-library/tests/aiohttp/test_status_utils.py
@@ -1,17 +1,22 @@
 from http import HTTPStatus
 
+import pytest
 from servicelib.aiohttp import status
-from servicelib.status_utils import (
+from servicelib.aiohttp.web_exceptions_extension import (
+    STATUS_CODES_WITHOUT_AIOHTTP_EXCEPTION_CLASS,
+    get_all_aiohttp_http_exceptions,
+)
+from servicelib.status_codes_utils import (
     _INVALID_STATUS_CODE_MSG,
     get_code_description,
     get_code_display_name,
     get_http_status_codes,
-    is_client_error,
+    is_1xx_informational,
+    is_2xx_success,
+    is_3xx_redirect,
+    is_4xx_client_error,
+    is_5xx_server_error,
     is_error,
-    is_informational,
-    is_redirect,
-    is_server_error,
-    is_success,
 )
 
 
@@ -31,12 +36,12 @@ def test_description():
 
 def test_status_codes_checks():
 
-    assert is_informational(status.HTTP_102_PROCESSING)
-    assert is_success(status.HTTP_202_ACCEPTED)
-    assert is_redirect(status.HTTP_301_MOVED_PERMANENTLY)
+    assert is_1xx_informational(status.HTTP_102_PROCESSING)
+    assert is_2xx_success(status.HTTP_202_ACCEPTED)
+    assert is_3xx_redirect(status.HTTP_301_MOVED_PERMANENTLY)
 
-    assert is_client_error(status.HTTP_401_UNAUTHORIZED)
-    assert is_server_error(status.HTTP_503_SERVICE_UNAVAILABLE)
+    assert is_4xx_client_error(status.HTTP_401_UNAUTHORIZED)
+    assert is_5xx_server_error(status.HTTP_503_SERVICE_UNAVAILABLE)
 
     assert is_error(status.HTTP_401_UNAUTHORIZED)
     assert is_error(status.HTTP_503_SERVICE_UNAVAILABLE)
@@ -45,7 +50,7 @@ def test_status_codes_checks():
 def test_predicates_with_status():
 
     # in formational
-    assert get_http_status_codes(status, is_informational) == [
+    assert get_http_status_codes(status, is_1xx_informational) == [
         status.HTTP_100_CONTINUE,
         status.HTTP_101_SWITCHING_PROTOCOLS,
         status.HTTP_102_PROCESSING,
@@ -61,3 +66,15 @@ def test_predicates_with_status():
         for c in get_http_status_codes(status)
         if c != status.HTTP_306_RESERVED
     ]
+
+
+AIOHTTP_EXCEPTION_CLASSES_MAP = get_all_aiohttp_http_exceptions()
+
+
+@pytest.mark.parametrize("status_code", get_http_status_codes(status))
+def test_how_status_codes_map_to_aiohttp_exception_class(status_code):
+    aiohttp_exception_cls = AIOHTTP_EXCEPTION_CLASSES_MAP.get(status_code)
+    if status_code in STATUS_CODES_WITHOUT_AIOHTTP_EXCEPTION_CLASS:
+        assert aiohttp_exception_cls is None
+    else:
+        assert aiohttp_exception_cls is not None
