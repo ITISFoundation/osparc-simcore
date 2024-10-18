59ed5635784d1c9e405c404476235696475faa7a
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-10-18 17:33:50 +0200

    missing

--- services/web/server/src/simcore_service_webserver/workspaces/_groups_handlers.py
+++ services/web/server/src/simcore_service_webserver/workspaces/_groups_handlers.py
@@ -162,4 +162,4 @@ async def delete_workspace_group(request: web.Request):
         group_id=path_params.group_id,
         product_name=req_ctx.product_name,
     )
-    raise web.HTTPNoContent(content_type=MIMETYPE_APPLICATION_JSON)
+    return web.HTTPNoContent(content_type=MIMETYPE_APPLICATION_JSON)
