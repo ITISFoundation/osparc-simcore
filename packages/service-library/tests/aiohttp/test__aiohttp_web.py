9687f3202eb7c0be5a83faf683ca8d41e78882da
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-10-21 09:32:34 +0200

    rm unused test

--- packages/service-library/tests/aiohttp/test__aiohttp_web.py
+++
@@ -1,7 +0,0 @@
-from aiohttp import web
-from servicelib.mimetype_constants import MIMETYPE_APPLICATION_JSON
-
-
-def test_http_errors():
-
-    err = web.HTTPNoContent(content_type=MIMETYPE_APPLICATION_JSON)
