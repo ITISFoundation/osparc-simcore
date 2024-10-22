e178044451c5e2eee5f76462fd1716bcdb018c58
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-10-22 18:08:21 +0200

    drafts

---
+++ services/web/server/tests/unit/with_dbs/02/test_projects_trash.py
@@ -0,0 +1,12 @@
+from aiohttp.test_tools import TestClient
+
+
+def test_it(client: TestClient):
+    raise NotImplementedError
+
+    # list projects
+
+    # trash project
+    #
+
+    # list trashed projects
