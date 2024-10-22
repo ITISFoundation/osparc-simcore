a46c633dbd631e9322a9706d0094b20116fbbdeb
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-10-22 15:20:30 +0200

    fix test

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/main.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/main.py
@@ -11,8 +11,8 @@ from simcore_service_dynamic_scheduler.core.settings import ApplicationSettings
 
 _the_settings = ApplicationSettings.create_from_envs()
 
-logging.basicConfig(level=_the_settings.DYNAMIC_SCHEDULER_LOGLEVEL)
-logging.root.setLevel(_the_settings.DYNAMIC_SCHEDULER_LOGLEVEL)
+logging.basicConfig(level=_the_settings.DYNAMIC_SCHEDULER_LOGLEVEL.value)
+logging.root.setLevel(_the_settings.DYNAMIC_SCHEDULER_LOGLEVEL.value)
 config_all_loggers(
     log_format_local_dev_enabled=_the_settings.DYNAMIC_SCHEDULER_LOG_FORMAT_LOCAL_DEV_ENABLED
 )
