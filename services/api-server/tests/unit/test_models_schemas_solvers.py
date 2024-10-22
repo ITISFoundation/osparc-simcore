245739e7bb3f961fcd72c00b35858df2a2dd0079
Giancarlo Romeo <giancarloromeo@gmail.com>
2024-10-22 13:31:28 +0200

    fix mypy

--- services/api-server/tests/unit/test_models_schemas_solvers.py
+++ services/api-server/tests/unit/test_models_schemas_solvers.py
@@ -20,14 +20,14 @@ def test_solvers_sorting_by_name_and_version(faker: Faker):
 
     # and a different version of the same
     # NOTE: that id=None so that it can be re-coputed
-    earlier_release = one_solver.copy(
+    earlier_release = one_solver.model_copy(
         update={"version": f"{one_solver.version}beta"}, deep=True
     )
     assert earlier_release.pep404_version.is_prerelease
     assert earlier_release.pep404_version < one_solver.pep404_version
 
     # and yet a completely different solver
-    another_solver = one_solver.copy(update={"id": "simcore/services/comp/zSolve"})
+    another_solver = one_solver.model_copy(update={"id": "simcore/services/comp/zSolve"})
     assert one_solver.id != another_solver.id
     assert one_solver.pep404_version == another_solver.pep404_version
 
