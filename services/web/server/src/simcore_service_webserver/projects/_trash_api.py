a6f626bd508df62422f2370b29b6477253c7ea8c
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-10-22 18:08:21 +0200

    drafts

---
+++ services/web/server/src/simcore_service_webserver/projects/_trash_api.py
@@ -0,0 +1,10 @@
+async def trash_project():
+    raise NotImplementedError
+
+
+async def untrash_project():
+    raise NotImplementedError
+
+
+async def emtpy_trash():
+    raise NotImplementedError
