f68123be87f62060315c9d8c62741860cc89cc8d
Giancarlo Romeo <giancarloromeo@gmail.com>
2024-10-22 11:58:19 +0200

    fix mypy

--- services/api-server/tests/unit/_with_db/conftest.py
+++ services/api-server/tests/unit/_with_db/conftest.py
@@ -260,7 +260,7 @@ async def create_fake_api_keys(
             row = await result.fetchone()
             assert row
             _generate_fake_api_key.row_ids.append(row.id)
-            yield ApiKeyInDB.from_orm(row)
+            yield ApiKeyInDB.model_validate(row)
 
     _generate_fake_api_key.row_ids = []
     yield _generate_fake_api_key
