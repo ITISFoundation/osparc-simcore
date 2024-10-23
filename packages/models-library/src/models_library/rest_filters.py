31417c091513b55902d66a72a7d2337407e6045e
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-10-23 11:30:00 +0200

    aadds filters

--- packages/models-library/src/models_library/rest_filters.py
+++ packages/models-library/src/models_library/rest_filters.py
@@ -5,8 +5,9 @@ from pydantic.generics import GenericModel
 
 
 class Filters(BaseModel):
-    """inspired by Docker API https://docs.docker.com/engine/api/v1.43/#tag/Container/operation/ContainerList.
+    """
     Encoded as JSON. Each available filter can have its own logic (should be well documented)
+    Inspired by Docker API https://docs.docker.com/engine/api/v1.43/#tag/Container/operation/ContainerList.
     """
 
 
@@ -14,8 +15,8 @@ class Filters(BaseModel):
 FilterT = TypeVar("FilterT", bound=Filters)
 
 
-class FiltersQueryParam(GenericModel, Generic[FilterT]):
+class FiltersQueryParameters(GenericModel, Generic[FilterT]):
     filters: Json[FilterT] | None = Field(
         default=None,
-        description="Custom filter query parameter encoded JSON",
+        description="Custom filter query parameter encoded as JSON",
     )
--- packages/models-library/tests/test_rest_filters.py
+++ packages/models-library/tests/test_rest_filters.py
@@ -1,10 +1,11 @@
 import logging
 
 import pytest
-from models_library.rest_filters import Filters, FiltersQueryParam
+from models_library.rest_filters import Filters, FiltersQueryParameters
 from pydantic import Extra, ValidationError
 
 
+# 1. create filter model
 class CustomFilter(Filters):
     is_trashed: bool | None = None
     is_hidden: bool | None = None
@@ -17,20 +18,18 @@ class CustomFilterStrict(CustomFilter):
 
 def test_custom_filter_query_parameters():
 
-    # create filter model
+    # 2. use generic as query parameters
+    logging.info(
+        "json schema is for the query \n %s",
+        FiltersQueryParameters[CustomFilter].schema_json(indent=1),
+    )
 
     # lets filter only is_trashed and unset is_hidden
     custom_filter = CustomFilter(is_trashed=True)
     assert custom_filter.json() == '{"is_trashed": true, "is_hidden": null}'
 
-    # filter query parameters
-    logging.info(
-        "json schema is for the query \n %s",
-        FiltersQueryParam[CustomFilter].schema_json(indent=1),
-    )
-
     # default to None (optional)
-    query_param = FiltersQueryParam[CustomFilter]()
+    query_param = FiltersQueryParameters[CustomFilter]()
     assert query_param.filters is None
 
 
@@ -45,7 +44,7 @@ def test_custom_filter_query_parameters():
 def test_valid_filter_queries(
     url_query_value: str | None, expects: CustomFilter | None
 ):
-    query_param = FiltersQueryParam[CustomFilter](filters=url_query_value)
+    query_param = FiltersQueryParameters[CustomFilter](filters=url_query_value)
     assert query_param.filters == expects
 
 
@@ -53,7 +52,7 @@ def test_invalid_filter_query_is_ignored():
     # NOTE: invalid filter get ignored!
     url_query_value = '{"undefined_filter": true, "is_hidden": true}'
 
-    query_param = FiltersQueryParam[CustomFilter](filters=url_query_value)
+    query_param = FiltersQueryParameters[CustomFilter](filters=url_query_value)
     assert query_param.filters == CustomFilter(is_hidden=True)
 
 
@@ -63,4 +62,4 @@ def test_invalid_filter_query_fails():
     url_query_value = '{"undefined_filter": true, "is_hidden": true}'
 
     with pytest.raises(ValidationError):
-        FiltersQueryParam[CustomFilterStrict](filters=url_query_value)
+        FiltersQueryParameters[CustomFilterStrict](filters=url_query_value)
