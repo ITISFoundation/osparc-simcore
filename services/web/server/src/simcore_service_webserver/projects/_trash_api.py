206cd7f0134b5b165082b4239dc29e49e89ba41b
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-10-24 14:44:53 +0200

    @sanderegg review: stop before trashing

--- services/web/server/src/simcore_service_webserver/projects/_trash_api.py
+++ services/web/server/src/simcore_service_webserver/projects/_trash_api.py
@@ -7,8 +7,12 @@ from aiohttp import web
 from models_library.products import ProductName
 from models_library.projects import ProjectID
 from models_library.users import UserID
+from servicelib.common_headers import UNDEFINED_DEFAULT_SIMCORE_USER_AGENT_VALUE
+from simcore_service_webserver.director_v2.exceptions import DirectorServiceError
 
+from ..director_v2 import api as director_v2_api
 from . import projects_api
+from .exceptions import ProjectLockError, StopProjectBeforeTrashError
 from .models import ProjectPatchExtended
 from .settings import get_plugin_settings
 
@@ -18,7 +22,7 @@ _logger = logging.getLogger(__name__)
 async def empty_trash(app: web.Application, product_name: ProductName, user_id: UserID):
     # filter trashed=True and set them to False
     _logger.debug(
-        "CODE PLACEHOLDER: all projects marked of trashed of %s in %s are deleted",
+        "CODE PLACEHOLDER: all projects marked as trashed of %s in %s are deleted",
         f"{user_id=}",
         f"{product_name=}",
     )
@@ -38,7 +42,7 @@ async def prune_all_trashes(app: web.Application) -> list[str]:
     return []
 
 
-async def update_project(
+async def trash_project(
     app: web.Application,
     *,
     product_name: ProductName,
@@ -46,8 +50,29 @@ async def update_project(
     project_id: ProjectID,
     trashed: bool,
 ):
-    # FIXME: can you trash something that is running?
+    if trashed:
+        # stop first
+        try:
+            await projects_api.remove_project_dynamic_services(
+                user_id=user_id,
+                project_uuid=f"{project_id}",
+                app=app,
+                simcore_user_agent=UNDEFINED_DEFAULT_SIMCORE_USER_AGENT_VALUE,
+                notify_users=False,
+            )
 
+            await director_v2_api.delete_pipeline(
+                app, user_id=user_id, project_id=project_id
+            )
+        except (DirectorServiceError, ProjectLockError) as exc:
+            raise StopProjectBeforeTrashError(
+                project_uuid=project_id,
+                user_id=user_id,
+                product_name=product_name,
+                from_err=exc,
+            ) from exc
+
+    # mark as trash
     await projects_api.patch_project(
         app,
         user_id=user_id,
