220944d2cf74330a0475a3b5b47de0798539b1a8
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-10-24 10:30:35 +0200

    fixed validation

--- services/dask-sidecar/src/simcore_service_dask_sidecar/computational_sidecar/models.py
+++ services/dask-sidecar/src/simcore_service_dask_sidecar/computational_sidecar/models.py
@@ -3,7 +3,14 @@ import re
 from models_library.basic_regex import SIMPLE_VERSION_RE
 from models_library.services import ServiceMetaDataPublished
 from packaging import version
-from pydantic import BaseModel, ByteSize, ConfigDict, Field, field_validator
+from pydantic import (
+    BaseModel,
+    ByteSize,
+    ConfigDict,
+    Field,
+    field_validator,
+    model_validator,
+)
 
 LEGACY_INTEGRATION_VERSION = version.Version("0")
 PROGRESS_REGEXP: re.Pattern[str] = re.compile(
@@ -41,21 +48,15 @@ class ContainerHostConfig(BaseModel):
         ..., alias="NanoCPUs", description="CPU quota in units of 10-9 CPUs"
     )
 
-    @field_validator("memory_swap", mode="before")
-    @classmethod
-    def ensure_no_memory_swap_means_no_swap(cls, v, values):
-        if v is None:
-            # if not set it will be the same value as memory to ensure swap is disabled
-            return values["memory"]
-        return v
+    @model_validator(mode="after")
+    def ensure_memory_swap_is_not_unlimited(self) -> "ContainerHostConfig":
+        if self.memory_swap is None:
+            self.memory_swap = self.memory
 
-    @field_validator("memory_swap", mode="after")
-    @classmethod
-    def ensure_memory_swap_cannot_be_unlimited_nor_smaller_than_memory(cls, v, values):
-        if v < values["memory"]:
+        if self.memory_swap < self.memory:
             msg = "Memory swap cannot be set to a smaller value than memory"
             raise ValueError(msg)
-        return v
+        return self
 
 
 class DockerContainerConfig(BaseModel):
