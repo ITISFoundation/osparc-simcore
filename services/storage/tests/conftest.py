c4e166695c7ec8a7795993838d768856e57bcde8
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-10-25 10:36:30 +0200

    revert change

--- services/storage/tests/conftest.py
+++ services/storage/tests/conftest.py
@@ -465,7 +465,7 @@ async def create_empty_directory(
 
         directory_file_id = create_simcore_file_id(project_id, node_id, dir_name)
         directory_file_upload: FileUploadSchema = await create_upload_file_link_v2(
-            directory_file_id, link_type="s3", is_directory="true", file_size=0
+            directory_file_id, link_type="s3", is_directory="true", file_size=-1
         )
         # always returns a v2 link when dealing with directories
         assert isinstance(directory_file_upload, FileUploadSchema)
