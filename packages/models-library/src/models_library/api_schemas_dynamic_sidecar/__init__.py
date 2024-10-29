fc1e2a7952581cd7de2942df902b1fd35092b274
Andrei Neagu <5694077+GitHK@users.noreply.github.com>
2024-10-29 12:18:35 +0100

    ♻️ dynamic-sidecar rpc interfce namespace is now tied to the node_id (#6614)
    Co-authored-by: Andrei Neagu <neagu@itis.swiss>

--- packages/models-library/src/models_library/api_schemas_dynamic_sidecar/__init__.py
+++
@@ -1,9 +0,0 @@
-from typing import Final
-
-from pydantic import parse_obj_as
-
-from ..rabbitmq_basic_types import RPCNamespace
-
-DYNAMIC_SIDECAR_RPC_NAMESPACE: Final[RPCNamespace] = parse_obj_as(
-    RPCNamespace, "dynamic-sidecar"
-)
