3d22067c8a8204d904dede4b42cd7204d878b57b
Andrei Neagu <neagu@itis.swiss>
2024-11-01 10:26:13 +0100

    replace rate check with size check

--- services/director-v2/src/simcore_service_director_v2/modules/dynamic_sidecar/scheduler/_core/_events_utils.py
+++ services/director-v2/src/simcore_service_director_v2/modules/dynamic_sidecar/scheduler/_core/_events_utils.py
@@ -173,10 +173,10 @@ async def service_save_state(
         size = await sidecars_client.save_service_state(
             scheduler_data.endpoint, progress_callback=progress_callback
         )
-    if rate := get_rate(size, duration.to_float()) >= 1:
+    if size > 0:
         get_instrumentation(app).dynamic_sidecar_metrics.push_service_state_rate.labels(
             **get_metrics_labels(scheduler_data)
-        ).observe(rate)
+        ).observe(get_rate(size, duration.to_float()))
 
     await sidecars_client.update_volume_state(
         scheduler_data.endpoint,
@@ -480,13 +480,13 @@ async def prepare_services_environment(
             size: int = await sidecars_client.pull_service_output_ports(
                 dynamic_sidecar_endpoint
             )
-        if rate := get_rate(size, duration.to_float()) >= 1:
+        if size > 0:
             get_instrumentation(
                 app
             ).dynamic_sidecar_metrics.output_ports_pull_rate.labels(
                 **get_metrics_labels(scheduler_data)
             ).observe(
-                rate
+                get_rate(size, duration.to_float())
             )
 
     async def _pull_user_services_images_with_metrics() -> None:
@@ -505,13 +505,13 @@ async def prepare_services_environment(
         with track_duration() as duration:
             size = await sidecars_client.restore_service_state(dynamic_sidecar_endpoint)
 
-        if rate := get_rate(size, duration.to_float()) >= 1:
+        if size > 0:
             get_instrumentation(
                 app
             ).dynamic_sidecar_metrics.pull_service_state_rate.labels(
                 **get_metrics_labels(scheduler_data)
             ).observe(
-                rate
+                get_rate(size, duration.to_float())
             )
 
     tasks = [
