ec41289d7f48f4c1df5b0ffd1af520e304cd9d77
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-11-01 18:32:57 +0100

    revert pylint

--- services/director-v2/src/simcore_service_director_v2/api/routes/computations.py
+++ services/director-v2/src/simcore_service_director_v2/api/routes/computations.py
@@ -288,7 +288,7 @@ async def _try_start_pipeline(
 )
 # NOTE: in case of a burst of calls to that endpoint, we might end up in a weird state.
 @run_sequentially_in_context(target_args=["computation.project_id"])
-async def create_computation(  # noqa: PLR0913
+async def create_computation(  # noqa: PLR0913 # pylint: disable=too-many-positional-arguments
     computation: ComputationCreate,
     request: Request,
     project_repo: Annotated[
