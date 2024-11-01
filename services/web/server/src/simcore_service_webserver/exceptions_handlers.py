78e868add891668c4c6e0c532458eca31f26d0d3
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-11-01 20:37:12 +0100

    slowly conforming to fastapi

--- services/web/server/src/simcore_service_webserver/exceptions_handlers.py
+++ services/web/server/src/simcore_service_webserver/exceptions_handlers.py
@@ -8,7 +8,7 @@ from typing import NamedTuple, Protocol, TypeAlias
 
 from aiohttp import web
 from servicelib.aiohttp.typing_extension import Handler
-from servicelib.aiohttp.web_exceptions_extension import get_http_error_class_or_none
+from servicelib.aiohttp.web_exceptions_extension import get_all_aiohttp_http_exceptions
 from servicelib.logging_errors import create_troubleshotting_log_kwargs
 from servicelib.status_codes_utils import is_5xx_server_error
 
@@ -18,12 +18,12 @@ _logger = logging.getLogger(__name__)
 class WebApiExceptionHandler(Protocol):
     def __call__(
         self, exception: BaseException, request: web.Request
-    ) -> web.HTTPError | BaseException | None:
+    ) -> web.HTTPException | BaseException | None:
         """
         Callback to process an exception raised during a web request, allowing custom handling.
 
         This function can be implemented to  suppress, reraise, or transform the exception
-        into an `web.HTTPError` (i.e.the errors specified in the web-api)
+        into an `web.HTTPException` (i.e. exceptions defined at the web-api)
 
         Arguments:
             exception -- exception raised in web handler during this request
@@ -32,7 +32,7 @@ class WebApiExceptionHandler(Protocol):
         Returns:
             - None: to suppress `exception`
             - `exception`: to reraise it
-            - an instance of `web.HTTPError` to transform to HTTP Api exceptions
+            - an instance of `web.HTTPException` to transform to HTTP Api exceptions (NOTE: that they can either be errors or success!)
         """
 
 
@@ -59,8 +59,7 @@ def create_exception_handlers_decorator(
     exception_types: type[BaseException] | tuple[type[BaseException], ...] = Exception,
 ):
     """
-    Creates a function to decorate routes handlers functions
-    that can catch and handle exceptions raised in the decorated functions
+    Factory to create decorators that wraps exceptions on api-handler functions
     """
 
     def _decorator(handler: Handler):
@@ -79,8 +78,70 @@ def create_exception_handlers_decorator(
 
 
 #
-# Http Error Map Handler Customization
+# CUSTOM EXCEPTIONS HANDLERS
 #
+
+
+class _DefaultDict(dict):
+    def __missing__(self, key):
+        return f"'{key}=?'"
+
+
+_STATUS_CODE_TO_HTTP_EXCEPTIONS: dict[
+    int, type[web.HTTPException]
+] = get_all_aiohttp_http_exceptions(web.HTTPException)
+
+
+def make_handler_exception_to_status_code(
+    exception_cls: type[BaseException], status_code: int, user_msg_template: str
+) -> WebApiExceptionHandler:
+    """
+    Creates a custom `WebApiExceptionHandler` that maps specific exception to an http status code error
+
+    Given an `ExceptionToHttpErrorMap`, this function returns a handler that checks if an exception
+    matches one in the map, returning an HTTP error with the mapped status code and message.
+    Server errors (5xx) include additional logging with request context. Unmapped exceptions are
+    returned as-is for re-raising.
+
+    Arguments:
+        exception_cls: exception raise during the request
+        status_code: the http status code to associate at the web-api interface to this error
+        user_msg_template: a template string to pass to the HttpError
+
+    Returns:
+        A web api exception handler
+    """
+
+    def _exception_handler(
+        exception: BaseException,
+        request: web.Request,
+    ) -> web.HTTPException | BaseException | None:
+        assert isinstance(exception, exception_cls)  # nosec
+
+        # safe formatting, i.e. does not raise
+        user_msg = user_msg_template.format_map(
+            _DefaultDict(getattr(exception, "__dict__", {}))
+        )
+
+        http_error_cls = _STATUS_CODE_TO_HTTP_EXCEPTIONS[status_code]
+        assert http_error_cls  # nosec
+
+        if is_5xx_server_error(status_code):
+            _logger.exception(
+                **create_troubleshotting_log_kwargs(
+                    user_msg,
+                    error=exception,
+                    error_context={
+                        "request": request,
+                        "request.remote": f"{request.remote}",
+                        "request.method": f"{request.method}",
+                        "request.path": f"{request.path}",
+                    },
+                )
+            )
+        return http_error_cls(reason=user_msg)
+
+    return _exception_handler
 
 
 class HttpErrorInfo(NamedTuple):
@@ -89,11 +150,6 @@ class HttpErrorInfo(NamedTuple):
 
 
 ExceptionToHttpErrorMap: TypeAlias = dict[type[BaseException], HttpErrorInfo]
-
-
-class _DefaultDict(dict):
-    def __missing__(self, key):
-        return f"'{key}=?'"
 
 
 def _sort_exceptions_by_specificity(
@@ -162,54 +218,29 @@ def create_exception_handlers_decorator(
 def create__http_error_map_handler(
     to_http_error_map: ExceptionToHttpErrorMap,
 ) -> WebApiExceptionHandler:
-    """
-    Creates a custom `WebApiExceptionHandler` that maps specific exceptions to HTTP errors.
 
-    Given an `ExceptionToHttpErrorMap`, this function returns a handler that checks if an exception
-    matches one in the map, returning an HTTP error with the mapped status code and message.
-    Server errors (5xx) include additional logging with request context. Unmapped exceptions are
-    returned as-is for re-raising.
+    _exception_handlers = {
+        exc_cls: make_handler_exception_to_status_code(
+            exception_cls=exc_cls,
+            status_code=http_error_info.status_code,
+            user_msg_template=http_error_info.msg_template,
+        )
+        for exc_cls, http_error_info in to_http_error_map.items()
+    }
 
-    Arguments:
-        to_http_error_map -- Maps exceptions to HTTP status codes and messages.
-
-    Returns:
-        A web api exception handler
-    """
-    included: list[type[BaseException]] = _sort_exceptions_by_specificity(
-        list(to_http_error_map.keys())
+    _catch_exceptions = _sort_exceptions_by_specificity(
+        list(_exception_handlers.keys())
     )
 
-    def _handler(
+    def _exception_handler(
         exception: BaseException,
         request: web.Request,
     ) -> web.HTTPError | BaseException | None:
-        if exc_cls := next((_ for _ in included if isinstance(exception, _)), None):
-            http_error_info = to_http_error_map[exc_cls]
-
-            # safe formatting, i.e. does not raise
-            user_msg = http_error_info.msg_template.format_map(
-                _DefaultDict(getattr(exception, "__dict__", {}))
-            )
-
-            http_error_cls = get_http_error_class_or_none(http_error_info.status_code)
-            assert http_error_cls  # nosec
-
-            if is_5xx_server_error(http_error_info.status_code):
-                _logger.exception(
-                    **create_troubleshotting_log_kwargs(
-                        user_msg,
-                        error=exception,
-                        error_context={
-                            "request": request,
-                            "request.remote": f"{request.remote}",
-                            "request.method": f"{request.method}",
-                            "request.path": f"{request.path}",
-                        },
-                    )
-                )
-            return http_error_cls(reason=user_msg)
+        if exc_cls := next(
+            (_ for _ in _catch_exceptions if isinstance(exception, _)), None
+        ):
+            return _exception_handlers[exc_cls](request=request, exception=exception)
         # NOTE: not in my list, return so it gets reraised
         return exception
 
-    return _handler
+    return _exception_handler
