65511dc173bd244bac489ce5b6810b4dddb73f51
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-11-04 19:24:08 +0100

    last one today

--- services/director/src/simcore_service_director/system_utils.py
+++ services/director/src/simcore_service_director/system_utils.py
@@ -1,8 +1,7 @@
 from pathlib import Path
-from typing import List
 
 
-def get_system_extra_hosts_raw(extra_host_domain: str) -> List[str]:
+def get_system_extra_hosts_raw(extra_host_domain: str) -> list[str]:
     extra_hosts = []
     hosts_path = Path("/etc/hosts")
     if hosts_path.exists() and extra_host_domain != "undefined":
