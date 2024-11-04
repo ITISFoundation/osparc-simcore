a1afb3381fd570dc651435baf4728e82bb858709
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-11-04 15:16:47 +0100

    refactoring test_handlers

---
+++ services/director/tests/api/test_rest_services.py
@@ -0,0 +1,256 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+# pylint: disable=too-many-arguments
+
+import json
+from urllib.parse import quote
+
+import httpx
+from fastapi import status
+from fixtures.fake_services import ServiceInRegistryInfoDict
+from helpers import json_schema_validator
+from simcore_service_director import resources
+
+
+def _assert_response_and_unwrap_envelope(got: httpx.Response):
+    assert got.encoding == "application/json"
+
+    body = got.json()
+    assert isinstance(body, dict)
+    assert "data" in body or "error" in body
+    return body.get("data"), body.get("error")
+
+
+async def test_get_root_path(client: httpx.AsyncClient, api_version_prefix: str):
+    resp = await client.get(f"/{api_version_prefix}/")
+
+    assert resp.is_success
+    assert resp.status_code == status.HTTP_200_OK
+
+    data, error = _assert_response_and_unwrap_envelope(resp)
+    assert data
+    assert not error
+
+    assert data["name"] == "simcore-service-director"
+    assert data["status"] == "SERVICE_RUNNING"
+    assert data["version"] == "0.1.0"
+    assert data["api_version"] == "0.1.0"
+
+
+def _assert_services(
+    *,
+    expected: list[ServiceInRegistryInfoDict],
+    got: list[dict],
+    schema_version="v1",
+):
+    assert len(expected) == len(got)
+
+    expected_key_version_tuples = [
+        (s["service_description"]["key"], s["service_description"]["version"])
+        for s in expected
+    ]
+
+    json_schema_path = resources.get_path(resources.RESOURCE_NODE_SCHEMA)
+    assert json_schema_path.exists() is True
+    with json_schema_path.open() as file_pt:
+        service_schema = json.load(file_pt)
+
+    for service in got:
+        service.pop("image_digest", None)
+        if schema_version == "v1":
+            assert (
+                expected_key_version_tuples.count((service["key"], service["version"]))
+                == 1
+            )
+        json_schema_validator.validate_instance_object(service, service_schema)
+
+
+async def test_list_services_with_empty_registry(
+    docker_registry: str,
+    client: httpx.AsyncClient,
+    api_version_prefix: str,
+):
+    assert docker_registry, "docker-registry is not ready?"
+
+    # empty case
+    resp = await client.get(f"/{api_version_prefix}/services")
+    assert resp.status_code == status.HTTP_200_OK
+
+    services, error = _assert_response_and_unwrap_envelope(resp.json())
+    assert not error
+    assert isinstance(services, list)
+
+    _assert_services(expected=[], got=services)
+
+
+async def test_list_services(
+    docker_registry: str,
+    client: httpx.AsyncClient,
+    created_services: list[ServiceInRegistryInfoDict],
+    api_version_prefix: str,
+):
+    assert docker_registry, "docker-registry is not ready?"
+
+    resp = await client.get(f"/{api_version_prefix}/services")
+    assert resp.status_code == status.HTTP_200_OK
+
+    services, error = _assert_response_and_unwrap_envelope(resp.json())
+    assert not error
+    assert isinstance(services, list)
+
+    _assert_services(expected=created_services, got=services)
+
+
+async def test_get_service_bad_request(
+    docker_registry: str,
+    client: httpx.AsyncClient,
+    created_services: list[ServiceInRegistryInfoDict],
+    api_version_prefix: str,
+):
+    assert docker_registry, "docker-registry is not ready?"
+    assert len(created_services) > 0
+
+    resp = await client.get(f"/{api_version_prefix}/services?service_type=blahblah")
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+    services, error = _assert_response_and_unwrap_envelope(resp.json())
+    assert not services
+    assert error
+
+
+async def test_list_services_by_service_type(
+    docker_registry: str,
+    client: httpx.AsyncClient,
+    created_services: list[ServiceInRegistryInfoDict],
+    api_version_prefix: str,
+):
+    assert docker_registry, "docker-registry is not ready?"
+    assert len(created_services) == 5
+
+    resp = await client.get(
+        f"/{api_version_prefix}/services?service_type=computational"
+    )
+    assert resp.status_code == status.HTTP_200_OK
+
+    services, error = _assert_response_and_unwrap_envelope(resp.json())
+    assert not error
+    assert services
+    assert len(services) == 3
+
+    resp = await client.get(f"/{api_version_prefix}/services?service_type=interactive")
+    assert resp.status_code == status.HTTP_200_OK
+
+    services, error = _assert_response_and_unwrap_envelope(resp.json())
+    assert not error
+    assert services
+    assert len(services) == 2
+
+
+async def test_get_services_by_key_and_version_with_empty_registry(
+    client: httpx.AsyncClient, api_version_prefix: str
+):
+    resp = await client.get(f"/{api_version_prefix}/services/whatever/someversion")
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+    resp = await client.get(
+        f"/{api_version_prefix}/services/simcore/services/dynamic/something/someversion"
+    )
+    assert resp.status_code == status.HTTP_404_NOT_FOUND
+
+    resp = await client.get(
+        f"/{api_version_prefix}/services/simcore/services/dynamic/something/1.5.2"
+    )
+    assert resp.status_code == status.HTTP_404_NOT_FOUND
+
+
+async def test_get_services_by_key_and_version(
+    client: httpx.AsyncClient,
+    created_services: list[ServiceInRegistryInfoDict],
+    api_version_prefix: str,
+):
+    assert len(created_services) == 5
+
+    retrieved_services: list[dict] = []
+    for created_service in created_services:
+        service_description = created_service["service_description"]
+        # note that it is very important to remove the safe="/" from quote!!!!
+        key, version = (
+            quote(service_description[key], safe="") for key in ("key", "version")
+        )
+        url = f"/{api_version_prefix}/services/{key}/{version}"
+        resp = await client.get(url)
+
+        assert resp.status_code == status.HTTP_200_OK, f"Got f{resp.text}"
+
+        services, error = _assert_response_and_unwrap_envelope(resp.json())
+        assert not error
+        assert isinstance(services, list)
+        assert len(services) == 1
+
+        retrieved_services.append(services[0])
+
+    _assert_services(expected=created_services, got=retrieved_services)
+
+
+async def test_get_service_labels(
+    client: httpx.AsyncClient,
+    created_services: list[ServiceInRegistryInfoDict],
+    api_version_prefix: str,
+):
+    assert len(created_services) == 5
+
+    for service in created_services:
+        service_description = service["service_description"]
+        # note that it is very important to remove the safe="/" from quote!!!!
+        key, version = (
+            quote(service_description[key], safe="") for key in ("key", "version")
+        )
+        url = f"/{api_version_prefix}/services/{key}/{version}/labels"
+        resp = await client.get(url)
+        assert resp.status_code == status.HTTP_200_OK, f"Got f{resp.text}"
+
+        labels, error = _assert_response_and_unwrap_envelope(resp.json())
+        assert not error
+
+        assert service["docker_labels"] == labels
+
+
+async def test_get_services_extras_by_key_and_version_with_empty_registry(
+    client: httpx.AsyncClient, api_version_prefix: str
+):
+    resp = await client.get(
+        f"/{api_version_prefix}/service_extras/whatever/someversion"
+    )
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+    resp = await client.get(
+        f"/{api_version_prefix}/service_extras/simcore/services/dynamic/something/someversion"
+    )
+    assert resp.status_code == status.HTTP_404_NOT_FOUND
+    resp = await client.get(
+        f"/{api_version_prefix}/service_extras/simcore/services/dynamic/something/1.5.2"
+    )
+    assert resp.status_code == status.HTTP_404_NOT_FOUND
+
+
+async def test_get_services_extras_by_key_and_version(
+    client: httpx.AsyncClient,
+    created_services: list[ServiceInRegistryInfoDict],
+    api_version_prefix: str,
+):
+    assert len(created_services) == 5
+
+    for created_service in created_services:
+        service_description = created_service["service_description"]
+        # note that it is very important to remove the safe="/" from quote!!!!
+        key, version = (
+            quote(service_description[key], safe="") for key in ("key", "version")
+        )
+        url = f"/{api_version_prefix}/service_extras/{key}/{version}"
+        resp = await client.get(url)
+
+        assert resp.status_code == status.HTTP_200_OK, f"Got {resp.text=}"
+
+        service_extras, error = _assert_response_and_unwrap_envelope(resp.json())
+        assert not error
+        assert created_service["service_extras"] == service_extras
--- services/director/tests/fixtures/fake_services.py
+++ services/director/tests/fixtures/fake_services.py
@@ -1,13 +1,18 @@
-# pylint:disable=unused-argument
-# pylint:disable=redefined-outer-name
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+# pylint: disable=too-many-arguments
 
 
 import asyncio
 import json
 import logging
 import random
+import sys
+from collections.abc import Awaitable, Iterator
 from io import BytesIO
 from pathlib import Path
+from typing import Any, Literal, Protocol, TypedDict
 
 import pytest
 import requests
@@ -19,105 +24,132 @@ from simcore_service_director.config import DEFAULT_MAX_MEMORY, DEFAULT_MAX_NANO
 _logger = logging.getLogger(__name__)
 
 
-@pytest.fixture()
-def push_services(docker_registry, tmpdir):
-    registry_url = docker_registry
-    tmp_dir = Path(tmpdir)
-
-    list_of_pushed_images_tags = []
-    dependent_images = []
-
-    async def build_push_images(
-        number_of_computational_services,
-        number_of_interactive_services,
-        inter_dependent_services=False,
-        bad_json_format=False,
-        version="1.0.",
-    ):
-        try:
-            dependent_image = None
-            if inter_dependent_services:
-                dependent_image = await _build_push_image(
-                    tmp_dir,
-                    registry_url,
-                    "computational",
-                    "dependency",
-                    "10.52.999999",
-                    None,
-                    bad_json_format=bad_json_format,
-                )
-                dependent_images.append(dependent_image)
-
-            images_to_build = []
-
-            for image_index in range(number_of_computational_services):
-                images_to_build.append(
-                    _build_push_image(
-                        tmp_dir,
-                        registry_url,
-                        "computational",
-                        "test",
-                        version + str(image_index),
-                        dependent_image,
-                        bad_json_format=bad_json_format,
-                    )
-                )
-
-            for image_index in range(number_of_interactive_services):
-                images_to_build.append(
-                    _build_push_image(
-                        tmp_dir,
-                        registry_url,
-                        "dynamic",
-                        "test",
-                        version + str(image_index),
-                        dependent_image,
-                        bad_json_format=bad_json_format,
-                    )
-                )
-            results = await asyncio.gather(*images_to_build)
-            list_of_pushed_images_tags.extend(results)
-        except DockerError:
-            _logger.exception("Unexpected docker API error")
-            raise
-
-        return list_of_pushed_images_tags
-
-    yield build_push_images
-
-    _logger.info("clean registry")
-    _clean_registry(registry_url, list_of_pushed_images_tags)
-    _clean_registry(registry_url, dependent_images)
+CURRENT_DIR = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
 
-async def _build_push_image(
-    docker_dir,
-    registry_url,
-    service_type,
-    name,
-    tag,
+class NodeRequirementsDict(TypedDict):
+    CPU: float
+    RAM: float
+
+
+class ServiceExtrasDict(TypedDict):
+    node_requirements: NodeRequirementsDict
+    build_date: str
+    vcs_ref: str
+    vcs_url: str
+
+
+class ServiceDescriptionDict(TypedDict):
+    key: str
+    version: str
+    type: Literal["computational", "dynamic"]
+
+
+class ServiceInRegistryInfoDict(TypedDict):
+    service_description: ServiceDescriptionDict
+    docker_labels: dict[str, Any]
+    image_path: str
+    internal_port: int | None
+    entry_point: str
+    service_extras: ServiceExtrasDict
+
+
+def _create_service_description(
+    service_type: Literal["computational", "dynamic"], name: str, tag: str
+) -> ServiceDescriptionDict:
+    service_desc = json.loads(
+        (CURRENT_DIR / "dummy_service_description-v1.json").read_text()
+    )
+
+    if service_type == "computational":
+        service_key_type = "comp"
+    elif service_type == "dynamic":
+        service_key_type = "dynamic"
+    else:
+        msg = f"Invalid {service_type=}"
+        raise ValueError(msg)
+
+    service_desc["key"] = f"simcore/services/{service_key_type}/{name}"
+    service_desc["version"] = tag
+    service_desc["type"] = service_type
+
+    return service_desc
+
+
+def _create_docker_labels(
+    service_description: ServiceDescriptionDict, *, bad_json_format: bool
+) -> dict[str, str]:
+    docker_labels = {}
+    for key, value in service_description.items():
+        docker_labels[".".join(["io", "simcore", key])] = json.dumps({key: value})
+        if bad_json_format:
+            docker_labels[".".join(["io", "simcore", key])] = (
+                "d32;'" + docker_labels[".".join(["io", "simcore", key])]
+            )
+
+    return docker_labels
+
+
+async def _create_base_image(labels, tag) -> dict[str, Any]:
+    dockerfile = """
+FROM alpine
+CMD while true; do sleep 10; done
+    """
+    f = BytesIO(dockerfile.encode("utf-8"))
+    tar_obj = utils.mktar_from_dockerfile(f)
+
+    # build docker base image
+    docker = Docker()
+    base_docker_image = await docker.images.build(
+        fileobj=tar_obj, encoding="gzip", rm=True, labels=labels, tag=tag
+    )
+    await docker.close()
+    return base_docker_image
+
+
+async def _build_and_push_image(
+    registry_url: str,
+    service_type: Literal["computational", "dynamic"],
+    name: str,
+    tag: str,
     dependent_image=None,
     *,
-    bad_json_format=False,
-):  # pylint: disable=R0913
+    bad_json_format: bool = False,
+) -> ServiceInRegistryInfoDict:  # pylint: disable=R0913
 
     # crate image
     service_description = _create_service_description(service_type, name, tag)
-    docker_labels = _create_docker_labels(service_description, bad_json_format)
+    docker_labels = _create_docker_labels(
+        service_description, bad_json_format=bad_json_format
+    )
     additional_docker_labels = [
-        {"name": "constraints", "type": "string", "value": ["node.role==manager"]}
+        {
+            "name": "constraints",
+            "type": "string",
+            "value": ["node.role==manager"],
+        }
     ]
 
     internal_port = None
     entry_point = ""
     if service_type == "dynamic":
-        internal_port = random.randint(1, 65535)
+        internal_port = random.randint(1, 65535)  # noqa: S311
         additional_docker_labels.append(
-            {"name": "ports", "type": "int", "value": internal_port}
+            {
+                "name": "ports",
+                "type": "int",
+                "value": internal_port,
+            }
         )
         entry_point = "/test/entry_point"
         docker_labels["simcore.service.bootsettings"] = json.dumps(
-            [{"name": "entry_point", "type": "string", "value": entry_point}]
+            [
+                {
+                    "name": "entry_point",
+                    "type": "string",
+                    "value": entry_point,
+                }
+            ]
         )
     docker_labels["simcore.service.settings"] = json.dumps(additional_docker_labels)
     if bad_json_format:
@@ -142,15 +174,15 @@ async def _build_push_image(
             )
 
     # create the typical org.label-schema labels
-    service_extras = {
-        "node_requirements": {
-            "CPU": DEFAULT_MAX_NANO_CPUS / 1e9,
-            "RAM": DEFAULT_MAX_MEMORY,
-        },
-        "build_date": "2020-08-19T15:36:27Z",
-        "vcs_ref": "ca180ef1",
-        "vcs_url": "git@github.com:ITISFoundation/osparc-simcore.git",
-    }
+    service_extras = ServiceExtrasDict(
+        node_requirements=NodeRequirementsDict(
+            CPU=DEFAULT_MAX_NANO_CPUS / 1e9,
+            RAM=DEFAULT_MAX_MEMORY,
+        ),
+        build_date="2020-08-19T15:36:27Z",
+        vcs_ref="ca180ef1",
+        vcs_url="git@github.com:ITISFoundation/osparc-simcore.git",
+    )
     docker_labels["org.label-schema.build-date"] = service_extras["build_date"]
     docker_labels["org.label-schema.schema-version"] = "1.0"
     docker_labels["org.label-schema.vcs-ref"] = service_extras["vcs_ref"]
@@ -162,22 +194,26 @@ async def _build_push_image(
     await _create_base_image(docker_labels, image_tag)
 
     # push image to registry
-    docker = Docker()
-    await docker.images.push(image_tag)
-    await docker.close()
+    try:
+        docker = Docker()
+        await docker.images.push(image_tag)
+    finally:
+        await docker.close()
+
     # remove image from host
     # docker.images.remove(image_tag)
-    return {
-        "service_description": service_description,
-        "docker_labels": docker_labels,
-        "image_path": image_tag,
-        "internal_port": internal_port,
-        "entry_point": entry_point,
-        "service_extras": service_extras,
-    }
+
+    return ServiceInRegistryInfoDict(
+        service_description=service_description,
+        docker_labels=docker_labels,
+        image_path=image_tag,
+        internal_port=internal_port,
+        entry_point=entry_point,
+        service_extras=service_extras,
+    )
 
 
-def _clean_registry(registry_url, list_of_images):
+def _clean_registry(registry_url: str, list_of_images: list[ServiceInRegistryInfoDict]):
     request_headers = {"accept": "application/vnd.docker.distribution.manifest.v2+json"}
     for image in list_of_images:
         service_description = image["service_description"]
@@ -197,51 +233,83 @@ def _clean_registry(registry_url, list_of_images):
         response = requests.delete(url, headers=request_headers, timeout=5)
 
 
-async def _create_base_image(labels, tag):
-    dockerfile = """
-FROM alpine
-CMD while true; do sleep 10; done
-    """
-    f = BytesIO(dockerfile.encode("utf-8"))
-    tar_obj = utils.mktar_from_dockerfile(f)
-
-    # build docker base image
-    docker = Docker()
-    base_docker_image = await docker.images.build(
-        fileobj=tar_obj, encoding="gzip", rm=True, labels=labels, tag=tag
-    )
-    await docker.close()
-    return base_docker_image[0]
+class PushServicesCallable(Protocol):
+    async def __call__(
+        self,
+        *,
+        number_of_computational_services: int,
+        number_of_interactive_services: int,
+        inter_dependent_services: bool = False,
+        bad_json_format: bool = False,
+        version="1.0.",
+    ) -> list[ServiceInRegistryInfoDict]:
+        ...
 
 
-def _create_service_description(service_type, name, tag):
-    file_name = "dummy_service_description-v1.json"
-    dummy_description_path = Path(__file__).parent / file_name
-    with dummy_description_path.open() as file_pt:
-        service_desc = json.load(file_pt)
+@pytest.fixture
+def push_services(docker_registry: str) -> Iterator[PushServicesCallable]:
+    registry_url = docker_registry
+    list_of_pushed_images_tags: list[ServiceInRegistryInfoDict] = []
+    dependent_images = []
 
-    if service_type == "computational":
-        service_key_type = "comp"
-    elif service_type == "dynamic":
-        service_key_type = "dynamic"
-    else:
-        msg = f"Invalid {service_type=}"
-        raise ValueError(msg)
+    async def _build_push_images_to_docker_registry(
+        *,
+        number_of_computational_services,
+        number_of_interactive_services,
+        inter_dependent_services=False,
+        bad_json_format=False,
+        version="1.0.",
+    ) -> list[ServiceInRegistryInfoDict]:
+        try:
+            dependent_image = None
+            if inter_dependent_services:
+                dependent_image = await _build_and_push_image(
+                    registry_url=registry_url,
+                    service_type="computational",
+                    name="dependency",
+                    tag="10.52.999999",
+                    dependent_image=None,
+                    bad_json_format=bad_json_format,
+                )
+                dependent_images.append(dependent_image)
 
-    service_desc["key"] = f"simcore/services/{service_key_type}/{name}"
-    service_desc["version"] = tag
-    service_desc["type"] = service_type
+            images_to_build: list[Awaitable] = [
+                _build_and_push_image(
+                    registry_url=registry_url,
+                    service_type="computational",
+                    name="test",
+                    tag=f"{version}{image_index}",
+                    dependent_image=dependent_image,
+                    bad_json_format=bad_json_format,
+                )
+                for image_index in range(number_of_computational_services)
+            ]
 
-    return service_desc
-
-
-def _create_docker_labels(service_description, bad_json_format):
-    docker_labels = {}
-    for key, value in service_description.items():
-        docker_labels[".".join(["io", "simcore", key])] = json.dumps({key: value})
-        if bad_json_format:
-            docker_labels[".".join(["io", "simcore", key])] = (
-                "d32;'" + docker_labels[".".join(["io", "simcore", key])]
+            images_to_build.extend(
+                [
+                    _build_and_push_image(
+                        registry_url=registry_url,
+                        service_type="dynamic",
+                        name="test",
+                        tag=f"{version}{image_index}",
+                        dependent_image=dependent_image,
+                        bad_json_format=bad_json_format,
+                    )
+                    for image_index in range(number_of_interactive_services)
+                ]
             )
 
-    return docker_labels
+            results = await asyncio.gather(*images_to_build)
+            list_of_pushed_images_tags.extend(results)
+
+        except DockerError:
+            _logger.exception("Docker API error while building and pushing images")
+            raise
+
+        return list_of_pushed_images_tags
+
+    yield _build_push_images_to_docker_registry
+
+    _logger.info("clean registry")
+    _clean_registry(registry_url, list_of_pushed_images_tags)
+    _clean_registry(registry_url, dependent_images)
