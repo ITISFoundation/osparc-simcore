3748f936fc0c9570a2541c7d947fb7dbb5a82087
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-11-05 17:32:09 +0100

    ensure HEAD works

--- services/director/src/simcore_service_director/api/rest/_health.py
+++ services/director/src/simcore_service_director/api/rest/_health.py
@@ -5,7 +5,12 @@ from fastapi.responses import PlainTextResponse
 router = APIRouter()
 
 
-@router.get("/", include_in_schema=True, response_class=PlainTextResponse)
+@router.api_route(
+    "/",
+    methods=["GET", "HEAD"],
+    include_in_schema=False,
+    response_class=PlainTextResponse,
+)
 async def health_check() -> str:
     # NOTE: sync url in docker/healthcheck.py with this entrypoint!
     return f"{__name__}.health_check@{arrow.utcnow().isoformat()}"
