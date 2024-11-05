c74b2b234f6f5639c2d6dd4e719e73b4baf99e8c
Giancarlo Romeo <giancarloromeo@gmail.com>
2024-11-05 16:38:04 +0100

    fix nullable

--- packages/models-library/src/models_library/resource_tracker.py
+++ packages/models-library/src/models_library/resource_tracker.py
@@ -259,7 +259,7 @@ class PricingUnitWithCostUpdate(BaseModel):
     unit_extra_info: UnitExtraInfo
     default: bool
     specific_info: SpecificInfo
-    pricing_unit_cost_update: None | PricingUnitCostUpdate = Field(default=None)
+    pricing_unit_cost_update: PricingUnitCostUpdate | None
 
     model_config = ConfigDict(
         json_schema_extra={
