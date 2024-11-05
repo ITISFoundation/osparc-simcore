649982994194be460528d77698dd15a9716f5517
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-11-05 15:56:48 +0100

    LOG_LEVEL instead of log_level

--- services/director/src/simcore_service_director/main.py
+++ services/director/src/simcore_service_director/main.py
@@ -12,8 +12,8 @@ from simcore_service_director.core.settings import ApplicationSettings
 _the_settings = ApplicationSettings.create_from_envs()
 
 # SEE https://github.com/ITISFoundation/osparc-simcore/issues/3148
-logging.basicConfig(level=_the_settings.log_level)  # NOSONAR
-logging.root.setLevel(_the_settings.log_level)
+logging.basicConfig(level=_the_settings.DIRECTOR_LOGLEVEL)
+logging.root.setLevel(_the_settings.DIRECTOR_LOGLEVEL)
 config_all_loggers(
     log_format_local_dev_enabled=_the_settings.DIRECTOR_LOG_FORMAT_LOCAL_DEV_ENABLED,
     logger_filter_mapping=_the_settings.DIRECTOR_LOG_FILTER_MAPPING,
