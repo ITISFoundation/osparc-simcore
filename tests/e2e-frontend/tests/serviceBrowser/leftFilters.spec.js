387698131743241687315ec1149fe84b2211b774
Odei Maiz <maiz@itis.swiss>
2024-11-05 09:51:40 +0100

    service browser

---
+++ tests/e2e-frontend/tests/serviceBrowser/leftFilters.spec.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+
+const { test, expect } = require('@playwright/test');
+
+import { LoginPage } from '../fixtures/loginPage';
+
+import products from '../products.json';
+import users from '../users.json';
+
+const product = "osparc";
+const productUrl = products[product];
+const user = users[product][0];
+
+test.describe.serial(`Left Filters:`, () => {
+  let page = null;
+  let loginPageFixture = null;
+
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    loginPageFixture = new LoginPage(page, productUrl);
+    const role = await loginPageFixture.login(user.email, user.password);
+    expect(role).toBe(user.role);
+  });
+
+  test.afterAll(async ({ browser }) => {
+    await loginPageFixture.logout();
+    await page.close();
+    await browser.close();
+  });
+
+  test(`Filters`, async () => {
+    const contextTree = page.getByTestId("contextTree");
+    await expect(contextTree).toBeVisible({
+      timeout: 30000 // it will take some time to load the Study Browser
+    });
+
+    const workspacesAndFoldersTreeItems = page.getByTestId("workspacesAndFoldersTreeItem");
+    const count = await workspacesAndFoldersTreeItems.count();
+    // at least two: My Workspace and Shared Workspaces
+    expect(count > 1).toBeTruthy();
+  });
+});
