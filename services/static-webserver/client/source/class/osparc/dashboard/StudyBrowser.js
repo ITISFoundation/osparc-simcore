1c25be19b6336e4ccdb2f2317fd02104d0ea014d
Odei Maiz <maiz@itis.swiss>
2024-11-06 16:05:50 +0100

    one more

--- services/static-webserver/client/source/class/osparc/dashboard/StudyBrowser.js
+++ services/static-webserver/client/source/class/osparc/dashboard/StudyBrowser.js
@@ -1725,9 +1725,10 @@ qx.Class.define("osparc.dashboard.StudyBrowser", {
 
     __createConfirmWindow: function(studyNames) {
       const rUSure = this.tr("Are you sure you want to delete");
-      const studiesText = osparc.product.Utils.getStudyAlias({plural: true});
-      const msg = rUSure + (studyNames.length > 1 ? ` ${studyNames.length} ${studiesText} ?` : ` <b>${studyNames[0]}</b>?`)
+      const studyAlias = osparc.product.Utils.getStudyAlias({plural: studyNames.length});
+      const msg = rUSure + (studyNames.length > 1 ? ` ${studyNames.length} ${studyAlias} ?` : ` <b>${studyNames[0]}</b>?`)
       const confirmationWin = new osparc.ui.window.Confirmation(msg).set({
+        caption: this.tr("Delete") + " " + studyAlias,
         confirmText: this.tr("Delete"),
         confirmAction: "delete"
       });
