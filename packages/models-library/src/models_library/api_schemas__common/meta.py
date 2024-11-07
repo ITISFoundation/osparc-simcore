3377d0201f5aed108c971176fa350d8368a86fda
Mads Bisgaard <bisgaard@itis.swiss>
2024-11-07 09:32:26 +0100

    reset meta.py

--- packages/models-library/src/models_library/api_schemas__common/meta.py
+++ packages/models-library/src/models_library/api_schemas__common/meta.py
@@ -6,9 +6,9 @@ from ..basic_types import VersionStr
 class BaseMeta(BaseModel):
     name: str
     version: VersionStr
-    released: dict[str, VersionStr] = Field(
+    released: dict[str, VersionStr] | None = Field(
         default=None, description="Maps every route's path tag with a released version"
-    )  # TODO should be nullable
+    )
 
     model_config = ConfigDict(
         json_schema_extra={
