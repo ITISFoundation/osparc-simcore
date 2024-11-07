1706b7ffc45865faee70c2834c25e689c8c8d9de
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-11-07 12:02:47 +0100

    @giancarloromeo review: rm constructor

--- services/web/server/src/simcore_service_webserver/errors.py
+++ services/web/server/src/simcore_service_webserver/errors.py
@@ -1,10 +1,5 @@
-from typing import Any
-
 from models_library.errors_classes import OsparcErrorMixin
 
 
 class WebServerBaseError(OsparcErrorMixin, Exception):
-    msg_template = "Unexpected error in web-server"
-
-    def __init__(self, **ctx: Any) -> None:
-        super().__init__(**ctx)
+    msg_template = "Error in web-server service"
