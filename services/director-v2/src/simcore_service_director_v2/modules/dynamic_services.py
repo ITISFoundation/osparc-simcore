22dba141b72c83620b9afb759d0683dea8361a6d
Mads Bisgaard <bisgaard@itis.swiss>
2024-11-13 16:10:50 +0100

    test fixes

--- services/director-v2/src/simcore_service_director_v2/modules/dynamic_services.py
+++ services/director-v2/src/simcore_service_director_v2/modules/dynamic_services.py
@@ -16,7 +16,7 @@ from ..utils.client_decorators import handle_errors, handle_retry
 logger = logging.getLogger(__name__)
 
 
-def setup(app: FastAPI, tracing_settings: TracingSettings) -> None:
+def setup(app: FastAPI, tracing_settings: TracingSettings | None) -> None:
     def on_startup() -> None:
         client = httpx.AsyncClient(
             timeout=app.state.settings.CLIENT_REQUEST.HTTP_CLIENT_REQUEST_TOTAL_TIMEOUT
