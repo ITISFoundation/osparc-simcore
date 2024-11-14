9596adc529cd10da6eeeaf1a9469c75bd45a5540
Odei Maiz <maiz@itis.swiss>
2024-11-14 14:41:00 +0100

    minor

--- services/static-webserver/client/source/class/osparc/dashboard/CardBase.js
+++ services/static-webserver/client/source/class/osparc/dashboard/CardBase.js
@@ -926,7 +926,7 @@ qx.Class.define("osparc.dashboard.CardBase", {
     },
 
     _filterTags: function(tags) {
-      const checks = this.getTags().map(tag => tag.id);
+      const checks = this.getTags().map(tag => tag.getId());
       return this.self().filterTags(checks, tags);
     },
 
