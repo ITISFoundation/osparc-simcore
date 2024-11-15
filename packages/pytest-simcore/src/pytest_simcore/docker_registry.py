a66bc5e957fcb68afa80d1d59b35fdcb6c7c2aa9
Andrei Neagu <neagu@itis.swiss>
2024-11-15 08:55:28 +0100

    using newer version

--- packages/pytest-simcore/src/pytest_simcore/docker_registry.py
+++ packages/pytest-simcore/src/pytest_simcore/docker_registry.py
@@ -261,7 +261,7 @@ def jupyter_service(docker_registry: str, node_meta_schema: dict) -> dict[str, A
     )
 
 
-@pytest.fixture(scope="session", params=["2.0.4"])
+@pytest.fixture(scope="session", params=["2.0.5"])
 def dy_static_file_server_version(request: pytest.FixtureRequest):
     return request.param
 
