5fe0cc949f326b9c9de60ea0c8c6d8854c64e570
Giancarlo Romeo <giancarloromeo@gmail.com>
2024-11-15 08:57:51 +0100

    migrate to pydantic v2

--- services/web/server/src/simcore_service_webserver/folders/_models.py
+++ services/web/server/src/simcore_service_webserver/folders/_models.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Annotated
 
 from models_library.basic_types import IDStr
 from models_library.folders import FolderID
@@ -11,7 +12,14 @@ from models_library.utils.common_validators import (
     null_or_none_str_to_none_validator,
 )
 from models_library.workspaces import WorkspaceID
-from pydantic import BaseModel, Extra, Field, Json, validator
+from pydantic import (
+    BaseModel,
+    BeforeValidator,
+    ConfigDict,
+    Field,
+    Json,
+    field_validator,
+)
 from servicelib.aiohttp.requests_validation import RequestParams, StrictRequestParams
 from servicelib.request_keys import RQT_USERID_KEY
 
@@ -41,11 +49,11 @@ class FolderListSortParams(BaseModel):
     order_by: Json[OrderBy] = Field(
         default=OrderBy(field=IDStr("modified"), direction=OrderDirection.DESC),
         description="Order by field (modified_at|name|description) and direction (asc|desc). The default sorting order is ascending.",
-        example='{"field": "name", "direction": "desc"}',
+        examples=['{"field": "name", "direction": "desc"}'],
         alias="order_by",
     )
 
-    @validator("order_by", check_fields=False)
+    @field_validator("order_by", check_fields=False)
     @classmethod
     def _validate_order_by_field(cls, v):
         if v.field not in {
@@ -59,51 +67,41 @@ class FolderListSortParams(BaseModel):
             v.field = "modified"
         return v
 
-    class Config:
-        extra = Extra.forbid
+    model_config = ConfigDict(extra="forbid")
 
 
 class FolderListWithJsonStrQueryParams(
     PageQueryParameters, FolderListSortParams, FiltersQueryParameters[FolderFilters]
 ):
-    folder_id: FolderID | None = Field(
+    folder_id: Annotated[
+        FolderID | None, BeforeValidator(null_or_none_str_to_none_validator)
+    ] = Field(
         default=None,
         description="List the subfolders of this folder. By default, list the subfolders of the root directory (Folder ID is None).",
     )
-    workspace_id: WorkspaceID | None = Field(
+    workspace_id: Annotated[
+        WorkspaceID | None, BeforeValidator(null_or_none_str_to_none_validator)
+    ] = Field(
         default=None,
         description="List folders in specific workspace. By default, list in the user private workspace",
     )
 
-    class Config:
-        extra = Extra.forbid
-
-    # validators
-    _null_or_none_str_to_none_validator = validator(
-        "folder_id", allow_reuse=True, pre=True
-    )(null_or_none_str_to_none_validator)
-
-    _null_or_none_str_to_none_validator2 = validator(
-        "workspace_id", allow_reuse=True, pre=True
-    )(null_or_none_str_to_none_validator)
+    model_config = ConfigDict(extra="forbid")
 
 
 class FolderListFullSearchWithJsonStrQueryParams(
     PageQueryParameters, FolderListSortParams, FiltersQueryParameters[FolderFilters]
 ):
-    text: str | None = Field(
+    text: Annotated[
+        str | None, BeforeValidator(empty_str_to_none_pre_validator)
+    ] = Field(
         default=None,
         description="Multi column full text search, across all folders and workspaces",
         max_length=100,
-        example="My Project",
+        examples=["My Project"],
     )
 
-    _empty_is_none = validator("text", allow_reuse=True, pre=True)(
-        empty_str_to_none_pre_validator
-    )
-
-    class Config:
-        extra = Extra.forbid
+    model_config = ConfigDict(extra="forbid")
 
 
 class RemoveQueryParams(BaseModel):
