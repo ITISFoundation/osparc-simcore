f4f7b0eef4841ff042b97c1c99fd69c3e9efca0b
Giancarlo Romeo <giancarloromeo@gmail.com>
2024-11-18 11:28:22 +0100

    fix copy

--- services/director-v2/tests/unit/test_utils_dags.py
+++ services/director-v2/tests/unit/test_utils_dags.py
@@ -422,7 +422,7 @@ def pipeline_test_params(
 
     # resolve the comp_tasks
     resolved_list_comp_tasks = [
-        c.copy(update={"node_id": node_name_to_uuid_map[c.node_id]})
+        c.model_copy(update={"node_id": node_name_to_uuid_map[c.node_id]})
         for c in list_comp_tasks
     ]
 
