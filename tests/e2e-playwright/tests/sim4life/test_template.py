5cf06356c76356dfd3fc56211eda6407ba634e66
Matus Drobuliak <60785969+matusdrobuliak66@users.noreply.github.com>
2024-11-19 14:10:17 +0100

    ♻️ improving e2e tests (socketio reconnect) (#6759)

--- tests/e2e-playwright/tests/sim4life/test_template.py
+++ tests/e2e-playwright/tests/sim4life/test_template.py
@@ -10,8 +10,9 @@
 from collections.abc import Callable
 from typing import Any
 
-from playwright.sync_api import Page, WebSocket
-from pytest_simcore.helpers.playwright import web_socket_default_log_handler
+from playwright.sync_api import Page
+from pydantic import AnyUrl
+from pytest_simcore.helpers.playwright import RestartableWebSocket
 from pytest_simcore.helpers.playwright_sim4life import (
     check_video_streaming,
     interact_with_s4l,
@@ -22,10 +23,11 @@ from pytest_simcore.helpers.playwright_sim4life import (
 def test_template(
     page: Page,
     create_project_from_template_dashboard: Callable[[str], dict[str, Any]],
-    log_in_and_out: WebSocket,
+    log_in_and_out: RestartableWebSocket,
     template_id: str,
     is_autoscaled: bool,
     check_videostreaming: bool,
+    product_url: AnyUrl,
 ):
     project_data = create_project_from_template_dashboard(template_id)
 
@@ -37,12 +39,16 @@ def test_template(
     assert len(node_ids) == 1, "Expected 1 node in the workbench!"
 
     resp = wait_for_launched_s4l(
-        page, node_ids[0], log_in_and_out, autoscaled=is_autoscaled, copy_workspace=True
+        page,
+        node_ids[0],
+        log_in_and_out,
+        autoscaled=is_autoscaled,
+        copy_workspace=True,
+        product_url=product_url,
     )
     s4l_websocket = resp["websocket"]
-    with web_socket_default_log_handler(s4l_websocket):
-        s4l_iframe = resp["iframe"]
-        interact_with_s4l(page, s4l_iframe)
+    s4l_iframe = resp["iframe"]
+    interact_with_s4l(page, s4l_iframe)
 
-        if check_videostreaming:
-            check_video_streaming(page, s4l_iframe, s4l_websocket)
+    if check_videostreaming:
+        check_video_streaming(page, s4l_iframe, s4l_websocket)
