68f9f56694f3728f5f0135092c92053f9a13391d
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-11-20 18:31:47 +0100

    missing service dependencies

--- services/director-v2/tests/unit/with_dbs/comp_scheduler/test_worker.py
+++ services/director-v2/tests/unit/with_dbs/comp_scheduler/test_worker.py
@@ -3,6 +3,9 @@ from simcore_service_director_v2.modules.comp_scheduler._worker import (
     _get_scheduler_worker,
 )
 
+pytest_simcore_core_services_selection = ["postgres", "rabbit", "redis"]
+pytest_simcore_ops_services_selection = ["adminer"]
+
 
 async def test_worker_starts_and_stops(initialized_app: FastAPI):
     assert _get_scheduler_worker(initialized_app) is not None
