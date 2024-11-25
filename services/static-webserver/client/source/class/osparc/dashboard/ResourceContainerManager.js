1e2313f88042867f6e30c8ba91c0247d3d6878a4
Odei Maiz <maiz@itis.swiss>
2024-11-25 16:50:12 +0100

    [skip ci] undo

--- services/static-webserver/client/source/class/osparc/dashboard/ResourceContainerManager.js
+++ services/static-webserver/client/source/class/osparc/dashboard/ResourceContainerManager.js
@@ -263,7 +263,7 @@ qx.Class.define("osparc.dashboard.ResourceContainerManager", {
       this.__resourcesList = resourcesList;
     },
 
-    cleanAll: function() {
+    __cleanAll: function() {
       if (this.__workspacesContainer) {
         this.__workspacesContainer.removeAll();
       }
@@ -302,7 +302,7 @@ qx.Class.define("osparc.dashboard.ResourceContainerManager", {
     },
 
     __rebuildLayout: function(resourceType) {
-      this.cleanAll();
+      this.__cleanAll();
       if (osparc.utils.DisabledPlugins.isFoldersEnabled()) {
         this.__addFoldersContainer();
       }
@@ -365,7 +365,7 @@ qx.Class.define("osparc.dashboard.ResourceContainerManager", {
     },
 
     reloadWorkspaces: function() {
-      this.cleanAll();
+      this.__cleanAll();
       this._add(this.__workspacesContainer);
       let workspacesCards = [];
       this.__workspacesList.forEach(workspaceData => workspacesCards.push(this.__workspaceToCard(workspaceData)));
