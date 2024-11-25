3a801604085c38b485c1ba4470b4c12588063d6f
Odei Maiz <maiz@itis.swiss>
2024-11-25 09:46:23 +0100

    workspace trashedAt

--- services/static-webserver/client/source/class/osparc/data/model/Workspace.js
+++ services/static-webserver/client/source/class/osparc/data/model/Workspace.js
@@ -37,6 +37,7 @@ qx.Class.define("osparc.data.model.Workspace", {
       accessRights: workspaceData.accessRights,
       createdAt: new Date(workspaceData.createdAt),
       modifiedAt: new Date(workspaceData.modifiedAt),
+      trashedAt: workspaceData.trashedAt ? new Date(workspaceData.trashedAt) : null,
     });
   },
 
@@ -95,7 +96,13 @@ qx.Class.define("osparc.data.model.Workspace", {
       nullable: true,
       init: null,
       event: "changeModifiedAt"
-    }
+    },
+
+    trashedAt: {
+      check: "Date",
+      nullable: true,
+      init: null,
+    },
   },
 
   statics: {
