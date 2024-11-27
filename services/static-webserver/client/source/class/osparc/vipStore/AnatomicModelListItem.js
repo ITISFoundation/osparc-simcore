52fc036ab7f9c52f1c682005e00814501246151c
odeimaiz <maiz@itis.swiss>
2024-11-27 19:05:16 +0100

    list models

---
+++ services/static-webserver/client/source/class/osparc/vipStore/AnatomicModelListItem.js
@@ -0,0 +1,174 @@
+/* ************************************************************************
+
+   osparc - the simcore frontend
+
+   https://osparc.io
+
+   Copyright:
+     2024 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+qx.Class.define("osparc.vipStore.AnatomicModelListItem", {
+  extend: qx.ui.core.Widget,
+  implement : [qx.ui.form.IModel, osparc.filter.IFilterable],
+  include : [qx.ui.form.MModelProperty, osparc.filter.MFilterable],
+
+  construct: function() {
+    this.base(arguments);
+
+    const layout = new qx.ui.layout.Grid(8, 1);
+    layout.setColumnWidth(0, 32);
+    layout.setRowFlex(0, 1);
+    layout.setColumnFlex(1, 1);
+    layout.setColumnAlign(0, "center", "middle");
+    layout.setColumnAlign(2, "center", "middle");
+    this._setLayout(layout);
+
+    this.set({
+      padding: 5,
+      minHeight: 48,
+      alignY: "middle",
+    });
+
+    this.addListener("pointerover", this._onPointerOver, this);
+    this.addListener("pointerout", this._onPointerOut, this);
+  },
+
+  events: {
+    /** (Fired by {@link qx.ui.form.List}) */
+    "action" : "qx.event.type.Event"
+  },
+
+  properties: {
+    appearance: {
+      refine: true,
+      init: "selectable"
+    },
+
+    modelId: {
+      check: "String",
+      init: null,
+      nullable: false,
+      event: "changemodelId",
+    },
+
+    thumbnail: {
+      check: "String",
+      init: null,
+      nullable: true,
+      event: "changeThumbnail",
+      apply: "__applyThumbnail",
+    },
+
+    name: {
+      check: "String",
+      init: null,
+      nullable: false,
+      event: "changeName",
+      apply: "__applyName",
+    },
+
+    date: {
+      check: "Date",
+      init: null,
+      nullable: true,
+      event: "changeDate",
+    },
+  },
+
+  members: { // eslint-disable-line qx-rules/no-refs-in-members
+    // overridden
+    _forwardStates: {
+      focused : true,
+      hovered : true,
+      selected : true,
+      dragover : true
+    },
+
+    _createChildControlImpl: function(id) {
+      let control;
+      switch (id) {
+        case "thumbnail":
+          control = new qx.ui.basic.Image().set({
+            alignY: "middle",
+            scale: true,
+            allowGrowX: true,
+            allowGrowY: true,
+            allowShrinkX: true,
+            allowShrinkY: true,
+            maxWidth: 32,
+            maxHeight: 32
+          });
+          this._add(control, {
+            row: 0,
+            column: 0,
+            rowSpan: 2
+          });
+          break;
+        case "name":
+          control = new qx.ui.basic.Label().set({
+            font: "text-14"
+          });
+          this._add(control, {
+            row: 0,
+            column: 1
+          });
+          break;
+      }
+
+      return control || this.base(arguments, id);
+    },
+
+    __applyThumbnail: function(value) {
+      this.getChildControl("thumbnail").setSource(value);
+    },
+
+    __applyName: function(value) {
+      this.getChildControl("name").setValue(value);
+    },
+
+    
+
+    _onPointerOver: function() {
+      this.addState("hovered");
+    },
+
+    _onPointerOut : function() {
+      this.removeState("hovered");
+    },
+
+    _filter: function() {
+      this.exclude();
+    },
+
+    _unfilter: function() {
+      this.show();
+    },
+
+    _shouldApplyFilter: function(data) {
+      if (data.text) {
+        const checks = [
+          this.getName(),
+        ];
+        if (checks.filter(check => check && check.toLowerCase().trim().includes(data.text)).length == 0) {
+          return true;
+        }
+      }
+      return false;
+    },
+
+    _shouldReactToFilter: function(data) {
+      if (data.text && data.text.length > 1) {
+        return true;
+      }
+      return false;
+    }
+  }
+});
--- services/static-webserver/client/source/class/osparc/vipStore/VIPStore.js
+++ services/static-webserver/client/source/class/osparc/vipStore/VIPStore.js
@@ -47,21 +47,56 @@ qx.Class.define("osparc.vipStore.VIPStore", {
   },
 
   members: {
-    __dummyViewer: null,
     __anatomicalModelsRaw: null,
+    __anatomicalModelsModel: null,
 
     __buildLayout: async function() {
-      this.__dummyViewer = new osparc.ui.basic.JsonTreeWidget();
-      this._add(this.__dummyViewer);
+      const modelsUIList = new qx.ui.form.List().set({
+        decorator: "no-border",
+        spacing: 3,
+        width: 250
+      });
+      this._add(modelsUIList)
+
+      const anatomicalModelsModel = this.__anatomicalModelsModel = new qx.data.Array();
+      const membersCtrl = new qx.data.controller.List(anatomicalModelsModel, modelsUIList, "name");
+      membersCtrl.setDelegate({
+        createItem: () => new osparc.vipStore.AnatomicModelListItem(),
+        bindItem: (ctrl, item, id) => {
+          ctrl.bindProperty("id", "modelId", null, item, id);
+          ctrl.bindProperty("thumbnail", "thumbnail", null, item, id);
+          ctrl.bindProperty("name", "name", null, item, id);
+          ctrl.bindProperty("date", "date", null, item, id);
+        },
+      });
 
       // fetch data
       const resp = await fetch("https://itis.swiss/PD_DirectDownload/getDownloadableItems/AnatomicalModels", {method:"POST"});
-      const anatomicalModelsRaw = this.__anatomicalModelsRaw = await resp.json();
+      const anatomicalModelsRaw = await resp.json();
       this.__populateModels(anatomicalModelsRaw);
     },
 
-    __populateModels: function(anatomicalModels) {
-      this.__dummyViewer.setJson(anatomicalModels);
+    __populateModels: function(anatomicalModelsRaw) {
+      const anatomicalModels = this.__anatomicalModels = anatomicalModelsRaw["availableDownloads"];
+
+      const anatomicalModelsModel = this.__anatomicalModelsModel;
+      anatomicalModelsModel.removeAll();
+
+      anatomicalModels.forEach(anatomicalModelData => {
+        // this is a JSON but it's missing the quotes
+        const rawData = anatomicalModelData["Features"];
+        let formattedData = rawData
+          .replace(/([a-zA-Z0-9_]+):/g, '"$1":') // Add quotes around keys
+          .replace(/: ([^,{}]+)/g, ': "$1"'); // Add quotes around all values
+
+        features = JSON.parse(formattedData);
+        const anatomicalModel = {};
+        anatomicalModel["id"] = anatomicalModelData["ID"];
+        anatomicalModel["thumbnail"] = anatomicalModelData["Thumbnail"];
+        anatomicalModel["name"] = features["name"];
+        anatomicalModel["date"] = new Date(features["date"]);
+        anatomicalModelsModel.append(qx.data.marshal.Json.createModel(anatomicalModel));
+      });
     },
   }
 });
