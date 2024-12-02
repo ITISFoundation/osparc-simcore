7f05645a74e693a4b752dbd7e337f09f316d2b4b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-12-02 17:00:12 +0100

    typo

--- services/director-v2/src/simcore_service_director_v2/core/settings.py
+++ services/director-v2/src/simcore_service_director_v2/core/settings.py
@@ -88,7 +88,7 @@ class ComputationalBackendSettings(BaseCustomSettings):
         "tcp://dask-scheduler:8786, tls://dask-scheduler:8786 for the internal cluster",
     )
     COMPUTATIONAL_BACKEND_DEFAULT_CLUSTER_AUTH: ClusterAuthentication = Field(
-        default_factory=...,
+        default=...,
         description="this is the cluster authentication that will be used by default",
     )
     COMPUTATIONAL_BACKEND_DEFAULT_CLUSTER_FILE_LINK_TYPE: FileLinkType = Field(
