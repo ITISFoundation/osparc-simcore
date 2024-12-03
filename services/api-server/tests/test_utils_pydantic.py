e6a8f182f480cc9e0aeaf27acf9376992548aa0c
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-12-03 17:00:15 +0100

    @GitHK review: split tests and adds xfail

--- services/api-server/tests/test_utils_pydantic.py
+++ services/api-server/tests/test_utils_pydantic.py
@@ -2,31 +2,46 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 # pylint: disable=too-many-arguments
-from typing import Annotated
 
+from typing import Annotated, Any
+
+import fastapi
+import pydantic
+import pytest
 from fastapi import FastAPI
-from pydantic import BaseModel, HttpUrl
+from pydantic import (
+    AnyHttpUrl,
+    AnyUrl,
+    BaseModel,
+    HttpUrl,
+    TypeAdapter,
+    ValidationError,
+)
 from simcore_service_api_server.models._utils_pydantic import UriSchema
 
 
-def test_annotated_url_in_pydantic_changes_in_fastapi():
-    class TestModel(BaseModel):
-        urls0: list[HttpUrl]
-        urls1: list[Annotated[HttpUrl, UriSchema()]]
+class _FakeModel(BaseModel):
+    urls0: list[HttpUrl]
+    urls1: list[Annotated[HttpUrl, UriSchema()]]
 
-        # with and w/o
-        url0: HttpUrl
-        url1: Annotated[HttpUrl, UriSchema()]
+    # with and w/o
+    url0: HttpUrl
+    url1: Annotated[HttpUrl, UriSchema()]
 
-        # # including None inside/outside annotated
-        url2: Annotated[HttpUrl, UriSchema()] | None
-        url3: Annotated[HttpUrl | None, UriSchema()]
+    # # including None inside/outside annotated
+    url2: Annotated[HttpUrl, UriSchema()] | None
+    url3: Annotated[HttpUrl | None, UriSchema()]
 
-        # # mistake
-        int0: Annotated[int, UriSchema()]
+    # # mistake
+    int0: Annotated[int, UriSchema()]
 
-    pydantic_schema = TestModel.model_json_schema()
 
+@pytest.fixture
+def pydantic_schema() -> dict[str, Any]:
+    return _FakeModel.model_json_schema()
+
+
+def test_pydantic_json_schema(pydantic_schema: dict[str, Any]):
     assert pydantic_schema["properties"] == {
         "int0": {"title": "Int0", "type": "integer"},
         "url0": {
@@ -79,14 +94,20 @@ def test_annotated_url_in_pydantic_changes_in_fastapi():
         },
     }
 
+
+@pytest.fixture
+def fastapi_schema() -> dict[str, Any]:
     app = FastAPI()
 
-    @app.get("/", response_model=TestModel)
+    @app.get("/", response_model=_FakeModel)
     def _h():
         ...
 
     openapi = app.openapi()
-    fastapi_schema = openapi["components"]["schemas"]["TestModel"]
+    return openapi["components"]["schemas"][_FakeModel.__name__]
+
+
+def test_fastapi_openapi_component_schemas(fastapi_schema: dict[str, Any]):
 
     assert fastapi_schema["properties"] == {
         "int0": {"title": "Int0", "type": "integer"},
@@ -119,5 +140,66 @@ def test_annotated_url_in_pydantic_changes_in_fastapi():
         },
     }
 
+
+@pytest.mark.xfail(
+    reason=f"{pydantic.__version__=} and {fastapi.__version__=} produce different json-schemas for the same model"
+)
+def test_compare_pydantic_vs_fastapi_schemas(
+    fastapi_schema: dict[str, Any], pydantic_schema: dict[str, Any]
+):
+
     # NOTE @all: I cannot understand this?!
     assert fastapi_schema["properties"] != pydantic_schema["properties"]
+
+
+def test_differences_between_new_pydantic_url_types():
+    # SEE https://docs.pydantic.dev/2.10/api/networks/
+
+    # | **URL**                       | **AnyUrl** | **HttpUrl** | **AnyHttpUrl** |
+    # |-------------------------------|------------|-------------|----------------|
+    # | `http://example.com`          | ✅          | ✅           | ✅              |
+    # | `https://example.com/resource`| ✅          | ✅           | ✅              |
+    # | `ftp://example.com`           | ✅          | ❌           | ❌              |
+    # | `http://localhost`            | ✅          | ✅           | ✅              |
+    # | `http://127.0.0.1`            | ✅          | ✅           | ✅              |
+    # | `http://127.0.0.1:8080`       | ✅          | ✅           | ✅              |
+    # | `customscheme://example.com`  | ✅          | ❌           | ❌              |
+
+    url = "http://example.com"
+    TypeAdapter(AnyUrl).validate_python(url)
+    TypeAdapter(HttpUrl).validate_python(url)
+    TypeAdapter(AnyHttpUrl).validate_python(url)
+
+    url = "https://example.com/resource"
+    TypeAdapter(AnyUrl).validate_python(url)
+    TypeAdapter(HttpUrl).validate_python(url)
+    TypeAdapter(AnyHttpUrl).validate_python(url)
+
+    url = "ftp://example.com"
+    TypeAdapter(AnyUrl).validate_python(url)
+    with pytest.raises(ValidationError):
+        TypeAdapter(HttpUrl).validate_python(url)
+    with pytest.raises(ValidationError):
+        TypeAdapter(AnyHttpUrl).validate_python(url)
+
+    url = "http://localhost"
+    TypeAdapter(AnyUrl).validate_python(url)
+    TypeAdapter(HttpUrl).validate_python(url)
+    TypeAdapter(AnyHttpUrl).validate_python(url)
+
+    url = "http://127.0.0.1"
+    TypeAdapter(AnyUrl).validate_python(url)
+    TypeAdapter(HttpUrl).validate_python(url)
+    TypeAdapter(AnyHttpUrl).validate_python(url)
+
+    url = "http://127.0.0.1:8080"
+    TypeAdapter(AnyUrl).validate_python(url)
+    TypeAdapter(HttpUrl).validate_python(url)
+    TypeAdapter(AnyHttpUrl).validate_python(url)
+
+    url = "customscheme://example.com"
+    TypeAdapter(AnyUrl).validate_python(url)
+    with pytest.raises(ValidationError):
+        TypeAdapter(HttpUrl).validate_python(url)
+    with pytest.raises(ValidationError):
+        TypeAdapter(AnyHttpUrl).validate_python(url)
