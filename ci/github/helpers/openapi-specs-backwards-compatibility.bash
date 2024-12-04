864d19024fe4b1f50ce17f76f52f5995f6666d9e
Mads Bisgaard <bisgaard@itis.swiss>
2024-12-04 13:47:52 +0100

    improve ci script

--- ci/github/helpers/openapi-specs-backwards-compatibility.bash
+++ ci/github/helpers/openapi-specs-backwards-compatibility.bash
@@ -1,26 +1,47 @@
 #!/bin/bash
 
-# Recursively checks if all openapi specs within a local osparc-simcore revision are backwards compatible with a remote base
-# Example: bash osparc-simcore/ci/github/helpers/openapi-specs-backwards-compatibility.bash \
-#    https://raw.githubusercontent.com/ITISFoundation/osparc-simcore/refs/heads/master
-#    ./osparc-simcore/
+# Recursively checks if all openapi specs within a local osparc-simcore revision are different/backwards compatible with a remote base
+# Example:
+#    bash osparc-simcore/ci/github/helpers/openapi-specs-backwards-compatibility.bash diff \
+#      https://raw.githubusercontent.com/ITISFoundation/osparc-simcore/refs/heads/master \
+#      ./osparc-simcore/
+# or
+#    bash osparc-simcore/ci/github/helpers/openapi-specs-backwards-compatibility.bash breaking \
+#      https://raw.githubusercontent.com/ITISFoundation/osparc-simcore/refs/heads/master \
+#      ./osparc-simcore/
+#
+# The script generates github error annotations for better being able to locate issues.
 
-base_remote=$1
-revision_local=$2
+operation=$1
+base_remote=$2
+revision_local=$3
 
 repo_base_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../..")
 openapi_specs=$(find "${revision_local}" -type f \( -name 'openapi.json' -o -name 'openapi.yaml' \) -not -path '*/.*' -exec realpath --relative-to="${revision_local}" {} \;)
 
 cd "${revision_local}" || exit 1 # required to mount correct dir for diff tool
 
-exit_status=0
-for spec in ${openapi_specs}; do
-  echo "Comparing ${spec}"
-  if ! "${repo_base_dir}/scripts/openapi-diff.bash" breaking --fail-on ERR "${base_remote}/${spec}" "/specs/${spec}"; then
-    echo "::error file=${spec}::${spec} is not backwards compatible with ${base_remote}/${spec}"
-    exit_status=$(("${exit_status}" + "1"))
-  fi
-  printf "%0.s=" {1..100} && printf "\n"
-done
 
-exit "${exit_status}"
+function run_diff_tool() {
+  exit_status=0
+  for spec in ${openapi_specs}; do
+    echo "Comparing ${spec}"
+    if ! "${repo_base_dir}/scripts/openapi-diff.bash" "$@" "${base_remote}/${spec}" "/specs/${spec}"; then
+      echo "::error file=${spec}:: Error when checking ${spec}"
+      exit_status=$(("${exit_status}" + "1"))
+    fi
+    printf "%0.s=" {1..100} && printf "\n"
+  done
+
+  exit "${exit_status}"
+}
+
+
+if [[ "${operation}" == "diff" ]]; then
+  run_diff_tool "diff" "--fail-on-diff"
+elif [[ "${operation}" == "breaking" ]]; then
+  run_diff_tool "breaking" "--fail-on" "ERR"
+else
+  echo "the operation '${operation}' is not supported"
+  exit 1
+fi
