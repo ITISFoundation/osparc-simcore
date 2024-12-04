4f09be269117f693a3cf122c8e703cd1ba14ca02
Giancarlo Romeo <giancarloromeo@gmail.com>
2024-12-04 12:43:31 +0100

    fix typecheck

--- services/web/server/src/simcore_service_webserver/api_keys/_rpc.py
+++ services/web/server/src/simcore_service_webserver/api_keys/_rpc.py
@@ -5,7 +5,9 @@ from models_library.api_schemas_webserver import WEBSERVER_RPC_NAMESPACE
 from models_library.api_schemas_webserver.auth import ApiKeyCreate, ApiKeyGet
 from models_library.products import ProductName
 from models_library.users import UserID
+from pydantic import TypeAdapter
 from servicelib.rabbitmq import RPCRouter
+from simcore_service_webserver.api_keys._models import ApiKey
 from simcore_service_webserver.api_keys.errors import ApiKeyNotFoundError
 
 from ..rabbitmq import get_rabbitmq_rpc_server
@@ -22,13 +24,15 @@ async def create_api_key(
     product_name: ProductName,
     api_key: ApiKeyCreate,
 ) -> ApiKeyGet:
-    return await _api.create_api_key(
+    created_api_key: ApiKey = await _api.create_api_key(
         app,
         user_id=user_id,
         product_name=product_name,
         display_name=api_key.display_name,
         expiration=api_key.expiration,
     )
+
+    return TypeAdapter(ApiKeyGet).validate_python(created_api_key)
 
 
 @router.expose(reraise_if_error_type=(ApiKeyNotFoundError,))
@@ -38,13 +42,14 @@ async def get_api_key(
     user_id: UserID,
     product_name: ProductName,
     api_key_id: int,
-) -> ApiKeyGet | None:
-    return await _api.get_api_key(
+) -> ApiKeyGet:
+    api_key: ApiKey = await _api.get_api_key(
         app,
         user_id=user_id,
         product_name=product_name,
         api_key_id=api_key_id,
     )
+    return TypeAdapter(ApiKeyGet).validate_python(api_key)
 
 
 @router.expose()
@@ -56,13 +61,14 @@ async def get_or_create_api_key(
     display_name: str,
     expiration: timedelta | None = None,
 ) -> ApiKeyGet:
-    return await _api.get_or_create_api_key(
+    api_key: ApiKey = await _api.get_or_create_api_key(
         app,
         user_id=user_id,
         product_name=product_name,
         display_name=display_name,
         expiration=expiration,
     )
+    return TypeAdapter(ApiKeyGet).validate_python(api_key)
 
 
 @router.expose()
