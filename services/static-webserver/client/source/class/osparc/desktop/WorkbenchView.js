ff5a7238a2f2416f581916893537b295322e953b
odeimaiz <maiz@itis.swiss>
2024-12-05 12:12:31 +0100

    add logs

--- services/static-webserver/client/source/class/osparc/desktop/WorkbenchView.js
+++ services/static-webserver/client/source/class/osparc/desktop/WorkbenchView.js
@@ -33,6 +33,27 @@ qx.Class.define("osparc.desktop.WorkbenchView", {
     this.__workbenchUI = this.__workbenchPanel.getMainView();
 
     this.__attachEventHandlers();
+
+    // OM removed this
+    setTimeout(() => {
+      [{
+        nodeId: null,
+        messages: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."],
+        logLevel: "DEBUG",
+      }, {
+        nodeId: null,
+        messages: ["Contrary to popular belief, Lorem Ipsum is not simply random text.\nIt has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.\nRichard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC.\nThis book is a treatise on the theory of ethics, very popular during the Renaissance.\nThe first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32."],
+        logLevel: "INFO",
+      }, {
+        nodeId: null,
+        messages: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."],
+        logLevel: "WARNING",
+      }, {
+        nodeId: null,
+        messages: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."],
+        logLevel: "ERROR",
+      }].forEach(log => this.logsToLogger(log.nodeId, log.messages, log.logLevel));
+    }, 5000);
   },
 
   statics: {
