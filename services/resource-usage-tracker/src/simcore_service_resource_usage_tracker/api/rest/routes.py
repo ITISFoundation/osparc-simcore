1a9f19a6ead91522f01ecd22ef6aa5ea69cf0038
Matus Drobuliak <60785969+matusdrobuliak66@users.noreply.github.com>
2024-12-09 14:30:34 +0100

    ♻️ Add startup logs to RUT (#6921)

--- services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/api/rest/routes.py
+++ services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/api/rest/routes.py
@@ -1,16 +1,26 @@
+import logging
+
 from fastapi import APIRouter, FastAPI
+from servicelib.logging_utils import log_context
 
 from ..._meta import API_VTAG
 from . import _health, _meta, _resource_tracker
+
+_logger = logging.getLogger(__name__)
 
 
 def setup_api_routes(app: FastAPI):
     """
     Composes resources/sub-resources routers
     """
-    app.include_router(_health.router)
+    with log_context(
+        _logger,
+        logging.INFO,
+        msg="RUT setup_api_routes",
+    ):
+        app.include_router(_health.router)
 
-    api_router = APIRouter(prefix=f"/{API_VTAG}")
-    api_router.include_router(_meta.router, tags=["meta"])
-    api_router.include_router(_resource_tracker.router)
-    app.include_router(api_router)
+        api_router = APIRouter(prefix=f"/{API_VTAG}")
+        api_router.include_router(_meta.router, tags=["meta"])
+        api_router.include_router(_resource_tracker.router)
+        app.include_router(api_router)
