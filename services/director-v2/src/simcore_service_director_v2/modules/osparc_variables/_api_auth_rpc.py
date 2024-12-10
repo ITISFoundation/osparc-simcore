34374b4ac83285a7de3696d0e40fcd27c5633265
Giancarlo Romeo <giancarloromeo@gmail.com>
2024-12-10 10:13:43 +0100

    ♻️ Refactor API-keys service (#6843)
    Co-authored-by: odeimaiz <maiz@itis.swiss>

--- services/director-v2/src/simcore_service_director_v2/modules/osparc_variables/_api_auth_rpc.py
+++ services/director-v2/src/simcore_service_director_v2/modules/osparc_variables/_api_auth_rpc.py
@@ -2,9 +2,9 @@ from datetime import timedelta
 
 from fastapi import FastAPI
 from models_library.api_schemas_webserver import WEBSERVER_RPC_NAMESPACE
-from models_library.api_schemas_webserver.auth import ApiKeyGet
 from models_library.products import ProductName
 from models_library.rabbitmq_basic_types import RPCMethodName
+from models_library.rpc.webserver.auth.api_keys import ApiKeyGet
 from models_library.users import UserID
 from pydantic import TypeAdapter
 
@@ -20,16 +20,16 @@ async def get_or_create_api_key_and_secret(
     *,
     product_name: ProductName,
     user_id: UserID,
-    name: str,
+    display_name: str,
     expiration: timedelta | None = None,
 ) -> ApiKeyGet:
     rpc_client = get_rabbitmq_rpc_client(app)
     result = await rpc_client.request(
         WEBSERVER_RPC_NAMESPACE,
-        TypeAdapter(RPCMethodName).validate_python("get_or_create_api_keys"),
-        product_name=product_name,
+        TypeAdapter(RPCMethodName).validate_python("get_or_create_api_key"),
         user_id=user_id,
-        name=name,
+        display_name=display_name,
         expiration=expiration,
+        product_name=product_name,
     )
     return ApiKeyGet.model_validate(result)
