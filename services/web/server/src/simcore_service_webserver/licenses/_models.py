825c22ae1c23386cd3e747c3865e5cb672acfbb3
Matus Drobuliak <60785969+matusdrobuliak66@users.noreply.github.com>
2024-12-11 11:18:59 +0100

    ✨ introduce `licensed_items_purchases` endpoints 🗃️ (#6928)
    On behalf of @matusdrobuliak66

--- services/web/server/src/simcore_service_webserver/licenses/_models.py
+++ services/web/server/src/simcore_service_webserver/licenses/_models.py
@@ -2,6 +2,10 @@ import logging
 
 from models_library.basic_types import IDStr
 from models_library.licensed_items import LicensedItemID
+from models_library.resource_tracker import PricingPlanId, PricingUnitId
+from models_library.resource_tracker_licensed_items_purchases import (
+    LicensedItemPurchaseID,
+)
 from models_library.rest_base import RequestParameters, StrictRequestParameters
 from models_library.rest_ordering import (
     OrderBy,
@@ -14,7 +18,7 @@ from models_library.wallets import WalletID
 from pydantic import BaseModel, ConfigDict, Field
 from servicelib.request_keys import RQT_USERID_KEY
 
-from ..._constants import RQ_PRODUCT_KEY
+from .._constants import RQ_PRODUCT_KEY
 
 _logger = logging.getLogger(__name__)
 
@@ -49,6 +53,32 @@ class LicensedItemsListQueryParams(
 
 class LicensedItemsBodyParams(BaseModel):
     wallet_id: WalletID
-    num_of_seeds: int
+    pricing_plan_id: PricingPlanId
+    pricing_unit_id: PricingUnitId
+    num_of_seats: int
 
     model_config = ConfigDict(extra="forbid")
+
+
+class LicensedItemsPurchasesPathParams(StrictRequestParameters):
+    licensed_item_purchase_id: LicensedItemPurchaseID
+
+
+_LicensedItemsPurchasesListOrderQueryParams: type[
+    RequestParameters
+] = create_ordering_query_model_class(
+    ordering_fields={
+        "purchased_at",
+        "modified_at",
+        "name",
+    },
+    default=OrderBy(field=IDStr("purchased_at"), direction=OrderDirection.DESC),
+    ordering_fields_api_to_column_map={"modified_at": "modified"},
+)
+
+
+class LicensedItemsPurchasesListQueryParams(
+    PageQueryParameters,
+    _LicensedItemsPurchasesListOrderQueryParams,  # type: ignore[misc, valid-type]
+):
+    ...
