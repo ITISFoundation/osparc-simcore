c715cdf688e44476f3205f35fb03ad6d02bacf4b
Mads Bisgaard <bisgaard@itis.swiss>
2024-12-11 16:12:12 +0100

    minor change

--- services/api-server/src/simcore_service_api_server/api/routes/licensed_items.py
+++ services/api-server/src/simcore_service_api_server/api/routes/licensed_items.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from models_library.api_schemas_webserver.licensed_items import LicensedItemGetPage
 from servicelib.rabbitmq._client_rpc import RabbitMQRPCClient
 from servicelib.rabbitmq.rpc_interfaces.webserver.licenses.licensed_items import (
-    get_licensed_items,
+    get_licensed_items as _get_licensed_items,
 )
 from simcore_service_api_server.api.dependencies.rabbitmq import get_rabbitmq_rpc_client
 
@@ -14,10 +14,10 @@ router = APIRouter()
 @router.get(
     "/", response_model=LicensedItemGetPage, description="Get all licensed items"
 )
-async def get_licensed_items_asd(
+async def get_licensed_items(
     wallet_id: int,
     webserver_rpc_api: Annotated[RabbitMQRPCClient, Depends(get_rabbitmq_rpc_client)],
 ) -> LicensedItemGetPage:
-    return await get_licensed_items(
+    return await _get_licensed_items(
         rabbitmq_rpc_client=webserver_rpc_api,
     )
