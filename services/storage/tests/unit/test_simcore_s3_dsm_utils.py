a0d4011e7c44cb66951759f0c3fe6be0f5c28ed5
Giancarlo Romeo <giancarloromeo@gmail.com>
2024-12-13 09:47:08 +0100

    fix test

--- services/storage/tests/unit/test_simcore_s3_dsm_utils.py
+++ services/storage/tests/unit/test_simcore_s3_dsm_utils.py
@@ -7,12 +7,12 @@ from simcore_service_storage.simcore_s3_dsm_utils import compute_file_id_prefix
     [
         (
             "b21a3b80-d578-4b33-a224-e24ee2e4966a/42b9cc07-60f5-4d29-a063-176d1467901c/my/amazing/sub/folder/with/a/file.bin",
-            2,
+            3,
             "b21a3b80-d578-4b33-a224-e24ee2e4966a/42b9cc07-60f5-4d29-a063-176d1467901c/my",
         ),
         (
             "api/42b9cc07-60f5-4d29-a063-176d1467901c/my/amazing/sub/folder/with/a/file.bin",
-            2,
+            3,
             "api/42b9cc07-60f5-4d29-a063-176d1467901c/my",
         ),
     ],
