eb9b0fa986706f21e64499b8796a6ff15c21547c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2024-12-16 08:32:40 +0100

    removed submit column

--- services/director-v2/src/simcore_service_director_v2/modules/db/repositories/comp_tasks/_utils.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/repositories/comp_tasks/_utils.py
@@ -145,12 +145,12 @@ async def _get_node_infos(
             None,
         )
 
-    result: tuple[ServiceMetaDataPublished, ServiceExtras, SimcoreServiceLabels] = (
-        await asyncio.gather(
-            _get_service_details(catalog_client, user_id, product_name, node),
-            director_client.get_service_extras(node.key, node.version),
-            director_client.get_service_labels(node),
-        )
+    result: tuple[
+        ServiceMetaDataPublished, ServiceExtras, SimcoreServiceLabels
+    ] = await asyncio.gather(
+        _get_service_details(catalog_client, user_id, product_name, node),
+        director_client.get_service_extras(node.key, node.version),
+        director_client.get_service_labels(node),
     )
     return result
 
@@ -246,9 +246,9 @@ async def _get_pricing_and_hardware_infos(
     return pricing_info, hardware_info
 
 
-_RAM_SAFE_MARGIN_RATIO: Final[float] = (
-    0.1  # NOTE: machines always have less available RAM than advertised
-)
+_RAM_SAFE_MARGIN_RATIO: Final[
+    float
+] = 0.1  # NOTE: machines always have less available RAM than advertised
 _CPUS_SAFE_MARGIN: Final[float] = 0.1
 
 
@@ -266,11 +266,11 @@ async def _update_project_node_resources_from_hardware_info(
     if not hardware_info.aws_ec2_instances:
         return
     try:
-        unordered_list_ec2_instance_types: list[EC2InstanceTypeGet] = (
-            await get_instance_type_details(
-                rabbitmq_rpc_client,
-                instance_type_names=set(hardware_info.aws_ec2_instances),
-            )
+        unordered_list_ec2_instance_types: list[
+            EC2InstanceTypeGet
+        ] = await get_instance_type_details(
+            rabbitmq_rpc_client,
+            instance_type_names=set(hardware_info.aws_ec2_instances),
         )
 
         assert unordered_list_ec2_instance_types  # nosec
@@ -439,7 +439,6 @@ async def generate_tasks_list_from_project(
             inputs=node.inputs,
             outputs=node.outputs,
             image=image,
-            submit=arrow.utcnow().datetime,
             state=task_state,
             internal_id=internal_id,
             node_class=to_node_class(node.key),
