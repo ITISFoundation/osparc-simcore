dcae958c2080f42346267fd76049c5644e603cb9
Giancarlo Romeo <giancarloromeo@gmail.com>
2024-12-17 13:25:10 +0100

    Sync nodes when patching

--- services/web/server/src/simcore_service_webserver/projects/projects_api.py
+++ services/web/server/src/simcore_service_webserver/projects/projects_api.py
@@ -1047,13 +1047,17 @@ async def patch_project_node(
     if _node_patch_exclude_unset.get("label"):
         await dynamic_scheduler_api.update_projects_networks(app, project_id=project_id)
 
-    # 5. Updates project states for user, if inputs have been changed
-    if "inputs" in _node_patch_exclude_unset:
+    # 5. Updates project states for user, if inputs/outputs have been changed
+    if {"inputs", "outputs"} & _node_patch_exclude_unset.keys():
         updated_project = await add_project_states_for_user(
-           user_id=user_id, project=updated_project, is_template=False, app=app
+            user_id=user_id, project=updated_project, is_template=False, app=app
         )
+        for node_uuid in updated_project["workbench"]:
+            await notify_project_node_update(
+                app, updated_project, node_uuid, errors=None
+            )
+        return
 
-    # 6. Notify project node update
     await notify_project_node_update(app, updated_project, node_id, errors=None)
 
 
