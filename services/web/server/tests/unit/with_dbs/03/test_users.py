694a0d7bc2a8cfc1f30fe32ad35281c9e7453cb9
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-12-17 18:26:40 +0100

    pylint

--- services/web/server/tests/unit/with_dbs/03/test_users.py
+++ services/web/server/tests/unit/with_dbs/03/test_users.py
@@ -1,6 +1,7 @@
 # pylint: disable=protected-access
 # pylint: disable=redefined-outer-name
 # pylint: disable=too-many-arguments
+# pylint: disable=too-many-statements
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
