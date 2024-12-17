c01e35a743ca77384d7f4a56b9e8e1edeae53814
Andrei Neagu <neagu@itis.swiss>
2024-12-17 13:45:53 +0100

    refactor

--- packages/service-library/src/servicelib/archiving_utils/_api.py
+++ packages/service-library/src/servicelib/archiving_utils/_api.py
@@ -1,7 +1,10 @@
 # used to switch between implementations
 
+from ._interface_zipfile import archive_dir, unarchive_dir
 
 __all__: tuple[str, ...] = (
     "archive_dir",
     "unarchive_dir",
 )
+
+# nopycln: file
