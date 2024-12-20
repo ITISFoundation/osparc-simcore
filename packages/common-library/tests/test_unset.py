6762e0b75b71d3f4cdbe89f88335de1ba9ce38cd
Andrei Neagu <neagu@itis.swiss>
2024-12-20 15:41:20 +0100

    fixed issue

--- packages/common-library/tests/test_unset.py
+++ packages/common-library/tests/test_unset.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from common_library.unset import UnSet, as_dict_exclude_unset
+from common_library.unset import UnSet, as_dict_exclude_none, as_dict_exclude_unset
 
 
 def test_as_dict_exclude_unset():
@@ -13,3 +13,10 @@ def test_as_dict_exclude_unset():
     assert f(par1="hi") == {"par1": "hi"}
     assert f(par2=4) == {"par2": 4}
     assert f(par1="hi", par2=4) == {"par1": "hi", "par2": 4}
+
+    # still expected behavior
+    assert as_dict_exclude_unset(par1=None) == {"par1": None}
+
+
+def test_as_dict_exclude_none():
+    assert as_dict_exclude_none(par1=None) == {}
