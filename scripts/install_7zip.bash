4a5a3a6f40552cd96852fd2e03cfc7d26ad4124e
Andrei Neagu <neagu@itis.swiss>
2024-12-20 10:56:17 +0100

    added arm64 target install

--- scripts/install_7zip.bash
+++ scripts/install_7zip.bash
@@ -9,6 +9,13 @@ set -o nounset  # abort on unbound variable
 set -o pipefail # don't hide errors within pipes
 IFS=$'\n\t'
 
+TARGETARCH="${TARGETARCH:-amd64}"
+
+case "${TARGETARCH}" in \
+  "amd64") ARCH="x64" ;; \
+  "arm64") ARCH="arm64" ;; \
+  *) echo "Unsupported architecture: ${TARGETARCH}" && exit 1 ;; \
+esac
 
 SEVEN_ZIP_VERSION="2409"
 ## 7z compression
@@ -17,8 +24,8 @@ rm -rf /tmp/7zip
 mkdir -p /tmp/7zip
 cd /tmp/7zip
 
-curl -LO https://www.7-zip.org/a/7z${SEVEN_ZIP_VERSION}-linux-x64.tar.xz
-tar -xvf 7z${SEVEN_ZIP_VERSION}-linux-x64.tar.xz
+curl -LO https://www.7-zip.org/a/7z${SEVEN_ZIP_VERSION}-linux-${ARCH}.tar.xz
+tar -xvf 7z${SEVEN_ZIP_VERSION}-linux-${ARCH}.tar.xz
 cp 7zz /usr/bin/7z
 
 echo "remove install dir"
