e37ccf2b3967d989f56dfc84c4c6e4d105636667
Mads Bisgaard <bisgaard@itis.swiss>
2025-01-09 10:45:24 +0100

    @pcrespov int -> PositiveInt

--- services/api-server/src/simcore_service_api_server/models/schemas/licensed_items.py
+++ services/api-server/src/simcore_service_api_server/models/schemas/licensed_items.py
@@ -1,7 +1,7 @@
 from models_library.services_types import ServiceRunID
-from pydantic import BaseModel
+from pydantic import BaseModel, PositiveInt
 
 
 class LicensedItemCheckoutData(BaseModel):
-    number_of_seats: int
+    number_of_seats: PositiveInt
     service_run_id: ServiceRunID
