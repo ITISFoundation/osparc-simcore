79c9e3309c0521646fe9049738e5d22862a610fc
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-01-10 13:10:55 +0100

    @GitHK review: strict argument

--- services/web/server/tests/unit/with_dbs/04/garbage_collector/test_resource_manager.py
+++ services/web/server/tests/unit/with_dbs/04/garbage_collector/test_resource_manager.py
@@ -347,7 +347,7 @@ async def test_websocket_multiple_connections(
         clients.append(sio)
         resource_keys.append(resource_key)
 
-    for sio, resource_key in zip(clients, resource_keys, strict=False):
+    for sio, resource_key in zip(clients, resource_keys, strict=True):
         sid = sio.get_sid()
         await sio.disconnect()
         await sio.wait()
