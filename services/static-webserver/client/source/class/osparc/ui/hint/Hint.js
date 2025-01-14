84d2c03040ec26ef801d02e2505e32a24fba0e79
odeimaiz <maiz@itis.swiss>
2025-01-14 10:41:15 +0100

    center Hint after updating text

--- services/static-webserver/client/source/class/osparc/ui/hint/Hint.js
+++ services/static-webserver/client/source/class/osparc/ui/hint/Hint.js
@@ -71,6 +71,7 @@ qx.Class.define("osparc.ui.hint.Hint", {
 
     setText: function(text) {
       this.getChildControl("label").setValue(text);
+      setTimeout(() => this.updatePosition(), 10);
     }
   }
 });
