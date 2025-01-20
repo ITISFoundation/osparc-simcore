9d80eddf2b480ba12289f2a30d406aa50db1b7ff
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-01-20 14:33:14 +0100

    remove validation

--- services/web/server/src/simcore_service_webserver/projects/_crud_api_create.py
+++ services/web/server/src/simcore_service_webserver/projects/_crud_api_create.py
@@ -132,7 +132,7 @@ async def _copy_project_nodes_from_source_project(
     db: ProjectDBAPI = ProjectDBAPI.get_from_app_context(app)
 
     def _mapped_node_id(node: ProjectNode) -> NodeID:
-        return NodeID(nodes_map[TypeAdapter(NodeIDStr).validate_python(f"{node.node_id}")])
+        return NodeID(nodes_map[f"{node.node_id}"])
 
     return {
         _mapped_node_id(node): ProjectNodeCreate(
