5bb62eac5143b8cc3ebd5fb3690f0868507e23a7
Andrei Neagu <neagu@itis.swiss>
2025-01-23 11:40:46 +0100

    added secure flag to settings

--- packages/settings-library/src/settings_library/docker_api_proxy.py
+++ packages/settings-library/src/settings_library/docker_api_proxy.py
@@ -13,10 +13,12 @@ class DockerApiProxysettings(BaseCustomSettings):
     DOCKER_API_PROXY_PORT: PortInt = Field(
         8888, description="port of the docker-api-proxy service"
     )
+    DOCKER_API_PROXY_SECURE: bool = False
 
     DOCKER_API_PROXY_USER: str | None = None
     DOCKER_API_PROXY_PASSWORD: SecretStr | None = None
 
     @cached_property
     def base_url(self) -> str:
-        return f"http://{self.DOCKER_API_PROXY_HOST}:{self.DOCKER_API_PROXY_PORT}"
+        protocl = "https" if self.DOCKER_API_PROXY_SECURE else "http"
+        return f"{protocl}://{self.DOCKER_API_PROXY_HOST}:{self.DOCKER_API_PROXY_PORT}"
