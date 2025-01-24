f9e623e31ef1cb5ee9a7911f7617feff7b2e5c02
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-01-24 10:20:29 +0100

    add aws installation

---
+++ ci/github/helpers/install_aws_cli_v2.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#  Installs the latest version of AWS CLI V2
+#
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -o errexit  # abort on nonzero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
+IFS=$'\n\t'
+
+AWS_CLI_VERSION="2.11.11"
+ARCH="x86_64"
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-${ARCH}-${AWS_CLI_VERSION}.zip" --output "awscliv2.zip" &&
+  apt-get update &&
+  apt-get install -y unzip &&
+  unzip awscliv2.zip &&
+  ./aws/install &&
+  apt-get remove --purge -y unzip &&
+  rm awscliv2.zip &&
+  rm -rf awscliv2
--- ci/github/integration-testing/simcore-sdk.bash
+++ ci/github/integration-testing/simcore-sdk.bash
@@ -8,6 +8,7 @@ install() {
   make devenv
   # shellcheck source=/dev/null
   source .venv/bin/activate
+  ./ci/github/helpers/install_aws_cli_v2.bash
   pushd packages/simcore-sdk
   make install-ci
   popd
