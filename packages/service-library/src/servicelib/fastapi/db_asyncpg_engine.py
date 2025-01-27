8d6f4682c9cdfa777fd86d4d1e3854eebd0f7032
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-01-27 13:18:42 +0100

    added some missing stuff

--- packages/service-library/src/servicelib/fastapi/db_asyncpg_engine.py
+++ packages/service-library/src/servicelib/fastapi/db_asyncpg_engine.py
@@ -5,6 +5,7 @@ from settings_library.postgres import PostgresSettings
 from simcore_postgres_database.utils_aiosqlalchemy import (  # type: ignore[import-not-found] # this on is unclear
     get_pg_engine_stateinfo,
 )
+from sqlalchemy.ext.asyncio import AsyncEngine
 
 from ..db_asyncpg_utils import create_async_engine_and_pg_database_ready
 from ..logging_utils import log_context
@@ -31,3 +32,8 @@ async def close_db_connection(app: FastAPI) -> None:
     with log_context(_logger, logging.DEBUG, f"db disconnect of {app.state.engine}"):
         if engine := app.state.engine:
             await engine.dispose()
+
+
+def get_engine(app: FastAPI) -> AsyncEngine:
+    assert isinstance(app.state.engine, AsyncEngine)  # nosec
+    return app.state.engine
