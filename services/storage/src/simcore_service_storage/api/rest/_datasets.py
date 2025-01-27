2656193916605e7751577204d5698ffc11dda157
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-01-27 16:48:16 +0100

    more

---
+++ services/storage/src/simcore_service_storage/api/rest/_datasets.py
@@ -0,0 +1,59 @@
+import logging
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Request
+from models_library.api_schemas_storage import DatasetMetaDataGet, FileMetaDataGet
+from models_library.generics import Envelope
+
+# Exclusive for simcore-s3 storage -----------------------
+from ...dsm import get_dsm_provider
+from ...models import (
+    FilesMetadataDatasetQueryParams,
+    LocationID,
+    StorageQueryParamsBase,
+)
+
+_logger = logging.getLogger(__name__)
+
+router = APIRouter(
+    tags=[
+        "datasets",
+    ],
+)
+
+
+@router.get(
+    "/locations/{location_id}/datasets",
+    response_model=Envelope[list[DatasetMetaDataGet]],
+)
+async def list_datasets_metadata(
+    query_params: Annotated[StorageQueryParamsBase, Depends()],
+    location_id: LocationID,
+    request: Request,
+) -> Envelope[DatasetMetaDataGet]:
+    dsm = get_dsm_provider(request.app).get(location_id)
+    data = await dsm.list_datasets(query_params.user_id)
+    return Envelope[list[DatasetMetaDataGet]](
+        data=[DatasetMetaDataGet(**d.model_dump()) for d in data]
+    )
+
+
+@router.get(
+    "/locations/{location_id}/datasets/{dataset_id}/metadata",
+    response_model=Envelope[list[FileMetaDataGet]],
+)
+async def list_dataset_files_metadata(
+    query_params: Annotated[FilesMetadataDatasetQueryParams, Depends()],
+    location_id: LocationID,
+    dataset_id: str,
+    request: Request,
+) -> Envelope[list[FileMetaDataGet]]:
+    dsm = get_dsm_provider(request.app).get(location_id)
+    data = await dsm.list_files_in_dataset(
+        user_id=query_params.user_id,
+        dataset_id=dataset_id,
+        expand_dirs=query_params.expand_dirs,
+    )
+    return Envelope[list[FileMetaDataGet]](
+        data=[FileMetaDataGet(**d.model_dump()) for d in data]
+    )
---
+++ services/storage/src/simcore_service_storage/api/rest/routes.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, FastAPI
+
+from . import _datasets, _files, _health, _locations, _simcore_s3
+
+v0_router = APIRouter()
+
+# health
+v0_router.include_router(_health.router)
+
+# locations
+v0_router.include_router(_locations.router)
+
+# datasets
+v0_router.include_router(_datasets.router)
+
+# files
+v0_router.include_router(_files.router)
+
+# simcore-s3
+v0_router.include_router(_simcore_s3.router)
+
+
+def setup_rest_api_routes(app: FastAPI, vtag: str):
+    # healthcheck at / and at /v0/
+    app.include_router(health_router)
+    # api under /v*
+    app.include_router(v0_router, prefix=f"/{vtag}")
