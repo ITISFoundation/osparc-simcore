b5672afb2ef02b346a0734826b19199700f9a13f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-01-27 08:35:03 +0100

    big move

--- services/storage/src/simcore_service_storage/modules/long_running_tasks.py
+++ services/storage/src/simcore_service_storage/modules/long_running_tasks.py
@@ -1,7 +1,7 @@
 from aiohttp import web
 from servicelib.aiohttp.long_running_tasks.server import setup
 
-from ._meta import API_VTAG
+from .._meta import API_VTAG
 
 
 def setup_rest_api_long_running_tasks(app: web.Application) -> None:
