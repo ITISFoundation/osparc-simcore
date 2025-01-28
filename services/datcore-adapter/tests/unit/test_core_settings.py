47f8dd45be14168cf01d8ede6951bc911c11ee9f
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-01-28 18:51:13 +0100

    updates datcore-adapter

---
+++ services/datcore-adapter/tests/unit/test_core_settings.py
@@ -0,0 +1,42 @@
+# pylint: disable=unused-variable
+# pylint: disable=unused-argument
+# pylint: disable=redefined-outer-name
+
+
+import pytest
+from pytest_simcore.helpers.monkeypatch_envs import (
+    EnvVarsDict,
+    delenvs_from_dict,
+    setenvs_from_dict,
+)
+from simcore_service_datcore_adapter.core.settings import ApplicationSettings
+
+
+@pytest.fixture
+def app_environment(
+    monkeypatch: pytest.MonkeyPatch,
+    app_environment: EnvVarsDict,
+    external_envfile_dict: EnvVarsDict,
+) -> EnvVarsDict:
+    """
+    NOTE: To run against repo.config in osparc-config repo
+
+    ln -s /path/to/osparc-config/deployments/mydeploy.com/repo.config .secrets
+    pytest --external-envfile=.secrets tests/unit/test_core_settings.py
+
+    """
+    if external_envfile_dict:
+        delenvs_from_dict(monkeypatch, app_environment, raising=False)
+        return setenvs_from_dict(
+            monkeypatch,
+            {**external_envfile_dict},
+        )
+    return app_environment
+
+
+def test_unit_app_environment(app_environment: EnvVarsDict):
+    assert app_environment
+    settings = ApplicationSettings.create_from_envs()
+    print("captured settings: \n", settings.model_dump_json(indent=2, warnings="none"))
+
+    assert settings.PENNSIEVE
