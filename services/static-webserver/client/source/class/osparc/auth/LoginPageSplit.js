43243224c9a7f913d7eacdd39c3ed7158afc247a
odeimaiz <maiz@itis.swiss>
2025-01-28 15:37:54 +0100

    Do not recreate login page

--- services/static-webserver/client/source/class/osparc/auth/LoginPageSplit.js
+++ services/static-webserver/client/source/class/osparc/auth/LoginPageSplit.js
@@ -25,6 +25,9 @@ qx.Class.define("osparc.auth.LoginPageSplit", {
     const layout = new qx.ui.layout.HBox();
     this._setLayout(layout);
 
+    const loginPage = this.__loginPage = new osparc.auth.LoginWithDecorators();
+    loginPage.addListener("done", e => this.fireDataEvent("done", e.getData()));
+
     this.__rebuildLayout();
 
     setTimeout(() => this.__resized(), 100);
@@ -67,8 +70,7 @@ qx.Class.define("osparc.auth.LoginPageSplit", {
     __rebuildLayout: function() {
       this._removeAll();
 
-      const loginPage = new osparc.auth.LoginWithDecorators();
-      loginPage.addListener("done", e => this.fireDataEvent("done", e.getData()));
+      const loginPage = this.__loginWithDecorators;
       const container = new qx.ui.container.Scroll();
       container.add(loginPage);
       const spacers = [
