1335665b964b0fc0988c63be3a6d003fd50711f0
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-01-28 10:17:36 +0100

    @sanderegg review: engine fixture in api-server

--- services/api-server/tests/unit/_with_db/conftest.py
+++ services/api-server/tests/unit/_with_db/conftest.py
@@ -8,16 +8,14 @@ import os
 import shutil
 import subprocess
 import sys
-from collections.abc import AsyncGenerator, AsyncIterator, Callable
+from collections.abc import AsyncGenerator, AsyncIterator, Callable, Iterable
 from pathlib import Path
 
-import aiopg.sa
-import aiopg.sa.engine as aiopg_sa_engine
 import httpx
 import pytest
 import simcore_postgres_database.cli as pg_cli
 import sqlalchemy as sa
-import sqlalchemy.engine as sa_engine
+import sqlalchemy.engine
 import yaml
 from aiopg.sa.connection import SAConnection
 from fastapi import FastAPI
@@ -41,7 +39,9 @@ from simcore_service_api_server.core.settings import PostgresSettings
 ## POSTGRES -----
 
 
-CURRENT_DIR = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+_CURRENT_DIR = (
+    Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+)
 
 
 @pytest.fixture(scope="session")
@@ -54,7 +54,7 @@ def docker_compose_file(
     environ = dict(os.environ)
     environ.update(default_app_env_vars)
 
-    src_path = CURRENT_DIR / "data" / "docker-compose.yml"
+    src_path = _CURRENT_DIR / "data" / "docker-compose.yml"
     assert src_path.exists
 
     dst_path = Path(str(tmpdir_factory.mktemp("config").join("docker-compose.yml")))
@@ -114,19 +114,14 @@ def postgres_service(docker_services, docker_ip, docker_compose_file: Path) -> d
 
 
 @pytest.fixture(scope="session")
-def make_engine(postgres_service: dict) -> Callable:
-    dsn = postgres_service["dsn"]  # session scope freezes dsn
-
-    def maker(*, is_async=True) -> aiopg_sa_engine.Engine | sa_engine.Engine:
-        if is_async:
-            return aiopg.sa.create_engine(dsn)
-        return sa.create_engine(dsn)
-
-    return maker
+def sync_engine(postgres_service: str) -> Iterable[sqlalchemy.engine.Engine]:
+    _engine: sqlalchemy.engine.Engine = sa.create_engine(url=postgres_service)
+    yield _engine
+    _engine.dispose()
 
 
 @pytest.fixture
-def migrated_db(postgres_service: dict, make_engine: Callable):
+def migrated_db(postgres_service: dict, sync_engine: sqlalchemy.engine.Engin):
     # NOTE: this is equivalent to packages/pytest-simcore/src/pytest_simcore/postgres_service.py::postgres_db
     # but we do override postgres_dsn -> postgres_engine -> postgres_db because we want the latter
     # fixture to have local scope
@@ -140,13 +135,8 @@ def migrated_db(postgres_service: dict, make_engine: Callable):
 
     pg_cli.downgrade.callback("base")
     pg_cli.clean.callback()
-    # FIXME: deletes all because downgrade is not reliable!
 
-    try:
-        sync_engine = make_engine(is_async=False)
-        postgres_tools.force_drop_all_tables(sync_engine)
-    finally:
-        sync_engine.dispose()
+    postgres_tools.force_drop_all_tables(sync_engine)
 
 
 @pytest.fixture
