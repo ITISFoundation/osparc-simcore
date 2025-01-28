9cd8f6c7fbdb50bb72b69b4a7dc2477de6e4faee
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-01-28 09:53:37 +0100

    test handlers green

--- services/storage/tests/unit/test_utils_handlers.py
+++ services/storage/tests/unit/test_utils_handlers.py
@@ -11,7 +11,8 @@ import httpx
 import pytest
 from asyncpg import PostgresError
 from aws_library.s3._errors import S3AccessError, S3KeyNotFoundError
-from fastapi import FastAPI, status
+from fastapi import FastAPI, HTTPException, status
+from fastapi.exceptions import RequestValidationError
 from httpx import AsyncClient
 from simcore_service_storage.api.rest.utils import set_exception_handlers
 from simcore_service_storage.exceptions.errors import (
@@ -20,6 +21,9 @@ from simcore_service_storage.exceptions.errors import (
     LinkAlreadyExistsError,
     ProjectAccessRightError,
     ProjectNotFoundError,
+)
+from simcore_service_storage.modules.datcore_adapter.datcore_adapter_exceptions import (
+    DatcoreAdapterTimeoutError,
 )
 from simcore_service_storage.modules.db.db_access_layer import (
     InvalidFileIdentifierError,
@@ -88,6 +92,14 @@ async def client(initialized_app: FastAPI) -> AsyncIterator[AsyncClient]:
             S3AccessError(),
             status.HTTP_503_SERVICE_UNAVAILABLE,
         ),
+        (
+            DatcoreAdapterTimeoutError(msg="pytest datcore adapter timeout"),
+            status.HTTP_504_GATEWAY_TIMEOUT,
+        ),
+        (
+            NotImplementedError("pytest not implemented error"),
+            status.HTTP_501_NOT_IMPLEMENTED,
+        ),
     ],
     ids=str,
 )
@@ -104,3 +116,41 @@ async def test_exception_handlers(
     response = await client.get("/test")
     assert response.status_code == status_code
     assert response.json() == {"errors": [f"{exception}"]}
+
+
+async def test_generic_http_exception_handler(
+    initialized_app: FastAPI, client: AsyncClient
+):
+    @initialized_app.get("/test")
+    async def test_endpoint():
+        raise HTTPException(status_code=status.HTTP_410_GONE)
+
+    response = await client.get("/test")
+    assert response.status_code == status.HTTP_410_GONE
+    assert response.json() == {"errors": ["Gone"]}
+
+
+async def test_request_validation_error_handler(
+    initialized_app: FastAPI, client: AsyncClient
+):
+    @initialized_app.get("/test")
+    async def test_endpoint():
+        raise RequestValidationError(errors=["pytest request validation error"])
+
+    response = await client.get("/test")
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert response.json() == {"errors": ["pytest request validation error"]}
+
+
+@pytest.mark.xfail(
+    reason="Generic exception handler is not working as expected as shown in https://github.com/ITISFoundation/osparc-simcore/blob/5732a12e07e63d5ce55010ede9b9ab543bb9b278/packages/service-library/tests/fastapi/test_exceptions_utils.py"
+)
+async def test_generic_exception_handler(initialized_app: FastAPI, client: AsyncClient):
+    @initialized_app.get("/test")
+    async def test_endpoint():
+        msg = "Generic exception"
+        raise Exception(msg)
+
+    response = await client.get("/test")
+    assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+    assert response.json() == {"detail": "Generic exception"}
