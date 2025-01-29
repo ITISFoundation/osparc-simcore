1e54972f002e9bfecb21c661b793423cbae07e14
Andrei Neagu <neagu@itis.swiss>
2025-01-29 15:46:04 +0100

    remove unused

--- services/docker-api-proxy/docker/entrypoint.sh
+++ services/docker-api-proxy/docker/entrypoint.sh
@@ -14,27 +14,21 @@ INFO="INFO: [$(basename "$0")] "
 echo "$INFO" "Entrypoint for stage ${SC_BUILD_TARGET} ..."
 echo "$INFO" "User :$(id "$(whoami)")"
 
-#chown -R ${SC_USER_NAME}:${SC_USER_NAME} /var/run/docker.sock
-
-
-# Appends docker group if socket is mounted
+# Appends docker group
 DOCKER_MOUNT=/var/run/docker.sock
-if stat $DOCKER_MOUNT >/dev/null 2>&1; then
-  echo "INFO: detected docker socket is mounted, adding user to group..."
-  GROUPID=$(stat -c %g $DOCKER_MOUNT) # Alpine uses `-c` instead of `--format`
-  GROUPNAME=scdocker
+echo "INFO: detected docker socket is mounted, adding user to group..."
+GROUPID=$(stat -c %g $DOCKER_MOUNT) # Alpine uses `-c` instead of `--format`
+GROUPNAME=scdocker
 
-  # Check if a group with the specified GID exists
-  if ! addgroup -g "$GROUPID" $GROUPNAME >/dev/null 2>&1; then
-    echo "WARNING: docker group with GID $GROUPID already exists, getting group name..."
-    # Get the group name based on GID
-    GROUPNAME=$(getent group | awk -F: "\$3 == $GROUPID {print \$1}")
-    echo "WARNING: docker group with GID $GROUPID has name $GROUPNAME"
-  fi
-
-  # Add the user to the group
-  adduser "$SC_USER_NAME" $GROUPNAME
+# Check if a group with the specified GID exists
+if ! addgroup -g "$GROUPID" $GROUPNAME >/dev/null 2>&1; then
+  echo "WARNING: docker group with GID $GROUPID already exists, getting group name..."
+  # Get the group name based on GID
+  GROUPNAME=$(getent group | awk -F: "\$3 == $GROUPID {print \$1}")
+  echo "WARNING: docker group with GID $GROUPID has name $GROUPNAME"
 fi
 
+# Add the user to the group
+adduser "$SC_USER_NAME" $GROUPNAME
 
 exec gosu "$SC_USER_NAME" "$@"
