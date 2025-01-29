73e5625dedc70e2a9a624512ed232c907c20c3de
odeimaiz <maiz@itis.swiss>
2025-01-29 09:53:43 +0100

    Company name required

--- services/static-webserver/client/source/class/osparc/auth/ui/RequestAccount.js
+++ services/static-webserver/client/source/class/osparc/auth/ui/RequestAccount.js
@@ -86,6 +86,7 @@ qx.Class.define("osparc.auth.ui.RequestAccount", {
       switch (osparc.product.Utils.getProductName()) {
         case "s4l":
           this._form.add(organization, this.tr("Company Name"), null, "company");
+          organization.setRequired(true);
           break;
         case "s4lacad":
         case "s4ldesktopacad":
