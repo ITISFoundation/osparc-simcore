2d90725f8b2058e38c671d06f5be716461e86649
Andrei Neagu <neagu@itis.swiss>
2025-01-31 11:34:36 +0100

    rename

--- packages/service-library/src/servicelib/fastapi/lifespan_utils.py
+++ packages/service-library/src/servicelib/fastapi/lifespan_utils.py
@@ -7,9 +7,7 @@ from fastapi import FastAPI
 LifespanContextManager: TypeAlias = Callable[[FastAPI], AsyncContextManager[None]]
 
 
-def combine_lfiespans(
-    *context_managers: LifespanContextManager,
-) -> LifespanContextManager:
+def combine_lfiespans(*lifespans: LifespanContextManager) -> LifespanContextManager:
     """Applyes the `setup` part of the context mangers in the order they are provided.
     The `teardown` is in revere order with regarad to the `seutp`.
     """
@@ -17,7 +15,7 @@ def combine_lfiespans(
     @asynccontextmanager
     async def _(app: FastAPI) -> AsyncGenerator[None, None]:
         async with AsyncExitStack() as stack:
-            for context_manager in context_managers:
+            for context_manager in lifespans:
                 await stack.enter_async_context(context_manager(app))
             yield
 
