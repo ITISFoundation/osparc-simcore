b70feb41b88fd439d03bd31a6eef974ad6a26bcc
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-01-31 17:19:19 +0100

    minor fix on try again

--- services/web/server/src/simcore_service_webserver/licenses/_itis_vip_settings.py
+++ services/web/server/src/simcore_service_webserver/licenses/_itis_vip_settings.py
@@ -1,5 +1,7 @@
 from typing import Annotated
 
+from common_library.pydantic_basic_types import ShortTruncatedStr
+from models_library.basic_types import IDStr
 from pydantic import AfterValidator, HttpUrl
 from settings_library.base import BaseCustomSettings
 
@@ -20,3 +22,9 @@ class ItisVipSettings(BaseCustomSettings):
             HttpUrl(self.ITIS_VIP_API_URL.format(category=category))
             for category in self.ITIS_VIP_CATEGORIES
         ]
+
+
+class ItisVipCategorySettings(BaseCustomSettings):
+    ITIS_VIP_CATEGORY_URL: HttpUrl
+    ITIS_VIP_CATEGORY_ID: IDStr  # use same as in vip-api
+    ITIS_VIP_CATEGORY_DISPLAY_NAME: ShortTruncatedStr
