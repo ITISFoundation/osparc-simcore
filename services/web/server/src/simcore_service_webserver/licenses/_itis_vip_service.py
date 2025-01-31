a5b3d0715d03f46764c342e9afa36aae6e4d6b38
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-01-31 18:47:27 +0100

    retry policy

--- services/web/server/src/simcore_service_webserver/licenses/_itis_vip_service.py
+++ services/web/server/src/simcore_service_webserver/licenses/_itis_vip_service.py
@@ -1,20 +1,31 @@
 import httpx
 from pydantic import HttpUrl
+from tenacity import (
+    retry,
+    retry_if_exception_cause_type,
+    stop_after_attempt,
+    wait_exponential,
+)
 
-from ._itis_vip_models import AvailableDownload, ResponseData
+from ._itis_vip_models import ItisVipApiResponse, ItisVipData
 
 
+@retry(
+    wait=wait_exponential(multiplier=1, min=4, max=10),
+    stop=stop_after_attempt(5),
+    retry=retry_if_exception_cause_type(httpx.RequestError),
+)
 async def get_category_items(
     client: httpx.AsyncClient, url: HttpUrl
-) -> list[AvailableDownload]:
+) -> list[ItisVipData]:
     """
 
     Raises:
-        Any https://www.python-httpx.org/exceptions/
-        httpx.HTTPStatusCode:
-
+        httpx.HTTPStatusError
+        pydantic.ValidationError
     """
     response = await client.post(f"{url}")
     response.raise_for_status()
-    validated_data = ResponseData.model_validate(response.json())
+
+    validated_data = ItisVipApiResponse.model_validate(response.json())
     return validated_data.available_downloads
