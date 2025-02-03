db5fcdacf2d440678bf665e9883ed332e2cb3b1a
odeimaiz <maiz@itis.swiss>
2025-02-03 09:34:47 +0100

    suggestIcon

--- services/static-webserver/client/source/class/osparc/dashboard/CardBase.js
+++ services/static-webserver/client/source/class/osparc/dashboard/CardBase.js
@@ -488,7 +488,7 @@ qx.Class.define("osparc.dashboard.CardBase", {
         lastChangeDate: resourceData.lastChangeDate ? new Date(resourceData.lastChangeDate) : null,
         trashedAt: resourceData.trashedAt ? new Date(resourceData.trashedAt) : null,
         trashedBy: resourceData.trashedBy || null,
-        icon: resourceData.thumbnail || this.self().PRODUCT_ICON,
+        icon: ["study", "template"].includes(resourceData.resourceType) ? osparc.study.Utils.suggestIcon(resourceData) : this.self().PRODUCT_ICON,
         thumbnail: resourceData.thumbnail || this.self().PRODUCT_ICON,
         state: resourceData.state ? resourceData.state : {},
         classifiers: resourceData.classifiers && resourceData.classifiers ? resourceData.classifiers : [],
