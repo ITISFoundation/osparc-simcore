b2ab1eb8e7164b1aae903c844320320ab36058da
odeimaiz <maiz@itis.swiss>
2025-02-05 15:54:58 +0100

    Usage only in one place

--- services/static-webserver/client/source/class/osparc/desktop/account/MyAccount.js
+++ services/static-webserver/client/source/class/osparc/desktop/account/MyAccount.js
@@ -28,8 +28,11 @@ qx.Class.define("osparc.desktop.account.MyAccount", {
 
     this.__profilePage = this.__addProfilePage();
 
-    if (osparc.data.Permissions.getInstance().canDo("usage.all.read")) {
-      this.__usagePage = this.__addUsagePage();
+    // show Usage in My Account if wallets are not enabled. If they are enabled it will be in the BIlling Center
+    if (!osparc.desktop.credits.Utils.areWalletsEnabled()) {
+      if (osparc.data.Permissions.getInstance().canDo("usage.all.read")) {
+        this.__usagePage = this.__addUsagePage();
+      }
     }
   },
 
