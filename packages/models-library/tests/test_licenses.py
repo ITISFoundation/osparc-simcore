7407dd8e77b45efa78943cf6c3e9335b61416be4
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-02-06 10:29:40 +0100

    minor

--- packages/models-library/tests/test_licenses.py
+++ packages/models-library/tests/test_licenses.py
@@ -4,13 +4,17 @@ from models_library.licenses import LicensedItem
 
 def test_licensed_item_from_domain_model():
     for example in LicensedItem.model_json_schema()["examples"]:
-        example["licensed_resource_data"]["source"]["doi"] = None
-
         item = LicensedItem.model_validate(example)
+
         payload = LicensedItemRestGet.from_domain_model(item)
 
         assert item.display_name == payload.display_name
+
+        # nullable doi
         assert (
-            item.licensed_resource_data["source"]["doi"]
-            == payload.licensed_resource_data.source.doi
+            payload.licensed_resource_data.source.doi
+            == item.licensed_resource_data["source"]["doi"]
         )
+
+        # date is required
+        assert payload.licensed_resource_data.source.features["date"]
