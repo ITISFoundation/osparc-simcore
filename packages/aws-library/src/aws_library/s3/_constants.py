a7e0867d6e35a7de673b4c5616df9229bf4cbfd3
Andrei Neagu <neagu@itis.swiss>
2025-02-07 18:43:43 +0100

    fixed more broken imports

--- packages/aws-library/src/aws_library/s3/_constants.py
+++ packages/aws-library/src/aws_library/s3/_constants.py
@@ -3,12 +3,12 @@ from typing import Final
 from pydantic import ByteSize, TypeAdapter
 
 # NOTE: AWS S3 upload limits https://docs.aws.amazon.com/AmazonS3/latest/userguide/qfacts.html
-MIN_MULTIPART_UPLOAD_CHUNK_SIZE: Final[int] = TypeAdapter(ByteSize).validate_python(
-    "5MiB"
-)
 MULTIPART_UPLOADS_MIN_TOTAL_SIZE: Final[ByteSize] = TypeAdapter(
     ByteSize
 ).validate_python("100MiB")
+MULTIPART_COPY_THRESHOLD: Final[ByteSize] = TypeAdapter(ByteSize).validate_python(
+    "100MiB"
+)
 
 PRESIGNED_LINK_MAX_SIZE: Final[ByteSize] = TypeAdapter(ByteSize).validate_python("5GiB")
 S3_MAX_FILE_SIZE: Final[ByteSize] = TypeAdapter(ByteSize).validate_python("5TiB")
