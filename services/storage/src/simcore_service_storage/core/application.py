027f68baf3155ece36c665dadf48dc6e613d622b
Mads Bisgaard <bisgaard@itis.swiss>
2025-02-10 16:08:10 +0100

    add call to setup function

--- services/storage/src/simcore_service_storage/core/application.py
+++ services/storage/src/simcore_service_storage/core/application.py
@@ -31,6 +31,7 @@ from ..dsm_cleaner import setup_dsm_cleaner
 from ..exceptions.handlers import set_exception_handlers
 from ..modules.db import setup_db
 from ..modules.long_running_tasks import setup_rest_api_long_running_tasks_for_uploads
+from ..modules.rabbitmq import setup as setup_rabbitmq
 from ..modules.redis import setup as setup_redis
 from ..modules.s3 import setup_s3
 from .settings import ApplicationSettings
@@ -77,6 +78,7 @@ def create_app(settings: ApplicationSettings) -> FastAPI:
     setup_s3(app)
     setup_client_session(app)
 
+    setup_rabbitmq(app)
     setup_rest_api_long_running_tasks_for_uploads(app)
     setup_rest_api_routes(app, API_VTAG)
     set_exception_handlers(app)
