a82badc25110a1fee7febc54ce49a8894f651114
odeimaiz <maiz@itis.swiss>
2025-02-12 19:01:52 +0100

    refactor

--- services/static-webserver/client/source/class/osparc/desktop/MainPage.js
+++ services/static-webserver/client/source/class/osparc/desktop/MainPage.js
@@ -67,6 +67,7 @@ qx.Class.define("osparc.desktop.MainPage", {
     }
     preloadPromises.push(store.getAllClassifiers(true));
     preloadPromises.push(osparc.store.Tags.getInstance().fetchTags());
+    preloadPromises.push(osparc.store.Products.getInstance().fetchPlusButtonUiConfig());
     Promise.all(preloadPromises)
       .then(() => {
         const mainStack = this.__createMainStack();
--- services/static-webserver/client/source/class/osparc/store/Products.js
+++ services/static-webserver/client/source/class/osparc/store/Products.js
@@ -24,25 +24,20 @@ qx.Class.define("osparc.store.Products", {
 
     fetchPlusButtonUiConfig: function() {
       return osparc.utils.Utils.fetchJSON("/resource/osparc/ui_config.json")
-        .then(newStudiesData => {
+        .then(uiConfig => {
           const product = osparc.product.Utils.getProductName()
-          if (product in newStudiesData) {
-            this.__plusButtonUiConfig = newStudiesData[product];
-            return this.__plusButtonUiConfig;
+          if (product in uiConfig && "plusButton" in uiConfig[product]) {
+            this.__plusButtonUiConfig = uiConfig[product]["plusButton"];
+          } else {
+            this.__plusButtonUiConfig = false;
           }
-          return {};
+          return this.__plusButtonUiConfig;
         })
         .catch(console.error);
     },
 
     getPlusButtonUiConfig: function() {
-      return new Promise(resolve => {
-        if (this.__plusButtonUiConfig) {
-          resolve(this.__plusButtonUiConfig);
-        } else {
-          resolve(this.fetchPlusButtonUiConfig())
-        }
-      });
+      return this.__plusButtonUiConfig;
     },
   }
 });
