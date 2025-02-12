83c5e0d808e34495f2c9737e04dcfcc05ca8249f
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-02-12 16:54:41 +0100

    minor

--- packages/models-library/src/models_library/api_schemas_webserver/product.py
+++ packages/models-library/src/models_library/api_schemas_webserver/product.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import Annotated, Any, TypeAlias
 
+from common_library.basic_types import DEFAULT_FACTORY
 from pydantic import (
     ConfigDict,
     Field,
@@ -10,6 +11,7 @@ from pydantic import (
     PlainSerializer,
     PositiveInt,
 )
+from pydantic.config import JsonDict
 
 from ..basic_types import IDStr, NonNegativeDecimal
 from ..emails import LowerCaseEmailStr
@@ -27,32 +29,40 @@ class GetCreditPrice(OutputSchema):
         description="Price of a credit in USD. "
         "If None, then this product's price is UNDEFINED",
     )
-    min_payment_amount_usd: NonNegativeInt | None = Field(
-        ...,
-        description="Minimum amount (included) in USD that can be paid for this product"
-        "Can be None if this product's price is UNDEFINED",
-    )
+    min_payment_amount_usd: Annotated[
+        NonNegativeInt | None,
+        Field(
+            description="Minimum amount (included) in USD that can be paid for this product"
+            "Can be None if this product's price is UNDEFINED",
+        ),
+    ]
+
+    @staticmethod
+    def _update_json_schema_extra(schema: JsonDict) -> None:
+        schema.update(
+            {
+                "examples": [
+                    {
+                        "productName": "osparc",
+                        "usdPerCredit": None,
+                        "minPaymentAmountUsd": None,
+                    },
+                    {
+                        "productName": "osparc",
+                        "usdPerCredit": "10",
+                        "minPaymentAmountUsd": "10",
+                    },
+                ]
+            }
+        )
 
     model_config = ConfigDict(
-        json_schema_extra={
-            "examples": [
-                {
-                    "productName": "osparc",
-                    "usdPerCredit": None,
-                    "minPaymentAmountUsd": None,
-                },
-                {
-                    "productName": "osparc",
-                    "usdPerCredit": "10",
-                    "minPaymentAmountUsd": "10",
-                },
-            ]
-        }
+        json_schema_extra=_update_json_schema_extra,
     )
 
 
 class GetProductTemplate(OutputSchema):
-    id_: IDStr = Field(..., alias="id")
+    id_: Annotated[IDStr, Field(alias="id")]
     content: str
 
 
@@ -87,13 +97,14 @@ class ProductGet(OutputSchema):
             description="List of templates available to this product for communications (e.g. emails, sms, etc)",
             default_factory=list,
         ),
-    ]
+    ] = DEFAULT_FACTORY
 
 
 class ProductUIGet(OutputSchema):
     product_name: ProductName
     ui: Annotated[
-        dict[str, Any], Field(description="Front-end owned ui product configuration")
+        dict[str, Any],
+        Field(description="Front-end owned ui product configuration"),
     ]
 
 
@@ -115,26 +126,32 @@ class InvitationGenerated(OutputSchema):
     created: datetime
     invitation_link: HttpUrl
 
+    @staticmethod
+    def _update_json_schema_extra(schema: JsonDict) -> None:
+        schema.update(
+            {
+                "examples": [
+                    {
+                        "productName": "osparc",
+                        "issuer": "john.doe",
+                        "guest": "guest@example.com",
+                        "trialAccountDays": 7,
+                        "extraCreditsInUsd": 30,
+                        "created": "2023-09-27T15:30:00",
+                        "invitationLink": "https://example.com/invitation#1234",
+                    },
+                    # w/o optional
+                    {
+                        "productName": "osparc",
+                        "issuer": "john.doe@email.com",
+                        "guest": "guest@example.com",
+                        "created": "2023-09-27T15:30:00",
+                        "invitationLink": "https://example.com/invitation#1234",
+                    },
+                ]
+            }
+        )
+
     model_config = ConfigDict(
-        json_schema_extra={
-            "examples": [
-                {
-                    "productName": "osparc",
-                    "issuer": "john.doe",
-                    "guest": "guest@example.com",
-                    "trialAccountDays": 7,
-                    "extraCreditsInUsd": 30,
-                    "created": "2023-09-27T15:30:00",
-                    "invitationLink": "https://example.com/invitation#1234",
-                },
-                # w/o optional
-                {
-                    "productName": "osparc",
-                    "issuer": "john.doe@email.com",
-                    "guest": "guest@example.com",
-                    "created": "2023-09-27T15:30:00",
-                    "invitationLink": "https://example.com/invitation#1234",
-                },
-            ]
-        }
+        json_schema_extra=_update_json_schema_extra,
     )
--- services/web/server/src/simcore_service_webserver/products/_api.py
+++ services/web/server/src/simcore_service_webserver/products/_api.py
@@ -1,5 +1,4 @@
 from decimal import Decimal
-from itertools import product
 from pathlib import Path
 from typing import Any, cast
 
