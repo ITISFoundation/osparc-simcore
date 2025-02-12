14ddba8d9806cb7d40d9bf55ca39c74c428e4bf7
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-02-12 17:39:13 +0100

    adds new env

---
+++ services/web/server/tests/unit/isolated/test_licenses_settings.py
@@ -0,0 +1,32 @@
+# pylint: disable=protected-access
+# pylint: disable=redefined-outer-name
+# pylint: disable=too-many-arguments
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+
+
+import datetime
+
+import pytest
+from pytest_simcore.helpers.typing_env import EnvVarsDict
+from simcore_service_webserver.licenses.settings import LicensesSettings
+
+
+def test_itis_vip_syncer_settings(
+    mock_webserver_service_environment: EnvVarsDict, monkeypatch: pytest.MonkeyPatch
+):
+
+    assert "LICENSES_ITIS_VIP_SYNCER_ENABLED" in mock_webserver_service_environment
+    assert "LICENSES_ITIS_VIP_SYNCER_PERIODICITY" in mock_webserver_service_environment
+
+    settings = LicensesSettings.create_from_envs()
+    assert settings
+
+    with monkeypatch.context() as patch:
+        patch.setenv("LICENSES_ITIS_VIP_SYNCER_PERIODICITY", "1D00:00:00")
+
+        settings: LicensesSettings = LicensesSettings.create_from_envs()
+        assert settings
+        assert settings.LICENSES_ITIS_VIP_SYNCER_PERIODICITY == datetime.timedelta(
+            days=1
+        )
