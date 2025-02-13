6847d11e014519d6ac142f9f255de5490133b1eb
Mads Bisgaard <bisgaard@itis.swiss>
2025-02-13 07:15:42 +0100

    @pcrespov use fastapi lifespan

--- services/storage/src/simcore_service_storage/modules/rabbitmq.py
+++ services/storage/src/simcore_service_storage/modules/rabbitmq.py
@@ -1,4 +1,5 @@
 import logging
+from contextlib import asynccontextmanager
 from typing import cast
 
 from fastapi import FastAPI
@@ -15,40 +16,38 @@ from ..exceptions.errors import ConfigurationError
 _logger = logging.getLogger(__name__)
 
 
-def setup(app: FastAPI) -> None:
-    async def on_startup() -> None:
-        with log_context(
-            _logger,
-            logging.INFO,
-            msg="Storage startup Rabbitmq",
-        ):
-            app.state.rabbitmq_client = None
-            rabbit_settings: RabbitSettings | None = app.state.settings.STORAGE_RABBITMQ
-            if not rabbit_settings:
-                raise ConfigurationError(
-                    msg="RabbitMQ client is de-activated in the settings"
-                )
-            await wait_till_rabbitmq_responsive(rabbit_settings.dsn)
-            app.state.rabbitmq_client = RabbitMQClient(
-                client_name="storage", settings=rabbit_settings
+@asynccontextmanager
+async def rabbitmq_lifespan(app: FastAPI):
+    with log_context(
+        _logger,
+        logging.INFO,
+        msg="Storage startup Rabbitmq",
+    ):
+        app.state.rabbitmq_client = None
+        rabbit_settings: RabbitSettings | None = app.state.settings.STORAGE_RABBITMQ
+        if not rabbit_settings:
+            raise ConfigurationError(
+                msg="RabbitMQ client is de-activated in the settings"
             )
-            app.state.rabbitmq_rpc_server = await RabbitMQRPCClient.create(
-                client_name="storage_rpc_server", settings=rabbit_settings
-            )
+        await wait_till_rabbitmq_responsive(rabbit_settings.dsn)
+        app.state.rabbitmq_client = RabbitMQClient(
+            client_name="storage", settings=rabbit_settings
+        )
+        app.state.rabbitmq_rpc_server = await RabbitMQRPCClient.create(
+            client_name="storage_rpc_server", settings=rabbit_settings
+        )
 
-    async def on_shutdown() -> None:
-        with log_context(
-            _logger,
-            logging.INFO,
-            msg="Storage shutdown Rabbitmq",
-        ):
-            if app.state.rabbitmq_client:
-                await app.state.rabbitmq_client.close()
-            if app.state.rabbitmq_rpc_server:
-                await app.state.rabbitmq_rpc_server.close()
+    yield
 
-    app.add_event_handler("startup", on_startup)
-    app.add_event_handler("shutdown", on_shutdown)
+    with log_context(
+        _logger,
+        logging.INFO,
+        msg="Storage shutdown Rabbitmq",
+    ):
+        if app.state.rabbitmq_client:
+            await app.state.rabbitmq_client.close()
+        if app.state.rabbitmq_rpc_server:
+            await app.state.rabbitmq_rpc_server.close()
 
 
 def get_rabbitmq_client(app: FastAPI) -> RabbitMQClient:
