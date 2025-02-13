a0d47c80ac4102f16e841202ac9d5e7fd30fe70e
odeimaiz <maiz@itis.swiss>
2025-02-13 13:44:08 +0100

    validate

--- services/static-webserver/client/source/class/osparc/store/Products.js
+++ services/static-webserver/client/source/class/osparc/store/Products.js
@@ -17,6 +17,10 @@
 
 /**
  * @asset(osparc/ui_config.json")
+ * @asset(form/product-ui.json)
+ * @asset(object-path/object-path-0-11-4.min.js)
+ * @asset(ajv/ajv-6-11-0.min.js)
+ * @ignore(Ajv)
  */
 
 qx.Class.define("osparc.store.Products", {
@@ -27,31 +31,48 @@ qx.Class.define("osparc.store.Products", {
     __uiConfig: null,
 
     fetchUiConfig: function() {
-      if (osparc.auth.Data.getInstance().isGuest()) {
-        return new Promise(resolve => {
+      return new Promise(resolve => {
+        if (osparc.auth.Data.getInstance().isGuest()) {
           this.__uiConfig = {};
           resolve(this.__uiConfig);
-        });
-      }
+        }
 
-      return Promise.all([
-        osparc.data.Resources.fetch("productMetadata", "getUiConfig"),
-        osparc.utils.Utils.fetchJSON("/resource/osparc/ui_config.json"),
-      ])
-        .then(values => {
-          let uiConfig = {};
-          if (values[0] && values[0]["ui"] && Object.keys(values[0]["ui"]).length) {
-            uiConfig = values[0]["ui"];
-          } else {
-            const product = osparc.product.Utils.getProductName();
-            if (values[1] && product in values[1]) {
-              uiConfig = values[1][product];
+        Promise.all([
+          osparc.data.Resources.fetch("productMetadata", "getUiConfig"),
+          osparc.utils.Utils.fetchJSON("/resource/osparc/ui_config.json"),
+          osparc.utils.Utils.fetchJSON("/resource/form/product-ui.json"),
+        ])
+          .then(values => {
+            let uiConfig = {};
+            if (values[0] && values[0]["ui"] && Object.keys(values[0]["ui"]).length) {
+              uiConfig = values[0]["ui"];
+            } else {
+              const product = osparc.product.Utils.getProductName();
+              if (values[1] && product in values[1]) {
+                uiConfig = values[1][product];
+              }
             }
-          }
-          this.__uiConfig = uiConfig;
-          return this.__uiConfig;
-        })
-        .catch(console.error);
+            const schema = values[2];
+            const ajvLoader = new qx.util.DynamicScriptLoader([
+              "/resource/ajv/ajv-6-11-0.min.js",
+              "/resource/object-path/object-path-0-11-4.min.js"
+            ]);
+            ajvLoader.addListener("ready", () => {
+              this.__ajv = new Ajv();
+              if (this.__validate(schema.$schema, schema)) {
+                // Schema is valid
+                if (this.__validate(schema, uiConfig)) {
+                  // Validate data if present
+                  this.__uiConfig = uiConfig;
+                }
+                resolve(this.__uiConfig);
+              } else {
+                console.error("wrong ui_config")
+              }
+            });
+          })
+          .catch(console.error);
+      });
     },
 
     getPlusButtonUiConfig: function() {
