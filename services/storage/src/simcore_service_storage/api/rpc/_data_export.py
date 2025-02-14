156a19a03af104d06e8d62675b3920ad1bffcc4d
Mads Bisgaard <bisgaard@itis.swiss>
2025-02-14 14:26:03 +0100

    factor out tasks part in storage rpc api

--- services/storage/src/simcore_service_storage/api/rpc/_data_export.py
+++ services/storage/src/simcore_service_storage/api/rpc/_data_export.py
@@ -1,15 +1,8 @@
-from datetime import datetime
 from uuid import uuid4
 
 from fastapi import FastAPI
-from models_library.api_schemas_rpc_data_export.tasks import (
-    TaskRpcGet,
-    TaskRpcId,
-    TaskRpcResult,
-    TaskRpcStatus,
-)
+from models_library.api_schemas_rpc_data_export.tasks import TaskRpcGet
 from models_library.api_schemas_storage.data_export_tasks import (
-    DataExportTaskAbortOutput,
     DataExportTaskStartInput,
 )
 from servicelib.rabbitmq import RPCRouter
@@ -26,30 +19,3 @@ async def start_data_export(
         task_id=uuid4(),
         task_name=", ".join(str(p) for p in paths.paths),
     )
-
-
-@router.expose()
-async def abort_data_export(
-    app: FastAPI, task_id: TaskRpcId
-) -> DataExportTaskAbortOutput:
-    assert app  # nosec
-    return DataExportTaskAbortOutput(result=True, task_id=task_id)
-
-
-@router.expose()
-async def get_data_export_status(app: FastAPI, task_id: TaskRpcId) -> TaskRpcStatus:
-    assert app  # nosec
-    return TaskRpcStatus(
-        task_id=task_id,
-        task_progress=0.5,
-        done=False,
-        started=datetime.now(),
-        stopped=None,
-    )
-
-
-@router.expose()
-async def get_data_export_result(app: FastAPI, task_id: TaskRpcId) -> TaskRpcResult:
-    assert app  # nosec
-    assert task_id  # nosec
-    return TaskRpcResult(result="Here's your result.", error=None)
--- services/storage/src/simcore_service_storage/api/rpc/routes.py
+++ services/storage/src/simcore_service_storage/api/rpc/routes.py
@@ -6,12 +6,12 @@ from servicelib.logging_utils import log_context
 from servicelib.rabbitmq import RPCRouter
 
 from ...modules.rabbitmq import get_rabbitmq_rpc_server
-from . import _data_export
+from . import _data_export, _tasks
 
 _logger = logging.getLogger(__name__)
 
 
-ROUTERS: list[RPCRouter] = [_data_export.router]
+ROUTERS: list[RPCRouter] = [_data_export.router, _tasks.router]
 
 
 def setup_rpc_api_routes(app: FastAPI) -> None:
