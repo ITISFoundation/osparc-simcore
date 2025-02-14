d9a40edab6dd77e3c826a99648de457c0bd3590c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-02-14 14:51:09 +0100

    return PathMetaDataGet

--- services/storage/src/simcore_service_storage/api/rest/_paths.py
+++ services/storage/src/simcore_service_storage/api/rest/_paths.py
@@ -4,7 +4,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends
 from fastapi_pagination import LimitOffsetPage, LimitOffsetParams, create_page
-from models_library.api_schemas_storage import FileMetaDataGet
+from models_library.api_schemas_storage import PathMetaDataGet
 from models_library.users import UserID
 
 from ...dsm_factory import BaseDataManager
@@ -21,7 +21,7 @@ router = APIRouter(
 
 @router.get(
     "/locations/{location_id}/paths",
-    response_model=LimitOffsetPage[FileMetaDataGet],
+    response_model=LimitOffsetPage[PathMetaDataGet],
 )
 async def list_paths(
     page_params: Annotated[LimitOffsetParams, Depends()],
@@ -30,7 +30,7 @@ async def list_paths(
     file_filter: Path | None = None,
 ):
     """Returns one level of files (paginated)"""
-    items, total_number = await dsm.list_files_paginated(
+    items, total_number = await dsm.list_paths(
         user_id=user_id,
         file_filter=file_filter,
         limit=page_params.limit,
