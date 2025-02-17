e8684ce95d856b05fdeaa6ff6c3006b0a67033f5
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-02-17 14:13:02 +0100

    exposes apis

---
+++ services/web/server/src/simcore_service_webserver/projects/projects_trash_service.py
@@ -0,0 +1,8 @@
+from ._trash_service import delete_trashed_project, list_trashed_projects
+
+__all__: tuple[str, ...] = (
+    "delete_trashed_project",
+    "list_trashed_projects",
+)
+
+# nopycln: file
