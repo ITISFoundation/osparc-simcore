b67392a63185fbde40c7f442acfa7e5ea311c916
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-02-18 00:27:00 +0100

    fixes wrong import

--- services/web/server/src/simcore_service_webserver/projects/projects_service.py
+++ services/web/server/src/simcore_service_webserver/projects/projects_service.py
@@ -2,6 +2,7 @@ from ._projects_service import (
     create_user_notification_cb,
     get_project_for_user,
     is_node_id_present_in_any_project_workbench,
+    list_node_ids_in_project,
     notify_project_node_update,
     notify_project_state_update,
     remove_project_dynamic_services,
@@ -12,13 +13,13 @@ from ._projects_service import (
 __all__: tuple[str, ...] = (
     "create_user_notification_cb",
     "get_project_for_user",
-    "notify_project_node_update",
-    "notify_project_state_update",
-    "submit_delete_project_task",
-    "update_project_node_state",
-    "remove_project_dynamic_services",
     "is_node_id_present_in_any_project_workbench",
     "list_node_ids_in_project",
+    "notify_project_node_update",
+    "notify_project_state_update",
+    "remove_project_dynamic_services",
+    "submit_delete_project_task",
+    "update_project_node_state",
 )
 
 # nopycln: file
