b5c1229cfd06ddcb43299933f8dce8e7515a779e
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-02-17 10:44:24 +0100

    nodes

--- services/web/server/src/simcore_service_webserver/projects/_common/exception_handlers.py
+++ services/web/server/src/simcore_service_webserver/projects/_common/exception_handlers.py
@@ -1,6 +1,7 @@
 import logging
 
 from servicelib.aiohttp import status
+from servicelib.rabbitmq.rpc_interfaces.catalog.errors import CatalogForbiddenError
 
 from ...exception_handling import (
     ExceptionToHttpErrorMap,
@@ -9,13 +10,24 @@ from ...exception_handling import (
     to_exceptions_handlers_map,
 )
 from ...folders.errors import FolderAccessForbiddenError, FolderNotFoundError
+from ...resource_usage.errors import DefaultPricingPlanNotFoundError
+from ...users.exceptions import UserDefaultWalletNotFoundError
+from ...wallets.errors import WalletAccessForbiddenError, WalletNotEnoughCreditsError
 from ...workspaces.errors import WorkspaceAccessForbiddenError, WorkspaceNotFoundError
 from ..exceptions import (
+    ClustersKeeperNotAvailableError,
+    DefaultPricingUnitNotFoundError,
+    NodeNotFoundError,
+    ParentNodeNotFoundError,
     ProjectDeleteError,
     ProjectGroupNotFoundError,
+    ProjectInDebtCanNotChangeWalletError,
     ProjectInvalidRightsError,
+    ProjectInvalidUsageError,
+    ProjectNodeRequiredInputsNotSetError,
     ProjectNotFoundError,
     ProjectOwnerNotFoundInTheProjectAccessRightsError,
+    ProjectStartsTooManyDynamicNodesError,
     WrongTagIdsInQueryError,
 )
 
@@ -33,6 +45,14 @@ _TO_HTTP_ERROR_MAP: ExceptionToHttpErrorMap = {
         status.HTTP_404_NOT_FOUND,
         "Folder not found: {reason}",
     ),
+    NodeNotFoundError: HttpErrorInfo(
+        status.HTTP_404_NOT_FOUND,
+        "Node '{node_uuid}' not found in project '{project_uuid}'",
+    ),
+    ParentNodeNotFoundError: HttpErrorInfo(
+        status.HTTP_404_NOT_FOUND,
+        "Parent node '{node_uuid}' not found",
+    ),
     ProjectDeleteError: HttpErrorInfo(
         status.HTTP_409_CONFLICT,
         "Failed to complete deletion of '{project_uuid}': {reason}",
@@ -44,6 +64,10 @@ _TO_HTTP_ERROR_MAP: ExceptionToHttpErrorMap = {
     ProjectInvalidRightsError: HttpErrorInfo(
         status.HTTP_403_FORBIDDEN,
         "Do not have sufficient access rights on project {project_uuid} for this action",
+    ),
+    ProjectInvalidUsageError: HttpErrorInfo(
+        status.HTTP_422_UNPROCESSABLE_ENTITY,
+        "Invalid usage for project",
     ),
     ProjectNotFoundError: HttpErrorInfo(
         status.HTTP_404_NOT_FOUND,
@@ -65,8 +89,47 @@ _TO_HTTP_ERROR_MAP: ExceptionToHttpErrorMap = {
         status.HTTP_400_BAD_REQUEST,
         "Wrong tag IDs in query",
     ),
+    UserDefaultWalletNotFoundError: HttpErrorInfo(
+        status.HTTP_404_NOT_FOUND,
+        "User default wallet not found",
+    ),
+    DefaultPricingPlanNotFoundError: HttpErrorInfo(
+        status.HTTP_404_NOT_FOUND,
+        "Default pricing plan not found",
+    ),
+    DefaultPricingUnitNotFoundError: HttpErrorInfo(
+        status.HTTP_404_NOT_FOUND,
+        "Default pricing unit not found",
+    ),
+    WalletNotEnoughCreditsError: HttpErrorInfo(
+        status.HTTP_402_PAYMENT_REQUIRED,
+        "Wallet does not have enough credits. {reason}",
+    ),
+    ProjectInDebtCanNotChangeWalletError: HttpErrorInfo(
+        status.HTTP_402_PAYMENT_REQUIRED,
+        "Unable to change the credit account linked to the project. The project is embargoed because the last transaction of {debt_amount} resulted in the credit account going negative.",
+    ),
+    ProjectStartsTooManyDynamicNodesError: HttpErrorInfo(
+        status.HTTP_409_CONFLICT,
+        "The maximal amount of concurrently running dynamic services was reached. Please manually stop a service and retry.",
+    ),
+    ClustersKeeperNotAvailableError: HttpErrorInfo(
+        status.HTTP_503_SERVICE_UNAVAILABLE,
+        "Clusters-keeper service is not available",
+    ),
+    ProjectNodeRequiredInputsNotSetError: HttpErrorInfo(
+        status.HTTP_409_CONFLICT,
+        "Project node is required but input is not set",
+    ),
+    CatalogForbiddenError: HttpErrorInfo(
+        status.HTTP_403_FORBIDDEN,
+        "Catalog forbidden: Insufficient access rights for {name}",
+    ),
+    WalletAccessForbiddenError: HttpErrorInfo(
+        status.HTTP_403_FORBIDDEN,
+        "Payment required, but the user lacks access to the project's linked wallet: Wallet access forbidden. {reason}",
+    ),
 }
-
 handle_plugin_requests_exceptions = exception_handling_decorator(
     to_exceptions_handlers_map(_TO_HTTP_ERROR_MAP)
 )
