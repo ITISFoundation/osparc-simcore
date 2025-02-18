67f7c5db280a59e4f433c17ac93f7dd84f9f84d1
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-02-18 18:44:51 +0100

    tests issue

--- services/web/server/tests/unit/with_dbs/04/licenses/test_itis_vip_models.py
+++ services/web/server/tests/unit/with_dbs/04/licenses/test_itis_vip_models.py
@@ -5,6 +5,9 @@
 # pylint: disable=unused-variable
 
 
+import datetime
+from typing import Any
+
 import pytest
 from faker import Faker
 from pydantic import ValidationError
@@ -25,23 +28,45 @@ def test_pre_validator_feature_descriptor_to_dict():
     assert err_info.value.errors()[0]["type"] == "string_too_long"
 
 
-def test_validation_of_itis_vip_response_model(faker: Faker):
+@pytest.mark.parametrize(
+    "features_str,expected",
+    [
+        (
+            # checks fix: regex expected at least one space after `:`
+            "{species:Mouse, functionality:Static, height:95 mm, date: 2012-01-01, name:Male OF1 Mouse, sex:Male, version:1.0, weight:35.5 g}",
+            {"version": "1.0", "weight": "35.5 g"},
+        ),
+        (
+            # Checks spaces before `,` are removed
+            "{date: 2012-01-01, name:  Male OF1 Mouse    ,}",
+            {"date": datetime.date(2012, 1, 1), "name": "Male OF1 Mouse"},
+        ),
+    ],
+)
+def test_validation_of_itis_vip_response_model(
+    faker: Faker, features_str: str, expected: dict[str, Any]
+):
 
     available_download = random_itis_vip_available_download_item(
         identifier=0,
-        features_functionality="Posable",
         fake=faker,
+        Features=features_str,
     )
 
     vip_data = ItisVipData.model_validate(available_download)
 
+    # Checks how features BeforeValidator and parser
+    assert {k: vip_data.features[k] for k in expected} == expected
+
     # Dumped as in the source
     assert vip_data.model_dump(by_alias=True)["Features"] == vip_data.features
 
-    license_resource_data = ItisVipResourceData(
-        category_id="123",
-        category_display="This is a resource",
-        source=vip_data,
+    license_resource_data = ItisVipResourceData.model_validate(
+        {
+            "category_id": "SomeCategoryID",
+            "category_display": "This is a resource",
+            "source": vip_data,
+        }
     )
 
     assert license_resource_data.source.features == vip_data.features
