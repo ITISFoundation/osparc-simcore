4b08136eabee95e4e34b3126a67ea75d7918f343
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-02-20 14:51:20 +0100

    add progress

--- services/storage/src/simcore_service_storage/modules/celery/client/_interface.py
+++ services/storage/src/simcore_service_storage/modules/celery/client/_interface.py
@@ -1,33 +1,79 @@
-from typing import Any
+from typing import Any, Final, TypeAlias
 from uuid import uuid4
 
 from celery import Celery
 from celery.result import AsyncResult
-from models_library.users import UserID
+from models_library.progress_bar import ProgressReport
+from pydantic import ValidationError
 
-from ..models import TaskID
+from ..models import TaskID, TaskProgress
+
+_PREFIX: Final = "AJ"
+
+TaskIdComponents: TypeAlias = dict[str, Any]
+
+
+def _get_task_id_components(task_id_components: TaskIdComponents) -> list[str]:
+    return [f"{v}" for _, v in sorted(task_id_components.items())]
+
+
+def _get_components_prefix(
+    name: str, task_id_components: TaskIdComponents
+) -> list[str]:
+    return [_PREFIX, name, *_get_task_id_components(task_id_components)]
+
+
+def _get_task_id_prefix(name: str, task_id_components: TaskIdComponents) -> TaskID:
+    return "::".join(_get_components_prefix(name, task_id_components))
+
+
+def _get_task_id(name: str, task_id_components: TaskIdComponents) -> TaskID:
+    return "::".join([*_get_components_prefix(name, task_id_components), f"{uuid4()}"])
 
 
 class CeleryClientInterface:
     def __init__(self, celery_app: Celery):
         self._celery_app = celery_app
 
-    def submit(self, name: str, *, user_id: UserID, **kwargs) -> TaskID:
-        task_id = f"{user_id}_{name}_{uuid4()}"
-        return self._celery_app.send_task(name, task_id=task_id, kwargs=kwargs).id
+    def submit(
+        self, task_name: str, *, task_id_components: TaskIdComponents, **task_params
+    ) -> TaskID:
+        task_id = _get_task_id(task_name, task_id_components)
+        task = self._celery_app.send_task(
+            task_name, task_id=task_id, kwargs=task_params
+        )
+        return task.id
 
-    def _get_result(self, task_id: TaskID) -> AsyncResult:
-        return self._celery_app.AsyncResult(task_id)
-
-    def get_state(self, task_id: TaskID) -> str:
-        # task_id , state, progress
-        return self._get_result(task_id).state
-
-    def get_result(self, task_id: TaskID) -> Any:
-        return self._get_result(task_id).result
+    def get(self, task_id: TaskID) -> Any:
+        return self._celery_app.tasks(task_id)
 
     def cancel(self, task_id: TaskID) -> None:
         self._celery_app.control.revoke(task_id, terminate=True)
 
-    def list(self, user_id: UserID) -> list[TaskID]:
+    def _get_async_result(self, task_id: TaskID) -> AsyncResult:
+        return self._celery_app.AsyncResult(task_id)
+
+    def get_result(self, task_id: TaskID) -> Any:
+        # se manca il risultato o se va in FAILURE, ritorna error
+        return self._get_async_result(task_id).result
+
+    def _get_progress_report(self, task_id: TaskID) -> ProgressReport | None:
+        result = self._get_async_result(task_id).result
+        if result:
+            try:
+                return ProgressReport.model_validate(result)
+            except ValidationError:
+                return None
+
+    def get_progress(self, task_id: TaskID) -> TaskProgress:
+        return TaskProgress(
+            task_id=task_id,
+            task_state=self._get_async_result(task_id).state,
+            progress_report=self._get_progress_report(task_id),
+        )
+
+    def list(
+        self, task_name: str, *, task_id_components: TaskIdComponents
+    ) -> list[TaskID]:
+        prefix_to_search_in_redis = _get_task_id_prefix(task_name, task_id_components)
         return []
