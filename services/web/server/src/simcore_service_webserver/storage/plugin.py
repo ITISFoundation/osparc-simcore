f7c8b5358dd6f9083a5fa88c0a71febab8879691
Mads Bisgaard <126242332+bisgaard-itis@users.noreply.github.com>
2025-02-21 09:54:20 +0100

    Propagate data export endpoints to webserver (#7244)

--- services/web/server/src/simcore_service_webserver/storage/plugin.py
+++ services/web/server/src/simcore_service_webserver/storage/plugin.py
@@ -9,7 +9,7 @@ from servicelib.aiohttp.application_setup import ModuleCategory, app_module_setu
 
 from .._constants import APP_SETTINGS_KEY
 from ..rest.plugin import setup_rest
-from . import _handlers
+from . import _rest
 
 _logger = logging.getLogger(__name__)
 
@@ -21,4 +21,4 @@ def setup_storage(app: web.Application):
     assert app[APP_SETTINGS_KEY].WEBSERVER_STORAGE  # nosec
 
     setup_rest(app)
-    app.router.add_routes(_handlers.routes)
+    app.router.add_routes(_rest.routes)
