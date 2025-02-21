3d7ae6bb167efcba70f01af17170e030200a5555
Mads Bisgaard <bisgaard@itis.swiss>
2025-02-21 15:02:53 +0100

    start implementation

--- services/storage/src/simcore_service_storage/api/rpc/_data_export.py
+++ services/storage/src/simcore_service_storage/api/rpc/_data_export.py
@@ -10,11 +10,9 @@ from models_library.api_schemas_storage.data_export_async_jobs import (
     InvalidFileIdentifierError,
 )
 from servicelib.rabbitmq import RPCRouter
-from simcore_service_storage.dsm import (
-    DatCoreDataManager,
-    SimcoreS3DataManager,
-    get_dsm_provider,
-)
+from simcore_service_storage.dsm import SimcoreS3DataManager, get_dsm_provider
+
+from ...modules.db.access_layer import get_file_access_rights
 
 router = RPCRouter()
 
@@ -31,12 +29,18 @@ async def start_data_export(
 ) -> AsyncJobGet:
     assert app  # nosec
 
-    if paths.location_id != DatCoreDataManager.get_location_id():
+    if paths.location_id != SimcoreS3DataManager.get_location_id():
         dsm = cast(
             SimcoreS3DataManager,
             get_dsm_provider(app).get(SimcoreS3DataManager.get_location_id()),
         )
+        async with dsm.engine.connect() as conn:
+            for _id in paths.file_and_folder_ids:
+                acces_right = await get_file_access_rights(conn, paths.user_id, _id)
+                if not acces_right.read:
+                    raise AccessRightError(user_id=paths.user_id, file_id=_id)
+
     return AsyncJobGet(
         job_id=AsyncJobId(f"{uuid4()}"),
-        job_name=", ".join(str(p) for p in paths.paths),
+        job_name=", ".join(str(p) for p in paths.file_and_folder_ids),
     )
