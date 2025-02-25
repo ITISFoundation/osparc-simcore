ceb8d201b1d3442756d9350fa151c621068759bd
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-02-25 17:43:01 +0100

    @pcrespov review: json_schema_extra

--- packages/models-library/src/models_library/api_schemas_storage/storage_schemas.py
+++ packages/models-library/src/models_library/api_schemas_storage/storage_schemas.py
@@ -23,6 +23,7 @@ from pydantic import (
     field_validator,
     model_validator,
 )
+from pydantic.config import JsonDict
 from pydantic.networks import AnyUrl
 
 from ..basic_regex import (
@@ -71,14 +72,20 @@ class FileLocation(BaseModel):
     name: LocationName
     id: LocationID
 
+    @staticmethod
+    def _update_json_schema_extra(schema: JsonDict) -> None:
+        schema.update(
+            {
+                "examples": [
+                    {"name": "simcore.s3", "id": 0},
+                    {"name": "datcore", "id": 1},
+                ]
+            }
+        )
+
     model_config = ConfigDict(
         extra="forbid",
-        json_schema_extra={
-            "examples": [
-                {"name": "simcore.s3", "id": 0},
-                {"name": "datcore", "id": 1},
-            ]
-        },
+        json_schema_extra=_update_json_schema_extra,
     )
 
 
@@ -89,35 +96,42 @@ FileLocationArray: TypeAlias = ListModel[FileLocation]
 class DatasetMetaDataGet(BaseModel):
     dataset_id: UUID | DatCoreDatasetName
     display_name: str
+
+    @staticmethod
+    def _update_json_schema_extra(schema: JsonDict) -> None:
+        schema.update(
+            {
+                "examples": [
+                    # simcore dataset
+                    {
+                        "dataset_id": "74a84992-8c99-47de-b88a-311c068055ea",
+                        "display_name": "api",
+                    },
+                    {
+                        "dataset_id": "1c46752c-b096-11ea-a3c4-02420a00392e",
+                        "display_name": "Octave JupyterLab",
+                    },
+                    {
+                        "dataset_id": "2de04d1a-f346-11ea-9c22-02420a00085a",
+                        "display_name": "Sleepers",
+                    },
+                    # datcore datasets
+                    {
+                        "dataset_id": "N:dataset:be862eb8-861e-4b36-afc3-997329dd02bf",
+                        "display_name": "simcore-testing-bucket",
+                    },
+                    {
+                        "dataset_id": "N:dataset:9ad8adb0-8ea2-4be6-bc45-ecbec7546393",
+                        "display_name": "YetAnotherTest",
+                    },
+                ]
+            }
+        )
+
     model_config = ConfigDict(
         extra="forbid",
         from_attributes=True,
-        json_schema_extra={
-            "examples": [
-                # simcore dataset
-                {
-                    "dataset_id": "74a84992-8c99-47de-b88a-311c068055ea",
-                    "display_name": "api",
-                },
-                {
-                    "dataset_id": "1c46752c-b096-11ea-a3c4-02420a00392e",
-                    "display_name": "Octave JupyterLab",
-                },
-                {
-                    "dataset_id": "2de04d1a-f346-11ea-9c22-02420a00085a",
-                    "display_name": "Sleepers",
-                },
-                # datcore datasets
-                {
-                    "dataset_id": "N:dataset:be862eb8-861e-4b36-afc3-997329dd02bf",
-                    "display_name": "simcore-testing-bucket",
-                },
-                {
-                    "dataset_id": "N:dataset:9ad8adb0-8ea2-4be6-bc45-ecbec7546393",
-                    "display_name": "YetAnotherTest",
-                },
-            ]
-        },
+        json_schema_extra=_update_json_schema_extra,
     )
 
 
@@ -181,84 +195,90 @@ class FileMetaDataGet(BaseModel):
         description="SHA256 message digest of the file content. Main purpose: cheap lookup.",
     )
 
+    @staticmethod
+    def _update_json_schema_extra(schema: JsonDict) -> None:
+        schema.update(
+            {
+                "examples": [
+                    # typical S3 entry
+                    {
+                        "created_at": "2020-06-17 12:28:55.705340",
+                        "entity_tag": "8711cf258714b2de5498f5a5ef48cc7b",
+                        "file_id": "1c46752c-b096-11ea-a3c4-02420a00392e/e603724d-4af1-52a1-b866-0d4b792f8c4a/work.zip",
+                        "file_name": "work.zip",
+                        "file_size": 17866343,
+                        "file_uuid": "1c46752c-b096-11ea-a3c4-02420a00392e/e603724d-4af1-52a1-b866-0d4b792f8c4a/work.zip",
+                        "is_soft_link": False,
+                        "last_modified": "2020-06-22 13:48:13.398000+00:00",
+                        "location_id": 0,
+                        "node_name": "JupyterLab Octave",
+                        "project_name": "Octave JupyterLab",
+                    },
+                    # typical directory entry
+                    {
+                        "created_at": "2020-06-17 12:28:55.705340",
+                        "entity_tag": "8711cf258714b2de5498f5a5ef48cc7b",
+                        "file_id": "9a759caa-9890-4537-8c26-8edefb7a4d7c/be165f45-ddbf-4911-a04d-bc0b885914ef/workspace",
+                        "file_name": "workspace",
+                        "file_size": -1,
+                        "file_uuid": "9a759caa-9890-4537-8c26-8edefb7a4d7c/be165f45-ddbf-4911-a04d-bc0b885914ef/workspace",
+                        "is_soft_link": False,
+                        "last_modified": "2020-06-22 13:48:13.398000+00:00",
+                        "location_id": 0,
+                        "node_name": None,
+                        "project_name": None,
+                        "is_directory": True,
+                    },
+                    # api entry (not soft link)
+                    {
+                        "created_at": "2020-06-17 12:28:55.705340",
+                        "entity_tag": "8711cf258714b2de5498f5a5ef48cc7b",
+                        "file_id": "api/7b6b4e3d-39ae-3559-8765-4f815a49984e/tmpf_qatpzx",
+                        "file_name": "tmpf_qatpzx",
+                        "file_size": 86,
+                        "file_uuid": "api/7b6b4e3d-39ae-3559-8765-4f815a49984e/tmpf_qatpzx",
+                        "is_soft_link": False,
+                        "last_modified": "2020-06-22 13:48:13.398000+00:00",
+                        "location_id": 0,
+                        "node_name": None,
+                        "project_name": None,
+                    },
+                    # api entry (soft link)
+                    {
+                        "created_at": "2020-06-17 12:28:55.705340",
+                        "entity_tag": "36aa3644f526655a6f557207e4fd25b8",
+                        "file_id": "api/6f788ad9-0ad8-3d0d-9722-72f08c24a212/output_data.json",
+                        "file_name": "output_data.json",
+                        "file_size": 183,
+                        "file_uuid": "api/6f788ad9-0ad8-3d0d-9722-72f08c24a212/output_data.json",
+                        "is_soft_link": True,
+                        "last_modified": "2020-06-22 13:48:13.398000+00:00",
+                        "location_id": 0,
+                        "node_name": None,
+                        "project_name": None,
+                    },
+                    # datcore entry
+                    {
+                        "created_at": "2020-05-28T15:48:34.386302+00:00",
+                        "entity_tag": None,
+                        "file_id": "N:package:ce145b61-7e4f-470b-a113-033653e86d3d",
+                        "file_name": "templatetemplate.json",
+                        "file_size": 238,
+                        "file_uuid": "Kember Cardiac Nerve Model/templatetemplate.json",
+                        "is_soft_link": False,
+                        "last_modified": "2020-05-28T15:48:37.507387+00:00",
+                        "location_id": 1,
+                        "node_name": None,
+                        "project_name": None,
+                    },
+                ]
+            }
+        )
+
     model_config = ConfigDict(
         extra="ignore",
         from_attributes=True,
-        json_schema_extra={
-            "examples": [
-                # typical S3 entry
-                {
-                    "created_at": "2020-06-17 12:28:55.705340",
-                    "entity_tag": "8711cf258714b2de5498f5a5ef48cc7b",
-                    "file_id": "1c46752c-b096-11ea-a3c4-02420a00392e/e603724d-4af1-52a1-b866-0d4b792f8c4a/work.zip",
-                    "file_name": "work.zip",
-                    "file_size": 17866343,
-                    "file_uuid": "1c46752c-b096-11ea-a3c4-02420a00392e/e603724d-4af1-52a1-b866-0d4b792f8c4a/work.zip",
-                    "is_soft_link": False,
-                    "last_modified": "2020-06-22 13:48:13.398000+00:00",
-                    "location_id": 0,
-                    "node_name": "JupyterLab Octave",
-                    "project_name": "Octave JupyterLab",
-                },
-                # typical directory entry
-                {
-                    "created_at": "2020-06-17 12:28:55.705340",
-                    "entity_tag": "8711cf258714b2de5498f5a5ef48cc7b",
-                    "file_id": "9a759caa-9890-4537-8c26-8edefb7a4d7c/be165f45-ddbf-4911-a04d-bc0b885914ef/workspace",
-                    "file_name": "workspace",
-                    "file_size": -1,
-                    "file_uuid": "9a759caa-9890-4537-8c26-8edefb7a4d7c/be165f45-ddbf-4911-a04d-bc0b885914ef/workspace",
-                    "is_soft_link": False,
-                    "last_modified": "2020-06-22 13:48:13.398000+00:00",
-                    "location_id": 0,
-                    "node_name": None,
-                    "project_name": None,
-                    "is_directory": True,
-                },
-                # api entry (not soft link)
-                {
-                    "created_at": "2020-06-17 12:28:55.705340",
-                    "entity_tag": "8711cf258714b2de5498f5a5ef48cc7b",
-                    "file_id": "api/7b6b4e3d-39ae-3559-8765-4f815a49984e/tmpf_qatpzx",
-                    "file_name": "tmpf_qatpzx",
-                    "file_size": 86,
-                    "file_uuid": "api/7b6b4e3d-39ae-3559-8765-4f815a49984e/tmpf_qatpzx",
-                    "is_soft_link": False,
-                    "last_modified": "2020-06-22 13:48:13.398000+00:00",
-                    "location_id": 0,
-                    "node_name": None,
-                    "project_name": None,
-                },
-                # api entry (soft link)
-                {
-                    "created_at": "2020-06-17 12:28:55.705340",
-                    "entity_tag": "36aa3644f526655a6f557207e4fd25b8",
-                    "file_id": "api/6f788ad9-0ad8-3d0d-9722-72f08c24a212/output_data.json",
-                    "file_name": "output_data.json",
-                    "file_size": 183,
-                    "file_uuid": "api/6f788ad9-0ad8-3d0d-9722-72f08c24a212/output_data.json",
-                    "is_soft_link": True,
-                    "last_modified": "2020-06-22 13:48:13.398000+00:00",
-                    "location_id": 0,
-                    "node_name": None,
-                    "project_name": None,
-                },
-                # datcore entry
-                {
-                    "created_at": "2020-05-28T15:48:34.386302+00:00",
-                    "entity_tag": None,
-                    "file_id": "N:package:ce145b61-7e4f-470b-a113-033653e86d3d",
-                    "file_name": "templatetemplate.json",
-                    "file_size": 238,
-                    "file_uuid": "Kember Cardiac Nerve Model/templatetemplate.json",
-                    "is_soft_link": False,
-                    "last_modified": "2020-05-28T15:48:37.507387+00:00",
-                    "location_id": 1,
-                    "node_name": None,
-                    "project_name": None,
-                },
-            ]
-        },
+        json_schema_extra=_update_json_schema_extra,
     )
 
     @field_validator("location_id", mode="before")
@@ -292,23 +312,28 @@ class FileUploadSchema(BaseModel):
     urls: list[AnyUrl]
     links: FileUploadLinks
 
-    model_config = ConfigDict(
-        extra="forbid",
-        json_schema_extra={
-            "examples": [
-                # typical S3 entry
-                {
-                    "chunk_size": "10000000",
-                    "urls": [
-                        "https://s3.amazonaws.com/bucket-name/key-name?AWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&Expires=1698298164&Signature=WObYM%2F%2B4t7O3%2FZS3Kegb%2Bc4%3D",
-                    ],
-                    "links": {
-                        "abort_upload": "https://storage.com:3021/bucket-name/key-name:abort",
-                        "complete_upload": "https://storage.com:3021/bucket-name/key-name:complete",
+    @staticmethod
+    def _update_json_schema_extra(schema: JsonDict) -> None:
+        schema.update(
+            {
+                "examples": [
+                    # typical S3 entry
+                    {
+                        "chunk_size": "10000000",
+                        "urls": [
+                            "https://s3.amazonaws.com/bucket-name/key-name?AWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&Expires=1698298164&Signature=WObYM%2F%2B4t7O3%2FZS3Kegb%2Bc4%3D",
+                        ],
+                        "links": {
+                            "abort_upload": "https://storage.com:3021/bucket-name/key-name:abort",
+                            "complete_upload": "https://storage.com:3021/bucket-name/key-name:complete",
+                        },
                     },
-                },
-            ]
-        },
+                ]
+            }
+        )
+
+    model_config = ConfigDict(
+        extra="forbid", json_schema_extra=_update_json_schema_extra
     )
 
 
@@ -390,40 +415,43 @@ class PathMetaDataGet(BaseModel):
         Field(description="if filled, this is the file meta data of the s3 object"),
     ] = None
 
+    @staticmethod
+    def _update_json_schema_extra(schema: JsonDict) -> None:
+        schema.update(
+            {
+                "examples": [
+                    # ls no filter
+                    {
+                        "path": "f8da77a9-24b9-4eab-aee7-1f0608da1e3e",
+                        "display_path": "my amazing project",
+                    },
+                    # ls f8da77a9-24b9-4eab-aee7-1f0608da1e3e
+                    {
+                        "path": "f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7",
+                        "display_path": "my amazing project/awesome node",
+                    },
+                    # ls f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7
+                    {
+                        "path": "f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7/outputs",
+                        "display_path": "my amazing project/awesome node/outputs",
+                    },
+                    # ls f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7/outputs
+                    {
+                        "path": "f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7/outputs/output5",
+                        "display_path": "my amazing project/awesome node/outputs/output5",
+                    },
+                    # ls f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7/outputs/output_5
+                    {
+                        "path": f"f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7/outputs/output5/{FileMetaDataGet.model_json_schema()['examples'][0]['file_name']}",
+                        "display_path": f"my amazing project/awesome node/outputs/output5/{FileMetaDataGet.model_json_schema()['examples'][0]['file_name']}",
+                        "file_meta_data": FileMetaDataGet.model_json_schema()[
+                            "examples"
+                        ][0],
+                    },
+                ]
+            }
+        )
+
     model_config = ConfigDict(
-        extra="forbid",
-        json_schema_extra={
-            "examples": [
-                # ls no filter
-                {
-                    "path": "f8da77a9-24b9-4eab-aee7-1f0608da1e3e",
-                    "display_path": "my amazing project",
-                },
-                # ls f8da77a9-24b9-4eab-aee7-1f0608da1e3e
-                {
-                    "path": "f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7",
-                    "display_path": "my amazing project/awesome node",
-                },
-                # ls f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7
-                {
-                    "path": "f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7/outputs",
-                    "display_path": "my amazing project/awesome node/outputs",
-                },
-                # ls f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7/outputs
-                {
-                    "path": "f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7/outputs/output5",
-                    "display_path": "my amazing project/awesome node/outputs/output5",
-                },
-                # ls f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7/outputs/output_5
-                {
-                    "path": f"f8da77a9-24b9-4eab-aee7-1f0608da1e3e/2f94f80f-633e-4dfa-a983-226b7babe3d7/outputs/output5/{FileMetaDataGet.model_config['json_schema_extra']['examples'][0]['file_name']}",  # type: ignore[index, call-overload]
-                    "display_path": f"my amazing project/awesome node/outputs/output5/{FileMetaDataGet.model_config['json_schema_extra']['examples'][0]['file_name']}",  # type: ignore[index, call-overload]
-                    "file_meta_data": FileMetaDataGet.model_config["json_schema_extra"][  # type: ignore[index]
-                        "examples"
-                    ][
-                        0  # type: ignore[index]
-                    ],
-                },
-            ]
-        },
+        extra="forbid", json_schema_extra=_update_json_schema_extra
     )
