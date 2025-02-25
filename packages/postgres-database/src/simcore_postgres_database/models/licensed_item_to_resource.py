32f8f80763c9095fadd43f38d10e147e63e1f2fa
Matus Drobuliak <60785969+matusdrobuliak66@users.noreply.github.com>
2025-02-25 16:18:55 +0100

    🐛 add `product_name` to licensed item/resource mapping table (🗃️) (#7272)

--- packages/postgres-database/src/simcore_postgres_database/models/licensed_item_to_resource.py
+++ packages/postgres-database/src/simcore_postgres_database/models/licensed_item_to_resource.py
@@ -29,14 +29,31 @@ licensed_item_to_resource = sa.Table(
         ),
         nullable=False,
     ),
+    sa.Column(
+        "product_name",
+        sa.String,
+        sa.ForeignKey(
+            "products.name",
+            onupdate=RefActions.CASCADE,
+            ondelete=RefActions.CASCADE,
+            name="fk_licensed_item_to_resource_product_name",
+        ),
+        nullable=False,
+    ),
     column_created_datetime(timezone=True),
     column_modified_datetime(timezone=True),
     #########
+    sa.PrimaryKeyConstraint(
+        "licensed_item_id",
+        "licensed_resource_id",
+        name="pk_licensed_item_to_resource_item_and_resource_id",
+    ),
     # NOTE: Currently, there is a constraint that a resource item ID cannot be in multiple licensed items.
     # The reason is that the license key and license version coming from the internal license server are part of the licensed resource domain.
     # Sim4Life performs a mapping on their side, where the license key and version are mapped to a licensed item.
     # If this constraint is broken, the mapping logic in Sim4Life might break.
     sa.UniqueConstraint(
+        "product_name",
         "licensed_resource_id",
         name="uq_licensed_item_to_resource_resource_id",
     ),
