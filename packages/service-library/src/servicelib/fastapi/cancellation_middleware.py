c2615eea949e523fa65bbcb5696a52d737a83218
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-02-26 12:35:15 +0100

    test fixed + precommit update

--- packages/service-library/src/servicelib/fastapi/cancellation_middleware.py
+++ packages/service-library/src/servicelib/fastapi/cancellation_middleware.py
@@ -1,10 +1,7 @@
 import asyncio
-import functools
 import logging
 
-from fastapi import Request, Response
 from servicelib.logging_utils import log_context
-from starlette.middleware.base import RequestResponseEndpoint
 from starlette.types import ASGIApp, Receive, Scope, Send
 
 _logger = logging.getLogger(__name__)
@@ -14,56 +11,18 @@ class _TerminateTaskGroupError(Exception):
     pass
 
 
-async def _client_disconnected(request: Request) -> None:
-    while not await request.is_disconnected():  # noqa: ASYNC110
-        await asyncio.sleep(0.01)
-    _logger.info("Client disconnected! Terminating task group!")
-    raise _TerminateTaskGroupError
+async def _message_poller(queue, receive):
+    while True:
+        message = await receive()
+        if message["type"] == "http.disconnect":
+            raise _TerminateTaskGroupError
+
+        # Puts the message in the queue
+        await queue.put(message)
 
 
-async def _handler(request: Request, call_next: RequestResponseEndpoint) -> Response:
-    try:
-        return await call_next(request)
-    except asyncio.CancelledError:
-        _logger.info("Handler was cancelled")
-        raise
-
-
-async def cancellation_middleware(
-    request: Request, call_next: RequestResponseEndpoint
-) -> Response:
-    sentinel = object()
-    call_id = id(sentinel)
-
-    with log_context(_logger, logging.DEBUG, f"cancellable request {request.url}"):
-        try:
-            async with asyncio.TaskGroup() as tg:
-                monitoring_task = tg.create_task(
-                    _client_disconnected(request),
-                    name=f"client_connection_monitoring/poller/{request.url}/{call_id}",
-                )
-                handler_task = tg.create_task(
-                    _handler(request, call_next),
-                    name=f"handler/{request.url}/{call_id}",
-                )
-                response = await handler_task
-                monitoring_task.cancel()
-                return response
-        except _TerminateTaskGroupError:
-            _logger.info("The client disconnected. The task group was cancelled.")
-            handler_task.cancel()
-    return Response(status_code=499, content="Client disconnected")
-
-
-def cancellation_middleware2():
-    def _decorator(app):
-        @functools.wraps(app)
-        async def _wrapped_app(scope, receive, send):
-            await app(scope, receive, send)
-
-        return _wrapped_app
-
-    return _decorator
+async def _handler(app: ASGIApp, scope: Scope, queue: asyncio.Queue, send: Send):
+    return await app(scope, queue.get, send)
 
 
 class CancellationMiddleware:
@@ -78,23 +37,15 @@ class CancellationMiddleware:
         # Let's make a shared queue for the request messages
         queue = asyncio.Queue()
 
-        async def message_poller(sentinel, handler_task):
-            nonlocal queue
-            while True:
-                message = await receive()
-                if message["type"] == "http.disconnect":
-                    handler_task.cancel()
-                    return sentinel  # Break the loop
-
-                # Puts the message in the queue
-                await queue.put(message)
-
-        sentinel = object()
-        async with asyncio.TaskGroup() as tg:
-            handler_task = tg.create_task(self.app(scope, queue.get, send))
-            poller_task = tg.create_task(message_poller(sentinel, handler_task))
-
-        try:
-            return await handler_task
-        except asyncio.CancelledError:
-            _logger.info("Cancelling request due to disconnect")
+        with log_context(_logger, logging.DEBUG, f"cancellable request {scope}"):
+            try:
+                async with asyncio.TaskGroup() as tg:
+                    handler_task = tg.create_task(
+                        _handler(self.app, scope, queue, send)
+                    )
+                    poller_task = tg.create_task(_message_poller(queue, receive))
+                    response = await handler_task
+                    poller_task.cancel()
+                    return response
+            except* _TerminateTaskGroupError:
+                _logger.info("The client disconnected. The task group was cancelled.")
