c0663d2dff8dcd00aa9cc55167473dd669e28f0c
matusdrobuliak66 <drobuliak66@gmail.com>
2025-02-26 14:51:00 +0100

    pin licensed resources

--- services/api-server/src/simcore_service_api_server/models/schemas/model_adapter.py
+++ services/api-server/src/simcore_service_api_server/models/schemas/model_adapter.py
@@ -1,14 +1,20 @@
 # Models added here "cover" models from within the deployment in order to restore backwards compatibility
 
-from datetime import datetime
+from datetime import date, datetime
 from decimal import Decimal
-from typing import Annotated, Any
+from typing import Annotated, Literal, NotRequired
 
 from models_library.api_schemas_api_server.pricing_plans import (
     ServicePricingPlanGet as _ServicePricingPlanGet,
 )
 from models_library.api_schemas_webserver.licensed_items import (
     LicensedItemRpcGet as _LicensedItemGet,
+)
+from models_library.api_schemas_webserver.licensed_items import (
+    LicensedResource as _LicensedResource,
+)
+from models_library.api_schemas_webserver.licensed_items import (
+    LicensedResourceFeaturesDict as _LicensedResourceFeaturesDict,
 )
 from models_library.api_schemas_webserver.licensed_items_checkouts import (
     LicensedItemCheckoutRpcGet as _LicensedItemCheckoutRpcGet,
@@ -46,10 +52,12 @@ from pydantic import (
     BaseModel,
     ConfigDict,
     Field,
+    HttpUrl,
     NonNegativeFloat,
     NonNegativeInt,
     PlainSerializer,
 )
+from typing_extensions import TypedDict
 
 
 class GetCreditPriceLegacy(BaseModel):
@@ -140,13 +148,61 @@ assert set(ServicePricingPlanGetLegacy.model_fields.keys()) == set(
 )
 
 
+class LicensedResourceFeaturesDict(TypedDict):
+    age: NotRequired[str]
+    date: date
+    ethnicity: NotRequired[str]
+    functionality: NotRequired[str]
+    height: NotRequired[str]
+    name: NotRequired[str]
+    sex: NotRequired[str]
+    species: NotRequired[str]
+    version: NotRequired[str]
+    weight: NotRequired[str]
+
+
+assert set(LicensedResourceFeaturesDict.__annotations__.keys()) == set(
+    _LicensedResourceFeaturesDict.__annotations__.keys()
+), "LicensedResourceFeaturesDict keys do not match"
+
+for key in LicensedResourceFeaturesDict.__annotations__:
+    assert (
+        LicensedResourceFeaturesDict.__annotations__[key]
+        == _LicensedResourceFeaturesDict.__annotations__[key]
+    ), f"Type of {key} in LicensedResourceFeaturesDict does not match"
+
+
+class LicensedResource(BaseModel):
+    id: int
+    description: str
+    thumbnail: str
+    features: LicensedResourceFeaturesDict
+    doi: str | None
+    license_key: str
+    license_version: str
+    protection: Literal["Code", "PayPal"]
+    available_from_url: HttpUrl | None
+
+
+assert set(LicensedResource.model_fields.keys()) == set(
+    _LicensedResource.model_fields.keys()
+), "LicensedResource keys do not match"
+
+for key in LicensedResource.model_fields.keys():
+    if key == "features":
+        continue
+    assert (
+        LicensedResource.__annotations__[key] == _LicensedResource.__annotations__[key]
+    ), f"Type of {key} in LicensedResource does not match"
+
+
 class LicensedItemGet(BaseModel):
     licensed_item_id: LicensedItemID
     key: LicensedItemKey
     version: LicensedItemVersion
     display_name: str
     licensed_resource_type: LicensedResourceType
-    licensed_resources: list[dict[str, Any]]
+    licensed_resources: list[LicensedResource]
     pricing_plan_id: PricingPlanId
     is_hidden_on_market: bool
     created_at: datetime
