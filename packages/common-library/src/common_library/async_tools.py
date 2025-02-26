e94f73655f3808c41401a1325c813efbd8aee106
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-02-26 11:09:10 +0100

    improve typehints

--- packages/common-library/src/common_library/async_tools.py
+++ packages/common-library/src/common_library/async_tools.py
@@ -1,15 +1,16 @@
 import asyncio
 import functools
 from collections.abc import Callable, Coroutine
+from concurrent.futures import Executor
 from typing import Any, TypeVar
 
 R = TypeVar("R")
 
 
 def make_async(
-    executor=None,
+    executor: Executor | None = None,
 ) -> Callable[[Callable[..., R]], Callable[..., Coroutine[Any, Any, R]]]:
-    def decorator(func) -> Callable[..., Coroutine[Any, Any, R]]:
+    def decorator(func: Callable[..., R]) -> Callable[..., Coroutine[Any, Any, R]]:
         @functools.wraps(func)
         async def wrapper(*args, **kwargs) -> R:
             loop = asyncio.get_running_loop()
