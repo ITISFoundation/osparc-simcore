17f23c90a254c56fa60bc6157890d73df7f10249
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-02-26 11:42:23 +0100

    ongoing

--- packages/service-library/src/servicelib/fastapi/cancellation_middleware.py
+++ packages/service-library/src/servicelib/fastapi/cancellation_middleware.py
@@ -1,14 +1,100 @@
+import asyncio
+import functools
+import logging
+
 from fastapi import Request, Response
-from starlette.middleware.base import BaseHTTPMiddleware
+from servicelib.logging_utils import log_context
+from starlette.middleware.base import RequestResponseEndpoint
+from starlette.types import ASGIApp, Receive, Scope, Send
+
+_logger = logging.getLogger(__name__)
 
 
-class CancellationMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
+class _TerminateTaskGroupError(Exception):
+    pass
+
+
+async def _client_disconnected(request: Request) -> None:
+    while not await request.is_disconnected():  # noqa: ASYNC110
+        await asyncio.sleep(0.01)
+    _logger.info("Client disconnected! Terminating task group!")
+    raise _TerminateTaskGroupError
+
+
+async def _handler(request: Request, call_next: RequestResponseEndpoint) -> Response:
+    try:
+        return await call_next(request)
+    except asyncio.CancelledError:
+        _logger.info("Handler was cancelled")
+        raise
+
+
+async def cancellation_middleware(
+    request: Request, call_next: RequestResponseEndpoint
+) -> Response:
+    sentinel = object()
+    call_id = id(sentinel)
+
+    with log_context(_logger, logging.DEBUG, f"cancellable request {request.url}"):
         try:
-            response = await call_next(request)
-        except Exception as e:
-            if "Client disconnected" in str(e):
-                # Handle client disconnection
-                return Response(status_code=499, content="Client disconnected")
-            raise
-        return response
+            async with asyncio.TaskGroup() as tg:
+                monitoring_task = tg.create_task(
+                    _client_disconnected(request),
+                    name=f"client_connection_monitoring/poller/{request.url}/{call_id}",
+                )
+                handler_task = tg.create_task(
+                    _handler(request, call_next),
+                    name=f"handler/{request.url}/{call_id}",
+                )
+                response = await handler_task
+                monitoring_task.cancel()
+                return response
+        except _TerminateTaskGroupError:
+            _logger.info("The client disconnected. The task group was cancelled.")
+            handler_task.cancel()
+    return Response(status_code=499, content="Client disconnected")
+
+
+def cancellation_middleware2():
+    def _decorator(app):
+        @functools.wraps(app)
+        async def _wrapped_app(scope, receive, send):
+            await app(scope, receive, send)
+
+        return _wrapped_app
+
+    return _decorator
+
+
+class CancellationMiddleware:
+    def __init__(self, app: ASGIApp) -> None:
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        if scope["type"] != "http":
+            await self.app(scope, receive, send)
+            return None
+
+        # Let's make a shared queue for the request messages
+        queue = asyncio.Queue()
+
+        async def message_poller(sentinel, handler_task):
+            nonlocal queue
+            while True:
+                message = await receive()
+                if message["type"] == "http.disconnect":
+                    handler_task.cancel()
+                    return sentinel  # Break the loop
+
+                # Puts the message in the queue
+                await queue.put(message)
+
+        sentinel = object()
+        async with asyncio.TaskGroup() as tg:
+            handler_task = tg.create_task(self.app(scope, queue.get, send))
+            poller_task = tg.create_task(message_poller(sentinel, handler_task))
+
+        try:
+            return await handler_task
+        except asyncio.CancelledError:
+            _logger.info("Cancelling request due to disconnect")
--- packages/service-library/tests/fastapi/test_cancellation_middleware.py
+++ packages/service-library/tests/fastapi/test_cancellation_middleware.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from collections.abc import Iterator
 from threading import Thread
+from unittest.mock import Mock
 
 import httpx
 import pytest
@@ -12,28 +13,49 @@ from servicelib.fastapi.cancellation_middleware import CancellationMiddleware
 from servicelib.utils import unused_port
 from yarl import URL
 
-router = APIRouter()
+
+@pytest.fixture
+def server_done_event() -> asyncio.Event:
+    return asyncio.Event()
 
 
-@router.get("/normal")
-async def normal_endpoint():
-    await asyncio.sleep(2)
-    return {"message": "Normal response"}
+@pytest.fixture
+def server_cancelled_mock() -> Mock:
+    return Mock()
 
 
-@pytest.fixture(scope="module")
-def fastapi_app() -> FastAPI:
+@pytest.fixture
+def fastapi_router(
+    server_done_event: asyncio.Event, server_cancelled_mock: Mock
+) -> APIRouter:
+    router = APIRouter()
+
+    @router.get("/sleep")
+    async def sleep(sleep_time: float) -> dict[str, str]:
+        with log_context(logging.INFO, msg="sleeper") as ctx:
+            try:
+                await asyncio.sleep(sleep_time)
+                return {"message": f"Slept for {sleep_time} seconds"}
+            except asyncio.CancelledError:
+                ctx.logger.info("sleeper cancelled!")
+                server_cancelled_mock()
+                return {"message": "Cancelled"}
+            finally:
+                server_done_event.set()
+
+    return router
+
+
+@pytest.fixture
+def fastapi_app(fastapi_router: APIRouter) -> FastAPI:
     app = FastAPI()
-    app.include_router(router)
+    app.include_router(fastapi_router)
     app.add_middleware(CancellationMiddleware)
     return app
 
 
-@pytest.fixture(scope="module")
-def uvicorn_server(
-    fastapi_app: FastAPI,
-    # app_environment: EnvVarsDict,
-) -> Iterator[URL]:
+@pytest.fixture
+def uvicorn_server(fastapi_app: FastAPI) -> Iterator[URL]:
     random_port = unused_port()
     with log_context(
         logging.INFO,
@@ -62,16 +84,29 @@ def uvicorn_server(
         thread.join(timeout=10)
 
 
-async def test_normal_mode(uvicorn_server: URL):
+async def test_server_cancels_when_client_disconnects(
+    uvicorn_server: URL, server_done_event: asyncio.Event, server_cancelled_mock: Mock
+):
     async with httpx.AsyncClient(base_url=f"{uvicorn_server}") as client:
-        response = await client.get("/normal")
+        # check standard call still complete as expected
+        with log_context(logging.INFO, msg="client calling endpoint"):
+            response = await client.get("/sleep", params={"sleep_time": 0.1})
         assert response.status_code == 200
-        assert response.json() == {"message": "Normal response"}
+        assert response.json() == {"message": "Slept for 0.1 seconds"}
+        async with asyncio.timeout(10):
+            await server_done_event.wait()
+        server_done_event.clear()
 
+        # check slow call get cancelled
+        with log_context(
+            logging.INFO, msg="client calling endpoint for cancellation"
+        ) as ctx:
+            with pytest.raises(httpx.ReadTimeout):
+                response = await client.get(
+                    "/sleep", params={"sleep_time": 10}, timeout=0.1
+                )
+            ctx.logger.info("client disconnected from server")
 
-async def test_client_disconnects(uvicorn_server: URL):
-    # with pytest.raises(Exception) as excinfo:
-    async with httpx.AsyncClient(base_url=f"{uvicorn_server}") as client:
-        with pytest.raises(httpx.ReadTimeout):
-            await client.get("/normal", timeout=0.1)
-    # assert "Client disconnected" in str(excinfo.value)
+        async with asyncio.timeout(10):
+            await server_done_event.wait()
+        server_cancelled_mock.assert_called_once()
