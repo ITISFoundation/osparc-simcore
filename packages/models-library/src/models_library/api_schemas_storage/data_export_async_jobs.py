5c9e45b7e4a8bc021ff34530ed2dbb827dd097d2
Mads Bisgaard <126242332+bisgaard-itis@users.noreply.github.com>
2025-02-27 11:18:45 +0100

    1848 add permission rights to async jobs (#7262)
    Co-authored-by: Andrei Neagu <5694077+GitHK@users.noreply.github.com>
    Co-authored-by: Giancarlo Romeo <giancarloromeo@gmail.com>

--- packages/models-library/src/models_library/api_schemas_storage/data_export_async_jobs.py
+++ packages/models-library/src/models_library/api_schemas_storage/data_export_async_jobs.py
@@ -1,33 +1,33 @@
 # pylint: disable=R6301
-from pathlib import Path
 
 from common_library.errors_classes import OsparcErrorMixin
-from models_library.projects_nodes_io import LocationID
-from models_library.users import UserID
+from models_library.projects_nodes_io import LocationID, StorageFileID
 from pydantic import BaseModel, Field
 
 
 class DataExportTaskStartInput(BaseModel):
-    user_id: UserID
-    product_name: str
     location_id: LocationID
-    paths: list[Path] = Field(..., min_length=1)
+    file_and_folder_ids: list[StorageFileID] = Field(..., min_length=1)
 
 
 ### Exceptions
 
 
-class StorageRpcError(OsparcErrorMixin, RuntimeError):
+class StorageRpcBaseError(OsparcErrorMixin, RuntimeError):
     pass
 
 
-class InvalidFileIdentifierError(StorageRpcError):
+class InvalidLocationIdError(StorageRpcBaseError):
+    msg_template: str = "Invalid location_id {location_id}"
+
+
+class InvalidFileIdentifierError(StorageRpcBaseError):
     msg_template: str = "Could not find the file {file_id}"
 
 
-class AccessRightError(StorageRpcError):
-    msg_template: str = "User {user_id} does not have access to file {file_id}"
+class AccessRightError(StorageRpcBaseError):
+    msg_template: str = "User {user_id} does not have access to file {file_id} with location {location_id}"
 
 
-class DataExportError(StorageRpcError):
+class DataExportError(StorageRpcBaseError):
     msg_template: str = "Could not complete data export job with id {job_id}"
--- services/web/server/tests/unit/with_dbs/01/storage/test_storage_rpc.py
+++ services/web/server/tests/unit/with_dbs/01/storage/test_storage_rpc.py
@@ -65,7 +65,7 @@ def create_storage_rpc_client_mock(mocker: MockerFixture) -> Callable[[str, Any]
 @pytest.mark.parametrize(
     "backend_result_or_exception",
     [
-        AsyncJobGet(job_id=AsyncJobId(_faker.uuid4()), job_name=_faker.text()),
+        AsyncJobGet(job_id=AsyncJobId(f"{_faker.uuid4()}")),
         InvalidFileIdentifierError(file_id=Path("/my/file")),
         AccessRightError(user_id=_faker.pyint(min_value=0), file_id=Path("/my/file")),
         DataExportError(job_id=_faker.pyint(min_value=0)),
@@ -85,7 +85,9 @@ async def test_data_export(
         backend_result_or_exception,
     )
 
-    _body = DataExportPost(paths=[Path(".")])
+    _body = DataExportPost(
+        paths=[f"{faker.uuid4()}/{faker.uuid4()}/{faker.file_name()}"]
+    )
     response = await client.post(
         "/v0/storage/locations/0/export-data", data=_body.model_dump_json()
     )
@@ -106,7 +108,7 @@ async def test_data_export(
     "backend_result_or_exception",
     [
         AsyncJobStatus(
-            job_id=_faker.uuid4(),
+            job_id=f"{_faker.uuid4()}",
             progress=ProgressReport(actual_value=0.5, total=1.0),
             done=False,
             started=datetime.now(),
