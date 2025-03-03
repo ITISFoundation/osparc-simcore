14d99ef458c7e4eff763076bd968a87100cb7b84
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-03-03 14:34:05 +0100

    removed BF variables

--- services/storage/src/simcore_service_storage/modules/db/tokens.py
+++ services/storage/src/simcore_service_storage/modules/db/tokens.py
@@ -7,7 +7,6 @@ from models_library.users import UserID
 from simcore_postgres_database.storage_models import tokens
 from sqlalchemy.ext.asyncio import AsyncEngine
 
-from ...core.settings import get_application_settings
 from . import get_db_engine
 
 _logger = logging.getLogger(__name__)
@@ -27,17 +26,11 @@ async def _get_tokens_from_db(engine: AsyncEngine, user_id: UserID) -> dict[str,
 async def get_api_token_and_secret(
     app: FastAPI, user_id: UserID
 ) -> tuple[str | None, str | None]:
-    # from the client side together with the userid?
     engine = get_db_engine(app)
-    app_settings = get_application_settings(app)
-    # defaults from config if any, othewise None
-    api_token = app_settings.BF_API_KEY
-    api_secret = app_settings.BF_API_SECRET
-
     data = await _get_tokens_from_db(engine, user_id)
 
     data = data.get("token_data", {})
-    api_token = data.get("token_key", api_token)
-    api_secret = data.get("token_secret", api_secret)
+    api_token = data.get("token_key")
+    api_secret = data.get("token_secret")
 
     return api_token, api_secret
