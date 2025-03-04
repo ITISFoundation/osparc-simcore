0b624ec0e7b5a7172aace244ac26cc653d37a40b
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-03-04 15:54:10 +0100

    remove register

--- services/storage/src/simcore_service_storage/modules/celery/worker.py
+++ services/storage/src/simcore_service_storage/modules/celery/worker.py
@@ -1,8 +1,6 @@
 import logging
-from collections.abc import Callable
 
 from celery import Celery
-from celery.contrib.abortable import AbortableTask
 from models_library.progress_bar import ProgressReport
 from servicelib.logging_utils import log_context
 
@@ -14,11 +12,6 @@ _logger = logging.getLogger(__name__)
 class CeleryTaskQueueWorker:
     def __init__(self, celery_app: Celery) -> None:
         self.celery_app = celery_app
-
-    def register_task(self, fn: Callable, task_name: str | None = None) -> None:
-        name = task_name or fn.__name__
-        with log_context(_logger, logging.INFO, msg=f"Registering {name} task"):
-            self.celery_app.task(name=name, base=AbortableTask, bind=True)(fn)
 
     def set_task_progress(
         self, task_name: str, task_id: TaskID, report: ProgressReport
