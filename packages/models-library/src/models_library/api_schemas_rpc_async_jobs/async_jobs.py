52d9219a8c60ce1e226472b5338ae39cad761ee1
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-03-04 09:51:39 +0100

    remove started and stopped

--- packages/models-library/src/models_library/api_schemas_rpc_async_jobs/async_jobs.py
+++ packages/models-library/src/models_library/api_schemas_rpc_async_jobs/async_jobs.py
@@ -1,10 +1,8 @@
-from datetime import datetime
 from typing import Any, TypeAlias
 from uuid import UUID
 
 from models_library.users import UserID
-from pydantic import BaseModel, model_validator
-from typing_extensions import Self
+from pydantic import BaseModel
 
 from ..progress_bar import ProgressReport
 
@@ -15,18 +13,6 @@ class AsyncJobStatus(BaseModel):
     job_id: AsyncJobId
     progress: ProgressReport | None
     done: bool
-    started: datetime
-    stopped: datetime | None
-
-    @model_validator(mode="after")
-    def _check_consistency(self) -> Self:
-        is_done = self.done
-        is_stopped = self.stopped is not None
-
-        if is_done != is_stopped:
-            msg = f"Inconsistent data: {self.done=}, {self.stopped=}"
-            raise ValueError(msg)
-        return self
 
 
 class AsyncJobResult(BaseModel):
