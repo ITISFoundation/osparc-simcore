4b1bc031e4006f685f44551d99868bfe86148d1d
Mads Bisgaard <bisgaard@itis.swiss>
2025-03-04 13:06:32 +0100

    make the catalog rest client private

--- services/web/server/src/simcore_service_webserver/catalog/catalog_service.py
+++ services/web/server/src/simcore_service_webserver/catalog/catalog_service.py
@@ -1,13 +1,4 @@
-from ._catalog_rest_client import (
-    get_service,
-    get_service_access_rights,
-    get_service_resources,
-    get_services_for_user_in_product,
-    is_catalog_service_responsive,
-    to_backend_service,
-)
-
-__all__ = (
+__all__: tuple[str, ...] = (
     "is_catalog_service_responsive",
     "to_backend_service",
     "get_services_for_user_in_product",
