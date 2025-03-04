59bf9c30e3966ce8ba85d42d6c3fa82581293235
Mads Bisgaard <126242332+bisgaard-itis@users.noreply.github.com>
2025-03-04 10:31:21 +0100

    ♻️ Refactor and polish api keys domain of webserver (#7293)

--- api/specs/web-server/_auth_api_keys.py
+++ api/specs/web-server/_auth_api_keys.py
@@ -9,8 +9,8 @@ from models_library.api_schemas_webserver.auth import (
 from models_library.generics import Envelope
 from models_library.rest_error import EnvelopedError
 from simcore_service_webserver._meta import API_VTAG
+from simcore_service_webserver.api_keys._controller_rest import ApiKeysPathParams
 from simcore_service_webserver.api_keys._exceptions_handlers import _TO_HTTP_ERROR_MAP
-from simcore_service_webserver.api_keys._rest import ApiKeysPathParams
 
 router = APIRouter(
     prefix=f"/{API_VTAG}",
