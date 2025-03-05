039e14f9c17d7ecab03e59ebbaaeca1558a5083d
Mads Bisgaard <bisgaard@itis.swiss>
2025-03-05 14:47:54 +0100

    add test for getting access rights

--- services/web/server/tests/unit/with_dbs/01/test_catalog_rest_client.py
+++ services/web/server/tests/unit/with_dbs/01/test_catalog_rest_client.py
@@ -4,9 +4,13 @@ import pytest
 from aiohttp.test_utils import TestClient
 from aioresponses import aioresponses as AioResponsesMock
 from common_library.users_enums import UserRole
+from models_library.api_schemas_catalog.service_access_rights import (
+    ServiceAccessRightsGet,
+)
 from pytest_simcore.helpers.webserver_login import UserInfoDict
 from servicelib.aiohttp import status
 from simcore_service_webserver.catalog.catalog_service import (
+    get_service_access_rights,
     get_services_for_user_in_product,
     is_catalog_service_responsive,
 )
@@ -16,16 +20,23 @@ from simcore_service_webserver.catalog.catalog_service import (
     "user_role",
     [UserRole.USER],
 )
+@pytest.mark.parametrize(
+    "backend_status_code", [status.HTTP_200_OK, status.HTTP_500_INTERNAL_SERVER_ERROR]
+)
 async def test_server_responsive(
-    client: TestClient, logged_user: UserInfoDict, aioresponses_mocker: AioResponsesMock
+    client: TestClient,
+    logged_user: UserInfoDict,
+    aioresponses_mocker: AioResponsesMock,
+    backend_status_code: int,
 ):
-    aioresponses_mocker.get(
-        "http://catalog:8000",
-        status=status.HTTP_200_OK,
-    )
+    aioresponses_mocker.get("http://catalog:8000", status=backend_status_code)
 
     assert client.app
-    assert await is_catalog_service_responsive(app=client.app) == True
+    is_responsive = await is_catalog_service_responsive(app=client.app)
+    if backend_status_code == status.HTTP_200_OK:
+        assert is_responsive == True
+    else:
+        assert is_responsive == False
 
 
 @pytest.mark.parametrize(
@@ -53,3 +64,37 @@ async def test_get_services_for_user_in_product(
         product_name="osparc",
         only_key_versions=False,
     )
+
+
+@pytest.mark.parametrize(
+    "user_role",
+    [UserRole.USER],
+)
+async def test_get_service_access_rights(
+    client: TestClient,
+    logged_user: UserInfoDict,
+    aioresponses_mocker: AioResponsesMock,
+):
+    url_pattern = re.compile(r"http://catalog:8000/.*")
+    example = ServiceAccessRightsGet(
+        service_key="simcore/services/comp/itis/sleeper",
+        service_version="2.1.4",
+        gids_with_access_rights={
+            1: {"execute_access": True},
+            5: {"execute_access": True},
+        },
+    )
+    aioresponses_mocker.get(
+        url_pattern,
+        status=status.HTTP_200_OK,
+        payload=example.model_dump(),
+    )
+    assert client.app
+    access_rights = await get_service_access_rights(
+        app=client.app,
+        user_id=logged_user["id"],
+        service_key="simcore/services/comp/itis/sleeper",
+        service_version="2.1.4",
+        product_name="osparc",
+    )
+    assert isinstance(access_rights, ServiceAccessRightsGet)
