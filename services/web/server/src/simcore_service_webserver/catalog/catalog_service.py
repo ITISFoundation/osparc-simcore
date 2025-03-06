16b56ee1cfcacec0453aeb3a0ec1b37d19c1e2d5
Mads Bisgaard <bisgaard@itis.swiss>
2025-03-06 07:48:14 +0100

    make pylint happy

--- services/web/server/src/simcore_service_webserver/catalog/catalog_service.py
+++ services/web/server/src/simcore_service_webserver/catalog/catalog_service.py
@@ -1,4 +1,12 @@
-from ._service_catalog_rest_client import *  # noqa
+from ._service import batch_get_my_services  # noqa
+from ._service_catalog_rest_client import (  # noqa
+    get_service,
+    get_service_access_rights,
+    get_service_resources,
+    get_services_for_user_in_product,
+    is_catalog_service_responsive,
+    to_backend_service,
+)
 
 __all__: tuple[str, ...] = (
     "is_catalog_service_responsive",
