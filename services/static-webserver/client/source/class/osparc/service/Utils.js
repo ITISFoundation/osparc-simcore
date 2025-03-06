c106b0eaf7d5b3c075f980d14584f4663841fd4c
odeimaiz <maiz@itis.swiss>
2025-03-06 12:44:00 +0100

    [skip ci] deprecated -> retired

--- services/static-webserver/client/source/class/osparc/service/Utils.js
+++ services/static-webserver/client/source/class/osparc/service/Utils.js
@@ -237,8 +237,8 @@ qx.Class.define("osparc.service.Utils", {
     },
 
     isDeprecated: function(metadata) {
-      if (metadata && "deprecated" in metadata && ![null, undefined].includes(metadata["deprecated"])) {
-        const deprecationTime = new Date(metadata["deprecated"]);
+      if (metadata && "retired" in metadata && ![null, undefined].includes(metadata["retired"])) {
+        const deprecationTime = new Date(metadata["retired"]);
         const now = new Date();
         return deprecationTime.getTime() > now.getTime();
       }
@@ -246,8 +246,8 @@ qx.Class.define("osparc.service.Utils", {
     },
 
     isRetired: function(metadata) {
-      if (metadata && "deprecated" in metadata && ![null, undefined].includes(metadata["deprecated"])) {
-        const deprecationTime = new Date(metadata["deprecated"]);
+      if (metadata && "retired" in metadata && ![null, undefined].includes(metadata["retired"])) {
+        const deprecationTime = new Date(metadata["retired"]);
         const now = new Date();
         return deprecationTime.getTime() < now.getTime();
       }
@@ -255,7 +255,7 @@ qx.Class.define("osparc.service.Utils", {
     },
 
     getDeprecationDateText: function(metadata) {
-      const deprecationTime = new Date(metadata["deprecated"]);
+      const deprecationTime = new Date(metadata["retired"]);
       return qx.locale.Manager.tr("It will be Retired: ") + osparc.utils.Utils.formatDate(deprecationTime);
     },
 
