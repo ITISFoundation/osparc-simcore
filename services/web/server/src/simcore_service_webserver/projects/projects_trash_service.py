dba37dd9596dffb8bcbb469fd2e0d72b98f2557c
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-03-07 17:35:11 +0100

    fixe imports

--- services/web/server/src/simcore_service_webserver/projects/projects_trash_service.py
+++ services/web/server/src/simcore_service_webserver/projects/projects_trash_service.py
@@ -2,12 +2,16 @@ from ._trash_service import (
     batch_delete_trashed_projects_as_admin,
     delete_explicitly_trashed_project,
     list_explicitly_trashed_projects,
+    trash_project,
+    untrash_project,
 )
 
 __all__: tuple[str, ...] = (
     "batch_delete_trashed_projects_as_admin",
     "delete_explicitly_trashed_project",
     "list_explicitly_trashed_projects",
+    "trash_project",
+    "untrash_project",
 )
 
 # nopycln: file
