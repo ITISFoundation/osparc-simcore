3232be10717543719b7e6d424a8ebae3dae6ac99
odeimaiz <maiz@itis.swiss>
2025-03-07 16:35:06 +0100

    [skip ci] history deprecated

--- services/static-webserver/client/source/class/osparc/store/Services.js
+++ services/static-webserver/client/source/class/osparc/store/Services.js
@@ -32,9 +32,6 @@ qx.Class.define("osparc.store.Services", {
 
         osparc.data.Resources.getInstance().getAllPages("servicesV2")
           .then(servicesArray => {
-            // OM: delete history, it is deprecated
-            servicesArray.forEach(service => delete service["history"]);
-
             const servicesObj = osparc.service.Utils.convertArrayToObject(servicesArray);
             this.__addHits(servicesObj);
             this.__addTSRInfos(servicesObj);
