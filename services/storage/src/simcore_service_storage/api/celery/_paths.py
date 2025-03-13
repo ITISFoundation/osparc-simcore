51d059141df7e3f56eec34d46aff45465472cb99
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-03-13 10:40:05 +0100

    prepare calls to celery

---
+++ services/storage/src/simcore_service_storage/api/celery/_paths.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from fastapi import FastAPI
+from models_library.projects_nodes_io import LocationID
+from models_library.users import UserID
+
+from ...dsm import get_dsm_provider
+
+
+async def compute_path_size(
+    app: FastAPI, user_id: UserID, location_id: LocationID, path: Path
+):
+    dsm = get_dsm_provider(app).get(location_id)
+    return await dsm.compute_path_size(user_id, path=path)
