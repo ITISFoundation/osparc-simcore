e2ca9a755a18d7e541c9d68158e0c4fa89186e24
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2025-03-15 16:49:55 +0100

    maxWorkers

--- tests/e2e/jest.config.js
+++ tests/e2e/jest.config.js
@@ -7,5 +7,6 @@ module.exports = {
     url: "http://127.0.0.1.nip.io:9081/", // For local testing, set your deployed url here
     apiVersion: 'v0/',
     ourTimeout: 40000,
-  }
+  },
+  maxWorkers: 1
 }
