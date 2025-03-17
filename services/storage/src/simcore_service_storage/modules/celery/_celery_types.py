57fa3d8be433c3639a427f10042b65c6ea993b23
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-03-17 13:09:09 +0100

    @GitHK review: remove unused stuff

--- services/storage/src/simcore_service_storage/modules/celery/_celery_types.py
+++ services/storage/src/simcore_service_storage/modules/celery/_celery_types.py
@@ -1,9 +1,6 @@
-from functools import partial
 from pathlib import Path
-from typing import Any
 
 from kombu.utils.json import register_type  # type: ignore[import-untyped]
-from pydantic import BaseModel
 
 
 def _path_encoder(obj):
@@ -19,32 +16,14 @@ def _path_decoder(obj):
     return obj
 
 
-def _class_full_name(clz: type[BaseModel]) -> str:
+def _class_full_name(clz: type) -> str:
     return ".".join([clz.__module__, clz.__qualname__])
-
-
-def _encoder(obj: BaseModel, *args, **kwargs) -> dict[str, Any]:
-    return obj.model_dump(*args, **kwargs)
-
-
-def _decoder(clz: type[BaseModel], data: dict[str, Any]) -> BaseModel:
-    return clz(**data)
-
-
-def _register_pydantic_types(*models: type[BaseModel]) -> None:
-    for model in models:
-        register_type(
-            model,
-            _class_full_name(model),
-            encoder=_encoder,
-            decoder=partial(_decoder, model),
-        )
 
 
 def register_celery_types() -> None:
     register_type(
         Path,
-        ".".join([Path.__module__, Path.__qualname__]),
+        _class_full_name(Path),
         _path_encoder,
         _path_decoder,
     )
