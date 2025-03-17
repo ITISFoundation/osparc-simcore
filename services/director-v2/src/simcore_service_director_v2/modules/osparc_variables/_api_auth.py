a0f8260d6996ac2b2da6f7b34d3deb0eec5ed35e
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-03-17 15:34:38 +0100

    add key deletion

--- services/director-v2/src/simcore_service_director_v2/modules/osparc_variables/_api_auth.py
+++ services/director-v2/src/simcore_service_director_v2/modules/osparc_variables/_api_auth.py
@@ -1,19 +1,14 @@
-import uuid
-from uuid import uuid5
+from datetime import timedelta
+from uuid import uuid4
 
+import _api_auth_rpc
 from aiocache import cached  # type: ignore[import-untyped]
 from fastapi import FastAPI
 from models_library.products import ProductName
 from models_library.rpc.webserver.auth.api_keys import ApiKeyGet
 from models_library.users import UserID
 
-from ._api_auth_rpc import create_api_key
-
-
-def create_unique_api_name_for(product_name: ProductName, user_id: UserID) -> str:
-    # NOTE: The namespace chosen doesn't significantly impact the resulting UUID
-    # as long as it's consistently used across the same context
-    return f"__auto_{uuid5(uuid.NAMESPACE_DNS, f'{product_name}/{user_id}')}"
+_EXPIRATION_ORPHAN_KEYS = timedelta(weeks=4)
 
 
 # NOTE: Uses caching to prevent multiple calls to the external service
@@ -29,13 +24,12 @@ async def _create_for(
     product_name: ProductName,
     user_id: UserID,
 ) -> ApiKeyGet:
-    display_name = create_unique_api_name_for(product_name, user_id)
-    return await create_api_key(
+    return await _api_auth_rpc.create_api_key(
         app,
         user_id=user_id,
         product_name=product_name,
-        display_name=display_name,
-        expiration=None,
+        display_name=f"__auto_{uuid4()}",
+        expiration=_EXPIRATION_ORPHAN_KEYS,
     )
 
 
@@ -44,12 +38,12 @@ async def create_user_api_key(
     product_name: ProductName,
     user_id: UserID,
 ) -> str:
-    data: ApiKeyGet = await _create_for(
+    data = await _create_for(
         app,
         product_name=product_name,
         user_id=user_id,
     )
-    assert data.api_key
+    assert data.api_key  # nosec
     return data.api_key
 
 
@@ -58,17 +52,16 @@ async def create_user_api_secret(
     product_name: ProductName,
     user_id: UserID,
 ) -> str:
-    data: ApiKeyGet = await _create_for(
+    data = await _create_for(
         app,
         product_name=product_name,
         user_id=user_id,
     )
-    assert data.api_secret
+    assert data.api_secret  # nosec
     return data.api_secret
 
 
 __all__: tuple[str, ...] = (
-    "create_unique_api_name_for",
     "create_user_api_key",
     "create_user_api_secret",
 )
