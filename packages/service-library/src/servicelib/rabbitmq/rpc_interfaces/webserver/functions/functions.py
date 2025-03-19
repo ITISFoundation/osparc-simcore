4b49f93804276b18267392ae011b6fe078a8b6ff
Mads Bisgaard <bisgaard@itis.swiss>
2025-03-19 16:53:39 +0100

    introduce functions section in api server and expose ping endpoint

---
+++ packages/service-library/src/servicelib/rabbitmq/rpc_interfaces/webserver/functions/functions.py
@@ -0,0 +1,21 @@
+import logging
+
+from models_library.api_schemas_webserver import WEBSERVER_RPC_NAMESPACE
+from models_library.rabbitmq_basic_types import RPCMethodName
+from pydantic import TypeAdapter
+from servicelib.logging_utils import log_decorator
+from servicelib.rabbitmq import RabbitMQRPCClient
+
+_logger = logging.getLogger(__name__)
+
+
+@log_decorator(_logger, level=logging.DEBUG)
+async def ping(
+    rabbitmq_rpc_client: RabbitMQRPCClient,
+) -> str:
+    result = await rabbitmq_rpc_client.request(
+        WEBSERVER_RPC_NAMESPACE,
+        TypeAdapter(RPCMethodName).validate_python("ping"),
+    )
+    assert isinstance(result, str)  # nosec
+    return result
---
+++ services/api-server/src/simcore_service_api_server/api/routes/functions.py
@@ -0,0 +1,16 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from simcore_service_api_server.api.dependencies.webserver_rpc import (
+    get_wb_api_rpc_client,
+)
+from simcore_service_api_server.services_rpc.wb_api_server import WbApiRpcClient
+
+router = APIRouter()
+
+
+@router.post("/ping")
+async def ping(
+    wb_api_rpc: Annotated[WbApiRpcClient, Depends(get_wb_api_rpc_client)],
+):
+    return wb_api_rpc.ping()
