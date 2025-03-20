d9e8ef807901f31e2dd973f54bc8b20247f4bfca
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-03-20 20:27:23 +0100

    copy with progress bar

--- services/storage/src/simcore_service_storage/api/_worker_tasks/_simcore_s3.py
+++ services/storage/src/simcore_service_storage/api/_worker_tasks/_simcore_s3.py
@@ -1,36 +1,56 @@
+import functools
 import logging
 from typing import Any
 
 from celery import Task
 from models_library.api_schemas_storage.storage_schemas import FoldersBody
+from models_library.progress_bar import ProgressReport
 from models_library.users import UserID
 from servicelib.logging_utils import log_context
+from servicelib.progress_bar import ProgressBarData
 
 from ...dsm import get_dsm_provider
-from ...modules.celery.utils import get_fastapi_app
+from ...modules.celery.utils import get_celery_worker, get_fastapi_app
 from ...simcore_s3_dsm import SimcoreS3DataManager
 
 _logger = logging.getLogger(__name__)
 
 
+def _task_progress_cb(task: Task, task_id: str, report: ProgressReport) -> None:
+    _logger.error("task_progress_cb %s %s", task.name, task_id)
+    worker = get_celery_worker(task.app)
+    assert task.name  # nosec
+    worker.set_task_progress(
+        task_name=task.name,
+        task_id=task_id,
+        report=report,
+    )
+
+
 async def deep_copy_files_from_project(
-    task: Task, user_id: UserID, body: FoldersBody
+    task: Task, task_id: str, user_id: UserID, body: FoldersBody
 ) -> dict[str, Any]:
+    # _logger.error("%s", f"{task=}, {task.request.id=}, {task_id=}")
     with log_context(
         _logger,
         logging.INFO,
-        msg=f"copying {body.source['uuid']} -> {body.destination['uuid']}",
+        msg=f"copying {body.source['uuid']} -> {body.destination['uuid']} with {task.request.id}",
     ):
         dsm = get_dsm_provider(get_fastapi_app(task.app)).get(
             SimcoreS3DataManager.get_location_id()
         )
         assert isinstance(dsm, SimcoreS3DataManager)  # nosec
-        await dsm.deep_copy_project_simcore_s3(
-            user_id,
-            body.source,
-            body.destination,
-            body.nodes_map,
-            task_progress=None,  # TODO: fix by using a real progress bar
-        )
+        async with ProgressBarData(
+            num_steps=1,
+            description="copying files",
+            progress_report_cb=functools.partial(_task_progress_cb, task, task_id),
+        ) as task_progress:
+            await dsm.deep_copy_project_simcore_s3(
+                user_id,
+                body.source,
+                body.destination,
+                body.nodes_map,
+                task_progress=task_progress,
+            )
 
     return body.destination
