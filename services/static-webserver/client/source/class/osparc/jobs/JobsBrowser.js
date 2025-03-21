332ef05c0d6022f9fdcbd1f6dcece76897b714b3
odeimaiz <maiz@itis.swiss>
2025-03-21 11:24:05 +0100

    filters

--- services/static-webserver/client/source/class/osparc/jobs/JobsBrowser.js
+++ services/static-webserver/client/source/class/osparc/jobs/JobsBrowser.js
@@ -22,8 +22,10 @@ qx.Class.define("osparc.jobs.JobsBrowser", {
   construct() {
     this.base(arguments);
 
-    this._setLayout(new qx.ui.layout.VBox());
+    this._setLayout(new qx.ui.layout.VBox(10));
 
+    this.getChildControl("jobs-filter");
+    this.getChildControl("jobs-ongoing");
     this.getChildControl("jobs-table");
   },
 
@@ -43,6 +45,27 @@ qx.Class.define("osparc.jobs.JobsBrowser", {
     _createChildControlImpl: function(id) {
       let control;
       switch (id) {
+        case "header-filter":
+          control = new qx.ui.container.Composite(new qx.ui.layout.HBox(5));
+          this._add(control);
+          break;
+        case "jobs-filter":
+          control = new osparc.filter.TextFilter("text", "jobsList").set({
+            allowStretchX: true,
+            margin: 0
+          });
+          this.getChildControl("header-filter").add(control, {
+            flex: 1
+          });
+          break;
+        case "jobs-ongoing":
+          control = new qx.ui.form.CheckBox().set({
+            label: "Hide finished jobs",
+            value: true,
+            enabled: false,
+          });
+          this.getChildControl("header-filter").add(control);
+          break;
         case "jobs-table":
           control = new osparc.jobs.JobsTable();
           this._add(control);
