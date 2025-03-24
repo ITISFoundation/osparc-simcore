1413afbec9b3e6f8b2c64f8206e41a2d236ac763
Andrei Neagu <neagu@itis.swiss>
2025-03-24 16:51:54 +0100

    refactor

--- services/storage/src/simcore_service_storage/api/_worker_tasks/_data_export.py
+++ services/storage/src/simcore_service_storage/api/_worker_tasks/_data_export.py
@@ -1,5 +1,4 @@
 import logging
-from typing import cast
 
 from celery import Task  # type: ignore[import-untyped]
 from models_library.progress_bar import ProgressReport
@@ -7,6 +6,7 @@ from models_library.projects_nodes_io import StorageFileID
 from models_library.users import UserID
 
 from ...dsm import get_dsm_provider
+from ...modules.celery.models import TaskID
 from ...modules.celery.utils import get_celery_worker_client, get_fastapi_app
 from ...simcore_s3_dsm import SimcoreS3DataManager
 from ._progress_utils import get_tqdm_progress, set_tqdm_absolute_progress
@@ -16,24 +16,25 @@ _logger = logging.getLogger(__name__)
 
 async def data_export(
     task: Task,
+    task_id: TaskID,
     *,
     user_id: UserID,
     paths_to_export: list[StorageFileID],
 ) -> StorageFileID:
     _logger.info("Exporting (for user='%s') selection: %s", user_id, paths_to_export)
 
-    dsm = cast(
-        SimcoreS3DataManager,
-        get_dsm_provider(get_fastapi_app(task.app)).get(
-            SimcoreS3DataManager.get_location_id()
-        ),
+    dsm = get_dsm_provider(get_fastapi_app(task.app)).get(
+        SimcoreS3DataManager.get_location_id()
     )
+    assert isinstance(dsm, SimcoreS3DataManager)  # nosec
 
     with get_tqdm_progress(total=1, description=f"{task.name}") as pbar:
 
         async def _progress_cb(report: ProgressReport) -> None:
             set_tqdm_absolute_progress(pbar, report)
-            await get_celery_worker_client(task.app).set_task_progress(task, report)
+            await get_celery_worker_client(task.app).set_task_progress(
+                task, task_id, report
+            )
 
         return await dsm.create_s3_export(
             user_id, paths_to_export, progress_cb=_progress_cb
--- services/storage/src/simcore_service_storage/modules/celery/worker.py
+++ services/storage/src/simcore_service_storage/modules/celery/worker.py
@@ -5,6 +5,8 @@ from common_library.async_tools import make_async
 from models_library.progress_bar import ProgressReport
 from servicelib.logging_utils import log_context
 
+from ..celery.models import TaskID
+
 _logger = logging.getLogger(__name__)
 
 
@@ -13,22 +15,19 @@ class CeleryWorkerClient:
         self.celery_app = celery_app
 
     @make_async()
-    def set_task_progress(self, task: Task, report: ProgressReport) -> None:
+    def set_task_progress(
+        self, task: Task, task_id: TaskID, report: ProgressReport
+    ) -> None:
         assert task.name  # nosec
         task_name = task.name
-        task_id = task.request.id
 
         with log_context(
             _logger,
             logging.DEBUG,
             msg=f"Setting progress for {task_name}: {report.model_dump_json()}",
         ):
-            try:
-                self.celery_app.tasks[task_name].update_state(
-                    task_id=task_id,
-                    state="RUNNING",
-                    meta=report.model_dump(mode="json"),
-                )
-            except ValueError as e:
-                if "task_id must not be empty. Got None instead." not in f"{e}":
-                    raise
+            self.celery_app.tasks[task_name].update_state(
+                task_id=task_id,
+                state="RUNNING",
+                meta=report.model_dump(mode="json"),
+            )
