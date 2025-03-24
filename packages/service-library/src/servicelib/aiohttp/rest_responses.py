e3c6aacb581d4b58c1349a73b6fc596087bf5934
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-03-24 13:46:29 +0100

    adds missing supportID

--- packages/service-library/src/servicelib/aiohttp/rest_responses.py
+++ packages/service-library/src/servicelib/aiohttp/rest_responses.py
@@ -1,11 +1,13 @@
 """Utils to check, convert and compose server responses for the RESTApi"""
 
 import inspect
-from typing import Any
+from typing import Any, cast
 
 from aiohttp import web, web_exceptions
 from aiohttp.web_exceptions import HTTPError, HTTPException
+from common_library.error_codes import ErrorCodeStr
 from common_library.json_serialization import json_dumps
+from models_library.basic_types import IDStr
 from models_library.rest_error import ErrorGet, ErrorItemType
 
 from ..aiohttp.status import HTTP_200_OK
@@ -52,6 +54,7 @@ def create_http_error(
     http_error_cls: type[HTTPError] = web.HTTPInternalServerError,
     *,
     skip_internal_error_details: bool = False,
+    error_code: ErrorCodeStr | None = None
 ) -> HTTPError:
     """
     - Response body conforms OAS schema model
@@ -61,25 +64,24 @@ def create_http_error(
     if not isinstance(errors, list):
         errors = [errors]
 
-    # TODO: guarantee no throw!
+    support_id: IDStr | None = cast(IDStr, error_code) if error_code else None
 
     is_internal_error: bool = http_error_cls == web.HTTPInternalServerError
     default_message = reason or get_code_description(http_error_cls.status_code)
 
     if is_internal_error and skip_internal_error_details:
         error = ErrorGet(
-            errors=[],
-            logs=[],
             status=http_error_cls.status_code,
             message=default_message,
+            support_id=support_id,
         )
     else:
         items = [ErrorItemType.from_error(err) for err in errors]
         error = ErrorGet(
-            errors=items,
-            logs=[],
+            errors=items,  # NOTE: deprecated!
             status=http_error_cls.status_code,
             message=default_message,
+            support_id=support_id,
         )
 
     assert not http_error_cls.empty_body  # nosec
