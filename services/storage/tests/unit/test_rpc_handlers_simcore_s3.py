490f7b9899fae3226ef81fb7f0cfbe2912f644f0
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-03-25 07:36:09 +0100

    pylint

--- services/storage/tests/unit/test_rpc_handlers_simcore_s3.py
+++ services/storage/tests/unit/test_rpc_handlers_simcore_s3.py
@@ -1,3 +1,11 @@
+# pylint:disable=no-name-in-module
+# pylint:disable=protected-access
+# pylint:disable=redefined-outer-name
+# pylint:disable=too-many-arguments
+# pylint:disable=too-many-positional-arguments
+# pylint:disable=unused-argument
+# pylint:disable=unused-variable
+
 import asyncio
 import datetime
 import logging
