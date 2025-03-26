885aadf4dc66a4fff9afd7165985f43b1cc54121
Andrei Neagu <neagu@itis.swiss>
2025-03-26 15:40:14 +0100

    remove unused

--- services/notifications/src/simcore_service_notifications/api/rest/_dependencies.py
+++ services/notifications/src/simcore_service_notifications/api/rest/_dependencies.py
@@ -5,18 +5,9 @@ from typing import Annotated, cast
 from fastapi import Depends, FastAPI, Request
 from servicelib.rabbitmq._client_rpc import RabbitMQRPCClient
 
-from ...core.settings import ApplicationSettings
-
 
 def get_application(request: Request) -> FastAPI:
     return cast(FastAPI, request.app)
-
-
-def get_settings(
-    app: Annotated[FastAPI, Depends(get_application)],
-) -> ApplicationSettings:
-    assert isinstance(app.state.settings, ApplicationSettings)  # nosec
-    return app.state.settings
 
 
 def get_rabbitmq_client(
