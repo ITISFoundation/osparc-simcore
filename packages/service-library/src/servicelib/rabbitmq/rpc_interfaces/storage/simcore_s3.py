c9296d748d3f89fe71e655a2a74ac9287a88e23a
Sylvain <35365065+sanderegg@users.noreply.github.com>
2025-03-27 18:10:54 +0100

    🎨Storage: HA (🚨🚨🚨 test with multiple replicas) (#7375)

--- packages/service-library/src/servicelib/rabbitmq/rpc_interfaces/storage/simcore_s3.py
+++ packages/service-library/src/servicelib/rabbitmq/rpc_interfaces/storage/simcore_s3.py
@@ -1,12 +1,9 @@
-from pathlib import Path
-
 from models_library.api_schemas_rpc_async_jobs.async_jobs import (
     AsyncJobGet,
     AsyncJobNameData,
 )
 from models_library.api_schemas_storage import STORAGE_RPC_NAMESPACE
-from models_library.products import ProductName
-from models_library.projects_nodes_io import LocationID
+from models_library.api_schemas_storage.storage_schemas import FoldersBody
 from models_library.rabbitmq_basic_types import RPCMethodName
 from models_library.users import UserID
 
@@ -14,21 +11,19 @@ from ..._client_rpc import RabbitMQRPCClient
 from ..async_jobs.async_jobs import submit
 
 
-async def compute_path_size(
+async def copy_folders_from_project(
     client: RabbitMQRPCClient,
     *,
     user_id: UserID,
-    product_name: ProductName,
-    location_id: LocationID,
-    path: Path,
+    product_name: str,
+    body: FoldersBody,
 ) -> tuple[AsyncJobGet, AsyncJobNameData]:
     job_id_data = AsyncJobNameData(user_id=user_id, product_name=product_name)
     async_job_rpc_get = await submit(
         rabbitmq_rpc_client=client,
         rpc_namespace=STORAGE_RPC_NAMESPACE,
-        method_name=RPCMethodName("compute_path_size"),
+        method_name=RPCMethodName("copy_folders_from_project"),
         job_id_data=job_id_data,
-        location_id=location_id,
-        path=path,
+        body=body,
     )
     return async_job_rpc_get, job_id_data
--- services/storage/src/simcore_service_storage/modules/long_running_tasks.py
+++ services/storage/src/simcore_service_storage/modules/long_running_tasks.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from fastapi import FastAPI
 from servicelib.fastapi.long_running_tasks._server import setup
 
@@ -11,10 +9,3 @@ def setup_rest_api_long_running_tasks_for_uploads(app: FastAPI) -> None:
         app,
         router_prefix=f"/{API_VTAG}/futures",
     )
-
-    app.state.completed_upload_tasks = {}
-
-
-def get_completed_upload_tasks(app: FastAPI) -> dict[str, asyncio.Task]:
-    assert isinstance(app.state.completed_upload_tasks, dict)  # nosec
-    return app.state.completed_upload_tasks
