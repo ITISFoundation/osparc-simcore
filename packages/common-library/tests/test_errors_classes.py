7099475a704b6d81623ab1e8af5c3da085f9c37e
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-03-28 18:45:39 +0100

    new error factory

--- packages/common-library/tests/test_errors_classes.py
+++ packages/common-library/tests/test_errors_classes.py
@@ -9,24 +9,24 @@ from datetime import datetime
 from typing import Any
 
 import pytest
-from common_library.errors_classes import OsparcErrorMixin
+from common_library.errors_classes import (
+    ForbiddenError,
+    NotFoundError,
+    OsparcErrorMixin,
+    make_resource_error,
+)
 
 
 def test_get_full_class_name():
-    class A(OsparcErrorMixin):
-        ...
+    class A(OsparcErrorMixin): ...
 
-    class B1(A):
-        ...
+    class B1(A): ...
 
-    class B2(A):
-        ...
+    class B2(A): ...
 
-    class C(B2):
-        ...
+    class C(B2): ...
 
-    class B12(B1, ValueError):
-        ...
+    class B12(B1, ValueError): ...
 
     assert B1._get_full_class_name() == "A.B1"
     assert C._get_full_class_name() == "A.B2.C"
@@ -159,3 +159,44 @@ def test_exception_context():
         "message": "42 and 'missing=?'",
         "value": 42,
     }
+
+
+def test_resource_error_factory():
+    ProjectNotFoundError = make_resource_error("project", NotFoundError)
+
+    error_1 = ProjectNotFoundError(resource_id="abc123")
+    assert "resource_id" in error_1.error_context()
+    assert error_1.resource_id in error_1.message  # type: ignore
+
+
+def test_resource_error_factory_auto_detect_resource_id():
+    ProjectForbiddenError = make_resource_error("project", ForbiddenError)
+    error_2 = ProjectForbiddenError(project_id="abc123", other_id="foo")
+    assert (
+        error_2.resource_id == error_2.project_id  # type: ignore
+    ), "auto-detects project ids as resourceid"
+    assert error_2.other_id  # type: ignore
+    assert error_2.code == "ForbiddenError.ProjectForbiddenError"
+
+    assert error_2.error_context() == {
+        "project_id": "abc123",
+        "other_id": "foo",
+        "resource": "project",
+        "resource_id": "abc123",
+        "message": "Access to project is forbidden: id='abc123'",
+        "code": "ForbiddenError.ProjectForbiddenError",
+    }
+
+
+def test_resource_error_factory_different_base_exception():
+
+    class MyBaseError(Exception): ...
+
+    OtherProjectForbiddenError = make_resource_error(
+        "other_project", ForbiddenError, MyBaseError
+    )
+
+    assert issubclass(OtherProjectForbiddenError, MyBaseError)
+
+    error_3 = OtherProjectForbiddenError(project_id="abc123")
+    assert error_3.code == "MyBaseError.ForbiddenError.OtherProjectForbiddenError"
