d0f3e0299475a41f71f8c63cb3ede3a073060581
Andrei Neagu <neagu@itis.swiss>
2025-03-28 05:27:23 +0100

    removed unused

--- services/notifications/tests/conftest.py
+++ services/notifications/tests/conftest.py
@@ -2,10 +2,6 @@
 # pylint: disable=unused-argument
 
 
-import pytest
-from models_library.basic_types import BootModeEnum
-from pytest_simcore.helpers.monkeypatch_envs import EnvVarsDict, setenvs_from_dict
-
 pytest_plugins = [
     "pytest_simcore.docker_compose",
     "pytest_simcore.docker_swarm",
@@ -13,18 +9,3 @@ pytest_plugins = [
     "pytest_simcore.rabbit_service",
     "pytest_simcore.repository_paths",
 ]
-
-
-@pytest.fixture
-def mock_environment(monkeypatch: pytest.MonkeyPatch) -> EnvVarsDict:
-    return setenvs_from_dict(
-        monkeypatch,
-        {
-            "LOGLEVEL": "DEBUG",
-            "SC_BOOT_MODE": BootModeEnum.DEBUG,
-            "RABBIT_HOST": "test",
-            "RABBIT_PASSWORD": "test",
-            "RABBIT_SECURE": "false",
-            "RABBIT_USER": "test",
-        },
-    )
