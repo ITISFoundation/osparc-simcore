d3437c4ee65b8fa2e034ae2196410ced64e40b29
Andrei Neagu <neagu@itis.swiss>
2025-03-28 10:55:25 +0100

    refactor

--- packages/service-library/src/servicelib/rabbitmq/rpc_interfaces/storage/simcore_s3.py
+++ packages/service-library/src/servicelib/rabbitmq/rpc_interfaces/storage/simcore_s3.py
@@ -6,8 +6,9 @@ from models_library.api_schemas_storage import STORAGE_RPC_NAMESPACE
 from models_library.api_schemas_storage.storage_schemas import FoldersBody
 from models_library.rabbitmq_basic_types import RPCMethodName
 from models_library.users import UserID
+from pydantic import TypeAdapter
 
-from ..._client_rpc import RabbitMQRPCClient
+from ... import RabbitMQRPCClient
 from ..async_jobs.async_jobs import submit
 
 
@@ -27,3 +28,15 @@ async def copy_folders_from_project(
         body=body,
     )
     return async_job_rpc_get, job_id_data
+
+
+async def start_data_export(
+    rabbitmq_rpc_client: RabbitMQRPCClient, *, job_id_data: AsyncJobNameData, **kwargs
+) -> AsyncJobGet:
+    return await submit(
+        rabbitmq_rpc_client,
+        rpc_namespace=STORAGE_RPC_NAMESPACE,
+        method_name=TypeAdapter(RPCMethodName).validate_python("start_data_export"),
+        job_id_data=job_id_data,
+        **kwargs,
+    )
