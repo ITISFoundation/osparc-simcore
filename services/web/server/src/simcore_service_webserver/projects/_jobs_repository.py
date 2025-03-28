ee54419e890591a498ab58d40205e6046b2abc54
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-03-28 17:47:17 +0100

    jobs service and repo layers

---
+++ services/web/server/src/simcore_service_webserver/projects/_jobs_repository.py
@@ -0,0 +1,39 @@
+import logging
+
+from models_library.projects import ProjectID
+from simcore_postgres_database.models.projects_to_jobs import projects_to_jobs
+from simcore_postgres_database.utils_repos import transaction_context
+from sqlalchemy.dialects.postgresql import insert as pg_insert
+from sqlalchemy.ext.asyncio import AsyncConnection
+
+from ..db.base_repository import BaseRepository
+
+_logger = logging.getLogger(__name__)
+
+
+class ProjectJobsRepository(BaseRepository):
+
+    async def set_project_as_job(
+        self,
+        connection: AsyncConnection | None = None,
+        *,
+        project_uuid: ProjectID,
+        job_parent_resource_name: str,
+    ) -> int:
+        async with transaction_context(self.engine, connection) as conn:
+            stmt = (
+                pg_insert(projects_to_jobs)
+                .values(
+                    project_uuid=project_uuid,
+                    job_parent_resource_name=job_parent_resource_name,
+                )
+                .on_conflict_do_update(
+                    index_elements=["project_uuid", "job_parent_resource_name"],
+                    set_={"job_parent_resource_name": job_parent_resource_name},
+                )
+                .returning(projects_to_jobs.c.id)
+            )
+
+            result = await conn.execute(stmt)
+            row = result.one()
+            return row.id
