7ba0b4610a819b7de20ac2ddf82be08b9ce52671
Andrei Neagu <neagu@itis.swiss>
2025-03-28 08:03:49 +0100

    making test async to try in ci

--- services/notifications/tests/unit/test_cli.py
+++ services/notifications/tests/unit/test_cli.py
@@ -26,7 +26,7 @@ def _format_cli_error(result: Result) -> str:
     return f"Below exception was raised by the cli:\n{tb_message}"
 
 
-def test_process_cli_options(cli_runner: CliRunner):
+async def test_process_cli_options(cli_runner: CliRunner):
     result = cli_runner.invoke(main, ["--help"])
     print(result.stdout)
     assert result.exit_code == 0, _format_cli_error(result)
