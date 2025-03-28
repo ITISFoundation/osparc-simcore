5e6c7634704516f46300ab70ead8c3b5891a9635
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-03-28 23:16:07 +0100

    errors templates

--- packages/common-library/tests/test_errors_classes.py
+++ packages/common-library/tests/test_errors_classes.py
@@ -9,7 +9,12 @@ from datetime import datetime
 from typing import Any
 
 import pytest
-from common_library.errors_classes import OsparcErrorMixin
+from common_library.errors_classes import (
+    ForbiddenError,
+    NotFoundError,
+    OsparcErrorMixin,
+    make_resource_error,
+)
 
 
 def test_get_full_class_name():
@@ -154,3 +159,47 @@ def test_exception_context():
         "message": "42 and 'missing=?'",
         "value": 42,
     }
+
+
+def test_resource_error_factory():
+    ProjectNotFoundError = make_resource_error("project", NotFoundError)
+
+    error_1 = ProjectNotFoundError(resource_id="abc123")
+    assert "resource_id" in error_1.error_context()
+    assert error_1.resource_id in error_1.message  # type: ignore
+
+
+def test_resource_error_factory_auto_detect_resource_id():
+    ProjectForbiddenError = make_resource_error("project", ForbiddenError)
+    error_2 = ProjectForbiddenError(project_id="abc123", other_id="foo")
+    assert (
+        error_2.resource_id == error_2.project_id  # type: ignore
+    ), "auto-detects project ids as resourceid"
+    assert error_2.other_id  # type: ignore
+    assert error_2.code == "BaseOsparcError.ForbiddenError.ProjectForbiddenError"
+
+    assert error_2.error_context() == {
+        "project_id": "abc123",
+        "other_id": "foo",
+        "resource": "project",
+        "resource_id": "abc123",
+        "message": "Access to project is forbidden: id='abc123'",
+        "code": "BaseOsparcError.ForbiddenError.ProjectForbiddenError",
+    }
+
+
+def test_resource_error_factory_different_base_exception():
+
+    class MyServiceError(Exception): ...
+
+    OtherProjectForbiddenError = make_resource_error(
+        "other_project", ForbiddenError, MyServiceError
+    )
+
+    assert issubclass(OtherProjectForbiddenError, MyServiceError)
+
+    error_3 = OtherProjectForbiddenError(project_id="abc123")
+    assert (
+        error_3.code
+        == "MyServiceError.BaseOsparcError.ForbiddenError.OtherProjectForbiddenError"
+    )
