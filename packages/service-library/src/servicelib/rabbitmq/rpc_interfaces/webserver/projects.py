f4ec4984e6533507e6c886741906d3f3f2dba3d3
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-03-28 17:16:47 +0100

    server-client rpc interface

--- packages/service-library/src/servicelib/rabbitmq/rpc_interfaces/webserver/projects.py
+++ packages/service-library/src/servicelib/rabbitmq/rpc_interfaces/webserver/projects.py
@@ -1,7 +1,11 @@
 import logging
 
+from models_library.api_schemas_webserver import WEBSERVER_RPC_NAMESPACE
+from models_library.products import ProductName
 from models_library.projects import ProjectID
-from pydantic import TypeAdapter
+from models_library.rabbitmq_basic_types import RPCMethodName
+from models_library.users import UserID
+from pydantic import TypeAdapter, validate_call
 from servicelib.logging_utils import log_decorator
 from servicelib.rabbitmq import RabbitMQRPCClient
 
@@ -9,19 +13,26 @@ _logger = logging.getLogger(__name__)
 
 
 @log_decorator(_logger, level=logging.DEBUG)
+@validate_call(config={"arbitrary_types_allowed": True})
 async def mark_project_as_job(
     rpc_client: RabbitMQRPCClient,
     *,
+    product_name: ProductName,
+    user_id: UserID,
     project_uuid: ProjectID,
     job_parent_resource_name: str,
 ) -> None:
-
-    assert rpc_client
 
     assert not job_parent_resource_name.startswith("/")  # nosec
     assert "/" in job_parent_resource_name  # nosec
     assert not job_parent_resource_name.endswith("/")  # nosec
 
-    TypeAdapter(ProjectID).validate_python(project_uuid)
-
-    raise NotImplementedError
+    result = await rpc_client.request(
+        WEBSERVER_RPC_NAMESPACE,
+        TypeAdapter(RPCMethodName).validate_python("mark_project_as_job"),
+        product_name=product_name,
+        user_id=user_id,
+        project_uuid=project_uuid,
+        job_parent_resource_name=job_parent_resource_name,
+    )
+    assert result is None
