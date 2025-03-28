ac40e6ce930d1ca8d8e7213f8f3f55962e96c265
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-03-29 00:07:42 +0100

    rpc errors

--- packages/service-library/src/servicelib/rabbitmq/rpc_interfaces/webserver/errors.py
+++ packages/service-library/src/servicelib/rabbitmq/rpc_interfaces/webserver/errors.py
@@ -1,23 +1,7 @@
-from common_library.errors_classes import OsparcErrorMixin
-
-from ..._errors import RPCServerError
+from ..._errors import RPCInterfaceError
 
 
-class WebServerRpcError(RPCServerError):
-    msg_template = "{domain_error_nessage} [{domain_error_type_name}]"
-
-    @classmethod
-    def from_domain_error(cls, err: OsparcErrorMixin):
-        return cls(
-            # composes a  message
-            domain_error_nessage=err.message,
-            domain_error_type_name=f"{err.__class__.__name__}",
-            # copies context
-            **err.error_context(),
-        )
+class ProjectNotFoundRpcError(RPCInterfaceError): ...
 
 
-class ProjectNotFoundRpcError(WebServerRpcError): ...
-
-
-class ProjectForbiddenRpcError(WebServerRpcError): ...
+class ProjectForbiddenRpcError(RPCInterfaceError): ...
