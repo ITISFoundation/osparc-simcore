15a066c39c839ce771569f91fc0b51046d1a4e02
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-03-31 14:35:40 +0200

    test runs

--- services/storage/src/simcore_service_storage/api/rpc/_paths.py
+++ services/storage/src/simcore_service_storage/api/rpc/_paths.py
@@ -11,6 +11,7 @@ from servicelib.rabbitmq import RPCRouter
 
 from ...modules.celery import get_celery_client
 from .._worker_tasks._paths import compute_path_size as remote_compute_path_size
+from .._worker_tasks._paths import delete_paths as remote_delete_paths
 
 _logger = logging.getLogger(__name__)
 router = RPCRouter()
@@ -20,7 +21,6 @@ router = RPCRouter()
 async def compute_path_size(
     app: FastAPI,
     job_id_data: AsyncJobNameData,
-    # user_id: UserID,
     location_id: LocationID,
     path: Path,
 ) -> AsyncJobGet:
@@ -32,4 +32,21 @@ async def compute_path_size(
         path=path,
     )
 
+    return AsyncJobGet(job_id=task_uuid)
+
+
+@router.expose(reraise_if_error_type=None)
+async def delete_paths(
+    app: FastAPI,
+    job_id_data: AsyncJobNameData,
+    location_id: LocationID,
+    paths: set[Path],
+) -> AsyncJobGet:
+    task_uuid = await get_celery_client(app).send_task(
+        remote_delete_paths.__name__,
+        task_context=job_id_data.model_dump(),
+        user_id=job_id_data.user_id,
+        location_id=location_id,
+        paths=paths,
+    )
     return AsyncJobGet(job_id=task_uuid)
