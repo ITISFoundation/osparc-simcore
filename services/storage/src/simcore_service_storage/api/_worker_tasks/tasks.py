8e214d8dede418cde724032efb011045a994038a
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-03-31 14:35:40 +0200

    test runs

--- services/storage/src/simcore_service_storage/api/_worker_tasks/tasks.py
+++ services/storage/src/simcore_service_storage/api/_worker_tasks/tasks.py
@@ -7,7 +7,7 @@ from ...modules.celery._celery_types import register_celery_types
 from ...modules.celery._task import define_task
 from ...modules.celery.tasks import export_data
 from ._files import complete_upload_file
-from ._paths import compute_path_size
+from ._paths import compute_path_size, delete_paths
 from ._simcore_s3 import deep_copy_files_from_project
 
 _logger = logging.getLogger(__name__)
@@ -22,5 +22,6 @@ def setup_worker_tasks(app: Celery) -> None:
     ):
         define_task(app, export_data)
         define_task(app, compute_path_size)
+        define_task(app, delete_paths)
         define_task(app, complete_upload_file)
         define_task(app, deep_copy_files_from_project)
