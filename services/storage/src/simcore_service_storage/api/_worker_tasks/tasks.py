0d5c6f934b377d8017e8dba636eaab64eb671fee
Andrei Neagu <neagu@itis.swiss>
2025-04-02 09:26:25 +0200

    removed timeout on soem tasks

--- services/storage/src/simcore_service_storage/api/_worker_tasks/tasks.py
+++ services/storage/src/simcore_service_storage/api/_worker_tasks/tasks.py
@@ -15,7 +15,7 @@ _logger = logging.getLogger(__name__)
 def setup_worker_tasks(app: Celery) -> None:
     register_celery_types()
     with log_context(_logger, logging.INFO, msg="celery task registration"):
-        define_task(app, data_export)
+        define_task(app, data_export, timeout=None)
         define_task(app, compute_path_size)
         define_task(app, complete_upload_file)
-        define_task(app, deep_copy_files_from_project)
+        define_task(app, deep_copy_files_from_project, timeout=None)
