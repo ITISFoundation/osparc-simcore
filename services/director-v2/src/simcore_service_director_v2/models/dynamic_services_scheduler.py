bcc60b5e509ab2f028e3e2bf61d848b484b2b7d0
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-04-02 09:25:56 +0200

    🐛 Clean Pydantic `model_dump`s warnings (#7358)
    Co-authored-by: sanderegg <35365065+sanderegg@users.noreply.github.com>

--- services/director-v2/src/simcore_service_director_v2/models/dynamic_services_scheduler.py
+++ services/director-v2/src/simcore_service_director_v2/models/dynamic_services_scheduler.py
@@ -541,7 +541,8 @@ class SchedulerData(CommonServiceDetails, DynamicSidecarServiceLabels):
         # compose_spec needs to be json encoded before encoding it to json
         # and storing it in the label
         return self.model_copy(
-            update={"compose_spec": json.dumps(self.compose_spec)}, deep=True
+            update={"compose_spec": json.dumps(self.compose_spec)},
+            deep=True,
         ).model_dump_json()
 
     model_config = ConfigDict(extra="allow", populate_by_name=True)
