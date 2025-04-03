d589ffd5ecc075c316606c570c1852547a254c6f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-04-03 15:58:20 +0200

    fixed tests

--- services/web/server/src/simcore_service_webserver/users/_preferences_repository.py
+++ services/web/server/src/simcore_service_webserver/users/_preferences_repository.py
@@ -4,7 +4,7 @@ from models_library.user_preferences import FrontendUserPreference, PreferenceNa
 from models_library.users import UserID
 from simcore_postgres_database.utils_user_preferences import FrontendUserPreferencesRepo
 
-from ..db.plugin import get_database_engine
+from ..db.plugin import get_asyncpg_engine
 
 
 def _get_user_preference_name(user_id: UserID, preference_name: PreferenceName) -> str:
@@ -18,7 +18,7 @@ async def get_user_preference(
     product_name: ProductName,
     preference_class: type[FrontendUserPreference],
 ) -> FrontendUserPreference | None:
-    async with get_database_engine(app).acquire() as conn:
+    async with get_asyncpg_engine(app).connect() as conn:
         preference_payload: dict | None = await FrontendUserPreferencesRepo.load(
             conn,
             user_id=user_id,
@@ -42,7 +42,7 @@ async def set_user_preference(
     product_name: ProductName,
     preference: FrontendUserPreference,
 ) -> None:
-    async with get_database_engine(app).acquire() as conn:
+    async with get_asyncpg_engine(app).begin() as conn:
         await FrontendUserPreferencesRepo.save(
             conn,
             user_id=user_id,
