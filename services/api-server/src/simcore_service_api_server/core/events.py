c49c145473ae21f5798217547f154b9389b1962c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-04-03 17:09:31 +0200

    connect with asyncpg

--- services/api-server/src/simcore_service_api_server/core/events.py
+++ services/api-server/src/simcore_service_api_server/core/events.py
@@ -4,7 +4,13 @@ from collections.abc import Callable
 from fastapi import FastAPI
 
 from .._meta import APP_FINISHED_BANNER_MSG, APP_STARTED_BANNER_MSG
-from ..db.events import close_db_connection, connect_to_db
+from ..db.events import (
+    asyncpg_close_db_connection,
+    asyncpg_connect_to_db,
+    close_db_connection,
+    connect_to_db,
+)
+from .settings import ApplicationSettings
 
 logger = logging.getLogger(__name__)
 
@@ -14,10 +20,12 @@ def create_start_app_handler(app: FastAPI) -> Callable:
         logger.info("Application starting ...")
         if app.state.settings.API_SERVER_POSTGRES:
             # database
+            assert isinstance(app.state.settings, ApplicationSettings)  # nosec
             await connect_to_db(app)
+            await asyncpg_connect_to_db(app, app.state.settings.API_SERVER_POSTGRES)
             assert app.state.engine  # nosec
 
-        print(APP_STARTED_BANNER_MSG, flush=True)
+        print(APP_STARTED_BANNER_MSG, flush=True)  # noqa: T201
 
     return _on_startup
 
@@ -27,7 +35,9 @@ def create_stop_app_handler(app: FastAPI) -> Callable:
         logger.info("Application stopping, ...")
 
         if app.state.settings.API_SERVER_POSTGRES:
+            assert isinstance(app.state.settings, ApplicationSettings)  # nosec
             try:
+                await asyncpg_close_db_connection(app)
                 await close_db_connection(app)
 
             except Exception as err:  # pylint: disable=broad-except
@@ -38,6 +48,6 @@ def create_stop_app_handler(app: FastAPI) -> Callable:
                     stack_info=app.state.settings.debug,
                 )
 
-        print(APP_FINISHED_BANNER_MSG, flush=True)
+        print(APP_FINISHED_BANNER_MSG, flush=True)  # noqa: T201
 
     return _on_shutdown
