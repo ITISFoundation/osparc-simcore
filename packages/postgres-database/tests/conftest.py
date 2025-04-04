ff952789c6b5a03c904f0070f0429e36ecf853c9
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-04-04 08:43:45 +0200

    fix fixture and test

--- packages/postgres-database/tests/conftest.py
+++ packages/postgres-database/tests/conftest.py
@@ -19,6 +19,7 @@ from aiopg.sa.connection import SAConnection
 from aiopg.sa.engine import Engine
 from aiopg.sa.result import ResultProxy, RowProxy
 from faker import Faker
+from models_library.products import ProductName
 from pytest_simcore.helpers import postgres_tools
 from pytest_simcore.helpers.faker_factories import (
     random_group,
@@ -38,6 +39,7 @@ from simcore_postgres_database.webserver_models import (
     user_to_groups,
     users,
 )
+from sqlalchemy.engine.row import Row
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
 pytest_plugins = [
@@ -324,18 +326,26 @@ async def create_fake_projects_node(
 
 
 @pytest.fixture
-def create_fake_product(
-    connection: aiopg.sa.connection.SAConnection,
-) -> Callable[..., Awaitable[RowProxy]]:
-    async def _creator(product_name: str) -> RowProxy:
-        result = await connection.execute(
-            sa.insert(products)
-            .values(name=product_name, host_regex=".*")
-            .returning(sa.literal_column("*"))
-        )
-        assert result
-        row = await result.first()
-        assert row
-        return row
+async def create_fake_product(
+    asyncpg_engine: AsyncEngine,
+) -> AsyncIterator[Callable[[ProductName], Awaitable[Row]]]:
+    created_product_names = set()
 
-    return _creator
+    async def _creator(product_name: ProductName) -> Row:
+        async with asyncpg_engine.begin() as connection:
+            result = await connection.execute(
+                sa.insert(products)
+                .values(name=product_name, host_regex=".*")
+                .returning(sa.literal_column("*"))
+            )
+            assert result
+            row = result.one()
+            created_product_names.add(row.name)
+            return row
+
+    yield _creator
+
+    async with asyncpg_engine.begin() as conn:
+        await conn.execute(
+            products.delete().where(products.c.name.in_(created_product_names))
+        )
