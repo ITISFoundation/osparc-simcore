7c7dbe7248f6618765509e74ffd273a977ab9c8a
matusdrobuliak66 <drobuliak66@gmail.com>
2025-04-07 16:31:34 +0200

    fix cyclic dependencies

--- services/web/server/src/simcore_service_webserver/projects/api.py
+++ services/web/server/src/simcore_service_webserver/projects/api.py
@@ -11,19 +11,11 @@ from ._groups_service import (
     delete_project_group_without_checking_permissions,
 )
 from ._projects_service import delete_project_by_user
-from ._wallets_service import (
-    check_project_financial_status,
-    connect_wallet_to_project,
-    get_project_wallet,
-)
 
 __all__: tuple[str, ...] = (
-    "check_project_financial_status",
     "check_user_project_permission",
-    "connect_wallet_to_project",
     "create_project_group_without_checking_permissions",
     "delete_project_group_without_checking_permissions",
-    "get_project_wallet",
     "has_user_project_access_rights",
     "list_projects",
     "delete_project_by_user",
