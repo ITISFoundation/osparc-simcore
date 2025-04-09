cee8c654180592693bc3d63a1f3677245b614f53
odeimaiz <maiz@itis.swiss>
2025-04-09 11:35:29 +0200

    block card

--- services/static-webserver/client/source/class/osparc/dashboard/CardBase.js
+++ services/static-webserver/client/source/class/osparc/dashboard/CardBase.js
@@ -665,13 +665,20 @@ qx.Class.define("osparc.dashboard.CardBase", {
       }
 
       // Block card
-      const inaccessibleServices = osparc.study.Utils.getCantExecuteServices(services);
-      if (inaccessibleServices.length) {
+      const cantReadServices = osparc.study.Utils.getCantExecuteServices(services);
+      let inaccessibleServices = [];
+      if (this.isResourceType("study") || this.isResourceType("template")) {
+        inaccessibleServices = osparc.store.Services.getInaccessibleServices(this.getResourceData()["workbench"]);
+      }
+      if (cantReadServices.length || inaccessibleServices.length) {
         this.setBlocked("UNKNOWN_SERVICES");
         const image = "@FontAwesome5Solid/ban/";
         let toolTipText = this.tr("Inaccessible service(s):");
-        inaccessibleServices.forEach(unSrv => {
+        cantReadServices.forEach(unSrv => {
           toolTipText += "<br>" + unSrv.key + ":" + osparc.service.Utils.extractVersionDisplay(unSrv.release);
+        });
+        inaccessibleServices.forEach(unSrv => {
+          toolTipText += "<br>" + unSrv.key + ":" + unSrv.version;
         });
         this.__showBlockedCard(image, toolTipText);
       }
