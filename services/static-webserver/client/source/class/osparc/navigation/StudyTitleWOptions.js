78aa02fcc805349c7fd0dd3ed0323ad1f869ccd5
odeimaiz <maiz@itis.swiss>
2025-04-14 15:50:37 +0200

    re-enable ConvertToPipelineEnabled

--- services/static-webserver/client/source/class/osparc/navigation/StudyTitleWOptions.js
+++ services/static-webserver/client/source/class/osparc/navigation/StudyTitleWOptions.js
@@ -162,21 +162,12 @@ qx.Class.define("osparc.navigation.StudyTitleWOptions", {
         });
 
         const convertToPipelineButton = this.getChildControl("study-menu-convert-to-pipeline");
-        const convertToStandaloneButton = this.getChildControl("study-menu-convert-to-standalone");
         if (osparc.product.Utils.hasConvertToPipelineEnabled()) {
           study.getUi().bind("mode", convertToPipelineButton, "visibility", {
             converter: mode => mode === "standalone" ? "visible" : "excluded"
           });
-
-          const evaluateConvertToStandaloneButton = () => {
-            // exclude until we have the export to standalone backend functionality
-            convertToStandaloneButton.exclude();
-          };
-          study.getWorkbench().addListener("pipelineChanged", () => evaluateConvertToStandaloneButton());
-          study.getUi().addListener("changeMode", () => evaluateConvertToStandaloneButton());
         } else {
           convertToPipelineButton.exclude();
-          convertToStandaloneButton.exclude();
         }
 
         const restoreButton = this.getChildControl("study-menu-restore");
