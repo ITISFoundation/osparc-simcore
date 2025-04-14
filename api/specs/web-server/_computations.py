4e10a56011ddfaa460eb0449b7af62ae54410747
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-04-14 16:36:22 +0200

    models

--- api/specs/web-server/_computations.py
+++ api/specs/web-server/_computations.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, status
-from models_library.api_schemas_directorv2.computations import ComputationGet
-from models_library.api_schemas_webserver.computations import ComputationStart
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status
+from models_library.api_schemas_webserver.computations import (
+    ComputationGet,
+    ComputationPathParams,
+    ComputationStart,
+    ComputationStarted,
+)
 from models_library.generics import Envelope
-from models_library.projects import ProjectID
 from simcore_service_webserver._meta import API_VTAG
-from simcore_service_webserver.director_v2._handlers import _ComputationStarted
 
 router = APIRouter(
     prefix=f"/{API_VTAG}",
@@ -19,12 +23,12 @@ router = APIRouter(
     "/computations/{project_id}",
     response_model=Envelope[ComputationGet],
 )
-async def get_computation(project_id: ProjectID): ...
+async def get_computation(_path: Annotated[ComputationPathParams, Depends()]): ...
 
 
 @router.post(
     "/computations/{project_id}:start",
-    response_model=Envelope[_ComputationStarted],
+    response_model=Envelope[ComputationStarted],
     responses={
         status.HTTP_402_PAYMENT_REQUIRED: {
             "description": "Insufficient credits to run computation"
@@ -38,11 +42,14 @@ async def get_computation(project_id: ProjectID): ...
         status.HTTP_503_SERVICE_UNAVAILABLE: {"description": "Service not available"},
     },
 )
-async def start_computation(project_id: ProjectID, _start: ComputationStart): ...
+async def start_computation(
+    _path: Annotated[ComputationPathParams, Depends()],
+    _body: ComputationStart,
+): ...
 
 
 @router.post(
     "/computations/{project_id}:stop",
     status_code=status.HTTP_204_NO_CONTENT,
 )
-async def stop_computation(project_id: ProjectID): ...
+async def stop_computation(_path: Annotated[ComputationPathParams, Depends()]): ...
--- packages/models-library/src/models_library/api_schemas_webserver/computations.py
+++ packages/models-library/src/models_library/api_schemas_webserver/computations.py
@@ -1,9 +1,40 @@
-from pydantic import BaseModel
+from typing import Annotated
+
+from common_library.basic_types import DEFAULT_FACTORY
+from pydantic import BaseModel, Field
+
+from ..api_schemas_directorv2.computations import (
+    ComputationGet as _ComputationGetDirectorV2,
+)
+from ..projects import CommitID, ProjectID
+from ._base import InputSchema, OutputSchema
 
 
-class ComputationStart(BaseModel):
+class ComputationPathParams(BaseModel):
+    project_id: ProjectID
+
+
+class ComputationGet(_ComputationGetDirectorV2, OutputSchema):
+    # FIXME: camelcase!!
+    # NOTE: this is a copy of the same class in models_library.api_schemas_directorv2
+    #       but it is used in a different context (webserver)
+    #       and we need to add the `OutputSchema` mixin
+    #       so that it can be used as a response model in FastAPI
+    pass
+
+
+class ComputationStart(InputSchema):
+    # FIXME: camelcase!!
     force_restart: bool = False
-    subgraph: set[str] = set()
+    subgraph: Annotated[set[str], Field(default_factory=set)]
 
 
-__all__: tuple[str, ...] = ("ComputationStart",)
+class ComputationStarted(OutputSchema):
+    # FIXME: camelcase!!
+    pipeline_id: Annotated[
+        ProjectID, Field(description="ID for created pipeline (=project identifier)")
+    ]
+    ref_ids: Annotated[
+        list[CommitID],
+        Field(default_factory=list, description="Checkpoints IDs for created pipeline"),
+    ] = DEFAULT_FACTORY
