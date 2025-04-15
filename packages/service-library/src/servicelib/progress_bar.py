4681148839d44d0166b54499bf2b25f419a215d7
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-04-15 09:52:38 +0200

    fix

--- packages/service-library/src/servicelib/progress_bar.py
+++ packages/service-library/src/servicelib/progress_bar.py
@@ -109,8 +109,7 @@ class ProgressBarData:  # pylint: disable=too-many-instance-attributes
         return self
 
     async def __aexit__(self, exc_type, exc_value, traceback) -> None:
-        if exc_type is not None:
-            await self.finish()
+        await self.finish()
 
     async def _update_parent(self, value: float) -> None:
         if self._parent:
