e6f13b112666669c1bfc740e4a9a3320015253ea
Sylvain <35365065+sanderegg@users.noreply.github.com>
2025-04-22 16:38:05 +0200

    ♻️🎨Dockerfile: fix start period/start interval healthchecks (#7557)

--- services/agent/docker/healthcheck.py
+++ services/agent/docker/healthcheck.py
@@ -6,7 +6,8 @@ Example of usage in a Dockerfile
     COPY --chown=scu:scu docker/healthcheck.py docker/healthcheck.py
     HEALTHCHECK --interval=30s \
                 --timeout=30s \
-                --start-period=1s \
+                --start-period=20s \
+                --start-interval=1s \
                 --retries=3 \
                 CMD python3 docker/healthcheck.py http://localhost:8000/
 ```
@@ -15,6 +16,7 @@ Q&A:
     1. why not to use curl instead of a python script?
         - SEE https://blog.sixeyed.com/docker-healthchecks-why-not-to-use-curl-or-iwr/
 """
+
 import os
 import sys
 from urllib.request import urlopen
