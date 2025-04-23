36f7c51b6b036ec78f84a3ec77a2889339d0395a
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-04-23 16:29:58 +0200

    fix name

--- api/specs/web-server/_long_running_tasks.py
+++ api/specs/web-server/_long_running_tasks.py
@@ -58,7 +58,7 @@ def get_async_job_status(
     responses=_export_data_responses,
     status_code=status.HTTP_204_NO_CONTENT,
 )
-def selete_async_job(
+def delete_async_job(
     _path_params: Annotated[_PathParam, Depends()],
 ): ...
 
