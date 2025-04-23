8af5ed0c8c143bfdaefff661eee7ee6867af0385
odeimaiz <maiz@itis.swiss>
2025-04-23 13:51:41 +0200

    delete after downloading

--- services/static-webserver/client/source/class/osparc/data/Resources.js
+++ services/static-webserver/client/source/class/osparc/data/Resources.js
@@ -561,6 +561,10 @@ qx.Class.define("osparc.data.Resources", {
           get: {
             method: "GET",
             url: statics.API + "/tasks"
+          },
+          delete: {
+            method: "DELETE",
+            url: statics.API + "/tasks/{taskId}"
           }
         }
       },
