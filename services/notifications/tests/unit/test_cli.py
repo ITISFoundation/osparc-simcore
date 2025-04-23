44ea04c5dc505fbf58da1541f6562ef923d9fa35
Andrei Neagu <neagu@itis.swiss>
2025-04-23 11:32:58 +0200

    bumped test coverage

--- services/notifications/tests/unit/test_cli.py
+++ services/notifications/tests/unit/test_cli.py
@@ -5,8 +5,13 @@ import traceback
 
 import pytest
 from click.testing import Result
-from pytest_simcore.helpers.monkeypatch_envs import EnvVarsDict
+from pytest_simcore.helpers.monkeypatch_envs import (
+    EnvVarsDict,
+    load_dotenv,
+    setenvs_from_dict,
+)
 from simcore_service_notifications.cli import main
+from simcore_service_notifications.core.settings import ApplicationSettings
 from typer.testing import CliRunner
 
 pytest_simcore_core_services_selection = [
@@ -38,3 +43,15 @@ async def test_process_cli_options(cli_runner: CliRunner):
     result = cli_runner.invoke(main, ["--version"])
     print(result.stdout)
     assert result.exit_code == 0, _format_cli_error(result)
+
+
+async def test_echo_dotenv(cli_runner: CliRunner, monkeypatch: pytest.MonkeyPatch):
+    result = cli_runner.invoke(main, ["echo-dotenv"])
+    print(result.stdout)
+    assert result.exit_code == 0, _format_cli_error(result)
+
+    environs = load_dotenv(result.stdout)
+
+    with monkeypatch.context() as patch:
+        setenvs_from_dict(patch, environs)
+        assert ApplicationSettings.create_from_envs()
