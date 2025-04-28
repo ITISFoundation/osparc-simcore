1fc4d8df1a7e63531b93b8f695b03f08352abf82
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-04-28 13:49:37 +0200

    minor

--- services/api-server/src/simcore_service_api_server/repository/api_keys.py
+++ services/api-server/src/simcore_service_api_server/repository/api_keys.py
@@ -4,8 +4,7 @@ from typing import NamedTuple
 import sqlalchemy as sa
 from models_library.products import ProductName
 from pydantic.types import PositiveInt
-from simcore_postgres_database.aiopg_errors import DatabaseError
-from simcore_postgres_database.models.api_keys import api_keys as api_keys_table
+from simcore_postgres_database.models.api_keys import api_keys as auth_api_keys_table
 from simcore_postgres_database.utils_repos import pass_or_acquire_connection
 from sqlalchemy.ext.asyncio import AsyncConnection
 
@@ -31,28 +30,22 @@ class ApiKeysRepository(BaseRepository):
     ) -> UserAndProductTuple | None:
 
         stmt = sa.select(
-            api_keys_table.c.user_id,
-            api_keys_table.c.product_name,
+            auth_api_keys_table.c.user_id,
+            auth_api_keys_table.c.product_name,
         ).where(
             (
-                api_keys_table.c.api_key == api_key
+                auth_api_keys_table.c.api_key == api_key
             )  # NOTE: keep order, api_key is indexed
             & (
-                api_keys_table.c.api_secret
-                == sa.func.crypt(api_secret, api_keys_table.c.api_secret)
+                auth_api_keys_table.c.api_secret
+                == sa.func.crypt(api_secret, auth_api_keys_table.c.api_secret)
             )
         )
-        result: UserAndProductTuple | None = None
-        try:
-            async with pass_or_acquire_connection(self.db_engine, connection) as conn:
-                db_result = await conn.execute(stmt)
-                row = db_result.one_or_none()
-                if row:
-                    result = UserAndProductTuple(
-                        user_id=row.user_id, product_name=row.product_name
-                    )
-
-        except DatabaseError as err:
-            _logger.debug("Failed to get user id: %s", err)
-
-        return result
+        async with pass_or_acquire_connection(self.db_engine, connection) as conn:
+            result = await conn.execute(stmt)
+            row = result.one_or_none()
+            return (
+                UserAndProductTuple(user_id=row.user_id, product_name=row.product_name)
+                if row
+                else None
+            )
