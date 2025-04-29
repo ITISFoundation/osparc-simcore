b31b233223a108ae79582b2fc835fb220c0939ce
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-04-29 09:54:48 +0200

    @bisgaard-itis review: tuples

--- services/api-server/src/simcore_service_api_server/models/api_resources.py
+++ services/api-server/src/simcore_service_api_server/models/api_resources.py
@@ -53,26 +53,31 @@ def compose_resource_name(*collection_or_resource_ids) -> RelativeResourceName:
     return TypeAdapter(RelativeResourceName).validate_python("/".join(quoted_parts))
 
 
-def split_resource_name(resource_name: RelativeResourceName) -> list[str]:
-    quoted_parts = resource_name.split("/")
-    return [f"{urllib.parse.unquote_plus(p)}" for p in quoted_parts]
-
-
-def parse_collections_ids(resource_name: RelativeResourceName) -> list[str]:
+def split_resource_name(resource_name: RelativeResourceName) -> tuple[str, ...]:
     """
     Example:
         resource_name = "solvers/simcore%2Fservices%2Fcomp%2Fisolve/releases/1.3.4/jobs/f622946d-fd29-35b9-a193-abdd1095167c/outputs/output+22"
-        returns ["solvers", "releases", "jobs", "outputs"]
+        returns ("solvers", "simcore/services/comp/isolve", "releases", "1.3.4", "jobs", "f622946d-fd29-35b9-a193-abdd1095167c", "outputs", "output 22")
+    """
+    quoted_parts = resource_name.split("/")
+    return tuple(f"{urllib.parse.unquote_plus(p)}" for p in quoted_parts)
+
+
+def parse_collections_ids(resource_name: RelativeResourceName) -> tuple[str, ...]:
+    """
+    Example:
+        resource_name = "solvers/simcore%2Fservices%2Fcomp%2Fisolve/releases/1.3.4/jobs/f622946d-fd29-35b9-a193-abdd1095167c/outputs/output+22"
+        returns ("solvers", "releases", "jobs", "outputs")
     """
     parts = split_resource_name(resource_name)
     return parts[::2]
 
 
-def parse_resources_ids(resource_name: RelativeResourceName) -> list[str]:
+def parse_resources_ids(resource_name: RelativeResourceName) -> tuple[str, ...]:
     """
     Example:
         resource_name = "solvers/simcore%2Fservices%2Fcomp%2Fisolve/releases/1.3.4/jobs/f622946d-fd29-35b9-a193-abdd1095167c/outputs/output+22"
-        returns ["simcore/services/comp/isolve", "1.3.4", "f622946d-fd29-35b9-a193-abdd1095167c", "output 22"]
+        returns ("simcore/services/comp/isolve", "1.3.4", "f622946d-fd29-35b9-a193-abdd1095167c", "output 22")
     """
     parts = split_resource_name(resource_name)
     return parts[1::2]
