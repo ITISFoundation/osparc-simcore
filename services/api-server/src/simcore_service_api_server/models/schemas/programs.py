2f98a0b622c7e334dd3ab400fc40bf249ab135e7
Mads Bisgaard <126242332+bisgaard-itis@users.noreply.github.com>
2025-04-29 10:31:14 +0200

    🎨 Program jobs: Pass metadata and return display version  (#7562)

--- services/api-server/src/simcore_service_api_server/models/schemas/programs.py
+++ services/api-server/src/simcore_service_api_server/models/schemas/programs.py
@@ -30,6 +30,8 @@ ProgramKeyId = Annotated[
 class Program(BaseService, ApiServerOutputSchema):
     """A released program with a specific version"""
 
+    version_display: str | None
+
     model_config = ConfigDict(
         extra="ignore",
         json_schema_extra={
@@ -40,6 +42,7 @@ class Program(BaseService, ApiServerOutputSchema):
                 "description": "Simulation framework",
                 "maintainer": "info@itis.swiss",
                 "url": "https://api.osparc.io/v0/solvers/simcore%2Fservices%2Fdynamic%2Fsim4life/releases/8.0.0",
+                "version_display": "8.0.0",
             }
         },
     )
@@ -47,26 +50,42 @@ class Program(BaseService, ApiServerOutputSchema):
     @classmethod
     def create_from_image(cls, image_meta: ServiceMetaDataPublished) -> "Program":
         data = image_meta.model_dump(
-            include={"name", "key", "version", "description", "contact"},
+            include={
+                "name",
+                "key",
+                "version",
+                "description",
+                "contact",
+                "version_display",
+            },
         )
         return cls(
             id=data.pop("key"),
             version=data.pop("version"),
             title=data.pop("name"),
             url=None,
+            version_display=data.pop("version_display"),
             **data,
         )
 
     @classmethod
     def create_from_service(cls, service: ServiceGetV2) -> "Program":
         data = service.model_dump(
-            include={"name", "key", "version", "description", "contact"},
+            include={
+                "name",
+                "key",
+                "version",
+                "description",
+                "contact",
+                "version_display",
+            },
         )
         return cls(
             id=data.pop("key"),
             version=data.pop("version"),
             title=data.pop("name"),
             url=None,
+            version_display=data.pop("version_display"),
             **data,
         )
 
