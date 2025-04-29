190be9ab7e19d1515cc24f276e27696580c1774a
Mads Bisgaard <126242332+bisgaard-itis@users.noreply.github.com>
2025-04-29 14:08:26 +0200

    ✨🎨 Remove all http catalog client from api-server and introduce listing of programs endpoint (#7575)

--- services/api-server/src/simcore_service_api_server/api/dependencies/services.py
+++ services/api-server/src/simcore_service_api_server/api/dependencies/services.py
@@ -1,6 +1,5 @@
-""" Dependences with any other services (except webserver)
+"""Dependences with any other services (except webserver)"""
 
-"""
 from collections.abc import Callable
 
 from fastapi import HTTPException, Request, status
@@ -15,7 +14,6 @@ def get_api_client(client_type: type[BaseServiceClientApi]) -> Callable:
     Usage:
 
         director_client: DirectorApi = Depends(get_api_client(DirectorApi)),
-        catalog_client: CatalogApi = Depends(get_api_client(CatalogApi)),
         storage_client: StorageApi = Depends(get_api_client(StorageApi)),
     """
     assert issubclass(client_type, BaseServiceClientApi)  # nosec
--- services/api-server/src/simcore_service_api_server/api/routes/health.py
+++ services/api-server/src/simcore_service_api_server/api/routes/health.py
@@ -10,7 +10,6 @@ from servicelib.aiohttp import status
 
 from ..._meta import API_VERSION, PROJECT_NAME
 from ...core.health_checker import ApiServerHealthChecker, get_health_checker
-from ...services_http.catalog import CatalogApi
 from ...services_http.director_v2 import DirectorV2Api
 from ...services_http.storage import StorageApi
 from ...services_http.webserver import WebserverApi
@@ -22,7 +21,7 @@ router = APIRouter()
 
 @router.get("/", include_in_schema=False, response_class=PlainTextResponse)
 async def check_service_health(
-    health_checker: Annotated[ApiServerHealthChecker, Depends(get_health_checker)]
+    health_checker: Annotated[ApiServerHealthChecker, Depends(get_health_checker)],
 ):
     if not health_checker.healthy:
         raise HTTPException(
@@ -39,14 +38,12 @@ async def check_service_health(
     response_model_exclude_unset=True,
 )
 async def get_service_state(
-    catalog_client: Annotated[CatalogApi, Depends(get_api_client(CatalogApi))],
     director2_api: Annotated[DirectorV2Api, Depends(get_api_client(DirectorV2Api))],
     storage_client: Annotated[StorageApi, Depends(get_api_client(StorageApi))],
     webserver_client: Annotated[WebserverApi, Depends(get_api_client(WebserverApi))],
     url_for: Annotated[Callable, Depends(get_reverse_url_mapper)],
 ):
     apis = (
-        catalog_client,
         director2_api,
         storage_client,
         webserver_client,
