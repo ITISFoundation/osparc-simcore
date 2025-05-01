c174aea196949798392a077a22c100bc274eba99
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-05-01 10:37:03 +0200

    clean

--- services/dask-sidecar/src/simcore_service_dask_sidecar/settings.py
+++ services/dask-sidecar/src/simcore_service_dask_sidecar/settings.py
@@ -4,14 +4,12 @@ from typing import Annotated, Any
 from models_library.basic_types import LogLevel
 from pydantic import AliasChoices, Field, field_validator
 from servicelib.logging_utils_filtering import LoggerName, MessageSubstring
-from settings_library.base import BaseCustomSettings
+from settings_library.application import BaseApplicationSettings
 from settings_library.utils_logging import MixinLoggingSettings
 
 
-class ApplicationSettings(BaseCustomSettings, MixinLoggingSettings):
-    SC_BUILD_TARGET: str | None = None
-    SC_BOOT_MODE: str | None = None
-    LOG_LEVEL: Annotated[
+class ApplicationSettings(BaseApplicationSettings, MixinLoggingSettings):
+    DASK_SIDECAR_LOGLEVEL: Annotated[
         LogLevel,
         Field(
             validation_alias=AliasChoices(
@@ -23,7 +21,7 @@ class ApplicationSettings(BaseCustomSettings, MixinLoggingSettings):
     SIDECAR_COMP_SERVICES_SHARED_VOLUME_NAME: str
     SIDECAR_COMP_SERVICES_SHARED_FOLDER: Path
 
-    SIDECAR_INTERVAL_TO_CHECK_TASK_ABORTED_S: int | None = 5
+    DASK_SIDECAR_INTERVAL_TO_CHECK_TASK_ABORTED_S: int | None = 5
 
     DASK_START_AS_SCHEDULER: bool | None = Field(
         default=False, description="If this env is set, then the app boots as scheduler"
@@ -48,16 +46,7 @@ class ApplicationSettings(BaseCustomSettings, MixinLoggingSettings):
         description="is a dictionary that maps specific loggers (such as 'uvicorn.access' or 'gunicorn.access') to a list of log message patterns that should be filtered out.",
     )
 
-    def as_scheduler(self) -> bool:
-        return bool(self.DASK_START_AS_SCHEDULER)
-
-    def as_worker(self) -> bool:
-        as_worker = not self.as_scheduler()
-        if as_worker:
-            assert self.DASK_SCHEDULER_HOST is not None  # nosec
-        return as_worker
-
-    @field_validator("LOG_LEVEL", mode="before")
+    @field_validator("DASK_SIDECAR_LOGLEVEL", mode="before")
     @classmethod
     def _validate_loglevel(cls, value: Any) -> str:
         return cls.validate_log_level(f"{value}")
