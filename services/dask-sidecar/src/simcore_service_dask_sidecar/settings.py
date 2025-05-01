f61b4e05743369bb2610934d5b0a3015c79a3283
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-05-01 10:41:54 +0200

    added RABBIT_MQ

--- services/dask-sidecar/src/simcore_service_dask_sidecar/settings.py
+++ services/dask-sidecar/src/simcore_service_dask_sidecar/settings.py
@@ -5,6 +5,7 @@ from models_library.basic_types import LogLevel
 from pydantic import AliasChoices, Field, field_validator
 from servicelib.logging_utils_filtering import LoggerName, MessageSubstring
 from settings_library.application import BaseApplicationSettings
+from settings_library.rabbit import RabbitSettings
 from settings_library.utils_logging import MixinLoggingSettings
 
 
@@ -23,28 +24,42 @@ class ApplicationSettings(BaseApplicationSettings, MixinLoggingSettings):
 
     DASK_SIDECAR_INTERVAL_TO_CHECK_TASK_ABORTED_S: int | None = 5
 
-    DASK_START_AS_SCHEDULER: bool | None = Field(
-        default=False, description="If this env is set, then the app boots as scheduler"
-    )
+    DASK_START_AS_SCHEDULER: Annotated[
+        bool | None,
+        Field(description="If this env is set, then the app boots as scheduler"),
+    ] = False
 
-    DASK_SCHEDULER_HOST: str | None = Field(
-        None,
-        description="Address of the scheduler to register (only if started as worker )",
-    )
-
-    DASK_LOG_FORMAT_LOCAL_DEV_ENABLED: bool = Field(
-        default=False,
-        validation_alias=AliasChoices(
-            "DASK_LOG_FORMAT_LOCAL_DEV_ENABLED",
-            "LOG_FORMAT_LOCAL_DEV_ENABLED",
+    DASK_SCHEDULER_HOST: Annotated[
+        str | None,
+        Field(
+            description="Address of the scheduler to register (only if started as worker )",
         ),
-        description="Enables local development log format. WARNING: make sure it is disabled if you want to have structured logs!",
-    )
-    DASK_LOG_FILTER_MAPPING: dict[LoggerName, list[MessageSubstring]] = Field(
-        default_factory=dict,
-        validation_alias=AliasChoices("DASK_LOG_FILTER_MAPPING", "LOG_FILTER_MAPPING"),
-        description="is a dictionary that maps specific loggers (such as 'uvicorn.access' or 'gunicorn.access') to a list of log message patterns that should be filtered out.",
-    )
+    ] = None
+
+    DASK_LOG_FORMAT_LOCAL_DEV_ENABLED: Annotated[
+        bool,
+        Field(
+            validation_alias=AliasChoices(
+                "DASK_LOG_FORMAT_LOCAL_DEV_ENABLED",
+                "LOG_FORMAT_LOCAL_DEV_ENABLED",
+            ),
+            description="Enables local development log format. WARNING: make sure it is disabled if you want to have structured logs!",
+        ),
+    ] = False
+    DASK_LOG_FILTER_MAPPING: Annotated[
+        dict[LoggerName, list[MessageSubstring]],
+        Field(
+            default_factory=dict,
+            validation_alias=AliasChoices(
+                "DASK_LOG_FILTER_MAPPING", "LOG_FILTER_MAPPING"
+            ),
+            description="is a dictionary that maps specific loggers (such as 'uvicorn.access' or 'gunicorn.access') to a list of log message patterns that should be filtered out.",
+        ),
+    ]
+
+    DASK_SIDECAR_RABBITMQ: Annotated[
+        RabbitSettings | None, Field(json_schema_extra={"auto_default_from_env": True})
+    ]
 
     @field_validator("DASK_SIDECAR_LOGLEVEL", mode="before")
     @classmethod
