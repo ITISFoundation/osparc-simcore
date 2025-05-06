547cbcbb0a51c5e66cac7bfa84a604deb77abc32
matusdrobuliak66 <drobuliak66@gmail.com>
2025-05-06 14:10:30 +0200

    review @sanderegg

--- services/director-v2/src/simcore_service_director_v2/utils/computations.py
+++ services/director-v2/src/simcore_service_director_v2/utils/computations.py
@@ -26,7 +26,7 @@ _TASK_TO_PIPELINE_CONVERSIONS = {
         RunningState.NOT_STARTED,
         RunningState.WAITING_FOR_CLUSTER,
     ): RunningState.WAITING_FOR_CLUSTER,
-    # if there are tasks waiting for resources, then the pipeline is also waiting for resources
+    # if there are tasks waiting for resources and nothing is running/pending, then the pipeline is also waiting for resources
     (
         RunningState.PUBLISHED,
         RunningState.NOT_STARTED,
