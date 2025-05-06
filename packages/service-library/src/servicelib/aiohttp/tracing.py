0f685de753526c5a781409d06fed858f0e98c484
Mads Bisgaard <bisgaard@itis.swiss>
2025-05-06 14:11:16 +0200

    autoinstrument aiopika in aihttp case

--- packages/service-library/src/servicelib/aiohttp/tracing.py
+++ packages/service-library/src/servicelib/aiohttp/tracing.py
@@ -1,6 +1,4 @@
-""" Adds aiohttp middleware for tracing using opentelemetry instrumentation.
-
-"""
+"""Adds aiohttp middleware for tracing using opentelemetry instrumentation."""
 
 import logging
 
@@ -44,6 +42,13 @@ try:
 except ImportError:
     HAS_REQUESTS = False
 
+try:
+    from opentelemetry.instrumentation.aio_pika import AioPikaInstrumentor
+
+    HAS_AIO_PIKA = True
+except ImportError:
+    HAS_AIO_PIKA = False
+
 
 def setup_tracing(
     app: web.Application,
@@ -74,7 +79,9 @@ def setup_tracing(
     trace.set_tracer_provider(TracerProvider(resource=resource))
     tracer_provider: trace.TracerProvider = trace.get_tracer_provider()
 
-    tracing_destination: str = f"{URL(opentelemetry_collector_endpoint).with_port(opentelemetry_collector_port).with_path('/v1/traces')}"
+    tracing_destination: str = (
+        f"{URL(opentelemetry_collector_endpoint).with_port(opentelemetry_collector_port).with_path('/v1/traces')}"
+    )
 
     _logger.info(
         "Trying to connect service %s to tracing collector at %s.",
@@ -128,3 +135,11 @@ def setup_tracing(
             msg="Attempting to add requests opentelemetry autoinstrumentation...",
         ):
             RequestsInstrumentor().instrument()
+
+    if HAS_AIO_PIKA:
+        with log_context(
+            _logger,
+            logging.INFO,
+            msg="Attempting to add aio_pika opentelemetry autoinstrumentation...",
+        ):
+            AioPikaInstrumentor().instrument()
