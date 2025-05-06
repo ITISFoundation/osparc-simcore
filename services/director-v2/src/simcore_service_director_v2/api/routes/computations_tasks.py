9442bfd93f2dab5fc135b52b7b97a963eb09a5ec
matusdrobuliak66 <drobuliak66@gmail.com>
2025-05-06 11:08:54 +0200

    fix

--- services/director-v2/src/simcore_service_director_v2/api/routes/computations_tasks.py
+++ services/director-v2/src/simcore_service_director_v2/api/routes/computations_tasks.py
@@ -154,7 +154,7 @@ async def get_task_log_file(
     comp_tasks_repo: Annotated[
         CompTasksRepository, Depends(get_repository(CompTasksRepository))
     ],
-) -> TaskLogFileGet:
+) -> TaskLogFileGet:  # MATUS CHECK
     """Returns a link to download logs file of a give task.
     The log is only available when the task is done
     """
