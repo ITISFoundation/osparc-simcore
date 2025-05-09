1169f279ecaf5ddabd4062a611034e16b322ed53
odeimaiz <maiz@itis.swiss>
2025-05-09 16:54:25 +0200

    exposedInputs, exposedOutputs

--- services/static-webserver/client/source/class/osparc/study/CreateFunction.js
+++ services/static-webserver/client/source/class/osparc/study/CreateFunction.js
@@ -56,18 +56,9 @@ qx.Class.define("osparc.study.CreateFunction", {
       });
       form.add(description, this.tr("Description"), null, "description");
 
-      const createFunctionBtn = this.__createFunctionBtn = new osparc.ui.form.FetchButton().set({
-        appearance: "strong-button",
-        label: this.tr("Create"),
-        allowGrowX: false,
-        alignX: "right"
-      });
-      createFunctionBtn.addListener("execute", () => {
-        if (this.__form.validate()) {
-          this.__createFunction();
-        }
-      }, this);
 
+      const exposedInputs = {};
+      const exposedOutputs = {};
 
       // INPUTS
       const inGrid = new qx.ui.layout.Grid(10, 6);
@@ -128,10 +119,12 @@ qx.Class.define("osparc.study.CreateFunction", {
           row,
           column,
         });
+        exposedInputs[filePicker["label"]] = true;
+        fpExposed.addListener("changeValue", e => exposedInputs[filePicker["label"]] = e.getData());
         column++;
 
         const outputValue = osparc.file.FilePicker.getOutput(filePicker);
-        const fpDefaultValue = new qx.ui.basic.Label(outputValue && outputValue["path"] ? outputValue["path"] : null);
+        const fpDefaultValue = new qx.ui.basic.Label(outputValue && outputValue["path"] ? outputValue["path"] : "");
         inputsLayout.add(fpDefaultValue, {
           row,
           column,
@@ -166,6 +159,8 @@ qx.Class.define("osparc.study.CreateFunction", {
           row,
           column,
         });
+        exposedInputs[parameter["label"]] = true;
+        parameterExposed.addListener("changeValue", e => exposedInputs[parameter["label"]] = e.getData());
         column++;
 
         const parameterDefaultValue = new qx.ui.basic.Label(String(osparc.service.Utils.getParameterValue(parameter)));
@@ -179,6 +174,7 @@ qx.Class.define("osparc.study.CreateFunction", {
         row++;
       });
 
+
       // OUTPUTS
       const outGrid = new qx.ui.layout.Grid(10, 6);
       const outputsLayout = new qx.ui.container.Composite(outGrid).set({
@@ -189,24 +185,75 @@ qx.Class.define("osparc.study.CreateFunction", {
       this._add(outputsLayout);
 
       // header
+      row = 0;
+      column = 0;
       const nameLabel2 = new qx.ui.basic.Label(this.tr("Output"));
       outputsLayout.add(nameLabel2, {
-        row: 0,
-        column: 0,
+        row,
+        column,
       });
+      column++;
       const typeLabel2 = new qx.ui.basic.Label(this.tr("Type"));
       outputsLayout.add(typeLabel2, {
-        row: 0,
-        column: 1,
+        row,
+        column,
       });
+      column++;
       const exposedLabel2 = new qx.ui.basic.Label(this.tr("Exposed"));
       outputsLayout.add(exposedLabel2, {
-        row: 0,
-        column: 2,
+        row,
+        column,
       });
+      column++;
+      column = 0;
+      row++;
+
+      const probes = osparc.study.Utils.extractProbes(this.__studyData["workbench"]);
+      probes.forEach(probe => {
+        const parameterLabel = new qx.ui.basic.Label(probe["label"]);
+        outputsLayout.add(parameterLabel, {
+          row,
+          column,
+        });
+        column++;
+
+        const probeMetadata = osparc.store.Services.getMetadata(probe["key"], probe["version"]);
+        if (probeMetadata) {
+          const probeType = new qx.ui.basic.Label(osparc.service.Utils.getProbeType(probeMetadata));
+          outputsLayout.add(probeType, {
+            row,
+            column,
+          });
+        }
+        column++;
+
+        const probeExposed = new qx.ui.form.CheckBox().set({ value: true });
+        outputsLayout.add(probeExposed, {
+          row,
+          column,
+        });
+        exposedOutputs[probe["label"]] = true;
+        probeExposed.addListener("changeValue", e => exposedOutputs[probe["label"]] = e.getData());
+        column++;
+
+        column = 0;
+        row++;
+      });
+
+      const createFunctionBtn = this.__createFunctionBtn = new osparc.ui.form.FetchButton().set({
+        appearance: "strong-button",
+        label: this.tr("Create"),
+        allowGrowX: false,
+        alignX: "right"
+      });
+      createFunctionBtn.addListener("execute", () => {
+        if (this.__form.validate()) {
+          this.__createFunction(exposedInputs, exposedOutputs);
+        }
+      }, this);
     },
 
-    __createFunction: function() {
+    __createFunction: function(exposedInputs, exposedOutputs) {
       this.__createFunctionBtn.setFetching(true);
 
       // first publish it as a template
@@ -225,7 +272,7 @@ qx.Class.define("osparc.study.CreateFunction", {
         .then(task => {
           task.addListener("resultReceived", e => {
             const templateData = e.getData();
-            this.__doCreateFunction(templateData);
+            this.__doCreateFunction(templateData, exposedInputs, exposedOutputs);
           });
         })
         .catch(err => {
@@ -234,7 +281,7 @@ qx.Class.define("osparc.study.CreateFunction", {
         });
     },
 
-    __doCreateFunction: function(templateData) {
+    __doCreateFunction: function(templateData, exposedInputs, exposedOutputs) {
       const nameField = this.__form.getItem("name");
       const descriptionField = this.__form.getItem("description");
 
@@ -261,33 +308,39 @@ qx.Class.define("osparc.study.CreateFunction", {
       const filePickers = osparc.study.Utils.extractFilePickers(templateData["workbench"]);
       filePickers.forEach(filePicker => {
         const fpLabel = filePicker["label"];
-        functionData["input_schema"]["schema_dict"]["properties"][fpLabel] = {
-          "type": "FileID",
-        };
-        const outputValue = osparc.file.FilePicker.getOutput(filePicker);
-        functionData["default_inputs"][fpLabel] = outputValue && outputValue["path"] ? outputValue["path"] : null;
+        if (exposedInputs[fpLabel]) {
+          functionData["input_schema"]["schema_dict"]["properties"][fpLabel] = {
+            "type": "FileID",
+          };
+          const outputValue = osparc.file.FilePicker.getOutput(filePicker);
+          functionData["default_inputs"][fpLabel] = outputValue && outputValue["path"] ? outputValue["path"] : null;
+        }
       });
 
       const parameters = osparc.study.Utils.extractParameters(templateData["workbench"]);
       parameters.forEach(parameter => {
         const parameterLabel = parameter["label"];
-        const parameterMetadata = osparc.store.Services.getMetadata(parameter["key"], parameter["version"]);
-        if (parameterMetadata) {
-          functionData["input_schema"]["schema_dict"]["properties"][parameterLabel] = {
-            "type": osparc.service.Utils.getParameterType(parameterMetadata),
-          };
+        if (exposedInputs[parameterLabel]) {
+          const parameterMetadata = osparc.store.Services.getMetadata(parameter["key"], parameter["version"]);
+          if (parameterMetadata) {
+            functionData["input_schema"]["schema_dict"]["properties"][parameterLabel] = {
+              "type": osparc.service.Utils.getParameterType(parameterMetadata),
+            };
+          }
+          functionData["default_inputs"][parameterLabel] = osparc.service.Utils.getParameterValue(parameter);
         }
-        functionData["default_inputs"][parameterLabel] = osparc.service.Utils.getParameterValue(parameter);
       });
 
       const probes = osparc.study.Utils.extractProbes(templateData["workbench"]);
       probes.forEach(probe => {
         const probeLabel = probe["label"];
-        const probeMetadata = osparc.store.Services.getMetadata(probe["key"], probe["version"]);
-        if (probeMetadata) {
-          functionData["output_schema"]["schema_dict"]["properties"][probeLabel] = {
-            "type": osparc.service.Utils.getProbeType(probeMetadata),
-          };
+        if (exposedOutputs[probeLabel]) {
+          const probeMetadata = osparc.store.Services.getMetadata(probe["key"], probe["version"]);
+          if (probeMetadata) {
+            functionData["output_schema"]["schema_dict"]["properties"][probeLabel] = {
+              "type": osparc.service.Utils.getProbeType(probeMetadata),
+            };
+          }
         }
       });
 
