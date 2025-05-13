26a01e1366099848e686a6804a7325391282752a
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-05-13 12:25:40 +0200

    fix

--- services/director-v2/tests/integration/02/test_dynamic_sidecar_nodeports_integration.py
+++ services/director-v2/tests/integration/02/test_dynamic_sidecar_nodeports_integration.py
@@ -265,6 +265,7 @@ async def current_study(
     fake_dy_workbench: dict[str, Any],
     async_client: httpx.AsyncClient,
     osparc_product_name: str,
+    osparc_product_api_base_url: str,
     create_pipeline: Callable[..., Awaitable[ComputationGet]],
 ) -> ProjectAtDB:
     project_at_db = await project(current_user, workbench=fake_dy_workbench)
@@ -276,7 +277,7 @@ async def current_study(
         user_id=current_user["id"],
         start_pipeline=False,
         product_name=osparc_product_name,
-        product_api_base_url=osparc_product_name,
+        product_api_base_url=osparc_product_api_base_url,
     )
 
     return project_at_db
