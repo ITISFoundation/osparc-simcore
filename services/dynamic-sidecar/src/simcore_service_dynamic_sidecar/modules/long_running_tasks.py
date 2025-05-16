57d25187311d869a41764f2743b75f14410cdb50
Andrei Neagu <neagu@itis.swiss>
2025-05-16 09:45:30 +0200

    decompressing legacy state path

--- services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/modules/long_running_tasks.py
+++ services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/modules/long_running_tasks.py
@@ -43,7 +43,7 @@ from ..core.rabbitmq import (
     post_progress_message,
     post_sidecar_log_message,
 )
-from ..core.settings import ApplicationSettings
+from ..core.settings import ApplicationSettings, LegacyState
 from ..core.utils import CommandResult
 from ..core.validation import parse_compose_spec
 from ..models.schemas.application_health import ApplicationHealth
@@ -337,6 +337,19 @@ def _get_satate_folders_size(paths: list[Path]) -> int:
     return total_size
 
 
+def _get_legacy_state_with_dy_volumes_path(
+    settings: ApplicationSettings,
+) -> LegacyState | None:
+    legacy_state = settings.DY_SIDECAR_LEGACY_STATE
+    if legacy_state is None:
+        return None
+    dy_volumes = settings.DYNAMIC_SIDECAR_DY_VOLUMES_MOUNT_DIR
+    return LegacyState(
+        old_state_path=dy_volumes / legacy_state.old_state_path.relative_to("/"),
+        new_state_path=dy_volumes / legacy_state.new_state_path.relative_to("/"),
+    )
+
+
 async def _restore_state_folder(
     app: FastAPI,
     *,
@@ -348,13 +361,14 @@ async def _restore_state_folder(
         user_id=settings.DY_SIDECAR_USER_ID,
         project_id=settings.DY_SIDECAR_PROJECT_ID,
         node_uuid=settings.DY_SIDECAR_NODE_ID,
-        destination_path=state_path,
+        destination_path=Path(state_path),
         io_log_redirect_cb=functools.partial(
             post_sidecar_log_message, app, log_level=logging.INFO
         ),
         r_clone_settings=settings.DY_SIDECAR_R_CLONE_SETTINGS,
         progress_bar=progress_bar,
         aws_s3_cli_settings=settings.DY_SIDECAR_AWS_S3_CLI_SETTINGS,
+        legacy_state=_get_legacy_state_with_dy_volumes_path(settings),
     )
 
 
@@ -429,6 +443,7 @@ async def _save_state_folder(
         ),
         progress_bar=progress_bar,
         aws_s3_cli_settings=settings.DY_SIDECAR_AWS_S3_CLI_SETTINGS,
+        legacy_state=_get_legacy_state_with_dy_volumes_path(settings),
     )
 
 
