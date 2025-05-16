9f5d9aabf4d3b962daae3abfe58b7236bf2f4373
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-05-16 15:57:16 +0200

    sending events about task status

--- services/dask-sidecar/src/simcore_service_dask_sidecar/task_life_cycle_worker_plugin.py
+++ services/dask-sidecar/src/simcore_service_dask_sidecar/task_life_cycle_worker_plugin.py
@@ -3,8 +3,10 @@ from collections.abc import Awaitable
 from typing import Any
 
 from dask.typing import Key
-from distributed import Worker, WorkerPlugin
-from distributed.scheduler import TaskStateState
+from dask_task_models_library.models import TASK_LIFE_CYCLE_EVENT, TaskLifeCycleState
+from distributed import WorkerPlugin
+from distributed.worker import Worker
+from distributed.worker_state_machine import TaskStateState
 from servicelib.logging_utils import log_context
 
 _logger = logging.getLogger(__name__)
@@ -17,7 +19,7 @@ class TaskLifecycleWorkerPlugin(WorkerPlugin):
             logging.INFO,
             "TaskLifecycleWorkerPlugin init",
         ):
-            self.worker = None
+            self._worker = None
 
     def setup(self, worker: Worker) -> Awaitable[None]:
         async def _() -> None:
@@ -26,7 +28,8 @@ class TaskLifecycleWorkerPlugin(WorkerPlugin):
                 logging.INFO,
                 "TaskLifecycleWorkerPlugin start",
             ):
-                self.worker = worker
+                assert worker  # nosec
+                self._worker = worker
 
         return _()
 
@@ -37,19 +40,15 @@ class TaskLifecycleWorkerPlugin(WorkerPlugin):
         finish: TaskStateState,
         **kwargs: Any,
     ):
-        # Start state: one of released, waiting, processing, memory, error
         with log_context(
             _logger,
             logging.INFO,
-            f"Task {key} transition from {start} to {finish}",
+            f"Task {key!r} transition from {start} to {finish}",
         ):
-            assert self.worker  # nosec
-            self.worker.log_event(
-                f"task-lifecycle-{key}",
-                {
-                    "key": key,
-                    "worker": kwargs.get("worker"),
-                    "start": start,
-                    "finish": finish,
-                },
+            assert self._worker  # nosec
+            self._worker.log_event(
+                TASK_LIFE_CYCLE_EVENT.format(key=key),
+                TaskLifeCycleState.from_worker_task_state(
+                    key, kwargs.get("worker"), finish
+                ).model_dump(mode="json"),
             )
