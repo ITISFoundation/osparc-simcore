941817cb14c18f0c038fe275b859ee80c73707a2
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-05-19 15:30:40 +0200

    @mguidon review: skip tests to enforece master

--- services/catalog/tests/unit/with_dbs/test_api_rpc.py
+++ services/catalog/tests/unit/with_dbs/test_api_rpc.py
@@ -155,6 +155,9 @@ async def test_rpc_list_services_paginated_with_filters(
     assert page.meta.total == 0
 
 
+@pytest.skip(
+    reason="Issue with mocked_director_rest_api fixture. Urgent feature in master needed. Will follow up."
+)
 async def test_rpc_list_services_paginated_with_filter_combinations(
     background_sync_task_mocked: None,
     mocked_director_rest_api: MockRouter,
