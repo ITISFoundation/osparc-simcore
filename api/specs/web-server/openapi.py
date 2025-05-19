d6ae5e6871c4a604deb5eab787e6551d7012517d
matusdrobuliak66 <drobuliak66@gmail.com>
2025-05-19 13:56:16 +0200

    migration

--- api/specs/web-server/openapi.py
+++ api/specs/web-server/openapi.py
@@ -44,7 +44,6 @@ openapi_modules = [
         "_nih_sparc_redirections",
         "_projects",
         "_projects_access_rights",
-        "_projects_comments",
         "_projects_conversations",
         "_projects_folders",
         "_projects_metadata",
--- packages/postgres-database/src/simcore_postgres_database/models/tokens.py
+++ packages/postgres-database/src/simcore_postgres_database/models/tokens.py
@@ -1,6 +1,7 @@
-""" User Tokens table
-"""
+"""User Tokens table"""
+
 import sqlalchemy as sa
+from simcore_postgres_database.models._common import RefActions
 
 from .base import metadata
 from .users import users
@@ -10,7 +11,14 @@ tokens = sa.Table(
     "tokens",
     metadata,
     sa.Column("token_id", sa.BigInteger, nullable=False, primary_key=True),
-    sa.Column("user_id", sa.BigInteger, sa.ForeignKey(users.c.id), nullable=False),
+    sa.Column(
+        "user_id",
+        sa.BigInteger,
+        sa.ForeignKey(
+            users.c.id, onupdate=RefActions.CASCADE, ondelete=RefActions.CASCADE
+        ),
+        nullable=False,
+    ),
     sa.Column("token_service", sa.String, nullable=False),
     sa.Column("token_data", sa.JSON, nullable=False),
 )
