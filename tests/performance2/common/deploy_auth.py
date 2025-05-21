8d45efdcb2ff1d0426bbc8ece6cd237053d7fad3
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-05-21 23:05:55 +0200

    optimizing

--- tests/performance2/common/deploy_auth.py
+++ tests/performance2/common/deploy_auth.py
@@ -6,3 +6,6 @@ class MonitoringBasicAuth(BaseSettings):
     model_config = SettingsConfigDict(extra="ignore")
     SC_USER_NAME: str = Field(default=..., examples=["<your username>"])
     SC_PASSWORD: str = Field(default=..., examples=["<your password>"])
+
+    def to_auth(self) -> tuple[str, str]:
+        return (self.SC_USER_NAME, self.SC_PASSWORD)
