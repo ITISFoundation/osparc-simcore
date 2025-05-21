c7cc77a162ed0b4a4945aa851154d20dc592a916
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-05-21 09:04:35 +0200

    minor

---
+++ tests/performance2/deployment_max_rps_test.py
@@ -0,0 +1,39 @@
+#
+# SEE https://docs.locust.io/en/stable/quickstart.html
+#
+
+import logging
+
+import locust_plugins
+from locust import task
+from locust.contrib.fasthttp import FastHttpUser
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+logging.basicConfig(level=logging.INFO)
+
+
+# NOTE: 'import locust_plugins' is necessary to use --check-fail-ratio
+# this assert is added to avoid that pycln pre-commit hook does not
+# remove the import (the tool assumes the import is not necessary)
+assert locust_plugins  # nosec
+
+
+class MonitoringBasicAuth(BaseSettings):
+    model_config = SettingsConfigDict(extra="ignore")
+    SC_USER_NAME: str = Field(default=..., examples=["<your username>"])
+    SC_PASSWORD: str = Field(default=..., examples=["<your password>"])
+
+
+class WebApiUser(FastHttpUser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        _auth = MonitoringBasicAuth()
+        self.auth = (
+            _auth.SC_USER_NAME,
+            _auth.SC_PASSWORD,
+        )
+
+    @task
+    def get_root(self):
+        self.client.get("", auth=self.auth)
