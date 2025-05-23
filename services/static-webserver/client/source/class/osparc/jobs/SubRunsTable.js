3877fa66ff3ef3b8b7a64c64786ea36ce71e4746
odeimaiz <maiz@itis.swiss>
2025-05-23 10:58:16 +0200

    .logs -> .zip

--- services/static-webserver/client/source/class/osparc/jobs/SubRunsTable.js
+++ services/static-webserver/client/source/class/osparc/jobs/SubRunsTable.js
@@ -173,7 +173,7 @@ qx.Class.define("osparc.jobs.SubRunsTable", {
           }
           const logDownloadLink = subJob.getLogDownloadLink()
           if (logDownloadLink) {
-            osparc.utils.Utils.downloadLink(logDownloadLink, "GET", rowData["nodeName"] + ".logs");
+            osparc.utils.Utils.downloadLink(logDownloadLink, "GET", rowData["nodeName"] + ".zip");
           } else {
             osparc.FlashMessenger.logAs(this.tr("No logs available"), "WARNING");
           }
