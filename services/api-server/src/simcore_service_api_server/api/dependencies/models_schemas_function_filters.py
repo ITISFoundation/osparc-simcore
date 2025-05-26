e22c9f952f43470ebe5c419fa3ef04d0c1eedd3c
Werner Van Geit <werner.vangeit@gmail.com>
2025-05-26 10:10:52 +0200

    Fix typecheck in functions

--- services/api-server/src/simcore_service_api_server/api/dependencies/models_schemas_function_filters.py
+++ services/api-server/src/simcore_service_api_server/api/dependencies/models_schemas_function_filters.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 from fastapi import Query
-from models_library.functions import FunctionJobCollectionsListFilters
+from models_library.functions import FunctionIDString, FunctionJobCollectionsListFilters
 
 from ._utils import _get_query_params
 
@@ -9,7 +9,7 @@ from ._utils import _get_query_params
 def get_function_job_collections_filters(
     # pylint: disable=unsubscriptable-object
     has_function_id: Annotated[
-        str | None,
+        FunctionIDString | None,
         Query(
             **_get_query_params(
                 FunctionJobCollectionsListFilters.model_fields["has_function_id"]
