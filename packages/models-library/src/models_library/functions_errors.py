520f55d58a1e87e18bf9d7b1648eef6ae56220f0
Werner Van Geit <werner.vangeit@gmail.com>
2025-06-04 16:52:03 +0200

    Remove fastapi import from function models

--- packages/models-library/src/models_library/functions_errors.py
+++ packages/models-library/src/models_library/functions_errors.py
@@ -1,5 +1,4 @@
 from common_library.errors_classes import OsparcErrorMixin
-from fastapi import status  # type: ignore
 
 
 class FunctionBaseError(OsparcErrorMixin, Exception):
@@ -10,93 +9,93 @@ class FunctionJobReadAccessDeniedError(FunctionBaseError):
     msg_template: str = (
         "Function job {function_job_id} read access denied for user {user_id}"
     )
-    status_code: int = status.HTTP_403_FORBIDDEN
+    status_code: int = 403  # Forbidden
 
 
 class FunctionIDNotFoundError(FunctionBaseError):
     msg_template: str = "Function {function_id} not found"
-    status_code: int = status.HTTP_404_NOT_FOUND
+    status_code: int = 404  # Not Found
 
 
 class FunctionJobIDNotFoundError(FunctionBaseError):
     msg_template: str = "Function job {function_job_id} not found"
-    status_code: int = status.HTTP_404_NOT_FOUND
+    status_code: int = 404  # Not Found
 
 
 class FunctionInputsValidationError(FunctionBaseError):
     msg_template: str = "Function inputs validation failed: {error}"
-    status_code: int = status.HTTP_422_UNPROCESSABLE_ENTITY
+    status_code: int = 422  # Unprocessable Entity
 
 
 class FunctionReadAccessDeniedError(FunctionBaseError):
     msg_template: str = "Function {function_id} read access denied for user {user_id}"
-    status_code: int = status.HTTP_403_FORBIDDEN
+    status_code: int = 403  # Forbidden
 
 
 class FunctionJobCollectionIDNotFoundError(FunctionBaseError):
     msg_template: str = "Function job collection {function_job_collection_id} not found"
-    status_code: int = status.HTTP_404_NOT_FOUND
+    status_code: int = 404  # Not Found
 
 
 class UnsupportedFunctionClassError(FunctionBaseError):
     msg_template: str = "Function class {function_class} is not supported"
-    status_code: int = status.HTTP_400_BAD_REQUEST
+    status_code: int = 400  # Bad Request
 
 
 class UnsupportedFunctionJobClassError(FunctionBaseError):
     msg_template: str = "Function job class {function_job_class} is not supported"
-    status_code: int = status.HTTP_400_BAD_REQUEST
+    status_code: int = 400  # Bad Request
 
 
 class UnsupportedFunctionFunctionJobClassCombinationError(FunctionBaseError):
     msg_template: str = (
         "Function class {function_class} and function job class {function_job_class} combination is not supported"
     )
-    status_code: int = status.HTTP_400_BAD_REQUEST
+    status_code: int = 400  # Bad Request
 
 
 class FunctionJobCollectionReadAccessDeniedError(FunctionBaseError):
     msg_template: str = (
         "Function job collection {function_job_collection_id} read access denied for user {user_id}"
     )
-    status_code: int = status.HTTP_403_FORBIDDEN
+    status_code: int = 403  # Forbidden
 
 
 class FunctionWriteAccessDeniedError(FunctionBaseError):
     msg_template: str = "Function {function_id} write access denied for user {user_id}"
-    status_code: int = status.HTTP_403_FORBIDDEN
+    status_code: int = 403  # Forbidden
 
 
 class FunctionJobWriteAccessDeniedError(FunctionBaseError):
     msg_template: str = (
         "Function job {function_job_id} write access denied for user {user_id}"
     )
-    status_code: int = status.HTTP_403_FORBIDDEN
+    status_code: int = 403  # Forbidden
 
 
 class FunctionJobCollectionWriteAccessDeniedError(FunctionBaseError):
     msg_template: str = (
         "Function job collection {function_job_collection_id} write access denied for user {user_id}"
     )
-    status_code: int = status.HTTP_403_FORBIDDEN
+    status_code: int = 403  # Forbidden
 
 
 class FunctionExecuteAccessDeniedError(FunctionBaseError):
     msg_template: str = (
         "Function {function_id} execute access denied for user {user_id}"
     )
-    status_code: int = status.HTTP_403_FORBIDDEN
+    status_code: int = 403  # Forbidden
 
 
 class FunctionJobExecuteAccessDeniedError(FunctionBaseError):
     msg_template: str = (
         "Function job {function_job_id} execute access denied for user {user_id}"
     )
-    status_code: int = status.HTTP_403_FORBIDDEN
+    status_code: int = 403  # Forbidden
 
 
 class FunctionJobCollectionExecuteAccessDeniedError(FunctionBaseError):
     msg_template: str = (
         "Function job collection {function_job_collection_id} execute access denied for user {user_id}"
     )
-    status_code: int = status.HTTP_403_FORBIDDEN
+    status_code: int = 403  # Forbidden
