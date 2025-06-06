716fd710b97d004417ef2a3abf54b1778fc874ba
odeimaiz <maiz@itis.swiss>
2025-06-06 13:37:11 +0200

    tokens for testers

--- services/static-webserver/client/source/class/osparc/product/Utils.js
+++ services/static-webserver/client/source/class/osparc/product/Utils.js
@@ -264,6 +264,10 @@ qx.Class.define("osparc.product.Utils", {
     },
 
     showPreferencesTokens: function() {
+      if (osparc.data.Permissions.getInstance().isTester()) {
+        return true;
+      }
+
       if (this.isProduct("s4llite") || this.isProduct("tis") || this.isProduct("tiplite")) {
         return false;
       }
