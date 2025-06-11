c30f80d3cd11013229e4a8d0c2b395cf70fa97cc
Andrei Neagu <neagu@itis.swiss>
2025-06-11 10:52:30 +0200

    removed some constants

--- packages/service-library/src/servicelib/long_running_tasks/constants.py
+++ packages/service-library/src/servicelib/long_running_tasks/constants.py
@@ -1,8 +1,6 @@
 from datetime import timedelta
 from typing import Final
 
-MINUTE: Final[int] = 60  # in secs
-HOUR: Final[int] = 60 * MINUTE  # in secs
 DEFAULT_POLL_INTERVAL_S: Final[float] = 1
 
 DEFAULT_STALE_TASK_CHECK_INTERVAL: Final[timedelta] = timedelta(minutes=1)
