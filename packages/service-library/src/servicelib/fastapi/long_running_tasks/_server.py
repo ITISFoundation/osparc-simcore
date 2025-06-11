221e051d3a2ac271e88938e3deecfdfbb3a8e5ef
Andrei Neagu <neagu@itis.swiss>
2025-06-11 09:30:33 +0200

    fixed failing tests

--- packages/service-library/src/servicelib/fastapi/long_running_tasks/_server.py
+++ packages/service-library/src/servicelib/fastapi/long_running_tasks/_server.py
@@ -44,7 +44,7 @@ def setup(
     async def on_shutdown() -> None:
         if app.state.long_running_task_manager:
             task_manager: TasksManager = app.state.long_running_task_manager
-            await task_manager.close()
+            await task_manager.teardown()
 
     app.add_event_handler("startup", on_startup)
     app.add_event_handler("shutdown", on_shutdown)
