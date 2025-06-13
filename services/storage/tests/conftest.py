22513d879dba9dbb54b9b7c0b840a1aefe62021d
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-06-13 12:48:31 +0200

    fix: mock

--- services/storage/tests/conftest.py
+++ services/storage/tests/conftest.py
@@ -975,7 +975,7 @@ def celery_config() -> dict[str, Any]:
 def mock_celery_app(mocker: MockerFixture, celery_config: dict[str, Any]) -> Celery:
     celery_app = Celery(**celery_config)
 
-    for module in ("celery_library.common.create_app",):
+    for module in ("simcore_service_storage.modules.celery.create_app",):
         mocker.patch(module, return_value=celery_app)
 
     return celery_app
