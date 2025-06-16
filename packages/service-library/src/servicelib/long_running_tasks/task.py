56d08bc85034039629baced4c15912b9fdd92cc1
Andrei Neagu <neagu@itis.swiss>
2025-06-16 14:28:42 +0200

    revert

--- packages/service-library/src/servicelib/long_running_tasks/task.py
+++ packages/service-library/src/servicelib/long_running_tasks/task.py
@@ -35,8 +35,7 @@ TaskContext: TypeAlias = dict[str, Any]
 
 
 async def _await_task(task: asyncio.Task) -> None:
-    with log_catch(_logger):
-        await task
+    await task
 
 
 def _get_tasks_to_remove(
