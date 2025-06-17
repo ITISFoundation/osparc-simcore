1c2800918f96322e9c7e1855f4b680fcece5cde9
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-06-17 21:53:15 +0200

    fix: pydantic types

--- services/storage/src/simcore_service_storage/api/_worker_tasks/tasks.py
+++ services/storage/src/simcore_service_storage/api/_worker_tasks/tasks.py
@@ -2,10 +2,15 @@ import logging
 
 from celery import Celery  # type: ignore[import-untyped]
 from celery_library.task import register_task
-from celery_library.types import register_celery_types
+from celery_library.types import register_celery_types, register_pydantic_types
 from models_library.api_schemas_storage.export_data_async_jobs import AccessRightError
+from models_library.api_schemas_storage.storage_schemas import (
+    FileUploadCompletionBody,
+    FoldersBody,
+)
 from servicelib.logging_utils import log_context
 
+from ...models import FileMetaData
 from ._files import complete_upload_file
 from ._paths import compute_path_size, delete_paths
 from ._simcore_s3 import deep_copy_files_from_project, export_data
@@ -15,6 +20,7 @@ _logger = logging.getLogger(__name__)
 
 def setup_worker_tasks(app: Celery) -> None:
     register_celery_types()
+    register_pydantic_types(FileUploadCompletionBody, FileMetaData, FoldersBody)
 
     with log_context(_logger, logging.INFO, msg="worker task registration"):
         register_task(app, export_data, dont_autoretry_for=(AccessRightError,))
