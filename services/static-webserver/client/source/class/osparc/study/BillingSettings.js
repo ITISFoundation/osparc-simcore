b8858e49e25461fae108d84dea92bbde5eac0989
odeimaiz <maiz@itis.swiss>
2025-06-19 11:06:01 +0200

    found another way to trigger debt

--- services/static-webserver/client/source/class/osparc/study/BillingSettings.js
+++ services/static-webserver/client/source/class/osparc/study/BillingSettings.js
@@ -286,7 +286,12 @@ qx.Class.define("osparc.study.BillingSettings", {
           const msg = this.tr("Credit Account saved");
           osparc.FlashMessenger.logAs(msg, "INFO");
         })
-        .catch(err => osparc.FlashMessenger.logError(err))
+        .catch(err => {
+          if ("status" in err && err["status"] == 402) {
+            osparc.study.Utils.extractDebtFromError(this.__studyData["uuid"], err);
+          }
+          osparc.FlashMessenger.logError(err);
+        })
         .finally(() => {
           creditAccountBox.setEnabled(true);
         });
