6771ffff36e2b992097e43b0519bfe3b7bcd71f7
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-06-22 13:49:51 +0200

    🐛 Refactor user messages to use common_library.user_messages for consistency

--- services/web/server/src/simcore_service_webserver/security/_constants.py
+++ services/web/server/src/simcore_service_webserver/security/_constants.py
@@ -1,5 +1,9 @@
 from typing import Final
+from common_library.user_messages import user_message
 
-MSG_AUTH_NOT_AVAILABLE: Final[str] = "Authentication service is temporary unavailable"
 MSG_UNAUTHORIZED: Final[str] = "Unauthorized"
+MSG_AUTH_NOT_AVAILABLE: Final[str] = user_message(
+    "Authentication service is temporary unavailable"
+)
+
 PERMISSION_PRODUCT_LOGIN_KEY: Final[str] = "product.login"
