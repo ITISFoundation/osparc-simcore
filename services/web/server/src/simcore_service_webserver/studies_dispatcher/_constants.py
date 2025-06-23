5abe616616c674978e3ec716b1e7f7b8039c6462
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-06-23 19:17:19 +0200

    updatess

--- services/web/server/src/simcore_service_webserver/studies_dispatcher/_constants.py
+++ services/web/server/src/simcore_service_webserver/studies_dispatcher/_constants.py
@@ -8,33 +8,37 @@ from common_library.user_messages import user_message
 #
 
 MSG_PROJECT_NOT_FOUND: Final[str] = user_message(
-    "Cannot find any study with ID '{project_id}'"
+    "The project with ID '{project_id}' could not be found.", _version=1
 )
 
-# This error happens when the linked study ID does not exists OR is not shared with everyone
+# This error happens when the linked project ID does not exists OR is not shared with everyone
 MSG_PROJECT_NOT_PUBLISHED: Final[str] = user_message(
-    "Cannot find any study with ID '{project_id}'"
+    "The project with ID '{project_id}' is not available or not shared.", _version=1
 )
 
-# This error happens when the linked study ID does not exists OR is not shared with everyone OR is NOT public
+# This error happens when the linked project ID does not exists OR is not shared with everyone OR is NOT public
 MSG_PUBLIC_PROJECT_NOT_PUBLISHED: Final[str] = user_message(
-    "Only available for registered users.<br/><br/>"
-    "Please login and try again.<br/><br/>"
-    "If you don't have an account, please request one at {support_email}<br/><br/>"
+    "This project is only available for registered users.<br/><br/>"
+    "Please log in and try again.<br/><br/>"
+    "If you don't have an account, please request one at {support_email}.<br/><br/>",
+    _version=1,
 )
 
 MSG_GUESTS_NOT_ALLOWED: Final[str] = user_message(
-    "Access restricted to registered users.<br/><br/>"
-    "If you don't have an account, please email to support and request one<br/><br/>"
+    "Access is restricted to registered users.<br/><br/>"
+    "If you don't have an account, please contact support to request one.<br/><br/>",
+    _version=1,
 )
 
 MSG_TOO_MANY_GUESTS: Final[str] = user_message(
-    "We have reached the maximum of anonymous users allowed the platform. "
-    "Please try later or login with a registered account."
+    "We have reached the maximum number of anonymous users allowed on the platform. "
+    "Please try again later or log in with a registered account.",
+    _version=1,
 )
 
 MSG_UNEXPECTED_DISPATCH_ERROR: Final[str] = user_message(
-    "Sorry, but looks like something unexpected went wrong!"
-    "We track these errors automatically, but if the problem persists feel free to contact us."
-    "In the meantime, try refreshing."
+    "Sorry, something unexpected went wrong! "
+    "We track these errors automatically, but if the problem persists please contact us. "
+    "In the meantime, try refreshing the page.",
+    _version=1,
 )
