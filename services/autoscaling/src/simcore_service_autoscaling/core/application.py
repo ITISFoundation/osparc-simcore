73998a08b52d9ef6b7b5ceccedaa9fae8078459f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-06-23 16:14:54 +0200

    movinf files around

--- services/autoscaling/src/simcore_service_autoscaling/core/application.py
+++ services/autoscaling/src/simcore_service_autoscaling/core/application.py
@@ -17,7 +17,9 @@ from .._meta import (
     APP_STARTED_DYNAMIC_BANNER_MSG,
 )
 from ..api.routes import setup_api_routes
-from ..modules.auto_scaling_task import setup as setup_auto_scaler_background_task
+from ..modules.cluster_scaling.auto_scaling_task import (
+    setup as setup_auto_scaler_background_task,
+)
 from ..modules.buffer_machines_pool_task import setup as setup_buffer_machines_pool_task
 from ..modules.docker import setup as setup_docker
 from ..modules.ec2 import setup as setup_ec2
