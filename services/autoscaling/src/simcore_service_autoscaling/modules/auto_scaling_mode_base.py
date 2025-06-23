90bd35d92ed51fd33d99bd43e73e0bfbd553e5ed
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-06-23 10:57:01 +0200

    replaced ABC by a protocol

--- services/autoscaling/src/simcore_service_autoscaling/modules/auto_scaling_mode_base.py
+++ services/autoscaling/src/simcore_service_autoscaling/modules/auto_scaling_mode_base.py
@@ -1,5 +1,4 @@
-from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from typing import Protocol
 
 from aws_library.ec2 import EC2InstanceData, EC2Tags, Resources
 from fastapi import FastAPI
@@ -10,66 +9,41 @@ from types_aiobotocore_ec2.literals import InstanceTypeType
 from ..models import AssociatedInstance
 
 
-@dataclass
-class BaseAutoscaling(ABC):  # pragma: no cover
-    @staticmethod
-    @abstractmethod
-    async def get_monitored_nodes(app: FastAPI) -> list[DockerNode]: ...
+class BaseAutoscaling(Protocol):
+    async def get_monitored_nodes(self, app: FastAPI) -> list[DockerNode]: ...
 
-    @staticmethod
-    @abstractmethod
-    def get_ec2_tags(app: FastAPI) -> EC2Tags: ...
+    def get_ec2_tags(self, app: FastAPI) -> EC2Tags: ...
 
-    @staticmethod
-    @abstractmethod
     def get_new_node_docker_tags(
-        app: FastAPI, ec2_instance_data: EC2InstanceData
+        self, app: FastAPI, ec2_instance_data: EC2InstanceData
     ) -> dict[DockerLabelKey, str]: ...
 
-    @staticmethod
-    @abstractmethod
-    async def list_unrunnable_tasks(app: FastAPI) -> list: ...
+    async def list_unrunnable_tasks(self, app: FastAPI) -> list: ...
 
-    @staticmethod
-    @abstractmethod
-    def get_task_required_resources(task) -> Resources: ...
+    def get_task_required_resources(self, task) -> Resources: ...
 
-    @staticmethod
-    @abstractmethod
     async def get_task_defined_instance(
-        app: FastAPI, task
+        self, app: FastAPI, task
     ) -> InstanceTypeType | None: ...
 
-    @staticmethod
-    @abstractmethod
     async def compute_node_used_resources(
-        app: FastAPI, instance: AssociatedInstance
+        self, app: FastAPI, instance: AssociatedInstance
     ) -> Resources: ...
 
-    @staticmethod
-    @abstractmethod
     async def compute_cluster_used_resources(
-        app: FastAPI, instances: list[AssociatedInstance]
+        self, app: FastAPI, instances: list[AssociatedInstance]
     ) -> Resources: ...
 
-    @staticmethod
-    @abstractmethod
     async def compute_cluster_total_resources(
-        app: FastAPI, instances: list[AssociatedInstance]
+        self, app: FastAPI, instances: list[AssociatedInstance]
     ) -> Resources: ...
 
-    @staticmethod
-    @abstractmethod
     async def is_instance_active(
-        app: FastAPI, instance: AssociatedInstance
+        self, app: FastAPI, instance: AssociatedInstance
     ) -> bool: ...
 
-    @staticmethod
-    @abstractmethod
     async def is_instance_retired(
-        app: FastAPI, instance: AssociatedInstance
+        self, app: FastAPI, instance: AssociatedInstance
     ) -> bool: ...
 
-    @staticmethod
-    @abstractmethod
-    async def try_retire_nodes(app: FastAPI) -> None: ...
+    async def try_retire_nodes(self, app: FastAPI) -> None: ...
