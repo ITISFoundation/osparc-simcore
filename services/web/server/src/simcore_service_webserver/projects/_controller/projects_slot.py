e61a649a19e345c448049c4e52f9e5376b2248d5
Sylvain <35365065+sanderegg@users.noreply.github.com>
2025-06-25 23:11:45 +0200

    🐛Ensure log/progress queue is restored when websocket connection is restored (#7971)

--- services/web/server/src/simcore_service_webserver/projects/_controller/projects_slot.py
+++ services/web/server/src/simcore_service_webserver/projects/_controller/projects_slot.py
@@ -5,6 +5,7 @@ import logging
 from aiohttp import web
 from models_library.products import ProductName
 from models_library.projects import ProjectID
+from models_library.users import UserID
 from servicelib.aiohttp.observer import (
     registed_observers_report,
     register_observer,
@@ -20,8 +21,40 @@ from .._projects_service import retrieve_and_notify_project_locked_state
 _logger = logging.getLogger(__name__)
 
 
+async def _on_user_connected(
+    user_id: UserID,
+    app: web.Application,
+    product_name: ProductName,
+    client_session_id: str,
+) -> None:
+    assert product_name  # nosec
+    # check if there is a project resource
+    with managed_resource(user_id, client_session_id, app) as user_session:
+        projects: list[str] = await user_session.find(PROJECT_ID_KEY)
+    assert len(projects) <= 1, "At the moment, at most one project per session"  # nosec
+
+    if projects:
+        with log_context(
+            _logger,
+            logging.DEBUG,
+            msg=f"user connects and subscribes to following {projects=}",
+        ):
+            await logged_gather(
+                *[project_logs.subscribe(app, ProjectID(prj)) for prj in projects]
+            )
+
+        await logged_gather(
+            *[
+                retrieve_and_notify_project_locked_state(
+                    user_id, prj, app, notify_only_prj_user=True
+                )
+                for prj in projects
+            ]
+        )
+
+
 async def _on_user_disconnected(
-    user_id: int,
+    user_id: UserID,
     client_session_id: str,
     app: web.Application,
     product_name: ProductName,
@@ -56,6 +89,7 @@ async def _on_user_disconnected(
 def setup_project_observer_events(app: web.Application) -> None:
     setup_observer_registry(app)
 
+    register_observer(app, _on_user_connected, event="SIGNAL_USER_CONNECTED")
     register_observer(app, _on_user_disconnected, event="SIGNAL_USER_DISCONNECTED")
 
     _logger.info(
