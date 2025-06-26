91b7593e3c3f637ca4214c658e565bb354d479c4
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-06-26 14:31:45 +0200

    refactor: storage rpc routes

--- services/storage/src/simcore_service_storage/core/application.py
+++ services/storage/src/simcore_service_storage/core/application.py
@@ -32,7 +32,7 @@ from .._meta import (
     APP_WORKER_STARTED_BANNER_MSG,
 )
 from ..api.rest.routes import setup_rest_api_routes
-from ..api.rpc.routes import setup_rpc_api_routes
+from ..api.rpc.routes import setup_rpc_routes
 from ..dsm import setup_dsm
 from ..dsm_cleaner import setup_dsm_cleaner
 from ..exceptions.handlers import set_exception_handlers
@@ -92,10 +92,11 @@ def create_app(settings: ApplicationSettings) -> FastAPI:  # noqa: C901
 
     if not settings.STORAGE_WORKER_MODE:
         setup_rabbitmq(app)
-        setup_rpc_api_routes(app)
 
         assert settings.STORAGE_CELERY  # nosec
         setup_task_manager(app, celery_settings=settings.STORAGE_CELERY)
+
+        setup_rpc_routes(app)
     setup_rest_api_long_running_tasks_for_uploads(app)
     setup_rest_api_routes(app, API_VTAG)
     set_exception_handlers(app)
