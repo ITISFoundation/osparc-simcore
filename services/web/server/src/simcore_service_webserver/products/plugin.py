1598ada9e19d9598a9f6746a5b21d32dee4c81db
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-06-27 14:08:51 +0200

    ✨ web-server: Add Stand-alone Auth-App Entrypoint to Web-Server (#7818)

--- services/web/server/src/simcore_service_webserver/products/plugin.py
+++ services/web/server/src/simcore_service_webserver/products/plugin.py
@@ -11,9 +11,32 @@ At every request to this service API, a middleware discovers which product is th
 import logging
 
 from aiohttp import web
-from servicelib.aiohttp.application_setup import ModuleCategory, app_module_setup
+from servicelib.aiohttp.application_setup import (
+    ModuleCategory,
+    app_module_setup,
+    ensure_single_setup,
+)
 
 _logger = logging.getLogger(__name__)
+
+
+@ensure_single_setup(f"{__name__}.without_rpc", logger=_logger)
+def setup_products_without_rpc(app: web.Application):
+    #
+    # NOTE: internal import speeds up booting app
+    # specially if this plugin is not set up to be loaded
+    #
+    from ..constants import APP_SETTINGS_KEY
+    from . import _web_events, _web_middlewares
+    from ._controller import rest
+
+    assert app[APP_SETTINGS_KEY].WEBSERVER_PRODUCTS is True  # nosec
+
+    # rest API
+    app.middlewares.append(_web_middlewares.discover_product_middleware)
+    app.router.add_routes(rest.routes)
+
+    _web_events.setup_web_events(app)
 
 
 @app_module_setup(
@@ -24,20 +47,9 @@ _logger = logging.getLogger(__name__)
     logger=_logger,
 )
 def setup_products(app: web.Application):
-    #
-    # NOTE: internal import speeds up booting app
-    # specially if this plugin is not set up to be loaded
-    #
-    from ..constants import APP_SETTINGS_KEY
-    from . import _web_events, _web_middlewares
-    from ._controller import rest, rpc
+    from ._controller import rpc
 
-    assert app[APP_SETTINGS_KEY].WEBSERVER_PRODUCTS is True  # nosec
+    setup_products_without_rpc(app)
 
-    app.middlewares.append(_web_middlewares.discover_product_middleware)
-
-    app.router.add_routes(rest.routes)
-
+    # rpc API (optional)
     rpc.setup_rpc(app)
-
-    _web_events.setup_web_events(app)
