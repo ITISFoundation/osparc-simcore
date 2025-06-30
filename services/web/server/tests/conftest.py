6ce16b845a7cbb504f631a268d42c6c047b6a57f
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-06-30 15:16:27 +0200

    upgrades pytest_asyncio using https://pytest-asyncio.readthedocs.io/en/stable/how-to-guides/migrate_from_0_23.html

--- services/web/server/tests/conftest.py
+++ services/web/server/tests/conftest.py
@@ -3,7 +3,6 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
-import asyncio
 import contextlib
 import json
 import logging
@@ -488,11 +487,3 @@ def mock_dynamic_scheduler(mocker: MockerFixture) -> None:
         "simcore_service_webserver.dynamic_scheduler.api.update_projects_networks",
         autospec=True,
     )
-
-
-@pytest.fixture
-async def loop(
-    event_loop: asyncio.AbstractEventLoop,
-) -> asyncio.AbstractEventLoop:
-    """Override the event loop inside pytest-aiohttp with the one from pytest-asyncio."""
-    return event_loop
