9918b372ae0dc91168cbf903c194cff991ba1a2e
odeimaiz <maiz@itis.swiss>
2025-06-30 08:50:24 +0200

    AnnotationUI

--- packages/models-library/src/models_library/api_schemas_webserver/projects_ui.py
+++ packages/models-library/src/models_library/api_schemas_webserver/projects_ui.py
@@ -40,7 +40,7 @@ class SlideshowUI(TypedDict):
 
 
 class AnnotationUI(BaseModel):
-    type: Literal["note", "rect", "text"]
+    type: Literal["note", "rect", "text", "conversation"]
     color: Annotated[Color, PlainSerializer(Color.as_hex)]
     attributes: Annotated[dict, Field(description="svg attributes")]
 
@@ -70,6 +70,10 @@ class AnnotationUI(BaseModel):
                         "type": "text",
                         "color": "#0000FF",
                         "attributes": {"x": 415, "y": 100, "text": "Hey!"},
+                    },
+                    {
+                        "type": "conversation",
+                        "attributes": {"x": 415, "y": 100},
                     },
                 ]
             },
@@ -167,6 +171,13 @@ class StudyUI(OutputSchema):
                                     "text": "pppoo",
                                     "color": "#E9AEAB",
                                     "fontSize": 12,
+                                },
+                            },
+                            "cf94f068-259c-4192-89f9-b2a56d51249d": {
+                                "type": "conversation",
+                                "attributes": {
+                                    "x": 119,
+                                    "y": 223,
                                 },
                             },
                         },
