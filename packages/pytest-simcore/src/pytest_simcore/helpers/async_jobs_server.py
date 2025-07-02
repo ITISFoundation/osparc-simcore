10fbffd2329d53859926facacfbed6c2f1af3118
Mads Bisgaard <bisgaard@itis.swiss>
2025-07-02 14:47:05 +0200

    start adding unit tests

---
+++ packages/pytest-simcore/src/pytest_simcore/helpers/async_jobs_server.py
@@ -0,0 +1,71 @@
+from models_library.api_schemas_rpc_async_jobs.async_jobs import (
+    AsyncJobId,
+    AsyncJobNameData,
+    AsyncJobResult,
+    AsyncJobStatus,
+)
+from models_library.progress_bar import ProgressReport
+from models_library.rabbitmq_basic_types import RPCNamespace
+from pydantic import validate_call
+from servicelib.rabbitmq._client_rpc import RabbitMQRPCClient
+
+
+class AsyncJobSideEffects:
+
+    @validate_call(config={"arbitrary_types_allowed": True})
+    async def cancel(
+        self,
+        rabbitmq_rpc_client: RabbitMQRPCClient,
+        *,
+        rpc_namespace: RPCNamespace,
+        job_id: AsyncJobId,
+        job_id_data: AsyncJobNameData,
+    ) -> None:
+        pass
+
+    @validate_call(config={"arbitrary_types_allowed": True})
+    async def status(
+        self,
+        rabbitmq_rpc_client: RabbitMQRPCClient,
+        *,
+        rpc_namespace: RPCNamespace,
+        job_id: AsyncJobId,
+        job_id_data: AsyncJobNameData,
+    ) -> AsyncJobStatus:
+        return AsyncJobStatus(
+            job_id=job_id,
+            progress=ProgressReport(
+                actual_value=50.0,
+                total=100.0,
+                attempt=1,
+            ),
+            done=False,
+        )
+
+    @validate_call(config={"arbitrary_types_allowed": True})
+    async def result(
+        self,
+        rabbitmq_rpc_client: RabbitMQRPCClient,
+        *,
+        rpc_namespace: RPCNamespace,
+        job_id: AsyncJobId,
+        job_id_data: AsyncJobNameData,
+    ) -> AsyncJobResult:
+        return AsyncJobResult(result="Success")
+
+    @validate_call(config={"arbitrary_types_allowed": True})
+    async def list_jobs(
+        self,
+        rabbitmq_rpc_client: RabbitMQRPCClient,
+        *,
+        rpc_namespace: RPCNamespace,
+        job_id_data: AsyncJobNameData,
+        filter_: str = "",
+    ) -> list[AsyncJobStatus]:
+        return [
+            AsyncJobStatus(
+                job_id=AsyncJobId("123e4567-e89b-12d3-a456-426614174000"),
+                progress=ProgressReport(actual_value=50.0, total=100.0, attempt=1),
+                done=False,
+            )
+        ]
---
+++ services/api-server/tests/unit/test_tasks.py
@@ -0,0 +1,55 @@
+from typing import Any
+
+import pytest
+from httpx import AsyncClient
+from models_library.api_schemas_long_running_tasks.tasks import TaskGet
+from pytest_mock import MockerFixture, MockType
+from pytest_simcore.helpers.async_jobs_server import AsyncJobSideEffects
+from simcore_service_api_server.models.schemas.tasks import ApiServerEnvelope
+
+
+@pytest.fixture
+async def async_jobs_rpc_side_effects() -> Any:
+    return AsyncJobSideEffects()
+
+
+@pytest.fixture
+def mocked_async_jobs_rpc_api(
+    mocker: MockerFixture, async_jobs_rpc_side_effects: Any
+) -> dict[str, MockType]:
+    """
+    Mocks the catalog's simcore service RPC API for testing purposes.
+    """
+    from servicelib.rabbitmq.rpc_interfaces.async_jobs import async_jobs
+
+    mocks = {}
+
+    # Get all callable methods from the side effects class that are not built-ins
+    side_effect_methods = [
+        method_name
+        for method_name in dir(async_jobs_rpc_side_effects)
+        if not method_name.startswith("_")
+        and callable(getattr(async_jobs_rpc_side_effects, method_name))
+    ]
+
+    # Create mocks for each method in catalog_rpc that has a corresponding side effect
+    for method_name in side_effect_methods:
+        assert hasattr(async_jobs, method_name)
+        mocks[method_name] = mocker.patch.object(
+            async_jobs,
+            method_name,
+            autospec=True,
+            side_effect=getattr(async_jobs_rpc_side_effects, method_name),
+        )
+
+    return mocks
+
+
+async def test_get_async_jobs(
+    client: AsyncClient, mocked_async_jobs_rpc_api: dict[str, MockType]
+):
+
+    response = await client.get("/v0/tasks")
+    assert response.status_code == 200
+    ApiServerEnvelope[list[TaskGet]].model_validate_json(response.json())
+    assert mocked_async_jobs_rpc_api["list_jobs"].called
