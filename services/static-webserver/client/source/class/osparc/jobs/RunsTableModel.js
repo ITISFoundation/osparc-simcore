10a2def83f1dca51de589348fec3e154c0f27100
odeimaiz <maiz@itis.swiss>
2025-07-03 17:03:25 +0200

    minor

--- services/static-webserver/client/source/class/osparc/jobs/RunsTableModel.js
+++ services/static-webserver/client/source/class/osparc/jobs/RunsTableModel.js
@@ -119,7 +119,7 @@ qx.Class.define("osparc.jobs.RunsTableModel", {
     _loadRowData(firstRow, qxLastRow) {
       this.setIsFetching(true);
 
-      qxLastRow = osparc.store.Jobs; // Default to 2 rows for testing purposes
+      qxLastRow = osparc.store.Jobs.SERVER_MAX_LIMIT; // Default to 2 rows for testing purposes
       const lastRow = Math.min(qxLastRow, this._rowCount - 1);
       // Returns a request promise with given offset and limit
       const getFetchPromise = (offset, limit) => {
