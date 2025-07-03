217d6a552006fd515b72c85deb30e70b7ad6be0c
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-07-03 10:22:28 +0200

    feat: docker boot

--- services/notifications/docker/boot.sh
+++ services/notifications/docker/boot.sh
@@ -47,23 +47,49 @@ APP_LOG_LEVEL=${LOGLEVEL:-${LOG_LEVEL:-${LOGLEVEL:-INFO}}}
 SERVER_LOG_LEVEL=$(echo "${APP_LOG_LEVEL}" | tr '[:upper:]' '[:lower:]')
 echo "$INFO" "Log-level app/server: $APP_LOG_LEVEL/$SERVER_LOG_LEVEL"
 
-if [ "${SC_BOOT_MODE}" = "debug" ]; then
-  reload_dir_packages=$(find /devel/packages -maxdepth 3 -type d -path "*/src/*" ! -path "*.*" -exec echo '--reload-dir {} \' \;)
+if [ "${NOTIFICATIONS_WORKER_MODE}" = "true" ]; then
+  if [ "${SC_BOOT_MODE}" = "debug" ]; then
+    exec watchmedo auto-restart \
+      --directory /devel/packages \
+      --directory services/notifications \
+      --pattern "*.py" \
+      --recursive \
+      -- \
+      celery \
+      --app=simcore_service_notifications.modules.celery.worker_main:the_celery_app \
+      worker --pool=threads \
+      --loglevel="${SERVER_LOG_LEVEL}" \
+      --concurrency="${CELERY_CONCURRENCY}" \
+      --hostname="${NOTIFICATIONS_WORKER_NAME}" \
+      --queues="${CELERY_QUEUES:-default}"
+  else
+    exec celery \
+      --app=simcore_service_notifications.modules.celery.worker_main:the_celery_app \
+      worker --pool=threads \
+      --loglevel="${SERVER_LOG_LEVEL}" \
+      --concurrency="${CELERY_CONCURRENCY}" \
+      --hostname="${NOTIFICATIONS_WORKER_NAME}" \
+      --queues="${CELERY_QUEUES:-default}"
+  fi
+else
+  if [ "${SC_BOOT_MODE}" = "debug" ]; then
+    reload_dir_packages=$(find /devel/packages -maxdepth 3 -type d -path "*/src/*" ! -path "*.*" -exec echo '--reload-dir {} \' \;)
 
-  exec sh -c "
-    cd services/notifications/src/simcore_service_notifications && \
-    python -Xfrozen_modules=off -m debugpy --listen 0.0.0.0:${NOTIFICATIONS_REMOTE_DEBUGGING_PORT} -m uvicorn main:the_app \
+    exec sh -c "
+      cd services/notifications/src/simcore_service_notifications && \
+      python -Xfrozen_modules=off -m debugpy --listen 0.0.0.0:${NOTIFICATIONS_REMOTE_DEBUGGING_PORT} -m uvicorn main:the_app \
+        --host 0.0.0.0 \
+        --port 8000 \
+        --reload \
+        $reload_dir_packages
+        --reload-dir . \
+        --log-level \"${SERVER_LOG_LEVEL}\"
+    "
+  else
+    exec uvicorn simcore_service_notifications.main:the_app \
       --host 0.0.0.0 \
       --port 8000 \
-      --reload \
-      $reload_dir_packages
-      --reload-dir . \
-      --log-level \"${SERVER_LOG_LEVEL}\"
-  "
-else
-  exec uvicorn simcore_service_notifications.main:the_app \
-    --host 0.0.0.0 \
-    --port 8000 \
-    --log-level "${SERVER_LOG_LEVEL}" \
-    --no-access-log
+      --log-level "${SERVER_LOG_LEVEL}" \
+      --no-access-log
+  fi
 fi
