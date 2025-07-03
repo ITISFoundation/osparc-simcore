dad8e8696878f92423befdeed9618fe162c2a81f
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-07-03 17:46:01 +0200

    fixes loop aiohttp

--- services/web/server/tests/conftest.py
+++ services/web/server/tests/conftest.py
@@ -3,6 +3,7 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
+import asyncio
 import contextlib
 import json
 import logging
@@ -487,3 +488,9 @@ def mock_dynamic_scheduler(mocker: MockerFixture) -> None:
         "simcore_service_webserver.dynamic_scheduler.api.update_projects_networks",
         autospec=True,
     )
+
+
+@pytest.fixture
+async def loop() -> asyncio.AbstractEventLoop:
+    """Override the event loop inside pytest-aiohttp with the one from pytest-asyncio."""
+    return asyncio.get_running_loop()
