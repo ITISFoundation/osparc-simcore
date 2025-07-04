eb206e4e42415649d08e57303cf91eb918d2920c
odeimaiz <maiz@itis.swiss>
2025-07-04 11:46:08 +0200

    minor

--- services/static-webserver/client/source/class/osparc/jobs/RunsTable.js
+++ services/static-webserver/client/source/class/osparc/jobs/RunsTable.js
@@ -33,7 +33,7 @@ qx.Class.define("osparc.jobs.RunsTable", {
     this.setTableModel(model);
 
     this.set({
-      statusBarVisible: true,
+      statusBarVisible: false,
       headerCellHeight: 26,
       rowHeight: 26,
     });
