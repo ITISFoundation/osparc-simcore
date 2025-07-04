e1ce60026cff3b8c5f14f0d8ad6712985dd43663
Mads Bisgaard <bisgaard@itis.swiss>
2025-07-04 10:35:41 +0200

    map exceptions

--- services/api-server/src/simcore_service_api_server/services_rpc/director_v2.py
+++ services/api-server/src/simcore_service_api_server/services_rpc/director_v2.py
@@ -6,6 +6,7 @@ from models_library.projects import ProjectID
 from servicelib.rabbitmq._client_rpc import RabbitMQRPCClient
 from servicelib.rabbitmq.rpc_interfaces.director_v2 import computations_tasks
 
+from ..exceptions.backend_errors import JobNotFoundError
 from ..exceptions.service_errors_utils import service_exception_mapper
 
 _exception_mapper = partial(service_exception_mapper, service_name="DirectorV2")
@@ -15,7 +16,9 @@ _exception_mapper = partial(service_exception_mapper, service_name="DirectorV2")
 class DirectorV2Service:
     _rpc_client: RabbitMQRPCClient
 
-    @_exception_mapper(rpc_exception_map={ComputationalTaskMissingError: Exception})
+    @_exception_mapper(
+        rpc_exception_map={ComputationalTaskMissingError: JobNotFoundError}
+    )
     async def get_computation_task_log_file_ids(self, project_id: ProjectID):
         return await computations_tasks.get_computation_task_log_file_ids(
             self._rpc_client, project_id=project_id
