9f0663c74e02dcbcba8a9e70758f66af24d0eb50
Mads Bisgaard <bisgaard@itis.swiss>
2025-07-07 12:24:30 +0200

    add rpc storage client in api-server

---
+++ services/api-server/src/simcore_service_api_server/services_rpc/storage.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from functools import partial
+
+from models_library.api_schemas_webserver.storage import PathToExport
+from models_library.products import ProductName
+from models_library.users import UserID
+from servicelib.rabbitmq._client_rpc import RabbitMQRPCClient
+from servicelib.rabbitmq.rpc_interfaces.storage.simcore_s3 import start_export_data
+
+from ..exceptions.service_errors_utils import service_exception_mapper
+
+_exception_mapper = partial(service_exception_mapper, service_name="Storage")
+
+
+@dataclass(frozen=True, kw_only=True)
+class StorageService:
+    _rpc_client: RabbitMQRPCClient
+
+    @_exception_mapper(rpc_exception_map={})
+    async def start_data_export(
+        self,
+        user_id: UserID,
+        product_name: ProductName,
+        paths_to_export: list[PathToExport],
+    ):
+        return await start_export_data(
+            self._rpc_client,
+            user_id=user_id,
+            product_name=product_name,
+            paths_to_export=paths_to_export,
+        )
