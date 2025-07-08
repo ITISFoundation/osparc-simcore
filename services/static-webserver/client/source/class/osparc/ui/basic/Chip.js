5d039eac1ab9c16d04f6b3dd29420db40d1a0f5c
odeimaiz <maiz@itis.swiss>
2025-07-08 09:35:15 +0200

    enh chip

--- services/static-webserver/client/source/class/osparc/ui/basic/Chip.js
+++ services/static-webserver/client/source/class/osparc/ui/basic/Chip.js
@@ -16,6 +16,45 @@ qx.Class.define("osparc.ui.basic.Chip", {
     appearance: {
       init: "chip",
       refine: true
+    },
+
+    statusColor: {
+      check: ["success", "warning", "error"],
+      init: null,
+      apply: "__applyStatusColor",
     }
-  }
+  },
+
+  statics: {
+    STATUS: {
+      SUCCESS: "success",
+      WARNING: "warning",
+      ERROR: "error",
+    },
+  },
+
+  members: {
+    __applyStatusColor: function(status) {
+      switch (status.toLowerCase()) {
+        case this.self().STATUS.SUCCESS:
+          this.set({
+            textColor: "white",
+            backgroundColor: "product-color",
+          });
+          break;
+        case this.self().STATUS.WARNING:
+          this.set({
+            textColor: "black",
+            backgroundColor: "warning-yellow",
+          });
+          break;
+        case this.self().STATUS.ERROR:
+          this.set({
+            textColor: "black",
+            backgroundColor: "failed-red",
+          });
+          break;
+      }
+    }
+  },
 });
