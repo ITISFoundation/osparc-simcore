4492c2e82e9e06bcee2097f6b2be2be19e75409d
Mads Bisgaard <bisgaard@itis.swiss>
2025-07-09 05:14:14 +0200

    @GitHK use Self

--- packages/service-library/src/servicelib/celery/models.py
+++ packages/service-library/src/servicelib/celery/models.py
@@ -1,6 +1,6 @@
 import datetime
 from enum import StrEnum
-from typing import Annotated, Protocol, TypeAlias
+from typing import Annotated, Protocol, Self, TypeAlias
 from uuid import UUID
 
 from models_library.api_schemas_rpc_async_jobs.async_jobs import AsyncJobFilter
@@ -17,7 +17,7 @@ TaskUUID: TypeAlias = UUID
 class TaskFilter(BaseModel):
 
     @classmethod
-    def from_async_job_filter(cls, async_job_filter: AsyncJobFilter) -> "TaskFilter":
+    def from_async_job_filter(cls, async_job_filter: AsyncJobFilter) -> Self:
         return cls.model_validate(async_job_filter.model_dump())
 
 
