8b06ed3d7b0a3c2bf78484ebc359359ebe0dace9
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-07-09 15:41:39 +0200

    tests: fix postgres plugin

--- services/notifications/tests/conftest.py
+++ services/notifications/tests/conftest.py
@@ -12,6 +12,7 @@ pytest_plugins = [
     "pytest_simcore.docker_compose",
     "pytest_simcore.docker_swarm",
     "pytest_simcore.environment_configs",
+    "pytest_simcore.postgres_service",
     "pytest_simcore.rabbit_service",
     "pytest_simcore.redis_service",
     "pytest_simcore.repository_paths",
