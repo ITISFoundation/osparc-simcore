a0e7634b64cfe88838db36fea4e11602240af54e
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-07-09 17:57:39 +0200

    sonar

--- services/web/server/src/simcore_service_webserver/user_notifications/_models.py
+++ services/web/server/src/simcore_service_webserver/user_notifications/_models.py
@@ -7,6 +7,7 @@ from models_library.products import ProductName
 from models_library.users import UserID
 from models_library.utils.enums import StrAutoEnum
 from pydantic import BaseModel, ConfigDict, NonNegativeInt, field_validator
+from pydantic.config import JsonDict
 
 MAX_NOTIFICATIONS_FOR_USER_TO_SHOW: Final[NonNegativeInt] = 10
 MAX_NOTIFICATIONS_FOR_USER_TO_KEEP: Final[NonNegativeInt] = 100
@@ -64,81 +65,86 @@ class UserNotification(BaseUserNotification):
             id=f"{uuid4()}", read=False, **request_data.model_dump()
         )
 
-    model_config = ConfigDict(
-        json_schema_extra={
-            "examples": [
-                {
-                    "id": "3fb96d89-ff5d-4d27-b5aa-d20d46e20eb8",
-                    "user_id": "1",
-                    "category": "NEW_ORGANIZATION",
-                    "actionable_path": "organization/40",
-                    "title": "New organization",
-                    "text": "You're now member of a new Organization",
-                    "date": "2023-02-23T16:23:13.122Z",
-                    "product": "osparc",
-                    "read": True,
-                },
-                {
-                    "id": "ba64ffce-c58c-4382-aad6-96a7787251d6",
-                    "user_id": "1",
-                    "category": "STUDY_SHARED",
-                    "actionable_path": "study/27edd65c-b360-11ed-93d7-02420a000014",
-                    "title": "Study shared",
-                    "text": "A study was shared with you",
-                    "date": "2023-02-23T16:25:13.122Z",
-                    "product": "osparc",
-                    "read": False,
-                },
-                {
-                    "id": "390053c9-3931-40e1-839f-585268f6fd3c",
-                    "user_id": "1",
-                    "category": "TEMPLATE_SHARED",
-                    "actionable_path": "template/f60477b6-a07e-11ed-8d29-02420a00002d",
-                    "title": "Template shared",
-                    "text": "A template was shared with you",
-                    "date": "2023-02-23T16:28:13.122Z",
-                    "product": "osparc",
-                    "read": False,
-                },
-                {
-                    "id": "390053c9-3931-40e1-839f-585268f6fd3d",
-                    "user_id": "1",
-                    "category": "CONVERSATION_NOTIFICATION",
-                    "actionable_path": "study/27edd65c-b360-11ed-93d7-02420a000014",
-                    "title": "New notification",
-                    "text": "You were notified in a conversation",
-                    "date": "2023-02-23T16:28:13.122Z",
-                    "product": "s4l",
-                    "read": False,
-                    "resource_id": "3fb96d89-ff5d-4d27-b5aa-d20d46e20e12",
-                    "user_from_id": "2",
-                },
-                {
-                    "id": "390053c9-3931-40e1-839f-585268f6fd3d",
-                    "user_id": "1",
-                    "category": "ANNOTATION_NOTE",
-                    "actionable_path": "study/27edd65c-b360-11ed-93d7-02420a000014",
-                    "title": "Note added",
-                    "text": "A Note was added for you",
-                    "date": "2023-02-23T16:28:13.122Z",
-                    "product": "s4l",
-                    "read": False,
-                    "resource_id": "3fb96d89-ff5d-4d27-b5aa-d20d46e20e12",
-                    "user_from_id": "2",
-                },
-                {
-                    "id": "390053c9-3931-40e1-839f-585268f6fd3e",
-                    "user_id": "1",
-                    "category": "WALLET_SHARED",
-                    "actionable_path": "wallet/21",
-                    "title": "Credits shared",
-                    "text": "A Credit account was shared with you",
-                    "date": "2023-09-29T16:28:13.122Z",
-                    "product": "tis",
-                    "read": False,
-                    "resource_id": "3fb96d89-ff5d-4d27-b5aa-d20d46e20e13",
-                    "user_from_id": "2",
-                },
-            ]
-        }
-    )
+    @staticmethod
+    def _update_json_schema_extra(schema: JsonDict) -> None:
+
+        schema.update(
+            {
+                "examples": [  # NOSONAR
+                    {
+                        "id": "3fb96d89-ff5d-4d27-b5aa-d20d46e20eb8",
+                        "user_id": "1",
+                        "category": "NEW_ORGANIZATION",
+                        "actionable_path": "organization/40",
+                        "title": "New organization",
+                        "text": "You're now member of a new Organization",
+                        "date": "2023-02-23T16:23:13.122Z",
+                        "product": "osparc",
+                        "read": True,
+                    },
+                    {
+                        "id": "ba64ffce-c58c-4382-aad6-96a7787251d6",
+                        "user_id": "1",
+                        "category": "STUDY_SHARED",
+                        "actionable_path": "study/27edd65c-b360-11ed-93d7-02420a000014",  # NOSONAR
+                        "title": "Study shared",
+                        "text": "A study was shared with you",
+                        "date": "2023-02-23T16:25:13.122Z",
+                        "product": "osparc",
+                        "read": False,
+                    },
+                    {
+                        "id": "390053c9-3931-40e1-839f-585268f6fd3c",
+                        "user_id": "1",
+                        "category": "TEMPLATE_SHARED",
+                        "actionable_path": "template/f60477b6-a07e-11ed-8d29-02420a00002d",
+                        "title": "Template shared",
+                        "text": "A template was shared with you",
+                        "date": "2023-02-23T16:28:13.122Z",
+                        "product": "osparc",
+                        "read": False,
+                    },
+                    {
+                        "id": "390053c9-3931-40e1-839f-585268f6fd3d",
+                        "user_id": "1",
+                        "category": "CONVERSATION_NOTIFICATION",
+                        "actionable_path": "study/27edd65c-b360-11ed-93d7-02420a000014",  # NOSONAR
+                        "title": "New notification",
+                        "text": "You were notified in a conversation",
+                        "date": "2023-02-23T16:28:13.122Z",
+                        "product": "s4l",
+                        "read": False,
+                        "resource_id": "3fb96d89-ff5d-4d27-b5aa-d20d46e20e12",
+                        "user_from_id": "2",
+                    },
+                    {
+                        "id": "390053c9-3931-40e1-839f-585268f6fd3d",
+                        "user_id": "1",
+                        "category": "ANNOTATION_NOTE",
+                        "actionable_path": "study/27edd65c-b360-11ed-93d7-02420a000014",  # NOSONAR
+                        "title": "Note added",
+                        "text": "A Note was added for you",
+                        "date": "2023-02-23T16:28:13.122Z",
+                        "product": "s4l",
+                        "read": False,
+                        "resource_id": "3fb96d89-ff5d-4d27-b5aa-d20d46e20e12",
+                        "user_from_id": "2",
+                    },
+                    {
+                        "id": "390053c9-3931-40e1-839f-585268f6fd3e",
+                        "user_id": "1",
+                        "category": "WALLET_SHARED",
+                        "actionable_path": "wallet/21",
+                        "title": "Credits shared",
+                        "text": "A Credit account was shared with you",
+                        "date": "2023-09-29T16:28:13.122Z",
+                        "product": "tis",
+                        "read": False,
+                        "resource_id": "3fb96d89-ff5d-4d27-b5aa-d20d46e20e13",
+                        "user_from_id": "2",
+                    },
+                ]
+            }
+        )
+
+    model_config = ConfigDict(json_schema_extra=_update_json_schema_extra)
