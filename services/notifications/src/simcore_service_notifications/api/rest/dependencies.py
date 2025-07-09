f322d02f760f5ae43cb3f1d1e6893143f38bb607
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-07-09 15:21:32 +0200

    feat: revert postgres removal

--- services/notifications/src/simcore_service_notifications/api/rest/dependencies.py
+++ services/notifications/src/simcore_service_notifications/api/rest/dependencies.py
@@ -5,6 +5,9 @@ from typing import Annotated, cast
 from fastapi import Depends, FastAPI, Request
 from servicelib.rabbitmq._client_rpc import RabbitMQRPCClient
 
+from ...clients.postgres import PostgresLiveness
+from ...clients.postgres import get_postgres_liveness as get_postgress_db_liveness
+
 
 def get_application(request: Request) -> FastAPI:
     return cast(FastAPI, request.app)
@@ -15,3 +18,9 @@ def get_rabbitmq_client(
 ) -> RabbitMQRPCClient:
     assert isinstance(app.state.rabbitmq_rpc_server, RabbitMQRPCClient)  # nosec
     return app.state.rabbitmq_rpc_server
+
+
+def get_postgres_liveness(
+    app: Annotated[FastAPI, Depends(get_application)],
+) -> PostgresLiveness:
+    return get_postgress_db_liveness(app)
---
+++ services/notifications/src/simcore_service_notifications/clients/postgres/_liveness.py
@@ -0,0 +1,43 @@
+import logging
+from asyncio import Task
+from datetime import timedelta
+from typing import Final
+
+from common_library.async_tools import cancel_wait_task
+from fastapi import FastAPI
+from models_library.healthchecks import IsResponsive, LivenessResult
+from servicelib.background_task import create_periodic_task
+from servicelib.db_asyncpg_utils import check_postgres_liveness
+from servicelib.fastapi.db_asyncpg_engine import get_engine
+from servicelib.logging_utils import log_catch
+
+_logger = logging.getLogger(__name__)
+
+_LVENESS_CHECK_INTERVAL: Final[timedelta] = timedelta(seconds=10)
+
+
+class PostgresLiveness:
+    def __init__(self, app: FastAPI) -> None:
+        self.app = app
+
+        self._liveness_result: LivenessResult = IsResponsive(elapsed=timedelta(0))
+        self._task: Task | None = None
+
+    async def _check_task(self) -> None:
+        self._liveness_result = await check_postgres_liveness(get_engine(self.app))
+
+    @property
+    def is_responsive(self) -> bool:
+        return isinstance(self._liveness_result, IsResponsive)
+
+    async def setup(self) -> None:
+        self._task = create_periodic_task(
+            self._check_task,
+            interval=_LVENESS_CHECK_INTERVAL,
+            task_name="posgress_liveness_check",
+        )
+
+    async def teardown(self) -> None:
+        if self._task is not None:
+            with log_catch(_logger, reraise=False):
+                await cancel_wait_task(self._task, max_delay=5)
