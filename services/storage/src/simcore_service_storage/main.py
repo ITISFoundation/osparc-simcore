d19e8391d6d2ed69628b675ec299b40251c42ea2
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-07-09 14:49:39 +0200

    resources

--- services/storage/src/simcore_service_storage/main.py
+++ services/storage/src/simcore_service_storage/main.py
@@ -1,20 +1,17 @@
 """Main application to be deployed in for example uvicorn."""
 
-import logging
-
 from servicelib.logging_utils import setup_loggers
 from simcore_service_storage.core.application import create_app
 from simcore_service_storage.core.settings import ApplicationSettings
 
 _settings = ApplicationSettings.create_from_envs()
 
-# SEE https://github.com/ITISFoundation/osparc-simcore/issues/3148
-logging.basicConfig(level=_settings.log_level)  # NOSONAR
-logging.root.setLevel(_settings.log_level)
 setup_loggers(
     log_format_local_dev_enabled=_settings.STORAGE_LOG_FORMAT_LOCAL_DEV_ENABLED,
     logger_filter_mapping=_settings.STORAGE_LOG_FILTER_MAPPING,
     tracing_settings=_settings.STORAGE_TRACING,
+    log_base_level=_settings.log_level,
+    noisy_loggers=None,
 )
 
 app = create_app(_settings)
