3dca72a888f4a1315402b8e8322119ef10dfd12f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-07-09 14:49:23 +0200

    resources

--- services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/main.py
+++ services/resource-usage-tracker/src/simcore_service_resource_usage_tracker/main.py
@@ -1,7 +1,5 @@
 """Main application to be deployed by uvicorn (or equivalent) server"""
 
-import logging
-
 from fastapi import FastAPI
 from servicelib.logging_utils import setup_loggers
 from simcore_service_resource_usage_tracker.core.application import create_app
@@ -9,13 +7,12 @@ from simcore_service_resource_usage_tracker.core.settings import ApplicationSett
 
 the_settings = ApplicationSettings.create_from_envs()
 
-# SEE https://github.com/ITISFoundation/osparc-simcore/issues/3148
-logging.basicConfig(level=the_settings.log_level)  # NOSONAR
-logging.root.setLevel(the_settings.log_level)
 setup_loggers(
     log_format_local_dev_enabled=the_settings.RESOURCE_USAGE_TRACKER_LOG_FORMAT_LOCAL_DEV_ENABLED,
     logger_filter_mapping=the_settings.RESOURCE_USAGE_TRACKER_LOG_FILTER_MAPPING,
     tracing_settings=the_settings.RESOURCE_USAGE_TRACKER_TRACING,
+    log_base_level=the_settings.log_level,
+    noisy_loggers=None,
 )
 
 # SINGLETON FastAPI app
