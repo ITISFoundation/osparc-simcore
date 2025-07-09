f7c94f337772e81a828a5d35fe8389d12ef5333f
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-07-09 16:06:18 +0200

    uypdate repo

--- services/web/server/src/simcore_service_webserver/user_tokens/_service.py
+++ services/web/server/src/simcore_service_webserver/user_tokens/_service.py
@@ -4,88 +4,118 @@ Implemented as a stand-alone API but currently only exposed to the handlers
 """
 
 import sqlalchemy as sa
-from aiohttp import web
 from models_library.users import UserID, UserThirdPartyToken
+from simcore_postgres_database.utils_repos import (
+    pass_or_acquire_connection,
+    transaction_context,
+)
 from sqlalchemy import and_, literal_column
+from sqlalchemy.ext.asyncio import AsyncConnection
 
+from ..db.base_repository import BaseRepository
 from ..db.models import tokens
-from ..db.plugin import get_database_engine_legacy
 from ..users.exceptions import TokenNotFoundError
 
 
-async def create_token(
-    app: web.Application, user_id: UserID, token: UserThirdPartyToken
-) -> UserThirdPartyToken:
-    async with get_database_engine_legacy(app).acquire() as conn:
-        await conn.execute(
-            tokens.insert().values(
-                user_id=user_id,
-                token_service=token.service,
-                token_data=token.model_dump(mode="json"),
+class UserTokensRepository(BaseRepository):
+    async def create_token(
+        self,
+        connection: AsyncConnection | None = None,
+        *,
+        user_id: UserID,
+        token: UserThirdPartyToken,
+    ) -> UserThirdPartyToken:
+        async with transaction_context(self.engine, connection) as conn:
+            await conn.execute(
+                tokens.insert().values(
+                    user_id=user_id,
+                    token_service=token.service,
+                    token_data=token.model_dump(mode="json"),
+                )
             )
-        )
-        return token
+            return token
 
-
-async def list_tokens(
-    app: web.Application, user_id: UserID
-) -> list[UserThirdPartyToken]:
-    user_tokens: list[UserThirdPartyToken] = []
-    async with get_database_engine_legacy(app).acquire() as conn:
-        async for row in conn.execute(
-            sa.select(tokens.c.token_data).where(tokens.c.user_id == user_id)
-        ):
-            user_tokens.append(UserThirdPartyToken.model_construct(**row["token_data"]))
-        return user_tokens
-
-
-async def get_token(
-    app: web.Application, user_id: UserID, service_id: str
-) -> UserThirdPartyToken:
-    async with get_database_engine_legacy(app).acquire() as conn:
-        result = await conn.execute(
-            sa.select(tokens.c.token_data).where(
-                and_(tokens.c.user_id == user_id, tokens.c.token_service == service_id)
+    async def list_tokens(
+        self,
+        connection: AsyncConnection | None = None,
+        *,
+        user_id: UserID,
+    ) -> list[UserThirdPartyToken]:
+        async with pass_or_acquire_connection(self.engine, connection) as conn:
+            result = await conn.execute(
+                sa.select(tokens.c.token_data).where(tokens.c.user_id == user_id)
             )
-        )
-        if row := await result.first():
-            return UserThirdPartyToken.model_construct(**row["token_data"])
-        raise TokenNotFoundError(service_id=service_id)
+            return [
+                UserThirdPartyToken.model_construct(**row["token_data"])
+                for row in result.fetchall()
+            ]
 
-
-async def update_token(
-    app: web.Application, user_id: UserID, service_id: str, token_data: dict[str, str]
-) -> UserThirdPartyToken:
-    async with get_database_engine_legacy(app).acquire() as conn:
-        result = await conn.execute(
-            sa.select(tokens.c.token_data, tokens.c.token_id).where(
-                (tokens.c.user_id == user_id) & (tokens.c.token_service == service_id)
+    async def get_token(
+        self,
+        connection: AsyncConnection | None = None,
+        *,
+        user_id: UserID,
+        service_id: str,
+    ) -> UserThirdPartyToken:
+        async with pass_or_acquire_connection(self.engine, connection) as conn:
+            result = await conn.execute(
+                sa.select(tokens.c.token_data).where(
+                    and_(
+                        tokens.c.user_id == user_id,
+                        tokens.c.token_service == service_id,
+                    )
+                )
             )
-        )
-        row = await result.first()
-        if not row:
+            if row := result.one_or_none():
+                return UserThirdPartyToken.model_construct(**row["token_data"])
             raise TokenNotFoundError(service_id=service_id)
 
-        data = dict(row["token_data"])
-        tid = row["token_id"]
-        data.update(token_data)
-
-        resp = await conn.execute(
-            tokens.update()
-            .where(tokens.c.token_id == tid)
-            .values(token_data=data)
-            .returning(literal_column("*"))
-        )
-        assert resp.rowcount == 1  # nosec
-        updated_token = await resp.fetchone()
-        assert updated_token  # nosec
-        return UserThirdPartyToken.model_construct(**updated_token["token_data"])
-
-
-async def delete_token(app: web.Application, user_id: UserID, service_id: str) -> None:
-    async with get_database_engine_legacy(app).acquire() as conn:
-        await conn.execute(
-            tokens.delete().where(
-                and_(tokens.c.user_id == user_id, tokens.c.token_service == service_id)
+    async def update_token(
+        self,
+        connection: AsyncConnection | None = None,
+        *,
+        user_id: UserID,
+        service_id: str,
+        token_data: dict[str, str],
+    ) -> UserThirdPartyToken:
+        async with transaction_context(self.engine, connection) as conn:
+            result = await conn.execute(
+                sa.select(tokens.c.token_data, tokens.c.token_id).where(
+                    (tokens.c.user_id == user_id)
+                    & (tokens.c.token_service == service_id)
+                )
             )
-        )
+            row = result.one_or_none()
+            if not row:
+                raise TokenNotFoundError(service_id=service_id)
+
+            data = dict(row["token_data"])
+            tid = row["token_id"]
+            data.update(token_data)
+
+            result = await conn.execute(
+                tokens.update()
+                .where(tokens.c.token_id == tid)
+                .values(token_data=data)
+                .returning(literal_column("*"))
+            )
+            updated_token = result.one()
+            assert updated_token  # nosec
+            return UserThirdPartyToken.model_construct(**updated_token["token_data"])
+
+    async def delete_token(
+        self,
+        connection: AsyncConnection | None = None,
+        *,
+        user_id: UserID,
+        service_id: str,
+    ) -> None:
+        async with transaction_context(self.engine, connection) as conn:
+            await conn.execute(
+                tokens.delete().where(
+                    and_(
+                        tokens.c.user_id == user_id,
+                        tokens.c.token_service == service_id,
+                    )
+                )
+            )
