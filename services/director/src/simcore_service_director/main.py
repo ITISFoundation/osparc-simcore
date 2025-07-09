a76a8f6f2f0871dc464dc25ad1daf9f6f0d1353e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-07-09 14:46:39 +0200

    director

--- services/director/src/simcore_service_director/main.py
+++ services/director/src/simcore_service_director/main.py
@@ -1,7 +1,5 @@
 """Main application to be deployed by uvicorn (or equivalent) server"""
 
-import logging
-
 from fastapi import FastAPI
 from servicelib.logging_utils import setup_loggers
 from simcore_service_director.core.application import create_app
@@ -9,13 +7,12 @@ from simcore_service_director.core.settings import ApplicationSettings
 
 _the_settings = ApplicationSettings.create_from_envs()
 
-# SEE https://github.com/ITISFoundation/osparc-simcore/issues/3148
-logging.basicConfig(level=_the_settings.DIRECTOR_LOGLEVEL)
-logging.root.setLevel(_the_settings.DIRECTOR_LOGLEVEL)
 setup_loggers(
     log_format_local_dev_enabled=_the_settings.DIRECTOR_LOG_FORMAT_LOCAL_DEV_ENABLED,
     logger_filter_mapping=_the_settings.DIRECTOR_LOG_FILTER_MAPPING,
     tracing_settings=_the_settings.DIRECTOR_TRACING,
+    log_base_level=_the_settings.log_level,
+    noisy_loggers=None,
 )
 
 # SINGLETON FastAPI app
