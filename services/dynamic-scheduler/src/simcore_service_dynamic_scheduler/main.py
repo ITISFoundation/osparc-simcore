ef558581c24f9eb2f752f8aae9deb124f662e032
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-07-09 14:47:05 +0200

    dynamic-scheduler

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/main.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/main.py
@@ -1,19 +1,17 @@
 """Main application to be deployed by uvicorn (or equivalent) server"""
 
-import logging
-
 from fastapi import FastAPI
 from servicelib.logging_utils import setup_loggers
 from simcore_service_dynamic_scheduler.core.application import create_app
 from simcore_service_dynamic_scheduler.core.settings import ApplicationSettings
 
 _the_settings = ApplicationSettings.create_from_envs()
-logging.basicConfig(level=_the_settings.DYNAMIC_SCHEDULER_LOGLEVEL.value)
-logging.root.setLevel(_the_settings.DYNAMIC_SCHEDULER_LOGLEVEL.value)
 setup_loggers(
     log_format_local_dev_enabled=_the_settings.DYNAMIC_SCHEDULER_LOG_FORMAT_LOCAL_DEV_ENABLED,
     logger_filter_mapping=_the_settings.DYNAMIC_SCHEDULER_LOG_FILTER_MAPPING,
     tracing_settings=_the_settings.DYNAMIC_SCHEDULER_TRACING,
+    log_base_level=_the_settings.log_level,
+    noisy_loggers=None,
 )
 
 # SINGLETON FastAPI app
