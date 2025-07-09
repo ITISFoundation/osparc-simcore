fdb2b6e1b29f8cf676e0a5192104c4c77e61085b
odeimaiz <maiz@itis.swiss>
2025-07-09 12:36:30 +0200

    back to New

--- services/static-webserver/client/source/class/osparc/dashboard/NewPlusButton.js
+++ services/static-webserver/client/source/class/osparc/dashboard/NewPlusButton.js
@@ -24,7 +24,7 @@ qx.Class.define("osparc.dashboard.NewPlusButton", {
     this.set({
       appearance: "strong-button",
       icon: osparc.dashboard.CardBase.NEW_ICON + "20",
-      label: this.tr("New 102"),
+      label: this.tr("New"),
       font: "text-16",
       gap: 15,
       padding: 15,
