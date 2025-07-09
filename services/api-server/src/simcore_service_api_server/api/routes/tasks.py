a80e2938711e675adb075fbf005ba4b6b93ca0e1
Mads Bisgaard <bisgaard@itis.swiss>
2025-07-09 05:16:10 +0200

    improve function naming

--- services/api-server/src/simcore_service_api_server/api/routes/tasks.py
+++ services/api-server/src/simcore_service_api_server/api/routes/tasks.py
@@ -32,7 +32,7 @@ router = APIRouter()
 _logger = logging.getLogger(__name__)
 
 
-def _get_job_id_data(user_id: UserID, product_name: ProductName) -> AsyncJobFilter:
+def _get_job_filter(user_id: UserID, product_name: ProductName) -> AsyncJobFilter:
     return AsyncJobFilter(
         user_id=user_id, product_name=product_name, client_name=_ASYNC_JOB_CLIENT_NAME
     )
@@ -65,7 +65,7 @@ async def list_tasks(
     async_jobs: Annotated[AsyncJobClient, Depends(get_async_jobs_client)],
 ):
     user_async_jobs = await async_jobs.list_jobs(
-        job_filter=_get_job_id_data(user_id, product_name),
+        job_filter=_get_job_filter(user_id, product_name),
         filter_="",
     )
     app_router = app.router
@@ -106,7 +106,7 @@ async def get_task_status(
 ):
     async_job_rpc_status = await async_jobs.status(
         job_id=task_id,
-        job_filter=_get_job_id_data(user_id, product_name),
+        job_filter=_get_job_filter(user_id, product_name),
     )
     _task_id = f"{async_job_rpc_status.job_id}"
     return TaskStatus(
@@ -138,7 +138,7 @@ async def cancel_task(
 ):
     await async_jobs.cancel(
         job_id=task_id,
-        job_filter=_get_job_id_data(user_id, product_name),
+        job_filter=_get_job_filter(user_id, product_name),
     )
 
 
@@ -172,6 +172,6 @@ async def get_task_result(
 ):
     async_job_rpc_result = await async_jobs.result(
         job_id=task_id,
-        job_filter=_get_job_id_data(user_id, product_name),
+        job_filter=_get_job_filter(user_id, product_name),
     )
     return TaskResult(result=async_job_rpc_result.result, error=None)
