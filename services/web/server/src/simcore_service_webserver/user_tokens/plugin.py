a7c277eb587b84b89a7e51ed2428f2cb19f33217
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-07-09 14:57:59 +0200

    rest

--- services/web/server/src/simcore_service_webserver/user_tokens/plugin.py
+++ services/web/server/src/simcore_service_webserver/user_tokens/plugin.py
@@ -3,11 +3,11 @@ import logging
 from aiohttp import web
 from servicelib.aiohttp.application_setup import ensure_single_setup
 
-from ._controller import rest
+from ._controller import user_tokens_rest
 
 _logger = logging.getLogger(__name__)
 
 
 @ensure_single_setup(__name__, logger=_logger)
 def setup_user_tokens_feature(app: web.Application):
-    app.add_routes(rest.routes)
+    app.add_routes(user_tokens_rest.routes)
