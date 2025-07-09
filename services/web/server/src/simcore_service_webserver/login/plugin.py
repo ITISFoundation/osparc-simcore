bdacf61fa3b317d573ebc5269aae1eb4def76cc9
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-07-09 15:35:45 +0200

    fix: correct function name from setup_login_account to setup_login_accounts

--- services/web/server/src/simcore_service_webserver/login/plugin.py
+++ services/web/server/src/simcore_service_webserver/login/plugin.py
@@ -11,7 +11,7 @@ from servicelib.aiohttp.application_setup import (
 )
 from settings_library.email import SMTPSettings
 from settings_library.postgres import PostgresSettings
-from simcore_service_webserver.login_accounts.plugin import setup_login_account
+from simcore_service_webserver.login_accounts.plugin import setup_login_accounts
 
 from ..constants import (
     APP_PUBLIC_CONFIG_PER_PRODUCT,
@@ -147,7 +147,7 @@ def setup_login(app: web.Application):
 
     app.router.add_routes(auth.routes)
     setup_login_auth(app)
-    setup_login_account(app)
+    setup_login_accounts(app)
 
     app.router.add_routes(confirmation.routes)
     app.router.add_routes(registration.routes)
