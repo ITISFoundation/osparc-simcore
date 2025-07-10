16c505eb759c3cbeea8919df377e7426ae211bb1
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-07-10 09:35:07 +0200

    rename

--- packages/service-library/src/servicelib/fastapi/logging_lifespan.py
+++ packages/service-library/src/servicelib/fastapi/logging_lifespan.py
@@ -16,7 +16,7 @@ from .lifespan_utils import Lifespan
 _logger = logging.getLogger(__name__)
 
 
-def setup_logging(
+def setup_logging_lifespan(
     *,
     log_format_local_dev_enabled: bool,
     logger_filter_mapping: dict[LoggerName, list[MessageSubstring]],
