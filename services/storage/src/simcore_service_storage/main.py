61f059a899e495ac75097f8ea5b8deb1582dca97
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-07-10 10:53:06 +0200

    storage uses async loggers

--- services/storage/src/simcore_service_storage/main.py
+++ services/storage/src/simcore_service_storage/main.py
@@ -1,10 +1,15 @@
 """Main application to be deployed in for example uvicorn."""
 
+import logging
 from typing import Final
 
-from servicelib.logging_utils import setup_loggers
+from common_library.json_serialization import json_dumps
+from fastapi import FastAPI
+from servicelib.fastapi.logging_lifespan import setup_logging_shutdown_event
 from simcore_service_storage.core.application import create_app
 from simcore_service_storage.core.settings import ApplicationSettings
+
+_logger = logging.getLogger(__name__)
 
 _NOISY_LOGGERS: Final[tuple[str, ...]] = (
     "aio_pika",
@@ -16,14 +21,21 @@ _NOISY_LOGGERS: Final[tuple[str, ...]] = (
     "werkzeug",
 )
 
-_settings = ApplicationSettings.create_from_envs()
 
-setup_loggers(
-    log_format_local_dev_enabled=_settings.STORAGE_LOG_FORMAT_LOCAL_DEV_ENABLED,
-    logger_filter_mapping=_settings.STORAGE_LOG_FILTER_MAPPING,
-    tracing_settings=_settings.STORAGE_TRACING,
-    log_base_level=_settings.log_level,
-    noisy_loggers=_NOISY_LOGGERS,
-)
+def app_factory() -> FastAPI:
+    app_settings = ApplicationSettings.create_from_envs()
+    logging_shutdown_event = setup_logging_shutdown_event(
+        log_format_local_dev_enabled=app_settings.STORAGE_LOG_FORMAT_LOCAL_DEV_ENABLED,
+        logger_filter_mapping=app_settings.STORAGE_LOG_FILTER_MAPPING,
+        tracing_settings=app_settings.STORAGE_TRACING,
+        log_base_level=app_settings.log_level,
+        noisy_loggers=_NOISY_LOGGERS,
+    )
 
-app = create_app(_settings)
+    _logger.info(
+        "Application settings: %s",
+        json_dumps(app_settings, indent=2, sort_keys=True),
+    )
+    app = create_app(settings=app_settings)
+    app.add_event_handler("shutdown", logging_shutdown_event)
+    return app
