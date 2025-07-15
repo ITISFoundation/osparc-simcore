9a2c13e44b42041bf20d8cd6c29f91e170800b7d
Mads Bisgaard <bisgaard@itis.swiss>
2025-07-15 09:43:10 +0200

    make project_id a kwargs

--- services/api-server/src/simcore_service_api_server/services_rpc/director_v2.py
+++ services/api-server/src/simcore_service_api_server/services_rpc/director_v2.py
@@ -19,7 +19,7 @@ class DirectorV2Service:
     @_exception_mapper(
         rpc_exception_map={ComputationalTaskMissingError: JobNotFoundError}
     )
-    async def get_computation_task_log_file_ids(self, project_id: ProjectID):
+    async def get_computation_task_log_file_ids(self, *, project_id: ProjectID):
         return await computations_tasks.get_computation_task_log_file_ids(
             self._rpc_client, project_id=project_id
         )
