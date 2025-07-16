66a231a95b9e63e6cd5f63e596a13b6e59aacafb
Andrei Neagu <neagu@itis.swiss>
2025-07-16 14:27:51 +0200

    quadruple dollar escape

--- packages/service-integration/src/service_integration/__init__.py
+++ packages/service-integration/src/service_integration/__init__.py
@@ -1,5 +1,6 @@
-""" Library to facilitate the integration of user services running in osparc-simcore
+"""Library to facilitate the integration of user services running in osparc-simcore"""
 
-"""
-
+from . import _patch
 from ._meta import __version__
+
+_patch.patch_osparc_variable_identifier()
