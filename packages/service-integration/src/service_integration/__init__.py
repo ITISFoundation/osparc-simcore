844b0f41746e268e8461090b27f466ac2aba8414
Andrei Neagu <neagu@itis.swiss>
2025-07-17 15:01:36 +0200

    removed patch solution

--- packages/service-integration/src/service_integration/__init__.py
+++ packages/service-integration/src/service_integration/__init__.py
@@ -1,6 +1,3 @@
 """Library to facilitate the integration of user services running in osparc-simcore"""
 
-from . import _patch
 from ._meta import __version__
-
-_patch.patch_osparc_variable_identifier()
