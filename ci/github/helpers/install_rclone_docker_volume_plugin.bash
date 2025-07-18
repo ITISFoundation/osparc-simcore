b33d48e2182a6a9e8c4323febbe694578320a924
Andrei Neagu <neagu@itis.swiss>
2025-07-18 12:35:19 +0200

    upgraded rclone version

--- ci/github/helpers/install_rclone_docker_volume_plugin.bash
+++ ci/github/helpers/install_rclone_docker_volume_plugin.bash
@@ -10,7 +10,7 @@ set -o pipefail # don't hide errors within pipes
 IFS=$'\n\t'
 
 # Installation instructions from https://rclone.org/docker/
-R_CLONE_VERSION="1.66.0"
+R_CLONE_VERSION="1.70.3"
 mkdir --parents /var/lib/docker-plugins/rclone/config
 mkdir --parents /var/lib/docker-plugins/rclone/cache
 docker plugin install rclone/docker-volume-rclone:amd64-${R_CLONE_VERSION} args="-v" --alias rclone --grant-all-permissions
