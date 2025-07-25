f6ae1d71a4b56605e76001cd675b3ba972c20628
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-07-25 14:31:56 +0200

    fix: remove workbench from faker factory

--- packages/pytest-simcore/src/pytest_simcore/helpers/faker_factories.py
+++ packages/pytest-simcore/src/pytest_simcore/helpers/faker_factories.py
@@ -173,7 +173,6 @@ def random_project(fake: Faker = DEFAULT_FAKER, **overrides) -> dict[str, Any]:
         "prj_owner": fake.pyint(),
         "thumbnail": fake.image_url(width=120, height=120),
         "access_rights": {},
-        "workbench": {},
         "published": False,
     }
 
