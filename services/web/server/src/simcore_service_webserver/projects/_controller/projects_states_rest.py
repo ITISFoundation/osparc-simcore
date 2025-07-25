e8fd370712332b9c4c92feb140429c3e2e040b55
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-07-25 09:51:05 +0200

    refactor

--- services/web/server/src/simcore_service_webserver/projects/_controller/projects_states_rest.py
+++ services/web/server/src/simcore_service_webserver/projects/_controller/projects_states_rest.py
@@ -119,8 +119,8 @@ async def open_project(request: web.Request) -> web.Response:
         # Connect the socket_id to a project room
         with managed_resource(
             req_ctx.user_id, client_session_id, request.app
-        ) as resource_registry:
-            _socket_id = await resource_registry.get_socket_id()
+        ) as user_session:
+            _socket_id = await user_session.get_socket_id()
         if _socket_id is None:
             raise web.HTTPUnprocessableEntity(
                 text=user_message(
