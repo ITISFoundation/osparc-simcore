77d26ecbfdafc165291b58b89e753768fb82bb5c
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-07-28 10:17:41 +0200

    fixes projecst repo

--- services/director-v2/src/simcore_service_director_v2/modules/db/repositories/projects.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/repositories/projects.py
@@ -4,9 +4,10 @@ import sqlalchemy as sa
 from models_library.projects import ProjectAtDB, ProjectID
 from models_library.projects_nodes_io import NodeID
 from simcore_postgres_database.utils_projects_nodes import ProjectNodesRepo
+from simcore_postgres_database.utils_repos import pass_or_acquire_connection
 
 from ....core.errors import ProjectNotFoundError
-from ..tables import projects
+from ..tables import projects, projects_nodes
 from ._base import BaseRepository
 
 logger = logging.getLogger(__name__)
@@ -27,11 +28,15 @@ class ProjectsRepository(BaseRepository):
     async def is_node_present_in_workbench(
         self, project_id: ProjectID, node_uuid: NodeID
     ) -> bool:
-        try:
-            project = await self.get_project(project_id)
-            return f"{node_uuid}" in project.workbench
-        except ProjectNotFoundError:
-            return False
+        async with pass_or_acquire_connection(self.db_engine) as conn:
+            result = await conn.execute(
+                sa.select(projects_nodes.c.project_node_id).where(
+                    projects_nodes.c.project_uuid == str(project_id),
+                    projects_nodes.c.node_id == str(node_uuid),
+                )
+            )
+            project_node = result.one_or_none()
+            return project_node is not None
 
     async def get_project_id_from_node(self, node_id: NodeID) -> ProjectID:
         async with self.db_engine.connect() as conn:
--- services/director-v2/src/simcore_service_director_v2/modules/db/tables.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/tables.py
@@ -1,26 +1,14 @@
-from simcore_postgres_database.models.comp_pipeline import StateType, comp_pipeline
-from simcore_postgres_database.models.comp_run_snapshot_tasks import (
-    comp_run_snapshot_tasks,
-)
-from simcore_postgres_database.models.comp_runs import comp_runs
-from simcore_postgres_database.models.comp_tasks import NodeClass, comp_tasks
-from simcore_postgres_database.models.groups import user_to_groups
-from simcore_postgres_database.models.groups_extra_properties import (
-    groups_extra_properties,
-)
-from simcore_postgres_database.models.projects import ProjectType, projects
-from simcore_postgres_database.models.projects_networks import projects_networks
-
-__all__ = [
+__all__: tuple[str, ...] = (
+    "NodeClass",
+    "ProjectType",
+    "StateType",
     "comp_pipeline",
+    "comp_run_snapshot_tasks",
     "comp_runs",
     "comp_tasks",
     "groups_extra_properties",
-    "NodeClass",
-    "projects_networks",
     "projects",
-    "ProjectType",
-    "StateType",
+    "projects_networks",
+    "projects_nodes",
     "user_to_groups",
-    "comp_run_snapshot_tasks",
-]
+)
