2d44252785570417a7e5537f4f4aa310e4fac4b6
Andrei Neagu <neagu@itis.swiss>
2025-08-06 15:54:54 +0200

    avoid tests hanging

--- packages/service-library/tests/long_running_tasks/conftest.py
+++ packages/service-library/tests/long_running_tasks/conftest.py
@@ -1,8 +1,11 @@
+import logging
 from collections.abc import AsyncIterator, Awaitable, Callable
 from datetime import timedelta
 
 import pytest
 from faker import Faker
+from pytest_mock import MockerFixture
+from servicelib.logging_utils import log_catch
 from servicelib.long_running_tasks.task import (
     RedisNamespace,
     TasksManager,
@@ -10,13 +13,19 @@ from servicelib.long_running_tasks.task import (
 from settings_library.redis import RedisSettings
 from utils import TEST_CHECK_STALE_INTERVAL_S
 
+_logger = logging.getLogger(__name__)
+
 
 @pytest.fixture
 async def get_tasks_manager(
-    faker: Faker,
+    faker: Faker, mocker: MockerFixture
 ) -> AsyncIterator[
     Callable[[RedisSettings, RedisNamespace | None], Awaitable[TasksManager]]
 ]:
+    mocker.patch(
+        "servicelib.long_running_tasks.task._CANCEL_TASKS_CHECK_INTERVAL",
+        new=timedelta(seconds=TEST_CHECK_STALE_INTERVAL_S),
+    )
     managers: list[TasksManager] = []
 
     async def _(
@@ -35,4 +44,5 @@ async def get_tasks_manager(
     yield _
 
     for manager in managers:
-        await manager.teardown()
+        with log_catch(_logger, reraise=False):
+            await manager.teardown()
