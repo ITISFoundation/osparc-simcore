d4b90fff6dcc26bd8a3ce47397e53f0253ddc715
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-08-07 13:27:37 +0200

    Add trigger/function when updating project nodes

---
+++ packages/postgres-database/src/simcore_postgres_database/migration/versions/c9c165644731_update_project_last_changed_date_column_.py
@@ -0,0 +1,63 @@
+"""Update project last_changed_date column when a node is created, updated or deleted.
+
+Revision ID: c9c165644731
+Revises: 201aa37f4d9a
+Create Date: 2025-08-07 10:26:37.577990+00:00
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "c9c165644731"
+down_revision = "201aa37f4d9a"
+branch_labels = None
+depends_on = None
+
+
+update_projects_last_changed_date = sa.DDL(
+    """
+CREATE OR REPLACE FUNCTION update_projects_last_changed_date()
+RETURNS TRIGGER AS $$
+DECLARE
+    project_uuid VARCHAR;
+BEGIN
+    IF TG_OP = 'DELETE' THEN
+        project_uuid := OLD.project_uuid;
+    ELSE
+        project_uuid := NEW.project_uuid;
+    END IF;
+
+    UPDATE projects
+    SET last_changed_date = NOW()
+    WHERE uuid = project_uuid;
+
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+"""
+)
+
+
+projects_nodes_changed = sa.DDL(
+    """
+DROP TRIGGER IF EXISTS projects_nodes_changed on projects_nodes;
+CREATE TRIGGER projects_nodes_changed
+AFTER INSERT OR UPDATE OR DELETE ON projects_nodes
+FOR EACH ROW
+EXECUTE FUNCTION update_projects_last_changed_date();
+"""
+)
+
+
+def upgrade():
+    op.execute(update_projects_last_changed_date)
+    op.execute(projects_nodes_changed)
+
+
+def downgrade():
+    op.execute(sa.DDL("DROP FUNCTION IF EXISTS update_projects_last_changed_date();"))
+    op.execute(
+        sa.DDL("DROP TRIGGER IF EXISTS projects_nodes_changed ON projects_nodes;")
+    )
