a56062b040c31e2a55bddf190c5b1850d53bb785
Mads Bisgaard <bisgaard@itis.swiss>
2025-08-11 16:00:45 +0200

    first attempt to do migration

--- packages/postgres-database/src/simcore_postgres_database/models/projects_to_jobs.py
+++ packages/postgres-database/src/simcore_postgres_database/models/projects_to_jobs.py
@@ -28,6 +28,13 @@ projects_to_jobs = sa.Table(
         "the relative resource name is shelves/shelf1/jobs/job2, "
         "the parent resource name is shelves/shelf1.",
     ),
+    sa.Column(
+        "storage_data_deleted",
+        sa.Boolean,
+        nullable=False,
+        default=False,
+        doc="Indicates whether the job's S3 data has been actively deleted.",
+    ),
     # Composite key (project_uuid, job_parent_resource_name) uniquely identifies very row
     sa.UniqueConstraint(
         "project_uuid",
