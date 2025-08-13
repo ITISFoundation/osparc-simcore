393e7dbe4b4004016d8d569f3795c462210bc244
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-08-13 11:48:15 +0200

    fix: tags

--- services/web/server/src/simcore_service_webserver/projects/_tags_service.py
+++ services/web/server/src/simcore_service_webserver/projects/_tags_service.py
@@ -8,6 +8,7 @@ from models_library.users import UserID
 from models_library.workspaces import UserWorkspaceWithAccessRights
 
 from ..workspaces import _workspaces_repository as workspaces_workspaces_repository
+from . import _projects_repository
 from ._access_rights_service import check_user_project_permission
 from ._projects_repository_legacy import ProjectDBAPI
 from .models import ProjectDict
@@ -20,7 +21,9 @@ async def add_tag(
 ) -> ProjectDict:
     db: ProjectDBAPI = ProjectDBAPI.get_from_app_context(app)
 
-    product_name = await db.get_project_product(project_uuid)
+    product_name = await _projects_repository.get_project_product(
+        app, project_uuid=project_uuid
+    )
     await check_user_project_permission(
         app,
         project_id=project_uuid,
