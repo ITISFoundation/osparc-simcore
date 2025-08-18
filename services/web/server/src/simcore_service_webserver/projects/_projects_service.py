ad688c473f0180ddac14bc1c8661579b971e60c3
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-08-18 11:18:48 +0200

    this is not looking good

--- services/web/server/src/simcore_service_webserver/projects/_projects_service.py
+++ services/web/server/src/simcore_service_webserver/projects/_projects_service.py
@@ -714,7 +714,7 @@ async def _check_project_node_has_all_required_inputs(
         )
 
 
-async def _start_dynamic_service(
+async def _start_dynamic_service(  # noqa: C901, PLR0915
     request: web.Request,
     *,
     service_key: ServiceKey,
