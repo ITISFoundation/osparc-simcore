5d0d7bee901e1e5632e3f9984e1a0e581a459d7f
Andrei Neagu <neagu@itis.swiss>
2025-08-18 14:03:37 +0200

    fixed

--- services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/services/outputs.py
+++ services/dynamic-sidecar/src/simcore_service_dynamic_sidecar/services/outputs.py
@@ -1,4 +1,7 @@
+from ..modules.outputs import disable_event_propagation, enable_event_propagation
+
 __all__: tuple[str, ...] = (
     "disable_event_propagation",
     "enable_event_propagation",
 )
+# nopycln: file
