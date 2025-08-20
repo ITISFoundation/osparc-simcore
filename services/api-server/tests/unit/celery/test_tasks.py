c297397ba4330c3de99008feff0a64f10887404a
Mads Bisgaard <bisgaard@itis.swiss>
2025-08-20 21:49:50 +0200

    minor change

--- services/api-server/tests/unit/celery/test_tasks.py
+++ services/api-server/tests/unit/celery/test_tasks.py
@@ -1,7 +1,6 @@
 # pylint: disable=redefined-outer-name
 # pylint: disable=unused-argument
 
-from typing import Any
 
 import pytest
 from celery.contrib.testing.worker import TestWorkController
@@ -10,61 +9,18 @@ from fastapi import status
 from httpx import AsyncClient, BasicAuth
 from models_library.api_schemas_long_running_tasks.tasks import TaskGet, TaskStatus
 from models_library.api_schemas_rpc_async_jobs.exceptions import (
-    BaseAsyncjobRpcError,
     JobAbortedError,
     JobError,
     JobNotDoneError,
     JobSchedulerError,
 )
-from pytest_mock import MockerFixture, MockType
-from pytest_simcore.helpers.async_jobs_server import AsyncJobSideEffects
+from pytest_mock import MockType
 from simcore_service_api_server.models.schemas.base import ApiServerEnvelope
 
 pytest_simcore_core_services_selection = ["postgres", "rabbit"]
 
 
 _faker = Faker()
-
-
-@pytest.fixture
-async def async_jobs_rpc_side_effects(
-    async_job_error: BaseAsyncjobRpcError | None,
-) -> Any:
-    return AsyncJobSideEffects(exception=async_job_error)
-
-
-@pytest.fixture
-def mocked_async_jobs_rpc_api(
-    mocker: MockerFixture,
-    async_jobs_rpc_side_effects: Any,
-    mocked_app_dependencies: None,
-) -> dict[str, MockType]:
-    """
-    Mocks the catalog's simcore service RPC API for testing purposes.
-    """
-    from servicelib.rabbitmq.rpc_interfaces.async_jobs import async_jobs
-
-    mocks = {}
-
-    # Get all callable methods from the side effects class that are not built-ins
-    side_effect_methods = [
-        method_name
-        for method_name in dir(async_jobs_rpc_side_effects)
-        if not method_name.startswith("_")
-        and callable(getattr(async_jobs_rpc_side_effects, method_name))
-    ]
-
-    # Create mocks for each method in catalog_rpc that has a corresponding side effect
-    for method_name in side_effect_methods:
-        assert hasattr(async_jobs, method_name)
-        mocks[method_name] = mocker.patch.object(
-            async_jobs,
-            method_name,
-            autospec=True,
-            side_effect=getattr(async_jobs_rpc_side_effects, method_name),
-        )
-
-    return mocks
 
 
 @pytest.mark.parametrize(
