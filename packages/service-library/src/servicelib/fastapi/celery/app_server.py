dd0a684662e45ce188329ece363c0f616c4b2407
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-08-20 14:58:31 +0200

    fix: typecheck

--- packages/service-library/src/servicelib/fastapi/celery/app_server.py
+++ packages/service-library/src/servicelib/fastapi/celery/app_server.py
@@ -21,7 +21,9 @@ class FastAPIAppServer(BaseAppServer[FastAPI]):
 
     @property
     def task_manager(self) -> TaskManager:
-        return self.app.state.task_manager
+        assert self.app.state.task_manager, "Task manager is not initialized"  # nosec
+        task_manager: TaskManager = self.app.state.task_manager
+        return task_manager
 
     async def lifespan(self, startup_completed_event: threading.Event) -> None:
         async with LifespanManager(
