2f57d5efa02b8f286d3792526c6a23293c13b2c9
Mads Bisgaard <bisgaard@itis.swiss>
2025-08-20 09:56:21 +0200

    register task

--- services/api-server/src/simcore_service_api_server/celery/_worker_tasks/tasks.py
+++ services/api-server/src/simcore_service_api_server/celery/_worker_tasks/tasks.py
@@ -1,15 +1,33 @@
 import logging
 
 from celery import Celery  # type: ignore[import-untyped]
+from celery_library.task import register_task
 from celery_library.types import register_celery_types, register_pydantic_types
+from models_library.functions import (
+    RegisteredProjectFunction,
+    RegisteredPythonCodeFunction,
+    RegisteredSolverFunction,
+)
 from servicelib.logging_utils import log_context
+
+from ...api.dependencies.authentication import Identity
+from ...models.api_resources import JobLinks
+from ...models.schemas.jobs import JobPricingSpecification
+from ._functions_tasks import run_function
 
 _logger = logging.getLogger(__name__)
 
 
 def setup_worker_tasks(app: Celery) -> None:
     register_celery_types()
-    register_pydantic_types()
+    register_pydantic_types(
+        Identity,
+        RegisteredProjectFunction,
+        RegisteredPythonCodeFunction,
+        RegisteredSolverFunction,
+        JobPricingSpecification,
+        JobLinks,
+    )
 
     with log_context(_logger, logging.INFO, msg="worker task registration"):
-        pass
+        register_task(app, run_function)
