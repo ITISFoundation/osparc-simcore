1d4df87dc5168b233e0d424532194ea450e39096
Mads Bisgaard <bisgaard@itis.swiss>
2025-08-20 14:54:26 +0200

    further improvements to test

--- services/api-server/tests/unit/celery/test_functions.py
+++ services/api-server/tests/unit/celery/test_functions.py
@@ -6,17 +6,26 @@ from celery import Celery, Task
 from celery.contrib.testing.worker import TestWorkController
 from celery_library.task import register_task
 from faker import Faker
+from fastapi import FastAPI
+from httpx import AsyncClient, BasicAuth
 from models_library.functions import (
     FunctionClass,
     FunctionID,
     FunctionInputs,
     FunctionJobID,
     RegisteredFunction,
+    RegisteredProjectFunction,
     RegisteredProjectFunctionJob,
 )
 from models_library.projects import ProjectID
 from servicelib.celery.models import TaskID
+from servicelib.common_headers import (
+    X_SIMCORE_PARENT_NODE_ID,
+    X_SIMCORE_PARENT_PROJECT_UUID,
+)
+from simcore_service_api_server._meta import API_VTAG
 from simcore_service_api_server.api.dependencies.authentication import Identity
+from simcore_service_api_server.api.routes.functions_routes import get_function
 from simcore_service_api_server.celery._worker_tasks._functions_tasks import (
     run_function as run_function_task,
 )
@@ -72,5 +81,28 @@ def _register_fake_run_function_task() -> Callable[[Celery], None]:
 
 @pytest.mark.parametrize("register_celery_tasks", [_register_fake_run_function_task()])
 @pytest.mark.parametrize("add_worker_tasks", [False])
-async def test_with_fake_run_function(with_storage_celery_worker: TestWorkController):
-    pass
+async def test_with_fake_run_function(
+    app: FastAPI,
+    client: AsyncClient,
+    auth: BasicAuth,
+    with_storage_celery_worker: TestWorkController,
+):
+
+    extra = RegisteredProjectFunction.model_config.get("json_schema_extra")
+    assert extra
+    app.dependency_overrides[get_function] = (
+        lambda: RegisteredProjectFunction.model_validate(extra["examples"][0])
+    )
+
+    headers = {}
+    headers[X_SIMCORE_PARENT_PROJECT_UUID] = "null"
+    headers[X_SIMCORE_PARENT_NODE_ID] = "null"
+
+    response = await client.post(
+        f"/{API_VTAG}/functions/{_faker.uuid4()}:run",
+        auth=auth,
+        json={},
+        headers=headers,
+    )
+
+    assert response.status_code == 200
