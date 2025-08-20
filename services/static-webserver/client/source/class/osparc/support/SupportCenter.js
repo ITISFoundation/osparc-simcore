e7ff09fcfe768fe155b5c09562df924243bdc42d
odeimaiz <maiz@itis.swiss>
2025-08-20 18:08:28 +0200

    cleanup

--- services/static-webserver/client/source/class/osparc/support/SupportCenter.js
+++ services/static-webserver/client/source/class/osparc/support/SupportCenter.js
@@ -37,10 +37,9 @@ qx.Class.define("osparc.support.SupportCenter", {
 
     this.getChildControl("conversations-intro-text");
     this.getChildControl("conversations-list");
-    // OM uncomment this
-    // if (!osparc.store.Products.getInstance().amIASupportUser()) {
-      this.getChildControl("ask-a-question-button")
-    // }
+    if (!osparc.store.Products.getInstance().amIASupportUser()) {
+      this.getChildControl("ask-a-question-button");
+    }
   },
 
   statics: {
