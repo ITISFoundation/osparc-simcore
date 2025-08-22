1009d666448aeb24755ba1ea3b52614250448176
odeimaiz <maiz@itis.swiss>
2025-08-22 09:38:33 +0200

    fix

--- services/static-webserver/client/source/class/osparc/dashboard/GroupedCardContainer.js
+++ services/static-webserver/client/source/class/osparc/dashboard/GroupedCardContainer.js
@@ -100,7 +100,7 @@ qx.Class.define("osparc.dashboard.GroupedCardContainer", {
             paddingBottom: 5,
             allowGrowX: false
           });
-          control.getChildControl("icon").set(this.getThumbnailProps(32));
+          control.getChildControl("icon").set(osparc.utils.Utils.getThumbnailProps(32));
           control.getChildControl("label").set({
             rich: true,
             wrap: true
