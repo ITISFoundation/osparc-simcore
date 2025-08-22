1a9d1eb992034616f19b7603e98dd8180398f6df
Andrei Neagu <neagu@itis.swiss>
2025-08-22 11:21:47 +0200

    updated specs

--- api/specs/web-server/_long_running_tasks.py
+++ api/specs/web-server/_long_running_tasks.py
@@ -32,44 +32,40 @@ _export_data_responses: dict[int | str, dict[str, Any]] = {
 @router.get(
     "/tasks",
     response_model=Envelope[list[TaskGet]],
-    name="list_tasks",
-    description="Lists all long running tasks",
     responses=_export_data_responses,
 )
-def get_async_jobs(): ...
+def list_tasks():
+    """Lists all long running tasks"""
 
 
 @router.get(
     "/tasks/{task_id}",
     response_model=Envelope[TaskStatus],
-    name="get_task_status",
-    description="Retrieves the status of a task",
     responses=_export_data_responses,
 )
-def get_async_job_status(
+def get_task_status(
     _path_params: Annotated[_PathParam, Depends()],
-): ...
+):
+    """Retrieves the status of a task"""
 
 
 @router.delete(
     "/tasks/{task_id}",
-    name="remove_task",
-    description="Cancels and removes a task",
     responses=_export_data_responses,
     status_code=status.HTTP_204_NO_CONTENT,
 )
-def cancel_async_job(
+def remove_task(
     _path_params: Annotated[_PathParam, Depends()],
-): ...
+):
+    """Cancels and removes a task"""
 
 
 @router.get(
     "/tasks/{task_id}/result",
     response_model=Any,
-    name="get_task_result",
-    description="Retrieves the result of a task",
     responses=_export_data_responses,
 )
-def get_async_job_result(
+def get_task_result(
     _path_params: Annotated[_PathParam, Depends()],
-): ...
+):
+    """Retrieves the result of a task"""
