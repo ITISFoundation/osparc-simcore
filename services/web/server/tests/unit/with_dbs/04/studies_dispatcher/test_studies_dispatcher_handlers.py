399dc0125f00eae97b91c04aca9c978597e44457
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-08-26 18:19:34 +0200

    patch

--- services/web/server/tests/unit/with_dbs/04/studies_dispatcher/test_studies_dispatcher_handlers.py
+++ services/web/server/tests/unit/with_dbs/04/studies_dispatcher/test_studies_dispatcher_handlers.py
@@ -432,7 +432,7 @@ async def test_dispatch_study_anonymously(
         return_value=None,
     )
     mock_dynamic_scheduler_update_project_networks = mocker.patch(
-        "simcore_service_webserver.studies_dispatcher._redirects_handlers.dynamic_scheduler_service.update_projects_networks",
+        "simcore_service_webserver.studies_dispatcher._controller.rest.redirects.dynamic_scheduler_service.update_projects_networks",
         return_value=None,
     )
 
@@ -498,7 +498,7 @@ async def test_dispatch_logged_in_user(
         return_value=None,
     )
     mock_dynamic_scheduler_update_project_networks = mocker.patch(
-        "simcore_service_webserver.studies_dispatcher._redirects_handlers.dynamic_scheduler_service.update_projects_networks",
+        "simcore_service_webserver.studies_dispatcher._controller.rest.redirects.dynamic_scheduler_service.update_projects_networks",
         return_value=None,
     )
 
