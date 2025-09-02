f9e1ba39594427eb5e88627f359e5a28244cd5aa
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-09-02 13:29:11 +0200

    change default

--- services/director-v2/src/simcore_service_director_v2/core/settings.py
+++ services/director-v2/src/simcore_service_director_v2/core/settings.py
@@ -83,7 +83,7 @@ class ComputationalBackendSettings(BaseCustomSettings):
         description=f"Default file link type to use with computational backend on-demand clusters '{list(FileLinkType)}'",
     )
     COMPUTATIONAL_BACKEND_MAX_WAITING_FOR_CLUSTER_TIMEOUT: datetime.timedelta = Field(
-        default=datetime.timedelta(minutes=30),
+        default=datetime.timedelta(minutes=10),
         description="maximum time a pipeline can wait for a cluster to start"
         "(default to seconds, or see https://pydantic-docs.helpmanual.io/usage/types/#datetime-types for string formating).",
     )
