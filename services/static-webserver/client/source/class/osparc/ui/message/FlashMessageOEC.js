314609829c8c422a8b31a14f5e6927dcc06030e2
odeimaiz <maiz@itis.swiss>
2025-09-02 12:19:39 +0200

    openSupportChat

--- services/static-webserver/client/source/class/osparc/ui/message/FlashMessageOEC.js
+++ services/static-webserver/client/source/class/osparc/ui/message/FlashMessageOEC.js
@@ -29,7 +29,7 @@ qx.Class.define("osparc.ui.message.FlashMessageOEC", {
     this.base(arguments, message, "ERROR", duration*2);
 
     if (osparc.product.Utils.isSupportEnabled()) {
-      console.log("bring it to support chat");
+      this.getChildControl("contact-support");
     } else {
       const oecAtom = this.getChildControl("oec-atom");
       this.bind("supportId", oecAtom, "label");
@@ -40,6 +40,7 @@ qx.Class.define("osparc.ui.message.FlashMessageOEC", {
   properties: {
     supportId: {
       check: "String",
+      init: "",
       nullable: true,
       event: "changeSupportId",
     },
@@ -58,22 +59,46 @@ qx.Class.define("osparc.ui.message.FlashMessageOEC", {
             alignX: "center",
             allowGrowX: false,
           });
-          control.addListener("tap", () => {
-            const currentStudy = osparc.store.Store.getInstance().getCurrentStudy();
-            const dataToClipboard = {
-              message: this.getMessage(),
-              supportId: this.getSupportId(),
-              timestamp: new Date().toString(),
-              url: window.location.href,
-              releaseTag: osparc.utils.Utils.getReleaseTag(),
-              studyId: currentStudy ? currentStudy.getUuid() : "",
-            }
-            osparc.utils.Utils.copyTextToClipboard(osparc.utils.Utils.prettifyJson(dataToClipboard));
+          control.addListener("tap", () => this.__copyToClipboard());
+          this.addWidget(control);
+          break;
+        case "contact-support":
+          control = new qx.ui.basic.Atom().set({
+            label: this.tr("Contact Support"),
+            icon: "@FontAwesome5Solid/comments/10",
+            iconPosition: "left",
+            gap: 8,
+            cursor: "pointer",
+            alignX: "center",
+            allowGrowX: false,
           });
+          control.addListener("tap", () => this.__openSupportChat());
           this.addWidget(control);
           break;
       }
       return control || this.base(arguments, id);
+    },
+
+    __getContext: function() {
+      const currentStudy = osparc.store.Store.getInstance().getCurrentStudy();
+      const dataToClipboard = {
+        message: this.getMessage(),
+        supportId: this.getSupportId(),
+        timestamp: new Date().toString(),
+        url: window.location.href,
+        releaseTag: osparc.utils.Utils.getReleaseTag(),
+        studyId: currentStudy ? currentStudy.getUuid() : "",
+      }
+      osparc.utils.Utils.prettifyJson(dataToClipboard);
+    },
+
+    __copyToClipboard: function() {
+      osparc.utils.Utils.copyTextToClipboard(this.__getContext());
+    },
+
+    __openSupportChat: function() {
+      console.log(this.__getContext());
+      osparc.support.SupportCenter.openWindow();
     },
   }
 });
