387ed15420d5b354432aafcd794fb52a768518b6
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-09-04 10:14:12 +0200

    fixes adapter

--- services/web/server/src/simcore_service_webserver/users/_controller/rest/users_rest.py
+++ services/web/server/src/simcore_service_webserver/users/_controller/rest/users_rest.py
@@ -70,11 +70,11 @@ async def get_my_profile(request: web.Request) -> web.Response:
 
     # Get profile address
     try:
-        user_invoice_address = await _users_service.get_user_invoice_address(
+        user_billing_details = await _users_service.get_user_billing_details(
             request.app, product_name=product.name, user_id=req_ctx.user_id
         )
         my_address = MyProfileAddressGet.model_validate(
-            user_invoice_address, from_attributes=True
+            user_billing_details, from_attributes=True
         )
     except BillingDetailsNotFoundError:
         my_address = None
