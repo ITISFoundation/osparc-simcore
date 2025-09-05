99d5daa5bbe0b9ff3c2e9becef3c3ede6a3f0379
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-09-05 11:22:40 +0200

    disable tracing to remove warnings

--- services/web/server/tests/unit/with_dbs/03/conftest.py
+++ services/web/server/tests/unit/with_dbs/03/conftest.py
@@ -6,15 +6,33 @@
 
 from collections.abc import AsyncIterator
 
-import aiopg.sa
 import pytest
+from pytest_simcore.helpers.monkeypatch_envs import setenvs_from_dict
+from pytest_simcore.helpers.typing_env import EnvVarsDict
 from simcore_postgres_database.models.user_preferences import user_preferences_frontend
+from sqlalchemy.ext.asyncio import AsyncEngine
 
 
 @pytest.fixture
 async def drop_all_preferences(
-    aiopg_engine: aiopg.sa.engine.Engine,
+    asyncpg_engine: AsyncEngine,
 ) -> AsyncIterator[None]:
     yield
-    async with aiopg_engine.acquire() as conn:
+    async with asyncpg_engine.connect() as conn:
         await conn.execute(user_preferences_frontend.delete())
+
+
+@pytest.fixture
+def app_environment(
+    monkeypatch: pytest.MonkeyPatch,
+    app_environment: EnvVarsDict,
+) -> EnvVarsDict:
+    return app_environment | setenvs_from_dict(
+        monkeypatch,
+        {
+            # disable tracing  for tests
+            "TRACING_OPENTELEMETRY_COLLECTOR_ENDPOINT": "null",
+            "TRACING_OPENTELEMETRY_COLLECTOR_PORT": "null",
+            "WEBSERVER_TRACING": "null",
+        },
+    )
