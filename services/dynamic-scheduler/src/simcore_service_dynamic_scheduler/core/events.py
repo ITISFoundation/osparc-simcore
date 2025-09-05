ed822b130d25f07dc4b982e515d6f8b4724d4933
Andrei Neagu <neagu@itis.swiss>
2025-09-05 15:45:42 +0200

    added events manger

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/core/events.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/core/events.py
@@ -23,6 +23,7 @@ from ..services.catalog import catalog_lifespan
 from ..services.deferred_manager import deferred_manager_lifespan
 from ..services.director_v0 import director_v0_lifespan
 from ..services.director_v2 import director_v2_lifespan
+from ..services.generic_scheduler import get_generic_scheduler_lifespans
 from ..services.notifier import get_notifier_lifespans
 from ..services.rabbitmq import rabbitmq_lifespan
 from ..services.redis import redis_lifespan
@@ -77,6 +78,9 @@ def create_app_lifespan(
     app_lifespan.add(redis_lifespan)
 
     for lifespan in get_notifier_lifespans():
+        app_lifespan.add(lifespan)
+
+    for lifespan in get_generic_scheduler_lifespans():
         app_lifespan.add(lifespan)
 
     app_lifespan.add(service_tracker_lifespan)
