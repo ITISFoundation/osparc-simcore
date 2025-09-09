e5a1d23f29f3a4dd1444a8cc8d5db682150bcfa3
odeimaiz <maiz@itis.swiss>
2025-09-09 11:40:31 +0200

    COLLAB_TYPE_ORDER

--- services/static-webserver/client/source/class/osparc/data/model/Group.js
+++ services/static-webserver/client/source/class/osparc/data/model/Group.js
@@ -97,13 +97,6 @@ qx.Class.define("osparc.data.model.Group", {
     getProperties: function() {
       return Object.keys(qx.util.PropertyUtil.getProperties(osparc.data.model.Group));
     },
-
-    COLLAB_TYPE_ORDER: [
-      osparc.store.Groups.COLLAB_TYPE.EVERYONE,
-      osparc.store.Groups.COLLAB_TYPE.SUPPORT,
-      osparc.store.Groups.COLLAB_TYPE.ORGANIZATION,
-      osparc.store.Groups.COLLAB_TYPE.USER
-    ],
   },
 
   members: {
