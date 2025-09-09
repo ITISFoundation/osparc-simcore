94d365e58b74bab98ce9f2aee4e129ab3c210d75
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-09-09 17:44:36 +0200

    check if start time is missing, output warning

--- services/director-v2/src/simcore_service_director_v2/modules/comp_scheduler/_scheduler_base.py
+++ services/director-v2/src/simcore_service_director_v2/modules/comp_scheduler/_scheduler_base.py
@@ -321,6 +321,19 @@ class BaseCompScheduler(ABC):
         def _need_heartbeat(task: CompTaskAtDB) -> bool:
             if task.state not in RUNNING_STATES:
                 return False
+            if task.start is None:
+                _logger.warning(
+                    "Task %s is in state %s but has no start time. This should not happen. Skipping heartbeat as this cannot be computed. (%s, %s, %s, %s, %s)",
+                    task.job_id,
+                    task.state,
+                    user_id,
+                    project_id,
+                    iteration,
+                    run_id,
+                    task.node_id,
+                )
+                return False
+
             if task.last_heartbeat is None:
                 assert task.start  # nosec
                 return bool(
