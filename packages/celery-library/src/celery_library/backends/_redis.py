0f3e00f2a17398350476173aec862f2d7acf9938
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-09-11 14:10:02 +0200

    fix: reduce count

--- packages/celery-library/src/celery_library/backends/_redis.py
+++ packages/celery-library/src/celery_library/backends/_redis.py
@@ -19,7 +19,7 @@ from ..utils import build_task_id_prefix
 _CELERY_TASK_INFO_PREFIX: Final[str] = "celery-task-info-"
 _CELERY_TASK_ID_KEY_ENCODING = "utf-8"
 _CELERY_TASK_ID_KEY_SEPARATOR: Final[str] = ":"
-_CELERY_TASK_SCAN_COUNT_PER_BATCH: Final[int] = 10000
+_CELERY_TASK_SCAN_COUNT_PER_BATCH: Final[int] = 1000
 _CELERY_TASK_METADATA_KEY: Final[str] = "metadata"
 _CELERY_TASK_PROGRESS_KEY: Final[str] = "progress"
 
