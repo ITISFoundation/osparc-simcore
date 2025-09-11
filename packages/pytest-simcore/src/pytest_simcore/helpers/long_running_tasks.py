7cd3a237a17a5d360897fdc12b00337bcb4ff344
Andrei Neagu <neagu@itis.swiss>
2025-09-11 11:16:28 +0200

    refactor

--- packages/pytest-simcore/src/pytest_simcore/helpers/long_running_tasks.py
+++ packages/pytest-simcore/src/pytest_simcore/helpers/long_running_tasks.py
@@ -10,8 +10,7 @@ from servicelib.long_running_tasks.models import TaskContext
 from servicelib.long_running_tasks.task import TaskId
 from tenacity import (
     AsyncRetrying,
-    TryAgain,
-    retry_if_exception_type,
+    retry_if_not_exception_type,
     stop_after_delay,
     wait_fixed,
 )
@@ -30,12 +29,11 @@ async def assert_task_is_no_longer_present(
         reraise=True,
         wait=wait_fixed(0.1),
         stop=stop_after_delay(60),
-        retry=retry_if_exception_type((AssertionError, TryAgain)),
+        retry=retry_if_not_exception_type(TaskNotFoundError),
     ):
         with attempt:  # noqa: SIM117
-            with pytest.raises(TaskNotFoundError):  # noqa: PT012
+            with pytest.raises(TaskNotFoundError):
                 # use internals to detirmine when it's no longer here
                 await manager._tasks_manager._get_tracked_task(  # noqa: SLF001
                     task_id, task_context
                 )
-                raise TryAgain
