ab48e26321f365e645df896398c1d006ff5f497c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-09-11 16:19:15 +0200

    default TTL

--- packages/service-library/src/servicelib/redis/_constants.py
+++ packages/service-library/src/servicelib/redis/_constants.py
@@ -6,7 +6,7 @@ from pydantic import NonNegativeInt
 DEFAULT_LOCK_TTL: Final[datetime.timedelta] = datetime.timedelta(seconds=10)
 DEFAULT_SOCKET_TIMEOUT: Final[datetime.timedelta] = datetime.timedelta(seconds=30)
 
-DEFAULT_SEMAPHORE_TTL: Final[datetime.timedelta] = datetime.timedelta(minutes=5)
+DEFAULT_SEMAPHORE_TTL: Final[datetime.timedelta] = datetime.timedelta(minutes=10)
 SEMAPHORE_KEY_PREFIX: Final[str] = "semaphores:"
 SEMAPHORE_HOLDER_KEY_PREFIX: Final[str] = "semaphore_holders:"
 
