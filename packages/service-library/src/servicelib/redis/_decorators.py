152d1f7710fe2cf03719ec6c25df5addeea1bf75
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-09-12 14:17:59 +0200

    fix

--- packages/service-library/src/servicelib/redis/_decorators.py
+++ packages/service-library/src/servicelib/redis/_decorators.py
@@ -119,6 +119,8 @@ def exclusive(
                     )
                     res = await work_task
                     # cancel the auto-extend task (work is done)
+                    # NOTE: if we do not explicitely await the task inside the context manager
+                    # it sometimes hangs forever (Python issue?)
                     await cancel_wait_task(auto_extend_lock_task, max_delay=None)
                 return res
 
