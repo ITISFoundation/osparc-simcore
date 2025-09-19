0f09f84b3a612595f372e79d8cbcb93f50f7c091
Mads Bisgaard <bisgaard@itis.swiss>
2025-09-19 12:48:29 +0200

    fix

--- packages/service-library/src/servicelib/celery/models.py
+++ packages/service-library/src/servicelib/celery/models.py
@@ -46,7 +46,6 @@ class OwnerMetadata(BaseModel):
 
     """
 
-    model_config = ConfigDict(extra="allow", arbitrary_types_allowed=True)
     owner: Annotated[str, StringConstraints(min_length=1, pattern=r"^[a-z_-]+$")]
 
     @model_validator(mode="after")
