379719494f2364996fc40298db0c2708d68f3555
Andrei Neagu <neagu@itis.swiss>
2025-09-19 10:12:01 +0200

    exposed more

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/generic_scheduler/__init__.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/generic_scheduler/__init__.py
@@ -1,5 +1,5 @@
 from ._core import cancel_operation, start_operation
-from ._deferred_runner import get_step_group_proxy
+from ._deferred_runner import get_step_group_proxy, get_step_store_proxy
 from ._lifespan import get_generic_scheduler_lifespans
 from ._models import (
     OperationName,
@@ -14,13 +14,14 @@ from ._operation import (
     ParallelStepGroup,
     SingleStepGroup,
 )
-from ._store import StepGroupProxy
+from ._store import StepGroupProxy, StepStoreProxy
 
 __all__: tuple[str, ...] = (
     "BaseStep",
     "cancel_operation",
     "get_generic_scheduler_lifespans",
     "get_step_group_proxy",
+    "get_step_store_proxy",
     "Operation",
     "OperationName",
     "OperationRegistry",
@@ -31,4 +32,5 @@ __all__: tuple[str, ...] = (
     "SingleStepGroup",
     "start_operation",
     "StepGroupProxy",
+    "StepStoreProxy",
 )
