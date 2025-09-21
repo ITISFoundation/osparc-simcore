758d97ffd2540aa295c43787360e7e90589aaf86
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-09-21 17:17:06 +0200

    reverted test

--- packages/service-library/tests/redis/test_semaphore_decorator.py
+++ packages/service-library/tests/redis/test_semaphore_decorator.py
@@ -366,11 +366,11 @@ async def test_with_large_capacity(
     redis_client_sdk: RedisClientSDK,
     semaphore_name: str,
 ):
-    large_capacity = 1
+    large_capacity = 20
     concurrent_count = 0
     max_concurrent = 0
-    sleep_time_s = 50
-    num_tasks = 10
+    sleep_time_s = 10
+    num_tasks = 500
 
     @with_limited_concurrency(
         redis_client_sdk,
