d05f44d29725f8197e926502dd694a0ec0313612
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-09-25 11:25:25 +0200

    cleanup

--- services/web/server/src/simcore_service_webserver/application_setup.py
+++ services/web/server/src/simcore_service_webserver/application_setup.py
@@ -1,17 +1,24 @@
 import functools
 
 import servicelib.aiohttp.application_setup
+from servicelib.aiohttp.application_setup import ModuleCategory, ensure_single_setup
 
 from .constants import APP_SETTINGS_KEY
 
-ensure_single_setup = servicelib.aiohttp.application_setup.ensure_single_setup
+# models
+assert ModuleCategory  # nosec
+
+
+# decorators
+assert callable(ensure_single_setup)  # nosec
+
 app_setup_func = functools.partial(
     servicelib.aiohttp.application_setup.app_module_setup,
     app_settings_key=APP_SETTINGS_KEY,
 )
 
-
 __all__: tuple[str, ...] = (
+    "ModuleCategory",
     "app_setup_func",
     "ensure_single_setup",
 )
