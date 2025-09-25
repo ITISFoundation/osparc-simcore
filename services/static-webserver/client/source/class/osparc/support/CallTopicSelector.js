d8ca05cd7a1001922388eb4de597e8172afa52e2
odeimaiz <maiz@itis.swiss>
2025-09-25 10:48:42 +0200

    CallTopicSelector

---
+++ services/static-webserver/client/source/class/osparc/support/CallTopicSelector.js
@@ -0,0 +1,139 @@
+/* ************************************************************************
+
+   osparc - the simcore frontend
+
+   https://osparc.io
+
+   Copyright:
+     2025 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+
+qx.Class.define("osparc.support.CallTopicSelector", {
+  extend: qx.ui.core.Widget,
+
+  construct: function() {
+    this.base(arguments);
+
+    this._setLayout(new qx.ui.layout.VBox(10));
+
+    this.__buildLayout();
+  },
+
+  members: {
+    _createChildControlImpl: function(id) {
+      let control;
+      switch (id) {
+        case "intro-label":
+          control = new qx.ui.basic.Label().set({
+            value: this.tr("I would like..."),
+            font: "text-14",
+          });
+          this._add(control);
+          break;
+        case "generic-intro-button":
+          control = new qx.ui.form.RadioButton().set({
+            label: this.tr("a generic introduction"),
+            value: false,
+          });
+          this._add(control);
+          break;
+        case "specific-topic-button":
+          control = new qx.ui.form.RadioButton().set({
+            label: this.tr("A specific topic"),
+            value: false,
+          });
+          this._add(control);
+          break;
+        case "specific-topic-select-box":
+          control = new qx.ui.form.SelectBox().set({
+            paddingLeft: 20,
+          });
+          this._add(control);
+          this.getChildControl("specific-topic-button").bind("value", control, "visibility", {
+            converter: val => val ? "visible" : "excluded"
+          });
+          break;
+        case "help-with-project-button":
+          control = new qx.ui.form.RadioButton().set({
+            label: this.tr("some help with my project"),
+            value: false,
+          });
+          this._add(control);
+          break;
+        case "share-project-checkbox":
+          control = new qx.ui.form.CheckBox().set({
+            value: true,
+            label: this.tr("share current project with support team (optional)"),
+            paddingLeft: 20,
+          });
+          this._add(control);
+          this.getChildControl("help-with-project-button").bind("value", control, "visibility", {
+            converter: val => val ? "visible" : "excluded"
+          });
+          break;
+        case "specific-topic-button":
+          control = new qx.ui.form.RadioButton().set({
+            label: this.tr("to discuss a specific topic"),
+            value: false,
+          });
+          this._add(control);
+          break;
+        case "specific-topic-textfield":
+          control = new qx.ui.form.TextField().set({
+            placeholder: this.tr("please provide any background information that could help us make this meeting more productive"),
+            paddingLeft: 20,
+          });
+          this._add(control);
+          this.getChildControl("specific-topic-button").bind("value", control, "visibility", {
+            converter: val => val ? "visible" : "excluded"
+          });
+          break;
+        case "next-button":
+          control = new qx.ui.form.Button().set({
+            value: this.tr("Next"),
+            alignX: "right",
+            marginTop: 10,
+            appearance: "strong-button",
+          });
+          this._add(control);
+          break;
+      }
+      return control || this.base(arguments, id);
+    },
+
+    __buildLayout: function() {
+      this.getChildControl("generic-intro-button");
+      this.getChildControl("specific-topic-button");
+      const selectBox = this.getChildControl("specific-topic-select-box");
+      const topics = [
+        this.tr("How to use osparc"),
+        this.tr("How to create and manage projects"),
+        this.tr("How to use the Workbench"),
+        this.tr("How to use the Data Manager"),
+        this.tr("How to use the App Store"),
+        this.tr("How to use the Dashboard"),
+        this.tr("How to use Teams"),
+        this.tr("Billing and Subscription"),
+        this.tr("Other"),
+      ];
+      topics.forEach(topic => {
+        const item = new qx.ui.form.ListItem(topic);
+        selectBox.add(item);
+      });
+      this.getChildControl("help-with-project-button");
+      this.getChildControl("share-project-checkbox");
+      this.getChildControl("specific-topic-button");
+      this.getChildControl("specific-topic-textfield");
+      this.getChildControl("intro-label");
+      this.getChildControl("next-button");
+    },
+  }
+});
