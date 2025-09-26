2254e8d0657ca38e4d07437e443e510dcb328cac
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-09-26 15:39:01 +0200

    fix: param worker side

--- services/storage/src/simcore_service_storage/api/_worker_tasks/tasks.py
+++ services/storage/src/simcore_service_storage/api/_worker_tasks/tasks.py
@@ -2,10 +2,16 @@ import logging
 
 from celery import Celery  # type: ignore[import-untyped]
 from celery_library.task import register_task
-from celery_library.types import register_celery_types
+from celery_library.types import register_celery_types, register_pydantic_types
 from models_library.api_schemas_storage.export_data_async_jobs import AccessRightError
+from models_library.api_schemas_storage.storage_schemas import (
+    FileUploadCompletionBody,
+    FoldersBody,
+    PresignedLink,
+)
 from servicelib.logging_utils import log_context
 
+from ...models import FileMetaData
 from ._files import complete_upload_file
 from ._paths import compute_path_size, delete_paths
 from ._simcore_s3 import (
@@ -20,7 +26,12 @@ _logger = logging.getLogger(__name__)
 
 def setup_worker_tasks(app: Celery) -> None:
     register_celery_types()
-
+    register_pydantic_types(
+        FileUploadCompletionBody,
+        FileMetaData,
+        FoldersBody,
+        PresignedLink,
+    )
     with log_context(_logger, logging.INFO, msg="worker tasks registration"):
         register_task(app, export_data, dont_autoretry_for=(AccessRightError,))
         register_task(
