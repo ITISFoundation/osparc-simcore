0c9695e984bc26629e1af5c885b5203ce7fb2196
Mads Bisgaard <bisgaard@itis.swiss>
2025-09-26 13:36:48 +0200

    task_id -> task_key @giancarloromeo

--- packages/celery-library/tests/unit/test_async_jobs.py
+++ packages/celery-library/tests/unit/test_async_jobs.py
@@ -121,15 +121,15 @@ async def _process_action(action: str, payload: Any) -> Any:
     return None
 
 
-def sync_job(task: Task, task_id: TaskKey, action: Action, payload: Any) -> Any:
+def sync_job(task: Task, task_key: TaskKey, action: Action, payload: Any) -> Any:
     _ = task
-    _ = task_id
+    _ = task_key
     return asyncio.run(_process_action(action, payload))
 
 
-async def async_job(task: Task, task_id: TaskKey, action: Action, payload: Any) -> Any:
+async def async_job(task: Task, task_key: TaskKey, action: Action, payload: Any) -> Any:
     _ = task
-    _ = task_id
+    _ = task_key
     return await _process_action(action, payload)
 
 
