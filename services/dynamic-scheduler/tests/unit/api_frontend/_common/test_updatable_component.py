8e9483165fe4c9868954a79eb3c9a36f7b056e4b
Andrei Neagu <neagu@itis.swiss>
2025-09-26 10:46:52 +0200

    refactor

--- services/dynamic-scheduler/tests/unit/api_frontend/_common/test_updatable_component.py
+++ services/dynamic-scheduler/tests/unit/api_frontend/_common/test_updatable_component.py
@@ -10,7 +10,6 @@ from fastapi import FastAPI
 from helpers import assert_contains_text
 from nicegui import APIRouter, ui
 from playwright.async_api import Page
-from pytest_mock import MockerFixture
 from simcore_service_dynamic_scheduler.api.frontend._common.base_display_model import (
     BaseUpdatableDisplayModel,
 )
@@ -88,17 +87,23 @@ class PersonComponent(BaseUpdatableComponent[Person]):
         ui.label(f"Age: {self.display_model.age}")
 
         @ui.refreshable
-        def _ui_friend_or_pret() -> None:
+        def _friend_or_pet_ui() -> None:
             if isinstance(self.display_model.companion, Friend):
                 FriendComponent(self.display_model.companion).add_to_ui()
 
             elif isinstance(self.display_model.companion, Pet):
                 PetComponent(self.display_model.companion).add_to_ui()
 
-        _ui_friend_or_pret()
+        _friend_or_pet_ui()
 
         # self.display_model.on_value_change("companion", comp_ui.refresh)
-        self.display_model.on_type_change("companion", _ui_friend_or_pret.refresh)
+        self.display_model.on_type_change("companion", _friend_or_pet_ui.refresh)
+
+
+def _index_page_ui(person: Person) -> None:
+    ui.label("BEFORE_LABEL")
+    PersonComponent(person).add_to_ui()
+    ui.label("AFTER_LABEL")
 
 
 @pytest.fixture
@@ -107,20 +112,12 @@ def use_internal_scheduler() -> bool:
 
 
 @pytest.fixture
-def person() -> Person:
-    return Person(name="Alice", age=30, companion=Pet(name="Fluffy", species="cat"))
-
-
-@pytest.fixture
 def router(person: Person) -> APIRouter:
-
     router = APIRouter()
 
     @ui.page("/", api_router=router)
     async def index():
-        ui.label("BEFORE_LABEL")
-        PersonComponent(person).add_to_ui()
-        ui.label("AFTER_LABEL")
+        _index_page_ui(person)
 
     return router
 
@@ -143,33 +140,67 @@ def not_initialized_app(not_initialized_app: FastAPI, router: APIRouter) -> Fast
     return minimal_app
 
 
+async def _ensure_before_label(async_page: Page) -> None:
+    await assert_contains_text(async_page, "BEFORE_LABEL")
+
+
+async def _ensure_after_label(async_page: Page) -> None:
+    await assert_contains_text(async_page, "AFTER_LABEL")
+
+
+async def _ensure_person_name(async_page: Page, name: str) -> None:
+    await assert_contains_text(async_page, f"Name: {name}")
+
+
+async def _ensure_person_age(async_page: Page, age: int) -> None:
+    await assert_contains_text(async_page, f"Age: {age}")
+
+
+async def _esnure_person_companion(async_page: Page, companion: Pet | Friend) -> None:
+    if isinstance(companion, Pet):
+        await assert_contains_text(async_page, f"Pet Name: {companion.name}")
+        await assert_contains_text(async_page, f"Pet Species: {companion.species}")
+    elif isinstance(companion, Friend):
+        await assert_contains_text(async_page, f"Friend Name: {companion.name}")
+        await assert_contains_text(async_page, f"Friend Age: {companion.age}")
+
+
+@pytest.mark.parametrize(
+    "person, expected",
+    [
+        pytest.param(
+            Person(name="Alice", age=30, companion=Pet(name="Fluffy", species="cat")),
+            True,
+            id="initial-test",
+        )
+    ],
+)
 async def test_updatable_component(
-    mocker: MockerFixture,
     app_runner: None,
     async_page: Page,
     server_host_port: str,
     person: Person,
-    # person_display: PersonDisplay,
+    expected: bool,
 ):
     await async_page.goto(
         f"{server_host_port}{get_settings().DYNAMIC_SCHEDULER_UI_MOUNT_PATH}"
     )
 
-    # ensre render worked
-    await assert_contains_text(async_page, "BEFORE_LABEL")
-    await assert_contains_text(async_page, "AFTER_LABEL")
+    # INITIAL RENDER
+    await _ensure_before_label(async_page)
+    await _ensure_after_label(async_page)
 
-    await assert_contains_text(async_page, "Name: Alice")
-    await assert_contains_text(async_page, "Age: 30")
+    await _ensure_person_name(async_page, person.name)
+    await _ensure_person_age(async_page, person.age)
+    await _esnure_person_companion(async_page, person.companion)
 
-    # _draw_spy = mocker.spy(person_display, "_draw")
-    # _recreate_ui_spy = mocker.spy(person_display, "_recrate_ui")
+    # AFTER CHNGE RENDER
 
     # # TODO: test that it changes when accessing the propeties directly
 
     await assert_contains_text(async_page, "Pet Name: Fluffy")
     person.companion.name = "Buddy"
-    await assert_contains_text(async_page, "Pet Name: Buddy", timeout=2)
+    await assert_contains_text(async_page, "Pet Name: Buddy")
 
     # # TODO: check that UI was rerendered with new pet name
 
@@ -203,3 +234,6 @@ async def test_updatable_component(
     # # TODO: check that ui has changed as expected
     # assert person.requires_rerender({"companion": {"age": 28}}) is False
     # person_display.update_model({"companion": {"name": "Eve", "age": 28}})
+
+
+# TODO: make tests go faster since we are running differently
