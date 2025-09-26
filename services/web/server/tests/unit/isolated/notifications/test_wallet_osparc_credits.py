2db45bf260bcece0f66765ba1517beefcfb68972
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-09-26 10:12:27 +0200

    wallet subscriptions

--- services/web/server/tests/unit/isolated/notifications/test_wallet_osparc_credits.py
+++ services/web/server/tests/unit/isolated/notifications/test_wallet_osparc_credits.py
@@ -2,18 +2,23 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-import
 import asyncio
+from collections import defaultdict
 from unittest.mock import AsyncMock, patch
 
 import pytest
 from models_library.wallets import WalletID
 from simcore_service_webserver.notifications import wallet_osparc_credits
+from simcore_service_webserver.notifications._rabbitmq_exclusive_queue_consumers import (
+    WALLET_SUBSCRIPTION_LOCK_APPKEY,
+    WALLET_SUBSCRIPTIONS_APPKEY,
+)
 
 
 @pytest.fixture
 def app_with_wallets():
     app = {
-        "wallet_subscription_lock": asyncio.Lock(),
-        "wallet_subscriptions": {},
+        WALLET_SUBSCRIPTION_LOCK_APPKEY: asyncio.Lock(),
+        WALLET_SUBSCRIPTIONS_APPKEY: defaultdict,
     }
     return app
 
@@ -25,7 +30,7 @@ def wallet_id():
 
 async def test_subscribe_first_and_second(app_with_wallets, wallet_id):
     app = app_with_wallets
-    app["wallet_subscriptions"][wallet_id] = 0
+    app[WALLET_SUBSCRIPTIONS_APPKEY][wallet_id] = 0
     mock_rabbit = AsyncMock()
     with patch(
         "simcore_service_webserver.notifications.wallet_osparc_credits.get_rabbitmq_client",
@@ -36,12 +41,12 @@ async def test_subscribe_first_and_second(app_with_wallets, wallet_id):
         # Second subscribe should not call add_topics again
         await wallet_osparc_credits.subscribe(app, wallet_id)
         assert mock_rabbit.add_topics.await_count == 1
-        assert app["wallet_subscriptions"][wallet_id] == 2
+        assert app[WALLET_SUBSCRIPTIONS_APPKEY][wallet_id] == 2
 
 
 async def test_unsubscribe_last_and_not_last(app_with_wallets, wallet_id):
     app = app_with_wallets
-    app["wallet_subscriptions"][wallet_id] = 2
+    app[WALLET_SUBSCRIPTIONS_APPKEY][wallet_id] = 2
     mock_rabbit = AsyncMock()
     with patch(
         "simcore_service_webserver.notifications.wallet_osparc_credits.get_rabbitmq_client",
@@ -50,11 +55,11 @@ async def test_unsubscribe_last_and_not_last(app_with_wallets, wallet_id):
         # Not last unsubscribe
         await wallet_osparc_credits.unsubscribe(app, wallet_id)
         mock_rabbit.remove_topics.assert_not_awaited()
-        assert app["wallet_subscriptions"][wallet_id] == 1
+        assert app[WALLET_SUBSCRIPTIONS_APPKEY][wallet_id] == 1
         # Last unsubscribe
         await wallet_osparc_credits.unsubscribe(app, wallet_id)
         mock_rabbit.remove_topics.assert_awaited_once()
-        assert app["wallet_subscriptions"][wallet_id] == 0
+        assert app[WALLET_SUBSCRIPTIONS_APPKEY][wallet_id] == 0
 
 
 async def test_unsubscribe_when_not_subscribed(app_with_wallets, wallet_id):
@@ -67,4 +72,4 @@ async def test_unsubscribe_when_not_subscribed(app_with_wallets, wallet_id):
     ):
         await wallet_osparc_credits.unsubscribe(app, wallet_id)
         mock_rabbit.remove_topics.assert_not_awaited()
-        assert app["wallet_subscriptions"].get(wallet_id, 0) == 0
+        assert app[WALLET_SUBSCRIPTIONS_APPKEY].get(wallet_id, 0) == 0
