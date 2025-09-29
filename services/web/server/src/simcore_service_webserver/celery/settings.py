e552c15095d22ca3e41d8f6f2da747cd7150a897
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-09-29 15:04:22 +0200

    fix: settings

--- services/web/server/src/simcore_service_webserver/celery/settings.py
+++ services/web/server/src/simcore_service_webserver/celery/settings.py
@@ -1,11 +1,11 @@
 from aiohttp import web
 from settings_library.celery import CelerySettings
 
-from ..constants import APP_SETTINGS_KEY
+from ..application_keys import APP_SETTINGS_APPKEY
 
 
 def get_plugin_settings(app: web.Application) -> CelerySettings:
-    settings: CelerySettings | None = app[APP_SETTINGS_KEY].WEBSERVER_CELERY
+    settings = app[APP_SETTINGS_APPKEY].WEBSERVER_CELERY
     assert settings, "setup_settings not called?"  # nosec
     assert isinstance(settings, CelerySettings)  # nosec
     return settings
