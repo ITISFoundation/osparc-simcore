41f68a850ca1dbcdc111146148686edd965e3a8f
Mads Bisgaard <bisgaard@itis.swiss>
2025-10-07 12:49:26 +0200

    fix various test fixtures

--- services/director-v2/tests/unit/test_modules_dynamic_sidecar_client_api_thin.py
+++ services/director-v2/tests/unit/test_modules_dynamic_sidecar_client_api_thin.py
@@ -16,6 +16,8 @@ from pytest_simcore.helpers.typing_env import EnvVarsDict
 from respx import MockRouter, Route
 from respx.types import SideEffectTypes
 from servicelib.docker_constants import SUFFIX_EGRESS_PROXY_NAME
+from servicelib.tracing import TracingData
+from simcore_service_director_v2._meta import APP_NAME
 from simcore_service_director_v2.core.settings import AppSettings
 from simcore_service_director_v2.modules.dynamic_sidecar.api_client._thin import (
     ThinSidecarsClient,
@@ -52,6 +54,11 @@ def mocked_app(
 
     app = FastAPI()
     app.state.settings = AppSettings.create_from_envs()
+    tracing_data = TracingData.create(
+        service_name=APP_NAME,
+        tracing_settings=app.state.settings.DIRECTOR_V2_TRACING,
+    )
+    app.state.tracing_data = tracing_data
     return app
 
 
