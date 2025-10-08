eabc46ef194829a682d850e6732da6558eb7a7a0
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-10-08 10:59:56 +0200

    refactor

--- services/web/server/tests/unit/with_dbs/01/conftest.py
+++ services/web/server/tests/unit/with_dbs/01/conftest.py
@@ -2,13 +2,14 @@
 # pylint: disable=unused-argument
 # pylint: disable=unused-variable
 
-from collections.abc import Callable
 from functools import partial
 from typing import Any
 
 import pytest
 from pytest_mock import MockerFixture, MockType
 from pytest_simcore.helpers.typing_mock import HandlerMockFactory
+from simcore_service_webserver.storage import _rest
+from simcore_service_webserver.tasks import _tasks_service
 
 
 @pytest.fixture
@@ -20,30 +21,27 @@ def app_environment(
     return app_environment | {"WEBSERVER_GARBAGE_COLLECTOR": "null"}
 
 
-def _result_or_exception_side_effect(result_or_exception, *args, **kwargs):
+def _result_or_exception_side_effect(result_or_exception: Any, *args, **kwargs):
     if isinstance(result_or_exception, Exception):
         raise result_or_exception
 
     return result_or_exception
 
 
-@pytest.fixture()
-def mock_handler_in_storage_rest(
-    mocker: MockerFixture,
+def _create_handler_mock_factory(
+    mocker: MockerFixture, module: Any
 ) -> HandlerMockFactory:
     def _create(
         handler_name: str,
         return_value: Any = None,
         exception: Exception | None = None,
-        side_effect: Callable | None = None,
+        side_effect: Any | None = None,
     ) -> MockType:
-
-        from simcore_service_webserver.storage import _rest
 
         assert exception is None or side_effect is None
 
         return mocker.patch.object(
-            _rest,
+            module,
             handler_name,
             return_value=return_value,
             side_effect=(
@@ -54,34 +52,17 @@ def mock_handler_in_storage_rest(
         )
 
     return _create
+
+
+@pytest.fixture()
+def mock_handler_in_storage_rest(
+    mocker: MockerFixture,
+) -> HandlerMockFactory:
+    return _create_handler_mock_factory(mocker, _rest)
 
 
 @pytest.fixture()
 def mock_handler_in_task_service(
     mocker: MockerFixture,
 ) -> HandlerMockFactory:
-    def _create(
-        handler_name: str,
-        return_value: Any = None,
-        exception: Exception | None = None,
-        side_effect: Callable | None = None,
-    ) -> MockType:
-
-        from simcore_service_webserver.tasks import (
-            _tasks_service,
-        )
-
-        assert exception is None or side_effect is None
-
-        return mocker.patch.object(
-            _tasks_service,
-            handler_name,
-            return_value=return_value,
-            side_effect=(
-                partial(_result_or_exception_side_effect, side_effect)
-                if side_effect
-                else None
-            ),
-        )
-
-    return _create
+    return _create_handler_mock_factory(mocker, _tasks_service)
