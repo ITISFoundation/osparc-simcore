eb5501269a8d6bac4d73f61181fa86bcc05e80e4
Andrei Neagu <neagu@itis.swiss>
2025-10-13 11:59:18 +0200

    added manager and opration names

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/scheduler/_lifespan.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/scheduler/_lifespan.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from fastapi_lifespan_manager import State
 
 from ...core.settings import ApplicationSettings
+from ._operations import registry
 from ._redis import RedisStore
 
 
@@ -13,8 +14,10 @@ async def scheduler_lifespan(app: FastAPI) -> AsyncIterator[State]:
     store = RedisStore(settings.DYNAMIC_SCHEDULER_REDIS)
     store.set_to_app_state(app)
 
+    registry.register_operataions()
     await store.setup()
 
     yield {}
 
     await store.shutdown()
+    registry.unregister_operations()
