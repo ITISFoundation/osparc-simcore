54c8461cd999c642b31d14d4d25bcf7d8700219b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-10-14 13:19:33 +0200

    ensure registry login is done

--- services/autoscaling/src/simcore_service_autoscaling/utils/cluster_scaling.py
+++ services/autoscaling/src/simcore_service_autoscaling/utils/cluster_scaling.py
@@ -64,20 +64,17 @@ async def ec2_startup_script(
         )
     )
     assert app_settings.AUTOSCALING_EC2_INSTANCES  # nosec
-    if app_settings.AUTOSCALING_REGISTRY:  # noqa: SIM102
+    if app_settings.AUTOSCALING_REGISTRY:
+        startup_commands.append(
+            utils_docker.get_docker_login_on_start_bash_command(
+                app_settings.AUTOSCALING_REGISTRY
+            )
+        )
+
         if pull_image_cmd := utils_docker.get_docker_pull_images_on_start_bash_command(
             app_settings.AUTOSCALING_EC2_INSTANCES.EC2_INSTANCES_COLD_START_DOCKER_IMAGES_PRE_PULLING
         ):
-            startup_commands.append(
-                " && ".join(
-                    [
-                        utils_docker.get_docker_login_on_start_bash_command(
-                            app_settings.AUTOSCALING_REGISTRY
-                        ),
-                        pull_image_cmd,
-                    ]
-                )
-            )
+            startup_commands.append(pull_image_cmd)
 
     return " && ".join(startup_commands)
 
