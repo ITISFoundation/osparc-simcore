a629a985a2a1d8b7e5a30b82ae0934aa8b476fb0
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-10-14 16:41:16 +0200

    fix: update import statements for APP_NAME to use absolute paths

--- services/agent/src/simcore_service_agent/main.py
+++ services/agent/src/simcore_service_agent/main.py
@@ -5,10 +5,9 @@ from common_library.json_serialization import json_dumps
 from fastapi import FastAPI
 from servicelib.fastapi.logging_lifespan import create_logging_shutdown_event
 from servicelib.tracing import TracingConfig
+from simcore_service_agent._meta import APP_NAME
 from simcore_service_agent.core.application import create_app
 from simcore_service_agent.core.settings import ApplicationSettings
-
-from ._meta import APP_NAME
 
 _logger = logging.getLogger(__name__)
 
