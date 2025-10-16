a7ec4de104c825e8ca3549b1b8847e538547e6bf
odeimaiz <maiz@itis.swiss>
2025-10-16 12:41:22 +0200

    SafeLabel

---
+++ services/static-webserver/client/source/class/osparc/ui/basic/SafeLabel.js
@@ -0,0 +1,46 @@
+/* ************************************************************************
+
+   osparc - the simcore frontend
+
+   https://osparc.io
+
+   Copyright:
+     2025 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+/**
+ * A Label that sanitizes its value when in rich mode to avoid XSS attacks
+ */
+
+qx.Class.define("osparc.ui.basic.SafeLabel", {
+  extend: qx.ui.basic.Label,
+
+  construct() {
+    this.base(arguments);
+
+    this.set({
+      rich: true,
+    });
+
+    this.addListener("changeValue", this._onChangeValue, this);
+  },
+
+  members: {
+    _onChangeValue(e) {
+      const val = e.getData();
+      if (this.getRich() && typeof val === "string") {
+        const sanitized = osparc.wrapper.DOMPurify.sanitize(val);
+        if (sanitized !== val) {
+          this.setValue(sanitized);
+        }
+      }
+    }
+  }
+});
