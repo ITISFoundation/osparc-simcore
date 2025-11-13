203c937c98eaea190c5e6fd6d4690071320fa118
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-11-13 08:38:30 +0100

    fix search result for dirs

--- packages/models-library/src/models_library/api_schemas_storage/search_async_jobs.py
+++ packages/models-library/src/models_library/api_schemas_storage/search_async_jobs.py
@@ -1,18 +1,20 @@
 import datetime
-from typing import Final, Literal
+from typing import Final
 
 from models_library.projects import ProjectID
 from pydantic import BaseModel, ByteSize, ConfigDict
 from pydantic.alias_generators import to_camel
+
+from .storage_schemas import UNDEFINED_SIZE_TYPE
 
 SEARCH_TASK_NAME: Final[str] = "files_search"
 
 
 class SearchResultItem(BaseModel):
     name: str
-    created_at: datetime.datetime
-    modified_at: datetime.datetime
-    size: ByteSize | Literal[-1]
+    created_at: datetime.datetime | None
+    modified_at: datetime.datetime | None
+    size: ByteSize | UNDEFINED_SIZE_TYPE
     path: str
     is_directory: bool
     project_id: ProjectID | None
