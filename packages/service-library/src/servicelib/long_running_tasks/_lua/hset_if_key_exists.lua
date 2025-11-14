16e31a7c35fd432732f7fd3b59ace22b6334ab00
Andrei Neagu <neagu@itis.swiss>
2025-11-14 11:43:48 +0100

    added docs

--- packages/service-library/src/servicelib/long_running_tasks/_lua/hset_if_key_exists.lua
+++ packages/service-library/src/servicelib/long_running_tasks/_lua/hset_if_key_exists.lua
@@ -1,4 +1,13 @@
--- Check if the key (hash) given in KEYS[1] exists
+-- Check if the has_key exists before setting the arguments exists
+-- KEYS[1]: has_key (HASH where all the )
+--
+-- ARGV[1]: a unique key
+-- ARGV[2]: value of key 1
+-- ARGV[n]: another unique key
+-- ARGV[n+1]: value of the key n+1
+--
+-- Returns: the number of affected keys
+
 if redis.call('exists', KEYS[1]) == 1 then
     -- If it exists, set multiple field-value pairs from ARGV using HSET and unpack
     -- ARGV should be a flat list: field1, value1, field2, value2, ...
