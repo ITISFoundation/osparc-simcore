51cec9d56ee007c022ca392eeaa50fdb35c0cfd0
Andrei Neagu <neagu@itis.swiss>
2025-11-28 09:09:41 +0100

    refactor

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/scheduler/_operations/legacy/monitor.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/scheduler/_operations/legacy/monitor.py
@@ -1,16 +1,8 @@
-from datetime import timedelta
-from typing import Final
-
 from ....generic_scheduler import BaseStepGroup, Operation, SingleStepGroup
 from .._common_steps import DoNothing, SetCurrentScheduleId
 
-_WAIT_BEFORE_REPEAT: Final[timedelta] = timedelta(seconds=5)
-
-
 _steps: list[BaseStepGroup] = [
-    SingleStepGroup(
-        DoNothing, repeat_steps=True, wait_before_repeat=_WAIT_BEFORE_REPEAT
-    ),
+    SingleStepGroup(DoNothing, repeat_steps=True),
 ]
 
 
