4032c4ce1b00a3683a24e7bac36d9d97b72f7b7a
Mads Bisgaard <bisgaard@itis.swiss>
2025-12-05 10:12:38 +0100

    move unused exception

--- services/web/server/src/simcore_service_webserver/chatbot/exceptions.py
+++ services/web/server/src/simcore_service_webserver/chatbot/exceptions.py
@@ -5,10 +5,6 @@ class BaseChatbotException(OsparcErrorMixin, Exception):
     """Base exception for chatbot errors"""
 
 
-class InvalidUserInConversationError(BaseChatbotException):
-    msg_template = "Encountered unexpected user {primary_group_id} in conversation {conversation_id}"
-
-
 class NoResponseFromChatbotError(BaseChatbotException):
     msg_template = (
         "No response received from chatbot for chat completion {chat_completion_id}"
