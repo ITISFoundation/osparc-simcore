6776fb3b4c85308238dcd1f1c0ff76a35b370b1c
Giancarlo Romeo <giancarloromeo@gmail.com>
2025-12-08 15:38:24 +0100

    migrate existing data

--- packages/postgres-database/src/simcore_postgres_database/migration/versions/ce69cc44246a_add_product_name_to_projects.py
+++ packages/postgres-database/src/simcore_postgres_database/migration/versions/ce69cc44246a_add_product_name_to_projects.py
@@ -17,9 +17,21 @@ depends_on = None
 
 
 def upgrade():
-    op.add_column("projects", sa.Column("product_name", sa.String(), nullable=False))
+    op.add_column("projects", sa.Column("product_name", sa.String(), nullable=True))
+
+    op.execute(
+        """
+        UPDATE projects
+        SET product_name = ptp.product_name
+        FROM projects_to_products ptp
+        WHERE projects.uuid = ptp.project_uuid
+        """
+    )
+
+    op.alter_column("projects", "product_name", nullable=False)
+
     op.create_foreign_key(
-        "fk_projects_to_products_name",
+        "fk_projects_to_product_name",
         "projects",
         "products",
         ["product_name"],
@@ -30,5 +42,16 @@ def upgrade():
 
 
 def downgrade():
-    op.drop_constraint("fk_projects_to_products_name", "projects", type_="foreignkey")
+    op.execute(
+        """
+        INSERT INTO projects_to_products (project_uuid, product_name, created, modified)
+        SELECT uuid, product_name, NOW(), NOW()
+        FROM projects
+        WHERE product_name IS NOT NULL
+        ON CONFLICT (project_uuid, product_name) DO NOTHING
+        """
+    )
+
+    op.drop_constraint("fk_projects_to_product_name", "projects", type_="foreignkey")
+
     op.drop_column("projects", "product_name")
