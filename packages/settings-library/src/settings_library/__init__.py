352006d6e3c5ac1616b417e86377feffb289e30e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-12-11 08:48:57 +0100

    revert

--- packages/settings-library/src/settings_library/__init__.py
+++ packages/settings-library/src/settings_library/__init__.py
@@ -1,7 +1,3 @@
 from importlib.metadata import version
 
-from ._constants import CUSTOM_PLACEMENT_LABEL_KEYS
-
 __version__: str = version("simcore-settings-library")
-
-__all__ = ["CUSTOM_PLACEMENT_LABEL_KEYS"]
