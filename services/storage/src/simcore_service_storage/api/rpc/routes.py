901d6bc97d770ab037e4e327433070d0d1eb635a
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-01-09 09:45:39 +0100

    fix typecheck

--- services/storage/src/simcore_service_storage/api/rpc/routes.py
+++ services/storage/src/simcore_service_storage/api/rpc/routes.py
@@ -8,14 +8,13 @@ from servicelib.rabbitmq import RPCRouter
 from simcore_service_storage.modules.celery import get_task_manager_from_app
 
 from ...modules.rabbitmq import get_rabbitmq_rpc_server
-from . import _paths, _simcore_s3
+from . import _simcore_s3
 
 _logger = logging.getLogger(__name__)
 
 
 ROUTERS: list[RPCRouter] = [
     _async_jobs.router,
-    _paths.router,
     _simcore_s3.router,
 ]
 
@@ -30,8 +29,6 @@ def setup_rpc_routes(app: FastAPI) -> None:
             rpc_server = get_rabbitmq_rpc_server(app)
             task_manager = get_task_manager_from_app(app)
             for router in ROUTERS:
-                await rpc_server.register_router(
-                    router, STORAGE_RPC_NAMESPACE, task_manager=task_manager
-                )
+                await rpc_server.register_router(router, STORAGE_RPC_NAMESPACE, task_manager=task_manager)
 
     app.add_event_handler("startup", startup)
