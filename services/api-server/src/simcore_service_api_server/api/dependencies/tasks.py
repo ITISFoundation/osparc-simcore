acb980828d9326a4e8a4faa36c09d1eb978c5255
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-01-13 15:59:03 +0100

    fix

--- services/api-server/src/simcore_service_api_server/api/dependencies/tasks.py
+++ services/api-server/src/simcore_service_api_server/api/dependencies/tasks.py
@@ -1,13 +1,13 @@
 from typing import Annotated
 
 from fastapi import Depends
-from servicelib.rabbitmq._client_rpc import RabbitMQRPCClient
+from servicelib.celery.task_manager import TaskManager
 
 from ...services_rpc.async_jobs import AsyncJobClient
 from .celery import get_task_manager
 
 
 def get_async_jobs_client(
-    task_manager: Annotated[RabbitMQRPCClient, Depends(get_task_manager)],
+    task_manager: Annotated[TaskManager, Depends(get_task_manager)],
 ) -> AsyncJobClient:
-    return AsyncJobClient(_rabbitmq_rpc_client=task_manager)
+    return AsyncJobClient(_task_manager=task_manager)
