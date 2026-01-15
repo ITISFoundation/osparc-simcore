74319c06dafbf300effbfc2327b3c062ce2f4df3
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-01-15 15:08:22 +0100

    fix test

--- services/storage/tests/unit/test_simcore_s3_dsm.py
+++ services/storage/tests/unit/test_simcore_s3_dsm.py
@@ -505,6 +505,10 @@ async def test_create_s3_export_abort_upload_upon_error(
     indirect=True,
 )
 async def test_search_directories(
+    user_id: UserID,
+    product_name: ProductName,
+    project_id: ProjectID,
+    node_id: NodeID,
     simcore_s3_dsm: SimcoreS3DataManager,
     create_directory_with_files: Callable[
         [str, ByteSize, int, int, ProjectID, NodeID],
@@ -512,10 +516,6 @@ async def test_search_directories(
     ],
     upload_file: Callable[..., Awaitable[tuple[Path, SimcoreS3FileID]]],
     file_size: ByteSize,
-    user_id: UserID,
-    product_name: ProductName,
-    project_id: ProjectID,
-    node_id: NodeID,
     faker: Faker,
 ):
     """Test that search functionality can find directories."""
