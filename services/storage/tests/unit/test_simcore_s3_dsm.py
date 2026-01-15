4646317c45c6ae6a5aa7915ccfde9553512125e8
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-01-15 15:24:54 +0100

    fix test

--- services/storage/tests/unit/test_simcore_s3_dsm.py
+++ services/storage/tests/unit/test_simcore_s3_dsm.py
@@ -72,6 +72,9 @@ async def cleanup_files_closure(
     indirect=True,
 )
 async def test__copy_path_s3_s3(
+    user_id: UserID,
+    project_id: ProjectID,
+    node_id: NodeID,
     simcore_s3_dsm: SimcoreS3DataManager,
     create_directory_with_files: Callable[
         [str, ByteSize, int, int, ProjectID, NodeID],
@@ -79,9 +82,6 @@ async def test__copy_path_s3_s3(
     ],
     upload_file: Callable[[ByteSize, str], Awaitable[tuple[Path, SimcoreS3FileID]]],
     file_size: ByteSize,
-    user_id: UserID,
-    project_id: ProjectID,
-    node_id: NodeID,
     mock_copy_transfer_cb: Callable[..., None],
     sqlalchemy_async_engine: AsyncEngine,
     cleanup_files_closure: Callable[[SimcoreS3FileID], None],
