fd9573f833ddf94e681de52e05ce02a256e81c12
odeimaiz <maiz@itis.swiss>
2026-01-16 17:31:13 +0100

    preview

---
+++ services/static-webserver/client/source/class/osparc/editor/EmailEditor.js
@@ -0,0 +1,142 @@
+/* ************************************************************************
+
+   osparc - the simcore frontend
+
+   https://osparc.io
+
+   Copyright:
+     2026 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+qx.Class.define("osparc.editor.EmailEditor", {
+  extend: qx.ui.tabview.TabView,
+
+  construct: function() {
+    this.base(arguments);
+
+    this.set({
+      contentPadding: 0,
+      barPosition: "top"
+    });
+
+    this.getChildControl("text-editor");
+    this.getChildControl("preview-email");
+
+    this.addListener("changeSelection", () => {
+      const selectedPage = this.getSelection()[0];
+      if (selectedPage.getUserData("id") === "preview-email") {
+        this.__renderPreview();
+      }
+    }, this);
+
+    osparc.store.Faker.getInstance().fetchEmailTemplate("free-email")
+      .then((templateEmail) => {
+        this.setTemplateEmail(templateEmail);
+      });
+  },
+
+  events: {
+    "textChanged": "qx.event.type.Data",
+    "cancel": "qx.event.type.Event"
+  },
+
+  properties: {
+    templateEmail: {
+      check: "String",
+      init: "",
+      nullable: true,
+    },
+  },
+
+  members: {
+    _createChildControlImpl: function(id) {
+      let control;
+      switch (id) {
+        case "editor-page":
+          control = new qx.ui.tabview.Page(this.tr("Editor")).set({
+            userData: {
+              id: "text-editor"
+            }
+          });
+          this.add(control);
+          break;
+        case "preview-page":
+          control = new qx.ui.tabview.Page(this.tr("Preview")).set({
+            userData: {
+              id: "preview-email"
+            }
+          });
+          this.add(control);
+          break;
+        case "text-editor":
+          control = new osparc.editor.TextEditor().set({
+            placeholder: "Write your email..."
+          });
+          this.getChildControl("editor-page").add(control);
+          break;
+        case "preview-email":
+          // using qx.ui.embed.Iframe instead of qx.ui.embed.Html because:
+          // - CSS isolation
+          // - The template is a full HTML document
+          // - Security: avoids script execution
+          // - Much closer to real email rendering
+          control = new qx.ui.embed.Iframe();
+          this.getChildControl("preview-page").add(control);
+          break;
+      }
+
+      return control || this.base(arguments, id);
+    },
+
+    __renderPreview: function() {
+      const previewEmail = this.getChildControl("preview-email");
+      previewEmail.setHtml("<b>Rendering preview...</b>");
+
+
+      const textEditor = this.getChildControl("text-editor");
+      const emailContent = textEditor.getText();
+      const templateEmail = this.getTemplateEmail();
+
+      const previewHtml = this.__buildPreviewHtml(templateEmail, emailContent);
+      previewEmail.setHtml(previewHtml);
+    },
+
+    __escapeHtml: function(str) {
+      return (str || "")
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;")
+        .replaceAll("'", "&#039;");
+    },
+
+    __draftTextToHtml(text) {
+      // simple + safe: plain text -> <br>
+      const safe = this.__escapeHtml((text || "").trim());
+      return safe.replaceAll("\n", "<br/>");
+    },
+
+    __buildPreviewHtml(templateHtml, contentHtml) {
+      if (!templateHtml) return "";
+
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(templateHtml, "text/html");
+
+      // Prefer a stable marker if you can add it later:
+      // const mount = doc.querySelector("[data-email-content]") || doc.querySelector(".content");
+      const mount = doc.querySelector(".content");
+      if (mount) {
+        mount.innerHTML = contentHtml;
+      }
+
+      return "<!DOCTYPE html>\n" + doc.documentElement.outerHTML;
+    },
+  }
+});
--- services/static-webserver/client/source/class/osparc/po/SendEmail.js
+++ services/static-webserver/client/source/class/osparc/po/SendEmail.js
@@ -22,105 +22,16 @@ qx.Class.define("osparc.po.SendEmail", {
     _createChildControlImpl: function(id) {
       let control;
       switch (id) {
-        case "search-preregistration":
-          control = this.__searchPreRegistration();
+        case "email-editor":
+          control = new osparc.editor.EmailEditor();
           this._add(control);
-          break;
-        case "finding-status":
-          control = new qx.ui.basic.Label().set({
-            rich: true
-          });
-          this._add(control);
-          break;
-        case "pre-registration-container":
-          control = new qx.ui.container.Scroll();
-          this._add(control, {
-            flex: 1
-          });
           break;
       }
       return control || this.base(arguments, id);
     },
 
     _buildLayout: function() {
-      this.getChildControl("search-preregistration");
-      this.getChildControl("finding-status");
-      this.getChildControl("pre-registration-container");
+      this.getChildControl("email-editor");
     },
-
-    __searchPreRegistration: function() {
-      const groupBox = osparc.po.BaseView.createGroupBox(this.tr("Pre-Registration"));
-      const form = this.__preRegistrationForm();
-      const formRenderer = new qx.ui.form.renderer.Single(form);
-      groupBox.add(formRenderer);
-
-      return groupBox;
-    },
-
-    __preRegistrationForm: function() {
-      const form = new qx.ui.form.Form();
-      const requestAccountData = new qx.ui.form.TextArea().set({
-        required: true,
-        minHeight: 200,
-        placeholder: this.tr("Copy&Paste the Request Account Form in JSON format here ...")
-      });
-      form.add(requestAccountData, this.tr("Request Form"));
-
-      const submitBtn = new osparc.ui.form.FetchButton(this.tr("Submit"));
-      submitBtn.set({
-        appearance: "form-button"
-      });
-
-      submitBtn.addListener("execute", () => {
-        if (!osparc.data.Permissions.getInstance().canDo("user.users.pre-register", true)) {
-          return;
-        }
-        if (form.validate()) {
-          submitBtn.setFetching(true);
-
-          const flashErrorMsg = this.tr("Unsuccessful Pre-Registration. See details below");
-          const findingStatus = this.getChildControl("finding-status");
-          findingStatus.setValue(this.tr("Searching Pre-Registered users..."));
-
-          let params;
-          try {
-            params = {
-              data: JSON.parse(requestAccountData.getValue())
-            };
-          } catch (err) {
-            const detailErrorMsg = `Error parsing Request Form JSON. ${err}`;
-            findingStatus.setValue(detailErrorMsg);
-            osparc.FlashMessenger.logError(err, flashErrorMsg);
-            submitBtn.setFetching(false);
-            return
-          }
-
-          osparc.data.Resources.fetch("poUsers", "preRegister", params)
-            .then(data => {
-              if (data.length) {
-                findingStatus.setValue(this.tr("Pre-Registered as:"));
-              } else {
-                findingStatus.setValue(this.tr("No Pre-Registered user found"));
-              }
-              this.__populatePreRegistrationLayout(data);
-            })
-            .catch(err => {
-              const detailErrorMsg = this.tr(`Error during Pre-Registration: ${err.message}`)
-              findingStatus.setValue(detailErrorMsg);
-              osparc.FlashMessenger.logError(err, flashErrorMsg);
-            })
-            .finally(() => submitBtn.setFetching(false));
-        }
-      }, this);
-      form.addButton(submitBtn);
-
-      return form;
-    },
-
-    __populatePreRegistrationLayout: function(respData) {
-      const preRegistrationContainer = this.getChildControl("pre-registration-container");
-      const preregistrationRespViewer = new osparc.ui.basic.JsonTreeWidget(respData, "preregistration-data");
-      preRegistrationContainer.add(preregistrationRespViewer);
-    }
   }
 });
