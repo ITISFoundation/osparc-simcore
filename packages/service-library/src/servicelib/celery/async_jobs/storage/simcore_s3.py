01e8fc3a84c76a70cb50c007467491b97b724274
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-01-16 10:04:09 +0100

    fix import

--- packages/service-library/src/servicelib/celery/async_jobs/storage/simcore_s3.py
+++ packages/service-library/src/servicelib/celery/async_jobs/storage/simcore_s3.py
@@ -5,18 +5,18 @@ from models_library.api_schemas_webserver.storage import PathToExport
 from models_library.products import ProductName
 from models_library.users import UserID
 
-from ....celery.models import (
+from ...models import (
     ExecutionMetadata,
     OwnerMetadata,
     TasksQueue,
 )
-from ....celery.task_manager import TaskManager
+from ...task_manager import TaskManager
 
 EXPORT_DATA_TASK_NAME: Final[str] = "export_data"
 EXPORT_DATA_AS_DOWNLOAD_LINK_TASK_NAME: Final[str] = "export_data_as_download_link"
 
 
-async def submit_export_data(  # noqa: PLR0913
+async def submit_export_data(
     task_manager: TaskManager,
     owner_metadata: OwnerMetadata,
     user_id: UserID,
