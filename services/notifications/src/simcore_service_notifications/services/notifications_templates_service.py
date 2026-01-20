360c2ba56fb55660c8bcefba866318b37db977a5
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-01-20 14:50:53 +0100

    fix order

--- services/notifications/src/simcore_service_notifications/services/notifications_templates_service.py
+++ services/notifications/src/simcore_service_notifications/services/notifications_templates_service.py
@@ -11,10 +11,8 @@ class NotificationsTemplatesService:
     renderer: NotificationsRenderer
 
     def preview_template(self, template_name: str, channel_name: str, variables: dict[str, Any]) -> str:
-        env = self.repository.get_jinja_environment()
-        template_path = self.repository.template_path(template_name, channel_name)
         return self.renderer.render(
-            template_path=template_path,
-            env=env,
+            env=self.repository.get_jinja_environment(),
+            template_path=self.repository.template_path(template_name, channel_name),
             variables=variables,
         )
