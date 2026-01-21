d01a8d250a4f8d0381944c2e8a5919395ca0c852
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-01-21 23:16:16 +0100

    private

--- services/notifications/src/simcore_service_notifications/template/content/registry.py
+++ services/notifications/src/simcore_service_notifications/template/content/registry.py
@@ -2,19 +2,19 @@ from ...exceptions.errors import ContentModelNotFoundError
 from ...models.channel import ChannelType
 from .base import NotificationContent
 
-CONTENTS: dict[ChannelType, type[NotificationContent]] = {}
+_CONTENTS: dict[ChannelType, type[NotificationContent]] = {}
 
 
 def register_content(channel: ChannelType):
     def _(cls: type[NotificationContent]):
-        CONTENTS[channel] = cls
+        _CONTENTS[channel] = cls
         return cls
 
     return _
 
 
 def get_content_cls(channel: ChannelType) -> type[NotificationContent]:
-    content_model = CONTENTS.get(channel)
+    content_model = _CONTENTS.get(channel)
     if not content_model:
         raise ContentModelNotFoundError(channel=channel)
     return content_model
--- services/notifications/src/simcore_service_notifications/template/variables/registry.py
+++ services/notifications/src/simcore_service_notifications/template/variables/registry.py
@@ -4,19 +4,19 @@ from ...exceptions.errors import VariablesModelNotFoundError
 from ...models.channel import ChannelType
 from ...models.template import TemplateRef
 
-VARIABLES_MODELS: dict[TemplateRef, type[BaseModel]] = {}
+_VARIABLES_MODELS: dict[TemplateRef, type[BaseModel]] = {}
 
 
 def register_variables_model(channel: ChannelType, template_name: str):
     def _(cls: type[BaseModel]):
-        VARIABLES_MODELS[TemplateRef(channel, template_name)] = cls
+        _VARIABLES_MODELS[TemplateRef(channel, template_name)] = cls
         return cls
 
     return _
 
 
 def get_variables_model(ref: TemplateRef) -> type[BaseModel]:
-    variables_model = VARIABLES_MODELS.get(ref)
+    variables_model = _VARIABLES_MODELS.get(ref)
     if not variables_model:
         raise VariablesModelNotFoundError(template_ref=ref)
     return variables_model
