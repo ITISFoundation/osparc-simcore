df4508cb6afa62182090bee7fd844cce3b0ca903
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-01-21 21:28:38 +0100

    skip json schema

--- services/notifications/src/simcore_service_notifications/variables/models.py
+++ services/notifications/src/simcore_service_notifications/variables/models.py
@@ -1,12 +1,13 @@
 from typing import Any
 
 from pydantic import BaseModel
+from pydantic.json_schema import SkipJsonSchema
 
 from .registry import register_variables_model
 
 
 class BaseVariablesModel(BaseModel):
-    product: dict[str, Any]
+    product: SkipJsonSchema[dict[str, Any]]
 
 
 @register_variables_model(channel="email", template_name="empty")
