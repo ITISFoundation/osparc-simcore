e6ac10f9aed87bc0de827dba43a6fc4f287b803a
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-01-22 09:42:38 +0100

    rename

--- services/notifications/src/simcore_service_notifications/variables/models.py
+++ services/notifications/src/simcore_service_notifications/variables/models.py
@@ -4,6 +4,6 @@ from .registry import register_variables_model
 
 
 @register_variables_model(channel=ChannelType.email, template_name="empty")
-class EmptyVariablesModel(BaseVariablesModel):
+class EmptyTemplateVariablesModel(BaseVariablesModel):
     body: str
     subject: str
