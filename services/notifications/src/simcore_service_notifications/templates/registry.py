7aa91e6a45e0d436c8934ffae631bcc7f85ee999
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-01-27 19:22:39 +0100

    typecheck

--- services/notifications/src/simcore_service_notifications/templates/registry.py
+++ services/notifications/src/simcore_service_notifications/templates/registry.py
@@ -1,21 +1,20 @@
 from models_library.notifications import ChannelType
-from pydantic import BaseModel
 
 from ..models.context import NotificationsContext
 from ..models.template import NotificationsTemplateRef
 
-_CONTEXT_MODELS: dict[NotificationsTemplateRef, type[BaseModel]] = {}
+_CONTEXT_MODELS: dict[NotificationsTemplateRef, type[NotificationsContext]] = {}
 
 
 def register_context(channel: ChannelType, template_name: str):
-    def _(cls: type[BaseModel]):
+    def _(cls: type[NotificationsContext]):
         _CONTEXT_MODELS[NotificationsTemplateRef(channel, template_name)] = cls
         return cls
 
     return _
 
 
-def get_context_model(ref: NotificationsTemplateRef) -> type[BaseModel]:
+def get_context_model(ref: NotificationsTemplateRef) -> type[NotificationsContext]:
     context_model = _CONTEXT_MODELS.get(ref)
     if not context_model:
         return NotificationsContext
