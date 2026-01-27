80d0fd759dd7368e156f77681f560dc2a13009c7
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-01-27 09:54:21 +0100

    remove unused

--- services/notifications/src/simcore_service_notifications/exceptions/errors.py
+++ services/notifications/src/simcore_service_notifications/exceptions/errors.py
@@ -5,8 +5,4 @@ class NotificationsRuntimeError(OsparcErrorMixin, RuntimeError): ...
 
 
 class ContentModelNotFoundError(NotificationsRuntimeError):
-    msg_template = "Content model for channel '{channel}' not found"
-
-
-class ContextModelNotFoundError(NotificationsRuntimeError):
-    msg_template = "Context model for template '{template_name}' for channel '{channel}' not found"
+    msg_template = "Content model for channel '{channel}' not found."
