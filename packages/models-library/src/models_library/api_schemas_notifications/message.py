445bce3f9a8b5426282f23e86634a9fe821a4dc9
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-02-02 10:48:01 +0100

    revert

--- packages/models-library/src/models_library/api_schemas_notifications/message.py
+++ packages/models-library/src/models_library/api_schemas_notifications/message.py
@@ -10,28 +10,16 @@ class EmailAddress(BaseModel):
     display_name: str = ""
     addr_spec: LowerCaseEmailStr
 
-    model_config = ConfigDict(
-        frozen=True,
-    )
-
 
 class EmailAttachment(BaseModel):
     content: bytes
     filename: str
-
-    model_config = ConfigDict(
-        frozen=True,
-    )
 
 
 class EmailContent(BaseModel):
     subject: str
     body_text: str
     body_html: str | None = None
-
-    model_config = ConfigDict(
-        frozen=True,
-    )
 
 
 class EmailNotificationMessage(BaseModel):
@@ -47,7 +35,6 @@ class EmailNotificationMessage(BaseModel):
     attachments: list[EmailAttachment] | None = None
 
     model_config = ConfigDict(
-        frozen=True,
         validate_by_alias=True,
         validate_by_name=True,
     )
