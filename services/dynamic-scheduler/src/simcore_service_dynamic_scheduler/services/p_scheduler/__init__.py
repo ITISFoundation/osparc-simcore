584c38abf6a87d51a0eebcd229baef4c154f5793
Andrei Neagu <neagu@itis.swiss>
2026-02-06 13:47:12 +0100

    expose public api

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/p_scheduler/__init__.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/p_scheduler/__init__.py
@@ -1,3 +1,12 @@
+from ._api import request_absent, request_present, retry_step, skip_step
+from ._lifespan import p_scheduler_lifespan
 from ._repositories import repositories
 
-__all__: tuple[str, ...] = ("repositories",)
+__all__: tuple[str, ...] = (
+    "p_scheduler_lifespan",
+    "repositories",
+    "request_absent",
+    "request_present",
+    "retry_step",
+    "skip_step",
+)
