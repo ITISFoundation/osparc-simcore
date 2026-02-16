7ab143026ac4c71cd651b42116a3f89a6a6e2835
Andrei Neagu <neagu@itis.swiss>
2026-02-16 11:41:22 +0100

    refactor interface

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/p_scheduler/_abc.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/p_scheduler/_abc.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from datetime import timedelta
 
 from ._models import DagNodeUniqueReference, InData, InDataKeys, OutData, OutDataKeys
 
@@ -8,6 +9,8 @@ class BaseStep(ABC):
     def get_unique_reference(cls) -> DagNodeUniqueReference:
         """Unique reference for this step class"""
         return f"{cls.__module__}.{cls.__name__}"
+
+    ### APPLY ###
 
     @classmethod
     def apply_requests_inputs(cls) -> InDataKeys:
@@ -25,6 +28,13 @@ class BaseStep(ABC):
         """actions that construct/create resources in the system"""
 
     @classmethod
+    @abstractmethod
+    def get_apply_timeout(cls) -> timedelta:
+        """max amount of time after which the apply step will be cancelled"""
+
+    ### REVERT ###
+
+    @classmethod
     def revert_requests_inputs(cls) -> InDataKeys:
         """defines which inputs are required to run the revert step"""
         return set()
@@ -38,3 +48,8 @@ class BaseStep(ABC):
     @abstractmethod
     async def revert(cls, in_data: InData) -> OutData:
         """actions that destroy/remove resources from the system"""
+
+    @classmethod
+    @abstractmethod
+    def get_revert_timeout(cls) -> timedelta:
+        """max amount of time after which the revert step will be cancelled"""
