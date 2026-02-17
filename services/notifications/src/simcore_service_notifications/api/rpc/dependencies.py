755f8e834167b7c23154abfb7a78d3dfddfc56b4
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-02-17 18:15:08 +0100

    fix

--- services/notifications/src/simcore_service_notifications/api/rpc/dependencies.py
+++ services/notifications/src/simcore_service_notifications/api/rpc/dependencies.py
@@ -12,6 +12,6 @@ def get_jinja_env() -> Environment:
 
 def get_notifications_templates_service() -> TemplatesService:
     env = get_jinja_env()
-    repository = FileTemplatesRepository(env)
-    renderer = JinjaNotificationsRenderer(repository)
-    return TemplatesService(repository, renderer)
+    templates_repo = FileTemplatesRepository(env)
+    renderer = JinjaNotificationsRenderer(templates_repo)
+    return TemplatesService(templates_repo, renderer)
