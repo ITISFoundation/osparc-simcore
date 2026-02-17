94e2382121491fb60973ca7a764bf2eb3c72f2ab
odeimaiz <maiz@itis.swiss>
2026-02-17 11:15:08 +0100

    minor

--- services/static-webserver/client/source/class/osparc/po/UsersPending.js
+++ services/static-webserver/client/source/class/osparc/po/UsersPending.js
@@ -376,8 +376,8 @@ qx.Class.define("osparc.po.UsersPending", {
       previewApproval.set({
         invitationUrl,
         email,
-        subject: messageContent.subject,
-        bodyHtml: messageContent.bodyHtml,
+        subject: messageContent["subject"],
+        bodyHtml: messageContent["bodyHtml"],
       });
       osparc.ui.window.Window.popUpInWindow(previewApproval, qx.locale.Manager.tr("Preview email"));
     },
