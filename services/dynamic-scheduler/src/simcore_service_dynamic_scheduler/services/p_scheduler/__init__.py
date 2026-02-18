ef9c70c8aef919fc43834938282bd24f20086bc5
Andrei Neagu <neagu@itis.swiss>
2026-02-18 09:12:52 +0100

    exposing interface

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/p_scheduler/__init__.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/p_scheduler/__init__.py
@@ -1,9 +1,15 @@
-from ._api import request_absent, request_present, retry_step, skip_step
+from ._abc import BaseStep
+from ._api import register_workflow, request_absent, request_present, retry_step, skip_step
 from ._lifespan import p_scheduler_lifespan
+from ._models import WorkflowDefinition, WorkflowName
 from ._repositories import repositories
 
 __all__: tuple[str, ...] = (
+    "BaseStep",
+    "WorkflowDefinition",
+    "WorkflowName",
     "p_scheduler_lifespan",
+    "register_workflow",
     "repositories",
     "request_absent",
     "request_present",
--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/p_scheduler/_api.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/p_scheduler/_api.py
@@ -3,10 +3,11 @@ from models_library.api_schemas_dynamic_scheduler.dynamic_services import Dynami
 from models_library.projects_nodes_io import NodeID
 
 from ..base_repository import get_repository
-from ._models import StepId
+from ._models import StepId, WorkflowDefinition, WorkflowName
 from ._notifications import NotificationsManager
 from ._repositories import UserRequestsRepository
 from ._workflow_manager import WorkflowManager
+from ._workflow_registry import WorkflowRegistry
 
 
 async def request_present(app: FastAPI, node_id: NodeID, dynamic_service_start: DynamicServiceStart) -> None:
@@ -33,3 +34,8 @@ async def retry_step(app: FastAPI, node_id: NodeID, step_id: StepId) -> None:
 async def skip_step(app: FastAPI, node_id: NodeID, step_id: StepId) -> None:
     workflow_manager = WorkflowManager.get_from_app_state(app)
     await workflow_manager.skip_workflow_step(node_id, step_id)
+
+
+def register_workflow(app: FastAPI, name: WorkflowName, definition: WorkflowDefinition) -> None:
+    workflow_manager = WorkflowRegistry.get_from_app_state(app)
+    workflow_manager.register_workflow(name, definition)
