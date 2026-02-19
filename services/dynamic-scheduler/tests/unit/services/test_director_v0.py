9d670fc22613c9e0ae0f0391354095b5774045ab
Andrei Neagu <neagu@itis.swiss>
2026-02-19 14:29:07 +0100

    properly fixed interface

--- services/dynamic-scheduler/tests/unit/services/test_director_v0.py
+++ services/dynamic-scheduler/tests/unit/services/test_director_v0.py
@@ -6,7 +6,8 @@ from collections.abc import Iterator
 
 import pytest
 import respx
-from fastapi import FastAPI
+from faker import Faker
+from fastapi import FastAPI, status
 from models_library.api_schemas_webserver.projects_nodes import NodeGet
 from models_library.projects_nodes_io import NodeID
 from pydantic import TypeAdapter
@@ -38,23 +39,27 @@ def legacy_service_details() -> NodeGet:
 
 
 @pytest.fixture
-def mock_director_v0(node_id: NodeID, legacy_service_details: NodeGet) -> Iterator[None]:
+def missing_node_id(faker: Faker) -> NodeID:
+    return faker.uuid4(cast_to=None)
+
+
+@pytest.fixture
+def mock_director_v0(node_id: NodeID, legacy_service_details: NodeGet, missing_node_id: NodeID) -> Iterator[None]:
     with respx.mock(
         base_url="http://director:8000",
         assert_all_called=False,
         assert_all_mocked=True,  # IMPORTANT: KEEP always True!
     ) as respx_mock:
         respx_mock.request(method="GET", path=f"/v0/running_interactive_services/{node_id}").respond(
-            status_code=200,
-            json={
-                "data": legacy_service_details.model_dump(mode="json"),
-                "error": None,
-            },
+            status_code=status.HTTP_200_OK, json={"data": legacy_service_details.model_dump(mode="json"), "error": None}
+        )
+        respx_mock.request(method="GET", path=f"/v0/running_interactive_services/{missing_node_id}").respond(
+            status_code=status.HTTP_404_NOT_FOUND,
+            json={"error": {"errors": [f"Service with uuid {missing_node_id} was not found"]}},
         )
 
         respx_mock.request(method="GET", path="/v0/running_interactive_services").respond(
-            status_code=200,
-            json={"data": [legacy_service_details.model_dump(mode="json")]},
+            status_code=status.HTTP_200_OK, json={"data": [legacy_service_details.model_dump(mode="json")]}
         )
 
         yield
@@ -65,10 +70,15 @@ async def test_get_running_service_details(
     app: FastAPI,
     node_id: NodeID,
     legacy_service_details: NodeGet,
+    missing_node_id: NodeID,
 ):
+    # 1. service that is present
     client = DirectorV0PublicClient.get_from_app_state(app)
     result = await client.get_running_service_details(node_id)
     assert result == legacy_service_details
+
+    # 2. missing sevrvice
+    assert await client.get_running_service_details(missing_node_id) is None
 
 
 async def test_get_running_services(
