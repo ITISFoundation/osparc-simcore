9314692e9db897f7131baad88064eefc03f1987a
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-02-23 13:30:04 +0100

    fix

--- packages/service-library/src/servicelib/celery/async_jobs/notifications.py
+++ packages/service-library/src/servicelib/celery/async_jobs/notifications.py
@@ -1,6 +1,6 @@
 from typing import Any, Final
 
-from ..models import ExecutionMetadata, OwnerMetadata, TaskName, TaskUUID
+from ..models import ExecutionMetadata, GroupUUID, OwnerMetadata, TaskName, TaskUUID
 from ..task_manager import TaskManager
 
 SEND_MESSAGE_TASK_NAME_TEMPLATE: Final[TaskName] = "send_{}_message"
@@ -20,3 +20,24 @@ async def submit_send_message_task(
         owner_metadata=owner_metadata,
         message=message,
     ), SEND_MESSAGE_TASK_NAME_TEMPLATE.format(message["channel"])
+
+
+async def submit_send_messages_task(
+    task_manager: TaskManager,
+    *,
+    owner_metadata: OwnerMetadata,
+    messages: list[dict[str, Any]],  # NOTE: validated internally
+) -> tuple[GroupUUID, list[TaskUUID], TaskName]:
+    return await task_manager.submit_group(
+        [
+            (
+                ExecutionMetadata(
+                    name=SEND_MESSAGE_TASK_NAME_TEMPLATE.format(message["channel"]),
+                    queue="notifications",
+                ),
+                message,
+            )
+            for message in messages
+        ],
+        owner_metadata=owner_metadata,
+    ) + (SEND_MESSAGE_TASK_NAME_TEMPLATE.format(messages[0]["channel"]),)
