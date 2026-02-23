bdea0b7fbf68991b81ad7f4ca7e7226bf7a61829
odeimaiz <maiz@itis.swiss>
2026-02-23 14:19:36 +0100

    supportButton

--- services/static-webserver/client/source/class/osparc/support/SupportButton.js
+++ services/static-webserver/client/source/class/osparc/support/SupportButton.js
@@ -23,6 +23,8 @@ qx.Class.define("osparc.support.SupportButton", {
 
     this._setLayout(new qx.ui.layout.Canvas());
 
+    osparc.utils.Utils.setIdToWidget(this, "supportButton");
+
     this.set({
       toolTipText: this.tr("Help & Support"),
     });
