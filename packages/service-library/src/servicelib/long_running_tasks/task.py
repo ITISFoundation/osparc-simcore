f8a31c20939155691fb3791b79dfa56a5bb58bfd
Andrei Neagu <neagu@itis.swiss>
2026-02-24 15:10:13 +0100

    refactor

--- packages/service-library/src/servicelib/long_running_tasks/task.py
+++ packages/service-library/src/servicelib/long_running_tasks/task.py
@@ -57,7 +57,7 @@ _STATUS_UPDATE_CHECK_INTERNAL: Final[datetime.timedelta] = datetime.timedelta(se
 _MAX_EXCLUSIVE_TASK_CANCEL_TIMEOUT: Final[NonNegativeFloat] = 5
 _TASK_REMOVAL_MAX_WAIT: Final[NonNegativeFloat] = 60
 _PARALLEL_TASKS_CANCELLATION: Final[int] = 5
-_AGGRASSIVE_WAIT_FIXED: Final[NonNegativeFloat] = datetime.timedelta(seconds=0.1).total_seconds()
+_TASK_REMOVED_CHECK_INTERNAL: Final[NonNegativeFloat] = datetime.timedelta(seconds=0.1).total_seconds()
 
 type AllowedErrors = tuple[type[BaseException], ...]
 
@@ -595,18 +595,15 @@ class TasksManager:  # pylint:disable=too-many-instance-attributes
 
         task_id = self._get_task_id(task_name, is_unique=unique, unique_args=unique_args, **task_kwargs)
 
-        # if task is being removed, wait for the operation to finish
+        # wait for a task being removed to go away before starting a new one
         async for attempt in AsyncRetrying(
             stop=stop_after_delay(_TASK_REMOVAL_MAX_WAIT),
             reraise=True,
-            # aggressive retry since we want to start the task as soon as possible
-            wait=wait_fixed(_AGGRASSIVE_WAIT_FIXED),
+            wait=wait_fixed(_TASK_REMOVED_CHECK_INTERNAL),
         ):
             with attempt:
                 if await self._tasks_data.is_marked_for_removal(task_id):
-                    _logger.info(
-                        "Waiting for task '%s' to be removed before starting a new one with the same id", task_id
-                    )
+                    _logger.info("task='%s' waiting to be removed before starting a new one with the same id", task_id)
                     raise TryAgain
 
         # only one unique task can be running
