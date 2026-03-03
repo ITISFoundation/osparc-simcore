b468bfcc9a0514b60b4a7033d930346c008ec076
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-03-03 08:56:13 +0100

    protocol

--- services/notifications/src/simcore_service_notifications/repositories/_templates.py
+++ services/notifications/src/simcore_service_notifications/repositories/_templates.py
@@ -1,15 +1,13 @@
-from abc import ABC, abstractmethod
+from typing import Protocol
 
 from jinja2 import Template as JinjaTemplate
 
 from ..models.template import Template
 
 
-class TemplatesRepository(ABC):
-    @abstractmethod
+class TemplatesRepository(Protocol):
     def get_jinja_template(self, template: Template, part: str) -> JinjaTemplate: ...
 
-    @abstractmethod
     def search_templates(
         self, *, channel: str | None = None, template_name: str | None = None, part: str | None = None
     ) -> list[Template]: ...
