fdf22701c55260b1318e160cad255131d0def2b4
Andrei Neagu <5694077+GitHK@users.noreply.github.com>
2026-03-12 14:05:57 +0100

    🐛 director-v2 now checks for the RPC client's health as well (#8902)
    Co-authored-by: Andrei Neagu <neagu@itis.swiss>

--- services/director-v2/src/simcore_service_director_v2/api/routes/health.py
+++ services/director-v2/src/simcore_service_director_v2/api/routes/health.py
@@ -4,9 +4,9 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from models_library.api_schemas__common.health import HealthCheckGet
 from models_library.errors import RABBITMQ_CLIENT_UNHEALTHY_MSG
-from servicelib.rabbitmq import RabbitMQClient
+from servicelib.rabbitmq import RabbitMQClient, RabbitMQRPCClient
 
-from ...api.dependencies.rabbitmq import get_rabbitmq_client_from_request
+from ...api.dependencies.rabbitmq import get_rabbitmq_client_from_request, rabbitmq_rpc_client
 
 
 class HealthCheckError(RuntimeError):
@@ -19,8 +19,9 @@ router = APIRouter()
 @router.get("/", response_model=HealthCheckGet)
 async def check_service_health(
     rabbitmq_client: Annotated[RabbitMQClient, Depends(get_rabbitmq_client_from_request)],
+    rabbitmq_rpc: Annotated[RabbitMQRPCClient, Depends(rabbitmq_rpc_client)],
 ):
-    if not rabbitmq_client.healthy:
+    if not rabbitmq_client.healthy or not rabbitmq_rpc.healthy:
         raise HealthCheckError(RABBITMQ_CLIENT_UNHEALTHY_MSG)
 
     return {"timestamp": f"{__name__}@{datetime.datetime.now(tz=datetime.UTC).isoformat()}"}
