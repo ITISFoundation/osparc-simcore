830b5268356e22fd37f8367ae649d7e0cdb35d3b
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-03-16 10:03:28 +0100

    rename

--- packages/models-library/src/models_library/notifications/rpc/_message.py
+++ packages/models-library/src/models_library/notifications/rpc/_message.py
@@ -3,10 +3,20 @@ from typing import Any
 from pydantic import BaseModel, ConfigDict
 
 from models_library.celery import GroupUUID, TaskUUID
+from models_library.notifications import TemplateRef
 
 
 class SendMessageRequest(BaseModel):
     message: dict[str, Any]
+
+    model_config = ConfigDict(frozen=True)
+
+
+class SendMessageFromTemplateRequest(BaseModel):
+    template_ref: TemplateRef
+    context: dict[str, Any]
+
+    # GCR: add envelope here!
 
     model_config = ConfigDict(frozen=True)
 
--- packages/service-library/src/servicelib/rabbitmq/rpc_interfaces/notifications/__init__.py
+++ packages/service-library/src/servicelib/rabbitmq/rpc_interfaces/notifications/__init__.py
@@ -1,5 +1,5 @@
-from ._messages import send_message
-from ._templates import preview_template, search_templates
+from ._message import send_message
+from ._template import preview_template, search_templates
 
 __all__: tuple[str, ...] = (
     "preview_template",
