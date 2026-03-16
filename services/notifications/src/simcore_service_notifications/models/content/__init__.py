52562c1a0b1d4793e104c0a6dc75f29142ce8059
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-03-16 15:05:43 +0100

    fix

--- services/notifications/src/simcore_service_notifications/models/content/__init__.py
+++ services/notifications/src/simcore_service_notifications/models/content/__init__.py
@@ -1,20 +1,9 @@
-from models_library.notifications import ChannelType
-
-from ..._channel_registry import ChannelRegistry
 from ._content import Content
 from ._email import EmailContent
+from ._registry import for_channel
 
 __all__: tuple[str, ...] = (
     "Content",
     "EmailContent",
     "for_channel",
 )
-
-_CONTENT_MODELS: ChannelRegistry[type[Content]] = ChannelRegistry(
-    {ChannelType.email: EmailContent},
-)
-
-
-def for_channel(channel: ChannelType) -> type[Content]:
-    """Get content model class for a specific channel."""
-    return _CONTENT_MODELS.get(channel)
