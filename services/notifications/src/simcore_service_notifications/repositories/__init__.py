5c898560a72f504bc4c26f21dfce382683507e87
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-03-16 10:58:20 +0100

    rename

--- services/notifications/src/simcore_service_notifications/repositories/__init__.py
+++ services/notifications/src/simcore_service_notifications/repositories/__init__.py
@@ -1,4 +1,4 @@
-from ._file_templates import FileTemplateRepository, template_path_prefix
-from ._templates import TemplateRepository
+from ._file_template import FileTemplateRepository, template_path_prefix
+from ._template import TemplateRepository
 
 __all__: tuple[str, ...] = ("FileTemplateRepository", "TemplateRepository", "template_path_prefix")
