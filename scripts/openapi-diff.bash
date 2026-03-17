77bacad6465beb9ef30890161de85b1feceb2451
Mads Bisgaard <bisgaard@itis.swiss>
2026-03-17 14:35:25 +0100

    use stable version

--- scripts/openapi-diff.bash
+++ scripts/openapi-diff.bash
@@ -16,5 +16,5 @@ exec docker run \
   --volume="/etc/passwd:/etc/passwd:ro" \
   --user="$(id --user "$USER")":"$(id --group "$USER")" \
   --volume "$(pwd):/specs" \
-  tufin/oasdiff:feat-openapi-3.1-support \
+  tufin/oasdiff:stable \
   "$@"
