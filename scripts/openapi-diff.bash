1b02d638712fa5e23973d75929bb2c528e78fdc6
Andrei Neagu <5694077+GitHK@users.noreply.github.com>
2026-03-17 13:21:10 +0100

    Update openapi-diff.bash

--- scripts/openapi-diff.bash
+++ scripts/openapi-diff.bash
@@ -15,5 +15,6 @@ exec docker run \
   --volume="/etc/group:/etc/group:ro" \
   --volume="/etc/passwd:/etc/passwd:ro" \
   --user="$(id --user "$USER")":"$(id --group "$USER")" \
-  --volume "$(pwd):/specs" \n  "tufin/oasdiff:feat-openapi-3.1-support" \
+  --volume "$(pwd):/specs" \
+  "tufin/oasdiff:feat-openapi-3.1-support" \
   "$@"
