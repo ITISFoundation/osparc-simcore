37429d37784d4c5a798c65d88cb39bca50c175cb
Andrei Neagu <neagu@itis.swiss>
2026-03-18 15:05:51 +0100

    exposing necessary

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/p_scheduler/__init__.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/p_scheduler/__init__.py
@@ -1,11 +1,13 @@
 from ._abc import BaseStep
 from ._api import register_workflow, request_absent, request_present, retry_step, skip_step
 from ._lifespan import p_scheduler_lifespan
-from ._models import StepFailHistory, WorkflowDefinition, WorkflowName
+from ._models import InData, OutData, StepFailHistory, WorkflowDefinition, WorkflowName
 from ._repositories import repositories
 
 __all__: tuple[str, ...] = (
     "BaseStep",
+    "InData",
+    "OutData",
     "StepFailHistory",
     "WorkflowDefinition",
     "WorkflowName",
