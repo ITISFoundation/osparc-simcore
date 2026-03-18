25778cb8b4d07c8c44e250d39faaaddd9e75e4bc
Andrei Neagu <neagu@itis.swiss>
2026-03-18 15:05:19 +0100

    addedmissing

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/p_scheduler/_workflow_name_resolver.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/p_scheduler/_workflow_name_resolver.py
@@ -4,6 +4,8 @@ from models_library.projects_nodes_io import NodeID
 from ._models import WorkflowName
 
 
-async def get_workflow_name_from_node_id(app: FastAPI, node_id: NodeID) -> WorkflowName:  # noqa: ARG001
+async def get_workflow_name_from_node_id(app: FastAPI, node_id: NodeID) -> WorkflowName:
     # transformers a node_id into a workflow name
+    _ = app
+    _ = node_id
     return "TODO: add implementation here"
