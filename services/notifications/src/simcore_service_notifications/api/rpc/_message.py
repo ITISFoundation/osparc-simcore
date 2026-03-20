7e950d951c3eb6fcb8dce6ca00c4c78ea92e55cc
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-03-20 09:28:49 +0100

    rename

--- services/notifications/src/simcore_service_notifications/api/rpc/_message.py
+++ services/notifications/src/simcore_service_notifications/api/rpc/_message.py
@@ -47,7 +47,7 @@ async def send_message_from_template(
 
     message_service = get_message_service(app)
     task_or_group_uuid, task_name = await message_service.send_message_from_template(
-        addressing=request.envelope,
+        addressing=request.addressing,
         ref=TemplateRef(**request.template_ref.model_dump()),
         context=request.context,
         owner_metadata=request.owner_metadata,
