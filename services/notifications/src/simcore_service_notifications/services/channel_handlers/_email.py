90e0695a6717c990cf9c324ef11903a82751aca8
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-03-23 14:59:59 +0100

    🎨 Interleave email recipients by domain (#8952)

--- services/notifications/src/simcore_service_notifications/services/channel_handlers/_email.py
+++ services/notifications/src/simcore_service_notifications/services/channel_handlers/_email.py
@@ -1,9 +1,18 @@
 from typing import Any
 
+from common_library.network import extract_email_domain
+from common_library.sequence_tools import interleave_by_key
 from models_library.notifications.celery import EmailMessage as CeleryEmailMessage
-from models_library.notifications.rpc import EmailMessage
+from models_library.notifications.rpc import EmailContact, EmailMessage
 
 from ._base import ChannelHandler
+
+
+def _interleave_recipients_by_domain(
+    recipients: list[EmailContact],
+) -> list[EmailContact]:
+    """Reorder recipients so that domains are spread as far apart as possible."""
+    return interleave_by_key(recipients, key=lambda r: extract_email_domain(r.email))
 
 
 class EmailChannelHandler(ChannelHandler):
@@ -14,6 +23,8 @@ class EmailChannelHandler(ChannelHandler):
         content_dict = message.content.model_dump()
         from_dict = message.addressing.from_.model_dump()
 
+        recipients = _interleave_recipients_by_domain(message.addressing.to)
+
         return [
             CeleryEmailMessage.model_validate(
                 {
@@ -23,5 +34,5 @@ class EmailChannelHandler(ChannelHandler):
                     "content": content_dict,
                 }
             ).model_dump(by_alias=True)
-            for recipient in message.addressing.to
+            for recipient in recipients
         ]
