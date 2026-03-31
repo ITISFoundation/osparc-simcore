d91043243450d44ee032ca475d8ccca690843705
Andrei Neagu <neagu@itis.swiss>
2026-03-31 14:19:34 +0200

    send out notifications

--- services/storage/src/simcore_service_storage/api/_worker_tasks/_paths.py
+++ services/storage/src/simcore_service_storage/api/_worker_tasks/_paths.py
@@ -6,6 +6,7 @@ from celery_library.worker.app_server import get_app_server
 from models_library.celery import TaskKey
 from models_library.products import ProductName
 from models_library.projects_nodes_io import LocationID, StorageFileID
+from models_library.rabbitmq_messages import FileNotificationEventType
 from models_library.users import UserID
 from pydantic import ByteSize, TypeAdapter
 from servicelib.logging_utils import log_context
@@ -13,6 +14,7 @@ from servicelib.utils import limited_gather
 
 from ...constants import MAX_CONCURRENT_S3_TASKS
 from ...dsm import get_dsm_provider
+from ...modules.rabbitmq import post_file_notification
 
 _logger = logging.getLogger(__name__)
 
@@ -48,9 +50,22 @@ async def delete_paths(
         logging.INFO,
         msg=f"delete {paths=} in {location_id=} for {user_id=}",
     ):
-        dsm = get_dsm_provider(get_app_server(task.app).app).get(location_id)
+        app = get_app_server(task.app).app
+        dsm = get_dsm_provider(app).get(location_id)
         files_ids: set[StorageFileID] = {TypeAdapter(StorageFileID).validate_python(f"{path}") for path in paths}
         await limited_gather(
             *[dsm.delete_file(user_id, file_id) for file_id in files_ids],
+            limit=MAX_CONCURRENT_S3_TASKS,
+        )
+        await limited_gather(
+            *[
+                post_file_notification(
+                    app,
+                    event_type=FileNotificationEventType.FILE_DELETED,
+                    user_id=user_id,
+                    file_id=file_id,
+                )
+                for file_id in files_ids
+            ],
             limit=MAX_CONCURRENT_S3_TASKS,
         )
