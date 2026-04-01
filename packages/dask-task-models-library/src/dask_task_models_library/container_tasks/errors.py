23e113c5052b5bdda08c485e454a62a3e9008b00
sanderegg <35365065+sanderegg@users.noreply.github.com>
2026-04-01 17:18:54 +0200

    @pcrespov review: use user_message

--- packages/dask-task-models-library/src/dask_task_models_library/container_tasks/errors.py
+++ packages/dask-task-models-library/src/dask_task_models_library/container_tasks/errors.py
@@ -1,10 +1,11 @@
 """Dask task exceptions"""
 
 from common_library.errors_classes import OsparcErrorMixin
+from common_library.user_messages import user_message
 
 
 class ContainerTaskError(OsparcErrorMixin, RuntimeError):
-    msg_template = (
+    msg_template = user_message(
         "The service {service_key}:{service_version}"
         " running in container {container_id} encountered an unexpected error: {error_message}."
     )
@@ -12,14 +13,14 @@ class ContainerTaskError(OsparcErrorMixin, RuntimeError):
 
 class ServiceRuntimeError(ContainerTaskError):
     code = "runtime"  # type: ignore[assignment]
-    msg_template = (
+    msg_template = user_message(
         "The service {service_key}:{service_version} running in container {container_id} failed with code {exit_code}."
     )
 
 
 class ServiceOutOfMemoryError(ServiceRuntimeError):
     code = "runtime.oom"
-    msg_template = (
+    msg_template = user_message(
         "The service {service_key}:{service_version}"
         " running in container {container_id} ran out of memory and was terminated."
         " Current limits are {service_resources}."
@@ -28,7 +29,7 @@ class ServiceOutOfMemoryError(ServiceRuntimeError):
 
 class ServiceTimeoutLoggingError(ServiceRuntimeError):
     code = "runtime.timeout"
-    msg_template = (
+    msg_template = user_message(
         "The service {service_key}:{service_version}"
         " running in container {container_id} was silent/hanging for longer than {timeout_timedelta} "
         "and was terminated."
@@ -36,11 +37,11 @@ class ServiceTimeoutLoggingError(ServiceRuntimeError):
 
 
 class TaskCancelledError(ContainerTaskError):
-    msg_template = "The task was cancelled"
+    msg_template = user_message("The task was cancelled")
 
 
 class ServiceInputsUseFileToKeyMapButReceivesZipDataError(ContainerTaskError):
-    msg_template = (
+    msg_template = user_message(
         "The service {service_key}:{service_version} {input} uses a file-to-key {file_to_key_map} "
         "map but receives zip data instead. "
         "TIP: either pass a single file or zip file and remove the file-to-key map parameter."
