b0e07160c3dd48772259b02e5ccdc6af9d025a92
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-04-02 12:54:59 +0200

    fix

--- services/web/server/tests/unit/with_dbs/03/test_email.py
+++ services/web/server/tests/unit/with_dbs/03/test_email.py
@@ -9,7 +9,6 @@ import json
 from collections import Counter
 from html.parser import HTMLParser
 from pathlib import Path
-from types import SimpleNamespace
 from unittest.mock import MagicMock
 
 import pytest
@@ -200,7 +199,7 @@ def test_render_templates(template_path: Path, faker: Faker):
             "code": "123",
             "reason": "no reason",
             "link": faker.url(),
-            "product": SimpleNamespace(name="foobar", display_name="Foo Bar"),
+            "product": {"name": "foobar", "display_name": "Foo Bar"},
             "retention_days": 30,
             "dumps": functools.partial(json_dumps, indent=1),
             "request_form": fake_request_form,
