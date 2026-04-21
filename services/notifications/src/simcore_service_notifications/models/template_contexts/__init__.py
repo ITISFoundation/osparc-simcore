57d417476547795de337a5eb86b5ea4deb1605f4
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-04-21 15:01:41 +0200

    🐛 fix(notifications): 2FA email template context (#9047)

--- services/notifications/src/simcore_service_notifications/models/template_contexts/__init__.py
+++ services/notifications/src/simcore_service_notifications/models/template_contexts/__init__.py
@@ -4,6 +4,7 @@ from ._account_requested import AccountRequestedTemplateContext
 from ._change_email import ChangeEmailTemplateContext
 from ._credit_reimbursement import CreditReimbursementTemplateContext
 from ._empty import EmptyTemplateContext
+from ._new_2fa_code import New2faCodeTemplateContext
 from ._registered import RegisteredTemplateContext
 from ._reset_password import ResetPasswordTemplateContext
 from ._unregister import UnregisterTemplateContext
@@ -15,6 +16,7 @@ __all__: tuple[str, ...] = (
     "ChangeEmailTemplateContext",
     "CreditReimbursementTemplateContext",
     "EmptyTemplateContext",
+    "New2faCodeTemplateContext",
     "RegisteredTemplateContext",
     "ResetPasswordTemplateContext",
     "UnregisterTemplateContext",
