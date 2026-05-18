69e72305c84cb1f47027215d7fae87c181110b97
Andrei Neagu <neagu@itis.swiss>
2026-05-18 14:25:34 +0200

    added proudct_name to legacy dynamic services as well

--- packages/models-library/src/models_library/api_schemas_webserver/projects_nodes.py
+++ packages/models-library/src/models_library/api_schemas_webserver/projects_nodes.py
@@ -1,5 +1,5 @@
 # mypy: disable-error-code=truthy-function
-from typing import Annotated, Any, Literal, TypeAlias
+from typing import Annotated, Any, Literal
 
 from pydantic import ConfigDict, Field
 from pydantic.config import JsonDict
@@ -8,6 +8,7 @@ from ..access_rights import ExecutableAccessRights
 from ..api_schemas_directorv2.dynamic_services import RetrieveDataOut
 from ..basic_types import PortInt
 from ..groups import GroupID
+from ..products import ProductName
 from ..projects import ProjectID
 from ..projects_nodes import InputID, InputsDict, PartialNode
 from ..projects_nodes_io import NodeID
@@ -28,7 +29,7 @@ class NodeCreate(InputSchemaWithoutCamelCase):
     service_id: str | None = None
 
 
-BootOptions: TypeAlias = dict
+type BootOptions = dict
 
 
 class NodePatch(InputSchemaWithoutCamelCase):
@@ -106,31 +107,29 @@ class NodeGet(OutputSchema):
     )
     service_state: ServiceState = Field(
         ...,
-        description="the service state * 'pending' - The service is waiting for resources to start * 'pulling' - The service is being pulled from the registry * 'starting' - The service is starting * 'running' - The service is running * 'complete' - The service completed * 'failed' - The service failed to start\n",
+        description=(
+            "the service state * 'pending' "
+            "- The service is waiting for resources to start * 'pulling' "
+            "- The service is being pulled from the registry * 'starting' "
+            "- The service is starting * 'running' "
+            "- The service is running * 'complete' "
+            "- The service completed * 'failed' "
+            "- The service failed to start"
+        ),
     )
     service_message: str | None = Field(
         None,
         description="the service message",
     )
     user_id: str = Field(..., description="the user that started the service")
+    product_name: ProductName = Field(
+        ...,
+        description="Product upon which this service is scheduled.",
+    )
     model_config = ConfigDict(
         json_schema_extra={
             "examples": [
-                # computational
-                {
-                    "published_port": 30000,
-                    "entrypoint": "/the/entry/point/is/here",
-                    "service_uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                    "service_key": "simcore/services/comp/itis/sleeper",
-                    "service_version": "1.2.3",
-                    "service_host": "jupyter_E1O2E-LAH",
-                    "service_port": 8081,
-                    "service_basepath": "/x/E1O2E-LAH",
-                    "service_state": "pending",
-                    "service_message": "no suitable node (insufficient resources on 1 node)",
-                    "user_id": "123",
-                },
-                # dynamic
+                # legacy dynamic service (director-v0 /v0/running_interactive_services)
                 {
                     "published_port": 30000,
                     "entrypoint": "/the/entry/point/is/here",
@@ -143,6 +142,7 @@ class NodeGet(OutputSchema):
                     "service_state": "pending",
                     "service_message": "no suitable node (insufficient resources on 1 node)",
                     "user_id": "123",
+                    "product_name": "osparc",
                 },
             ]
         }
@@ -196,7 +196,7 @@ class NodeOutputs(InputSchemaWithoutCamelCase):
 
 
 class NodeRetrieve(InputSchemaWithoutCamelCase):
-    port_keys: list[ServicePortKey] = []
+    port_keys: list[ServicePortKey] = Field(default_factory=list)
 
 
 class NodeRetrieved(RetrieveDataOut):
