f28d90167b9cfab1278d7a4bcc4f68764f2f3f09
Mads Bisgaard <bisgaard@itis.swiss>
2026-05-20 10:38:52 +0200

    support using a single db connection for batch metadata get

--- services/web/server/src/simcore_service_webserver/projects/_metadata_service.py
+++ services/web/server/src/simcore_service_webserver/projects/_metadata_service.py
@@ -20,17 +20,17 @@ async def get_project_custom_metadata_for_user(
     app: web.Application, user_id: UserID, project_uuid: ProjectID
 ) -> MetadataDict:
     """raises: ProjectNotFoundError"""
-    await validate_project_ownership(app, user_id=user_id, project_uuid=project_uuid)
+    await validate_project_ownership(get_asyncpg_engine(app), user_id=user_id, project_uuid=project_uuid)
 
     return await _metadata_repository.get_project_custom_metadata(
-        engine=get_asyncpg_engine(app), project_uuid=project_uuid
+        engine=get_asyncpg_engine(app), connection=None, project_uuid=project_uuid
     )
 
 
 async def get_project_custom_metadata_or_empty_dict(app: web.Application, project_uuid: ProjectID) -> MetadataDict:
     try:
         output = await _metadata_repository.get_project_custom_metadata(
-            engine=get_asyncpg_engine(app), project_uuid=project_uuid
+            engine=get_asyncpg_engine(app), connection=None, project_uuid=project_uuid
         )
     except ProjectNotFoundError:
         # This is a valid case when the project is not found
@@ -45,7 +45,7 @@ async def set_project_custom_metadata(
     project_uuid: ProjectID,
     value: MetadataDict,
 ) -> MetadataDict:
-    await validate_project_ownership(app, user_id=user_id, project_uuid=project_uuid)
+    await validate_project_ownership(get_asyncpg_engine(app), user_id=user_id, project_uuid=project_uuid)
 
     return await _metadata_repository.set_project_custom_metadata(
         engine=get_asyncpg_engine(app),
@@ -58,7 +58,7 @@ _NIL_NODE_UUID: Final[NodeID] = NodeID(int=0)
 
 
 async def _project_has_ancestors(app: web.Application, *, user_id: UserID, project_uuid: ProjectID) -> bool:
-    await validate_project_ownership(app, user_id=user_id, project_uuid=project_uuid)
+    await validate_project_ownership(get_asyncpg_engine(app), user_id=user_id, project_uuid=project_uuid)
 
     return await _metadata_repository.project_has_ancestors(engine=get_asyncpg_engine(app), project_uuid=project_uuid)
 
@@ -70,7 +70,7 @@ async def set_project_ancestors_from_custom_metadata(
     custom_metadata: MetadataDict,
 ) -> None:
     """NOTE: this should not be used anywhere else than from the metadata handler!"""
-    await validate_project_ownership(app, user_id=user_id, project_uuid=project_uuid)
+    await validate_project_ownership(get_asyncpg_engine(app), user_id=user_id, project_uuid=project_uuid)
     if await _project_has_ancestors(app, user_id=user_id, project_uuid=project_uuid):
         # we do not override any existing ancestors via this method
         return
@@ -102,7 +102,7 @@ async def set_project_ancestors(
     parent_project_uuid: ProjectID | None,
     parent_node_id: NodeID | None,
 ) -> None:
-    await validate_project_ownership(app, user_id=user_id, project_uuid=project_uuid)
+    await validate_project_ownership(get_asyncpg_engine(app), user_id=user_id, project_uuid=project_uuid)
 
     await _metadata_repository.set_project_ancestors(
         get_asyncpg_engine(app),
