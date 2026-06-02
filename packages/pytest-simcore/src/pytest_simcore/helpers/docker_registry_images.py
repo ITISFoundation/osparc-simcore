25e43a566069b8030568503845583492eefd2cca
sanderegg <35365065+sanderegg@users.noreply.github.com>
2026-06-02 08:42:12 +0200

    add helper

---
+++ packages/pytest-simcore/src/pytest_simcore/helpers/docker_registry_images.py
@@ -0,0 +1,40 @@
+from typing import Any, Literal, Protocol, TypedDict
+
+
+class NodeRequirementsDict(TypedDict):
+    CPU: float
+    RAM: float
+
+
+class ServiceExtrasDict(TypedDict):
+    node_requirements: NodeRequirementsDict
+    build_date: str
+    vcs_ref: str
+    vcs_url: str
+
+
+class ServiceDescriptionDict(TypedDict):
+    key: str
+    version: str
+    type: Literal["computational", "dynamic"]
+
+
+class ServiceInRegistryInfoDict(TypedDict):
+    service_description: ServiceDescriptionDict
+    docker_labels: dict[str, Any]
+    image_path: str
+    internal_port: int | None
+    entry_point: str
+    service_extras: ServiceExtrasDict
+
+
+class PushServicesCallable(Protocol):
+    async def __call__(
+        self,
+        *,
+        number_of_computational_services: int,
+        number_of_interactive_services: int,
+        inter_dependent_services: bool = False,
+        bad_json_format: bool = False,
+        version="1.0.",
+    ) -> list[ServiceInRegistryInfoDict]: ...
