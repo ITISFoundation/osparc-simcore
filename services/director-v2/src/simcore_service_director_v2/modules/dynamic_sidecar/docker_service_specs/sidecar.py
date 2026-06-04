ebd4f223da0ed507859258ad156996768bfbccfd
Andrei Neagu <neagu@itis.swiss>
2026-06-04 08:22:17 +0200

    refactor

--- services/director-v2/src/simcore_service_director_v2/modules/dynamic_sidecar/docker_service_specs/sidecar.py
+++ services/director-v2/src/simcore_service_director_v2/modules/dynamic_sidecar/docker_service_specs/sidecar.py
@@ -175,13 +175,13 @@ def _get_environment_variables(
         "S3_REGION": r_clone_settings.R_CLONE_S3.S3_REGION,
         "S3_SECRET_KEY": r_clone_settings.R_CLONE_S3.S3_SECRET_KEY.get_secret_value(),
         "SC_BOOT_MODE": f"{app_settings.DYNAMIC_SERVICES.DYNAMIC_SIDECAR.DYNAMIC_SIDECAR_SC_BOOT_MODE}",
-        "SSL_CERT_FILE": app_settings.DIRECTOR_V2_SELF_SIGNED_SSL_FILENAME,
-        "DYNAMIC_SIDECAR_TRACING": (
-            app_settings.DIRECTOR_V2_TRACING.json() if app_settings.DIRECTOR_V2_TRACING else "null"
-        ),
-        "DY_SIDECAR_USER_SERVICES_TRACING_ENABLED": f"{scheduler_data.tracing}",
         # For background info on this special env-var above, see
         # - https://stackoverflow.com/questions/31448854/how-to-force-requests-use-the-certificates-on-my-ubuntu-system#comment78596389_37447847
+        "SSL_CERT_FILE": app_settings.DIRECTOR_V2_SELF_SIGNED_SSL_FILENAME,
+        "DYNAMIC_SIDECAR_TRACING": (
+            app_settings.DIRECTOR_V2_TRACING.model_dump_json() if app_settings.DIRECTOR_V2_TRACING else "null"
+        ),
+        "DY_SIDECAR_USER_SERVICES_TRACING_ENABLED": f"{scheduler_data.tracing}",
         "SIMCORE_HOST_NAME": scheduler_data.service_name,
         "STORAGE_HOST": storage_config.host,
         "STORAGE_PASSWORD": storage_config.password,
