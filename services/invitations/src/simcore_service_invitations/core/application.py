c98df0efb9a47c0126530b399fe3d5c8679e6166
sanderegg <35365065+sanderegg@users.noreply.github.com>
2026-06-05 16:18:22 +0200

    invitations done

--- services/invitations/src/simcore_service_invitations/core/application.py
+++ services/invitations/src/simcore_service_invitations/core/application.py
@@ -1,29 +1,57 @@
 import logging
+from collections.abc import AsyncIterator
 
 from common_library.json_serialization import json_dumps
 from fastapi import FastAPI
+from fastapi_lifespan_manager import LifespanManager, State
 from servicelib.fastapi.lifespan_utils import Lifespan
-from servicelib.fastapi.monitoring import (
-    initialize_prometheus_instrumentation,
-)
+from servicelib.fastapi.monitoring import configure_prometheus_instrumentation
 from servicelib.fastapi.openapi import override_fastapi_openapi_method
-from servicelib.fastapi.tracing import (
-    initialize_fastapi_app_tracing,
-    setup_tracing,
-)
+from servicelib.fastapi.tracing import configure_fastapi_app_tracing
 from servicelib.tracing import TracingConfig
 
 from .._meta import (
     API_VERSION,
     API_VTAG,
+    APP_FINISHED_BANNER_MSG,
+    APP_STARTED_BANNER_MSG,
     PROJECT_NAME,
     SUMMARY,
 )
 from ..api.routes import setup_api_routes
-from . import events, exceptions_handlers
+from . import exceptions_handlers
 from .settings import ApplicationSettings
 
 _logger = logging.getLogger(__name__)
+
+
+async def _banners_lifespan(_: FastAPI) -> AsyncIterator[State]:
+    print(APP_STARTED_BANNER_MSG, flush=True)  # noqa: T201
+    yield {}
+    print(APP_FINISHED_BANNER_MSG, flush=True)  # noqa: T201
+
+
+def _configure_plugins(
+    app: FastAPI,
+    app_lifespan: LifespanManager[FastAPI],
+    settings: ApplicationSettings,
+    tracing_config: TracingConfig,
+    logging_lifespan: Lifespan | None,
+) -> None:
+    if logging_lifespan:
+        app_lifespan.add(logging_lifespan)
+
+    if settings.INVITATIONS_PROMETHEUS_INSTRUMENTATION_ENABLED:
+        configure_prometheus_instrumentation(app, app_lifespan)
+
+    if tracing_config.tracing_enabled:
+        configure_fastapi_app_tracing(
+            app,
+            app_lifespan,
+            tracing_config=tracing_config,
+        )
+
+    app_lifespan.add(_banners_lifespan)
 
 
 def create_app(
@@ -38,6 +66,8 @@ def create_app(
             json_dumps(settings, indent=2, sort_keys=True),
         )
 
+    app_lifespan = LifespanManager()
+
     app = FastAPI(
         title=f"{PROJECT_NAME} web API",
         description=SUMMARY,
@@ -45,7 +75,7 @@ def create_app(
         openapi_url=f"/api/{API_VTAG}/openapi.json",
         docs_url="/dev/doc",
         redoc_url=None,  # default disabled, see below
-        lifespan=events.create_app_lifespan(settings=settings, logging_lifespan=logging_lifespan),
+        lifespan=app_lifespan,
     )
     override_fastapi_openapi_method(app)
 
@@ -54,17 +84,10 @@ def create_app(
     app.state.tracing_config = tracing_config
     assert app.state.settings.API_VERSION == API_VERSION  # nosec
 
-    if tracing_config.tracing_enabled:
-        setup_tracing(app, tracing_config=tracing_config)
+    _configure_plugins(app, app_lifespan, settings, tracing_config, logging_lifespan)
 
     # PLUGINS SETUP
     setup_api_routes(app)
-
-    if settings.INVITATIONS_PROMETHEUS_INSTRUMENTATION_ENABLED:
-        initialize_prometheus_instrumentation(app)
-
-    if tracing_config.tracing_enabled:
-        initialize_fastapi_app_tracing(app, tracing_config=tracing_config)
 
     # ERROR HANDLERS
     exceptions_handlers.setup(app)
