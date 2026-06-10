e898ccac922cb677cb8e83f5d9df543547a30b82
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-06-10 15:46:45 +0200

    fix

--- services/notifications/src/simcore_service_notifications/clients/smtp.py
+++ services/notifications/src/simcore_service_notifications/clients/smtp.py
@@ -3,27 +3,29 @@ from contextlib import asynccontextmanager
 
 from aiosmtplib import SMTP
 from settings_library.email import EmailProtocol, SMTPSettings
+
+
 @asynccontextmanager
 async def create_session(
     settings: SMTPSettings,
 ) -> AsyncIterator[SMTP]:
     async with SMTP(
-        hostname=settings.SMTP_HOST,
-        port=settings.SMTP_PORT,
+        hostname=settings.host,
+        port=settings.port,
         # FROM https://aiosmtplib.readthedocs.io/en/stable/usage.html#starttls-connections
         # By default, if the server advertises STARTTLS support, aiosmtplib will upgrade the connection automatically.
         # Setting use_tls=True for STARTTLS servers will typically result in a connection error
         # To opt out of STARTTLS on connect, pass start_tls=False.
         # NOTE: for that reason TLS and STARTTLS are mutually exclusive
-        use_tls=settings.SMTP_PROTOCOL == EmailProtocol.TLS,
-        start_tls=settings.SMTP_PROTOCOL == EmailProtocol.STARTTLS,
+        use_tls=settings.protocol == EmailProtocol.TLS,
+        start_tls=settings.protocol == EmailProtocol.STARTTLS,
     ) as smtp:
         if settings.has_credentials:
-            assert settings.SMTP_USERNAME  # nosec
-            assert settings.SMTP_PASSWORD  # nosec
+            assert settings.username  # nosec
+            assert settings.password  # nosec
             await smtp.login(
-                settings.SMTP_USERNAME,
-                settings.SMTP_PASSWORD.get_secret_value(),
+                settings.username,
+                settings.password.get_secret_value(),
             )
 
         yield smtp
