03a32ba579dab39ec2772fb431a9019abaf15fc9
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-06-10 13:14:53 +0200

    fix

--- packages/pytest-simcore/src/pytest_simcore/aioresponses_mocker.py
+++ packages/pytest-simcore/src/pytest_simcore/aioresponses_mocker.py
@@ -1,7 +1,9 @@
 from collections.abc import Iterator
+from unittest.mock import Mock
 
 import pytest
 from aioresponses import aioresponses as AioResponsesMock  # noqa: N812
+from aioresponses.core import RequestMatch
 
 from .helpers.host import get_localhost_ip
 
@@ -13,6 +15,48 @@ PASSTHROUGH_REQUESTS_PREFIXES = [
     "ws://",
     f"http://{get_localhost_ip()}",
 ]
+
+
+def _patch_aioresponses_for_aiohttp_314():
+    """Monkey-patch aioresponses to work with aiohttp>=3.14.0.
+
+    aiohttp 3.14.0 added a required `stream_writer` keyword argument to
+    ClientResponse.__init__(). The aioresponses library constructs
+    ClientResponse objects directly in RequestMatch._build_response without
+    passing this argument, causing TypeError at runtime.
+
+    This patch injects a mock `stream_writer` into the kwargs before
+    ClientResponse is instantiated.
+
+    SEE https://github.com/pnuckowski/aioresponses/issues/290
+    """
+    _original_build_response = RequestMatch._build_response
+
+    def _patched_build_response(self, *args, **kwargs):
+        # Intercept to inject stream_writer into the internal kwargs
+        original_init = self.response_class.__init__ if self.response_class else None
+
+        import aiohttp
+
+        _orig_cr_init = aiohttp.ClientResponse.__init__
+
+        def _init_with_stream_writer(self_resp, *a, **kw):
+            if "stream_writer" not in kw:
+                mock_writer = Mock()
+                mock_writer.output_size = 0
+                kw["stream_writer"] = mock_writer
+            return _orig_cr_init(self_resp, *a, **kw)
+
+        aiohttp.ClientResponse.__init__ = _init_with_stream_writer  # type: ignore[assignment]
+        try:
+            return _original_build_response(self, *args, **kwargs)
+        finally:
+            aiohttp.ClientResponse.__init__ = _orig_cr_init  # type: ignore[assignment]
+
+    RequestMatch._build_response = _patched_build_response  # type: ignore[assignment]
+
+
+_patch_aioresponses_for_aiohttp_314()
 
 
 @pytest.fixture
