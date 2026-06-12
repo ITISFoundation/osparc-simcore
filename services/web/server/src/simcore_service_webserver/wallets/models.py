bb5ce449749df1c1f5c63b801a93acd5beaba1f8
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2026-06-12 12:53:46 +0200

    fixes mport

--- services/web/server/src/simcore_service_webserver/wallets/models.py
+++ services/web/server/src/simcore_service_webserver/wallets/models.py
@@ -2,9 +2,11 @@
 
 from ._groups_api import WalletGroupGet
 from ._groups_db import WalletGroupGetDB
+from ._schemas import WalletsPathParams
 
 __all__: tuple[str, ...] = (
     # models
     "WalletGroupGet",
     "WalletGroupGetDB",
+    "WalletsPathParams",
 )
