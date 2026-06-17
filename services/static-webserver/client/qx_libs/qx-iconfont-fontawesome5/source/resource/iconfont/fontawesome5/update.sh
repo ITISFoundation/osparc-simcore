ebb4569bbb46d9ed45792909a27f633e16dc61d5
odeimaiz <maiz@itis.swiss>
2026-06-17 10:25:08 +0200

    upgrade to 6.7.2

--- services/static-webserver/client/qx_libs/qx-iconfont-fontawesome5/source/resource/iconfont/fontawesome5/update.sh
+++ services/static-webserver/client/qx_libs/qx-iconfont-fontawesome5/source/resource/iconfont/fontawesome5/update.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-git clone --depth=1 https://github.com/FortAwesome/Font-Awesome.git fa-git
+# Updates the bundled Font Awesome Free assets to the version pinned below.
+# Font Awesome 6 ships its webfonts only as .woff2 and .ttf.
+set -e
+FA_VERSION="6.7.2"
+
+git clone --depth=1 --branch "${FA_VERSION}" https://github.com/FortAwesome/Font-Awesome.git fa-git
 cp fa-git/LICENSE.txt .
 cp fa-git/CHANGELOG.md .
-cp fa-git/webfonts/* .
+cp fa-git/webfonts/fa-brands-400.woff2 fa-git/webfonts/fa-brands-400.ttf .
+cp fa-git/webfonts/fa-regular-400.woff2 fa-git/webfonts/fa-regular-400.ttf .
+cp fa-git/webfonts/fa-solid-900.woff2 fa-git/webfonts/fa-solid-900.ttf .
 cp fa-git/metadata/icons.yml .
-rm *.svg
 rm -rf fa-git
 node ./fa-map-convert.js
