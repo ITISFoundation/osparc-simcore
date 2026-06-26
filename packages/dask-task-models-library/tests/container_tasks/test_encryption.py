2a9e6e387790a9f9e5dd00a59b01fe9aaea8d9a6
sanderegg <35365065+sanderegg@users.noreply.github.com>
2026-06-26 15:46:45 +0200

    added model tester, and removed useless test

--- packages/dask-task-models-library/tests/container_tasks/test_encryption.py
+++ packages/dask-task-models-library/tests/container_tasks/test_encryption.py
@@ -8,17 +8,6 @@ from pydantic import ValidationError
 
 
 @pytest.mark.parametrize("model_cls", [JobEncryptionContext, TransferEncryptionSettings])
-def test_encryption_models_examples(model_cls):
-    examples = model_cls.model_json_schema()["examples"]
-
-    for index, example in enumerate(examples):
-        print(f"{index:-^10}:\n", example)
-
-        model_instance = model_cls(**example)
-        assert model_instance
-
-
-@pytest.mark.parametrize("model_cls", [JobEncryptionContext, TransferEncryptionSettings])
 @pytest.mark.parametrize("invalid_length", [KEY_SIZE_BYTES - 1, KEY_SIZE_BYTES + 1])
 def test_root_key_must_have_exact_length(model_cls, invalid_length: int):
     example = model_cls.model_json_schema()["examples"][0]
@@ -26,12 +15,3 @@ def test_root_key_must_have_exact_length(model_cls, invalid_length: int):
 
     with pytest.raises(ValidationError):
         model_cls(**example)
-
-
-@pytest.mark.parametrize("model_cls", [JobEncryptionContext, TransferEncryptionSettings])
-def test_models_are_frozen(model_cls):
-    example = model_cls.model_json_schema()["examples"][0]
-    model_instance = model_cls(**example)
-
-    with pytest.raises(ValidationError):
-        model_instance.root_key = b"\x01" * KEY_SIZE_BYTES
