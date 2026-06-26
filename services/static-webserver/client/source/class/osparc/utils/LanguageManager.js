7b51fc959e3d4651bd17406cf75de5b1ad3ca105
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2026-06-26 16:47:42 +0200

    zh instead of zh_CN

--- services/static-webserver/client/source/class/osparc/utils/LanguageManager.js
+++ services/static-webserver/client/source/class/osparc/utils/LanguageManager.js
@@ -19,7 +19,7 @@ qx.Class.define("osparc.utils.LanguageManager", {
     __localeLabels: {
       "en_US": "English",
       "es_ES": "Español [Spanish]",
-      "zh_CN": "中文 [Chinese]",
+      "zh": "中文 [Chinese]",
     },
 
 
@@ -27,23 +27,23 @@ qx.Class.define("osparc.utils.LanguageManager", {
      * Returns the locales for which translations were compiled (see compile.json).
      * @return {String[]} e.g. ["en_US", "es_ES"]
      */
-    getAvailableLocales: function() {
+    getAvailableLocales: function () {
       return qx.locale.Manager.getInstance().getAvailableLocales();
     },
 
-    getLocaleLabel: function(localeCode) {
+    getLocaleLabel: function (localeCode) {
       return this.__localeLabels[localeCode] || localeCode;
     },
 
-    isSwitchUseful: function() {
+    isSwitchUseful: function () {
       return this.getAvailableLocales().length > 1;
     },
 
-    getStoredLocale: function() {
+    getStoredLocale: function () {
       return osparc.utils.Utils.localCache.getLocalStorageItem(this.LOCALE_KEY);
     },
 
-    setLocale: function(localeCode) {
+    setLocale: function (localeCode) {
       if (!this.getAvailableLocales().includes(localeCode)) {
         return;
       }
@@ -55,7 +55,7 @@ qx.Class.define("osparc.utils.LanguageManager", {
      * Applies the locale stored in localStorage (if any and still available).
      * Meant to be called early during application startup.
      */
-    applyStoredLocale: function() {
+    applyStoredLocale: function () {
       const storedLocale = this.getStoredLocale();
       if (storedLocale && this.getAvailableLocales().includes(storedLocale)) {
         qx.locale.Manager.getInstance().setLocale(storedLocale);
