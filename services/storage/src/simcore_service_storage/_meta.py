c5c3f9ac444e5aa8ad8379d6183bd396d4fd0168
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-06-29 14:20:27 +0200

    fix

--- services/storage/src/simcore_service_storage/_meta.py
+++ services/storage/src/simcore_service_storage/_meta.py
@@ -1,6 +1,6 @@
 from typing import Final
 
-from common_library.basic_types import ServiceMode
+from celery_library.basic_types import BootServerMode
 from models_library.basic_types import VersionStr
 from packaging.version import Version
 from servicelib.utils_meta import PackageInfo
@@ -19,9 +19,9 @@ APP_STARTING_BANNER_MSG: Final[str] = info.get_starting_banner()
 
 
 # https://patorjk.com/software/taag/#p=display&f=Standard&t=Storage
-def get_started_banner(service_mode: ServiceMode) -> str:
-    match service_mode:
-        case ServiceMode.SERVER:
+def get_started_banner(boot_server_mode: BootServerMode) -> str:
+    match boot_server_mode:
+        case BootServerMode.AS_REST:
             return r"""
  ____  _
 / ___|| |_ ___  _ __ __ _  __ _  ___
@@ -31,7 +31,7 @@ ___) | || (_) | | | (_| | (_| |  __/
                         |___/          {}
 
 """.format(f"v{__version__}")
-        case ServiceMode.WORKER:
+        case BootServerMode.AS_CELERY_WORKER:
             return r"""
   ____  _                                __        __         _
  / ___|| |_ ___  _ __ __ _  __ _  ___    \ \      / /__  _ __| | _____ _ __
