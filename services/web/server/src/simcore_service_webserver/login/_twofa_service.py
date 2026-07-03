e7869f29af55ecfc65bbd924d3be46444bec209f
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2026-07-03 11:28:52 +0200

    @GitHK review: cleanup message

--- services/web/server/src/simcore_service_webserver/login/_twofa_service.py
+++ services/web/server/src/simcore_service_webserver/login/_twofa_service.py
@@ -9,7 +9,6 @@ Currently includes two parts:
 
 import asyncio
 import logging
-from typing import Final
 
 import twilio.rest  # type: ignore[import-untyped]
 from aiohttp import web
@@ -94,13 +93,6 @@ class SMSError(RuntimeError):
     pass
 
 
-_SMS_CODE_MESSAGE_FORMAT: Final = user_message(
-    "Dear {first_name}, your verification code is {code}",
-    _hint="Must be VERY SHORT to fit in an SMS",
-    _version=1,
-)
-
-
 @log_decorator(log, level=logging.DEBUG)
 async def send_sms_code(
     app: web.Application,
@@ -126,7 +118,13 @@ async def send_sms_code(
         create_kwargs = {
             "messaging_service_sid": twilio_messaging_sid,
             "to": phone_number,
-            "body": translator.gettext(_SMS_CODE_MESSAGE_FORMAT).format(first_name=first_name, code=code),
+            "body": translator.gettext(
+                user_message(
+                    "Dear {first_name}, your verification code is {code}",
+                    _hint="SMS message of at most 70 characters",
+                    _version=1,
+                )
+            ).format(first_name=first_name[:15], code=code),
         }
         if twilio_auth.is_alphanumeric_supported(phone_number):
             create_kwargs["from_"] = twilio_alpha_numeric_sender
