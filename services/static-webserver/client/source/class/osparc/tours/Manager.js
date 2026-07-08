2d397566b63302a6b218684e64d8a8461c7b8135
odeimaiz <maiz@itis.swiss>
2026-07-08 14:43:30 +0200

    mark them for translation

--- services/static-webserver/client/source/class/osparc/tours/Manager.js
+++ services/static-webserver/client/source/class/osparc/tours/Manager.js
@@ -248,10 +248,10 @@ qx.Class.define("osparc.tours.Manager", {
       }
 
       if (step.title) {
-        stepWidget.setTitle(step.title);
+        stepWidget.setTitle(qx.locale.Manager.tr(step.title));
       }
       if (step.text) {
-        stepWidget.setText(step.text);
+        stepWidget.setText(qx.locale.Manager.tr(step.text));
       }
       if (steps.length > 1) {
         stepWidget.set({
