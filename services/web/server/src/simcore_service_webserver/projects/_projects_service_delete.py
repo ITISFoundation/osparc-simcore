c59938133dfb378021b5705429239516ca2a5764
sanderegg <35365065+sanderegg@users.noreply.github.com>
2026-07-14 16:25:44 +0200

    fix deletion

--- services/web/server/src/simcore_service_webserver/projects/_projects_service_delete.py
+++ services/web/server/src/simcore_service_webserver/projects/_projects_service_delete.py
@@ -1,39 +1,20 @@
 import asyncio
 import logging
-import time
-from contextlib import contextmanager
-from typing import Any, Protocol
+from typing import Protocol
 
 from aiohttp import web
 from models_library.products import ProductName
 from models_library.projects import ProjectID
 from models_library.users import UserID
 from servicelib.common_headers import UNDEFINED_DEFAULT_SIMCORE_USER_AGENT_VALUE
-from servicelib.redis._errors import ProjectLockError
+from servicelib.logging_utils import log_context
 
 from ..director_v2 import director_v2_service
+from ..storage.api import delete_data_folders_of_project
 from . import _projects_repository, _projects_service
 from .exceptions import ProjectDeleteError, ProjectNotFoundError
 
 _logger = logging.getLogger(__name__)
-
-
-@contextmanager
-def _monitor_step(steps: dict[str, Any], *, name: str, elapsed: bool = False):
-    # util
-    start_time = time.perf_counter()
-    steps[name] = {"status": "starting"}
-    try:
-        yield
-    except Exception as exc:
-        steps[name]["status"] = "raised"
-        steps[name]["exception"] = f"{exc.__class__.__name__}:{exc}"
-        raise
-    else:
-        steps[name]["status"] = "success"
-    finally:
-        if elapsed:
-            steps[name]["elapsed"] = time.perf_counter() - start_time
 
 
 class StopServicesCallback(Protocol):
@@ -44,7 +25,7 @@ async def batch_stop_services_in_project(
     app: web.Application, *, user_id: UserID, project_uuid: ProjectID, product_name: ProductName
 ) -> None:
     await asyncio.gather(
-        director_v2_service.stop_pipeline(app, user_id=user_id, project_id=project_uuid),
+        director_v2_service.delete_pipeline(app, user_id=user_id, project_id=project_uuid, force=True),
         _projects_service.remove_project_dynamic_services(
             user_id=user_id,
             project_uuid=project_uuid,
@@ -61,12 +42,9 @@ async def delete_project_as_admin(
     *,
     project_uuid: ProjectID,
     product_name: ProductName,
-):
-    state: dict[str, Any] = {}
-
+) -> None:
     try:
-        # 1. hide
-        with _monitor_step(state, name="hide"):
+        with log_context(_logger, logging.INFO, "hide project"):
             # NOTE: We do not need to use PROJECT_DB_UPDATE_REDIS_LOCK_KEY lock, as hidden field is not passed to frontend
             project = await _projects_repository.patch_project(
                 app,
@@ -74,15 +52,16 @@ async def delete_project_as_admin(
                 new_partial_project_data={"hidden": True},
             )
 
-        # 2. stop
-        with _monitor_step(state, name="stop", elapsed=True):
+        with log_context(_logger, logging.INFO, "stop project"):
             # NOTE: this callback could take long or raise whatever!
             await batch_stop_services_in_project(
                 app, user_id=project.prj_owner, project_uuid=project_uuid, product_name=product_name
             )
 
-        # 3. delete
-        with _monitor_step(state, name="delete"):
+        with log_context(_logger, logging.INFO, "delete project data"):
+            await delete_data_folders_of_project(app, project_uuid, project.prj_owner)
+
+        with log_context(_logger, logging.INFO, "delete project"):
             await _projects_repository.delete_project(app, project_uuid=project_uuid)
 
     except ProjectNotFoundError as err:
@@ -91,17 +70,8 @@ async def delete_project_as_admin(
             project_uuid,
             err,
         )
-
-    except ProjectLockError as err:
-        raise ProjectDeleteError(
-            project_uuid=project_uuid,
-            details=f"Cannot delete project {project_uuid} because it is currently in use. Details: {err}",
-            state=state,
-        ) from err
-
     except Exception as err:
         raise ProjectDeleteError(
             project_uuid=project_uuid,
-            details=f"Unexpected error. Deletion sequence: {state=}",
-            state=state,
+            details=f"Cannot delete project {project_uuid} because of unexpected error. Details: {err}",
         ) from err
