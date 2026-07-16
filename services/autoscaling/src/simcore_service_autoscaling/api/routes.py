504cb80324c6ccedb95b30bec48633f9294c1ab3
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2026-07-16 16:49:04 +0200

    ♻️ refactor(routes): optimize API router setup by caching and adjusting prefix usage

--- services/autoscaling/src/simcore_service_autoscaling/api/routes.py
+++ services/autoscaling/src/simcore_service_autoscaling/api/routes.py
@@ -1,17 +1,20 @@
+from functools import cache
+
 from fastapi import APIRouter, FastAPI
 
 from .._meta import API_VTAG
 from . import health
 
 
+@cache
 def setup_api_routes(app: FastAPI):
     """
     Composes resources/sub-resources routers
     """
-    router = APIRouter()
 
     # include operations in /
     app.include_router(health.router, tags=["operations"])
 
+    router = APIRouter(prefix=f"/{API_VTAG}")
     # include the rest under /vX
-    app.include_router(router, prefix=f"/{API_VTAG}")
+    app.include_router(router)
