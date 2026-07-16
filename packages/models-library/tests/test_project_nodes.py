49b41636e11351ba6b750b17e4b84dfc0408d536
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-07-16 11:05:02 +0200

    fix test

--- packages/models-library/tests/test_project_nodes.py
+++ packages/models-library/tests/test_project_nodes.py
@@ -43,16 +43,13 @@ def test_create_minimal_node_with_new_data_type(
     # found some old data with this aspect
     old_node_data.update(
         {
+            # NOTE: has been removed and is ignored
             "thumbnail": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fregtechassociation.org%2Fwp-content%2Fuploads%2F2018%2F10%2FStandards-stock-image-1400x650.jpg&imgrefurl=https%3A%2F%2Fregtechassociation.org%2Fnews%2Firta-launches-new-open-standard-principles-for-regtech-firms-in-support-of-key-initiatives-for-2018-19%2Fstandards-stock-image-1400x650%2F&tbnid=se_y-TktvwvEMM&vet=12ahUKEwjmsNDs66ruAhWEtqQKHSLRBT8QMygBegUIARCEAQ..i&docid=UiHvpBPeE3G8KM&w=1400&h=650&q=standard%20image&ved=2ahUKEwjmsNDs66ruAhWEtqQKHSLRBT8QMygBegUIARCEAQ",
             "state": {"currentStatus": "STARTED"},
         }
     )
 
     node = Node(**old_node_data)
-    assert (
-        node.thumbnail
-        == "https://www.google.com/imgres?imgurl=https%3A%2F%2Fregtechassociation.org%2Fwp-content%2Fuploads%2F2018%2F10%2FStandards-stock-image-1400x650.jpg&imgrefurl=https%3A%2F%2Fregtechassociation.org%2Fnews%2Firta-launches-new-open-standard-principles-for-regtech-firms-in-support-of-key-initiatives-for-2018-19%2Fstandards-stock-image-1400x650%2F&tbnid=se_y-TktvwvEMM&vet=12ahUKEwjmsNDs66ruAhWEtqQKHSLRBT8QMygBegUIARCEAQ..i&docid=UiHvpBPeE3G8KM&w=1400&h=650&q=standard%20image&ved=2ahUKEwjmsNDs66ruAhWEtqQKHSLRBT8QMygBegUIARCEAQ"
-    )
 
     assert node.state.current_status == RunningState.STARTED
     assert node.state.modified is True
@@ -66,7 +63,6 @@ def test_backwards_compatibility_node_data(minimal_node_data_sample: dict[str, A
 
     node = Node(**old_node_data)
 
-    assert node.thumbnail is None
     assert node.state.current_status == RunningState.FAILED
     assert node.state.modified is True
     assert node.state.dependencies == set()
