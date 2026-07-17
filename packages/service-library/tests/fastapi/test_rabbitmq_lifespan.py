0f89b0827b653b6a9f5f77cdd99b6acf008b351f
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2026-07-17 11:25:45 +0200

    @sanderegg review: linter

--- packages/service-library/tests/fastapi/test_rabbitmq_lifespan.py
+++ packages/service-library/tests/fastapi/test_rabbitmq_lifespan.py
@@ -83,9 +83,7 @@ async def test_configure_rabbitmq_client_publishes_client_in_app_state(
         startup_timeout=None if is_pdb_enabled else 10,
         shutdown_timeout=None if is_pdb_enabled else 10,
     ):
-        mock_rabbitmq_connection.assert_called_once_with(
-            settings.RABBITMQ.dsn  # pylint: disable=no-member
-        )
+        mock_rabbitmq_connection.assert_called_once_with(settings.RABBITMQ.dsn)
         mock_rabbitmq_client_class.assert_called_once_with(
             client_name="test_rabbit_client",
             settings=settings.RABBITMQ,
