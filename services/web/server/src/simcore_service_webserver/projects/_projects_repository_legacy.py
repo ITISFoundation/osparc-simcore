cecd1b2d88f5d314dd38b86d432cf4e3efa66ada
sanderegg <35365065+sanderegg@users.noreply.github.com>
2026-07-21 16:38:53 +0200

    pylint

--- services/web/server/src/simcore_service_webserver/projects/_projects_repository_legacy.py
+++ services/web/server/src/simcore_service_webserver/projects/_projects_repository_legacy.py
@@ -245,6 +245,7 @@ class ProjectDBAPI(BaseProjectDB):
 
         # NOTE: tags are removed in convert_to_db_names so we keep it
         project_tag_ids = TypeAdapter(list[int]).validate_python(project.get("tags", []).copy())
+        trashed_at = project.get("trashed")
         insert_values = convert_to_db_names(project)
         insert_values.update(
             {
@@ -257,7 +258,7 @@ class ProjectDBAPI(BaseProjectDB):
                 "creation_date": now(),
                 "last_change_date": now(),
                 "product_name": product_name,
-                "trashed": arrow.get(trashed_at).datetime if (trashed_at := project.get("trashed")) else None,
+                "trashed": arrow.get(trashed_at).datetime if trashed_at else None,
             }
         )
 
