9ba37e1fc5f995ff8ea26361602e233c2f6e9d69
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-08-10 10:52:43 +0200

    fix test

--- services/storage/tests/unit/test_async_jobs_handlers_paths.py
+++ services/storage/tests/unit/test_async_jobs_handlers_paths.py
@@ -32,6 +32,8 @@ from pytest_simcore.helpers.storage_utils import FileIDDict, ProjectWithFilesPar
 from servicelib.celery.task_manager import TaskManager
 from simcore_service_storage.simcore_s3_dsm import SimcoreS3DataManager
 
+from services.storage.tests.unit.test_async_jobs_handlers_simcore_s3 import submit_delete_paths_task
+
 pytest_simcore_core_services_selection = ["postgres", "rabbit", "redis"]
 pytest_simcore_ops_services_selection = ["adminer"]
 
@@ -101,9 +103,8 @@ async def _assert_delete_paths(
     *,
     paths: set[Path],
 ) -> None:
-    async_job = await submit_job(
+    task_id, _ = await submit_delete_paths_task(
         task_manager,
-        execution_metadata=TaskExecutionMetadata(name="delete_paths"),
         owner_metadata=TestOwnerMetadata(user_id=user_id, product_name=product_name, owner="pytest_client_name"),
         location_id=location_id,
         user_id=user_id,
@@ -113,7 +114,7 @@ async def _assert_delete_paths(
     async for job_composed_result in wait_and_get_job_result(
         task_manager,
         owner_metadata=TestOwnerMetadata(user_id=user_id, product_name=product_name, owner="pytest_client_name"),
-        job_id=async_job.job_id,
+        job_id=task_id,
         stop_after=datetime.timedelta(seconds=120),
     ):
         if job_composed_result.done:
