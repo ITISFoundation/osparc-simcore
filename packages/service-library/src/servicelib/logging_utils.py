db1e1f9a254ba9039224af0ab4a58692793e6bbf
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-08-14 13:21:12 +0200

    fix

--- packages/service-library/src/servicelib/logging_utils.py
+++ packages/service-library/src/servicelib/logging_utils.py
@@ -458,14 +458,11 @@ def _log_before_call(logger_obj: logging.Logger, level: LogLevelInt, func: Calla
     formatted_arguments = ", ".join(args_passed_in_function + kwargs_passed_in_function)
 
     extra_args = {"func_name_override": func.__name__}
-    frame = currentframe()
-    try:
-        if frame is None or frame.f_back is None:
-            logger_obj.warning("Cannot determine log caller filename; filename override will be omitted")
-        else:
-            extra_args["file_name_override"] = Path(frame.f_back.f_code.co_filename).name
-    finally:
-        del frame
+    frame = _get_frame(1)
+    if frame is None:
+        logger_obj.warning("Cannot determine log caller filename; filename override will be omitted")
+    else:
+        extra_args["file_name_override"] = Path(frame.f_code.co_filename).name
 
     #  Before to the function execution, log function details.
     logger_obj.log(
