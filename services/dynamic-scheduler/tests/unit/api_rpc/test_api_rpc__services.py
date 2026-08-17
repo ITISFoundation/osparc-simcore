5c2fd3c3c76f2fdd6c6c23ba040753b8f0c3dd40
Andrei Neagu <neagu@itis.swiss>
2026-08-17 14:16:48 +0200

    solved issue with traceback

--- services/dynamic-scheduler/tests/unit/api_rpc/test_api_rpc__services.py
+++ services/dynamic-scheduler/tests/unit/api_rpc/test_api_rpc__services.py
@@ -514,7 +514,7 @@ async def test_restart_user_services(
 
 
 @pytest.fixture
-def mock_director_v2_service_retrieve_inputs(node_id: NodeID) -> Iterator[None]:
+def mock_director_v2_service_retrieve_inputs(node_id: NodeID, node_id_not_found: NodeID) -> Iterator[None]:
     with respx.mock(
         base_url="http://director-v2:8000/v2",
         assert_all_called=False,
@@ -527,6 +527,9 @@ def mock_director_v2_service_retrieve_inputs(node_id: NodeID) -> Iterator[None]:
             .model_dump_json(),
         )
 
+        # service was not found (e.g. already stopped/removed)
+        mock.post(f"/dynamic_services/{node_id_not_found}:retrieve").respond(status.HTTP_404_NOT_FOUND)
+
         yield None
 
 
@@ -537,6 +540,15 @@ async def test_retrieve_inputs(
 ):
     results = await services.retrieve_inputs(rpc_client, node_id=node_id, port_keys=[], timeout_s=10)
     assert results.model_dump(mode="python") == RetrieveDataOutEnveloped.model_json_schema()["examples"][0]
+
+
+async def test_retrieve_inputs_service_not_found(
+    mock_director_v2_service_retrieve_inputs: None,
+    rpc_client: RabbitMQRPCClient,
+    node_id_not_found: NodeID,
+):
+    with pytest.raises(ServiceWasNotFoundError):
+        await services.retrieve_inputs(rpc_client, node_id=node_id_not_found, port_keys=[], timeout_s=10)
 
 
 @pytest.fixture
