7a9c07a31a4262bb08a8055a6999ded19a94130d
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-08-20 13:43:44 +0200

    fix

--- packages/service-library/tests/rabbitmq/test_rpc_interfaces_director_v2_computations.py
+++ packages/service-library/tests/rabbitmq/test_rpc_interfaces_director_v2_computations.py
@@ -7,11 +7,8 @@ from models_library.api_schemas_directorv2.comp_runs import (
 from models_library.projects import ProjectID
 from pydantic import TypeAdapter, ValidationError
 from pytest_mock import MockerFixture
-from servicelib.rabbitmq import RabbitMQRPCClient, RPCNotInitializedError
+from servicelib.rabbitmq import RabbitMQRPCClient
 from servicelib.rabbitmq.rpc_interfaces.director_v2 import computations
-from servicelib.rabbitmq.rpc_interfaces.director_v2.errors import (
-    ComputationRunStatesRetrievalError,
-)
 
 
 async def test_batch_get_computations_latest_states_enforces_maximum_batch_size(
@@ -37,18 +34,3 @@ async def test_batch_get_computations_latest_states_enforces_maximum_batch_size(
             project_ids=[ProjectID(f"{uuid4()}") for _ in range(max_batch_size + 1)],
         )
     rpc_client.request.assert_not_awaited()
-
-
-@pytest.mark.parametrize("rpc_error", [RPCNotInitializedError(), TimeoutError()])
-async def test_list_computations_latest_states_translates_rpc_errors(
-    mocker: MockerFixture,
-    rpc_error: BaseException,
-):
-    rpc_client = mocker.Mock(spec=RabbitMQRPCClient)
-    rpc_client.request = mocker.AsyncMock(side_effect=rpc_error)
-
-    with pytest.raises(ComputationRunStatesRetrievalError):
-        await computations.batch_get_computations_latest_states(
-            rpc_client,
-            project_ids=[ProjectID("de2578c5-431e-6257-a462-d7bf73b76c0c")],
-        )
