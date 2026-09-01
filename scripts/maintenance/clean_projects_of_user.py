84bcc3b2d7057209bad21c45ea38a842dc7c5e19
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-09-01 10:22:18 +0200

    upgrade

--- scripts/maintenance/clean_projects_of_user.py
+++ scripts/maintenance/clean_projects_of_user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S uv run --script
 # /// script
-# requires-python = ">=3.13"
+# requires-python = ">=3.14"
 # dependencies = [
 #     "httpx",
 #     "pydantic[email]",
--- services/autoscaling/setup.py
+++ services/autoscaling/setup.py
@@ -49,7 +49,7 @@ SETUP = {
     "description": DESCRIPTION,
     "long_description": README,
     "license": "MIT license",
-    "python_requires": "~=3.13",
+    "python_requires": "~=3.14",
     "packages": find_packages(where="src"),
     "package_dir": {
         "": "src",
