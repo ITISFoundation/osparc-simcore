6b716dca4495c7fc8e454cdf27f2577747bf289c
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2026-09-08 21:07:46 +0200

    fix(tests): remove unused noqa directives and correct comments in test cases

--- packages/models-library/tests/test_utils_change_case.py
+++ packages/models-library/tests/test_utils_change_case.py
@@ -14,14 +14,14 @@ from models_library.utils.change_case import (
 @pytest.mark.parametrize(
     "subject,expected",
     [
-        ("snAke_Fun", "snakeFun"),
+        ("snAche_Fun", "snakeFun"),
         ("", ""),
         # WARNING: the algorithm does not check if the subject is already camelcase.
         # It will flatten the output like you can see in these examples.
         ("camelAlready", "camelalready"),
         ("AlmostCamel", "almostcamel"),
         ("_S", "S"),
-        # NOTE : that conversion is not always reversable (non-injective)
+        # NOTE : that conversion is not always reversible (non-injective)
         ("snakes_on_a_plane", "snakesOnAPlane"),
         ("Snakes_On_A_Plane", "snakesOnAPlane"),
         ("snakes_On_a_Plane", "snakesOnAPlane"),
@@ -38,7 +38,7 @@ def test_snake_to_camel(subject, expected):
     "subject,expected",
     [
         ("already_snake", "already_snake"),
-        # NOTE : that conversion is not always reversable (non-injective)
+        # NOTE : that conversion is not always reversible (non-injective)
         ("snakesOnAPlane", "snakes_on_a_plane"),
         ("SnakesOnAPlane", "snakes_on_a_plane"),
         ("IPhoneHysteria", "i_phone_hysteria"),
@@ -52,7 +52,7 @@ def test_camel_to_snake(subject, expected):
 @pytest.mark.parametrize(
     "subject,expected",
     [
-        # NOTE : that conversion is not always reversable (non-injective)
+        # NOTE : that conversion is not always reversible (non-injective)
         ("snakes_on_a_plane", "SnakesOnAPlane"),
         ("Snakes_On_A_Plane", "SnakesOnAPlane"),
         ("snakes_On_a_Plane", "SnakesOnAPlane"),
