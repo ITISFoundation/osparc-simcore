262a97703a36520a20cb0db18a8cad0f48e16aac
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-09-10 23:32:32 +0200

    optimize

--- services/web/server/src/simcore_service_webserver/workspaces/_groups_repository.py
+++ services/web/server/src/simcore_service_webserver/workspaces/_groups_repository.py
@@ -49,21 +49,20 @@ async def create_workspace_group(
 ) -> WorkspaceGroupGetDB:
     row: object | None
     async with transaction_context(get_asyncpg_engine(app), connection) as conn:
-        row = await (
-            await conn.stream(
-                workspaces_access_rights.insert()
-                .values(
-                    workspace_id=workspace_id,
-                    gid=group_id,
-                    read=read,
-                    write=write,
-                    delete=delete,
-                    created=func.now(),
-                    modified=func.now(),
-                )
-                .returning(literal_column("*"))
+        result = await conn.execute(
+            workspaces_access_rights.insert()
+            .values(
+                workspace_id=workspace_id,
+                gid=group_id,
+                read=read,
+                write=write,
+                delete=delete,
+                created=func.now(),
+                modified=func.now(),
             )
-        ).first()
+            .returning(*workspaces_access_rights.c)
+        )
+        row = result.first()
         if row is None:
             raise WorkspaceGroupNotFoundError(workspace_id=workspace_id, group_id=group_id)
         return WorkspaceGroupGetDB.model_validate(row)
