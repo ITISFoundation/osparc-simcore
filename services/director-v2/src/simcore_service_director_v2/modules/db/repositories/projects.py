f2f1d733427aa2bc264a79770cc7f322db0a3e38
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-09-11 15:21:18 +0200

    fix

--- services/director-v2/src/simcore_service_director_v2/modules/db/repositories/projects.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/repositories/projects.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 class ProjectsRepository(BaseRepository):
     async def exists(self, project_id: ProjectID) -> bool:
         async with pass_or_acquire_connection(self.db_engine) as conn:
-            stmt = sa.select(sa.exists(sa.select(sa.literal(1)).where(projects.c.uuid == f"{project_id}")))
+            stmt = sa.select(sa.exists().where(projects.c.uuid == f"{project_id}"))
             result = await conn.execute(stmt)
             return result.scalar_one()
 
