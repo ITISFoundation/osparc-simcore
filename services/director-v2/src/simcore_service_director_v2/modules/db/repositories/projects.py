6b2e08dd76c8b3a379302a35a5e830ac8651bfca
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-09-11 15:13:06 +0200

    fix

--- services/director-v2/src/simcore_service_director_v2/modules/db/repositories/projects.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/repositories/projects.py
@@ -14,14 +14,14 @@ logger = logging.getLogger(__name__)
 class ProjectsRepository(BaseRepository):
     async def exists(self, project_id: ProjectID) -> bool:
         async with pass_or_acquire_connection(self.db_engine) as conn:
-            stmt = sa.select(sa.exists().where(projects.c.uuid == f"{project_id}"))
+            stmt = sa.select(sa.exists(sa.select(sa.literal(1)).where(projects.c.uuid == f"{project_id}")))
             result = await conn.execute(stmt)
             return result.scalar_one()
 
     async def get(self, project_id: ProjectID) -> ProjectAtDB:
         async with pass_or_acquire_connection(self.db_engine) as conn:
-            query = sa.select(projects).where(projects.c.uuid == f"{project_id}")
-            result = await conn.execute(query)
+            stmt = sa.select(projects).where(projects.c.uuid == f"{project_id}")
+            result = await conn.execute(stmt)
             row = result.one_or_none()
             if row is None:
                 raise ProjectNotFoundError(project_id=project_id)
