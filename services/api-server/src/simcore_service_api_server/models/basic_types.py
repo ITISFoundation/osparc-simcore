a903cf9c3751c1247274343adcfe781ef3d30117
Andrei Neagu <neagu@itis.swiss>
2026-09-17 10:37:41 +0200

    removed unused

--- services/api-server/src/simcore_service_api_server/models/basic_types.py
+++ services/api-server/src/simcore_service_api_server/models/basic_types.py
@@ -6,8 +6,6 @@ from pydantic import StringConstraints
 
 VersionStr: TypeAlias = Annotated[str, StringConstraints(strip_whitespace=True, pattern=SIMPLE_VERSION_RE)]  # noqa: UP040
 
-FileNameStr: TypeAlias = Annotated[str, StringConstraints(strip_whitespace=True)]  # noqa: UP040
-
 
 class LogStreamingResponse(StreamingResponse):
     media_type = "application/x-ndjson"
