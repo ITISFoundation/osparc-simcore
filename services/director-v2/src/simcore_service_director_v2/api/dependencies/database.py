81218bcc095387204ae55fca1592b9d3348c8daf
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-09-18 15:23:10 +0200

    fix

--- services/director-v2/src/simcore_service_director_v2/api/dependencies/database.py
+++ services/director-v2/src/simcore_service_director_v2/api/dependencies/database.py
@@ -1,11 +1,5 @@
-import logging
-
 from fastapi.requests import Request
 from sqlalchemy.ext.asyncio import AsyncEngine
-
-_logger = logging.getLogger(__name__)
-
-_POOL_UTILIZATION_WARNING_RATIO = 0.9
 
 
 def get_db_engine(request: Request) -> AsyncEngine:
