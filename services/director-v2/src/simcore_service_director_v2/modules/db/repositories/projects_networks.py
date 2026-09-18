09a15bd90796a2373c677dcc5117476296aa6a6a
Giancarlo Romeo <giancarloromeo@gmail.com>
2026-09-18 08:59:53 +0200

    fix

--- services/director-v2/src/simcore_service_director_v2/modules/db/repositories/projects_networks.py
+++ services/director-v2/src/simcore_service_director_v2/modules/db/repositories/projects_networks.py
@@ -2,7 +2,7 @@ import sqlalchemy as sa
 from common_library.json_serialization import json_loads
 from models_library.projects import ProjectID
 from models_library.projects_networks import NetworksWithAliases, ProjectsNetworks
-from simcore_postgres_database.utils_repos import transaction_context
+from simcore_postgres_database.utils_repos import pass_or_acquire_connection, transaction_context
 from sqlalchemy.dialects.postgresql import insert as pg_insert
 from sqlalchemy.ext.asyncio import AsyncConnection
 
@@ -18,7 +18,7 @@ class ProjectsNetworksRepository(BaseRepository):
         *,
         project_id: ProjectID,
     ) -> ProjectsNetworks:
-        async with transaction_context(self.db_engine, connection) as conn:
+        async with pass_or_acquire_connection(self.db_engine, connection) as conn:
             row = (
                 await conn.execute(
                     sa.select(projects_networks).where(projects_networks.c.project_uuid == f"{project_id}")
@@ -36,7 +36,10 @@ class ProjectsNetworksRepository(BaseRepository):
         networks_with_aliases: NetworksWithAliases,
     ) -> None:
         projects_networks_to_insert = ProjectsNetworks.model_validate(
-            {"project_uuid": project_id, "networks_with_aliases": networks_with_aliases}
+            {
+                "project_uuid": project_id,
+                "networks_with_aliases": networks_with_aliases,
+            }
         )
         row_data = json_loads(projects_networks_to_insert.model_dump_json())
 
