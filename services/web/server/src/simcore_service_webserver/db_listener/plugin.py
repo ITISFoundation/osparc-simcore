266495814174cf3c380db931d67a403c66cfafd5
Sylvain <35365065+sanderegg@users.noreply.github.com>
2026-09-21 08:04:29 +0200

    clean

--- services/web/server/src/simcore_service_webserver/db_listener/plugin.py
+++ services/web/server/src/simcore_service_webserver/db_listener/plugin.py
@@ -25,6 +25,5 @@ _logger = logging.getLogger(__name__)
 def setup_db_listener(app: web.Application):
     setup_socketio(app)
     setup_projects_db(app)
-    # Creates a task to listen to comp_task pg-db's table events
     setup_db(app)
     app.cleanup_ctx.append(create_comp_tasks_listening_task)
